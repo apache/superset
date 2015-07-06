059c02aed05093a8ba7973611a0429443db2256b
Maxime <maxime.beauchemin@airbnb.com>
2015-07-06 15:56:41 +0000

    Checkpoint

--- viz.py
+++ viz.py
@@ -1,73 +1,16 @@
 from pydruid import client
 from pydruid.utils.filters import Dimension, Filter
-from dateutil.parser import parse
-from datetime import datetime, timedelta
-from flask import Flask, render_template, request, flash
-from flask_bootstrap import Bootstrap
-from wtforms import Form, SelectMultipleField, SelectField, TextField
+from datetime import datetime
+from flask import render_template, flash
 import pandas as pd
 from pandas_highcharts.core import serialize
 
-pd.set_option('display.max_colwidth', -1)
-since_l = {
-    '1hour': timedelta(hours=1),
-    '1day': timedelta(days=1),
-    '7days': timedelta(days=7),
-    '28days': timedelta(days=28),
-    'all': timedelta(days=365*100)
+
+CHART_ARGS = {
+    'figsize': (None, 700),
+    'title': None,
+    'render_to': 'chart',
 }
-
-metric = "count"
-
-
-class DruidDataSource(object):
-
-    def __init__(self, name):
-        self.name = name
-        self.cols = self.latest_metadata()
-        self.col_names = sorted([col for col in self.cols.keys()])
-
-    def latest_metadata(self):
-        max_time = query.time_boundary(
-            datasource=self.name)[0]['result']['maxTime']
-        max_time = parse(max_time)
-        intervals = (max_time - timedelta(seconds=1)).isoformat() + '/'
-        intervals += max_time.isoformat()
-        return query.segment_metadata(
-            datasource=self.name,
-            intervals=intervals)[-1]['columns']
-
-def form_factory(datasource, form_args=None):
-    grain = ['all', 'none', 'minute', 'hour', 'day']
-    limits = [0, 5, 10, 25, 50, 100, 500]
-
-    if form_args:
-        limit = form_args.get("limit")
-        try:
-            limit = int(limit)
-            if limit not in limits:
-                limits.append(limit)
-                limits = sorted(limits)
-        except:
-            pass
-
-    class QueryForm(Form):
-        viz_type = SelectField(
-            'Viz', choices=[(k, v.verbose_name) for k, v in viz_types.items()])
-        groupby = SelectMultipleField(
-            'Group by', choices=[(m, m) for m in datasource.col_names])
-        granularity = SelectField(
-            'Granularity', choices=[(g, g) for g in grain])
-        since = SelectField(
-            'Since', choices=[(s, s) for s in since_l.keys()])
-        limit = SelectField(
-            'Limit', choices=[(s, s) for s in limits])
-        flt_col_1 = SelectField(
-            'Filter 1', choices=[(m, m) for m in datasource.col_names])
-        flt_op_1 = SelectField(
-            'Filter 1', choices=[(m, m) for m in ['==', '!=', 'in',]])
-        flt_eq_1 = TextField("Super")
-    return QueryForm
 
 
 class BaseViz(object):
@@ -232,23 +175,3 @@ viz_types = {
     'area': TimeSeriesAreaViz,
     'dist_bar': DistributionBarViz,
 }
-
-
-@app.route("/datasource/<name>/")
-def datasource(name):
-    viz_type = request.args.get("viz_type", "table")
-    datasource = DruidDataSource(name)
-    viz = viz_types[viz_type](
-        datasource,
-        form_class=form_factory(datasource, request.args),
-        form_data=request.args)
-    return viz.render()
-
-
-if __name__ == '__main__':
-    app = Flask(__name__)
-    app.secret_key = "monkeys"
-    Bootstrap(app)
-
-    app.debug = True
-    app.run(host='0.0.0.0', port=PORT)
