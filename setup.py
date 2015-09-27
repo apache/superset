e64367149e54401736cc8f2edf78a0a78e644ff5
Maxime Beauchemin <maximebeauchemin@gmail.com>
2015-09-26 21:39:45 -0700

    v0.4.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '0.3.0'
+version = '0.4.0'
 
 setup(
     name='panoramix',
@@ -17,17 +17,17 @@ setup(
     zip_safe=False,
     scripts=['panoramix/bin/panoramix'],
     install_requires=[
-        'flask-appbuilder>=1.4.5',
+        'flask-appbuilder>=1.4.5, <2.0.0',
         'flask-login==0.2.11',
-        'flask-migrate>=1.5.1',
-        'flask-login==0.2.11',
-        'gunicorn>=19.3.0',
-        'pandas>=0.16.2',
-        'pydruid>=0.2.2',
-        'parsedatetime>=1.5',
-        'python-dateutil>=2.4.2',
-        'requests>=2.7.0',
-        'sqlparse>=0.1.16',
+        'flask-migrate>=1.5.1, <2.0.0',
+        'flask-script>=2.0.5, <3.0.0',
+        'gunicorn>=19.3.0, <20.0.0',
+        'pandas>=0.16.2, <1.0.0',
+        'parsedatetime>=1.5, <2.0.0',
+        'pydruid>=0.2.2, <0.3',
+        'python-dateutil>=2.4.2, <3.0.0',
+        'requests>=2.7.0, <3.0.0',
+        'sqlparse>=0.1.16, <0.2.0',
     ],
     author='Maxime Beauchemin',
     author_email='maximebeauchemin@gmail.com',
