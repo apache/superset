db272f831bbc40bc3ac7746a498e6ea5ab0b214e
Maxime Beauchemin <maximebeauchemin@gmail.com>
2015-09-28 12:46:05 -0700

    Adding markdown dep

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         'flask-script>=2.0.5, <3.0.0',
         'flask-testing>=0.4.2, <0.5.0',
         'gunicorn>=19.3.0, <20.0.0',
+        'markdown>=2.6.2, <3.0.0',
         'pandas>=0.16.2, <1.0.0',
         'parsedatetime>=1.5, <2.0.0',
         'pydruid>=0.2.2, <0.3',
