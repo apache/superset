a4065e4be6552efacd3c3351cfb08d16c231edc7
Maxime Beauchemin <maximebeauchemin@gmail.com>
2015-12-10 17:16:59 -0800

    v0.6.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '0.5.3'
+version = '0.6.0'
 
 setup(
     name='panoramix',
@@ -19,12 +19,13 @@ setup(
     install_requires=[
         'alembic>=0.7.7, <0.8.0',
         'cryptography>=1.1.1, <2.0.0',
-        'flask>=0.10.1, <1.0.0',
         'flask-appbuilder>=1.4.5, <2.0.0',
         'flask-login==0.2.11',
         'flask-migrate>=1.5.1, <2.0.0',
         'flask-script>=2.0.5, <3.0.0',
+        'flask-sqlalchemy==2.0',
         'flask-testing>=0.4.2, <0.5.0',
+        'flask>=0.10.1, <1.0.0',
         'gunicorn>=19.3.0, <20.0.0',
         'markdown>=2.6.2, <3.0.0',
         'numpy>=1.9, <2',
@@ -33,10 +34,10 @@ setup(
         'pydruid>=0.2.2, <0.3',
         'python-dateutil>=2.4.2, <3.0.0',
         'requests>=2.7.0, <3.0.0',
-        'sqlparse>=0.1.16, <0.2.0',
         'sqlalchemy-utils>=0.31.3, <0.32.0',
         'sqlalchemy==1.0.8',
-        'flask-sqlalchemy==2.0',
+        'sqlparse>=0.1.16, <0.2.0',
+        'werkzeug==0.11.2, <0.12.0',
     ],
     author='Maxime Beauchemin',
     author_email='maximebeauchemin@gmail.com',
