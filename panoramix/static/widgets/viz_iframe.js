5f880682c0887c5ef8e34168ad40da377a474cda
Maxime Beauchemin <maximebeauchemin@gmail.com>
2016-01-19 12:58:44 -0800

    Fixing a bug

--- panoramix/static/widgets/viz_iframe.js
+++ panoramix/static/widgets/viz_iframe.js
@@ -4,9 +4,11 @@ px.registerViz('iframe', function(slice) {
       $('#code').attr('rows', '15')
       $.getJSON(slice.jsonEndpoint(), function(payload) {
         slice.container.html(
-          '<iframe style="width:100%; height:100%;"></iframe>');
+          '<iframe style="width:100%;"></iframe>');
         console.log(slice);
-        slice.container.find('iframe').attr('src', payload.form_data.url);
+        iframe = slice.container.find('iframe');
+        iframe.css('height', slice.height());
+        iframe.attr('src', payload.form_data.url);
         slice.done();
       })
       .fail(function(xhr) {
