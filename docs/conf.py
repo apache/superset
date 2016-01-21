2885227050a865cca8c4d72f77d162bce10a3987
Maxime Beauchemin <maximebeauchemin@gmail.com>
2016-01-20 18:08:52 -0800

    Autodocs

--- docs/conf.py
+++ docs/conf.py
@@ -179,7 +179,7 @@ html_static_path = ['_static']
 #html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 #html_show_sphinx = True
