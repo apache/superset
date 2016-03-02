b6a2521722adc8d57c07e43151b2e46435604a40
Maxime Beauchemin <maximebeauchemin@gmail.com>
2016-03-02 10:42:43 -0800

    hotfix - forgot to add standalone.js

---
+++ panoramix/assets/javascripts/standalone.js
@@ -0,0 +1,12 @@
+var $ = window.$ = require('jquery');
+var jQuery = window.jQuery = $;
+var px = require('./modules/panoramix.js');
+
+require('bootstrap')
+
+$(document).ready(function() {
+  var slice;
+  var data = $('.slice').data('slice');
+  slice = px.Slice(data);
+  slice.render();
+});
