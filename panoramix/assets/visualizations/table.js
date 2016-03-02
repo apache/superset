0a8ab6c499a230eafbbad9ed11f2386833f0202c
Maxime Beauchemin <maximebeauchemin@gmail.com>
2016-03-02 13:31:28 -0800

    Shipping with a CSS template out of the box

--- panoramix/assets/visualizations/table.js
+++ panoramix/assets/visualizations/table.js
@@ -1,9 +1,8 @@
 var $ = window.$ = require('jquery');
 var jQuery = window.jQuery = $;
 
-require('datatables');
-// CSS
 require('./table.css');
+require('datatables');
 require('../node_modules/datatables-bootstrap3-plugin/media/css/datatables-bootstrap3.css');
 
 function tableVis(slice) {
