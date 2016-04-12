51f1f66c7ede5b58e332ff520f188dd7e2032fa2
Maxime Beauchemin <maximebeauchemin@gmail.com>
2016-04-11 22:12:27 -0700

    Adding version subcommand to CLI

--- caravel/__init__.py
+++ caravel/__init__.py
@@ -13,6 +13,8 @@ from flask.ext.appbuilder.baseviews import expose
 from flask.ext.cache import Cache
 from flask.ext.migrate import Migrate
 
+VERSION = '0.8.6'
+
 APP_DIR = os.path.dirname(__file__)
 CONFIG_MODULE = os.environ.get('CARAVEL_CONFIG', 'caravel.config')
 
