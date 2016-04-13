0728538a3bf4995d29875d23415debce36bdc097
gbrian <Ramones77>
2016-04-13 07:30:41 +0200

    Cut comment line size

--- caravel/models.py
+++ caravel/models.py
@@ -578,8 +578,8 @@ class SqlaTable(Model, Queryable, AuditMixinNullable):
                 groupby_exprs += [timestamp_grain]
 
             # UGLY: I guess correct way is to delegate on SQLAlchemy dialect
-            # UPDATE: Datetime depends on each dialect and I haven't found an easy way to manage
-            #         Maybe we can allow user to define its custome format at Database definition
+            # UPDATE: Datetime depends on each dialect and I haven't found an
+            #         easy way to manage Maybe we can allow user to define its custome format at Database definition
             def get_dtformat(_type):
                 if _type == 'SMALLDATETIME' or _type == 'DATETIME':
                     return '%Y-%m-%d %H:%M:%S'
