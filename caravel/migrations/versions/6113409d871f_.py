2c33668949b83914c30e46885e2251e01e7519bd
x4base <x4base@gmail.com>
2016-04-28 18:07:41 +0800

    Merge alembic branches in the database

---
+++ caravel/migrations/versions/6113409d871f_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 6113409d871f
+Revises: ('c3a8f8611885', 'b4a0abe21630')
+Create Date: 2016-04-28 17:32:51.688497
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '6113409d871f'
+down_revision = ('c3a8f8611885', 'b4a0abe21630')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
