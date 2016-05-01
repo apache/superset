d7ea47387f13f13e6586cc8d897bcf25dfb45827
Siddharth Gupta <sid88in@gmail.com>
2016-05-01 07:59:08 -0700

    enable timerotateloghandler (configurable) (#311)
    * attempt to enchance logging
    
    * clean up logging
    
    * clean up logging
    
    * reset to logger
    
    * clean up imports
    
    * add comments in config.py
    
    * remove redundant declaration of logging.config. Already exists in caravel/__init__.py
    
    * replace RotatingFileHandler with TimedRotatingFileHandler
    
    * revert back running web server in debug mode
    
    * fix debug in bin/caravel
    
    * resolve build errors - formatting
    
    * need to test
    
    * enable time rotateloghandler
    
    * revert back print statements - add feature for rotatetimelog which is needed and make it configurable
    
    * revert back to default in master
    
    * fix build issues
    
    * remove extra print statement
    
    * change log location to default
    
    * configure console log level and format

--- caravel/config.py
+++ caravel/config.py
@@ -130,6 +130,29 @@ CACHE_CONFIG = {'CACHE_TYPE': 'null'}
 
 VIZ_TYPE_BLACKLIST = []
 
+"""
+1) http://docs.python-guide.org/en/latest/writing/logging/
+2) https://docs.python.org/2/library/logging.config.html
+"""
+
+# Console Log Settings
+
+LOG_FORMAT = '%(asctime)s:%(levelname)s:%(name)s:%(message)s'
+LOG_LEVEL = 'DEBUG'
+
+# ---------------------------------------------------
+# Enable Time Rotate Log Handler
+# ---------------------------------------------------
+# LOG_LEVEL = DEBUG, INFO, WARNING, ERROR, CRITICAL
+
+ENABLE_TIME_ROTATE = False
+TIME_ROTATE_LOG_LEVEL = 'DEBUG'
+FILENAME = '/tmp/caravel.log'
+ROLLOVER = 'midnight'
+INTERVAL = 1
+BACKUP_COUNT = 30
+
+
 try:
     from caravel_config import *  # noqa
 except Exception:
