53d7b243d0858e07c7b418bb21f95ec12630ca39
Junxian Wu <rupert@yahoo-inc.com>
2016-06-02 16:51:31 -0700

    get the most updated migration and add the migration on the head of it

--- caravel/migrations/versions/c9f8bef6f6a0_add_timestamp_format_for_tables.py
+++ caravel/migrations/versions/c9f8bef6f6a0_add_timestamp_format_for_tables.py
@@ -1,14 +1,14 @@
-"""add d timestamp format for tables
+"""Add timestamp format for tables
 
-Revision ID: 422116f431b7
-Revises: 956a063c52b3
-Create Date: 2016-05-20 19:11:57.218062
+Revision ID: c9f8bef6f6a0
+Revises: 1226819ee0e3
+Create Date: 2016-06-02 16:42:24.573924
 
 """
 
 # revision identifiers, used by Alembic.
-revision = '422116f431b7'
-down_revision = '956a063c52b3'
+revision = 'c9f8bef6f6a0'
+down_revision = '1226819ee0e3'
 
 from alembic import op
 import sqlalchemy as sa
@@ -16,6 +16,7 @@ import sqlalchemy as sa
 
 def upgrade():
     op.add_column('tables', sa.Column('timestamp_format', sa.String(length=256), nullable=True))
+
 
 def downgrade():
     op.drop_column('tables', 'timestamp_format')
