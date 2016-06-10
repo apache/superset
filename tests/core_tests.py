2644dd19848f7b0934c5893cbaef2e723cd43cd5
Maxime Beauchemin <maxime.beauchemin@apache.org>
2016-06-09 18:11:24 -0700

    Adding a test for welcome page

--- tests/core_tests.py
+++ tests/core_tests.py
@@ -71,6 +71,11 @@ class CaravelTestCase(unittest.TestCase):
     def logout(self):
         resp = self.client.get('/logout/', follow_redirects=True)
 
+    def test_welcome(self):
+        self.login()
+        resp = self.client.get('/caravel/welcome')
+        assert 'Welcome' in resp.data.decode('utf-8')
+
     def setup_public_access_for_dashboard(self, table_name):
         public_role = appbuilder.sm.find_role('Public')
         perms = db.session.query(ab_models.PermissionView).all()
