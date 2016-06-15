cba68c98bdd0fd3c1313b28d090e88f20d962610
yxjames <zhugeyx@163.com>
2016-06-15 10:48:04 -0700

    add comments

--- caravel/migrations/versions/d8bc074f7aad_add_new_field_is_restricted_to_.py
+++ caravel/migrations/versions/d8bc074f7aad_add_new_field_is_restricted_to_.py
@@ -13,43 +13,24 @@ down_revision = '1226819ee0e3'
 from alembic import op
 import sqlalchemy as sa
 from caravel import db
-from caravel import models
-
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import (
-    Column, Integer, String, ForeignKey, Boolean)
-import traceback
-import logging
+    Column, Integer, Boolean)
 
 Base = declarative_base()
+
 class DruidMetric(Base):
+    """Declarative class used to do query in upgrade"""
     __tablename__ = 'metrics'
     id = Column(Integer, primary_key=True)
-    # metric_name = Column(String(512))
-    # verbose_name = Column(String(1024))
-    # metric_type = Column(String(32))
-    # datasource_name = Column(
-    #     String(250),
-    #     ForeignKey('datasources.datasource_name'))
-    # # Setting enable_typechecks=False disables polymorphic inheritance.
-    # datasource = relationship('DruidDatasource', backref='metrics',
-    #                           enable_typechecks=False)
-    # json = Column(Text)
-    # description = Column(Text)
     is_restricted = Column(Boolean, default=False, nullable=True)
 
 class SqlMetric(Base):
+    """Declarative class used to do query in upgrade"""
     __tablename__ = 'sql_metrics'
     id = Column(Integer, primary_key=True)
-    # metric_name = Column(String(512))
-    # verbose_name = Column(String(1024))
-    # metric_type = Column(String(32))
-    # table_id = Column(Integer, ForeignKey('tables.id'))
-    # table = relationship(
-    #     'SqlaTable', backref='metrics', foreign_keys=[table_id])
-    # expression = Column(Text)
-    # description = Column(Text)
     is_restricted = Column(Boolean, default=False, nullable=True)
+    
 def upgrade():
     op.add_column('metrics', sa.Column('is_restricted', sa.Boolean(), nullable=True))
     op.add_column('sql_metrics', sa.Column('is_restricted', sa.Boolean(), nullable=True))
@@ -57,6 +38,8 @@ def upgrade():
     bind = op.get_bind()
     session = db.Session(bind=bind)
 
+    # don't use models.DruidMetric 
+    # because it assumes the context is consistent with the application
     for obj in session.query(DruidMetric).all():
         obj.is_restricted = False
 
