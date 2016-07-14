b40e33cbbbaefbeac38f460c9c13102b5114a04d
vimxiang <vimxiang@gmail.com>
2016-07-14 11:17:10 +0800

    fix development env  req
    when i install for development, get an error:
    error: Flask-SQLAlchemy 2.1 is installed but Flask-SQLAlchemy==2.0 is required by set(['flask-appbuilder'])

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         'flask-migrate==1.5.1',
         'flask-script==2.0.5',
         'flask-testing==0.5.0',
+        'flask-sqlalchemy==2.0',
         'humanize==0.5.1',
         'gunicorn==19.6.0',
         'markdown==2.6.6',
