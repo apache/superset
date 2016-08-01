87e03471ec404ae5b34ead08500c139e67fcca70
Bogdan Kyryliuk <bogdan.kyryliuk>
2016-08-01 10:32:01 -0700

    Create table as

--- tests/caravel_test_config.py
+++ tests/caravel_test_config.py
@@ -2,16 +2,20 @@ from caravel.config import *
 
 AUTH_USER_REGISTRATION_ROLE = 'alpha'
 SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/caravel_unittests.db'
+# MySQL connection string for unit tests:
+# SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/caravel_db'
 DEBUG = True
 CARAVEL_WEBSERVER_PORT = 8081
 
 # Allowing SQLALCHEMY_DATABASE_URI to be defined as an env var for
 # continuous integration
 if 'CARAVEL__SQLALCHEMY_DATABASE_URI' in os.environ:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('CARAVEL__SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'CARAVEL__SQLALCHEMY_DATABASE_URI')
 
 SQL_CELERY_DB_FILE_PATH = '/tmp/celerydb.sqlite'
 SQL_CELERY_RESULTS_DB_FILE_PATH = '/tmp/celery_results.sqlite'
+SQL_SELECT_AS_CTA = True
 
 
 class CeleryConfig(object):
@@ -21,3 +25,4 @@ class CeleryConfig(object):
     CELERY_ANNOTATIONS = {'tasks.add': {'rate_limit': '10/s'}}
     CONCURRENCY = 1
 CELERY_CONFIG = CeleryConfig
+
