d72dd63d08ee3cae7697a956a891b350942607a8
Maxime Beauchemin <maximebeauchemin@gmail.com>
2016-08-09 15:33:32 -0700

    [bugfix] nvd3's API changed and broke the range filter

--- caravel/assets/visualizations/nvd3_vis.js
+++ caravel/assets/visualizations/nvd3_vis.js
@@ -48,10 +48,12 @@ function nvd3Vis(slice) {
           case 'line':
             if (fd.show_brush) {
               chart = nv.models.lineWithFocusChart();
-              chart.lines2.xScale(d3.time.scale.utc());
               chart.x2Axis
               .showMaxMin(fd.x_axis_showminmax)
               .staggerLabels(false);
+              if (chart.focus) {
+                chart.focus.xScale(d3.time.scale.utc());
+              }
             } else {
               chart = nv.models.lineChart();
             }
