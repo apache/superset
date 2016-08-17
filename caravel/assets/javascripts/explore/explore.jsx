379cf6cbd9527617b6aae60501ad2fa4e252bb8f
Alanna Scott <alanna.scott@airbnb.com>
2016-08-17 08:05:47 -0700

    [ui] tweaks and improvements (#965)
    * update panel headings, titles
    
    * remove panel heading collapser
    
    * style slice meta controls
    
    * fix favstar on dashboard
    
    * add space between heat map and list

--- caravel/assets/javascripts/explore/explore.jsx
+++ caravel/assets/javascripts/explore/explore.jsx
@@ -18,12 +18,9 @@ require('bootstrap');
 
 require('./../caravel-select2.js');
 
-require('../../node_modules/bootstrap-toggle/js/bootstrap-toggle.min.js');
-
 // css
 require('../../vendor/pygments.css');
 require('../../stylesheets/explore.css');
-require('../../node_modules/bootstrap-toggle/css/bootstrap-toggle.min.css');
 
 let slice;
 
@@ -130,11 +127,6 @@ function initExploreView() {
   }
 
   px.initFavStars();
-
-  $('form .panel-heading').click(function () {
-    toggle_fieldset($(this), true);
-    $(this).css('cursor', 'pointer');
-  });
 
   function copyURLToClipboard(url) {
     var textArea = document.createElement('textarea');
