bb87f0a00721567c03c53e8a6e14c0a4613bdc07
Maxime Beauchemin <maximebeauchemin@gmail.com>
2016-08-18 11:25:44 -0700

    [bugfix] dashboard list doesn't populate in explore->saveas

--- caravel/views.py
+++ caravel/views.py
@@ -728,7 +728,7 @@ appbuilder.add_view(
 
 
 class DashboardModelViewAsync(DashboardModelView):  # noqa
-    list_columns = ['dashboard_link', 'creator', 'modified']
+    list_columns = ['dashboard_link', 'creator', 'modified', 'dashboard_title']
     label_columns = {
         'dashboard_link': 'Dashboard',
     }
