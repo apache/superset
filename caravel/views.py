6315dcad56396a4d6e1589f5319a45bbb1975a7a
Chris Williams <chris.williams@airbnb.com>
2016-08-25 16:03:02 -0700

    format properly #eyeroll

--- caravel/views.py
+++ caravel/views.py
@@ -1181,7 +1181,7 @@ class Caravel(BaseCaravelView):
         if slc:
             url = '{slc.slice_url}&standalone={standalone}'.format(
                 slc=slc, standalone=request.args.get('standalone', 'false'))
-            return redirect('https://caravel.d.musta.ch{url}'.format(url))
+            return redirect('https://caravel.d.musta.ch{url}'.format(url=url))
         else:
             flash("The specified slice could not be found", "danger")
             return redirect('/slicemodelview/list/')
