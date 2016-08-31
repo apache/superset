77da8a196ae347079596c8857de3415e05d3c395
Bogdan Kyryliuk <bogdan.kyryliuk>
2016-08-30 20:38:38 -0700

    Set up istanbul with codeclimate and travis.

--- caravel/assets/js_build.sh
+++ caravel/assets/js_build.sh
@@ -2,7 +2,10 @@
 set -e
 cd "$(dirname "$0")"
 npm --version
+node --version
 npm install
 npm run lint
 npm run test
 npm run prod
+npm run cover
+./node_modules/.bin/codeclimate-test-reporter < ./coverage/lcov.info
