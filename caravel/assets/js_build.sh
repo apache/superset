20cd2ebb61198c0e219f51bf581ef4730f38bc15
Bogdan Kyryliuk <bogdan.kyryliuk>
2016-08-30 21:54:38 -0700

    Put the codeclimate tocken inside .travis.yml

--- caravel/assets/js_build.sh
+++ caravel/assets/js_build.sh
@@ -8,4 +8,4 @@ npm run lint
 npm run test
 npm run prod
 npm run cover
-./node_modules/.bin/codeclimate-test-reporter < ./coverage/lcov.info
+CODECLIMATE_REPO_TOKEN=5f3a06c425eef7be4b43627d7d07a3e46c45bdc07155217825ff7c49cb6a470c ./node_modules/.bin/codeclimate-test-reporter < ./coverage/lcov.info
