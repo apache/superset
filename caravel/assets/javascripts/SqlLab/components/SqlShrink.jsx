d11a37f975807f94959f231b9a4669d2fbb236b8
Maxime Beauchemin <maximebeauchemin@gmail.com>
2016-09-01 13:43:41 -0700

    Adding a ShrinkSql component

---
+++ caravel/assets/javascripts/SqlLab/components/SqlShrink.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { github } from 'react-syntax-highlighter/dist/styles';
+
+const SqlShrink = (props) => {
+  let lines = props.sql.split('\n');
+  if (lines.length >= props.maxLines) {
+    lines = lines.slice(0, props.maxLines);
+    lines.push('{...}');
+  }
+  const shrunk = lines.map((line) => {
+    if (line.length > props.maxWidth) {
+      return line.slice(0, props.maxWidth) + '{...}';
+    }
+    return line;
+  })
+  .join('\n');
+  return (
+    <div>
+      <SyntaxHighlighter language="sql" style={github}>
+        {shrunk}
+      </SyntaxHighlighter>
+    </div>
+  );
+};
+
+SqlShrink.defaultProps = {
+  maxWidth: 60,
+  maxLines: 6,
+};
+
+SqlShrink.propTypes = {
+  sql: React.PropTypes.string,
+  maxWidth: React.PropTypes.integer,
+  maxLines: React.PropTypes.integer,
+};
+
+export default SqlShrink;
