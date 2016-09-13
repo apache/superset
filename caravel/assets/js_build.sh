b198d5d0a2823329e2886fd716297a65f9ba0e36
Maxime Beauchemin <maximebeauchemin@gmail.com>
2016-09-12 20:21:45 -0700

    Adding dummy coveralls call in JS to trigger webhook

--- caravel/assets/js_build.sh
+++ caravel/assets/js_build.sh
@@ -9,3 +9,4 @@ npm run test
 npm run prod
 npm run cover
 CODECLIMATE_REPO_TOKEN=5f3a06c425eef7be4b43627d7d07a3e46c45bdc07155217825ff7c49cb6a470c ./node_modules/.bin/codeclimate-test-reporter < ./coverage/lcov.info
+coveralls
