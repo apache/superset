e7bcf0401f75ae53acd74a9e4ca028b8356fa107
alanna scott <alanna.scott@airbnb.com>
2016-09-13 00:13:17 -0700

    create structure for new forked explore view

---
+++ caravel/assets/javascripts/explorev2/components/ControlPanelsContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Panel } from 'react-bootstrap';
+
+export default class ChartContainer extends React.Component {
+  render() {
+    return (
+      <Panel>
+        control panels here
+      </Panel>
+    );
+  }
+}
---
+++ caravel/assets/javascripts/explorev2/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExploreViewContainer from './components/ExploreViewContainer';
+
+const exploreViewContainer = document.getElementById('js-explore-view-container');
+ReactDOM.render(
+  <ExploreViewContainer />,
+  exploreViewContainer
+);
