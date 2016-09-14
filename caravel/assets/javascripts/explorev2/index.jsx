c828a54027bed01abe4c356c6e06814e4c164cbc
alanna scott <alanna.scott@airbnb.com>
2016-09-13 18:00:03 -0700

    add bootstrap data pattern

--- caravel/assets/javascripts/explorev2/index.jsx
+++ caravel/assets/javascripts/explorev2/index.jsx
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import ExploreViewContainer from './components/ExploreViewContainer';
 
 const exploreViewContainer = document.getElementById('js-explore-view-container');
+const bootstrapData = exploreViewContainer.getAttribute('data-bootstrap');
+console.log('bootstrapData', bootstrapData)
 ReactDOM.render(
-  <ExploreViewContainer />,
+  <ExploreViewContainer
+    data={bootstrapData}
+  />,
   exploreViewContainer
 );
