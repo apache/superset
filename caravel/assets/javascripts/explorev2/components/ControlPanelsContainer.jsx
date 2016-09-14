2a5a3ede1260110a2d98675df22e6bca513f7fd5
Alanna Scott <alanna.scott@airbnb.com>
2016-09-13 19:16:11 -0700

    create structure for new forked explore view (#1099)
    * create structure for new forked explore view
    
    * update component name
    
    * add bootstrap data pattern
    
    * remove console.log

---
+++ caravel/assets/javascripts/explorev2/components/ControlPanelsContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Panel } from 'react-bootstrap';
+
+export default class ControlPanelsContainer extends React.Component {
+  render() {
+    return (
+      <Panel>
+        control panels here
+      </Panel>
+    );
+  }
+}
