c025c44a7c4c1e418cc6d537a983b7cc61b3df47
alanna scott <alanna.scott@airbnb.com>
2016-09-13 18:02:11 -0700

    remove console.log

--- caravel/assets/javascripts/explorev2/index.jsx
+++ caravel/assets/javascripts/explorev2/index.jsx
@@ -4,7 +4,7 @@ import ExploreViewContainer from './components/ExploreViewContainer';
 
 const exploreViewContainer = document.getElementById('js-explore-view-container');
 const bootstrapData = exploreViewContainer.getAttribute('data-bootstrap');
-console.log('bootstrapData', bootstrapData)
+
 ReactDOM.render(
   <ExploreViewContainer
     data={bootstrapData}
