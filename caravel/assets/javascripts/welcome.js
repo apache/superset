190e0d5eda96bed393d3bc7d62704f9a29b94391
Maxime Beauchemin <maximebeauchemin@gmail.com>
2016-09-16 08:32:59 -0700

    [welcome] removing calendar heatmap from welcome page (#1119)

--- caravel/assets/javascripts/welcome.js
+++ caravel/assets/javascripts/welcome.js
@@ -5,9 +5,7 @@ require('../stylesheets/welcome.css');
 require('bootstrap');
 require('datatables.net-bs');
 require('../node_modules/datatables-bootstrap3-plugin/media/css/datatables-bootstrap3.css');
-require('../node_modules/cal-heatmap/cal-heatmap.css');
 const d3 = require('d3');
-const CalHeatMap = require('cal-heatmap');
 function modelViewTable(selector, modelView, orderCol, order) {
   // Builds a dataTable from a flask appbuilder api endpoint
   let url = '/' + modelView.toLowerCase() + '/api/read';
@@ -51,32 +49,5 @@ function modelViewTable(selector, modelView, orderCol, order) {
   });
 }
 $(document).ready(function () {
-  d3.json('/caravel/activity_per_day', function (json) {
-    const ext = d3.extent(d3.values(json));
-    const cal = new CalHeatMap();
-    const range = 10;
-    const legendBounds = [];
-    const step = (ext[1] - ext[0]) / (range - 1);
-    for (let i = 0; i < range; i++) {
-      legendBounds.push(i * step + ext[0]);
-    }
-    cal.init({
-      start: new Date().setFullYear(new Date().getFullYear() - 1),
-      range: 13,
-      data: json,
-      legend: legendBounds,
-      legendColors: [
-        '#D6E685',
-        '#1E6823',
-      ],
-      domain: 'month',
-      subDomain: 'day',
-      itemName: 'action',
-      tooltip: true,
-      cellSize: 10,
-      cellPadding: 2,
-      domainGutter: 22,
-    });
-  });
   modelViewTable('#dash_table', 'DashboardModelViewAsync', 'changed_on', 'desc');
 });
