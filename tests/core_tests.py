ca66ba489399a3c37e39875f6d1f6c278cfcb9be
Dennis O'Brien <dennis@dennisobrien.net>
2016-09-19 15:14:00 -0700

    Fix initialization of Database sqlalchemy_uri and password (#1137)
    * move initialization of Database sqlalchemy_uri and password from DatabaseView.pre_add to utils.get_or_create_main_db.
    Unit tests for mysql and postgres include username and password in the SQLALCHEMY_DATABASE_URI.
    
    * modified test_testconn to work with sqlalchemy uri with a username and password.

--- tests/core_tests.py
+++ tests/core_tests.py
@@ -115,16 +115,29 @@ class CoreTests(CaravelTestCase):
         assert self.client.get('/ping').data.decode('utf-8') == "OK"
 
     def test_testconn(self):
-        data = json.dumps({'uri': 'sqlite:////tmp/caravel_unittests.db'})
-        response = self.client.post('/caravel/testconn', data=data, content_type='application/json')
-        assert response.status_code == 200
+        database = (
+            db.session
+            .query(models.Database)
+            .filter_by(database_name='main')
+            .first()
+        )
 
+        # validate that the endpoint works with the password-masked sqlalchemy uri
         data = json.dumps({
-            'uri': 'postgresql+psycopg2://foo:XXXXXXXXXX@127.0.0.1/bar',
+            'uri': database.safe_sqlalchemy_uri(),
             'name': 'main'
         })
         response = self.client.post('/caravel/testconn', data=data, content_type='application/json')
         assert response.status_code == 200
+
+        # validate that the endpoint works with the decrypted sqlalchemy uri
+        data = json.dumps({
+            'uri': database.sqlalchemy_uri_decrypted,
+            'name': 'main'
+        })
+        response = self.client.post('/caravel/testconn', data=data, content_type='application/json')
+        assert response.status_code == 200
+
 
     def test_warm_up_cache(self):
         slice = db.session.query(models.Slice).first()
