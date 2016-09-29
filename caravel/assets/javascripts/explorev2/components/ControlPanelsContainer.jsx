e29bca9734fe90016aea646c6f4e9442d5be2599
alanna scott <alanna.scott@airbnb.com>
2016-09-28 23:39:56 -0700

    remove dummy control panels

--- caravel/assets/javascripts/explorev2/components/ControlPanelsContainer.jsx
+++ caravel/assets/javascripts/explorev2/components/ControlPanelsContainer.jsx
@@ -5,16 +5,7 @@ import ControlPanel from './ControlPanel';
 const ControlPanelsContainer = function () {
   return (
     <Panel>
-      <ControlPanel name="panel 1" />
-      <ControlPanel name="panel 2" />
-      <ControlPanel name="panel 3" />
-      <ControlPanel name="panel 4" />
-      <ControlPanel name="panel 5" />
-      <ControlPanel name="panel 6" />
-      <ControlPanel name="panel 7" />
-      <ControlPanel name="panel 8" />
-      <ControlPanel name="panel 9" />
-      <ControlPanel name="panel 10" />
+      control panels here
     </Panel>
   );
 };
