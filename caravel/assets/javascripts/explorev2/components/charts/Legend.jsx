142ec07f6c3179dbc8792f45a8ecea42167a39a0
alanna scott <alanna.scott@airbnb.com>
2016-10-05 12:10:29 -0700

    break labelItem into own SFC, make legend SFC

--- caravel/assets/javascripts/explorev2/components/charts/Legend.jsx
+++ caravel/assets/javascripts/explorev2/components/charts/Legend.jsx
@@ -1,31 +1,21 @@
 import React, { PropTypes } from 'react';
+import LegendItem from './LegendItem';
 
 const propTypes = {
   data: PropTypes.array.isRequired,
   keysToColorsMap: PropTypes.object.isRequired,
 };
 
-export default class Legend extends React.Component {
-  legendItem(key) {
-    return (
-      <li style={{ float: 'left' }} key={key}>
-        <i
-          className="fa fa-circle"
-          style={{ color: this.props.keysToColorsMap[key] }}
-        /> &nbsp;&nbsp;
-        <span>{key}</span>
-      </li>
-    );
-  }
-
-  render() {
-    const legendEls = this.props.data.map((d) => this.legendItem(d.key));
-    return (
-      <ul className="list-unstyled list-inline">
-        {legendEls}
-      </ul>
-    );
-  }
+export default function Legend({ data, keysToColorsMap }) {
+  const legendEls = data.map((d) => {
+    const color = keysToColorsMap[d.key] ? keysToColorsMap[d.key] : '#000';
+    return <LegendItem label={d.key} color={color} key={d.key} />;
+  });
+  return (
+    <ul className="list-unstyled list-inline">
+      {legendEls}
+    </ul>
+  );
 }
 
 Legend.propTypes = propTypes;
