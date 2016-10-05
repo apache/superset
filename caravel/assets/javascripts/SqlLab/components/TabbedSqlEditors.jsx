421a86ade5ac5ec4d147bdfb913ce32a7004b455
vera-liu <vera.liu@airbnb.com>
2016-10-05 11:43:05 -0700

    Some polish on query search (#1222)
    * Some polish
    - Changed query search icon
    - CopyToClipboard in action bar
    
    * Added dbId as linked-button, made modifications based on comments
    
    * Fix duplicated import (linting)

--- caravel/assets/javascripts/SqlLab/components/TabbedSqlEditors.jsx
+++ caravel/assets/javascripts/SqlLab/components/TabbedSqlEditors.jsx
@@ -5,7 +5,7 @@ import { bindActionCreators } from 'redux';
 import * as Actions from '../actions';
 import SqlEditor from './SqlEditor';
 import shortid from 'shortid';
-import { getParamFromQuery } from '../../../utils/common';
+import { getParamFromQuery, getLink } from '../../../utils/common';
 import CopyQueryTabUrl from './CopyQueryTabUrl';
 
 let queryCount = 1;
@@ -47,10 +47,7 @@ class TabbedSqlEditors extends React.Component {
     if (qe.autorun) params.push('autorun=' + qe.autorun);
     if (qe.sql) params.push('sql=' + qe.sql);
 
-    const queryString = params.join('&');
-    const queryLink = this.state.cleanUri + '?' + queryString;
-
-    return queryLink;
+    return getLink(this.state.cleanUri, params);
   }
   renameTab(qe) {
     /* eslint no-alert: 0 */
