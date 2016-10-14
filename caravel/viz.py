472bdf76a867815c7acacde16bfde4e645b604c5
Maxime Beauchemin <maximebeauchemin@gmail.com>
2016-10-14 13:48:49 -0700

    Specify the metric to order by for Series Limit

--- caravel/viz.py
+++ caravel/viz.py
@@ -240,6 +240,7 @@ class BaseViz(object):
             form_data.get("granularity") or form_data.get("granularity_sqla")
         )
         limit = int(form_data.get("limit", 0))
+        timeseries_limit_metric = form_data.get("timeseries_limit_metric")
         row_limit = int(
             form_data.get("row_limit", config.get("ROW_LIMIT")))
         since = (
@@ -275,6 +276,7 @@ class BaseViz(object):
             'filter': self.query_filters(),
             'timeseries_limit': limit,
             'extras': extras,
+            'timeseries_limit_metric': timeseries_limit_metric,
         }
         return d
 
@@ -999,7 +1001,8 @@ class NVD3TimeSeriesViz(NVD3Viz):
         'label': None,
         'fields': (
             'metrics',
-            'groupby', 'limit',
+            'groupby',
+            ('limit', 'timeseries_limit_metric'),
         ),
     }, {
         'label': _('Chart Options'),
--- run_specific_test.sh
+++ run_specific_test.sh
@@ -5,4 +5,4 @@ export CARAVEL_CONFIG=tests.caravel_test_config
 set -e
 caravel/bin/caravel version -v
 export SOLO_TEST=1
-nosetests tests.core_tests:CoreTests.test_slices
+nosetests tests.core_tests:CoreTests
