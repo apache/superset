6ccc2b4db0cbf4488ea23341767c5e97f7f87f80
Nick Barnwell <nb@ul.io>
2016-10-14 17:37:16 -0700

    Fix CodeClimate feedback

--- caravel/assets/spec/javascripts/sqllab/reducers_spec.js
+++ caravel/assets/spec/javascripts/sqllab/reducers_spec.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import * as r from '../../../javascripts/SqlLab/reducers'
+import * as r from '../../../javascripts/SqlLab/reducers';
 import * as actions from '../../../javascripts/SqlLab/actions';
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
 
 describe('sqlLabReducer', () => {
   describe('CLONE_QUERY_TO_NEW_TAB', () => {
-    const testQuery = { sql: "SELECT * FROM...", dbId: 1, id: 1 };
-    const state = Object.assign(r.initialState, { queries: [ testQuery ] });
-    const newState = r.sqlLabReducer(state, actions.cloneQueryToNewTab(testQuery))
+    const testQuery = { sql: 'SELECT * FROM...', dbId: 1, id: 1 };
+    const state = Object.assign(r.initialState, { queries: [testQuery] });
+    const newState = r.sqlLabReducer(state, actions.cloneQueryToNewTab(testQuery));
 
     it('should have at most one more tab', () => {
       expect(newState.queryEditors).have.length(2);
-      //expect(newState.queryEditors.filter((qe) => qe.sql == testQuery.SQL).length).to.equal(2)
     });
   
     it('should have the same SQL as the cloned query', () => {
@@ -20,7 +18,7 @@ describe('sqlLabReducer', () => {
     });
 
     it('should prefix the new tab title with "Copy of"', () => {
-      expect(newState.queryEditors[1].title).to.include("Copy of");
+      expect(newState.queryEditors[1].title).to.include('Copy of');
     });
 
     it('should push the cloned tab onto tab history stack', () => {
