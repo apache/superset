3cb737f8c890b018bd6b3275047a02f79abb7444
Maxime Beauchemin <maximebeauchemin@gmail.com>
2016-10-15 08:11:37 -0700

    [hotfix] use  instead of prod for Travis build, take2

--- caravel/assets/js_build.sh
+++ caravel/assets/js_build.sh
@@ -6,6 +6,6 @@ node --version
 npm install
 npm run lint
 npm run test
-npm run dev  # `npm run dev` was timing out on travis
+npm run dev
 npm run cover
 CODECLIMATE_REPO_TOKEN=5f3a06c425eef7be4b43627d7d07a3e46c45bdc07155217825ff7c49cb6a470c ./node_modules/.bin/codeclimate-test-reporter < ./coverage/lcov.info
