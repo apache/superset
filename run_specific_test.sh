944f1dbf41497c10be5f43a4a745563598069ecd
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2016-10-28 09:24:48 +0200

    run_specific_test: take the test as parameter
    Instead of hardcoding it, e.g.:
    ./run_specific_test.sh tests.core_tests:CoreTests.test_templated_sql_json

--- run_specific_test.sh
+++ run_specific_test.sh
@@ -5,4 +5,5 @@ export CARAVEL_CONFIG=tests.caravel_test_config
 set -e
 caravel/bin/caravel version -v
 export SOLO_TEST=1
-nosetests tests.core_tests:CoreTests.test_templated_sql_json
+# e.g. tests.core_tests:CoreTests.test_templated_sql_json
+nosetests $1
