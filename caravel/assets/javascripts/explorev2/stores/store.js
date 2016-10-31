ab3a7f880939b93f62daa46f8c2b8987f8e63ce5
alanna scott <alanna.scott@airbnb.com>
2016-10-31 11:30:41 -0700

    add the rest of the control panel sections and field overrides

--- caravel/assets/javascripts/explorev2/stores/store.js
+++ caravel/assets/javascripts/explorev2/stores/store.js
@@ -131,6 +131,29 @@ export const commonControlPanelSections = {
       ['metrics', 'groupby']
     ],
   },
+  NVD3TimeSeries: [
+    {
+      label: null,
+      fieldsSetRows: [
+        ['metrics'],
+        ['groupby'],
+        ['limit', 'timeseries_limit_metric'],
+      ],
+    },
+    {
+      label: 'Advanced Analytics'),
+      description: 'This section contains options ' +
+                   'that allow for advanced analytical post processing ' +
+                    'of query results',
+      fieldsSetRows: [
+          ['rolling_type', 'rolling_periods'],
+          ['time_compare'],
+          ['num_period_compare', 'period_ratio_type'],
+          ['resample_how', 'resample_rule'],
+          ['resample_fillmethod'],
+      ],
+    },
+  ],
 };
 
 // todo: add fieldSetRows and fieldSetOverrides from viz.py for the rest of the viz types
@@ -163,117 +186,591 @@ export const visTypes = {
 
   pie: {
     label: 'Pie Chart',
-    controlPanelSections: [],
-    fieldSetOverrides: {},
+    controlPanelSections: [
+      {
+        label: null,
+        fields: [
+          ['metrics', 'groupby'],
+          ['limit'],
+          ['pie_label_type'],
+          ['donut', 'show_legend'],
+          ['labels_outside'],
+        ],
+      },
+    ],
   },
 
   line: {
     label: 'Time Series - Line Chart',
     requiresTime: true,
+    controlPanelSections: [
+      commonControlPanelSections[NVD3TimeSeries][0],
+      {
+        label: 'Chart Options',
+        fieldsSetRows: [
+          ['show_brush', 'show_legend'],
+          ['rich_tooltip', 'y_axis_zero'],
+          ['y_log_scale', 'contribution'],
+          ['show_markers', 'x_axis_showminmax'],
+          ['line_interpolation'],
+          ['x_axis_format', 'y_axis_format'],
+          ['x_axis_label', 'y_axis_label'],
+        ],
+      },
+      commonControlPanelSections[NVD3TimeSeries][1],
+    ],
   },
 
   bar: {
     label: 'Time Series - Bar Chart',
-    requiresTime: true
+    requiresTime: true,
+    controlPanelSections: [
+      commonControlPanelSections[NVD3TimeSeries][0],
+      {
+        label: 'Chart Options',
+        fieldsSetRows: [
+          ['show_brush', 'show_legend', 'show_bar_value'],
+          ['rich_tooltip', 'y_axis_zero'],
+          ['y_log_scale', 'contribution'],
+          ['x_axis_format', 'y_axis_format'],
+          ['line_interpolation', 'bar_stacked'],
+          ['x_axis_showminmax', 'bottom_margin'],
+          ['x_axis_label', 'y_axis_label'],
+          ['reduce_x_ticks', 'show_controls'],
+        ],
+      },
+      commonControlPanelSections[NVD3TimeSeries][1],
+    ],
   },
 
   compare: {
     label: 'Time Series - Percent Change',
-    requiresTime: true
+    requiresTime: true,
+    controlPanelSections: [
+      commonControlPanelSections[NVD3TimeSeries][0],
+      commonControlPanelSections[NVD3TimeSeries][1],
+    ],
   },
 
   area: {
     label: 'Time Series - Stacked',
-    requiresTime: true
+    requiresTime: true,
+    controlPanelSections: [
+      commonControlPanelSections[NVD3TimeSeries][0],
+      {
+        label: 'Chart Options',
+        fieldsSetRows: [
+          ['show_brush', 'show_legend'],
+          ['rich_tooltip', 'y_axis_zero'],
+          ['y_log_scale', 'contribution'],
+          ['x_axis_format', 'y_axis_format'],
+          ['x_axis_showminmax', 'show_controls'],
+          ['line_interpolation', 'stacked_style'],
+        ],
+      },
+      commonControlPanelSections[NVD3TimeSeries][1],
+    ],
   },
 
   table: {
     label: 'Table View',
+    controlPanelSections: [
+      {
+        label:'GROUP BY',
+        description: 'Use this section if you want a query that aggregates',
+        fieldSetRows: [
+          ['groupby', 'metrics'],
+        ],
+      },
+      {
+        label: 'NOT GROUPED BY',
+        description: 'Use this section if you want to query atomic rows'),
+        fieldSetRows: [
+          ['all_columns', 'order_by_cols'],
+        ],
+      },
+      {
+        label: 'Options',
+        fieldsSetRows: [
+          ['table_timestamp_format'],
+          ['row_limit'],
+          ['include_search'],
+        ],
+      }
+    ],
   },
 
   markup: {
     label: 'Markup',
+    controlPanelSections: [
+      {
+        label: null,
+        fieldsSetRows: [
+          ['markup_type', 'code']
+        ],
+      },
+    ]
   },
 
   pivot_table: {
     label: 'Pivot Table',
+    controlPanelSections: [
+      {
+        'label': null,
+        'fieldSetRows': [
+          ['groupby', 'columns'],
+          ['metrics', 'pandas_aggfunc'],
+        ]
+      }
+    ]
   },
 
   separator: {
     label: 'Separator',
+    controlPanelSections: [
+      {
+        'label': null,
+        'fieldSetRows': [
+          ['code'],
+        ]
+      }
+    ],
+    feildOverrides: {
+      code: {
+        default: '####Section Title\n' +
+                 'A paragraph describing the section' +
+                 'of the dashboard, right before the separator line ' +
+                 '\n\n' +
+                 '---------------',
+      }
   },
 
   word_cloud: {
     label: 'Word Cloud',
+    controlPanelSections: [
+      {
+        label: null,
+        fieldsSetRows: [
+            ['series', 'metric', 'limit'],
+            ['size_from', 'size_to'],
+            ['rotation'],
+        ],
+      },
+    ],
   },
 
   treemap: {
     label: 'Treemap',
+    controlPanelSections: [
+      {
+        label: null,
+        fieldsSetRows: [
+          ['metrics'],
+          ['groupby'],
+        ],
+      },
+      {
+        label: 'Chart Options',
+        fieldsSetRows: [
+            ['treemap_ratio'],
+            ['number_format'],
+        ],
+      },
+    ],
   },
 
   cal_heatmap: {
     label: 'Calendar Heatmap',
-    requiresTime: true
+    requiresTime: true,
+    controlPanelSections: [
+      {
+        label: null,
+        fields: [
+            ['metric'],
+            ['domain_granularity'],
+            ['subdomain_granularity'],
+        ],
+      },
+    ],
   },
 
   box_plot: {
     label: 'Box Plot',
+    controlPanelSections: [
+      {
+        label: null,
+        fieldSetRows: [
+          ['metrics'],
+          ['groupby', 'limit'],
+        ],
+      },
+      {
+        label: 'Chart Options',
+        fieldsSetRows: [
+          ['whisker_options'],
+        ]
+      },
+    ],
   },
 
   bubble: {
     label: 'Bubble Chart',
+    controlPanelSections: [
+      {
+        label: null,
+        fieldsSetRows: [
+          ['series', 'entity'],
+          ['x', 'y'],
+          ['size', 'limit'],
+        ],
+      },
+      {
+        label: 'Chart Options',
+        fieldsSetRows: [
+            ['x_log_scale', 'y_log_scale'],
+            ['show_legend'],
+            ['max_bubble_size'],
+            ['x_axis_label', 'y_axis_label'],
+        ],
+      },
+    ],
   },
 
   big_number: {
     label: 'Big Number with Trendline',
+    controlPanelSections: [
+      {
+        label: null,
+        fieldsSetRows: [
+          ['metric'],
+          ['compare_lag'],
+          ['compare_suffix'],
+          ['y_axis_format'],
+        ],
+      },
+    ]
+    fieldOverrides: [
+      {
+        y_axis_format: {
+          label: 'Number format',
+        },
+      },
+    ],
   },
 
   bubble: {
     label: 'Bubble Chart',
+    controlPanelSections: [
+      {
+        label: null,
+        fieldsSetRows: [
+          ['metric'],
+          ['subheader'],
+          ['y_axis_format'],
+        ],
+      },
+    ]
+    fieldOverrides: {
+      y_axis_format: {
+        label: 'Number format',
+      },
+    }
   },
 
   histogram: {
     label: 'Histogram',
+    controlPanelSections: [
+      {
+        'label': null,
+        'fields': [
+          ['all_columns_x'],
+          ['row_limit'],
+        ],
+      },
+      {
+        label: 'Histogram Options',
+        fields: [
+          ['link_length'],
+        ]
+      }
+    ],
+    fieldOverrides: {
+      all_columns_x: {
+        label: 'Numeric Column',
+        description: 'Select the numeric column to draw the histogram',
+      },
+      link_length: {
+        label: 'No of Bins',
+        description: 'Select number of bins for the histogram',
+        default: 5,
+      },
+    }
   },
 
   sunburst: {
     label: 'Sunburst',
+    controlPanelSections: [
+      {
+        label: null,
+        fieldsSetRows: [
+          ['groupby'],
+          ['metric', 'secondary_metric'],
+          ['row_limit'],
+        ],
+      }
+    ],
+    fieldOverrides: {
+      metric: {
+        label: 'Primary Metric',
+        description: 'The primary metric is used to define the arc segment sizes',
+      },
+      secondary_metric: {
+        label: 'Secondary Metric',
+        description: 'This secondary metric is used to ' +
+                     'define the color as a ratio against the primary metric. ' +
+                     'If the two metrics match, color is mapped level groups',
+      },
+      groupby: {
+        label: 'Hierarchy',
+        description: 'This defines the level of the hierarchy',
+      },
+    },
   },
 
   sankey: {
     label: 'Sankey',
+    controlPanelSections: [
+      {
+        label: null,
+        fieldsSetRows: [
+          ['groupby'],
+          ['metric'],
+          ['row_limit'],
+        ],
+      },
+    ],
+    fieldOverrides: {
+      groupby: {
+        label: 'Source / Target',
+        description: 'Choose a source and a target',
+      },
+    },
   },
 
   directed_force: {
     label: 'Directed Force Layout',
+    controlPanelSections: [
+      {
+        label: null,
+        fields: [
+          ['groupby'],
+          ['metric'],
+          ['row_limit'],
+        ],
+      },
+      {
+        label: 'Force Layout',
+        fields: [
+          ['link_length'],
+          ['charge'],
+        ],
+      },
+    ],
+    fieldOverrides: {
+      groupby: {
+        label: 'Source / Target',
+        description: 'Choose a source and a target',
+      },
   },
 
   world_map: {
     label: 'World Map',
+    controlPanelSections: [
+      {
+        label: null,
+        fieldsSetRows: [
+          ['entity'],
+          ['country_fieldtype'],
+          ['metric'],
+        ],
+      },
+      {
+        label: 'Bubbles',
+        fields: [
+          ['show_bubbles'],
+          ['secondary_metric'],
+          ['max_bubble_size'],
+        ],
+      },
+    ].
+    fieldOverrides: {
+      entity: {
+        label: 'Country Field',
+        description: '3 letter code of the country',
+      },
+      metric: {
+        label: 'Metric for color',
+        description: 'Metric that defines the color of the country',
+      },
+      secondary_metric: {
+        label: 'Bubble size',
+        description: 'Metric that defines the size of the bubble',
+      },
+    },
   },
 
   filter_box: {
     label: 'Filter Box',
+    controlPanelSections: [
+      {
+        label: null,
+        fields: [
+          ['date_filter'],
+          ['groupby'],
+          ['metric'],
+        ],
+      },
+    ],
+    feildOverrides: {
+      groupby: {
+        label: 'Filter fields',
+        description: 'The fields you want to filter on',
+        default: [],
+      }
   },
 
   iframe: {
     label: 'iFrame',
+    controlPanelSections: [
+      {
+        label: null,
+        fieldsSetRows: [
+          ['url'],
+        ]
+      },
+    ],
   },
 
   para: {
     label: 'Parallel Coordinates',
+    controlPanelSections: [
+      {
+        label: null,
+        fieldsSetRows: [
+          ['series'],
+          ['metrics'],
+          ['secondary_metric'],
+          ['limit'],
+          ['show_datatable', 'include_series'],
+        ],
+      },
+    ],
   },
 
   heatmap: {
     label: 'Heatmap',
+    controlPanelSections: [
+      {
+        label: null,
+        fieldsSetRows: [
+          ['all_columns_x'],
+          ['all_columns_y'],
+          ['metric'],
+        ]
+      },
+      {
+        label: 'Heatmap Options',
+        fieldsSetRows: [
+          ['linear_color_scheme'],
+          ['xscale_interval', 'yscale_interval'],
+          ['canvas_image_rendering'],
+          ['normalize_across'],
+        ],
+      },
+    ],
   },
 
   horizon: {
-    label: 'Horizon'
+    label: 'Horizon',
+    controlPanelSections: [
+      commonControlPanelSections[NVD3TimeSeries][0],
+      {
+        label: 'Chart Options',
+        fieldsSetRows: [
+         ['series_height', 'horizon_color_scale'],
+        ],
+      },
+    ],
   },
 
   mapbox: {
     label: 'Mapbox',
+    controlPanelSections: [
+      {
+        label: null,
+        fieldSetRows: [
+          ['all_columns_x', 'all_columns_y'],
+          ['clustering_radius'],
+          ['row_limit'],
+          ['groupby'],
+          ['render_while_dragging'],
+        ],
+      },
+      {
+        label: 'Points',
+        fieldSetRows: [
+          ['point_radius'],
+          ['point_radius_unit'],
+        ],
+      },
+      {
+        label: 'Labelling',
+        fieldSetRows: [
+          ['mapbox_label'],
+          ['pandas_aggfunc'],
+        ],
+      },
+      {
+        label: 'Visual Tweaks',
+        fieldSetRows: [
+          ['mapbox_style'],
+          ['global_opacity'],
+          ['mapbox_color'],
+        ],
+      },
+      {
+        label: 'Viewport',
+        fieldSetRows: [
+          ['viewport_longitude'],
+          ['viewport_latitude'],
+          ['viewport_zoom'],
+        ],
+      },
+    ],
+    fieldOverrides: {
+      all_columns_x: {
+        label: 'Longitude',
+        description: 'Column containing longitude data',
+      },
+      all_columns_y: {
+        label: 'Latitude',
+        description: 'Column containing latitude data',
+      },
+      pandas_aggfunc: {
+        label: 'Cluster label aggregator',
+        description: 'Aggregate function applied to the list of points ' +
+                     'in each cluster to produce the cluster label.',
+      },
+      rich_tooltip: {
+        label: 'Tooltip',
+        description: 'Show a tooltip when hovering over points and clusters ' +
+                     'describing the label',
+      },
+      groupby: {
+        description: 'One or many fields to group by. If grouping, latitude ' +
+                     'and longitude columns must be present.',
+      },
+    },
   },
 };
 
