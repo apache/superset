95fd0385184cd98d606203a8126820c30eb89e5d
skyjur <ski@tictrac.com>
2016-11-02 18:20:16 +0000

    Make it possible to import caravel.bin.caravel script
    It's sometimes convinient to be able to import functionality
    from script for example in caravel case:
    
       from caravel.bin.caravel import manager
       manager.do_something()
    
    But because caravel/bin/caravel does not end with '.py' it
    is not possible to import it.
    
    I suggest to move
    
       caravel/bin/caravel -> caravel/bin/caravel.py
    
    So it is possible to import it
    
    Then `caravel` script is generated in environment `bin/`
    folder using `entry_points.scripts`:
    
      https://packaging.python.org/distributing/#console-scripts

--- run_tests.sh
+++ run_tests.sh
@@ -6,8 +6,8 @@ rm ~/.caravel/celery_results.sqlite
 rm -f .coverage
 export CARAVEL_CONFIG=tests.caravel_test_config
 set -e
-caravel/bin/caravel db upgrade
-caravel/bin/caravel db upgrade  # running twice on purpose as a test
-caravel/bin/caravel version -v
+caravel db upgrade
+caravel db upgrade  # running twice on purpose as a test
+caravel version -v
 python setup.py nosetests
 coveralls
