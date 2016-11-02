cdf4dd03024cff40f17845ab9b57ae6a6070e197
plumbeo <plumbeo@users.noreply.github.com>
2016-11-02 16:00:34 +0100

    Add yearly and quarterly granularities to mysql engine backend (#1518)

--- caravel/db_engine_specs.py
+++ caravel/db_engine_specs.py
@@ -127,6 +127,10 @@ class MySQLEngineSpec(BaseEngineSpec):
               "INTERVAL DAYOFWEEK({col}) - 1 DAY))"),
         Grain("month", _('month'), "DATE(DATE_SUB({col}, "
               "INTERVAL DAYOFMONTH({col}) - 1 DAY))"),
+        Grain("quarter", _('quarter'), "MAKEDATE(YEAR({col}), 1) "
+              "+ INTERVAL QUARTER({col}) QUARTER - INTERVAL 1 QUARTER"),
+        Grain("year", _('year'), "DATE(DATE_SUB({col}, "
+              "INTERVAL DAYOFYEAR({col}) - 1 DAY))"),
     )
 
     @classmethod
