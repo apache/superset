88b1f956c7cd8bd457d04648447fb23700b859ae
Alanna Scott <alanna.scott@airbnb.com>
2016-11-03 13:38:17 -0700

    [explore-v2] handle field overrides (#1535)
    * pass all props to *Field components
    
    * s/fieldSetOverrides/fieldOverrides
    
    * handle field overrides

--- caravel/assets/javascripts/explorev2/components/FieldSetRow.jsx
+++ caravel/assets/javascripts/explorev2/components/FieldSetRow.jsx
@@ -4,14 +4,32 @@ import { fields } from '../stores/store';
 
 const propTypes = {
   fieldSets: PropTypes.array.isRequired,
+  fieldOverrides: PropTypes.object,
 };
 
-export default function FieldSetRow({ fieldSets }) {
+const defaultProps = {
+  fieldOverrides: {},
+};
+
+function getFieldData(fs, fieldOverrides) {
+  let fieldData = fields[fs];
+  if (fieldOverrides.hasOwnProperty(fs)) {
+    const overrideData = fieldOverrides[fs];
+    fieldData = Object.assign({}, fieldData, overrideData);
+  }
+  return fieldData;
+}
+
+export default function FieldSetRow({ fieldSets, fieldOverrides }) {
   return (
     <ul className="list-unstyled">
-      {fieldSets.map((fs) => <li key={fs}><FieldSet {...fields[fs]} /></li>)}
+      {fieldSets.map((fs) => {
+        const fieldData = getFieldData(fs, fieldOverrides);
+        return <li key={fs}><FieldSet name={fs} {...fieldData} /></li>;
+      })}
     </ul>
   );
 }
 
 FieldSetRow.propTypes = propTypes;
+FieldSetRow.defaultProps = defaultProps;
