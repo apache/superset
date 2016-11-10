8ad00e71e7696b01d7dd9c8e7a6bf5988114d689
Maxime Beauchemin <maximebeauchemin@gmail.com>
2016-11-09 21:27:23 -0800

    Linting

--- superset/assets/webpack.config.js
+++ superset/assets/webpack.config.js
@@ -33,8 +33,9 @@ const config = {
     ],
     alias: {
       webworkify: 'webworkify-webpack',
-      'mapbox-gl/js/geo/transform': path.join(__dirname, "/node_modules/mapbox-gl/js/geo/transform"),
-      'mapbox-gl': path.join(__dirname, "/node_modules/mapbox-gl/dist/mapbox-gl.js"),
+      'mapbox-gl/js/geo/transform': path.join(
+        __dirname, '/node_modules/mapbox-gl/js/geo/transform'),
+      'mapbox-gl': path.join(__dirname, '/node_modules/mapbox-gl/dist/mapbox-gl.js'),
     },
 
   },
