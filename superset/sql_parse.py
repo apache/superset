cffb9663544981954f3fc42b87fdfa26ae7246f4
Bogdan Kyryliuk <bogdan.kyryliuk>
2016-11-14 10:51:43 -0800

    Implement table name extraction tests.

---
+++ superset/sql_parse.py
@@ -0,0 +1,47 @@
+import sqlparse
+from sqlparse.sql import IdentifierList, Identifier
+from sqlparse.tokens import Keyword, DML
+
+
+def is_subselect(parsed):
+    if not parsed.is_group:
+        return False
+    for item in parsed.tokens:
+        if item.ttype is DML and item.value.upper() == 'SELECT':
+            return True
+    return False
+
+
+def extract_from_part(parsed):
+    from_seen = False
+    for item in parsed.tokens:
+        if from_seen:
+            if is_subselect(item):
+                for x in extract_from_part(item):
+                    yield x
+            elif item.ttype is Keyword:
+                raise StopIteration
+            else:
+                yield item
+        elif item.ttype is Keyword and item.value.upper() == 'FROM':
+            from_seen = True
+
+
+def extract_table_identifiers(token_stream):
+    for item in token_stream:
+        print("item:")
+        print(str(item))
+        if isinstance(item, IdentifierList):
+            for identifier in item.get_identifiers():
+                yield identifier.get_name()
+        elif isinstance(item, Identifier):
+            yield item.get_name()
+        # It's a bug to check for Keyword here, but in the example
+        # above some tables names are identified as keywords...
+        elif item.ttype is Keyword:
+            yield item.value
+
+
+def extract_tables(sql):
+    stream = extract_from_part(sqlparse.parse(sql)[0])
+    return list(extract_table_identifiers(stream))
---
+++ tests/sql_parse_tests.py
@@ -0,0 +1,119 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import unittest
+
+from superset import sql_parse
+
+
+class SupersetTestCase(unittest.TestCase):
+
+    def test_simple_select(self):
+        self.assertEquals(["tbname"],
+                          sql_parse.extract_tables("SELECT * FROM tbname"))
+        # # underscores
+        self.assertEquals(["tb_name"],
+                          sql_parse.extract_tables("SELECT * FROM tb_name"))
+
+        # quotes
+        self.assertEquals(["tbname"],
+                          sql_parse.extract_tables('SELECT * FROM "tbname"'))
+
+        # schema
+        # self.assertEquals(
+        #     ["schemaname.tbname"],
+        #     sql_parse.extract_tables("SELECT * FROM schemaname.tbname"))
+
+        # quotes
+        self.assertEquals(
+            ["tb_name"],
+            sql_parse.extract_tables("SELECT field1, field2 FROM tb_name"))
+
+    def test_select_named_table(self):
+        query = "SELECT a.date, a.field FROM left_table a LIMIT 10"
+        self.assertEquals(
+            ["left_table"], sql_parse.extract_tables(query))
+
+    def test_subselect(self):
+        query = """
+            SELECT a.date, b.name FROM
+                left_table a
+                JOIN (
+                  SELECT
+                    CAST((b.year) as VARCHAR) date,
+                    name
+                  FROM right_table
+                ) b
+                ON a.date = b.date
+        """
+        self.assertEquals(["left_table", "right_table"],
+                          sql_parse.extract_tables(query))
+
+    def test_union(self):
+        query1 = "SELECT t1 UNION SELECT * t2"
+        self.assertEquals(["t1", "t2"], sql_parse.extract_tables(query1))
+
+        query2 = "SELECT t1 UNION ALL SELECT * t2"
+        self.assertEquals(["t1", "t2"], sql_parse.extract_tables(query2))
+
+        query3 = "SELECT t1 INTERSECT SELECT * t2"
+        self.assertEquals(["t1", "t2"], sql_parse.extract_tables(query3))
+
+    def test_select_from_values(self):
+        query = "SELECT * FROM VALUES (13, 42)"
+        self.assertEquals([], sql_parse.extract_tables(query))
+
+    def test_select_array(self):
+        query = """
+            SELECT ARRAY[1, 2, 3] AS my_array
+            FROM t1 LIMIT 10
+        """
+        self.assertEquals(["t1"], sql_parse.extract_tables(query))
+
+    def test_select_if(self):
+        query = """
+            SELECT IF(CARDINALITY(my_array) >= 3, my_array[3], NULL)
+            FROM t1 LIMIT 10
+        """
+        self.assertEquals(["t1"], sql_parse.extract_tables(query))
+
+    # SHOW TABLES ((FROM | IN) qualifiedName)? (LIKE pattern=STRING)?
+    def test_show_tables(self):
+        query = 'SHOW TABLES FROM s1 like "%order%"'
+        # TODO: figure out what should code do here
+        self.assertEquals(["s1.*"], sql_parse.extract_tables(query))
+
+    # SHOW COLUMNS (FROM | IN) qualifiedName
+    def test_show_columns(self):
+        query = "SHOW COLUMNS FROM t1"
+        self.assertEquals(["t1"], sql_parse.extract_tables(query))
+
+    # DESCRIBE | DESC qualifiedName
+    def test_describe(self):
+        self.assertEquals(["t1"], sql_parse.extract_tables("DESCRIBE t1"))
+        self.assertEquals(["t1"], sql_parse.extract_tables("DESC t1"))
+
+    # SHOW PARTITIONS (FROM | IN) qualifiedName (WHERE booleanExpression)?
+    # (ORDER BY sortItem (',' sortItem)*)? (LIMIT limit=(INTEGER_VALUE | ALL))?
+    def test_show_partitions(self):
+        query = """
+            SHOW PARTITIONS FROM orders
+            WHERE ds >= '2013-01-01' ORDER BY ds DESC;
+        """
+        self.assertEquals(["orders"], sql_parse.extract_tables(query))
+
+    def test_join(self):
+        query = "SELECT t1.*, t2.* FROM t1 JOIN t2 ON t1.a = t2.a;"
+        self.assertEquals(["t1", "t2"], sql_parse.extract_tables(query))
+
+    def test_with(self):
+        query = """
+            WITH
+              x AS (SELECT a FROM t),
+              y AS (SELECT a AS b FROM x),
+              z AS (SELECT b AS c FROM y)
+            SELECT c FROM z;
+        """
+        self.assertEquals(["x", "y", "z"], sql_parse.extract_tables(query))
