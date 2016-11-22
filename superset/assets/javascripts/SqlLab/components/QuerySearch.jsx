10982dec3c69f1bed709b38616417eada995d2f4
vera-liu <vera.liu@airbnb.com>
2016-11-22 11:19:23 -0800

    Make QueryTable scrollable in Query Search page (#1656)

--- superset/assets/javascripts/SqlLab/components/QuerySearch.jsx
+++ superset/assets/javascripts/SqlLab/components/QuerySearch.jsx
@@ -192,16 +192,22 @@ class QuerySearch extends React.PureComponent {
         {this.state.queriesLoading ?
           (<img className="loading" alt="Loading..." src="/static/assets/images/loading.gif" />)
           :
-          (<QueryTable
-            columns={[
-              'state', 'db', 'user', 'date',
-              'progress', 'rows', 'sql', 'querylink',
-            ]}
-            onUserClicked={this.onUserClicked.bind(this)}
-            onDbClicked={this.onDbClicked.bind(this)}
-            queries={this.state.queriesArray}
-            actions={this.props.actions}
-          />)
+          (
+          <div className="scrollbar-container">
+            <div className="scrollbar-content">
+              <QueryTable
+                columns={[
+                  'state', 'db', 'user', 'date',
+                  'progress', 'rows', 'sql', 'querylink',
+                ]}
+                onUserClicked={this.onUserClicked.bind(this)}
+                onDbClicked={this.onDbClicked.bind(this)}
+                queries={this.state.queriesArray}
+                actions={this.props.actions}
+              />
+            </div>
+          </div>
+          )
         }
       </div>
     );
