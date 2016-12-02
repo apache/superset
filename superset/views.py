5455e5b19de6c1cfa2cfdfd442571a3d967c229e
Gowtham Sai <Gowtham95india@gmail.com>
2016-12-02 13:35:01 +0530

    Update to meet pep8 requirement

--- superset/views.py
+++ superset/views.py
@@ -2611,7 +2611,7 @@ class Superset(BaseSupersetView):
             error_msg=get_error_msg(),
         ), 500
 
-    @has_access  # Gowtham95india - Login issue when logged out. 
+    @has_access  # Gowtham95india - Login issue when logged out.
     @expose("/welcome")
     def welcome(self):
         """Personalized welcome page"""
