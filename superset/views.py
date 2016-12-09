7425b65a4c53fab416097aadb0ebc644d2ee6137
Ryan Wood <ryan.wood@roke.co.uk>
2016-12-09 09:59:14 +0000

    Moved Csv form from views.py to forms.py and removed unused imports

--- superset/views.py
+++ superset/views.py
@@ -19,7 +19,7 @@ import functools
 import sqlalchemy as sqla
 
 from flask import (
-    g, request, redirect, flash, Response, render_template, Markup, url_for, send_from_directory, session)
+    g, request, redirect, flash, Response, render_template, Markup, url_for, send_from_directory)
 from flask_appbuilder import ModelView, CompactCRUDMixin, BaseView, expose, SimpleFormView
 from flask_appbuilder.actions import action
 from flask_appbuilder.models.sqla.interface import SQLAInterface
@@ -27,15 +27,11 @@ from flask_appbuilder.security.decorators import has_access, has_access_api
 from flask_appbuilder.widgets import ListWidget
 from flask_appbuilder.models.sqla.filters import BaseFilter
 from flask_appbuilder.security.sqla import models as ab_models
-from flask_appbuilder.fieldwidgets import BS3TextFieldWidget
-from flask_appbuilder.forms import DynamicForm
 
 from flask_babel import gettext as __
 from flask_babel import lazy_gettext as _
 
-from flask_wtf.file import FileField, FileAllowed, FileRequired
-from wtforms import Form, StringField, IntegerField, FieldList, BooleanField, SelectField
-from wtforms.validators import ValidationError, DataRequired, InputRequired, Optional
+from wtforms.validators import ValidationError
 
 from sqlalchemy import create_engine
 from werkzeug.routing import BaseConverter
@@ -44,11 +40,12 @@ from werkzeug.utils import secure_filename
 import superset
 from superset import (
     appbuilder, cache, db, models, viz, utils, app,
-    sm, sql_lab, results_backend, security, dataframe
+    sm, sql_lab, results_backend, security
 )
 from superset.source_registry import SourceRegistry
 from superset.models import DatasourceAccessRequest as DAR
 from superset.widgets import CsvListWidget
+from superset.forms import CsvToDatabaseForm
 
 config = app.config
 log_this = models.Log.log_this
@@ -632,78 +629,6 @@ def uploaded_file(filename):
                                filename)
 
 
-class CsvToDatabaseForm(DynamicForm):
-    # These are the fields exposed by Pandas read_csv()
-    csv_file = FileField(_('CSV File'), description=_('Select a CSV file to be uploaded to a database.'),
-                         validators=[FileRequired(), FileAllowed(['csv'], _('CSV Files Only!'))])
-    sep = StringField(_('Delimiter'), description=_('Delimiter used by CSV file (for whitespace use \s+).'),
-                      validators=[DataRequired()], widget=BS3TextFieldWidget())
-    header = IntegerField(_('Header Row'), description=_('Row containing the headers to use as column names (0 is '
-                                                         'first line of data). Leave empty if there is no header row.'),
-                          validators=[Optional()], widget=BS3TextFieldWidget(), filters=[lambda x: x or None])
-    names = StringField(_('Column Names'), description=_('List of comma-separated column names to use if header row '
-                                                         'not specified above. Leave empty if header field populated.'),
-                        validators=[Optional()], widget=BS3TextFieldWidget(), filters=[lambda x: x or None])
-    index_col = IntegerField(_('Index Column'), description=_('Column to use as the row labels of the dataframe. Leave '
-                                                              'empty if no index column.'),
-                             validators=[Optional()], widget=BS3TextFieldWidget(), filters=[lambda x: x or None])
-    squeeze = BooleanField(_('Squeeze'), description=_('Parse the data as a series (specify this option if the data '
-                                                       'contains only one column.'))
-    prefix = StringField(_('Prefix'), description=_('Prefix to add to column numbers when no header '
-                                                    '(e.g. "X" for "X0, X1").'),
-                         validators=[Optional()], widget=BS3TextFieldWidget(), filters=[lambda x: x or None])
-    mangle_dupe_cols = BooleanField(_('Mangle Duplicate Columns'), description=_('Specify duplicate columns as '
-                                                                                 '"X.0, X.1".'))
-    skipinitialspace = BooleanField(_('Skip Initial Space'), description=_('Skip spaces after delimiter.'))
-    skiprows = IntegerField(_('Skip Rows'), description=_('Number of rows to skip at start of file.'),
-                            validators=[Optional()], widget=BS3TextFieldWidget(), filters=[lambda x: x or None])
-    nrows = IntegerField(_('Rows to Read'), description=_('Number of rows of file to read.'),
-                         validators=[Optional()], widget=BS3TextFieldWidget(), filters=[lambda x: x or None])
-    skip_blank_lines = BooleanField(_('Skip Blank Lines'), description=_('Skip blank lines rather than interpreting '
-                                                                         'them as NaN values.'))
-    parse_dates = BooleanField(_('Parse Dates'), description=_('Parse date values.'))
-    infer_datetime_format = BooleanField(_('Infer Datetime Format'), description=_('Use Pandas to interpret the '
-                                                                                   'datetime format automatically.'))
-    dayfirst = BooleanField(_('Day First'), description=_('Use DD/MM (European/International) date format.'))
-    thousands = StringField(_('Thousands Separator'), description=_('Separator for values in thousands.'),
-                            validators=[Optional()], widget=BS3TextFieldWidget(), filters=[lambda x: x or None])
-    decimal = StringField(_('Decimal Character'), description=_('Character to interpret as decimal point.'),
-                          validators=[Optional()], widget=BS3TextFieldWidget(), filters=[lambda x: x or '.'])
-    quotechar = StringField(_('Quote Character'), description=_('Character used to denote the start and end of a '
-                                                                'quoted item.'),
-                            validators=[Optional()], widget=BS3TextFieldWidget(), filters=[lambda x: x or "'"])
-    escapechar = StringField(_('Escape Character'), description=_('Character used to escape a quoted item.'),
-                             validators=[Optional()], widget=BS3TextFieldWidget(), filters=[lambda x: x or None])
-    comment = StringField(_('Comment Character'), description=_('Character used to denote the start of a comment.'),
-                          validators=[Optional()], widget=BS3TextFieldWidget(), filters=[lambda x: x or None])
-    encoding = StringField(_('Encoding'), description=_('Encoding to use for UTF when reading/writing (e.g. "utf-8").'),
-                           validators=[Optional()], widget=BS3TextFieldWidget(), filters=[lambda x: x or None])
-    error_bad_lines = BooleanField(_('Error On Bad Lines'), description=_('Error on bad lines (e.g. a line with too '
-                                                                          'many commas). If false these bad lines will '
-                                                                          'instead be dropped from the resulting '
-                                                                          'dataframe.'))
-
-    # These are the fields exposed by Pandas .to_sql()
-    name = StringField(_('Table Name'), description=_('Name of table to be created from csv data.'),
-                       validators=[DataRequired()], widget=BS3TextFieldWidget())
-    con = StringField(_('Database URI'), description=_('URI of database in which to add above table.'),
-                      validators=[DataRequired()], widget=BS3TextFieldWidget())
-    schema = StringField(_('Schema'), description=_('Specify a schema (if database flavour supports this).'),
-                         validators=[Optional()], widget=BS3TextFieldWidget(), filters=[lambda x: x or None])
-    if_exists = SelectField(_('Table Exists'), description=_('If table exists do one of the following: Fail (do '
-                                                             'nothing), Replace (drop and recreate table) or Append '
-                                                             '(insert data).'),
-                            choices=[('fail', _('Fail')), ('replace', _('Replace')), ('append', _('Append'))],
-                            validators=[DataRequired()])
-    index = BooleanField(_('Dataframe Index'), description=_('Write dataframe index as a column.'))
-    index_label = StringField(_('Column Label(s)'), description=_('Column label for index column(s). If None is given '
-                                                                  'and Dataframe Index is True, Index Names are used.'),
-                              validators=[Optional()], widget=BS3TextFieldWidget(), filters=[lambda x: x or None])
-    chunksize = IntegerField(_('Chunksize'), description=_('If not None, rows will be written in batches of this size '
-                                                           'at a time. If None, all rows will be written at once.'),
-                             validators=[Optional()], widget=BS3TextFieldWidget(), filters=[lambda x: x or None])
-
-
 class CsvToDatabaseView(SimpleFormView):
     form = CsvToDatabaseForm
     form_title = _('CSV to Database configuration')
@@ -825,17 +750,12 @@ class CsvToDatabaseView(SimpleFormView):
                              encoding=encoding,
                              error_bad_lines=error_bad_lines,
                              )
-        # Convert to superset dataframe?
-        # sdf = dataframe.SupersetDataFrame(df)
         return df
 
     @staticmethod
     def df_to_db(df, name, con, schema, if_exists, index, index_label, chunksize):
 
-        # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(DATA_DIR, 'superset.db')
-        # DATA_DIR = os.path.join(os.path.expanduser('~'), '.superset')
-
-        engine = create_engine(con, echo=False)  # Can only add to existing database - make dropdown?
+        engine = create_engine(con, echo=False)
 
         # Use Pandas to parse dataframe to database
         df.to_sql(name=name, con=engine, schema=schema, if_exists=if_exists, index=index,
