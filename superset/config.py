94c5eadded87ac107ba137b4350a604af2c90e3f
JR Minnaar <jr.minnaar@gmail.com>
2016-12-13 14:06:27 +0200

    Add ADDITIONAL_MIDDLEWARE option to config
    Add documentation to explain ADDITIONAL_MIDDLEWARE

--- superset/config.py
+++ superset/config.py
@@ -168,10 +168,11 @@ VIZ_TYPE_BLACKLIST = []
 DRUID_DATA_SOURCE_BLACKLIST = []
 
 # --------------------------------------------------
-# Modules and datasources to be registered
+# Modules, datasources and middleware to be registered
 # --------------------------------------------------
 DEFAULT_MODULE_DS_MAP = {'superset.models': ['DruidDatasource', 'SqlaTable']}
 ADDITIONAL_MODULE_DS_MAP = {}
+ADDITIONAL_MIDDLEWARE = []
 
 """
 1) http://docs.python-guide.org/en/latest/writing/logging/
