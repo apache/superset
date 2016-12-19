a1ec727d3b9402ee63260e479894958bb1e04169
10105 <zhuo.deng@hand-china.com>
2016-12-19 16:46:45 +0800

    modify style

--- superset/assets/javascripts/dashboard/components/GridLayout.jsx
+++ superset/assets/javascripts/dashboard/components/GridLayout.jsx
@@ -70,17 +70,18 @@ class GridLayout extends React.Component {
   }
 
   doPrint(sliceId) {
-    var bdhtml=window.document.body.innerHTML;
-    // 去掉滚动条,改变样式
+    let bdhtml = window.document.body.innerHTML;
+    // remove scroll and change style
     $('.slice_container').attr('class', 'mySlice');
-    if($('.dataTables_scrollBody').length > 0){
-      $('.dataTables_scrollHead').attr('style', 'border: 0px;')
+    if ($('.dataTables_scrollBody').length > 0) {
+      $('.dataTables_scrollHead').attr('style', 'border: 0px;');
       $('.dataTables_scrollBody').attr('style', '');
-    } 
-    var html = $("#slice_"+sliceId+" .mySlice").parent().parent().html();
-    window.document.body.innerHTML=html; //把需要打印的指定内容赋给body.innerHTML
-    window.print(); //调用浏览器的打印功能打印指定区域
-    window.document.body.innerHTML=bdhtml;//重新给页面内容赋值；
+    }
+    let html = $('#slice_' + sliceId + ' .mySlice').parent().parent()
+                .html();
+    window.document.body.innerHTML = html; 
+    window.print(); 
+    window.document.body.innerHTML = bdhtml;
     window.location.reload();
   }
 
--- superset/assets/javascripts/explore/components/DownloadButton.jsx
+++ superset/assets/javascripts/explore/components/DownloadButton.jsx
@@ -1,6 +1,5 @@
+import $ from 'jquery';
 import React, { PropTypes } from 'react';
-import { Popover, OverlayTrigger } from 'react-bootstrap';
-import CopyToClipboard from './../../components/CopyToClipboard';
 
 const propTypes = {
   slice: PropTypes.object.isRequired,
@@ -15,18 +14,18 @@ export default class DownloadButton extends React.Component {
     };
   }
 
-  doPrint() { 
-    var bdhtml=window.document.body.innerHTML; 
-    // 去掉滚动条,改变样式
+  doPrint() {
+    let bdhtml = window.document.body.innerHTML; 
+    // remove scroll and change style
     $('.slice_container').attr('class', 'mySlice');
-    if($('.dataTables_scrollBody').length > 0){
-      $('.dataTables_scrollHead').attr('style', 'border: 0px;')
+    if($('.dataTables_scrollBody').length > 0) {
+      $('.dataTables_scrollHead').attr('style', 'border: 0px;');
       $('.dataTables_scrollBody').attr('style', '');
-    } 
-    var html = $('.mySlice').parent().parent().html();
-    window.document.body.innerHTML=html; //把需要打印的指定内容赋给body.innerHTML
-    window.print(); //调用浏览器的打印功能打印指定区域
-    window.document.body.innerHTML=bdhtml;//重新给页面内容赋值；
+    }
+    let html = $('.mySlice').parent().parent().html();
+    window.document.body.innerHTML = html; 
+    window.print(); 
+    window.document.body.innerHTML = bdhtml;
     window.location.reload();
   }
 
