a1e3fc1c230239c5535573cf708ff0e03d5eaa8a
Maxime Beauchemin <maximebeauchemin@gmail.com>
2017-01-03 18:06:49 -0800

    [explorev2] giving more room for long textboxes (#1881)

--- superset/assets/javascripts/explorev2/stores/visTypes.js
+++ superset/assets/javascripts/explorev2/stores/visTypes.js
@@ -25,7 +25,8 @@ export const commonControlPanelSections = {
   sqlClause: {
     label: 'SQL',
     fieldSetRows: [
-      ['where', 'having'],
+      ['where'],
+      ['having'],
     ],
     description: 'This section exposes ways to include snippets of SQL in your query',
   },
@@ -223,7 +224,8 @@ const visTypes = {
       {
         label: null,
         fieldSetRows: [
-          ['markup_type', 'code'],
+          ['markup_type'],
+          ['code'],
         ],
       },
     ],
@@ -722,4 +724,3 @@ export function sectionsToRender(vizType, datasourceType) {
   );
   return sections;
 }
-
