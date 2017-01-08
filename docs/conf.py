14ed10bdb0f6868e87c8cf39d770fcc616273e72
Maxime Beauchemin <maximebeauchemin@gmail.com>
2017-01-08 07:48:02 -0800

    Fixing docs generation

--- docs/conf.py
+++ docs/conf.py
@@ -119,10 +119,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    'collapse_navigation': False,
-    'display_version': False,
-}
+html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
