396b4a9ac7b6465d6520d174a308bff40b0013ec
Vera Liu <vera.liu@airbnb.com>
2017-01-10 15:27:46 -0800

    Change the downversion after after-version

--- superset/migrations/versions/1296d28ec131_druid_exports.py
+++ superset/migrations/versions/1296d28ec131_druid_exports.py
@@ -1,14 +1,14 @@
 """Adds params to the datasource (druid) table
 
 Revision ID: 1296d28ec131
-Revises: e46f2d27a08e
+Revises: 6414e83d82b7
 Create Date: 2016-12-06 17:40:40.389652
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '1296d28ec131'
-down_revision = '1b2c3f7c96f9'
+down_revision = '6414e83d82b7'
 
 from alembic import op
 import sqlalchemy as sa
