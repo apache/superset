1d49ba7abc614dad8ade89d72c17959fc77c0802
alanna scott <alanna.scott@airbnb.com>
2017-01-11 14:57:37 -0800

    remove Panel

--- superset/assets/javascripts/explorev2/components/ControlPanelSection.jsx
+++ superset/assets/javascripts/explorev2/components/ControlPanelSection.jsx
@@ -1,5 +1,4 @@
 import React, { PropTypes } from 'react';
-import { Panel } from 'react-bootstrap';
 import InfoTooltipWithTrigger from '../../components/InfoTooltipWithTrigger';
 
 const propTypes = {
