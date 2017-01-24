2f27353015e18a1eab8ca415131023e91adf5399
Maxime Beauchemin <maximebeauchemin@gmail.com>
2017-01-24 11:33:11 -0800

    v0.15.4

--- pypi_push.sh
+++ pypi_push.sh
@@ -1,8 +1,8 @@
+# first bump up package.json manually, commit and tag
 rm superset/assets/dist/*
 cd superset/assets/
 rm build/*
 npm run prod
 cd ../..
-python setup.py register
 python setup.py sdist upload
 
