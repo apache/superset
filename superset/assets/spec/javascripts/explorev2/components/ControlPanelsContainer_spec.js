7e01ea0e40ad51cf91f47af15e9fd6e109034dfc
Vera Liu <vera.liu@airbnb.com>
2017-02-07 13:55:00 -0800

    Fixed tests for control panels container and filters

--- superset/assets/spec/javascripts/explorev2/components/ControlPanelsContainer_spec.js
+++ superset/assets/spec/javascripts/explorev2/components/ControlPanelsContainer_spec.js
@@ -3,24 +3,19 @@ import { expect } from 'chai';
 import { describe, it, beforeEach } from 'mocha';
 import { shallow } from 'enzyme';
 import { Panel } from 'react-bootstrap';
-import { defaultFormData, initialState } from '../../../../javascripts/explorev2/stores/store';
-
+import { getFormDataFromFields, defaultFields }
+  from '../../../../javascripts/explorev2/stores/store';
 import {
   ControlPanelsContainer,
 } from '../../../../javascripts/explorev2/components/ControlPanelsContainer';
-import { fields } from '../../../../javascripts/explorev2/stores/fields';
 
 const defaultProps = {
-  datasource_id: 1,
-  datasource_type: 'type',
-  exploreState: initialState(),
-  form_data: defaultFormData(),
-  fields,
-  actions: {
-    fetchFieldOptions: () => {
-      // noop
-    },
-  },
+  datasource_type: 'table',
+  actions: {},
+  fields: defaultFields,
+  form_data: getFormDataFromFields(defaultFields),
+  isDatasourceMetaLoading: false,
+  exploreState: {},
 };
 
 describe('ControlPanelsContainer', () => {
