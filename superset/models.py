246c62ca76e67df5a05cdf5101fc526f2c83fb08
Dmitry Samsonov <dmitry.samsonov@odnoklassniki.ru>
2017-03-03 19:27:05 +0200

    Fix metrics for Druid v0.6

--- superset/models.py
+++ superset/models.py
@@ -1730,7 +1730,7 @@ class DruidColumn(Model, AuditMixinNullable, ImportMixin):
         if self.dimension_spec_json:
             return json.loads(self.dimension_spec_json)
 
-    def generate_metrics(self):
+    def generate_metrics(self, ver):
         """Generate metrics based on the column metadata"""
         M = DruidMetric  # noqa
         metrics = []
@@ -1758,18 +1758,38 @@ class DruidColumn(Model, AuditMixinNullable, ImportMixin):
             ))
 
         if self.avg and self.is_num:
-            mt = corrected_type.lower() + 'Avg'
-            name = 'avg__' + self.column_name
-            metrics.append(DruidMetric(
-                metric_name=name,
-                metric_type='avg',
-                verbose_name='AVG({})'.format(self.column_name),
-                json=json.dumps({
-                    'type': mt, 'name': name, 'fieldName': self.column_name})
-            ))
+            if ver > '0':
+                mt = corrected_type.lower() + 'Avg'
+                name = 'avg__' + self.column_name
+                metrics.append(DruidMetric(
+                    metric_name=name,
+                    metric_type='avg',
+                    verbose_name='AVG({})'.format(self.column_name),
+                    json=json.dumps({
+                        'type': mt, 'name': name, 'fieldName': self.column_name})
+                ))
+            else:
+                name = 'avg__' + self.column_name
+                metrics.append(DruidMetric(
+                    metric_name=name,
+                    metric_type='postagg',
+                    verbose_name='AVG({})'.format(self.column_name),
+                    json=json.dumps({
+                        'type': 'arithmetic',
+                        'name': name,
+                        'fn': '/',
+                        'fields': [
+                          { 'type': 'fieldAccess', 'fieldName': 'sum__' + self.column_name },
+                          { 'type': 'fieldAccess', 'fieldName': 'count' }
+                        ]
+                    })
+                ))
 
         if self.min and self.is_num:
-            mt = corrected_type.lower() + 'Min'
+            if ver > '0':
+                mt = corrected_type.lower() + 'Min'
+            else:
+                mt = 'min'
             name = 'min__' + self.column_name
             metrics.append(DruidMetric(
                 metric_name=name,
@@ -1779,7 +1799,10 @@ class DruidColumn(Model, AuditMixinNullable, ImportMixin):
                     'type': mt, 'name': name, 'fieldName': self.column_name})
             ))
         if self.max and self.is_num:
-            mt = corrected_type.lower() + 'Max'
+            if ver > '0':
+                mt = corrected_type.lower() + 'Max'
+            else:
+                mt = 'max'
             name = 'max__' + self.column_name
             metrics.append(DruidMetric(
                 metric_name=name,
@@ -2112,7 +2135,7 @@ class DruidDatasource(Model, AuditMixinNullable, Datasource, ImportMixin):
 
     def generate_metrics(self):
         for col in self.columns:
-            col.generate_metrics()
+            col.generate_metrics(self.cluster.druid_version)
 
     @classmethod
     def sync_to_db_from_config(cls, druid_config, user, cluster):
@@ -2226,9 +2249,14 @@ class DruidDatasource(Model, AuditMixinNullable, Datasource, ImportMixin):
                 col_obj.count_distinct = True
             if col_obj:
                 col_obj.type = cols[col]['type']
+            if datatype != "STRING":
+                col_obj.avg = "1"
+                col_obj.min = "1"
+                col_obj.max = "1"
+                col_obj.sum = "1"
             session.flush()
             col_obj.datasource = datasource
-            col_obj.generate_metrics()
+            col_obj.generate_metrics(cluster.druid_version)
             session.flush()
 
     @staticmethod
