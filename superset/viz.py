20abd4352e85a91bfb5a8fd9f2585d225a89e141
Yilei Cai <ycai@yahoo-inc.com>
2017-03-15 11:15:33 -0700

    test

--- superset/viz.py
+++ superset/viz.py
@@ -1086,11 +1086,11 @@ class DistributionPieViz(NVD3Viz):
 
     def get_data(self, df):
         df = self.get_df()
-        df = df.pivot_table(
-            index=self.groupby,
-            values=[self.metrics[0]])
-        df.sort_values(by=self.metrics[0], ascending=False, inplace=True)
-        df = df.reset_index()
+        # df = df.pivot_table(
+        #     index=self.groupby,
+        #     values=[self.metrics[0]])
+        # df.sort_values(by=self.metrics[0], ascending=False, inplace=True)
+        # df = df.reset_index()
         df.columns = ['x', 'y']
         return df.to_dict(orient="records")
 
