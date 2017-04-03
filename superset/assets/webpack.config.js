dcc6f2a18fadda5ddb1a507508cf049b9932f02b
Alanna Scott <alanna.scott@airbnb.com>
2017-04-03 13:05:13 -0700

    serve roboto font locally (#2519)
    * serve roboto locally
    
    * add trailing comma
    
    * fix long line for linter

--- superset/assets/webpack.config.js
+++ superset/assets/webpack.config.js
@@ -85,7 +85,7 @@ const config = {
       /* for font-awesome */
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&minetype=application/font-woff',
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
