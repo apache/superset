0995cdf45d74c2242a8f404102d25ac9f494bbf4
Saleh Hindi <shindi@haverford.edu>
2017-04-03 01:48:33 -0400

    Fix linting errors

--- superset/forms.py
+++ superset/forms.py
@@ -1162,101 +1162,143 @@ class FormFactory(object):
 class CsvToDatabaseForm(DynamicForm):
     # These are the fields exposed by Pandas read_csv()
     csv_file = FileField(_('CSV File'),
-        description=_('Select a CSV file to be uploaded to a database.'),
-        validators=[FileRequired(), FileAllowed(['csv'], _('CSV Files Only!'))])
+                         description=_('Select a CSV file to be '
+                                       'uploaded to a database.'),
+                         validators=[FileRequired(),
+                                     FileAllowed(['csv'],
+                                                 _('CSV Files '
+                                                   'Only!'))])
     sep = StringField(_('Delimiter'),
-        description=_('Delimiter used by CSV file (for whitespace use \s+).'),
-        validators=[DataRequired()],
-        widget=BS3TextFieldWidget())
+                      description=_('Delimiter used by CSV '
+                                    'file (for whitespace '
+                                    'use \s+).'),
+                      validators=[DataRequired()],
+                      widget=BS3TextFieldWidget())
     header = IntegerField(_('Header Row'),
-        description=_('Row containing the headers to use as column names (0 is '
-                    'first line of data). Leave empty if there is no header row.'),
-        validators=[Optional(), NumberRange(0, 1E+20)],
-        widget=BS3TextFieldWidget(),
-        filters=[lambda x: x or None])
+                          description=_('Row containing the '
+                                        'headers to use as '
+                                        'column names (0 is '
+                                        'first line of data). '
+                                        'Leave empty if there is no '
+                                        'header row.'),
+                          validators=[Optional(),
+                                      NumberRange(0, 1E+20)],
+                          widget=BS3TextFieldWidget(),
+                          filters=[lambda x: x or None])
     names = StringField(_('Column Names'),
-        description=_('List of comma-separated column names to use if header row '
-                        'not specified above. Leave empty if header field populated.'),
-        validators=[Optional()],
-        widget=BS3TextFieldWidget(),
-        filters=[lambda x: x or None])
+                        description=_('List of comma-separated '
+                                      'column names to use if '
+                                      'header row not '
+                                      'specified above. '
+                                      'Leave empty if header '
+                                      'field populated.'),
+                        validators=[Optional()],
+                        widget=BS3TextFieldWidget(),
+                        filters=[lambda x: x or None])
     index_col = IntegerField(_('Index Column'),
-        description=_('Column to use as the row labels of the dataframe. ' 
-                        'Leave empty if no index column.'),
-        validators=[Optional(), NumberRange(0, 1E+20)],
-        widget=BS3TextFieldWidget(),
-        filters=[lambda x: x or None])
+                             description=_('Column to use as the '
+                                           'row labels of the '
+                                           'dataframe. Leave empty '
+                                           'if no index column.'),
+                             validators=[Optional(),
+                                         NumberRange(0, 1E+20)],
+                             widget=BS3TextFieldWidget(),
+                             filters=[lambda x: x or None])
     squeeze = BetterBooleanField(_('Squeeze'),
-        description=_('Parse the data as a series (specify this option if the '
-                        'data contains only one column.)'))
+                                 description=_('Parse the data as '
+                                               'a series (specify '
+                                               'this option if the '
+                                               'data contains only '
+                                               'one column.)'))
     prefix = StringField(_('Prefix'),
-        description=_('Prefix to add to column numbers when no header '
-                        '(e.g. "X" for "X0, X1").'),
-        validators=[Optional()],
-        widget=BS3TextFieldWidget(),
-        filters=[lambda x: x or None])
+                         description=_('Prefix to add to column '
+                                       'numbers when no header '
+                                       '(e.g. "X" for "X0, X1").'),
+                         validators=[Optional()],
+                         widget=BS3TextFieldWidget(),
+                         filters=[lambda x: x or None])
     mangle_dupe_cols = BetterBooleanField(_('Mangle Duplicate Columns'),
-        description=_('Specify duplicate columns as '
-                       '"X.0, X.1".'))
+                                          description=_('Specify duplicate '
+                                                        'columns as '
+                                                        '"X.0, X.1".'))
     skipinitialspace = BetterBooleanField(_('Skip Initial Space'),
-        description=_('Skip spaces after delimiter.'))
+                                          description=_('Skip spaces '
+                                                        'after delimiter.'))
     skiprows = IntegerField(_('Skip Rows'),
-        description=_('Number of rows to skip at start of file.'),
-        validators=[Optional(), NumberRange(0, 1E+20)],
-        widget=BS3TextFieldWidget(),
-        filters=[lambda x: x or None])
+                            description=_('Number of rows to skip '
+                                          'at start of file.'),
+                            validators=[Optional(),
+                                        NumberRange(0, 1E+20)],
+                            widget=BS3TextFieldWidget(),
+                            filters=[lambda x: x or None])
     nrows = IntegerField(_('Rows to Read'),
-        description=_('Number of rows of file to read.'),
-        validators=[Optional(), NumberRange(0, 1E+20)],
-        widget=BS3TextFieldWidget(),
-        filters=[lambda x: x or None])
+                         description=_('Number of rows of file to read.'),
+                         validators=[Optional(), NumberRange(0, 1E+20)],
+                         widget=BS3TextFieldWidget(),
+                         filters=[lambda x: x or None])
     skip_blank_lines = BetterBooleanField(_('Skip Blank Lines'),
-        description=_('Skip blank lines rather than '
-                       'interpreting them as NaN values.'))
+                                          description=_('Skip blank '
+                                                        'lines rather than '
+                                                        'interpreting them '
+                                                        'as NaN values.'))
     parse_dates = BetterBooleanField(_('Parse Dates'),
-        description=_('Parse date values.'))
+                                     description=_('Parse '
+                                                   'date values.'))
     infer_datetime_format = BetterBooleanField(_('Infer Datetime Format'),
-        description=_('Use Pandas to interpret the '
-                         'datetime format '
-                         'automatically.'))
+                                               description=_('Use Pandas to '
+                                                             'interpret '
+                                                             'the datetime '
+                                                             'format '
+                                                             'automatically.'))
     dayfirst = BetterBooleanField(_('Day First'),
-        description=_('Use DD/MM (European/International) date format.'))
+                                  description=_('Use DD/MM '
+                                                '(European/International) '
+                                                'date format.'))
     thousands = StringField(_('Thousands Separator'),
-        description=_('Separator for values in thousands.'),
-        validators=[Optional()],
-        widget=BS3TextFieldWidget(),
-        filters=[lambda x: x or None])
+                            description=_('Separator for values in '
+                                          'thousands.'),
+                            validators=[Optional()],
+                            widget=BS3TextFieldWidget(),
+                            filters=[lambda x: x or None])
     decimal = StringField(_('Decimal Character'),
-        description=_('Character to interpret as decimal point.'),
-        validators=[Optional()],
-        widget=BS3TextFieldWidget(),
-        filters=[lambda x: x or '.'])
+                          description=_('Character to interpret as '
+                                        'decimal point.'),
+                          validators=[Optional()],
+                          widget=BS3TextFieldWidget(),
+                          filters=[lambda x: x or '.'])
     quotechar = StringField(_('Quote Character'),
-        description=_('Character used to denote the start and end of a '
-                        'quoted item.'),
-        validators=[Optional()],
-        widget=BS3TextFieldWidget(),
-        filters=[lambda x: x or "'"])
+                            description=_('Character used to denote the start '
+                                          'and end of a '
+                                          'quoted item.'),
+                            validators=[Optional()],
+                            widget=BS3TextFieldWidget(),
+                            filters=[lambda x: x or "'"])
     escapechar = StringField(_('Escape Character'),
-        description=_('Character used to escape a quoted item.'),
-        validators=[Optional()],
-        widget=BS3TextFieldWidget(),
-        filters=[lambda x: x or None])
+                             description=_('Character used to '
+                                           'escape a quoted item.'),
+                             validators=[Optional()],
+                             widget=BS3TextFieldWidget(),
+                             filters=[lambda x: x or None])
     comment = StringField(_('Comment Character'),
-        description=_('Character used to denote the start of a comment.'),
-        validators=[Optional()],
-        widget=BS3TextFieldWidget(),
-        filters=[lambda x: x or None])
+                          description=_('Character used to denote the '
+                                        'start of a comment.'),
+                          validators=[Optional()],
+                          widget=BS3TextFieldWidget(),
+                          filters=[lambda x: x or None])
     encoding = StringField(_('Encoding'),
-        description=_('Encoding to use for UTF when reading/writing (e.g. "utf-8").'),
-        validators=[Optional()],
-        widget=BS3TextFieldWidget(),
-        filters=[lambda x: x or None])
+                           description=_('Encoding to use for UTF when '
+                                         'reading/writing (e.g. "utf-8").'),
+                           validators=[Optional()],
+                           widget=BS3TextFieldWidget(),
+                           filters=[lambda x: x or None])
     error_bad_lines = BetterBooleanField(_('Error On Bad Lines'),
-        description=_('Error on bad lines (e.g. a line with '
-                        'too many commas). If false these bad '
-                        'lines will instead be dropped from '
-                        'the resulting dataframe.'))
+                                         description=_('Error on bad lines '
+                                                       '(e.g. a line with '
+                                                       'too many commas). '
+                                                       'If false these bad '
+                                                       'lines will instead '
+                                                       'be dropped from the '
+                                                       'resulting dataframe.'))
 
     # These are the fields exposed by Pandas .to_sql()
     name = StringField(_('Table Name'),
