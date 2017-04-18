f8f24115a1aebabd7b26ff65d797daa8d3b5d2aa
Giacomo Tagliabue <giacomo.tag@gmail.com>
2017-04-18 12:25:43 -0400

    use full name in SqlaTable repr

--- superset/connectors/sqla/models.py
+++ superset/connectors/sqla/models.py
@@ -190,7 +190,7 @@ class SqlaTable(Model, BaseDatasource):
             name='_customer_location_uc'),)
 
     def __repr__(self):
-        return self.name
+        return self.full_name
 
     @property
     def description_markeddown(self):
