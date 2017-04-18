f6a18d8da1b464709673876ae50aa77bb8e44df1
alanna scott <alanna.scott@airbnb.com>
2017-04-18 12:23:29 -0700

    fix result set spec

--- superset/assets/spec/javascripts/sqllab/ResultSet_spec.jsx
+++ superset/assets/spec/javascripts/sqllab/ResultSet_spec.jsx
@@ -1,33 +1,26 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
-import ResultSet from '../../../javascripts/SqlLab/components/ResultSet';
-import FilterableTable from '../../../javascripts/components/FilterableTable/FilterableTable';
-
->>>>>>> update name to FilterableTable
 import { shallow } from 'enzyme';
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
+import { Table } from 'reactable';
+
 import ResultSet from '../../../javascripts/SqlLab/components/ResultSet';
-import FilterTable from '../../../javascripts/components/FilterTable';
+import { queries } from './fixtures';
 
 describe('ResultSet', () => {
   const mockedProps = {
     query: queries[0],
   };
-  it('is a valid component', () => {
+  it('renders', () => {
     expect(React.isValidElement(<ResultSet />)).to.equal(true);
   });
-<<<<<<< HEAD
-=======
   it('renders with props', () => {
     expect(
-      React.isValidElement(<ResultSet />)
+      React.isValidElement(<ResultSet />),
     ).to.equal(true);
   });
   it('renders a Table', () => {
     const wrapper = shallow(<ResultSet {...mockedProps} />);
-    expect(wrapper.find(FilterableTable)).to.have.length(1);
+    expect(wrapper.find(Table)).to.have.length(1);
   });
->>>>>>> update name to FilterableTable
 });
