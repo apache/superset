a27164b5a3228680425dbd566c7e34188aab01e6
Maxime Beauchemin <maximebeauchemin@gmail.com>
2017-05-12 16:28:06 +0000

    Fix mysql

--- superset/db_engine_specs.py
+++ superset/db_engine_specs.py
@@ -312,6 +312,7 @@ class SqliteEngineSpec(BaseEngineSpec):
 
 class MySQLEngineSpec(BaseEngineSpec):
     engine = 'mysql'
+    cursor_execute_kwargs = {'args': {}}
     time_grains = (
         Grain('Time Column', _('Time Column'), '{col}'),
         Grain("second", _('second'), "DATE_ADD(DATE({col}), "
