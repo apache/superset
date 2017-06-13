e1751c065cb556284e85560a39d3ba58e8d85b66
Alanna Scott <alanna.scott@airbnb.com>
2017-06-13 09:44:26 -0700

    add new slice test (#2939)
    * sort explicitly on label
    
    * add simple test for /slicemodelview/add endpoint
    
    * make comments and method names more clear
    
    * fix test name
    
    * be more explicit, test status_code

--- tests/core_tests.py
+++ tests/core_tests.py
@@ -216,16 +216,23 @@ class CoreTests(SupersetTestCase):
             logging.info("[{name}]/[{method}]: {url}".format(**locals()))
             self.client.get(url)
 
-    def test_add_slice(self):
+    def test_tablemodelview_list(self):
         self.login(username='admin')
 
-        # Click on the + to add a slice
         url = '/tablemodelview/list/'
         resp = self.get_resp(url)
 
+        # assert that a table is listed
         table = db.session.query(SqlaTable).first()
         assert table.name in resp
         assert '/superset/explore/table/{}'.format(table.id) in resp
+
+    def test_add_slice(self):
+        self.login(username='admin')
+        # assert that /slicemodelview/add responds with 200
+        url = '/slicemodelview/add'
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
 
     def test_slices_V2(self):
         # Add explore-v2-beta role to admin user
