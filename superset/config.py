cddc28f4877cd749d1d853ca843e15a1d3b74e0c
Andrew Chen <andrewchen@databricks.com>
2017-06-16 13:13:30 -0700

    Fix importing config from env var

--- superset/config.py
+++ superset/config.py
@@ -12,6 +12,7 @@ from __future__ import unicode_literals
 import imp
 import json
 import os
+import sys
 from collections import OrderedDict
 
 from dateutil import tz
@@ -308,7 +309,12 @@ try:
         # for case where app is being executed via pex.
         print('Loaded your LOCAL configuration at [{}]'.format(
             os.environ[CONFIG_PATH_ENV_VAR]))
-        imp.load_source('superset_config', os.environ[CONFIG_PATH_ENV_VAR])
+        module = sys.modules[__name__]
+        override_conf = imp.load_source('superset_config', os.environ[CONFIG_PATH_ENV_VAR])
+        for key in dir(override_conf):
+            if key.isupper():
+                setattr(module, key, getattr(override_conf, key))
+
     else:
         from superset_config import *  # noqa
         import superset_config
