53dead9c2983ca9e3f940354e1fecb3d85f62b2b
Maxime Beauchemin <maximebeauchemin@gmail.com>
2017-06-20 22:25:58 -0700

    Revert "Bumping FAB to 1.9.1"
    This reverts commit f64e2ba7d52c442461b59ee1230476eae6b9ff22.

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         'celery==4.0.2',
         'colorama==0.3.9',
         'cryptography==1.7.2',
-        'flask-appbuilder==1.9.1',
+        'flask-appbuilder==1.9.0',
         'flask-cache==0.13.1',
         'flask-migrate==2.0.3',
         'flask-script==2.0.5',
