7c9efb51d68128a8875ebb5444d730b2f7c58b30
Malcolm Flint <mflint@marchedparched.corp.gq1.yahoo.com>
2017-06-29 23:14:13 +0000

    began work on optimizing table. broke paging among other things

--- superset/assets/visualizations/table.js
+++ superset/assets/visualizations/table.js
@@ -43,86 +43,12 @@ function tableVis(slice, payload) {
 
   const div = d3.select(slice.selector);
   div.html('');
-  const table = div.append('table')
+  const table = div.append('table.table')
     .classed(
       'dataframe dataframe table table-striped table-bordered ' +
       'table-condensed table-hover dataTable no-footer', true)
     .attr('width', '100%');
 
-  table.append('thead').append('tr')
-    .selectAll('th')
-    .data(data.columns)
-    .enter()
-    .append('th')
-    .text(function (d) {
-      return d;
-    });
-
-  table.append('tbody')
-    .selectAll('tr')
-    .data(data.records)
-    .enter()
-    .append('tr')
-    .selectAll('td')
-    .data(row => data.columns.map((c) => {
-      const val = row[c];
-      let html;
-      const isMetric = metrics.indexOf(c) >= 0;
-      if (c === 'timestamp') {
-        html = timestampFormatter(val);
-      }
-      if (typeof (val) === 'string') {
-        html = `<span class="like-pre">${val}</span>`;
-      }
-      if (isMetric) {
-        html = slice.d3format(c, val);
-      }
-      return {
-        col: c,
-        val,
-        html,
-        isMetric,
-      };
-    }))
-    .enter()
-    .append('td')
-    .style('background-image', function (d) {
-      if (d.isMetric) {
-        const perc = Math.round((d.val / maxes[d.col]) * 100);
-        // The 0.01 to 0.001 is a workaround for what appears to be a
-        // CSS rendering bug on flat, transparent colors
-        return (
-          `linear-gradient(to right, rgba(0,0,0,0.2), rgba(0,0,0,0.2) ${perc}%, ` +
-          `rgba(0,0,0,0.01) ${perc}%, rgba(0,0,0,0.001) 100%)`
-        );
-      }
-      return null;
-    })
-    .attr('title', (d) => {
-      if (!isNaN(d.val)) {
-        return fC(d.val);
-      }
-      return null;
-    })
-    .attr('data-sort', function (d) {
-      return (d.isMetric) ? d.val : null;
-    })
-    .on('click', function (d) {
-      if (!d.isMetric && fd.table_filter) {
-        const td = d3.select(this);
-        if (td.classed('filtered')) {
-          slice.removeFilter(d.col, [d.val]);
-          d3.select(this).classed('filtered', false);
-        } else {
-          d3.select(this).classed('filtered', true);
-          slice.addFilter(d.col, [d.val]);
-        }
-      }
-    })
-    .style('cursor', function (d) {
-      return (!d.isMetric) ? 'pointer' : '';
-    })
-    .html(d => d.html ? d.html : d.val);
   const height = slice.height();
   let paging = false;
   let pageLength;
@@ -130,17 +56,114 @@ function tableVis(slice, payload) {
     paging = true;
     pageLength = parseInt(fd.page_length, 10);
   }
-  const datatable = container.find('.dataTable').DataTable({
-    paging,
-    deferRender:true,
-    pageLength,
-    aaSorting: [],
-    searching: fd.include_search,
-    bInfo: false,
-    scrollY: height + 'px',
-    scrollCollapse: true,
-    scrollX: true,
-  });
+  console.log(paging);
+  console.log(pageLength);
+  
+  let datatable;
+  
+  if (data.columns.find((c) => {
+        return metrics.indexOf(c) >= 0;
+      })) {
+    datatable = container.find('.dataTable').DataTable({
+      data: data.records,
+      columns: data.columns,
+      paging: paging,
+      deferRender: true,
+      pageLength: pageLength,
+      aaSorting: [],
+      searching: fd.include_search,
+      bInfo: false,
+      scrollY: height + 'px',
+      scrollCollapse: true,
+      scrollX: true,
+    });
+  } else {
+   table.append('thead').append('tr')
+    .selectAll('th')
+    .data(data.columns)
+    .enter()
+    .append('th')
+    .text(function (d) {
+      return d;
+    });
+    table.append('tbody')
+      .selectAll('tr')
+      .data(data.records)
+      .enter()
+      .append('tr')
+      .selectAll('td')
+      .data(row => data.columns.map((c) => {
+        const val = row[c];
+        let html;
+        const isMetric = metrics.indexOf(c) >= 0;
+        if (c === 'timestamp') {
+          html = timestampFormatter(val);
+        }
+        if (typeof (val) === 'string') {
+          html = `<span class="like-pre">${val}</span>`;
+        }
+        if (isMetric) {
+          html = slice.d3format(c, val);
+        }
+        return {
+          col: c,
+          val,
+          html,
+          isMetric,
+        };
+      }))
+      .enter()
+      .append('td')
+      .style('background-image', function (d) {
+        if (d.isMetric) {
+          const perc = Math.round((d.val / maxes[d.col]) * 100);
+          // The 0.01 to 0.001 is a workaround for what appears to be a
+          // CSS rendering bug on flat, transparent colors
+          return (
+            `linear-gradient(to right, rgba(0,0,0,0.2), rgba(0,0,0,0.2) ${perc}%, ` +
+            `rgba(0,0,0,0.01) ${perc}%, rgba(0,0,0,0.001) 100%)`
+          );
+        }
+        return null;
+      })
+      .attr('title', (d) => {
+        if (!isNaN(d.val)) {
+          return fC(d.val);
+        }
+        return null;
+      })
+      .attr('data-sort', function (d) {
+        return (d.isMetric) ? d.val : null;
+      })
+      .on('click', function (d) {
+        if (!d.isMetric && fd.table_filter) {
+          const td = d3.select(this);
+          if (td.classed('filtered')) {
+            slice.removeFilter(d.col, [d.val]);
+            d3.select(this).classed('filtered', false);
+          } else {
+            d3.select(this).classed('filtered', true);
+            slice.addFilter(d.col, [d.val]);
+          }
+        }
+      })
+      .style('cursor', function (d) {
+        return (!d.isMetric) ? 'pointer' : '';
+      })
+      .html(d => d.html ? d.html : d.val);
+
+    datatable = container.find('.dataTable').DataTable({
+      paging: paging,
+      deferRender: true,
+      pageLength: pageLength,
+      aaSorting: [],
+      searching: fd.include_search,
+      bInfo: false,
+      scrollY: height + 'px',
+      scrollCollapse: true,
+      scrollX: true,
+    });
+  }
   fixDataTableBodyHeight(
       container.find('.dataTables_wrapper'), height);
   // Sorting table by main column
