2899027f8e46b0c8465bea27192d2a2a852f443f
Rogan <quangenw@gmail.com>
2017-07-09 17:47:43 +0800

    add missing import

--- superset/views/sql_lab.py
+++ superset/views/sql_lab.py
@@ -4,6 +4,7 @@ from flask_appbuilder import expose
 from flask_appbuilder.models.sqla.interface import SQLAInterface
 
 from flask_babel import gettext as __
+from flask_babel import lazy_gettext as _
 
 from superset import appbuilder
 from superset.models.sql_lab import Query, SavedQuery
