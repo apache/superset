b043d7e61dc0c74ee0f6c7cb981f3101daf20fbb
Rogan <quangenw@gmail.com>
2017-07-30 23:36:58 +0800

    fix none object

--- superset/assets/visualizations/filter_box.jsx
+++ superset/assets/visualizations/filter_box.jsx
@@ -76,7 +76,7 @@ class FilterBox extends React.Component {
     }
     // Add created options to filtersChoices, even though it doesn't exist,
     // or these options will exist in query sql but invisible to end user.
-    if (this.state.selectedValues) {
+    if (this.state.selectedValues.hasOwnProperty()) {
       for (const filterKey of this.state.selectedValues) {
         const existValues = this.props.filtersChoices[filterKey].map(f => f.id);
         for (const v of this.state.selectedValues[filterKey]) {
