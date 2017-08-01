ff7397bd502743385cfe24f69ba21c7df7fbf7ac
Maxime Beauchemin <maximebeauchemin@gmail.com>
2017-07-31 20:42:48 -0700

    Merging db migrations

---
+++ superset/migrations/versions/14bb909b78aa_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 14bb909b78aa
+Revises: ('ca69c70ec99b', 'e58b53fc8605')
+Create Date: 2017-07-31 20:42:34.935832
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '14bb909b78aa'
+down_revision = ('ca69c70ec99b', 'e58b53fc8605')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
