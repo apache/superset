1c35313460d11130be99ed1bdc8c036da9050c8d
Sienna Svob <sienna.avril.pearl@gmail.com>
2017-08-14 10:05:26 -0400

    change map to d3.scale.quantile instead of linear

--- superset/assets/javascripts/modules/colors.js
+++ superset/assets/javascripts/modules/colors.js
@@ -93,17 +93,6 @@ export const spectrums = {
     'black',
     'white',
   ],
-  YlGnBu: [
-    '#ffffd9',
-    '#edf8b1',
-    '#c7e9b4',
-    '#7fcdbb',
-    '#41b6c4',
-    '#1d91c0',
-    '#225ea8',
-    '#253494',
-    '#081d5',
-    ],
 };
 
 export const getColorFromScheme = (function () {
