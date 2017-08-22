8f5d9c872223fae3f27c4e43fd81c335f85aa74f
Timi Fasubaa <timi.fasubaa@airbnb.com>
2017-08-22 12:03:55 -0700

    cleanup tests

--- tests/core_tests.py
+++ tests/core_tests.py
@@ -730,18 +730,17 @@ class CoreTests(SupersetTestCase):
     def test_slice_id_is_always_logged_correctly_on_web_request(self):
         # superset/explore case
         slc = db.session.query(models.Slice).filter_by(slice_name='Girls').one()
-        db.session.expunge_all()
-        self.get_resp(slc.slice_url)
         qry = db.session.query(models.Log).filter_by(slice_id=slc.id)
+        self.get_resp(slc.slice_url)
         self.assertEqual(1, qry.count())
 
     def test_slice_id_is_always_logged_correctly_on_ajax_request(self):
         # superset/explore_json case
         self.login(username="admin")
         slc = db.session.query(models.Slice).filter_by(slice_name='Girls').one()
+        qry = db.session.query(models.Log).filter_by(slice_id=slc.id)
         slc_url = slc.slice_url.replace("explore", "explore_json")
         self.get_json_resp(slc_url)
-        qry = db.session.query(models.Log).filter_by(slice_id=slc.id)
         self.assertEqual(1, qry.count())
 
 
