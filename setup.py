187dfb9625c66b79dda84139a0945b30b1aae56e
Maxime Beauchemin <maxime.beauchemin@apache.org>
2017-08-23 23:55:53 -0700

    Bump fab to 1.9.4
    Important bug/security fixes detailed here:
    https://github.com/dpgaspar/Flask-AppBuilder/blob/master/docs/versions.rst

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         'colorama==0.3.9',
         'cryptography==1.9',
         'flask==0.12.2',
-        'flask-appbuilder==1.9.1',
+        'flask-appbuilder==1.9.4',
         'flask-cache==0.13.1',
         'flask-migrate==2.0.3',
         'flask-script==2.0.5',
