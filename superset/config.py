d00f0d7e93d8cac498d66c5b7f6eb3bb16b49ade
Malcolm Flint <mflint@yahoo-inc.com>
2017-08-25 11:03:01 -0700

    removed logo from config

--- superset/config.py
+++ superset/config.py
@@ -80,10 +80,10 @@ ENABLE_PROXY_FIX = False
 # GLOBALS FOR APP Builder
 # ------------------------------
 # Uncomment to setup Your App name
-APP_NAME = "NAP Alerts"
+APP_NAME = "Superset"
 
 # Uncomment to setup an App icon
-APP_ICON = "/static/assets/images/yahoo.png"
+APP_ICON = "/static/assets/images/superset-logo@2x.png"
 
 # Druid query timezone
 # tz.tzutc() : Using utc timezone
