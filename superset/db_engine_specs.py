a11095494f3fb73fe718c29634767363a25324c8
CyrilLeMat <cyril.lemat@gmail.com>
2017-09-28 17:55:51 +0200

    add time granularity to redshift

--- superset/db_engine_specs.py
+++ superset/db_engine_specs.py
@@ -866,6 +866,18 @@ class MssqlEngineSpec(BaseEngineSpec):
 
 class RedshiftEngineSpec(PostgresEngineSpec):
     engine = 'redshift'
+    
+    time_grains = (
+        Grain("Time Column", _('Time Column'), "{col}"),
+        Grain("second", _('second'), "DATE_TRUNC('second', {col})"),
+        Grain("minute", _('minute'), "DATE_TRUNC('minute', {col})"),
+        Grain("hour", _('hour'), "DATE_TRUNC('hour', {col})"),
+        Grain("day", _('day'), "DATE_TRUNC('day', {col})"),
+        Grain("week", _('week'), "DATE_TRUNC('week', {col})"),
+        Grain("month", _('month'), "DATE_TRUNC('month', {col})"),
+        Grain("quarter", _('quarter'), "DATE_TRUNC('quarter', {col})"),
+        Grain("year", _('year'), "DATE_TRUNC('year', {col})"),
+    )
 
 
 class OracleEngineSpec(PostgresEngineSpec):
