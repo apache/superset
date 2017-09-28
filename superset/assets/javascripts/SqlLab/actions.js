690cbe7b271f6b66add1b60c1c3e7cdc9ce42313
Grace Guo <grace.guo@airbnb.com>
2017-09-27 21:22:22 -0700

    try to fix problem that chrome window not opening after ajax requrest (#3528)

--- superset/assets/javascripts/SqlLab/actions.js
+++ superset/assets/javascripts/SqlLab/actions.js
@@ -413,6 +413,7 @@ export function createDatasource(vizOptions, context) {
     return $.ajax({
       type: 'POST',
       url: '/superset/sqllab_viz/',
+      async: false,
       data: {
         data: JSON.stringify(vizOptions),
       },
