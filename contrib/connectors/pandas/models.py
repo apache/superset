fa3825631ec62f24ea94d69edb7b508913f25cda
Roger Hunwicks <roger@tonic-solutions.com>
2017-09-28 16:29:20 +0200

    Python27 compatibility for PandasConnector - see #3302

--- contrib/connectors/pandas/models.py
+++ contrib/connectors/pandas/models.py
@@ -1,3 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 from collections import OrderedDict
 from datetime import datetime
 import logging
