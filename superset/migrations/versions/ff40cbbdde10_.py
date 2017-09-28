9df1786470f77b43c9a2fbd5e2ac5fc35c12360b
Roger Hunwicks <roger@tonic-solutions.com>
2017-09-28 12:29:50 +0200

    Add PandasConnector migration to main versions folder - see #3302

--- superset/migrations/versions/ff40cbbdde10_.py
+++ superset/migrations/versions/ff40cbbdde10_.py
@@ -1,4 +1,4 @@
-"""empty message
+"""merge PandasConnector
 
 Revision ID: ff40cbbdde10
 Revises: ('a9c47e2c1547', 'b2cd059e8803')
@@ -8,7 +8,7 @@ Create Date: 2017-09-28 09:07:11.852491
 
 # revision identifiers, used by Alembic.
 revision = 'ff40cbbdde10'
-down_revision = ('a9c47e2c1547', 'b2cd059e8803')
+down_revision = ('f959a6652acd', 'b2cd059e8803')
 
 from alembic import op
 import sqlalchemy as sa
