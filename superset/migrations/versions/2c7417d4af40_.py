b189984dd5bcf7fa42027e8958007e44537fa3ac
Jeff Niu <jeffniu22@gmail.com>
2017-10-02 18:53:54 -0700

    Merged alembic heads

---
+++ superset/migrations/versions/2c7417d4af40_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 2c7417d4af40
+Revises: ('9fb37232bbff', 'f959a6652acd')
+Create Date: 2017-10-02 18:52:18.023368
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '2c7417d4af40'
+down_revision = ('9fb37232bbff', 'f959a6652acd')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
