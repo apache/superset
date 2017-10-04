3caa7fe7beddaea590c82f72926c3caa9a5349b0
Jeff Niu <jeffniu22@gmail.com>
2017-10-03 17:16:25 -0700

    Fixed creating new filter options in FilterBox

--- superset/assets/visualizations/filter_box.jsx
+++ superset/assets/visualizations/filter_box.jsx
@@ -77,19 +77,20 @@ class FilterBox extends React.Component {
     }
     // Add created options to filtersChoices, even though it doesn't exist,
     // or these options will exist in query sql but invisible to end user.
-    if (this.state.selectedValues.hasOwnProperty()) {
-      for (const filterKey of this.state.selectedValues) {
-        const existValues = this.props.filtersChoices[filterKey].map(f => f.id);
-        for (const v of this.state.selectedValues[filterKey]) {
-          if (existValues.indexOf(v) === -1) {
-            const addChoice = {
-              filter: filterKey,
-              id: v,
-              text: v,
-              metric: 0,
-            };
-            this.props.filtersChoices[filterKey].push(addChoice);
-          }
+    for (const filterKey in this.state.selectedValues) {
+      if (!this.state.selectedValues.hasOwnProperty(filterKey)) {
+        continue;
+      }
+      const existValues = this.props.filtersChoices[filterKey].map(f => f.id);
+      for (const v of this.state.selectedValues[filterKey]) {
+        if (existValues.indexOf(v) === -1) {
+          const addChoice = {
+            filter: filterKey,
+            id: v,
+            text: v,
+            metric: 0,
+          };
+          this.props.filtersChoices[filterKey].push(addChoice);
         }
       }
     }
