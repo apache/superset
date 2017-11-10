68f5f9f7810cabff7cb636d0f570eb00b3d02be8
Grace Guo <grace.guo@airbnb.com>
2017-11-09 19:07:49 -0800

    fix error message format when long query timeout (#3823)

--- superset/assets/javascripts/chart/chartReducer.js
+++ superset/assets/javascripts/chart/chartReducer.js
@@ -62,7 +62,7 @@ export default function chartReducer(charts = {}, action) {
       return { ...state,
         chartStatus: 'failed',
         chartAlert: (
-        "<strong>{t('Query timeout')}</strong> - " +
+        `<strong>${t('Query timeout')}</strong> - ` +
         t(`visualization queries are set to timeout at ${action.timeout} seconds. `) +
         t('Perhaps your data has grown, your database is under unusual load, ' +
           'or you are simply querying a data source that is too large ' +
