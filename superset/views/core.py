0168ba9a576c256063ac7d7a30b662d4decf202f
timifasubaa <30888507+timifasubaa@users.noreply.github.com>
2017-12-04 15:39:46 -0800

    Update core.py

--- superset/views/core.py
+++ superset/views/core.py
@@ -1095,10 +1095,10 @@ class Superset(BaseSupersetView):
             status = 400
 
         return json_success(viz_obj.json_dumps(payload), status=status)
-
-    @expose('/import_dashboards', methods=['GET', 'POST'])
+    
     @log_this
     @has_access
+    @expose('/import_dashboards', methods=['GET', 'POST'])
     def import_dashboards(self):
         """Overrides the dashboards using pickled instances from the file."""
         f = request.files.get('file')
