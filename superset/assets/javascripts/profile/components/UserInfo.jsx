c6914ad7513244f4e61a623cc467e5ae3b7c3325
jpesculis <james.pesculis@gmail.com>
2017-12-25 22:50:50 -0500

    Update UserInfo.jsx and set additional properties for react-gravatar

--- superset/assets/javascripts/profile/components/UserInfo.jsx
+++ superset/assets/javascripts/profile/components/UserInfo.jsx
@@ -15,6 +15,7 @@ const UserInfo = ({ user }) => (
         email={user.email}
         width="100%"
         height=""
+        size="220"
         alt={t('Profile picture provided by Gravatar')}
         className="img-rounded"
         style={{ borderRadius: 15 }}
