9e94150eb8c5474302ec3245f4f851ca1823c98e
Hugh Miles <hmiles@lyft.com>
2018-01-17 13:38:10 -0800

    add js controls

--- superset/assets/visualizations/deckgl/layers/polygon.jsx
+++ superset/assets/visualizations/deckgl/layers/polygon.jsx
@@ -1,12 +1,21 @@
 import { PolygonLayer } from 'deck.gl';
 
-export default function polygonLayer(formData, payload) {
+import * as common from './common';
+import sandboxedEval from '../../../javascripts/modules/sandbox';
+
+export default function polygonLayer(formData, payload, slice) {
   const fd = formData;
   const fc = fd.fill_color_picker;
-  const data = payload.data.polygons.map(d => ({
+  let data = payload.data.polygons.map(d => ({
     ...d,
     fillColor: [fc.r, fc.g, fc.b, 255 * fc.a],
   }));
+
+  if (fd.js_datapoint_mutator) {
+    // Applying user defined data mutator if defined
+    const jsFnMutator = sandboxedEval(fd.js_datapoint_mutator);
+    data = data.map(jsFnMutator);
+  }
 
   return new PolygonLayer({
     id: `path-layer-${fd.slice_id}`,
@@ -14,5 +23,6 @@ export default function polygonLayer(formData, payload) {
     filled: fd.filled,
     stroked: fd.stoked,
     extruded: fd.extruded,
+    ...common.commonLayerProps(fd, slice),
   });
 }
