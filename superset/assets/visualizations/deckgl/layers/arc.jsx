cd4b9b6708d06d8ae0ec9b88240776dfb0d60c91
Hugh A. Miles II <hughmil3s@gmail.com>
2018-01-25 14:07:34 -0800

    [geo] Add JS controls to remaining layers (#4272)
    * Update viz.py
    
    * added JS controls
    
    * add JS to grid layout
    
    * add JS to hexagon layer
    
    * added JS controls to screengrid
    
    * update to js_data_mutator controls
    
    * remove .map()

--- superset/assets/visualizations/deckgl/layers/arc.jsx
+++ superset/assets/visualizations/deckgl/layers/arc.jsx
@@ -1,16 +1,26 @@
 import { ArcLayer } from 'deck.gl';
 
-export default function arcLayer(formData, payload) {
+import * as common from './common';
+import sandboxedEval from '../../../javascripts/modules/sandbox';
+
+export default function arcLayer(formData, payload, slice) {
   const fd = formData;
   const fc = fd.color_picker;
-  const data = payload.data.arcs.map(d => ({
+  let data = payload.data.arcs.map(d => ({
     ...d,
     color: [fc.r, fc.g, fc.b, 255 * fc.a],
   }));
+
+  if (fd.js_data_mutator) {
+    // Applying user defined data mutator if defined
+    const jsFnMutator = sandboxedEval(fd.js_data_mutator);
+    data = jsFnMutator(data);
+  }
 
   return new ArcLayer({
     id: `path-layer-${fd.slice_id}`,
     data,
     strokeWidth: (fd.stroke_width) ? fd.stroke_width : 3,
+    ...common.commonLayerProps(fd, slice),
   });
 }
