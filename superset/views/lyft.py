84ee6d5084742943bb6b07465691f57d7c9e9ae2
Hugh Miles <hmiles@lyft.com>
2018-02-02 16:41:14 -0800

    Lyft additional endpoints

--- superset/views/lyft.py
+++ superset/views/lyft.py
@@ -14,7 +14,7 @@ from flask_appbuilder import expose
 from flask_babel import gettext as __
 
 from superset import (
-    app, appbuilder, db, utils,
+    app, appbuilder, db, utils, sm,
 )
 
 import superset.models.core as models
@@ -40,16 +40,6 @@ USER_MISSING_ERR = __('The user seems to have been deleted')
 DATASOURCE_ACCESS_ERR = __("You don't have access to this datasource")
 
 
-def get_database_access_error_msg(database_name):
-    return __('This view requires the database %(name)s or '
-              '`all_datasource_access` permission', name=database_name)
-
-
-def get_datasource_access_error_msg(datasource_name):
-    return __('This endpoint requires the datasource %(name)s, database or '
-              '`all_datasource_access` permission', name=datasource_name)
-
-
 def json_success(json_msg, status=200):
     return Response(json_msg, status=status, mimetype='application/json')
 
@@ -57,101 +47,10 @@ def json_success(json_msg, status=200):
 class Lyft(Superset):
 
     @log_this
-    @expose('/lyft_explore_json/<datasource_type>/<datasource_id>/')
-    def lyft_explore_json(self, datasource_type, datasource_id):
-        try:
-            csv = request.args.get('csv') == 'true'
-            query = request.args.get('query') == 'true'
-            force = request.args.get('force') == 'true'
-            form_data = self.get_form_data()
-        except Exception as e:
-                return json_error_response(
-                    utils.error_msg_from_exception(e),
-                    stacktrace=traceback.format_exc())
-        return self.generate_json(datasource_type=datasource_type,
-                                  datasource_id=datasource_id,
-                                  form_data=form_data,
-                                  csv=csv,
-                                  query=query,
-                                  force=force)
-
-    @log_this
-    @expose('/lyft_dashboard_json/<dashboard_id>/')
-    def lyft_dashboard_json(self, dashboard_id):
-        """Server side rendering for a dashboard"""
-        session = db.session()
-        qry = session.query(models.Dashboard)
-        if dashboard_id.isdigit():
-            qry = qry.filter_by(id=int(dashboard_id))
-        else:
-            qry = qry.filter_by(slug=dashboard_id)
-
-        dash = qry.one()
-        datasources = set()
-        for slc in dash.slices:
-            datasource = slc.datasource
-            if datasource:
-                datasources.add(datasource)
-
-        # Hack to log the dashboard_id properly, even when getting a slug
-        @log_this
-        def dashboard(**kwargs):  # noqa
-            pass
-        dashboard(dashboard_id=dash.id)
-
-        standalone_mode = request.args.get('standalone') == 'true'
-
-        dashboard_data = dash.data
-        dashboard_data.update({
-            'standalone_mode': standalone_mode,
-            'dash_save_perm': False,
-            'dash_edit_perm': False,
-        })
-
-        bootstrap_data = {
-            'user_id': g.user.get_id(),
-            'dashboard_data': dashboard_data,
-            'datasources': {ds.uid: ds.data for ds in datasources},
-            'common': self.common_bootsrap_payload(),
-        }
-
-        return json_success(json.dumps(bootstrap_data))
-
-    def generate_json(self, datasource_type, datasource_id, form_data,
-                      csv=False, query=False, force=False):
-        try:
-            viz_obj = self.get_viz(
-                datasource_type=datasource_type,
-                datasource_id=datasource_id,
-                form_data=form_data)
-        except Exception as e:
-            logging.exception(e)
-            return json_error_response(
-                utils.error_msg_from_exception(e),
-                stacktrace=traceback.format_exc())
-
-        if csv:
-            return CsvResponse(
-                viz_obj.get_csv(),
-                status=200,
-                headers=generate_download_headers('csv'),
-                mimetype='application/csv')
-
-        if query:
-            return self.get_query_string_response(viz_obj)
-
-        try:
-            payload = viz_obj.get_payload(
-                force=force)
-        except Exception as e:
-            logging.exception(e)
-            return json_error_response(utils.error_msg_from_exception(e))
-
-        status = 200
-        if payload.get('status') == QueryStatus.FAILED:
-            status = 400
-
-        return json_success(viz_obj.json_dumps(payload), status=status)
-
+    @expose('/explore_json/<datasource_type>/<datasource_id>/')
+    def explore_json(self, datasource_type, datasource_id):
+        if sm.check_api_access():
+            return super(Lyft, self).explore_json(datasource_type, datasource_id)
+        return json_error_response("Access denied")
 
 appbuilder.add_view_no_menu(Lyft)
