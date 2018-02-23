97c25b83483f4e16d166a5bd9d16d6c958ce6531
Andre Neidert <acneidert@gmail.com>
2018-02-23 18:53:16 -0300

    Improving language description

--- superset/config.py
+++ superset/config.py
@@ -163,7 +163,7 @@ LANGUAGES = {
     'zh': {'flag': 'cn', 'name': 'Chinese'},
     'ja': {'flag': 'jp', 'name': 'Japanese'},
     'de': {'flag': 'de', 'name': 'German'},
-    'pt-BR': {'flag': 'pt-BR', 'name': 'Portuguese/Brazil'},
+    'pt-BR': {'flag': 'pt-BR', 'name': 'Brazilian Portuguese'},
 }
 # ---------------------------------------------------
 # Image and file configuration
