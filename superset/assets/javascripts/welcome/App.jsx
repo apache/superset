07fc8abb39e4b7c35bd34e116f1f9c302f1e7455
Hugh A. Miles II <hughmil3s@gmail.com>
2018-02-26 17:02:41 -0800

    New Landing Page v1.0 (#4463)
    * Updated welcome landing page
    
    * fixed test and linting
    
    * linting
    
    * addressing comments
    
    * fix test
    
    * fix test
    
    * remove unneeded var
    
    * add magic comments

--- superset/assets/javascripts/welcome/App.jsx
+++ superset/assets/javascripts/welcome/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
-import { Panel, Row, Col, FormControl } from 'react-bootstrap';
-
+import PropTypes from 'prop-types';
+import { Panel, Row, Col, Tabs, Tab, FormControl } from 'react-bootstrap';
+import RecentActivity from '../profile/components/RecentActivity';
+import Favorites from '../profile/components/Favorites';
 import DashboardTable from './DashboardTable';
+import { t } from '../locales';
+
+const propTypes = {
+  user: PropTypes.object.isRequired,
+};
 
 export default class App extends React.PureComponent {
   constructor(props) {
@@ -17,24 +24,48 @@ export default class App extends React.PureComponent {
   render() {
     return (
       <div className="container welcome">
-        <Panel>
-          <Row>
-            <Col md={8}><h2>Dashboards</h2></Col>
-            <Col md={4}>
-              <FormControl
-                type="text"
-                bsSize="sm"
-                style={{ marginTop: '25px' }}
-                placeholder="Search"
-                value={this.state.search}
-                onChange={this.onSearchChange}
-              />
-            </Col>
-          </Row>
-          <hr />
-          <DashboardTable search={this.state.search} />
-        </Panel>
+        <Tabs defaultActiveKey={1} id="uncontrolled-tab-example">
+          <Tab eventKey={1} title={t('Recently Viewed')}>
+            <Panel>
+              <Row>
+                <Col md={8}><h2>{t('Recently Viewed')}</h2></Col>
+              </Row>
+              <hr />
+              <RecentActivity user={this.props.user} />
+            </Panel>
+          </Tab>
+          <Tab eventKey={2} title={t('Favorites')}>
+            <Panel>
+              <Row>
+                <Col md={8}><h2>{t('Favorites')}</h2></Col>
+              </Row>
+              <hr />
+              <Favorites user={this.props.user} />
+            </Panel>
+          </Tab>
+          <Tab eventKey={3} title={t('Dashboards')}>
+            <Panel>
+              <Row>
+                <Col md={8}><h2>{t('Dashboards')}</h2></Col>
+                <Col md={4}>
+                  <FormControl
+                    type="text"
+                    bsSize="sm"
+                    style={{ marginTop: '25px' }}
+                    placeholder="Search"
+                    value={this.state.search}
+                    onChange={this.onSearchChange}
+                  />
+                </Col>
+              </Row>
+              <hr />
+              <DashboardTable search={this.state.search} />
+            </Panel>
+          </Tab>
+        </Tabs>
       </div>
     );
   }
 }
+
+App.propTypes = propTypes;
