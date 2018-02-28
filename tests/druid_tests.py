f6e98b6aaad31cf968074221fb8d42eacf7d7c89
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-02-28 10:12:17 -0800

    Add https support for Druid (#4480)
    * Add https support for Druid
    
    * addressing comment

--- tests/druid_tests.py
+++ tests/druid_tests.py
@@ -77,6 +77,16 @@ class DruidTests(SupersetTestCase):
     def __init__(self, *args, **kwargs):
         super(DruidTests, self).__init__(*args, **kwargs)
 
+    def get_test_cluster_obj(self):
+        return DruidCluster(
+            cluster_name='test_cluster',
+            coordinator_host='localhost',
+            coordinator_endpoint='druid/coordinator/v1/metadata',
+            coordinator_port=7979,
+            broker_host='localhost',
+            broker_port=7980,
+            metadata_last_refreshed=datetime.now())
+
     @patch('superset.connectors.druid.models.PyDruid')
     def test_client(self, PyDruid):
         self.login(username='admin')
@@ -95,13 +105,7 @@ class DruidTests(SupersetTestCase):
             db.session.delete(cluster)
         db.session.commit()
 
-        cluster = DruidCluster(
-            cluster_name='test_cluster',
-            coordinator_host='localhost',
-            coordinator_port=7979,
-            broker_host='localhost',
-            broker_port=7980,
-            metadata_last_refreshed=datetime.now())
+        cluster = self.get_test_cluster_obj()
 
         db.session.add(cluster)
         cluster.get_datasources = PickableMock(return_value=['test_datasource'])
@@ -322,6 +326,21 @@ class DruidTests(SupersetTestCase):
         pv = sm.get_session.query(sm.permissionview_model).filter_by(
             permission=permission, view_menu=view_menu).first()
         assert pv is not None
+
+    def test_urls(self):
+        cluster = self.get_test_cluster_obj()
+        self.assertEquals(
+            cluster.get_base_url('localhost', '9999'), 'http://localhost:9999')
+        self.assertEquals(
+            cluster.get_base_url('http://localhost', '9999'),
+            'http://localhost:9999')
+        self.assertEquals(
+            cluster.get_base_url('https://localhost', '9999'),
+            'https://localhost:9999')
+
+        self.assertEquals(
+            cluster.get_base_coordinator_url(),
+            'http://localhost:7979/druid/coordinator/v1/metadata')
 
 
 if __name__ == '__main__':
