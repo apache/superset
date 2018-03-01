c0ac9c53f504def0fb09e9525c7ace195f9e3772
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-02-28 17:30:14 -0800

    Expose hook to inject database connection logic on the fly
    This environment configuration setting hook allows administrators to
    alter the database connection parameters on the fly based on user
    information. This can be use for a variety of purposes:
    
    * rewire a subset of users to use different database user accounts
    * pass user related information to the database for logging or QoS
    purposes
    * ...

--- superset/models/core.py
+++ superset/models/core.py
@@ -675,6 +675,8 @@ class Database(Model, AuditMixinNullable, ImportMixin):
         if configuration:
             params['connect_args'] = {'configuration': configuration}
 
+        if config.DB_CONNECTION_MUTATOR:
+            url, params = config.DB_CONNECTION_MUTATOR(url, params, g.user)
         return create_engine(url, **params)
 
     def get_reserved_words(self):
