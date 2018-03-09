ad7556d3fbdc443e3f0778859d1e747f8f356ff0
John Bodley <john.bodley@airbnb.com>
2018-03-09 11:17:20 -0800

    [logs] Dropping dt column

--- superset/models/core.py
+++ superset/models/core.py
@@ -6,7 +6,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from copy import copy, deepcopy
-from datetime import date, datetime
+from datetime import datetime
 import functools
 import json
 import logging
@@ -20,7 +20,7 @@ import numpy
 import pandas as pd
 import sqlalchemy as sqla
 from sqlalchemy import (
-    Boolean, Column, create_engine, Date, DateTime, ForeignKey, Integer,
+    Boolean, Column, create_engine, DateTime, ForeignKey, Integer,
     MetaData, select, String, Table, Text,
 )
 from sqlalchemy.engine import url
@@ -865,7 +865,6 @@ class Log(Model):
     user = relationship(
         security_manager.user_model, backref='logs', foreign_keys=[user_id])
     dttm = Column(DateTime, default=datetime.utcnow)
-    dt = Column(Date, default=date.today())
     duration_ms = Column(Integer)
     referrer = Column(String(1024))
 
