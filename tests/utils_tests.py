d49c6b9cc3bcc88fd81924d5d55a75a953cb3c35
Hugh Miles <hmiles@lyft.com>
2018-03-09 11:31:13 -0800

    fix test

--- tests/utils_tests.py
+++ tests/utils_tests.py
@@ -15,8 +15,8 @@ import numpy
 from superset.exceptions import SupersetException
 from superset.utils import (
     base_json_conv, datetime_f, json_int_dttm_ser, json_iso_dttm_ser,
-    JSONEncodedDict, memoized, merge_extra_filters, parse_human_timedelta,
-    validate_json, zlib_compress, zlib_decompress_to_string, merge_request_params,
+    JSONEncodedDict, memoized, merge_extra_filters, merge_request_params,
+    parse_human_timedelta, validate_json, zlib_compress, zlib_decompress_to_string,
 )
 
 
