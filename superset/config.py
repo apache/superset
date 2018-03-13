c4635ce2f0118fa1a3002a8295722240dd79b0ba
Nuno Beires <34506966+nunohelibeires@users.noreply.github.com>
2018-03-13 10:35:45 +0000

    Update config.py

--- superset/config.py
+++ superset/config.py
@@ -166,6 +166,7 @@ LANGUAGES = {
     'de': {'flag': 'de', 'name': 'German'},
     'pt-BR': {'flag': 'br', 'name': 'Brazilian Portuguese'},
     'ru': {'flag': 'ru', 'name': 'Russian'},
+    'ptpt': {'flag': 'pt', 'name': 'Portuguese'},
 }
 # ---------------------------------------------------
 # Image and file configuration
