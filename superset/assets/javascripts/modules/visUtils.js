723e77eb4e60bbb655ab58f7ce0a201cf3ae3982
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-03-29 09:36:03 -0700

    [sqllab] fix data grid's instant search function
    It looks like any non-string type would break the search feature.
    of `FilterableTable`

--- superset/assets/javascripts/modules/visUtils.js
+++ superset/assets/javascripts/modules/visUtils.js
@@ -1,9 +1,13 @@
 export function getTextWidth(text, fontDetails = '12px Roboto') {
   const canvas = document.createElement('canvas');
   const context = canvas.getContext('2d');
-  context.font = fontDetails;
-  const metrics = context.measureText(text);
-  return metrics.width;
+  if (context) {
+    // Won't work outside of a browser context (ie unit tests)
+    context.font = fontDetails;
+    const metrics = context.measureText(text);
+    return metrics.width;
+  }
+  return 100;
 }
 
 export default {
--- superset/assets/spec/javascripts/components/FilterableTable/FilterableTable_spec.jsx
+++ superset/assets/spec/javascripts/components/FilterableTable/FilterableTable_spec.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
+import { mount } from 'enzyme';
 import FilterableTable from '../../../../javascripts/components/FilterableTable/FilterableTable';
 
 describe('FilterableTable', () => {
   const mockedProps = {
-    orderedColumnKeys: [],
-    data: [],
-    height: 0,
+    orderedColumnKeys: ['a', 'b', 'c'],
+    data: [
+      { a: 'a1', b: 'b1', c: 'c1', d: 0 },
+      { a: 'a2', b: 'b2', c: 'c2', d: 100 },
+    ],
+    height: 500,
   };
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(<FilterableTable {...mockedProps} />);
+  });
   it('is valid element', () => {
     expect(React.isValidElement(<FilterableTable {...mockedProps} />)).to.equal(true);
+  });
+  it('renders a grid with 2 rows', () => {
+    expect(wrapper.find('.ReactVirtualized__Grid')).to.have.length(1);
+    expect(wrapper.find('.ReactVirtualized__Table__row')).to.have.length(2);
+  });
+  it('filters on a string', () => {
+    const props = {
+      ...mockedProps,
+      filterText: 'b1',
+    };
+    wrapper = mount(<FilterableTable {...props} />);
+    expect(wrapper.find('.ReactVirtualized__Table__row')).to.have.length(1);
+  });
+  it('filters on a number', () => {
+    const props = {
+      ...mockedProps,
+      filterText: '100',
+    };
+    wrapper = mount(<FilterableTable {...props} />);
+    expect(wrapper.find('.ReactVirtualized__Table__row')).to.have.length(1);
   });
 });
