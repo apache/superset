a92ce5f0494adcc15bccf3f119fb24883a79d160
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-04-03 08:21:29 -0700

    Filtering out SQLLab views out of table list view by default
    This adds a `is_sqllab_view` flag in the "tables" table, and makes the
    filters those out in the Tables list view.
    
    The filter showing on top may be a bit confusing, but certainly less
    than seeing lots of user generated views.

--- superset/assets/javascripts/addSlice/AddSliceContainer.jsx
+++ superset/assets/javascripts/addSlice/AddSliceContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Panel, Grid, Row, Col } from 'react-bootstrap';
+import { Button, Panel } from 'react-bootstrap';
 import Select from 'react-virtualized-select';
 import visTypes from '../explore/stores/visTypes';
 import { t } from '../locales';
@@ -11,6 +11,8 @@ const propTypes = {
     value: PropTypes.string.isRequired,
   })).isRequired,
 };
+
+const styleSelectWidth = { width: 300 };
 
 export default class AddSliceContainer extends React.PureComponent {
   constructor(props) {
@@ -55,44 +57,50 @@ export default class AddSliceContainer extends React.PureComponent {
     return (
       <div className="container">
         <Panel header={<h3>{t('Create a new slice')}</h3>}>
-          <Grid>
-            <Row>
-              <Col xs={12} sm={6}>
-                <div>
-                  <p>{t('Choose a datasource')}</p>
-                  <Select
-                    clearable={false}
-                    name="select-datasource"
-                    onChange={this.changeDatasource.bind(this)}
-                    options={this.props.datasources}
-                    placeholder={t('Choose a datasource')}
-                    value={this.state.datasourceValue}
-                  />
-                </div>
-                <br />
-                <div>
-                  <p>{t('Choose a visualization type')}</p>
-                  <Select
-                    clearable={false}
-                    name="select-vis-type"
-                    onChange={this.changeVisType.bind(this)}
-                    options={this.vizTypeOptions}
-                    placeholder={t('Choose a visualization type')}
-                    value={this.state.visType}
-                  />
-                </div>
-                <br />
-                <Button
-                  bsStyle="primary"
-                  disabled={this.isBtnDisabled()}
-                  onClick={this.gotoSlice.bind(this)}
-                >
-                  {t('Create new slice')}
-                </Button>
-                <br /><br />
-              </Col>
-            </Row>
-          </Grid>
+          <div>
+            <p>{t('Choose a datasource')}</p>
+            <div style={styleSelectWidth}>
+              <Select
+                clearable={false}
+                style={styleSelectWidth}
+                name="select-datasource"
+                onChange={this.changeDatasource.bind(this)}
+                options={this.props.datasources}
+                placeholder={t('Choose a datasource')}
+                value={this.state.datasourceValue}
+                width={200}
+              />
+            </div>
+            <p className="text-muted">
+              {t(
+                'If the datasource your are looking for is not ' +
+                'available in the list, ' +
+                'follow the instructions on the how to add it on the ')}
+              <a href="http://superset.apache.org/tutorial.html">{t('Superset tutorial')}</a>
+            </p>
+          </div>
+          <br />
+          <div>
+            <p>{t('Choose a visualization type')}</p>
+            <Select
+              clearable={false}
+              name="select-vis-type"
+              style={styleSelectWidth}
+              onChange={this.changeVisType.bind(this)}
+              options={this.vizTypeOptions}
+              placeholder={t('Choose a visualization type')}
+              value={this.state.visType}
+            />
+          </div>
+          <br />
+          <Button
+            bsStyle="primary"
+            disabled={this.isBtnDisabled()}
+            onClick={this.gotoSlice.bind(this)}
+          >
+            {t('Create new slice')}
+          </Button>
+          <br /><br />
         </Panel>
       </div>
     );
