32aa5196fad7ef0b751bc4823a9d6ef364247f50
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-04-09 14:02:43 -0700

    [explore] forcing .1% number format when using 'Period Ratio' (#4774)

--- superset/assets/javascripts/explore/stores/controls.jsx
+++ superset/assets/javascripts/explore/stores/controls.jsx
@@ -1199,6 +1199,11 @@ export const controls = {
     default: '.3s',
     choices: D3_FORMAT_OPTIONS,
     description: D3_FORMAT_DOCS,
+    mapStateToProps: state => ({
+      warning: state.controls && state.controls.num_period_compare.value !== '' ?
+        t('When `Period Ratio` is set, the Y Axis Format is forced to `.1%`') : null,
+      disabled: state.controls && state.controls.num_period_compare.value !== '',
+    }),
   },
 
   y_axis_2_format: {
