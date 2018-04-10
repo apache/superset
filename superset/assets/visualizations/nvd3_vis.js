813fc3773f5d126a71821c627c1b09feb8100767
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-04-09 23:09:23 -0700

    [line] do not show 0 or nulls in rich tooltip

--- superset/assets/visualizations/nvd3_vis.js
+++ superset/assets/visualizations/nvd3_vis.js
@@ -432,17 +432,24 @@ export default function nvd3Vis(slice, payload) {
             + '</td></tr></thead><tbody>';
           d.series.sort((a, b) => a.value >= b.value ? -1 : 1);
           d.series.forEach((series) => {
-            tooltip += (
-              `<tr class="${series.highlight ? 'emph' : ''}">` +
-                `<td class='legend-color-guide' style="opacity: ${series.highlight ? '1' : '0.75'};"">` +
-                  '<div ' +
-                    `style="border: 2px solid ${series.highlight ? 'black' : 'transparent'}; background-color: ${series.color};"` +
-                  '></div>' +
-                '</td>' +
-                `<td>${series.key}</td>` +
-                `<td>${yAxisFormatter(series.value)}</td>` +
-              '</tr>'
-            );
+            if (series.value) {
+              tooltip += (
+                `<tr class="${series.highlight ? 'emph' : ''}">
+                  <td
+                    class='legend-color-guide'
+                    style="opacity: ${series.highlight ? '1' : '0.75'};""
+                  >
+                    <div
+                      style="
+                        border: 2px solid ${series.highlight ? 'black' : 'transparent'};
+                        background-color: ${series.color};"
+                    ></div>
+                  </td>
+                  <td>${series.key}</td>
+                  <td>${yAxisFormatter(series.value)}</td>
+                </tr>`
+              );
+            }
           });
           tooltip += '</tbody></table>';
           return tooltip;
