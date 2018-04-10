c93cffe3edb987f3bc1460ab5e8f925f0957e6d1
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-04-10 13:37:34 -0700

    Improve xAxis ticks, thinner bottom margin (#4756)
    * Improve xAxis ticks, thinner bottom margin
    
    * Moving utils folder
    
    * Add isTruthy

--- superset/assets/javascripts/explore/stores/visTypes.js
+++ superset/assets/javascripts/explore/stores/visTypes.js
@@ -977,17 +977,34 @@ export const visTypes = {
         label: t('Query'),
         expanded: true,
         controlSetRows: [
-          ['metric'],
+          ['domain_granularity', 'subdomain_granularity'],
+          ['metrics'],
         ],
       },
       {
-        label: t('Options'),
+        label: t('Chart Options'),
+        expanded: true,
         controlSetRows: [
-          ['domain_granularity'],
-          ['subdomain_granularity'],
+          ['linear_color_scheme'],
+          ['cell_size', 'cell_padding'],
+          ['cell_radius', 'steps'],
+          ['y_axis_format', 'x_axis_time_format'],
+          ['show_legend', 'show_values'],
+          ['show_metric_name', null],
         ],
       },
     ],
+    controlOverrides: {
+      y_axis_format: {
+        label: t('Number Format'),
+      },
+      x_axis_time_format: {
+        label: t('Time Format'),
+      },
+      show_values: {
+        default: false,
+      },
+    },
   },
 
   box_plot: {
