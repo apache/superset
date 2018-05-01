158e02e79f56a7ce8db5314fadfa33b7ace2d186
Beto Dealmeida <roberto@dealmeida.net>
2018-05-01 10:31:21 -0700

    Clean unit tests

--- superset/assets/spec/javascripts/modules/time_spec.js
+++ superset/assets/spec/javascripts/modules/time_spec.js
@@ -9,17 +9,17 @@ describe('getPlaySliderParams', () => {
 
   it('handles durations', () => {
     const timestamps = [
-      new Date('2018-01-01').getTime(),
-      new Date('2018-01-02').getTime(),
-      new Date('2018-01-03').getTime(),
-      new Date('2018-01-04').getTime(),
-      new Date('2018-01-05').getTime(),
-      new Date('2018-01-06').getTime(),
-      new Date('2018-01-07').getTime(),
-      new Date('2018-01-08').getTime(),
-      new Date('2018-01-09').getTime(),
-      new Date('2018-01-10').getTime(),
-    ];
+      new Date('2018-01-01'),
+      new Date('2018-01-02'),
+      new Date('2018-01-03'),
+      new Date('2018-01-04'),
+      new Date('2018-01-05'),
+      new Date('2018-01-06'),
+      new Date('2018-01-07'),
+      new Date('2018-01-08'),
+      new Date('2018-01-09'),
+      new Date('2018-01-10'),
+    ].map(d => d.getTime());
     const { start, end, step, values, disabled } = getPlaySliderParams(timestamps, 'P2D');
     expect(new Date(start)).to.eql(new Date('2018-01-01'));
     expect(new Date(end)).to.eql(new Date('2018-01-11'));
@@ -33,17 +33,17 @@ describe('getPlaySliderParams', () => {
 
   it('handles intervals', () => {
     const timestamps = [
-      new Date('2018-01-01').getTime(),
-      new Date('2018-01-02').getTime(),
-      new Date('2018-01-03').getTime(),
-      new Date('2018-01-04').getTime(),
-      new Date('2018-01-05').getTime(),
-      new Date('2018-01-06').getTime(),
-      new Date('2018-01-07').getTime(),
-      new Date('2018-01-08').getTime(),
-      new Date('2018-01-09').getTime(),
-      new Date('2018-01-10').getTime(),
-    ];
+      new Date('2018-01-01'),
+      new Date('2018-01-02'),
+      new Date('2018-01-03'),
+      new Date('2018-01-04'),
+      new Date('2018-01-05'),
+      new Date('2018-01-06'),
+      new Date('2018-01-07'),
+      new Date('2018-01-08'),
+      new Date('2018-01-09'),
+      new Date('2018-01-10'),
+    ].map(d => d.getTime());
     // 1970-01-03 was a Saturday
     const { start, end, step, values, disabled } = getPlaySliderParams(timestamps, 'P1W/1970-01-03T00:00:00Z');
     expect(new Date(start)).to.eql(new Date('2017-12-30'));  // Saturday
