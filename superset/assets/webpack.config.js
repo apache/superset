4a9971ed0727c858526de47ffc5676c6e54642e0
Chris Williams <chris.williams@airbnb.com>
2018-05-03 18:42:34 -0700

    [webpack] working dev version of webpack v4

--- superset/assets/webpack.config.js
+++ superset/assets/webpack.config.js
@@ -1,4 +1,3 @@
-const webpack = require('webpack');
 const path = require('path');
 const ManifestPlugin = require('webpack-manifest-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
@@ -35,10 +34,6 @@ const config = {
       '.js',
       '.jsx',
     ],
-    alias: {
-      webworkify: 'webworkify-webpack',
-    },
-
   },
   module: {
     rules: [
@@ -51,7 +46,6 @@ const config = {
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
-      // Extract css files
       {
         test: /\.css$/,
         include: APP_DIR,
@@ -63,8 +57,6 @@ const config = {
           allChunks: true,
         }),
       },
-      // Optionally extract less files
-      // or any other compile-to-css language
       {
         test: /\.less$/,
         include: APP_DIR,
@@ -106,27 +98,10 @@ const config = {
     'react/lib/ReactContext': true,
   },
   plugins: [
-    new ManifestPlugin(),
+    new ManifestPlugin(), // this is used to look up hashed filenames in html templates
     new CleanWebpackPlugin(['dist']),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      },
-    }),
     new ExtractTextPlugin('[name].[chunkhash].css'),
   ],
 };
-if (process.env.NODE_ENV === 'production') {
-  // Using settings suggested in https://github.com/webpack/webpack/issues/537
-  const UJSplugin = new webpack.optimize.UglifyJsPlugin({
-    sourceMap: false,
-    minimize: true,
-    parallel: {
-      cache: true,
-      workers: 4,
-    },
-    compress: false,
-  });
-  config.plugins.push(UJSplugin);
-}
+
 module.exports = config;
