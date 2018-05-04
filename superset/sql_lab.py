8e825f1eaa238ce8f7685bb1f547b3e411ead4ad
Paulius Klyvis <paul@convious.com>
2018-05-04 09:39:34 +0300

    Fixing return inside generator

--- superset/sql_lab.py
+++ superset/sql_lab.py
@@ -91,15 +91,15 @@ def get_query(query_id, session, retry_count=5):
 @contextmanager
 def session_scope(nullpool):
     """Provide a transactional scope around a series of operations."""
-
     if nullpool:
         engine = sqlalchemy.create_engine(
             app.config.get('SQLALCHEMY_DATABASE_URI'), poolclass=NullPool)
         session_class = sessionmaker()
         session_class.configure(bind=engine)
-        return session_class()
-    session = db.session()
-    session.commit()  # HACK
+        session = session_class()
+    else:
+        session = db.session()
+        session.commit()  # HACK
 
     try:
         yield session
