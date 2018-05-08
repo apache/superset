45ffed99760597b632bb72f6678380d6f8e08ced
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-05-07 23:18:46 -0700

    Move from deprecated flask-cache to flask-caching (#4944)
    It appears the officially maintained fork of flask-cache is
    flask-caching https://github.com/sh4nks/flask-caching . It is fully
    compatible with flask-cache.

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
         'cryptography',
         'flask',
         'flask-appbuilder',
-        'flask-cache',
+        'flask-caching',
         'flask-compress',
         'flask-migrate',
         'flask-script',
