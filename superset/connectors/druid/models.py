e6f0c7a6aa96c8d99a3dd09daf1e9081d10db167
Yuance.Li <liyuance@gmail.com>
2018-05-08 23:24:18 +0800

    add 30 minutes support under time granularity (#4954)

--- superset/connectors/druid/models.py
+++ superset/connectors/druid/models.py
@@ -529,8 +529,8 @@ class DruidDatasource(Model, BaseDatasource):
     def time_column_grains(self):
         return {
             'time_columns': [
-                'all', '5 seconds', '30 seconds', '1 minute',
-                '5 minutes', '1 hour', '6 hour', '1 day', '7 days',
+                'all', '5 seconds', '30 seconds', '1 minute', '5 minutes'
+                '30 minutes', '1 hour', '6 hour', '1 day', '7 days',
                 'week', 'week_starting_sunday', 'week_ending_saturday',
                 'month',
             ],
@@ -762,6 +762,7 @@ class DruidDatasource(Model, BaseDatasource):
             '30 seconds': 'PT30S',
             '1 minute': 'PT1M',
             '5 minutes': 'PT5M',
+            '30 minutes': 'PT30M',
             '1 hour': 'PT1H',
             '6 hour': 'PT6H',
             'one day': 'P1D',
