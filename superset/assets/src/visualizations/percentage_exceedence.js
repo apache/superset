6af6442e9b1c75d48caaa282ad76ecdd31e12c59
Basit-qc <basit.qc@gmail.com>
2018-05-22 05:30:20 +0500

    Removed css file for percentage_exceedence

--- superset/assets/src/visualizations/percentage_exceedence.js
+++ superset/assets/src/visualizations/percentage_exceedence.js
@@ -2,7 +2,7 @@ import d3 from 'd3';
 import nv from 'nvd3';
 import 'nvd3/build/nv.d3.min.css';
 import '../../stylesheets/d3tip.css';
-import './percentage_exceedence.css';
+
 
 function percentageExceedenceVis(slice, payload) {
     const div = d3.select(slice.selector);
