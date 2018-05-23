bbf0de65c0977e758cd69471c8a8c50b705762e0
Chris Williams <chris.williams@airbnb.com>
2018-05-22 23:43:12 -0700

    [dashboard v2] fix lint errors

--- superset/assets/src/dashboard/components/SliceHeader.jsx
+++ superset/assets/src/dashboard/components/SliceHeader.jsx
@@ -68,7 +68,7 @@ class SliceHeader extends React.PureComponent {
             title={
               sliceName ||
               (this.props.editMode
-                ? '       ' // this makes an empty title clickable
+                ? '---' // this makes an empty title clickable
                 : '')
             }
             canEdit={this.props.editMode}
