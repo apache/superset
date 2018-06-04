d2bc4ece3ebece5434db0a0764471ac122fbc302
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-06-04 14:54:36 -0700

    Bump celery to 4.1.1 (#5134)
    * Bump celery to 4.1.1
    
    Docs reference `celery worker --app=superset.sql_lab:celery_app
    --pool=gevent -Ofair` command which seems only to work with Celery 4.1.1
    
    * Add UPDATING.md message

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     install_requires=[
         'bleach',
         'boto3>=1.4.6',
-        'celery',
+        'celery>=4.1.1',
         'colorama',
         'contextlib2',
         'cryptography',
