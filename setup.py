b79f6d9d944f37795898ea919176cee31e9ab482
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2018-06-04 17:47:47 +0200

    Add required flask-appbuilder to setup (#5091)
    1.10.0 is needed to run superset db upgrade:
    Traceback (most recent call last):
      File "/home/rm/superset/venv/bin/superset", line 12, in <module>
        from superset.cli import manager
      File "/home/rm/superset/venv/lib/python3.4/site-packages/superset/__init__.py", line 169, in <module>
        update_perms=utils.get_update_perms_flag(),
    TypeError: __init__() got an unexpected keyword argument 'update_perms'

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
         'contextlib2',
         'cryptography',
         'flask<1.0.0',
-        'flask-appbuilder',
+        'flask-appbuilder==1.10.0',
         'flask-caching',
         'flask-compress',
         'flask-migrate',
