6ea911f1c9f50568deada2b87a26fae25f75f3f6
Gabe Lyons <gabe.lyons@airbnb.com>
2018-06-07 15:02:43 -0700

    empty lists are invalid comparators (#5160)

--- superset/assets/src/explore/AdhocFilter.js
+++ superset/assets/src/explore/AdhocFilter.js
@@ -83,7 +83,13 @@ export default class AdhocFilter {
 
   isValid() {
     if (this.expressionType === EXPRESSION_TYPES.SIMPLE) {
-      return !!(this.operator && this.subject && this.comparator && this.clause);
+      return !!(
+        this.operator &&
+        this.subject &&
+        this.comparator &&
+        this.comparator.length > 0 &&
+        this.clause
+      );
     } else if (this.expressionType === EXPRESSION_TYPES.SQL) {
       return !!(this.sqlExpression && this.clause);
     }
