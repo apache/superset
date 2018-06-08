f4428c3eddd968fe90a4f79ecd295867724104a7
Grace Guo <grace.guo@airbnb.com>
2018-06-08 16:28:11 -0700

    combine markdown code and markdown slice name

--- superset/assets/src/dashboard/util/dashboardLayoutConverter.js
+++ superset/assets/src/dashboard/util/dashboardLayoutConverter.js
@@ -88,13 +88,13 @@ function getChartHolder(item) {
     Math.round(size_y / GRID_RATIO * 100 / ROW_HEIGHT),
   );
   if (code !== undefined) {
-    let markdownContent = '';
-    if (slice_name) {
+    let markdownContent = ' '; // white-space markdown
+    if (code) {
+      markdownContent = code;
+    } else if (slice_name.trim()) {
       markdownContent = `##### **${slice_name}**\n`;
     }
-    if (code) {
-      markdownContent += code;
-    }
+
     return {
       type: MARKDOWN_TYPE,
       id: `DASHBOARD_MARKDOWN_TYPE-${generateId()}`,
