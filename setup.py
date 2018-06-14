b04dbf6aa5878bbf696574a106cb644b7305c696
Louis-Etienne Dorval <louis-etienne.dorval@ticketmaster.com>
2018-06-14 10:17:26 -0400

    Version range for botocore

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
     install_requires=[
         'bleach',
         'boto3>=1.4.6',
+        'botocore>=1.7.0, <1.8.0',
         'celery>=4.1.1',
         'colorama',
         'contextlib2',
