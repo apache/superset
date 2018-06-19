70679d4c937e932636ccf5e6d872c849c1eae384
Louis-Etienne <ledor473@hotmail.com>
2018-06-19 11:49:51 -0400

    Pin botocore version (#5184)
    * Pin botocore version
    
    * Version range for botocore

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
     install_requires=[
         'bleach',
         'boto3>=1.4.6',
+        'botocore>=1.7.0, <1.8.0',
         'celery>=4.2.0',
         'colorama',
         'contextlib2',
