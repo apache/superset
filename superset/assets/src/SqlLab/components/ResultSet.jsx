93cdf60920dc6234771876a195838c6458b80e3d
timifasubaa <30888507+timifasubaa@users.noreply.github.com>
2018-06-19 20:33:24 -0700

    [sqllab] Fix sql lab resolution link (#5216)
    * pass link
    
    * update frontend to show link differently
    
    * nits

--- superset/assets/src/SqlLab/components/ResultSet.jsx
+++ superset/assets/src/SqlLab/components/ResultSet.jsx
@@ -155,7 +155,11 @@ export default class ResultSet extends React.PureComponent {
     if (query.state === 'stopped') {
       return <Alert bsStyle="warning">Query was stopped</Alert>;
     } else if (query.state === 'failed') {
-      return <Alert bsStyle="danger">{query.errorMessage}</Alert>;
+      return (
+        <Alert bsStyle="danger">
+          {query.errorMessage}
+          {query.link && <a href={query.link}> {t('(Common errors and their resolutions)')} </a>}
+        </Alert>);
     } else if (query.state === 'success' && query.ctas) {
       return (
         <div>
