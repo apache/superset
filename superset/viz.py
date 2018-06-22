037ae1677940d87696a436796d0955c404ce73e6
John Bodley <4567245+john-bodley@users.noreply.github.com>
2018-06-22 14:31:43 -0700

    [metric] Fixing ad-hoc metric for dual-line chart (#5271)

--- superset/viz.py
+++ superset/viz.py
@@ -1317,6 +1317,7 @@ class NVD3DualLineViz(NVD3Viz):
             self.form_data.get('metric_2'),
         ]
         for i, m in enumerate(metrics):
+            m = utils.get_metric_name(m)
             ys = series[m]
             if df[m].dtype.kind not in 'biufc':
                 continue
