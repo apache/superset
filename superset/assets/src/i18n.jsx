d68aa3bd7452e36c25259ae6d6aea9d0da6fa16f
baiyuchuan <byc233518@hotmail.com>
2018-06-26 18:50:57 +0800

    fix bug
    can't  translation in add_slice page

--- superset/assets/src/i18n.jsx
+++ superset/assets/src/i18n.jsx
@@ -17,7 +17,7 @@ const i18n = (function () {
   let languagePack = DEFAULT_LANGUAGE_PACK;
 
   if (typeof window !== 'undefined') {
-    const root = document.getElementById('app');
+    const root = document.getElementById('app') || document.getElementById('js-add-slice-container');
     const bootstrapData = root ? JSON.parse(root.getAttribute('data-bootstrap')) : {};
     if (bootstrapData.common && bootstrapData.common.language_pack) {
       languagePack = bootstrapData.common.language_pack;
