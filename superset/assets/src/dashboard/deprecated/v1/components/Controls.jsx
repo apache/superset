b392e6a199338f44cf2366e6a032fd4773468aef
Grace Guo <grace.guo@airbnb.com>
2018-07-06 16:53:09 -0700

    [dashboard] Fix save issue at Force_V2_Edit mode

--- superset/assets/src/dashboard/deprecated/v1/components/Controls.jsx
+++ superset/assets/src/dashboard/deprecated/v1/components/Controls.jsx
@@ -144,6 +144,7 @@ class Controls extends React.PureComponent {
             }
           />
           {dashboard.dash_save_perm &&
+            dashboard.force_v2_edit &&
             <SaveModal
               dashboard={dashboard}
               filters={filters}
