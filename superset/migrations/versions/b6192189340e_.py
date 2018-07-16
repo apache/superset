36f121679172f291ff71927757fee186e27f10bb
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-07-16 09:36:04 -0700

    Merge db migrations

---
+++ superset/migrations/versions/b6192189340e_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: b6192189340e
+Revises: ('c48871eed6ca', 'bddc498dd179')
+Create Date: 2018-07-16 09:35:50.038797
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'b6192189340e'
+down_revision = ('c48871eed6ca', 'bddc498dd179')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
