1d75d189a7df65f7de7f4e2d9e4c1592e89a7353
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-07-22 08:16:01 -0700

    add migration merge point

---
+++ superset/migrations/versions/e83de60ac8dc_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: e83de60ac8dc
+Revises: ('b6192189340e', '1d9e835a84f9')
+Create Date: 2018-07-22 08:15:47.146340
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'e83de60ac8dc'
+down_revision = ('b6192189340e', '1d9e835a84f9')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
