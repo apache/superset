7de8e1a66eb38304b232344f0a0594cc567854b3
harsh.takkar <harsh.takkar@guavus.com>
2018-07-23 13:38:18 +0530

    Merge heads with master

---
+++ superset/migrations/versions/91626e3b2c07_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 91626e3b2c07
+Revises: ('4451805bbaa1', 'b64d216e2da2')
+Create Date: 2018-07-23 13:31:48.644422
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '91626e3b2c07'
+down_revision = ('4451805bbaa1', 'b64d216e2da2')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
