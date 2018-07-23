adb378ffb42b83ad0c4addd3622fdd82e0b77386
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-07-23 12:29:21 -0700

    Move flake8-related packages deps to reqs-dev.txt (#5460)
    * Move flake8-related packages deps to reqs-dev.txt
    
    My VIM which is integrated with flake8 wouldn't match the output from
    travis and would often miss things related to the flake8 plugins
    installed using Tox.
    
    By moving this to requirements-dev.txt, we can expect developers would
    have the proper configuration locally and get matching results with
    Travis when running flake8 or in their IDEs if its integrated with
    flake8..
    
    * merging migratinos
    
    * sorting packages
    
    * Specify folder for flake8 processing
    
    * pin pycodestyle==2.3.1
    
    * merge db migrations

---
+++ superset/migrations/versions/e3970889f38e_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: e3970889f38e
+Revises: ('4451805bbaa1', '1d9e835a84f9')
+Create Date: 2018-07-22 09:32:36.986561
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'e3970889f38e'
+down_revision = ('4451805bbaa1', '1d9e835a84f9')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
