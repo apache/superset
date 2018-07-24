8eada37f9a3e1bf91256a8c92f6765f6cbff634d
Grace Guo <grace.guo@airbnb.com>
2018-07-24 15:14:11 -0700

    Migrate dashboard positions data from v1 to v2 format (#5463)
    * Migrate dashboard positions data from v1 to v2 format
    
    * UPDATING.md
    
    * rebase onto master
    
    (cherry picked from commit fd2d4b0)

---
+++ superset/migrations/versions/c18bd4186f15_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: c18bd4186f15
+Revises: ('46ba6aaaac97', 'ec1f88a35cc6')
+Create Date: 2018-07-24 14:29:41.341098
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'c18bd4186f15'
+down_revision = ('46ba6aaaac97', 'ec1f88a35cc6')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
