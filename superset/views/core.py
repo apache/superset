aec684428bd19aaa36d44aabf05301be99602a5c
Timi Fasubaa <timi.fasubaa@airbnb.com>
2018-08-03 11:35:34 -0700

    permissions bug fixes

--- superset/views/core.py
+++ superset/views/core.py
@@ -1091,8 +1091,7 @@ class Superset(BaseSupersetView):
             return json_error_response(
                 security_manager.get_datasource_access_error_msg(viz_obj.datasource),
                 status=404,
-                link=security_manager.security_manager.get_datasource_error_link(
-                    viz_obj.datasource))
+                link=security_manager.get_datasource_access_link(viz_obj.datasource))
 
         if csv:
             return CsvResponse(
@@ -2569,7 +2568,7 @@ class Superset(BaseSupersetView):
         if not security_manager.datasource_access(datasource):
             return json_error_response(
                 security_manager.get_datasource_access_error_msg(datasource),
-                link=security_manager.get_datasource_error_link(datasource))
+                link=security_manager.get_datasource_access_link(datasource))
         return json_success(json.dumps(datasource.data))
 
     @expose('/queries/<last_updated_ms>')
@@ -2749,7 +2748,7 @@ class Superset(BaseSupersetView):
             return json_error_response(
                 security_manager.get_datasource_access_error_msg(viz_obj.datasource),
                 status=401,
-                link=security_manager.get_datasource_error_link(viz_obj.datasource))
+                link=security_manager.get_datasource_access_link(viz_obj.datasource))
         return self.get_query_string_response(viz_obj)
 
 
