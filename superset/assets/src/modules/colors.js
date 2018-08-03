6461153db216e1e6dacd441dde6a33b25f574c9a
Jillian Rowe <jillian.e.rowe@gmail.com>
2018-08-03 10:40:23 -0400

    again

--- superset/assets/src/modules/colors.js
+++ superset/assets/src/modules/colors.js
@@ -1,8 +1,8 @@
 import d3 from 'd3';
-import {TIME_SHIFT_PATTERN} from '../utils/common';
+import { TIME_SHIFT_PATTERN } from '../utils/common';
 
 export const brandColor = '#00A699';
-export const colorPrimary = {r: 0, g: 122, b: 135, a: 1};
+export const colorPrimary = { r: 0, g: 122, b: 135, a: 1 };
 
 // Color related utility functions go in this object
 export const bnbColors = [
