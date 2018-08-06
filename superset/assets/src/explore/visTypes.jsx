6616365fca8ab62929d2d4203c127f34872be203
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-08-06 14:32:29 -0700

    Add option for BigNumber to not start y-axis at 0 (#5552)
    * Add option to not start y-axis at 0
    
    * Update language to positive.

--- superset/assets/src/explore/visTypes.jsx
+++ superset/assets/src/explore/visTypes.jsx
@@ -1251,7 +1251,7 @@ export const visTypes = {
         controlSetRows: [
           ['compare_lag', 'compare_suffix'],
           ['y_axis_format', null],
-          ['show_trend_line', null],
+          ['show_trend_line', 'start_y_axis_at_zero'],
         ],
       },
     ],
