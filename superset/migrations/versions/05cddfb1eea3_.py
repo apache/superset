b708540c9082c07b4f24262a210ae94fc085eeb3
Beto Dealmeida <roberto@dealmeida.net>
2018-08-08 14:24:32 -0700

    Migration script heads

---
+++ superset/migrations/versions/05cddfb1eea3_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 05cddfb1eea3
+Revises: ('b3591036f8d4', '0c5070e96b57')
+Create Date: 2018-08-08 10:47:53.077871
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '05cddfb1eea3'
+down_revision = ('b3591036f8d4', '0c5070e96b57')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
