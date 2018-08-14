769d53c782baa4216f061200f23ad8839dce5c9a
Hugh Miles <hmiles@lyft.com>
2018-08-14 10:14:58 -0700

    add time back

--- superset/connectors/sqla/models.py
+++ superset/connectors/sqla/models.py
@@ -47,6 +47,9 @@ class AnnotationDatasource(BaseDatasource):
         error_message = None
         qry = db.session.query(Annotation)
         qry = qry.filter(Annotation.layer_id == query_obj['filter'][0]['val'])
+        if query_obj['from_dttm']:
+            qry = qry.filter(Annotation.start_dttm >= query_obj['from_dttm'])
+        qry = qry.filter(Annotation.end_dttm <= query_obj['to_dttm'])
         status = QueryStatus.SUCCESS
         try:
             df = pd.read_sql_query(qry.statement, db.engine)
