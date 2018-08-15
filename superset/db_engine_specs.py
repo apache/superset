ea0270de4b3adf03dda15c0da326190d8d46a37f
Sumedh Sakdeo <ssakdeo@lyft.com>
2018-08-15 15:14:39 -0700

    Field names in big query can contain only alphanumeric and underscore

--- superset/db_engine_specs.py
+++ superset/db_engine_specs.py
@@ -422,6 +422,10 @@ class BaseEngineSpec(object):
 
         return df.rename(index=str, columns=rename_cols)
 
+    @staticmethod
+    def mutate_expression_label(label):
+        return label
+
 
 class PostgresBaseEngineSpec(BaseEngineSpec):
     """ Abstract class for Postgres 'like' databases """
@@ -1409,6 +1413,18 @@ class BQEngineSpec(BaseEngineSpec):
         if len(data) != 0 and type(data[0]).__name__ == 'Row':
             data = [r.values() for r in data]
         return data
+
+    @staticmethod
+    def mutate_expression_label(label):
+        mutated_label = re.sub('[^\w]+', '_', label)
+        if not re.match("^[a-zA-Z_]+.*", mutated_label):
+            raise SupersetTemplateException('BigQuery field_name used is invalid {}, '
+                                            'should start with a letter or '
+                                            'underscore'.format(mutated_label))
+        if len(mutated_label) > 128:
+            raise SupersetTemplateException('BigQuery field_name {}, should be atmost '
+                                            '128 characters'.format(mutated_label))
+        return mutated_label
 
 
 class ImpalaEngineSpec(BaseEngineSpec):
