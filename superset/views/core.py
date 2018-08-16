3c67e9ab619da1b29f34853d1a92d5c3a8d8e773
timifasubaa <30888507+timifasubaa@users.noreply.github.com>
2018-08-16 14:17:41 -0700

    make filters use security manager (#5567)
    * make filters use security manager
    
    * remove the superset short-circuit

--- superset/views/core.py
+++ superset/views/core.py
@@ -97,7 +97,7 @@ def is_owner(obj, user):
 
 class SliceFilter(SupersetFilter):
     def apply(self, query, func):  # noqa
-        if self.has_all_datasource_access():
+        if security_manager.all_datasource_access():
             return query
         perms = self.get_view_menus('datasource_access')
         # TODO(bogdan): add `schema_access` support here
@@ -109,7 +109,7 @@ class DashboardFilter(SupersetFilter):
     """List dashboards for which users have access to at least one slice or are owners"""
 
     def apply(self, query, func):  # noqa
-        if self.has_all_datasource_access():
+        if security_manager.all_datasource_access():
             return query
         Slice = models.Slice  # noqa
         Dash = models.Dashboard  # noqa
