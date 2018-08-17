8a9a7dad3a2f675dc5de903927abb6d2c600542b
Sumedh Sakdeo <773250+sumedhsakdeo@users.noreply.github.com>
2018-08-16 20:04:08 -0700

    handle exception and set dtype value to JSON, when column type JSON (#5644)
    * handle exception and set dtype value to JSON, when column type JSON
    
    * review comment
    
    * only when we handle exception

--- superset/views/core.py
+++ superset/views/core.py
@@ -2252,6 +2252,8 @@ class Superset(BaseSupersetView):
             try:
                 dtype = '{}'.format(col['type'])
             except Exception:
+                # sqla.types.JSON __str__ has a bug, so using __class__.
+                dtype = col['type'].__class__.__name__
                 pass
             payload_columns.append({
                 'name': col['name'],
