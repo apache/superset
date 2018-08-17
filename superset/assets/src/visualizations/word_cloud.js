70dee8219a47e4e1412baf89c22314af35d801cd
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-08-17 16:35:12 -0700

    Repair and refactor word cloud

--- superset/assets/src/visualizations/word_cloud.js
+++ superset/assets/src/visualizations/word_cloud.js
@@ -1,63 +1,81 @@
-/* eslint-disable  no-use-before-define */
 import d3 from 'd3';
 import cloudLayout from 'd3-cloud';
 import { getColorFromScheme } from '../modules/colors';
 
-function wordCloudChart(slice, payload) {
-  const chart = d3.select(slice.selector);
-  const data = payload.data;
-  const fd = slice.formData;
-  const range = [
-    fd.size_from,
-    fd.size_to,
-  ];
-  const rotation = fd.rotation;
-  let fRotation;
-  if (rotation === 'square') {
-    fRotation = () => Math.floor((Math.random() * 2)) * 90;
-  } else if (rotation === 'flat') {
-    fRotation = () => 0;
-  } else {
-    fRotation = () => Math.floor(((Math.random() * 6) - 3)) * 30;
-  }
-  const size = [slice.width(), slice.height()];
+const ROTATION = {
+  square: () => Math.floor((Math.random() * 2)) * 90,
+  flat: () => 0,
+  random: () => Math.floor(((Math.random() * 6) - 3)) * 30,
+};
+
+function wordCloud(element, data, {
+  width,
+  height,
+  rotation,
+  sizeRange,
+  colorScheme,
+}) {
+  const chart = d3.select(element);
+  const size = [width, height];
+  const rotationFn = ROTATION[rotation] || ROTATION.random;
 
   const scale = d3.scale.linear()
-    .range(range)
+    .range(sizeRange)
     .domain(d3.extent(data, d => d.size));
-
-  function draw(words) {
-    chart.selectAll('*').remove();
-
-    chart.append('svg')
-        .attr('width', layout.size()[0])
-        .attr('height', layout.size()[1])
-      .append('g')
-        .attr('transform', `translate(${layout.size()[0] / 2},${layout.size()[1] / 2})`)
-      .selectAll('text')
-        .data(words)
-      .enter()
-        .append('text')
-        .style('font-size', d => d.size + 'px')
-        .style('font-weight', 'bold')
-        .style('font-family', 'Helvetica')
-        .style('fill', d => getColorFromScheme(d.text, fd.color_scheme))
-        .attr('text-anchor', 'middle')
-        .attr('transform', d => `translate(${d.x}, ${d.y}) rotate(${d.rotate})`)
-        .text(d => d.text);
-  }
 
   const layout = cloudLayout()
     .size(size)
     .words(data)
     .padding(5)
-    .rotate(fRotation)
+    .rotate(rotationFn)
     .font('Helvetica')
     .fontWeight('bold')
-    .fontSize(d => scale(d.size))
-    .on('end', draw);
+    .fontSize(d => scale(d.size));
 
-  layout.start();
+  function draw(words) {
+    chart.selectAll('*').remove();
+
+    const [w, h] = layout.size();
+
+    chart.append('svg')
+        .attr('width', w)
+        .attr('height', h)
+      .append('g')
+        .attr('transform', `translate(${w / 2},${h / 2})`)
+      .selectAll('text')
+        .data(words)
+      .enter()
+        .append('text')
+        .style('font-size', d => `${d.size}px`)
+        .style('font-weight', 'bold')
+        .style('font-family', 'Helvetica')
+        .style('fill', d => getColorFromScheme(d.text, colorScheme))
+        .attr('text-anchor', 'middle')
+        .attr('transform', d => `translate(${d.x}, ${d.y}) rotate(${d.rotate})`)
+        .text(d => d.text);
+  }
+
+  layout.on('end', draw).start();
 }
 
-module.exports = wordCloudChart;
+function adaptor(slice, payload) {
+  const { selector, formData } = slice;
+  const {
+    rotation,
+    size_to: sizeTo,
+    size_from: sizeFrom,
+    color_scheme: colorScheme,
+  } = formData;
+  const element = document.querySelector(selector);
+  const data = payload.data;
+
+  return wordCloud(element, data, {
+    width: slice.width(),
+    height: slice.height(),
+    rotation,
+    sizeRange: [sizeFrom, sizeTo],
+    colorScheme,
+  });
+}
+
+module.exports = adaptor;
