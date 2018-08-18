0e0213d9574845e39beea2d0c22108c39da12953
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-08-17 22:17:11 -0700

    remove old code

--- superset/assets/src/visualizations/markup.js
+++ superset/assets/src/visualizations/markup.js
@@ -14,10 +14,6 @@ function markupWidget(slice, payload) {
   // document.getElementById('brace-editor')
   //   .setAttribute('rows', 15);
 
-  // const jqdiv = slice.container;
-  // jqdiv.css({
-  //   overflow: 'auto',
-  // });
   const container = document.querySelector(selector);
   container.style.overflow = 'auto';
 
@@ -25,12 +21,6 @@ function markupWidget(slice, payload) {
   const iframeHeight = vizType === 'separator'
     ? height - 20
     : height + headerHeight;
-
-  // let iframeHeight = height - 20;
-  // if (vizType === 'separator') {
-  //   // separator height is the entire chart container: slice height + header
-  //   iframeHeight = height + headerHeight;
-  // }
 
   const html = `
     <html>
@@ -50,15 +40,6 @@ function markupWidget(slice, payload) {
   iframe.setAttribute('sandbox', 'allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation');
   container.appendChild(iframe);
 
-  // jqdiv.html(`
-  //   <iframe id="${iframeId}"
-  //     frameborder="0"
-  //     height="${iframeHeight}"
-  //     sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation">
-  //   </iframe>
-  // `);
-
-  // const iframe = document.getElementById(iframeId);
   srcdoc.set(iframe, html);
 }
 
