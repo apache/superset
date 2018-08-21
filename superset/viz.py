b9d2027b88f0e6c7086f35792c4ea59bb618c0b6
Sumedh Sakdeo <ssakdeo@lyft.com>
2018-08-20 23:13:55 -0700

    lint again

--- superset/viz.py
+++ superset/viz.py
@@ -122,7 +122,7 @@ class BaseViz(object):
             return metric
         if isinstance(metric, dict):
             return self.datasource.database.db_engine_spec.mutate_expression_label(
-                    metric.get('label'))
+                metric.get('label'))
 
     @staticmethod
     def handle_js_int_overflow(data):
