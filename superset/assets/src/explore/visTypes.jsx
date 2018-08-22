e460d7faf5d844fa65d93275af516c9660965336
Hugh A. Miles II <hughmil3s@gmail.com>
2018-08-22 10:27:19 -0700

    Allow users to select color for big number (#5694)
    * allow user to choose color
    
    * add color control
    
    * remove rgbToHex reference
    
    * change to color picker control
    
    * add d3-color
    
    * remove module
    
    * use d3-colors
    
    * add semicolon

--- superset/assets/src/explore/visTypes.jsx
+++ superset/assets/src/explore/visTypes.jsx
@@ -1255,6 +1255,7 @@ export const visTypes = {
           ['compare_lag', 'compare_suffix'],
           ['y_axis_format', null],
           ['show_trend_line', 'start_y_axis_at_zero'],
+          ['color_picker', null],
         ],
       },
     ],
