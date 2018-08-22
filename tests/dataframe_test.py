6e8c7f7b2044ba3c59baa7892c105aef098b3a59
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-08-21 21:33:34 -0700

    [viz flow] detect TIMESTAMP, transition to line chart (#5634)
    * [viz flow] detect TIMESTAMP, transition to line chart
    
    * Refactor is_date

--- tests/dataframe_test.py
+++ tests/dataframe_test.py
@@ -122,10 +122,14 @@ class SupersetDataFrameTestCase(SupersetTestCase):
 
     def test_is_date(self):
         f = SupersetDataFrame.is_date
-        self.assertEquals(f(np.dtype('M')), True)
+        self.assertEquals(f(np.dtype('M'), ''), True)
+        self.assertEquals(f(np.dtype('f'), 'DATETIME'), True)
+        self.assertEquals(f(np.dtype('i'), 'TIMESTAMP'), True)
+        self.assertEquals(f(None, 'DATETIME'), True)
+        self.assertEquals(f(None, 'TIMESTAMP'), True)
 
-        self.assertEquals(f(None), False)
-        self.assertEquals(f(np.dtype(np.int32)), False)
+        self.assertEquals(f(None, ''), False)
+        self.assertEquals(f(np.dtype(np.int32), ''), False)
 
     def test_dedup_with_data(self):
         data = [
