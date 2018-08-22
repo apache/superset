b6283e1ff2051465617f475dfbd21051407d5140
Hugh Miles <hmiles@lyft.com>
2018-08-21 18:04:47 -0700

    add color control

--- superset/assets/src/explore/visTypes.jsx
+++ superset/assets/src/explore/visTypes.jsx
@@ -1247,6 +1247,7 @@ export const visTypes = {
           ['compare_lag', 'compare_suffix'],
           ['y_axis_format', null],
           ['show_trend_line', 'start_y_axis_at_zero'],
+          ['color', null],
         ],
       },
     ],
