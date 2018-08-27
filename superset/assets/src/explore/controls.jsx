244629a4affadd22ecc3c3cc1421610fa051ace1
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-08-27 10:42:42 -0700

    [SIP-5] Repair and refactor Horizon Chart (#5690)
    * Migrate horizon chart to react
    
    * remove unused code
    
    * rename files
    
    * update props
    
    * enable renderTrigger
    
    * fix canvas transform issue
    
    * Address Chris' comment

--- superset/assets/src/explore/controls.jsx
+++ superset/assets/src/explore/controls.jsx
@@ -383,14 +383,15 @@ export const controls = {
 
   horizon_color_scale: {
     type: 'SelectControl',
-    label: t('Horizon Color Scale'),
+    renderTrigger: true,
+    label: t('Value Domain'),
     choices: [
       ['series', 'series'],
       ['overall', 'overall'],
       ['change', 'change'],
     ],
     default: 'series',
-    description: t('Defines how the color are attributed.'),
+    description: t('series: Treat each series independently; overall: All series use the same scale; change: Show changes compared to the first data point in each series'),
   },
 
   canvas_image_rendering: {
@@ -1205,6 +1206,7 @@ export const controls = {
 
   series_height: {
     type: 'SelectControl',
+    renderTrigger: true,
     freeForm: true,
     label: t('Series Height'),
     default: '25',
