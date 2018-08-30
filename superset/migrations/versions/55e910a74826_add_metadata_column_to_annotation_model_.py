e44df932927e40588c88ed57257ab60b322d8049
Hugh Miles <hmiles@lyft.com>
2018-08-29 17:30:53 -0700

    update version

--- superset/migrations/versions/55e910a74826_add_metadata_column_to_annotation_model_.py
+++ superset/migrations/versions/55e910a74826_add_metadata_column_to_annotation_model_.py
@@ -1,14 +1,14 @@
 """add_metadata_column_to_annotation_model.py
 
 Revision ID: 55e910a74826
-Revises: 40a0a483dd12
+Revises: 1a1d627ebd8e
 Create Date: 2018-08-29 14:35:20.407743
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '55e910a74826'
-down_revision = '40a0a483dd12'
+down_revision = '1a1d627ebd8e'
 
 from alembic import op
 import sqlalchemy as sa
