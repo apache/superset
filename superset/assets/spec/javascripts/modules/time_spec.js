591d68ee9e5d435e79bcaa38c44b30315f454f14
Beto Dealmeida <roberto@dealmeida.net>
2018-08-30 23:17:51 -0700

    Improve unit tests

--- superset/assets/spec/javascripts/modules/time_spec.js
+++ superset/assets/spec/javascripts/modules/time_spec.js
@@ -66,9 +66,10 @@ describe('getPlaySliderParams', () => {
       new Date('2018-01-09'),
       new Date('2018-01-10'),
     ].map(d => d.getTime());
-    const { start, end, values, disabled } = getPlaySliderParams(timestamps, 'P2D');
+    const { start, end, step, values, disabled } = getPlaySliderParams(timestamps, 'P2D');
     expect(new Date(start)).to.eql(new Date('2018-01-01'));
     expect(new Date(end)).to.eql(new Date('2018-01-11'));
+    expect(step.addTo(start) - start).to.equal(2 * 24 * 60 * 60 * 1000);
     expect(values.map(v => new Date(v))).to.eql([
       new Date('2018-01-01'),
       new Date('2018-01-03'),
@@ -90,9 +91,10 @@ describe('getPlaySliderParams', () => {
       new Date('2018-01-10'),
     ].map(d => d.getTime());
     // 1970-01-03 was a Saturday
-    const { start, end, values, disabled } = getPlaySliderParams(timestamps, 'P1W/1970-01-03T00:00:00Z');
+    const { start, end, step, values, disabled } = getPlaySliderParams(timestamps, 'P1W/1970-01-03T00:00:00Z');
     expect(new Date(start)).to.eql(new Date('2017-12-30'));  // Saturday
     expect(new Date(end)).to.eql(new Date('2018-01-13'));  // Saturday
+    expect(step.addTo(start) - start).to.equal(7 * 24 * 60 * 60 * 1000);
     expect(values.map(v => new Date(v))).to.eql([
       new Date('2017-12-30'),
       new Date('2018-01-06'),
