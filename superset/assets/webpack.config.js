5451705690c9f6a5ed74d09e520b7275b92fb7e6
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-08-31 14:15:53 -0700

    add devtools

--- superset/assets/webpack.config.js
+++ superset/assets/webpack.config.js
@@ -147,6 +147,7 @@ const config = {
     'react/lib/ReactContext': true,
   },
   plugins,
+  devtool: isDevMode ? 'cheap-module-eval-source-map' : false,
   devServer: {
     historyApiFallback: true,
     hot: true,
