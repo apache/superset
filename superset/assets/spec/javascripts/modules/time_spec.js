f5374dbc6f06b15e98ae1b85216c081ddfdf949e
Beto Dealmeida <roberto@dealmeida.net>
2018-08-30 22:53:39 -0700

    WIP

--- superset/assets/spec/javascripts/modules/time_spec.js
+++ superset/assets/spec/javascripts/modules/time_spec.js
@@ -1,6 +1,52 @@
 import { it, describe } from 'mocha';
 import { expect } from 'chai';
-import { getPlaySliderParams } from '../../../src/modules/time';
+import { getPlaySliderParams, IsoDuration } from '../../../src/modules/time';
+
+describe('IsoDuration', () => {
+  it('adds', () => {
+    const isoDuration = new IsoDuration('PT1H');  // 1 hour
+    const timestamp = new Date('2018-01-01T00:00:00').getTime();
+    const result = isoDuration.addTo(timestamp);
+    const expected = new Date('2018-01-01T01:00:00').getTime();
+    expect(result).to.eql(expected);
+  });
+
+  it('adds ambiguous durations', () => {
+    let isoDuration = new IsoDuration('P1M');  // 1 month
+    let timestamp = new Date('2018-01-01T00:00:00').getTime();
+    let result = isoDuration.addTo(timestamp);
+    let expected = new Date('2018-02-01T00:00:00').getTime();
+    expect(result).to.eql(expected);
+
+    isoDuration = new IsoDuration('P1Y');  // 1 year
+    timestamp = new Date('2018-01-01T00:00:00').getTime();
+    result = isoDuration.addTo(timestamp);
+    expected = new Date('2019-01-01T00:00:00').getTime();
+    expect(result).to.eql(expected);
+  });
+
+  it('subtracts', () => {
+    const isoDuration = new IsoDuration('PT1H');  // 1 hour
+    const timestamp = new Date('2018-01-01T00:00:00').getTime();
+    const result = isoDuration.subtractFrom(timestamp);
+    const expected = new Date('2017-12-31T23:00:00').getTime();
+    expect(result).to.eql(expected);
+  });
+
+  it('subtracts ambiguous durations', () => {
+    let isoDuration = new IsoDuration('P1M');  // 1 month
+    let timestamp = new Date('2018-01-01T00:00:00').getTime();
+    let result = isoDuration.subtractFrom(timestamp);
+    let expected = new Date('2017-12-01T00:00:00').getTime();
+    expect(result).to.eql(expected);
+
+    isoDuration = new IsoDuration('P1Y');  // 1 year
+    timestamp = new Date('2018-01-01T00:00:00').getTime();
+    result = isoDuration.addTo(timestamp);
+    expected = new Date('2019-01-01T00:00:00').getTime();
+    expect(result).to.eql(expected);
+  });
+});
 
 describe('getPlaySliderParams', () => {
   it('is a function', () => {
@@ -20,13 +66,12 @@ describe('getPlaySliderParams', () => {
       new Date('2018-01-09'),
       new Date('2018-01-10'),
     ].map(d => d.getTime());
-    const { start, end, step, values, disabled } = getPlaySliderParams(timestamps, 'P2D');
-    expect(new Date(start)).to.eql(new Date('2018-01-01'));
-    expect(new Date(end)).to.eql(new Date('2018-01-11'));
-    expect(step).to.equal(2 * 24 * 60 * 60 * 1000);
+    const { start, end, values, disabled } = getPlaySliderParams(timestamps, 'P2D');
+    expect(new Date(start)).to.eql(new Date('2017-12-31'));
+    expect(new Date(end)).to.eql(new Date('2018-01-10'));
     expect(values.map(v => new Date(v))).to.eql([
-      new Date('2018-01-01'),
-      new Date('2018-01-03'),
+      new Date('2017-12-31'),
+      new Date('2018-01-02'),
     ]);
     expect(disabled).to.equal(false);
   });
@@ -45,10 +90,9 @@ describe('getPlaySliderParams', () => {
       new Date('2018-01-10'),
     ].map(d => d.getTime());
     // 1970-01-03 was a Saturday
-    const { start, end, step, values, disabled } = getPlaySliderParams(timestamps, 'P1W/1970-01-03T00:00:00Z');
+    const { start, end, values, disabled } = getPlaySliderParams(timestamps, 'P1W/1970-01-03T00:00:00Z');
     expect(new Date(start)).to.eql(new Date('2017-12-30'));  // Saturday
     expect(new Date(end)).to.eql(new Date('2018-01-13'));  // Saturday
-    expect(step).to.equal(7 * 24 * 60 * 60 * 1000);
     expect(values.map(v => new Date(v))).to.eql([
       new Date('2017-12-30'),
       new Date('2018-01-06'),
--- superset/assets/src/modules/time.js
+++ superset/assets/src/modules/time.js
@@ -1,38 +1,153 @@
 import parseIsoDuration from 'parse-iso-duration';
 
 
-export const getPlaySliderParams = function (timestamps, timeGrain) {
-  let start = Math.min(...timestamps);
-  let end = Math.max(...timestamps);
-  let step;
+export class IsoDuration {
+  /*
+   * Class that handles ambiguous durations like `P1M` or `P1Y`.
+   */
 
-  if (timeGrain.indexOf('/') > 0) {
+  constructor(isoDuration) {
+    this.isoDuration = isoDuration;
+
+    // special cases not handled by parse-iso-duration
+    this.pattern = /P(\d+)(M|Y)/;
+
+    this.addTo = this.addTo.bind(this);
+    this.subtractFrom = this.subtractFrom.bind(this);
+    this.operateOn = this.operateOn.bind(this);
+    this.truncate = this.truncate.bind(this);
+    this.explode = this.explode.bind(this);
+  }
+
+  addTo(timestamp) {
+    const op = (a, b) => a + b;
+    return this.operateOn(op, timestamp);
+  }
+
+  subtractFrom(timestamp) {
+    const op = (a, b) => a - b;
+    return this.operateOn(op, timestamp);
+  }
+
+  operateOn(op, timestamp) {
+    try {
+      return op(timestamp, parseIsoDuration(this.isoDuration));
+    } catch (error) {
+      const match = this.pattern.exec(this.isoDuration);
+      if (match === null) {
+        throw error;
+      }
+      const n = parseInt(match[1], 10);  // how many months or years
+
+      const date = new Date(timestamp);
+      let year = date.getFullYear();
+      let month = date.getMonth();
+
+      if (match[2] === 'M') {
+        year = op(year, Math.floor(n / 12));
+        month = op(month, (n % 12));
+        if (month < 0 || month > 11) {
+          month = op(month, -12);
+          year = op(year, 1);
+        }
+      } else if (match[2] === 'Y') {
+        year = op(year, n);
+      } else {
+        throw error;  // should never happen
+      }
+      date.setFullYear(year);
+      date.setMonth(month);
+      return date.getTime();
+    }
+  }
+
+  explode(timestamp) {
+    // Return year, month, day, hour, minute, second, millisecond
+    const date = new Date(timestamp);
+    return [
+      date.getFullYear(),
+      date.getMonth() + 1,  // fuck Javascript
+      date.getDate(),
+      date.getHours(),
+      date.getMinutes(),
+      date.getSeconds(),
+      date.getMilliseconds(),
+    ];
+  }
+
+  truncate(timestamp) {
+    /*
+     * Truncate timestamp down to duration resolution.
+     *
+     *  > const duration = new IsoDuration('PT2H');  // 2 hours
+     *  > const date = new Date('2018-01-01 23:37:00').getTime();
+     *  > new Date(duration.truncate(date));
+     *  Mon Jan 01 2018 22:00:00 GMT
+     */
+    const lowerBound = this.subtractFrom(timestamp);
+    const explodedTimestamp = this.explode(timestamp);
+    const explodedLowerBound = this.explode(lowerBound);
+    let foundDifference = false;
+    const args = [];
+    for (let i = 0; i < explodedTimestamp.length; i++) {
+      if (explodedLowerBound[i] !== explodedTimestamp[i]) {
+        foundDifference = true;
+      }
+      if (foundDifference) {
+        args.push(0);
+      } else {
+        args.push(explodedTimestamp[i]);
+      }
+    }
+    return Date.UTC(...args);
+  }
+}
+
+export const getPlaySliderParams = function (timestamps, timeGrain) {
+  const minTimestamp = Math.min(...timestamps);
+  const maxTimestamp = Math.max(...timestamps);
+  let step;
+  let reference;
+
+  if (timeGrain.indexOf('/') !== -1) {
     // Here, time grain is a time interval instead of a simple duration, either
     // `reference/duration` or `duration/reference`. We need to parse the
     // duration and make sure that start and end are in the right places. For
     // example, if `reference` is a Saturday and `duration` is 1 week (P1W)
     // then both start and end should be Saturdays.
     const parts = timeGrain.split('/', 2);
-    let reference;
     if (parts[0].endsWith('Z')) {  // ISO string
       reference = new Date(parts[0]).getTime();
-      step = parseIsoDuration(parts[1]);
+      step = new IsoDuration(parts[1]);
     } else {
       reference = new Date(parts[1]).getTime();
-      step = parseIsoDuration(parts[0]);
+      step = new IsoDuration(parts[0]);
     }
-    start = reference + step * Math.floor((start - reference) / step);
-    end = reference + step * (Math.floor((end - reference) / step) + 1);
   } else {
-    // lock start and end to the closest steps
-    step = parseIsoDuration(timeGrain);
-    start -= start % step;
-    end += step - end % step;
+    step = new IsoDuration(timeGrain);
+    reference = step.truncate(minTimestamp);
   }
 
-  const values = timeGrain != null ? [start, start + step] : [start, end];
+  // find the largest `reference + n * step` smaller than the minimum timestamp
+  let start = reference;
+  while (start < minTimestamp) {
+    start = step.addTo(start);
+  }
+  while (start > minTimestamp) {
+    start = step.subtractFrom(start);
+  }
+
+  // find the smallest `reference + n * step` larger than the maximum timestamp
+  let end = reference;
+  while (end > maxTimestamp) {
+    end = step.subtractFrom(end);
+  }
+  while (end < maxTimestamp) {
+    end = step.addTo(end);
+  }
+
+  const values = timeGrain != null ? [start, step.addTo(start)] : [start, end];
   const disabled = timestamps.every(timestamp => timestamp === null);
 
   return { start, end, step, values, disabled };
 };
-
