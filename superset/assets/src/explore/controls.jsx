a411516ff57c73c5dfca41838ed84928dc713041
Christine Chambers <christine.d.hang@gmail.com>
2018-09-04 18:08:00 -0700

    bug: don't show query overlay when panning mapbox maps (#5814)
    * bug: don't show query overlay when panning mapbox maps
    
    Since we don't want to prompt user to rerun query every time they pan a map, prevent the query overlay from showing by setting dontRefreshOnChange for viewport_latitude and viewport_longitude controls.
    
    * Prevent query overlay on viewport zoom also.

--- superset/assets/src/explore/controls.jsx
+++ superset/assets/src/explore/controls.jsx
@@ -1806,6 +1806,8 @@ export const controls = {
     default: 11,
     description: t('Zoom level of the map'),
     places: 8,
+    // Viewport zoom shouldn't prompt user to re-run query
+    dontRefreshOnChange: true,
   },
 
   viewport_latitude: {
@@ -1815,6 +1817,8 @@ export const controls = {
     isFloat: true,
     description: t('Latitude of default viewport'),
     places: 8,
+    // Viewport latitude changes shouldn't prompt user to re-run query
+    dontRefreshOnChange: true,
   },
 
   viewport_longitude: {
@@ -1824,6 +1828,8 @@ export const controls = {
     isFloat: true,
     description: t('Longitude of default viewport'),
     places: 8,
+    // Viewport longitude changes shouldn't prompt user to re-run query
+    dontRefreshOnChange: true,
   },
 
   render_while_dragging: {
