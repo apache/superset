562e6cfde1d7d14334456e6a87526df8fdd372c8
Michelle Thomas <michelle.q.thomas@gmail.com>
2018-09-05 13:15:36 -0700

    Adding Cypress to tox

---
+++ superset/assets/cypress_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+superset/bin/superset db upgrade
+superset/bin/superset load_test_users
+superset/bin/superset load_examples
+superset/bin/superset init
+superset/bin/superset runserver &
+
+cd "$(dirname "$0")"
+
+npm install -g yarn
+yarn
+npm run build
+npm run cypress run
+kill %1
