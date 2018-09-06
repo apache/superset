ab6bef757d5af71eefcef44d6e7e778f9bb96ec4
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-09-06 05:29:41 +0000

    Fix tests

--- superset/assets/spec/javascripts/sqllab/actions_spec.js
+++ superset/assets/spec/javascripts/sqllab/actions_spec.js
@@ -56,7 +56,7 @@ describe('async actions', () => {
     });
 
     it('calls querySuccess on ajax success', () => {
-      ajaxStub.yieldsTo('success', { data: '' });
+      ajaxStub.yieldsTo('success', '{ "data": "" }');
       makeRequest();
       expect(dispatch.callCount).to.equal(2);
       expect(dispatch.getCall(1).args[0].type).to.equal(actions.QUERY_SUCCESS);
@@ -84,13 +84,6 @@ describe('async actions', () => {
     it('calls startQuery', () => {
       makeRequest();
       expect(dispatch.args[0][0].type).to.equal(actions.START_QUERY);
-    });
-
-    it('calls querySuccess on ajax success', () => {
-      ajaxStub.yieldsTo('success', { data: '' });
-      makeRequest();
-      expect(dispatch.callCount).to.equal(2);
-      expect(dispatch.getCall(1).args[0].type).to.equal(actions.QUERY_SUCCESS);
     });
 
     it('calls queryFailed on ajax error', () => {
