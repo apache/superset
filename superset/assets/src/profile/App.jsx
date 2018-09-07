62a3db6e7593c0567e391101fff8ad05f7388a3e
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-09-06 18:23:29 -0700

    enable react-hot-reload for Explore, Dashboard, Profile and Sqllab

---
+++ superset/assets/src/profile/App.jsx
@@ -0,0 +1,19 @@
+/* eslint no-unused-vars: 0 */
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+import App from './components/App';
+import { appSetup } from '../common';
+
+import './main.css';
+
+appSetup();
+
+const profileViewContainer = document.getElementById('app');
+const bootstrap = JSON.parse(profileViewContainer.getAttribute('data-bootstrap'));
+
+const Application = () => (
+  <App user={bootstrap.user} />
+);
+
+export default hot(module)(Application);
