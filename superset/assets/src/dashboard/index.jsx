5306110e544710825267820bf935e181c33b8232
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-09-07 10:50:21 -0700

    fix lint

--- superset/assets/src/dashboard/index.jsx
+++ superset/assets/src/dashboard/index.jsx
@@ -2,7 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app'),
-);
+ReactDOM.render(<App />, document.getElementById('app'));
