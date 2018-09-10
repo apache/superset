73db918fbe0995375b442a1596a402ca27eac9d2
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-09-10 14:48:06 -0700

    Upgrade to React==16.4.1 &  Enzyme==3.3.0 (#5359)
    * Bumping react==16.4.1 &  enzyme==3.3.0
    
    The upgrade was pretty smooth except for a cryptic message coming
    out of react-select around running multiple copies of React. It turns
    out the `common` bundle had React and was conflicting with explore and
    dashboard apps, only in 16.x. This somehow wasn't a problem in 15.x
    outside of the wasted resources.
    
    Running 16.4 should bring in all sorts of perf improvements and features
    we've all been waiting for.
    
    https://reactjs.org/blog/2017/09/26/react-v16.0.html
    
    TODO: react-bootstrap-datetimepicker isn't compatible with React 16
    
    * Trying to deprecate react-bootstrap-datetime
    
    * Moving forward
    
    * Reintroducing tests

--- superset/assets/spec/javascripts/explore/components/DateFilterControl_spec.jsx
+++ superset/assets/spec/javascripts/explore/components/DateFilterControl_spec.jsx
@@ -4,7 +4,7 @@ import sinon from 'sinon';
 import { expect } from 'chai';
 import { describe, it, beforeEach } from 'mocha';
 import { shallow } from 'enzyme';
-import { Button } from 'react-bootstrap';
+import { Button, Label } from 'react-bootstrap';
 
 import DateFilterControl from '../../../../src/explore/components/controls/DateFilterControl';
 import ControlHeader from '../../../../src/explore/components/ControlHeader';
@@ -29,36 +29,28 @@ describe('DateFilterControl', () => {
     expect(controlHeader).to.have.lengthOf(1);
   });
   it('renders 3 Buttons', () => {
-    const label = wrapper.find('.label').first();
+    const label = wrapper.find(Label).first();
     label.simulate('click');
     setTimeout(() => {
       expect(wrapper.find(Button)).to.have.length(3);
     }, 10);
   });
   it('loads the right state', () => {
-    const label = wrapper.find('.label').first();
+    const label = wrapper.find(Label).first();
     label.simulate('click');
     setTimeout(() => {
       expect(wrapper.state().num).to.equal('90');
     }, 10);
   });
-  it('sets now and closes', () => {
-    const label = wrapper.find('.now').first();
-    label.simulate('click');
-    setTimeout(() => {
-      expect(wrapper.state().free).to.equal('now');
-      expect(wrapper.find('.popover')).to.have.length(0);
-    }, 10);
-  });
   it('renders 2 dimmed sections', () => {
-    const label = wrapper.find('.label').first();
+    const label = wrapper.find(Label).first();
     label.simulate('click');
     setTimeout(() => {
       expect(wrapper.find(Button)).to.have.length(3);
     }, 10);
   });
   it('opens and closes', () => {
-    const label = wrapper.find('.label').first();
+    const label = wrapper.find(Label).first();
     label.simulate('click');
     setTimeout(() => {
       expect(wrapper.find('.popover')).to.have.length(1);
