c8b83ec7249fa3d988a94392d5660fe0fd4e5790
Chris Williams <chris.williams@airbnb.com>
2018-09-12 13:31:17 -0700

    [superset client][charts][tests] fix and improve chartActions_spec

--- superset/assets/src/chart/chartAction.js
+++ superset/assets/src/chart/chartAction.js
@@ -176,21 +176,19 @@ export function runQuery(formData, force = false, timeout = 60, key) {
         } else if (err.statusText === 'AbortError') {
           dispatch(chartUpdateStopped(key));
         } else {
-          let errObject;
+          let errObject = err;
           if (err.responseJSON) {
             errObject = err.responseJSON;
           } else if (err.stack) {
             errObject = {
-              error: t('Unexpected error: ') + err.description,
+              error:
+                t('Unexpected error: ') +
+                (err.description || t('(no description, click to see stack trace)')),
               stacktrace: err.stack,
             };
           } else if (err.responseText && err.responseText.indexOf('CSRF') >= 0) {
             errObject = {
               error: COMMON_ERR_MESSAGES.SESSION_TIMED_OUT,
-            };
-          } else {
-            errObject = {
-              error: t('Unexpected error.'),
             };
           }
           dispatch(chartUpdateFailed(errObject, key));
