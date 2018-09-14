a0df8d5867c5f948dcf74b223e4a3a9b684c95c4
Beto Dealmeida <roberto@dealmeida.net>
2018-09-14 16:18:08 -0700

    Merge heads in alembic

---
+++ superset/migrations/versions/7c96c5fd462e_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 7c96c5fd462e
+Revises: ('55e910a74826', '05cddfb1eea3')
+Create Date: 2018-09-14 16:17:57.967417
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '7c96c5fd462e'
+down_revision = ('55e910a74826', '05cddfb1eea3')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
