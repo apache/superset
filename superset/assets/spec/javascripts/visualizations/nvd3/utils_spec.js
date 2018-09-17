f264468d7a5cb01c07850a5f311b39bed97a25c5
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-09-16 20:38:30 -0700

    [SIP-5] Refactor nvd3 (#5838)
    * move into folder and scaffold adaptor
    
    * extract width and height
    
    * remove jquery
    
    * extract showBrush
    
    * extract lineInterpolation
    
    * extract xAxisFormat and yAxisFormat
    
    * extract annotationData
    
    * extract xTicksLayout and colorScheme
    
    * extract showXXX
    
    * extract x and y axis labels
    
    * extract showminmax
    
    * extract pie chart props
    
    * extract area chart props
    
    * extract logscale and yAxisBounds
    
    * extract margin
    
    * extract bubble props x,y,size
    
    * extract contribution, comparisonType and color_picker
    
    * remove the last fd.xxx
    
    * remove unnecessary variables
    
    * remove slice.container
    
    * fix unit test reference
    
    * Rewrite logic to compute max label lengths to use only d3, not jquery.
    
    * extract annotationLayers and no more slice.xxx in nvd3vis
    
    * rename x, y, size to xField, yField, sizeField
    
    * use arrow function
    
    * move tooltip function
    
    * extract helper functions into utils
    
    * remove unused argument
    
    * fix height calculation and show bar labels
    
    * rename function
    
    * update unit test
    
    * organize tooltip generator
    
    * update line_multi
    
    * Add proptypes for data
    
    * list proptypes for data
    
    * extract tooltip function for bubble chart
    
    * rename variables
    
    * remove console.log
    
    * enumerate vizTypes and pieLabelType
    
    * parse maxBubble
    
    * use new color scale
    
    * fix import"
    
    * remove line

--- superset/assets/spec/javascripts/visualizations/nvd3/utils_spec.js
+++ superset/assets/spec/javascripts/visualizations/nvd3/utils_spec.js
@@ -1,13 +1,13 @@
 import { expect } from 'chai';
 
-import { formatLabel } from '../../../src/visualizations/nvd3_vis';
+import { formatLabel, tryNumify } from '../../../../src/visualizations/nvd3/utils';
 
-describe('nvd3 viz', () => {
+describe('nvd3/utils', () => {
   const verboseMap = {
     foo: 'Foo',
     bar: 'Bar',
   };
-  describe('formatLabel', () => {
+  describe('formatLabel()', () => {
     it('formats simple labels', () => {
       expect(formatLabel('foo')).to.equal('foo');
       expect(formatLabel(['foo'])).to.equal('foo');
@@ -22,6 +22,14 @@ describe('nvd3 viz', () => {
     it('deals with time shift properly', () => {
       expect(formatLabel(['foo', '1 hour offset'], verboseMap)).to.equal('Foo, 1 hour offset');
       expect(formatLabel(['foo', 'bar', 'baz', '2 hours offset'], verboseMap)).to.equal('Foo, Bar, baz, 2 hours offset');
+    });
+  });
+  describe('tryNumify()', () => {
+    it('tryNumify works as expected', () => {
+      expect(tryNumify(5)).to.equal(5);
+      expect(tryNumify('5')).to.equal(5);
+      expect(tryNumify('5.1')).to.equal(5.1);
+      expect(tryNumify('a string')).to.equal('a string');
     });
   });
 });
