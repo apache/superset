473a4b591691460f7d893a3c67c33a1fd236d8ee
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-09-19 16:23:00 -0700

    add sunburst test

--- superset/assets/cypress/integration/explore/visualizations/dual_line.js
+++ superset/assets/cypress/integration/explore/visualizations/dual_line.js
@@ -3,7 +3,6 @@ describe('Dual Line', () => {
     datasource: '3__table',
     viz_type: 'dual_line',
     slice_id: 58,
-    url_params: {},
     granularity_sqla: 'ds',
     time_grain_sqla: 'P1D',
     time_range: '100+years+ago+:+now',
