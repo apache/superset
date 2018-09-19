2e2c7ff725a45c0599a57b1f8212cd75d109fa65
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-09-18 23:35:36 -0700

    [SIP-6] Add reactify function and convert world map to new directory structure. (#5893)
    * reactify world map
    
    * add createAdaptor
    
    * fix typo
    
    * add schema
    
    * move directory
    
    * remove unnecessary lines
    
    * make setRef a function
    
    * convert keys to camelcase
    
    * add unit test
    
    * update formatting
    
    * add check for displayName
    
    * pass width and height as separate inputs

---
+++ superset/assets/src/visualizations/WorldMap/ReactWorldMap.js
@@ -0,0 +1,4 @@
+import reactify from '../../utils/reactify';
+import WorldMap from './WorldMap';
+
+export default reactify(WorldMap);
---
+++ superset/assets/src/visualizations/WorldMap/adaptor.jsx
@@ -0,0 +1,5 @@
+import createAdaptor from '../../utils/createAdaptor';
+import WorldMap from './ReactWorldMap';
+import transformProps from './transformProps';
+
+export default createAdaptor(WorldMap, transformProps);
