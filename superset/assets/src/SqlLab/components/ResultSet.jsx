62382c2c75c3f22080742a7987e1824d24e05aa1
Junda Yang <youngyjd@gmail.com>
2018-09-20 11:20:36 -0700

    fix sqlab progress bar and status inconsistency (#5848)

--- superset/assets/src/SqlLab/components/ResultSet.jsx
+++ superset/assets/src/SqlLab/components/ResultSet.jsx
@@ -200,7 +200,7 @@ export default class ResultSet extends React.PureComponent {
     }
     let progressBar;
     let trackingUrl;
-    if (query.progress > 0 && query.state === 'running') {
+    if (query.progress > 0) {
       progressBar = (
         <ProgressBar
           striped
