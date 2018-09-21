edfaf91887b4a017cbb831ee49e273c42db62f66
Chris Williams <chris.williams@airbnb.com>
2018-09-21 15:59:51 -0700

    [cypress] add sqllab/sourcePanel integration tests

---
+++ superset/assets/cypress/integration/sqllab/sourcePanel.js
@@ -0,0 +1,53 @@
+describe('SqlLab datasource panel', () => {
+  beforeEach(() => {
+    cy.login();
+    cy.server();
+    cy.visit('/superset/sqllab');
+  });
+
+  it('renders dropdowns to select database, schema, and table', () => {
+    cy.get('.sql-toolbar .Select').then((nodes) => {
+      expect(nodes).to.have.length(3);
+    });
+  });
+
+  it('creates a table schema and preview when a database, schema, and table are selected', () => {
+    cy.get('.sql-toolbar .table-schema').should('not.exist');
+    cy.get('.SouthPane .tab-content .filterable-table-container').should('not.exist');
+
+    cy.get('.sql-toolbar .Select')
+      .eq(0)
+      .within(() => {
+        cy.get('input').type('main{enter}', { force: true });
+      })
+      .then(() => {
+        cy.get('.sql-toolbar .Select')
+          .eq(1)
+          .within(() => {
+            cy.get('input').type('main{enter}', { force: true });
+          });
+      })
+      .then(() => {
+        cy.get('.sql-toolbar .Select')
+          .eq(2)
+          .within(() => {
+            cy.get('input').type('birth_names{enter}', { force: true });
+          });
+      })
+      .then(() => {
+        cy.get('.sql-toolbar .table-schema').should('have.length', 1);
+        cy.get('.SouthPane .tab-content .filterable-table-container').should('have.length', 1);
+      })
+      .then(() => {
+        cy.get('.sql-toolbar .Select')
+          .eq(2)
+          .within(() => {
+            cy.get('input').type('logs{enter}', { force: true });
+          });
+      })
+      .then(() => {
+        cy.get('.sql-toolbar .table-schema').should('have.length', 2);
+        cy.get('.SouthPane .tab-content .filterable-table-container').should('have.length', 2);
+      });
+  });
+});
