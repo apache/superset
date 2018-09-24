166b35b3ff8e40d22b4e32a6b55497e381f5281e
pphszx <pphszx@gmail.com>
2018-09-24 18:03:55 +0800

    [bugfix] NoneType df has no attribute to_dict in IFrame

--- superset/viz.py
+++ superset/viz.py
@@ -1826,6 +1826,8 @@ class IFrameViz(BaseViz):
     def get_df(self, query_obj=None):
         return None
 
+    def get_data(self, df):
+        return None
 
 class ParallelCoordinatesViz(BaseViz):
 
