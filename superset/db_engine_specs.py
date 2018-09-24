b988748aa6bc1e235adc359302c1c062652bdc5e
Victor Noel <victor.noel@brennus-analytics.com>
2018-09-24 10:52:07 +0200

    [bugfix] Fix case sensitivity of date column for postgres
    Closes #5886

--- superset/db_engine_specs.py
+++ superset/db_engine_specs.py
@@ -397,15 +397,15 @@ class PostgresBaseEngineSpec(BaseEngineSpec):
     engine = ''
 
     time_grain_functions = {
-        None: '{col}',
-        'PT1S': "DATE_TRUNC('second', {col}) AT TIME ZONE 'UTC'",
-        'PT1M': "DATE_TRUNC('minute', {col}) AT TIME ZONE 'UTC'",
-        'PT1H': "DATE_TRUNC('hour', {col}) AT TIME ZONE 'UTC'",
-        'P1D': "DATE_TRUNC('day', {col}) AT TIME ZONE 'UTC'",
-        'P1W': "DATE_TRUNC('week', {col}) AT TIME ZONE 'UTC'",
-        'P1M': "DATE_TRUNC('month', {col}) AT TIME ZONE 'UTC'",
-        'P0.25Y': "DATE_TRUNC('quarter', {col}) AT TIME ZONE 'UTC'",
-        'P1Y': "DATE_TRUNC('year', {col}) AT TIME ZONE 'UTC'",
+        None: '"{col}"',
+        'PT1S': "DATE_TRUNC('second', \"{col}\") AT TIME ZONE 'UTC'",
+        'PT1M': "DATE_TRUNC('minute', \"{col}\") AT TIME ZONE 'UTC'",
+        'PT1H': "DATE_TRUNC('hour', \"{col}\") AT TIME ZONE 'UTC'",
+        'P1D': "DATE_TRUNC('day', \"{col}\") AT TIME ZONE 'UTC'",
+        'P1W': "DATE_TRUNC('week', \"{col}\") AT TIME ZONE 'UTC'",
+        'P1M': "DATE_TRUNC('month', \"{col}\") AT TIME ZONE 'UTC'",
+        'P0.25Y': "DATE_TRUNC('quarter', \"{col}\") AT TIME ZONE 'UTC'",
+        'P1Y': "DATE_TRUNC('year', \"{col}\") AT TIME ZONE 'UTC'",
     }
 
     @classmethod
