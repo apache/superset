970eecfb9b0cfd0af3f1bd0a69d6b5a735d5d1c2
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-09-25 14:45:13 -0700

    fix unit test

--- superset/assets/spec/javascripts/visualizations/table_spec.jsx
+++ superset/assets/spec/javascripts/visualizations/table_spec.jsx
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import $ from 'jquery';
 import '../../helpers/shim';
-import tableVis from '../../../src/visualizations/table';
+import tableVis from '../../../src/visualizations/Table/adaptor';
 
 describe('table viz', () => {
   const div = '<div id="slice-container"><div class="dataTables_wrapper"></div></div>';
@@ -17,6 +17,7 @@ describe('table viz', () => {
     getFilters: () => ({}),
     removeFilter() {},
     addFilter() {},
+    width: () => 0,
     height: () => 0,
   };
   const basePayload = {
