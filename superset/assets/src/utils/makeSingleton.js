ed7a53a59acbfeab1416d3ea0530116c241e2383
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-10-03 14:09:26 -0700

    add test structure

--- superset/assets/src/utils/makeSingleton.js
+++ superset/assets/src/utils/makeSingleton.js
@@ -1,17 +1,17 @@
 export default function makeSingleton(BaseClass) {
   let singleton;
 
-  function getInstance() {
+  return function getInstance() {
     if (!singleton) {
       singleton = new BaseClass();
     }
     return singleton;
-  }
+  };
+}
 
-  const staticFunctions = Object.getOwnPropertyNames(BaseClass.prototype)
-    .filter(fn => fn !== 'constructor')
+export function surfaceStaticFunctions(getInstance, functionNames) {
+  const staticFunctions = functionNames
     .reduce((all, fn) => {
-      console.log('fn', fn);
       const functions = all;
       functions[fn] = function (...args) {
         return getInstance()[fn](...args);
