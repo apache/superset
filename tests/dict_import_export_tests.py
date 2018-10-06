e555d994ef842ae0beace43e80fb95238537a932
Timi Fasubaa <timi.fasubaa@airbnb.com>
2018-10-05 20:13:20 -0700

    remove future imports

--- tests/dict_import_export_tests.py
+++ tests/dict_import_export_tests.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
 """Unit tests for Superset"""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 import json
 import unittest
 
@@ -25,7 +20,6 @@ ID_PREFIX = 20000
 
 class DictImportExportTests(SupersetTestCase):
     """Testing export import functionality for dashboards"""
-
     def __init__(self, *args, **kwargs):
         super(DictImportExportTests, self).__init__(*args, **kwargs)
 
--- tests/utils_tests.py
+++ tests/utils_tests.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 from datetime import date, datetime, time, timedelta
 from decimal import Decimal
 import unittest
