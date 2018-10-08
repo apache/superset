58ad704c6fa1555372e91cabe9da21720cde1a7e
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-10-08 13:52:02 -0700

    update test

---
+++ superset/assets/spec/javascripts/visualizations/models/Plugin_spec.js
@@ -0,0 +1,31 @@
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+import Plugin from '../../../../src/visualizations/core/models/Plugin';
+
+describe('Plugin', () => {
+  it('exists', () => {
+    expect(Plugin).to.not.equal(undefined);
+  });
+
+  describe('new Plugin()', () => {
+    it('creates a new plugin', () => {
+      const plugin = new Plugin();
+      expect(plugin).to.be.instanceof(Plugin);
+    });
+  });
+
+  describe('.configure(config, replace)', () => {
+    it('extends the default config with given config when replace is not set or false', () => {
+
+    });
+    it('replaces the default config with given config when replace is true', () => {
+
+    });
+  });
+
+  describe('.resetConfig()', () => {
+    it('resets config back to default', () => {
+
+    });
+  });
+});
