0deeecb6569f725d5b157ab0aebb0291420d17fb
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-10-08 15:13:46 -0700

    update plugin unit test

--- superset/assets/spec/javascripts/visualizations/models/Plugin_spec.js
+++ superset/assets/spec/javascripts/visualizations/models/Plugin_spec.js
@@ -16,22 +16,33 @@ describe('Plugin', () => {
 
   describe('.configure(config, replace)', () => {
     it('extends the default config with given config when replace is not set or false', () => {
-
+      const plugin = new Plugin();
+      plugin.configure({ key: 'abc', foo: 'bar' });
+      plugin.configure({ key: 'def' });
+      expect(plugin.config).to.deep.equal({ key: 'def', foo: 'bar' });
     });
     it('replaces the default config with given config when replace is true', () => {
-
+      const plugin = new Plugin();
+      plugin.configure({ key: 'abc', foo: 'bar' });
+      plugin.configure({ key: 'def' }, true);
+      expect(plugin.config).to.deep.equal({ key: 'def' });
     });
     it('returns the plugin itself', () => {
-
+      const plugin = new Plugin();
+      expect(plugin.configure({ key: 'abc' })).to.equal(plugin);
     });
   });
 
   describe('.resetConfig()', () => {
     it('resets config back to default', () => {
-
+      const plugin = new Plugin();
+      plugin.configure({ key: 'abc', foo: 'bar' });
+      plugin.resetConfig();
+      expect(plugin.config).to.deep.equal({});
     });
     it('returns the plugin itself', () => {
-
+      const plugin = new Plugin();
+      expect(plugin.resetConfig()).to.equal(plugin);
     });
   });
 });
