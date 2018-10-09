f2879056bc9943cf7331eba0733bc57c5e06e377
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-10-09 14:46:28 -0700

    add credit field

--- superset/assets/src/visualizations/core/models/ChartMetadata.js
+++ superset/assets/src/visualizations/core/models/ChartMetadata.js
@@ -1,11 +1,13 @@
 export default class ChartMetadata {
   constructor({
     name,
+    credits = [],
     description,
     thumbnail,
     show = true,
   }) {
     this.name = name;
+    this.credits = credits;
     this.description = description;
     this.thumbnail = thumbnail;
     this.show = show;
