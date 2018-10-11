b260c37d560bd33d71d5e2570c12b9dcdf7564b6
timifasubaa <30888507+timifasubaa@users.noreply.github.com>
2018-10-11 09:46:25 -0700

    remove future imports (#6050)

--- tests/fixtures/datasource.py
+++ tests/fixtures/datasource.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
 """Fixtures for test_datasource.py"""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 datasource_post = {
     'id': None,
     'column_formats': {'ratio': '.2%'},
--- tests/viz_tests.py
+++ tests/viz_tests.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 from datetime import datetime
 import uuid
 
