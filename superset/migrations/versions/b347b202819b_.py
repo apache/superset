27200628e01a315e248d50ffeba6440efb02a66f
timifasubaa <30888507+timifasubaa@users.noreply.github.com>
2018-10-11 14:22:56 -0700

    remove future imports from db migrations (#6063)

--- superset/migrations/versions/b347b202819b_.py
+++ superset/migrations/versions/b347b202819b_.py
@@ -6,11 +6,6 @@ Revises: ('33d996bcc382', '65903709c321')
 Create Date: 2016-09-19 17:22:40.138601
 
 """
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 # revision identifiers, used by Alembic.
 revision = 'b347b202819b'
 down_revision = ('33d996bcc382', '65903709c321')
