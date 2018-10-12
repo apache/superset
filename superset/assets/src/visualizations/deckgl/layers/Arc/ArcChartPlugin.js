761782849c25e990da293d87171c5735b895cd70
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-10-12 11:16:55 -0700

    Add ChartPlugin and metadata for deck.gl vis

---
+++ superset/assets/src/visualizations/deckgl/layers/Arc/ArcChartPlugin.js
@@ -0,0 +1,19 @@
+import ChartPlugin from '../../../core/models/ChartPlugin';
+import ChartMetadata from '../../../core/models/ChartMetadata';
+import thumbnail from './images/thumbnail.png';
+
+const metadata = new ChartMetadata({
+  name: 'deck.gl Arc',
+  description: '',
+  credits: ['https://uber.github.io/deck.gl'],
+  thumbnail,
+});
+
+export default class ArcChartPlugin extends ChartPlugin {
+  constructor() {
+    super({
+      metadata,
+      loadChart: () => import('./Arc.jsx'),
+    });
+  }
+}
