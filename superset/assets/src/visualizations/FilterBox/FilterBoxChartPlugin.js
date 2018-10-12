cc2dbc9f8c176058ef6b5cabc1bf9403d669688b
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-10-11 20:55:40 -0700

    Create chart plugins and metadata (#6072)
    * add credit field
    
    * change image size limit for url-loader
    
    * create chord ChartPlugin
    
    * Update config to include original file name in the output
    
    * add calendar
    
    * add country map
    
    * add eventflow
    
    * add thumbnails
    
    * add eventflow, filterbox, force-directed and heatmap
    
    * Add histogram, horizon, mapbox, paired t-test, parallel coordinates
    
    * Create plugins for the rest one-off charts
    
    * ignore chartplugin files from istanbul

---
+++ superset/assets/src/visualizations/FilterBox/FilterBoxChartPlugin.js
@@ -0,0 +1,20 @@
+import ChartPlugin from '../core/models/ChartPlugin';
+import ChartMetadata from '../core/models/ChartMetadata';
+import transformProps from './transformProps';
+import thumbnail from './images/thumbnail.png';
+
+const metadata = new ChartMetadata({
+  name: 'Filter Box',
+  description: 'A multi filter, multi-choice filter box to make dashboards interactive',
+  thumbnail,
+});
+
+export default class FilterBoxChartPlugin extends ChartPlugin {
+  constructor() {
+    super({
+      metadata,
+      transformProps,
+      loadChart: () => import('./FilterBox.jsx'),
+    });
+  }
+}
---
+++ superset/assets/src/visualizations/TimeTable/TimeTableChartPlugin.js
@@ -0,0 +1,20 @@
+import ChartPlugin from '../core/models/ChartPlugin';
+import ChartMetadata from '../core/models/ChartMetadata';
+import transformProps from './transformProps';
+import thumbnail from './images/thumbnail.png';
+
+const metadata = new ChartMetadata({
+  name: 'Time-series Table',
+  description: '',
+  thumbnail,
+});
+
+export default class TimeTableChartPlugin extends ChartPlugin {
+  constructor() {
+    super({
+      metadata,
+      transformProps,
+      loadChart: () => import('./TimeTable.jsx'),
+    });
+  }
+}
