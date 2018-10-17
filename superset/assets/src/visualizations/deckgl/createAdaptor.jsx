ba785571078645ec7a781b0fcd5542c0495e7b47
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-10-17 12:58:14 -0700

    remove deck.gl creataAdaptor

--- superset/assets/src/visualizations/deckgl/createAdaptor.jsx
+++
@@ -1,6 +0,0 @@
-import createAdaptor from '../../utils/createAdaptor';
-import transformProps from './transformProps';
-
-export default function createDeckGLAdaptor(Component) {
-  return createAdaptor(Component, transformProps);
-}
