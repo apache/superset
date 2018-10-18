951013543f6291eaa6e67c30bd23def2df56eec2
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-10-18 15:27:06 -0700

    define chart presets

---
+++ superset/assets/src/visualizations/presets/HierarchyChartPreset.js
@@ -0,0 +1,17 @@
+import Preset from '../core/models/Preset';
+import PartitionChartPlugin from '../Partition/PartitionChartPlugin';
+import SunburstChartPlugin from '../Sunburst/SunburstChartPlugin';
+import TreemapChartPlugin from '../Treemap/TreemapChartPlugin';
+
+export default class HierarchyChartPreset extends Preset {
+  constructor() {
+    super({
+      name: 'Hierarchy charts',
+      plugins: [
+        new PartitionChartPlugin().configure({ key: 'partition' }),
+        new SunburstChartPlugin().configure({ key: 'sunburst' }),
+        new TreemapChartPlugin().configure({ key: 'treemap' }),
+      ],
+    });
+  }
+}
