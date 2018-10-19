e7d025e32d457648c4e7e35be83c3bf539f61505
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-10-19 11:44:31 -0700

    rename manager to registry

--- superset/assets/spec/javascripts/modules/colors/ColorSchemeRegistry_spec.js
+++ superset/assets/spec/javascripts/modules/colors/ColorSchemeRegistry_spec.js
@@ -2,7 +2,7 @@ import ColorSchemeRegistry from '../../../../src/modules/colors/ColorSchemeRegis
 import CategoricalScheme from '../../../../src/modules/colors/CategoricalScheme';
 
 describe('ColorSchemeRegistry', () => {
-  const manager = new ColorSchemeRegistry();
+  const registry = new ColorSchemeRegistry();
   const SCHEME1 = new CategoricalScheme({
     name: 'test',
     colors: ['red', 'green', 'blue'],
@@ -17,48 +17,48 @@ describe('ColorSchemeRegistry', () => {
   });
 
   beforeEach(() => {
-    manager.clear();
-    manager.registerValue('test', SCHEME1);
-    manager.registerValue('test2', SCHEME2);
-    manager.setDefaultSchemeName('test');
+    registry.clear();
+    registry.registerValue('test', SCHEME1);
+    registry.registerValue('test2', SCHEME2);
+    registry.setDefaultSchemeName('test');
   });
   describe('.get()', () => {
     it('.get() returns default color scheme', () => {
-      const scheme = manager.get();
+      const scheme = registry.get();
       expect(scheme).toEqual(SCHEME1);
     });
     it('.get(name) returns color scheme with specified name', () => {
-      const scheme = manager.get('test2');
+      const scheme = registry.get('test2');
       expect(scheme).toEqual(SCHEME2);
     });
   });
   describe('.getDefaultSchemeName()', () => {
     it('returns default scheme name', () => {
-      const name = manager.getDefaultSchemeName();
+      const name = registry.getDefaultSchemeName();
       expect(name).toBe('test');
     });
   });
   describe('.setDefaultSchemeName()', () => {
     it('set default scheme name', () => {
-      manager.setDefaultSchemeName('test2');
-      const name = manager.getDefaultSchemeName();
+      registry.setDefaultSchemeName('test2');
+      const name = registry.getDefaultSchemeName();
       expect(name).toBe('test2');
-      manager.setDefaultSchemeName('test');
+      registry.setDefaultSchemeName('test');
     });
     it('returns the ColorSchemeRegistry instance', () => {
-      const instance = manager.setDefaultSchemeName('test');
-      expect(instance).toBe(manager);
+      const instance = registry.setDefaultSchemeName('test');
+      expect(instance).toBe(registry);
     });
   });
   describe('.registerValue(name, colors)', () => {
     it('sets schemename and color', () => {
-      manager.registerValue('test3', SCHEME3);
-      const scheme = manager.get('test3');
+      registry.registerValue('test3', SCHEME3);
+      const scheme = registry.get('test3');
       expect(scheme).toEqual(SCHEME3);
     });
     it('returns the ColorSchemeRegistry instance', () => {
-      const instance = manager.registerValue('test3', SCHEME3);
-      expect(instance).toBe(manager);
+      const instance = registry.registerValue('test3', SCHEME3);
+      expect(instance).toBe(registry);
     });
   });
 });
