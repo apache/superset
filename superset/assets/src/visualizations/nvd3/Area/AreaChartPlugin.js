fe000efa3bb0953ff52a6162e334812e1062679f
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-10-18 17:59:30 -0700

    Add annotation information to ChartMetadata (#6136)
    * add annotations information to ChartMetadata
    
    * rename fields

--- superset/assets/src/visualizations/nvd3/Area/AreaChartPlugin.js
+++ superset/assets/src/visualizations/nvd3/Area/AreaChartPlugin.js
@@ -2,11 +2,16 @@ import ChartPlugin from '../../core/models/ChartPlugin';
 import ChartMetadata from '../../core/models/ChartMetadata';
 import transformProps from '../transformProps';
 import thumbnail from './images/thumbnail.png';
+import { ANNOTATION_TYPES } from '../../../modules/AnnotationTypes';
 
 const metadata = new ChartMetadata({
   name: 'Area Chart',
   description: '',
   credits: ['http://nvd3.org'],
+  supportedAnnotationTypes: [
+    ANNOTATION_TYPES.INTERVAL,
+    ANNOTATION_TYPES.EVENT,
+  ],
   thumbnail,
 });
 
