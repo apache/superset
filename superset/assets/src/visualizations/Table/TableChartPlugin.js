c0ed0855c372d88f24f20e5c6239427632465350
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-10-18 17:59:30 -0700

    Add annotation information to ChartMetadata (#6136)
    * add annotations information to ChartMetadata
    
    * rename fields

--- superset/assets/src/visualizations/Table/TableChartPlugin.js
+++ superset/assets/src/visualizations/Table/TableChartPlugin.js
@@ -2,10 +2,15 @@ import ChartPlugin from '../core/models/ChartPlugin';
 import ChartMetadata from '../core/models/ChartMetadata';
 import transformProps from './transformProps';
 import thumbnail from './images/thumbnail.png';
+import { ANNOTATION_TYPES } from '../../modules/AnnotationTypes';
 
 const metadata = new ChartMetadata({
   name: 'Table',
   description: '',
+  canBeAnnotationTypes: [
+    ANNOTATION_TYPES.EVENT,
+    ANNOTATION_TYPES.INTERVAL,
+  ],
   thumbnail,
 });
 
--- superset/assets/src/visualizations/nvd3/Line/LineChartPlugin.js
+++ superset/assets/src/visualizations/nvd3/Line/LineChartPlugin.js
@@ -2,11 +2,21 @@ import ChartPlugin from '../../core/models/ChartPlugin';
 import ChartMetadata from '../../core/models/ChartMetadata';
 import transformProps from '../transformProps';
 import thumbnail from './images/thumbnail.png';
+import { ANNOTATION_TYPES } from '../../../modules/AnnotationTypes';
 
 const metadata = new ChartMetadata({
   name: 'Line Chart',
   description: '',
   credits: ['http://nvd3.org'],
+  canBeAnnotationTypes: [
+    ANNOTATION_TYPES.TIME_SERIES,
+  ],
+  supportedAnnotationTypes: [
+    ANNOTATION_TYPES.TIME_SERIES,
+    ANNOTATION_TYPES.INTERVAL,
+    ANNOTATION_TYPES.EVENT,
+    ANNOTATION_TYPES.FORMULA,
+  ],
   thumbnail,
 });
 
