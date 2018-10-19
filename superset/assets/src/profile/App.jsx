dbbdbbce0fdf8098ada4b933fb7915b4a7690d71
Chris Williams <chris.williams@airbnb.com>
2018-10-18 21:59:49 -0700

    [superset-client] make Profile a redux app for toasts

--- superset/assets/src/profile/App.jsx
+++ superset/assets/src/profile/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
 import App from './components/App';
+import messageToastReducer from '../messageToasts/reducers';
+import { initEnhancer } from '../reduxUtils';
 import { appSetup } from '../common';
 
 import './main.css';
@@ -10,8 +16,21 @@ appSetup();
 const profileViewContainer = document.getElementById('app');
 const bootstrap = JSON.parse(profileViewContainer.getAttribute('data-bootstrap'));
 
+const store = createStore(
+  combineReducers({
+    messageToasts: messageToastReducer,
+  }),
+  {},
+  compose(
+    applyMiddleware(thunk),
+    initEnhancer(false),
+  ),
+);
+
 const Application = () => (
-  <App user={bootstrap.user} />
+  <Provider store={store}>
+    <App user={bootstrap.user} />
+  </Provider>
 );
 
 export default hot(module)(Application);
