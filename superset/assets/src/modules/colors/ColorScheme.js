3632fcd9eab0850bcc040030bdccd457437055ef
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-10-19 11:12:28 -0700

    Add label field

--- superset/assets/src/modules/colors/ColorScheme.js
+++ superset/assets/src/modules/colors/ColorScheme.js
@@ -3,10 +3,12 @@ import isRequired from '../../utils/isRequired';
 export default class ColorScheme {
   constructor({
     name = isRequired('name'),
+    label,
     colors = isRequired('colors'),
     description = '',
   }) {
     this.name = name;
+    this.label = label || name;
     this.colors = colors;
     this.description = description;
   }
