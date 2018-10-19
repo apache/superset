61c77c8a1e39952321e4a6de5e88bf1db7ca2a29
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-10-19 11:54:09 -0700

    Split sequential schemes into multiple files

--- superset/assets/src/setup/setupColors.js
+++ superset/assets/src/setup/setupColors.js
@@ -1,15 +1,16 @@
 import airbnb from '../modules/colors/colorSchemes/categorical/airbnb';
-import d3 from '../modules/colors/colorSchemes/categorical/d3';
+import categoricalD3 from '../modules/colors/colorSchemes/categorical/d3';
 import google from '../modules/colors/colorSchemes/categorical/google';
 import lyft from '../modules/colors/colorSchemes/categorical/lyft';
 import sequentialCommon from '../modules/colors/colorSchemes/sequential/common';
+import sequentialD3 from '../modules/colors/colorSchemes/sequential/d3';
 import getCategoricalSchemeRegistry from '../modules/colors/CategoricalSchemeRegistrySingleton';
 import getSequentialSchemeRegistry from '../modules/colors/SequentialSchemeRegistrySingleton';
 
 export default function setupColors() {
   // Register color schemes
   const categoricalSchemeRegistry = getCategoricalSchemeRegistry();
-  [airbnb, d3, google, lyft].forEach((group) => {
+  [airbnb, categoricalD3, google, lyft].forEach((group) => {
     group.forEach((scheme) => {
       categoricalSchemeRegistry.registerValue(scheme.name, scheme);
     });
@@ -17,7 +18,9 @@ export default function setupColors() {
   categoricalSchemeRegistry.setDefaultSchemeName('bnbColors');
 
   const sequentialSchemeRegistry = getSequentialSchemeRegistry();
-  sequentialCommon.forEach((scheme) => {
-    sequentialSchemeRegistry.registerValue(scheme.name, scheme);
+  [sequentialCommon, sequentialD3].forEach((group) => {
+    group.forEach((scheme) => {
+      sequentialSchemeRegistry.registerValue(scheme.name, scheme);
+    });
   });
 }
