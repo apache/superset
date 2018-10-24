8f0fd16c7605fb6590d4d60dbd61e2d4299767f6
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-10-24 14:43:19 -0700

    Merge pull request #12 from apache-superset/kristw--translation
    Add @superset-ui/translation

---
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-translation/src/Translator.js
@@ -0,0 +1,29 @@
+import Jed from 'jed';
+import { sprintf } from 'sprintf-js';
+
+const DEFAULT_LANGUAGE_PACK = {
+  domain: 'superset',
+  locale_data: {
+    superset: {
+      '': {
+        domain: 'superset',
+        lang: 'en',
+        plural_forms: 'nplurals=1; plural=0',
+      },
+    },
+  },
+};
+
+export default class Translator {
+  constructor({ languagePack = DEFAULT_LANGUAGE_PACK } = {}) {
+    this.i18n = new Jed(languagePack);
+  }
+
+  translate(input, ...args) {
+    if (input === null || input === undefined) {
+      return input;
+    }
+    const text = this.i18n.gettext(input);
+    return (args.length > 0) ? sprintf(text, ...args) : text;
+  }
+}
---
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-translation/src/index.js
@@ -0,0 +1,1 @@
+export { configure, t } from './TranslatorSingleton';
