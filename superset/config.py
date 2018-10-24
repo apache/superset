11827873e817a795711c1ef87f73c5e8d5b354b7
Lorenzo Bagni <lorenzo.bagni@gmail.com>
2018-10-24 09:37:52 +0200

    Public role like read-only Rho

--- superset/config.py
+++ superset/config.py
@@ -186,10 +186,11 @@ AUTH_TYPE = AUTH_DB
 # ---------------------------------------------------
 # Roles config
 # ---------------------------------------------------
-# Grant public role the same set of permissions as for the GAMMA role.
+# Grant public role the same set of permissions as for the GAMMA or RHO role.
 # This is useful if one wants to enable anonymous users to view
 # dashboards. Explicit grant on specific datasets is still required.
 PUBLIC_ROLE_LIKE_GAMMA = False
+PUBLIC_ROLE_LIKE_RHO = False
 
 # ---------------------------------------------------
 # Babel config for translations
