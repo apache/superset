e7e8e9d00d926384e289a489847ae441aeef079e
Chris Williams <williaster@users.noreply.github.com>
2018-10-25 11:44:05 -0700

    [dashboard][bugfix][save as] re-direct to copied dashboard upon saveas (#6189)
    * [dashboard][bugfix][save as] re-direct to copied dashboard upon saveas
    
    * [dashboard][save modal] safer destructuring
    
    * [dashboard][save as] simplify Promise resolution
    
    * [dashboard] simply fetch slices error

--- superset/assets/src/dashboard/actions/dashboardState.js
+++ superset/assets/src/dashboard/actions/dashboardState.js
@@ -132,16 +132,14 @@ export function saveDashboardRequest(data, id, saveType) {
     SupersetClient.post({
       endpoint: `/superset/${path}/${id}/`,
       postPayload: { data },
-      parseMethod: null,
     })
       .then(response =>
         Promise.all([
-          Promise.resolve(response),
           dispatch(saveDashboardRequestSuccess()),
           dispatch(
             addSuccessToast(t('This dashboard was saved successfully.')),
           ),
-        ]),
+        ]).then(() => Promise.resolve(response)),
       )
       .catch(response =>
         getClientErrorObject(response).then(({ error }) =>
