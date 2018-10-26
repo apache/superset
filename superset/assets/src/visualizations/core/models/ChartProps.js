236186ad232aa384df092e7fecd9b099a037ca02
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-10-26 11:38:30 -0700

    Implement SuperChart and enable the chart plugins (#6154)
    * setup plugin and add SuperChart
    
    * Integrate SuperChart into Chart.jsx
    
    * Add vizType as class name
    
    * Remove .slice_container
    
    * add snakeCase to sanitize class name
    
    * Remove old code to load charts
    
    * Fix supportedAnnotationTypes
    
    * Update AnnotationTypes, remove unnecessary imports and dependency from VIZ_TYPES
    
    * remove index.js and update unit test
    
    * resolve tree map issue
    
    * fix issue with annotation types
    
    * fix proptypes
    
    * add )
    
    * address a few comments
    
    * create bound functions
    
    * bound more functions
    
    * add reselect
    
    * use reselect for chartprops
    
    * improve performance with reselect
    
    * remove unused props
    
    * Remove getFilters() and update table test
    
    * Remove unused code
    
    * Delete adaptors
    
    * switch to react-loadable
    
    * Rewrite with reloadable
    
    * Add timeout back
    
    * remove loading
    
    * update table unit test
    
    * remove pastDelay

--- superset/assets/src/visualizations/core/models/ChartProps.js
+++ superset/assets/src/visualizations/core/models/ChartProps.js
@@ -1,3 +1,4 @@
+import { createSelector } from 'reselect';
 import convertKeysToCamelCase from '../../../utils/convertKeysToCamelCase';
 
 export default class ChartProps {
@@ -29,3 +30,44 @@ export default class ChartProps {
     this.setTooltip = setTooltip;
   }
 }
+
+ChartProps.createSelector = function () {
+  return createSelector(
+    input => input.width,
+    input => input.height,
+    input => input.annotationData,
+    input => input.datasource,
+    input => input.filters,
+    input => input.formData,
+    input => input.onAddFilter,
+    input => input.onError,
+    input => input.payload,
+    input => input.setControlValue,
+    input => input.setTooltip,
+    (
+      width,
+      height,
+      annotationData,
+      datasource,
+      filters,
+      formData,
+      onAddFilter,
+      onError,
+      payload,
+      setControlValue,
+      setTooltip,
+    ) => new ChartProps({
+      width,
+      height,
+      annotationData,
+      datasource,
+      filters,
+      formData,
+      onAddFilter,
+      onError,
+      payload,
+      setControlValue,
+      setTooltip,
+    }),
+  );
+};
