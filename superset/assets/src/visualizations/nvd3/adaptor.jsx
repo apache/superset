a44965daa02b9e63197d76b9d39f913b25c2b885
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-10-26 11:38:30 -0700

    Implement SuperChart and enable the chart plugins (#6154)
    * setup plugin and add SuperChart
    
    * Integrate SuperChart into Chart.jsx
    
    * Add vizType as class name
    
    * Remove .slice_container
    
    * add snakeCase to sanitize class name
    
    * Remove old code to load charts
    
    * Fix supportedAnnotationTypes
    
    * Update AnnotationTypes, remove unnecessary imports and dependency from VIZ_TYPES
    
    * remove index.js and update unit test
    
    * resolve tree map issue
    
    * fix issue with annotation types
    
    * fix proptypes
    
    * add )
    
    * address a few comments
    
    * create bound functions
    
    * bound more functions
    
    * add reselect
    
    * use reselect for chartprops
    
    * improve performance with reselect
    
    * remove unused props
    
    * Remove getFilters() and update table test
    
    * Remove unused code
    
    * Delete adaptors
    
    * switch to react-loadable
    
    * Rewrite with reloadable
    
    * Add timeout back
    
    * remove loading
    
    * update table unit test
    
    * remove pastDelay

--- superset/assets/src/visualizations/nvd3/adaptor.jsx
+++
@@ -1,5 +0,0 @@
-import createAdaptor from '../../utils/createAdaptor';
-import Component from './ReactNVD3';
-import transformProps from './transformProps';
-
-export default createAdaptor(Component, transformProps);
