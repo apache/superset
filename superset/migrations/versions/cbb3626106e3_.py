079a3622e402b8c0f88be9726c60f074840a7ba6
yuguang <yuguang@analyticsmd.com>
2018-10-29 15:47:36 -0700

    add the merge migration

---
+++ superset/migrations/versions/cbb3626106e3_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: cbb3626106e3
+Revises: ('c5e6fada92be', '90fb06717cbd')
+Create Date: 2018-10-29 15:32:16.600808
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'cbb3626106e3'
+down_revision = ('c5e6fada92be', '90fb06717cbd')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
