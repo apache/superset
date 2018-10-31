741c06fd51250b4f90820abe56bf49b6e7120ad7
Timi Fasubaa <timi.fasubaa@airbnb.com>
2018-10-31 01:48:49 -0700

    split into three

--- superset/assets/cypress_build.sh
+++ superset/assets/cypress_build.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 set -e
 
-<<<<<<< HEAD
-superset/bin/superset db upgrade
-superset/bin/superset load_test_users
-superset/bin/superset load_examples
-superset/bin/superset init
-flask run -p 8081 --with-threads --reload --debugger &
-
-=======
->>>>>>> split into three
 cd "$(dirname "$0")"
 
-yarn install --frozen-lockfile & superset/bin/superset db upgrade
-superset/bin/superset load_test_users & superset/bin/superset load_examples
-superset/bin/superset init
-npm run build & flask run -p 8081 --with-threads --reload --debugger &
+#run all the python steps in a background process
+(time /home/travis/build/apache/incubator-superset/superset/bin/superset db upgrade; time /home/travis/build/apache/incubator-superset/superset/bin/superset load_test_users; /home/travis/build/apache/incubator-superset/superset/bin/superset load_examples; time /home/travis/build/apache/incubator-superset/superset/bin/superset init; echo "[completed python build steps]"; flask run -p 8081 --with-threads --reload --debugger) &
 
-npm run cypress run -- --spec 'cypress/integration/explore/*' & npm run cypress run -- --spec 'cypress/integration/dashboard/*' & npm run cypress run -- --spec 'cypress/integration/explore/*'
+#block on the longer running javascript process
+(time yarn install --frozen-lockfile; time npm run build; echo "[completed js build steps]")
+
+CYPRESS_PATH='cypress/integration/'${1}'/*'
+time npm run cypress run -- --spec "$CYPRESS_PATH" --record false --config video=false
+
 kill %1
