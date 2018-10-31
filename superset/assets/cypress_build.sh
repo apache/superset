d32f635d9aada5b4badb75884749426ab12fb1d7
Timi Fasubaa <timi.fasubaa@airbnb.com>
2018-10-31 01:32:08 -0700

    aplit cypress tests

--- superset/assets/cypress_build.sh
+++ superset/assets/cypress_build.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 set -e
 
+<<<<<<< HEAD
 superset/bin/superset db upgrade
 superset/bin/superset load_test_users
 superset/bin/superset load_examples
 superset/bin/superset init
 flask run -p 8081 --with-threads --reload --debugger &
 
+=======
+>>>>>>> split into three
 cd "$(dirname "$0")"
 
-yarn install --frozen-lockfile
-npm run build
-npm run cypress run
+yarn install --frozen-lockfile & superset/bin/superset db upgrade
+superset/bin/superset load_test_users & superset/bin/superset load_examples
+superset/bin/superset init
+npm run build & flask run -p 8081 --with-threads --reload --debugger &
+
+npm run cypress run -- --spec 'cypress/integration/explore/*' & npm run cypress run -- --spec 'cypress/integration/dashboard/*' & npm run cypress run -- --spec 'cypress/integration/explore/*'
 kill %1
