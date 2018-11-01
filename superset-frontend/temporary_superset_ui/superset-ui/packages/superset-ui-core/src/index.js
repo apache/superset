26ec1b5004b4fb4c43a809825a891debc18dd9e3
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-11-01 14:20:12 -0700

    Merge pull request #19 from apache-superset/kristw--new-core
    Add new core modules from incubator-superset

---
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-core/src/index.js
@@ -0,0 +1,8 @@
+export { default as Plugin } from './models/Plugin';
+export { default as Preset } from './models/Preset';
+export { default as Registry } from './models/Registry';
+
+export { default as convertKeysToCamelCase } from './utils/convertKeysToCamelCase';
+export { default as isDefined } from './utils/isDefined';
+export { default as isRequired } from './utils/isRequired';
+export { default as makeSingleton } from './utils/makeSingleton';
