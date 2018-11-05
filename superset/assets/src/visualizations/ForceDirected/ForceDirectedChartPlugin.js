7373be71633df4ad07c29cf1a3bf07e3f6669c6c
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-11-05 12:47:32 -0800

    Add chart names translation (#6273)
    * add translation for chart names
    
    * add translation for all chart names
    
    * add missing translation

--- superset/assets/src/visualizations/ForceDirected/ForceDirectedChartPlugin.js
+++ superset/assets/src/visualizations/ForceDirected/ForceDirectedChartPlugin.js
@@ -1,10 +1,11 @@
+import { t } from '@superset-ui/translation';
 import ChartPlugin from '../core/models/ChartPlugin';
 import ChartMetadata from '../core/models/ChartMetadata';
 import transformProps from './transformProps';
 import thumbnail from './images/thumbnail.png';
 
 const metadata = new ChartMetadata({
-  name: 'Force-directed Graph',
+  name: t('Force-directed Graph'),
   description: '',
   credits: ['http://bl.ocks.org/d3noob/5141278'],
   thumbnail,
