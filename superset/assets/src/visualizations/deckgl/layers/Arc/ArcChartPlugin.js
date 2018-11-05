b58d7d4e16ec5ad8c95ed21cf1ac93303634ef99
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-11-05 12:47:32 -0800

    Add chart names translation (#6273)
    * add translation for chart names
    
    * add translation for all chart names
    
    * add missing translation

--- superset/assets/src/visualizations/deckgl/layers/Arc/ArcChartPlugin.js
+++ superset/assets/src/visualizations/deckgl/layers/Arc/ArcChartPlugin.js
@@ -1,10 +1,11 @@
+import { t } from '@superset-ui/translation';
 import ChartPlugin from '../../../core/models/ChartPlugin';
 import ChartMetadata from '../../../core/models/ChartMetadata';
 import thumbnail from './images/thumbnail.png';
 import transformProps from '../../transformProps';
 
 const metadata = new ChartMetadata({
-  name: 'deck.gl Arc',
+  name: t('deck.gl Arc'),
   description: '',
   credits: ['https://uber.github.io/deck.gl'],
   thumbnail,
--- superset/assets/src/visualizations/nvd3/DistBar/DistBarChartPlugin.js
+++ superset/assets/src/visualizations/nvd3/DistBar/DistBarChartPlugin.js
@@ -1,11 +1,12 @@
+import { t } from '@superset-ui/translation';
 import ChartPlugin from '../../core/models/ChartPlugin';
 import ChartMetadata from '../../core/models/ChartMetadata';
 import transformProps from '../transformProps';
 import thumbnail from './images/thumbnail.png';
 
 const metadata = new ChartMetadata({
-  name: 'Bar Chart',
-  description: 'A bar chart where the x axis is time',
+  name: t('Bar Chart'),
+  description: t('A bar chart where the x axis is time'),
   credits: ['http://nvd3.org'],
   thumbnail,
 });
