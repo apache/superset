fbdefa7e3c6e3ce5c2c0ad5082ce884c938f3930
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-11-05 17:14:07 -0800

    Update color module (#22)
    * add brand color
    
    * update test
    
    * add unit tests

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-color/src/SequentialScheme.js
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-color/src/SequentialScheme.js
@@ -1,9 +1,38 @@
+import { scaleLinear } from 'd3-scale';
 import ColorScheme from './ColorScheme';
+
+function range(count) {
+  return [...Array(count).keys()];
+}
 
 export default class SequentialScheme extends ColorScheme {
   constructor(input) {
     super(input);
     const { isDiverging = false } = input;
     this.isDiverging = isDiverging;
+  }
+
+  createLinearScale(extent = [0, 1]) {
+    // Create matching domain
+    // because D3 continuous scale uses piecewise mapping
+    // between domain and range.
+    const valueScale = scaleLinear().range(extent);
+    const denominator = this.colors.length - 1;
+    const domain = range(this.colors.length).map(i => valueScale(i / denominator));
+
+    return scaleLinear()
+      .domain(domain)
+      .range(this.colors)
+      .clamp(true);
+  }
+
+  getColors(numColors = this.colors.length) {
+    if (numColors === this.colors.length) {
+      return this.colors;
+    }
+    const colorScale = this.createLinearScale();
+    const denominator = numColors - 1;
+
+    return range(numColors).map(i => colorScale(i / denominator));
   }
 }
--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-color/src/index.js
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-color/src/index.js
@@ -6,3 +6,5 @@ export { default as CategoricalScheme } from './CategoricalScheme';
 export { default as getCategoricalSchemeRegistry } from './CategoricalSchemeRegistrySingleton';
 export { default as getSequentialSchemeRegistry } from './SequentialSchemeRegistrySingleton';
 export { default as SequentialScheme } from './SequentialScheme';
+
+export const BRAND_COLOR = '#00A699';
