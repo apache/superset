ff2d0058b44a6f325ac2ffc56e1f81bc8ce1b761
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-11-06 15:54:35 -0800

    remove files that are extracted

--- superset/assets/spec/javascripts/utils/isRequired_spec.js
+++
@@ -1,7 +0,0 @@
-import isRequired from '../../../src/utils/isRequired';
-
-describe('isRequired(field)', () => {
-  it('should throw error with the given field in the message', () => {
-    expect(() => isRequired('myField')).toThrowError(Error);
-  });
-});
