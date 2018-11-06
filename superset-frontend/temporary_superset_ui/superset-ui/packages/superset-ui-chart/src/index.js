ae476317d6fb550e1af0f8e69fdf585143a421d7
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-11-06 14:45:50 -0800

    Add @superset-ui/chart with extract modules and additional unit tests (#23)
    * initialize chart package
    
    * add unit tests for metadata and chartprops
    
    * fix lint
    
    * add unit test for ChartPlugin
    
    * update README

---
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-chart/src/index.js
@@ -0,0 +1,11 @@
+export { default as ChartMetadata } from './models/ChartMetadata';
+export { default as ChartPlugin } from './models/ChartPlugin';
+export { default as ChartProps } from './models/ChartProps';
+export {
+  default as getChartBuildQueryRegistry,
+} from './registries/ChartBuildQueryRegistrySingleton';
+export { default as getChartComponentRegistry } from './registries/ChartComponentRegistrySingleton';
+export { default as getChartMetadataRegistry } from './registries/ChartMetadataRegistrySingleton';
+export {
+  default as getChartTransformPropsRegistry,
+} from './registries/ChartTransformPropsRegistrySingleton';
