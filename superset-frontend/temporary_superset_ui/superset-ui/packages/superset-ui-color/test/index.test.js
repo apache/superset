5b9ce32cb95a27fb51f2b9304968df3c7005650f
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-11-05 17:14:07 -0800

    Update color module (#22)
    * add brand color
    
    * update test
    
    * add unit tests

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-color/test/index.test.js
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-color/test/index.test.js
@@ -1,4 +1,5 @@
 import {
+  BRAND_COLOR,
   CategoricalColorNamespace,
   CategoricalColorScale,
   CategoricalScheme,
@@ -10,6 +11,7 @@ import {
 describe('index', () => {
   it('exports modules', () => {
     [
+      BRAND_COLOR,
       CategoricalColorNamespace,
       CategoricalColorScale,
       CategoricalScheme,
