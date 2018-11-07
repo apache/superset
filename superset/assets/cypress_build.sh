b333bc816642c4db7e440bc5384dba80bd73cccc
Beto Dealmeida <roberto@dealmeida.net>
2018-11-07 15:23:41 -0800

    Fix cypress port

--- superset/assets/cypress_build.sh
+++ superset/assets/cypress_build.sh
@@ -5,7 +5,7 @@ superset/bin/superset db upgrade
 superset/bin/superset load_test_users
 superset/bin/superset load_examples
 superset/bin/superset init
-flask run -p 8080 --with-threads --reload --debugger &
+flask run -p 8081 --with-threads --reload --debugger &
 
 cd "$(dirname "$0")"
 
