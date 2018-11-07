fad2a0df589d3aa800d8d8e6f8700079197b7444
Chris Williams <williaster@users.noreply.github.com>
2018-11-06 21:56:40 -0800

    [demo] add package + storybook (#25)
    * [demo] add package + storybook
    
    * [demo] add color stories
    
    * [demo] update readme, add gh-pages hooks, remove vx deps
    
    * [demo] remove nesting for color stories
    
    * [gh-pages] run postrelease
    
    * [gh-pages] add link in readme
    
    * [demo] exclude demo package from build script

---
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-demo/storybook/stories/superset-ui-color/index.js
@@ -0,0 +1,7 @@
+import CategoricalStories from './CategoricalStories';
+import SequentialStories from './SequentialStories';
+import './color-styles.css';
+
+export default {
+  examples: [...CategoricalStories, ...SequentialStories],
+};
