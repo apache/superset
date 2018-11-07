ad0fd024efa10dd773b697c114506986be813e9e
yuguang <yuguang@analyticsmd.com>
2018-11-07 13:21:43 -0800

    add merge migration

---
+++ superset/migrations/versions/96190fb1f488_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 96190fb1f488
+Revises: ('c5e6fada92be', '90fb06717cbd')
+Create Date: 2018-11-07 13:21:25.692340
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '96190fb1f488'
+down_revision = ('c5e6fada92be', '90fb06717cbd')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
