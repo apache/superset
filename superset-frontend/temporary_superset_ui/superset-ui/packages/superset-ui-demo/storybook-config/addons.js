eb1bff65426e957594e02b7b98288eb26f022832
Chris Williams <williaster@users.noreply.github.com>
2018-11-06 21:56:40 -0800

    [demo] add package + storybook (#25)
    * [demo] add package + storybook
    
    * [demo] add color stories
    
    * [demo] update readme, add gh-pages hooks, remove vx deps
    
    * [demo] remove nesting for color stories
    
    * [gh-pages] run postrelease
    
    * [gh-pages] add link in readme
    
    * [demo] exclude demo package from build script

---
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-demo/storybook-config/addons.js
@@ -0,0 +1,4 @@
+// note that the import order here determines the order in the UI!
+import '@storybook/addon-knobs/register';
+import '@storybook/addon-options/register';
+import 'storybook-addon-jsx/register';
