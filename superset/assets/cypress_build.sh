ac151d40c16a9894dfc1adefe753ca219a43cd07
Beto Dealmeida <roberto@dealmeida.net>
2018-11-07 16:51:22 -0800

    Geoviz state management fix (#6260)
    * Fix deckgl getPoints
    
    * Fix CSS
    
    * Fix zoom
    
    * Fix CategoricalDeckGLContainer
    
    * Fix cypress

--- superset/assets/cypress_build.sh
+++ superset/assets/cypress_build.sh
@@ -5,7 +5,7 @@ superset/bin/superset db upgrade
 superset/bin/superset load_test_users
 superset/bin/superset load_examples
 superset/bin/superset init
-superset/bin/superset runserver &
+flask run -p 8081 --with-threads --reload --debugger &
 
 cd "$(dirname "$0")"
 
