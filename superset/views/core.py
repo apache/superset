a97bce0b4eda1585a74fcdc01539cf216d5abef7
Timi Fasubaa <timi.fasubaa@airbnb.com>
2018-11-07 16:53:07 -0800

    add logging to csv upload

--- superset/views/core.py
+++ superset/views/core.py
@@ -372,6 +372,7 @@ class CsvToDatabaseView(SimpleFormView):
             flash(
                 message,
                 'danger')
+            stats_logger.incr('failed_csv_upload')
             return redirect('/csvtodatabaseview/form')
 
         os.remove(path)
@@ -382,6 +383,7 @@ class CsvToDatabaseView(SimpleFormView):
                                             form.name.data,
                                             db_name))
         flash(message, 'info')
+        stats_logger.incr('successful_csv_upload')
         return redirect('/tablemodelview/list/')
 
     def is_schema_allowed(self, database, schema):
