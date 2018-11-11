17d1384b2373cdf08289d6dca2e4b2865117cd52
James Finucane <jnishiyama@users.noreply.github.com>
2018-11-11 19:02:09 +0200

    Fix EventFlow transform is not a function
    transformProps was being passed into EventFlowChartPlugin as object (module) as opposed to function. This commit changes the implementation to be consistent with the other ChartPlugins and fixes the typeError that causes EventFlow Chart to not display.

--- superset/assets/src/visualizations/EventFlow/EventFlowChartPlugin.js
+++ superset/assets/src/visualizations/EventFlow/EventFlowChartPlugin.js
@@ -1,6 +1,7 @@
 import { t } from '@superset-ui/translation';
 import ChartPlugin from '../core/models/ChartPlugin';
 import ChartMetadata from '../core/models/ChartMetadata';
+import transformProps from './transformProps';
 import thumbnail from './images/thumbnail.png';
 
 const metadata = new ChartMetadata({
@@ -14,7 +15,7 @@ export default class EventFlowChartPlugin extends ChartPlugin {
   constructor() {
     super({
       metadata,
-      loadTransformProps: () => import('./transformProps.js'),
+      transformProps,
       loadChart: () => import('./EventFlow.jsx'),
     });
   }
