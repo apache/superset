807158c3fbede4f3d3819f97fa0796efda1ac9f5
James Finucane <jnishiyama@users.noreply.github.com>
2018-11-12 11:04:47 +0200

    Revert to lazy-loading transformProps
    - Revert to lazy-loading transformProps
    - Fix in Superchart will default to transformProps.default if available to allow for the lazy-loading

--- superset/assets/src/visualizations/EventFlow/EventFlowChartPlugin.js
+++ superset/assets/src/visualizations/EventFlow/EventFlowChartPlugin.js
@@ -1,7 +1,6 @@
 import { t } from '@superset-ui/translation';
 import ChartPlugin from '../core/models/ChartPlugin';
 import ChartMetadata from '../core/models/ChartMetadata';
-import transformProps from './transformProps';
 import thumbnail from './images/thumbnail.png';
 
 const metadata = new ChartMetadata({
@@ -15,7 +14,7 @@ export default class EventFlowChartPlugin extends ChartPlugin {
   constructor() {
     super({
       metadata,
-      transformProps,
+      loadTransformProps: () => import("./transformProps.js"),
       loadChart: () => import('./EventFlow.jsx'),
     });
   }
