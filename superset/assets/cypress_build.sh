906554abac2915b68e021b7889873bdc1706b812
timifasubaa <30888507+timifasubaa@users.noreply.github.com>
2018-11-15 12:26:33 -0800

    Split cypress tests (#6241)
    * aplit cypress tests
    
    * split into three

--- superset/assets/cypress_build.sh
+++ superset/assets/cypress_build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e
 
-superset/bin/superset db upgrade
-superset/bin/superset load_test_users
-superset/bin/superset load_examples
-superset/bin/superset init
-flask run -p 8081 --with-threads --reload --debugger &
-
 cd "$(dirname "$0")"
 
-yarn install --frozen-lockfile
-npm run build
-npm run cypress run
+#run all the python steps in a background process
+(time /home/travis/build/apache/incubator-superset/superset/bin/superset db upgrade; time /home/travis/build/apache/incubator-superset/superset/bin/superset load_test_users; /home/travis/build/apache/incubator-superset/superset/bin/superset load_examples; time /home/travis/build/apache/incubator-superset/superset/bin/superset init; echo "[completed python build steps]"; flask run -p 8081 --with-threads --reload --debugger) &
+
+#block on the longer running javascript process
+(time yarn install --frozen-lockfile; time npm run build; echo "[completed js build steps]")
+
+CYPRESS_PATH='cypress/integration/'${1}'/*'
+time npm run cypress run -- --spec "$CYPRESS_PATH" --record false --config video=false
+
 kill %1
