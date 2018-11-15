5b656f5cdca296b3ebaebf563f8b1a6725322447
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-11-15 11:10:57 -0800

    fix: npm exclude template files (#34)

--- superset-frontend/temporary_superset_ui/superset-ui/packages/generator-superset/generators/package/index.js
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/generator-superset/generators/package/index.js
@@ -23,7 +23,7 @@ module.exports = class extends Generator {
 
   writing() {
     this.fs.copyTpl(
-      this.templatePath('package.json'),
+      this.templatePath('_package.json'),
       this.destinationPath('package.json'),
       this.answers,
     );
