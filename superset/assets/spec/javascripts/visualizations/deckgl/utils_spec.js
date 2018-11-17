4c94447635114e3270f6198232216edca7f1fa70
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-11-16 17:06:21 -0800

    Fix adhoc metrics in Polygon (#6399)
    * Fix adhoc metrics in Polygon
    
    * Addressing comments

--- superset/assets/spec/javascripts/visualizations/deckgl/utils_spec.js
+++ superset/assets/spec/javascripts/visualizations/deckgl/utils_spec.js
@@ -4,6 +4,8 @@ import {
   getBuckets,
 } from '../../../../src/visualizations/deckgl/utils';
 
+const metricAccessor = d => d.count;
+
 describe('getBreakPoints', () => {
   it('is a function', () => {
     expect(typeof getBreakPoints).toBe('function');
@@ -11,7 +13,7 @@ describe('getBreakPoints', () => {
 
   it('returns sorted break points', () => {
     const fd = { break_points: ['0', '10', '100', '50', '1000'] };
-    const result = getBreakPoints(fd, []);
+    const result = getBreakPoints(fd, [], metricAccessor);
     const expected = ['0', '10', '50', '100', '1000'];
     expect(result).toEqual(expected);
   });
@@ -19,7 +21,7 @@ describe('getBreakPoints', () => {
   it('returns evenly distributed break points when no break points are specified', () => {
     const fd = { metric: 'count' };
     const features = [0, 1, 2, 10].map(count => ({ count }));
-    const result = getBreakPoints(fd, features);
+    const result = getBreakPoints(fd, features, metricAccessor);
     const expected = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
     expect(result).toEqual(expected);
   });
@@ -27,7 +29,7 @@ describe('getBreakPoints', () => {
   it('formats number with proper precision', () => {
     const fd = { metric: 'count', num_buckets: 2 };
     const features = [0, 1 / 3, 2 / 3, 1].map(count => ({ count }));
-    const result = getBreakPoints(fd, features);
+    const result = getBreakPoints(fd, features, metricAccessor);
     const expected = ['0.0', '0.5', '1.0'];
     expect(result).toEqual(expected);
   });
@@ -35,7 +37,7 @@ describe('getBreakPoints', () => {
   it('works with a zero range', () => {
     const fd = { metric: 'count', num_buckets: 1 };
     const features = [1, 1, 1].map(count => ({ count }));
-    const result = getBreakPoints(fd, features);
+    const result = getBreakPoints(fd, features, metricAccessor);
     const expected = ['1', '1'];
     expect(result).toEqual(expected);
   });
@@ -53,7 +55,7 @@ describe('getBreakPointColorScaler', () => {
       opacity: 100,
     };
     const features = [10, 20, 30].map(count => ({ count }));
-    const scaler = getBreakPointColorScaler(fd, features);
+    const scaler = getBreakPointColorScaler(fd, features, metricAccessor);
     expect(scaler({ count: 10 })).toEqual([0, 0, 0, 255]);
     expect(scaler({ count: 15 })).toEqual([64, 64, 64, 255]);
     expect(scaler({ count: 30 })).toEqual([255, 255, 255, 255]);
@@ -67,7 +69,7 @@ describe('getBreakPointColorScaler', () => {
       opacity: 100,
     };
     const features = [];
-    const scaler = getBreakPointColorScaler(fd, features);
+    const scaler = getBreakPointColorScaler(fd, features, metricAccessor);
     expect(scaler({ count: 0 })).toEqual([0, 0, 0, 255]);
     expect(scaler({ count: 0.5 })).toEqual([0, 0, 0, 255]);
     expect(scaler({ count: 1 })).toEqual([255, 255, 255, 255]);
@@ -82,7 +84,7 @@ describe('getBreakPointColorScaler', () => {
       opacity: 100,
     };
     const features = [];
-    const scaler = getBreakPointColorScaler(fd, features);
+    const scaler = getBreakPointColorScaler(fd, features, metricAccessor);
     expect(scaler({ count: -1 })).toEqual([0, 0, 0, 0]);
     expect(scaler({ count: 11 })).toEqual([255, 255, 255, 0]);
   });
@@ -101,7 +103,7 @@ describe('getBuckets', () => {
       opacity: 100,
     };
     const features = [];
-    const result = getBuckets(fd, features);
+    const result = getBuckets(fd, features, metricAccessor);
     const expected = {
       '0 - 1': { color: [0, 0, 0, 255], enabled: true },
       '1 - 10': { color: [255, 255, 255, 255], enabled: true },
