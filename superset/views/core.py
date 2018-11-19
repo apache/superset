aa12dd7b79a63a91c0d40ba3836dd888b31933f0
bipinsoniguavus <bipin.soni@guavus.com>
2018-11-19 13:31:03 +0530

    feat(export-dashboard): fixed dashboard exporting issue (#17)
    * feat(export-dashboard): fixed dashboard exporting issue
    
    UIC-724
    
    * feat(export-dashboard): Refactored Code

--- superset/views/core.py
+++ superset/views/core.py
@@ -620,7 +620,7 @@ class DashboardModelView(SupersetModelView, DeleteMixin):  # noqa
             items = [items]
         ids = ''.join('&id={}'.format(d.id) for d in items)
         return redirect(
-            '/dashboard/export_dashboards_form?{}'.format(ids[1:]))
+            '/dashboard/export_dashboards_form?{}&action=go'.format(ids[1:]))
 
     @log_this
     @has_access
