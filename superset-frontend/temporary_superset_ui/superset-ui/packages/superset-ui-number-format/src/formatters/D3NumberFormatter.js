11f650a35a14a6bccea855234c44aa4c6b9d970d
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-11-19 10:33:33 -0800

    Rename `D3Formatter` to `D3NumberFormatter` (#36)
    BREAKING CHANGE: rename D3Formatter to D3NumberFormatter

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-number-format/src/formatters/D3NumberFormatter.js
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-number-format/src/formatters/D3NumberFormatter.js
@@ -3,15 +3,15 @@ import { format as d3Format } from 'd3-format';
 import { isRequired } from '@superset-ui/core';
 import NumberFormatter from '../NumberFormatter';
 
-export default class D3Formatter extends NumberFormatter {
+export default class D3NumberFormatter extends NumberFormatter {
   /**
    * Pass only the D3 format string to constructor
    *
-   * new D3Formatter('.2f');
+   * new D3NumberFormatter('.2f');
    *
    * or accompany it with human-readable label and description
    *
-   * new D3Formatter({
+   * new D3NumberFormatter({
    *   id: '.2f',
    *   label: 'Float with 2 decimal points',
    *   description: 'lorem ipsum dolor sit amet',
@@ -27,14 +27,16 @@ export default class D3Formatter extends NumberFormatter {
     let formatFunc;
     let isInvalid = false;
 
+    const { id, label, description } = config;
+
     try {
-      formatFunc = d3Format(config.id);
+      formatFunc = d3Format(id);
     } catch (e) {
-      formatFunc = () => `Invalid format: ${config.id}`;
+      formatFunc = () => `Invalid format: ${id}`;
       isInvalid = true;
     }
 
-    super({ ...config, formatFunc });
+    super({ description, formatFunc, id, label });
     this.isInvalid = isInvalid;
   }
 }
