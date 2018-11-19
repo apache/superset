0d1c64f0c5779ac8b81e4861fc8635be46181953
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-11-19 10:33:33 -0800

    Rename `D3Formatter` to `D3NumberFormatter` (#36)
    BREAKING CHANGE: rename D3Formatter to D3NumberFormatter

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-number-format/test/formatters/D3NumberFormatter.test.js
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-number-format/test/formatters/D3NumberFormatter.test.js
@@ -1,26 +1,26 @@
-import D3Formatter from '../../src/formatters/D3Formatter';
+import D3NumberFormatter from '../../src/formatters/D3NumberFormatter';
 
-describe('D3Formatter', () => {
-  describe('new D3Formatter(config)', () => {
+describe('D3NumberFormatter', () => {
+  describe('new D3NumberFormatter(config)', () => {
     it('requires configOrFormatString', () => {
-      expect(() => new D3Formatter()).toThrow();
+      expect(() => new D3NumberFormatter()).toThrow();
     });
     describe('if configOrFormatString is string', () => {
       it('uses the input as d3.format string', () => {
-        const formatter = new D3Formatter('.2f');
+        const formatter = new D3NumberFormatter('.2f');
         expect(formatter.format(100)).toEqual('100.00');
       });
     });
     describe('if configOrFormatString is not string', () => {
       it('requires field config.id', () => {
-        expect(() => new D3Formatter({})).toThrow();
+        expect(() => new D3NumberFormatter({})).toThrow();
       });
       it('uses d3.format(config.id) as format function', () => {
-        const formatter = new D3Formatter({ id: ',.4f' });
+        const formatter = new D3NumberFormatter({ id: ',.4f' });
         expect(formatter.format(12345.67)).toEqual('12,345.6700');
       });
       it('if it is an invalid d3 format, the format function displays error message', () => {
-        const formatter = new D3Formatter({ id: 'i-am-groot' });
+        const formatter = new D3NumberFormatter({ id: 'i-am-groot' });
         expect(formatter.format(12345.67)).toEqual('Invalid format: i-am-groot');
       });
     });
