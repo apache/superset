76e4b888925efa3b612bf346644068529cdef3ba
Phil Brocker <phil.brocker@pondurance.com>
2018-11-26 12:18:47 -0500

    remove TODO comment

--- superset/views/core.py
+++ superset/views/core.py
@@ -158,7 +158,6 @@ class DatabaseFilter(SupersetFilter):
         if security_manager.all_database_access():
             return query
         perms = self.get_view_menus('database_access')
-        # TODO(bogdan): add `schema_access` support here
         return query.filter(self.model.perm.in_(perms))
 
 
