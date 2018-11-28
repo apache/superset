ac04a96346acb5bb03936a87b74d6b61f2b8ffed
Christine Chambers <christine.d.hang@gmail.com>
2018-11-28 16:30:26 -0500

    addressing PR comments

--- superset/assets/src/query/Column.ts
+++ superset/assets/src/query/Column.ts
@@ -1,6 +1,24 @@
+export enum ColumnType {
+  DOUBLE = 'DOUBLE',
+  FLOAT = 'FLOAT',
+  INT = 'INT',
+  BIGINT = 'BIGINT',
+  LONG = 'LONG',
+  REAL = 'REAL',
+  NUMERIC = 'NUMERIC',
+  DECIMAL = 'DECIMAL',
+  MONEY = 'MONEY',
+  DATE = 'DATE',
+  TIME = 'TIME',
+  DATETIME = 'DATETIME',
+  VARCHAR = 'VARCHAR',
+  STRING = 'STRING',
+  CHAR = 'CHAR',
+}
+
 // TODO: fill out additional fields of the Column interface
 export default interface Column {
   id: number;
-  type: string;
+  type: ColumnType;
   columnName: string;
 }
