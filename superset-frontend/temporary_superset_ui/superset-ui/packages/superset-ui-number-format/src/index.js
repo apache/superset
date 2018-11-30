d07e896c5ee9c9a0f9e3b7b712c42d879fbbb2be
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-11-30 10:31:14 -0800

    Expose factory functions as public API (#43)
    feat: Expose factory functions through API

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-number-format/src/index.js
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-number-format/src/index.js
@@ -1,4 +1,5 @@
-import * as NumberFormats from './NumberFormats';
+export { default as NumberFormats } from './NumberFormats';
+export { default as NumberFormatter, PREVIEW_VALUE } from './NumberFormatter';
 
 export {
   default as getNumberFormatterRegistry,
@@ -6,5 +7,7 @@ export {
   getNumberFormatter,
 } from './NumberFormatterRegistrySingleton';
 
-export { default as NumberFormatter, PREVIEW_VALUE } from './NumberFormatter';
-export { NumberFormats };
+export { default as createD3NumberFormatter } from './factories/createD3NumberFormatter';
+export {
+  default as createSiAtMostNDigitFormatter,
+} from './factories/createSiAtMostNDigitFormatter';
