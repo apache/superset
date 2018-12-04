ff1f92e362a1d0ac34821d779aacd71222159ee7
Christine Chambers <christine.d.hang@gmail.com>
2018-12-04 16:21:49 -0500

    Moving query module into superset-ui from incubator-superset (#48)
    * migrate query code from incubator-superset
    
    * Getting tests to pass
    
    - Up build-config version to pick up a fix to eslint-typescript-parser
    - Remove usage of default exports in favor of named exports unless the export is the only thing being exported out of a module
    - Fixing up a few linting errors
    
    * - Remove DatasourceKey interface in favor of readonly id and type properties on the DatasourceKey class directly.
    - Adding tests for DatasourceKey.

---
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-chart/src/query/FormData.ts
@@ -0,0 +1,32 @@
+import { AdhocMetric, MetricKey } from './Metric';
+
+// Type signature and utility functions for formData shared by all viz types
+// It will be gradually filled out as we build out the query object
+
+// Define mapped type separately to work around a limitation of TypeScript
+// https://github.com/Microsoft/TypeScript/issues/13573
+// The Metrics in formData is either a string or a proper metric. It will be
+// unified into a proper Metric type during buildQuery (see `/query/Metrics.ts`).
+type Metrics = Partial<Record<MetricKey, AdhocMetric | string>>;
+
+type BaseFormData = {
+  datasource: string;
+} & Metrics;
+
+// FormData is either sqla-based or druid-based
+type SqlaFormData = {
+  // FormData uses snake_cased keys.
+  // eslint-disable-next-line camelcase
+  granularity_sqla: string;
+} & BaseFormData;
+
+type DruidFormData = {
+  granularity: string;
+} & BaseFormData;
+
+type FormData = SqlaFormData | DruidFormData;
+export default FormData;
+
+export function getGranularity(formData: FormData): string {
+  return 'granularity_sqla' in formData ? formData.granularity_sqla : formData.granularity;
+}
---
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-chart/src/query/index.ts
@@ -0,0 +1,3 @@
+// Public API of the query module
+export { default } from './buildQueryContext';
+export { default as FormData } from './FormData';
