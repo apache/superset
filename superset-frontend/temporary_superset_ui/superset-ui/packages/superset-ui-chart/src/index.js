b96a59d877ce7a4282e53852f6795bf73773e659
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-12-05 14:27:45 -0800

    Add `createLoadableRenderer` (#49)
    feat: Add `createLoadableRenderer`

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-chart/src/index.js
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-chart/src/index.js
@@ -1,6 +1,9 @@
 export { default as ChartMetadata } from './models/ChartMetadata';
 export { default as ChartPlugin } from './models/ChartPlugin';
 export { default as ChartProps } from './models/ChartProps';
+
+export { default as createLoadableRenderer } from './components/createLoadableRenderer';
+
 export {
   default as getChartBuildQueryRegistry,
 } from './registries/ChartBuildQueryRegistrySingleton';
