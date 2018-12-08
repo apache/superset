b541471e05d8da1f36b7e31a583f0bb5c101ff32
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-12-07 16:56:57 -0800

    Separate SupersetClient and SupersetClientClass (#53)
    feat: Export `SupersetClientClass`
    internal: Increase test coverage.

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-connection/src/index.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-connection/src/index.ts
@@ -1,3 +1,4 @@
 export { default as callApi } from './callApi';
 export { default as SupersetClient } from './SupersetClient';
+export { SupersetClientClass } from './SupersetClientClass';
 export * from './types';
