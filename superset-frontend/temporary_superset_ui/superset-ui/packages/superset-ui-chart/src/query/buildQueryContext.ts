c7c2af712585f16e19108c4bbd3f3f5289169f15
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-12-11 11:58:02 -0800

    Convert chart models to TS and export additional modules (#55)
    internal: Convert Chart models to ts

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-chart/src/query/buildQueryContext.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-chart/src/query/buildQueryContext.ts
@@ -1,14 +1,21 @@
-import buildQueryObject, { QueryObject } from './buildQueryObject';
-import { DatasourceKey } from './DatasourceKey';
-import FormData from './FormData';
+import { buildQueryObject, QueryObject } from './buildQueryObject';
+import { DatasourceKey, DatasourceType } from './DatasourceKey';
+import { FormData } from './FormData';
 
 const WRAP_IN_ARRAY = (baseQueryObject: QueryObject) => [baseQueryObject];
 
-// Note: let TypeScript infer the return type
-export default function buildQueryContext(
+export interface QueryContext {
+  datasource: {
+    id: number;
+    type: DatasourceType;
+  };
+  queries: Array<QueryObject>;
+}
+
+export function buildQueryContext(
   formData: FormData,
   buildQuery: (baseQueryObject: QueryObject) => QueryObject[] = WRAP_IN_ARRAY,
-) {
+): QueryContext {
   return {
     datasource: new DatasourceKey(formData.datasource).toObject(),
     queries: buildQuery(buildQueryObject(formData)),
