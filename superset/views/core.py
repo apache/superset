50de1e7280befb7dbdc14bd3152595c033f8020f
Mahendra M <mahendra@affirm.com>
2018-12-10 22:29:29 -0800

    [SIP-3] Scheduled email reports for Slices / Dashboards (#5294)
    * [scheduled reports] Add support for scheduled reports
    
    * Scheduled email reports for slice and dashboard visualization
      (attachment or inline)
    * Scheduled email reports for slice data (CSV attachment on inline table)
    * Each schedule has a list of recipients (all of them can receive a single mail,
      or separate mails)
    * All outgoing mails can have a mandatory bcc - for audit purposes.
    * Each dashboard/slice can have multiple schedules.
    
    In addition, this PR also makes a few minor improvements to the celery
    infrastructure.
    * Create a common celery app
    * Added more celery annotations for the tasks
    * Introduced celery beat
    * Update docs about concurrency / pools
    
    * [scheduled reports] - Debug mode for scheduled emails
    
    * [scheduled reports] - Ability to send test mails
    
    * [scheduled reports] - Test email functionality - minor improvements
    
    * [scheduled reports] - Rebase with master. Minor fixes
    
    * [scheduled reports] - Add warning messages
    
    * [scheduled reports] - flake8
    
    * [scheduled reports] - fix rebase
    
    * [scheduled reports] - fix rebase
    
    * [scheduled reports] - fix flake8
    
    * [scheduled reports] Rebase in prep for merge
    
    * Fixed alembic tree after rebase
    * Updated requirements to latest version of packages (and tested)
    * Removed py2 stuff
    
    * [scheduled reports] - fix flake8
    
    * [scheduled reports] - address review comments
    
    * [scheduled reports] - rebase with master
    
    (cherry picked from commit 808622414c0f901bbecc53678fca0085af41a04c)

--- superset/views/core.py
+++ superset/views/core.py
@@ -1640,7 +1640,6 @@ class Superset(BaseSupersetView):
     @staticmethod
     def _set_dash_metadata(dashboard, data):
         positions = data['positions']
-
         # find slices in the position data
         slice_ids = []
         slice_id_to_name = {}
