c719198b0a475d60d54953184288d35636a51321
Conglei Shi <conglei.shi@airbnb.com>
2018-12-14 17:32:46 -0800

    fixed test

--- superset/viz.py
+++ superset/viz.py
@@ -253,18 +253,19 @@ class BaseViz(object):
 
     def query_obj(self):
         query_obj = self._query_obj()
-        metrics = query_obj.get('metrics')
-        formatted_metrics = []
-        for metric in metrics:
-            if not isinstance(metric, dict):
-                metric = {
-                    'label': metric,
-                    'expressionType': 'BUILTIN',
-                }
-            formatted_metrics.append(metric)
-        query_obj.update({
-            'metrics': formatted_metrics,
-        })
+        if (query_obj):
+            metrics = query_obj.get('metrics', [])
+            formatted_metrics = []
+            for metric in metrics:
+                if not isinstance(metric, dict):
+                    metric = {
+                        'label': metric,
+                        'expressionType': 'BUILTIN',
+                    }
+                formatted_metrics.append(metric)
+            query_obj.update({
+                'metrics': formatted_metrics,
+            })
         return query_obj
 
     def _query_obj(self):
