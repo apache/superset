cf975569a6ca19c4d6c73bda8611640e3286d83a
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-12-16 13:59:34 -0800

    fix cypress

--- superset/assets/cypress_build.sh
+++ superset/assets/cypress_build.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")"
 (time /home/travis/build/apache/incubator-superset/superset/bin/superset db upgrade; time /home/travis/build/apache/incubator-superset/superset/bin/superset load_test_users; /home/travis/build/apache/incubator-superset/superset/bin/superset load_examples --load-test-data; time /home/travis/build/apache/incubator-superset/superset/bin/superset init; echo "[completed python build steps]"; flask run -p 8081 --with-threads --reload --debugger) &
 
 #block on the longer running javascript process
-(time yarn install --frozen-lockfile; time npm run build; echo "[completed js build steps]")
+(time npm install; time npm run build; echo "[completed js build steps]")
 
 CYPRESS_PATH='cypress/integration/'${1}'/*'
 time npm run cypress run -- --spec "$CYPRESS_PATH" --record false --config video=false
