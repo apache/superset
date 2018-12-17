76c014098b3c0691272b647c4d0da31e2ff8c11b
Tresdon Jones <tjones@spotx.tv>
2018-12-17 12:05:31 -0700

    update alembic migration

--- superset/migrations/versions/d6ffdf31bdd4_add_published_column_to_dashboards.py
+++ superset/migrations/versions/d6ffdf31bdd4_add_published_column_to_dashboards.py
@@ -8,7 +8,7 @@ Create Date: 2018-03-30 14:00:44.929483
 
 # revision identifiers, used by Alembic.
 revision = 'd6ffdf31bdd4'
-down_revision = '55e910a74826'
+down_revision = '6c7537a6004a'
 
 from alembic import op
 import sqlalchemy as sa
