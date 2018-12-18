a321a59d83aa8c8a52904330d870cbddd56cda61
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-12-17 23:03:46 -0800

    Prefix with 'time'

--- superset/assets/js_build.sh
+++ superset/assets/js_build.sh
@@ -3,7 +3,7 @@ set -e
 cd "$(dirname "$0")"
 npm --version
 node --version
-npm ci
-npm run lint
-npm run cover  # this also runs the tests, so no need to 'npm run test'
-npm run build
+time npm ci
+time npm run lint
+time npm run cover  # this also runs the tests, so no need to 'npm run test'
+time npm run build
