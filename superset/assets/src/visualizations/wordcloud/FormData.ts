5eba3202481c5e5ff6744137fbc0754de2cbe9bd
Conglei Shi <conglei.shi@airbnb.com>
2018-12-17 16:16:15 -0800

    fixed ts test

--- superset/assets/src/visualizations/wordcloud/FormData.ts
+++ superset/assets/src/visualizations/wordcloud/FormData.ts
@@ -4,7 +4,7 @@ import { FormData as GenericFormData } from 'src/query';
 interface WordCloudFormData {
   series: string;
   metric: string;
-  time_range: string;
+  time_range?: string;
 }
 
 // FormData for wordcloud contains both common properties of all form data
