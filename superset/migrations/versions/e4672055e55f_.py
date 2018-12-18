d149bf4c6a956817f2c1006ad30cf47d201ded09
Beto Dealmeida <roberto@dealmeida.net>
2018-12-18 10:57:45 -0800

    Increase size of column `name` in table `ab_view_meu` (#6532)
    * Create migration script
    
    * Use batch operation for ALTER COLUMN
    
    (cherry picked from commit e2b9625d84490e7d7bc82be74a84766be66498bd)
    (cherry picked from commit 0e51dfa963d76dfb509c6fda7459808a48f8346e)

---
+++ superset/migrations/versions/e4672055e55f_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: e4672055e55f
+Revises: ('5c8b05ae6086', 'cefabc8f7d38')
+Create Date: 2019-01-28 15:43:46.521226
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'e4672055e55f'
+down_revision = ('5c8b05ae6086', 'cefabc8f7d38')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
