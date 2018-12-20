7991988d963cb084126f17dadd1abd1848f66072
bipin.soni <bipin.soni@guavus.com>
2018-12-20 20:04:11 +0530

    fix(kerberos-support): fixed import statement
    UIC-811

--- superset/models/core.py
+++ superset/models/core.py
@@ -45,6 +45,7 @@ custom_password_store = config.get('SQLALCHEMY_CUSTOM_PASSWORD_STORE')
 stats_logger = config.get('STATS_LOGGER')
 log_query = config.get('QUERY_LOGGER')
 metadata = Model.metadata  # pylint: disable=no-member
+from superset.models.helpers import has_kerberos_ticket
 
 PASSWORD_MASK = 'X' * 10
 
