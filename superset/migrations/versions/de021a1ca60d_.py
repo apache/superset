844f24a1140a8dfad33190719f2d563e79b0bf0c
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-12-21 09:54:53 -0800

    merge heads

--- superset/migrations/versions/de021a1ca60d_.py
+++ superset/migrations/versions/de021a1ca60d_.py
@@ -5,13 +5,9 @@ Revises: ('0b1f1ab473c0', 'cefabc8f7d38')
 Create Date: 2018-12-18 22:45:55.783083
 
 """
-
 # revision identifiers, used by Alembic.
 revision = 'de021a1ca60d'
-down_revision = ('0b1f1ab473c0', 'cefabc8f7d38')
-
-from alembic import op
-import sqlalchemy as sa
+down_revision = ('0b1f1ab473c0', 'cefabc8f7d38', 'a7ca4a272e0a')
 
 
 def upgrade():
