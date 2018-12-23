1f832955c3a72146d5a06b198dc3fc692ac20760
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-12-22 17:26:26 -0800

    db merge

---
+++ superset/migrations/versions/fbd55e0f83eb_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: fbd55e0f83eb
+Revises: ('7467e77870e4', 'de021a1ca60d')
+Create Date: 2018-12-22 17:26:16.113317
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'fbd55e0f83eb'
+down_revision = ('7467e77870e4', 'de021a1ca60d')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
