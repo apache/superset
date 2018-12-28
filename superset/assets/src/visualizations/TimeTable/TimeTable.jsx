7d9ea662f1314a8d4a1275b9e65f4491e35fa688
Maxime Beauchemin <maximebeauchemin@gmail.com>
2018-12-28 10:45:51 -0800

    [bugfix] moving from reactable to reactable-arc fork (#6576)

--- superset/assets/src/visualizations/TimeTable/TimeTable.jsx
+++ superset/assets/src/visualizations/TimeTable/TimeTable.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Mustache from 'mustache';
 import { scaleLinear } from 'd3-scale';
-import { Table, Thead, Th, Tr, Td } from 'reactable';
+import { Table, Thead, Th, Tr, Td } from 'reactable-arc';
 import { formatNumber } from '@superset-ui/number-format';
 import { formatTime } from '@superset-ui/time-format';
 
