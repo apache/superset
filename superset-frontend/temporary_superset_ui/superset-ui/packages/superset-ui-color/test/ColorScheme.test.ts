1b4010c68b97b1fe2db4898f6494f2630825bcb2
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-01-08 01:35:16 +0700

    Migrate @superset-ui/color to TypeScript (#69)
    * Refactor: Convert color to TS

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-color/test/ColorScheme.test.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-color/test/ColorScheme.test.ts
@@ -2,11 +2,6 @@ import ColorScheme from '../src/ColorScheme';
 
 describe('ColorScheme', () => {
   describe('new ColorScheme()', () => {
-    it('requires name and color', () => {
-      expect(() => new ColorScheme()).toThrow();
-      expect(() => new ColorScheme({ id: 'test' })).toThrow();
-      expect(() => new ColorScheme({ colors: ['red', 'blue'] })).toThrow();
-    });
     it('returns an instance of ColorScheme', () => {
       const scheme = new ColorScheme({ id: 'test', colors: ['red', 'blue'] });
       expect(scheme).toBeInstanceOf(ColorScheme);
