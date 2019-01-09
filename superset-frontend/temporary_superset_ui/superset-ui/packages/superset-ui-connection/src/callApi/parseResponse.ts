323d21562ac976c97f33dfd4df689037ae754203
Christopher Council <soboko@users.noreply.github.com>
2019-01-08 17:35:59 -0800

    Handle BigNumber conversions in JSON properly (without loss of precision) (#71)
    * Handle BigNumber conversions in JSON properly (without loss of precision)
    * Rewrap "errors" from JSONbig.parse in proper error object.
    * Use DefinitelyTyped's fetch-mock type def
    * Improved type def for json-bigint
    * Added type casts to some tests due to addition of fetchMock type def

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-connection/src/callApi/parseResponse.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-connection/src/callApi/parseResponse.ts
@@ -1,9 +1,19 @@
+import JSONbig from 'json-bigint';
 import { ParseMethod, SupersetClientResponse } from '../types';
 
 function rejectIfNotOkay(response: Response): Promise<Response> {
   if (!response.ok) return Promise.reject<Response>(response);
 
   return Promise.resolve<Response>(response);
+}
+
+function parseJson(text: string): any {
+  try {
+    return JSONbig.parse(text);
+  } catch (e) {
+    // if JSONbig.parse fails, it throws an object (not a proper Error), so let's re-wrap the message.
+    throw new Error(e.message);
+  }
 }
 
 export default function parseResponse(
@@ -17,7 +27,9 @@ export default function parseResponse(
   } else if (parseMethod === 'text') {
     return checkedPromise.then(response => response.text().then(text => ({ response, text })));
   } else if (parseMethod === 'json') {
-    return checkedPromise.then(response => response.json().then(json => ({ json, response })));
+    return checkedPromise.then(response =>
+      response.text().then(text => ({ json: parseJson(text), response })),
+    );
   }
 
   throw new Error(`Expected parseResponse=null|json|text, got '${parseMethod}'.`);
