3197896b441bdef2308a23aed591ea323b803a1a
Maxime Beauchemin <maximebeauchemin@gmail.com>
2019-01-12 18:00:38 -0800

    Improvements

--- scripts/build_license.py
+++ scripts/build_license.py
@@ -4,14 +4,14 @@ from pprint import pprint
 import subprocess
 
 LICENSE_MAP = {
-    '(BSD-2-Clause OR MIT OR Apache-2.0)': '(BSD-2-Clause OR MIT OR Apache-2.0)',
-    '(BSD-2-Clause OR MIT)': '(BSD-2-Clause OR MIT)',
-    '(GPL-2.0 OR MIT)': '(GPL-2.0 OR MIT)',
+    '(BSD-2-Clause OR MIT OR Apache-2.0)': 'MIT',
+    '(BSD-2-Clause OR MIT)': 'MIT',
+    '(GPL-2.0 OR MIT)': 'MIT',
     '(MIT AND BSD-3-Clause)': '(MIT AND BSD-3-Clause)',
     '(MIT AND CC-BY-3.0)': '(MIT AND CC-BY-3.0)',
     '(MIT AND Zlib)': '(MIT AND Zlib)',
-    '(MIT OR Apache-2.0)': '(MIT OR Apache-2.0)',
-    '(WTFPL OR MIT)': '(WTFPL OR MIT)',
+    '(MIT OR Apache-2.0)': 'MIT',
+    '(WTFPL OR MIT)': 'MIT',
     'AFLv2.1': 'AFLv2.1',
     'Apache License, Version 2.0': 'Apache License, Version 2.0',
     'Apache*': 'Apache*',
@@ -33,16 +33,30 @@ LICENSE_MAP = {
     'Custom: https://ppl.family': 'Custom: https://ppl.family',
     'GNU Library General Public License': 'GNU Library General Public License',
     'ISC': 'ISC',
-    'MIT OR GPL-2.0': 'MIT OR GPL-2.0',
+    'MIT OR GPL-2.0': 'MIT',
     'MIT': 'MIT',
     'MIT*': 'MIT*',
-    'MPL-2.0 OR Apache-2.0': 'MPL-2.0 OR Apache-2.0',
+    'MPL-2.0 OR Apache-2.0': 'Apache-2.0',
     'Public Domain': 'Public Domain',
     'The MIT License': 'The MIT License',
     'UNKNOWN': 'UNKNOWN',
     'Unlicense': 'Unlicense',
     'WTFPL': 'WTFPL',
     'Zlib': 'Zlib',
+}
+
+COMPATIBLE_LICENSES = {
+    'Apache-2.0',
+    'BSD',
+    'BSD-2-Clause',
+    'BSD-3-Clause',
+    'ISC',
+    'MIT',
+    'Zlib',
+}
+LIB_WHITELIST = {
+    # AFL or BSD 3-Clause: https://github.com/kriszyp/json-schema
+    'json-schema',
 }
 
 
@@ -63,7 +77,12 @@ def build_license():
             lic = LICENSE_MAP.get(lic) or lic or 'UNKNOWN'
             license_dict[lic].add(npm_short)
 
-    pprint(license_dict)
+    incompatible_dict = {
+        k: [lib for lib in v if lib not in LIB_WHITELIST]
+        for k, v in license_dict.items()
+        if k not in COMPATIBLE_LICENSES
+    }
+    print(json.dumps(incompatible_dict, indent=2))
 
 
 if __name__ == '__main__':
