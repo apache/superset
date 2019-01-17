67185887812fc41f516567dce95358a9ba2f5c34
Maxime Beauchemin <maximebeauchemin@gmail.com>
2019-01-17 15:48:40 -0800

    Add universal "New" button (#6670)
    * [cosmetic] CRUD's list from 'Add Filter' to 'Filter Results'
    
    * Add global new button
    
    * A better viz type selector
    
    * lint
    
    * Move view to new module, add unit test
    
    * 'Filter Results' -> 'Filter'
    
    * db merge
    
    * Filter -> 'Filter List'

---
+++ superset/migrations/versions/8b70aa3d0f87_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 8b70aa3d0f87
+Revises: ('fbd55e0f83eb', 'fb13d49b72f9')
+Create Date: 2019-01-17 08:31:55.781032
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '8b70aa3d0f87'
+down_revision = ('fbd55e0f83eb', 'fb13d49b72f9')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
