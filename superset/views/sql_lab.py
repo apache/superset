abcedd1734e685f48481f564961b0efe13e35484
Beto Dealmeida <roberto@dealmeida.net>
2019-01-18 09:38:07 -0800

    Add `extra` column to `SavedQuery` (#6687)
    * Expose more columns in saved queries API
    
    * Add extra column to SavedQuery
    
    * Add ASF header

--- superset/views/sql_lab.py
+++ superset/views/sql_lab.py
@@ -91,6 +91,9 @@ class SavedQueryView(SupersetModelView, DeleteMixin):
 
 
 class SavedQueryViewApi(SavedQueryView):
+    list_columns = [
+        'label', 'sqlalchemy_uri', 'user_email', 'schema', 'description',
+        'sql']
     show_columns = ['label', 'db_id', 'schema', 'description', 'sql']
     add_columns = show_columns
     edit_columns = add_columns
