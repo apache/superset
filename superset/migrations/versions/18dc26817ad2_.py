c409dddcc062492ad99fc441b151fc5720a2b217
Beto Dealmeida <roberto@dealmeida.net>
2019-01-18 14:56:51 -0800

    Merge heads

---
+++ superset/migrations/versions/18dc26817ad2_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 18dc26817ad2
+Revises: ('8b70aa3d0f87', 'a33a03f16c4a')
+Create Date: 2019-01-18 14:56:26.307684
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '18dc26817ad2'
+down_revision = ('8b70aa3d0f87', 'a33a03f16c4a')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
