7ab6070df4392b0ab14e5e347e5cc7358dca9d52
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-01-22 22:05:16 -0800

    Use lazy load and add description

--- superset/assets/src/visualizations/Iframe/IframeChartPlugin.js
+++ superset/assets/src/visualizations/Iframe/IframeChartPlugin.js
@@ -19,12 +19,11 @@
 import { t } from '@superset-ui/translation';
 import { ChartMetadata, ChartPlugin } from '@superset-ui/chart';
 import thumbnail from './images/thumbnail.png';
-import Iframe from './Iframe';
 import transformProps from './transformProps';
 
 const metadata = new ChartMetadata({
-  name: t('Iframe'),
-  description: '',
+  name: t('IFrame'),
+  description: 'HTML Inline Frame',
   thumbnail,
 });
 
@@ -32,7 +31,7 @@ export default class IframeChartPlugin extends ChartPlugin {
   constructor() {
     super({
       metadata,
-      chart: Iframe,
+      loadChart: () => import('./Iframe.jsx'),
       transformProps,
     });
   }
