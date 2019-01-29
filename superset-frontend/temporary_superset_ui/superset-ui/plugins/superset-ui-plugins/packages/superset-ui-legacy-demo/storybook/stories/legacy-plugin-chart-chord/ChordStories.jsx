560d17fe649120318412a6722379b1a9bceae211
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-01-29 00:31:10 -0800

    Add storybook for chord

---
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-demo/storybook/stories/legacy-plugin-chart-chord/ChordStories.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable no-magic-numbers */
+import React from 'react';
+import { SuperChart } from '@superset-ui/chart';
+
+export default [
+  {
+    renderStory: () => (
+      <SuperChart
+        className="chord"
+        chartType="chord"
+        chartProps={{
+          formData: {
+            colorScheme: 'd3Category10',
+            yAxisFormat: '.2f',
+          },
+          height: 500,
+          payload: {
+            data: {
+              matrix: [
+                [381, 16, 14, 5, 4, 6],
+                [29, 535, 132, 20, 21, 16],
+                [22, 18, 1, 3, 0, 214],
+                [3, 4, 462, 152, 132, 0],
+                [16, 32, 299, 123, 146, 1],
+                [22, 83, 53, 21, 22, 6],
+              ],
+              nodes: ['Hong Kong', 'Tokyo', 'Taipei', 'Beijing', 'Bangkok', 'Jakarta', 'Singapore'],
+            },
+          },
+          width: 500,
+        }}
+      />
+    ),
+    storyName: 'ChordChartPlugin',
+    storyPath: 'plugin-chart-chord',
+  },
+];
