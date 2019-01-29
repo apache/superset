3e8f2287d6155a21b86a391b9b50000bb7fbbf06
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-01-28 23:24:46 -0800

    storybook working for iframe

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-demo/storybook/stories/legacy-plugin-chart-iframe/index.js
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-demo/storybook/stories/legacy-plugin-chart-iframe/index.js
@@ -1,4 +1,7 @@
+import IframeChartPlugin from '@superset-ui/legacy-plugin-chart-iframe';
 import IframeStories from './IframeStories';
+
+new IframeChartPlugin().configure({ key: 'iframe' }).register();
 
 export default {
   examples: [...IframeStories],
