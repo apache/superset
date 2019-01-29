b5f5e1bcf035b05aaaf10f75b83259876eea4682
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-01-29 14:51:04 -0800

    update vendor css

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-plugin-chart-heatmap/src/Heatmap.js
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-plugin-chart-heatmap/src/Heatmap.js
@@ -24,7 +24,7 @@ import d3tip from 'd3-tip';
 import { getSequentialSchemeRegistry } from '@superset-ui/color';
 import { getNumberFormatter, NumberFormats } from '@superset-ui/number-format';
 
-import './d3tip.css';
+import './vendor/d3tip.css';
 import './Heatmap.css';
 
 const propTypes = {
