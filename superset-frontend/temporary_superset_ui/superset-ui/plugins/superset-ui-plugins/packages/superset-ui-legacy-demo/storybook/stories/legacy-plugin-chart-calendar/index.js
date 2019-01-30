b9bd1217ec93f10e2975e2a68e14b0fe212e752b
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-01-30 15:45:08 -0800

    Add calendar demo

---
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-demo/storybook/stories/legacy-plugin-chart-calendar/index.js
@@ -0,0 +1,8 @@
+import CalendarChartPlugin from '@superset-ui/legacy-plugin-chart-calendar';
+import Stories from './Stories';
+
+new CalendarChartPlugin().configure({ key: 'calendar' }).register();
+
+export default {
+  examples: [...Stories],
+};
