c40f6fd1e81a07d9cd5499be6de105a10f3dfa75
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-01-30 14:37:05 -0800

    resolve package version issue

---
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-demo/storybook/stories/legacy-plugin-chart-horizon/index.js
@@ -0,0 +1,8 @@
+import HorizonChartPlugin from '@superset-ui/legacy-plugin-chart-horizon';
+import Stories from './Stories';
+
+new HorizonChartPlugin().configure({ key: 'horizon' }).register();
+
+export default {
+  examples: [...Stories],
+};
