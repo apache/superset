5f6a5140b61504f92ee229d6603efb98cbcd8699
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-01-29 17:51:48 -0800

    Add histogram

---
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-demo/storybook/stories/legacy-plugin-chart-histogram/index.js
@@ -0,0 +1,8 @@
+import HistogramChartPlugin from '@superset-ui/legacy-plugin-chart-histogram';
+import Stories from './Stories';
+
+new HistogramChartPlugin().configure({ key: 'histogram' }).register();
+
+export default {
+  examples: [...Stories],
+};
