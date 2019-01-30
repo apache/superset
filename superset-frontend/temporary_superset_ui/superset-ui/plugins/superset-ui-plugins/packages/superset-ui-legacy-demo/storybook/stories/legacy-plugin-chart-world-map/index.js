83889b99b97acec6e46bf312cd39a08b2998037f
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-01-30 15:51:40 -0800

    Add world map demo

---
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-demo/storybook/stories/legacy-plugin-chart-world-map/index.js
@@ -0,0 +1,8 @@
+import WorldMapChartPlugin from '@superset-ui/legacy-plugin-chart-world-map';
+import Stories from './Stories';
+
+new WorldMapChartPlugin().configure({ key: 'world-map' }).register();
+
+export default {
+  examples: [...Stories],
+};
