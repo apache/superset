0bbf76bb26a036652e04eccf7d17e81be4fbdffa
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-01-30 13:55:25 -0800

    Add more subgenerators to create package and demo in superset-ui-legacy (#86)
    * feat: add new subgenerators for legacy package and demo
    * feat: make the default generator provide choices to choose subgenerators

--- superset-frontend/temporary_superset_ui/superset-ui/packages/generator-superset/generators/app/index.js
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/generator-superset/generators/app/index.js
@@ -1,12 +1,45 @@
+/* eslint-disable sort-keys */
+
 const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
 
 module.exports = class extends Generator {
-  initializing() {
+  async prompting() {
+    // Have Yeoman greet the user.
+    this.log(yosay(`Welcome to the rad ${chalk.red('generator-superset')} generator!`));
+
+    this.option('skipInstall');
+
+    this.answers = await this.prompt([
+      {
+        type: 'list',
+        name: 'subgenerator',
+        message: 'What do you want to do?',
+        choices: [
+          {
+            name: 'Create superset-ui package in the monorepo',
+            value: 'package',
+          },
+          {
+            name: 'Create superset-ui-legacy package in the monorepo',
+            value: 'legacy-plugin-chart',
+          },
+          {
+            name: 'Create superset-ui-legacy chart demo in storybook',
+            value: 'legacy-plugin-chart-demo',
+          },
+        ],
+      },
+    ]);
+  }
+
+  configuring() {
     // Redirect the default 'app' generator
     // to 'package' subgenerator
     // until there are multiple subgenerators
     // then this can be changed into a menu to select
     // subgenerator.
-    this.composeWith(require.resolve('../package'));
+    this.composeWith(require.resolve(`../${this.answers.subgenerator}`));
   }
 };
