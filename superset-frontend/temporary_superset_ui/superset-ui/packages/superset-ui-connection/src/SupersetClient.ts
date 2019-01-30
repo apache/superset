b12bb31bd0eaf931302224d9964bde9a89dbb724
Maxime Beauchemin <maximebeauchemin@gmail.com>
2019-01-30 13:56:40 -0800

    [SupersetClient] Implement put and delete HTTP methods (#85)
    * feat: Allow any http METHOD
    
    * Add method to RequestConfig
    
    * linting
    
    * fix tests

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-connection/src/SupersetClient.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-connection/src/SupersetClient.ts
@@ -13,12 +13,15 @@ function getInstance(maybeClient: SupersetClientClass | undefined): SupersetClie
 
 export interface SupersetClientInterface {
   configure: (config?: ClientConfig) => SupersetClientClass;
+  delete: (request: RequestConfig) => Promise<SupersetClientResponse>;
   get: (request: RequestConfig) => Promise<SupersetClientResponse>;
   getInstance: (maybeClient?: SupersetClientClass) => SupersetClientClass;
   init: (force?: boolean) => Promise<string | undefined>;
   isAuthenticated: () => boolean;
   post: (request: RequestConfig) => Promise<SupersetClientResponse>;
+  put: (request: RequestConfig) => Promise<SupersetClientResponse>;
   reAuthenticate: () => Promise<string | undefined>;
+  request: (request: RequestConfig) => Promise<SupersetClientResponse>;
   reset: () => void;
 }
 
@@ -28,12 +31,15 @@ const SupersetClient: SupersetClientInterface = {
 
     return singletonClient;
   },
+  delete: (request: RequestConfig) => getInstance(singletonClient).delete(request),
   get: (request: RequestConfig) => getInstance(singletonClient).get(request),
   getInstance,
   init: (force?: boolean) => getInstance(singletonClient).init(force),
   isAuthenticated: () => getInstance(singletonClient).isAuthenticated(),
   post: (request: RequestConfig) => getInstance(singletonClient).post(request),
+  put: (request: RequestConfig) => getInstance(singletonClient).put(request),
   reAuthenticate: () => getInstance(singletonClient).init(/* force = */ true),
+  request: (request: RequestConfig) => getInstance(singletonClient).get(request),
   reset: () => {
     singletonClient = undefined;
   },
