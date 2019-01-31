00522494851c3b100a30ebfe501050fb2fdd7362
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-01-30 17:47:19 -0800

    Treemap demo WIP

---
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-demo/storybook/stories/legacy-plugin-chart-treemap/Stories.jsx
@@ -0,0 +1,61 @@
+/* eslint-disable no-magic-numbers, sort-keys */
+import React from 'react';
+import { SuperChart } from '@superset-ui/chart';
+
+export default [
+  {
+    renderStory: () => (
+      <SuperChart
+        chartType="treemap"
+        chartProps={{
+          formData: {
+            colorScheme: 'd3Category10',
+            numberFormat: '.3s',
+            treeMapRatio: 1.618033988749895,
+          },
+          height: 600,
+          payload: {
+            data: [
+              {
+                name: 'sum__SP_POP_TOTL',
+                children: [
+                  {
+                    name: 'East Asia & Pacific',
+                    value: 92886288081,
+                  },
+                  {
+                    name: 'South Asia',
+                    value: 60081663698,
+                  },
+                  {
+                    name: 'Europe & Central Asia',
+                    value: 44338871387,
+                  },
+                  {
+                    name: 'Sub-Saharan Africa',
+                    value: 28161513610,
+                  },
+                  {
+                    name: 'Latin America & Caribbean',
+                    value: 23202014769,
+                  },
+                  {
+                    name: 'North America',
+                    value: 15077904555,
+                  },
+                  {
+                    name: 'Middle East & North Africa',
+                    value: 13187931450,
+                  },
+                ],
+              },
+            ],
+          },
+          width: 600,
+        }}
+      />
+    ),
+    storyName: 'TreemapChartPlugin',
+    storyPath: 'plugin-chart-treemap',
+  },
+];
