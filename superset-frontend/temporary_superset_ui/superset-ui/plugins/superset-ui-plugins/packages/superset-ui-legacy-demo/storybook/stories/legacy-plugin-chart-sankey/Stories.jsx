f94ba5631da5e7f065138cb7fb0b6ba1a28ce44c
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-01-31 12:02:59 -0800

    add sankey demo

---
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-demo/storybook/stories/legacy-plugin-chart-sankey/Stories.jsx
@@ -0,0 +1,76 @@
+/* eslint-disable no-magic-numbers */
+import React from 'react';
+import { SuperChart } from '@superset-ui/chart';
+
+export default [
+  {
+    renderStory: () => (
+      <SuperChart
+        chartType="sankey"
+        chartProps={{
+          formData: {
+            colorScheme: 'd3Category10',
+          },
+          height: 400,
+          payload: {
+            data: [
+              {
+                source: 'Energy',
+                target: 'Electricity and heat',
+                value: 24.9,
+              },
+              {
+                source: 'Energy',
+                target: 'Industry',
+                value: 14.7,
+              },
+              {
+                source: 'Energy',
+                target: 'Transportation',
+                value: 14.3,
+              },
+              {
+                source: 'Deforestation',
+                target: 'Carbon Dioxide',
+                value: 10.9,
+              },
+              {
+                source: 'Land Use Change',
+                target: 'Deforestation',
+                value: 10.9,
+              },
+              {
+                source: 'Road',
+                target: 'Carbon Dioxide',
+                value: 10.5,
+              },
+              {
+                source: 'Transportation',
+                target: 'Road',
+                value: 10.5,
+              },
+              {
+                source: 'Residential Buildings',
+                target: 'Carbon Dioxide',
+                value: 10.2,
+              },
+              {
+                source: 'Energy',
+                target: 'Other Fuel Combustion',
+                value: 8.6,
+              },
+              {
+                source: 'Other Industry',
+                target: 'Carbon Dioxide',
+                value: 6.6,
+              },
+            ],
+          },
+          width: 400,
+        }}
+      />
+    ),
+    storyName: 'SankeyChartPlugin',
+    storyPath: 'plugin-chart-sankey',
+  },
+];
