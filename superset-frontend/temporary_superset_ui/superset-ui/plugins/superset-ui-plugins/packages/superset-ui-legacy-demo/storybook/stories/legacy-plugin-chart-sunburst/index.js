e0d3056eb743ca0afcec2d255f79ccc53c97acfe
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-01-31 13:34:09 -0800

    add sunburst demo

---
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-demo/storybook/stories/legacy-plugin-chart-sunburst/index.js
@@ -0,0 +1,8 @@
+import SunburstChartPlugin from '@superset-ui/legacy-plugin-chart-sunburst';
+import Stories from './Stories';
+
+new SunburstChartPlugin().configure({ key: 'sunburst' }).register();
+
+export default {
+  examples: [...Stories],
+};
