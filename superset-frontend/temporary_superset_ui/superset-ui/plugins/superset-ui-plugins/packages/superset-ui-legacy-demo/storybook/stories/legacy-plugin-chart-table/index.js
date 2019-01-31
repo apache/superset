1ab50e3932fa58313886a7b349aeb9f740a67f60
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-01-31 01:26:35 -0800

    Add table demo and fix table jquery issue.

---
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-demo/storybook/stories/legacy-plugin-chart-table/index.js
@@ -0,0 +1,8 @@
+import TableChartPlugin from '@superset-ui/legacy-plugin-chart-table/src/index';
+import Stories from './Stories';
+
+new TableChartPlugin().configure({ key: 'table' }).register();
+
+export default {
+  examples: [...Stories],
+};
