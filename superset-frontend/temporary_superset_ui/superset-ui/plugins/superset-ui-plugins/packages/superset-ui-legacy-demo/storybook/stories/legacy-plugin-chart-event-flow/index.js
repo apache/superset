679cf78cf7aeb61c7fe1b48b7560cb2d8cd3bea7
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-01-31 15:53:12 -0800

    add event flow demo

---
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-demo/storybook/stories/legacy-plugin-chart-event-flow/index.js
@@ -0,0 +1,8 @@
+import EventFlowChartPlugin from '@superset-ui/legacy-plugin-chart-event-flow';
+import Stories from './Stories';
+
+new EventFlowChartPlugin().configure({ key: 'event-flow' }).register();
+
+export default {
+  examples: [...Stories],
+};
