53a51ff462a4d94fe5dda41d208e2fcc6aa036ac
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-02-05 13:45:56 -0800

    Add WordCloud plugin (#50)
    * scaffold
    
    * resolve dependency issues
    
    * use d3 v4
    
    * - Update peer dependencies
    - Getting src to build
    
    * Type word cloud
    
    * Add tests for transformProps
    
    * Update and simplify d3-cloud typing as the fix we've been waiting was merged.
    
    * resolve all tests
    
    * update unit tests
    
    * fix unit test
    
    * update readme

---
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-plugin-chart-word-cloud/test/transformProps.test.ts
@@ -0,0 +1,34 @@
+import { ChartProps } from '@superset-ui/chart';
+import transformProps from '../src/transformProps';
+
+describe('WordCloud tranformProps', () => {
+  const formData = {
+    colorScheme: 'bnbColors',
+    datasource: '3__table',
+    granularity_sqla: 'ds',
+    metric: 'sum__num',
+    rotation: 'square',
+    series: 'name',
+    sizeFrom: 10,
+    sizeTo: 70,
+  };
+  const chartProps = new ChartProps({
+    formData,
+    width: 800,
+    height: 600,
+    payload: {
+      data: [{ name: 'Hulk', sum__num: 1 }],
+    },
+  });
+
+  it('should tranform chart props for word cloud viz', () => {
+    expect(transformProps(chartProps)).toEqual({
+      colorScheme: 'bnbColors',
+      width: 800,
+      height: 600,
+      rotation: 'square',
+      sizeRange: [10, 70],
+      data: [{ size: 1, text: 'Hulk' }],
+    });
+  });
+});
