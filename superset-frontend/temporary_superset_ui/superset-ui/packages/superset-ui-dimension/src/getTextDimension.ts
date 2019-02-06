826e741b0e2d396f6312dcab396101a4054675a8
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-02-05 16:56:50 -0800

    make svg ignore pointer events in getTextDimension (#92)

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-dimension/src/getTextDimension.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-dimension/src/getTextDimension.ts
@@ -41,6 +41,7 @@ export default function getTextDimension(
   const svg = document.createElementNS(SVG_NS, 'svg');
   svg.style.position = 'absolute'; // so it won't disrupt page layout
   svg.style.opacity = '0'; // and not visible
+  svg.style.pointerEvents = 'none'; // and not capturing mouse events
   svg.appendChild(textNode);
   container.appendChild(svg);
 
