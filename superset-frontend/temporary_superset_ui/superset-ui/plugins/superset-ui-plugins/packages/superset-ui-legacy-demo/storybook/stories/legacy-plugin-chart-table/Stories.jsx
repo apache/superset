3343711b47d5dace7e60db279ac5b0e4f21b6bc6
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-02-08 15:53:24 -0800

    separate data files

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-demo/storybook/stories/legacy-plugin-chart-table/Stories.jsx
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-demo/storybook/stories/legacy-plugin-chart-table/Stories.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-magic-numbers */
 import React from 'react';
 import { SuperChart } from '@superset-ui/chart';
+import data from './data';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default [
@@ -30,53 +31,7 @@ export default [
             timeseriesLimitMetric: null,
           },
           height: 400,
-          payload: {
-            data: {
-              columns: ['name', 'sum__num'],
-              records: [
-                {
-                  name: 'Michael',
-                  sum__num: 2467063,
-                },
-                {
-                  name: 'Christopher',
-                  sum__num: 1725265,
-                },
-                {
-                  name: 'David',
-                  sum__num: 1570516,
-                },
-                {
-                  name: 'James',
-                  sum__num: 1506025,
-                },
-                {
-                  name: 'John',
-                  sum__num: 1426074,
-                },
-                {
-                  name: 'Matthew',
-                  sum__num: 1355803,
-                },
-                {
-                  name: 'Robert',
-                  sum__num: 1314800,
-                },
-                {
-                  name: 'Daniel',
-                  sum__num: 1159354,
-                },
-                {
-                  name: 'Joseph',
-                  sum__num: 1114098,
-                },
-                {
-                  name: 'William',
-                  sum__num: 1113701,
-                },
-              ],
-            },
-          },
+          payload: { data },
           width: 400,
         }}
       />
