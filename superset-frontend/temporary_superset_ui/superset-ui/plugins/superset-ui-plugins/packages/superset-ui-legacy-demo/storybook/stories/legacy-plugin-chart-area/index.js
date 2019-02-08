7d5e9e80c8dab05e3770ae9b5d37a1682166ec37
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-02-08 13:37:03 -0800

    add area chart demo

---
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-demo/storybook/stories/legacy-plugin-chart-area/index.js
@@ -0,0 +1,8 @@
+import { AreaChartPlugin } from '@superset-ui/legacy-preset-chart-nvd3';
+import Stories from './Stories';
+
+new AreaChartPlugin().configure({ key: 'area' }).register();
+
+export default {
+  examples: [...Stories],
+};
