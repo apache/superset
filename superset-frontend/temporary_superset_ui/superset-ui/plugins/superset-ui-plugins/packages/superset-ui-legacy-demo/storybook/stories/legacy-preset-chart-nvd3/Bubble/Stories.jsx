43f563e04a594bf7eafa6cb0843f923d5106f661
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-02-08 16:15:51 -0800

    add boxplot and bubble demo

---
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-demo/storybook/stories/legacy-preset-chart-nvd3/Bubble/Stories.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable no-magic-numbers */
+import React from 'react';
+import { SuperChart } from '@superset-ui/chart';
+import data from './data';
+
+export default [
+  {
+    renderStory: () => (
+      <SuperChart
+        chartType="bubble"
+        chartProps={{
+          datasource: { verboseMap: {} },
+          formData: {
+            bottomMargin: 'auto',
+            colorScheme: 'd3Category10',
+            entity: 'country_name',
+            leftMargin: 'auto',
+            maxBubbleSize: '50',
+            series: 'region',
+            showLegend: true,
+            size: 'sum__SP_POP_TOTL',
+            vizType: 'bubble',
+            x: 'sum__SP_RUR_TOTL_ZS',
+            xAxisFormat: '.3s',
+            xAxisLabel: 'x-axis label',
+            xAxisShowminmax: false,
+            xLogScale: false,
+            xTicksLayout: 'auto',
+            y: 'sum__SP_DYN_LE00_IN',
+            yAxisFormat: '.3s',
+            yAxisLabel: '',
+            yAxisShowminmax: false,
+            yLogScale: false,
+          },
+          height: 400,
+          payload: { data },
+          width: 400,
+        }}
+      />
+    ),
+    storyName: 'Basic',
+    storyPath: 'preset-chart-nvd3|BubbleChartPlugin',
+  },
+];
