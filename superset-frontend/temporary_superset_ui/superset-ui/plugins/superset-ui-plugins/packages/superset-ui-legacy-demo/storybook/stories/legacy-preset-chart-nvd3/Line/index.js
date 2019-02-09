6385b6b4b84bb7a9fbbaef92c4af4b4cf526b9d4
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-02-08 17:14:02 -0800

    add storybooks for nvd3

---
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-demo/storybook/stories/legacy-preset-chart-nvd3/Line/index.js
@@ -0,0 +1,8 @@
+import { LineChartPlugin } from '@superset-ui/legacy-preset-chart-nvd3';
+import Stories from './Stories';
+
+new LineChartPlugin().configure({ key: 'line' }).register();
+
+export default {
+  examples: [...Stories],
+};
