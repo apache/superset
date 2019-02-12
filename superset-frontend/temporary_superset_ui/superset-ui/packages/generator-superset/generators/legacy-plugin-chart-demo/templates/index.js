d7da5ff0cb034d2e5d73d258eb1fe213cb46a289
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-02-12 11:06:03 -0800

    Merge branch 'kristw--template'

--- superset-frontend/temporary_superset_ui/superset-ui/packages/generator-superset/generators/legacy-plugin-chart-demo/templates/index.js
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/generator-superset/generators/legacy-plugin-chart-demo/templates/index.js
@@ -1,4 +1,4 @@
-import <%= packageLabel %>ChartPlugin from '@superset-ui/legacy-plugin-chart-<%= packageName %>';
+import <%= packageLabel %>ChartPlugin from '../../../../superset-ui-legacy-plugin-chart-<%= packageName %>';
 import Stories from './Stories';
 
 new <%= packageLabel %>ChartPlugin().configure({ key: '<%= packageName %>' }).register();
