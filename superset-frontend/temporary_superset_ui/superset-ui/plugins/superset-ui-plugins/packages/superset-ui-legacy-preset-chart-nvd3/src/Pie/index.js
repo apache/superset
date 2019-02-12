a05cd770e2f5cc30d370558f2e6205b97bc0a998
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-02-12 14:43:35 -0800

    Update references

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-preset-chart-nvd3/src/Pie/index.js
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-preset-chart-nvd3/src/Pie/index.js
@@ -32,7 +32,7 @@ const metadata = new ChartMetadata({
 export default class PieChartPlugin extends ChartPlugin {
   constructor() {
     super({
-      loadChart: () => import('../ReactNVD3.js'),
+      loadChart: () => import('../ReactNVD3'),
       metadata,
       transformProps,
     });
