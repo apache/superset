48a320fae67dbc80e54c8a3c919c2abf7b0d6dd0
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-02-13 12:14:15 -0800

    add mapbox demo

---
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-demo/storybook/stories/legacy-plugin-chart-map-box/index.js
@@ -0,0 +1,8 @@
+import MapBoxChartPlugin from '../../../../superset-ui-legacy-plugin-chart-map-box';
+import Stories from './Stories';
+
+new MapBoxChartPlugin().configure({ key: 'map-box' }).register();
+
+export default {
+  examples: [...Stories],
+};
