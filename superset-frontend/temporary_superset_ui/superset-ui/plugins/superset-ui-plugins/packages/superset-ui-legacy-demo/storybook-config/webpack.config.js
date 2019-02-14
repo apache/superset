6e442aab7651fd3d04ca43b2498a2962759951bb
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-02-13 20:36:00 -0800

    enable live development through storybook

---
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-demo/storybook-config/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+// Export a function. Accept the base config as the only param.
+module.exports = (storybookBaseConfig, configType, defaultConfig) => {
+  // configType has a value of 'DEVELOPMENT' or 'PRODUCTION'
+  // You can change the configuration based on that.
+  // 'PRODUCTION' is used when building the static version of storybook.
+
+  defaultConfig.module.rules[0].use[0].options.plugins.push('@babel/plugin-syntax-dynamic-import');
+
+  defaultConfig.module.rules.push({
+    include: new RegExp(`${path.resolve(__dirname, '../../superset-ui-legacy-')}.+/src`),
+    test: /\.jsx?$/,
+    use: defaultConfig.module.rules[0].use,
+  });
+
+  // Return the altered config
+  return defaultConfig;
+};
