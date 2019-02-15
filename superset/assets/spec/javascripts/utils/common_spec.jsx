c4745400d078847291548569f20d02d1809fbf96
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-02-15 11:51:49 -0800

    remove dead code (#6884)

--- superset/assets/spec/javascripts/utils/common_spec.jsx
+++ superset/assets/spec/javascripts/utils/common_spec.jsx
@@ -16,46 +16,9 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import { isTruthy, optionFromValue, prepareCopyToClipboardTabularData } from '../../../src/utils/common';
+import { optionFromValue, prepareCopyToClipboardTabularData } from '../../../src/utils/common';
 
 describe('utils/common', () => {
-  describe('isTruthy', () => {
-    it('evals false-looking strings properly', () => {
-      expect(isTruthy('f')).toBe(false);
-      expect(isTruthy('false')).toBe(false);
-      expect(isTruthy('no')).toBe(false);
-      expect(isTruthy('n')).toBe(false);
-      expect(isTruthy('F')).toBe(false);
-      expect(isTruthy('False')).toBe(false);
-      expect(isTruthy('NO')).toBe(false);
-      expect(isTruthy('N')).toBe(false);
-    });
-    it('evals true-looking strings properly', () => {
-      expect(isTruthy('t')).toBe(true);
-      expect(isTruthy('true')).toBe(true);
-      expect(isTruthy('yes')).toBe(true);
-      expect(isTruthy('y')).toBe(true);
-      expect(isTruthy('Y')).toBe(true);
-      expect(isTruthy('True')).toBe(true);
-      expect(isTruthy('Yes')).toBe(true);
-      expect(isTruthy('YES')).toBe(true);
-    });
-    it('evals bools properly', () => {
-      expect(isTruthy(false)).toBe(false);
-      expect(isTruthy(true)).toBe(true);
-    });
-    it('evals ints properly', () => {
-      expect(isTruthy(0)).toBe(false);
-      expect(isTruthy(1)).toBe(true);
-    });
-    it('evals constants properly', () => {
-      expect(isTruthy(null)).toBe(false);
-      expect(isTruthy(undefined)).toBe(false);
-    });
-    it('string auto is false', () => {
-      expect(isTruthy('false')).toBe(false);
-    });
-  });
   describe('optionFromValue', () => {
     it('converts values as expected', () => {
       expect(optionFromValue(false)).toEqual({ value: false, label: '<false>' });
