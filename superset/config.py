100196249e3363eee9810e3af477483bb370ad86
Grace Guo <grace.guo@airbnb.com>
2019-02-15 23:10:05 -0800

    Improve Superset logger (#6879)
    * enable beacon
    
    * add middleware
    
    * add unit tests

--- superset/config.py
+++ superset/config.py
@@ -96,7 +96,7 @@ QUERY_SEARCH_LIMIT = 1000
 WTF_CSRF_ENABLED = True
 
 # Add endpoints that need to be exempt from CSRF protection
-WTF_CSRF_EXEMPT_LIST = []
+WTF_CSRF_EXEMPT_LIST = ['superset.views.core.log']
 
 # Whether to run the web server in debug mode or not
 DEBUG = os.environ.get('FLASK_ENV') == 'development'
