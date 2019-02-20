0332e8d602da1234495a58afbb2efc7212105004
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-02-20 11:46:59 -0800

    Feat: Set default color scheme for the color registries (#108)
    * Set default color scheme for the color registries
    
    * fix: Add unit test

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-color/src/CategoricalSchemeRegistrySingleton.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-color/src/CategoricalSchemeRegistrySingleton.ts
@@ -1,8 +1,15 @@
 import { makeSingleton } from '@superset-ui/core';
 import CategoricalScheme from './CategoricalScheme';
 import ColorSchemeRegistry from './ColorSchemeRegistry';
+import schemes from './colorSchemes/categorical/d3';
 
-class CategoricalSchemeRegistry extends ColorSchemeRegistry<CategoricalScheme> {}
+class CategoricalSchemeRegistry extends ColorSchemeRegistry<CategoricalScheme> {
+  constructor() {
+    super();
+
+    this.registerValue('SUPERSET_DEFAULT', schemes[0]);
+  }
+}
 
 const getInstance = makeSingleton(CategoricalSchemeRegistry);
 
