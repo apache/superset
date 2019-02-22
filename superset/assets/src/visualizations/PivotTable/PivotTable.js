56a604f3218ff790e3d84c97a3f74e7a2df0e276
Lahiru <lahiru.perera@qmatic.com>
2019-02-22 10:39:00 +0530

    162011357 : Copy of canned report for print in Superset
    Fix the rows sorting

--- superset/assets/src/visualizations/PivotTable/PivotTable.js
+++ superset/assets/src/visualizations/PivotTable/PivotTable.js
@@ -80,7 +80,7 @@ function PivotTable(element, props) {
       scrollCollapse: true,
       scrollX: true,
     });
-    table.column('-1').order('desc').draw();
+    table.column('0').order('asc').draw();
     fixDataTableBodyHeight($container.find('.dataTables_wrapper'), height);
   } else {
     // When there is more than 1 group by column we just render the table, without using
