6d6a43ff4602a2bb9d09f1eded553a95ef78c63c
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-02-25 15:57:35 -0800

    chore: style commitlint config file

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/commitlint.config.js
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/commitlint.config.js
@@ -1,1 +1,3 @@
-module.exports = { extends: ['@commitlint/config-conventional', '@commitlint/config-lerna-scopes'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional', '@commitlint/config-lerna-scopes'],
+};
