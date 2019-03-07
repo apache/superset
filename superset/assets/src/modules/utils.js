65c8f019cf7da4e52bb3dd194fe7f8aa82c56de3
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-03-06 22:30:05 -0800

    refactor: bump @superset-ui versions  (#6982)
    * feat: bump @superset-ui versions
    
    * refactor: remove visUtils and use @superset-ui/dimension
    
    * refactor: remove dead code
    
    * fix: update package lock
    
    * fix: lint two files
    
    * fix: package lock

--- superset/assets/src/modules/utils.js
+++ superset/assets/src/modules/utils.js
@@ -18,54 +18,6 @@
  */
 /* eslint camelcase: 0 */
 import $ from 'jquery';
-import { select as d3Select } from 'd3-selection';
-
-/*
-  Utility function that takes a d3 svg:text selection and a max width, and splits the
-  text's text across multiple tspan lines such that any given line does not exceed max width
-
-  If text does not span multiple lines AND adjustedY is passed,
-  will set the text to the passed val
-*/
-export function wrapSvgText(text, width, adjustedY) {
-  const lineHeight = 1;
-  // ems
-  text.each(function () {
-    const d3Text = d3Select(this);
-    const words = d3Text.text().split(/\s+/);
-    let word;
-    let line = [];
-    let lineNumber = 0;
-    const x = d3Text.attr('x');
-    const y = d3Text.attr('y');
-    const dy = parseFloat(d3Text.attr('dy'));
-    let tspan =
-      d3Text.text(null).append('tspan').attr('x', x)
-            .attr('y', y)
-            .attr('dy', dy + 'em');
-
-    let didWrap = false;
-    for (let i = 0; i < words.length; i++) {
-      word = words[i];
-      line.push(word);
-      tspan.text(line.join(' '));
-      if (tspan.node().getComputedTextLength() > width) {
-        line.pop();
-        // remove word that pushes over the limit
-        tspan.text(line.join(' '));
-        line = [word];
-        tspan =
-          d3Text.append('tspan').attr('x', x).attr('y', y)
-                .attr('dy', ++lineNumber * lineHeight + dy + 'em')
-                .text(word);
-        didWrap = true;
-      }
-    }
-    if (!didWrap && typeof adjustedY !== 'undefined') {
-      tspan.attr('y', adjustedY);
-    }
-  });
-}
 
 /**
  * Sets the body and title content of a modal, and shows it. Assumes HTML for modal exists and that
