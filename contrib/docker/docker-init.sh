6b05cee8651cefc5ea88ff01c46bb0aa72faf7a9
yuguang <yuguang@analyticsmd.com>
2019-03-07 14:42:29 -0800

    update docker files with Qventus changes

--- contrib/docker/docker-init.sh
+++ contrib/docker/docker-init.sh
@@ -2,8 +2,20 @@
 
 set -ex
 
-# Create an admin user (you will be prompted to set username, first and last name before setting a password)
-fabmanager create-admin --app superset
+export APP_NAME=superset
+export APP_HOME=/home/work
+
+if [ "${ENVIRONMENT}" = "" ] || [ "${STACK}" = "" ]; then
+    echo "ENVIRONMENT and STACK environment variables must be set" >&2
+    exit 1
+fi
+
+# autodetect AWS_REGION if not specified
+if [ "${AWS_REGION}" = "" ]; then
+    EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+    EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
+    export AWS_REGION="${EC2_REGION}"
+fi
 
 # Initialize the database
 superset db upgrade
@@ -15,3 +27,23 @@ fi
 
 # Create default roles and permissions
 superset init
+
+# Start superset worker for SQL Lab
+superset worker &
+
+# fetch SSL certificates
+for name in cert privkey ; do
+    aws ssm get-parameter \
+        --region "${AWS_REGION}" \
+        --with-decryption \
+        --output text --query "Parameter.Value" \
+        --name "/${ENVIRONMENT}/${STACK}/ssl/${name}" \
+        > "${APP_HOME}/${name}.pem"
+done
+
+gunicorn --certfile=${APP_HOME}/cert.pem  --keyfile=${APP_HOME}/privkey.pem -w 50 -k gevent \
+  -b  0.0.0.0:8088 \
+  --timeout 1200 \
+  --limit-request-line 0 \
+  --limit-request-field_size 0 \
+  superset:app
