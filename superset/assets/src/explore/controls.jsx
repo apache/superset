e1f69e712ea8b60bca9ee00fa785fb78c69ccd6c
Lana <cittalana@gmail.com>
2019-03-06 21:06:41 -0700

    Fix groupable only columns populating in filters

--- superset/assets/src/explore/controls.jsx
+++ superset/assets/src/explore/controls.jsx
@@ -1966,7 +1966,7 @@ export const controls = {
     default: null,
     description: '',
     mapStateToProps: state => ({
-      columns: state.datasource ? state.datasource.columns : [],
+      columns: state.datasource ? state.datasource.columns.filter(c => c.filterable) : [],
       savedMetrics: state.datasource ? state.datasource.metrics : [],
       datasource: state.datasource,
     }),
