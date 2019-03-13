6c8db64442393f6fe896a3030fa5e0f38caaa178
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-03-13 16:59:43 -0700

    fix: nvd3 line chart y axis bounds (#17)
    * fix: 🐛 y axis bounds
    
    * fix: clip edge only when necessary
    
    * docs: update storybook
    
    * fix: remove src ref from storybook
    
    * fix: import order

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-plugins-demo/storybook/stories/legacy-preset-chart-nvd3/Line/index.js
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-plugins-demo/storybook/stories/legacy-preset-chart-nvd3/Line/index.js
@@ -1,8 +1,9 @@
 import { LineChartPlugin } from '../../../../../superset-ui-legacy-preset-chart-nvd3';
 import Stories from './Stories';
+import YAxisStories from './YAxisStories';
 
 new LineChartPlugin().configure({ key: 'line' }).register();
 
 export default {
-  examples: [...Stories],
+  examples: [...Stories, ...YAxisStories],
 };
