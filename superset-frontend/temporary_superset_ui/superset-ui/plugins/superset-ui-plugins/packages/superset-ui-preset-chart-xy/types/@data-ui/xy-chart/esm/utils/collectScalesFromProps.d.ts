d0f7f8cb7cb039402143e7ae4ecf8506fdb8c564
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-03-13 10:13:15 -0700

    feat: add typescript declaration for external packages (#12)
    * feat: add declaration for external packages
    
    * feat: add dependency
    
    * fix: address comments

---
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-preset-chart-xy/types/@data-ui/xy-chart/esm/utils/collectScalesFromProps.d.ts
@@ -0,0 +1,34 @@
+
+declare module '@data-ui/xy-chart/esm/utils/collectScalesFromProps' {
+  import { ScaleLinear, ScaleBand, ScaleContinuousNumeric, ScaleDiverging, ScaleIdentity, ScaleLogarithmic, ScaleOrdinal, ScalePoint, ScalePower, ScaleQuantile, ScaleQuantize, ScaleSequential, ScaleThreshold, ScaleTime } from "d3-scale";
+  import React from "react";
+  import { ChartTheme } from "@data-ui/theme";
+
+  interface ScaleConfig {
+    [key: string]: any;
+  }
+
+  interface Scale {
+    domain(): any[];
+    ticks(count?: number): number[];
+    tickFormat(count?: number, specifier?: string): ((d: number | { valueOf(): number }) => string);
+  }
+
+  export default function collectScalesFromProps(props: {
+    width: number;
+    height: number;
+    margin: {
+      top: number;
+      left: number;
+      bottom: number;
+      right: number;
+    },
+    xScale: ScaleConfig,
+    yScale: ScaleConfig,
+    theme: ChartTheme,
+    children: React.ReactElement[],
+  }): {
+    xScale: Scale;
+    yScale: Scale;
+  };
+}
