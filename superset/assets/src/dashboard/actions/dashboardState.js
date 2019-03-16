a889be8ae0e08164f33f1cd39b9517ab5ec42f83
Grace Guo <grace.guo@airbnb.com>
2019-03-16 10:44:14 -0700

    [fix] explore chart from dashboard missed slice title (#7046)

--- superset/assets/src/dashboard/actions/dashboardState.js
+++ superset/assets/src/dashboard/actions/dashboardState.js
@@ -236,7 +236,10 @@ export function addSliceToDashboard(id) {
         ),
       );
     }
-    const form_data = selectedSlice.form_data;
+    const form_data = {
+      ...selectedSlice.form_data,
+      slice_id: selectedSlice.slice_id,
+    };
     const newChart = {
       ...initChart,
       id,
