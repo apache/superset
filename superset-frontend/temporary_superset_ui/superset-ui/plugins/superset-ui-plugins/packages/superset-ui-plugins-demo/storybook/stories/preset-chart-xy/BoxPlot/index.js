2c4cde361aa116369f2184ac49f07f6d711fbf75
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-03-22 11:37:45 -0700

    refactor: remove new box plot code until refactored (#25)

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-plugins-demo/storybook/stories/preset-chart-xy/BoxPlot/index.js
+++
@@ -1,8 +0,0 @@
-import { BoxPlotChartPlugin } from '../../../../../superset-ui-preset-chart-xy/src';
-import Stories from './Stories';
-
-new BoxPlotChartPlugin().configure({ key: 'box-plot2' }).register();
-
-export default {
-  examples: [...Stories],
-};
