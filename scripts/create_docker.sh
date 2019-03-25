aff2197d49fb648718c5686f535335100f1b5fed
bipinsoniguavus <bipin.soni@guavus.com>
2019-03-25 23:09:29 +0530

    fix(docker-script): Fixed build issue found in regression testing (#100)
    Removed unavailable file from scripts
    
    UIC-1021

--- scripts/create_docker.sh
+++ scripts/create_docker.sh
@@ -1,10 +1,10 @@
 set -e
 # moving docker files to root
-cp ./docker/{docker-build.sh,docker-compose.yml,docker-entrypoint.sh,docker-init.sh,Dockerfile} .
+cp ./docker/{docker-build.sh,docker-init.sh,Dockerfile} .
 cp ./docker/superset_config.py superset/
 
 bash -x docker-build.sh
 
 # deleting moved files
-rm -rf ./{docker-build.sh,docker-compose.yml,docker-entrypoint.sh,docker-init.sh,Dockerfile}
+rm -rf ./{docker-build.sh,docker-init.sh,Dockerfile}
 rm -rf ./superset/superset_config.py
