905ef0d9fd5a3dbb69b369aecde79e66d57befc9
Don Bowman <db@donbowman.ca>
2019-03-25 19:20:13 -0400

    Add HTTP Basic Auth for Druid (Issue #4776) (#6795)
    * Add HTTP Basic Auth for Druid (Issue #4776)
    
    This should resolve [Issue #4776](https://github.com/apache/incubator-superset/issues/4776)
    
    Add HTTP Basic Auth to the Druid Broker.
    
    Signed-off-by: Don Bowman <don@agilicus.com>
    
    * Encrypt broker password in database
    
    Signed-off-by: Don Bowman <don@agilicus.com>
    
    * Merge from master, new migration needed
    
    Signed-off-by: Don Bowman <don@agilicus.com>
    
    * Do not expose hashed druid basic auth password
    
    * docs: add description_columns for druid basic auth

--- superset/connectors/druid/models.py
+++ superset/connectors/druid/models.py
@@ -45,6 +45,7 @@ from sqlalchemy import (
     Boolean, Column, DateTime, ForeignKey, Integer, String, Table, Text, UniqueConstraint,
 )
 from sqlalchemy.orm import backref, relationship
+from sqlalchemy_utils import EncryptedType
 
 from superset import conf, db, security_manager
 from superset.connectors.base.models import BaseColumn, BaseDatasource, BaseMetric
@@ -102,9 +103,11 @@ class DruidCluster(Model, AuditMixinNullable, ImportMixin):
     broker_endpoint = Column(String(255), default='druid/v2')
     metadata_last_refreshed = Column(DateTime)
     cache_timeout = Column(Integer)
+    broker_user = Column(String(255))
+    broker_pass = Column(EncryptedType(String(255), conf.get('SECRET_KEY')))
 
     export_fields = ('cluster_name', 'broker_host', 'broker_port',
-                     'broker_endpoint', 'cache_timeout')
+                     'broker_endpoint', 'cache_timeout', 'broker_user')
     update_from_object_fields = export_fields
     export_children = ['datasources']
 
@@ -139,16 +142,20 @@ class DruidCluster(Model, AuditMixinNullable, ImportMixin):
         cli = PyDruid(
             self.get_base_url(self.broker_host, self.broker_port),
             self.broker_endpoint)
+        if self.broker_user and self.broker_pass:
+            cli.set_basic_auth_credentials(self.broker_user, self.broker_pass)
         return cli
 
     def get_datasources(self):
         endpoint = self.get_base_broker_url() + '/datasources'
-        return json.loads(requests.get(endpoint).text)
+        auth = requests.auth.HTTPBasicAuth(self.broker_user, self.broker_pass)
+        return json.loads(requests.get(endpoint, auth=auth).text)
 
     def get_druid_version(self):
         endpoint = self.get_base_url(
             self.broker_host, self.broker_port) + '/status'
-        return json.loads(requests.get(endpoint).text)['version']
+        auth = requests.auth.HTTPBasicAuth(self.broker_user, self.broker_pass)
+        return json.loads(requests.get(endpoint, auth=auth).text)['version']
 
     @property
     @utils.memoized
