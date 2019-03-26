ddadc645acf7c5a85c410b4a4d7d52ec94df9381
Tresdon Jones <tjones@spotx.tv>
2019-03-26 11:21:44 -0600

    Update migration

--- superset/migrations/versions/d6ffdf31bdd4_add_published_column_to_dashboards.py
+++ superset/migrations/versions/d6ffdf31bdd4_add_published_column_to_dashboards.py
@@ -17,14 +17,14 @@
 """Add published column to dashboards
 
 Revision ID: d6ffdf31bdd4
-Revises: 18dc26817ad2
+Revises: 45e7da7cfeba
 Create Date: 2018-03-30 14:00:44.929483
 
 """
 
 # revision identifiers, used by Alembic.
 revision = 'd6ffdf31bdd4'
-down_revision = '18dc26817ad2'
+down_revision = '45e7da7cfeba'
 
 from alembic import op
 import sqlalchemy as sa
