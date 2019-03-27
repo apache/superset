d862804287b08f5ab955847fc806b18d3a55be74
Dave Smith <dave.a.smith@gmail.com>
2019-03-26 18:36:14 -0700

    add license to test file, and remove blank line at EOF

--- tests/stats_logger_tests.py
+++ tests/stats_logger_tests.py
@@ -1,3 +1,20 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""Unit tests for Superset"""
 from unittest import TestCase
 from unittest.mock import Mock, patch
 
@@ -31,4 +48,3 @@ class StatsdStatsLoggerTest(TestCase):
 
             stats_logger = StatsdStatsLogger()
             self.verify_client_calls(stats_logger, mock_client)
-
