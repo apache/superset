689d7d6e8bbb581cf83045f4e76b35eb5e1e05ac
Maxime Beauchemin <maximebeauchemin@gmail.com>
2019-03-27 17:53:32 -0700

    [bugfix] SQL Lab 'Filter Results' doesn't stick (#7104)
    When using a "Search Results" criteria, the subset of rows that match
    the criteria get displayed. While this the filter is applied, if another
    query is run, the filter is still active, but not displayed in the input
    text box. After this change, the state of the input box sticks after
    subsequent queries.

--- superset/assets/src/SqlLab/components/ResultSet.jsx
+++ superset/assets/src/SqlLab/components/ResultSet.jsx
@@ -153,8 +153,9 @@ export default class ResultSet extends React.PureComponent {
                 <input
                   type="text"
                   onChange={this.changeSearch.bind(this)}
+                  value={this.state.searchText}
                   className="form-control input-sm"
-                  placeholder={t('Search Results')}
+                  placeholder={t('Filter Results')}
                 />
               }
             </div>
