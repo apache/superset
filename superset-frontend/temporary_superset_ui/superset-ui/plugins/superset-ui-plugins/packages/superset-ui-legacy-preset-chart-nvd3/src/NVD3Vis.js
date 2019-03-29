4bd482dbc4702030c2b79aae3676950ba1490805
Thomas Wang <17309187+datability-io@users.noreply.github.com>
2019-03-29 10:20:51 -0700

    fix: line chart tooltip should use full datetime format (#24)

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-preset-chart-nvd3/src/NVD3Vis.js
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-legacy-preset-chart-nvd3/src/NVD3Vis.js
@@ -554,7 +554,7 @@ function nvd3Vis(element, props) {
       chart.useInteractiveGuideline(true);
       if (vizType === 'line') {
         chart.interactiveLayer.tooltip.contentGenerator(d =>
-          generateRichLineTooltipContent(d, xAxisFormatter, yAxisFormatter),
+          generateRichLineTooltipContent(d, smartDateVerboseFormatter, yAxisFormatter),
         );
       }
     }
