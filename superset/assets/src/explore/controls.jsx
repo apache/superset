5766f05fa71cf1b8413030f8de4de95c4e8dffba
marcussorealheis <marcuseagan@gmail.com>
2019-03-30 20:06:18 -0700

    simple change adding the tab override boolean

--- superset/assets/src/explore/controls.jsx
+++ superset/assets/src/explore/controls.jsx
@@ -1303,6 +1303,7 @@ export const controls = {
     label: t('Rotation'),
     choices: formatSelectOptions(['random', 'flat', 'square']),
     renderTrigger: true,
+    tabOverride: 'data',
     default: 'flat',
     description: t('Rotation to apply to words in the cloud'),
   },
