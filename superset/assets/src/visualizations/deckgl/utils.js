f58bf28b3855cbb3cf721161dc02841ebd8b97c3
Christine Chambers <christine.d.hang@gmail.com>
2019-04-03 11:19:56 -0700

    chore: revert bignumber.js patch for charts and reapply the original bignumber.js change to SQL editor in an opt-in fashion (#7210)
    * revert: bignumber patch to prevent rendering regression in charts
    
    * Revert "Fix rendering regression from the introduction of bignumber (#6937)"
    
    * fix: consume the bignumber.js reversion -- apply the bignumber conversion in `actions/sqlLab.js` where it is needed (aka opt into bignumber.js).
    
    * Revert "Fix deck.gl form data (#6953)" b/c formData now returns snake_cased properties for deck vizzes.

--- superset/assets/src/visualizations/deckgl/utils.js
+++ superset/assets/src/visualizations/deckgl/utils.js
@@ -24,8 +24,8 @@ import { hexToRGB } from '../../modules/colors';
 const DEFAULT_NUM_BUCKETS = 10;
 
 export function getBreakPoints({
-    breakPoints: formDataBreakPoints,
-    numBuckets: formDataNumBuckets,
+    break_points: formDataBreakPoints,
+    num_buckets: formDataNumBuckets,
   }, features, accessor) {
   if (!features) {
     return [];
@@ -46,15 +46,15 @@ export function getBreakPoints({
 }
 
 export function getBreakPointColorScaler({
-    breakPoints: formDataBreakPoints,
-    numBuckets: formDataNumBuckets,
-    linearColorScheme,
+    break_points: formDataBreakPoints,
+    num_buckets: formDataNumBuckets,
+    linear_color_scheme: linearColorScheme,
     opacity,
   }, features, accessor) {
   const breakPoints = formDataBreakPoints || formDataNumBuckets
     ? getBreakPoints({
-      breakPoints: formDataBreakPoints,
-      numBuckets: formDataNumBuckets,
+      break_points: formDataBreakPoints,
+      num_buckets: formDataNumBuckets,
     }, features, accessor)
     : null;
   const colorScheme = Array.isArray(linearColorScheme)
