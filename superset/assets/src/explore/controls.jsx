05792979149076036c84950b2009468ee4ec0aa9
michellethomas <michelle.q.thomas@gmail.com>
2019-04-08 15:08:47 -0700

    [filter_box] allow empty filters list (#7220) (#7244)
    in some cases, people want a time filter only on filter box, without
    specifying dimensions (filters), this allows that
    
    (cherry picked from commit e39b16994946b2a7975af3ed76245e77303329e2)

--- superset/assets/src/explore/controls.jsx
+++ superset/assets/src/explore/controls.jsx
@@ -2319,7 +2319,7 @@ export const controls = {
     type: 'CollectionControl',
     label: 'Filters',
     description: t('Filter configuration for the filter box'),
-    validators: [v.nonEmpty],
+    validators: [],
     controlName: 'FilterBoxItemControl',
     mapStateToProps: ({ datasource }) => ({ datasource }),
   },
