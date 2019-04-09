2a67e8e457bcf07d9f07ab56ddf232d057c4c838
Kim Truong <47833996+khtruong@users.noreply.github.com>
2019-04-09 15:46:47 -0700

    fix: Handle rendering a single point (#7256)
    * fix: Handle rendering a single point
    
    * fix: typo

--- superset/assets/src/visualizations/deckgl/layers/common.jsx
+++ superset/assets/src/visualizations/deckgl/layers/common.jsx
@@ -20,12 +20,50 @@ import { fitBounds } from 'viewport-mercator-project';
 import * as d3array from 'd3-array';
 import sandboxedEval from '../../../modules/sandbox';
 
+const PADDING = 0.25;
+const GEO_BOUNDS = {
+  LAT_MIN: -90,
+  LAT_MAX: 90,
+  LNG_MIN: -180,
+  LNG_MAX: 180,
+};
+
+/**
+ * Get the latitude bounds if latitude is a single coordinate
+ * @param latExt Latitude range
+ */
+function getLatBoundsForSingleCoordinate(latExt) {
+  const latMin = latExt[0] - PADDING < GEO_BOUNDS.LAT_MIN
+    ? GEO_BOUNDS.LAT_MIN
+    : latExt[0] - PADDING;
+  const latMax = latExt[1] + PADDING > GEO_BOUNDS.LAT_MAX
+    ? GEO_BOUNDS.LAT_MAX
+    : latExt[1] + PADDING;
+  return [latMin, latMax];
+}
+
+/**
+ * Get the longitude bounds if longitude is a single coordinate
+ * @param lngExt Longitude range
+ */
+function getLngBoundsForSingleCoordinate(lngExt) {
+  const lngMin = lngExt[0] - PADDING < GEO_BOUNDS.LNG_MIN
+    ? GEO_BOUNDS.LNG_MIN
+    : lngExt[0] - PADDING;
+  const lngMax = lngExt[1] + PADDING > GEO_BOUNDS.LNG_MAX
+    ? GEO_BOUNDS.LNG_MAX
+    : lngExt[1] + PADDING;
+  return [lngMin, lngMax];
+}
+
 export function getBounds(points) {
   const latExt = d3array.extent(points, d => d[1]);
   const lngExt = d3array.extent(points, d => d[0]);
+  const latBounds = latExt[0] === latExt[1] ? getLatBoundsForSingleCoordinate(latExt) : latExt;
+  const lngBounds = lngExt[0] === lngExt[1] ? getLngBoundsForSingleCoordinate(lngExt) : lngExt;
   return [
-    [lngExt[0], latExt[0]],
-    [lngExt[1], latExt[1]],
+    [lngBounds[0], latBounds[0]],
+    [lngBounds[1], latBounds[1]],
   ];
 }
 
