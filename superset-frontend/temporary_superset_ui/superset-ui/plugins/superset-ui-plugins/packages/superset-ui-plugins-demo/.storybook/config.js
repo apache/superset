413cb7f0d867e64caf08f8b5065f53d037f0b05e
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-04-12 09:59:23 -0700

    build: multi-level nesting in storybook (#58)
    * build: multi-level nesting in storybook
    
    * feat: select country
    
    * fix: config
    
    * fix: minor

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-plugins-demo/.storybook/config.js
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-plugins-demo/.storybook/config.js
@@ -6,6 +6,7 @@ addParameters({
     addonPanelInRight: false,
     enableShortcuts: false,
     goFullScreen: false,
+    hierarchyRootSeparator: null,
     hierarchySeparator: /\|/,
     selectedAddonPanel: undefined, // The order of addons in the "Addon panel" is the same as you import them in 'addons.js'. The first panel will be opened by default as you run Storybook
     showAddonPanel: true,
