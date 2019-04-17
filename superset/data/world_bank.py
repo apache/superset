3d082667140fdd3c9c8918e3a0408b8339d80405
Maxime Beauchemin <maximebeauchemin@gmail.com>
2019-04-17 13:19:14 -0700

    [load_examples] download data at runtime (#7314)
    * [load_examples] download data at runtime
    
    When running `superset load_examples` to load example data sets,
    Superset used to load from the local package. This created a few issues
    notably around licensing (what are these datasets licensed as?) and
    around package size.
    
    For now, I moved the data sets here:
    https://github.com/apache-superset/examples-data
    
    Altered the logic to download the data from where it is stored.
    
    * flakes

--- superset/data/world_bank.py
+++ superset/data/world_bank.py
@@ -16,7 +16,6 @@
 # under the License.
 """Loads datasets, dashboards and slices in a new superset instance"""
 # pylint: disable=C,R,W
-import gzip
 import json
 import os
 import textwrap
@@ -31,6 +30,7 @@ from .helpers import (
     config,
     Dash,
     DATA_FOLDER,
+    get_example_data,
     get_slice_json,
     merge_slice,
     misc_dash_slices,
@@ -43,8 +43,8 @@ from .helpers import (
 def load_world_bank_health_n_pop():
     """Loads the world bank health dataset, slices and a dashboard"""
     tbl_name = 'wb_health_population'
-    with gzip.open(os.path.join(DATA_FOLDER, 'countries.json.gz')) as f:
-        pdf = pd.read_json(f)
+    data = get_example_data('countries.json.gz')
+    pdf = pd.read_json(data)
     pdf.columns = [col.replace('.', '_') for col in pdf.columns]
     pdf.year = pd.to_datetime(pdf.year)
     pdf.to_sql(
