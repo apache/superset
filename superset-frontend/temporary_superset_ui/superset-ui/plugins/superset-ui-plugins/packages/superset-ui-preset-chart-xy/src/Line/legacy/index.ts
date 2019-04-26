ede2e92d97f0eee8e23fc7fd4a0567d1e2b49029
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-04-26 13:25:51 -0700

    fix: lazily create metadata (#74)

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-preset-chart-xy/src/Line/legacy/index.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-preset-chart-xy/src/Line/legacy/index.ts
@@ -1,12 +1,12 @@
 import { ChartPlugin } from '@superset-ui/chart';
-import metadata from './metadata';
 import transformProps from './transformProps';
+import createMetadata from '../createMetadata';
 
 export default class LineChartPlugin extends ChartPlugin {
   constructor() {
     super({
       loadChart: () => import('../Line'),
-      metadata,
+      metadata: createMetadata(true),
       transformProps,
     });
   }
