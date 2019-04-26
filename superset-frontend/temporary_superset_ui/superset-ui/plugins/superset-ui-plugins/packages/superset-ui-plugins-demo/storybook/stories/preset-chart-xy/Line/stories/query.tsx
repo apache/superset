23c4aa3d1e4c0bc35ce07f7441d9f21bb06cb6e9
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-04-25 21:50:05 -0700

    feat: integrate line chart with build query and update typings (#73)
    * feat: integrate line chart with buildQuery
    
    * fix: typings
    
    * fix: minor
    
    * feat: derive groupbys
    
    * fix: tooltip for single series

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-plugins-demo/storybook/stories/preset-chart-xy/Line/stories/query.tsx
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-plugins-demo/storybook/stories/preset-chart-xy/Line/stories/query.tsx
@@ -26,15 +26,40 @@ export default [
         formData: {
           viz_type: LINE_PLUGIN_TYPE,
           datasource: '3__table',
-          url_params: {},
           granularity_sqla: 'ds',
           time_grain_sqla: 'P1D',
           time_range: '100 years ago : now',
           metrics: ['sum__num'],
-          adhoc_filters: [],
-          groupby: [],
           limit: 25,
           row_limit: 50000,
+          encoding: {
+            x: {
+              field: '__timestamp',
+              type: 'temporal',
+              format: '%Y',
+              scale: {
+                type: 'time',
+              },
+              axis: {
+                title: 'Time',
+              },
+            },
+            y: {
+              field: 'sum__num',
+              type: 'quantitative',
+              scale: {
+                type: 'linear',
+              },
+              axis: {
+                title: 'Number of Babies',
+              },
+            },
+            color: {
+              field: 'gender',
+              type: 'nominal',
+              legend: true,
+            },
+          },
         },
       },
     },
