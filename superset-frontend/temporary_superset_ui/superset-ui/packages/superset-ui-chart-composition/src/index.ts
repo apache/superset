1ca10375de4d96aa10ee995f2919bc54fc32ddc6
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-04-26 17:14:28 -0700

    feat: add chart companion components (#139)
    * feat: add chart companion components
    
    * test: add more tests
    
    * test: improve coverage
    
    * test: fix failed test
    
    * test: mock ResizeObserver
    
    * fix: test coverage
    
    * test: fix set dimension
    
    * feat: make keyColumn optional
    
    * fix: address comments
    
    * fix: test delay

---
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-chart-composition/src/index.ts
@@ -0,0 +1,4 @@
+export { default as ChartFrame } from './ChartFrame';
+export { default as WithLegend } from './legend/WithLegend';
+export { default as TooltipFrame } from './tooltip/TooltipFrame';
+export { default as TooltipTable } from './tooltip/TooltipTable';
