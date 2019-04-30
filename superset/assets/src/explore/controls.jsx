6c6c19f4e904cc3e90eb5be108a4001e1955fafd
michellethomas <michelle.q.thomas@gmail.com>
2019-04-30 11:26:13 -0700

    Setting renderTrigger on label_colors (#7410)

--- superset/assets/src/explore/controls.jsx
+++ superset/assets/src/explore/controls.jsx
@@ -2095,6 +2095,7 @@ export const controls = {
     type: 'ColorMapControl',
     label: t('Color Map'),
     default: {},
+    renderTrigger: true,
     mapStateToProps: state => ({
       colorNamespace: state.form_data.color_namespace,
       colorScheme: state.form_data.color_scheme,
