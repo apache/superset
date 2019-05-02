8df6e89bf1ce97c629e66ae23eaf34c0d1eac986
Robert Harris <trebor@lyft.com>
2019-05-02 13:02:19 -0700

    feat: add sankey chart with loops (#77)
    * feat(chart): add basic working look sankey chart
    
    * fix(doc): update readme and remove dead code
    
    * fix(clean): clean up both style and code
    
    * fix(reorg): reogranize for clarity
    
    * fix(path): remove dev path to chart
    
    * fix(lint): remove lint
    
    * fix(pr): fixes for PR
    
    * fix(path): fix dev path
    
    * fix(lint): remove unused

---
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-plugins-demo/storybook/stories/legacy-plugin-chart-sankey-loop/data.js
@@ -0,0 +1,33 @@
+/* eslint-disable sort-keys, no-magic-numbers */
+export default [
+  {
+    source: 'Lisdoonvarna',
+    target: 'Cliffs of Moher',
+    value: 50,
+  },
+  {
+    source: 'Cliffs of Moher',
+    target: 'Lisdoonvarna',
+    value: 35,
+  },
+  {
+    source: 'Cliffs of Moher',
+    target: 'Killarney',
+    value: 25,
+  },
+  {
+    source: 'Lisdoonvarna',
+    target: 'Killarney',
+    value: 25,
+  },
+  {
+    source: 'Lisdoonvarna',
+    target: 'Kinvarra',
+    value: 25,
+  },
+  {
+    source: 'Kinvarra',
+    target: 'Lisdoonvarna',
+    value: 25,
+  },
+];
