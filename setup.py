9e44fb4fefdca5483c2323afe99de55389c4a92a
Russell Jurney <russell.jurney@gmail.com>
2019-05-01 20:46:39 -0700

    Removed --console-log and superset runserver (#7421)

--- setup.py
+++ setup.py
@@ -108,7 +108,6 @@ setup(
     ],
     extras_require={
         'cors': ['flask-cors>=2.0.0'],
-        'console_log': ['console_log==0.2.10'],
         'hive': [
             'pyhive[hive]>=0.6.1',
             'tableschema',
