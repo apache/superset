a6be8d885e5a105791a005f357ad56d0f3f59f71
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-05-08 01:18:25 -0700

    feat: bring lazy import back (#84)

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-preset-chart-xy/src/Line/legacy/index.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-preset-chart-xy/src/Line/legacy/index.ts
@@ -1,12 +1,11 @@
 import { ChartPlugin } from '@superset-ui/chart';
 import transformProps from './transformProps';
 import createMetadata from '../createMetadata';
-import Chart from '../Line';
 
 export default class LineChartPlugin extends ChartPlugin {
   constructor() {
     super({
-      Chart,
+      loadChart: () => import('../Line'),
       metadata: createMetadata(true),
       transformProps,
     });
