3a0dd2741d868b3016ff1345698697eb9105c4b2
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-05-12 00:52:03 -0700

    feat: add scatter plot (#90)
    * feat: bubble in progress
    
    * feat: working scatter plot with basic and bubble demo
    
    * feat: add legacy shim
    
    * feat: support group as separate channel
    
    * fix: box plot formatting
    
    * fix: lint
    
    * fix: remove/clarify comments

---
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-preset-chart-xy/src/ScatterPlot/createTooltip.tsx
@@ -0,0 +1,57 @@
+/* eslint-disable no-magic-numbers */
+
+import React from 'react';
+import TooltipFrame from '../components/tooltip/TooltipFrame';
+import TooltipTable from '../components/tooltip/TooltipTable';
+import Encoder from './Encoder';
+import { isFieldDef } from '../encodeable/types/ChannelDef';
+import { EncodedPoint } from './ScatterPlot';
+
+export default function createTooltip(encoder: Encoder) {
+  function Tooltip({ datum }: { datum: EncodedPoint }) {
+    const { channels, commonChannels } = encoder;
+    const { x, y, size, fill, stroke } = channels;
+
+    const tooltipRows = [
+      { key: 'x', keyColumn: x.getTitle(), valueColumn: x.format(datum.data) },
+      { key: 'y', keyColumn: y.getTitle(), valueColumn: y.format(datum.data) },
+    ];
+
+    if (isFieldDef(fill.definition)) {
+      tooltipRows.push({
+        key: 'fill',
+        keyColumn: fill.getTitle(),
+        valueColumn: fill.format(datum.data),
+      });
+    }
+    if (isFieldDef(stroke.definition)) {
+      tooltipRows.push({
+        key: 'stroke',
+        keyColumn: stroke.getTitle(),
+        valueColumn: stroke.format(datum.data),
+      });
+    }
+    if (isFieldDef(size.definition)) {
+      tooltipRows.push({
+        key: 'size',
+        keyColumn: size.getTitle(),
+        valueColumn: size.format(datum.data),
+      });
+    }
+    commonChannels.group.forEach(g => {
+      tooltipRows.push({
+        key: `${g.name}`,
+        keyColumn: g.getTitle(),
+        valueColumn: g.format(datum.data),
+      });
+    });
+
+    return (
+      <TooltipFrame>
+        <TooltipTable data={tooltipRows} />
+      </TooltipFrame>
+    );
+  }
+
+  return Tooltip;
+}
---
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-preset-chart-xy/src/ScatterPlot/legacy/transformProps.ts
@@ -0,0 +1,96 @@
+/* eslint-disable sort-keys */
+import { ChartProps } from '@superset-ui/chart';
+import { flatMap } from 'lodash';
+
+interface DataRow {
+  key: string[];
+  values: {
+    [key: string]: any;
+  }[];
+}
+
+export default function transformProps(chartProps: ChartProps) {
+  const { width, height, formData, payload } = chartProps;
+  const {
+    colorScheme,
+    entity,
+    maxBubbleSize,
+    series,
+    showLegend,
+    size,
+    x,
+    xAxisFormat,
+    xAxisLabel,
+    // TODO: These fields are not supported yet
+    // xAxisShowminmax,
+    // xLogScale,
+    y,
+    yAxisLabel,
+    yAxisFormat,
+    // TODO: These fields are not supported yet
+    // yAxisShowminmax,
+    // yLogScale,
+  } = formData;
+  const data = payload.data as DataRow[];
+
+  return {
+    data: flatMap(
+      data.map((row: DataRow) =>
+        row.values.map(v => ({
+          [x]: v[x],
+          [y]: v[y],
+          [series]: v[series],
+          [size]: v[size],
+          [entity]: v[entity],
+        })),
+      ),
+    ),
+    width,
+    height,
+    encoding: {
+      x: {
+        field: x,
+        type: 'quantitive',
+        format: xAxisFormat,
+        scale: {
+          type: 'linear',
+        },
+        axis: {
+          orient: 'bottom',
+          title: xAxisLabel,
+        },
+      },
+      y: {
+        field: y,
+        type: 'quantitative',
+        format: yAxisFormat,
+        scale: {
+          type: 'linear',
+        },
+        axis: {
+          orient: 'left',
+          title: yAxisLabel,
+        },
+      },
+      size: {
+        field: size,
+        type: 'quantitative',
+        scale: {
+          type: 'linear',
+          range: [0, maxBubbleSize],
+        },
+      },
+      fill: {
+        field: series,
+        type: 'nominal',
+        scale: {
+          scheme: colorScheme,
+        },
+        legend: showLegend,
+      },
+    },
+    commonEncoding: {
+      group: [{ field: entity }],
+    },
+  };
+}
