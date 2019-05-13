14f138eadf292a1cc8475e2ec7a571d2cfddc763
Maxime Beauchemin <maximebeauchemin@gmail.com>
2019-05-13 00:34:34 -0500

    Break line before LIMIT statement to prevent trailing comment issue (#7485)
    * Break line before LIMIT statement to prevent trailing comment issue
    
    This may not be a perfect solution but it addresses the issue in 7483
    
    closes https://github.com/apache/incubator-superset/issues/7483
    
    * fix tests

--- superset/sql_parse.py
+++ superset/sql_parse.py
@@ -168,7 +168,7 @@ class ParsedQuery(object):
         """returns the query with the specified limit"""
         """does not change the underlying query"""
         if not self._limit:
-            return self.sql + ' LIMIT ' + str(new_limit)
+            return f'{self.sql}\nLIMIT {new_limit}'
         limit_pos = None
         tokens = self._parsed[0].tokens
         # Add all items to before_str until there is a limit
