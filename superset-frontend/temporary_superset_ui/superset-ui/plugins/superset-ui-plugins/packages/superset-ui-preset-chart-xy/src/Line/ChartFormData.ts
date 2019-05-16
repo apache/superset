cb2ccaaafc76f75e64b116209617fd7500f88b36
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-05-16 10:51:13 -0700

    refactor: rename channel color to stroke (#100)
    * refactor: rename channel color to stroke
    
    * fix: legend item color and types
    
    * feat: can override createTooltip
    
    * refactor: renderTooltip

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-preset-chart-xy/src/Line/ChartFormData.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-preset-chart-xy/src/Line/ChartFormData.ts
@@ -1,10 +1,12 @@
 import { ChartFormData } from '@superset-ui/chart';
 import { Margin } from '@superset-ui/dimension';
+import { ChartTheme } from '@data-ui/theme';
 import { Encoding } from './Encoder';
 
 type LineFormData = ChartFormData & {
   encoding: Encoding;
   margin?: Margin;
+  theme?: ChartTheme;
 };
 
 export default LineFormData;
--- superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-preset-chart-xy/src/Line/transformProps.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-preset-chart-xy/src/Line/transformProps.ts
@@ -1,9 +1,11 @@
 import { ChartProps } from '@superset-ui/chart';
+import ChartFormData from './ChartFormData';
 
 /* eslint-disable sort-keys */
 
 export default function transformProps(chartProps: ChartProps) {
-  const { width, height, formData, payload } = chartProps;
+  const { width, height, payload } = chartProps;
+  const formData = chartProps.formData as ChartFormData;
   const { encoding, margin, theme } = formData;
   const { data } = payload;
 
