0ed4fff6841e7a24334e0cf5ca004cf948f7ea22
Maxime Beauchemin <maximebeauchemin@gmail.com>
2019-05-16 21:11:14 -0700

    Disabling flask-talisman by default
    flask-talisman was enabled recently and while it may be virtuous in some
    cases, it seems to break things out of the box.
    
    Locally and in dev mode, upon my first redirect it sends to HTTPS and
    things it crashes.
    
    I think it should be opt-in, maybe we can recommend turning this on in
    production in the docs?

--- superset/config.py
+++ superset/config.py
@@ -613,7 +613,7 @@ SQL_VALIDATORS_BY_ENGINE = {
 }
 
 # Do you want Talisman enabled?
-TALISMAN_ENABLED = True
+TALISMAN_ENABLED = False
 # If you want Talisman, how do you want it configured??
 TALISMAN_CONFIG = {
     'content_security_policy': None,
