634555e0980cbac422467b00df0230236bb173d7
Russell Jurney <russell.jurney@gmail.com>
2019-05-18 22:11:24 +0200

    Now migrating all tables that inherit directly or indirectly from ImportMixin.

--- superset/migrations/versions/e5200a951e62_add_dashboards_uuid.py
+++ superset/migrations/versions/e5200a951e62_add_dashboards_uuid.py
@@ -14,7 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""Adds a dashboards.uuid column.
+"""Adds uuid columns to all classes with ImportMixin: dashboards, datasources, dbs, slices, tables, dashboard_email_schedules, slice_email_schedules
 
 Revision ID: e5200a951e62
 Revises: e9df189e5c7e
@@ -31,7 +31,7 @@ from superset import db
 
 # revision identifiers, used by Alembic.
 revision = 'e5200a951e62'
-down_revision = 'e9df189e5c7e'
+down_revision = 'afc69274c25a'
 
 Base = declarative_base()
 
@@ -48,13 +48,43 @@ class Datasource(Base):
     id = Column(Integer, primary_key=True)
     uuid = Column(UUIDType(binary=False), default=get_uuid)
 
+class Database(Base):
+    __tablename__ = 'dbs'
+    id = Column(Integer, primary_key=True)
+    uuid = Column(UUIDType(binary=False), default=get_uuid)
+
+class DruidCluster(Base):
+    __tablename__ = 'clusters'
+    id = Column(Integer, primary_key=True)
+    uuid = Column(UUIDType(binary=False), default=get_uuid)
+
+class DruidMetric(Base):
+    __tablename__ = 'metrics'
+    id = Column(Integer, primary_key=True)
+    uuid = Column(UUIDType(binary=False), default=get_uuid)
+    
 class Slice(Base):
     __tablename__ = 'slices'
     id = Column(Integer, primary_key=True)
     uuid = Column(UUIDType(binary=False), default=get_uuid)
 
-class Database(Base):
-    __tablename__ = 'dbs'
+class SqlaTable(Base):
+    __tablename__ = 'tables'
+    id = Column(Integer, primary_key=True)
+    uuid = Column(UUIDType(binary=False), default=get_uuid)
+
+class SqlMetric(Base):
+    __tablename__ = 'sql_metrics'
+    id = Column(Integer, primary_key=True)
+    uuid = Column(UUIDType(binary=False), default=get_uuid)
+
+class DashboardEmailSchedule(Base):
+    __tablename__ = 'dashboard_email_schedules'
+    id = Column(Integer, primary_key=True)
+    uuid = Column(UUIDType(binary=False), default=get_uuid)
+
+class SliceEmailSchedule(Base):
+    __tablename__ = 'slice_email_schedules'
     id = Column(Integer, primary_key=True)
     uuid = Column(UUIDType(binary=False), default=get_uuid)
 
@@ -63,46 +93,29 @@ def upgrade():
     bind = op.get_bind()
     session = db.Session(bind=bind)
 
-    with op.batch_alter_table('dashboards') as batch_op:
-        batch_op.add_column(Column('uuid', UUIDType(binary=False), default=get_uuid))
-    for d in session.query(Dashboard):
-        d.uuid = get_uuid()
-        session.merge(d)
-    with op.batch_alter_table('dashboards') as batch_op:
-        batch_op.alter_column('uuid', nullable=False)
-        batch_op.create_unique_constraint('uq_uuid', 'uuid')
-    session.commit()
-    
-    with op.batch_alter_table('datasources') as batch_op:
-        batch_op.add_column(Column('uuid', UUIDType(binary=False), default=get_uuid))
-    for d in session.query(Datasource):
-        d.uuid = get_uuid()
-        session.merge(d)
-    with op.batch_alter_table('datasources') as batch_op:
-        batch_op.alter_column('uuid', nullable=False)
-        batch_op.create_unique_constraint('uq_uuid', 'uuid')
-    session.commit()
+    def add_uuid_column(col_name, _type):
+        """Add a uuid column to a given table"""
+        with op.batch_alter_table(col_name) as batch_op:
+            batch_op.add_column(Column('uuid', UUIDType(binary=False), default=get_uuid))
+        for s in session.query(_type):
+            s.uuid = get_uuid()
+            session.merge(s)
+        with op.batch_alter_table(col_name) as batch_op:
+            batch_op.alter_column('uuid', nullable=False)
+            batch_op.create_unique_constraint('uq_uuid', 'uuid')
+        session.commit()
 
-    with op.batch_alter_table('slices') as batch_op:
-        batch_op.add_column(Column('uuid', UUIDType(binary=False), default=get_uuid))
-    for s in session.query(Slice):
-        s.uuid = get_uuid()
-        session.merge(s)
-    with op.batch_alter_table('slices') as batch_op:
-        batch_op.alter_column('uuid', nullable=False)
-        batch_op.create_unique_constraint('uq_uuid', 'uuid')
-    session.commit()
+    add_uuid_column('dashboards', Dashboard)
+    add_uuid_column('datasources', Datasource)
+    add_uuid_column('dbs', Database)
+    add_uuid_column('clusters', DruidCluster)
+    add_uuid_column('metrics', DruidMetric)
+    add_uuid_column('slices', Slice)
+    add_uuid_column('sql_metrics', SqlMetric)
+    add_uuid_column('tables', SqlaTable)
+    add_uuid_column('dashboard_email_schedules', DashboardEmailSchedule)
+    add_uuid_column('slice_email_schedules', SliceEmailSchedule)
 
-    with op.batch_alter_table('dbs') as batch_op:
-        batch_op.add_column(Column('uuid', UUIDType(binary=False), default=get_uuid))
-    for d in session.query(Database):
-        d.uuid = get_uuid()
-        session.merge(d)
-    with op.batch_alter_table('slices') as batch_op:
-        batch_op.alter_column('uuid', nullable=False)
-        batch_op.create_unique_constraint('uq_uuid', 'uuid')
-    session.commit()
-    
     session.close()
 
 def downgrade():
@@ -112,8 +125,26 @@ def downgrade():
     with op.batch_alter_table('datasources') as batch_op:
         batch_op.drop_column('uuid')
 
+    with op.batch_alter_table('dbs') as batch_op:
+        batch_op.drop_column('uuid')
+    
+    with op.batch_alter_table('clusters') as batch_op:
+        batch_op.drop_column('uuid')
+    
+    with op.batch_alter_table('metrics') as batch_op:
+        batch_op.drop_column('uuid')
+
     with op.batch_alter_table('slices') as batch_op:
         batch_op.drop_column('uuid')
     
-    with op.batch_alter_table('dbs') as batch_op:
+    with op.batch_alter_table('sql_metrics') as batch_op:
+        batch_op.drop_column('uuid')
+    
+    with op.batch_alter_table('tables') as batch_op:
+        batch_op.drop_column('uuid')
+
+    with op.batch_alter_table('dashboard_email_schedules') as batch_op:
+        batch_op.drop_column('uuid')
+
+    with op.batch_alter_table('slice_email_schedules') as batch_op:
         batch_op.drop_column('uuid')
