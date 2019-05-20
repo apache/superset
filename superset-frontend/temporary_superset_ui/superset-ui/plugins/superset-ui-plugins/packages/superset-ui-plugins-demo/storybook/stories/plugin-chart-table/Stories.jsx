df33931235b9b4de619c68aa8b601e26dc55feac
Conglei Shi <conglei.shi@airbnb.com>
2019-05-20 13:43:30 -0700

    feat(tablevis): this pr is to add a new tablevis plguin to the system

---
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-plugins-demo/storybook/stories/plugin-chart-table/Stories.jsx
@@ -0,0 +1,76 @@
+/* eslint-disable no-magic-numbers */
+import React from 'react';
+import { SuperChart } from '@superset-ui/chart';
+import dataLegacy from './dataLegacy';
+
+export default [
+  {
+    renderStory: () => (
+      <SuperChart
+        chartType="table2-legacy"
+        chartProps={{
+          datasource: {
+            columnFormats: {},
+            verboseMap: {
+              name: 'name',
+              sum__num: 'sum__num',
+            },
+          },
+          filters: {},
+          formData: {
+            alignPn: false,
+            colorPn: true,
+            includeSearch: false,
+            metrics: ['sum__num', 'trend'],
+            orderDesc: true,
+            pageLength: 0,
+            percentMetrics: ['sum__num'],
+            tableFilter: false,
+            tableTimestampFormat: '%Y-%m-%d %H:%M:%S',
+            timeseriesLimitMetric: 'trend',
+          },
+          height: 400,
+          payload: { data: dataLegacy },
+          width: 400,
+        }}
+      />
+    ),
+    storyName: 'Legacy',
+    storyPath: 'plugin-chart-table|TableChartPlugin',
+  },
+  {
+    renderStory: () => (
+      <SuperChart
+        chartType="table2-legacy"
+        chartProps={{
+          datasource: {
+            columnFormats: {},
+            verboseMap: {
+              name: 'name',
+              sum__num: 'sum__num',
+            },
+          },
+          filters: {},
+          formData: {
+            alignPn: false,
+            colorPn: true,
+            includeSearch: false,
+            pageLength: 0,
+            metrics: ['sum__num', 'trend'],
+            orderDesc: true,
+            pageLength: 0,
+            percentMetrics: [],
+            tableFilter: true,
+            tableTimestampFormat: '%Y-%m-%d %H:%M:%S',
+            timeseriesLimitMetric: null,
+          },
+          height: 400,
+          payload: { data: dataLegacy },
+          width: 400,
+        }}
+      />
+    ),
+    storyName: 'Legacy-TableFilter',
+    storyPath: 'plugin-chart-table|TableChartPlugin',
+  },
+];
