eb443a8af99e79a2a9de47d247ae112445579728
Beto Dealmeida <roberto@dealmeida.net>
2019-05-30 16:48:59 -0700

    Fix unit tests

--- superset/polygon.py
+++ superset/polygon.py
@@ -1,4 +1,25 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+# pylint: disable=C,R,W
+
+
 class PolygonEncoding:
+
+    name = 'undefined'
 
     @classmethod
     def for_json(cls):
@@ -10,13 +31,13 @@ class PolygonEncoding:
 
         def deser(code):
             return polygons[code]
-        
+
         return deser
 
     @classmethod
-    def to_location(codes, cache):
+    def to_location(cls, codes, cache):
         raise NotImplementedError('Subclasses MUST implement to_location')
 
     @classmethod
-    def to_polygon(codes, cache):
+    def to_polygon(cls, codes, cache):
         raise NotImplementedError('Subclasses MUST implement to_polygon')
