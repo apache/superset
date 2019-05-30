e39964118515b337615bc55ba879524465a2e79d
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-05-30 10:19:05 -0700

    fix: convert scale config to data-ui's config correctly (#115)

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-preset-chart-xy/src/BoxPlot/BoxPlot.tsx
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-preset-chart-xy/src/BoxPlot/BoxPlot.tsx
@@ -13,6 +13,7 @@ import { PartialSpec } from '../encodeable/types/Specification';
 import createMarginSelector, { DEFAULT_MARGIN } from '../utils/selectors/createMarginSelector';
 import createXYChartLayoutSelector from '../utils/selectors/createXYChartLayoutSelector';
 import { BoxPlotDataRow } from './types';
+import convertScaleToDataUIScale from '../utils/convertScaleToDataUIScaleShape';
 
 export interface TooltipProps {
   datum: BoxPlotDataRow;
@@ -116,8 +117,8 @@ export default class BoxPlot extends React.PureComponent<Props> {
         )}
         showYGrid
         theme={theme}
-        xScale={channels.x.definition.scale}
-        yScale={channels.y.definition.scale}
+        xScale={convertScaleToDataUIScale(channels.x.scale!.config)}
+        yScale={convertScaleToDataUIScale(channels.y.scale!.config)}
       >
         {children}
         {layout.renderXAxis()}
--- superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-preset-chart-xy/src/encodeable/types/Scale.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-preset-chart-xy/src/encodeable/types/Scale.ts
@@ -5,11 +5,13 @@ import { SchemeParams, ScaleType } from 'vega-lite/build/src/scale';
 export interface Scale<Output extends Value = Value> {
   type?: ScaleType;
   domain?: number[] | string[] | boolean[] | DateTime[];
+  paddingInner?: number;
+  paddingOuter?: number;
   range?: Output[];
   clamp?: boolean;
   nice?: boolean;
   scheme?: string | SchemeParams;
-  // vega-lite does not have this
+  /** vega-lite does not have this */
   namespace?: string;
 }
 
