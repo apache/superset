5ba48cf6ae786cc1ae3b3f5e73d6b2cc2b6e2402
Conglei Shi <conglei.shi@airbnb.com>
2019-05-30 11:38:09 -0700

    fix(adjusted upstream break change): adjusted upstream break change

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-plugin-chart-table/src/processMetrics.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-plugin-chart-table/src/processMetrics.ts
@@ -1,4 +1,4 @@
-import { FormDataMetric, Metric } from '@superset-ui/chart';
+import { ChartFormDataMetric, AdhocMetric } from '@superset-ui/chart';
 import { PlainObject } from './types';
 
 export default function processMetrics({
@@ -6,14 +6,14 @@ export default function processMetrics({
   percentMetrics,
   records,
 }: {
-  metrics: FormDataMetric[];
-  percentMetrics: FormDataMetric[];
+  metrics: ChartFormDataMetric[];
+  percentMetrics: ChartFormDataMetric[];
   records: PlainObject[];
 }) {
-  const processedMetrics = (metrics || []).map(m => (m as Metric).label || (m as string));
+  const processedMetrics = (metrics || []).map(m => (m as AdhocMetric).label || (m as string));
 
   const processedPercentMetrics = (percentMetrics || [])
-    .map(m => (m as Metric).label || (m as string))
+    .map(m => (m as AdhocMetric).label || (m as string))
     .map(m => `%${m}`);
 
   return processedMetrics
