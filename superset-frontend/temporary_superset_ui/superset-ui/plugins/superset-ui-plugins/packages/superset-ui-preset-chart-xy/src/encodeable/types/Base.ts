1c9c3adbb15fc95e2aa003d414bca008db84af6b
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-06-05 14:25:25 -0700

    fix: legend types

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-preset-chart-xy/src/encodeable/types/Base.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-preset-chart-xy/src/encodeable/types/Base.ts
@@ -1,2 +1,13 @@
 // eslint-disable-next-line import/prefer-default-export
 export type ObjectWithKeysFromAndValueType<T extends {}, V> = { [key in keyof T]: V };
+
+export type Unarray<T> = T extends Array<infer U> ? U : T;
+export type MayBeArray<T> = T | T[];
+
+export function isArray<T>(maybeArray: T | T[]): maybeArray is T[] {
+  return Array.isArray(maybeArray);
+}
+
+export function isNotArray<T>(maybeArray: T | T[]): maybeArray is T {
+  return !Array.isArray(maybeArray);
+}
