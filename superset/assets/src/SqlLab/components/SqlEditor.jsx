cec69056b4d75b7178b896c1aeda6492a7cfe00c
Enrico Berti <hello@enricoberti.com>
2019-06-05 07:59:36 +0200

    [SQL Lab] CTAS now uses the right async configuration (#7175)
    Closes #6224

--- superset/assets/src/SqlLab/components/SqlEditor.jsx
+++ superset/assets/src/SqlLab/components/SqlEditor.jsx
@@ -242,10 +242,10 @@ class SqlEditor extends React.PureComponent {
   }
   runQuery() {
     if (this.props.database) {
-      this.startQuery(this.props.database.allow_run_async);
+      this.startQuery();
     }
   }
-  startQuery(runAsync = false, ctas = false) {
+  startQuery(ctas = false) {
     const qe = this.props.queryEditor;
     const query = {
       dbId: qe.dbId,
@@ -256,7 +256,7 @@ class SqlEditor extends React.PureComponent {
       tempTableName: ctas ? this.state.ctas : '',
       templateParams: qe.templateParams,
       queryLimit: qe.queryLimit || this.props.defaultQueryLimit,
-      runAsync,
+      runAsync: this.props.database ? this.props.database.allow_run_async : false,
       ctas,
     };
     this.props.actions.runQuery(query);
@@ -268,7 +268,7 @@ class SqlEditor extends React.PureComponent {
     }
   }
   createTableAs() {
-    this.startQuery(true, true);
+    this.startQuery(true);
   }
   ctasChanged(event) {
     this.setState({ ctas: event.target.value });
