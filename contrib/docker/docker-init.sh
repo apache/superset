b9f3c1eae83b11159d227feb7a5888ba43fb395b
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2019-06-07 06:15:00 +0100

    [docs] Fix, deprecate fabmanager cli (#7660)

--- contrib/docker/docker-init.sh
+++ contrib/docker/docker-init.sh
@@ -18,7 +18,7 @@
 set -ex
 
 # Create an admin user (you will be prompted to set username, first and last name before setting a password)
-fabmanager create-admin --app superset
+flask fab create-admin --app superset
 
 # Initialize the database
 superset db upgrade
