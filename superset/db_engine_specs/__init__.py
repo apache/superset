ad712152121681c2eb61b34d87cd8bb807b1d161
Ville Brofeldt <ville.brofeldt@iki.fi>
2019-06-08 15:00:31 +0300

    Dynamically load all engine specs

--- superset/db_engine_specs/__init__.py
+++ superset/db_engine_specs/__init__.py
@@ -28,12 +28,22 @@ at all. The classes here will use a common interface to specify all this.
 
 The general idea is to use static classes and an inheritance scheme.
 """
+from importlib import import_module
 import inspect
+from pathlib import Path
+import pkgutil
+from typing import Dict, Type
 
 from superset.db_engine_specs.base import BaseEngineSpec
 
+engines: Dict[str, Type[BaseEngineSpec]] = {}
 
-engines = {
-    o.engine: o for o in globals().values()
-    if inspect.isclass(o) and issubclass(o, BaseEngineSpec)
-}
+for (_, name, _) in pkgutil.iter_modules([Path(__file__).parent]):
+    imported_module = import_module('.' + name, package=__name__)
+
+    for i in dir(imported_module):
+        attribute = getattr(imported_module, i)
+
+        if inspect.isclass(attribute) and issubclass(attribute, BaseEngineSpec) \
+                and attribute.engine != '':
+            engines[attribute.engine] = attribute
