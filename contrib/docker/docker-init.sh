b6ab691ebd246c47bbebf28e67992ffbe7b28b4a
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2019-06-14 17:02:12 +0100

    [docker] Fix docker-init missing FLASK_APP (#7709)

--- contrib/docker/docker-init.sh
+++ contrib/docker/docker-init.sh
@@ -18,6 +18,7 @@
 set -ex
 
 # Create an admin user (you will be prompted to set username, first and last name before setting a password)
+export FLASK_APP=superset:app
 flask fab create-admin
 
 # Initialize the database
