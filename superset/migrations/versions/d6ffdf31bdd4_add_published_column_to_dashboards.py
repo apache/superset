495f2c680d9f0f13956ddf4ba071827b10b7f922
Tresdon Jones <tjones@spotx.tv>
2019-06-26 14:21:07 -0600

    UPDATE statement works with postgresql and sqlite hopefully

--- superset/migrations/versions/d6ffdf31bdd4_add_published_column_to_dashboards.py
+++ superset/migrations/versions/d6ffdf31bdd4_add_published_column_to_dashboards.py
@@ -33,7 +33,7 @@ import sqlalchemy as sa
 def upgrade():
     with op.batch_alter_table("dashboards") as batch_op:
         batch_op.add_column(sa.Column("published", sa.Boolean(), nullable=True))
-    op.execute("UPDATE dashboards SET published=True")
+    op.execute("UPDATE dashboards SET published='1'")
 
 
 def downgrade():
