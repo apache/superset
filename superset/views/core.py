5c0568e23ea11b53948608c272269504137310d5
rahul-rahul-sp <rahul.rahul@sailpoint.com>
2019-06-28 13:52:42 -0500

    Added an extra blank line after return

--- superset/views/core.py
+++ superset/views/core.py
@@ -208,6 +208,7 @@ class DatabaseViewFilter(SupersetFilter):
         perms = self.get_view_menus('datasource_access')
         return query.filter(self.model.perm.in_(perms))
 
+
 class DatabaseView(SupersetModelView, DeleteMixin, YamlExportMixin):  # noqa
     datamodel = SQLAInterface(models.Database)
 
