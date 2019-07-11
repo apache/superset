560ae569e86621146b3151c14248743cb39fd07f
Rob DiCiuccio <rob.diciuccio@gmail.com>
2019-07-11 14:44:58 -0700

    Tests for setup_cache

--- tests/utils_tests.py
+++ tests/utils_tests.py
@@ -22,6 +22,9 @@ import uuid
 
 import numpy
 
+from flask import Flask
+from flask_caching import Cache
+
 from superset.exceptions import SupersetException
 from superset.utils.core import (
     base_json_conv,
@@ -37,6 +40,7 @@ from superset.utils.core import (
     parse_human_timedelta,
     parse_js_uri_path_item,
     parse_past_timedelta,
+    setup_cache,
     validate_json,
     zlib_compress,
     zlib_decompress_to_string,
@@ -763,3 +767,33 @@ class UtilsTestCase(unittest.TestCase):
     def test_parse_js_uri_path_items_item_optional(self):
         self.assertIsNone(parse_js_uri_path_item(None))
         self.assertIsNotNone(parse_js_uri_path_item("item"))
+
+    def test_setup_cache_no_config(self):
+        app = Flask(__name__)
+        cache_config = None
+        self.assertIsNone(setup_cache(app, cache_config))
+
+    def test_setup_cache_null_config(self):
+        app = Flask(__name__)
+        cache_config = {
+            'CACHE_TYPE': 'null',
+        }
+        self.assertIsNone(setup_cache(app, cache_config))
+
+    def test_setup_cache_standard_config(self):
+        app = Flask(__name__)
+        cache_config = {
+            'CACHE_TYPE': 'redis',
+            'CACHE_DEFAULT_TIMEOUT': 60,
+            'CACHE_KEY_PREFIX': 'superset_results',
+            'CACHE_REDIS_URL': 'redis://localhost:6379/0',
+        }
+        assert isinstance(setup_cache(app, cache_config), Cache) is True
+
+    def test_setup_cache_custom_function(self):
+        app = Flask(__name__)
+        CustomCache = type('CustomCache', (object,), {"__init__": lambda *args: None})
+        def init_cache(app):
+            return CustomCache(app, {})
+
+        assert isinstance(setup_cache(app, init_cache), CustomCache) is True
