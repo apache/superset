9b7d31758b2e192ea52926e5ebe83eae88177fff
rahul-rahul-sp <rahul.rahul@sailpoint.com>
2019-07-17 16:26:56 -0500

    shortner: use origin instead of host

--- superset/views/core.py
+++ superset/views/core.py
@@ -1037,9 +1037,7 @@ class R(BaseSupersetView):
         db.session.add(obj)
         db.session.commit()
         return Response(
-            "{scheme}://{request.headers[Host]}/r/{obj.id}".format(
-                scheme=request.scheme, request=request, obj=obj
-            ),
+            "{request.headers[origin]}/r/{obj.id}".format(request=request, obj=obj),
             mimetype="text/plain",
         )
 
