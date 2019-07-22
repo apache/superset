a6c10eb3cfd7532e7127932773d8226a3ab7d5b3
Erik Ritter <erik.ritter@airbnb.com>
2019-07-22 13:53:09 -0700

    fix: responsive y-axis on stacked charts (#141)

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-plugins-demo/storybook/stories/legacy-preset-chart-nvd3/Area/Stories.jsx
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-plugins-demo/storybook/stories/legacy-preset-chart-nvd3/Area/Stories.jsx
@@ -61,6 +61,82 @@ export default [
             showBrush: 'auto',
             showControls: false,
             showLegend: true,
+            stackedStyle: 'stack',
+            vizType: 'area',
+            xAxisFormat: '%Y',
+            xAxisLabel: '',
+            xAxisShowminmax: false,
+            xTicksLayout: 'auto',
+            yAxisBounds: [0, 3000000000],
+            yAxisFormat: '.3s',
+            yLogScale: false,
+          },
+          height: 400,
+          payload: { data },
+          width: 400,
+        }}
+      />
+    ),
+    storyName: 'Stacked with yAxisBounds',
+    storyPath: 'legacy-|preset-chart-nvd3|AreaChartPlugin',
+  },
+  {
+    renderStory: () => (
+      <SuperChart
+        chartType="area"
+        chartProps={{
+          datasource: {
+            verboseMap: {},
+          },
+          formData: {
+            bottomMargin: 'auto',
+            colorCcheme: 'd3Category10',
+            contribution: false,
+            groupby: ['region'],
+            lineInterpolation: 'linear',
+            metrics: ['sum__SP_POP_TOTL'],
+            richTooltip: true,
+            showBrush: 'auto',
+            showControls: false,
+            showLegend: true,
+            stackedStyle: 'stack',
+            vizType: 'area',
+            xAxisFormat: '%Y',
+            xAxisLabel: '',
+            xAxisShowminmax: false,
+            xTicksLayout: 'auto',
+            yAxisBounds: [1000000000, null],
+            yAxisFormat: '.3s',
+            yLogScale: false,
+          },
+          height: 400,
+          payload: { data },
+          width: 400,
+        }}
+      />
+    ),
+    storyName: 'Stacked with yAxisBounds min only',
+    storyPath: 'legacy-|preset-chart-nvd3|AreaChartPlugin',
+  },
+  {
+    renderStory: () => (
+      <SuperChart
+        chartType="area"
+        chartProps={{
+          datasource: {
+            verboseMap: {},
+          },
+          formData: {
+            bottomMargin: 'auto',
+            colorCcheme: 'd3Category10',
+            contribution: false,
+            groupby: ['region'],
+            lineInterpolation: 'linear',
+            metrics: ['sum__SP_POP_TOTL'],
+            richTooltip: true,
+            showBrush: 'auto',
+            showControls: false,
+            showLegend: true,
             stackedStyle: 'expand',
             vizType: 'area',
             xAxisFormat: '%Y',
@@ -78,6 +154,44 @@ export default [
       />
     ),
     storyName: 'Expanded',
+    storyPath: 'legacy-|preset-chart-nvd3|AreaChartPlugin',
+  },
+  {
+    renderStory: () => (
+      <SuperChart
+        chartType="area"
+        chartProps={{
+          datasource: {
+            verboseMap: {},
+          },
+          formData: {
+            bottomMargin: 'auto',
+            colorCcheme: 'd3Category10',
+            contribution: false,
+            groupby: ['region'],
+            lineInterpolation: 'linear',
+            metrics: ['sum__SP_POP_TOTL'],
+            richTooltip: true,
+            showBrush: 'auto',
+            showControls: true,
+            showLegend: true,
+            stackedStyle: 'stack',
+            vizType: 'area',
+            xAxisFormat: '%Y',
+            xAxisLabel: '',
+            xAxisShowminmax: false,
+            xTicksLayout: 'auto',
+            yAxisBounds: [null, null],
+            yAxisFormat: '.3s',
+            yLogScale: false,
+          },
+          height: 400,
+          payload: { data },
+          width: 400,
+        }}
+      />
+    ),
+    storyName: 'Controls Shown',
     storyPath: 'legacy-|preset-chart-nvd3|AreaChartPlugin',
   },
 ];
