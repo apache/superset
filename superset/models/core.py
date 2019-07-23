648f9fa54b90ce4cca84737a21214fd92c0e9fcc
Maxim Sukharev <max@smacker.ru>
2019-07-23 07:30:28 +0200

    Set owner to dashboards and charts on import (#7894)
    * Allow to pass user for dashboard import cli
    
    Dashboard import assign current user from flask context during import.
    But in case of cli import there is no flask user and imported charts
    don't have an owner which prevents ability to edit them.
    
    * Reset ownership on dashboard import
    
    For overriding existing charts it requires `owners` property to be set.
    
    * Add tests for reset ownership
    
    * Use ORM to decode dashboards json
    
    Creating instances using ORM allows to normally work with relations
    
    * Fix test_import_dashboard_1_slice test
    
    Previously tests used side-effect of slices import which kept id from
    json on insert into db.

--- superset/models/core.py
+++ superset/models/core.py
@@ -361,6 +361,7 @@ class Slice(Model, AuditMixinNullable, ImportMixin):
         slc_to_import.alter_params(remote_id=slc_to_import.id, import_time=import_time)
 
         slc_to_import = slc_to_import.copy()
+        slc_to_import.reset_ownership()
         params = slc_to_import.params_dict
         slc_to_import.datasource_id = ConnectorRegistry.get_datasource_by_name(
             session,
@@ -617,7 +618,9 @@ class Dashboard(Model, AuditMixinNullable, ImportMixin):
             ):
                 existing_dashboard = dash
 
+        dashboard_to_import = dashboard_to_import.copy()
         dashboard_to_import.id = None
+        dashboard_to_import.reset_ownership()
         # position_json can be empty for dashboards
         # with charts added from chart-edit page and without re-arranging
         if dashboard_to_import.position_json:
@@ -646,14 +649,10 @@ class Dashboard(Model, AuditMixinNullable, ImportMixin):
             session.flush()
             return existing_dashboard.id
         else:
-            # session.add(dashboard_to_import) causes sqlachemy failures
-            # related to the attached users / slices. Creating new object
-            # allows to avoid conflicts in the sql alchemy state.
-            copied_dash = dashboard_to_import.copy()
-            copied_dash.slices = new_slices
-            session.add(copied_dash)
+            dashboard_to_import.slices = new_slices
+            session.add(dashboard_to_import)
             session.flush()
-            return copied_dash.id
+            return dashboard_to_import.id
 
     @classmethod
     def export_dashboards(cls, dashboard_ids):
