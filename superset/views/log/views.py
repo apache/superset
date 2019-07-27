2667626139ddf31af23975065b0ccdcc6eead124
Daniel Gaspar <danielvazgaspar@gmail.com>
2019-07-27 17:41:36 +0100

    [log] Fix, pylint

--- superset/views/log/views.py
+++ superset/views/log/views.py
@@ -14,6 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+# pylint: disable=C,R,W
 from flask_appbuilder.models.sqla.interface import SQLAInterface
 from flask_babel import gettext as __
 
