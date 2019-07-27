d19af4dec8f8c4aec42d8f6fb8611fed92bc8dae
Daniel Gaspar <danielvazgaspar@gmail.com>
2019-07-27 19:40:41 +0100

    [log] Fix, pylint at log module

--- superset/views/log/__init__.py
+++ superset/views/log/__init__.py
@@ -14,6 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+# pylint: disable=C,R,W
 from flask_babel import lazy_gettext as _
 
 
