bfb277688947c21c447da60d5b9e3c53523f9ba5
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2019-07-28 01:42:10 +0100

    [log] Disable Log view when FAB security views are disabled (#7920)
    * [log] Disable Log view when FAB security views are disabled
    
    * [tests] New, config flag SUPERSET_LOG_VIEW to have more control
    
    * [tests] Fix, flake8
    
    * [log] Fix, pylint
    
    * [log] Fix, pylint at log module
    
    * [log] Fix, pylint at log module
    
    * [log] Fix, black

--- superset/views/core.py
+++ superset/views/core.py
@@ -591,35 +591,6 @@ class DashboardAddView(DashboardModelView):  # noqa
 appbuilder.add_view_no_menu(DashboardAddView)
 
 
-class LogModelView(SupersetModelView):
-    datamodel = SQLAInterface(models.Log)
-
-    list_title = _("Logs")
-    show_title = _("Show Log")
-    add_title = _("Add Log")
-    edit_title = _("Edit Log")
-
-    list_columns = ("user", "action", "dttm")
-    edit_columns = ("user", "action", "dttm", "json")
-    base_order = ("dttm", "desc")
-    label_columns = {
-        "user": _("User"),
-        "action": _("Action"),
-        "dttm": _("dttm"),
-        "json": _("JSON"),
-    }
-
-
-appbuilder.add_view(
-    LogModelView,
-    "Action Log",
-    label=__("Action Log"),
-    category="Security",
-    category_label=__("Security"),
-    icon="fa-list-ol",
-)
-
-
 @app.route("/health")
 def health():
     return "OK"
--- superset/views/log/__init__.py
+++ superset/views/log/__init__.py
@@ -14,14 +14,22 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-from . import base  # noqa
-from . import api  # noqa
-from . import core  # noqa
-from . import sql_lab  # noqa
-from . import dashboard  # noqa
-from . import annotations  # noqa
-from . import datasource  # noqa
-from . import schedules  # noqa
-from . import tags  # noqa
-from .log import views  # noqa
-from .log import api as log_api  # noqa
+# pylint: disable=C,R,W
+from flask_babel import lazy_gettext as _
+
+
+class LogMixin:
+    list_title = _("Logs")
+    show_title = _("Show Log")
+    add_title = _("Add Log")
+    edit_title = _("Edit Log")
+
+    list_columns = ("user", "action", "dttm")
+    edit_columns = ("user", "action", "dttm", "json")
+    base_order = ("dttm", "desc")
+    label_columns = {
+        "user": _("User"),
+        "action": _("Action"),
+        "dttm": _("dttm"),
+        "json": _("JSON"),
+    }
