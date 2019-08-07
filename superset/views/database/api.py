7bab05515d5299ad3f535e1ea1eeab7d25526528
Daniel Gaspar <danielvazgaspar@gmail.com>
2019-08-07 16:55:36 +0100

    [sqllab] Remove unused import

--- superset/views/database/api.py
+++ superset/views/database/api.py
@@ -14,7 +14,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-from flask import current_app
 from flask_appbuilder import ModelRestApi
 from flask_appbuilder.models.sqla.interface import SQLAInterface
 
