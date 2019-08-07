cfc816d83991835848e06a5d89689b1e4afd5da2
Christopher Guan <51972312+cguan7@users.noreply.github.com>
2019-08-07 16:23:10 -0700

    feat: add basic functionality for icicle chart to display static data (#165)
    * feat: add basic functionality for icicle chart to display static data
    
    adds functionality for the icicle chart to have data passed in and display (static, no interactions
    yet)
    
    * feat: increase code coverage to pass check
    
    * feat: clarify contentRenderer argument types

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-plugin-chart-icicle-event/src/IcicleEventChart.tsx
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-plugin-chart-icicle-event/src/IcicleEventChart.tsx
@@ -18,7 +18,9 @@
  */
 import React, { Component, createRef } from 'react';
 import { HierarchyRectangularNode } from 'd3-hierarchy';
+import { BaseType, select as d3Select } from 'd3-selection';
 import { IcicleEventNode } from '../types/IcicleEventNode';
+import { x0, y0, rectWidth, rectHeight } from './utils/RenderedIcicleAccessors';
 
 interface Props {
   className?: string;
@@ -29,14 +31,48 @@ interface Props {
     y: number;
   };
   color: (name: string) => string;
-  contentRenderer: () => void;
+  contentRenderer: (
+    datum: HierarchyRectangularNode<IcicleEventNode>,
+    container: BaseType,
+    rect: {
+      width: number;
+      height: number;
+    },
+  ) => void;
   d3TreeRoot: HierarchyRectangularNode<IcicleEventNode>;
   isVertical: boolean;
   rounding: number;
   transitionDuration: number;
 }
 
+function defaultContentRenderer(
+  datum: HierarchyRectangularNode<IcicleEventNode>,
+  container: HTMLDivElement,
+  rect: { width: number; height: number },
+) {
+  const minRectHeight = 20;
+  const minRectWidth = 10;
+
+  d3Select(container)
+    .attr('class', 'icicleContent')
+    .style('text-overflow', 'ellipsis')
+    .style('white-space', 'nowrap')
+    .style('overflow', 'hidden')
+    .style('font-size', `${rect.height / 2}px`)
+    .style('line-height', `${rect.height}px`)
+    .text(`${rect.height > minRectHeight && rect.width > minRectWidth ? datum.data.name : ''}`);
+}
+
 export default class IcicleEventChart extends Component<Props> {
+  static defaultProps = {
+    boxMargin: {
+      x: 1,
+      y: 3,
+    },
+    color: (name: string) => 'pink',
+    contentRenderer: defaultContentRenderer,
+  };
+
   private chartRef = createRef<HTMLDivElement>();
 
   constructor(props: Props) {
@@ -50,10 +86,82 @@ export default class IcicleEventChart extends Component<Props> {
   }
 
   // Check for changed data to rerender the icicle chart
-  componentDidUpdate(prevProps: Props) {}
+  componentDidUpdate(prevProps: Props) {
+    const root = this.props.d3TreeRoot;
+    const prevRoot = prevProps.d3TreeRoot;
+
+    if (root.data.id !== prevRoot.data.id || root.data.value !== prevRoot.data.value) {
+      this.renderIcicleChart();
+    }
+  }
 
   // Creates chart using svg & chartRef to the div element
-  renderIcicleChart() {}
+  renderIcicleChart() {
+    const { boxMargin, color, contentRenderer, isVertical, width, height, rounding } = this.props;
+
+    const root = this.props.d3TreeRoot;
+
+    // Clear all elements and redraw the Icicle Chart
+    d3Select(this.chartRef.current)
+      .selectAll('*')
+      .remove();
+
+    const svg = d3Select(this.chartRef.current)
+      .append('svg')
+      .style('width', `${width}px`)
+      .style('height', `${height}px`)
+      .style('overflow', 'hidden');
+
+    const cell = svg
+      .selectAll('g')
+      .data(root.descendants())
+      .enter()
+      .append('g')
+      .attr(
+        'transform',
+        d => `translate(${y0(isVertical, d) + boxMargin.x},${x0(isVertical, d) + boxMargin.y})`,
+      )
+      .attr('key', (d, i) => `${i}`);
+
+    // Create the color coded rectangles for the events
+    cell
+      .append('rect')
+      .attr('width', d => rectWidth(isVertical, boxMargin, d))
+      .attr('height', d => rectHeight(isVertical, boxMargin, d))
+      .attr('rx', rounding)
+      .attr('fill', d => color(d.data.name || ''))
+      .style('cursor', 'pointer');
+
+    // Create container for each rectangle to append content (name of event)
+    const content = cell
+      .append('foreignObject')
+      .classed('container', true)
+      .attr('pointer-events', 'none')
+      .style('width', d => `${rectWidth(isVertical, boxMargin, d)}px`)
+      .style('height', d => `${rectHeight(isVertical, boxMargin, d)}px`)
+      .style('padding', '0px')
+      .style('overflow', 'hidden')
+      .style('background', 'none');
+
+    if (!isVertical) {
+      content
+        .attr('transform', d => `translate(${rectWidth(isVertical, boxMargin, d)}) rotate(90)`)
+        .style('height', d => `${rectWidth(isVertical, boxMargin, d)}px`)
+        .style('width', d => `${rectHeight(isVertical, boxMargin, d)}px`);
+    }
+
+    content
+      .append('xhtml:div')
+      .style('width', '100%')
+      .style('height', '100%')
+      .style('padding-left', '2px')
+      .each((d, i, elements) =>
+        contentRenderer(d, elements[i], {
+          height: rectHeight(isVertical, boxMargin, d),
+          width: rectWidth(isVertical, boxMargin, d),
+        }),
+      );
+  }
 
   render() {
     return (
