44e2112b5597bf60dbf6311b3df77ee169a91274
Michelle Thomas <michelle.q.thomas@gmail.com>
2019-08-26 16:36:29 -0700

    Testing a fix to cypress build

--- superset/assets/cypress_build.sh
+++ superset/assets/cypress_build.sh
@@ -28,6 +28,7 @@ echo "[completed python build steps]"
 flask run -p 8081 --with-threads --reload --debugger &
 
 #block on the longer running javascript process
+npm cache clean --force
 time npm ci
 time npm run install-cypress
 time npm run build
