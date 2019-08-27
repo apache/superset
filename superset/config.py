4cfa0147db1075ddab3cfdddc90b878984c0df97
Eric Andrew Meadows <emeadows@pax.com>
2019-08-26 17:26:53 -0700

    Fix to werkzeug proxy; expose additional configuration items

--- superset/config.py
+++ superset/config.py
@@ -111,6 +111,13 @@ SHOW_STACKTRACE = True
 
 # Extract and use X-Forwarded-For/X-Forwarded-Proto headers?
 ENABLE_PROXY_FIX = False
+PROXY_FIX_CONFIG = {
+    "x_for": 1,
+    "x_proto": 0,
+    "x_host": 1,
+    "x_port": 0,
+    "x_prefix": 0,
+}
 
 # ------------------------------
 # GLOBALS FOR APP Builder
