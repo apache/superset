2a454a0828926c6fc20cfbeceafbf01a545cea91
Craig Rueda <craig@craigrueda.com>
2019-08-27 14:51:35 -0700

    Moving towards a whitelist approach for MANIFEST.in (#8109)
    * Moving towards a whitelist approach for MANIFEST.in when it comes to static resources
    
    * Tuning static exclude
    
    * Fix for fetching version string from package.json, which no longer exists
    
    * Adding package.json fallback for unit tests

--- superset/config.py
+++ superset/config.py
@@ -49,9 +49,22 @@ else:
 # Superset specific config
 # ---------------------------------------------------------
 PACKAGE_DIR = os.path.join(BASE_DIR, "static", "assets")
-PACKAGE_FILE = os.path.join(PACKAGE_DIR, "package.json")
-with open(PACKAGE_FILE) as package_file:
-    VERSION_STRING = json.load(package_file)["version"]
+VERSION_INFO_FILE = os.path.join(PACKAGE_DIR, "version_info.json")
+PACKAGE_JSON_FILE = os.path.join(PACKAGE_DIR, "package.json")
+
+#
+# Depending on the context in which this config is loaded, the version_info.json file
+# may or may not be available, as it is generated on install via setup.py. In the event
+# that we're actually running Superset, we will have already installed, therefore it WILL
+# exist. When unit tests are running, however, it WILL NOT exist, so we fall
+# back to reading package.json
+#
+try:
+    with open(VERSION_INFO_FILE) as version_file:
+        VERSION_STRING = json.load(version_file)["version"]
+except Exception:
+    with open(PACKAGE_JSON_FILE) as version_file:
+        VERSION_STRING = json.load(version_file)["version"]
 
 ROW_LIMIT = 50000
 VIZ_ROW_LIMIT = 10000
