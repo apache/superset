52e984ffbbad67563bc7c2e333786c54e94dc392
Dave Smith <davesmith@lyft.com>
2019-09-04 15:46:56 -0700

    add missing semicolon

--- superset/assets/src/visualizations/Kepler/transformProps.js
+++ superset/assets/src/visualizations/Kepler/transformProps.js
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-const NOOP = () => {}
+const NOOP = () => {};
 
 export default function transformProps(chartProps) {
   const {
