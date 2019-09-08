343bc973fba990a3d448e86059d18938a414d59a
John Bodley <4567245+john-bodley@users.noreply.github.com>
2019-09-07 23:32:17 -0700

    Update core.py (#8191)

--- superset/views/core.py
+++ superset/views/core.py
@@ -2447,10 +2447,7 @@ class Superset(BaseSupersetView):
         )
         if rejected_tables:
             return json_error_response(
-                security_manager.get_table_access_error_msg(
-                    "{}".format(rejected_tables)
-                ),
-                status=403,
+                security_manager.get_table_access_error_msg(rejected_tables), status=403
             )
 
         payload = utils.zlib_decompress(blob, decode=not results_backend_use_msgpack)
