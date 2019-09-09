055d2ef252f11846701b9dcfd0252df7eb77126f
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2019-09-08 22:06:24 -0700

    Bump FAB to 2.1.13 (#8176)
    * Bump FAB to 2.1.13
    
    * [SQLLab] dummy commit
    
    * [SQLLab] remove dummy commit
    
    * [SQLLab] test
    
    * [SQLLab] bump back to 13

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
         "croniter>=0.3.28",
         "cryptography>=2.4.2",
         "flask>=1.1.0, <2.0.0",
-        "flask-appbuilder>=2.1.9, <2.3.0",
+        "flask-appbuilder>=2.1.13, <2.3.0",
         "flask-caching",
         "flask-compress",
         "flask-talisman",
