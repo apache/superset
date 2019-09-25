93231b033f0ce39024decf3d8189c9896c45b221
Azhar Khan <azhar.khan@peak.ai>
2019-09-25 15:38:22 +0530

    Allow oriogins

--- superset/config.py
+++ superset/config.py
@@ -282,7 +282,9 @@ TABLE_NAMES_CACHE_CONFIG = {"CACHE_TYPE": "null"}
 
 # CORS Options
 ENABLE_CORS = True
-CORS_OPTIONS = {}
+CORS_OPTIONS = {
+  'origins': '*',
+}
 
 # Chrome allows up to 6 open connections per domain at a time. When there are more
 # than 6 slices in dashboard, a lot of time fetch requests are queued up and wait for
