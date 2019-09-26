35b176421dba0d3a754b91d8a4fc0be1135fc33e
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-09-26 00:49:43 -0700

    refactor: remove unused code

--- superset/assets/src/modules/utils.js
+++ superset/assets/src/modules/utils.js
@@ -86,13 +86,3 @@ export function mainMetric(savedMetrics) {
   }
   return metric;
 }
-
-export function roundDecimal(number, precision) {
-  let roundedNumber;
-  if (precision) {
-    roundedNumber = Math.round(number * (precision = Math.pow(10, precision))) / precision;
-  } else {
-    roundedNumber = Math.round(number);
-  }
-  return roundedNumber;
-}
