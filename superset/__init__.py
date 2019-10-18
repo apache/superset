be37d243fbf8ca5877b142ccd2cde92783f88d0f
Craig <craig@craigrueda.com>
2019-10-18 15:39:31 -0700

    Working with new FLASK_APP

--- superset/__init__.py
+++ superset/__init__.py
@@ -15,41 +15,36 @@
 # specific language governing permissions and limitations
 # under the License.
 """Package's main module!"""
-from flask import current_app
+from flask import current_app as flask_current_app
 from werkzeug.local import LocalProxy
 
 from superset.app import create_app
 from superset.connectors.connector_registry import ConnectorRegistry
-from superset.extensions import appbuilder
+from superset.extensions import (
+    appbuilder as ab,
+    cache_manager as ext_cache_manager,
+    db as ext_db,
+    event_logger as ext_event_logger,
+    feature_flag_manager as ext_feature_flag_manager,
+    manifest_processor as ext_manifest_processor,
+    results_backend_manager as ext_results_backend_manager,
+    security_manager as ext_security_manager,
+    talisman as ext_talisman
+)
 from superset.security import SupersetSecurityManager
 from superset.utils.log import DBEventLogger, get_event_logger_from_cfg_value
 
-appbuilder = extensions.appbuilder
-cache = LocalProxy(lambda: extensions.cache_manager.cache)
-conf = LocalProxy(lambda: current_app.config)
-db = extensions.db
-event_logger = extensions.event_logger
-get_feature_flags = extensions.feature_flag_manager.get_feature_flags
-is_feature_enabled = extensions.feature_flag_manager.is_feature_enabled
-results_backend = LocalProxy(lambda: extensions.results_backend_manager.results_backend)
-results_backend_use_msgpack = LocalProxy(lambda: extensions.results_backend_manager.should_use_msgpack)
-security_manager = extensions.security_manager
-tables_cache = LocalProxy(lambda: extensions.cache_manager.tables_cache)
-talisman = extensions.talisman
-
-
-__app_inited = False
-
-
-def __fetch_or_init_app():
-    global __app_inited
-    if not __app_inited:
-        create_app()
-        __app_inited = True
-
-    return current_app
-
-
-app = LocalProxy(__fetch_or_init_app)
-
-created_app = create_app()
+app = flask_current_app
+appbuilder = ab
+cache = LocalProxy(lambda: ext_cache_manager.cache)
+conf = LocalProxy(lambda: flask_current_app.config)
+db = ext_db
+event_logger = ext_event_logger
+get_feature_flags = ext_feature_flag_manager.get_feature_flags
+get_css_manifest_files = ext_manifest_processor.get_css_manifest_files
+is_feature_enabled = ext_feature_flag_manager.is_feature_enabled
+results_backend = LocalProxy(lambda: ext_results_backend_manager.results_backend)
+results_backend_use_msgpack = LocalProxy(lambda: ext_results_backend_manager.should_use_msgpack)
+security_manager = ext_security_manager
+tables_cache = LocalProxy(lambda: ext_cache_manager.tables_cache)
+talisman = ext_talisman
