b575f6a67f6833bacdbc933c0ed565b6a17d4905
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2019-10-28 16:04:14 +0000

    [db engine] Add support for Elasticsearch (#8441)
    * [db engine] Add support for Elasticsearch

--- setup.py
+++ setup.py
@@ -115,6 +115,7 @@ setup(
         "mysql": ["mysqlclient==1.4.2.post1"],
         "postgres": ["psycopg2-binary==2.7.5"],
         "presto": ["pyhive[presto]>=0.4.0"],
+        "elasticsearch": ["elasticsearch-dbapi>=0.1.0, <0.2.0"],
         "druid": ["pydruid==0.5.7", "requests==2.22.0"],
     },
     python_requires="~=3.6",
