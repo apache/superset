355c2bac301f2e5de27831ea34b940e6ac813cf5
rvenzin <rvenzin@hsr.ch>
2019-11-04 12:21:15 +0100

    add type hints and first draft of docstrings for our methods

---
+++ tests/csv_upload_edge_tests.py
@@ -0,0 +1,32 @@
+"""Unit tests for CSV Upload"""
+from .base_tests import SupersetTestCase
+
+
+class CsvUploadTests(SupersetTestCase):
+    def __init__(self, *args, **kwargs):
+        super(CsvUploadTests, self).__init__(*args, **kwargs)
+
+    def setUp(self):
+        self.login()
+
+    def tearDown(self):
+        self.logout()
+
+    def create_csv_file(self, filename):
+        test_file = open(filename, "w+")
+        test_file.write("name,age\n")
+        test_file.write("john,35\n")
+        test_file.write("paul,47\n")
+        test_file.close()
+        test_file = open(filename, "rb")
+        return test_file
+
+    def test_menu_entry_upload_csv_exist(self):
+        url = "/databaseview/list/"
+        add_datasource_page = self.get_resp(url)
+        assert "Upload a CSV" in add_datasource_page
+
+    def test_upload_csv_view_load(self):
+        url = "/superset/csvtodatabase"
+        form_get = self.get_resp(url)
+        assert "CSV to Database configuration" in form_get
