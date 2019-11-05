f20a70a3227d3023bf222a3238d5a73e4c1570d1
Daniel Gaspar <danielvazgaspar@gmail.com>
2019-11-05 11:54:37 +0000

    [databases] Fix, Druid and databases view export cli compatible YAML

--- superset/views/base.py
+++ superset/views/base.py
@@ -208,12 +208,20 @@ def validate_json(form, field):
 
 
 class YamlExportMixin(object):
+    yaml_dict_key = None
+    """
+    Override this if you a dict response instead, with a certain key. 
+    Used on DatabaseView for cli compatibility
+    """
+
     @action("yaml_export", __("Export to YAML"), __("Export to YAML?"), "fa-download")
     def yaml_export(self, items):
         if not isinstance(items, list):
             items = [items]
 
         data = [t.export_to_dict() for t in items]
+        if self.yaml_dict_key:
+            data = {self.yaml_dict_key: data}
         return Response(
             yaml.safe_dump(data),
             headers=generate_download_headers("yaml"),
