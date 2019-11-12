d093af663b2101afc0358df4a62ea7c3367c9cc3
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-11-12 15:33:07 -0800

    fix: d3 scale types (#246)

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-encodable/src/types/Scale.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-encodable/src/types/Scale.ts
@@ -191,7 +191,7 @@ export interface WithScale<Output extends Value = Value> {
 /** Each ScaleCategory contains one or more ScaleType */
 export type ScaleCategory = 'continuous' | 'discrete' | 'discretizing';
 
-export type CategoricalScaleInput = HasToString | null | undefined;
+export type CategoricalScaleInput = HasToString;
 
 export interface ScaleTypeToD3ScaleType<Output extends Value = Value> {
   [ScaleType.LINEAR]: ScaleLinear<Output, Output>;
