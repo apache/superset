f40eab3a2f689a81bf47d6eeb6d5b1d16b88dd4f
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-11-15 12:58:34 -0800

    build: migrate encodable to separate repo

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-encodable/src/utils/removeUndefinedAndNull.ts
+++
@@ -1,6 +0,0 @@
-export default function removeUndefinedAndNull<T>(array: T[]) {
-  return array.filter(x => typeof x !== 'undefined' && x !== null) as Exclude<
-    T,
-    undefined | null
-  >[];
-}
