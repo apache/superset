7f500020c821722550b379156bef34ec3a8b099d
Conglei <shiconglei@gmail.com>
2019-11-14 16:41:32 -0800

    fix(table): fix rendering boolean (#256)

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-plugins-demo/storybook/stories/plugin-chart-table/dataLegacy.js
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/packages/superset-ui-plugins-demo/storybook/stories/plugin-chart-table/dataLegacy.js
@@ -1,66 +1,76 @@
 /* eslint-disable sort-keys */
 export default {
-  columns: ['name', 'sum__num', '%sum__num', 'trend'],
+  columns: ['name', 'sum__num', '%sum__num', 'trend', 'is_girl'],
   records: [
     {
       name: 'Michael',
       sum__num: 2467063,
       '%sum__num': 0.2,
-      trend: 10,
+      trend: 0,
+      is_girl: false,
     },
     {
       name: 'Christopher',
       sum__num: 1725265,
       '%sum__num': 0.2,
       trend: 17,
+      is_girl: true,
     },
     {
       name: 'David',
       sum__num: 1570516,
       '%sum__num': 0.15,
       trend: 21,
+      is_girl: true,
     },
     {
       name: 'James',
       sum__num: 1506025,
       '%sum__num': 0.14,
       trend: 1,
+      is_girl: true,
     },
     {
       name: 'john',
       sum__num: 1426074,
       '%sum__num': 0.12,
       trend: -2,
+      is_girl: true,
     },
     {
       name: 'Matthew',
       sum__num: 1355803,
       '%sum__num': 0.1,
       trend: -10,
+      is_girl: true,
     },
     {
       name: 'Robert',
       sum__num: 1314800,
       '%sum__num': 0.03,
       trend: 24,
+      is_girl: true,
     },
     {
       name: 'Daniel',
       sum__num: 1159354,
       '%sum__num': 0.02,
       trend: 25,
+      is_girl: true,
     },
     {
       name: 'Joseph',
       sum__num: 1114098,
       '%sum__num': 0.02,
       trend: 10,
+      is_girl: false,
     },
     {
       name: 'William',
       sum__num: 1113701,
       '%sum__num': 0.02,
       trend: 10,
+      is_girl: null,
     },
   ],
 };
