d2f4ada482320a892737d09f75100aa9a3839b0a
Craig <craig@craigrueda.com>
2019-11-19 12:36:04 -0800

    Blacking

--- superset/tasks/celery_app.py
+++ superset/tasks/celery_app.py
@@ -29,7 +29,7 @@ from superset.extensions import celery_app
 create_app()
 
 # Need to import late, as the celery_app will have been setup by "create_app()"
-from . import cache, schedules # isort:skip
+from . import cache, schedules  # isort:skip
 
 # Export the celery app globally for Celery (as run on the cmd line) to find
 app = celery_app
