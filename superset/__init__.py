f72438a32513a52ab1d8dee05704aab0daa764f4
Craig <craig@craigrueda.com>
2019-11-18 16:15:39 -0800

    Cleaning up __init__ a bit more

--- superset/__init__.py
+++ superset/__init__.py
@@ -21,15 +21,15 @@ from werkzeug.local import LocalProxy
 from superset.app import create_app
 from superset.connectors.connector_registry import ConnectorRegistry
 from superset.extensions import (
-    appbuilder as ab,
+    appbuilder,
     cache_manager,
-    db as ext_db,
-    event_logger as ext_event_logger,
+    db,
+    event_logger,
     feature_flag_manager,
     manifest_processor,
     results_backend_manager,
-    security_manager as ext_security_manager,
-    talisman as ext_talisman,
+    security_manager,
+    talisman,
 )
 from superset.security import SupersetSecurityManager
 from superset.utils.log import DBEventLogger, get_event_logger_from_cfg_value
@@ -39,11 +39,8 @@ from superset.utils.log import DBEventLogger, get_event_logger_from_cfg_value
 #  then initialize it in app.create_app(). These fields will be removed
 #  in subsequent PRs as things are migrated towards the factory pattern
 app: Flask = current_app
-appbuilder = ab
 cache = LocalProxy(lambda: cache_manager.cache)
 conf = LocalProxy(lambda: current_app.config)
-db = ext_db
-event_logger = ext_event_logger
 get_feature_flags = feature_flag_manager.get_feature_flags
 get_css_manifest_files = manifest_processor.get_css_manifest_files
 is_feature_enabled = feature_flag_manager.is_feature_enabled
@@ -51,6 +48,4 @@ results_backend = LocalProxy(lambda: results_backend_manager.results_backend)
 results_backend_use_msgpack = LocalProxy(
     lambda: results_backend_manager.should_use_msgpack
 )
-security_manager = ext_security_manager
 tables_cache = LocalProxy(lambda: cache_manager.tables_cache)
-talisman = ext_talisman
