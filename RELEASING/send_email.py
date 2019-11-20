5f53d4d0e7ff6da0bf5b1840ef901fca89b5cb4d
Daniel Gaspar <danielvazgaspar@gmail.com>
2019-11-20 15:13:48 +0000

    [docs] New, Email templates for all the release steps

--- RELEASING/send_email.py
+++ RELEASING/send_email.py
@@ -15,6 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import argparse
+from typing import Dict, List
 import os
 import smtplib
 import ssl
@@ -22,6 +24,38 @@ from string import Template
 
 SMTP_PORT = 587
 SMTP_SERVER = "mail-relay.apache.org"
+PROJECT_NAME = "Superset"
+PROJECT_MODULE = "superset"
+PROJECT_DESCRIPTION = "Apache Superset (incubating) is a modern, enterprise-ready business intelligence web application"
+
+voting_steps_info: Dict = {
+    "vote_pmc": {
+        "receiver_email": "danielvazgaspar@gmail.com",
+        "template": "email_templates/vote_pmc.tmpl",
+        "extra_input": {},
+    },
+    "vote_ipmc": {
+        "receiver_email": "danielvazgaspar@gmail.com",
+        "template": "email_templates/vote_ipmc.tmpl",
+        "extra_input": [
+            {"name": "voting_thread", "message": "The URL for the PMC voting thread: "},
+            {
+                "name": "mentors_voted",
+                "message": "A list of mentors that have already voted: ",
+            },
+        ],
+    },
+    "result_ipmc": {
+        "receiver_email": "danielvazgaspar@gmail.com",
+        "template": "email_templates/result_ipmc.tmpl",
+        "extra_input": {},
+    },
+    "announce": {
+        "receiver_email": "danielvazgaspar@gmail.com",
+        "template": "email_templates/announce.tmpl",
+        "extra_input": {},
+    },
+}
 
 
 def send_email(
@@ -31,8 +65,11 @@ def send_email(
     password: str,
     sender_email: str,
     receiver_email: str,
-    message: str
+    message: str,
 ):
+    """
+    Send a simple text email (SMTP)
+    """
     context = ssl.create_default_context()
     with smtplib.SMTP(smtp_server, smpt_port) as server:
         server.starttls(context=context)
@@ -40,7 +77,13 @@ def send_email(
         server.sendmail(sender_email, receiver_email, message)
 
 
-def required_input(message: str) -> str:
+def input_required(message: str) -> str:
+    """
+    Asks for required user input infinite times
+
+    :param message: The message to display when asking for user input
+    :return: User input
+    """
     answer = input(message)
     while not answer:
         print("This field is required")
@@ -48,15 +91,49 @@ def required_input(message: str) -> str:
     return answer
 
 
-def render_template(template: str, **kwargs) -> str:
-    with open('email_templates/vote_pmc.tmpl') as file:
-        template_source = file.read()
+def input_extra(parameters: List[Dict]) -> Dict:
+    """
+    Asks for dynamic extra user input
 
-        template = Template(template_source)
-        message = template.substitute(kwargs)
+    :param message: List of dicts, ex: [{"name": "input1", "message": "Write value for input1"}, ...]
+    :return: Dict, ex: {"input1": "some user input"}
+    """
+    answers = dict()
+    for parameter in parameters:
+        answers[parameter["name"]] = input_required(parameter["message"])
+    return answers
+
+
+def render_template(template_file: str, **kwargs) -> str:
+    """
+    Simple render template based on named parameters
+
+    :param template_file: The template file location
+    :kwargs: Named parameters to use when rendering the template
+    :return: Rendered template
+    """
+    with open(template_file) as file:
+        template_source = file.read()
+    template = Template(template_source)
+    message = template.substitute(kwargs)
     return message
 
 
+# Argument parsing
+parser = argparse.ArgumentParser(description="Apache voting mailer script")
+parser.add_argument(
+    "-t",
+    "--email_type",
+    help="The type of email to send choose from: vote_pmc, result_pmc, vote_ipmc, result_ipmc",
+)
+args = parser.parse_args()
+email_type: str = str(args.email_type)
+if email_type not in voting_steps_info:
+    exit("Expected required '--email_type' parameter")
+
+template = voting_steps_info[email_type]["template"]
+
+# Check for required environment variables
 version_rc = os.environ.get("SUPERSET_VERSION_RC")
 if not version_rc:
     exit("Expected SUPERSET_VERSION_RC environment variable to be set")
@@ -64,21 +141,28 @@ version = os.environ.get("SUPERSET_VERSION")
 if not version:
     exit("Expected SUPERSET_VERSION environment variable to be set")
 
-
-receiver_email = "danielvazgaspar@gmail.com"  # Enter receiver address
-sender_email = required_input("Sender email (ex: user@apache.org): ")
-username = required_input("Apache username: ")
-password = required_input("Apache password: ")
-
-message = render_template(
-    "email_templates/vote_pmc.tmpl",
-    receiver_email=receiver_email,
-    sender_email=sender_email,
-    project_name="Superset",
-    project_module="superset",
-    version=version,
-    version_rc=version_rc
+# Collect all necessary template arguments
+template_arguments = dict()
+template_arguments["receiver_email"] = voting_steps_info[email_type][
+    "receiver_email"
+]
+template_arguments["project_name"] = PROJECT_NAME
+template_arguments["project_module"] = PROJECT_MODULE
+template_arguments["project_description"] = PROJECT_DESCRIPTION
+template_arguments["version"] = version
+template_arguments["version_rc"] = version_rc
+# Ask for required user input
+template_arguments["sender_email"] = input_required(
+    "Sender email (ex: user@apache.org): "
 )
+username = input_required("Apache username: ")
+password = input_required("Apache password: ")
+template_arguments.update(
+    input_extra(voting_steps_info[email_type]["extra_input"])
+)
+
+
+message = render_template(template, **template_arguments)
 
 print("--------------------------")
 print("SMTP Message")
@@ -87,6 +171,20 @@ print(message)
 print("--------------------------")
 confirm = input("Is the Email message ok? (yes/no): ")
 if confirm not in ("Yes", "yes", "y"):
-    exit(1)
+    exit("Exit by user request")
 
-send_email(SMTP_SERVER, SMTP_PORT, username, password, sender_email, receiver_email, message)
+try:
+    send_email(
+        SMTP_SERVER,
+        SMTP_PORT,
+        username,
+        password,
+        template_arguments["sender_email"],
+        template_arguments["receiver_email"],
+        message,
+    )
+    print("Email sent successfully")
+except smtplib.SMTPAuthenticationError:
+    exit("SMTP User authentication error, Email not sent!")
+except Exception as e:
+    exit(f"SMTP exception {e}")
