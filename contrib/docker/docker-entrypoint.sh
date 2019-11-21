b7a508d5a7a690ef56180a1b2e7ec151c6572d52
maxmelnick <maxmelnick@gmail.com>
2019-11-21 03:44:17 -0500

    updated FLASK_APP values for dockerusage (#8621)

--- contrib/docker/docker-entrypoint.sh
+++ contrib/docker/docker-entrypoint.sh
@@ -24,7 +24,7 @@ elif [ "$SUPERSET_ENV" = "development" ]; then
     # needed by superset runserver
     (cd superset/assets/ && npm ci)
     (cd superset/assets/ && npm run dev) &
-    FLASK_ENV=development FLASK_APP=superset:app flask run -p 8088 --with-threads --reload --debugger --host=0.0.0.0
+    FLASK_ENV=development FLASK_APP="superset.app:create_app()" flask run -p 8088 --with-threads --reload --debugger --host=0.0.0.0
 elif [ "$SUPERSET_ENV" = "production" ]; then
     celery worker --app=superset.sql_lab:celery_app --pool=gevent -Ofair &
     exec gunicorn --bind  0.0.0.0:8088 \
@@ -32,7 +32,7 @@ elif [ "$SUPERSET_ENV" = "production" ]; then
         --timeout 60 \
         --limit-request-line 0 \
         --limit-request-field_size 0 \
-        superset:app
+        "superset.app:create_app()"
 else
     superset --help
 fi
