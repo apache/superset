20d88bfa3bd81bc52b64cc3ddcb323ec832544ed
Craig Rueda <craig@craigrueda.com>
2019-11-27 07:06:06 -0800

    Adding app context wrapper to Celery tasks (#8653)
    * Adding app context wrapper to Celery tasks

--- superset/app.py
+++ superset/app.py
@@ -96,6 +96,22 @@ class SupersetAppInitializer:
     def configure_celery(self) -> None:
         celery_app.config_from_object(self.config["CELERY_CONFIG"])
         celery_app.set_default()
+        flask_app = self.flask_app
+
+        # Here, we want to ensure that every call into Celery task has an app context
+        # setup properly
+        task_base = celery_app.Task
+
+        class AppContextTask(task_base):  # type: ignore
+            # pylint: disable=too-few-public-methods
+            abstract = True
+
+            # Grab each call into the task and set up an app context
+            def __call__(self, *args, **kwargs):
+                with flask_app.app_context():
+                    return task_base.__call__(self, *args, **kwargs)
+
+        celery_app.Task = AppContextTask
 
     @staticmethod
     def init_views() -> None:
