86234fa36009ba577882f24e63576d06b98d4ce8
altef <bradgill+github@gmail.com>
2019-11-30 05:55:50 +0000

    Adjusted import order for isort; I was sure I'd already done this..

--- superset/connectors/sqla/models.py
+++ superset/connectors/sqla/models.py
@@ -56,7 +56,7 @@ from superset.exceptions import DatabaseNotFound
 from superset.jinja_context import get_template_processor
 from superset.models.annotations import Annotation
 from superset.models.core import Database
-from superset.models.helpers import QueryResult, AuditMixinNullable
+from superset.models.helpers import AuditMixinNullable, QueryResult
 from superset.utils import core as utils, import_datasource
 
 config = app.config
