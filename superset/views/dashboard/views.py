cf811e394a77d44340b4b96cfaf9ff26826c5faa
Daniel Gaspar <danielvazgaspar@gmail.com>
2019-12-05 21:46:14 +0000

    [dashboard] [api] Fix missing import re

--- superset/views/dashboard/views.py
+++ superset/views/dashboard/views.py
@@ -14,6 +14,8 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+import re
+
 from flask import g, redirect, request, Response
 from flask_appbuilder import expose
 from flask_appbuilder.actions import action
