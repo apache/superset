2cddfec40efecc5185b68c3d4ab1b7fa884a2777
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2019-12-09 16:11:21 +0000

    Bump FAB to 2.2.1 (#8793)

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
         "croniter>=0.3.28",
         "cryptography>=2.4.2",
         "flask>=1.1.0, <2.0.0",
-        "flask-appbuilder>=2.2.0, <2.3.0",
+        "flask-appbuilder>=2.2.1, <2.3.0",
         "flask-caching",
         "flask-compress",
         "flask-talisman",
