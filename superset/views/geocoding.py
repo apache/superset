e6a21e6622d81d15c0980480f960a4885d7276cc
Sascha Gschwind <sascha.gschwind.88@gmail.com>
2019-12-11 11:51:33 +0100

    Fix filtering and make use of security_manager

--- superset/views/geocoding.py
+++ superset/views/geocoding.py
@@ -27,7 +27,7 @@ from sqlalchemy import Column, Float, text
 from sqlalchemy.engine import Connection, reflection
 
 import superset.models.core as models
-from superset import appbuilder, conf, db, SupersetSecurityManager
+from superset import appbuilder, conf, db, SupersetSecurityManager, security_manager
 from superset.connectors.sqla.models import SqlaTable
 from superset.exceptions import (
     SqlAddColumnException,
@@ -82,17 +82,15 @@ class Geocoder(BaseSupersetView):
             db.session.query(models.Database).filter_by(allow_dml=True).all()
         ):
             all_tables = db.session.query(SqlaTable).filter_by(database_id=database.id).all()
-            security_manager = SupersetSecurityManager()
             permitted_tables = security_manager.get_datasources_accessible_by_user(database, all_tables)
             for table in (
-                all_tables
+                permitted_tables
             ):
-                if table.table_name in permitted_tables:
-                    tables.append(
-                        models.TableDto(
-                            table.id, table.table_name, table.schema, table.database_id
-                        )
+                tables.append(
+                    models.TableDto(
+                        table.id, table.table_name, table.schema, table.database_id
                     )
+                )
 
         return tables
 
