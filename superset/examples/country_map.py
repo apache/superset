f7ed4a3ea9ed73261bac97412f39280daf01f483
David Aaron Suddjian <aasuddjian@gmail.com>
2019-12-12 10:54:58 -0800

    remove shortcuts & import indirection

--- superset/examples/country_map.py
+++ superset/examples/country_map.py
@@ -22,6 +22,7 @@ from sqlalchemy.sql import column
 
 from superset import db
 from superset.connectors.sqla.models import SqlMetric
+from superset.models.slice import Slice
 from superset.utils import core as utils
 
 from .helpers import (
@@ -29,7 +30,6 @@ from .helpers import (
     get_slice_json,
     merge_slice,
     misc_dash_slices,
-    Slice,
     TBL,
 )
 
