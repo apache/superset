945a4a0c8b24359429171f1eec48744fac74a9e0
Will Barrett <will@preset.io>
2019-12-18 14:06:20 -0800

    Add tox checks for unsynchronized DB migrations

---
+++ scripts/check_db_sync.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This script checks to ensure that Superset's models are in sync
+# with its database schema. It will exit 0 if no changes
+# are detected and 1 if a migration is required.
+superset db migrate -m "test" 2>&1 | grep "No changes in schema detectedt" 2>&1 | grep "No changes in schema detected."
