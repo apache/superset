ca13fe8c3e8fda5371da030a9a1ea39115458b2c
Chan Chak Shing <ccspasu@gmail.com>
2019-12-18 16:57:34 +0800

    Setup database dialect in docker/.env

--- docker/pythonpath_dev/superset_config.py
+++ docker/pythonpath_dev/superset_config.py
@@ -45,6 +45,7 @@ def get_env_variable(var_name, default=None):
             raise EnvironmentError(error_msg)
 
 
+DATABASE_DIALECT = get_env_variable("DATABASE_DIALECT")
 DATABASE_USER = get_env_variable("DATABASE_USER")
 DATABASE_PASSWORD = get_env_variable("DATABASE_PASSWORD")
 DATABASE_HOST = get_env_variable("DATABASE_HOST")
@@ -52,7 +53,8 @@ DATABASE_PORT = get_env_variable("DATABASE_PORT")
 DATABASE_DB = get_env_variable("DATABASE_DB")
 
 # The SQLAlchemy connection string.
-SQLALCHEMY_DATABASE_URI = "postgresql://%s:%s@%s:%s/%s" % (
+SQLALCHEMY_DATABASE_URI = "%s://%s:%s@%s:%s/%s" % (
+    DATABASE_DIALECT,
     DATABASE_USER,
     DATABASE_PASSWORD,
     DATABASE_HOST,
