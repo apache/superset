4837a0aeb1900126e31fdf0f0886b3a942476184
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2019-12-20 10:16:57 +0000

    [dashboard] New, get releated owners and slices (#8872)

--- superset/models/dashboard.py
+++ superset/models/dashboard.py
@@ -180,6 +180,25 @@ class Dashboard(  # pylint: disable=too-many-instance-attributes
         return Markup(f'<a href="{self.url}">{title}</a>')
 
     @property
+    def changed_by_name(self):
+        if not self.changed_by:
+            return ""
+        return str(self.changed_by)
+
+    @property
+    def changed_by_url(self):
+        if not self.changed_by:
+            return ""
+        return f"/superset/profile/{self.changed_by.username}"
+
+    @property
+    def owners_json(self) -> List[Dict[str, Any]]:
+        owners = []
+        for owner in self.owners:
+            owners.append({"name": owner.name})
+        return owners
+
+    @property
     def data(self) -> Dict[str, Any]:
         positions = self.position_json
         if positions:
