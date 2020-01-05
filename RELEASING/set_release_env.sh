63ddb1bd23a68f98513b7a009a98b85793fd0130
Ville Brofeldt <ville.brofeldt@iki.fi>
2020-01-05 15:50:12 +0200

    Add refinements from 0.35.2 release

--- RELEASING/set_release_env.sh
+++ RELEASING/set_release_env.sh
@@ -16,16 +16,27 @@
 # limitations under the License.
 #
 usage() {
-   echo "usage: . set_release_env.sh <SUPERSET_VERSION> <SUPERSET_RC> <PGP_KEY_FULLBANE>"
+   echo "usage: . set_release_env.sh <SUPERSET_RC_VERSION> <PGP_KEY_FULLBANE>"
+   echo "example: . set_relese_env.sh 0.35.2rc1 myid@apache.org"
 }
 
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+if [ -z "$1" ] || [ -z "$2" ]; then
   usage;
 else
-  export SUPERSET_VERSION="${1}"
-  export SUPERSET_RC="${2}"
-  export SUPERSET_PGP_FULLNAME="${3}"
-  export SUPERSET_VERSION_RC="${SUPERSET_VERSION}rc${SUPERSET_RC}"
+  if [[ ${1} =~ ^([0-9]+)\.([0-9]+)\.([0-9]+)rc([0-9]+)$ ]]; then
+    VERSION_MAJOR="${BASH_REMATCH[1]}"
+    VERSION_MINOR="${BASH_REMATCH[2]}"
+    VERSION_PATCH="${BASH_REMATCH[3]}"
+    VERSION_RC="${BASH_REMATCH[4]}"
+  else
+    echo "unable to parse version string ${1}. Example of valid version string: 0.35.2rc1"
+    exit 1
+  fi
+  export SUPERSET_VERSION="${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}"
+  export SUPERSET_RC="${VERSION_RC}"
+  export SUPERSET_GITHUB_BRANCH="${VERSION_MAJOR}.${VERSION_MINOR}"
+  export SUPERSET_PGP_FULLNAME="${2}"
+  export SUPERSET_VERSION_RC="${SUPERSET_VERSION}rc${VERSION_RC}"
   export SUPERSET_RELEASE=apache-superset-incubating-"${SUPERSET_VERSION}"
   export SUPERSET_RELEASE_RC=apache-superset-incubating-"${SUPERSET_VERSION_RC}"
   export SUPERSET_RELEASE_TARBALL="${SUPERSET_RELEASE}"-source.tar.gz
@@ -34,6 +45,6 @@ else
 
   echo -------------------------------
   echo Set Release env variables
-  env | grep SUPERSET
+  env | grep ^SUPERSET_
   echo -------------------------------
 fi
