e00a62713357da59b48da659e9c77b6acae654b4
aspedrosa <aspedrosa@ua.pt>
2020-01-29 23:45:20 +0000

    Avoid fetch fav dashboard stat not logged in (#8527)
    * Avoid fetch fav dashboard stat not logged in
    
    * Fix old Header tests and add a new one
    
    * Add userId to other Header tests to be consistent
    
    * Format code to make 'prettier' tests pass

--- superset/assets/src/dashboard/components/Header.jsx
+++ superset/assets/src/dashboard/components/Header.jsx
@@ -317,14 +317,16 @@ class Header extends React.PureComponent {
               canSave={userCanSaveAs}
             />
           </span>
-          <span className="favstar">
-            <FaveStar
-              itemId={dashboardInfo.id}
-              fetchFaveStar={this.props.fetchFaveStar}
-              saveFaveStar={this.props.saveFaveStar}
-              isStarred={this.props.isStarred}
-            />
-          </span>
+          {dashboardInfo.userId && (
+            <span className="favstar">
+              <FaveStar
+                itemId={dashboardInfo.id}
+                fetchFaveStar={this.props.fetchFaveStar}
+                saveFaveStar={this.props.saveFaveStar}
+                isStarred={this.props.isStarred}
+              />
+            </span>
+          )}
         </div>
 
         <div className="button-container">
