6ff3f25128ad385b4abfa73ddf2f830b3da2ff2b
David Aaron Suddjian <aasuddjian@gmail.com>
2020-02-10 18:15:44 -0800

    adjust version_info.json

--- superset-frontend/webpack.config.js
+++ superset-frontend/webpack.config.js
@@ -79,7 +79,6 @@ const plugins = [
 
   new CopyPlugin([
     'package.json',
-    'version_info.json',
     { from: 'images', to: 'images' },
     { from: 'stylesheets', to: 'stylesheets' },
   ]),
