27b371a4856d970dff93c8aba4605e3daf3b5eb2
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2020-02-24 10:40:51 +0000

    [core] Fix, sanitize errors returned from testconn (#9178)

--- superset/views/database/validators.py
+++ superset/views/database/validators.py
@@ -36,9 +36,9 @@ def sqlalchemy_uri_validator(
     except (ArgumentError, AttributeError):
         raise exception(
             _(
-                "Invalid connnection string, a valid string follows: "
-                " 'DRIVER://USER:PASSWORD@DB-HOST/DATABASE-NAME'"
-                " <p>Example:'postgresql://user:password@your-postgres-db/database'</p>"
+                "Invalid connection string, a valid string usually follows:"
+                "'DRIVER://USER:PASSWORD@DB-HOST/DATABASE-NAME'"
+                "<p>Example:'postgresql://user:password@your-postgres-db/database'</p>"
             )
         )
 
