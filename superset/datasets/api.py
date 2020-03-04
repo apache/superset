280d6f4dd372331b4f57ad5e94e822c041e63779
Daniel Gaspar <danielvazgaspar@gmail.com>
2020-03-04 16:58:20 +0000

    [dataset] Fix, license and parent class

--- superset/datasets/api.py
+++ superset/datasets/api.py
@@ -1,3 +1,10 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
 #
 #   http://www.apache.org/licenses/LICENSE-2.0
 #
@@ -28,13 +35,13 @@ from superset.datasets.commands.exceptions import (
 from superset.datasets.commands.update import UpdateDatasetCommand
 from superset.datasets.schemas import DatasetPostSchema, DatasetPutSchema
 from superset.views.base import DatasourceFilter
-from superset.views.base_api import BaseOwnedModelRestApi
+from superset.views.base_api import BaseSupersetModelRestApi
 from superset.views.database.filters import DatabaseFilter
 
 logger = logging.getLogger(__name__)
 
 
-class DatasetRestApi(BaseOwnedModelRestApi):
+class DatasetRestApi(BaseSupersetModelRestApi):
     datamodel = SQLAInterface(SqlaTable)
     base_filters = [["id", DatasourceFilter, lambda: []]]
 
