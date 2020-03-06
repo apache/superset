eef4699807d34c79a3e987f07b9f81c6a837d8c3
Erik Ritter <erik.ritter@airbnb.com>
2020-03-06 12:35:36 -0800

    fix: small rendering for no results message (#309)

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-chart/src/components/NoResultsComponent.tsx
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-chart/src/components/NoResultsComponent.tsx
@@ -1,8 +1,11 @@
 import React, { CSSProperties, useMemo } from 'react';
 
 const MESSAGE_STYLES: CSSProperties = { maxWidth: 800 };
-const TITLE_STYLES: CSSProperties = { fontSize: 20, fontWeight: 'bold', paddingBottom: 8 };
-const BODY_STYLES: CSSProperties = { fontSize: 16 };
+const TITLE_STYLES: CSSProperties = { fontSize: 16, fontWeight: 'bold', paddingBottom: 8 };
+const BODY_STYLES: CSSProperties = { fontSize: 14 };
+const MIN_WIDTH_FOR_BODY = 250;
+const BODY_STRING =
+  'No results were returned for this query. If you expected results to be returned, ensure any filters are configured properly and the datasource contains data for the selected time range.';
 
 const generateContainerStyles: (
   height: number | string,
@@ -28,15 +31,19 @@ type Props = {
 const NoResultsComponent = ({ className, height, id, width }: Props) => {
   const containerStyles = useMemo(() => generateContainerStyles(height, width), [height, width]);
 
+  // render the body if the width is auto/100% or greater than 250 pixels
+  const shouldRenderBody = typeof width === 'string' || width > MIN_WIDTH_FOR_BODY;
+
   return (
-    <div className={className} id={id} style={containerStyles}>
+    <div
+      className={className}
+      id={id}
+      style={containerStyles}
+      title={shouldRenderBody ? undefined : BODY_STRING}
+    >
       <div style={MESSAGE_STYLES}>
         <div style={TITLE_STYLES}>No Results</div>
-        <div style={BODY_STYLES}>
-          No results were returned for this query. If you expected results to be returned, ensure
-          any filters are configured properly and the datasource contains data for the selected time
-          range.
-        </div>
+        {shouldRenderBody && <div style={BODY_STYLES}>{BODY_STRING}</div>}
       </div>
     </div>
   );
