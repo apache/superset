92338b7720c49afe32f8ee28ff6cce68944006d0
Jianchao Yang <jyyjcc@gmail.com>
2020-03-09 12:45:33 -0700

    fix(legacy-table): avoid React DOM (#392)
    * fix(legacy-table): avoid React DOM
    
    `jquery.datatables` will manipulate DOMs, sometimes remove them. In case
    of component being reloaded with updated props, React will not be able
    to find those removed nodes, causing a `cannot removeChild` error.
    
    Because of the the way to assign row keys, if table shape changes (add
    or remove columns), React may also have difficulty match the cached
    nodes via keys.
    
    In general it's a bad idea to directly manipulate React rendered DOM
    nodes, so we better just let `jquery.datatables` handle everything.
    
    In the future, once we removed `jquery.datatables`, a pure React
    component will not have such issues.
    
    * fix(legacy-table): handle the case when percentMetrics is null
    
    * fix(legacy-table): linting errors
    
    * refactor: use nimbus build
    
    * test(legacy-table): add React component tests
    
    * test(legacy-table): more sophisticated cases
    
    * fix: address PR #392 comments
    
    * chore(legacy-table): clean up tests case setups
    
    Not seeing the console.warn errors anymore. So cleaning it up.
    
    Previously it was from `<SuperChart />` component, but since we have
    updated the test case to not use <SuperChart>, we are good now.
    
    * fix(legacy-table): misleading comment

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/scripts/build.js
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/superset-ui-plugins/scripts/build.js
@@ -4,21 +4,29 @@
 const { spawnSync, spawn } = require('child_process');
 
 const glob = process.argv[2];
+const extraArgs = process.argv.slice(2);
 
 process.env.PATH = `./node_modules/.bin:${process.env.PATH}`;
 
-const run = (cmd, async = false) => {
+const run = (cmd) => {
   console.log(`>> ${cmd}`);
   const [p, ...args] = cmd.split(' ');
-  const runner = async ? spawn : spawnSync;
-  runner(p, args, { stdio: 'inherit' });
+  const runner = spawnSync;
+  const { status } = runner(p, args, { stdio: 'inherit' });
+  if (status !== 0) {
+    process.exit(status);
+  }
 };
 
 if (glob) {
-  run(`nimbus prettier --check --workspaces=\"@superset-ui/${glob}"`);
-  run(`nimbus babel --clean --workspaces=\"@superset-ui/${glob}"`);
-  run(`nimbus babel --clean --workspaces=\"@superset-ui/${glob}" --esm`);
-  run(`nimbus typescript --build --workspaces=\"@superset-ui/${glob}"`);
+  run(`nimbus prettier packages/${glob}/{src,test}/**/*.{js,jsx,ts,tsx,css}"`);
+  // lint is slow, so not turning it on by default
+  if (extraArgs.includes('--lint')) {
+    run(`nimbus eslint packages/${glob}/{src,test}`);
+  }
+  run(`nimbus babel --clean --workspaces="@superset-ui/${glob}"`);
+  run(`nimbus babel --clean --workspaces="@superset-ui/${glob}" --esm`);
+  run(`nimbus typescript --build --workspaces="@superset-ui/${glob}"`);
   require('./buildAssets');
 } else {
   run('yarn build');
