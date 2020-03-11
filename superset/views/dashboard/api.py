2639ca1d6d655f8fbd68c497e1b6d8e6bf286ed1
Daniel Gaspar <danielvazgaspar@gmail.com>
2020-03-11 13:24:08 +0000

    [dashboard][api] simplify logic

--- superset/views/dashboard/api.py
+++ superset/views/dashboard/api.py
@@ -88,12 +88,10 @@ class BaseDashboardSchema(BaseOwnedSchema):
     @pre_load
     def pre_load(self, data):  # pylint: disable=no-self-use
         super().pre_load(data)
-        if "slug" in data:
-            data["slug"] = data.get("slug")
-            if data["slug"]:
-                data["slug"] = data["slug"].strip()
-                data["slug"] = data["slug"].replace(" ", "-")
-                data["slug"] = re.sub(r"[^\w\-]+", "", data["slug"])
+        if data.get("slug"):
+            data["slug"] = data["slug"].strip()
+            data["slug"] = data["slug"].replace(" ", "-")
+            data["slug"] = re.sub(r"[^\w\-]+", "", data["slug"])
         if "owners" in data:
             data["owners"] = data.get("owners", [])
 
