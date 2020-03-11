58cf0c74a0ed5684e254335ffea07f89e438f6d3
Daniel Gaspar <danielvazgaspar@gmail.com>
2020-03-11 09:27:18 +0000

    [dashboard][api] Fix, PUT publish/draft to not clean slug and owners

--- superset/views/dashboard/api.py
+++ superset/views/dashboard/api.py
@@ -88,12 +88,14 @@ class BaseDashboardSchema(BaseOwnedSchema):
     @pre_load
     def pre_load(self, data):  # pylint: disable=no-self-use
         super().pre_load(data)
-        data["slug"] = data.get("slug")
-        data["owners"] = data.get("owners", [])
-        if data["slug"]:
-            data["slug"] = data["slug"].strip()
-            data["slug"] = data["slug"].replace(" ", "-")
-            data["slug"] = re.sub(r"[^\w\-]+", "", data["slug"])
+        if "slug" in data:
+            data["slug"] = data.get("slug")
+            if data["slug"]:
+                data["slug"] = data["slug"].strip()
+                data["slug"] = data["slug"].replace(" ", "-")
+                data["slug"] = re.sub(r"[^\w\-]+", "", data["slug"])
+        if "owners" in data:
+            data["owners"] = data.get("owners", [])
 
 
 class DashboardPostSchema(BaseDashboardSchema):
