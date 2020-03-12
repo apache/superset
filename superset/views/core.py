8de98889aeaf8312b96eb23c6049f6164a83e0eb
Tai Dupree <tdupreetan@gmail.com>
2020-03-12 15:22:52 -0700

    cast tab_state_id to str in python instead of sql

--- superset/views/core.py
+++ superset/views/core.py
@@ -2685,7 +2685,7 @@ class Superset(BaseSupersetView):
             .filter_by(user_id=user_id)
             .all()
         )
-        tab_state_ids = [tab_state[0] for tab_state in tabs_state]
+        tab_state_ids = [str(tab_state[0]) for tab_state in tabs_state]
         # return first active tab, or fallback to another one if no tab is active
         active_tab = (
             db.session.query(TabState)
@@ -2705,19 +2705,13 @@ class Superset(BaseSupersetView):
                 }
                 for database in db.session.query(models.Database).all()
             }
-            try:
-                # return all user queries associated with existing SQL editors
-                user_queries = (
-                    db.session.query(Query)
-                    .filter_by(user_id=user_id)
-                    .filter(Query.sql_editor_id.cast(Integer).in_(tab_state_ids))
-                    .all()
-                )
-            except DataError as e:
-                logger.error(e, exc_info=True)
-                db.session.rollback()
-                user_queries = []
-
+            # return all user queries associated with existing SQL editors
+            user_queries = (
+                db.session.query(Query)
+                .filter_by(user_id=user_id)
+                .filter(Query.sql_editor_id.in_(tab_state_ids))
+                .all()
+            )
             queries = {
                 query.client_id: {k: v for k, v in query.to_dict().items()}
                 for query in user_queries
