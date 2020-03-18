2fdb92558ab56f080f34899e20fec36918a84c7c
auxten <auxtenwpc@gmail.com>
2020-03-18 18:00:47 +0800

    Fix add chart page frontend i18n issue

--- superset-frontend/src/addSlice/App.tsx
+++ superset-frontend/src/addSlice/App.tsx
@@ -26,7 +26,7 @@ import AddSliceContainer from './AddSliceContainer';
 setupApp();
 setupPlugins();
 
-const addSliceContainer = document.getElementById('js-add-slice-container');
+const addSliceContainer = document.getElementById('app');
 const bootstrapData = JSON.parse(
   addSliceContainer?.getAttribute('data-bootstrap') || '{}',
 );
