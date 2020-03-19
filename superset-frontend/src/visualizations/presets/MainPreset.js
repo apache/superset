cbed5a929799c5b32eaa28c55d5524970c6d5e10
Evan Rusackas <evan@preset.io>
2020-03-19 09:39:03 -0700

    lint :sparkles:

--- superset-frontend/src/visualizations/presets/MainPreset.js
+++ superset-frontend/src/visualizations/presets/MainPreset.js
@@ -50,7 +50,6 @@ export default class MainPreset extends Preset {
       name: 'Legacy charts',
       presets: [new DeckGLChartPreset()],
       plugins: [
-
         new BigNumberChartPlugin().configure({ key: 'big_number' }),
         new BigNumberTotalChartPlugin().configure({ key: 'big_number_total' }),
         // import('../../../../../superset-ui-plugins_preset/packages/superset-ui-legacy-preset-chart-big-number/src/index').then(module => {
@@ -76,34 +75,80 @@ export default class MainPreset extends Preset {
         // BOXPLOT
         new BoxPlotChartPlugin().configure({ key: 'box_plot' }),
 
-        
-        import('@superset-ui/legacy-plugin-chart-calendar').then(module => new module.default().configure({ key: 'cal_heatmap' })),
-        import('../FilterBox/FilterBoxChartPlugin').then(module => new module.default().configure({ key: 'filter_box' })),
-        import('@superset-ui/legacy-plugin-chart-chord').then(module => new module.default().configure({ key: 'chord' })),
-        import('@superset-ui/legacy-plugin-chart-country-map').then(module => new module.default().configure({ key: 'country_map' })),
-        import('@superset-ui/legacy-plugin-chart-event-flow').then(module => new module.default().configure({ key: 'event_flow' })),
-        import('@superset-ui/legacy-plugin-chart-force-directed').then(module => new module.default().configure({ key: 'directed_force' })),
-        import('@superset-ui/legacy-plugin-chart-heatmap').then(module => new module.default().configure({ key: 'heatmap' })),
-        import('@superset-ui/legacy-plugin-chart-histogram').then(module => new module.default().configure({ key: 'histogram' })),
-        import('@superset-ui/legacy-plugin-chart-horizon').then(module => new module.default().configure({ key: 'horizon' })),
-        import('@superset-ui/legacy-plugin-chart-iframe').then(module => new module.default().configure({ key: 'iframe' })),
-        import('@superset-ui/legacy-plugin-chart-map-box').then(module => new module.default().configure({ key: 'mapbox' })),
-        import('@superset-ui/legacy-plugin-chart-markup').then(module => new module.default().configure({ key: 'separator' })),
-        import('@superset-ui/legacy-plugin-chart-paired-t-test').then(module => new module.default().configure({ key: 'paired_ttest' })),
-        import('@superset-ui/legacy-plugin-chart-parallel-coordinates').then(module => new module.default().configure({ key: 'para' })),
-        import('@superset-ui/legacy-plugin-chart-partition').then(module => new module.default().configure({ key: 'partition' })),
-        import('@superset-ui/legacy-plugin-chart-pivot-table').then(module => new module.default().configure({ key: 'pivot_table' })),
-        import('@superset-ui/legacy-plugin-chart-rose').then(module => new module.default().configure({ key: 'rose' })),
-        import('@superset-ui/legacy-plugin-chart-sankey').then(module => new module.default().configure({ key: 'sankey' })),
-        import('@superset-ui/legacy-plugin-chart-sunburst').then(module => new module.default().configure({ key: 'sunburst' })),
-        import('@superset-ui/legacy-plugin-chart-table').then(module => new module.default().configure({ key: 'table' })),
-        import('../TimeTable/TimeTableChartPlugin').then(module => new module.default().configure({ key: 'time_table' })),
+        import('@superset-ui/legacy-plugin-chart-calendar').then(module =>
+          new module.default().configure({ key: 'cal_heatmap' }),
+        ),
+        import('../FilterBox/FilterBoxChartPlugin').then(module =>
+          new module.default().configure({ key: 'filter_box' }),
+        ),
+        import('@superset-ui/legacy-plugin-chart-chord').then(module =>
+          new module.default().configure({ key: 'chord' }),
+        ),
+        import('@superset-ui/legacy-plugin-chart-country-map').then(module =>
+          new module.default().configure({ key: 'country_map' }),
+        ),
+        import('@superset-ui/legacy-plugin-chart-event-flow').then(module =>
+          new module.default().configure({ key: 'event_flow' }),
+        ),
+        import('@superset-ui/legacy-plugin-chart-force-directed').then(module =>
+          new module.default().configure({ key: 'directed_force' }),
+        ),
+        import('@superset-ui/legacy-plugin-chart-heatmap').then(module =>
+          new module.default().configure({ key: 'heatmap' }),
+        ),
+        import('@superset-ui/legacy-plugin-chart-histogram').then(module =>
+          new module.default().configure({ key: 'histogram' }),
+        ),
+        import('@superset-ui/legacy-plugin-chart-horizon').then(module =>
+          new module.default().configure({ key: 'horizon' }),
+        ),
+        import('@superset-ui/legacy-plugin-chart-iframe').then(module =>
+          new module.default().configure({ key: 'iframe' }),
+        ),
+        import('@superset-ui/legacy-plugin-chart-map-box').then(module =>
+          new module.default().configure({ key: 'mapbox' }),
+        ),
+        import('@superset-ui/legacy-plugin-chart-markup').then(module =>
+          new module.default().configure({ key: 'separator' }),
+        ),
+        import('@superset-ui/legacy-plugin-chart-paired-t-test').then(module =>
+          new module.default().configure({ key: 'paired_ttest' }),
+        ),
+        import(
+          '@superset-ui/legacy-plugin-chart-parallel-coordinates'
+        ).then(module => new module.default().configure({ key: 'para' })),
+        import('@superset-ui/legacy-plugin-chart-partition').then(module =>
+          new module.default().configure({ key: 'partition' }),
+        ),
+        import('@superset-ui/legacy-plugin-chart-pivot-table').then(module =>
+          new module.default().configure({ key: 'pivot_table' }),
+        ),
+        import('@superset-ui/legacy-plugin-chart-rose').then(module =>
+          new module.default().configure({ key: 'rose' }),
+        ),
+        import('@superset-ui/legacy-plugin-chart-sankey').then(module =>
+          new module.default().configure({ key: 'sankey' }),
+        ),
+        import('@superset-ui/legacy-plugin-chart-sunburst').then(module =>
+          new module.default().configure({ key: 'sunburst' }),
+        ),
+        import('@superset-ui/legacy-plugin-chart-table').then(module =>
+          new module.default().configure({ key: 'table' }),
+        ),
+        import('../TimeTable/TimeTableChartPlugin').then(module =>
+          new module.default().configure({ key: 'time_table' }),
+        ),
         // import('../../../../../superset-ui-plugins_preset/packages/superset-ui-legacy-plugin-chart-treemap/esm/').then(module => new module.default().configure({ key: 'treemap' })),
-        import('@superset-ui/legacy-plugin-chart-treemap').then(module => new module.default().configure({ key: 'treemap' })),
+        import('@superset-ui/legacy-plugin-chart-treemap').then(module =>
+          new module.default().configure({ key: 'treemap' }),
+        ),
         // import('../../../../../superset-ui-plugins_preset/packages/superset-ui-legacy-plugin-chart-word-cloud').then(module => new module.default().configure({ key: 'word_cloud' })),
-        import('@superset-ui/legacy-plugin-chart-word-cloud').then(module => new module.default().configure({ key: 'word_cloud' })),
-        import('@superset-ui/legacy-plugin-chart-world-map').then(module => new module.default().configure({ key: 'world_map' })),
-
+        import('@superset-ui/legacy-plugin-chart-word-cloud').then(module =>
+          new module.default().configure({ key: 'word_cloud' }),
+        ),
+        import('@superset-ui/legacy-plugin-chart-world-map').then(module =>
+          new module.default().configure({ key: 'world_map' }),
+        ),
       ],
     });
   }
