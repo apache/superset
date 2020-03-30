16f24a76a2e7297a55962112e34cfae59631b6f8
miha_work <m7onov@gmail.com>
2020-03-30 10:40:27 +0300

    add redis and celery for prod

--- docker/superset_config.py
+++ docker/superset_config.py
@@ -1,5 +1,6 @@
 import os
 
+from werkzeug.contrib.cache import RedisCache
 
 def get_env_variable(var_name, default=None):
     """Get the environment variable or raise exception."""
@@ -32,4 +33,23 @@ SQLALCHEMY_DATABASE_URI = "%s://%s:%s@%s:%s/%s" % (
     DATABASE_DB,
 )
 SQLALCHEMY_ECHO = False
+
+REDIS_HOST = get_env_variable("REDIS_HOST")
+REDIS_PORT = get_env_variable("REDIS_PORT")
+
+class CeleryConfig(object):
+    BROKER_URL = "redis://%s:%s/0" % (REDIS_HOST, REDIS_PORT)
+    CELERY_IMPORTS = ("superset.sql_lab",)
+    CELERY_RESULT_BACKEND = "redis://%s:%s/1" % (REDIS_HOST, REDIS_PORT)
+    CELERY_ANNOTATIONS = {"tasks.add": {"rate_limit": "10/s"}}
+    CELERY_TASK_PROTOCOL = 1
+
+
+CELERY_CONFIG = CeleryConfig
+
+RESULTS_BACKEND = RedisCache(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    key_prefix='superset_results'
+)
 
