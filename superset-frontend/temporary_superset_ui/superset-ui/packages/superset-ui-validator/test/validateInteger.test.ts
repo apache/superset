e5be1a47109aa493eafeefba10efe05168a79077
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-03-31 17:08:59 -0700

    feat: add validator package (#322)
    * feat: add validator package
    
    * test: add unit tests
    
    * feat: add legacy function
    
    * fix: rename

---
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-validator/test/validateInteger.test.ts
@@ -0,0 +1,18 @@
+import { validateInteger } from '../src';
+
+describe('validateInteger()', () => {
+  it('returns the warning message if invalid', () => {
+    expect(validateInteger(10.1)).toBeTruthy();
+    expect(validateInteger(NaN)).toBeTruthy();
+    expect(validateInteger(Infinity)).toBeTruthy();
+    expect(validateInteger(undefined)).toBeTruthy();
+    expect(validateInteger(null)).toBeTruthy();
+    expect(validateInteger('abc')).toBeTruthy();
+    expect(validateInteger('')).toBeTruthy();
+  });
+  it('returns false if the input is valid', () => {
+    expect(validateInteger(0)).toBeFalsy();
+    expect(validateInteger(10)).toBeFalsy();
+    expect(validateInteger('10')).toBeFalsy();
+  });
+});
