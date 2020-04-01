36ade71070c28ff1273e272ee05862b0fe9f621c
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-03-31 17:08:59 -0700

    feat: add validator package (#322)
    * feat: add validator package
    
    * test: add unit tests
    
    * feat: add legacy function
    
    * fix: rename

---
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-validator/test/validateNonEmpty.test.ts
@@ -0,0 +1,15 @@
+import { validateNonEmpty } from '../src';
+
+describe('validateNonEmpty()', () => {
+  it('returns the warning message if invalid', () => {
+    expect(validateNonEmpty([])).toBeTruthy();
+    expect(validateNonEmpty(undefined)).toBeTruthy();
+    expect(validateNonEmpty(null)).toBeTruthy();
+    expect(validateNonEmpty('')).toBeTruthy();
+  });
+  it('returns false if the input is valid', () => {
+    expect(validateNonEmpty(0)).toBeFalsy();
+    expect(validateNonEmpty(10)).toBeFalsy();
+    expect(validateNonEmpty('abc')).toBeFalsy();
+  });
+});
