b6bca9f137b069716938ac1df5862c0b5d2c3461
Evan Rusackas <evan@preset.io>
2020-04-06 18:37:25 -0700

    Migrating shared DeckGL controls (#9455)
    * Missed a couple unique controls!
    
    * new shared module, with filter_nulls export.
    
    * autozoom
    
    * dimension
    
    * nixing unneeded import
    
    * stroke_width
    
    * js_columns
    
    * remaining js_* controls
    
    * cleaning up unused stuff
    
    * legend_format
    
    * exports for prior commit
    
    * legend_position
    
    * line_column
    
    * fill_color_picker
    
    * filled, stroked, stroke_color_picker
    
    * grid_size, extruded
    
    * ... more gridsize
    
    * viewport
    
    * point_radius_fixed, spatial
    
    * multiplier, line_type, line_width
    
    * reverse_long_lat
    
    * lint :sparkles:

--- superset-frontend/src/explore/controlPanels/DeckScatter.js
+++ superset-frontend/src/explore/controlPanels/DeckScatter.js
@@ -19,6 +19,21 @@
 import { t } from '@superset-ui/translation';
 import timeGrainSqlaAnimationOverrides from './timeGrainSqlaAnimationOverrides';
 import { nonEmpty } from '../validators';
+import {
+  filterNulls,
+  autozoom,
+  dimension,
+  jsColumns,
+  jsDataMutator,
+  jsTooltip,
+  jsOnclickHref,
+  legendFormat,
+  legendPosition,
+  viewport,
+  spatial,
+  pointRadiusFixed,
+  multiplier,
+} from './Shared_DeckGL';
 
 export default {
   requiresTime: true,
@@ -38,8 +53,8 @@ export default {
       label: t('Query'),
       expanded: true,
       controlSetRows: [
-        ['spatial', null],
-        ['row_limit', 'filter_nulls'],
+        [spatial, null],
+        ['row_limit', filterNulls],
         ['adhoc_filters'],
       ],
     },
@@ -47,15 +62,15 @@ export default {
       label: t('Map'),
       expanded: true,
       controlSetRows: [
-        ['mapbox_style', 'viewport'],
-        ['autozoom', null],
+        ['mapbox_style', viewport],
+        [autozoom, null],
       ],
     },
     {
       label: t('Point Size'),
       controlSetRows: [
         [
-          'point_radius_fixed',
+          pointRadiusFixed,
           {
             name: 'point_unit',
             config: {
@@ -109,34 +124,38 @@ export default {
             },
           },
         ],
-        ['multiplier', null],
+        [multiplier, null],
       ],
     },
     {
       label: t('Point Color'),
       controlSetRows: [
-        ['color_picker', 'legend_position'],
-        [null, 'legend_format'],
-        ['dimension', 'color_scheme', 'label_colors'],
+        ['color_picker', legendPosition],
+        [null, legendFormat],
+        [
+          {
+            ...dimension,
+            label: t('Categorical Color'),
+            description: t(
+              'Pick a dimension from which categorical colors are defined',
+            ),
+          },
+          'color_scheme',
+          'label_colors',
+        ],
       ],
     },
     {
       label: t('Advanced'),
       controlSetRows: [
-        ['js_columns'],
-        ['js_data_mutator'],
-        ['js_tooltip'],
-        ['js_onclick_href'],
+        [jsColumns],
+        [jsDataMutator],
+        [jsTooltip],
+        [jsOnclickHref],
       ],
     },
   ],
   controlOverrides: {
-    dimension: {
-      label: t('Categorical Color'),
-      description: t(
-        'Pick a dimension from which categorical colors are defined',
-      ),
-    },
     size: {
       validators: [],
     },
