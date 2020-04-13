d22c6454217002a2780b341f54f7fcb230c4f228
Evan Rusackas <evan@preset.io>
2020-04-13 10:15:09 -0700

    feat: allow toggling of table viz's bar chart backgrounds (#352)
    * feat: allow toggling of bar chart backgrounds
    
    * fix: inverting bar-chart-bg hide/show
    
    * fix: keeping metrics right aligned

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/table/src/transformProps.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/table/src/transformProps.ts
@@ -38,6 +38,7 @@ export interface DataTableProps {
   alignPositiveNegative: boolean;
   colorPositiveNegative: boolean;
   columns: DataColumnMeta[];
+  showCellBars: boolean;
   metrics: string[];
   percentMetrics: string[];
   includeSearch: boolean;
@@ -55,6 +56,7 @@ export interface DataTableProps {
 export interface TableChartFormData {
   alignPn?: boolean;
   colorPn?: boolean;
+  showCellBars?: boolean;
   includeSearch?: boolean;
   orderDesc?: boolean;
   pageLength?: string;
@@ -80,6 +82,7 @@ export default function transformProps(chartProps: ChartProps): DataTableProps {
   const {
     alignPn = true,
     colorPn = true,
+    showCellBars = true,
     includeSearch = false,
     orderDesc = false,
     pageLength = 0,
@@ -118,6 +121,7 @@ export default function transformProps(chartProps: ChartProps): DataTableProps {
     percentMetrics,
     alignPositiveNegative: alignPn,
     colorPositiveNegative: colorPn,
+    showCellBars,
     includeSearch,
     orderDesc,
     pageLength: pageLength ? parseInt(pageLength, 10) : 0,
