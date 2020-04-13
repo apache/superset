be7aa64346df0c4b4a5fd7eee0f57b43f8f3e599
Evan Rusackas <evan@preset.io>
2020-04-13 16:53:50 -0700

    control to turn off table bar-chart backgrounds (#9500)
    * control to turn off table bar-chart backgrounds
    
    * flipping enable to disable, to support existing charts
    
    * lint :sparkles:
    
    * Turn ON to enable (reversing logic)
    
    * bumping big number chart
    
    * ling :sparkles:

--- superset-frontend/src/explore/controlPanels/Table.js
+++ superset-frontend/src/explore/controlPanels/Table.js
@@ -183,6 +183,21 @@ export default {
             },
           },
         ],
+        [
+          {
+            name: 'show_cell_bars',
+            config: {
+              type: 'CheckboxControl',
+              label: t('Show Cell Bars'),
+              renderTrigger: true,
+              default: true,
+              description: t(
+                'Enable to display bar chart background elements in table columns',
+              ),
+            },
+          },
+          null,
+        ],
       ],
     },
   ],
