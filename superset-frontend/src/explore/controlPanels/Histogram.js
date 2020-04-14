913b87391a3dbc672c2b97abc4deaebe23f8ae81
Evan Rusackas <evan@preset.io>
2020-04-14 12:00:25 -0700

    Importing validators module from superset-ui (#9465)
    * importing... this is going to break CI, but there it is.
    
    * adding validator package.
    
    * pluralization - maybe it should be plural, but whatevs
    
    * updating validators to match new exports
    
    * removing old validators
    
    * lintage :sparkles:
    
    * validateNumeric -> validateNumber
    
    * using legacyValidateInteger, legacyValidateNumber
    
    * linting :sparkles:
    
    * removing extraneous package lock file

--- superset-frontend/src/explore/controlPanels/Histogram.js
+++ superset-frontend/src/explore/controlPanels/Histogram.js
@@ -17,7 +17,7 @@
  * under the License.
  */
 import { t } from '@superset-ui/translation';
-import { nonEmpty } from '../validators';
+import { validateNonEmpty } from '@superset-ui/validator';
 
 export default {
   controlPanelSections: [
@@ -48,7 +48,7 @@ export default {
       label: t('Numeric Columns'),
       description: t('Select the numeric columns to draw the histogram'),
       multi: true,
-      validators: [nonEmpty],
+      validators: [validateNonEmpty],
     },
     link_length: {
       label: t('No of Bins'),
