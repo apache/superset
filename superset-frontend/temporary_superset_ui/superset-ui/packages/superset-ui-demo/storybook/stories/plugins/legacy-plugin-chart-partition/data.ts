4dad647139aea15d9378a1fcf3778a330e6cfef6
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-04-16 12:34:30 -0700

    Merge pull request #368 from apache-superset/kristw--migrate-pivot
    feat: migrate 4 chart plugins

---
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-demo/storybook/stories/plugins/legacy-plugin-chart-partition/data.ts
@@ -0,0 +1,94 @@
+export default [
+  {
+    name: 'World',
+    val: 165709793794.0,
+    children: [
+      {
+        name: 'East Asia & Pacific',
+        val: 74157936990.0,
+        children: [
+          {
+            name: 'CHN',
+            val: 58345455000.0,
+            children: [],
+          },
+          {
+            name: 'IDN',
+            val: 9357861231.0,
+            children: [],
+          },
+          {
+            name: 'JPN',
+            val: 6454620759.0,
+            children: [],
+          },
+        ],
+      },
+      {
+        name: 'Europe & Central Asia',
+        val: 7667188460.0,
+        children: [
+          {
+            name: 'RUS',
+            val: 7667188460.0,
+            children: [],
+          },
+        ],
+      },
+      {
+        name: 'Latin America & Caribbean',
+        val: 7752058955.0,
+        children: [
+          {
+            name: 'BRA',
+            val: 7752058955.0,
+            children: [],
+          },
+        ],
+      },
+      {
+        name: 'North America',
+        val: 13604468357.0,
+        children: [
+          {
+            name: 'USA',
+            val: 13604468357.0,
+            children: [],
+          },
+        ],
+      },
+      {
+        name: 'South Asia',
+        val: 57268340539.0,
+        children: [
+          {
+            name: 'BGD',
+            val: 5549261462.0,
+            children: [],
+          },
+          {
+            name: 'IND',
+            val: 46023037597.0,
+            children: [],
+          },
+          {
+            name: 'PAK',
+            val: 5696041480.0,
+            children: [],
+          },
+        ],
+      },
+      {
+        name: 'Sub-Saharan Africa',
+        val: 5259800493.0,
+        children: [
+          {
+            name: 'NGA',
+            val: 5259800493.0,
+            children: [],
+          },
+        ],
+      },
+    ],
+  },
+];
