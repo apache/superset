cfcee83eef60af480ed0fac74fc4d18d16bec7aa
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-04-17 00:43:46 -0700

    docs: update readme

--- superset-frontend/temporary_superset_ui/superset-ui/temporary-plugins/superset-ui-plugins-demo/storybook/stories/legacy-plugin-chart-iframe/index.js
+++
@@ -1,8 +0,0 @@
-import IframeChartPlugin from '../../../../superset-ui-legacy-plugin-chart-iframe';
-import Stories from './Stories';
-
-new IframeChartPlugin().configure({ key: 'iframe' }).register();
-
-export default {
-  examples: [...Stories],
-};
--- superset-frontend/temporary_superset_ui/superset-ui/temporary-plugins/superset-ui-plugins-demo/storybook/stories/legacy-plugin-chart-markup/index.js
+++
@@ -1,8 +0,0 @@
-import MarkupChartPlugin from '../../../../superset-ui-legacy-plugin-chart-markup';
-import Stories from './Stories';
-
-new MarkupChartPlugin().configure({ key: 'markup' }).register();
-
-export default {
-  examples: [...Stories],
-};
