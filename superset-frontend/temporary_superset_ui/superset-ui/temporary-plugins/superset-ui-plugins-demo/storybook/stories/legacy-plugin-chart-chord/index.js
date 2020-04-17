41d62ba64070a129a791c998c8df8d06a8d191ed
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-04-17 00:43:46 -0700

    docs: update readme

--- superset-frontend/temporary_superset_ui/superset-ui/temporary-plugins/superset-ui-plugins-demo/storybook/stories/legacy-plugin-chart-chord/index.js
+++
@@ -1,8 +0,0 @@
-import ChordChartPlugin from '../../../../superset-ui-legacy-plugin-chart-chord';
-import Stories from './Stories';
-
-new ChordChartPlugin().configure({ key: 'chord' }).register();
-
-export default {
-  examples: [...Stories],
-};
