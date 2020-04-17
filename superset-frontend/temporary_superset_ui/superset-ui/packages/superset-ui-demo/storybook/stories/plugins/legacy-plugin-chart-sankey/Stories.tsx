81ca3b980188f14f08c9eea7b16dbaccc7b11095
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-04-17 15:48:25 -0700

    feat: migrate chart plugins (#375)
    * feat(legacy-plugin-chart-rose): migrate package and lint
    
    * feat(legacy-plugin-chart-sankey): migrate
    
    * feat(legacy-plugin-chart-sankey-loop): migrate
    
    * feat(legacy-plugin-chart-sunburst): migrate and lint
    
    * feat(legacy-plugin-chart-treemap): migrate
    
    * feat(legacy-plugin-chart-world-map): migrate
    
    * docs: update storybooks
    
    * feat(legacy-plugin-chart-force-directed): migrate and lint
    
    * feat(legacy-plugin-chart-map-box): migrate and lint
    
    * feat(legacy-plugin-chart-histogram): migrate and lint
    
    * docs: update storybooks

---
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-demo/storybook/stories/plugins/legacy-plugin-chart-sankey/Stories.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable no-magic-numbers */
+import React from 'react';
+import { SuperChart } from '@superset-ui/chart';
+import SankeyChartPlugin from '@superset-ui/legacy-plugin-chart-sankey';
+import data from './data';
+
+new SankeyChartPlugin().configure({ key: 'sankey' }).register();
+
+export default {
+  title: 'Legacy Chart Plugins|legacy-plugin-chart-sankey',
+};
+
+export const basic = () => (
+  <SuperChart
+    chartType="sankey"
+    width={400}
+    height={400}
+    queryData={{ data }}
+    formData={{
+      colorScheme: 'd3Category10',
+    }}
+  />
+);
--- superset-frontend/temporary_superset_ui/superset-ui/temporary-plugins/superset-ui-plugins-demo/storybook/stories/legacy-plugin-chart-rose/index.js
+++
@@ -1,8 +0,0 @@
-import RoseChartPlugin from '../../../../superset-ui-legacy-plugin-chart-rose';
-import Stories from './Stories';
-
-new RoseChartPlugin().configure({ key: 'rose' }).register();
-
-export default {
-  examples: [...Stories],
-};
