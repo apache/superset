e24f6cd79ca1fd1216853f7820899c5e474b6718
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-04-17 15:48:25 -0700

    feat: migrate chart plugins (#375)
    * feat(legacy-plugin-chart-rose): migrate package and lint
    
    * feat(legacy-plugin-chart-sankey): migrate
    
    * feat(legacy-plugin-chart-sankey-loop): migrate
    
    * feat(legacy-plugin-chart-sunburst): migrate and lint
    
    * feat(legacy-plugin-chart-treemap): migrate
    
    * feat(legacy-plugin-chart-world-map): migrate
    
    * docs: update storybooks
    
    * feat(legacy-plugin-chart-force-directed): migrate and lint
    
    * feat(legacy-plugin-chart-map-box): migrate and lint
    
    * feat(legacy-plugin-chart-histogram): migrate and lint
    
    * docs: update storybooks

--- superset-frontend/temporary_superset_ui/superset-ui/temporary-plugins/superset-ui-plugins-demo/storybook/stories/legacy-plugin-chart-sankey/index.js
+++
@@ -1,8 +0,0 @@
-import SankeyChartPlugin from '../../../../superset-ui-legacy-plugin-chart-sankey';
-import Stories from './Stories';
-
-new SankeyChartPlugin().configure({ key: 'sankey' }).register();
-
-export default {
-  examples: [...Stories],
-};
