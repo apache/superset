1ac1462e6b93a59c62c3f667df36228dbcdcec6f
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-04-17 15:48:25 -0700

    feat: migrate chart plugins (#375)
    * feat(legacy-plugin-chart-rose): migrate package and lint
    
    * feat(legacy-plugin-chart-sankey): migrate
    
    * feat(legacy-plugin-chart-sankey-loop): migrate
    
    * feat(legacy-plugin-chart-sunburst): migrate and lint
    
    * feat(legacy-plugin-chart-treemap): migrate
    
    * feat(legacy-plugin-chart-world-map): migrate
    
    * docs: update storybooks
    
    * feat(legacy-plugin-chart-force-directed): migrate and lint
    
    * feat(legacy-plugin-chart-map-box): migrate and lint
    
    * feat(legacy-plugin-chart-histogram): migrate and lint
    
    * docs: update storybooks

--- superset-frontend/temporary_superset_ui/superset-ui/temporary-plugins/superset-ui-plugins-demo/storybook/stories/legacy-plugin-chart-world-map/index.js
+++
@@ -1,8 +0,0 @@
-import WorldMapChartPlugin from '../../../../superset-ui-legacy-plugin-chart-world-map';
-import Stories from './Stories';
-
-new WorldMapChartPlugin().configure({ key: 'world-map' }).register();
-
-export default {
-  examples: [...Stories],
-};
