b3710e23374d9ca2b10e73232e151a35056a095a
Evan Rusackas <evan@preset.io>
2020-04-17 22:42:20 -0700

    formatSelectOptions import

--- superset-frontend/src/explore/controlPanels/PivotTable.js
+++ superset-frontend/src/explore/controlPanels/PivotTable.js
@@ -17,6 +17,7 @@
  * under the License.
  */
 import { t } from '@superset-ui/translation';
+import { formatSelectOptions } from '../../modules/utils';
 
 export default {
   controlPanelSections: [
