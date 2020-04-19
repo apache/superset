d4aa69868e9d93e0b3ea6ba9efb1bf456317758e
John Bodley <john.bodley@airbnb.com>
2020-04-19 12:12:41 -0700

    [mypy] Enforcing typing for a number of modules

--- superset/tasks/schedules.py
+++ superset/tasks/schedules.py
@@ -23,10 +23,12 @@ import urllib.request
 from collections import namedtuple
 from datetime import datetime, timedelta
 from email.utils import make_msgid, parseaddr
+from typing import Any, Dict, Iterator, List, Optional, Tuple, Union
 from urllib.error import URLError  # pylint: disable=ungrouped-imports
 
 import croniter
 import simplejson as json
+from celery.app.task import Task
 from dateutil.tz import tzlocal
 from flask import render_template, Response, session, url_for
 from flask_babel import gettext as __
@@ -34,16 +36,20 @@ from flask_login import login_user
 from retry.api import retry_call
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver import chrome, firefox
+from werkzeug.datastructures import TypeConversionDict
 from werkzeug.http import parse_cookie
 
 # Superset framework imports
 from superset import app, db, security_manager
 from superset.extensions import celery_app
 from superset.models.schedules import (
+    DashboardEmailSchedule,
     EmailDeliveryType,
+    EmailSchedule,
     get_scheduler_model,
     ScheduleType,
     SliceEmailReportFormat,
+    SliceEmailSchedule,
 )
 from superset.utils.core import get_email_address_list, send_email_smtp
 
@@ -59,7 +65,9 @@ PAGE_RENDER_WAIT = 30
 EmailContent = namedtuple("EmailContent", ["body", "data", "images"])
 
 
-def _get_recipients(schedule):
+def _get_recipients(
+    schedule: Union[DashboardEmailSchedule, SliceEmailSchedule]
+) -> Iterator[Tuple[str, str]]:
     bcc = config["EMAIL_REPORT_BCC_ADDRESS"]
 
     if schedule.deliver_as_group:
@@ -70,7 +78,11 @@ def _get_recipients(schedule):
             yield (to, bcc)
 
 
-def _deliver_email(schedule, subject, email):
+def _deliver_email(
+    schedule: Union[DashboardEmailSchedule, SliceEmailSchedule],
+    subject: str,
+    email: EmailContent,
+) -> None:
     for (to, bcc) in _get_recipients(schedule):
         send_email_smtp(
             to,
@@ -85,7 +97,11 @@ def _deliver_email(schedule, subject, email):
         )
 
 
-def _generate_mail_content(schedule, screenshot, name, url):
+def _generate_mail_content(
+    schedule: EmailSchedule, screenshot: bytes, name: str, url: str
+) -> EmailContent:
+    data: Optional[Dict[str, Any]]
+
     if schedule.delivery_type == EmailDeliveryType.attachment:
         images = None
         data = {"screenshot.png": screenshot}
@@ -115,7 +131,7 @@ def _generate_mail_content(schedule, screenshot, name, url):
     return EmailContent(body, data, images)
 
 
-def _get_auth_cookies():
+def _get_auth_cookies() -> List[TypeConversionDict]:
     # Login with the user specified to get the reports
     with app.test_request_context():
         user = security_manager.find_user(config["EMAIL_REPORTS_USER"])
@@ -136,14 +152,16 @@ def _get_auth_cookies():
     return cookies
 
 
-def _get_url_path(view, **kwargs):
+def _get_url_path(view: str, **kwargs: Any) -> str:
     with app.test_request_context():
         return urllib.parse.urljoin(
             str(config["WEBDRIVER_BASEURL"]), url_for(view, **kwargs)
         )
 
 
-def create_webdriver():
+def create_webdriver() -> Union[
+    chrome.webdriver.WebDriver, firefox.webdriver.WebDriver
+]:
     # Create a webdriver for use in fetching reports
     if config["EMAIL_REPORTS_WEBDRIVER"] == "firefox":
         driver_class = firefox.webdriver.WebDriver
@@ -181,7 +199,9 @@ def create_webdriver():
     return driver
 
 
-def destroy_webdriver(driver):
+def destroy_webdriver(
+    driver: Union[chrome.webdriver.WebDriver, firefox.webdriver.WebDriver]
+) -> None:
     """
     Destroy a driver
     """
@@ -198,7 +218,7 @@ def destroy_webdriver(driver):
         pass
 
 
-def deliver_dashboard(schedule):
+def deliver_dashboard(schedule: DashboardEmailSchedule) -> None:
     """
     Given a schedule, delivery the dashboard as an email report
     """
@@ -243,7 +263,7 @@ def deliver_dashboard(schedule):
     _deliver_email(schedule, subject, email)
 
 
-def _get_slice_data(schedule):
+def _get_slice_data(schedule: SliceEmailSchedule) -> EmailContent:
     slc = schedule.slice
 
     slice_url = _get_url_path(
@@ -272,7 +292,7 @@ def _get_slice_data(schedule):
 
         # Parse the csv file and generate HTML
         columns = rows.pop(0)
-        with app.app_context():
+        with app.app_context():  # type: ignore
             body = render_template(
                 "superset/reports/slice_data.html",
                 columns=columns,
@@ -292,7 +312,7 @@ def _get_slice_data(schedule):
     return EmailContent(body, data, None)
 
 
-def _get_slice_visualization(schedule):
+def _get_slice_visualization(schedule: SliceEmailSchedule) -> EmailContent:
     slc = schedule.slice
 
     # Create a driver, fetch the page, wait for the page to render
@@ -327,7 +347,7 @@ def _get_slice_visualization(schedule):
     return _generate_mail_content(schedule, screenshot, slc.slice_name, slice_url)
 
 
-def deliver_slice(schedule):
+def deliver_slice(schedule: Union[DashboardEmailSchedule, SliceEmailSchedule]) -> None:
     """
     Given a schedule, delivery the slice as an email report
     """
@@ -352,9 +372,12 @@ def deliver_slice(schedule):
     bind=True,
     soft_time_limit=config["EMAIL_ASYNC_TIME_LIMIT_SEC"],
 )
-def schedule_email_report(
-    task, report_type, schedule_id, recipients=None
-):  # pylint: disable=unused-argument
+def schedule_email_report(  # pylint: disable=unused-argument
+    task: Task,
+    report_type: ScheduleType,
+    schedule_id: int,
+    recipients: Optional[str] = None,
+) -> None:
     model_cls = get_scheduler_model(report_type)
     schedule = db.create_scoped_session().query(model_cls).get(schedule_id)
 
@@ -368,15 +391,17 @@ def schedule_email_report(
         schedule.id = schedule_id
         schedule.recipients = recipients
 
-    if report_type == ScheduleType.dashboard.value:
+    if report_type == ScheduleType.dashboard:
         deliver_dashboard(schedule)
-    elif report_type == ScheduleType.slice.value:
+    elif report_type == ScheduleType.slice:
         deliver_slice(schedule)
     else:
         raise RuntimeError("Unknown report type")
 
 
-def next_schedules(crontab, start_at, stop_at, resolution=0):
+def next_schedules(
+    crontab: str, start_at: datetime, stop_at: datetime, resolution: int = 0
+) -> Iterator[datetime]:
     crons = croniter.croniter(crontab, start_at - timedelta(seconds=1))
     previous = start_at - timedelta(days=1)
 
@@ -396,13 +421,19 @@ def next_schedules(crontab, start_at, stop_at, resolution=0):
         previous = eta
 
 
-def schedule_window(report_type, start_at, stop_at, resolution):
+def schedule_window(
+    report_type: ScheduleType, start_at: datetime, stop_at: datetime, resolution: int
+) -> None:
     """
     Find all active schedules and schedule celery tasks for
     each of them with a specific ETA (determined by parsing
     the cron schedule for the schedule)
     """
     model_cls = get_scheduler_model(report_type)
+
+    if not model_cls:
+        return None
+
     dbsession = db.create_scoped_session()
     schedules = dbsession.query(model_cls).filter(model_cls.active.is_(True))
 
@@ -415,9 +446,11 @@ def schedule_window(report_type, start_at, stop_at, resolution):
         ):
             schedule_email_report.apply_async(args, eta=eta)
 
+    return None
+
 
 @celery_app.task(name="email_reports.schedule_hourly")
-def schedule_hourly():
+def schedule_hourly() -> None:
     """ Celery beat job meant to be invoked hourly """
 
     if not config["ENABLE_SCHEDULED_EMAIL_REPORTS"]:
@@ -429,5 +462,5 @@ def schedule_hourly():
     # Get the top of the hour
     start_at = datetime.now(tzlocal()).replace(microsecond=0, second=0, minute=0)
     stop_at = start_at + timedelta(seconds=3600)
-    schedule_window(ScheduleType.dashboard.value, start_at, stop_at, resolution)
-    schedule_window(ScheduleType.slice.value, start_at, stop_at, resolution)
+    schedule_window(ScheduleType.dashboard, start_at, stop_at, resolution)
+    schedule_window(ScheduleType.slice, start_at, stop_at, resolution)
