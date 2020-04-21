7cefc89c64d58c31709e33a0e9a79c857a771ad5
Lily Kuang <lily@preset.io>
2020-04-21 11:57:42 -0700

    [charts] feat: add statsd to charts api (#9571)
    * add statsd to charts api
    
    * update test for charts api
    
    * [charts] add statsd asserts wrapper
    
    * [charts] update api test
    
    * removed white space

--- superset/charts/api.py
+++ superset/charts/api.py
@@ -56,7 +56,11 @@ from superset.models.slice import Slice
 from superset.tasks.thumbnails import cache_chart_thumbnail
 from superset.utils.core import json_int_dttm_ser
 from superset.utils.screenshots import ChartScreenshot
-from superset.views.base_api import BaseSupersetModelRestApi, RelatedFieldFilter
+from superset.views.base_api import (
+    BaseSupersetModelRestApi,
+    RelatedFieldFilter,
+    statsd_metrics,
+)
 from superset.views.filters import FilterRelatedOwners
 
 logger = logging.getLogger(__name__)
@@ -147,6 +151,7 @@ class ChartRestApi(BaseSupersetModelRestApi):
     @expose("/", methods=["POST"])
     @protect()
     @safe
+    @statsd_metrics
     def post(self) -> Response:
         """Creates a new Chart
         ---
@@ -199,6 +204,7 @@ class ChartRestApi(BaseSupersetModelRestApi):
     @expose("/<pk>", methods=["PUT"])
     @protect()
     @safe
+    @statsd_metrics
     def put(  # pylint: disable=too-many-return-statements, arguments-differ
         self, pk: int
     ) -> Response:
@@ -266,6 +272,7 @@ class ChartRestApi(BaseSupersetModelRestApi):
     @expose("/<pk>", methods=["DELETE"])
     @protect()
     @safe
+    @statsd_metrics
     def delete(self, pk: int) -> Response:  # pylint: disable=arguments-differ
         """Deletes a Chart
         ---
@@ -312,6 +319,7 @@ class ChartRestApi(BaseSupersetModelRestApi):
     @expose("/", methods=["DELETE"])
     @protect()
     @safe
+    @statsd_metrics
     @rison(get_delete_ids_schema)
     def bulk_delete(
         self, **kwargs: Any
@@ -373,6 +381,7 @@ class ChartRestApi(BaseSupersetModelRestApi):
     @event_logger.log_this
     @protect()
     @safe
+    @statsd_metrics
     def data(self) -> Response:
         """
         Takes a query context constructed in the client and returns payload
@@ -429,6 +438,7 @@ class ChartRestApi(BaseSupersetModelRestApi):
     @protect()
     @rison(thumbnail_query_schema)
     @safe
+    @statsd_metrics
     def thumbnail(
         self, pk: int, digest: str, **kwargs: Dict[str, bool]
     ) -> WerkzeugResponse:
