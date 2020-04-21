f5c24dff9f0e262f95e8f8c2397189434b176760
Lily Kuang <lily@preset.io>
2020-04-20 17:54:10 -0700

    improve history for query params

--- superset-frontend/src/welcome/index.jsx
+++ superset-frontend/src/welcome/index.jsx
@@ -19,5 +19,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Route } from 'react-router-dom';
+import { QueryParamProvider } from 'use-query-params';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+
+ReactDOM.render(
+  <QueryParamProvider ReactRouterRoute={Route}>
+    <App />
+  </QueryParamProvider>, 
+  document.getElementById('app')
+);
