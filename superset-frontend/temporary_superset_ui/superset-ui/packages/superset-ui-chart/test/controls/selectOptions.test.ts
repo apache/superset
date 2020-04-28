c834d93e912966df30557a81686dea7c579fd43f
David Aaron Suddjian <1858430+suddjian@users.noreply.github.com>
2020-04-27 17:37:25 -0700

    feat(chart): add shared plugin controls utilities (#389)
    * add shared controls utilities
    
    * accidentally yarn lock
    
    * bring in date formatting fix
    
    * turn off no-plusplus rule
    
    * address PR feedback, reorganize modules a bit
    
    * turn off no-plusplus
    
    * bring in new external changes
    
    * superset-ui packages should be peer dependencies
    
    * add tests for ported in code

---
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-chart/test/controls/selectOptions.test.ts
@@ -0,0 +1,26 @@
+import { formatSelectOptions, formatSelectOptionsForRange } from '../../src';
+
+describe('formatSelectOptions', () => {
+  it('formats an array of options', () => {
+    expect(formatSelectOptions([1, 5, 10, 25, 50, 'unlimited'])).toEqual([
+      [1, '1'],
+      [5, '5'],
+      [10, '10'],
+      [25, '25'],
+      [50, '50'],
+      ['unlimited', 'unlimited'],
+    ]);
+  });
+});
+
+describe('formatSelectOptionsForRange', () => {
+  it('generates select options from a range', () => {
+    expect(formatSelectOptionsForRange(1, 5)).toEqual([
+      [1, '1'],
+      [2, '2'],
+      [3, '3'],
+      [4, '4'],
+      [5, '5'],
+    ]);
+  });
+});
