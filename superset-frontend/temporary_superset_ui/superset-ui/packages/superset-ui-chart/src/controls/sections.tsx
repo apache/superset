fb4664235bf3bb5dda2b332e59a74fbf8c9f702e
David Aaron Suddjian <1858430+suddjian@users.noreply.github.com>
2020-04-27 17:37:25 -0700

    feat(chart): add shared plugin controls utilities (#389)
    * add shared controls utilities
    
    * accidentally yarn lock
    
    * bring in date formatting fix
    
    * turn off no-plusplus rule
    
    * address PR feedback, reorganize modules a bit
    
    * turn off no-plusplus
    
    * bring in new external changes
    
    * superset-ui packages should be peer dependencies
    
    * add tests for ported in code

---
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-chart/src/controls/sections.tsx
@@ -0,0 +1,109 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import { t } from '@superset-ui/translation';
+
+// A few standard controls sections that are used internally.
+// Not recommended for use in third-party plugins.
+
+export const druidTimeSeries = {
+  label: t('Time'),
+  expanded: true,
+  description: t('Time related form attributes'),
+  controlSetRows: [['time_range']],
+};
+
+export const datasourceAndVizType = {
+  label: t('Datasource & Chart Type'),
+  expanded: true,
+  controlSetRows: [
+    ['datasource'],
+    ['viz_type'],
+    [
+      {
+        name: 'slice_id',
+        config: {
+          type: 'HiddenControl',
+          label: t('Chart ID'),
+          hidden: true,
+          description: t('The id of the active chart'),
+        },
+      },
+      {
+        name: 'cache_timeout',
+        config: {
+          type: 'HiddenControl',
+          label: t('Cache Timeout (seconds)'),
+          hidden: true,
+          description: t('The number of seconds before expiring the cache'),
+        },
+      },
+      {
+        name: 'url_params',
+        config: {
+          type: 'HiddenControl',
+          label: t('URL Parameters'),
+          hidden: true,
+          description: t('Extra parameters for use in jinja templated queries'),
+        },
+      },
+      {
+        name: 'time_range_endpoints',
+        config: {
+          type: 'HiddenControl',
+          label: t('Time range endpoints'),
+          hidden: true,
+          description: t('Time range endpoints (SIP-15)'),
+        },
+      },
+    ],
+  ],
+};
+
+export const colorScheme = {
+  label: t('Color Scheme'),
+  controlSetRows: [['color_scheme', 'label_colors']],
+};
+
+export const sqlaTimeSeries = {
+  label: t('Time'),
+  description: t('Time related form attributes'),
+  expanded: true,
+  controlSetRows: [['granularity_sqla'], ['time_range']],
+};
+
+export const annotations = {
+  label: t('Annotations and Layers'),
+  tabOverride: 'data',
+  expanded: true,
+  controlSetRows: [
+    [
+      {
+        name: 'annotation_layers',
+        config: {
+          type: 'AnnotationLayerControl',
+          label: '',
+          default: [],
+          description: 'Annotation Layers',
+          renderTrigger: true,
+          tabOverride: 'data',
+        },
+      },
+    ],
+  ],
+};
