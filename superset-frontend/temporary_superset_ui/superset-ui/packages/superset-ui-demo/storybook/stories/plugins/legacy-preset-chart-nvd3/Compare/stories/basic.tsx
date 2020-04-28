742a4a16ea0fe42314bdafbafed7f431ee371a80
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-04-28 15:43:48 -0700

    feat(preset-chart-nvd3): migration from plugins repo (#399)

---
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-demo/storybook/stories/plugins/legacy-preset-chart-nvd3/Compare/stories/basic.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { SuperChart } from '@superset-ui/chart';
+import dummyDatasource from '../../../../../shared/dummyDatasource';
+import data from '../data';
+
+export const basic = () => (
+  <SuperChart
+    chartType="compare"
+    width={400}
+    height={400}
+    datasource={dummyDatasource}
+    queryData={{ data }}
+    formData={{
+      bottomMargin: 'auto',
+      colorScheme: 'd3Category10',
+      contribution: false,
+      leftMargin: 'auto',
+      vizType: 'compare',
+      xAxisFormat: 'smart_date',
+      xAxisLabel: '',
+      xAxisShowminmax: false,
+      xTicksLayout: 'auto',
+      yAxisBounds: [null, null],
+      yAxisFormat: '.3s',
+      yAxisLabel: '',
+      yAxisShowminmax: false,
+      yLogscale: false,
+    }}
+  />
+);
