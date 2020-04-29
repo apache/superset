86e11b29a743b6438da6a736a5a1a0d57ad26744
John Bodley <4567245+john-bodley@users.noreply.github.com>
2020-04-29 12:16:47 -0700

    [debug] Debugging caching issue (#9665)
    Co-authored-by: John Bodley <john.bodley@airbnb.com>

--- tests/jinja_context_tests.py
+++ tests/jinja_context_tests.py
@@ -15,76 +15,85 @@
 # specific language governing permissions and limitations
 # under the License.
 import json
-from unittest import mock
 
-from superset.jinja_context import filter_values
+import tests.test_app
+from superset import app
+from superset.jinja_context import ExtraCache, filter_values
 from tests.base_tests import SupersetTestCase
 
 
 class Jinja2ContextTests(SupersetTestCase):
     def test_filter_values_default(self) -> None:
-        request = mock.MagicMock()
-        request.form = {}
-
-        with mock.patch("superset.jinja_context.request", request):
+        with app.test_request_context():
             self.assertEquals(filter_values("name", "foo"), ["foo"])
 
     def test_filter_values_no_default(self) -> None:
-        request = mock.MagicMock()
-        request.form = {}
-
-        with mock.patch("superset.jinja_context.request", request):
+        with app.test_request_context():
             self.assertEquals(filter_values("name"), [])
 
     def test_filter_values_adhoc_filters(self) -> None:
-        request = mock.MagicMock()
-
-        request.form = {
-            "form_data": json.dumps(
-                {
-                    "adhoc_filters": [
-                        {
-                            "clause": "WHERE",
-                            "comparator": "foo",
-                            "expressionType": "SIMPLE",
-                            "operator": "in",
-                            "subject": "name",
-                        }
-                    ],
-                }
-            )
-        }
-
-        with mock.patch("superset.jinja_context.request", request):
+        with app.test_request_context(
+            data={
+                "form_data": json.dumps(
+                    {
+                        "adhoc_filters": [
+                            {
+                                "clause": "WHERE",
+                                "comparator": "foo",
+                                "expressionType": "SIMPLE",
+                                "operator": "in",
+                                "subject": "name",
+                            }
+                        ],
+                    }
+                )
+            }
+        ):
             self.assertEquals(filter_values("name"), ["foo"])
 
-        request.form = {
-            "form_data": json.dumps(
-                {
-                    "adhoc_filters": [
-                        {
-                            "clause": "WHERE",
-                            "comparator": ["foo", "bar"],
-                            "expressionType": "SIMPLE",
-                            "operator": "in",
-                            "subject": "name",
-                        }
-                    ],
-                }
-            )
-        }
-
-        with mock.patch("superset.jinja_context.request", request):
+        with app.test_request_context(
+            data={
+                "form_data": json.dumps(
+                    {
+                        "adhoc_filters": [
+                            {
+                                "clause": "WHERE",
+                                "comparator": ["foo", "bar"],
+                                "expressionType": "SIMPLE",
+                                "operator": "in",
+                                "subject": "name",
+                            }
+                        ],
+                    }
+                )
+            }
+        ):
             self.assertEquals(filter_values("name"), ["foo", "bar"])
 
     def test_filter_values_extra_filters(self) -> None:
-        request = mock.MagicMock()
-
-        request.form = {
-            "form_data": json.dumps(
-                {"extra_filters": [{"col": "name", "op": "in", "val": "foo"}]}
-            )
-        }
-
-        with mock.patch("superset.jinja_context.request", request):
+        with app.test_request_context(
+            data={
+                "form_data": json.dumps(
+                    {"extra_filters": [{"col": "name", "op": "in", "val": "foo"}]}
+                )
+            }
+        ):
             self.assertEquals(filter_values("name"), ["foo"])
+
+    def test_url_param_default(self) -> None:
+        with app.test_request_context():
+            self.assertEquals(ExtraCache().url_param("foo", "bar"), "bar")
+
+    def test_url_param_no_default(self) -> None:
+        with app.test_request_context():
+            self.assertEquals(ExtraCache().url_param("foo"), None)
+
+    def test_url_param_query(self) -> None:
+        with app.test_request_context(query_string={"foo": "bar"}):
+            self.assertEquals(ExtraCache().url_param("foo"), "bar")
+
+    def test_url_param_form_data(self) -> None:
+        with app.test_request_context(
+            query_string={"form_data": json.dumps({"url_params": {"foo": "bar"}})}
+        ):
+            self.assertEquals(ExtraCache().url_param("foo"), "bar")
