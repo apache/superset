44af34e32f5c6abc3ada696fdb049b2fe38b6ae4
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-04-29 12:23:11 -0700

    chore: delete unused files (#412)
    * chore: delete unused files
    
    * chore: delete files
    
    * chore: move files back

--- superset-frontend/temporary_superset_ui/superset-ui/temporary-plugins/superset-ui-plugins-demo/storybook/stories/Bullet/index.js
+++
@@ -1,8 +0,0 @@
-import { BulletChartPlugin } from '../../../../../superset-ui-legacy-preset-chart-nvd3';
-import Stories from './Stories';
-
-new BulletChartPlugin().configure({ key: 'bullet' }).register();
-
-export default {
-  examples: [...Stories],
-};
--- superset-frontend/temporary_superset_ui/superset-ui/temporary-plugins/superset-ui-plugins-demo/storybook/stories/Compare/index.js
+++
@@ -1,8 +0,0 @@
-import { CompareChartPlugin } from '../../../../../superset-ui-legacy-preset-chart-nvd3';
-import Stories from './Stories';
-
-new CompareChartPlugin().configure({ key: 'compare' }).register();
-
-export default {
-  examples: [...Stories],
-};
