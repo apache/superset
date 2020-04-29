c21ad5cee42d7bead3fe15ad962161bd6bb83ee1
ʈᵃᵢ <tdupreetan@gmail.com>
2020-04-29 10:42:38 -0700

    fix: export superset theme props (#391)
    * fix: export superset theme props
    
    * add spec

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-style/test/index.test.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-style/test/index.test.ts
@@ -1,4 +1,4 @@
-import styled, { supersetTheme } from '../src';
+import styled, { supersetTheme, SupersetThemeProps } from '../src';
 
 describe('@superset-ui/style package', () => {
   it('exports a theme', () => {
@@ -7,5 +7,12 @@ describe('@superset-ui/style package', () => {
 
   it('exports styled component templater', () => {
     expect(typeof styled.div).toBe('function');
+  });
+
+  it('exports SupersetThemeProps', () => {
+    const props: SupersetThemeProps = {
+      theme: supersetTheme,
+    };
+    expect(typeof props).toBe('object');
   });
 });
