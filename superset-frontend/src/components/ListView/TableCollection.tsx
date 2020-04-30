74769bf66ae2d461bbec6b32696b8e95fd6ebe9f
ʈᵃᵢ <tdupreetan@gmail.com>
2020-04-29 21:54:07 -0700

    [Chart & Dashboard] improve listview filter ui and add expandable list support (#9651)
    * better styling for select filters and search input
    
    * add emotion babel plugin
    
    * add spec for search input
    
    * fix async select, add beeter error messages
    
    * add error boundary around views
    
    * cleanup and lint
    
    * add expandable list to dashboards
    
    * fix emotion babel config
    
    * fix specs
    
    * remove commented out code
    
    * add selected style
    
    * DRY up styles
    
    * add icons
    
    * fix type
    
    * add apache license
    
    * fix svgs
    
    * fix ExpandableList type
    
    * reduce custom css
    
    * lint

--- superset-frontend/src/components/ListView/TableCollection.tsx
+++ superset-frontend/src/components/ListView/TableCollection.tsx
@@ -69,11 +69,13 @@ export default function TableCollection({
       <tbody {...getTableBodyProps()}>
         {rows.map(row => {
           prepareRow(row);
-          const loadingProps = loading ? { className: 'table-row-loader' } : {};
           return (
             <tr
               {...row.getRowProps()}
-              {...loadingProps}
+              className={cx({
+                'table-row-loader': loading,
+                'table-row-selected': row.isSelected,
+              })}
               onMouseEnter={() => row.setState && row.setState({ hover: true })}
               onMouseLeave={() =>
                 row.setState && row.setState({ hover: false })
