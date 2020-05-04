4802f82a30d5b43176008dac1ff0fe904fac9485
ʈᵃᵢ <tdupreetan@gmail.com>
2020-05-04 09:47:29 -0700

    [sqllab] fix, strip comments before parsing statements (#9692)

--- superset/sql_parse.py
+++ superset/sql_parse.py
@@ -89,7 +89,10 @@ class ParsedQuery:
         self._limit: Optional[int] = None
 
         logger.debug("Parsing with sqlparse statement: %s", self.sql)
-        self._parsed = sqlparse.parse(self.stripped())
+        self._parsed = sqlparse.parse(
+            sqlparse.format(self.stripped(), strip_comments=True)
+        )
+
         for statement in self._parsed:
             self._limit = _extract_limit_from_query(statement)
 
