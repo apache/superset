4e766b51ca032cbb7f422c88c406a8a9de2a9501
Ville Brofeldt <33317356+villebro@users.noreply.github.com>
2020-05-06 10:46:58 +0300

    fix(legacy-plugin-world-map): set useLegacyApi to true by default (#444)

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/legacy-plugin-chart-world-map/src/index.js
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/legacy-plugin-chart-world-map/src/index.js
@@ -26,6 +26,7 @@ const metadata = new ChartMetadata({
   description: '',
   name: t('World Map'),
   thumbnail,
+  useLegacyApi: true,
 });
 
 export default class WorldMapChartPlugin extends ChartPlugin {
