2523d06a69de0540d63fa1c5e11d7faf7e2e1702
Ville Brofeldt <33317356+villebro@users.noreply.github.com>
2020-05-06 19:32:29 +0300

    feat(query): Add force to QueryContext (#445)
    * Add force to QueryContext
    
    * fix tests
    
    * fix test

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-query/test/buildQueryContext.test.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-query/test/buildQueryContext.test.ts
@@ -1,7 +1,7 @@
 import { buildQueryContext } from '../src';
 
 describe('buildQueryContext', () => {
-  it('should build datasource for table sources', () => {
+  it('should build datasource for table sources and default force to false', () => {
     const queryContext = buildQueryContext({
       datasource: '5__table',
       granularity_sqla: 'ds',
@@ -9,15 +9,18 @@ describe('buildQueryContext', () => {
     });
     expect(queryContext.datasource.id).toBe(5);
     expect(queryContext.datasource.type).toBe('table');
+    expect(queryContext.force).toBe(false);
   });
 
-  it('should build datasource for druid sources', () => {
+  it('should build datasource for druid sources and set force to true', () => {
     const queryContext = buildQueryContext({
       datasource: '5__druid',
       granularity: 'ds',
       viz_type: 'table',
+      force: true,
     });
     expect(queryContext.datasource.id).toBe(5);
     expect(queryContext.datasource.type).toBe('druid');
+    expect(queryContext.force).toBe(true);
   });
 });
