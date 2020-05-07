e0a198baea9739654d304a358c81fb0387225bbe
Phillip Kelley-Dotson <pkelleydotson@yahoo.com>
2020-05-07 14:01:34 -0700

    feat(control-utils): add infotooltipwithtrigger (#442)
    * PR to merge packges for controls migration
    
    * fix: add bootstrap
    
    * fix: change tooltip to tsx
    
    * fix: types
    
    * fix: tsx error
    
    * appease linter, enhance a11y
    
    * addressing PR feedback
    
    * tweaking bootstrap types
    
    * fix test
    
    * test enter key
    
    * moar tests
    
    * code > key
    
    * ugh fine
    
    Co-authored-by: Phillip Kelley-Dotson <pkd@pkd.lan>
    Co-authored-by: David Aaron Suddjian <aasuddjian@gmail.com>

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-control-utils/src/index.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-control-utils/src/index.ts
@@ -3,3 +3,4 @@ import * as sectionModules from './sections';
 export const sections = sectionModules;
 export { D3_FORMAT_DOCS, D3_FORMAT_OPTIONS, D3_TIME_FORMAT_OPTIONS } from './D3Formatting';
 export { formatSelectOptions, formatSelectOptionsForRange } from './selectOptions';
+export { default as InfoTooltipWithTrigger } from './InfoTooltipWithTrigger';
