5b24c10674fc159a1a5f4a7f07828679bb4ce440
Ville Brofeldt <ville.v.brofeldt@gmail.com>
2020-05-12 18:07:48 +0300

    make cachelib an explicit requirement

--- setup.py
+++ setup.py
@@ -69,6 +69,7 @@ setup(
     install_requires=[
         "backoff>=1.8.0",
         "bleach>=3.0.2, <4.0.0",
+        "cachelib>=0.1,<0.2",
         "celery>=4.3.0, <5.0.0, !=4.4.1",
         "click<8",
         "colorama",
