c7a94bb8a46e9b506c14b5e58db7d2f3737a841f
Ville Brofeldt <33317356+villebro@users.noreply.github.com>
2020-05-15 19:51:17 +0300

    feat(query): remove redundant metric label truncation (#492)
    * feat: remove redundant metric label truncation
    
    * fix lint

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-query/src/convertMetric.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-query/src/convertMetric.ts
@@ -2,19 +2,11 @@ import { QueryFormDataMetric } from './types/QueryFormData';
 import { QueryObjectMetric } from './types/Query';
 import { AdhocMetric } from './types/Metric';
 
-export const LABEL_MAX_LENGTH = 43;
-
 function getDefaultLabel(metric: AdhocMetric) {
-  let label: string;
   if (metric.expressionType === 'SIMPLE') {
-    label = `${metric.aggregate}(${metric.column.columnName})`;
-  } else {
-    label = metric.sqlExpression;
+    return `${metric.aggregate}(${metric.column.columnName})`;
   }
-
-  return label.length <= LABEL_MAX_LENGTH
-    ? label
-    : `${label.slice(0, Math.max(0, LABEL_MAX_LENGTH - 3))}...`;
+  return metric.sqlExpression;
 }
 
 export default function convertMetric(metric: QueryFormDataMetric): QueryObjectMetric {
@@ -24,9 +16,6 @@ export default function convertMetric(metric: QueryFormDataMetric): QueryObjectM
       label: metric,
     };
   } else {
-    // Note we further sanitize the metric label for BigQuery datasources
-    // TODO: move this logic to the client once client has more info on the
-    // the datasource
     const label = metric.label ?? getDefaultLabel(metric);
     formattedMetric = {
       ...metric,
