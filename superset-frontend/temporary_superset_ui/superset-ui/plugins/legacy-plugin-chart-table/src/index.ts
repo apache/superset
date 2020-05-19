68949ce1264d9723bf616c9968ba4dada5fa9653
Phillip Kelley-Dotson <pkelleydotson@yahoo.com>
2020-05-19 14:19:38 -0700

    chore: so 514 tablechart plugin controls migration (#508)
    * add controls to index
    
    * remove consoles
    
    * Update package.json

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/legacy-plugin-chart-table/src/index.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/legacy-plugin-chart-table/src/index.ts
@@ -20,6 +20,7 @@ import { t } from '@superset-ui/translation';
 import { ChartMetadata, ChartPlugin } from '@superset-ui/chart';
 import transformProps from './transformProps';
 import thumbnail from './images/thumbnail.png';
+import controlPanel from './controlPanel';
 
 const metadata = new ChartMetadata({
   canBeAnnotationTypes: ['EVENT', 'INTERVAL'],
@@ -35,6 +36,7 @@ export default class TableChartPlugin extends ChartPlugin {
       loadChart: () => import('./ReactDataTable'),
       metadata,
       transformProps,
+      controlPanel,
     });
   }
 }
