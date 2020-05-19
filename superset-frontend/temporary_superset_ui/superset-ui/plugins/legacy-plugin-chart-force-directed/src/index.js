88d6458515dc97ed7c773154348f6602c307e9a4
David Aaron Suddjian <1858430+suddjian@users.noreply.github.com>
2020-05-19 11:16:02 -0700

    feat(legacy-plugin-chart-force-directed): control panel (#502)

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/legacy-plugin-chart-force-directed/src/index.js
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/legacy-plugin-chart-force-directed/src/index.js
@@ -20,6 +20,7 @@ import { t } from '@superset-ui/translation';
 import { ChartMetadata, ChartPlugin } from '@superset-ui/chart';
 import transformProps from './transformProps';
 import thumbnail from './images/thumbnail.png';
+import controlPanel from './controlPanel';
 
 const metadata = new ChartMetadata({
   credits: ['http://bl.ocks.org/d3noob/5141278'],
@@ -35,6 +36,7 @@ export default class ForceDirectedChartPlugin extends ChartPlugin {
       loadChart: () => import('./ReactForceDirected.js'),
       metadata,
       transformProps,
+      controlPanel,
     });
   }
 }
