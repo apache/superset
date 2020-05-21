f09b20c710a3a45dbead51fbd6565776e8665d12
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-05-21 13:54:35 -0700

    feat(legacy-plugin-chart-event-flow): migrate package (#514)

--- superset-frontend/temporary_superset_ui/superset-ui/temporary-plugins/superset-ui-plugins-demo/storybook/stories/legacy-plugin-chart-event-flow/index.js
+++
@@ -1,8 +0,0 @@
-import EventFlowChartPlugin from '../../../../superset-ui-legacy-plugin-chart-event-flow';
-import Stories from './Stories';
-
-new EventFlowChartPlugin().configure({ key: 'event-flow' }).register();
-
-export default {
-  examples: [...Stories],
-};
