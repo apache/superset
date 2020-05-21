c2c9329c3004e0d382b9a561bff2902e1a547963
Phillip Kelley-Dotson <pkelleydotson@yahoo.com>
2020-05-21 12:11:57 -0700

    chore: paired t test chart plugin control migration (#515)
    * add controls to chart
    
    * update package

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/legacy-plugin-chart-paired-t-test/src/index.js
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/legacy-plugin-chart-paired-t-test/src/index.js
@@ -20,6 +20,7 @@ import { t } from '@superset-ui/translation';
 import { ChartMetadata, ChartPlugin } from '@superset-ui/chart';
 import transformProps from './transformProps';
 import thumbnail from './images/thumbnail.png';
+import controlPanel from './controlPanel.ts';
 
 const metadata = new ChartMetadata({
   description: '',
@@ -34,6 +35,7 @@ export default class PairedTTestChartPlugin extends ChartPlugin {
       loadChart: () => import('./PairedTTest'),
       metadata,
       transformProps,
+      controlPanel,
     });
   }
 }
