a337d997ec3e78b5b6c8e1565e00f6300cd1eee9
Ville Brofeldt <33317356+villebro@users.noreply.github.com>
2020-05-27 19:34:40 +0300

    feat: bump sinon to latest (#9926)

--- superset-frontend/spec/javascripts/middleware/logger_spec.js
+++ superset-frontend/spec/javascripts/middleware/logger_spec.js
@@ -48,7 +48,7 @@ describe('logger middleware', () => {
     postStub = sinon.stub(SupersetClient, 'post');
   });
   afterEach(() => {
-    next.reset();
+    next.resetHistory();
     postStub.restore();
   });
 
