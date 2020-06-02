2ad1ccaaf3c3cbc4cf03e5f5135274b07584dde8
Xiang Fu <fx19880617@gmail.com>
2020-06-01 23:15:59 -0700

    Change DATETIMECONVERT argument to use single quote for literals

--- superset/db_engine_specs/pinot.py
+++ superset/db_engine_specs/pinot.py
@@ -86,7 +86,7 @@ class PinotEngineSpec(BaseEngineSpec):  # pylint: disable=abstract-method
         else:
             return TimestampExpression(f"{{col}}", col)
         # In pinot the output is a string since there is no timestamp column like pg
-        time_expr = f'DATETIMECONVERT({{col}}, "{tf}", "{tf}", "{granularity}")'
+        time_expr = f"DATETIMECONVERT({{col}}, '{tf}', '{tf}', '{granularity}')"
         return TimestampExpression(time_expr, col)
 
     @classmethod
