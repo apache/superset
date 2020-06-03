88d7206a9a059f488eb6eed0723d6e69bf55bc93
axelet <axelet3@gmail.com>
2020-06-03 16:40:26 +0300

    Support downgrading from the new-fashioned formatted records

--- superset/migrations/versions/e557699a813e_add_tables_relation_to_row_level_.py
+++ superset/migrations/versions/e557699a813e_add_tables_relation_to_row_level_.py
@@ -82,13 +82,43 @@ def downgrade():
 
     rlsf = sa.Table("row_level_security_filters", metadata, autoload=True)
     rls_filter_tables = sa.Table("rls_filter_tables", metadata, autoload=True)
+    rls_filter_roles = sa.Table("rls_filter_roles", metadata, autoload=True)
 
-    move_table_ids = rlsf.update().values(
-        table_id=sa.select([rls_filter_tables.c.table_id]).where(
-            rlsf.c.id == rls_filter_tables.c.rls_filter_id
-        )
+    filter_tables = sa.select([rls_filter_tables.c.rls_filter_id]).group_by(
+        rls_filter_tables.c.rls_filter_id
     )
-    bind.execute(move_table_ids)
+
+    for row in bind.execute(filter_tables):
+        filters_copy_ids = []
+        filter_query = rlsf.select().where(rlsf.c.id == row["rls_filter_id"])
+        filter_params = dict(bind.execute(filter_query).fetchone())
+        origin_id = filter_params.pop("id", None)
+        table_ids = bind.execute(
+            sa.select([rls_filter_tables.c.table_id]).where(
+                rls_filter_tables.c.rls_filter_id == row["rls_filter_id"]
+            )
+        ).fetchall()
+        filter_params["table_id"] = table_ids.pop(0)[0]
+        move_table_id = (
+            rlsf.update().where(rlsf.c.id == origin_id).values(filter_params)
+        )
+        bind.execute(move_table_id)
+        for table_id in table_ids:
+            filter_params["table_id"] = table_id[0]
+            copy_filter = rlsf.insert().values(filter_params)
+            copy_id = bind.execute(copy_filter).inserted_primary_key[0]
+            filters_copy_ids.append(copy_id)
+
+        roles_query = rls_filter_roles.select().where(
+            rls_filter_roles.c.rls_filter_id == origin_id
+        )
+        for role in bind.execute(roles_query):
+            for copy_id in filters_copy_ids:
+                role_filter = rls_filter_roles.insert().values(
+                    role_id=role["role_id"], rls_filter_id=copy_id
+                )
+                bind.execute(role_filter)
+        filters_copy_ids.clear()
 
     op.alter_column("row_level_security_filters", "table_id", nullable=False)
     op.drop_table("rls_filter_tables")
