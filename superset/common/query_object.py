apache/superset

commit 062ad3d7cec392f93172a890be302435d4fa03a1

style: enforcing mypy typing for connectors (#9824)
Co-authored-by: John Bodley <john.bodley@airbnb.com>

superset/connectors/sqla/models.py
@@ -54,10 +54,15 @@ from superset.connectors.base.models import BaseColumn, BaseDatasource, BaseMetr
 from superset.constants import NULL_STRING
 from superset.db_engine_specs.base import TimestampExpression
 from superset.exceptions import DatabaseNotFound
-from superset.jinja_context import ExtraCache, get_template_processor
+from superset.jinja_context import (
+    BaseTemplateProcessor,
+    ExtraCache,
+    get_template_processor,
+)
 from superset.models.annotations import Annotation
 from superset.models.core import Database
 from superset.models.helpers import AuditMixinNullable, QueryResult
+from superset.typing import Metric, QueryObjectDict
 from superset.utils import core as utils, import_datasource
 
 config = app.config
@@ -86,7 +91,7 @@ class AnnotationDatasource(BaseDatasource):
     cache_timeout = 0
     changed_on = None
 
-    def query(self, query_obj: Dict[str, Any]) -> QueryResult:
+    def query(self, query_obj: QueryObjectDict) -> QueryResult:
         error_message = None
         qry = db.session.query(Annotation)
         qry = qry.filter(Annotation.layer_id == query_obj["filter"][0]["val"])
@@ -110,10 +115,10 @@ class AnnotationDatasource(BaseDatasource):
             error_message=error_message,
         )
 
-    def get_query_str(self, query_obj):
+    def get_query_str(self, query_obj: QueryObjectDict) -> str:
         raise NotImplementedError()
 
-    def values_for_column(self, column_name, limit=10000):
+    def values_for_column(self, column_name: str, limit: int = 10000) -> List[Any]:
         raise NotImplementedError()
 
 
@@ -239,8 +244,8 @@ class TableColumn(Model, BaseColumn):
         return self.table.make_sqla_column_compatible(time_expr, label)
 
     @classmethod
-    def import_obj(cls, i_column):
-        def lookup_obj(lookup_column):
+    def import_obj(cls, i_column: "TableColumn") -> "TableColumn":
+        def lookup_obj(lookup_column: TableColumn) -> TableColumn:
             return (
                 db.session.query(TableColumn)
                 .filter(
@@ -343,8 +348,8 @@ class SqlMetric(Model, BaseMetric):
         return self.perm
 
     @classmethod
-    def import_obj(cls, i_metric):
-        def lookup_obj(lookup_metric):
+    def import_obj(cls, i_metric: "SqlMetric") -> "SqlMetric":
+        def lookup_obj(lookup_metric: SqlMetric) -> SqlMetric:
             return (
                 db.session.query(SqlMetric)
                 .filter(
@@ -442,7 +447,7 @@ class SqlaTable(Model, BaseDatasource):
         sqla_col._df_label_expected = label_expected
         return sqla_col
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.name
 
     @property
@@ -521,14 +526,14 @@ class SqlaTable(Model, BaseDatasource):
         )
 
     @property
-    def dttm_cols(self) -> List:
+    def dttm_cols(self) -> List[str]:
         l = [c.column_name for c in self.columns if c.is_dttm]
         if self.main_dttm_col and self.main_dttm_col not in l:
             l.append(self.main_dttm_col)
         return l
 
     @property
-    def num_cols(self) -> List:
+    def num_cols(self) -> List[str]:
         return [c.column_name for c in self.columns if c.is_numeric]
 
     @property
@@ -550,7 +555,7 @@ class SqlaTable(Model, BaseDatasource):
     def sql_url(self) -> str:
         return self.database.sql_url + "?table_name=" + str(self.table_name)
 
-    def external_metadata(self):
+    def external_metadata(self) -> List[Dict[str, str]]:
         cols = self.database.get_columns(self.table_name, schema=self.schema)
         for col in cols:
             try:
@@ -567,7 +572,7 @@ class SqlaTable(Model, BaseDatasource):
         }
 
     @property
-    def select_star(self) -> str:
+    def select_star(self) -> Optional[str]:
         # show_cols and latest_partition set to false to avoid
         # the expensive cost of inspecting the DB
         return self.database.select_star(
@@ -589,7 +594,7 @@ class SqlaTable(Model, BaseDatasource):
             d["is_sqllab_view"] = self.is_sqllab_view
         return d
 
-    def values_for_column(self, column_name: str, limit: int = 10000) -> List:
+    def values_for_column(self, column_name: str, limit: int = 10000) -> List[Any]:
         """Runs query against sqla to retrieve some
         sample values for the given column.
         """
@@ -626,10 +631,10 @@ class SqlaTable(Model, BaseDatasource):
             sql = SQL_QUERY_MUTATOR(sql, username, security_manager, self.database)
         return sql
 
-    def get_template_processor(self, **kwargs):
+    def get_template_processor(self, **kwargs: Any) -> BaseTemplateProcessor:
         return get_template_processor(table=self, database=self.database, **kwargs)
 
-    def get_query_str_extended(self, query_obj: Dict[str, Any]) -> QueryStringExtended:
+    def get_query_str_extended(self, query_obj: QueryObjectDict) -> QueryStringExtended:
         sqlaq = self.get_sqla_query(**query_obj)
         sql = self.database.compile_sqla_query(sqlaq.sqla_query)
         logger.info(sql)
@@ -639,18 +644,20 @@ class SqlaTable(Model, BaseDatasource):
             labels_expected=sqlaq.labels_expected, sql=sql, prequeries=sqlaq.prequeries
         )
 
-    def get_query_str(self, query_obj: Dict[str, Any]) -> str:
+    def get_query_str(self, query_obj: QueryObjectDict) -> str:
         query_str_ext = self.get_query_str_extended(query_obj)
         all_queries = query_str_ext.prequeries + [query_str_ext.sql]
         return ";\n\n".join(all_queries) + ";"
 
-    def get_sqla_table(self):
+    def get_sqla_table(self) -> table:
         tbl = table(self.table_name)
         if self.schema:
             tbl.schema = self.schema
         return tbl
 
-    def get_from_clause(self, template_processor=None):
+    def get_from_clause(
+        self, template_processor: Optional[BaseTemplateProcessor] = None
+    ) -> Union[table, TextAsFrom]:
         # Supporting arbitrary SQL statements in place of tables
         if self.sql:
             from_sql = self.sql
@@ -687,7 +694,9 @@ class SqlaTable(Model, BaseDatasource):
 
         return self.make_sqla_column_compatible(sqla_metric, label)
 
-    def _get_sqla_row_level_filters(self, template_processor) -> List[str]:
+    def _get_sqla_row_level_filters(
+        self, template_processor: BaseTemplateProcessor
+    ) -> List[str]:
         """
         Return the appropriate row level security filters for this table and the current user.
 
@@ -702,22 +711,22 @@ class SqlaTable(Model, BaseDatasource):
 
     def get_sqla_query(  # sqla
         self,
-        metrics,
-        granularity,
-        from_dttm,
-        to_dttm,
-        columns=None,
-        groupby=None,
-        filter=None,
-        is_timeseries=True,
-        timeseries_limit=15,
-        timeseries_limit_metric=None,
-        row_limit=None,
-        inner_from_dttm=None,
-        inner_to_dttm=None,
-        orderby=None,
-        extras=None,
-        order_desc=True,
+        metrics: List[Metric],
+        granularity: str,
+        from_dttm: datetime,
+        to_dttm: datetime,
+        columns: Optional[List[str]] = None,
+        groupby: Optional[List[str]] = None,
+        filter: Optional[List[Dict[str, Any]]] = None,
+        is_timeseries: bool = True,
+        timeseries_limit: int = 15,
+        timeseries_limit_metric: Optional[Metric] = None,
+        row_limit: Optional[int] = None,
+        inner_from_dttm: Optional[datetime] = None,
+        inner_to_dttm: Optional[datetime] = None,
+        orderby: Optional[List[Tuple[ColumnElement, bool]]] = None,
+        extras: Optional[Dict[str, Any]] = None,
+        order_desc: bool = True,
     ) -> SqlaQuery:
         """Querying any sqla table from this common interface"""
         template_kwargs = {
@@ -765,8 +774,9 @@ class SqlaTable(Model, BaseDatasource):
         metrics_exprs: List[ColumnElement] = []
         for m in metrics:
             if utils.is_adhoc_metric(m):
+                assert isinstance(m, dict)
                 metrics_exprs.append(self.adhoc_metric_to_sqla(m, cols))
-            elif m in metrics_dict:
+            elif isinstance(m, str) and m in metrics_dict:
                 metrics_exprs.append(metrics_dict[m].get_sqla_col())
             else:
                 raise Exception(_("Metric '%(metric)s' does not exist", metric=m))
@@ -781,7 +791,9 @@ class SqlaTable(Model, BaseDatasource):
 
         if (is_sip_38 and metrics and columns) or (not is_sip_38 and groupby):
             # dedup columns while preserving order
-            groupby = list(dict.fromkeys(columns if is_sip_38 else groupby))
+            columns_ = columns if is_sip_38 else groupby
+            assert columns_
+            groupby = list(dict.fromkeys(columns_))
 
             select_exprs = []
             for s in groupby:
@@ -802,6 +814,7 @@ class SqlaTable(Model, BaseDatasource):
                 )
             metrics_exprs = []
 
+        assert extras is not None
         time_range_endpoints = extras.get("time_range_endpoints")
         groupby_exprs_with_timestamp = OrderedDict(groupby_exprs_sans_timestamp.items())
         if granularity:
@@ -845,7 +858,8 @@ class SqlaTable(Model, BaseDatasource):
 
         where_clause_and = []
         having_clause_and: List = []
-        for flt in filter:
+
+        for flt in filter:  # type: ignore
             if not all([flt.get(s) for s in ["col", "op"]]):
                 continue
             col = flt["col"]
@@ -1029,12 +1043,20 @@ class SqlaTable(Model, BaseDatasource):
             prequeries=prequeries,
         )
 
-    def _get_timeseries_orderby(self, timeseries_limit_metric, metrics_dict, cols):
+    def _get_timeseries_orderby(
+        self,
+        timeseries_limit_metric: Metric,
+        metrics_dict: Dict[str, SqlMetric],
+        cols: Dict[str, Column],
+    ) -> Optional[Column]:
         if utils.is_adhoc_metric(timeseries_limit_metric):
+            assert isinstance(timeseries_limit_metric, dict)
             ob = self.adhoc_metric_to_sqla(timeseries_limit_metric, cols)
-        elif timeseries_limit_metric in metrics_dict:
-            timeseries_limit_metric = metrics_dict.get(timeseries_limit_metric)
-            ob = timeseries_limit_metric.get_sqla_col()
+        elif (
+            isinstance(timeseries_limit_metric, str)
+            and timeseries_limit_metric in metrics_dict
+        ):
+            ob = metrics_dict[timeseries_limit_metric].get_sqla_col()
         else:
             raise Exception(
                 _("Metric '%(metric)s' does not exist", metric=timeseries_limit_metric)
@@ -1054,7 +1076,7 @@ class SqlaTable(Model, BaseDatasource):
 
         return or_(*groups)
 
-    def query(self, query_obj: Dict[str, Any]) -> QueryResult:
+    def query(self, query_obj: QueryObjectDict) -> QueryResult:
         qry_start_dttm = datetime.now()
         query_str_ext = self.get_query_str_extended(query_obj)
         sql = query_str_ext.sql
@@ -1101,7 +1123,7 @@ class SqlaTable(Model, BaseDatasource):
     def get_sqla_table_object(self) -> Table:
         return self.database.get_table(self.table_name, schema=self.schema)
 
-    def fetch_metadata(self, commit=True) -> None:
+    def fetch_metadata(self, commit: bool = True) -> None:
         """Fetches the metadata for the table and merges it in"""
         try:
             table = self.get_sqla_table_object()
@@ -1166,7 +1188,9 @@ class SqlaTable(Model, BaseDatasource):
             db.session.commit()
 
     @classmethod
-    def import_obj(cls, i_datasource, import_time=None) -> int:
+    def import_obj(
+        cls, i_datasource: "SqlaTable", import_time: Optional[int] = None
+    ) -> int:
         """Imports the datasource from the object to the database.
 
          Metrics and columns and datasource will be overrided if exists.
@@ -1174,7 +1198,7 @@ class SqlaTable(Model, BaseDatasource):
          superset instances. Audit metadata isn't copies over.
         """
 
-        def lookup_sqlatable(table):
+        def lookup_sqlatable(table: "SqlaTable") -> "SqlaTable":
             return (
                 db.session.query(SqlaTable)
                 .join(Database)
@@ -1186,7 +1210,7 @@ class SqlaTable(Model, BaseDatasource):
                 .first()
             )
 
-        def lookup_database(table):
+        def lookup_database(table: SqlaTable) -> Database:
             try:
                 return (
                     db.session.query(Database)
@@ -1207,7 +1231,11 @@ class SqlaTable(Model, BaseDatasource):
 
     @classmethod
     def query_datasources_by_name(
-        cls, session: Session, database: Database, datasource_name: str, schema=None
+        cls,
+        session: Session,
+        database: Database,
+        datasource_name: str,
+        schema: Optional[str] = None,
     ) -> List["SqlaTable"]:
         query = (
             session.query(cls)
@@ -1219,10 +1247,10 @@ class SqlaTable(Model, BaseDatasource):
         return query.all()
 
     @staticmethod
-    def default_query(qry) -> Query:
+    def default_query(qry: Query) -> Query:
         return qry.filter_by(is_sqllab_view=False)
 
-    def has_extra_cache_key_calls(self, query_obj: Dict[str, Any]) -> bool:
+    def has_extra_cache_key_calls(self, query_obj: QueryObjectDict) -> bool:
         """
         Detects the presence of calls to `ExtraCache` methods in items in query_obj that
         can be templated. If any are present, the query must be evaluated to extract
@@ -1248,7 +1276,7 @@ class SqlaTable(Model, BaseDatasource):
                 return True
         return False
 
-    def get_extra_cache_keys(self, query_obj: Dict[str, Any]) -> List[Hashable]:
+    def get_extra_cache_keys(self, query_obj: QueryObjectDict) -> List[Hashable]:
         """
         The cache key of a SqlaTable needs to consider any keys added by the parent
         class and any keys added via `ExtraCache`.

superset/utils/core.py
@@ -43,10 +43,12 @@ from typing import (
     Any,
     Callable,
     Dict,
+    Iterable,
     Iterator,
     List,
     NamedTuple,
     Optional,
+    Sequence,
     Set,
     Tuple,
     TYPE_CHECKING,
@@ -79,6 +81,7 @@ from superset.exceptions import (
     SupersetException,
     SupersetTimeoutException,
 )
+from superset.typing import Metric
 from superset.utils.dates import datetime_to_epoch, EPOCH
 
 try:
@@ -101,7 +104,7 @@ JS_MAX_INTEGER = 9007199254740991  # Largest int Java Script can handle 2^53-1
 try:
     # Having might not have been imported.
     class DimSelector(Having):
-        def __init__(self, **args):
+        def __init__(self, **args: Any) -> None:
             # Just a hack to prevent any exceptions
             Having.__init__(self, type="equalTo", aggregation=None, value=None)
 
@@ -118,7 +121,7 @@ except NameError:
     pass
 
 
-def flasher(msg, severity=None):
+def flasher(msg: str, severity: str) -> None:
     """Flask's flash if available, logging call if not"""
     try:
         flash(msg, severity)
@@ -235,7 +238,7 @@ def list_minus(l: List, minus: List) -> List:
     return [o for o in l if o not in minus]
 
 
-def parse_human_datetime(s: Optional[str]) -> Optional[datetime]:
+def parse_human_datetime(s: str) -> datetime:
     """
     Returns ``datetime.datetime`` from human readable strings
 
@@ -256,8 +259,6 @@ def parse_human_datetime(s: Optional[str]) -> Optional[datetime]:
     >>> year_ago_1 == year_ago_2
     True
     """
-    if not s:
-        return None
     try:
         dttm = parse(s)
     except Exception:
@@ -564,7 +565,9 @@ def generic_find_uq_constraint_name(table, columns, insp):
             return uq["name"]
 
 
-def get_datasource_full_name(database_name, datasource_name, schema=None):
+def get_datasource_full_name(
+    database_name: str, datasource_name: str, schema: Optional[str] = None
+) -> str:
     if not schema:
         return "[{}].[{}]".format(database_name, datasource_name)
     return "[{}].[{}].[{}]".format(database_name, schema, datasource_name)
@@ -792,7 +795,7 @@ def get_email_address_list(address_string: str) -> List[str]:
     return [x.strip() for x in address_string_list if x.strip()]
 
 
-def choicify(values):
+def choicify(values: Iterable[Any]) -> List[Tuple[Any, Any]]:
     """Takes an iterable and makes an iterable of tuples with it"""
     return [(v, v) for v in values]
 
@@ -967,7 +970,7 @@ def get_example_database() -> "Database":
     return get_or_create_db("examples", db_uri)
 
 
-def is_adhoc_metric(metric) -> bool:
+def is_adhoc_metric(metric: Metric) -> bool:
     return bool(
         isinstance(metric, dict)
         and (
@@ -985,11 +988,11 @@ def is_adhoc_metric(metric) -> bool:
     )
 
 
-def get_metric_name(metric):
-    return metric["label"] if is_adhoc_metric(metric) else metric
+def get_metric_name(metric: Metric) -> str:
+    return metric["label"] if is_adhoc_metric(metric) else metric  # type: ignore
 
 
-def get_metric_names(metrics):
+def get_metric_names(metrics: Sequence[Metric]) -> List[str]:
     return [get_metric_name(metric) for metric in metrics]
 
 

superset/connectors/base/models.py
@@ -20,12 +20,12 @@ from typing import Any, Dict, Hashable, List, Optional, Type
 from flask_appbuilder.security.sqla.models import User
 from sqlalchemy import and_, Boolean, Column, Integer, String, Text
 from sqlalchemy.ext.declarative import declared_attr
-from sqlalchemy.orm import foreign, Query, relationship
+from sqlalchemy.orm import foreign, Query, relationship, RelationshipProperty
 
 from superset.constants import NULL_STRING
 from superset.models.helpers import AuditMixinNullable, ImportMixin, QueryResult
 from superset.models.slice import Slice
-from superset.typing import FilterValue, FilterValues
+from superset.typing import FilterValue, FilterValues, QueryObjectDict
 from superset.utils import core as utils
 
 METRIC_FORM_DATA_PARAMS = [
@@ -93,7 +93,7 @@ class BaseDatasource(
     update_from_object_fields: List[str]
 
     @declared_attr
-    def slices(self):
+    def slices(self) -> RelationshipProperty:
         return relationship(
             "Slice",
             primaryjoin=lambda: and_(
@@ -117,7 +117,7 @@ class BaseDatasource(
         return sorted([c.column_name for c in self.columns], key=lambda x: x or "")
 
     @property
-    def columns_types(self) -> Dict:
+    def columns_types(self) -> Dict[str, str]:
         return {c.column_name: c.type for c in self.columns}
 
     @property
@@ -125,7 +125,7 @@ class BaseDatasource(
         return "timestamp"
 
     @property
-    def datasource_name(self):
+    def datasource_name(self) -> str:
         raise NotImplementedError()
 
     @property
@@ -143,7 +143,7 @@ class BaseDatasource(
         return sorted([c.column_name for c in self.columns if c.filterable])
 
     @property
-    def dttm_cols(self) -> List:
+    def dttm_cols(self) -> List[str]:
         return []
 
     @property
@@ -182,7 +182,7 @@ class BaseDatasource(
         }
 
     @property
-    def select_star(self):
+    def select_star(self) -> Optional[str]:
         pass
 
     @property
@@ -336,18 +336,18 @@ class BaseDatasource(
                 values = None
         return values
 
-    def external_metadata(self):
+    def external_metadata(self) -> List[Dict[str, str]]:
         """Returns column information from the external system"""
         raise NotImplementedError()
 
-    def get_query_str(self, query_obj) -> str:
+    def get_query_str(self, query_obj: QueryObjectDict) -> str:
         """Returns a query as a string
 
         This is used to be displayed to the user so that she/he can
         understand what is taking place behind the scene"""
         raise NotImplementedError()
 
-    def query(self, query_obj) -> QueryResult:
+    def query(self, query_obj: QueryObjectDict) -> QueryResult:
         """Executes the query and returns a dataframe
 
         query_obj is a dictionary representing Superset's query interface.
@@ -363,7 +363,7 @@ class BaseDatasource(
         raise NotImplementedError()
 
     @staticmethod
-    def default_query(qry) -> Query:
+    def default_query(qry: Query) -> Query:
         return qry
 
     def get_column(self, column_name: Optional[str]) -> Optional["BaseColumn"]:
@@ -376,8 +376,8 @@ class BaseDatasource(
 
     @staticmethod
     def get_fk_many_from_list(
-        object_list, fkmany, fkmany_class, key_attr
-    ):  # pylint: disable=too-many-locals
+        object_list: List[Any], fkmany: List[Column], fkmany_class: Type, key_attr: str,
+    ) -> List[Column]:  # pylint: disable=too-many-locals
         """Update ORM one-to-many list from object list
 
         Used for syncing metrics and columns using the same code"""
@@ -390,8 +390,9 @@ class BaseDatasource(
         # sync existing fks
         for fk in fkmany:
             obj = object_dict.get(getattr(fk, key_attr))
-            for attr in fkmany_class.update_from_object_fields:
-                setattr(fk, attr, obj.get(attr))
+            if obj:
+                for attr in fkmany_class.update_from_object_fields:
+                    setattr(fk, attr, obj.get(attr))
 
         # create new fks
         new_fks = []
@@ -409,7 +410,7 @@ class BaseDatasource(
         fkmany += new_fks
         return fkmany
 
-    def update_from_object(self, obj) -> None:
+    def update_from_object(self, obj: Dict[str, Any]) -> None:
         """Update datasource from a data structure
 
         The UI's table editor crafts a complex data structure that
@@ -426,18 +427,26 @@ class BaseDatasource(
         self.owners = obj.get("owners", [])
 
         # Syncing metrics
-        metrics = self.get_fk_many_from_list(
-            obj.get("metrics"), self.metrics, self.metric_class, "metric_name"
+        metrics = (
+            self.get_fk_many_from_list(
+                obj["metrics"], self.metrics, self.metric_class, "metric_name"
+            )
+            if self.metric_class and "metrics" in obj
+            else []
         )
         self.metrics = metrics
 
         # Syncing columns
-        self.columns = self.get_fk_many_from_list(
-            obj.get("columns"), self.columns, self.column_class, "column_name"
+        self.columns = (
+            self.get_fk_many_from_list(
+                obj["columns"], self.columns, self.column_class, "column_name"
+            )
+            if self.column_class and "columns" in obj
+            else []
         )
 
     def get_extra_cache_keys(  # pylint: disable=no-self-use
-        self, query_obj: Dict[str, Any]  # pylint: disable=unused-argument
+        self, query_obj: QueryObjectDict  # pylint: disable=unused-argument
     ) -> List[Hashable]:
         """ If a datasource needs to provide additional keys for calculation of
         cache keys, those can be provided via this method
@@ -474,7 +483,7 @@ class BaseColumn(AuditMixinNullable, ImportMixin):
     # [optional] Set this to support import/export functionality
     export_fields: List[Any] = []
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.column_name
 
     num_types = (
@@ -505,11 +514,11 @@ class BaseColumn(AuditMixinNullable, ImportMixin):
         return self.type and any(map(lambda t: t in self.type.upper(), self.str_types))
 
     @property
-    def expression(self):
+    def expression(self) -> Column:
         raise NotImplementedError()
 
     @property
-    def python_date_format(self):
+    def python_date_format(self) -> Column:
         raise NotImplementedError()
 
     @property
@@ -557,11 +566,11 @@ class BaseMetric(AuditMixinNullable, ImportMixin):
     """
 
     @property
-    def perm(self):
+    def perm(self) -> Optional[str]:
         raise NotImplementedError()
 
     @property
-    def expression(self):
+    def expression(self) -> Column:
         raise NotImplementedError()
 
     @property

commit 1a32f35e5dfb694895d4d853cb27604c7a48c34e

fix(mypy): Resolves regression introducted in #9824

superset/common/query_object.py
@@ -58,8 +58,8 @@ class QueryObject:
     """
 
     granularity: Optional[str]
-    from_dttm: datetime
-    to_dttm: datetime
+    from_dttm: Optional[datetime]
+    to_dttm: Optional[datetime]
     is_timeseries: bool
     time_shift: Optional[timedelta]
     groupby: List[str]

superset/connectors/sqla/models.py
@@ -714,8 +714,8 @@ class SqlaTable(Model, BaseDatasource):
         self,
         metrics: List[Metric],
         granularity: str,
-        from_dttm: datetime,
-        to_dttm: datetime,
+        from_dttm: Optional[datetime],
+        to_dttm: Optional[datetime],
         columns: Optional[List[str]] = None,
         groupby: Optional[List[str]] = None,
         filter: Optional[List[Dict[str, Any]]] = None,

superset/tasks/cache.py
@@ -163,7 +163,7 @@ class TopNDashboardsStrategy(Strategy):
     def __init__(self, top_n: int = 5, since: str = "7 days ago") -> None:
         super(TopNDashboardsStrategy, self).__init__()
         self.top_n = top_n
-        self.since = parse_human_datetime(since)
+        self.since = parse_human_datetime(since) if since else None
 
     def get_urls(self) -> List[str]:
         urls = []

superset/utils/core.py
@@ -1022,7 +1022,7 @@ def get_since_until(
     time_shift: Optional[str] = None,
     relative_start: Optional[str] = None,
     relative_end: Optional[str] = None,
-) -> Tuple[datetime, datetime]:
+) -> Tuple[Optional[datetime], Optional[datetime]]:
     """Return `since` and `until` date time tuple from string representations of
     time_range, since, until and time_shift.
 
@@ -1078,8 +1078,8 @@ def get_since_until(
             since, until = time_range.split(separator, 1)
             if since and since not in common_time_frames:
                 since = add_ago_to_since(since)
-            since = parse_human_datetime(since)  # type: ignore
-            until = parse_human_datetime(until)  # type: ignore
+            since = parse_human_datetime(since) if since else None  # type: ignore
+            until = parse_human_datetime(until) if until else None  # type: ignore
         elif time_range in common_time_frames:
             since, until = common_time_frames[time_range]
         elif time_range == "No filter":
@@ -1100,7 +1100,7 @@ def get_since_until(
         since = since or ""
         if since:
             since = add_ago_to_since(since)
-        since = parse_human_datetime(since)  # type: ignore
+        since = parse_human_datetime(since) if since else None  # type: ignore
         until = parse_human_datetime(until) if until else relative_end  # type: ignore
 
     if time_shift: