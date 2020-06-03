9a53bdfb681bdab00023df868a9944f1603acc1e
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-06-02 18:26:06 -0700

    feat(query): add functions to wrap api calls with typings (#555)
    * feat: add function to wrap api calls
    
    * feat: add function to wrap api calls with returned type
    
    * fix: remove deprecated
    
    * fix: rename
    
    * test: add unit test
    
    * test: add unit tests

---
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-query/src/api/legacy/fetchExploreJson.ts
@@ -0,0 +1,28 @@
+import { SupersetClientInterface, SupersetClient, RequestConfig } from '@superset-ui/connection';
+import { QueryFormData } from '../../types/QueryFormData';
+import { LegacyChartDataResponse } from './types';
+
+export interface Params {
+  client?: SupersetClientInterface;
+  method?: 'GET' | 'POST';
+  requestConfig?: Partial<RequestConfig>;
+  url?: string;
+  formData: QueryFormData;
+}
+
+export default function fetchExploreJson({
+  client = SupersetClient,
+  method = 'POST',
+  requestConfig,
+  url = '/superset/explore_json/',
+  formData,
+}: Params) {
+  const fetchFunc = method === 'GET' ? client.get : client.post;
+
+  return fetchFunc({
+    ...requestConfig,
+    // TODO: Have to transform formData as query string for GET
+    url,
+    postPayload: { form_data: formData },
+  } as RequestConfig).then(({ json }) => json as LegacyChartDataResponse);
+}
