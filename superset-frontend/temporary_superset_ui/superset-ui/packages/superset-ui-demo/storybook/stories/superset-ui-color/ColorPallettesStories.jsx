2d317774d6c3a1803ab6049b2140829a48ea86f5
Maxime Beauchemin <maximebeauchemin@gmail.com>
2020-06-12 06:50:12 -0700

    feat: add color pallettes defined in SIP-34 (#597)

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-demo/storybook/stories/superset-ui-color/ColorPallettesStories.jsx
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-demo/storybook/stories/superset-ui-color/ColorPallettesStories.jsx
@@ -4,6 +4,8 @@ import AirbnbPalettes from '@superset-ui/color/src/colorSchemes/categorical/airb
 import D3Palettes from '@superset-ui/color/src/colorSchemes/categorical/d3';
 import GooglePalettes from '@superset-ui/color/src/colorSchemes/categorical/google';
 import LyftPalettes from '@superset-ui/color/src/colorSchemes/categorical/lyft';
+import PresetPalettes from '@superset-ui/color/src/colorSchemes/categorical/preset';
+import SupersetPalettes from '@superset-ui/color/src/colorSchemes/categorical/superset';
 
 import SequantialCommonPalettes from '@superset-ui/color/src/colorSchemes/sequential/common';
 import SequantialD3Palettes from '@superset-ui/color/src/colorSchemes/sequential/d3';
@@ -16,10 +18,12 @@ export default {
 
 export const categoricalPalettes = () =>
   [
+    { palettes: SupersetPalettes, storyName: 'Superset' },
     { palettes: AirbnbPalettes, storyName: 'Airbnb' },
     { palettes: D3Palettes, storyName: 'd3' },
     { palettes: GooglePalettes, storyName: 'Google' },
     { palettes: LyftPalettes, storyName: 'Lyft' },
+    { palettes: PresetPalettes, storyName: 'Preset' },
   ].map(({ palettes, storyName }) => (
     <RenderPalettes key={storyName} title={storyName} palettes={palettes} />
   ));
