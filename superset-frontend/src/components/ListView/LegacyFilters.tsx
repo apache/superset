8d5119fef06af0809f3b8df611fd04d543665a35
Maxime Beauchemin <maximebeauchemin@gmail.com>
2020-06-17 19:21:57 -0700

    tweaks

--- superset-frontend/src/components/ListView/LegacyFilters.tsx
+++ superset-frontend/src/components/ListView/LegacyFilters.tsx
@@ -30,6 +30,8 @@ import { Select } from 'src/components/Select';
 import { Filters, InternalFilter, SelectOption } from './types';
 import { extractInputValue, getDefaultFilterOperator } from './utils';
 
+const styleWidth100p = { width: '100%' };
+
 export const FilterMenu = ({
   filters,
   internalFilters,
@@ -181,12 +183,13 @@ export const FilterInputs = ({
     {internalFilters.length > 0 && (
       <>
         <Row>
-          <Col md={10} />
-          <Col md={2}>
+          <Col md={11} />
+          <Col md={1}>
             <Button
               data-test="apply-filters"
               disabled={!!filtersApplied}
               bsStyle="primary"
+              style={styleWidth100p}
               onClick={applyFilters}
               bsSize="small"
             >
