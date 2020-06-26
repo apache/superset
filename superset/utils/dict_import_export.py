fd8679a422bb078d20fd7572fdfc6b39f1037b2d
Will Barrett <will@preset.io>
2020-06-26 08:49:12 -0700

    refactor: Fix lint on superset/utils/core.py (#10120)
    * Fix lint on superset/utils/core.py
    
    * black
    
    * mypy
    
    * Fix some missing renames

--- superset/utils/dict_import_export.py
+++ superset/utils/dict_import_export.py
@@ -14,7 +14,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-# pylint: disable=C,R,W
 import logging
 from typing import Any, Dict, List, Optional
 
