ad8c6729623cd7ff9c4e2f8c32d763ecb6f17a16
bogdan kyryliuk <bogdankyryliuk@dropbox.com>
2020-06-26 20:46:08 -0700

    Get coverage from 1 place

--- scripts/python_tests.sh
+++ scripts/python_tests.sh
@@ -24,4 +24,4 @@ echo "Superset config module: $SUPERSET_CONFIG"
 superset db upgrade
 superset init
 pytest --maxfail=1 tests/load_examples_test.py
-pytest --maxfail=1 --cov=superset --cov-append --ignore=load_examples_test tests
+pytest --maxfail=1 --cov=superset --ignore=load_examples_test tests
