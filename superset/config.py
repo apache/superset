d799ce5c949430985c3322f19c24e143779de89f
Evan Rusackas <evan@preset.io>
2020-07-06 17:30:18 -0700

    chore: Revised logo (new color), w/CSS for vert alignment (#10237)
    * out with the old, in with the new
    
    * vertically centering logo once and for all
    
    * lint: black instead of IDE formating
    
    * touch config.py
    
    * Styles -> StyledHeader
    
    * nixing vestigial SVG
    
    * removing trademark
    
    Co-authored-by: Maxime Beauchemin <maximebeauchemin@gmail.com>

--- superset/config.py
+++ superset/config.py
@@ -194,7 +194,7 @@ PROXY_FIX_CONFIG = {"x_for": 1, "x_proto": 1, "x_host": 1, "x_port": 1, "x_prefi
 APP_NAME = "Superset"
 
 # Uncomment to setup an App icon
-APP_ICON = "/static/assets/images/superset-logo@2x.png"
+APP_ICON = "/static/assets/images/superset-logo-horiz.png"
 APP_ICON_WIDTH = 126
 
 # Uncomment to specify where clicking the logo would take the user
