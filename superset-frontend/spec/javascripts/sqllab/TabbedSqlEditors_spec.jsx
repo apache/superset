3b4df51848f1ff3b4f22771eacdd49e77077125d
Lily Kuang <lily@preset.io>
2020-07-10 14:58:59 -0700

    style: new toast design closer to SIP-34 (#10178)

--- superset-frontend/spec/javascripts/sqllab/TabbedSqlEditors_spec.jsx
+++ superset-frontend/spec/javascripts/sqllab/TabbedSqlEditors_spec.jsx
@@ -23,6 +23,7 @@ import URI from 'urijs';
 import { Tab } from 'react-bootstrap';
 import { shallow, mount } from 'enzyme';
 import sinon from 'sinon';
+import { supersetTheme, ThemeProvider } from '@superset-ui/style';
 import TabbedSqlEditors from 'src/SqlLab/components/TabbedSqlEditors';
 import SqlEditor from 'src/SqlLab/components/SqlEditor';
 
@@ -98,6 +99,8 @@ describe('TabbedSqlEditors', () => {
       uriStub.returns({ id: 1 });
       wrapper = mount(<TabbedSqlEditors {...mockedProps} />, {
         context: { store },
+        wrappingComponent: ThemeProvider,
+        wrappingComponentProps: { theme: supersetTheme },
       });
       expect(TabbedSqlEditors.prototype.componentDidMount.calledOnce).toBe(
         true,
@@ -110,6 +113,8 @@ describe('TabbedSqlEditors', () => {
       uriStub.returns({ savedQueryId: 1 });
       wrapper = mount(<TabbedSqlEditors {...mockedProps} />, {
         context: { store },
+        wrappingComponent: ThemeProvider,
+        wrappingComponentProps: { theme: supersetTheme },
       });
       expect(TabbedSqlEditors.prototype.componentDidMount.calledOnce).toBe(
         true,
@@ -122,6 +127,8 @@ describe('TabbedSqlEditors', () => {
       uriStub.returns({ sql: 1, dbid: 1 });
       wrapper = mount(<TabbedSqlEditors {...mockedProps} />, {
         context: { store },
+        wrappingComponent: ThemeProvider,
+        wrappingComponentProps: { theme: supersetTheme },
       });
       expect(TabbedSqlEditors.prototype.componentDidMount.calledOnce).toBe(
         true,
