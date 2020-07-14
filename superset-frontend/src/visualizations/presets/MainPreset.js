65db6d06860562c153cf329b5cacd1610fb38cde
Ville Brofeldt <ville.v.brofeldt@gmail.com>
2020-07-14 14:36:46 +0300

    Change reference to new echarts plugin

--- superset-frontend/src/visualizations/presets/MainPreset.js
+++ superset-frontend/src/visualizations/presets/MainPreset.js
@@ -58,7 +58,7 @@ import {
 } from '@superset-ui/legacy-preset-chart-nvd3';
 import { LegacyBoxPlotChartPlugin } from '@superset-ui/preset-chart-xy';
 import { DeckGLChartPreset } from '@superset-ui/legacy-preset-chart-deckgl';
-import { EchartsLineChartPlugin } from '@superset-ui/plugin-chart-echarts-line';
+import { EchartsTimeseriesChartPlugin } from '@superset-ui/plugin-chart-echarts';
 
 import FilterBoxChartPlugin from '../FilterBox/FilterBoxChartPlugin';
 import TimeTableChartPlugin from '../TimeTable/TimeTableChartPlugin';
@@ -108,7 +108,9 @@ export default class MainPreset extends Preset {
         new TreemapChartPlugin().configure({ key: 'treemap' }),
         new WordCloudChartPlugin().configure({ key: 'word_cloud' }),
         new WorldMapChartPlugin().configure({ key: 'world_map' }),
-        new EchartsLineChartPlugin().configure({ key: 'echarts_line' }),
+        new EchartsTimeseriesChartPlugin().configure({
+          key: 'echarts_timeseries',
+        }),
       ],
     });
   }
