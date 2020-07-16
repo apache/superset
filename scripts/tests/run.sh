977415ff6eb697f977ce83fad99928e5a8411866
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2020-07-16 13:03:49 +0100

    tests: update test run doc and add some improvements  (#10339)
    * fix: update tests/run.sh README to pytest and some improvements
    
    * update docs
    
    * nit
    
    * fix doc

--- scripts/tests/run.sh
+++ scripts/tests/run.sh
@@ -59,19 +59,6 @@ function test_init() {
   pytest -s tests/load_examples_test.py
 }
 
-
-if [[ "$#" -eq  "0" ]]
-then
-  echo "No argument suplied"
-  echo ------------------------
-  echo use:
-  echo "run.sh <test module name> [options]"
-  echo "[options]:"
-  echo "--no-init: Dont restart docker and no db migrations, superset init and test data"
-  echo "--no-reset-db: Recreates test database (DROP, CREATE)"
-  exit 1
-fi
-
 #
 # Init global vars
 #
@@ -83,14 +70,20 @@ export SUPERSET_CONFIG=${SUPERSET_CONFIG:-tests.superset_test_config}
 RUN_INIT=1
 RUN_RESET_DB=1
 RUN_TESTS=1
-TEST_MODULE="${1}"
-
-# Shift to pass the first cmd parameter for the test module
-shift 1
+TEST_MODULE="tests"
 
 PARAMS=""
 while (( "$#" )); do
   case "$1" in
+    --help)
+      echo Switches:
+      echo --no-init : Will not, reset the test DB, superset init and load examples
+      echo --no-reset-db: Will not reset the test DB
+      echo --no-tests: Will not run any test, by default reset the DB, superset init and load_examples
+      echo --reset-db: Just resets the test DB, will not run any test
+      echo --module: Run a specific test module: --module tests/charts/api_tests.py for example
+      exit 0
+      ;;
     --no-init)
       RUN_INIT=0
       RUN_RESET_DB=0
@@ -108,6 +101,10 @@ while (( "$#" )); do
       RUN_TESTS=0
       RUN_INIT=0
       shift 1
+      ;;
+    --module)
+      TEST_MODULE=$2
+      shift 2
       ;;
     --) # end argument parsing
       shift
@@ -145,5 +142,5 @@ fi
 
 if [ $RUN_TESTS -eq 1 ]
 then
-  pytest -s --ignore=load_examples_test "${TEST_MODULE}"
+  pytest -x -s --ignore=load_examples_test "${TEST_MODULE}"
 fi
