7af8b2b3f822d74094f09609cd7e740415f90354
Ville Brofeldt <33317356+villebro@users.noreply.github.com>
2020-07-20 18:46:51 +0300

    feat: add optional prophet forecasting functionality to chart data api (#10324)
    * feat: add prophet post processing operation
    
    * add tests
    
    * lint
    
    * whitespace
    
    * remove whitespace
    
    * address comments
    
    * add note to UPDATING.md

--- tests/pandas_postprocessing_tests.py
+++ tests/pandas_postprocessing_tests.py
@@ -20,13 +20,14 @@ import math
 from typing import Any, List, Optional
 
 from pandas import DataFrame, Series
+import pytest
 
 from superset.exceptions import QueryObjectValidationError
 from superset.utils import pandas_postprocessing as proc
 from superset.utils.core import DTTM_ALIAS, PostProcessingContributionOrientation
 
 from .base_tests import SupersetTestCase
-from .fixtures.dataframes import categories_df, lonlat_df, timeseries_df
+from .fixtures.dataframes import categories_df, lonlat_df, timeseries_df, prophet_df
 
 AGGREGATES_SINGLE = {"idx_nulls": {"operator": "sum"}}
 AGGREGATES_MULTIPLE = {
@@ -508,3 +509,83 @@ class TestPostProcessing(SupersetTestCase):
         self.assertListEqual(df.columns.tolist(), ["a", "b"])
         self.assertListEqual(series_to_list(column_df["a"]), [0.25, 0.75])
         self.assertListEqual(series_to_list(column_df["b"]), [0.1, 0.9])
+
+    def test_prophet_valid(self):
+        pytest.importorskip("fbprophet")
+
+        df = proc.prophet(
+            df=prophet_df, time_grain="P1M", periods=3, confidence_interval=0.9
+        )
+        columns = {column for column in df.columns}
+        assert columns == {
+            DTTM_ALIAS,
+            "a__yhat",
+            "a__yhat_upper",
+            "a__yhat_lower",
+            "a",
+            "b__yhat",
+            "b__yhat_upper",
+            "b__yhat_lower",
+            "b",
+        }
+        assert df[DTTM_ALIAS].iloc[0].to_pydatetime() == datetime(2018, 12, 31)
+        assert df[DTTM_ALIAS].iloc[-1].to_pydatetime() == datetime(2022, 3, 31)
+        assert len(df) == 7
+
+        df = proc.prophet(
+            df=prophet_df, time_grain="P1M", periods=5, confidence_interval=0.9
+        )
+        assert df[DTTM_ALIAS].iloc[0].to_pydatetime() == datetime(2018, 12, 31)
+        assert df[DTTM_ALIAS].iloc[-1].to_pydatetime() == datetime(2022, 5, 31)
+        assert len(df) == 9
+
+    def test_prophet_missing_temporal_column(self):
+        df = prophet_df.drop(DTTM_ALIAS, axis=1)
+
+        self.assertRaises(
+            QueryObjectValidationError,
+            proc.prophet,
+            df=df,
+            time_grain="P1M",
+            periods=3,
+            confidence_interval=0.9,
+        )
+
+    def test_prophet_incorrect_confidence_interval(self):
+        self.assertRaises(
+            QueryObjectValidationError,
+            proc.prophet,
+            df=prophet_df,
+            time_grain="P1M",
+            periods=3,
+            confidence_interval=0.0,
+        )
+
+        self.assertRaises(
+            QueryObjectValidationError,
+            proc.prophet,
+            df=prophet_df,
+            time_grain="P1M",
+            periods=3,
+            confidence_interval=1.0,
+        )
+
+    def test_prophet_incorrect_periods(self):
+        self.assertRaises(
+            QueryObjectValidationError,
+            proc.prophet,
+            df=prophet_df,
+            time_grain="P1M",
+            periods=0,
+            confidence_interval=0.8,
+        )
+
+    def test_prophet_incorrect_time_grain(self):
+        self.assertRaises(
+            QueryObjectValidationError,
+            proc.prophet,
+            df=prophet_df,
+            time_grain="yearly",
+            periods=10,
+            confidence_interval=0.8,
+        )
