9f0eec076f73d0cb9285d0b70e31dcf33ca495fb
riahk <moar.riah@gmail.com>
2020-07-22 16:57:56 -0700

    lint fix

--- superset-frontend/src/components/Menu/Menu.jsx
+++ superset-frontend/src/components/Menu/Menu.jsx
@@ -61,25 +61,25 @@ const StyledHeader = styled.header`
   }
 
   .navbar-nav > li > a {
-	&:after {
-		content: '';
-		position: absolute;
-		bottom: -3px;
-		left: 0;
-		width: 100%;
-		height: 3px;
-		background-color: #20a7c9;
-		opacity: 0;
-		transition: opacity 0.5s;
-	}
+    &:after {
+      content: '';
+      position: absolute;
+      bottom: -3px;
+      left: 0;
+      width: 100%;
+      height: 3px;
+      background-color: #20a7c9;
+      opacity: 0;
+      transition: opacity 0.5s;
+    }
 
-	&:hover {
-		border-bottom: none;
+    &:hover {
+      border-bottom: none;
 
-		&:after {
-			opacity: 1;
-		}
-	}
+      &:after {
+        opacity: 1;
+      }
+    }
   }
 `;
 
