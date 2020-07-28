325d5c3cf60decabb7f6d2ddf016f0a210a1c429
Jason Davis <@dropbox.com>
2020-07-28 16:20:29 -0700

    add app_context to tests

--- tests/alerts_tests.py
+++ tests/alerts_tests.py
@@ -25,6 +25,7 @@ from superset.models.alerts import Alert, AlertLog
 from superset.models.slice import Slice
 from superset.tasks.schedules import run_alert_query
 from superset.utils import core as utils
+from tests.test_app import app
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -32,49 +33,50 @@ logger = logging.getLogger(__name__)
 
 @pytest.yield_fixture(scope="module")
 def setup_database():
-    slice_id = db.session.query(Slice).all()[0].id
-    database_id = utils.get_example_database().id
+    with app.app_context():
+        slice_id = db.session.query(Slice).all()[0].id
+        database_id = utils.get_example_database().id
 
-    alert1 = Alert(
-        id=1,
-        label="alert_1",
-        active=True,
-        crontab="*/1 * * * *",
-        sql="SELECT 0",
-        alert_type="email",
-        slice_id=slice_id,
-        database_id=database_id,
-    )
-    alert2 = Alert(
-        id=2,
-        label="alert_2",
-        active=True,
-        crontab="*/1 * * * *",
-        sql="SELECT 55",
-        alert_type="email",
-        slice_id=slice_id,
-        database_id=database_id,
-    )
-    alert3 = Alert(
-        id=3,
-        label="alert_3",
-        active=False,
-        crontab="*/1 * * * *",
-        sql="UPDATE 55",
-        alert_type="email",
-        slice_id=slice_id,
-        database_id=database_id,
-    )
-    alert4 = Alert(id=4, active=False, label="alert_4", database_id=-1)
-    alert5 = Alert(id=5, active=False, label="alert_5", database_id=database_id)
+        alert1 = Alert(
+            id=1,
+            label="alert_1",
+            active=True,
+            crontab="*/1 * * * *",
+            sql="SELECT 0",
+            alert_type="email",
+            slice_id=slice_id,
+            database_id=database_id,
+        )
+        alert2 = Alert(
+            id=2,
+            label="alert_2",
+            active=True,
+            crontab="*/1 * * * *",
+            sql="SELECT 55",
+            alert_type="email",
+            slice_id=slice_id,
+            database_id=database_id,
+        )
+        alert3 = Alert(
+            id=3,
+            label="alert_3",
+            active=False,
+            crontab="*/1 * * * *",
+            sql="UPDATE 55",
+            alert_type="email",
+            slice_id=slice_id,
+            database_id=database_id,
+        )
+        alert4 = Alert(id=4, active=False, label="alert_4", database_id=-1)
+        alert5 = Alert(id=5, active=False, label="alert_5", database_id=database_id)
 
-    for num in range(1, 6):
-        eval(f"db.session.add(alert{num})")
-    db.session.commit()
-    yield db.session
+        for num in range(1, 6):
+            eval(f"db.session.add(alert{num})")
+        db.session.commit()
+        yield db.session
 
-    db.session.query(AlertLog).delete()
-    db.session.query(Alert).delete()
+        db.session.query(AlertLog).delete()
+        db.session.query(Alert).delete()
 
 
 @patch("superset.tasks.schedules.deliver_alert")
