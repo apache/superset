1145ebe2c09ea85ad31cad6efa6dcac8cec819d0
Evan Rusackas <evan@preset.io>
2020-08-04 22:09:18 -0700

    lint

--- superset-frontend/src/components/Icon/icon.stories.jsx
+++ superset-frontend/src/components/Icon/icon.stories.jsx
@@ -64,21 +64,23 @@ const IconBlock = styled.div`
 export const SupersetIcon = () => {
   return (
     <IconSet>
-      {Object.keys(iconsRegistry).sort().map(iconName => (
-        <IconBlock>
-          <Icon
-            name={iconName}
-            key={iconName}
-            color={select(
-              colorKnob.label,
-              colorKnob.options,
-              colorKnob.defaultValue,
-              colorKnob.groupId,
-            )}
-          />
-          <div>{iconName}</div>
-        </IconBlock>
-      ))}
+      {Object.keys(iconsRegistry)
+        .sort()
+        .map(iconName => (
+          <IconBlock>
+            <Icon
+              name={iconName}
+              key={iconName}
+              color={select(
+                colorKnob.label,
+                colorKnob.options,
+                colorKnob.defaultValue,
+                colorKnob.groupId,
+              )}
+            />
+            <div>{iconName}</div>
+          </IconBlock>
+        ))}
     </IconSet>
   );
 };
