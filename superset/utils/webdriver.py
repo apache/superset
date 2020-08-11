97563f95f1ef654f6ba4172796bc512a5e5387f5
Craig <craig@craigrueda.com>
2020-08-10 17:13:18 -0700

    Adding license

--- superset/utils/webdriver.py
+++ superset/utils/webdriver.py
@@ -14,6 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+
 import logging
 from typing import Any, Callable, Dict, List, Optional, Tuple
 
