5dce7c1f57b5fae36b05f7a5aae0350209980168
Daniel Gaspar <danielvazgaspar@gmail.com>
2020-08-11 18:46:35 +0100

    lint

--- superset/datasets/commands/delete.py
+++ superset/datasets/commands/delete.py
@@ -52,7 +52,7 @@ class DeleteDatasetCommand(BaseCommand):
                 else None
             )
             permission_views = (
-                security_manager.get_session.query(
+                db.session.query(
                     security_manager.permissionview_model
                 )
                 .filter_by(view_menu=view_menu)
