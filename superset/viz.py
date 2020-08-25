713d559bbef867ace2cbb563eb61282a9c49049b
bogdan kyryliuk <bogdankyryliuk@dropbox.com>
2020-08-25 14:08:45 -0700

    Log cache keys to the logs

--- superset/viz.py
+++ superset/viz.py
@@ -48,12 +48,12 @@ import pandas as pd
 import polyline
 import simplejson as json
 from dateutil import relativedelta as rdelta
-from flask import request
+from flask import g, request
 from flask_babel import lazy_gettext as _
 from geopy.point import Point
 from pandas.tseries.frequencies import to_offset
 
-from superset import app, cache, security_manager
+from superset import app, cache, event_logger, security_manager
 from superset.constants import NULL_STRING
 from superset.errors import ErrorLevel, SupersetError, SupersetErrorType
 from superset.exceptions import (
@@ -93,6 +93,42 @@ METRIC_KEYS = [
     "y",
     "size",
 ]
+
+
+def set_and_log_cache(
+    cache_key: str,
+    df: pd.DataFrame,
+    query: str,
+    cached_dttm: str,
+    cache_timeout: int,
+    datasource: Optional["BaseDatasource"] = None,
+) -> None:
+    try:
+        cache_value = dict(dttm=cached_dttm, df=df, query=query)
+        stats_logger.incr("set_cache_key")
+        cache.set(cache_key, cache_value, timeout=cache_timeout)
+
+        user_id = g.user.get_id() if hasattr(g, "user") and g.user else None
+        kwargs = {}
+        if datasource:
+            kwargs["datasource_id"] = datasource.uid
+            kwargs["datasource_name"] = datasource.name
+            kwargs["database_id"] = datasource.database.id
+            kwargs["database_name"] = datasource.database.name
+
+        event_logger.log(
+            user_id,
+            "cache_set",
+            cache_key=cache_key,
+            cache_timeout=cache_timeout,
+            **kwargs,
+        )
+    except Exception as ex:
+        # cache.set call can fail if the backend is down or if
+        # the key is too large or whatever other reasons
+        logger.warning("Could not cache key {}".format(cache_key))
+        logger.exception(ex)
+        cache.delete(cache_key)
 
 
 class BaseViz:
@@ -536,16 +572,14 @@ class BaseViz:
                 and cache
                 and self.status != utils.QueryStatus.FAILED
             ):
-                try:
-                    cache_value = dict(dttm=cached_dttm, df=df, query=self.query)
-                    stats_logger.incr("set_cache_key")
-                    cache.set(cache_key, cache_value, timeout=self.cache_timeout)
-                except Exception as ex:
-                    # cache.set call can fail if the backend is down or if
-                    # the key is too large or whatever other reasons
-                    logger.warning("Could not cache key {}".format(cache_key))
-                    logger.exception(ex)
-                    cache.delete(cache_key)
+                set_and_log_cache(
+                    cache_key,
+                    df,
+                    self.query,
+                    cached_dttm,
+                    self.cache_timeout,
+                    datasource=self.datasource,
+                )
         return {
             "cache_key": self._any_cache_key,
             "cached_dttm": self._any_cached_dttm,
