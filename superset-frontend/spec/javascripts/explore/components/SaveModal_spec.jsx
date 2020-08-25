979d7a9fa79d07db52f8c0bf61fc790e8c57bca4
Evan Rusackas <evan@preset.io>
2020-08-25 14:02:19 -0700

    another fixed test

--- superset-frontend/spec/javascripts/explore/components/SaveModal_spec.jsx
+++ superset-frontend/spec/javascripts/explore/components/SaveModal_spec.jsx
@@ -21,7 +21,8 @@ import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { bindActionCreators } from 'redux';
 
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
+import { styledMount as mount } from 'spec/helpers/theming';
 import { FormControl, Modal, Radio } from 'react-bootstrap';
 import Button from 'src/components/Button';
 import sinon from 'sinon';
