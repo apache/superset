3756d03085cff994c405eeaf2868a617203787a7
riahk <moar.riah@gmail.com>
2020-08-31 14:36:44 -0700

    rm console statement

--- superset-frontend/src/views/CRUD/data/database/DatabaseModal.tsx
+++ superset-frontend/src/views/CRUD/data/database/DatabaseModal.tsx
@@ -118,8 +118,6 @@ const DatabaseModal: FunctionComponent<DatabaseModalProps> = ({
   const [db, setDB] = useState<DatabaseObject | null>(null);
   const [isHidden, setIsHidden] = useState<boolean>(true);
 
-  console.log('db', db);
-
   // Functions
   const hide = () => {
     setIsHidden(true);
