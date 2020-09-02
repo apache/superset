e8d8949b2cb8956b0692e5c50fd7afe96e9e2ce4
Jason Davis <@dropbox.com>
2020-09-02 12:18:12 -0700

    adjusted format of messages

--- tests/alerts_tests.py
+++ tests/alerts_tests.py
@@ -328,9 +328,9 @@ def test_deliver_alert_screenshot(
         "channels": alert.slack_channel,
         "file": screenshot,
         "initial_comment": f"\n*Triggered Alert: {alert.label} :redalert:*\n"
-        f"*SQL* *Statement*:```{alert.sql_observer[0].sql}```\n"
-        f"*SQL* *Result*: {alert.observations[-1].value}\n"
-        f"*Reason* *For* *Alert*: {alert.observations[-1].value} {str(alert.validators[0])}\n"
+        f"*Query*:```{alert.sql_observer[0].sql}```\n"
+        f"*Result*: {alert.observations[-1].value}\n"
+        f"*Reason*: {alert.observations[-1].value} {str(alert.validators[0])}\n"
         f"<http://0.0.0.0:8080/alert/show/{alert.id}"
         f"|View Alert Details>\n<http://0.0.0.0:8080/superset/slice/{alert.slice_id}/"
         "|*Explore in Superset*>",
