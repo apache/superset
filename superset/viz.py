0f245d876b66187e0dbcb337e7a5fd20cdf7de82
chuancy <3195234362@qq.com>
2020-09-02 15:20:00 +0800

    Update viz.py

--- superset/viz.py
+++ superset/viz.py
@@ -617,10 +617,7 @@ class BaseViz:
         df = self.get_df()
         columns = list(df.columns)
         verbose_map = self.datasource.data.get("verbose_map", {})
-
-        if verbose_map:
-            df.columns = [verbose_map.get(column, column) for column in columns]
-
+        df.columns = [verbose_map.get(column, column) for column in columns]
         include_index = not isinstance(df.index, pd.RangeIndex)
         return df.to_csv(index=include_index, **config["CSV_EXPORT"])
 
