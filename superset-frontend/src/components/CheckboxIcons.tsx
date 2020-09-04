fda26081ffa5f6e94162a57ef2d3e7f88f9bd70c
Elizabeth Thompson <elizabeth@preset.io>
2020-09-04 14:43:34 -0700

    add vertical align to svg

--- superset-frontend/src/components/CheckboxIcons.tsx
+++ superset-frontend/src/components/CheckboxIcons.tsx
@@ -25,6 +25,7 @@ export const CheckboxChecked = () => (
     viewBox="0 0 18 18"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    style={{ verticalAlign: 'top' }}
   >
     <path
       d="M16 0H2C0.89 0 0 0.9 0 2V16C0 17.1 0.89 18 2 18H16C17.11 18 18 17.1 18 16V2C18 0.9 17.11 0 16 0Z"
@@ -41,6 +42,7 @@ export const CheckboxHalfChecked = () => (
     viewBox="0 0 18 18"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    style={{ verticalAlign: 'top' }}
   >
     <path
       d="M16 0H2C0.9 0 0 0.9 0 2V16C0 17.1 0.9 18 2 18H16C17.1 18 18 17.1 18 16V2C18 0.9 17.1 0 16 0Z"
@@ -57,6 +59,7 @@ export const CheckboxUnchecked = () => (
     viewBox="0 0 18 18"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    style={{ verticalAlign: 'top' }}
   >
     <path
       d="M16 0H2C0.9 0 0 0.9 0 2V16C0 17.1 0.9 18 2 18H16C17.1 18 18 17.1 18 16V2C18 0.9 17.1 0 16 0Z"
