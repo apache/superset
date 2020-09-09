2878832c9c589e865780cd9494d54b25abd65e6a
ʈᵃᵢ <tdupreetan@gmail.com>
2020-09-08 18:54:02 -0700

    feat: database delete warning (#10800)

--- superset/databases/dao.py
+++ superset/databases/dao.py
@@ -15,11 +15,14 @@
 # specific language governing permissions and limitations
 # under the License.
 import logging
+from typing import Any, Dict
 
 from superset.dao.base import BaseDAO
 from superset.databases.filters import DatabaseFilter
 from superset.extensions import db
 from superset.models.core import Database
+from superset.models.dashboard import Dashboard
+from superset.models.slice import Slice
 
 logger = logging.getLogger(__name__)
 
@@ -41,3 +44,28 @@ class DatabaseDAO(BaseDAO):
             Database.database_name == database_name, Database.id != database_id,
         )
         return not db.session.query(database_query.exists()).scalar()
+
+    @classmethod
+    def get_related_objects(cls, database_id: int) -> Dict[str, Any]:
+        datasets = cls.find_by_id(database_id).tables
+        dataset_ids = [dataset.id for dataset in datasets]
+
+        charts = (
+            db.session.query(Slice)
+            .filter(
+                Slice.datasource_id.in_(dataset_ids), Slice.datasource_type == "table"
+            )
+            .all()
+        )
+        chart_ids = [chart.id for chart in charts]
+
+        dashboards = (
+            (
+                db.session.query(Dashboard)
+                .join(Dashboard.slices)
+                .filter(Slice.id.in_(chart_ids))
+            )
+            .distinct()
+            .all()
+        )
+        return dict(charts=charts, dashboards=dashboards)
