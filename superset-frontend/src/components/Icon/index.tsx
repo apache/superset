9d2362f1e17b751aa5aab1981dc57408e11a3a1b
Evan Rusackas <evan@preset.io>
2020-09-14 15:55:41 -0700

    forgot to change this when ranming the file

--- superset-frontend/src/components/Icon/index.tsx
+++ superset-frontend/src/components/Icon/index.tsx
@@ -36,7 +36,7 @@ import { ReactComponent as ErrorSolidIcon } from 'images/icons/error_solid.svg';
 import { ReactComponent as FavoriteSelectedIcon } from 'images/icons/favorite-selected.svg';
 import { ReactComponent as FavoriteUnselectedIcon } from 'images/icons/favorite-unselected.svg';
 import { ReactComponent as ListViewIcon } from 'images/icons/list_view.svg';
-import { ReactComponent as MoreHorizIcon } from 'images/icons/more-horiz.svg';
+import { ReactComponent as MoreHorizIcon } from 'images/icons/more_horiz.svg';
 import { ReactComponent as EditAltIcon } from 'images/icons/edit_alt.svg';
 import { ReactComponent as SearchIcon } from 'images/icons/search.svg';
 import { ReactComponent as ShareIcon } from 'images/icons/share.svg';
