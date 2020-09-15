497fc6bbacb7db52be09754a5270382bf6ea33e5
Bogdan <b.kyryliuk@gmail.com>
2020-09-15 11:17:21 -0700

    feat: implement cache invalidation api (#10761)
    * Add cache endpoints
    
    * Implement cache endpoint
    
    * Tests and address feedback
    
    * Set cache config
    
    * Address feedback
    
    * Expose only invalidate endpoint
    
    Co-authored-by: bogdan kyryliuk <bogdankyryliuk@dropbox.com>

--- superset/app.py
+++ superset/app.py
@@ -125,6 +125,7 @@ class SupersetAppInitializer:
         #
         # pylint: disable=too-many-locals
         # pylint: disable=too-many-statements
+        from superset.cachekeys.api import CacheRestApi
         from superset.charts.api import ChartRestApi
         from superset.connectors.druid.views import (
             Druid,
@@ -194,6 +195,7 @@ class SupersetAppInitializer:
         #
         # Setup API views
         #
+        appbuilder.add_api(CacheRestApi)
         appbuilder.add_api(ChartRestApi)
         appbuilder.add_api(DashboardRestApi)
         appbuilder.add_api(DatabaseRestApi)
