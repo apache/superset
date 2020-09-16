c181d6a0f640b18c0979bab6bb03fbda82d4c3e6
Evan Rusackas <evan@preset.io>
2020-09-16 13:24:26 -0700

    docs: Client side redirects (one POC) (#10898)
    * one simple redirect, as a test
    
    * adding license to header

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -71,5 +71,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    `gatsby-plugin-client-side-redirect`,
   ],
 };
