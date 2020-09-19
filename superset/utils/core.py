f0c49ed411cea40f7753a1b110f0f72ac97b9393
Ville Brofeldt <ville.v.brofeldt@gmail.com>
2020-09-19 09:52:21 +0300

    fix: simply is_adhoc_metric

--- superset/utils/core.py
+++ superset/utils/core.py
@@ -1040,24 +1040,9 @@ def backend() -> str:
 
 
 def is_adhoc_metric(metric: Metric) -> bool:
-    if not isinstance(metric, dict):
-        return False
-    metric = cast(Dict[str, Any], metric)
-    return bool(
-        (
-            (
-                metric.get("expressionType") == AdhocMetricExpressionType.SIMPLE
-                and metric.get("column")
-                and cast(Dict[str, Any], metric["column"]).get("column_name")
-                and metric.get("aggregate")
-            )
-            or (
-                metric.get("expressionType") == AdhocMetricExpressionType.SQL
-                and metric.get("sqlExpression")
-            )
-        )
-        and metric.get("label")
-    )
+    if isinstance(metric, dict):
+        return True
+    return False
 
 
 def get_metric_name(metric: Metric) -> str:
