ba8709d8e2608648d8cc175d32202561a606a57f
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2020-09-22 15:02:29 +0200

    Remove usages of reactable from TableLoader

--- superset-frontend/src/components/TableLoader.jsx
+++ superset-frontend/src/components/TableLoader.jsx
@@ -18,11 +18,10 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Table, Tr, Td } from 'reactable-arc';
 import { t, SupersetClient } from '@superset-ui/core';
 
+import ListView from 'src/components/ListView';
 import withToasts from '../messageToasts/enhancers/withToasts';
-import Loading from './Loading';
 import '../../stylesheets/reactable-pagination.less';
 
 const propTypes = {
@@ -44,8 +43,41 @@ class TableLoader extends React.PureComponent {
     };
   }
 
-  UNSAFE_componentWillMount() {
+  sortDataByColumn = (data, sortById, desc, elementSelector = e => e) => {
+    const id = data[0].hasOwnProperty(`_${sortById}`)
+      ? `_${sortById}`
+      : sortById;
+    return data.sort((row1, row2) => {
+      const rows = desc ? [row2, row1] : [row1, row2];
+      let firstVal = rows[0][id];
+      let secondVal = rows[1][id];
+      if (typeof firstVal === 'object' && typeof secondVal === 'object') {
+        firstVal = elementSelector(firstVal);
+        secondVal = elementSelector(secondVal);
+      }
+      if (typeof firstVal === 'string' && typeof secondVal === 'string') {
+        return secondVal.localeCompare(firstVal);
+      }
+      if (typeof firstVal === 'number' && typeof secondVal === 'number') {
+        return secondVal - firstVal;
+      }
+      if (typeof firstVal === 'undefined' || firstVal === null) {
+        return 1;
+      }
+      return -1;
+    });
+  };
+
+  fetchData = ({ sortBy }) => {
     const { dataEndpoint, mutator } = this.props;
+
+    if (this.state.data.length > 0 && sortBy.length > 0) {
+      const { id, desc } = sortBy[0];
+      this.setState(({ data }) => ({
+        data: this.sortDataByColumn(data, id, desc, e => e.props.children),
+      }));
+      return;
+    }
 
     SupersetClient.get({ endpoint: dataEndpoint })
       .then(({ json }) => {
@@ -56,13 +88,9 @@ class TableLoader extends React.PureComponent {
         this.setState({ isLoading: false });
         this.props.addDangerToast(t('An error occurred'));
       });
-  }
+  };
 
   render() {
-    if (this.state.isLoading) {
-      return <Loading />;
-    }
-
     const {
       addDangerToast,
       addInfoToast,
@@ -71,8 +99,8 @@ class TableLoader extends React.PureComponent {
       ...tableProps
     } = this.props;
 
-    let { columns } = this.props;
-    if (!columns && this.state.data.length > 0) {
+    let { columns = [] } = this.props;
+    if (columns.length === 0 && this.state.data.length > 0) {
       columns = Object.keys(this.state.data[0]).filter(col => col[0] !== '_');
     }
     delete tableProps.dataEndpoint;
@@ -80,31 +108,17 @@ class TableLoader extends React.PureComponent {
     delete tableProps.columns;
 
     return (
-      <Table
-        {...tableProps}
-        className="table"
-        itemsPerPage={50}
-        style={{ textTransform: 'capitalize' }}
-      >
-        {this.state.data.map((row, i) => (
-          <Tr key={i}>
-            {columns.map(col => {
-              if (row.hasOwnProperty(`_${col}`)) {
-                return (
-                  <Td key={col} column={col} value={row[`_${col}`]}>
-                    {row[col]}
-                  </Td>
-                );
-              }
-              return (
-                <Td key={col} column={col}>
-                  {row[col]}
-                </Td>
-              );
-            })}
-          </Tr>
-        ))}
-      </Table>
+      <ListView
+        columns={columns.map(column => ({
+          accessor: column,
+          Header: column,
+        }))}
+        data={this.state.data}
+        count={this.state.data.length}
+        pageSize={50}
+        fetchData={this.fetchData}
+        loading={this.state.isLoading}
+      />
     );
   }
 }
