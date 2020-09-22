95ba8e1fb758cb8fe90fdd2ccec1faccfb1627cc
Karthikeyan Singaravelan <tir.karthi@gmail.com>
2020-09-22 20:33:58 +0530

    fix: deprecation warnings due to invalid escape sequences. (#10710)
    * fix: Fix deprecation warnings due to invalid escape sequences.
    
    * fix: Fix black related errors.

--- superset/config.py
+++ superset/config.py
@@ -146,9 +146,7 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 # ---------------------------------------------------------
 
 # Your App secret key
-SECRET_KEY = (
-    "\2\1thisismyscretkey\1\2\e\y\y\h"  # pylint: disable=anomalous-backslash-in-string
-)
+SECRET_KEY = "\2\1thisismyscretkey\1\2\\e\\y\\y\\h"
 
 # The SQLAlchemy connection string.
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(DATA_DIR, "superset.db")
