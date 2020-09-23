247b67a894658d7b107a96c4b71b7a846cb04d33
Daniel Gaspar <danielvazgaspar@gmail.com>
2020-09-23 16:23:37 +0100

    fix(api): unable to delete virtual dataset because of wrong permission name

--- superset/connectors/sqla/models.py
+++ superset/connectors/sqla/models.py
@@ -551,7 +551,7 @@ class SqlaTable(  # pylint: disable=too-many-public-methods,too-many-instance-at
 
     @property
     def database_name(self) -> str:
-        return self.database.name
+        return self.database.database_name
 
     @classmethod
     def get_datasource_by_name(
