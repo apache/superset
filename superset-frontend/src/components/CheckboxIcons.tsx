c14a06db4185b9b043116708b649b0be2ad17a1f
Grace Guo <grace.guo@airbnb.com>
2020-09-24 15:48:59 -0700

    Revert "style: fix checkbox color (#10970)"
    This reverts commit e4e78b66fa82acbe8e5bc425d697ff11b4a0cc3f.

--- superset-frontend/src/components/CheckboxIcons.tsx
+++ superset-frontend/src/components/CheckboxIcons.tsx
@@ -28,7 +28,7 @@ export const CheckboxChecked = () => (
   >
     <path
       d="M16 0H2C0.89 0 0 0.9 0 2V16C0 17.1 0.89 18 2 18H16C17.11 18 18 17.1 18 16V2C18 0.9 17.11 0 16 0Z"
-      fill="currentColor"
+      fill="#00A699"
     />
     <path d="M7 14L2 9L3.41 7.59L7 11.17L14.59 3.58L16 5L7 14Z" fill="white" />
   </svg>
@@ -44,7 +44,7 @@ export const CheckboxHalfChecked = () => (
   >
     <path
       d="M16 0H2C0.9 0 0 0.9 0 2V16C0 17.1 0.9 18 2 18H16C17.1 18 18 17.1 18 16V2C18 0.9 17.1 0 16 0Z"
-      fill="currentColor"
+      fill="#999999"
     />
     <path d="M14 10H4V8H14V10Z" fill="white" />
   </svg>
@@ -60,7 +60,7 @@ export const CheckboxUnchecked = () => (
   >
     <path
       d="M16 0H2C0.9 0 0 0.9 0 2V16C0 17.1 0.9 18 2 18H16C17.1 18 18 17.1 18 16V2C18 0.9 17.1 0 16 0Z"
-      fill="currentColor"
+      fill="#CCCCCC"
     />
     <path d="M16 2V16H2V2H16V2Z" fill="white" />
   </svg>
