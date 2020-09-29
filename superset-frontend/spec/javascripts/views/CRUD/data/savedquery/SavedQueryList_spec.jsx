7b71caf1e0f1ddd3227a00d4f96191f1a4a9ce56
Moriah Kreeger <moar.riah@gmail.com>
2020-09-29 13:39:10 -0700

    feat: saved query list view + sort/filters (#11005)

--- superset-frontend/spec/javascripts/views/CRUD/data/savedquery/SavedQueryList_spec.jsx
+++ superset-frontend/spec/javascripts/views/CRUD/data/savedquery/SavedQueryList_spec.jsx
@@ -19,14 +19,67 @@
 import React from 'react';
 import thunk from 'redux-thunk';
 import configureStore from 'redux-mock-store';
+import fetchMock from 'fetch-mock';
 import { styledMount as mount } from 'spec/helpers/theming';
 import SavedQueryList from 'src/views/CRUD/data/savedquery/SavedQueryList';
 import SubMenu from 'src/components/Menu/SubMenu';
+import ListView from 'src/components/ListView';
+import Filters from 'src/components/ListView/Filters';
 import waitForComponentToPaint from 'spec/helpers/waitForComponentToPaint';
+import { act } from 'react-dom/test-utils';
 
 // store needed for withToasts(DatabaseList)
 const mockStore = configureStore([thunk]);
 const store = mockStore({});
+
+const queriesInfoEndpoint = 'glob:*/api/v1/saved_query/_info*';
+const queriesEndpoint = 'glob:*/api/v1/saved_query/?*';
+const queriesRelatedEndpoint = 'glob:*/api/v1/saved_query/related/database?*';
+const queriesDistinctEndpoint = 'glob:*/api/v1/saved_query/distinct/schema?*';
+
+const mockqueries = [...new Array(3)].map((_, i) => ({
+  created_by: {
+    id: i,
+    first_name: `user`,
+    last_name: `${i}`,
+  },
+  created_on: `${i}-2020`,
+  database: {
+    database_name: `db ${i}`,
+    id: i,
+  },
+  changed_on_delta_humanized: '1 day ago',
+  db_id: i,
+  description: `SQL for ${i}`,
+  label: `query ${i}`,
+  schema: 'public',
+  sql: `SELECT ${i} FROM table`,
+  sql_tables: [
+    {
+      catalog: null,
+      schema: null,
+      table: `${i}`,
+    },
+  ],
+}));
+
+fetchMock.get(queriesInfoEndpoint, {
+  permissions: ['can_delete'],
+});
+fetchMock.get(queriesEndpoint, {
+  result: mockqueries,
+  count: 3,
+});
+
+fetchMock.get(queriesRelatedEndpoint, {
+  count: 0,
+  result: [],
+});
+
+fetchMock.get(queriesDistinctEndpoint, {
+  count: 0,
+  result: [],
+});
 
 describe('SavedQueryList', () => {
   const wrapper = mount(<SavedQueryList />, { context: { store } });
@@ -41,5 +94,29 @@ describe('SavedQueryList', () => {
 
   it('renders a SubMenu', () => {
     expect(wrapper.find(SubMenu)).toExist();
+  });
+
+  it('renders a ListView', () => {
+    expect(wrapper.find(ListView)).toExist();
+  });
+
+  it('fetches saved queries', () => {
+    const callsQ = fetchMock.calls(/saved_query\/\?q/);
+    expect(callsQ).toHaveLength(1);
+    expect(callsQ[0][0]).toMatchInlineSnapshot(
+      `"http://localhost/api/v1/saved_query/?q=(order_column:changed_on_delta_humanized,order_direction:desc,page:0,page_size:25)"`,
+    );
+  });
+
+  it('searches', async () => {
+    const filtersWrapper = wrapper.find(Filters);
+    act(() => {
+      filtersWrapper.find('[name="label"]').first().props().onSubmit('fooo');
+    });
+    await waitForComponentToPaint(wrapper);
+
+    expect(fetchMock.lastCall()[0]).toMatchInlineSnapshot(
+      `"http://localhost/api/v1/saved_query/?q=(filters:!((col:label,opr:all_text,value:fooo)),order_column:changed_on_delta_humanized,order_direction:desc,page:0,page_size:25)"`,
+    );
   });
 });
