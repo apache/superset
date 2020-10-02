8b83130cdec2c2ad97e4174d8da2ed0c1bc526b1
ʈᵃᵢ <tdupreetan@gmail.com>
2020-10-02 09:45:36 -0700

    style: override ace edit error icon (#11139)

--- superset-frontend/webpack.config.js
+++ superset-frontend/webpack.config.js
@@ -386,6 +386,9 @@ const config = {
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: 'file-loader',
+        options: {
+          esModule: false,
+        },
       },
     ],
   },
