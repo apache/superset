69df39fb9660cbfd71076c63c6023fa4b8306386
John Bodley <4567245+john-bodley@users.noreply.github.com>
2020-10-02 01:12:03 -0700

    fix: Alembic migration 18532d70ab98 (#11136)

--- superset/migrations/versions/18532d70ab98_fix_table_unique_constraint_in_mysql.py
+++ superset/migrations/versions/18532d70ab98_fix_table_unique_constraint_in_mysql.py
@@ -28,14 +28,22 @@ down_revision = "3fbbc6e8d654"
 
 from alembic import op
 from sqlalchemy.dialects.mysql.base import MySQLDialect
+from sqlalchemy.engine.reflection import Inspector
+
+from superset.utils.core import generic_find_uq_constraint_name
 
 
 def upgrade():
     bind = op.get_bind()
+
+    # Uniqueness constraint if present only exists in MySQL.
     if isinstance(bind.dialect, MySQLDialect):
-        # index only exists in mysql db
-        with op.get_context().autocommit_block():
-            op.drop_constraint("table_name", "tables", type_="unique")
+        constraint_name = generic_find_uq_constraint_name(
+            "tables", {"table_name"}, Inspector.from_engine(bind)
+        )
+
+        if constraint_name:
+            op.drop_constraint(constraint_name, "tables", type_="unique")
 
 
 def downgrade():
