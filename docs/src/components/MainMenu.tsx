74402d98203f349ae28e190449eb91190a2ce662
Maxime Beauchemin <maximebeauchemin@gmail.com>
2020-10-06 13:51:40 -0700

    fix: 📜 `/docs/api` won't render (#11168)
    * fix: docs/api won't render
    
        fix: docs/api won't render
    
        It's unclear to me exactly why, but docz is flimsy with the way
        things are named and runs into odd collision and error messages.
    
        Somehow renaming the route seem to help. Jut rolling with it after many
        `gatsby clean`
    
    * kill paths-ignore on github-actions
    
    * trigger

--- docs/src/components/MainMenu.tsx
+++ docs/src/components/MainMenu.tsx
@@ -133,9 +133,9 @@ export default class MainMenu extends React.Component {
     const { visible } = this.state;
     return (
       <Layout.Header css={headerStyle}>
-        <Link to="https://superset.incubator.apache.org">
+        <a href="https://superset.incubator.apache.org">
           <img height="50" css={logoStyle} src={logoSvg} alt="logo" />
-        </Link>
+        </a>
         <MenuItems toggleDrawer={this.toggleDrawer} mode="horizontal" />
         <Drawer
           title="Menu"
