ed89389b6b46f9531d04dd6ee9027709ec1eb0d7
riahk <moar.riah@gmail.com>
2020-10-06 15:18:45 -0700

    Add list view

--- superset-frontend/src/views/CRUD/csstemplates/CssTemplatesList.tsx
+++ superset-frontend/src/views/CRUD/csstemplates/CssTemplatesList.tsx
@@ -17,15 +17,169 @@
  * under the License.
  */
 
-import React from 'react';
+import React, { useMemo } from 'react';
 import { t } from '@superset-ui/core';
+import moment from 'moment';
+import { useListViewResource } from 'src/views/CRUD/hooks';
 import withToasts from 'src/messageToasts/enhancers/withToasts';
 import SubMenu from 'src/components/Menu/SubMenu';
+import { IconName } from 'src/components/Icon';
+import ActionsBar, { ActionProps } from 'src/components/ListView/ActionsBar';
+// import ListView, { Filters } from 'src/components/ListView';
+import ListView from 'src/components/ListView';
 
-function CssTemplatesList() {
+const PAGE_SIZE = 25;
+
+interface CssTemplatesListProps {
+  addDangerToast: (msg: string) => void;
+  addSuccessToast: (msg: string) => void;
+}
+
+type TemplateObject = {
+  id?: number;
+  changed_on_delta_humanized: string;
+  created_on: string;
+  created_by: {
+    id: number;
+    first_name: string;
+    last_name: string;
+  };
+  css: string;
+  template_name: string;
+};
+
+function CssTemplatesList({
+  addDangerToast,
+  addSuccessToast,
+}: CssTemplatesListProps) {
+  const {
+    state: {
+      loading,
+      resourceCount: templatesCount,
+      resourceCollection: templates,
+    },
+    hasPerm,
+    fetchData,
+    // refreshData,
+  } = useListViewResource<TemplateObject>(
+    'css_template',
+    t('css templates'),
+    addDangerToast,
+  );
+
+  const canCreate = hasPerm('can_add');
+  const canEdit = hasPerm('can_edit');
+  const canDelete = hasPerm('can_delete');
+
+  const initialSort = [{ id: 'template_name', desc: true }];
+  const columns = useMemo(
+    () => [
+      {
+        accessor: 'template_name',
+        Header: t('Name'),
+      },
+      {
+        Cell: ({
+          row: {
+            original: { created_on: createdOn },
+          },
+        }: any) => {
+          const date = new Date(createdOn);
+          const utc = new Date(
+            Date.UTC(
+              date.getFullYear(),
+              date.getMonth(),
+              date.getDate(),
+              date.getHours(),
+              date.getMinutes(),
+              date.getSeconds(),
+              date.getMilliseconds(),
+            ),
+          );
+
+          return moment(utc).fromNow();
+        },
+        Header: t('Created On'),
+        accessor: 'created_on',
+        size: 'xl',
+      },
+      {
+        accessor: 'created_by',
+        disableSortBy: true,
+        Header: t('Created By'),
+        Cell: ({
+          row: {
+            original: { created_by: createdBy },
+          },
+        }: any) =>
+          createdBy ? `${createdBy.first_name} ${createdBy.last_name}` : '',
+        size: 'xl',
+      },
+      {
+        Cell: ({
+          row: {
+            original: { changed_on_delta_humanized: changedOn },
+          },
+        }: any) => changedOn,
+        Header: t('Last Modified'),
+        accessor: 'changed_on_delta_humanized',
+        size: 'xl',
+      },
+      {
+        Cell: ({ row: { original } }: any) => {
+          const handleEdit = () => {}; // handleDatabaseEdit(original);
+          const handleDelete = () => {}; // openDatabaseDeleteModal(original);
+
+          const actions = [
+            canEdit
+              ? {
+                  label: 'edit-action',
+                  tooltip: t('Edit template'),
+                  placement: 'bottom',
+                  icon: 'edit' as IconName,
+                  onClick: handleEdit,
+                }
+              : null,
+            canDelete
+              ? {
+                  label: 'delete-action',
+                  tooltip: t('Delete template'),
+                  placement: 'bottom',
+                  icon: 'trash' as IconName,
+                  onClick: handleDelete,
+                }
+              : null,
+          ].filter(item => !!item);
+
+          if (!canEdit && !canDelete) {
+            return null;
+          }
+
+          return <ActionsBar actions={actions as ActionProps[]} />;
+        },
+        Header: t('Actions'),
+        id: 'actions',
+        disableSortBy: true,
+        size: 'xl',
+      },
+    ],
+    [canDelete, canCreate],
+  );
+
   return (
     <>
       <SubMenu name={t('CSS Templates')} />
+      <ListView<TemplateObject>
+        className="css-templates-list-view"
+        columns={columns}
+        count={templatesCount}
+        data={templates}
+        fetchData={fetchData}
+        // filters={filters}
+        initialSort={initialSort}
+        loading={loading}
+        pageSize={PAGE_SIZE}
+      />
     </>
   );
 }
