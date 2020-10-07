3c04946e842e2e3c35289aa0e23b7a26c0bc92fd
Ville Brofeldt <33317356+villebro@users.noreply.github.com>
2020-10-07 23:51:07 +0300

    feat(plugin-chart-echarts): add BoxPlot chart (#801)
    * feat(plugin-chart-echarts): add BoxPlot chart
    
    * make headline stand out
    
    * address comments

---
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/plugin-chart-echarts/src/BoxPlot/buildQuery.ts
@@ -0,0 +1,63 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import { buildQueryContext, convertMetric } from '@superset-ui/core';
+import { BoxPlotQueryFormData, BoxPlotQueryObjectType } from './types';
+
+const PERCENTILE_REGEX = /(\d+)\/(\d+) percentiles/;
+
+export default function buildQuery(formData: BoxPlotQueryFormData) {
+  const { whiskerOptions, columns, groupby, metrics: formDataMetrics } = formData;
+  // TODO: Refactor superset-ui-cre/query and remove QueryFormResidual types which are causing confusion
+  // @ts-ignore
+  const metrics = formDataMetrics.map(metric => convertMetric(metric).label);
+  return buildQueryContext(formData, baseQueryObject => {
+    let type: BoxPlotQueryObjectType;
+    let percentiles: [number, number] | undefined;
+    const percentileMatch = PERCENTILE_REGEX.exec(whiskerOptions as string);
+    const distributionColumns = columns || [];
+
+    if (whiskerOptions === 'Tukey') type = 'tukey';
+    else if (whiskerOptions === 'Min/max (no outliers)') type = 'min/max';
+    else if (percentileMatch) {
+      type = 'percentile';
+      percentiles = [parseInt(percentileMatch[1], 10), parseInt(percentileMatch[2], 10)];
+    } else {
+      throw new Error(`Unsupported whisker type: ${whiskerOptions}`);
+    }
+    console.log(formData);
+    return [
+      {
+        ...baseQueryObject,
+        is_timeseries: distributionColumns.length === 0,
+        groupby: (groupby || []).concat(distributionColumns),
+        post_processing: [
+          {
+            operation: 'boxplot',
+            options: {
+              type,
+              percentiles,
+              groupby,
+              metrics,
+            },
+          },
+        ],
+      },
+    ];
+  });
+}
