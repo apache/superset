e0d75c7908d2e2ccd4eabad45b11537376f1a602
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2020-10-09 09:27:29 +0200

    Create styled Antd Tabs and Dropdown

--- superset-frontend/src/common/components/Tabs.tsx
+++ superset-frontend/src/common/components/Tabs.tsx
@@ -52,7 +52,10 @@ const StyledTabs = styled(AntdTabs, {
     `};
 
   .ant-tabs-tab-btn {
+    display: flex;
     flex: 1 1 auto;
+    align-items: center;
+    justify-content: center;
     font-size: ${({ theme }) => theme.typography.sizes.s}px;
     text-align: center;
     text-transform: uppercase;
