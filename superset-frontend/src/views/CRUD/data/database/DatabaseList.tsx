22aa5c911b2adaeb63ce297d68bfb6fce33a824e
Maxime Beauchemin <maximebeauchemin@gmail.com>
2020-10-12 11:55:34 -0700

    sorting by main metric DESC by default

--- superset-frontend/src/views/CRUD/data/database/DatabaseList.tsx
+++ superset-frontend/src/views/CRUD/data/database/DatabaseList.tsx
@@ -180,7 +180,7 @@ function DatabaseList({ addDangerToast, addSuccessToast }: DatabaseListProps) {
         Header: (
           <TooltipWrapper
             label="allow-dml-header"
-            tooltip={t('Allow Data Danipulation Language')}
+            tooltip={t('Allow Data Manipulation Language')}
             placement="top"
           >
             <span>{t('DML')}</span>
