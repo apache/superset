1bee785bc28b28e7e4949e1319a36f0574b29169
Jesse Yang <jesse.yang@airbnb.com>
2020-10-14 10:44:06 -0700

    refactor: use contextmanager for event_logger decorators (#11222)

--- superset/utils/log.py
+++ superset/utils/log.py
@@ -19,9 +19,10 @@ import inspect
 import json
 import logging
 import textwrap
+import time
 from abc import ABC, abstractmethod
-from datetime import datetime
-from typing import Any, Callable, cast, Optional, Type
+from contextlib import contextmanager
+from typing import Any, Callable, cast, Iterator, Optional, Type
 
 from flask import current_app, g, request
 from sqlalchemy.exc import SQLAlchemyError
@@ -36,58 +37,76 @@ class AbstractEventLogger(ABC):
     ) -> None:
         pass
 
-    def log_this(self, f: Callable[..., Any]) -> Callable[..., Any]:
+    @contextmanager
+    def log_context(self, action: str) -> Iterator[Callable[..., None]]:
+        """
+        Log an event while reading information from the request context.
+        `kwargs` will be appended directly to the log payload.
+        """
         from superset.views.core import get_form_data
+
+        start_time = time.time()
+        referrer = request.referrer[:1000] if request.referrer else None
+        user_id = g.user.get_id() if hasattr(g, "user") and g.user else None
+        payload = request.form.to_dict() or {}
+        # request parameters can overwrite post body
+        payload.update(request.args.to_dict())
+
+        # yield a helper to update additional kwargs
+        yield lambda **kwargs: payload.update(kwargs)
+
+        dashboard_id = payload.get("dashboard_id")
+
+        if "form_data" in payload:
+            form_data, _ = get_form_data()
+            payload["form_data"] = form_data
+            slice_id = form_data.get("slice_id")
+        else:
+            slice_id = payload.get("slice_id")
+
+        try:
+            slice_id = int(slice_id)  # type: ignore
+        except (TypeError, ValueError):
+            slice_id = 0
+
+        self.stats_logger.incr(action)
+
+        # bulk insert
+        try:
+            explode_by = payload.get("explode")
+            records = json.loads(payload.get(explode_by))  # type: ignore
+        except Exception:  # pylint: disable=broad-except
+            records = [payload]
+
+        self.log(
+            user_id,
+            action,
+            records=records,
+            dashboard_id=dashboard_id,
+            slice_id=slice_id,
+            duration_ms=round((time.time() - start_time) * 1000),
+            referrer=referrer,
+        )
+
+    def log_this(self, f: Callable[..., Any]) -> Callable[..., Any]:
+        @functools.wraps(f)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            with self.log_context(f.__name__) as log:
+                value = f(*args, **kwargs)
+                log(**kwargs)
+            return value
+
+        return wrapper
+
+    def log_manually(self, f: Callable[..., Any]) -> Callable[..., Any]:
+        """Allow a function to manually update"""
 
         @functools.wraps(f)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
-            user_id = None
-            if hasattr(g, "user") and g.user:
-                user_id = g.user.get_id()
-            payload = request.form.to_dict() or {}
-
-            # request parameters can overwrite post body
-            request_params = request.args.to_dict()
-            payload.update(request_params)
-            payload.update(kwargs)
-
-            dashboard_id = payload.get("dashboard_id")
-
-            if "form_data" in payload:
-                form_data, _ = get_form_data()
-                payload["form_data"] = form_data
-                slice_id = form_data.get("slice_id")
-            else:
-                slice_id = payload.get("slice_id")
-
-            try:
-                slice_id = int(slice_id)  # type: ignore
-            except (TypeError, ValueError):
-                slice_id = 0
-
-            self.stats_logger.incr(f.__name__)
-            start_dttm = datetime.now()
-            value = f(*args, **kwargs)
-            duration_ms = (datetime.now() - start_dttm).total_seconds() * 1000
-
-            # bulk insert
-            try:
-                explode_by = payload.get("explode")
-                records = json.loads(payload.get(explode_by))  # type: ignore
-            except Exception:  # pylint: disable=broad-except
-                records = [payload]
-
-            referrer = request.referrer[:1000] if request.referrer else None
-
-            self.log(
-                user_id,
-                f.__name__,
-                records=records,
-                dashboard_id=dashboard_id,
-                slice_id=slice_id,
-                duration_ms=duration_ms,
-                referrer=referrer,
-            )
+            with self.log_context(f.__name__) as log:
+                # updated_log_payload should be either the last positional
+                # argument or one of the named arguments of the decorated function
+                value = f(*args, update_log_payload=log, **kwargs)
             return value
 
         return wrapper
@@ -141,6 +160,8 @@ def get_event_logger_from_cfg_value(cfg_value: Any) -> AbstractEventLogger:
 
 
 class DBEventLogger(AbstractEventLogger):
+    """Event logger that commits logs to Superset DB"""
+
     def log(  # pylint: disable=too-many-locals
         self, user_id: Optional[int], action: str, *args: Any, **kwargs: Any
     ) -> None:
