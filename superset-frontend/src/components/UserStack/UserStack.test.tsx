fa8847b2cfc7214eae930ffbdda4e25f502146c6
Tai Dupree <tdupreetan@gmail.com>
2020-10-15 14:42:36 -0700

    add specs for utils

--- superset-frontend/src/components/UserStack/UserStack.test.tsx
+++ superset-frontend/src/components/UserStack/UserStack.test.tsx
@@ -21,13 +21,14 @@ import { styledMount as mount } from 'spec/helpers/theming';
 
 import { Avatar } from 'src/common/components';
 import UserStack from '.';
+import { getRandomColor } from './utils';
 
 const users = [...new Array(10)].map((_, i) => ({
   first_name: 'user',
   last_name: `${i}`,
 }));
 
-describe('UerStack', () => {
+describe('UserStack', () => {
   const wrapper = mount(<UserStack users={users} />);
 
   it('is a valid element', () => {
@@ -40,5 +41,31 @@ describe('UerStack', () => {
 
   it('hides overflow', () => {
     expect(wrapper.find(Avatar).length).toBe(5);
+  });
+});
+
+describe('utils', () => {
+  describe('getRandomColor', () => {
+    const colors = ['color1', 'color2', 'color3'];
+
+    it('produces the same color for the same input values', () => {
+      const name = 'foo';
+      expect(getRandomColor(name, colors)).toEqual(
+        getRandomColor(name, colors),
+      );
+    });
+
+    it('produces a different color for different input values', () => {
+      expect(getRandomColor('foo', colors)).not.toEqual(
+        getRandomColor('bar', colors),
+      );
+    });
+
+    it('handles non-ascii input values', () => {
+      expect(getRandomColor('泰', colors)).toMatchInlineSnapshot(`"color1"`);
+      expect(getRandomColor('مُحَمَّد‎', colors)).toMatchInlineSnapshot(
+        `"color2"`,
+      );
+    });
   });
 });
