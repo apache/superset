406107e6e45a77935416e14f89e9b6407909a1be
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2020-10-16 12:04:51 +0200

    Refactored QueryTable into functional component

--- superset-frontend/src/SqlLab/components/QueryTable.jsx
+++ superset-frontend/src/SqlLab/components/QueryTable.jsx
@@ -16,13 +16,12 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import { ProgressBar, Well } from 'react-bootstrap';
 import Label from 'src/components/Label';
 import { t } from '@superset-ui/core';
-import memoize from 'lodash/memoize';
 
 import TableView from 'src/components/TableView';
 import Button from 'src/components/Button';
@@ -48,194 +47,191 @@ const defaultProps = {
   onDbClicked: () => {},
 };
 
-class QueryTable extends React.PureComponent {
-  openQuery(id) {
-    const url = `/superset/sqllab?queryId=${id}`;
-    window.open(url);
-  }
+const openQuery = id => {
+  const url = `/superset/sqllab?queryId=${id}`;
+  window.open(url);
+};
 
-  restoreSql(query) {
-    this.props.actions.queryEditorSetSql({ id: query.sqlEditorId }, query.sql);
-  }
-
-  openQueryInNewTab(query) {
-    this.props.actions.cloneQueryToNewTab(query, true);
-  }
-
-  openAsyncResults(query, displayLimit) {
-    this.props.actions.fetchQueryResults(query, displayLimit);
-  }
-
-  clearQueryResults(query) {
-    this.props.actions.clearQueryResults(query);
-  }
-
-  removeQuery(query) {
-    this.props.actions.removeQuery(query);
-  }
-
-  render() {
-    const data = memoize(() =>
-      this.props.queries
-        .map(query => {
-          const q = { ...query };
-          if (q.endDttm) {
-            q.duration = fDuration(q.startDttm, q.endDttm);
-          }
-          const time = moment(q.startDttm).format().split('T');
-          q.time = (
-            <div>
-              <span>
-                {time[0]} <br /> {time[1]}
-              </span>
-            </div>
-          );
-          q.user = (
-            <Button
-              buttonSize="small"
-              buttonStyle="link"
-              onClick={this.props.onUserClicked.bind(this, q.userId)}
-            >
-              {q.user}
-            </Button>
-          );
-          q.db = (
-            <Button
-              buttonSize="small"
-              buttonStyle="link"
-              onClick={this.props.onDbClicked.bind(this, q.dbId)}
-            >
-              {q.db}
-            </Button>
-          );
-          q.started = moment(q.startDttm).format('HH:mm:ss');
-          q.querylink = (
-            <div style={{ width: '100px' }}>
-              <Button
-                buttonSize="small"
-                buttonStyle="link"
-                onClick={this.openQuery.bind(this, q.queryId)}
-              >
-                <i className="fa fa-external-link m-r-3" />
-                {t('Edit')}
-              </Button>
-            </div>
-          );
-          q.sql = (
-            <Well>
-              <HighlightedSql
-                sql={q.sql}
-                rawSql={q.executedSql}
-                shrink
-                maxWidth={60}
-              />
-            </Well>
-          );
-          if (q.resultsKey) {
-            q.output = (
-              <ModalTrigger
-                bsSize="large"
-                className="ResultsModal"
-                triggerNode={
-                  <Label bsStyle="info" className="pointer">
-                    {t('view results')}
-                  </Label>
-                }
-                modalTitle={t('Data preview')}
-                beforeOpen={this.openAsyncResults.bind(
-                  this,
-                  query,
-                  this.props.displayLimit,
-                )}
-                onExit={this.clearQueryResults.bind(this, query)}
-                modalBody={
-                  <ResultSet
-                    showSql
-                    query={query}
-                    actions={this.props.actions}
-                    height={400}
-                    displayLimit={this.props.displayLimit}
-                  />
-                }
-              />
-            );
-          } else {
-            // if query was run using ctas and force_ctas_schema was set
-            // tempTable will have the schema
-            const schemaUsed =
-              q.ctas && q.tempTable && q.tempTable.includes('.')
-                ? ''
-                : q.schema;
-            q.output = [schemaUsed, q.tempTable].filter(v => v).join('.');
-          }
-          q.progress = (
-            <ProgressBar
-              style={{ width: '75px' }}
-              striped
-              now={q.progress}
-              label={`${q.progress.toFixed(0)}%`}
-            />
-          );
-          let errorTooltip;
-          if (q.errorMessage) {
-            errorTooltip = (
-              <Link tooltip={q.errorMessage}>
-                <i className="fa fa-exclamation-circle text-danger" />
-              </Link>
-            );
-          }
-          q.state = (
-            <div>
-              <QueryStateLabel query={query} />
-              {errorTooltip}
-            </div>
-          );
-          q.actions = (
-            <div style={{ width: '75px' }}>
-              <Link
-                className="fa fa-pencil m-r-3"
-                onClick={this.restoreSql.bind(this, query)}
-                tooltip={t(
-                  'Overwrite text in the editor with a query on this table',
-                )}
-                placement="top"
-              />
-              <Link
-                className="fa fa-plus-circle m-r-3"
-                onClick={this.openQueryInNewTab.bind(this, query)}
-                tooltip={t('Run query in a new tab')}
-                placement="top"
-              />
-              <Link
-                className="fa fa-trash m-r-3"
-                tooltip={t('Remove query from log')}
-                onClick={this.removeQuery.bind(this, query)}
-              />
-            </div>
-          );
-          return q;
-        })
-        .reverse(),
-    );
-    const columns = memoize(() =>
-      this.props.columns.map(column => ({
+const QueryTable = props => {
+  const columns = useMemo(
+    () =>
+      props.columns.map(column => ({
         accessor: column,
         Header: column,
         disableSortBy: true,
       })),
-    );
-    return (
-      <div className="QueryTable">
-        <TableView
-          columns={columns()}
-          data={data()}
-          className="table-condensed"
-          pageSize={50}
-        />
-      </div>
-    );
-  }
-}
+    [props.columns],
+  );
+
+  const data = useMemo(() => {
+    const restoreSql = query => {
+      props.actions.queryEditorSetSql({ id: query.sqlEditorId }, query.sql);
+    };
+
+    const openQueryInNewTab = query => {
+      props.actions.cloneQueryToNewTab(query, true);
+    };
+
+    const openAsyncResults = (query, displayLimit) => {
+      props.actions.fetchQueryResults(query, displayLimit);
+    };
+
+    const clearQueryResults = query => {
+      props.actions.clearQueryResults(query);
+    };
+
+    const removeQuery = query => {
+      props.actions.removeQuery(query);
+    };
+
+    return props.queries
+      .map(query => {
+        const q = { ...query };
+        if (q.endDttm) {
+          q.duration = fDuration(q.startDttm, q.endDttm);
+        }
+        const time = moment(q.startDttm).format().split('T');
+        q.time = (
+          <div>
+            <span>
+              {time[0]} <br /> {time[1]}
+            </span>
+          </div>
+        );
+        q.user = (
+          <Button
+            buttonSize="small"
+            buttonStyle="link"
+            onClick={() => props.onUserClicked(q.userId)}
+          >
+            {q.user}
+          </Button>
+        );
+        q.db = (
+          <Button
+            buttonSize="small"
+            buttonStyle="link"
+            onClick={() => props.onDbClicked(q.dbId)}
+          >
+            {q.db}
+          </Button>
+        );
+        q.started = moment(q.startDttm).format('HH:mm:ss');
+        q.querylink = (
+          <div style={{ width: '100px' }}>
+            <Button
+              buttonSize="small"
+              buttonStyle="link"
+              onClick={() => openQuery(q.queryId)}
+            >
+              <i className="fa fa-external-link m-r-3" />
+              {t('Edit')}
+            </Button>
+          </div>
+        );
+        q.sql = (
+          <Well>
+            <HighlightedSql
+              sql={q.sql}
+              rawSql={q.executedSql}
+              shrink
+              maxWidth={60}
+            />
+          </Well>
+        );
+        if (q.resultsKey) {
+          q.output = (
+            <ModalTrigger
+              bsSize="large"
+              className="ResultsModal"
+              triggerNode={
+                <Label bsStyle="info" className="pointer">
+                  {t('view results')}
+                </Label>
+              }
+              modalTitle={t('Data preview')}
+              beforeOpen={() => openAsyncResults(query, props.displayLimit)}
+              onExit={() => clearQueryResults(query)}
+              modalBody={
+                <ResultSet
+                  showSql
+                  query={query}
+                  actions={props.actions}
+                  height={400}
+                  displayLimit={props.displayLimit}
+                />
+              }
+            />
+          );
+        } else {
+          // if query was run using ctas and force_ctas_schema was set
+          // tempTable will have the schema
+          const schemaUsed =
+            q.ctas && q.tempTable && q.tempTable.includes('.') ? '' : q.schema;
+          q.output = [schemaUsed, q.tempTable].filter(v => v).join('.');
+        }
+        q.progress = (
+          <ProgressBar
+            style={{ width: '75px' }}
+            striped
+            now={q.progress}
+            label={`${q.progress.toFixed(0)}%`}
+          />
+        );
+        let errorTooltip;
+        if (q.errorMessage) {
+          errorTooltip = (
+            <Link tooltip={q.errorMessage}>
+              <i className="fa fa-exclamation-circle text-danger" />
+            </Link>
+          );
+        }
+        q.state = (
+          <div>
+            <QueryStateLabel query={query} />
+            {errorTooltip}
+          </div>
+        );
+        q.actions = (
+          <div style={{ width: '75px' }}>
+            <Link
+              className="fa fa-pencil m-r-3"
+              onClick={() => restoreSql(query)}
+              tooltip={t(
+                'Overwrite text in the editor with a query on this table',
+              )}
+              placement="top"
+            />
+            <Link
+              className="fa fa-plus-circle m-r-3"
+              onClick={() => openQueryInNewTab(query)}
+              tooltip={t('Run query in a new tab')}
+              placement="top"
+            />
+            <Link
+              className="fa fa-trash m-r-3"
+              tooltip={t('Remove query from log')}
+              onClick={() => removeQuery(query)}
+            />
+          </div>
+        );
+        return q;
+      })
+      .reverse();
+  }, [props]);
+
+  return (
+    <div className="QueryTable">
+      <TableView
+        columns={columns}
+        data={data}
+        className="table-condensed"
+        pageSize={50}
+      />
+    </div>
+  );
+};
+
 QueryTable.propTypes = propTypes;
 QueryTable.defaultProps = defaultProps;
 
