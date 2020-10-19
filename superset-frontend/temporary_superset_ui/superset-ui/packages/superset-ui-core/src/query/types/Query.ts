00eabf7c1f5f2ea5ec54223a0171513f2f485c54
Ville Brofeldt <33317356+villebro@users.noreply.github.com>
2020-10-19 20:39:23 +0300

    feat(core): add applied_time_extras to QueryObject (#809)
    * feat(core): add applied_time_extras to QueryObject
    
    * address review comments

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-core/src/query/types/Query.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-core/src/query/types/Query.ts
@@ -2,7 +2,7 @@
 import { DatasourceType } from './Datasource';
 import { AdhocMetric } from './Metric';
 import { BinaryOperator, SetOperator, UnaryOperator } from './Operator';
-import { TimeRange } from './Time';
+import { AppliedTimeExtras, TimeRange } from './Time';
 import { QueryFormDataMetric, QueryFormResidualDataValue } from './QueryFormData';
 
 export type QueryObjectFilterClause = {
@@ -46,6 +46,8 @@ export type ResidualQueryObjectData = {
 };
 
 export type QueryObject = {
+  /** Time filters that have been applied to the query object */
+  applied_time_extras?: AppliedTimeExtras;
   /** Columns to group by */
   groupby?: string[];
   /** Metrics */
