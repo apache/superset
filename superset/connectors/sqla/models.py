542144d28a051e7356f1006f917e5dafe1ce318d
Lily Kuang <lily@preset.io>
2020-10-20 09:38:57 -0700

    fix: update table_name and schema on dataset editor (#11218)
    * fix dataset editor for updating table_name and schema
    
    * update test
    
    * remove table_name from restrict list
    
    * fix pylint
    
    * fix cypress test
    
    * fix test

--- superset/connectors/sqla/models.py
+++ superset/connectors/sqla/models.py
@@ -488,9 +488,7 @@ class SqlaTable(  # pylint: disable=too-many-public-methods,too-many-instance-at
         "filter_select_enabled",
         "fetch_values_predicate",
     ]
-    update_from_object_fields = [
-        f for f in export_fields if f not in ("table_name", "database_id")
-    ]
+    update_from_object_fields = [f for f in export_fields if not f == "database_id"]
     export_parent = "database"
     export_children = ["metrics", "columns"]
 
