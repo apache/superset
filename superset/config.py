f3ce7dd3191916201f757d63867dc2762aaaa594
Evan Rusackas <evan@preset.io>
2020-10-21 15:07:27 -0700

    feat: adding feature flags to escape/hide html in markdown (#11340)
    * utilizing feature flag for html escapement
    
    * use src alias
    
    * Feature flag to allow hiding of HTML tags
    
    * strips js attr
    
    * better feature flag naming
    
    * simplifying
    
    * Adding comments to new feature flags
    
    * shorter comment, because pylint,
    
    * pedantic change to get black to format a file

--- superset/config.py
+++ superset/config.py
@@ -313,6 +313,10 @@ DEFAULT_FEATURE_FLAGS: Dict[str, bool] = {
     "TAGGING_SYSTEM": False,
     "SQLLAB_BACKEND_PERSISTENCE": False,
     "LISTVIEWS_DEFAULT_CARD_VIEW": False,
+    # When True, this flag allows display of HTML tags in Markdown components
+    "DISPLAY_MARKDOWN_HTML": True,
+    # When True, this escapes HTML (rather than rendering it) in Markdown components
+    "ESCAPE_MARKDOWN_HTML": False,
 }
 
 # Set the default view to card/grid view if thumbnail support is enabled.
