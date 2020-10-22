99d7878423892877d91b9b3a85fe3e2f17b691b2
riahk <moar.riah@gmail.com>
2020-10-22 14:55:24 -0700

    Annotation Layers routing + basic list view + new empty state

--- superset/views/annotations.py
+++ superset/views/annotations.py
@@ -28,6 +28,7 @@ from superset.constants import RouteMethod
 from superset.models.annotations import Annotation, AnnotationLayer
 from superset.typing import FlaskResponse
 from superset.views.base import SupersetModelView
+from superset.typing import FlaskResponse
 
 
 class StartEndDttmValidator:  # pylint: disable=too-few-public-methods
