6080950a9b8d42cd336d85f43f6a745a2aff0d8a
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2020-10-23 10:04:33 +0200

    Refactor TimeTable into functional component

--- superset-frontend/src/visualizations/TimeTable/TimeTable.jsx
+++ superset-frontend/src/visualizations/TimeTable/TimeTable.jsx
@@ -16,10 +16,9 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import Mustache from 'mustache';
-import memoize from 'lodash/memoize';
 import { scaleLinear } from 'd3-scale';
 import TableView from 'src/components/TableView';
 import { formatNumber, formatTime, styled } from '@superset-ui/core';
@@ -98,33 +97,174 @@ const TimeTableStyles = styled.div`
   }
 `;
 
-class TimeTable extends React.PureComponent {
-  memoizedColumns = memoize(columnConfigs => [
-    { accessor: 'metric', Header: 'Metric' },
-    ...columnConfigs.map((columnConfig, i) => ({
-      accessor: columnConfig.key,
-      cellProps: columnConfig.colType === 'spark' && { style: { width: '1%' } },
-      Header: () => (
-        <>
-          {columnConfig.label}{' '}
-          {columnConfig.tooltip && (
-            <InfoTooltipWithTrigger
-              tooltip={columnConfig.tooltip}
-              label={`tt-col-${i}`}
-              placement="top"
-            />
-          )}
-        </>
-      ),
-      sortType: (rowA, rowB, columnId) => {
-        const rowAVal = rowA.values[columnId].props['data-value'];
-        const rowBVal = rowB.values[columnId].props['data-value'];
-        return rowAVal - rowBVal;
-      },
-    })),
-  ]);
+const TimeTable = ({
+  className,
+  height,
+  data,
+  columnConfigs,
+  rowType,
+  rows,
+  url,
+}) => {
+  const memoizedColumns = useMemo(
+    () => [
+      { accessor: 'metric', Header: 'Metric' },
+      ...columnConfigs.map((columnConfig, i) => ({
+        accessor: columnConfig.key,
+        cellProps: columnConfig.colType === 'spark' && {
+          style: { width: '1%' },
+        },
+        Header: () => (
+          <>
+            {columnConfig.label}{' '}
+            {columnConfig.tooltip && (
+              <InfoTooltipWithTrigger
+                tooltip={columnConfig.tooltip}
+                label={`tt-col-${i}`}
+                placement="top"
+              />
+            )}
+          </>
+        ),
+        sortType: (rowA, rowB, columnId) => {
+          const rowAVal = rowA.values[columnId].props['data-value'];
+          const rowBVal = rowB.values[columnId].props['data-value'];
+          return rowAVal - rowBVal;
+        },
+      })),
+    ],
+    [columnConfigs],
+  );
 
-  memoizedRows = memoize((data, rows, columnConfigs) => {
+  const memoizedRows = useMemo(() => {
+    const renderSparklineCell = (valueField, column, entries) => {
+      let sparkData;
+      if (column.timeRatio) {
+        // Period ratio sparkline
+        sparkData = [];
+        for (let i = column.timeRatio; i < entries.length; i += 1) {
+          const prevData = entries[i - column.timeRatio][valueField];
+          if (prevData && prevData !== 0) {
+            sparkData.push(entries[i][valueField] / prevData);
+          } else {
+            sparkData.push(null);
+          }
+        }
+      } else {
+        sparkData = entries.map(d => d[valueField]);
+      }
+
+      return (
+        <SparklineCell
+          width={parseInt(column.width, 10) || 300}
+          height={parseInt(column.height, 10) || 50}
+          data={sparkData}
+          data-value={sparkData[sparkData.length - 1]}
+          ariaLabel={`spark-${valueField}`}
+          numberFormat={column.d3format}
+          yAxisBounds={column.yAxisBounds}
+          showYAxis={column.showYAxis}
+          renderTooltip={({ index }) => (
+            <div>
+              <strong>{formatNumber(column.d3format, sparkData[index])}</strong>
+              <div>
+                {formatTime(
+                  column.dateFormat,
+                  moment.utc(entries[index].time).toDate(),
+                )}
+              </div>
+            </div>
+          )}
+        />
+      );
+    };
+
+    const renderValueCell = (valueField, column, reversedEntries) => {
+      const recent = reversedEntries[0][valueField];
+      let v;
+      let errorMsg;
+      if (column.colType === 'time') {
+        // Time lag ratio
+        const timeLag = column.timeLag || 0;
+        const totalLag = Object.keys(reversedEntries).length;
+        if (timeLag >= totalLag) {
+          errorMsg = `The time lag set at ${timeLag} is too large for the length of data at ${reversedEntries.length}. No data available.`;
+        } else {
+          v = reversedEntries[timeLag][valueField];
+        }
+        if (column.comparisonType === 'diff') {
+          v = recent - v;
+        } else if (column.comparisonType === 'perc') {
+          v = recent / v;
+        } else if (column.comparisonType === 'perc_change') {
+          v = recent / v - 1;
+        }
+        v = v || 0;
+      } else if (column.colType === 'contrib') {
+        // contribution to column total
+        v =
+          recent /
+          Object.keys(reversedEntries[0])
+            .map(k => (k !== 'time' ? reversedEntries[0][k] : null))
+            .reduce((a, b) => a + b);
+      } else if (column.colType === 'avg') {
+        // Average over the last {timeLag}
+        v =
+          reversedEntries
+            .map((k, i) => (i < column.timeLag ? k[valueField] : 0))
+            .reduce((a, b) => a + b) / column.timeLag;
+      }
+
+      const color = colorFromBounds(v, column.bounds);
+
+      return (
+        <span
+          key={column.key}
+          data-value={v}
+          style={
+            color && {
+              boxShadow: `inset 0px -2.5px 0px 0px ${color}`,
+              borderRight: '2px solid #fff',
+            }
+          }
+        >
+          {errorMsg ? (
+            { errorMsg }
+          ) : (
+            <span style={{ color }}>
+              <FormattedNumber num={v} format={column.d3format} />
+            </span>
+          )}
+        </span>
+      );
+    };
+
+    const renderLeftCell = row => {
+      const context = { metric: row };
+      const fullUrl = url ? Mustache.render(url, context) : null;
+
+      if (rowType === 'column') {
+        const column = row;
+        if (fullUrl) {
+          return (
+            <a href={fullUrl} rel="noopener noreferrer" target="_blank">
+              {column.label}
+            </a>
+          );
+        }
+        return column.label;
+      }
+
+      return (
+        <MetricOption
+          metric={row}
+          url={fullUrl}
+          showFormula={false}
+          openInNewWindow
+        />
+      );
+    };
+
     const entries = Object.keys(data)
       .sort()
       .map(time => ({ ...data[time], time }));
@@ -136,7 +276,7 @@ class TimeTable extends React.PureComponent {
         if (columnConfig.colType === 'spark') {
           return {
             ...acc,
-            [columnConfig.key]: this.renderSparklineCell(
+            [columnConfig.key]: renderSparklineCell(
               valueField,
               columnConfig,
               entries,
@@ -145,181 +285,39 @@ class TimeTable extends React.PureComponent {
         }
         return {
           ...acc,
-          [columnConfig.key]: this.renderValueCell(
+          [columnConfig.key]: renderValueCell(
             valueField,
             columnConfig,
             reversedEntries,
           ),
         };
       }, {});
-      return { ...row, ...cellValues, metric: this.renderLeftCell(row) };
+      return { ...row, ...cellValues, metric: renderLeftCell(row) };
     });
-  });
+  }, [columnConfigs, data, rowType, rows, url]);
 
-  initialSort = [{ id: 'metric', desc: false }];
+  const defaultSort =
+    rowType === 'column' && columnConfigs.length
+      ? [
+          {
+            id: columnConfigs[0].key,
+            desc: 'true',
+          },
+        ]
+      : [];
 
-  renderLeftCell(row) {
-    const { rowType, url } = this.props;
-    const context = { metric: row };
-    const fullUrl = url ? Mustache.render(url, context) : null;
-
-    if (rowType === 'column') {
-      const column = row;
-      if (fullUrl) {
-        return (
-          <a href={fullUrl} rel="noopener noreferrer" target="_blank">
-            {column.label}
-          </a>
-        );
-      }
-      return column.label;
-    }
-
-    return (
-      <MetricOption
-        metric={row}
-        url={fullUrl}
-        showFormula={false}
-        openInNewWindow
+  return (
+    <TimeTableStyles className={`time-table ${className}`} height={height}>
+      <TableView
+        className="table-no-hover"
+        columns={memoizedColumns}
+        data={memoizedRows}
+        initialSortBy={defaultSort}
+        withPagination={false}
       />
-    );
-  }
-
-  renderSparklineCell(valueField, column, entries) {
-    let sparkData;
-    if (column.timeRatio) {
-      // Period ratio sparkline
-      sparkData = [];
-      for (let i = column.timeRatio; i < entries.length; i += 1) {
-        const prevData = entries[i - column.timeRatio][valueField];
-        if (prevData && prevData !== 0) {
-          sparkData.push(entries[i][valueField] / prevData);
-        } else {
-          sparkData.push(null);
-        }
-      }
-    } else {
-      sparkData = entries.map(d => d[valueField]);
-    }
-
-    return (
-      <SparklineCell
-        width={parseInt(column.width, 10) || 300}
-        height={parseInt(column.height, 10) || 50}
-        data={sparkData}
-        data-value={sparkData[sparkData.length - 1]}
-        ariaLabel={`spark-${valueField}`}
-        numberFormat={column.d3format}
-        yAxisBounds={column.yAxisBounds}
-        showYAxis={column.showYAxis}
-        renderTooltip={({ index }) => (
-          <div>
-            <strong>{formatNumber(column.d3format, sparkData[index])}</strong>
-            <div>
-              {formatTime(
-                column.dateFormat,
-                moment.utc(entries[index].time).toDate(),
-              )}
-            </div>
-          </div>
-        )}
-      />
-    );
-  }
-
-  renderValueCell(valueField, column, reversedEntries) {
-    const recent = reversedEntries[0][valueField];
-    let v;
-    let errorMsg;
-    if (column.colType === 'time') {
-      // Time lag ratio
-      const timeLag = column.timeLag || 0;
-      const totalLag = Object.keys(reversedEntries).length;
-      if (timeLag >= totalLag) {
-        errorMsg = `The time lag set at ${timeLag} is too large for the length of data at ${reversedEntries.length}. No data available.`;
-      } else {
-        v = reversedEntries[timeLag][valueField];
-      }
-      if (column.comparisonType === 'diff') {
-        v = recent - v;
-      } else if (column.comparisonType === 'perc') {
-        v = recent / v;
-      } else if (column.comparisonType === 'perc_change') {
-        v = recent / v - 1;
-      }
-      v = v || 0;
-    } else if (column.colType === 'contrib') {
-      // contribution to column total
-      v =
-        recent /
-        Object.keys(reversedEntries[0])
-          .map(k => (k !== 'time' ? reversedEntries[0][k] : null))
-          .reduce((a, b) => a + b);
-    } else if (column.colType === 'avg') {
-      // Average over the last {timeLag}
-      v =
-        reversedEntries
-          .map((k, i) => (i < column.timeLag ? k[valueField] : 0))
-          .reduce((a, b) => a + b) / column.timeLag;
-    }
-
-    const color = colorFromBounds(v, column.bounds);
-
-    return (
-      <span
-        key={column.key}
-        data-value={v}
-        style={
-          color && {
-            boxShadow: `inset 0px -2.5px 0px 0px ${color}`,
-            borderRight: '2px solid #fff',
-          }
-        }
-      >
-        {errorMsg ? (
-          { errorMsg }
-        ) : (
-          <span style={{ color }}>
-            <FormattedNumber num={v} format={column.d3format} />
-          </span>
-        )}
-      </span>
-    );
-  }
-
-  render() {
-    const {
-      className,
-      height,
-      data,
-      columnConfigs,
-      rowType,
-      rows,
-    } = this.props;
-
-    const defaultSort =
-      rowType === 'column' && columnConfigs.length
-        ? [
-            {
-              id: columnConfigs[0].key,
-              desc: 'true',
-            },
-          ]
-        : [];
-
-    return (
-      <TimeTableStyles className={`time-table ${className}`} height={height}>
-        <TableView
-          className="table-no-hover"
-          columns={this.memoizedColumns(columnConfigs)}
-          data={this.memoizedRows(data, rows, columnConfigs)}
-          initialSortBy={defaultSort}
-          withPagination={false}
-        />
-      </TimeTableStyles>
-    );
-  }
-}
+    </TimeTableStyles>
+  );
+};
 
 TimeTable.propTypes = propTypes;
 TimeTable.defaultProps = defaultProps;
