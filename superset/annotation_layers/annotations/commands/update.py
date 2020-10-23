7fe8233630f9ecc18e3f27da0786eb35099c2b90
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2020-10-23 09:41:17 +0100

    feat: annotations REST API (#11344)
    * feat(api): annotations and layers CRUD REST API
    
    * lint
    
    * annotations API and tests
    
    * fix openapi spec
    
    * fix openapi spec
    
    * fix openapi spec
    
    * annotations bulk delete
    
    * fix openapi spec
    
    * fix older tests
    
    * fix older tests
    
    * small lint fixes
    
    * layer_id to pk to keep broad coherence on openapi spec
    
    * fix openapi spec
    
    * one more test and validation
    
    * fix test name
    
    * fix test
    
    * fix bulk delete
    
    * add name validation
    
    * annotation uniqueness validation
    
    * lint
    
    * add sorting reqs and tests
    
    * add statsd metrics
    
    * Update superset/annotation_layers/annotations/dao.py
    
    Co-authored-by: Ville Brofeldt <33317356+villebro@users.noreply.github.com>
    
    * Update tests/core_tests.py
    
    Co-authored-by: Ville Brofeldt <33317356+villebro@users.noreply.github.com>
    
    * address comments
    
    * address my comment :)
    
    * fix, address comments
    
    * lint
    
    Co-authored-by: riahk <moar.riah@gmail.com>
    Co-authored-by: Ville Brofeldt <33317356+villebro@users.noreply.github.com>

---
+++ superset/annotation_layers/annotations/commands/update.py
@@ -0,0 +1,92 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+import logging
+from datetime import datetime
+from typing import Any, Dict, List, Optional
+
+from flask_appbuilder.models.sqla import Model
+from flask_appbuilder.security.sqla.models import User
+from marshmallow import ValidationError
+
+from superset.annotation_layers.annotations.commands.exceptions import (
+    AnnotationDatesValidationError,
+    AnnotationInvalidError,
+    AnnotationNotFoundError,
+    AnnotationUniquenessValidationError,
+    AnnotationUpdateFailedError,
+)
+from superset.annotation_layers.annotations.dao import AnnotationDAO
+from superset.annotation_layers.commands.exceptions import AnnotationLayerNotFoundError
+from superset.annotation_layers.dao import AnnotationLayerDAO
+from superset.commands.base import BaseCommand
+from superset.dao.exceptions import DAOUpdateFailedError
+from superset.models.annotations import Annotation
+
+logger = logging.getLogger(__name__)
+
+
+class UpdateAnnotationCommand(BaseCommand):
+    def __init__(self, user: User, model_id: int, data: Dict[str, Any]):
+        self._actor = user
+        self._model_id = model_id
+        self._properties = data.copy()
+        self._model: Optional[Annotation] = None
+
+    def run(self) -> Model:
+        self.validate()
+        try:
+            annotation = AnnotationDAO.update(self._model, self._properties)
+        except DAOUpdateFailedError as ex:
+            logger.exception(ex.exception)
+            raise AnnotationUpdateFailedError()
+        return annotation
+
+    def validate(self) -> None:
+        exceptions: List[ValidationError] = list()
+        layer_id: Optional[int] = self._properties.get("layer")
+        short_descr: str = self._properties.get("short_descr", "")
+
+        # Validate/populate model exists
+        self._model = AnnotationDAO.find_by_id(self._model_id)
+        if not self._model:
+            raise AnnotationNotFoundError()
+        # Validate/populate layer exists
+        if layer_id:
+            annotation_layer = AnnotationLayerDAO.find_by_id(layer_id)
+            if not annotation_layer:
+                raise AnnotationLayerNotFoundError()
+            self._properties["layer"] = annotation_layer
+        else:
+            self._properties["layer"] = self._model.layer
+
+        # Validate short descr uniqueness on this layer
+        if not AnnotationDAO.validate_update_uniqueness(
+            layer_id, short_descr, annotation_id=self._model_id,
+        ):
+            exceptions.append(AnnotationUniquenessValidationError())
+
+        # validate date time sanity
+        start_dttm: Optional[datetime] = self._properties.get("start_dttm")
+        end_dttm: Optional[datetime] = self._properties.get("end_dttm")
+
+        if start_dttm and end_dttm and end_dttm < start_dttm:
+            exceptions.append(AnnotationDatesValidationError())
+
+        if exceptions:
+            exception = AnnotationInvalidError()
+            exception.add_list(exceptions)
+            raise exception
---
+++ superset/annotation_layers/annotations/schemas.py
@@ -0,0 +1,80 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+import json
+from typing import Union
+
+from marshmallow import fields, Schema, ValidationError
+from marshmallow.validate import Length
+
+from superset.exceptions import SupersetException
+from superset.utils import core as utils
+
+openapi_spec_methods_override = {
+    "get": {"get": {"description": "Get an Annotation layer"}},
+    "get_list": {
+        "get": {
+            "description": "Get a list of Annotation layers, use Rison or JSON "
+            "query parameters for filtering, sorting,"
+            " pagination and for selecting specific"
+            " columns and metadata.",
+        }
+    },
+    "post": {"post": {"description": "Create an Annotation layer"}},
+    "put": {"put": {"description": "Update an Annotation layer"}},
+    "delete": {"delete": {"description": "Delete Annotation layer"}},
+}
+
+get_delete_ids_schema = {"type": "array", "items": {"type": "integer"}}
+
+
+annotation_start_dttm = "The annotation start date time"
+annotation_end_dttm = "The annotation end date time"
+annotation_layer = "The annotation layer id"
+annotation_short_descr = "A short description"
+annotation_long_descr = "A long description"
+annotation_json_metadata = "JSON metadata"
+
+
+def validate_json(value: Union[bytes, bytearray, str]) -> None:
+    try:
+        utils.validate_json(value)
+    except SupersetException:
+        raise ValidationError("JSON not valid")
+
+
+class AnnotationPostSchema(Schema):
+    short_descr = fields.String(
+        description=annotation_short_descr, allow_none=False, validate=[Length(1, 500)]
+    )
+    long_descr = fields.String(description=annotation_long_descr, allow_none=True)
+    start_dttm = fields.DateTime(description=annotation_start_dttm, allow_none=False)
+    end_dttm = fields.DateTime(description=annotation_end_dttm, allow_none=False)
+    json_metadata = fields.String(
+        description=annotation_json_metadata, validate=validate_json, allow_none=True,
+    )
+
+
+class AnnotationPutSchema(Schema):
+    short_descr = fields.String(
+        description=annotation_short_descr, required=False, validate=[Length(1, 500)]
+    )
+    long_descr = fields.String(description=annotation_long_descr, required=False)
+    start_dttm = fields.DateTime(description=annotation_start_dttm, required=False)
+    end_dttm = fields.DateTime(description=annotation_end_dttm, required=False)
+    json_metadata = fields.String(
+        description=annotation_json_metadata, validate=validate_json, required=False
+    )
