1a7a53c6951a498bdc9ce63a031c8b6fbaa5755f
bogdan kyryliuk <bogdankyryliuk@dropbox.com>
2020-10-27 13:47:55 -0700

    Pylint

--- superset/annotation_layers/annotations/schemas.py
+++ superset/annotation_layers/annotations/schemas.py
@@ -14,7 +14,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-import json
 from typing import Union
 
 from marshmallow import fields, Schema, ValidationError
