b8be531d02c2f5f93e9a6cd4f242bd90a243035b
Moriah Kreeger <moar.riah@gmail.com>
2020-11-03 13:01:20 -0800

    feat: annotation layers delete logic + linking w/ annotation view (#11530)

--- superset-frontend/src/views/CRUD/annotationlayers/AnnotationLayersList.tsx
+++ superset-frontend/src/views/CRUD/annotationlayers/AnnotationLayersList.tsx
@@ -18,7 +18,9 @@
  */
 
 import React, { useMemo, useState } from 'react';
-import { t } from '@superset-ui/core';
+import rison from 'rison';
+import { t, SupersetClient } from '@superset-ui/core';
+import { Link, useHistory } from 'react-router-dom';
 import moment from 'moment';
 import { useListViewResource } from 'src/views/CRUD/hooks';
 import { createFetchRelated, createErrorHandler } from 'src/views/CRUD/utils';
@@ -26,8 +28,10 @@ import withToasts from 'src/messageToasts/enhancers/withToasts';
 import SubMenu, { SubMenuProps } from 'src/components/Menu/SubMenu';
 import { IconName } from 'src/components/Icon';
 import ActionsBar, { ActionProps } from 'src/components/ListView/ActionsBar';
-import ListView, { Filters } from 'src/components/ListView';
+import ListView, { ListViewProps, Filters } from 'src/components/ListView';
 import Button from 'src/components/Button';
+import DeleteModal from 'src/components/DeleteModal';
+import ConfirmStatusChange from 'src/components/ConfirmStatusChange';
 import AnnotationLayerModal from './AnnotationLayerModal';
 import { AnnotationLayerObject } from './types';
 
@@ -44,10 +48,16 @@ function AnnotationLayersList({
   addSuccessToast,
 }: AnnotationLayersListProps) {
   const {
-    state: { loading, resourceCount: layersCount, resourceCollection: layers },
+    state: {
+      loading,
+      resourceCount: layersCount,
+      resourceCollection: layers,
+      bulkSelectEnabled,
+    },
     hasPerm,
     fetchData,
     refreshData,
+    toggleBulkSelect,
   } = useListViewResource<AnnotationLayerObject>(
     'annotation_layer',
     t('annotation layers'),
@@ -61,6 +71,44 @@ function AnnotationLayersList({
     currentAnnotationLayer,
     setCurrentAnnotationLayer,
   ] = useState<AnnotationLayerObject | null>(null);
+
+  const [
+    layerCurrentlyDeleting,
+    setLayerCurrentlyDeleting,
+  ] = useState<AnnotationLayerObject | null>(null);
+
+  const handleLayerDelete = ({ id, name }: AnnotationLayerObject) => {
+    SupersetClient.delete({
+      endpoint: `/api/v1/annotation_layer/${id}`,
+    }).then(
+      () => {
+        refreshData();
+        setLayerCurrentlyDeleting(null);
+        addSuccessToast(t('Deleted: %s', name));
+      },
+      createErrorHandler(errMsg =>
+        addDangerToast(t('There was an issue deleting %s: %s', name, errMsg)),
+      ),
+    );
+  };
+
+  const handleBulkLayerDelete = (layersToDelete: AnnotationLayerObject[]) => {
+    SupersetClient.delete({
+      endpoint: `/api/v1/annotation_layer/?q=${rison.encode(
+        layersToDelete.map(({ id }) => id),
+      )}`,
+    }).then(
+      ({ json = {} }) => {
+        refreshData();
+        addSuccessToast(json.message);
+      },
+      createErrorHandler(errMsg =>
+        addDangerToast(
+          t('There was an issue deleting the selected layers: %s', errMsg),
+        ),
+      ),
+    );
+  };
 
   const canCreate = hasPerm('can_add');
   const canEdit = hasPerm('can_edit');
@@ -77,6 +125,28 @@ function AnnotationLayersList({
       {
         accessor: 'name',
         Header: t('Name'),
+        Cell: ({
+          row: {
+            original: { id, name },
+          },
+        }: any) => {
+          let hasHistory = true;
+
+          try {
+            useHistory();
+          } catch (err) {
+            // If error is thrown, we know not to use <Link> in render
+            hasHistory = false;
+          }
+
+          if (hasHistory) {
+            return (
+              <Link to={`/annotationmodelview/${id}/annotation`}>{name}</Link>
+            );
+          }
+
+          return <a href={`/annotationmodelview/${id}/annotation`}>{name}</a>;
+        },
       },
       {
         accessor: 'descr',
@@ -147,7 +217,7 @@ function AnnotationLayersList({
       {
         Cell: ({ row: { original } }: any) => {
           const handleEdit = () => handleAnnotationLayerEdit(original);
-          const handleDelete = () => {}; // openAnnotationLayerDeleteModal(original);
+          const handleDelete = () => setLayerCurrentlyDeleting(original);
 
           const actions = [
             canEdit
@@ -195,6 +265,14 @@ function AnnotationLayersList({
       onClick: () => {
         handleAnnotationLayerEdit(null);
       },
+    });
+  }
+
+  if (canDelete) {
+    subMenuButtons.push({
+      name: t('Bulk Select'),
+      onClick: toggleBulkSelect,
+      buttonStyle: 'secondary',
     });
   }
 
@@ -246,28 +324,69 @@ function AnnotationLayersList({
     slot: EmptyStateButton,
   };
 
+  const onLayerAdd = (id?: number) => {
+    window.location.href = `/annotationmodelview/${id}/annotation`;
+  };
+
   return (
     <>
       <SubMenu name={t('Annotation Layers')} buttons={subMenuButtons} />
       <AnnotationLayerModal
         addDangerToast={addDangerToast}
         layer={currentAnnotationLayer}
-        onLayerAdd={() => refreshData()}
+        onLayerAdd={onLayerAdd}
         onHide={() => setAnnotationLayerModalOpen(false)}
         show={annotationLayerModalOpen}
       />
-      <ListView<AnnotationLayerObject>
-        className="annotation-layers-list-view"
-        columns={columns}
-        count={layersCount}
-        data={layers}
-        fetchData={fetchData}
-        filters={filters}
-        initialSort={initialSort}
-        loading={loading}
-        pageSize={PAGE_SIZE}
-        emptyState={emptyState}
-      />
+      {layerCurrentlyDeleting && (
+        <DeleteModal
+          description={t('This action will permanently delete the layer.')}
+          onConfirm={() => {
+            if (layerCurrentlyDeleting) {
+              handleLayerDelete(layerCurrentlyDeleting);
+            }
+          }}
+          onHide={() => setLayerCurrentlyDeleting(null)}
+          open
+          title={t('Delete Layer?')}
+        />
+      )}
+      <ConfirmStatusChange
+        title={t('Please confirm')}
+        description={t('Are you sure you want to delete the selected layers?')}
+        onConfirm={handleBulkLayerDelete}
+      >
+        {confirmDelete => {
+          const bulkActions: ListViewProps['bulkActions'] = canDelete
+            ? [
+                {
+                  key: 'delete',
+                  name: t('Delete'),
+                  onSelect: confirmDelete,
+                  type: 'danger',
+                },
+              ]
+            : [];
+
+          return (
+            <ListView<AnnotationLayerObject>
+              className="annotation-layers-list-view"
+              columns={columns}
+              count={layersCount}
+              data={layers}
+              fetchData={fetchData}
+              filters={filters}
+              initialSort={initialSort}
+              loading={loading}
+              pageSize={PAGE_SIZE}
+              bulkActions={bulkActions}
+              bulkSelectEnabled={bulkSelectEnabled}
+              disableBulkSelect={toggleBulkSelect}
+              emptyState={emptyState}
+            />
+          );
+        }}
+      </ConfirmStatusChange>
     </>
   );
 }
