1b4cccfabd65d66b7f1a6c9c5de8fba016a16330
Evan Rusackas <evan@preset.io>
2020-11-09 14:03:49 -0800

    unnecessary flag
    Co-Authored-By: Evan Rusackas <evan@preset.io>

--- .github/workflows/bashlib.sh
+++ .github/workflows/bashlib.sh
@@ -186,7 +186,7 @@ cypress-run-all() {
   nohup flask run --no-debugger -p $port >"$flasklog" 2>&1 </dev/null &
   local flaskProcessId=$!
 
-  cypress-run "*/**/*" --parallel --record
+  cypress-run "*/**/*"
 
   # Upload code coverage separately so each page can have separate flags
   # -c will clean existing coverage reports, -F means add flags
