25b45a69a3d69ab96d698191778f525a1d39d13d
Adam Stasiak <adam.stasiak@polidea.com>
2020-11-09 19:20:17 +0100

    Modified test logic - changed user's action to selection from list instead of typing (unstable with cypress)

--- superset-frontend/cypress-base/cypress/integration/dashboard/filter.test.ts
+++ superset-frontend/cypress-base/cypress/integration/dashboard/filter.test.ts
@@ -63,7 +63,6 @@ describe('Dashboard filter', () => {
       cy.wait(aliases);
     });
   });
-  // TODO fix and reactivate this flaky test
   it('should apply filter', () => {
     cy.get('.Select__control input[type=text]')
       .first()
@@ -71,35 +70,23 @@ describe('Dashboard filter', () => {
       .focus();
 
     // should open the filter indicator
-    cy.get('.filter-indicator.active')
+    cy.get('[data-test="filter"]')
       .should('be.visible', { timeout: 10000 })
       .should(nodes => {
         expect(nodes).to.have.length(9);
       });
 
+    cy.get('[data-test="chart-container"]').find('svg').should('be.visible');
+
     cy.get('.Select__control input[type=text]').first().focus().blur();
 
-    // should hide the filter indicator
-    cy.get('.filter-indicator')
-      .not('.active')
-      .should(nodes => {
-        expect(nodes).to.have.length(18);
-      });
-
     cy.get('.Select__control input[type=text]')
       .first()
       .focus()
-      .type('So', { force: true });
+      .type('So', { force: true, delay: 100 });
 
-    cy.get('.Select__menu').first().contains('Create "So"');
-    // Somehow Input loses focus after typing "So" while in Cypress, so
-    // we refocus the input again here. The is not happening in real life.
-    cy.get('.Select__control input[type=text]')
-      .first()
-      .focus()
-      .type('uth Asia{enter}', { force: true, delay: 40 });
+    cy.get('.Select__menu').first().contains('South Asia').click();
 
-    // by default, need to click Apply button to apply filter
     cy.get('.filter_box button').click({ force: true });
 
     // wait again after applied filters
@@ -116,5 +103,7 @@ describe('Dashboard filter', () => {
         });
       });
     });
+
+    // TODO add test with South Asia{enter} type action to select filter
   });
 });
