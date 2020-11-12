10371f0ba703924a9f0a95931d46f962d0186bae
Craig <craig@craigrueda.com>
2020-11-12 11:43:28 -0800

    Fixing script path?

--- .github/workflows/docker-build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "${GITHUB_REF#refs/heads/}"
-git rev-parse HEAD
--- .github/workflows/docker_build_push.sh
+++ .github/workflows/docker_build_push.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+echo "${GITHUB_REF#refs/heads/}"
+git rev-parse HEAD
+
 # Login first
 docker login -u "${DOCKERHUB_USER}" -p "${DOCKERHUB_TOKEN}"
