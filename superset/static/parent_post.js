96cecc81fe87cde3f594504b29b277f1b5078f13
NejcZdovc <nejc.zdovc@3zsistemi.si>
2020-11-21 18:44:41 +0100

    Updates css

--- superset/static/parent_post.js
+++
@@ -1,8 +0,0 @@
-function parent_post() {
-  parent.postMessage({
-    type: "iframeUrl",
-    data: window.location.href
-  }, "*")
-}
-
-window.addEventListener('DOMContentLoaded', parent_post, false)
