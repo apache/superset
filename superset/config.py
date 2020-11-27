9133a641f25836164a0c97fb81adaab86049715c
Amit Miran <47772523+amitmiran137@users.noreply.github.com>
2020-11-27 15:06:57 +0200

    chore: rename comment as well

--- superset/config.py
+++ superset/config.py
@@ -341,7 +341,7 @@ DEFAULT_FEATURE_FLAGS: Dict[str, bool] = {
 }
 
 # Set the default view to card/grid view if thumbnail support is enabled.
-# Setting LISTVIEW_DEFAULT_CARD_VIEW to False will force the default view to
+# Setting LISTVIEWS_DEFAULT_CARD_VIEW to False will force the default view to
 # always be the table layout
 if DEFAULT_FEATURE_FLAGS["THUMBNAILS"]:
     DEFAULT_FEATURE_FLAGS["LISTVIEWS_DEFAULT_CARD_VIEW"] = True
