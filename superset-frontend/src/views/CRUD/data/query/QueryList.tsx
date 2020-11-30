8f9a47daaae88aa2eb30b11a1b474eec1abb2d63
ʈᵃᵢ <tdupreetan@gmail.com>
2020-11-30 14:21:53 -0800

    feat: list view filters for Query History  (#11702)

--- superset-frontend/src/views/CRUD/data/query/QueryList.tsx
+++ superset-frontend/src/views/CRUD/data/query/QueryList.tsx
@@ -19,21 +19,29 @@
 import React, { useMemo, useState, useCallback } from 'react';
 import { SupersetClient, t, styled } from '@superset-ui/core';
 import moment from 'moment';
-
-import { createErrorHandler, shortenSQL } from 'src/views/CRUD/utils';
+import {
+  createFetchRelated,
+  createFetchDistinct,
+  createErrorHandler,
+  shortenSQL,
+} from 'src/views/CRUD/utils';
 import withToasts from 'src/messageToasts/enhancers/withToasts';
 import { useListViewResource } from 'src/views/CRUD/hooks';
 import SubMenu, { SubMenuProps } from 'src/components/Menu/SubMenu';
 import { Popover } from 'src/common/components';
 import { commonMenuData } from 'src/views/CRUD/data/common';
-import ListView, { Filters, ListViewProps } from 'src/components/ListView';
+import ListView, {
+  Filters,
+  FilterOperators,
+  ListViewProps,
+} from 'src/components/ListView';
 import Icon, { IconName } from 'src/components/Icon';
 import { Tooltip } from 'src/common/components/Tooltip';
 import SyntaxHighlighter from 'react-syntax-highlighter/dist/cjs/light';
 import sql from 'react-syntax-highlighter/dist/cjs/languages/hljs/sql';
 import github from 'react-syntax-highlighter/dist/cjs/styles/hljs/github';
 import { DATETIME_WITH_TIME_ZONE, TIME_WITH_MS } from 'src/constants';
-import { QueryObject } from 'src/views/CRUD/types';
+import { QueryObject, QueryObjectColumns } from 'src/views/CRUD/types';
 
 import QueryPreviewModal from './QueryPreviewModal';
 
@@ -49,7 +57,7 @@ const TopAlignedListView = styled(ListView)<ListViewProps<QueryObject>>`
 SyntaxHighlighter.registerLanguage('sql', sql);
 const StyledSyntaxHighlighter = styled(SyntaxHighlighter)`
   height: ${({ theme }) => theme.gridUnit * 26}px;
-  overflow-x: hidden !important; /* needed to override inline styles */
+  overflow: hidden !important; /* needed to override inline styles */
   text-overflow: ellipsis;
   white-space: nowrap;
 `;
@@ -121,7 +129,7 @@ function QueryList({ addDangerToast, addSuccessToast }: QueryListProps) {
     ...commonMenuData,
   };
 
-  const initialSort = [{ id: 'changed_on', desc: true }];
+  const initialSort = [{ id: QueryObjectColumns.start_time, desc: true }];
   const columns = useMemo(
     () => [
       {
@@ -171,12 +179,12 @@ function QueryList({ addDangerToast, addSuccessToast }: QueryListProps) {
             </Tooltip>
           );
         },
-        accessor: 'status',
+        accessor: QueryObjectColumns.status,
         size: 'xs',
         disableSortBy: true,
       },
       {
-        accessor: 'start_time',
+        accessor: QueryObjectColumns.start_time,
         Header: t('Time'),
         size: 'lg',
         Cell: ({
@@ -212,17 +220,21 @@ function QueryList({ addDangerToast, addSuccessToast }: QueryListProps) {
         },
       },
       {
-        accessor: 'tab_name',
+        accessor: QueryObjectColumns.tab_name,
         Header: t('Tab Name'),
         size: 'lg',
       },
       {
-        accessor: 'database.database_name',
+        accessor: QueryObjectColumns.database_name,
         Header: t('Database'),
         size: 'lg',
       },
       {
-        accessor: 'schema',
+        accessor: QueryObjectColumns.database,
+        hidden: true,
+      },
+      {
+        accessor: QueryObjectColumns.schema,
         Header: t('Schema'),
         size: 'lg',
       },
@@ -259,13 +271,13 @@ function QueryList({ addDangerToast, addSuccessToast }: QueryListProps) {
 
           return main;
         },
-        accessor: 'sql_tables',
+        accessor: QueryObjectColumns.sql_tables,
         Header: t('Tables'),
         size: 'lg',
         disableSortBy: true,
       },
       {
-        accessor: 'user.first_name',
+        accessor: QueryObjectColumns.user,
         Header: t('User'),
         size: 'lg',
         Cell: ({
@@ -275,12 +287,12 @@ function QueryList({ addDangerToast, addSuccessToast }: QueryListProps) {
         }: any) => `${user.first_name} ${user.last_name}`,
       },
       {
-        accessor: 'rows',
+        accessor: QueryObjectColumns.rows,
         Header: t('Rows'),
         size: 'md',
       },
       {
-        accessor: 'sql',
+        accessor: QueryObjectColumns.sql,
         Header: t('SQL'),
         Cell: ({ row: { original, id } }: any) => {
           return (
@@ -319,7 +331,74 @@ function QueryList({ addDangerToast, addSuccessToast }: QueryListProps) {
     [],
   );
 
-  const filters: Filters = useMemo(() => [], []);
+  const filters: Filters = useMemo(
+    () => [
+      {
+        Header: t('Database'),
+        id: 'database',
+        input: 'select',
+        operator: FilterOperators.relationOneMany,
+        unfilteredLabel: 'All',
+        fetchSelects: createFetchRelated(
+          'query',
+          'database',
+          createErrorHandler(errMsg =>
+            addDangerToast(
+              t('An error occurred while fetching database values: %s', errMsg),
+            ),
+          ),
+        ),
+        paginate: true,
+      },
+      {
+        Header: t('State'),
+        id: 'status',
+        input: 'select',
+        operator: FilterOperators.equals,
+        unfilteredLabel: 'All',
+        fetchSelects: createFetchDistinct(
+          'query',
+          'status',
+          createErrorHandler(errMsg =>
+            addDangerToast(
+              t('An error occurred while fetching schema values: %s', errMsg),
+            ),
+          ),
+        ),
+        paginate: true,
+      },
+      {
+        Header: t('User'),
+        id: 'user',
+        input: 'select',
+        operator: FilterOperators.relationOneMany,
+        unfilteredLabel: 'All',
+        fetchSelects: createFetchRelated(
+          'query',
+          'user',
+          createErrorHandler(errMsg =>
+            addDangerToast(
+              t('An error occurred while fetching database values: %s', errMsg),
+            ),
+          ),
+        ),
+        paginate: true,
+      },
+      {
+        Header: t('Time Range'),
+        id: 'start_time',
+        input: 'datetime_range',
+        operator: FilterOperators.between,
+      },
+      {
+        Header: t('Search by query text'),
+        id: 'sql',
+        input: 'search',
+        operator: FilterOperators.contains,
+      },
+    ],
+    [addDangerToast],
+  );
 
   return (
     <>
