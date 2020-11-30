aab88c52f06b400acc6b620d4cfaea14893e0573
Nikola Gigic <nik.gigic@gmail.com>
2020-11-30 11:58:35 +0100

    Fixing front-end build errors

--- superset-frontend/src/common/components/Fade.tsx
+++ superset-frontend/src/common/components/Fade.tsx
@@ -21,8 +21,8 @@ import { styled } from '@superset-ui/core';
 type FadeProps = { hovered: boolean };
 
 const Fade = styled.div<FadeProps>`
-    transition: all ${({ theme }) => theme.transitionTiming}s;
-    opacity: ${props => (props.hovered ? 1 : 0)};
-`
+  transition: all ${({ theme }) => theme.transitionTiming}s;
+  opacity: ${props => (props.hovered ? 1 : 0)};
+`;
 
 export default Fade;
