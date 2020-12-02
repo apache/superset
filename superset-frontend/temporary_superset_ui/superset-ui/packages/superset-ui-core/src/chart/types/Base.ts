c280c5dc4d64afc9ee7d5132a21c584d36eb546e
Ville Brofeldt <33317356+villebro@users.noreply.github.com>
2020-12-02 08:20:00 +0200

    feat: native filter components (#840)
    * feat: add AntD filter components
    
    * add missing files
    
    * add filter hook
    
    * fix hook name
    
    * required thumbnail
    
    * add native filters to where clause
    
    * add isNativeFilter flag to ChartMetadata
    
    * rename native_filters to extra_form_data
    
    * lint
    
    * move hook to SuperChart props
    
    * add NOOP default to setSelectedValues
    
    * replace extra_form_data with override_ and append_
    
    * add support for appending freeform where
    
    * address review comments
    
    * Add default values
    
    * Update packages/superset-ui-core/test/query/processExtraFormData.test.ts
    
    Co-authored-by: Evan Rusackas <evan@preset.io>
    
    * Update packages/superset-ui-core/test/query/processExtraFormData.test.ts
    
    Co-authored-by: Evan Rusackas <evan@preset.io>
    
    * add storybook entry and change width to 100%
    
    * add placeholder
    
    * add default thumbnails
    
    Co-authored-by: Evan Rusackas <evan@preset.io>

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-core/src/chart/types/Base.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-core/src/chart/types/Base.ts
@@ -1,4 +1,10 @@
+import { ExtraFormData } from '../../query';
+
 export type HandlerFunction = (...args: unknown[]) => void;
+
+export type SetExtraFormDataHook = {
+  (extraFormData: ExtraFormData): void;
+};
 
 export interface PlainObject {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
