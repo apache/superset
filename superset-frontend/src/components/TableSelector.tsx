15ad2ef10b2f640d40def28ba8b4a2db231dad16
Erik Ritter <erik.ritter@airbnb.com>
2020-12-01 17:11:09 -0800

    fix: certification icon order (#11881)

--- superset-frontend/src/components/TableSelector.tsx
+++ superset-frontend/src/components/TableSelector.tsx
@@ -255,6 +255,9 @@ const TableSelector: FunctionComponent<TableSelectorProps> = ({
   function renderTableOption(option: any) {
     return (
       <TableLabel title={option.label}>
+        <small className="text-muted">
+          <i className={`fa fa-${option.type === 'view' ? 'eye' : 'table'}`} />
+        </small>
         {option.extra?.certification && (
           <CertifiedIconWithTooltip
             certifiedBy={option.extra.certification.certified_by}
@@ -262,9 +265,6 @@ const TableSelector: FunctionComponent<TableSelectorProps> = ({
             size={20}
           />
         )}
-        <small className="text-muted">
-          <i className={`fa fa-${option.type === 'view' ? 'eye' : 'table'}`} />
-        </small>
         {option.label}
       </TableLabel>
     );
