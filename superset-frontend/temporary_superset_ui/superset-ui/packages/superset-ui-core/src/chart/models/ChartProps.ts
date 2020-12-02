a88c7c9485fd6df6e93a0201d8bc0d714409420b
simchaNielsen <56388545+simchaNielsen@users.noreply.github.com>
2020-12-02 09:06:38 +0200

    feat: support multi queries request (#846)
    * feat: support multi queries request
    
    * chore: add comment according CR

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-core/src/chart/models/ChartProps.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-core/src/chart/models/ChartProps.ts
@@ -47,8 +47,11 @@ export interface ChartPropsConfig {
   height?: number;
   /** Programmatic overrides such as event handlers, renderers */
   hooks?: Hooks;
-  /** Formerly called "payload" */
+  /** Formerly called "payload". This property going to be deprecated because
+   * contains only first item in response data array (use `queriesData` instead) */
   queryData?: QueryData;
+  /** Formerly called "payload" */
+  queriesData?: QueryData[];
   /** Chart width */
   width?: number;
 }
@@ -77,6 +80,8 @@ export default class ChartProps {
 
   queryData: QueryData;
 
+  queriesData: QueryData[];
+
   width: number;
 
   constructor(config: ChartPropsConfig = {}) {
@@ -87,6 +92,7 @@ export default class ChartProps {
       hooks = {},
       initialValues = {},
       queryData = {},
+      queriesData = [],
       width = DEFAULT_WIDTH,
       height = DEFAULT_HEIGHT,
     } = config;
@@ -100,6 +106,7 @@ export default class ChartProps {
     this.hooks = hooks;
     this.initialValues = initialValues;
     this.queryData = queryData;
+    this.queriesData = queriesData;
   }
 }
 
@@ -113,8 +120,19 @@ ChartProps.createSelector = function create(): ChartPropsSelector {
     input => input.hooks,
     input => input.initialValues,
     input => input.queryData,
+    input => input.queriesData,
     input => input.width,
-    (annotationData, datasource, formData, height, hooks, initialValues, queryData, width) =>
+    (
+      annotationData,
+      datasource,
+      formData,
+      height,
+      hooks,
+      initialValues,
+      queryData,
+      queriesData,
+      width,
+    ) =>
       new ChartProps({
         annotationData,
         datasource,
@@ -123,6 +141,7 @@ ChartProps.createSelector = function create(): ChartPropsSelector {
         hooks,
         initialValues,
         queryData,
+        queriesData,
         width,
       }),
   );
