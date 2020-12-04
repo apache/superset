689c16a768058d6c614100bf31b05f31feefd0d2
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2020-12-04 16:28:04 +0100

    Remove console log

--- superset-frontend/src/explore/components/DataTablesPane.tsx
+++ superset-frontend/src/explore/components/DataTablesPane.tsx
@@ -172,7 +172,6 @@ export const DataTablesPane = ({
       if (data[type]?.length === 0) {
         return <span>No data</span>;
       }
-      console.log(filteredData[type]);
       return (
         <TableView
           columns={columns[type]}
