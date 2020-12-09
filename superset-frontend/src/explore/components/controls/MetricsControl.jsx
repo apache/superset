2a9229738fd9483fe0ed8eeefa9857572456c694
mayur <mayurnewase111@gmail.com>
2020-12-09 22:26:56 +0530

    removed log

--- superset-frontend/src/explore/components/controls/MetricsControl.jsx
+++ superset-frontend/src/explore/components/controls/MetricsControl.jsx
@@ -194,7 +194,6 @@ export default class MetricsControl extends React.PureComponent {
   }
 
   onMetricEdit(changedMetric) {
-    console.log(changedMetric);
     let newValue = this.state.value.map(value => {
       if (value.optionName === changedMetric.optionName) {
         return changedMetric;
