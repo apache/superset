2aa2e2c3e9519f40b86e5206e522bf610b9693af
mayur <mayurnewase111@gmail.com>
2020-12-09 19:06:56 +0530

    removed extra space

--- superset-frontend/src/explore/components/controls/MetricsControl.jsx
+++ superset-frontend/src/explore/components/controls/MetricsControl.jsx
@@ -16,7 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import { t } from '@superset-ui/core';
