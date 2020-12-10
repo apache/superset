d3049be315482f68ac97415e7f2e546bbd084c1d
Beto Dealmeida <roberto@dealmeida.net>
2020-12-10 13:54:47 -0800

    Small fixes

--- superset-frontend/src/components/ImportModal/index.tsx
+++ superset-frontend/src/components/ImportModal/index.tsx
@@ -236,7 +236,7 @@ const ImportModelsModal: FunctionComponent<ImportModelsModalProps> = ({
           <div>{confirmOverwriteMessage}</div>
           <div className="control-label">
             <label htmlFor="overwrite">
-              {t('Type "OVERWRITE" to confirm')}
+              {t('Type "%s" to confirm', t('OVERWRITE'))}
             </label>
           </div>
           <input
