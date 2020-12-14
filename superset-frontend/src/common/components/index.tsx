1afe91579e84e201ddb624f68fd7a27ae8ec3082
Geido <60598000+geido@users.noreply.github.com>
2020-12-14 08:57:08 +0200

    refactor: Existing Antd Collapse to enhanced (#12013)
    * Move ProgressBar to Antd
    
    * Remove trailing space
    
    * Fix linting issues
    
    * Export enhanced Progress only
    
    * Move existing antd Collapse to enhanced Collapse
    
    * Move enhanced props up to Collapse only
    
    * Add stories for enhanced Collapse
    
    * Add 100% height for overflow
    
    * Improve text light story

--- superset-frontend/src/common/components/index.tsx
+++ superset-frontend/src/common/components/index.tsx
@@ -32,7 +32,6 @@ export {
   Avatar,
   Button,
   Card,
-  Collapse,
   DatePicker,
   Dropdown,
   Empty,
@@ -47,8 +46,9 @@ export {
   Tooltip,
 } from 'antd';
 
-export { default as Badge } from 'src/common/components/Badge';
-export { default as Progress } from 'src/common/components/ProgressBar';
+export { default as Collapse } from './Collapse';
+export { default as Badge } from './Badge';
+export { default as Progress } from './ProgressBar';
 
 export const MenuItem = styled(AntdMenu.Item)`
   > a {
