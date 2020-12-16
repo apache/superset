c91e00af32acf87ea1188fff37c1febe7c9afff4
Phillip Kelley-Dotson <pkelleydotson@yahoo.com>
2020-12-16 13:28:04 -0800

    add native-filter feat flag config

--- superset/config.py
+++ superset/config.py
@@ -168,7 +168,7 @@ SQLALCHEMY_CUSTOM_PASSWORD_STORE = None
 QUERY_SEARCH_LIMIT = 1000
 
 # Flask-WTF flag for CSRF
-WTF_CSRF_ENABLED = True
+WTF_CSRF_ENABLED = False 
 
 # Add endpoints that need to be exempt from CSRF protection
 WTF_CSRF_EXEMPT_LIST = ["superset.views.core.log", "superset.charts.api.data"]
