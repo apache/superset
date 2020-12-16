18fd322ecbdda329872ec9b534d780aeff9794c0
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2020-12-16 11:49:03 +0000

    feat(datasets): security perm simplification (#12000)
    * feat(datasets): security perm simplification
    
    * feat(datasets): security perm simplification
    
    * fix tests
    
    * fix tests
    
    * fix tests
    
    * fix tests
    
    * fix tests
    
    * include SqlMetricInlineView converge and fix JS tests
    
    * update to current alembic revision

--- superset-frontend/src/views/CRUD/data/dataset/DatasetList.tsx
+++ superset-frontend/src/views/CRUD/data/dataset/DatasetList.tsx
@@ -141,10 +141,10 @@ const DatasetList: FunctionComponent<DatasetListProps> = ({
     refreshData();
   };
 
-  const canEdit = hasPerm('can_edit');
-  const canDelete = hasPerm('can_delete');
-  const canCreate = hasPerm('can_add');
-  const canExport = hasPerm('can_mulexport');
+  const canEdit = hasPerm('can_write');
+  const canDelete = hasPerm('can_write');
+  const canCreate = hasPerm('can_write');
+  const canExport = hasPerm('can_read');
 
   const initialSort = [{ id: 'changed_on_delta_humanized', desc: true }];
 
--- superset/constants.py
+++ superset/constants.py
@@ -82,6 +82,7 @@ MODEL_VIEW_RW_METHOD_PERMISSION_MAP = {
     "list": "read",
     "muldelete": "write",
     "show": "read",
+    "yaml_export": "read",
 }
 
 MODEL_API_RW_METHOD_PERMISSION_MAP = {
@@ -95,8 +96,10 @@ MODEL_API_RW_METHOD_PERMISSION_MAP = {
     "post": "write",
     "put": "write",
     "related": "read",
-    "favorite_status": "write",
+    "refresh": "read",
+    "related_objects": "read",
     "import_": "write",
+    "favorite_status": "write",
     "cache_screenshot": "read",
     "screenshot": "read",
     "data": "read",
