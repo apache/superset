f9cfae276ad91a6d6bd5abcf894dacdd94364138
David Aaron Suddjian <1858430+suddjian@users.noreply.github.com>
2020-12-17 12:12:51 -0800

    Update superset-frontend/src/dashboard/components/nativeFilters/FilterConfigModal.tsx
    Co-authored-by: Evan Rusackas <evan@preset.io>

--- superset-frontend/src/dashboard/components/nativeFilters/FilterConfigModal.tsx
+++ superset-frontend/src/dashboard/components/nativeFilters/FilterConfigModal.tsx
@@ -35,7 +35,7 @@ const StyledModalBody = styled.div`
   display: flex;
   flex-direction: row;
   .filters-list {
-    width: 200px;
+    width: ${({ theme }) => theme.gridUnit * 50}px;
     overflow: auto;
   }
 `;
