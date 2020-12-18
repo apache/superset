2dc0856067261112e37e2e0e46d7a11dad2db4c2
simchaNielsen <56388545+simchaNielsen@users.noreply.github.com>
2020-12-18 19:15:27 +0200

    feat: Support multiple queries per request (#11880)
    * refactor: add queriesData fields for multiple queries
    
    * feat: support multi queries request
    
    * lint: fix lint
    
    * lint: fix lint
    
    * lint: fix lint
    
    * fix: fix CR notes
    
    * fix: fix CR notes
    
    * fix: fix CR notes
    
    * fix: fix error case for multi queries
    
    * feat: change queryResponse to queriesResponse
    
    * fix: revert webpack
    
    * test: fix tests
    
    * chore: lint
    
    * chore: adjust asyncEvent to multiple results
    
    * fix: lint
    
    * fix: eslint
    
    * fix: another eslint rule
    
    Co-authored-by: Amit Miran <47772523+amitmiran137@users.noreply.github.com>
    Co-authored-by: amitmiran137 <amit.miran@nielsen.com>

--- superset-frontend/src/explore/reducers/getInitialState.js
+++ superset-frontend/src/explore/reducers/getInitialState.js
@@ -72,7 +72,7 @@ export default function getInitialState(bootstrapData) {
         latestQueryFormData: getFormDataFromControls(controls),
         sliceFormData,
         queryController: null,
-        queryResponse: null,
+        queriesResponse: null,
         triggerQuery: false,
         lastRendered: 0,
       },
