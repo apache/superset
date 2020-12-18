375ceddc0be7afa0364181711a06a19ca78af75e
NejcZdovc <nejc.zdovc@3zsistemi.si>
2020-12-18 07:10:22 +0100

    Fixes add button
    Resolves #31

--- superset/static/blotout.js
+++ superset/static/blotout.js
@@ -1,4 +1,4 @@
-function parentPost() {
+const parentPost = () => {
   parent.postMessage({
     type: "iframeUrl",
     data: window.location.href,
@@ -6,17 +6,43 @@ function parentPost() {
   }, "*")
 }
 
-const modifyElements = () => {
-  // Add + button to source database and table pages
-  if (window.location.pathname === '/databaseview/list/' ||
-      window.location.pathname === '/tablemodelview/list/') {
-    document.querySelectorAll('header nav .navbar-right .btn-primary')[0].style.display = 'inline-block'
+let modifyTimer
+const modifyCheck = (func) => {
+  const result = func()
+  if (!result) {
+    modifyTimer = setInterval(func, 500)
+  } else {
+    clearInterval(modifyTimer)
   }
+}
 
-  // remove reset password button from user page
-  if (window.location.pathname === '/users/userinfo/' ||
-      window.location.pathname.startsWith('/users/show/')) {
-    document.querySelector('.panel-body .well .btn-primary').style.display = 'none'
+const modifyElements = () => {
+  switch (window.location.pathname) {
+    case '/databaseview/list/':
+    case '/tablemodelview/list/': {
+      // Add + button to source database and table pages
+      modifyCheck(() => {
+        const elements = document.querySelectorAll('header nav .navbar-right .btn-primary')
+        if (elements.length === 0) {
+          return false
+        }
+        elements[0].style.display = 'inline-block'
+        return true
+      })
+      break
+    }
+    case '/users/userinfo/':
+    case '/users/show/': {
+      // remove reset password button from user page
+      modifyCheck(() => {
+        const elements = document.querySelectorAll('.panel-body .well .btn-primary')
+        if (elements.length === 0) {
+          return false
+        }
+        elements[0].style.display = 'none'
+        return true
+     })
+    }
   }
 }
 
