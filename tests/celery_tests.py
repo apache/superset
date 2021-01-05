a7a2eb3f19574a110fad92e9474f1b5228f04807
Karol Kostrzewa <karolkostrzew@gmail.com>
2021-01-05 09:10:02 +0100

    fix celery tests drop table

--- tests/celery_tests.py
+++ tests/celery_tests.py
@@ -175,7 +175,7 @@ def test_run_sync_query_cta(setup_sqllab, ctas_method):
     assert QueryStatus.SUCCESS == results["status"], results
     assert len(results["data"]) > 0
 
-    db.get_engine().execute(f"DROP {ctas_method} {tmp_table_name}")
+    db.get_engine().execute(f"DROP {ctas_method} IF EXISTS {tmp_table_name}")
 
 
 @pytest.mark.usefixtures("load_birth_names_dashboard_with_slices")
@@ -214,7 +214,9 @@ def test_run_sync_query_cta_config(setup_sqllab, ctas_method):
     results = run_sql(query.select_sql)
     assert QueryStatus.SUCCESS == results["status"], result
 
-    db.get_engine().execute(f"DROP {ctas_method} {CTAS_SCHEMA_NAME}.{tmp_table_name}")
+    db.get_engine().execute(
+        f"DROP {ctas_method}  IF EXISTS {CTAS_SCHEMA_NAME}.{tmp_table_name}"
+    )
 
 
 @pytest.mark.usefixtures("load_birth_names_dashboard_with_slices")
@@ -240,7 +242,9 @@ def test_run_async_query_cta_config(setup_sqllab, ctas_method):
         f"CREATE {ctas_method} {CTAS_SCHEMA_NAME}.{tmp_table_name} AS \n{QUERY}"
         == query.executed_sql
     )
-    db.get_engine().execute(f"DROP {ctas_method} {CTAS_SCHEMA_NAME}.{tmp_table_name}")
+    db.get_engine().execute(
+        f"DROP {ctas_method}  IF EXISTS {CTAS_SCHEMA_NAME}.{tmp_table_name}"
+    )
 
 
 @pytest.mark.usefixtures("load_birth_names_dashboard_with_slices")
@@ -263,7 +267,7 @@ def test_run_async_cta_query(setup_sqllab, ctas_method):
     assert query.select_as_cta
     assert query.select_as_cta_used
 
-    db.get_engine().execute(f"DROP {ctas_method} {table_name}")
+    db.get_engine().execute(f"DROP {ctas_method}  IF EXISTS {table_name}")
 
 
 @pytest.mark.usefixtures("load_birth_names_dashboard_with_slices")
@@ -286,7 +290,7 @@ def test_run_async_cta_query_with_lower_limit(setup_sqllab, ctas_method):
     assert query.select_as_cta
     assert query.select_as_cta_used
 
-    db.get_engine().execute(f"DROP {ctas_method} {tmp_table}")
+    db.get_engine().execute(f"DROP {ctas_method} IF EXISTS  {tmp_table}")
 
 
 SERIALIZATION_DATA = [("a", 4, 4.0, datetime.datetime(2019, 8, 18, 16, 39, 16, 660000))]
