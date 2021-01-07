073627811a8a21fe6ed0e540e4aba5e0f15514ec
Tai Dupree <tdupreetan@gmail.com>
2021-01-07 14:28:54 -0800

    use static template file over app config var

--- superset/config.py
+++ superset/config.py
@@ -733,11 +733,6 @@ ROBOT_PERMISSION_ROLES = ["Public", "Gamma", "Alpha", "Admin", "sql_lab"]
 
 CONFIG_PATH_ENV_VAR = "SUPERSET_CONFIG_PATH"
 
-# Use with caution!
-# Valid html code that will be injected into every view.
-# Use this to insert eg, google analytics, intercom, pendo, fullstory, etc.
-HTML_TEMPLATE_SCRIPT = ""
-
 # If a callable is specified, it will be called at app startup while passing
 # a reference to the Flask app. This can be used to alter the Flask app
 # in whatever way.
