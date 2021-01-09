0bf3f6a86dc3454ea43ef407c877afb4d2384d25
Geido <60598000+geido@users.noreply.github.com>
2021-01-09 21:18:59 +0100

    Closes #12389 (#12390)

--- superset-frontend/src/explore/exploreUtils.js
+++ superset-frontend/src/explore/exploreUtils.js
@@ -288,8 +288,9 @@ export const exploreChart = formData => {
   postForm(url, formData);
 };
 
-export const useDebouncedEffect = (effect, delay) => {
-  const callback = useCallback(effect, [effect]);
+export const useDebouncedEffect = (effect, delay, deps) => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const callback = useCallback(effect, deps);
 
   useEffect(() => {
     const handler = setTimeout(() => {
