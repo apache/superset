d80220eced8118f7780752af1699573ec05ed6b7
Jesse Yang <jesse.yang@airbnb.com>
2021-01-12 22:11:51 -0800

    build: add cancel-workflow-runs action locally (#12394)

---
+++ .github/actions/cancel-workflow-runs/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  clearMocks: true,
+  moduleFileExtensions: ['js', 'ts'],
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  testRunner: 'jest-circus/runner',
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  verbose: true
+}
