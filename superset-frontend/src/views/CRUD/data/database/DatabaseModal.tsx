e273d17e3d4207383a16d3f1ae57e8dfe40b79c1
Ville Brofeldt <33317356+villebro@users.noreply.github.com>
2021-01-13 13:39:28 +0200

    feat(db-engine-specs): add support for Postgres root cert (#11720)
    * feat(db-engine-specs): add support for Postgres root cert
    
    * remove logging of json decode exception message
    
    * fix error message
    
    * fix error message

--- superset-frontend/src/views/CRUD/data/database/DatabaseModal.tsx
+++ superset-frontend/src/views/CRUD/data/database/DatabaseModal.tsx
@@ -182,7 +182,9 @@ const DatabaseModal: FunctionComponent<DatabaseModalProps> = ({
               ? `${t('ERROR: ')}${
                   typeof error.message === 'string'
                     ? error.message
-                    : (error.message as Record<string, string[]>).sqlalchemy_uri
+                    : Object.entries(error.message as Record<string, string[]>)
+                        .map(([key, value]) => `(${key}) ${value.join(', ')}`)
+                        .join('\n')
                 }`
               : t('ERROR: Connection failed. '),
           );
