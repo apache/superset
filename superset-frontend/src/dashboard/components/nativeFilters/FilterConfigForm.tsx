24e01da6971b7cd5bfd29cea6828d1613c1d6e72
simchaNielsen <56388545+simchaNielsen@users.noreply.github.com>
2021-01-20 11:40:53 +0200

    refactor: Sync Scoping tree with Forms data (#12171)
    * refactor: sync Scoping tree with Forms data
    
    * refactor: update scoping tree
    
    * refactor: update scope tree logic to be more UX friendly
    
    * test: fix tests
    
    * lint: fix lin CR notes
    
    * fix: fix jsx
    
    * refactor: revert type changes
    
    * refactor: revert type changes
    
    * lint: fix lint
    
    * lint: fix lint
    
    * test: fix test
    
    * fix: process initial dashboard case
    
    * lint: fix lint / tests

--- superset-frontend/src/dashboard/components/nativeFilters/FilterConfigForm.tsx
+++ superset-frontend/src/dashboard/components/nativeFilters/FilterConfigForm.tsx
@@ -24,7 +24,6 @@ import {
   Checkbox,
   Form,
   Input,
-  Radio,
   Typography,
 } from 'src/common/components';
 import { Select } from 'src/components/Select/SupersetStyledSelect';
@@ -34,8 +33,8 @@ import SupersetResourceSelect, {
 import { addDangerToast } from 'src/messageToasts/actions';
 import { ClientErrorObject } from 'src/utils/getClientErrorObject';
 import { ColumnSelect } from './ColumnSelect';
-import ScopingTree from './ScopingTree';
-import { Filter, NativeFiltersForm, Scoping } from './types';
+import { Filter, NativeFiltersForm } from './types';
+import FilterScope from './FilterScope';
 
 type DatasetSelectValue = {
   value: number;
@@ -46,10 +45,6 @@ const datasetToSelectOption = (item: any): DatasetSelectValue => ({
   value: item.id,
   label: item.table_name,
 });
-
-const ScopingTreeNote = styled.div`
-  margin-bottom: ${({ theme }) => theme.gridUnit * 2}px;
-`;
 
 const RemovedContent = styled.div`
   display: flex;
@@ -103,9 +98,6 @@ export const FilterConfigForm: React.FC<FilterConfigFormProps> = ({
   form,
   parentFilters,
 }) => {
-  const [advancedScopingOpen, setAdvancedScopingOpen] = useState<Scoping>(
-    Scoping.all,
-  );
   const [dataset, setDataset] = useState<Value<number> | undefined>();
 
   const onDatasetSelectError = useCallback(
@@ -117,13 +109,6 @@ export const FilterConfigForm: React.FC<FilterConfigFormProps> = ({
       addDangerToast(errorText);
     },
     [],
-  );
-
-  const setFilterScope = useCallback(
-    value => {
-      form.setFields([{ name: ['filters', filterId, 'scope'], value }]);
-    },
-    [form, filterId],
   );
 
   if (removed) {
@@ -190,14 +175,6 @@ export const FilterConfigForm: React.FC<FilterConfigFormProps> = ({
           datasetId={dataset?.value}
         />
       </StyledFormItem>
-
-      <StyledFormItem
-        name={['filters', filterId, 'defaultValue']}
-        label={<StyledLabel>{t('Default Value')}</StyledLabel>}
-        initialValue={filterToEdit?.defaultValue}
-      >
-        <Input />
-      </StyledFormItem>
       <StyledFormItem
         name={['filters', filterId, 'parentFilter']}
         label={<StyledLabel>{t('Parent Filter')}</StyledLabel>}
@@ -244,36 +221,11 @@ export const FilterConfigForm: React.FC<FilterConfigFormProps> = ({
       >
         <Checkbox>{t('Required')}</Checkbox>
       </StyledCheckboxFormItem>
-      <Typography.Title level={5}>{t('Scoping')}</Typography.Title>
-      <StyledCheckboxFormItem
-        name={['filters', filterId, 'scoping']}
-        initialValue={advancedScopingOpen}
-      >
-        <Radio.Group
-          onChange={({ target: { value } }) => {
-            setAdvancedScopingOpen(value as Scoping);
-          }}
-        >
-          <Radio value={Scoping.all}>{t('Apply to all panels')}</Radio>
-          <Radio value={Scoping.specific}>
-            {t('Apply to specific panels')}
-          </Radio>
-        </Radio.Group>
-      </StyledCheckboxFormItem>
-      <>
-        <ScopingTreeNote>
-          <Typography.Text type="secondary">
-            {advancedScopingOpen === Scoping.specific
-              ? t('Only selected panels will be affected by this filter')
-              : t(
-                  'All panels with this column will be affected by this filter',
-                )}
-          </Typography.Text>
-        </ScopingTreeNote>
-        {advancedScopingOpen === Scoping.specific && (
-          <ScopingTree setFilterScope={setFilterScope} />
-        )}
-      </>
+      <FilterScope
+        filterId={filterId}
+        filterToEdit={filterToEdit}
+        form={form}
+      />
     </>
   );
 };
