4cabbae417ff7fc784b73cf58499ec1232a2c379
Bryan Keller <bkeller@netflix.com>
2021-01-20 06:07:02 -0800

    fix dict key lookup

--- superset/utils/core.py
+++ superset/utils/core.py
@@ -1025,8 +1025,8 @@ def merge_extra_filters(  # pylint: disable=too-many-branches
         for existing in adhoc_filters:
             if (
                 existing["expressionType"] == "SIMPLE"
-                and existing["comparator"] is not None
-                and existing["subject"] is not None
+                and "comparator" in existing
+                and "subject" in existing
             ):
                 existing_filters[get_filter_key(existing)] = existing["comparator"]
 
