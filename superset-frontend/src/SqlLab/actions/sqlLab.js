911b92f3b242d453e0b343af5edbbabe863f7709
Michael S. Molina <70410625+michael-s-molina@users.noreply.github.com>
2021-01-20 15:50:24 -0300

    Fix translation files and update documentation (#12595)

--- superset-frontend/src/SqlLab/actions/sqlLab.js
+++ superset-frontend/src/SqlLab/actions/sqlLab.js
@@ -1180,7 +1180,7 @@ export function popStoredQuery(urlId) {
       .then(({ json }) =>
         dispatch(
           addQueryEditor({
-            title: json.title ? json.title : t('Sjsonhared query'),
+            title: json.title ? json.title : t('Shared query'),
             dbId: json.dbId ? parseInt(json.dbId, 10) : null,
             schema: json.schema ? json.schema : null,
             autorun: json.autorun ? json.autorun : false,
