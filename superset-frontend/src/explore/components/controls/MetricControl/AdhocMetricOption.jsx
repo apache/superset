a3a6951b6dee1d20507a5ff855bdb9ad7e16362a
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2021-01-22 00:51:59 +0100

    fix(explore): don't allow selecting duplicated saved metric (#12657)

--- superset-frontend/src/explore/components/controls/MetricControl/AdhocMetricOption.jsx
+++ superset-frontend/src/explore/components/controls/MetricControl/AdhocMetricOption.jsx
@@ -31,7 +31,7 @@ const propTypes = {
   onMetricEdit: PropTypes.func.isRequired,
   onRemoveMetric: PropTypes.func,
   columns: PropTypes.arrayOf(columnType),
-  savedMetrics: PropTypes.arrayOf(savedMetricType),
+  savedMetricsOptions: PropTypes.arrayOf(savedMetricType),
   savedMetric: savedMetricType,
   datasourceType: PropTypes.string,
   onMoveLabel: PropTypes.func,
@@ -55,7 +55,7 @@ class AdhocMetricOption extends React.PureComponent {
       adhocMetric,
       onMetricEdit,
       columns,
-      savedMetrics,
+      savedMetricsOptions,
       savedMetric,
       datasourceType,
       onMoveLabel,
@@ -67,7 +67,7 @@ class AdhocMetricOption extends React.PureComponent {
         adhocMetric={adhocMetric}
         onMetricEdit={onMetricEdit}
         columns={columns}
-        savedMetrics={savedMetrics}
+        savedMetricsOptions={savedMetricsOptions}
         savedMetric={savedMetric}
         datasourceType={datasourceType}
       >
