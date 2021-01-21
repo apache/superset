350f21fc03aaa5e95eb016f10d944765934a181e
Kasia Kucharczyk <2536609+kkucharc@users.noreply.github.com>
2021-01-21 10:32:34 +0100

    [12601] Hovered menu items on dashboard - brought back padding and added margin on top of chart (#12603)

--- superset-frontend/src/dashboard/components/DashboardBuilder.jsx
+++ superset-frontend/src/dashboard/components/DashboardBuilder.jsx
@@ -93,7 +93,7 @@ const StyledDashboardContent = styled.div`
     width: 100%;
     flex-grow: 1;
     position: relative;
-    margin: ${({ theme }) => theme.gridUnit * 2}px
+    margin: ${({ theme }) => theme.gridUnit * 6}px
       ${({ theme }) => theme.gridUnit * 8}px
       ${({ theme }) => theme.gridUnit * 6}px
       ${({ theme, dashboardFiltersOpen }) => {
