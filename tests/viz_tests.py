d1b02f87169ad6818d331f3225c04fed6bd398b8
Duy Nguyen Hoang <nhduy88@gmail.com>
2021-01-21 17:18:35 +0700

    Update tests/viz_tests.py to use f-strings style

--- tests/viz_tests.py
+++ tests/viz_tests.py
@@ -79,10 +79,10 @@ class TestBaseViz(SupersetTestCase):
         datasource.type = "table"
         test_viz = viz.BaseViz(datasource, form_data)
         expect_metric_labels = [
-            u"sum__SP_POP_TOTL",
-            u"SUM(SE_PRM_NENR_MA)",
-            u"SUM(SP_URB_TOTL)",
-            u"count",
+            "sum__SP_POP_TOTL",
+            "SUM(SE_PRM_NENR_MA)",
+            "SUM(SP_URB_TOTL)",
+            "count",
         ]
         self.assertEqual(test_viz.metric_labels, expect_metric_labels)
         self.assertEqual(test_viz.all_metrics, expect_metric_labels)
@@ -500,6 +500,7 @@ class TestDistBarViz(SupersetTestCase):
             {"x": "2.0", "y": 29},
             {"x": NULL_STRING, "y": 3},
         ]
+
         self.assertEqual(expected_values, data["values"])
 
     def test_column_nulls(self):
@@ -590,7 +591,7 @@ class TestDistBarViz(SupersetTestCase):
 
         expected = [
             {
-                "key": "z_column, {}".format(NULL_STRING),
+                "key": f"z_column, {NULL_STRING}",
                 "values": [{"x": "pepperoni", "y": 2}, {"x": "cheese", "y": 1}],
             },
             {
@@ -598,7 +599,7 @@ class TestDistBarViz(SupersetTestCase):
                 "values": [{"x": "pepperoni", "y": 5}, {"x": "cheese", "y": 3}],
             },
             {
-                "key": "votes, {}".format(NULL_STRING),
+                "key": f"votes, {NULL_STRING}",
                 "values": [{"x": "pepperoni", "y": 2}, {"x": "cheese", "y": 1}],
             },
             {
@@ -606,7 +607,7 @@ class TestDistBarViz(SupersetTestCase):
                 "values": [{"x": "pepperoni", "y": 5}, {"x": "cheese", "y": 3}],
             },
             {
-                "key": "a_column, {}".format(NULL_STRING),
+                "key": f"a_column, {NULL_STRING}",
                 "values": [{"x": "pepperoni", "y": 2}, {"x": "cheese", "y": 1}],
             },
             {
@@ -1265,18 +1266,18 @@ class TestTimeSeriesViz(SupersetTestCase):
         viz_data = test_viz.get_data(df)
         expected = [
             {
-                u"values": [
-                    {u"y": 4, u"x": u"2018-02-20T00:00:00"},
-                    {u"y": 4, u"x": u"2018-03-09T00:00:00"},
+                "values": [
+                    {"y": 4, "x": "2018-02-20T00:00:00"},
+                    {"y": 4, "x": "2018-03-09T00:00:00"},
                 ],
-                u"key": (u"Real Madrid Basket",),
+                "key": ("Real Madrid Basket",),
             },
             {
-                u"values": [
-                    {u"y": 2, u"x": u"2018-02-20T00:00:00"},
-                    {u"y": 2, u"x": u"2018-03-09T00:00:00"},
+                "values": [
+                    {"y": 2, "x": "2018-02-20T00:00:00"},
+                    {"y": 2, "x": "2018-03-09T00:00:00"},
                 ],
-                u"key": (u"Real Madrid C.F.\U0001f1fa\U0001f1f8\U0001f1ec\U0001f1e7",),
+                "key": ("Real Madrid C.F.\U0001f1fa\U0001f1f8\U0001f1ec\U0001f1e7",),
             },
         ]
         self.assertEqual(expected, viz_data)
