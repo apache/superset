203c08adc123bd21ca6c7ef9c66d4f35f7d9c06e
Rob DiCiuccio <rob.diciuccio@gmail.com>
2021-01-22 10:41:04 -0800

    docs: Remove gatsby-plugin-offline

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -68,9 +68,6 @@ module.exports = {
         start_url: `/`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
     `gatsby-plugin-client-side-redirect`,
 		{
       resolve: `gatsby-plugin-gtag`,
