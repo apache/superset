9be2200deb7d300b556d6c1bd5b8cde4954f8898
Tobiasz Kędzierski <tobiasz.kedzierski@polidea.com>
2021-01-23 11:07:48 +0100

    Use git submodules for (securely) using third party Github Actions

--- .github/actions/cached-dependencies/__tests__/fixtures/bashlib.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-default-setup-command() {
-  print-cachescript-path
-}
--- .github/actions/file-changes-action/src/typings/Inferred/index.d.ts
+++
@@ -1,6 +0,0 @@
-export interface Inferred {
-  pr?: number
-  before?: string
-  after?: string
-  [key: string]: string | number | undefined
-}
