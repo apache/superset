3ccb23c4a3295c36c7d49344ee25649987e8cf9f
Yongjie Zhao <yongjie.zhao@gmail.com>
2021-01-25 15:58:15 +0800

    chore(explore): added tooltips to timepicker (#12580)
    * wip
    
    * wip
    
    * fix lint
    
    * fix: tooltip cosmetic
    
    * wip
    
    * add license

--- superset-frontend/src/explore/components/controls/DateFilterControl/frame/AdvancedFrame.tsx
+++ superset-frontend/src/explore/components/controls/DateFilterControl/frame/AdvancedFrame.tsx
@@ -21,6 +21,7 @@ import { t } from '@superset-ui/core';
 import { SEPARATOR } from 'src/explore/dateFilterUtils';
 import { Input } from 'src/common/components';
 import { FrameComponentProps } from '../types';
+import DateFunctionTooltip from './DateFunctionTooltip';
 
 export function AdvancedFrame(props: FrameComponentProps) {
   const [since, until] = getAdvancedRange(props.value || '').split(SEPARATOR);
@@ -48,7 +49,12 @@ export function AdvancedFrame(props: FrameComponentProps) {
 
   return (
     <>
-      <div className="section-title">{t('Configure advanced time range')}</div>
+      <div className="section-title">
+        {t('Configure Advanced Time Range ')}
+        <DateFunctionTooltip placement="rightBottom">
+          <i className="fa fa-info-circle text-muted" />
+        </DateFunctionTooltip>
+      </div>
       <div className="control-label">{t('START')}</div>
       <Input
         key="since"
