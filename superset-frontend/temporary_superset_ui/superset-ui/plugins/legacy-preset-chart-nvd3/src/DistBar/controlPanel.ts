114d78ed33f7df1a12c016952d516a233e179db8
Yongjie Zhao <yongjiezhao@apache.org>
2021-01-26 04:27:58 +0800

    chore: move all controls for NVD3 and Big Number charts to separate rows (#913)

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/legacy-preset-chart-nvd3/src/DistBar/controlPanel.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/legacy-preset-chart-nvd3/src/DistBar/controlPanel.ts
@@ -59,10 +59,12 @@ const config: ControlPanelConfig = {
       label: t('Chart Options'),
       expanded: true,
       controlSetRows: [
-        ['color_scheme', 'label_colors'],
-        [showLegend, showBarValue],
+        ['color_scheme'],
+        ['label_colors'],
+        [showLegend],
+        [showBarValue],
+        [barStacked],
         [
-          barStacked,
           {
             name: 'order_bars',
             config: {
@@ -74,17 +76,15 @@ const config: ControlPanelConfig = {
             },
           },
         ],
-        ['y_axis_format', yAxisLabel],
+        ['y_axis_format'],
+        [yAxisLabel],
         [showControls, null],
       ],
     },
     {
       label: t('X Axis'),
       expanded: true,
-      controlSetRows: [
-        [xAxisLabel, bottomMargin],
-        [xTicksLayout, reduceXTicks],
-      ],
+      controlSetRows: [[xAxisLabel], [bottomMargin], [xTicksLayout], [reduceXTicks]],
     },
   ],
   controlOverrides: {
