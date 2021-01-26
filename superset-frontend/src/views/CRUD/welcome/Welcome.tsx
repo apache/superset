56ebe81291b00e20cc5b4a9dcbb2daa8776dff59
Michael S. Molina <70410625+michael-s-molina@users.noreply.github.com>
2021-01-26 04:19:16 -0300

    Fix tests errors and warnings - iteration 7 (#12212) (#12245)

--- superset-frontend/src/views/CRUD/welcome/Welcome.tsx
+++ superset-frontend/src/views/CRUD/welcome/Welcome.tsx
@@ -62,9 +62,9 @@ const WelcomeContainer = styled.div`
       }
     }
     .nav.navbar-nav {
-      & > li:nth-child(1),
-      & > li:nth-child(2),
-      & > li:nth-child(3) {
+      & > li:nth-of-type(1),
+      & > li:nth-of-type(2),
+      & > li:nth-of-type(3) {
         margin-top: ${({ theme }) => theme.gridUnit * 2}px;
       }
     }
