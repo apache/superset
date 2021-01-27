83d76cec371f7ba7041da414514f04e1262da174
amitmiran137 <amit.miran@nielsen.com>
2021-01-27 17:07:10 +0200

    fix: pre commit ;)

--- tests/dashboards/base_case.py
+++ tests/dashboards/base_case.py
@@ -15,7 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 import json
-from typing import Dict, Union
+from typing import Dict, Union, Any
 
 import prison
 from flask import Response
@@ -45,13 +45,13 @@ class DashboardTestCase(SupersetTestCase):
         return self.client.get(DASHBOARDS_API_URL)
 
     def save_dashboard_via_view(
-        self, dashboard_id: Union[str, int], dashboard_data: Dict
+        self, dashboard_id: Union[str, int], dashboard_data: Dict[str, Any]
     ) -> Response:
         save_dash_url = SAVE_DASHBOARD_URL_FORMAT.format(dashboard_id)
         return self.get_resp(save_dash_url, data=dict(data=json.dumps(dashboard_data)))
 
     def save_dashboard(
-        self, dashboard_id: Union[str, int], dashboard_data: Dict
+        self, dashboard_id: Union[str, int], dashboard_data: Dict[str, Any]
     ) -> Response:
         return self.save_dashboard_via_view(dashboard_id, dashboard_data)
 
