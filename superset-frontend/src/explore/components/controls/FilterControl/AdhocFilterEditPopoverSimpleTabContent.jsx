1ee5e75093ce9e7ea22a9b5795ce250867c7215a
Nikola Gigic <nik.gigic@gmail.com>
2021-01-29 12:37:24 +0100

    address filter popup

--- superset-frontend/src/explore/components/controls/FilterControl/AdhocFilterEditPopoverSimpleTabContent.jsx
+++ superset-frontend/src/explore/components/controls/FilterControl/AdhocFilterEditPopoverSimpleTabContent.jsx
@@ -337,6 +337,7 @@ export default class AdhocFilterEditPopoverSimpleTabContent extends React.Compon
             {...this.selectProps}
             {...subjectSelectProps}
             name="filter-column"
+            getPopupContainer={triggerNode => triggerNode.parentNode}
           >
             {columns.map(column => (
               <Select.Option
@@ -355,6 +356,7 @@ export default class AdhocFilterEditPopoverSimpleTabContent extends React.Compon
           <Select
             {...this.selectProps}
             {...operatorSelectProps}
+            getPopupContainer={triggerNode => triggerNode.parentNode}
             name="filter-operator"
           >
             {OPERATORS_OPTIONS.filter(op =>
@@ -369,7 +371,11 @@ export default class AdhocFilterEditPopoverSimpleTabContent extends React.Compon
         <FormGroup data-test="adhoc-filter-simple-value">
           {MULTI_OPERATORS.has(operator) ||
           this.state.suggestions.length > 0 ? (
-            <SelectWithLabel name="filter-value" {...comparatorSelectProps}>
+            <SelectWithLabel
+              name="filter-value"
+              {...comparatorSelectProps}
+              getPopupContainer={triggerNode => triggerNode.parentNode}
+            >
               {this.state.suggestions.map(suggestion => (
                 <Select.Option value={suggestion} key={suggestion}>
                   {suggestion}
