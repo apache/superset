57de6f2b7b3a653f0e5e15ed856a98a07ce29961
Yongjie Zhao <yongjie.zhao@gmail.com>
2021-01-29 13:35:57 +0800

    fix(explore): time picker can not be switched between now and specific (#12793)
    * fix(explore): time picker can not be switched between now and specifc
    
    * fix linting
    
    * fix type
    
    * fix UT

--- superset-frontend/src/explore/components/controls/DateFilterControl/frame/CustomFrame.tsx
+++ superset-frontend/src/explore/components/controls/DateFilterControl/frame/CustomFrame.tsx
@@ -18,7 +18,7 @@
  */
 import React from 'react';
 import { t } from '@superset-ui/core';
-import moment, { Moment } from 'moment';
+import { Moment } from 'moment';
 import { isInteger } from 'lodash';
 import {
   Col,
@@ -36,22 +36,16 @@ import {
   MOMENT_FORMAT,
   MIDNIGHT,
 } from '../constants';
-import { customTimeRangeDecode, customTimeRangeEncode } from '../utils';
+import {
+  customTimeRangeDecode,
+  customTimeRangeEncode,
+  dttmToMoment,
+} from '../utils';
 import {
   CustomRangeKey,
   SelectOptionType,
   FrameComponentProps,
 } from '../types';
-
-const dttmToMoment = (dttm: string): Moment => {
-  if (dttm === 'now') {
-    return moment().utc().startOf('second');
-  }
-  if (dttm === 'today') {
-    return moment().utc().startOf('day');
-  }
-  return moment(dttm);
-};
 
 export function CustomFrame(props: FrameComponentProps) {
   const { customRange, matchedFlag } = customTimeRangeDecode(props.value);
