8e0fc73cc679bafd2ef81447322707a3079397e6
Victor Malai <malai9696@gmail.com>
2021-02-08 07:28:44 +0200

    feat(plugin-chart-word-cloud): subject Add order by desc on metric (#939)
    * feat(plugin-chart-word-cloud): subject Add order by desc
    
    * Add checkbox to control order by

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/plugin-chart-word-cloud/src/plugin/buildQuery.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/plugin-chart-word-cloud/src/plugin/buildQuery.ts
@@ -3,9 +3,12 @@ import { WordCloudFormData } from '../types';
 
 export default function buildQuery(formData: WordCloudFormData) {
   // Set the single QueryObject's groupby field with series in formData
+  const { metric, sort_by_metric } = formData;
+
   return buildQueryContext(formData, baseQueryObject => [
     {
       ...baseQueryObject,
+      ...(sort_by_metric && { orderby: [[metric, false]] }),
     },
   ]);
 }
