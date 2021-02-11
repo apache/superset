e4ab151d71beb7577e3a7c03c578442362af9caa
Arash <arash.afghahi@gmail.com>
2021-02-10 19:57:35 -0500

    query search

--- superset-frontend/src/SqlLab/components/QuerySearch.jsx
+++ superset-frontend/src/SqlLab/components/QuerySearch.jsx
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import React from 'react';
+import React, { useState, useEffect, memo } from 'react';
 import PropTypes from 'prop-types';
 import Button from 'src/components/Button';
 import Select from 'src/components/Select';
@@ -35,6 +35,7 @@ import AsyncSelect from '../../components/AsyncSelect';
 
 const propTypes = {
   actions: PropTypes.object.isRequired,
+  // why is height required and then never used or invoked?
   height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
   displayLimit: PropTypes.number.isRequired,
 };
@@ -62,61 +63,17 @@ const StyledTableStylesContainer = styled.div`
   overflow: auto;
 `;
 
-class QuerySearch extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      databaseId: null,
-      userId: null,
-      searchText: null,
-      from: '28 days ago',
-      to: 'now',
-      status: 'success',
-      queriesArray: [],
-      queriesLoading: true,
-    };
-    this.userMutator = this.userMutator.bind(this);
-    this.changeUser = this.changeUser.bind(this);
-    this.dbMutator = this.dbMutator.bind(this);
-    this.onChange = this.onChange.bind(this);
-    this.changeSearch = this.changeSearch.bind(this);
-    this.onKeyDown = this.onKeyDown.bind(this);
-    this.changeFrom = this.changeFrom.bind(this);
-    this.changeTo = this.changeTo.bind(this);
-    this.changeStatus = this.changeStatus.bind(this);
-    this.refreshQueries = this.refreshQueries.bind(this);
-    this.onUserClicked = this.onUserClicked.bind(this);
-    this.onDbClicked = this.onDbClicked.bind(this);
-  }
+function QuerySearch({ actions, height, displayLimit }) {
+  const [databaseId, setDatabaseId] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const [searchText, setSearchText] = useState(null);
+  const [from, setFrom] = useState('28 days ago');
+  const [to, setTo] = useState('now');
+  const [status, setStatus] = useState('success');
+  const [queriesArray, setQueriesArray] = useState([]);
+  const [queriesLoading, setQueriesLoading] = useState(true);
 
-  componentDidMount() {
-    this.refreshQueries();
-  }
-
-  onUserClicked(userId) {
-    this.setState({ userId }, () => {
-      this.refreshQueries();
-    });
-  }
-
-  onDbClicked(dbId) {
-    this.setState({ databaseId: dbId }, () => {
-      this.refreshQueries();
-    });
-  }
-
-  onChange(db) {
-    const val = db ? db.value : null;
-    this.setState({ databaseId: val });
-  }
-
-  onKeyDown(event) {
-    if (event.keyCode === 13) {
-      this.refreshQueries();
-    }
-  }
-
-  getTimeFromSelection(selection) {
+  const getTimeFromSelection = selection => {
     switch (selection) {
       case 'now':
         return now();
@@ -135,196 +92,222 @@ class QuerySearch extends React.PureComponent {
       default:
         return null;
     }
-  }
+  };
 
-  changeFrom(user) {
-    const val = user ? user.value : null;
-    this.setState({ from: val });
-  }
-
-  changeTo(status) {
-    const val = status ? status.value : null;
-    this.setState({ to: val });
-  }
-
-  changeUser(user) {
-    const val = user ? user.value : null;
-    this.setState({ userId: val });
-  }
-
-  insertParams(baseUrl, params) {
+  const insertParams = (baseUrl, params) => {
     const validParams = params.filter(function (p) {
       return p !== '';
     });
     return `${baseUrl}?${validParams.join('&')}`;
-  }
+  };
 
-  changeStatus(status) {
-    const val = status ? status.value : null;
-    this.setState({ status: val });
-  }
+  const refreshQueries = () => {
+    setQueriesLoading(true);
+    const params = [
+      userId ? `user_id=${userId}` : '',
+      databaseId ? `database_id=${databaseId}` : '',
+      searchText ? `search_text=${searchText}` : '',
+      status ? `status=${status}` : '',
+      from ? `from=${getTimeFromSelection(from)}` : '',
+      to ? `to=${getTimeFromSelection(to)}` : '',
+    ];
 
-  changeSearch(event) {
-    this.setState({ searchText: event.target.value });
-  }
+    SupersetClient.get({
+      endpoint: insertParams('/superset/search_queries', params),
+    })
+      .then(({ json }) => {
+        setQueriesArray(json);
+        setQueriesLoading(false);
+      })
+      .catch(() => {
+        actions.addDangerToast(t('An error occurred when refreshing queries'));
+      });
+  };
 
-  userLabel(user) {
-    if (user.first_name && user.last_name) {
-      return `${user.first_name} ${user.last_name}`;
+  useEffect(() => {
+    refreshQueries();
+  });
+
+  const onUserClicked = userId => {
+    setUserId(userId);
+    refreshQueries();
+  };
+
+  const onDbClicked = dbId => {
+    // By my reading of this, it is setting state, and then running the refreshQueries function. Is that correct?
+    // this.setState({ databaseId: dbId }, () => {
+    //   this.refreshQueries();
+    // });
+
+    setDatabaseId(dbId);
+    refreshQueries();
+    // Based on my reading of this code, we are first setting the new state, and then running refresh queries to update the query field? I am writing the rest of these with this understanding.
+  };
+
+  const onChange = db => {
+    const val = db ? db.value : null;
+    setDatabaseId(val);
+  };
+
+  const onKeyDown = event => {
+    if (event.keyCode === 13) {
+      refreshQueries();
     }
-    return user.username;
-  }
+  };
 
-  userMutator(data) {
-    return data.result.map(({ value, text }) => ({
+  const changeFrom = user => {
+    const val = user ? user.value : null;
+    setFrom(val);
+  };
+
+  const changeTo = status => {
+    const val = status ? status.value : null;
+    setTo(val);
+  };
+
+  const changeUser = user => {
+    const val = user ? user.value : null;
+    setUserId(val);
+  };
+
+  const changeStatus = status => {
+    const val = status ? status.value : null;
+    setStatus(val);
+  };
+
+  const changeSearch = event => {
+    setSearchText(event.target.value);
+  };
+
+  // This is defined here, but never used within the file. I commented it out because I didn't want to delete it.
+  // const userLabel = user => {
+  //   if (user.first_name && user.last_name) {
+  //     return `${user.first_name} ${user.last_name}`;
+  //   }
+  //   return user.username;
+  // };
+
+  const userMutator = data =>
+    data.result.map(({ value, text }) => ({
       label: text,
       value,
     }));
-  }
 
-  dbMutator(data) {
+  const dbMutator = data => {
     const options = data.result.map(db => ({
       value: db.id,
       label: db.database_name,
     }));
-    this.props.actions.setDatabases(data.result);
+    actions.setDatabases(data.result);
     if (data.result.length === 0) {
-      this.props.actions.addDangerToast(
+      actions.addDangerToast(
         t("It seems you don't have access to any database"),
       );
     }
     return options;
-  }
+  };
 
-  refreshQueries() {
-    this.setState({ queriesLoading: true });
-    const params = [
-      this.state.userId ? `user_id=${this.state.userId}` : '',
-      this.state.databaseId ? `database_id=${this.state.databaseId}` : '',
-      this.state.searchText ? `search_text=${this.state.searchText}` : '',
-      this.state.status ? `status=${this.state.status}` : '',
-      this.state.from
-        ? `from=${this.getTimeFromSelection(this.state.from)}`
-        : '',
-      this.state.to ? `to=${this.getTimeFromSelection(this.state.to)}` : '',
-    ];
-
-    SupersetClient.get({
-      endpoint: this.insertParams('/superset/search_queries', params),
-    })
-      .then(({ json }) => {
-        this.setState({ queriesArray: json, queriesLoading: false });
-      })
-      .catch(() => {
-        this.props.actions.addDangerToast(
-          t('An error occurred when refreshing queries'),
-        );
-      });
-  }
-
-  render() {
-    return (
-      <TableWrapper>
-        <div id="search-header" className="row space-1">
-          <div className="col-sm-2">
-            <AsyncSelect
-              dataEndpoint="api/v1/query/related/user"
-              mutator={this.userMutator}
-              value={this.state.userId}
-              onChange={this.changeUser}
-              placeholder={t('Filter by user')}
-            />
-          </div>
-          <div className="col-sm-2">
-            <AsyncSelect
-              onChange={this.onChange}
-              dataEndpoint="/api/v1/database/?q=(filters:!((col:expose_in_sqllab,opr:eq,value:!t)))"
-              value={this.state.databaseId}
-              mutator={this.dbMutator}
-              placeholder={t('Filter by database')}
-            />
-          </div>
-          <div className="col-sm-4">
-            <input
-              type="text"
-              onChange={this.changeSearch}
-              onKeyDown={this.onKeyDown}
-              className="form-control input-sm"
-              placeholder={t('Query search string')}
-            />
-          </div>
-          <div className="col-sm-4 search-date-filter-container">
-            <Select
-              name="select-from"
-              placeholder={t('[From]-')}
-              options={TIME_OPTIONS.slice(1, TIME_OPTIONS.length).map(xt => ({
-                value: xt,
-                label: xt,
-              }))}
-              value={this.state.from}
-              autosize={false}
-              onChange={this.changeFrom}
-            />
-
-            <Select
-              name="select-to"
-              placeholder={t('[To]-')}
-              options={TIME_OPTIONS.map(xt => ({ value: xt, label: xt }))}
-              value={this.state.to}
-              autosize={false}
-              onChange={this.changeTo}
-            />
-
-            <Select
-              name="select-status"
-              placeholder={t('Filter by status')}
-              options={Object.keys(STATUS_OPTIONS).map(s => ({
-                value: s,
-                label: s,
-              }))}
-              value={this.state.status}
-              isLoading={false}
-              autosize={false}
-              onChange={this.changeStatus}
-            />
-
-            <Button
-              buttonSize="small"
-              buttonStyle="success"
-              onClick={this.refreshQueries}
-            >
-              {t('Search')}
-            </Button>
-          </div>
+  return (
+    <TableWrapper>
+      <div id="search-header" className="row space-1">
+        <div className="col-sm-2">
+          <AsyncSelect
+            dataEndpoint="api/v1/query/related/user"
+            mutator={userMutator}
+            value={userId}
+            onChange={changeUser}
+            placeholder={t('Filter by user')}
+          />
         </div>
-        <StyledTableStylesContainer>
-          {this.state.queriesLoading ? (
-            <Loading />
-          ) : (
-            <TableStyles>
-              <QueryTable
-                columns={[
-                  'state',
-                  'db',
-                  'user',
-                  'time',
-                  'progress',
-                  'rows',
-                  'sql',
-                  'querylink',
-                ]}
-                onUserClicked={this.onUserClicked}
-                onDbClicked={this.onDbClicked}
-                queries={this.state.queriesArray}
-                actions={this.props.actions}
-                displayLimit={this.props.displayLimit}
-              />
-            </TableStyles>
-          )}
-        </StyledTableStylesContainer>
-      </TableWrapper>
-    );
-  }
+        <div className="col-sm-2">
+          <AsyncSelect
+            onChange={onChange}
+            dataEndpoint="/api/v1/database/?q=(filters:!((col:expose_in_sqllab,opr:eq,value:!t)))"
+            value={databaseId}
+            mutator={dbMutator}
+            placeholder={t('Filter by database')}
+          />
+        </div>
+        <div className="col-sm-4">
+          <input
+            type="text"
+            onChange={changeSearch}
+            onKeyDown={onKeyDown}
+            className="form-control input-sm"
+            placeholder={t('Query search string')}
+          />
+        </div>
+        <div className="col-sm-4 search-date-filter-container">
+          <Select
+            name="select-from"
+            placeholder={t('[From]-')}
+            options={TIME_OPTIONS.slice(1, TIME_OPTIONS.length).map(xt => ({
+              value: xt,
+              label: xt,
+            }))}
+            value={from}
+            autosize={false}
+            onChange={changeFrom}
+          />
+
+          <Select
+            name="select-to"
+            placeholder={t('[To]-')}
+            options={TIME_OPTIONS.map(xt => ({ value: xt, label: xt }))}
+            value={to}
+            autosize={false}
+            onChange={changeTo}
+          />
+
+          <Select
+            name="select-status"
+            placeholder={t('Filter by status')}
+            options={Object.keys(STATUS_OPTIONS).map(s => ({
+              value: s,
+              label: s,
+            }))}
+            value={status}
+            isLoading={false}
+            autosize={false}
+            onChange={changeStatus}
+          />
+
+          <Button
+            buttonSize="small"
+            buttonStyle="success"
+            onClick={refreshQueries}
+          >
+            {t('Search')}
+          </Button>
+        </div>
+      </div>
+      <StyledTableStylesContainer>
+        {queriesLoading ? (
+          <Loading />
+        ) : (
+          <TableStyles>
+            <QueryTable
+              columns={[
+                'state',
+                'db',
+                'user',
+                'time',
+                'progress',
+                'rows',
+                'sql',
+                'querylink',
+              ]}
+              onUserClicked={onUserClicked}
+              onDbClicked={onDbClicked}
+              queries={queriesArray}
+              actions={actions}
+              displayLimit={displayLimit}
+            />
+          </TableStyles>
+        )}
+      </StyledTableStylesContainer>
+    </TableWrapper>
+  );
 }
 QuerySearch.propTypes = propTypes;
-export default QuerySearch;
+export default memo(QuerySearch);
