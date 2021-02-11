42c4facb7ed4aaa875ee954f99589b486be876fc
Ville Brofeldt <33317356+villebro@users.noreply.github.com>
2021-02-11 11:23:40 +0200

    fix(explore): pie chart label bugs (#13052)

--- superset-frontend/src/explore/components/controls/VizTypeControl.jsx
+++ superset-frontend/src/explore/components/controls/VizTypeControl.jsx
@@ -19,7 +19,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Row, Col, FormControl } from 'react-bootstrap';
-import { t, getChartMetadataRegistry } from '@superset-ui/core';
+import { Behavior, t, getChartMetadataRegistry } from '@superset-ui/core';
 import { useDynamicPluginContext } from 'src/components/DynamicPlugins';
 import { Tooltip } from 'src/common/components/Tooltip';
 import Modal from 'src/common/components/Modal';
@@ -166,7 +166,10 @@ const VizTypeControl = props => {
   const filterString = filter.toLowerCase();
 
   const filteredTypes = DEFAULT_ORDER.filter(type => registry.has(type))
-    .filter(type => !registry.get(type).isNativeFilter)
+    .filter(type => {
+      const behaviors = registry.get(type)?.behaviors || [];
+      return behaviors.includes(Behavior.CROSS_FILTER) || !behaviors.length;
+    })
     .map(type => ({
       key: type,
       value: registry.get(type),
@@ -174,7 +177,10 @@ const VizTypeControl = props => {
     .concat(
       registry
         .entries()
-        .filter(entry => !entry.value.isNativeFilter)
+        .filter(entry => {
+          const behaviors = entry.value?.behaviors || [];
+          return behaviors.includes(Behavior.CROSS_FILTER) || !behaviors.length;
+        })
         .filter(({ key }) => !typesWithDefaultOrder.has(key)),
     )
     .filter(entry => entry.value.name.toLowerCase().includes(filterString));
