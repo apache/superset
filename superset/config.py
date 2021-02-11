0cf57756f03da47c2dd26b87341d2673eaaf25d7
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2021-02-11 18:18:27 +0000

    fix: add config to disable dataset ownership on the old api (#13051)
    * fix: add config to disable dataset ownership on the old api
    
    * fix CI docker build
    
    * fix logic
    
    * add deprecation comment on the config

--- superset/config.py
+++ superset/config.py
@@ -1057,6 +1057,12 @@ SIP_15_TOAST_MESSAGE = (
     'class="alert-link">here</a>.'
 )
 
+# Turn this key to False to disable ownership check on the old dataset MVC and
+# datasource API /datasource/save.
+#
+# Warning: This config key is deprecated and will be removed in version 2.0.0"
+OLD_API_CHECK_DATASET_OWNERSHIP = True
+
 # SQLA table mutator, every time we fetch the metadata for a certain table
 # (superset.connectors.sqla.models.SqlaTable), we call this hook
 # to allow mutating the object with this callback.
