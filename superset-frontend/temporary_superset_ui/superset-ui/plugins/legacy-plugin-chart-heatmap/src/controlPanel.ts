4313a517dfbe6b73d6f90ac9fc87ebc42372d083
Victor Malai <malai9696@gmail.com>
2021-02-12 13:35:19 +0200

    feat: Single metric sort (#953)
    * feat(legacy-plugin-chart-heatmap): subject Add sort by metric
    
    * feat(legacy-plugin-chart-sunburst): subject Add sort by metric
    
    * feat(legacy-plugin-chart-force-directed): subject Add sort by metric
    
    * feat(legacy-plugin-chart-chord): subject Add sort by metric
    
    * feat(legacy-plugin-chart-country-map): subject Add sort by metric

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/legacy-plugin-chart-heatmap/src/controlPanel.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/legacy-plugin-chart-heatmap/src/controlPanel.ts
@@ -70,6 +70,16 @@ const config: ControlPanelConfig = {
         ['metric'],
         ['adhoc_filters'],
         ['row_limit'],
+        [
+          {
+            name: 'sort_by_metric',
+            config: {
+              type: 'CheckboxControl',
+              label: t('Sort by metric'),
+              description: t('Whether to sort results by the selected metric in descending order.'),
+            },
+          },
+        ],
       ],
     },
     {
