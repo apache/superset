c4ba749bfab3d1354f762b283e77d076e61837a3
simcha90 <56388545+simcha90@users.noreply.github.com>
2021-02-12 09:58:19 +0200

    feat(cross-filters): add cross filters (#12662)
    * feat: add cross filters
    
    * refactor: fix CR notes
    
    * lint: fix lint
    
    * lint: fix lint
    
    * chore: pre-commit
    
    * refactor: under chage
    
    * refactor: move to behaviors
    
    * lint: fix lint
    
    Co-authored-by: amitmiran137 <amit.miran@nielsen.com>

--- superset/config.py
+++ superset/config.py
@@ -328,6 +328,7 @@ DEFAULT_FEATURE_FLAGS: Dict[str, bool] = {
     # When True, this escapes HTML (rather than rendering it) in Markdown components
     "ESCAPE_MARKDOWN_HTML": False,
     "DASHBOARD_NATIVE_FILTERS": False,
+    "DASHBOARD_CROSS_FILTERS": False,
     "GLOBAL_ASYNC_QUERIES": False,
     "VERSIONED_EXPORT": False,
     # Note that: RowLevelSecurityFilter is only given by default to the Admin role
