deae0bb791a0ec1aec34f7bc7ab0844ace92d6b3
Geido <60598000+geido@users.noreply.github.com>
2021-02-18 20:53:50 +0200

    test: Tests and Storybook entry for the ModalTriggerComponent (#13213)
    * Kickstart tests
    
    * Finalize tests
    
    * Add storybook entry
    
    * Remove unnecessary args types

--- superset-frontend/src/components/ModalTrigger/index.jsx
+++ superset-frontend/src/components/ModalTrigger/index.jsx
@@ -91,6 +91,7 @@ export default class ModalTrigger extends React.Component {
         <>
           <Button
             className="modal-trigger"
+            data-test-id="btn-modal-trigger"
             tooltip={this.props.tooltip}
             onClick={this.open}
           >
@@ -103,7 +104,11 @@ export default class ModalTrigger extends React.Component {
     /* eslint-disable jsx-a11y/interactive-supports-focus */
     return (
       <>
-        <span onClick={this.open} role="button">
+        <span
+          data-test-id="span-modal-trigger"
+          onClick={this.open}
+          role="button"
+        >
           {this.props.triggerNode}
         </span>
         {this.renderModal()}
