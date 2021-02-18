c54d219a64a021b41e14a1ec83498a566cfca34c
geido <diegopucci.me@gmail.com>
2021-02-18 18:12:06 +0200

    Finalize tests

--- superset-frontend/src/components/ModalTrigger/ModalTrigger.test.tsx
+++ superset-frontend/src/components/ModalTrigger/ModalTrigger.test.tsx
@@ -20,9 +20,6 @@ import React from 'react';
 import { render, screen, waitFor } from 'spec/helpers/testing-library';
 import userEvent from '@testing-library/user-event';
 import { supersetTheme } from '@superset-ui/core';
-import Icon from 'src/components/Icon';
-import Button from 'src/components/Button';
-import Modal from 'src/common/components/Modal';
 import ModalTrigger from '.';
 
 const mockedProps = {
@@ -37,6 +34,20 @@ test('should render', () => {
 test('should render a button', () => {
   render(<ModalTrigger {...mockedProps} />);
   expect(screen.getByRole('button')).toBeInTheDocument();
+});
+
+test('should render a span element by default', () => {
+  render(<ModalTrigger {...mockedProps} />);
+  expect(screen.getByTestId('span-modal-trigger')).toBeInTheDocument();
+});
+
+test('should render a button element when specified', () => {
+  const btnProps = {
+    ...mockedProps,
+    isButton: true,
+  };
+  render(<ModalTrigger {...btnProps} />);
+  expect(screen.getByTestId('btn-modal-trigger')).toBeInTheDocument();
 });
 
 test('should render triggerNode', () => {
@@ -54,7 +65,6 @@ test('should render a tooltip on hover', async () => {
 
   userEvent.hover(screen.getByRole('button'));
   await waitFor(() => expect(screen.getByRole('tooltip')).toBeInTheDocument());
-  // expect(screen.getByText('I am a tooltip')).toBeInTheDocument();
 });
 
 test('should not render a modal before click', () => {
@@ -66,4 +76,16 @@ test('should render a modal after click', () => {
   render(<ModalTrigger {...mockedProps} />);
   userEvent.click(screen.getByRole('button'));
   expect(screen.getByRole('dialog')).toBeInTheDocument();
+});
+
+test('renders with theme', () => {
+  const btnProps = {
+    ...mockedProps,
+    isButton: true,
+  };
+  render(<ModalTrigger {...btnProps} />);
+  const button = screen.getByRole('button');
+  expect(button.firstChild).toHaveStyle(`
+    fontSize: ${supersetTheme.typography.sizes.s};
+  `);
 });
