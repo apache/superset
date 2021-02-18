63a1f0f52b46edec19cdab98c7d32e179b199a94
Ville Brofeldt <ville.v.brofeldt@gmail.com>
2021-02-18 13:03:15 +0200

    fix test

--- tests/db_engine_specs/pinot_tests.py
+++ tests/db_engine_specs/pinot_tests.py
@@ -30,7 +30,7 @@ class TestPinotDbEngineSpec(TestDbEngineSpec):
         self.assertEqual(
             result,
             "DATETIMECONVERT(tstamp, '1:SECONDS:EPOCH', '1:SECONDS:EPOCH', '1:DAYS')",
-        )  # noqa
+        )
 
     def test_pinot_time_expression_simple_date_format_1d_grain(self):
         col = column("tstamp")
@@ -43,7 +43,7 @@ class TestPinotDbEngineSpec(TestDbEngineSpec):
                 + "'1:SECONDS:SIMPLE_DATE_FORMAT:yyyy-MM-dd HH:mm:ss', "
                 + "'1:SECONDS:SIMPLE_DATE_FORMAT:yyyy-MM-dd HH:mm:ss', '1:DAYS')"
             ),
-        )  # noqa
+        )
 
     def test_pinot_time_expression_simple_date_format_1w_grain(self):
         col = column("tstamp")
@@ -55,7 +55,7 @@ class TestPinotDbEngineSpec(TestDbEngineSpec):
                 "ToDateTime(DATETRUNC('week', FromDateTime(tstamp, "
                 + "'yyyy-MM-dd HH:mm:ss'), 'MILLISECONDS'), 'yyyy-MM-dd HH:mm:ss')"
             ),
-        )  # noqa
+        )
 
     def test_pinot_time_expression_sec_one_1m_grain(self):
         col = column("tstamp")
@@ -63,17 +63,13 @@ class TestPinotDbEngineSpec(TestDbEngineSpec):
         result = str(expr.compile())
         self.assertEqual(
             result, "DATETRUNC('month', tstamp, 'SECONDS')",
-        )  # noqa
+        )
 
     def test_invalid_get_time_expression_arguments(self):
-        with self.assertRaises(NotImplementedError) as context:
+        with self.assertRaises(NotImplementedError):
             PinotEngineSpec.get_timestamp_expr(column("tstamp"), None, "P1M")
-        self.assertEqual("Empty date format for 'tstamp'", str(context.exception))
 
-        with self.assertRaises(NotImplementedError) as context:
+        with self.assertRaises(NotImplementedError):
             PinotEngineSpec.get_timestamp_expr(
                 column("tstamp"), "epoch_s", "invalid_grain"
             )
-        self.assertEqual(
-            "No pinot grain spec for 'invalid_grain'", str(context.exception)
-        )
