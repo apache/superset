d47f6ceeae906fefe9db6d53105f121cb4e534d4
Mayur <mayurnewase111@gmail.com>
2021-02-20 00:36:36 +0530

    feat(plugin-chart-echarts): add graph echart (#918)

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-core/test/query/buildQueryContext.test.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-core/test/query/buildQueryContext.test.ts
@@ -28,7 +28,6 @@ describe('buildQueryContext', () => {
     expect(queryContext.datasource.id).toBe(5);
     expect(queryContext.datasource.type).toBe('table');
     expect(queryContext.force).toBe(false);
-    expect(queryContext.force).toBe(false);
     expect(queryContext.result_format).toBe('json');
     expect(queryContext.result_type).toBe('full');
   });
@@ -43,5 +42,71 @@ describe('buildQueryContext', () => {
     expect(queryContext.datasource.id).toBe(5);
     expect(queryContext.datasource.type).toBe('druid');
     expect(queryContext.force).toBe(true);
+  });
+  it('should build datasource for table sources with columns', () => {
+    const queryContext = buildQueryContext(
+      {
+        datasource: '5__table',
+        granularity_sqla: 'ds',
+        viz_type: 'table',
+        source: 'source_column',
+        source_category: 'source_category_column',
+        target: 'target_column',
+        target_category: 'target_category_column',
+      },
+      {
+        queryFields: {
+          source: 'columns',
+          source_category: 'columns',
+          target: 'columns',
+          target_category: 'columns',
+        },
+      },
+    );
+    expect(queryContext.datasource.id).toBe(5);
+    expect(queryContext.datasource.type).toBe('table');
+    expect(queryContext.force).toBe(false);
+    expect(queryContext.result_format).toBe('json');
+    expect(queryContext.result_type).toBe('full');
+    expect(queryContext.queries).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          columns: [
+            'source_column',
+            'source_category_column',
+            'target_column',
+            'target_category_column',
+          ],
+        }),
+      ]),
+    );
+  });
+  it('should build datasource for table sources and process with custom function', () => {
+    const queryContext = buildQueryContext(
+      {
+        datasource: '5__table',
+        granularity_sqla: 'ds',
+        viz_type: 'table',
+        source: 'source_column',
+        source_category: 'source_category_column',
+        target: 'target_column',
+        target_category: 'target_category_column',
+      },
+      function addExtraColumn(queryObject) {
+        return [{ ...queryObject, columns: ['dummy_column'] }];
+      },
+    );
+    expect(queryContext.datasource.id).toBe(5);
+    expect(queryContext.datasource.type).toBe('table');
+    expect(queryContext.force).toBe(false);
+    expect(queryContext.result_format).toBe('json');
+    expect(queryContext.result_type).toBe('full');
+    expect(queryContext.queries).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          columns: ['dummy_column'],
+        }),
+      ]),
+    );
   });
 });
--- superset-frontend/temporary_superset_ui/superset-ui/plugins/plugin-chart-echarts/src/index.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/plugin-chart-echarts/src/index.ts
@@ -19,6 +19,7 @@
 export { default as EchartsBoxPlotChartPlugin } from './BoxPlot';
 export { default as EchartsTimeseriesChartPlugin } from './Timeseries';
 export { default as EchartsPieChartPlugin } from './Pie';
+export { default as EchartsGraphChartPlugin } from './Graph';
 
 /**
  * Note: this file exports the default export from EchartsTimeseries.tsx.
