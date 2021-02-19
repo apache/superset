70242c1d16ee9923c0956b0862d19614d9005e9c
Mayur <mayurnewase111@gmail.com>
2021-02-20 00:36:36 +0530

    feat(plugin-chart-echarts): add graph echart (#918)

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-core/src/query/buildQueryContext.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-core/src/query/buildQueryContext.ts
@@ -1,18 +1,27 @@
 import buildQueryObject from './buildQueryObject';
 import DatasourceKey from './DatasourceKey';
-import { QueryFormData } from './types/QueryFormData';
+import { QueryFieldAliases, QueryFormData } from './types/QueryFormData';
 import { QueryContext, QueryObject } from './types/Query';
 
 const WRAP_IN_ARRAY = (baseQueryObject: QueryObject) => [baseQueryObject];
 
+export type BuildFinalQuerieObjects = (baseQueryObject: QueryObject) => QueryObject[];
+
 export default function buildQueryContext(
   formData: QueryFormData,
-  buildQuery: (baseQueryObject: QueryObject) => QueryObject[] = WRAP_IN_ARRAY,
+  options?:
+    | {
+        buildQuery?: BuildFinalQuerieObjects;
+        queryFields?: QueryFieldAliases;
+      }
+    | BuildFinalQuerieObjects,
 ): QueryContext {
+  const { queryFields, buildQuery = WRAP_IN_ARRAY } =
+    typeof options === 'function' ? { buildQuery: options, queryFields: {} } : options || {};
   return {
     datasource: new DatasourceKey(formData.datasource).toObject(),
     force: formData.force || false,
-    queries: buildQuery(buildQueryObject(formData)),
+    queries: buildQuery(buildQueryObject(formData, queryFields)),
     result_format: formData.result_format || 'json',
     result_type: formData.result_type || 'full',
   };
