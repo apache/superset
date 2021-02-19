9a84f7e5cec803aac84735bce60742f5fd043bf7
hughhhh <hughmil3s@gmail.com>
2021-02-19 18:02:08 -0500

    remove

--- superset/databases/api.py
+++ superset/databases/api.py
@@ -801,7 +801,7 @@ class DatabaseRestApi(BaseSupersetModelRestApi):
             logger.warning("Import database failed")
             return self.response_422(message=exc.normalized_messages())
         except DatabaseImportError as exc:
-            logger.warning("Import database failed", exc_info=True)
+            logger.exception("Import database failed")
             return self.response_500(message=str(exc))
 
     @expose("/<int:pk>/function_names/", methods=["GET"])
