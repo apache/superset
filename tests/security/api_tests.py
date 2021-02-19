d136e599edf982e0076a625bf5bea9c5c5583e96
Daniel Gaspar <danielvazgaspar@gmail.com>
2021-02-19 13:03:39 +0000

    fix test

--- tests/security/api_tests.py
+++ tests/security/api_tests.py
@@ -25,13 +25,23 @@ from flask_wtf.csrf import generate_csrf
 class TestSecurityApi(SupersetTestCase):
     resource_name = "security"
 
-    def test_get_dashboard(self):
-        """
-        Security API: Test get CSRF token
-        """
-        self.login(username="admin")
+    def _assert_get_csrf_token(self):
         uri = f"api/v1/{self.resource_name}/csrf_token/"
         response = self.client.get(uri)
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.data.decode("utf-8"))
         assert data["result"] == generate_csrf()
+
+    def test_get_csrf_token(self):
+        """
+        Security API: Test get CSRF token
+        """
+        self.login(username="admin")
+        self._assert_get_csrf_token()
+
+    def test_get_csrf_token_gamma(self):
+        """
+        Security API: Test get CSRF token by gamma
+        """
+        self.login(username="gamma")
+        self._assert_get_csrf_token()
