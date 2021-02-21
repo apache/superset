b6129b47df769039dbbcca4904c8c0c30e9b92ab
mayur <mayurnewase111@gmail.com>
2021-02-21 09:36:16 +0530

    lint

--- superset-frontend/cypress-base/cypress/integration/explore/visualizations/graph.test.js
+++ superset-frontend/cypress-base/cypress/integration/explore/visualizations/graph.test.js
@@ -17,54 +17,54 @@
  * under the License.
  */
 describe('Visualization > Graph', () => {
-    const GRAPH_FORM_DATA = {
-      datasource: '1__table',
-      viz_type: 'graph_chart',
-      slice_id: 55,
-      granularity_sqla: 'ds',
-      time_grain_sqla: 'P1D',
-      time_range: '100 years ago : now',
-      metric: 'sum__value',
-      adhoc_filters: [],
-      source: 'source',
-      target: 'target',
-      row_limit: 50000,
-      show_legend: true,
-      show_labels: true,
-      labels_outside: true,
-      color_scheme: 'bnbColors',
-    };
+  const GRAPH_FORM_DATA = {
+    datasource: '1__table',
+    viz_type: 'graph_chart',
+    slice_id: 55,
+    granularity_sqla: 'ds',
+    time_grain_sqla: 'P1D',
+    time_range: '100 years ago : now',
+    metric: 'sum__value',
+    adhoc_filters: [],
+    source: 'source',
+    target: 'target',
+    row_limit: 50000,
+    show_legend: true,
+    show_labels: true,
+    labels_outside: true,
+    color_scheme: 'bnbColors',
+  };
 
-    function verify(formData) {
-      cy.visitChartByParams(JSON.stringify(formData));
-      cy.verifySliceSuccess({ waitAlias: '@getJson' });
-    }
+  function verify(formData) {
+    cy.visitChartByParams(JSON.stringify(formData));
+    cy.verifySliceSuccess({ waitAlias: '@getJson' });
+  }
 
-    beforeEach(() => {
-      cy.login();
-      cy.intercept('POST', '/api/v1/chart/data*').as('getJson');
-    });
-
-    it('should work with ad-hoc metric', () => {
-      verify(GRAPH_FORM_DATA);
-      cy.get('.chart-container .graph_chart canvas').should('have.length', 1);
-    });
-
-    it('should work with simple filter', () => {
-      verify({
-        ...GRAPH_FORM_DATA,
-        adhoc_filters: [
-          {
-            expressionType: 'SIMPLE',
-            subject: 'source',
-            operator: '==',
-            comparator: 'Agriculture',
-            clause: 'WHERE',
-            sqlExpression: null,
-            filterOptionName: 'filter_tqx1en70hh_7nksse7nqic',
-          },
-        ],
-      });
-      cy.get('.chart-container .graph_chart canvas').should('have.length', 1);
-    });
+  beforeEach(() => {
+    cy.login();
+    cy.intercept('POST', '/api/v1/chart/data*').as('getJson');
   });
+
+  it('should work with ad-hoc metric', () => {
+    verify(GRAPH_FORM_DATA);
+    cy.get('.chart-container .graph_chart canvas').should('have.length', 1);
+  });
+
+  it('should work with simple filter', () => {
+    verify({
+      ...GRAPH_FORM_DATA,
+      adhoc_filters: [
+        {
+          expressionType: 'SIMPLE',
+          subject: 'source',
+          operator: '==',
+          comparator: 'Agriculture',
+          clause: 'WHERE',
+          sqlExpression: null,
+          filterOptionName: 'filter_tqx1en70hh_7nksse7nqic',
+        },
+      ],
+    });
+    cy.get('.chart-container .graph_chart canvas').should('have.length', 1);
+  });
+});
