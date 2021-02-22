cd75cdac62fb66b3d77645685f8240b65a04bb5d
Daniel Gaspar <danielvazgaspar@gmail.com>
2021-02-22 03:34:47 +0000

    fix tests

--- tests/fixtures/public_role.py
+++ tests/fixtures/public_role.py
@@ -30,3 +30,15 @@ def public_role_like_gamma():
 
         security_manager.get_public_role().permissions = []
         db.session.commit()
+
+
+@pytest.fixture()
+def public_role_like_test_role():
+    with app.app_context():
+        app.config["PUBLIC_ROLE_LIKE"] = "TestRole"
+        security_manager.sync_role_definitions()
+
+        yield
+
+        security_manager.get_public_role().permissions = []
+        db.session.commit()
