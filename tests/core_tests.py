42435f643a622eb8e9fca28ed8851deefac4ee63
hughhhh <hughmil3s@gmail.com>
2021-02-22 15:09:38 -0500

    yero

--- tests/core_tests.py
+++ tests/core_tests.py
@@ -70,6 +70,7 @@ from tests.fixtures.world_bank_dashboard import load_world_bank_dashboard_with_s
 from tests.fixtures.core import (
     bad_database_test_conn_request,
     create_query_editor_request,
+    dist_bar_form_data,
 )
 
 logger = logging.getLogger(__name__)
@@ -890,68 +891,7 @@ class TestCore(SupersetTestCase):
     @pytest.mark.usefixtures("load_birth_names_dashboard_with_slices")
     def test_explore_json_dist_bar_order(self):
         tbl_id = self.table_ids.get("birth_names")
-        form_data = {
-            "datasource": f"{tbl_id}__table",
-            "viz_type": "dist_bar",
-            "url_params": {},
-            "time_range_endpoints": ["inclusive", "exclusive"],
-            "granularity_sqla": "ds",
-            "time_range": 'DATEADD(DATETIME("2021-01-22T00:00:00"), -100, year) : 2021-01-22T00:00:00',
-            "metrics": [
-                {
-                    "expressionType": "SIMPLE",
-                    "column": {
-                        "id": 334,
-                        "column_name": "name",
-                        "verbose_name": "null",
-                        "description": "null",
-                        "expression": "",
-                        "filterable": True,
-                        "groupby": True,
-                        "is_dttm": False,
-                        "type": "VARCHAR(255)",
-                        "python_date_format": "null",
-                    },
-                    "aggregate": "COUNT",
-                    "sqlExpression": "null",
-                    "isNew": False,
-                    "hasCustomLabel": False,
-                    "label": "COUNT(name)",
-                    "optionName": "metric_xdzsijn42f9_khi4h3v3vci",
-                },
-                {
-                    "expressionType": "SIMPLE",
-                    "column": {
-                        "id": 332,
-                        "column_name": "ds",
-                        "verbose_name": "null",
-                        "description": "null",
-                        "expression": "",
-                        "filterable": True,
-                        "groupby": True,
-                        "is_dttm": True,
-                        "type": "TIMESTAMP WITHOUT TIME ZONE",
-                        "python_date_format": "null",
-                    },
-                    "aggregate": "COUNT",
-                    "sqlExpression": "null",
-                    "isNew": False,
-                    "hasCustomLabel": False,
-                    "label": "COUNT(ds)",
-                    "optionName": "metric_80g1qb9b6o7_ci5vquydcbe",
-                },
-            ],
-            "adhoc_filters": [],
-            "groupby": ["name"],
-            "columns": [],
-            "row_limit": 10,
-            "color_scheme": "supersetColors",
-            "label_colors": {},
-            "show_legend": True,
-            "y_axis_format": "SMART_NUMBER",
-            "bottom_margin": "auto",
-            "x_ticks_layout": "auto",
-        }
+        form_data = dist_bar_form_data(table_id=tbl_id)
 
         self.login(username="admin")
         rv = self.client.post(
@@ -988,17 +928,7 @@ class TestCore(SupersetTestCase):
     )
     def test_explore_json_async(self):
         tbl_id = self.table_ids.get("birth_names")
-        form_data = {
-            "datasource": f"{tbl_id}__table",
-            "viz_type": "dist_bar",
-            "time_range_endpoints": ["inclusive", "exclusive"],
-            "granularity_sqla": "ds",
-            "time_range": "No filter",
-            "metrics": ["count"],
-            "adhoc_filters": [],
-            "groupby": ["gender"],
-            "row_limit": 100,
-        }
+        form_data = dist_bar_form_data(table_id=tbl_id)
         async_query_manager.init_app(app)
         self.login(username="admin")
         rv = self.client.post(
--- tests/fixtures/core.py
+++ tests/fixtures/core.py
@@ -36,3 +36,68 @@ def create_query_editor_request():
             }
         )
     }
+
+
+def dist_bar_form_data(table_id):
+    return {
+        "datasource": f"{table_id}__table",
+        "viz_type": "dist_bar",
+        "url_params": {},
+        "time_range_endpoints": ["inclusive", "exclusive"],
+        "granularity_sqla": "ds",
+        "time_range": 'DATEADD(DATETIME("2021-01-22T00:00:00"), -100, year) : 2021-01-22T00:00:00',
+        "metrics": [
+            {
+                "expressionType": "SIMPLE",
+                "column": {
+                    "id": 334,
+                    "column_name": "name",
+                    "verbose_name": "null",
+                    "description": "null",
+                    "expression": "",
+                    "filterable": True,
+                    "groupby": True,
+                    "is_dttm": False,
+                    "type": "VARCHAR(255)",
+                    "python_date_format": "null",
+                },
+                "aggregate": "COUNT",
+                "sqlExpression": "null",
+                "isNew": False,
+                "hasCustomLabel": False,
+                "label": "COUNT(name)",
+                "optionName": "metric_xdzsijn42f9_khi4h3v3vci",
+            },
+            {
+                "expressionType": "SIMPLE",
+                "column": {
+                    "id": 332,
+                    "column_name": "ds",
+                    "verbose_name": "null",
+                    "description": "null",
+                    "expression": "",
+                    "filterable": True,
+                    "groupby": True,
+                    "is_dttm": True,
+                    "type": "TIMESTAMP WITHOUT TIME ZONE",
+                    "python_date_format": "null",
+                },
+                "aggregate": "COUNT",
+                "sqlExpression": "null",
+                "isNew": False,
+                "hasCustomLabel": False,
+                "label": "COUNT(ds)",
+                "optionName": "metric_80g1qb9b6o7_ci5vquydcbe",
+            },
+        ],
+        "adhoc_filters": [],
+        "groupby": ["name"],
+        "columns": [],
+        "row_limit": 10,
+        "color_scheme": "supersetColors",
+        "label_colors": {},
+        "show_legend": True,
+        "y_axis_format": "SMART_NUMBER",
+        "bottom_margin": "auto",
+        "x_ticks_layout": "auto",
+    }
