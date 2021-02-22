6a5d56978be8643c7601be1b58313c1344b7cd32
Bruno Motta <wadge.motta@gmail.com>
2021-02-22 18:41:12 -0300

    Rerun CI

--- superset-frontend/src/components/FlashProvider/FlashProvider.test.tsx
+++ superset-frontend/src/components/FlashProvider/FlashProvider.test.tsx
@@ -16,7 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
