87ccefedfbe570561846527808119e16f535d8df
Arash <arash.afghahi@gmail.com>
2021-02-23 10:07:59 -0500

    query search

--- superset-frontend/src/SqlLab/components/QuerySearch.tsx
+++ superset-frontend/src/SqlLab/components/QuerySearch.tsx
@@ -16,8 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import React, { useState, useEffect, memo } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
 import Button from 'src/components/Button';
 import Select from 'src/components/Select';
 import { styled, t, SupersetClient } from '@superset-ui/core';
@@ -33,11 +32,14 @@ import {
 import { STATUS_OPTIONS, TIME_OPTIONS } from '../constants';
 import AsyncSelect from '../../components/AsyncSelect';
 
-const propTypes = {
-  actions: PropTypes.object.isRequired,
+interface propTypes {
+  actions: {
+    addDangerToast: (msg: string) => void;
+    setDatabases: (data: Array<any>) => Array<any>;
+  };
   // why is height required and then never used or invoked?
-  displayLimit: PropTypes.number.isRequired,
-};
+  displayLimit: number;
+}
 
 const TableWrapper = styled.div`
   display: flex;
@@ -61,18 +63,17 @@ const TableStyles = styled.div`
 const StyledTableStylesContainer = styled.div`
   overflow: auto;
 `;
-// height is invoked in the interface above, and I also saw that there were some functions that pass height into this function, but it is never used
-function QuerySearch({ actions, displayLimit }) {
-  const [databaseId, setDatabaseId] = useState(null);
-  const [userId, setUserId] = useState(null);
-  const [searchText, setSearchText] = useState(null);
-  const [from, setFrom] = useState('28 days ago');
-  const [to, setTo] = useState('now');
-  const [status, setStatus] = useState('success');
+function QuerySearch({ actions, displayLimit }: propTypes) {
+  const [databaseId, setDatabaseId] = useState<string>('');
+  const [userId, setUserId] = useState<string>('');
+  const [searchText, setSearchText] = useState<string>('');
+  const [from, setFrom] = useState<string>('28 days ago');
+  const [to, setTo] = useState<string>('now');
+  const [status, setStatus] = useState<string>('success');
   const [queriesArray, setQueriesArray] = useState([]);
-  const [queriesLoading, setQueriesLoading] = useState(true);
+  const [queriesLoading, setQueriesLoading] = useState<boolean>(true);
 
-  const getTimeFromSelection = selection => {
+  const getTimeFromSelection = (selection: string) => {
     switch (selection) {
       case 'now':
         return now();
@@ -93,24 +94,25 @@ function QuerySearch({ actions, displayLimit }) {
     }
   };
 
-  const insertParams = (baseUrl, params) => {
+  const insertParams = (baseUrl: string, params: string[]) => {
     const validParams = params.filter(function (p) {
       return p !== '';
     });
     return `${baseUrl}?${validParams.join('&')}`;
   };
 
-  const refreshQueries = () => {
+  const refreshQueries = async () => {
     setQueriesLoading(true);
     const params = [
-      userId ? `user_id=${userId}` : '',
-      databaseId ? `database_id=${databaseId}` : '',
-      searchText ? `search_text=${searchText}` : '',
-      status ? `status=${status}` : '',
-      from ? `from=${getTimeFromSelection(from)}` : '',
-      to ? `to=${getTimeFromSelection(to)}` : '',
+      userId && `user_id=${userId}`,
+      databaseId && `database_id=${databaseId}`,
+      searchText && `search_text=${searchText}`,
+      status && `status=${status}`,
+      from && `from=${getTimeFromSelection(from)}`,
+      to && `to=${getTimeFromSelection(to)}`,
     ];
 
+    // make into async method
     SupersetClient.get({
       endpoint: insertParams('/superset/search_queries', params),
     })
@@ -122,38 +124,40 @@ function QuerySearch({ actions, displayLimit }) {
         actions.addDangerToast(t('An error occurred when refreshing queries'));
       });
   };
-  // functionally this is closest to componentDidMount, though Abramov recommends that it is better if you have a more robust dependency array, though I am afraid that will go into a useCallback loop like before. Though, I could remove some of the refreshQueries in the other methods, and instead have the dependency array focus on when values are changed? That would maybe not play nicely with onChange though.
+
   useEffect(() => {
     refreshQueries();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  });
 
-  const onUserClicked = userId => {
+  const onUserClicked = (userId: string) => {
     setUserId(userId);
     refreshQueries();
   };
-  // add these to dependency array instead of having their own functions.
 
-  const onDbClicked = dbId => {
+  const onDbClicked = (dbId: string) => {
     // By my reading of this, it is setting state, and then running the refreshQueries function. Is that correct? I ended up keeping these two, because I couldn't think of a good way of enabling these in useEffect without also having it refreshQueries when you change the DB or example in the DB (which was not part of original functionality)
     setDatabaseId(dbId);
     refreshQueries();
   };
 
-  const onKeyDown = event => {
+  const onKeyDown = (event: any) => {
     if (event.keyCode === 13) {
       refreshQueries();
     }
   };
 
-  const userMutator = data =>
-    data.result.map(({ value, text }) => ({
+  interface userMutatorProps {
+    value: number;
+    text: string;
+  }
+  const userMutator = (data: any) =>
+    data.result.map(({ value, text }: userMutatorProps) => ({
       label: text,
       value,
     }));
 
-  const dbMutator = data => {
-    const options = data.result.map(db => ({
+  const dbMutator = (data: any) => {
+    const options = data.result.map((db: any) => ({
       value: db.id,
       label: db.database_name,
     }));
@@ -174,13 +178,13 @@ function QuerySearch({ actions, displayLimit }) {
             dataEndpoint="api/v1/query/related/user"
             mutator={userMutator}
             value={userId}
-            onChange={selected => setUserId(selected?.value)}
+            onChange={(selected: any) => setUserId(selected?.value)}
             placeholder={t('Filter by user')}
           />
         </div>
         <div className="col-sm-2">
           <AsyncSelect
-            onChange={db => setDatabaseId(db?.value)}
+            onChange={(db: any) => setDatabaseId(db?.value)}
             dataEndpoint="/api/v1/database/?q=(filters:!((col:expose_in_sqllab,opr:eq,value:!t)))"
             value={databaseId}
             mutator={dbMutator}
@@ -190,7 +194,7 @@ function QuerySearch({ actions, displayLimit }) {
         <div className="col-sm-4">
           <input
             type="text"
-            onChange={event => setSearchText(event.target.value)}
+            onChange={(event: any) => setSearchText(event.target.value)}
             onKeyDown={onKeyDown}
             className="form-control input-sm"
             placeholder={t('Query search string')}
@@ -204,18 +208,18 @@ function QuerySearch({ actions, displayLimit }) {
               value: xt,
               label: xt,
             }))}
-            value={from}
+            value={(from as unknown) as undefined}
             autosize={false}
-            onChange={selected => setFrom(selected?.value)}
+            onChange={(selected: any) => setFrom(selected?.value)}
           />
 
           <Select
             name="select-to"
             placeholder={t('[To]-')}
             options={TIME_OPTIONS.map(xt => ({ value: xt, label: xt }))}
-            value={to}
+            value={(to as unknown) as undefined}
             autosize={false}
-            onChange={selected => setTo(selected?.value)}
+            onChange={(selected: any) => setTo(selected?.value)}
           />
 
           <Select
@@ -225,10 +229,10 @@ function QuerySearch({ actions, displayLimit }) {
               value: s,
               label: s,
             }))}
-            value={status}
+            value={(status as unknown) as undefined}
             isLoading={false}
             autosize={false}
-            onChange={selected => setStatus(selected?.value)}
+            onChange={(selected: any) => setStatus(selected?.value)}
           />
 
           <Button
@@ -268,6 +272,4 @@ function QuerySearch({ actions, displayLimit }) {
     </TableWrapper>
   );
 }
-QuerySearch.propTypes = propTypes;
-// using memo here because the original component was a Pure Component, and memo replicates that.
-export default memo(QuerySearch);
+export default QuerySearch;
