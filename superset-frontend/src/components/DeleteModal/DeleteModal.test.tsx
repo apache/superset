5dd202c55e1b52fcfca5f02032a3f368685a8ee4
Bruno Motta <wadge.motta@gmail.com>
2021-02-24 09:50:40 -0300

    re-run tests

--- superset-frontend/src/components/DeleteModal/DeleteModal.test.tsx
+++ superset-frontend/src/components/DeleteModal/DeleteModal.test.tsx
@@ -16,7 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 import React from 'react';
 import { supersetTheme, ThemeProvider } from '@superset-ui/core';
 import '@testing-library/jest-dom';
