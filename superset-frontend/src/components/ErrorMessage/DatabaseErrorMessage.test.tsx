bac91be16bf0385b999311c2b92993ff1ccc7ae7
geido <diegopucci.me@gmail.com>
2021-02-25 21:28:12 +0200

    Finalize DatabaseErrorMessage tests

--- superset-frontend/src/components/ErrorMessage/DatabaseErrorMessage.test.tsx
+++ superset-frontend/src/components/ErrorMessage/DatabaseErrorMessage.test.tsx
@@ -19,13 +19,13 @@
 
 import React from 'react';
 import { render, screen } from 'spec/helpers/testing-library';
-import { supersetTheme } from '@superset-ui/core';
+import userEvent from '@testing-library/user-event';
 import DatabaseErrorMessage from './DatabaseErrorMessage';
 import { ErrorLevel, ErrorSource, ErrorTypeEnum } from './types';
 
 const mockedProps = {
   error: {
-    error_type: ErrorTypeEnum.FRONTEND_NETWORK_ERROR,
+    error_type: ErrorTypeEnum.DATABASE_SECURITY_ACCESS_ERROR,
     extra: {
       engine_name: 'Engine name',
       issue_codes: [
@@ -46,36 +46,56 @@ const mockedProps = {
   source: 'dashboard' as ErrorSource,
 };
 
-jest.mock('../Icon', () => ({
-  __esModule: true,
-  default: ({ name }: { name: string }) => (
-    <div data-test="icon" data-name={name} />
-  ),
-}));
-
 test('should render', () => {
   const { container } = render(<DatabaseErrorMessage {...mockedProps} />);
   expect(container).toBeInTheDocument();
 });
 
 test('should render the default text', () => {
-  render(<DatabaseErrorMessage {...mockedProps} />);
-  expect(screen.getByText('This may be triggered by:')).toBeInTheDocument();
+  render(<DatabaseErrorMessage {...mockedProps} />, { useRedux: true });
+  const button = screen.getByText('See more');
+  userEvent.click(button);
+  expect(screen.getByText(/This may be triggered by:/)).toBeInTheDocument();
 });
 
 test('should render the issue codes', () => {
-  render(<DatabaseErrorMessage {...mockedProps} />);
-  expect(screen.getByText('Issue code message A')).toBeInTheDocument();
-  expect(screen.getByText('Issue code message B')).toBeInTheDocument();
+  render(<DatabaseErrorMessage {...mockedProps} />, { useRedux: true });
+  const button = screen.getByText('See more');
+  userEvent.click(button);
+  expect(screen.getByText(/Issue code message A/)).toBeInTheDocument();
+  expect(
+    screen.getByText(/Issue code message B/, { exact: false }),
+  ).toBeInTheDocument();
 });
 
 test('should render the engine name', () => {
   render(<DatabaseErrorMessage {...mockedProps} />);
-  expect(screen.getByText('Engine name')).toBeInTheDocument();
+  expect(screen.getByText(/Engine name/)).toBeInTheDocument();
 });
 
 test('should render the owners', () => {
-  render(<DatabaseErrorMessage {...mockedProps} />);
-  expect(screen.getByText('Owner A')).toBeInTheDocument();
-  expect(screen.getByText('Owner B')).toBeInTheDocument();
+  render(<DatabaseErrorMessage {...mockedProps} />, { useRedux: true });
+  const button = screen.getByText('See more');
+  userEvent.click(button);
+  expect(
+    screen.getByText('Please reach out to the Chart Owners for assistance.'),
+  ).toBeInTheDocument();
+  expect(
+    screen.getByText('Chart Owners: Owner A, Owner B'),
+  ).toBeInTheDocument();
+});
+
+test('should NOT render the owners', () => {
+  const noVisualizationProps = {
+    ...mockedProps,
+    source: 'sqllab' as ErrorSource,
+  };
+  render(<DatabaseErrorMessage {...noVisualizationProps} />, {
+    useRedux: true,
+  });
+  const button = screen.getByText('See more');
+  userEvent.click(button);
+  expect(
+    screen.queryByText('Chart Owners: Owner A, Owner B'),
+  ).not.toBeInTheDocument();
 });
