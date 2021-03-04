f9d5f3f2d9166db05a5aa505efc05978beae818d
hughhhh <hughmil3s@gmail.com>
2021-03-04 16:13:12 -0500

    add logs to test_connection

--- superset/databases/commands/test_connection.py
+++ superset/databases/commands/test_connection.py
@@ -32,6 +32,7 @@ from superset.databases.commands.exceptions import (
 )
 from superset.databases.dao import DatabaseDAO
 from superset.exceptions import SupersetSecurityException
+from superset.extensions import event_logger
 from superset.models.core import Database
 
 logger = logging.getLogger(__name__)
@@ -55,25 +56,49 @@ class TestConnectionDatabaseCommand(BaseCommand):
                 impersonate_user=self._properties.get("impersonate_user", False),
                 encrypted_extra=self._properties.get("encrypted_extra", "{}"),
             )
-            if database is not None:
-                database.set_sqlalchemy_uri(uri)
-                database.db_engine_spec.mutate_db_for_connection_test(database)
-                username = self._actor.username if self._actor is not None else None
-                engine = database.get_sqla_engine(user_name=username)
+            if database is None:
+                raise DBAPIError("Database is not found", None, None)
+
+            database.set_sqlalchemy_uri(uri)
+            database.db_engine_spec.mutate_db_for_connection_test(database)
+            username = self._actor.username if self._actor is not None else None
+            engine = database.get_sqla_engine(user_name=username)
             with closing(engine.raw_connection()) as conn:
                 if not engine.dialect.do_ping(conn):
                     raise DBAPIError(None, None, None)
-        except (NoSuchModuleError, ModuleNotFoundError):
+
+            with event_logger(
+                action="test_connection_success", engine=make_url(uri).drivername
+            ):
+                pass
+
+        except (NoSuchModuleError, ModuleNotFoundError) as ex:
             driver_name = make_url(uri).drivername
-            raise DatabaseTestConnectionDriverError(
-                message=_("Could not load database driver: {}").format(driver_name),
-            )
-        except DBAPIError:
-            raise DatabaseTestConnectionFailedError()
+            with event_logger(
+                action=f"test_connection_error.{ex.__class__.__name__}",
+                engine=make_url(uri).drivername,
+            ):
+                raise DatabaseTestConnectionDriverError(
+                    message=_("Could not load database driver: {}").format(driver_name),
+                )
+        except DBAPIError as ex:
+            with event_logger(
+                action=f"test_connection_error.{ex.__class__.__name__}",
+                engine=make_url(uri).drivername,
+            ):
+                raise DatabaseTestConnectionFailedError()
         except SupersetSecurityException as ex:
-            raise DatabaseSecurityUnsafeError(message=str(ex))
-        except Exception:
-            raise DatabaseTestConnectionUnexpectedError()
+            with event_logger(
+                action=f"test_connection_error.{ex.__class__.__name__}",
+                engine=make_url(uri).drivername,
+            ):
+                raise DatabaseSecurityUnsafeError(message=str(ex))
+        except Exception as ex:
+            with event_logger(
+                action=f"test_connection_error.{ex.__class__.__name__}",
+                engine=make_url(uri).drivername,
+            ):
+                raise DatabaseTestConnectionUnexpectedError()
 
     def validate(self) -> None:
         database_name = self._properties.get("database_name")
