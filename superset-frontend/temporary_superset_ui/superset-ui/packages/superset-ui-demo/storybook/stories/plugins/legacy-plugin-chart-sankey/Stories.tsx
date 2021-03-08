9c920cb7b27511757ebd9c70627778ad61c18166
Victor Malai <malai9696@gmail.com>
2021-03-08 11:51:43 +0200

    feat: change font size on responsive for sankey and sunburst chart (#977)
    * feat: change font size on responsive
    
    * remove logs
    
    * Sunburst
    
    * WIP
    
    * Sankey label hide
    
    * Format correct object
    
    * add styled components
    
    * Replace document with container
    
    * Return overlapping elements
    
    * Drag
    
    * Fix lint
    
    * Fix test & make tooltip absoliute based on cursor position
    
    * Update storybook for charts
    
    * Resizable in separate page
    
    * Fix responsive class for sunburst
    
    * type

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-demo/storybook/stories/plugins/legacy-plugin-chart-sankey/Stories.tsx
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-demo/storybook/stories/plugins/legacy-plugin-chart-sankey/Stories.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { SuperChart } from '@superset-ui/core';
 import SankeyChartPlugin from '@superset-ui/legacy-plugin-chart-sankey';
+import ResizableChartDemo from '../../../shared/components/ResizableChartDemo';
 import data from './data';
 
 new SankeyChartPlugin().configure({ key: 'sankey' }).register();
@@ -20,4 +21,20 @@ export const basic = () => (
       colorScheme: 'd3Category10',
     }}
   />
+);
+
+export const resizable = () => (
+  <ResizableChartDemo>
+    {({ width, height }) => (
+      <SuperChart
+        chartType="sankey"
+        width={width}
+        height={height}
+        queriesData={[{ data }]}
+        formData={{
+          colorScheme: 'd3Category10',
+        }}
+      />
+    )}
+  </ResizableChartDemo>
 );
