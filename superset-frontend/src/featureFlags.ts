0bf57062e12593b631d644c8387826da8c6f00ac
Jesse Yang <jesse.yang@airbnb.com>
2021-03-17 13:47:09 -0700

    fix: use FeatureFlags in @superset-ui/core

--- superset-frontend/src/featureFlags.ts
+++ superset-frontend/src/featureFlags.ts
@@ -16,46 +16,9 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-// We can codegen the enum definition based on a list of supported flags that we
-// check into source control. We're hardcoding the supported flags for now.
-export enum FeatureFlag {
-  ALLOW_DASHBOARD_DOMAIN_SHARDING = 'ALLOW_DASHBOARD_DOMAIN_SHARDING',
-  OMNIBAR = 'OMNIBAR',
-  CLIENT_CACHE = 'CLIENT_CACHE',
-  DYNAMIC_PLUGINS = 'DYNAMIC_PLUGINS',
-  SCHEDULED_QUERIES = 'SCHEDULED_QUERIES',
-  SQL_VALIDATORS_BY_ENGINE = 'SQL_VALIDATORS_BY_ENGINE',
-  ESTIMATE_QUERY_COST = 'ESTIMATE_QUERY_COST',
-  SHARE_QUERIES_VIA_KV_STORE = 'SHARE_QUERIES_VIA_KV_STORE',
-  SQLLAB_BACKEND_PERSISTENCE = 'SQLLAB_BACKEND_PERSISTENCE',
-  THUMBNAILS = 'THUMBNAILS',
-  LISTVIEWS_DEFAULT_CARD_VIEW = 'LISTVIEWS_DEFAULT_CARD_VIEW',
-  ENABLE_REACT_CRUD_VIEWS = 'ENABLE_REACT_CRUD_VIEWS',
-  DISABLE_DATASET_SOURCE_EDIT = 'DISABLE_DATASET_SOURCE_EDIT',
-  DISPLAY_MARKDOWN_HTML = 'DISPLAY_MARKDOWN_HTML',
-  ESCAPE_MARKDOWN_HTML = 'ESCAPE_MARKDOWN_HTML',
-  DASHBOARD_NATIVE_FILTERS = 'DASHBOARD_NATIVE_FILTERS',
-  DASHBOARD_CROSS_FILTERS = 'DASHBOARD_CROSS_FILTERS',
-  DASHBOARD_RBAC = 'DASHBOARD_RBAC',
-  DASHBOARD_NATIVE_FILTERS_SET = 'DASHBOARD_NATIVE_FILTERS_SET',
-  VERSIONED_EXPORT = 'VERSIONED_EXPORT',
-  GLOBAL_ASYNC_QUERIES = 'GLOBAL_ASYNC_QUERIES',
-  ENABLE_TEMPLATE_PROCESSING = 'ENABLE_TEMPLATE_PROCESSING',
-  ENABLE_EXPLORE_DRAG_AND_DROP = 'ENABLE_EXPLORE_DRAG_AND_DROP',
-}
+import { FeatureFlagMap, FeatureFlag } from '@superset-ui/core';
 
-export type FeatureFlagMap = {
-  [key in FeatureFlag]?: boolean;
-};
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-declare global {
-  interface Window {
-    featureFlags: FeatureFlagMap;
-    $: any;
-    jQuery: any;
-  }
-}
+export { FeatureFlagMap, FeatureFlag } from '@superset-ui/core';
 
 export function initFeatureFlags(featureFlags: FeatureFlagMap) {
   if (!window.featureFlags) {
