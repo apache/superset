e6c5bf4d5404e7e043af4e570987cfec227c60bf
Bruno Motta <wadge.motta@gmail.com>
2021-03-18 15:50:50 -0300

    test: useChangeEffect (#13555)

--- superset-frontend/src/common/hooks/useChangeEffect/index.ts
+++ superset-frontend/src/common/hooks/useChangeEffect/index.ts
@@ -17,22 +17,4 @@
  * under the License.
  */
 
-import { useEffect } from 'react';
-import { usePrevious } from './usePrevious';
-
-/**
- * Calls the callback when the value changes.
- *
- * Passes the previous and current values to the callback
- */
-export function useChangeEffect<T>(
-  value: T,
-  callback: (previous: T | undefined, current: T) => void,
-) {
-  const previous = usePrevious(value);
-  useEffect(() => {
-    if (value !== previous) {
-      callback(previous, value);
-    }
-  }, [value, previous, callback]);
-}
+export * from './useChangeEffect';
