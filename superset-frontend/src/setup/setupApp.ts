375a0f479e4846af0cd64b184a3b80a5d14be201
Beto Dealmeida <roberto@dealmeida.net>
2021-03-19 12:14:41 -0700

    chore: remove jquery

--- superset-frontend/src/setup/setupApp.ts
+++ superset-frontend/src/setup/setupApp.ts
@@ -18,11 +18,6 @@
  */
 /* eslint global-require: 0 */
 import $ from 'jquery';
-import { SupersetClient } from '@superset-ui/core';
-import {
-  getClientErrorObject,
-  ClientErrorObject,
-} from 'src/utils/getClientErrorObject';
 import setupErrorMessages from 'src/setup/setupErrorMessages';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -33,62 +28,7 @@ declare global {
   }
 }
 
-function showApiMessage(resp: ClientErrorObject) {
-  const template =
-    '<div class="alert"> ' +
-    '<button type="button" class="close" ' +
-    'data-dismiss="alert">\xD7</button> </div>';
-  const severity = resp.severity || 'info';
-  $(template)
-    .addClass(`alert-${severity}`)
-    .append(resp.message || '')
-    .appendTo($('#alert-container'));
-}
-
-function toggleCheckbox(apiUrlPrefix: string, selector: string) {
-  SupersetClient.get({
-    endpoint: apiUrlPrefix + ($(selector)[0] as HTMLInputElement).checked,
-  })
-    .then(() => undefined)
-    .catch(response =>
-      getClientErrorObject(response).then(parsedResp => {
-        if (parsedResp && parsedResp.message) {
-          showApiMessage(parsedResp);
-        }
-      }),
-    );
-}
-
 export default function setupApp() {
-  $(document).ready(function () {
-    $(':checkbox[data-checkbox-api-prefix]').change(function (
-      this: HTMLElement,
-    ) {
-      const $this = $(this);
-      const prefix = $this.data('checkbox-api-prefix');
-      const id = $this.attr('id');
-      toggleCheckbox(prefix, `#${id}`);
-    });
-
-    // for language picker dropdown
-    $('#language-picker a').click(function (
-      ev: JQuery.ClickEvent<
-        HTMLLinkElement,
-        null,
-        HTMLLinkElement,
-        HTMLLinkElement
-      >,
-    ) {
-      ev.preventDefault();
-      SupersetClient.get({
-        url: ev.currentTarget.href,
-        parseMethod: null,
-      }).then(() => {
-        window.location.reload();
-      });
-    });
-  });
-
   // A set of hacks to allow apps to run within a FAB template
   // this allows for the server side generated menus to function
   window.$ = $;
