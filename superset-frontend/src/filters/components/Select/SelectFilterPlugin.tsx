b070c1ce49c3eb723fd5b483370178874c9e4b68
Simcha Shats <simcha.shats@nielsen.com>
2021-03-22 17:02:31 +0200

    fix: fix CR notes

--- superset-frontend/src/filters/components/Select/SelectFilterPlugin.tsx
+++ superset-frontend/src/filters/components/Select/SelectFilterPlugin.tsx
@@ -137,7 +137,6 @@ export default function PluginFilterSelect(props: PluginFilterSelectProps) {
     multiSelect,
     enableEmptyFilter,
     inverseSelection,
-    defaultToFirstItem,
   ]);
 
   const placeholderText =
