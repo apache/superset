e90513f3f52e52eba0324ce13568f278f6985933
Arash <arash.afghahi@gmail.com>
2021-03-24 18:04:56 -0400

    rebased

--- superset-frontend/src/SqlLab/components/ScheduleQueryButton.tsx
+++ superset-frontend/src/SqlLab/components/ScheduleQueryButton.tsx
@@ -17,8 +17,6 @@
  * under the License.
  */
 import React, { FunctionComponent, useState } from 'react';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Form, { FormProps, FormValidation } from 'react-jsonschema-form';
 import { Col, FormControl, FormGroup, Row } from 'react-bootstrap';
 import { t, styled } from '@superset-ui/core';
@@ -27,22 +25,6 @@ import ModalTrigger from 'src/components/ModalTrigger';
 import FormLabel from 'src/components/FormLabel';
 import './ScheduleQueryButton.less';
 import Button from 'src/components/Button';
-=======
-import Form, { FormValidation } from 'react-jsonschema-form';
-=======
-import Form, { FormProps, FormValidation } from 'react-jsonschema-form';
->>>>>>> worked on changes
-import { Col, FormControl, FormGroup, Row } from 'react-bootstrap';
-import { t, styled } from '@superset-ui/core';
-import * as chrono from 'chrono-node';
-import ModalTrigger from 'src/components/ModalTrigger';
-import FormLabel from 'src/components/FormLabel';
-import './ScheduleQueryButton.less';
-<<<<<<< HEAD
->>>>>>> changed into TypeScript
-=======
-import Button from 'src/components/Button';
->>>>>>> worked on changes
 
 const validators = {
   greater: (a: number, b: number) => a > b,
@@ -51,9 +33,6 @@ const validators = {
   less_equal: (a: number, b: number) => a <= b,
 };
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 const getJSONSchema = () => {
   const jsonSchema = window.featureFlags.SCHEDULED_QUERIES?.JSONSCHEMA;
   // parse date-time into usable value (eg, 'today' => `new Date()`)
@@ -80,72 +59,6 @@ const getValidationRules = () =>
 
 const getValidator = () => {
   const rules: any = getValidationRules();
-=======
-=======
->>>>>>> changed file configs
-function getJSONSchema() {
-  const jsonSchema = window.featureFlags.SCHEDULED_QUERIES.JSONSCHEMA;
-  // parse date-time into usable value (eg, 'today' => `new Date()`)
-  Object.entries(jsonSchema.properties).forEach(
-    ([key, properties]: [string, any]) => {
-      if (properties.default && properties.format === 'date-time') {
-        jsonSchema.properties[key] = {
-          ...properties,
-          default: chrono.parseDate(properties.default).toISOString(),
-        };
-      }
-    },
-  );
-  return jsonSchema;
-}
-
-function getUISchema() {
-  return window.featureFlags.SCHEDULED_QUERIES.UISCHEMA;
-}
-
-function getValidationRules() {
-  return window.featureFlags.SCHEDULED_QUERIES.VALIDATION || [];
-}
-
-function getValidator() {
-  const rules = getValidationRules();
-<<<<<<< HEAD
->>>>>>> changed into TypeScript
-=======
-=======
-=======
->>>>>>> worked on changes
-const getJSONSchema = () => {
-  const jsonSchema = window.featureFlags.SCHEDULED_QUERIES?.JSONSCHEMA;
-  // parse date-time into usable value (eg, 'today' => `new Date()`)
-  if (jsonSchema) {
-    Object.entries(jsonSchema.properties).forEach(
-      ([key, value]: [string, any]) => {
-        if (value.default && value.format === 'date-time') {
-          jsonSchema.properties[key] = {
-            ...value,
-            default: chrono.parseDate(value.default).toISOString(),
-          };
-        }
-      },
-    );
-    return jsonSchema;
-  }
-  return {};
-};
-
-const getUISchema = () => window.featureFlags.SCHEDULED_QUERIES?.UISCHEMA;
-
-const getValidationRules = () =>
-  window.featureFlags.SCHEDULED_QUERIES?.VALIDATION || [];
-
-const getValidator = () => {
-  const rules: any = getValidationRules();
-<<<<<<< HEAD
->>>>>>> changed file configs
->>>>>>> changed file configs
-=======
->>>>>>> worked on changes
   return (formData: Record<string, any>, errors: FormValidation) => {
     rules.forEach((rule: any) => {
       const test = validators[rule.name];
@@ -157,15 +70,7 @@ const getValidator = () => {
     });
     return errors;
   };
-<<<<<<< HEAD
-<<<<<<< HEAD
 };
-=======
-}
->>>>>>> changed into TypeScript
-=======
-};
->>>>>>> changed file configs
 
 interface ScheduleQueryButtonProps {
   defaultLabel?: string;
@@ -176,26 +81,13 @@ interface ScheduleQueryButtonProps {
   onSchedule?: Function;
   scheduleQueryWarning: string | null;
   disabled: boolean;
-<<<<<<< HEAD
-<<<<<<< HEAD
   tooltip: string;
-=======
-  tooltip: string | null;
->>>>>>> changed into TypeScript
-=======
-  tooltip: string;
->>>>>>> worked on changes
 }
 
 const StyledRow = styled(Row)`
   padding-bottom: ${({ theme }) => theme.gridUnit * 2}px;
 `;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> worked on changes
 export const StyledButtonComponent = styled(Button)`
   background: none;
   text-transform: none;
@@ -205,66 +97,27 @@ export const StyledButtonComponent = styled(Button)`
   font-weight: ${({ theme }) => theme.typography.weights.normal};
   &: disabled {
     background: none;
-<<<<<<< HEAD
     color: rgba(0, 0, 0, 0.85);
     &: hover {
       background: none;
       color: rgba(0, 0, 0, 0.85);
-=======
-=======
-const ButtonComponent = styled.div`
-  background-color: none;
-  .ant-btn.superset-button {
-=======
->>>>>>> worked on changes
-    color: rgba(0, 0, 0, 0.85);
-    &: hover {
-      background: none;
-<<<<<<< HEAD
->>>>>>> changed file configs
-=======
-      color: rgba(0, 0, 0, 0.85);
->>>>>>> worked on changes
     }
   }
 `;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> changed into TypeScript
-=======
->>>>>>> changed file configs
->>>>>>> changed file configs
-=======
->>>>>>> worked on changes
 const ScheduleQueryButton: FunctionComponent<ScheduleQueryButtonProps> = ({
   defaultLabel = t('Undefined'),
   sql,
   schema,
   dbId,
   onSchedule = () => {},
-<<<<<<< HEAD
-<<<<<<< HEAD
   scheduleQueryWarning,
   tooltip,
   disabled = false,
-=======
-  scheduleQueryWarning = null,
-  disabled = false,
-  tooltip = null,
->>>>>>> changed into TypeScript
-=======
-  scheduleQueryWarning,
-  tooltip,
-  disabled = false,
->>>>>>> worked on changes
 }) => {
   const [description, setDescription] = useState('');
   const [label, setLabel] = useState(defaultLabel);
   const [showSchedule, setShowSchedule] = useState(false);
-<<<<<<< HEAD
-<<<<<<< HEAD
   let saveModal: ModalTrigger | null;
 
   const onScheduleSubmit = ({
@@ -272,20 +125,6 @@ const ScheduleQueryButton: FunctionComponent<ScheduleQueryButtonProps> = ({
   }: {
     formData: Omit<FormProps<Record<string, any>>, 'schema'>;
   }) => {
-=======
-  let saveModal: any;
-
-  const onScheduleSubmit = ({ formData }: Record<string, any>) => {
->>>>>>> changed into TypeScript
-=======
-  let saveModal: ModalTrigger | null;
-
-  const onScheduleSubmit = ({
-    formData,
-  }: {
-    formData: Omit<FormProps<Record<string, any>>, 'schema'>;
-  }) => {
->>>>>>> worked on changes
     const query = {
       label,
       description,
@@ -295,15 +134,7 @@ const ScheduleQueryButton: FunctionComponent<ScheduleQueryButtonProps> = ({
       extra_json: JSON.stringify({ schedule_info: formData }),
     };
     onSchedule(query);
-<<<<<<< HEAD
-<<<<<<< HEAD
     saveModal?.close();
-=======
-    saveModal.close();
->>>>>>> changed into TypeScript
-=======
-    saveModal?.close();
->>>>>>> worked on changes
   };
 
   const renderModalBody = () => (
@@ -339,15 +170,7 @@ const ScheduleQueryButton: FunctionComponent<ScheduleQueryButtonProps> = ({
           <div className="json-schema">
             <Form
               schema={getJSONSchema()}
-<<<<<<< HEAD
-<<<<<<< HEAD
               uiSchema={getUISchema}
-=======
-              uiSchema={getUISchema()}
->>>>>>> changed into TypeScript
-=======
-              uiSchema={getUISchema}
->>>>>>> worked on changes
               onSubmit={onScheduleSubmit}
               validate={getValidator()}
             />
@@ -373,8 +196,6 @@ const ScheduleQueryButton: FunctionComponent<ScheduleQueryButtonProps> = ({
         modalTitle={t('Schedule query')}
         modalBody={renderModalBody()}
         triggerNode={
-<<<<<<< HEAD
-<<<<<<< HEAD
           <StyledButtonComponent
             onClick={() => setShowSchedule(!showSchedule)}
             buttonSize="small"
@@ -384,26 +205,6 @@ const ScheduleQueryButton: FunctionComponent<ScheduleQueryButtonProps> = ({
           >
             {t('Schedule')}
           </StyledButtonComponent>
-=======
-          <div
-            role="button"
-            buttonSize="small"
-=======
-          <StyledButtonComponent
->>>>>>> worked on changes
-            onClick={() => setShowSchedule(!showSchedule)}
-            buttonSize="small"
-            buttonStyle="link"
-            tooltip={tooltip}
-            disabled={disabled}
-          >
-            {t('Schedule')}
-<<<<<<< HEAD
-          </div>
->>>>>>> changed into TypeScript
-=======
-          </StyledButtonComponent>
->>>>>>> worked on changes
         }
       />
     </span>
