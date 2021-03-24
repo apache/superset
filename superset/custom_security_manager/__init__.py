752c6d5c741ed179b4bbba1ee611566ab0815afd
Jackmin801 <ongjackm@gmail.com>
2021-03-24 19:26:54 +0000

    Implemented using real redis
    Dev server has a superset_global_kv redis database usd as a global
    key value store. This is used to implement the anti-concurrent
    login in a thread safe manner.

--- superset/custom_security_manager/__init__.py
+++ superset/custom_security_manager/__init__.py
@@ -4,9 +4,11 @@ from superset.security import SupersetSecurityManager
 from flask_appbuilder.security.views import expose
 from flask_login import login_user
 from flask_appbuilder.security.forms import LoginForm_db
+import redis
+
+global_kv_store = redis.StrictRedis(host="superset_global_kv", port=6379, password="")
 
 class CustomAuthDBView(AuthView):
-   fake_redis = {}
    login_template = "appbuilder/general/security/login_db.html"
    @expose("/login/", methods=["GET", "POST"])
    def login(self):
@@ -21,7 +23,8 @@ class CustomAuthDBView(AuthView):
                flash(as_unicode(self.invalid_login_message), "warning")
                return redirect(self.appbuilder.get_url_for_login)
            login_user(user, remember=False)
-           self.fake_redis[int(session.get("user_id"))] = session.get("_id")
+           global_kv_store.set(str(session.get("user_id")),str(session.get("_id")))
+           print(f'{session.get("_id")} stored {type(global_kv_store.get(str(1)))}')
            return redirect(self.appbuilder.get_url_for_index)
        return self.render_template(
            self.login_template, title=self.title, form=form, appbuilder=self.appbuilder
@@ -33,7 +36,8 @@ class CustomSecurityManager(SupersetSecurityManager):
    # Override load user method
    def load_user(self,pk):
        # If its not the most recent login, user is logged out
-       if self.authdbview.fake_redis.get(int(pk), None) != session.get("_id"):
+       if global_kv_store.get(str(pk)).decode('utf-8') != session.get("_id"):
+           print(f"{pk} got kicked hahhaha!", flush=True)
            session.clear()
            return None
        return self.get_user_by_id(int(pk))
