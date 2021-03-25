9f7b0b62d6140ba7024ecef967eadcdc889a0c9b
David Aaron Suddjian <aasuddjian@gmail.com>
2021-03-25 11:44:15 -0700

    get the line chart alias before clicking the tab

--- superset-frontend/cypress-base/cypress/integration/dashboard/tabs.test.ts
+++ superset-frontend/cypress-base/cypress/integration/dashboard/tabs.test.ts
@@ -127,10 +127,10 @@ describe('Dashboard tabs', () => {
       });
     });
 
-    // click row level tab, send 1 more query
-    cy.get('.ant-tabs-tab').contains('row tab 2').click();
-
     getChartAliasBySpec(LINE_CHART).then(lineChartAlias => {
+      // click row level tab, send 1 more query
+      cy.get('.ant-tabs-tab').contains('row tab 2').click();
+
       cy.wait(lineChartAlias).then(({ request }) => {
         const requestBody = parsePostForm(request.body);
         const requestParams = JSON.parse(requestBody.form_data as string);
