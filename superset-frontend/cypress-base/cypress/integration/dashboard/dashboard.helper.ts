4554bd344b112c92510c5fc60b5a7eb22ae7032a
David Aaron Suddjian <1858430+suddjian@users.noreply.github.com>
2021-03-26 09:20:13 -0700

    chore(cypress): Make the e2e tests more behavior-driven (#13784)
    * chore(cypress): make the load dashboard test behavior driven
    
    * remove bootstrap usage from controls test, + new utils
    
    * fix save test to not use bootstrap dat
    
    * remove bootstrap usage from the filter test
    
    * fix race condition
    
    * remove bootstrap from url params test
    
    * fix lint
    
    * remove unused const
    
    * add chart specs for the tab test
    
    * attempt removing bootstrap data from tabs tests
    
    * fix cypress async/sync confusion
    
    * remove redundant assertions
    
    * attempt fixing tab test
    
    * attempt fixing tabs test
    
    * cleanup commented code
    
    * better aliases so you can tell what they are in the logs
    
    * remove unused imports
    
    * get the line chart alias before clicking the tab
    
    * simplify getChartGridComponent
    
    * wait for all the charts before force refresh
    
    * fix tabs test
    
    * one more time with the tabs test
    
    * another edit to tabs test
    
    * fix flaky test
    
    Co-authored-by: Phillip Kelley-Dotson <pkelleydotson@yahoo.com>

--- superset-frontend/cypress-base/cypress/integration/dashboard/dashboard.helper.ts
+++ superset-frontend/cypress-base/cypress/integration/dashboard/dashboard.helper.ts
@@ -1,3 +1,5 @@
+import { getChartAlias, Slice } from 'cypress/utils/vizPlugins';
+
 /**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -21,6 +23,73 @@ export const TABBED_DASHBOARD = '/superset/dashboard/tabbed_dash/';
 
 export const CHECK_DASHBOARD_FAVORITE_ENDPOINT =
   '/superset/favstar/Dashboard/*/count';
+
+export const WORLD_HEALTH_CHARTS = [
+  { name: '% Rural', viz: 'world_map' },
+  { name: 'Most Populated Countries', viz: 'table' },
+  { name: 'Region Filter', viz: 'filter_box' },
+  { name: "World's Population", viz: 'big_number' },
+  { name: 'Growth Rate', viz: 'line' },
+  { name: 'Rural Breakdown', viz: 'sunburst' },
+  { name: "World's Pop Growth", viz: 'area' },
+  { name: 'Life Expectancy VS Rural %', viz: 'bubble' },
+  { name: 'Treemap', viz: 'treemap' },
+  { name: 'Box plot', viz: 'box_plot' },
+] as const;
+
+/** Used to specify charts expected by the test suite */
+export interface ChartSpec {
+  name: string;
+  viz: string;
+}
+
+export function getChartGridComponent({ name, viz }: ChartSpec) {
+  return cy
+    .get(`[data-test="chart-grid-component"][data-test-chart-name="${name}"]`)
+    .should('have.attr', 'data-test-viz-type', viz);
+}
+
+export function waitForChartLoad(chart: ChartSpec) {
+  return getChartGridComponent(chart).then(gridComponent => {
+    const chartId = gridComponent.attr('data-test-chart-id');
+    // the chart should load in under half a minute
+    return (
+      cy
+        // this id only becomes visible when the chart is loaded
+        .wrap(gridComponent)
+        .find(`#chart-id-${chartId}`, { timeout: 30000 })
+        .should('be.visible')
+        // return the chart grid component
+        .then(() => gridComponent)
+    );
+  });
+}
+
+const toSlicelike = ($chart: JQuery<HTMLElement>): Slice => ({
+  slice_id: parseInt($chart.attr('data-test-chart-id')!, 10),
+  form_data: {
+    viz_type: $chart.attr('data-test-viz-type')!,
+  },
+});
+
+export function getChartAliasBySpec(chart: ChartSpec) {
+  return getChartGridComponent(chart).then($chart =>
+    cy.wrap(getChartAlias(toSlicelike($chart))),
+  );
+}
+
+export function getChartAliasesBySpec(charts: readonly ChartSpec[]) {
+  const aliases: string[] = [];
+  charts.forEach(chart =>
+    getChartAliasBySpec(chart).then(alias => {
+      aliases.push(alias);
+    }),
+  );
+  // Wrapping the aliases is key.
+  // That way callers can chain off this function
+  // and actually get the list of aliases.
+  return cy.wrap(aliases);
+}
 
 /**
  * Drag an element and drop it to another element.
