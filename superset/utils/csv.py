62dd99f29301896864d142847e279aad1f577eb0
Ben Reinhart <benjreinhart@gmail.com>
2021-03-26 13:39:51 -0700

    Update escaping logic

--- superset/utils/csv.py
+++ superset/utils/csv.py
@@ -19,20 +19,39 @@ from typing import Any
 
 import pandas as pd
 
+negative_number_re = re.compile(r"^-[0-9.]+$")
+
+# This regex will match if the string starts with:
+#
+#     1. one of -, @, +, |, =, %
+#     2. two double quotes immediately followed by one of -, @, +, |, =, %
+#     3. one or more spaces immediately followed by one of -, @, +, |, =, %
+#
+problematic_chars_re = re.compile(r'^(?:"{2}|\s{1,})(?=[\-@+|=%])|^[\-@+|=%]')
+
 
 def escape_value(value: str) -> str:
     """
     Escapes a set of special characters.
 
-    Taken from: https://github.com/raphaelm/defusedcsv/blob/5911a6ae54a82e71fad4db5eb8769738e9620e31/defusedcsv/csv.py#L15-L23 # pylint: disable=line-too-long
+    http://georgemauer.net/2017/10/07/csv-injection.html
     """
-    if (
-        value
-        and value[0] in ("@", "+", "-", "=", "|", "%")
-        and not re.match("^-?[0-9,\\.]+$", value)
-    ):
+    needs_escaping = problematic_chars_re.match(value) is not None
+    is_negative_number = negative_number_re.match(value) is not None
+
+    if needs_escaping and not is_negative_number:
+        # Escape pipe to be extra safe as this
+        # can lead to remote code execution
         value = value.replace("|", "\\|")
+
+        # Precede the line with a single quote. This prevents
+        # evaluation of commands and some spreadsheet software
+        # will hide this visually from the user. Many articles
+        # claim a preceding space will work here too, however,
+        # when uploading a csv file in Google sheets, a leading
+        # space was ignored and code was still evaluated.
         value = "'" + value
+
     return value
 
 
--- tests/utils/csv_tests.py
+++ tests/utils/csv_tests.py
@@ -51,10 +51,30 @@ def test_escape_value():
     result = csv.escape_value("=cmd|' /C calc'!A0")
     assert result == "'=cmd\|' /C calc'!A0"
 
+    result = csv.escape_value('""=10+2')
+    assert result == '\'""=10+2'
+
+    result = csv.escape_value(" =10+2")
+    assert result == "' =10+2"
+
 
 def test_df_to_escaped_csv():
-    csv_str = "col_a,=col_b\n-10,=cmd|' /C calc'!A0\na,=b"
+    csv_rows = [
+        ["col_a", "=func()"],
+        ["-10", "=cmd|' /C calc'!A0"],
+        ["a", '""=b'],
+        [" =a", "b"],
+    ]
+    csv_str = "\n".join([",".join(row) for row in csv_rows])
+
     df = pd.read_csv(io.StringIO(csv_str))
 
-    result = csv.df_to_escaped_csv(df, encoding="utf8", index=False)
-    assert result == "col_a,'=col_b\n-10,'=cmd\|' /C calc'!A0\na,'=b\n"
+    escaped_csv_str = csv.df_to_escaped_csv(df, encoding="utf8", index=False)
+    escaped_csv_rows = [row.split(",") for row in escaped_csv_str.strip().split("\n")]
+
+    assert escaped_csv_rows == [
+        ["col_a", "'=func()"],
+        ["-10", "'=cmd\|' /C calc'!A0"],
+        ["a", "'=b"],  # pandas seems to be removing the leading ""
+        ["' =a", "b"],
+    ]
