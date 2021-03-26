afa5648b8d60697881dccd50118b51884a2707fb
Bruno Motta <wadge.motta@gmail.com>
2021-03-25 22:29:01 -0300

    add factories to props and fetch-mock

--- superset-frontend/src/explore/components/DisplayQueryButton/DisplayQueryButton.test.tsx
+++ superset-frontend/src/explore/components/DisplayQueryButton/DisplayQueryButton.test.tsx
@@ -22,10 +22,10 @@ import { render, screen, act } from 'spec/helpers/testing-library';
 import userEvent from '@testing-library/user-event';
 import * as chartAction from 'src/chart/chartAction';
 import * as downloadAsImage from 'src/utils/downloadAsImage';
-// eslint-disable-next-line import/no-named-as-default
-import DisplayQueryButton from '.';
+import fetchMock from 'fetch-mock';
+import ConectedDisplayQueryButton from '.';
 
-let props = {
+const createProps = () => ({
   latestQueryFormData: {
     viz_type: 'histogram',
     datasource: '49__table',
@@ -80,74 +80,25 @@ let props = {
   chartStatus: 'rendered',
   onOpenPropertiesModal: jest.fn(),
   onOpenInEditor: jest.fn(),
-};
-
-beforeEach(() => {
-  props = {
-    latestQueryFormData: {
-      viz_type: 'histogram',
-      datasource: '49__table',
-      slice_id: 318,
-      url_params: {},
-      time_range_endpoints: ['inclusive', 'exclusive'],
-      granularity_sqla: 'time_start',
-      time_range: 'No filter',
-      all_columns_x: ['age'],
-      adhoc_filters: [],
-      row_limit: 10000,
-      groupby: null,
-      color_scheme: 'supersetColors',
-      label_colors: {},
-      link_length: '25',
-      x_axis_label: 'age',
-      y_axis_label: 'count',
-    },
-    slice: {
-      cache_timeout: null,
-      changed_on: '2021-03-19T16:30:56.750230',
-      changed_on_humanized: '3 days ago',
-      datasource: 'FCC 2018 Survey',
-      description: null,
-      description_markeddown: '',
-      edit_url: '/chart/edit/318',
-      form_data: {
-        adhoc_filters: [],
-        all_columns_x: ['age'],
-        color_scheme: 'supersetColors',
-        datasource: '49__table',
-        granularity_sqla: 'time_start',
-        groupby: null,
-        label_colors: {},
-        link_length: '25',
-        queryFields: { groupby: 'groupby' },
-        row_limit: 10000,
-        slice_id: 318,
-        time_range: 'No filter',
-        time_range_endpoints: ['inclusive', 'exclusive'],
-        url_params: {},
-        viz_type: 'histogram',
-        x_axis_label: 'age',
-        y_axis_label: 'count',
-      },
-      modified: '<span class="no-wrap">3 days ago</span>',
-      owners: [],
-      slice_id: 318,
-      slice_name: 'Age distribution of respondents',
-      slice_url: '/superset/explore/?form_data=%7B%22slice_id%22%3A%20318%7D',
-    },
-    chartStatus: 'rendered',
-    onOpenPropertiesModal: jest.fn(),
-    onOpenInEditor: jest.fn(),
-  };
 });
 
+fetchMock.post(
+  'http://api/v1/chart/data?form_data=%7B%22slice_id%22%3A318%7D',
+  { body: {} },
+  {
+    sendAsJson: false,
+  },
+);
+
 test('Shoud render a button', () => {
-  render(<DisplayQueryButton {...props} />, { useRedux: true });
+  const props = createProps();
+  render(<ConectedDisplayQueryButton {...props} />, { useRedux: true });
   expect(screen.getByRole('button')).toBeInTheDocument();
 });
 
 test('Shoud open a menu', () => {
-  render(<DisplayQueryButton {...props} />, {
+  const props = createProps();
+  render(<ConectedDisplayQueryButton {...props} />, {
     useRedux: true,
   });
 
@@ -172,7 +123,8 @@ test('Shoud open a menu', () => {
 });
 
 test('Shoud call onOpenPropertiesModal when click on "Edit properties"', () => {
-  render(<DisplayQueryButton {...props} />, {
+  const props = createProps();
+  render(<ConectedDisplayQueryButton {...props} />, {
     useRedux: true,
   });
   expect(props.onOpenInEditor).toBeCalledTimes(0);
@@ -182,8 +134,9 @@ test('Shoud call onOpenPropertiesModal when click on "Edit properties"', () => {
 });
 
 test('Shoud call getChartDataRequest when click on "View query"', async () => {
+  const props = createProps();
   const getChartDataRequest = jest.spyOn(chartAction, 'getChartDataRequest');
-  render(<DisplayQueryButton {...props} />, {
+  render(<ConectedDisplayQueryButton {...props} />, {
     useRedux: true,
   });
 
@@ -202,7 +155,8 @@ test('Shoud call getChartDataRequest when click on "View query"', async () => {
 });
 
 test('Shoud call onOpenInEditor when click on "Run in SQL Lab"', () => {
-  render(<DisplayQueryButton {...props} />, {
+  const props = createProps();
+  render(<ConectedDisplayQueryButton {...props} />, {
     useRedux: true,
   });
 
@@ -215,8 +169,9 @@ test('Shoud call onOpenInEditor when click on "Run in SQL Lab"', () => {
 });
 
 test('Shoud call downloadAsImage when click on "Download as image"', () => {
+  const props = createProps();
   const spy = jest.spyOn(downloadAsImage, 'default');
-  render(<DisplayQueryButton {...props} />, {
+  render(<ConectedDisplayQueryButton {...props} />, {
     useRedux: true,
   });
 
