1134fda35bd46c432dcfc49cf868fab3d35b62bf
Bruno Motta <wadge.motta@gmail.com>
2021-03-25 22:33:22 -0300

    factory for props

--- superset-frontend/src/explore/components/controls/FilterBoxItemControl/FilterBoxItemControl.test.tsx
+++ superset-frontend/src/explore/components/controls/FilterBoxItemControl/FilterBoxItemControl.test.tsx
@@ -22,13 +22,11 @@ import { render, screen } from 'spec/helpers/testing-library';
 import userEvent from '@testing-library/user-event';
 import FilterBoxItemControl from '.';
 
-const datasource = {
-  columns: [],
-  metrics: [],
-};
-
-let props = {
-  datasource,
+const createProps = () => ({
+  datasource: {
+    columns: [],
+    metrics: [],
+  },
   asc: true,
   clearable: true,
   multiple: true,
@@ -38,33 +36,18 @@ let props = {
   searchAllOptions: false,
   defaultValue: undefined,
   onChange: jest.fn(),
-};
-
-beforeEach(() => {
-  props = {
-    datasource,
-    asc: true,
-    clearable: true,
-    multiple: true,
-    column: 'developer_type',
-    label: 'Developer Type',
-    metric: undefined,
-    searchAllOptions: false,
-    defaultValue: undefined,
-    onChange: jest.fn(),
-  };
 });
 
 test('Shoud render', () => {
-  const onChange = jest.fn();
-  render(<FilterBoxItemControl {...props} onChange={onChange} />);
+  const props = createProps();
+  render(<FilterBoxItemControl {...props} />);
   expect(screen.getByTestId('FilterBoxItemControl')).toBeInTheDocument();
   expect(screen.getByRole('button')).toBeInTheDocument();
 });
 
 test('Shoud open modal', () => {
-  const onChange = jest.fn();
-  render(<FilterBoxItemControl {...props} onChange={onChange} />);
+  const props = createProps();
+  render(<FilterBoxItemControl {...props} />);
   userEvent.click(screen.getByRole('button'));
   expect(screen.getByText('Filter configuration')).toBeInTheDocument();
   expect(screen.getByText('Column')).toBeInTheDocument();
