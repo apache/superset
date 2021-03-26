64e5cdcc498d33e4348a961a8f329a1a8df09ea4
Jack Fragassi <jfragassi98@gmail.com>
2021-03-25 20:09:28 -0700

    Fix migration head

--- superset/migrations/versions/301362411006_add_execution_id_to_report_execution_.py
+++ superset/migrations/versions/301362411006_add_execution_id_to_report_execution_.py
@@ -17,14 +17,14 @@
 """add_execution_id_to_report_execution_log_model.py
 
 Revision ID: 301362411006
-Revises: 67da9ef1ef9c
+Revises: 989bbe479899
 Create Date: 2021-03-23 05:23:15.641856
 
 """
 
 # revision identifiers, used by Alembic.
 revision = "301362411006"
-down_revision = "67da9ef1ef9c"
+down_revision = "989bbe479899"
 
 import sqlalchemy as sa
 from alembic import op
