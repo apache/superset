9bdbe43f3d354c6a0a7dbe34b217ce4ca02a5ad4
samtfm <sam@preset.io>
2021-03-29 16:55:14 -0700

    add report description to slack notification

--- superset/reports/notifications/slack.py
+++ superset/reports/notifications/slack.py
@@ -44,10 +44,11 @@ class SlackNotification(BaseNotification):  # pylint: disable=too-few-public-met
         return json.loads(self._recipient.recipient_config_json)["target"]
 
     @staticmethod
-    def _error_template(name: str, text: str) -> str:
+    def _error_template(name: str, description: str, text: str) -> str:
         return __(
             """
             *%(name)s*\n
+            %(description)s\n
             Error: %(text)s
             """,
             name=name,
@@ -56,13 +57,15 @@ class SlackNotification(BaseNotification):  # pylint: disable=too-few-public-met
 
     def _get_body(self) -> str:
         if self._content.text:
-            return self._error_template(self._content.name, self._content.text)
+            return self._error_template(self._content.name, self._content.description, self._content.text)
         return __(
             """
             *%(name)s*\n
+            %(description)s\n
             <%(url)s|Explore in Superset>
             """,
             name=self._content.name,
+            description=self._content.description,
             url=self._content.url,
         )
 
