50c48f04ecb831d9a8860eead8c203f2b8d3d1c5
samtfm <sam@preset.io>
2021-03-30 14:17:49 -0700

    fix: ex_info -> exc_info

--- superset/utils/screenshots.py
+++ superset/utils/screenshots.py
@@ -151,13 +151,13 @@ class BaseScreenshot:
         try:
             payload = self.get_screenshot(user=user, window_size=window_size)
         except Exception as ex:  # pylint: disable=broad-except
-            logger.warning("Failed at generating thumbnail %s", ex, ex_info=True)
+            logger.warning("Failed at generating thumbnail %s", ex, exc_info=True)
 
         if payload and window_size != thumb_size:
             try:
                 payload = self.resize_image(payload, thumb_size=thumb_size)
             except Exception as ex:  # pylint: disable=broad-except
-                logger.warning("Failed at resizing thumbnail %s", ex, ex_info=True)
+                logger.warning("Failed at resizing thumbnail %s", ex, exc_info=True)
                 payload = None
 
         if payload:
