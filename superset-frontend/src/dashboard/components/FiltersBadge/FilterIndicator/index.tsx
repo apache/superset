bec20e7c4a3ec14d3d79a3394bf124a5c8bf2d09
Bruno Motta <wadge.motta@gmail.com>
2021-03-30 15:18:44 -0300

    tests for FilterIndicator

--- superset-frontend/src/dashboard/components/FiltersBadge/FilterIndicator/index.tsx
+++ superset-frontend/src/dashboard/components/FiltersBadge/FilterIndicator/index.tsx
@@ -19,10 +19,14 @@
 
 import { SearchOutlined } from '@ant-design/icons';
 import React, { FC } from 'react';
-import { getFilterValueForDisplay } from '../nativeFilters/FilterBar/FilterSets/utils';
-import { FilterValue, Item, ItemIcon, Title } from './Styles';
-
-import { Indicator } from './selectors';
+import { getFilterValueForDisplay } from 'src/dashboard/components/nativeFilters/FilterBar/FilterSets/utils';
+import {
+  FilterValue,
+  Item,
+  ItemIcon,
+  Title,
+} from 'src/dashboard/components/FiltersBadge/Styles';
+import { Indicator } from 'src/dashboard/components/FiltersBadge/selectors';
 
 export interface IndicatorProps {
   indicator: Indicator;
