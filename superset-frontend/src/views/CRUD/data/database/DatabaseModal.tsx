2ef498554a0fc52c223973812f337143427681f7
hughhhh <hughmil3s@gmail.com>
2021-03-30 12:59:34 -0400

    remove consolde

--- superset-frontend/src/views/CRUD/data/database/DatabaseModal.tsx
+++ superset-frontend/src/views/CRUD/data/database/DatabaseModal.tsx
@@ -350,7 +350,6 @@ const DatabaseModal: FunctionComponent<DatabaseModalProps> = ({
   const expandableModalIsOpen = !!db?.expose_in_sqllab;
   const createAsOpen = !!(db?.allow_ctas || db?.allow_cvas);
 
-  console.log(conf);
   return (
     <Modal
       name="database"
