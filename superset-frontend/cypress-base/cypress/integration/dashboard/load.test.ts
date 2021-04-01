52b31118b074d3e2bf9c33e1880788c9da985b31
David Aaron Suddjian <aasuddjian@gmail.com>
2021-04-01 13:03:30 -0700

    more permutations (untested yolo)

--- superset-frontend/cypress-base/cypress/integration/dashboard/load.test.ts
+++ superset-frontend/cypress-base/cypress/integration/dashboard/load.test.ts
@@ -32,6 +32,16 @@ describe('Dashboard load', () => {
     WORLD_HEALTH_CHARTS.forEach(waitForChartLoad);
   });
 
+  it('should load in edit mode', () => {
+    cy.visit(`${WORLD_HEALTH_DASHBOARD}?edit=true&standalone=true`);
+    cy.get('[data-test="discard-changes-button"]').should('be.visible');
+  });
+
+  it('should load in standalone mode', () => {
+    cy.visit(`${WORLD_HEALTH_DASHBOARD}?edit=true&standalone=true`);
+    cy.get('#app-menu').should('not.exist');
+  });
+
   it('should load in edit/standalone mode', () => {
     cy.visit(`${WORLD_HEALTH_DASHBOARD}?edit=true&standalone=true`);
     cy.get('[data-test="discard-changes-button"]').should('be.visible');
