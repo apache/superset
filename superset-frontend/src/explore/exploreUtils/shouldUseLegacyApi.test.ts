fa8a466cb9143e74f3f298e39721f3620c3b4f04
Bruno Motta <wadge.motta@gmail.com>
2021-04-02 10:55:24 -0300

    Improving "shouldUseLegacyApi" tests

--- superset-frontend/src/explore/exploreUtils/shouldUseLegacyApi.test.ts
+++ superset-frontend/src/explore/exploreUtils/shouldUseLegacyApi.test.ts
@@ -16,8 +16,37 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+import * as Core from '@superset-ui/core';
 import { shouldUseLegacyApi } from '.';
 
-test('Should return false by viz_type:name', () => {
-  expect(shouldUseLegacyApi({ viz_type: 'name' })).toBe(false);
+test('Should return false', () => {
+  const spy = jest.spyOn(Core, 'getChartMetadataRegistry');
+  const get = jest.fn();
+  spy.mockReturnValue({ get } as any);
+  expect(get).toBeCalledTimes(0);
+  expect(shouldUseLegacyApi({ viz_type: 'name_test' })).toBe(false);
+  expect(get).toBeCalledTimes(1);
+  expect(get).toBeCalledWith('name_test');
+});
+
+test('Should return true', () => {
+  const spy = jest.spyOn(Core, 'getChartMetadataRegistry');
+  const get = jest.fn();
+  get.mockReturnValue({ useLegacyApi: true });
+  spy.mockReturnValue({ get } as any);
+  expect(get).toBeCalledTimes(0);
+  expect(shouldUseLegacyApi({ viz_type: 'name_test' })).toBe(true);
+  expect(get).toBeCalledTimes(1);
+  expect(get).toBeCalledWith('name_test');
+});
+
+test('Should return false when useLegacyApi:false', () => {
+  const spy = jest.spyOn(Core, 'getChartMetadataRegistry');
+  const get = jest.fn();
+  get.mockReturnValue({ useLegacyApi: false });
+  spy.mockReturnValue({ get } as any);
+  expect(get).toBeCalledTimes(0);
+  expect(shouldUseLegacyApi({ viz_type: 'name_test' })).toBe(false);
+  expect(get).toBeCalledTimes(1);
+  expect(get).toBeCalledWith('name_test');
 });
