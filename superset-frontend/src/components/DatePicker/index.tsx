fa1e02897442126dda7ad843a329af5d97bf855d
Geido <60598000+geido@users.noreply.github.com>
2021-04-05 23:11:50 +0300

    Add dedicated dir and storybook (#13553)

--- superset-frontend/src/components/DatePicker/index.tsx
+++ superset-frontend/src/components/DatePicker/index.tsx
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import { DatePicker as AntdDatePicker } from 'src/common/components';
+import { DatePicker as AntdDatePicker } from 'antd';
 
 export const { RangePicker } = AntdDatePicker;
 export const DatePicker = AntdDatePicker;
