9070ac954eb4988d4a6ab7ded7bd03d9ca102484
krsnik93 <ivankrsnik93@gmail.com>
2021-04-07 09:32:17 +0100

    feat: echarts gauge chart (#993)
    * feat: echarts gauge chart
    
    * remove unused legend imports
    
    * move font size multipliers to constants
    
    * add tests
    
    * rename roundcap
    
    * add modulo on color picking to wrap around the color scheme
    
    Co-authored-by: Ivan Krsnik <ivan.krsnik@unipart.io>

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/plugin-chart-echarts/src/Gauge/buildQuery.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/plugin-chart-echarts/src/Gauge/buildQuery.ts
@@ -16,16 +16,13 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-export { default as EchartsBoxPlotChartPlugin } from './BoxPlot';
-export { default as EchartsTimeseriesChartPlugin } from './Timeseries';
-export { default as EchartsPieChartPlugin } from './Pie';
-export { default as EchartsGraphChartPlugin } from './Graph';
-export { default as EchartsGaugeChartPlugin } from './Gauge';
+import { buildQueryContext, QueryFormData } from '@superset-ui/core';
 
-/**
- * Note: this file exports the default export from EchartsTimeseries.tsx.
- * If you want to export multiple visualization modules, you will need to
- * either add additional plugin folders (similar in structure to ./plugin)
- * OR export multiple instances of `ChartPlugin` extensions in ./plugin/index.ts
- * which in turn load exports from EchartsTimeseries.tsx
- */
+export default function buildQuery(formData: QueryFormData) {
+  return buildQueryContext(formData, baseQueryObject => [
+    {
+      ...baseQueryObject,
+      groupby: formData.groupby || [],
+    },
+  ]);
+}
