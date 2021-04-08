0eb17e5a9b15db752b4ee25bd765e38b023fe6ab
Beto Dealmeida <roberto@dealmeida.net>
2021-04-07 17:39:14 -0700

    Fix unit tests

--- superset/db_engine_specs/presto.py
+++ superset/db_engine_specs/presto.py
@@ -14,7 +14,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-import dataclasses
 import logging
 import re
 import textwrap
