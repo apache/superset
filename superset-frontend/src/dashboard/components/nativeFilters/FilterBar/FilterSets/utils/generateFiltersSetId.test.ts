1aa710091bd3b3961d7e7a0af54e9a335d58ee2c
Bruno Motta <wadge.motta@gmail.com>
2021-04-09 11:01:21 -0300

    Update superset-frontend/src/dashboard/components/nativeFilters/FilterBar/FilterSets/utils/generateFiltersSetId.test.ts
    Co-authored-by: Michael S. Molina <70410625+michael-s-molina@users.noreply.github.com>

--- superset-frontend/src/dashboard/components/nativeFilters/FilterBar/FilterSets/utils/generateFiltersSetId.test.ts
+++ superset-frontend/src/dashboard/components/nativeFilters/FilterBar/FilterSets/utils/generateFiltersSetId.test.ts
@@ -18,7 +18,7 @@
  */
 import { generateFiltersSetId } from '.';
 
-test('Should follow a pattern', () => {
+test('Should follow the pattern "FILTERS_SET-"', () => {
   const id = generateFiltersSetId();
   expect(id.startsWith('FILTERS_SET-', 0)).toBe(true);
 });
