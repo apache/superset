d1da29301b97d856e51855f259df5a1bf8b2a261
Erik Ritter <erik.ritter@airbnb.com>
2021-04-09 09:39:02 -0700

    feat: handle chart/data API errors (#14040)

--- superset/exceptions.py
+++ superset/exceptions.py
@@ -41,6 +41,14 @@ class SupersetException(Exception):
 class SupersetErrorException(SupersetException):
     """Exceptions with a single SupersetErrorType associated with them"""
 
+    def __init__(self, error: SupersetError) -> None:
+        super().__init__(error.message)
+        self.error = error
+
+
+class SupersetErrorFromParamsException(SupersetErrorException):
+    """Exceptions that pass in parameters to construct a SupersetError"""
+
     def __init__(
         self,
         error_type: SupersetErrorType,
@@ -48,9 +56,10 @@ class SupersetErrorException(SupersetException):
         level: ErrorLevel,
         extra: Optional[Dict[str, Any]] = None,
     ) -> None:
-        super().__init__(message)
-        self.error = SupersetError(
-            error_type=error_type, message=message, level=level, extra=extra or {}
+        super().__init__(
+            SupersetError(
+                error_type=error_type, message=message, level=level, extra=extra or {}
+            )
         )
 
 
@@ -62,11 +71,11 @@ class SupersetErrorsException(SupersetException):
         self.errors = errors
 
 
-class SupersetTimeoutException(SupersetErrorException):
+class SupersetTimeoutException(SupersetErrorFromParamsException):
     status = 408
 
 
-class SupersetGenericDBErrorException(SupersetErrorException):
+class SupersetGenericDBErrorException(SupersetErrorFromParamsException):
     status = 400
 
     def __init__(
@@ -80,7 +89,7 @@ class SupersetGenericDBErrorException(SupersetErrorException):
         )
 
 
-class SupersetTemplateParamsErrorException(SupersetErrorException):
+class SupersetTemplateParamsErrorException(SupersetErrorFromParamsException):
     status = 400
 
     def __init__(
@@ -94,14 +103,13 @@ class SupersetTemplateParamsErrorException(SupersetErrorException):
         )
 
 
-class SupersetSecurityException(SupersetException):
+class SupersetSecurityException(SupersetErrorException):
     status = 401
 
     def __init__(
         self, error: SupersetError, payload: Optional[Dict[str, Any]] = None
     ) -> None:
-        super().__init__(error.message)
-        self.error = error
+        super().__init__(error)
         self.payload = payload
 
 
