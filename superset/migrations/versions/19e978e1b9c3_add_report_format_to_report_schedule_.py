98cd21023e90b7b4638444cc36c4772eafd4d8d5
Lily Kuang <lily@preset.io>
2021-04-13 14:31:12 -0700

    fix migrations

--- superset/migrations/versions/19e978e1b9c3_add_report_format_to_report_schedule_.py
+++ superset/migrations/versions/19e978e1b9c3_add_report_format_to_report_schedule_.py
@@ -17,14 +17,14 @@
 """add_report_format_to_report_schedule_model.py
 
 Revision ID: 19e978e1b9c3
-Revises: 301362411006
+Revises: fc3a3a8ff221
 Create Date: 2021-04-06 21:39:52.259223
 
 """
 
 # revision identifiers, used by Alembic.
 revision = "19e978e1b9c3"
-down_revision = "301362411006"
+down_revision = "fc3a3a8ff221"
 
 import sqlalchemy as sa
 from alembic import op
@@ -34,7 +34,9 @@ from sqlalchemy.dialects import postgresql
 def upgrade():
     op.add_column(
         "report_schedule",
-        sa.Column("report_format", sa.String(length=50), nullable=True),
+        sa.Column(
+            "report_format", sa.String(length=50), server_default="PNG", nullable=True
+        ),
     )
 
 
