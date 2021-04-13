dc98bf64dc09b8785af5dece520482db86e4e23d
amitmiran137 <amit.miran@nielsen.com>
2021-04-13 08:29:46 +0300

    fix: after hugh CR

--- superset-frontend/src/dashboard/components/SliceHeaderControls/index.jsx
+++ superset-frontend/src/dashboard/components/SliceHeaderControls/index.jsx
@@ -189,6 +189,7 @@ class SliceHeaderControls extends React.PureComponent {
       addSuccessToast,
       addDangerToast,
       isFullSize,
+      supersetCanShare,
     } = this.props;
     const crossFilterItems = getChartMetadataRegistry().items;
     const isCrossFilter = Object.entries(crossFilterItems)
@@ -254,7 +255,7 @@ class SliceHeaderControls extends React.PureComponent {
           </Menu.Item>
         )}
 
-        {this.props.supersetCanShare && (
+        {supersetCanShare && (
           <ShareMenuItems
             url={getDashboardUrl(
               window.location.pathname,
