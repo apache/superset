f996b73099ddc33e3a66bebb3c853ab8000e2f3c
Craig <craig@craigrueda.com>
2021-04-15 09:19:14 -0700

    Adding license / update instructions

--- .github/workflows/codecov.sh
+++ .github/workflows/codecov.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Apache License Version 2.0, January 2004
-# https://github.com/codecov/codecov-bash/blob/master/LICENSE
+#
+# Note: this script was fetched from https://codecov.io/bash in response
+#   to a breach that occurred around it in Jan 2021 (https://about.codecov.io/security-update/)
+#   This script should probably be periodically updated and reviewed from the above URL :)
+#
 
 set -e +o pipefail
 
