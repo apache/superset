be5e02fbc90b6aa307ffc291cc82d75a06a8f81f
hughhhh <hughmil3s@gmail.com>
2021-04-19 15:12:22 -0700

    fux

--- superset/db_engine_specs/bigquery.py
+++ superset/db_engine_specs/bigquery.py
@@ -33,7 +33,8 @@ if TYPE_CHECKING:
 
 
 CONNECTION_DATABASE_PERMISSIONS_REGEX = re.compile(
-    "Access Denied: Project User does not have bigquery.jobs.create permission in project (?P<project>.+?)"
+    "Access Denied: Project User does not have bigquery.jobs.create"
+    + "permission in project (?P<project>.+?)"
 )
 
 
