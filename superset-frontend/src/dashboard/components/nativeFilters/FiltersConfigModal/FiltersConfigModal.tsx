a03442118a53b5f1c1c98e8b5d815141a6f42048
simcha90 <56388545+simcha90@users.noreply.github.com>
2021-04-20 14:07:08 +0300

    test(native-filters): Filter config modal test (#14245)
    * test: add tests for filter bar
    
    * test: merge filter bar tests with master
    
    * test: add test for filter set
    
    * test: filter set tests
    
    * test: merge with master
    
    * test: fix tests for filter bar
    
    * test: filter config modal
    
    * test: add select filter
    
    * feat: filter config modal
    
    * lint: fix lint
    
    * lint: fix TS
    
    * test: test testWithId

--- superset-frontend/src/dashboard/components/nativeFilters/FiltersConfigModal/FiltersConfigModal.tsx
+++ superset-frontend/src/dashboard/components/nativeFilters/FiltersConfigModal/FiltersConfigModal.tsx
@@ -22,7 +22,7 @@ import { t, styled } from '@superset-ui/core';
 import { Form } from 'src/common/components';
 import { StyledModal } from 'src/components/Modal';
 import ErrorBoundary from 'src/components/ErrorBoundary';
-import { testWithId } from 'src/utils/common';
+import { testWithId } from 'src/utils/testUtils';
 import { useFilterConfigMap, useFilterConfiguration } from '../state';
 import { FilterRemoval, NativeFiltersForm } from './types';
 import { FilterConfiguration } from '../types';
