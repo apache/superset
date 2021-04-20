c00fe44ba833921166f7518b310cc2096a0ef7c2
Simcha Shats <simcha.shats@nielsen.com>
2021-04-20 12:27:09 +0300

    feat: filter config modal

--- superset-frontend/src/dashboard/components/nativeFilters/FiltersConfigModal/FiltersConfigModal.test.tsx
+++ superset-frontend/src/dashboard/components/nativeFilters/FiltersConfigModal/FiltersConfigModal.test.tsx
@@ -27,13 +27,15 @@ import {
 } from 'spec/fixtures/mockStore';
 import fetchMock from 'fetch-mock';
 import React from 'react';
-import userEvent, { TargetElement } from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import { testWithId } from 'src/utils/testUtils';
 import { waitFor } from '@testing-library/react';
 import {
   FILTERS_CONFIG_MODAL_TEST_ID,
   FiltersConfigModal,
 } from './FiltersConfigModal';
+
+jest.useFakeTimers();
 
 class MainPreset extends Preset {
   constructor() {
@@ -51,11 +53,12 @@ const getTestId = testWithId<string>(FILTERS_CONFIG_MODAL_TEST_ID, true);
 
 describe('FilterConfigModal', () => {
   new MainPreset().register();
+  const onSave = jest.fn();
   const newFilterProps = {
     isOpen: true,
     initialFilterId: undefined,
     createNewOnOpen: true,
-    onSave: jest.fn(),
+    onSave,
     onCancel: jest.fn(),
   };
   fetchMock.get(
@@ -91,25 +94,31 @@ describe('FilterConfigModal', () => {
       result: {
         columns: [
           {
-            changed_on: '2021-04-12T07:51:28.794931',
             column_name: 'name',
-            created_on: '2021-04-12T07:48:43.369373',
-            description: null,
-            expression: '',
-            filterable: true,
             groupby: true,
             id: 334,
-            is_active: true,
-            is_dttm: false,
-            python_date_format: null,
-            type: 'VARCHAR(255)',
-            uuid: 'f364a616-41a0-4354-9e40-9cd20136d854',
-            verbose_name: null,
           },
         ],
         table_name: 'birth_names',
       },
       show_columns: ['id', 'table_name'],
+    },
+    { overwriteRoutes: true },
+  );
+  fetchMock.post(
+    'glob:*/api/v1/chart/data',
+    {
+      result: [
+        {
+          status: 'success',
+          data: [
+            { name: 'Aaron', count: 453 },
+            { name: 'Abigail', count: 228 },
+            { name: 'Adam', count: 454 },
+          ],
+          applied_filters: [{ column: 'name' }],
+        },
+      ],
     },
     { overwriteRoutes: true },
   );
@@ -131,23 +140,74 @@ describe('FilterConfigModal', () => {
     jest.clearAllMocks();
   });
 
-  it('Create Select Filter (with datasource and columns)', async () => {
+  it('Create Select Filter (with datasource and columns) with specific filter scope', async () => {
     renderWrapper();
 
     const FILTER_NAME = 'Select Filter 1';
 
+    // fill name
     userEvent.type(screen.getByTestId(getTestId('name-input')), FILTER_NAME);
+
+    // fill dataset
     await waitFor(() =>
       expect(screen.queryByText('Loading...')).not.toBeInTheDocument(),
     );
     userEvent.click(
       screen
         .getByTestId(getTestId('datasource-input'))
-        .querySelector('.Select__indicators') as TargetElement,
+        .querySelector('.Select__indicators')!,
     );
     userEvent.click(screen.getByText('birth_names'));
+
+    // fill column
     userEvent.click(screen.getByText('Select...'));
+    await waitFor(() =>
+      expect(screen.queryByText('Loading...')).not.toBeInTheDocument(),
+    );
     userEvent.click(screen.getByText('name'));
+
+    // fill controls
+    expect(screen.getByText('Multiple select').parentElement!).toHaveAttribute(
+      'class',
+      'ant-checkbox-wrapper ant-checkbox-wrapper-checked',
+    );
+    userEvent.click(screen.getByText('Multiple select'));
+    expect(
+      screen.getByText('Multiple select').parentElement!,
+    ).not.toHaveAttribute(
+      'class',
+      'ant-checkbox-wrapper ant-checkbox-wrapper-checked',
+    );
+
+    // choose default value
+    userEvent.click(await screen.findByText('3 options'));
+    userEvent.click(screen.getByTitle('Abigail'));
+
+    // fill scoping
+    userEvent.click(screen.getByText('Apply to specific panels'));
+    userEvent.click(screen.getByText('CHART_ID'));
+
+    // saving
     userEvent.click(screen.getByText('Save'));
+    await waitFor(() =>
+      expect(onSave.mock.calls[0][0][0]).toEqual(
+        expect.objectContaining({
+          cascadeParentIds: [],
+          controlValues: {
+            defaultToFirstItem: false,
+            enableEmptyFilter: false,
+            inverseSelection: false,
+            multiSelect: false,
+            sortAscending: true,
+          },
+          defaultValue: ['Abigail'],
+          filterType: 'filter_select',
+          isInstant: false,
+          name: 'Select Filter 1',
+          scope: { excluded: [], rootPath: [] },
+          targets: [{ column: { name: 'name' }, datasetId: 11 }],
+        }),
+      ),
+    );
   });
 });
