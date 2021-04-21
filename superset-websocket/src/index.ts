e92dbef85ceebf400b0ceaf7a9adc71925378763
Ben Reinhart <benjreinhart@gmail.com>
2021-04-20 17:28:08 -0700

    chore: WebSocket server improvements (#14257)
    * Update some types and resolve conflicts
    
    * Logger improvements
    
    * Ensure logger can log exception objects
    * Remove logging output in tests (for now)
    
    * Add License

--- superset-websocket/src/index.ts
+++ superset-websocket/src/index.ts
@@ -20,11 +20,11 @@ import * as http from 'http';
 import * as net from 'net';
 import WebSocket from 'ws';
 import { v4 as uuidv4 } from 'uuid';
+import jwt from 'jsonwebtoken';
+import cookie from 'cookie';
+import Redis from 'ioredis';
 
-const winston = require('winston');
-const jwt = require('jsonwebtoken');
-const cookie = require('cookie');
-const Redis = require('ioredis');
+import { createLogger } from './logger';
 
 export type StreamResult = [
   recordId: string,
@@ -114,20 +114,11 @@ try {
 Object.assign(opts, config);
 
 // init logger
-const logTransports = [
-  new winston.transports.Console({ handleExceptions: true }),
-];
-if (opts.logToFile && opts.logFilename) {
-  logTransports.push(
-    new winston.transports.File({
-      filename: opts.logFilename,
-      handleExceptions: true,
-    }),
-  );
-}
-const logger = winston.createLogger({
-  level: opts.logLevel,
-  transports: logTransports,
+const logger = createLogger({
+  silent: environment === 'test',
+  logLevel: opts.logLevel,
+  logToFile: opts.logToFile,
+  logFilename: opts.logFilename,
 });
 
 // enforce JWT secret length
@@ -219,7 +210,7 @@ export const fetchRangeFromStream = async ({
   try {
     const reply = await redis.xrange(streamName, startId, endId);
     if (!reply || !reply.length) return;
-    listener(reply);
+    listener(reply as StreamResult[]);
   } catch (e) {
     logger.error(e);
   }
@@ -254,7 +245,7 @@ export const subscribeToGlobalStream = async (
       if (!results.length) {
         continue;
       }
-      listener(results);
+      listener(results as StreamResult[]);
       setLastFirehoseId(results[length - 1][0]);
     } catch (e) {
       logger.error(e);
@@ -284,11 +275,11 @@ export const processStreamResults = (results: StreamResult[]): void => {
  * Returns the JWT payload or throws an error on invalid token.
  */
 const getJwtPayload = (request: http.IncomingMessage): JwtPayload => {
-  const cookies = cookie.parse(request.headers.cookie);
+  const cookies = cookie.parse(request.headers.cookie || '');
   const token = cookies[opts.jwtCookieName];
 
   if (!token) throw new Error('JWT not present');
-  return jwt.verify(token, opts.jwtSecret);
+  return jwt.verify(token, opts.jwtSecret) as JwtPayload;
 };
 
 /**
