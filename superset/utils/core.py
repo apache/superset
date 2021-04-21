a97f199f994d31ee23d6f32cb67907dca7a67fc2
Ben Reinhart <benjreinhart@gmail.com>
2021-04-21 15:59:45 -0700

    bug: Fix unintended cache misses with async queries

--- superset/utils/core.py
+++ superset/utils/core.py
@@ -99,6 +99,7 @@ from superset.exceptions import (
 )
 from superset.typing import FlaskResponse, FormData, Metric
 from superset.utils.dates import datetime_to_epoch, EPOCH
+from superset.utils.hashing import md5_sha_from_dict
 
 try:
     from pydruid.utils.having import Having
@@ -1050,7 +1051,6 @@ def to_adhoc(
     result = {
         "clause": clause.upper(),
         "expressionType": expression_type,
-        "filterOptionName": str(uuid.uuid4()),
         "isExtra": bool(filt.get("isExtra")),
     }
 
@@ -1064,6 +1064,9 @@ def to_adhoc(
         )
     elif expression_type == "SQL":
         result.update({"sqlExpression": filt.get(clause)})
+
+    deterministic_name = md5_sha_from_dict(result)
+    result["filterOptionName"] = deterministic_name
 
     return result
 
