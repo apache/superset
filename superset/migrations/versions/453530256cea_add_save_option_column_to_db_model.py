172c42a3f46f7dc5d6554160b4dd780643181063
Arash <arash.afghahi@gmail.com>
2021-04-30 11:06:07 -0400

    db migration for dbs

--- superset/migrations/versions/453530256cea_add_save_option_column_to_db_model.py
+++ superset/migrations/versions/453530256cea_add_save_option_column_to_db_model.py
@@ -17,14 +17,22 @@
 """add_save_form_column_to_db_model
 
 Revision ID: 453530256cea
+<<<<<<< HEAD
 Revises: f1410ed7ec95
+=======
+Revises: d416d0d715cc
+>>>>>>> db migration for dbs
 Create Date: 2021-04-30 10:55:07.009994
 
 """
 
 # revision identifiers, used by Alembic.
 revision = "453530256cea"
+<<<<<<< HEAD
 down_revision = "f1410ed7ec95"
+=======
+down_revision = "d416d0d715cc"
+>>>>>>> db migration for dbs
 
 import sqlalchemy as sa
 from alembic import op
@@ -33,14 +41,22 @@ from alembic import op
 def upgrade():
     with op.batch_alter_table("dbs") as batch_op:
         batch_op.add_column(
+<<<<<<< HEAD
             sa.Column(
                 "configuration_method",
                 sa.VARCHAR(255),
                 server_default="sqlalchemy_form",
             )
+=======
+            sa.Column("save_option", sa.VARCHAR(255), server_default="SQL_ALCHEMY")
+>>>>>>> db migration for dbs
         )
 
 
 def downgrade():
     with op.batch_alter_table("dbs") as batch_op:
+<<<<<<< HEAD
         batch_op.drop_column("configuration_method")
+=======
+        batch_op.drop_column("save_option")
+>>>>>>> db migration for dbs
