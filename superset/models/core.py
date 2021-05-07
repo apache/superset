277d4f9ad12008aaf88755a9363271954c4d78da
Arash <arash.afghahi@gmail.com>
2021-05-07 18:20:04 -0400

    added enum to migration file

--- superset/models/core.py
+++ superset/models/core.py
@@ -16,6 +16,7 @@
 # under the License.
 # pylint: disable=line-too-long,unused-argument,ungrouped-imports
 """A collection of ORM sqlalchemy models for Superset"""
+import enum
 import json
 import logging
 import textwrap
