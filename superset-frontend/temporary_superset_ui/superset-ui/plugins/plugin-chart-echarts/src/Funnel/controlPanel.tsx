59d650564a90bdaab2887861d3b69bbc868ee428
gordern <458957068@qq.com>
2021-05-10 22:08:09 +0800

    fix(plugin-chart-echarts): add orderby to funnel chart (#1099)
    * fix(plugin-chart-echarts): add orderby
    
    fix https://github.com/apache/superset/issues/14534
    
    * feat(plugin-chart-echarts): set orderby default value is true

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/plugin-chart-echarts/src/Funnel/controlPanel.tsx
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/plugin-chart-echarts/src/Funnel/controlPanel.tsx
@@ -57,6 +57,17 @@ const config: ControlPanelConfig = {
             },
           },
         ],
+        [
+          {
+            name: 'sort_by_metric',
+            config: {
+              default: true,
+              type: 'CheckboxControl',
+              label: t('Sort by metric'),
+              description: t('Whether to sort results by the selected metric in descending order.'),
+            },
+          },
+        ],
       ],
     },
     {
