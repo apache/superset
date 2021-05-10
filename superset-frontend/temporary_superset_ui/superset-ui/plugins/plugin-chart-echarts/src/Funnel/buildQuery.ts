ec160d6b5af65c20a91e2cbd288ecd79b41eb50b
gordern <458957068@qq.com>
2021-05-10 22:08:09 +0800

    fix(plugin-chart-echarts): add orderby to funnel chart (#1099)
    * fix(plugin-chart-echarts): add orderby
    
    fix https://github.com/apache/superset/issues/14534
    
    * feat(plugin-chart-echarts): set orderby default value is true

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/plugin-chart-echarts/src/Funnel/buildQuery.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/plugin-chart-echarts/src/Funnel/buildQuery.ts
@@ -19,9 +19,11 @@
 import { buildQueryContext, QueryFormData } from '@superset-ui/core';
 
 export default function buildQuery(formData: QueryFormData) {
+  const { metric, sort_by_metric } = formData;
   return buildQueryContext(formData, baseQueryObject => [
     {
       ...baseQueryObject,
+      ...(sort_by_metric && { orderby: [[metric, false]] }),
     },
   ]);
 }
