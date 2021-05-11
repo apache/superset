27d44eeb5de2c566fa1504f450ed8d8b83002b8e
Ubuntu <dafanic@vm-aaronfanick.53wq0amrcsbuxnt44apgcucn4h.ux.internal.cloudapp.net>
2021-05-11 13:36:21 +0000

    'CLDN-770': Displays all columns and metrics in data set explore window

--- superset-frontend/src/explore/components/DatasourcePanel/index.tsx
+++ superset-frontend/src/explore/components/DatasourcePanel/index.tsx
@@ -180,12 +180,9 @@ export default function DataSourcePanel({
     setInputValue('');
   }, [columns, datasource, metrics]);
 
-  const metricSlice = lists.metrics.slice(0, 50);
-  const columnSlice = lists.columns.slice(0, 50);
-
   const mainBody = (
     <>
-      <div className="field-selections" style={{overflowY: 'scroll', height: '85%'}}>
+      <div className="field-selections" style={{overflowY: 'scroll', height: '88%'}}>
         <input
           type="text"
           onChange={evt => {
@@ -207,9 +204,9 @@ export default function DataSourcePanel({
             key="metrics"
           >
             <div className="field-length">
-              {t(`Showing %s of %s`, metricSlice.length, lists.metrics.length)}
+              {t(`Showing %s`, lists.metrics.length)}
             </div>
-            {metricSlice.map(m => (
+            {lists.metrics.map(m => (
               <LabelContainer key={m.metric_name} className="column">
                 {enableExploreDnd ? (
                   <DatasourcePanelDragWrapper
@@ -229,9 +226,9 @@ export default function DataSourcePanel({
             key="column"
           >
             <div className="field-length">
-              {t(`Showing %s of %s`, columnSlice.length, lists.columns.length)}
+              {t(`Showing %s`, lists.columns.length)}
             </div>
-            {columnSlice.map(col => (
+            {lists.columns.map(col => (
               <LabelContainer key={col.column_name} className="column">
                 {enableExploreDnd ? (
                   <DatasourcePanelDragWrapper
