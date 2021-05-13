1014efa5e74a49aa4fd47cdc3384ac5950f6d80c
Ville Brofeldt <33317356+villebro@users.noreply.github.com>
2021-05-13 09:36:09 +0300

    feat: add generic type to column payload (#14547)
    * feat: add generic type to column payload
    
    * feat: add generic type to column payload
    
    * xit flaky test

--- tests/db_engine_specs/base_tests.py
+++ tests/db_engine_specs/base_tests.py
@@ -16,11 +16,13 @@
 # under the License.
 # isort:skip_file
 from datetime import datetime
+from typing import Tuple, Type
 
 from tests.test_app import app
 from tests.base_tests import SupersetTestCase
-from superset.db_engine_specs.mysql import MySQLEngineSpec
+from superset.db_engine_specs.base import BaseEngineSpec
 from superset.models.core import Database
+from superset.utils.core import GenericDataType
 
 
 class TestDbEngineSpec(SupersetTestCase):
@@ -28,10 +30,23 @@ class TestDbEngineSpec(SupersetTestCase):
         self,
         sql,
         expected_sql,
-        engine_spec_class=MySQLEngineSpec,
+        engine_spec_class=BaseEngineSpec,
         limit=1000,
         force=False,
     ):
         main = Database(database_name="test_database", sqlalchemy_uri="sqlite://")
         limited = engine_spec_class.apply_limit_to_sql(sql, limit, main, force)
         self.assertEqual(expected_sql, limited)
+
+
+def assert_generic_types(
+    spec: Type[BaseEngineSpec],
+    type_expectations: Tuple[Tuple[str, GenericDataType], ...],
+) -> None:
+    for type_str, expected_type in type_expectations:
+        column_spec = spec.get_column_spec(type_str)
+        assert column_spec is not None
+        actual_type = column_spec.generic_type
+        assert (
+            actual_type == expected_type
+        ), f"{type_str} should be {expected_type.name} but is {actual_type.name}"
