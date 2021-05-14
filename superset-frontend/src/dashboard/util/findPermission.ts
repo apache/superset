06845b1c83a6182ae327b27e7b8c3f1633adee42
David Aaron Suddjian <1858430+suddjian@users.noreply.github.com>
2021-05-14 12:18:38 -0700

    fix(dashboard): check edit permissions correctly on frontend (#14626)
    * fix(dashboard): check edit permissions correctly on frontend
    
    * fix types, appease linter
    
    * handle nulls better

--- superset-frontend/src/dashboard/util/findPermission.ts
+++ superset-frontend/src/dashboard/util/findPermission.ts
@@ -17,14 +17,37 @@
  * under the License.
  */
 import memoizeOne from 'memoize-one';
+import { UserWithPermissionsAndRoles } from 'src/types/bootstrapTypes';
+import Dashboard from 'src/types/Dashboard';
 
 type UserRoles = Record<string, [string, string][]>;
 
 const findPermission = memoizeOne(
-  (perm: string, view: string, roles: UserRoles) =>
+  (perm: string, view: string, roles?: UserRoles | null) =>
+    !!roles &&
     Object.values(roles).some(permissions =>
       permissions.some(([perm_, view_]) => perm_ === perm && view_ === view),
     ),
 );
 
 export default findPermission;
+
+// this should really be a config value,
+// but is hardcoded in backend logic already, so...
+const ADMIN_ROLE_NAME = 'admin';
+
+const isUserAdmin = (user: UserWithPermissionsAndRoles) =>
+  Object.keys(user.roles).some(role => role.toLowerCase() === ADMIN_ROLE_NAME);
+
+const isUserDashboardOwner = (
+  dashboard: Dashboard,
+  user: UserWithPermissionsAndRoles,
+) => dashboard.owners.some(owner => owner.username === user.username);
+
+export const canUserEditDashboard = (
+  dashboard: Dashboard,
+  user?: UserWithPermissionsAndRoles | null,
+) =>
+  !!user &&
+  (isUserAdmin(user) || isUserDashboardOwner(dashboard, user)) &&
+  findPermission('can_write', 'Dashboard', user.roles);
