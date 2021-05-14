0eaa79dc1ed6523e9486b413abb92267a3d3c13c
Michael S. Molina <70410625+michael-s-molina@users.noreply.github.com>
2021-05-14 12:21:56 -0300

    chore: removes react-bootstrap (#1108)

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-chart-controls/src/components/CertifiedIconWithTooltip.tsx
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-chart-controls/src/components/CertifiedIconWithTooltip.tsx
@@ -17,11 +17,9 @@
  * under the License.
  */
 import React from 'react';
-import { Tooltip, OverlayTrigger } from 'react-bootstrap';
 import { kebabCase } from 'lodash';
-import { t, supersetTheme } from '@superset-ui/core';
-
-const tooltipStyle: React.CSSProperties = { wordWrap: 'break-word' };
+import { t, useTheme, styled } from '@superset-ui/core';
+import Tooltip from './Tooltip';
 
 interface CertifiedIconWithTooltipProps {
   certifiedBy?: string | null;
@@ -29,19 +27,24 @@ interface CertifiedIconWithTooltipProps {
   metricName: string;
 }
 
+const StyledDiv = styled.div`
+  margin-bottom: ${({ theme }) => theme.gridUnit * 2}px;
+`;
+
 function CertifiedIconWithTooltip({
   certifiedBy,
   details,
   metricName,
 }: CertifiedIconWithTooltipProps) {
+  const theme = useTheme();
   return (
-    <OverlayTrigger
-      placement="top"
-      overlay={
-        <Tooltip id={`${kebabCase(metricName)}-tooltip`} style={tooltipStyle}>
-          {certifiedBy && <div>{t('Certified by %s', certifiedBy)}</div>}
+    <Tooltip
+      id={`${kebabCase(metricName)}-tooltip`}
+      title={
+        <div>
+          {certifiedBy && <StyledDiv>{t('Certified by %s', certifiedBy)}</StyledDiv>}
           <div>{details}</div>
-        </Tooltip>
+        </div>
       }
     >
       {/* TODO: move Icons to superset-ui to remove duplicated icon code here */}
@@ -54,12 +57,12 @@ function CertifiedIconWithTooltip({
       >
         <g>
           <path
-            fill={supersetTheme.colors.primary.base}
+            fill={theme.colors.primary.base}
             d="M23,12l-2.44-2.79l0.34-3.69l-3.61-0.82L15.4,1.5L12,2.96L8.6,1.5L6.71,4.69L3.1,5.5L3.44,9.2L1,12l2.44,2.79l-0.34,3.7 l3.61,0.82L8.6,22.5l3.4-1.47l3.4,1.46l1.89-3.19l3.61-0.82l-0.34-3.69L23,12z M9.38,16.01L7,13.61c-0.39-0.39-0.39-1.02,0-1.41 l0.07-0.07c0.39-0.39,1.03-0.39,1.42,0l1.61,1.62l5.15-5.16c0.39-0.39,1.03-0.39,1.42,0l0.07,0.07c0.39,0.39,0.39,1.02,0,1.41 l-5.92,5.94C10.41,16.4,9.78,16.4,9.38,16.01z"
           />
         </g>
       </svg>
-    </OverlayTrigger>
+    </Tooltip>
   );
 }
 
