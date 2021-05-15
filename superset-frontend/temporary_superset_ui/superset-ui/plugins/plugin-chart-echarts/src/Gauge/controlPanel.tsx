a8a9ed07d13e3a91452a7fc688cea9f30e75ccd3
Yaozong Liu <750188453@qq.com>
2021-05-15 22:52:37 +0800

    fix(plugin-chart-echarts): 'SORT BY METRIC' add checkbox to Gauge (#1113)

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/plugin-chart-echarts/src/Gauge/controlPanel.tsx
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/plugin-chart-echarts/src/Gauge/controlPanel.tsx
@@ -55,6 +55,16 @@ const config: ControlPanelConfig = {
             },
           },
         ],
+        [
+          {
+            name: 'sort_by_metric',
+            config: {
+              type: 'CheckboxControl',
+              label: t('Sort by metric'),
+              description: t('Whether to sort results by the selected metric in descending order.'),
+            },
+          },
+        ],
       ],
     },
     {
