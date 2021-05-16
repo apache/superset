2b79e49bf9ed884f63d6ee68257033aca471308b
hughhhh <hughmil3s@gmail.com>
2021-05-16 09:17:16 -0400

    put condition to override parameters

--- superset/databases/schemas.py
+++ superset/databases/schemas.py
@@ -248,6 +248,9 @@ class DatabaseParametersSchemaMixin:
         """
         parameters = data.pop("parameters", None)
         if parameters:
+            if len(parameters.keys()) < 2:
+                return data
+
             if "engine" not in parameters:
                 raise ValidationError(
                     [
