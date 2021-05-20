f88671cc8fe20cbd1bc7c2d29745f7391fe8a8c5
Daniel Gaspar <danielvazgaspar@gmail.com>
2021-05-20 12:27:01 +0100

    fix(docker): superset permissions and firefox config

--- superset/config.py
+++ superset/config.py
@@ -1027,15 +1027,16 @@ WEBDRIVER_WINDOW = {"dashboard": (1600, 2000), "slice": (3000, 1200)}
 WEBDRIVER_AUTH_FUNC = None
 
 # Any config options to be passed as-is to the webdriver
-WEBDRIVER_CONFIGURATION: Dict[Any, Any] = {}
+WEBDRIVER_CONFIGURATION: Dict[Any, Any] = {
+    "service_log_path": "/dev/null"
+}
 
 # Additional args to be passed as arguments to the config object
 # Note: these options are Chrome-specific. For FF, these should
 # only include the "--headless" arg
 WEBDRIVER_OPTION_ARGS = [
-    "--force-device-scale-factor=2.0",
-    "--high-dpi-support=2.0",
     "--headless",
+    "--marionette"
 ]
 
 # The base URL to query for accessing the user interface
