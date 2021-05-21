b92a21cf0bf53cabb4234e6d3037b98671e71b6e
Yongjie Zhao <yongjie.zhao@gmail.com>
2021-05-21 17:24:42 +0100

    fix(dashboard): multiple query trigger when native filter enabled (#14734)
    * fix(dashboard): multiple query trigger when native filter enabled
    
    * fix lint
    
    * wip

--- superset-frontend/src/dashboard/components/nativeFilters/FilterBar/state.ts
+++ superset-frontend/src/dashboard/components/nativeFilters/FilterBar/state.ts
@@ -107,10 +107,7 @@ export const useInitialization = () => {
     const numberOfLoadedCharts = Object.values(charts).filter(
       ({ chartStatus }) => chartStatus !== 'loading',
     ).length;
-    if (
-      numberOfLoadingCharts > 0 &&
-      numberOfLoadedCharts >= numberOfLoadingCharts
-    ) {
+    if (numberOfLoadedCharts >= numberOfLoadingCharts) {
       setIsInitialized(true);
     }
   }, [charts, isInitialized, numberOfLoadingCharts]);
