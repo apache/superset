6d33432b5846b24e8d71d923f8eb87fb6e51fa3d
Hugh A. Miles II <hughmil3s@gmail.com>
2021-05-23 12:45:48 -0400

    feat: Create BigQuery Parameters for DatabaseModal (#14721)

--- superset/databases/schemas.py
+++ superset/databases/schemas.py
@@ -246,6 +246,12 @@ class DatabaseParametersSchemaMixin:
         the constructed SQLAlchemy URI to be passed.
         """
         parameters = data.pop("parameters", None)
+        serialized_encrypted_extra = data.get("encrypted_extra", "{}")
+        try:
+            encrypted_extra = json.loads(serialized_encrypted_extra)
+        except json.decoder.JSONDecodeError:
+            encrypted_extra = {}
+
         if parameters:
             if "engine" not in parameters:
                 raise ValidationError(
@@ -264,12 +270,14 @@ class DatabaseParametersSchemaMixin:
                     [_('Engine "%(engine)s" is not a valid engine.', engine=engine,)]
                 )
             engine_spec = engine_specs[engine]
+
             if hasattr(engine_spec, "build_sqlalchemy_uri"):
                 data[
                     "sqlalchemy_uri"
                 ] = engine_spec.build_sqlalchemy_uri(  # type: ignore
-                    parameters
+                    parameters, encrypted_extra
                 )
+
         return data
 
 
@@ -546,3 +554,15 @@ class ImportV1DatabaseSchema(Schema):
         password = make_url(uri).password
         if password == PASSWORD_MASK and data.get("password") is None:
             raise ValidationError("Must provide a password for the database")
+
+
+class EncryptedField(fields.String):
+    pass
+
+
+def encrypted_field_properties(self, field: Any, **_) -> Dict[str, Any]:  # type: ignore
+    ret = {}
+    if isinstance(field, EncryptedField):
+        if self.openapi_version.major > 2:
+            ret["x-encrypted-extra"] = True
+    return ret
