e2f17d1f17f61dc2a967e4fad41bbff2d2d83358
Yongjie Zhao <yongjie.zhao@gmail.com>
2021-06-02 21:55:11 +0800

    fix lint

--- superset-frontend/src/datasource/DatasourceEditor.jsx
+++ superset-frontend/src/datasource/DatasourceEditor.jsx
@@ -418,7 +418,10 @@ class DatasourceEditor extends React.PureComponent {
           is_dttm: col.is_dttm,
         });
         results.added.push(col.name);
-      } else if (currentCol.type !== col.type || currentCol.is_dttm !== col.is_dttm) {
+      } else if (
+        currentCol.type !== col.type ||
+        currentCol.is_dttm !== col.is_dttm
+      ) {
         // modified column
         finalColumns.push({
           ...currentCol,
