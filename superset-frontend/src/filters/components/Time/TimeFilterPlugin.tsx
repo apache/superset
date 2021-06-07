6771a836890455fabd9300cb2e2e31210580f7e6
Ville Brofeldt <ville.v.brofeldt@gmail.com>
2021-06-07 14:26:34 +0300

    fix(native-filters): use default for time range from explore

--- superset-frontend/src/filters/components/Time/TimeFilterPlugin.tsx
+++ superset-frontend/src/filters/components/Time/TimeFilterPlugin.tsx
@@ -17,12 +17,10 @@
  * under the License.
  */
 import { styled } from '@superset-ui/core';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import DateFilterControl from 'src/explore/components/controls/DateFilterControl';
 import { PluginFilterTimeProps } from './types';
 import { Styles } from '../common';
-
-const DEFAULT_VALUE = 'Last week';
 
 const TimeFilterStyles = styled(Styles)`
   overflow-x: scroll;
@@ -34,35 +32,27 @@ const ControlContainer = styled.div`
 
 export default function TimeFilterPlugin(props: PluginFilterTimeProps) {
   const {
-    formData,
     setDataMask,
     setFocusedFilter,
     unsetFocusedFilter,
     width,
     filterState,
   } = props;
-  const { defaultValue } = formData;
 
-  const [value, setValue] = useState<string>(defaultValue ?? DEFAULT_VALUE);
-
-  const handleTimeRangeChange = (timeRange: string): void => {
-    setValue(timeRange);
-
+  const handleTimeRangeChange = (timeRange?: string): void => {
     setDataMask({
-      extraFormData: {
-        time_range: timeRange,
-      },
+      extraFormData: timeRange
+        ? {
+            time_range: timeRange,
+          }
+        : {},
       filterState: { value: timeRange },
     });
   };
 
   useEffect(() => {
-    handleTimeRangeChange(filterState.value ?? DEFAULT_VALUE);
+    handleTimeRangeChange(filterState.value);
   }, [filterState.value]);
-
-  useEffect(() => {
-    handleTimeRangeChange(defaultValue ?? DEFAULT_VALUE);
-  }, [defaultValue]);
 
   return (
     // @ts-ignore
@@ -72,7 +62,7 @@ export default function TimeFilterPlugin(props: PluginFilterTimeProps) {
         onMouseLeave={unsetFocusedFilter}
       >
         <DateFilterControl
-          value={value}
+          value={filterState.value}
           name="time_range"
           onChange={handleTimeRangeChange}
         />
