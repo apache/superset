11eef251b2348d1c895e70911ff4ad9d58e8cd19
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2021-06-08 03:32:33 +0300

    feat: Add "is_select_query" method to base engine spec to make it possible to override it (#15013)

--- superset/db_engine_specs/base.py
+++ superset/db_engine_specs/base.py
@@ -1255,6 +1255,14 @@ class BaseEngineSpec:  # pylint: disable=too-many-public-methods
         )
 
     @classmethod
+    def is_select_query(cls, parsed_query: ParsedQuery) -> bool:
+        """
+        Determine if the statement should be considered as SELECT statement.
+        Some query dialects do not contain "SELECT" word in queries (eg. Kusto)
+        """
+        return parsed_query.is_select()
+
+    @classmethod
     @utils.memoized
     def get_column_spec(
         cls,
