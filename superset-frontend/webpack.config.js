36ee58af0bc85aaeaf82741f83d9d31079343760
ʈᵃᵢ <tdupreetan@gmail.com>
2021-06-09 12:59:17 -0700

    build(webpack): use [contenthash] instead of [chunkhash] (#14942)

--- superset-frontend/webpack.config.js
+++ superset-frontend/webpack.config.js
@@ -59,11 +59,11 @@ if (isDevMode) {
   output.filename = '[name].[hash:8].entry.js';
   output.chunkFilename = '[name].[hash:8].chunk.js';
 } else if (nameChunks) {
-  output.filename = '[name].[chunkhash].entry.js';
-  output.chunkFilename = '[name].[chunkhash].chunk.js';
+  output.filename = '[name].[contenthash].entry.js';
+  output.chunkFilename = '[name].[contenthash].chunk.js';
 } else {
-  output.filename = '[name].[chunkhash].entry.js';
-  output.chunkFilename = '[chunkhash].chunk.js';
+  output.filename = '[name].[contenthash].entry.js';
+  output.chunkFilename = '[contenthash].chunk.js';
 }
 
 const plugins = [
@@ -155,8 +155,8 @@ if (!isDevMode) {
   // text loading (webpack 4+)
   plugins.push(
     new MiniCssExtractPlugin({
-      filename: '[name].[chunkhash].entry.css',
-      chunkFilename: '[name].[chunkhash].chunk.css',
+      filename: '[name].[contenthash].entry.css',
+      chunkFilename: '[name].[contenthash].chunk.css',
     }),
   );
   plugins.push(new OptimizeCSSAssetsPlugin());
