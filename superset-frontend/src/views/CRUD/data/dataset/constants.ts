a9d7e880fc5535921300e59d17a08ca6149a61c1
Yongjie Zhao <yongjie.zhao@gmail.com>
2021-06-10 03:05:33 +0100

    fix(datasets): consistent dataset list (#15014)

--- superset-frontend/src/views/CRUD/data/dataset/constants.ts
+++ superset-frontend/src/views/CRUD/data/dataset/constants.ts
@@ -16,11 +16,19 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import { SortingRule } from 'react-table';
+import { t } from '@superset-ui/core';
 
-export type SortByType = SortingRule<string>[];
-
-export interface ServerPagination {
-  pageIndex: number;
-  sortBy?: SortByType;
-}
+export const PAGE_SIZE = 25;
+export const SORT_BY = [{ id: 'changed_on_delta_humanized', desc: true }];
+export const PASSWORDS_NEEDED_MESSAGE = t(
+  'The passwords for the databases below are needed in order to ' +
+    'import them together with the datasets. Please note that the ' +
+    '"Secure Extra" and "Certificate" sections of ' +
+    'the database configuration are not present in export files, and ' +
+    'should be added manually after the import if they are needed.',
+);
+export const CONFIRM_OVERWRITE_MESSAGE = t(
+  'You are importing one or more datasets that already exist. ' +
+    'Overwriting might cause you to lose some of your work. Are you ' +
+    'sure you want to overwrite?',
+);
