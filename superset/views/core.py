fc11857b1fa727109152979ad85f28425b0471cd
Tai Dupree <tdupreetan@gmail.com>
2021-06-11 10:23:34 -0700

    fix(logging): downgrade csv export log to debug

--- superset/views/core.py
+++ superset/views/core.py
@@ -2665,7 +2665,7 @@ class Superset(BaseSupersetView):  # pylint: disable=too-many-public-methods
             "exported_format": "csv",
         }
         event_rep = repr(event_info)
-        logger.info("CSV exported: %s", event_rep, extra={"superset_event": event_info})
+        logger.debug("CSV exported: %s", event_rep, extra={"superset_event": event_info})
         return response
 
     @api
