4e4107f57d2c45f1767224da239351d63c999525
andrewbastian <andrewbastian@hosaka-deck.lan>
2021-06-14 15:06:24 -0700

    Fixed lint error.

--- superset-frontend/src/CRUD/CollectionTable.tsx
+++ superset-frontend/src/CRUD/CollectionTable.tsx
@@ -261,7 +261,7 @@ export default class CRUDCollection extends React.PureComponent<
             aria-label="Delete item"
             className="pointer"
             data-test="crud-delete-icon"
-            style={{fontSize:"18px"}}
+            style={{fontSize: '18px'}}
             role="button"
             tabIndex={0}
             onClick={this.deleteItem.bind(this, record.id)}
