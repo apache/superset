5ff19ae83d72da12ab4c29aa23cf160a74abf5e3
Ville Brofeldt <33317356+villebro@users.noreply.github.com>
2021-06-15 13:25:28 +0300

    feat(native-filters): add optional time col to time range (#15117)

--- superset-frontend/src/dashboard/components/nativeFilters/FiltersConfigModal/utils.ts
+++ superset-frontend/src/dashboard/components/nativeFilters/FiltersConfigModal/utils.ts
@@ -140,6 +140,7 @@ export const createHandleSave = (
         adhoc_filters: formInputs.adhoc_filters,
         time_range: formInputs.time_range,
         controlValues: formInputs.controlValues ?? {},
+        granularity_sqla: formInputs.granularity_sqla,
         requiredFirst: Object.values(formInputs.requiredFirst ?? {}).find(
           rf => rf,
         ),
