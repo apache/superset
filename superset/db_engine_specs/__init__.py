80264545401a5069117d73b093518e3a40b6fead
Hugh A. Miles II <hughmil3s@gmail.com>
2021-06-16 18:00:10 -0400

    Update superset/db_engine_specs/__init__.py
    Co-authored-by: Beto Dealmeida <roberto@dealmeida.net>

--- superset/db_engine_specs/__init__.py
+++ superset/db_engine_specs/__init__.py
@@ -121,10 +121,7 @@ def get_available_engine_specs() -> Dict[Type[BaseEngineSpec], Set[str]]:
         except Exception:  # pylint: disable=broad-except
             logger.warning("Unable to load SQLAlchemy dialect: %s", dialect)
         else:
-            if hasattr(dialect, "driver"):
-                drivers[dialect.name].add(dialect.driver)
-            else:
-                drivers[dialect.name].add(dialect.name)
+            drivers[dialect.name].add(getattr(dialect, "driver", dialect.name))
 
     engine_specs = get_engine_specs()
     return {
