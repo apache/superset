b9ffd413b2ee02a6b346ec89d213e13b850d143f
Hugh A. Miles II <hughmil3s@gmail.com>
2021-06-18 11:25:34 -0400

    fix: Edit for all dynamic forms (#15244)
    * fix edit for dynamic forms
    
    * fix linting
    
    * fix edit on sqlalchemy forms
    
    * fix big query reference
    
    * remove extra if

--- superset-frontend/src/views/CRUD/data/database/DatabaseModal/index.tsx
+++ superset-frontend/src/views/CRUD/data/database/DatabaseModal/index.tsx
@@ -192,13 +192,6 @@ function dbReducer(
         [action.payload.name]: action.payload.value,
       };
     case ActionType.parametersChange:
-      if (action.payload.name === 'encrypted_extra') {
-        return {
-          ...trimmedState,
-          encrypted_extra: action.payload.value,
-          parameters: {},
-        };
-      }
       return {
         ...trimmedState,
         parameters: {
@@ -242,6 +235,21 @@ function dbReducer(
         ).toString();
       }
 
+      if (action.payload.backend === 'bigquery') {
+        return {
+          ...action.payload,
+          engine: trimmedState.engine,
+          configuration_method: action.payload.configuration_method,
+          extra_json: deserializeExtraJSON,
+          parameters: {
+            query,
+            credentials_info: JSON.stringify(
+              action.payload?.parameters?.credentials_info || '',
+            ),
+          },
+        };
+      }
+
       return {
         ...action.payload,
         engine: trimmedState.engine,
@@ -250,9 +258,6 @@ function dbReducer(
         parameters: {
           ...action.payload.parameters,
           query,
-          credentials_info: JSON.stringify(
-            action.payload?.parameters?.credentials_info || '',
-          ),
         },
       };
     case ActionType.dbSelected:
@@ -359,6 +364,20 @@ const DatabaseModal: FunctionComponent<DatabaseModalProps> = ({
           .replace(/&/g, '","')
           .replace(/=/g, '":"')}"}`,
       );
+    } else if (update?.parameters?.query === '') {
+      update.parameters.query = {};
+    }
+
+    const engine = update.backend || update.engine;
+    if (
+      engine === 'bigquery' &&
+      update.configuration_method === CONFIGURATION_METHOD.DYNAMIC_FORM &&
+      update.parameters?.credentials_info
+    ) {
+      // wrap encrypted_extra in credentials_info only for BigQuery
+      update.encrypted_extra = JSON.stringify({
+        credentials_info: JSON.parse(update.parameters?.credentials_info),
+      });
     }
 
     if (db?.id) {
@@ -390,17 +409,6 @@ const DatabaseModal: FunctionComponent<DatabaseModalProps> = ({
       }
     } else if (db) {
       // Create
-      if (
-        update.engine === 'bigquery' &&
-        update.configuration_method === CONFIGURATION_METHOD.DYNAMIC_FORM &&
-        update.encrypted_extra
-      ) {
-        // wrap encrypted_extra in credentials_info only for BigQuery
-        update.encrypted_extra = JSON.stringify({
-          credentials_info: JSON.parse(update.encrypted_extra),
-        });
-      }
-
       if (update?.extra_json) {
         // convert extra_json to back to string
         update.extra = JSON.stringify({
@@ -607,7 +615,7 @@ const DatabaseModal: FunctionComponent<DatabaseModalProps> = ({
 
   const isDynamic = (engine: string | undefined) =>
     availableDbs?.databases.filter(
-      (DB: DatabaseObject) => DB.engine === engine,
+      (DB: DatabaseObject) => DB.backend === engine || DB.engine === engine,
     )[0].parameters !== undefined;
 
   return useTabLayout ? (
