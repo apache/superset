32f3f6821baea916325443065104b029f353e1d9
stephenLYZ <750188453@qq.com>
2021-06-18 11:58:46 +0800

    fix: update info

--- superset-frontend/src/views/CRUD/dashboard/DashboardList.tsx
+++ superset-frontend/src/views/CRUD/dashboard/DashboardList.tsx
@@ -157,8 +157,24 @@ function DashboardList(props: DashboardListProps) {
       ({ json = {} }) => {
         setDashboards(
           dashboards.map(dashboard => {
-            if (dashboard.id === json?.result.id) {
-              return json.result;
+            if (dashboard.id === json?.result?.id) {
+              const {
+                changed_by_name,
+                changed_by_url,
+                changed_by,
+                dashboard_title = '',
+                slug = '',
+                json_metadata = '',
+              } = json.result;
+              return {
+                ...dashboard,
+                changed_by_name,
+                changed_by_url,
+                changed_by,
+                dashboard_title,
+                slug,
+                json_metadata,
+              };
             }
             return dashboard;
           }),
