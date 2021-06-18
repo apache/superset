8a4a08d5fc737cdbf6f7a483fe0d04d1c9f9c511
maltoze <maltoze@qq.com>
2021-06-18 13:26:11 +0800

    fix: passing url params in sqllab

--- superset-frontend/src/SqlLab/actions/sqlLab.js
+++ superset-frontend/src/SqlLab/actions/sqlLab.js
@@ -329,7 +329,7 @@ export function runQuery(query) {
     };
 
     return SupersetClient.post({
-      endpoint: '/superset/sql_json/',
+      endpoint: `/superset/sql_json/${window.location.search}`,
       body: JSON.stringify(postPayload),
       headers: { 'Content-Type': 'application/json' },
       parseMethod: 'text',
