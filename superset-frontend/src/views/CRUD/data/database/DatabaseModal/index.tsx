c1325ab85a252f9615873f2f7df6b1b7d6c1d6c6
andrewbastian <andrew.bastian@gmail.com>
2021-06-17 21:54:05 -0700

    Fixed typo.

--- superset-frontend/src/views/CRUD/data/database/DatabaseModal/index.tsx
+++ superset-frontend/src/views/CRUD/data/database/DatabaseModal/index.tsx
@@ -1,7 +1,7 @@
 /**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
-placement* distributed with this work for additional information
+ * placement distributed with this work for additional information
  * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
