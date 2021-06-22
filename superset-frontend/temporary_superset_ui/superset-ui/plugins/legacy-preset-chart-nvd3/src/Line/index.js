1e6c0d29f7e2be176d48efac755d318a31000e99
David Aaron Suddjian <1858430+suddjian@users.noreply.github.com>
2021-06-22 11:36:03 -0700

    chore: Add new plugin metadata (#1179)
    * chore: add new plugin metadata
    
    * inital commit
    
    * capitalization
    
    * Update plugins/legacy-plugin-chart-calendar/src/index.js
    
    * Update plugins/legacy-preset-chart-big-number/src/BigNumber/index.ts
    
    * Update plugins/legacy-plugin-chart-histogram/src/index.js
    
    * edits
    
    * Update plugins/legacy-preset-chart-nvd3/src/Bar/index.js
    
    * fix: single quote
    
    * Update plugins/legacy-preset-chart-big-number/src/BigNumber/index.ts
    
    Co-authored-by: Michael S. Molina <70410625+michael-s-molina@users.noreply.github.com>
    
    * Update plugins/legacy-preset-chart-nvd3/src/Bullet/index.js
    
    Co-authored-by: Michael S. Molina <70410625+michael-s-molina@users.noreply.github.com>
    
    Co-authored-by: Phillip Kelley-Dotson <pkelleydotson@yahoo.com>
    Co-authored-by: Evan Rusackas <evan@preset.io>
    Co-authored-by: Michael S. Molina <70410625+michael-s-molina@users.noreply.github.com>

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/legacy-preset-chart-nvd3/src/Line/index.js
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/legacy-preset-chart-nvd3/src/Line/index.js
@@ -18,6 +18,9 @@
  */
 import { t, ChartMetadata, ChartPlugin } from '@superset-ui/core';
 import transformProps from '../transformProps';
+import example1 from './images/LineChart.jpg';
+import example2 from './images/LineChart2.jpg';
+import battery from './images/battery.jpg';
 import thumbnail from './images/thumbnail.png';
 import { ANNOTATION_TYPES } from '../vendor/superset/AnnotationTypes';
 import controlPanel from './controlPanel';
@@ -26,6 +29,11 @@ const metadata = new ChartMetadata({
   canBeAnnotationTypes: [ANNOTATION_TYPES.TIME_SERIES],
   credits: ['http://nvd3.org'],
   description: '',
+  exampleGallery: [
+    { url: example1 },
+    { url: example2 },
+    { url: battery, caption: t('Battery level over time') },
+  ],
   name: t('Line Chart'),
   supportedAnnotationTypes: [
     ANNOTATION_TYPES.TIME_SERIES,
