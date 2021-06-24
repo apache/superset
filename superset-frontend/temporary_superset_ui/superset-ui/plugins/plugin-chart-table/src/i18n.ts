49d9fce41fe8b16eddaae57d1490df88b3f96817
maltoze <maltoze@qq.com>
2021-06-24 17:59:59 +0800

    fix(plugin-chart-table): chinesee translation improve (#1189)

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/plugin-chart-table/src/i18n.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/plugin-chart-table/src/i18n.ts
@@ -37,7 +37,7 @@ const translations: Partial<Record<Locale, typeof en>> = {
     'Show Cell Bars': ['为指标添加条状图背景'],
     'page_size.show': ['每页显示'],
     'page_size.all': ['全部'],
-    'page_size.entries': ['上一页'],
+    'page_size.entries': ['条'],
     'table.previous_page': ['上一页'],
     'table.next_page': ['下一页'],
     'search.num_records': ['%s条记录...'],
