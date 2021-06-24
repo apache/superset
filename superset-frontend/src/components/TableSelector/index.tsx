680b447bc481624b39b4d9a13852cc52de27c33c
stellalc7 <stellalc7@gmail.com>
2021-06-24 11:04:04 -0400

    style(sqllab): update table count styling (#15200)
    * Change text style and opacity
    
    * Edit changes to text style and opacity for table length
    
    * Edit changes to text style and opacity

--- superset-frontend/src/components/TableSelector/index.tsx
+++ superset-frontend/src/components/TableSelector/index.tsx
@@ -65,6 +65,10 @@ const TableSelectorWrapper = styled.div`
     border-bottom: 1px solid ${({ theme }) => theme.colors.secondary.light5};
     margin: 15px 0;
   }
+
+  .table-length {
+    color: ${({ theme }) => theme.colors.grayscale.light1};
+  }
 `;
 
 const TableLabel = styled.span`
@@ -382,9 +386,8 @@ const TableSelector: FunctionComponent<TableSelectorProps> = ({
         <FormLabel>
           {t('See table schema')}{' '}
           {schema && (
-            <small>
-              {tableOptions.length} in
-              <i>{schema}</i>
+            <small className="table-length">
+              {tableOptions.length} in {schema}
             </small>
           )}
         </FormLabel>
