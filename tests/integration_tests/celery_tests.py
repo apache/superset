8e3b0e8eed5da707a69e36ddd3f11c3ed628466d
ofekisr <ofek1israel@gmail.com>
2021-07-01 15:43:14 +0300

    fix celery_tests.py

--- tests/integration_tests/celery_tests.py
+++ tests/integration_tests/celery_tests.py
@@ -23,7 +23,7 @@ import string
 import time
 import unittest.mock as mock
 from typing import Optional
-from tests.integration_tests.integration_tests.integration_tests.fixtures.birth_names_dashboard import (
+from tests.integration_tests.fixtures.birth_names_dashboard import (
     load_birth_names_dashboard_with_slices,
 )
 
