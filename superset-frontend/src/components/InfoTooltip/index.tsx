4b968378e8a5c51e2856c1e37a64165b05827b4c
Phillip Kelley-Dotson <pkelleydotson@yahoo.com>
2021-07-01 08:52:17 -0700

    refactor: icon to icons for infotooltip component (#15398)
    * intital commit
    
    * align label texts and icons
    
    * fix style

--- superset-frontend/src/components/InfoTooltip/index.tsx
+++ superset-frontend/src/components/InfoTooltip/index.tsx
@@ -20,7 +20,7 @@
 import React from 'react';
 import { styled } from '@superset-ui/core';
 import { Tooltip } from 'src/components/Tooltip';
-import Icon from 'src/components/Icon';
+import Icons from 'src/components/Icons';
 
 export interface InfoTooltipProps {
   className?: string;
@@ -74,7 +74,7 @@ export default function InfoTooltip({
       overlayStyle={overlayStyle}
       color={bgColor}
     >
-      <Icon name="info-solid-small" />
+      <Icons.InfoSolidSmall />
     </StyledTooltip>
   );
 }
