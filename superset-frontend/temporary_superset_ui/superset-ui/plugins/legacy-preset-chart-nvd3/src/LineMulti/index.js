5a26e10cbdfdf85e53ed9aaafa9968d14b409b36
David Aaron Suddjian <1858430+suddjian@users.noreply.github.com>
2021-07-02 14:21:48 -0700

    chore: add/edit plugin descriptions (#1202)

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/legacy-preset-chart-nvd3/src/LineMulti/index.js
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/legacy-preset-chart-nvd3/src/LineMulti/index.js
@@ -24,7 +24,9 @@ import controlPanel from './controlPanel';
 const metadata = new ChartMetadata({
   category: t('Evolution'),
   credits: ['http://nvd3.org'],
-  description: '',
+  description: t(
+    'Visualize two different time series using the same x-axis time range. This chart is being deprecated and we recommend using the Mixed Timeseries Chart instead!',
+  ),
   name: t('Multiple Line Charts'),
   tags: [t('Multi-Variables'), t('Legacy'), t('nvd3')],
   thumbnail,
