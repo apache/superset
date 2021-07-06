e5aedb81ca78dbaa70d5a0620ef3848ceba197d2
Victor Malai <malai9696@gmail.com>
2021-07-06 10:58:45 +0300

    feat(plugin-chart-echarts): Add cross filter for timeseries (#1016)
    * Add cross filter for timeseries chart
    
    * fix types
    
    Co-authored-by: Ville Brofeldt <ville.v.brofeldt@gmail.com>

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/plugin-chart-echarts/src/Timeseries/EchartsTimeseries.tsx
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/plugin-chart-echarts/src/Timeseries/EchartsTimeseries.tsx
@@ -16,10 +16,88 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import React from 'react';
-import { EchartsProps } from '../types';
+import React, { useCallback } from 'react';
+import { EventHandlers } from '../types';
 import Echart from '../components/Echart';
+import { TimeseriesChartTransformedProps } from './types';
 
-export default function EchartsTimeseries({ height, width, echartOptions }: EchartsProps) {
-  return <Echart height={height} width={width} echartOptions={echartOptions} />;
+// @ts-ignore
+export default function EchartsTimeseries({
+  formData,
+  height,
+  width,
+  echartOptions,
+  groupby,
+  labelMap,
+  setDataMask,
+}: TimeseriesChartTransformedProps) {
+  const { filterState } = formData;
+  console.log(formData);
+  const handleChange = useCallback(
+    (values: string[]) => {
+      if (!formData.emitFilter) {
+        return;
+      }
+      const groupbyValues = values.map(value => labelMap[value]);
+
+      setDataMask({
+        extraFormData: {
+          filters:
+            values.length === 0
+              ? []
+              : groupby.map((col, idx) => {
+                  const val = groupbyValues.map(v => v[idx]);
+                  if (val === null || val === undefined)
+                    return {
+                      col,
+                      op: 'IS NULL',
+                    };
+                  return {
+                    col,
+                    op: 'IN',
+                    val: val as (string | number | boolean)[],
+                  };
+                }),
+        },
+        filterState: {
+          label: groupbyValues.length ? groupbyValues : undefined,
+          value: groupbyValues.length ? groupbyValues : null,
+        },
+      });
+    },
+    [groupby, labelMap, setDataMask],
+  );
+
+  const selectedValues = (filterState || []).reduce(
+    (acc: Record<string, number>, selectedValue: string) => {
+      const index = Object.keys(labelMap).indexOf(selectedValue);
+      return {
+        ...acc,
+        [index]: selectedValue,
+      };
+    },
+    {},
+  );
+
+  const eventHandlers: EventHandlers = {
+    click: props => {
+      const { seriesId } = props;
+      const values: string[] = Object.values(selectedValues);
+      if (values.includes(seriesId)) {
+        handleChange(values.filter(v => v !== seriesId));
+      } else {
+        handleChange([...values, seriesId]);
+      }
+    },
+  };
+
+  return (
+    <Echart
+      height={height}
+      width={width}
+      echartOptions={echartOptions}
+      eventHandlers={eventHandlers}
+      selectedValues={selectedValues}
+    />
+  );
 }
