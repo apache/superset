ee217960bc226bfb0720998ba0285fe3c7a4e08f
Beto Dealmeida <roberto@dealmeida.net>
2021-07-07 14:39:27 -0700

    Move import inside fixture

--- tests/unit_tests/conftest.py
+++ tests/unit_tests/conftest.py
@@ -17,14 +17,14 @@
 
 import pytest
 
-from superset.app import create_app
-
 
 @pytest.fixture
 def app_context():
     """
     A fixture for running the test inside an app context.
     """
+    from superset.app import create_app
+
     app = create_app()
     with app.app_context():
         yield
