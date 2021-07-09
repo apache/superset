6d52c44ab7438c674a982d7140b861d164a61fd9
Yaozong Liu <750188453@qq.com>
2021-07-09 11:10:55 +0800

    feat(plugin-chart-echarts): x-filtering improvement in radar chart (#1205)
    * fix(plugin-chart-echarts): x-filtering improvement for radar chart
    
    * fix(plugin-chart-echarts): opacity constants
    
    * fix(plugin-chart-echarts): improve constant
    
    * fix(plugin-chart-echarts): place treemap constant

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/plugin-chart-echarts/src/Treemap/constants.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/plugin-chart-echarts/src/Treemap/constants.ts
@@ -23,7 +23,6 @@ export const COLOR_SATURATION = [0.7, 0.4];
 export const LABEL_FONTSIZE = 11;
 export const BORDER_WIDTH = 2;
 export const GAP_WIDTH = 2;
-export const COLOR_ALPHA = 0.3;
 export const BORDER_COLOR = '#fff';
 
 export const extractTreePathInfo = (treePathInfo: TreePathInfo[] | undefined) => {
