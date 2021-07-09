6b06dc81ff5dd3da261f364c221ec1aa0b796ae8
lyndsiWilliams <kcatgirl@gmail.com>
2021-07-09 18:43:52 -0500

    Gsheets-specific info alert implemented

--- superset-frontend/src/views/CRUD/data/database/DatabaseModal/index.tsx
+++ superset-frontend/src/views/CRUD/data/database/DatabaseModal/index.tsx
@@ -1069,7 +1069,25 @@ const DatabaseModal: FunctionComponent<DatabaseModalProps> = ({
                   dbName={dbName}
                   dbModel={dbModel}
                 />
-                {connectionAlert && (
+                {db?.engine === 'gsheets' && (
+                  <StyledAlertMargin>
+                    <Alert
+                      closable={false}
+                      css={(theme: SupersetTheme) => antDAlertStyles(theme)}
+                      type="info"
+                      showIcon
+                      message={t('Why do I need to create a database?')}
+                      description={t(
+                        'To begin using your Google Sheets, you need to ' +
+                          'create a database first. Databases are used as ' +
+                          'a way to identify your data so that it can be queried ' +
+                          'and visualized. This database will hold all of your ' +
+                          'individual Google Sheets you choose to connect here.',
+                      )}
+                    />
+                  </StyledAlertMargin>
+                )}
+                {db?.engine !== 'gsheets' && connectionAlert && (
                   <StyledAlertMargin>
                     <Alert
                       closable={false}
