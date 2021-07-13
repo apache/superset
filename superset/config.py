a2103bba13a0a72144da13aa0831c556ff39d349
Saurabh <saurabhnigam999@gmail.com>
2021-07-13 23:39:50 +0530

    config fix

--- superset/config.py
+++ superset/config.py
@@ -1233,38 +1233,6 @@ DATASET_HEALTH_CHECK: Optional[Callable[["SqlaTable"], str]] = None
 SQLALCHEMY_DOCS_URL = "https://docs.sqlalchemy.org/en/13/core/engines.html"
 SQLALCHEMY_DISPLAY_TEXT = "SQLAlchemy docs"
 
-# -------------------------------------------------------------------
-# *                WARNING:  STOP EDITING  HERE                    *
-# -------------------------------------------------------------------
-# Don't add config values below this line since local configs won't be
-# able to override them.
-if CONFIG_PATH_ENV_VAR in os.environ:
-    # Explicitly import config module that is not necessarily in pythonpath; useful
-    # for case where app is being executed via pex.
-    try:
-        cfg_path = os.environ[CONFIG_PATH_ENV_VAR]
-        module = sys.modules[__name__]
-        override_conf = imp.load_source("superset_config", cfg_path)
-        for key in dir(override_conf):
-            if key.isupper():
-                setattr(module, key, getattr(override_conf, key))
-
-        print(f"Loaded your LOCAL configuration at [{cfg_path}]")
-    except Exception:
-        logger.exception(
-            "Failed to import config for %s=%s", CONFIG_PATH_ENV_VAR, cfg_path
-        )
-        raise
-elif importlib.util.find_spec("superset_config") and not is_test():
-    try:
-        import superset_config  # pylint: disable=import-error
-        from superset_config import *  # type: ignore # pylint: disable=import-error,wildcard-import,unused-wildcard-import
-
-        print(f"Loaded your LOCAL configuration at [{superset_config.__file__}]")
-    except Exception:
-        logger.exception("Found but failed to import local superset_config")
-        raise
-
 
 
 from flask import  request
@@ -1415,3 +1383,37 @@ JINJA_CONTEXT_ADDONS = {
     'uri_filter': uri_filter
 
 }
+
+
+
+# -------------------------------------------------------------------
+# *                WARNING:  STOP EDITING  HERE                    *
+# -------------------------------------------------------------------
+# Don't add config values below this line since local configs won't be
+# able to override them.
+if CONFIG_PATH_ENV_VAR in os.environ:
+    # Explicitly import config module that is not necessarily in pythonpath; useful
+    # for case where app is being executed via pex.
+    try:
+        cfg_path = os.environ[CONFIG_PATH_ENV_VAR]
+        module = sys.modules[__name__]
+        override_conf = imp.load_source("superset_config", cfg_path)
+        for key in dir(override_conf):
+            if key.isupper():
+                setattr(module, key, getattr(override_conf, key))
+
+        print(f"Loaded your LOCAL configuration at [{cfg_path}]")
+    except Exception:
+        logger.exception(
+            "Failed to import config for %s=%s", CONFIG_PATH_ENV_VAR, cfg_path
+        )
+        raise
+elif importlib.util.find_spec("superset_config") and not is_test():
+    try:
+        import superset_config  # pylint: disable=import-error
+        from superset_config import *  # type: ignore # pylint: disable=import-error,wildcard-import,unused-wildcard-import
+
+        print(f"Loaded your LOCAL configuration at [{superset_config.__file__}]")
+    except Exception:
+        logger.exception("Found but failed to import local superset_config")
+        raise
