e1149886288aed008a620ac1f572ab9e7deb82e4
Lyndsi Kay Williams <55605634+lyndsiWilliams@users.noreply.github.com>
2021-07-16 11:16:08 -0500

    style: Rough draft of email report modal (#15666)
    * clears errors when closing out of modal (#15623)
    
    * fix: avoid fetching favorite status for anonymous user (#15590)
    
    * avoid fetching favorite status for anonymous user
    
    * add test + fix types
    
    * fix lint errors
    
    * Building ReportModal component
    
    * Continued ReportModal creation
    
    * Visual details updated
    
    * CronError style
    
    * Very basic testing added
    
    Co-authored-by: AAfghahi <48933336+AAfghahi@users.noreply.github.com>
    Co-authored-by: aspedrosa <aspedrosa@ua.pt>

--- superset-frontend/src/dashboard/components/Header/index.jsx
+++ superset-frontend/src/dashboard/components/Header/index.jsx
@@ -528,6 +528,20 @@ class Header extends React.PureComponent {
             </>
           )}
 
+          {!editMode && (
+            <>
+              <span
+                role="button"
+                title={t('Schedule email report')}
+                tabIndex={0}
+                className="action-button"
+                onClick={this.showReportModal}
+              >
+                <Icon name="calendar" />
+              </span>
+            </>
+          )}
+
           {this.state.showingPropertiesModal && (
             <PropertiesModal
               dashboardId={dashboardInfo.id}
