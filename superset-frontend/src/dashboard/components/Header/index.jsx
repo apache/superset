b62d63d953cc808192475b189f5ccae2c454b7f4
Arash <arash.afghahi@gmail.com>
2021-07-19 11:53:03 -0400

    first draft

--- superset-frontend/src/dashboard/components/Header/index.jsx
+++ superset-frontend/src/dashboard/components/Header/index.jsx
@@ -540,20 +540,6 @@ class Header extends React.PureComponent {
             </>
           )}
 
-          {!editMode && (
-            <>
-              <span
-                role="button"
-                title={t('Schedule email report')}
-                tabIndex={0}
-                className="action-button"
-                onClick={this.showReportModal}
-              >
-                <Icon name="calendar" />
-              </span>
-            </>
-          )}
-
           {this.state.showingPropertiesModal && (
             <PropertiesModal
               dashboardId={dashboardInfo.id}
