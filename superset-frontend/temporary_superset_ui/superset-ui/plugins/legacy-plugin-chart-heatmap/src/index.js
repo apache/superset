1c4b2aed45fdf6d5333a481cf5c475bad9c894d4
Yaozong Liu <750188453@qq.com>
2021-07-24 17:03:22 +0800

    chore: three charts to the correct category (#1235)

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/legacy-plugin-chart-heatmap/src/index.js
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/legacy-plugin-chart-heatmap/src/index.js
@@ -36,7 +36,14 @@ const metadata = new ChartMetadata({
     { url: employment, caption: t('Employment and education') },
   ],
   name: t('Heatmap'),
-  tags: [t('Business'), t('Intensity'), t('Legacy'), t('Highly-used'), t('Predictive')],
+  tags: [
+    t('Business'),
+    t('Intensity'),
+    t('Legacy'),
+    t('Density'),
+    t('Predictive'),
+    t('Single Metric'),
+  ],
   thumbnail,
   useLegacyApi: true,
 });
