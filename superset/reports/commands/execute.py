13b5a0e34b806994c80279672fcfac815996b9f9
Beto Dealmeida <roberto@dealmeida.net>
2021-07-26 15:40:38 -0700

    Change post-processing to use new endpoint

--- superset/reports/commands/execute.py
+++ superset/reports/commands/execute.py
@@ -152,6 +152,7 @@ class BaseReportState:
                     "ChartRestApi.get_data",
                     pk=self._report_schedule.chart_id,
                     format="csv",
+                    type="post_processed",
                 )
             return get_url_path(
                 "Superset.slice",
