a109dc2313820c366adddf8fe275d69805511e48
Ville Brofeldt <33317356+villebro@users.noreply.github.com>
2021-08-03 14:58:03 +0300

    feat(plugin-chart-pivot-table): add automatic conditional formatter (#1264)

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-chart-controls/src/types.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-chart-controls/src/types.ts
@@ -325,6 +325,7 @@ export type SectionOverrides = {
 // Ref:
 //  - superset-frontend/src/explore/components/ConditionalFormattingControl.tsx
 export enum COMPARATOR {
+  NONE = 'None',
   GREATER_THAN = '>',
   LESS_THAN = '<',
   GREATER_OR_EQUAL = '≥',
