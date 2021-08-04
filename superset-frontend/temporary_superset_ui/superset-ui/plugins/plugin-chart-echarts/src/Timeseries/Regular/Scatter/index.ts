74410090986867b9e3e895954725d24783fdfd84
Yaozong Liu <750188453@qq.com>
2021-08-04 21:43:20 +0800

    feat(plugin-chart-echarts): create separate entry points for timeseries (2nd) (#1262)
    * feat(plugin-chart-pivot-table): add sort by result to data pane
    
    * Use getMetricLabel helper function
    
    * feat(plugin-chart-echarts): create separate entry points for timeseries
    
    * Mark nvd3 area chart as deprecated
    
    * chore: improve metadata for ts charts
    
    * chore: rename
    
    * replace step charts with single entrypoint
    
    * add examples to remaining plugins
    
    * Rearrange tags
    
    Co-authored-by: Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
    Co-authored-by: Ville Brofeldt <ville.v.brofeldt@gmail.com>

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/plugin-chart-echarts/src/Timeseries/Regular/Scatter/index.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/plugin-chart-echarts/src/Timeseries/Regular/Scatter/index.ts
@@ -17,58 +17,60 @@
  * under the License.
  */
 import { t, ChartMetadata, ChartPlugin, AnnotationType, Behavior } from '@superset-ui/core';
-import buildQuery from './buildQuery';
-import controlPanel from './controlPanel';
-import transformProps from './transformProps';
+import buildQuery from '../../buildQuery';
+import controlPanel from '../controlPanel';
+import transformProps from '../../transformProps';
 import thumbnail from './images/thumbnail.png';
-import { EchartsTimeseriesChartProps, EchartsTimeseriesFormData } from './types';
-import example from './images/Time-series_Chart.jpg';
+import {
+  EchartsTimeseriesChartProps,
+  EchartsTimeseriesFormData,
+  EchartsTimeseriesSeriesType,
+} from '../../types';
+import example1 from './images/Scatter1.png';
 
-export default class EchartsTimeseriesChartPlugin extends ChartPlugin<
+const scatterTransformProps = (chartProps: EchartsTimeseriesChartProps) =>
+  transformProps({
+    ...chartProps,
+    formData: { ...chartProps.formData, seriesType: EchartsTimeseriesSeriesType.Scatter },
+  });
+
+export default class EchartsTimeseriesScatterChartPlugin extends ChartPlugin<
   EchartsTimeseriesFormData,
   EchartsTimeseriesChartProps
 > {
-  /**
-   * The constructor is used to pass relevant metadata and callbacks that get
-   * registered in respective registries that are used throughout the library
-   * and application. A more thorough description of each property is given in
-   * the respective imported file.
-   *
-   * It is worth noting that `buildQuery` and is optional, and only needed for
-   * advanced visualizations that require either post processing operations
-   * (pivoting, rolling aggregations, sorting etc) or submitting multiple queries.
-   */
   constructor() {
     super({
       buildQuery,
       controlPanel,
-      loadChart: () => import('./EchartsTimeseries'),
+      loadChart: () => import('../../EchartsTimeseries'),
       metadata: new ChartMetadata({
         behaviors: [Behavior.INTERACTIVE_CHART],
         category: t('Evolution'),
         credits: ['https://echarts.apache.org'],
         description: t(
-          'Swiss army knife for visualizing time series data. Choose between  step, line, scatter, and bar charts. This viz type has many customization options as well.',
+          'Time-series Scatter Plot has time on the horizontal axis in linear units, and the points are connected in order. It shows a statistical relationship between two variables.',
         ),
-        exampleGallery: [{ url: example }],
+        exampleGallery: [{ url: example1 }],
         supportedAnnotationTypes: [
           AnnotationType.Event,
           AnnotationType.Formula,
           AnnotationType.Interval,
           AnnotationType.Timeseries,
         ],
-        name: t('Time-series Chart'),
+        name: t('Time-series Scatter Plot'),
         tags: [
+          t('ECharts'),
+          t('Predictive'),
           t('Advanced-Analytics'),
           t('Aesthetic'),
-          t('Line'),
-          t('Predictive'),
           t('Time'),
           t('Transformable'),
+          t('Scatter'),
+          t('Highly-used'),
         ],
         thumbnail,
       }),
-      transformProps,
+      transformProps: scatterTransformProps,
     });
   }
 }
