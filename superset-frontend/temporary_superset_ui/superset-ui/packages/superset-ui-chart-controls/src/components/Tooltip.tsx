462496b0a54a75cdadaa0a09d07f38633e73bf29
Geido <60598000+geido@users.noreply.github.com>
2021-08-10 13:14:55 +0200

    fix: Hide Safari default tooltip (#1283)
    * Hide Safari default tooltip
    
    * Fix lint
    
    * Add emotion dependency
    
    * Fix package.json

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-chart-controls/src/components/Tooltip.tsx
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-chart-controls/src/components/Tooltip.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { useTheme } from '@superset-ui/core';
+import { useTheme, css } from '@superset-ui/core';
 import { Tooltip as BaseTooltip } from 'antd';
 import { TooltipProps } from 'antd/lib/tooltip';
+import { Global } from '@emotion/react';
 
 export { TooltipProps } from 'antd/lib/tooltip';
 
@@ -9,11 +10,25 @@ export const Tooltip = ({ overlayStyle, color, ...props }: TooltipProps) => {
   const theme = useTheme();
   const defaultColor = `${theme.colors.grayscale.dark2}e6`;
   return (
-    <BaseTooltip
-      overlayStyle={{ fontSize: theme.typography.sizes.s, lineHeight: '1.6', ...overlayStyle }}
-      color={defaultColor || color}
-      {...props}
-    />
+    <>
+      {/* Safari hack to hide browser default tooltips */}
+      <Global
+        styles={css`
+          .ant-tooltip-open {
+            display: inline-block;
+            &::after {
+              content: '';
+              display: block;
+            }
+          }
+        `}
+      />
+      <BaseTooltip
+        overlayStyle={{ fontSize: theme.typography.sizes.s, lineHeight: '1.6', ...overlayStyle }}
+        color={defaultColor || color}
+        {...props}
+      />
+    </>
   );
 };
 
