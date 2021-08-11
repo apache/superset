41174074363c539ed7ed72c649f058fbc102eda6
geido <diegopucci.me@gmail.com>
2021-08-11 11:20:12 +0200

    Show/hide tooltips

--- superset-frontend/src/explore/components/DatasourcePanel/DatasourcePanelDragOption/DatasourcePanelDragOption.test.tsx
+++ superset-frontend/src/explore/components/DatasourcePanel/DatasourcePanelDragOption/DatasourcePanelDragOption.test.tsx
@@ -21,37 +21,33 @@ import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { render, screen } from 'spec/helpers/testing-library';
 import { DndItemType } from 'src/explore/components/DndItemType';
-import DatasourcePanelDragWrapper from '.';
+import DatasourcePanelDragOption from '.';
 
 test('should render', () => {
   render(
     <DndProvider backend={HTML5Backend}>
-      <DatasourcePanelDragWrapper
+      <DatasourcePanelDragOption
         value={{ metric_name: 'test' }}
         type={DndItemType.Metric}
-      >
-        <div data-test="children" />
-      </DatasourcePanelDragWrapper>
+      />
     </DndProvider>,
   );
 
-  expect(screen.getByTestId('DatasourcePanelDragWrapper')).toBeInTheDocument();
-  expect(screen.getByTestId('children')).toBeInTheDocument();
+  expect(screen.getByTestId('DatasourcePanelDragOption')).toBeInTheDocument();
+  expect(screen.getByText('test')).toBeInTheDocument();
 });
 
 test('should have attribute draggable:true', () => {
   render(
     <DndProvider backend={HTML5Backend}>
-      <DatasourcePanelDragWrapper
+      <DatasourcePanelDragOption
         value={{ metric_name: 'test' }}
         type={DndItemType.Metric}
-      >
-        <div data-test="children" />
-      </DatasourcePanelDragWrapper>
+      />
     </DndProvider>,
   );
 
-  expect(screen.getByTestId('DatasourcePanelDragWrapper')).toHaveAttribute(
+  expect(screen.getByTestId('DatasourcePanelDragOption')).toHaveAttribute(
     'draggable',
     'true',
   );
