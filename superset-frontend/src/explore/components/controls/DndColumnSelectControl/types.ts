c178bbddcbbc4f9282e1da8fe46a352223949a71
Geido <60598000+geido@users.noreply.github.com>
2021-08-12 19:48:16 +0200

    fix(Explore): Show the tooltip only when label does not fit the container in METRICS/FILTERS/GROUP BY/SORT BY of the DATA panel (#16060)
    * Implement dynamic tooltip
    
    * Normalize and consolidate
    
    * Clean up
    
    * Refactor and clean up
    
    * Remove unnecessary var
    
    * Fix type import
    
    * Update superset-frontend/src/explore/components/controls/OptionControls/index.tsx
    
    Co-authored-by: Michael S. Molina <70410625+michael-s-molina@users.noreply.github.com>
    
    * Remove unnecessary styled span
    
    * Show full tooltip title
    
    * Force show tooltip
    
    * Force show tooltip D&D off
    
    Co-authored-by: Ville Brofeldt <ville.v.brofeldt@gmail.com>
    Co-authored-by: Michael S. Molina <70410625+michael-s-molina@users.noreply.github.com>
    (cherry picked from commit a1e18ed11079d9d06a141d537f01ad1c31eb6a48)

--- superset-frontend/src/explore/components/controls/DndColumnSelectControl/types.ts
+++ superset-frontend/src/explore/components/controls/DndColumnSelectControl/types.ts
@@ -23,8 +23,11 @@ import { DatasourcePanelDndItem } from '../../DatasourcePanel/types';
 import { DndItemType } from '../../DndItemType';
 
 export interface OptionProps {
-  children: ReactNode;
+  children?: ReactNode;
   index: number;
+  label?: string;
+  tooltipTitle?: string;
+  column?: ColumnMeta;
   clickClose: (index: number) => void;
   withCaret?: boolean;
   isExtra?: boolean;
