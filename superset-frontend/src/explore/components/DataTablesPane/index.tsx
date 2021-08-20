c5bae17686dfe08ce645327fe17320d1faf93094
AAfghahi <48933336+AAfghahi@users.noreply.github.com>
2021-08-20 18:04:57 -0400

    continued refactoring (#16377)

--- superset-frontend/src/explore/components/DataTablesPane/index.tsx
+++ superset-frontend/src/explore/components/DataTablesPane/index.tsx
@@ -281,7 +281,6 @@ export const DataTablesPane = ({
     },
     [queryFormData, columnNames],
   );
-  console.log(queryFormData);
   useEffect(() => {
     setInLocalStorage(STORAGE_KEYS.isOpen, panelOpen);
   }, [panelOpen]);
