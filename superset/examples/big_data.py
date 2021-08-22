d31551ee6e428f76e4965dcd74406f975f2e1fdc
hughhhh <hughmil3s@gmail.com>
2021-08-22 15:27:38 -0700

    add back commented out suff

--- superset/examples/big_data.py
+++ superset/examples/big_data.py
@@ -35,47 +35,50 @@ COLUMN_TYPES = [
 
 
 def load_big_data() -> None:
-    # print("Creating table `wide_table` with 100 columns")
-    # columns: List[ColumnInfo] = []
-    # for i in range(100):
-    #     column: ColumnInfo = {
-    #         "name": f"col{i}",
-    #         "type": COLUMN_TYPES[i % len(COLUMN_TYPES)],
-    #         "nullable": False,
-    #         "default": None,
-    #         "autoincrement": "auto",
-    #         "primary_key": 1 if i == 0 else 0,
-    #     }
-    #     columns.append(column)
-    # add_data(columns=columns, num_rows=1000, table_name="wide_table")
+    print("Creating table `wide_table` with 100 columns")
+    columns: List[ColumnInfo] = []
+    for i in range(100):
+        column: ColumnInfo = {
+            "name": f"col{i}",
+            "type": COLUMN_TYPES[i % len(COLUMN_TYPES)],
+            "nullable": False,
+            "default": None,
+            "autoincrement": "auto",
+            "primary_key": 1 if i == 0 else 0,
+            "group_by": False,
+        }
+        columns.append(column)
+    add_data(columns=columns, num_rows=1000, table_name="wide_table")
 
-    # print("Creating 1000 small tables")
-    # columns = [
-    #     {
-    #         "name": "id",
-    #         "type": sqlalchemy.sql.sqltypes.INTEGER(),
-    #         "nullable": False,
-    #         "default": None,
-    #         "autoincrement": "auto",
-    #         "primary_key": 1,
-    #     },
-    #     {
-    #         "name": "value",
-    #         "type": sqlalchemy.sql.sqltypes.VARCHAR(length=255),
-    #         "nullable": False,
-    #         "default": None,
-    #         "autoincrement": "auto",
-    #         "primary_key": 0,
-    #     },
-    # ]
-    # for i in range(1000):
-    #     add_data(columns=columns, num_rows=10, table_name=f"small_table_{i}")
+    print("Creating 1000 small tables")
+    columns = [
+        {
+            "name": "id",
+            "type": sqlalchemy.sql.sqltypes.INTEGER(),
+            "nullable": False,
+            "default": None,
+            "autoincrement": "auto",
+            "primary_key": 1,
+            "group_by": False,
+        },
+        {
+            "name": "value",
+            "type": sqlalchemy.sql.sqltypes.VARCHAR(length=255),
+            "nullable": False,
+            "default": None,
+            "autoincrement": "auto",
+            "primary_key": 0,
+            "group_by": False,
+        },
+    ]
+    for i in range(1000):
+        add_data(columns=columns, num_rows=10, table_name=f"small_table_{i}")
 
-    # print("Creating table with long name")
-    # name = "".join(random.choices(string.ascii_letters + string.digits, k=64))
-    # add_data(columns=columns, num_rows=10, table_name=name)
+    print("Creating table with long name")
+    name = "".join(random.choices(string.ascii_letters + string.digits, k=64))
+    add_data(columns=columns, num_rows=10, table_name=name)
 
-    columns: List[ColumnInfo] = [
+    columns = [
         {
             "name": "id",
             "type": sqlalchemy.sql.sqltypes.INTEGER(),
