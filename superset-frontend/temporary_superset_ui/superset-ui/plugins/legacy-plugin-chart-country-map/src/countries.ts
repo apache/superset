884b8ec098a664a259e1aa383698e007ca97dfa5
Ville Brofeldt <33317356+villebro@users.noreply.github.com>
2021-08-23 10:45:23 +0300

    feat(legacy-plugin-chart-country-map): add six new maps (#1327)
    * feat(legacy-plugin-chart-country-map): add six new maps
    
    * add svalbard
    
    * update notebook

--- superset-frontend/temporary_superset_ui/superset-ui/plugins/legacy-plugin-chart-country-map/src/countries.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/plugins/legacy-plugin-chart-country-map/src/countries.ts
@@ -6,10 +6,13 @@ import brazil from 'file-loader!./countries/brazil.geojson';
 import bulgaria from 'file-loader!./countries/bulgaria.geojson';
 import canada from 'file-loader!./countries/canada.geojson';
 import china from 'file-loader!./countries/china.geojson';
+import denmark from 'file-loader!./countries/denmark.geojson';
 import egypt from 'file-loader!./countries/egypt.geojson';
+import estonia from 'file-loader!./countries/estonia.geojson';
 import france from 'file-loader!./countries/france.geojson';
 import finland from 'file-loader!./countries/finland.geojson';
 import germany from 'file-loader!./countries/germany.geojson';
+import iceland from 'file-loader!./countries/iceland.geojson';
 import india from 'file-loader!./countries/india.geojson';
 import indonesia from 'file-loader!./countries/indonesia.geojson';
 import iran from 'file-loader!./countries/iran.geojson';
@@ -17,14 +20,17 @@ import italy from 'file-loader!./countries/italy.geojson';
 import japan from 'file-loader!./countries/japan.geojson';
 import korea from 'file-loader!./countries/korea.geojson';
 import liechtenstein from 'file-loader!./countries/liechtenstein.geojson';
+import norway from 'file-loader!./countries/norway.geojson';
 import malaysia from 'file-loader!./countries/malaysia.geojson';
 import morocco from 'file-loader!./countries/morocco.geojson';
 import myanmar from 'file-loader!./countries/myanmar.geojson';
 import netherlands from 'file-loader!./countries/netherlands.geojson';
+import poland from 'file-loader!./countries/poland.geojson';
 import portugal from 'file-loader!./countries/portugal.geojson';
 import russia from 'file-loader!./countries/russia.geojson';
 import singapore from 'file-loader!./countries/singapore.geojson';
 import slovenia from 'file-loader!./countries/slovenia.geojson';
+import sweden from 'file-loader!./countries/sweden.geojson';
 import spain from 'file-loader!./countries/spain.geojson';
 import switzerland from 'file-loader!./countries/switzerland.geojson';
 import syria from 'file-loader!./countries/syria.geojson';
@@ -44,10 +50,13 @@ export const countries = {
   bulgaria,
   canada,
   china,
+  denmark,
   egypt,
+  estonia,
   france,
   finland,
   germany,
+  iceland,
   india,
   indonesia,
   iran,
@@ -59,11 +68,14 @@ export const countries = {
   morocco,
   myanmar,
   netherlands,
+  norway,
+  poland,
   portugal,
   russia,
   singapore,
   slovenia,
   spain,
+  sweden,
   switzerland,
   syria,
   thailand,
