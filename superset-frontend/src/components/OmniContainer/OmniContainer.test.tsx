c14364c616e709b16774072504657caa857045dc
Geido <60598000+geido@users.noreply.github.com>
2021-08-24 11:52:07 +0300

    chore: Enhance Omnibar (#16273)
    * Fix style and implement ESC
    
    * Include ESC test case
    
    * Move pagination outside of table
    
    * Update superset-frontend/src/components/OmniContainer/OmniContainer.test.tsx
    
    Co-authored-by: Evan Rusackas <evan@preset.io>
    
    * Enhance
    
    * Handle close
    
    * Localize placeholder
    
    * Fix tests
    
    * Clear input on close
    
    * Destroy modal on close
    
    * Clean up
    
    * Fix tests
    
    Co-authored-by: Evan Rusackas <evan@preset.io>

--- superset-frontend/src/components/OmniContainer/OmniContainer.test.tsx
+++ superset-frontend/src/components/OmniContainer/OmniContainer.test.tsx
@@ -31,10 +31,9 @@ test('Do not open Omnibar with the featureflag disabled', () => {
   (isFeatureEnabled as jest.Mock).mockImplementation(
     (ff: string) => !(ff === 'OMNIBAR'),
   );
-  const logEvent = jest.fn();
   render(
     <div data-test="test">
-      <OmniContainer logEvent={logEvent} />
+      <OmniContainer />
     </div>,
   );
 
@@ -54,10 +53,9 @@ test('Open Omnibar with ctrl + k with featureflag enabled', () => {
   (isFeatureEnabled as jest.Mock).mockImplementation(
     (ff: string) => ff === 'OMNIBAR',
   );
-  const logEvent = jest.fn();
   render(
     <div data-test="test">
-      <OmniContainer logEvent={logEvent} />
+      <OmniContainer />
     </div>,
   );
 
@@ -81,17 +79,16 @@ test('Open Omnibar with ctrl + k with featureflag enabled', () => {
   });
   expect(
     screen.queryByPlaceholderText('Search all dashboards'),
-  ).not.toBeVisible();
+  ).not.toBeInTheDocument();
 });
 
 test('Open Omnibar with Command + k with featureflag enabled', () => {
   (isFeatureEnabled as jest.Mock).mockImplementation(
     (ff: string) => ff === 'OMNIBAR',
   );
-  const logEvent = jest.fn();
   render(
     <div data-test="test">
-      <OmniContainer logEvent={logEvent} />
+      <OmniContainer />
     </div>,
   );
 
@@ -115,17 +112,16 @@ test('Open Omnibar with Command + k with featureflag enabled', () => {
   });
   expect(
     screen.queryByPlaceholderText('Search all dashboards'),
-  ).not.toBeVisible();
+  ).not.toBeInTheDocument();
 });
 
 test('Open Omnibar with Cmd/Ctrl-K and close with ESC', () => {
   (isFeatureEnabled as jest.Mock).mockImplementation(
     (ff: string) => ff === 'OMNIBAR',
   );
-  const logEvent = jest.fn();
   render(
     <div data-test="test">
-      <OmniContainer logEvent={logEvent} />
+      <OmniContainer />
     </div>,
   );
 
@@ -149,5 +145,5 @@ test('Open Omnibar with Cmd/Ctrl-K and close with ESC', () => {
   });
   expect(
     screen.queryByPlaceholderText('Search all dashboards'),
-  ).not.toBeVisible();
+  ).not.toBeInTheDocument();
 });
