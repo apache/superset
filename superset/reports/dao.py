15c6ed2d729546634459c1637f9f1693d9cf7cd3
AAfghahi <48933336+AAfghahi@users.noreply.github.com>
2021-08-24 18:09:23 -0400

    Update superset/reports/dao.py
    Co-authored-by: Beto Dealmeida <roberto@dealmeida.net>

--- superset/reports/dao.py
+++ superset/reports/dao.py
@@ -128,6 +128,7 @@ class ReportScheduleDAO(BaseDAO):
 
         if query is not None and chart_id is not None:
             query = query.filter(ReportSchedule.chart_id == chart_id)
+
         return not db.session.query(query.exists()).scalar()
 
     @staticmethod
