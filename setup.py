13de2422d5d7b806481cced30209451f7ed11a9e
Daniel Wood <daniel@woods.us>
2021-08-30 21:48:51 -0700

    fix: Pin snowflake-sqlalchemy to 1.2.4 (#16515)
    * Pin snowflake-sqlalchemy to 1.2.4
    
    * lint files
    
    (cherry picked from commit ad8336a5b4acff0fb3dc1f9ec5d8954933ce9e6a)

--- setup.py
+++ setup.py
@@ -151,7 +151,9 @@ setup(
         "shillelagh": [
             "shillelagh[datasetteapi,gsheetsapi,socrata,weatherapi]>=1.0.3, <2"
         ],
-        "snowflake": ["snowflake-sqlalchemy>=1.2.3, <1.3"],
+        "snowflake": [
+            "snowflake-sqlalchemy>=1.2.4"
+        ],  # PINNED! 1.2.5 introduced breaking changes requiring sqlalchemy>=1.4.0
         "teradata": ["sqlalchemy-teradata==0.9.0.dev0"],
         "thumbnails": ["Pillow>=7.0.0, <8.0.0"],
         "vertica": ["sqlalchemy-vertica-python>=0.5.9, < 0.6"],
