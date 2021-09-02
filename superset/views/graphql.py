49e11bb6a3daeaab80697f2f63febb26725002d5
Arash <arash.afghahi@gmail.com>
2021-09-02 13:25:09 -0400

    added charts

--- superset/views/graphql.py
+++ superset/views/graphql.py
@@ -14,6 +14,8 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+from typing import Any, Dict
+
 from ariadne import (
     convert_kwargs_to_snake_case,
     graphql_sync,
@@ -28,11 +30,12 @@ from flask import jsonify, request
 from superset import app, db
 from superset.models.core import Database
 from superset.models.dashboard import Dashboard
+from superset.models.slice import Slice
 from superset.typing import FlaskResponse
 
 
 @convert_kwargs_to_snake_case
-def resolve_database(obj, info, database_id):
+def resolve_database(obj: Any, info: Any, database_id: int) -> Dict[str, Any]:
     database = db.session.query(Database).filter_by(id=database_id).one()
     try:
         payload = {
@@ -44,7 +47,8 @@ def resolve_database(obj, info, database_id):
     return payload
 
 
-def resolve_dashboardInfo(obj, info, dashboard_id):
+@convert_kwargs_to_snake_case
+def resolve_dashboard(obj: Any, info: Any, dashboard_id: int) -> Dict[str, Any]:
     dashboard = db.session.query(Dashboard).filter_by(id=dashboard_id).one()
     try:
         payload = {
@@ -57,23 +61,39 @@ def resolve_dashboardInfo(obj, info, dashboard_id):
     return payload
 
 
+@convert_kwargs_to_snake_case
+def resolve_chart(obj: Any, info: Any, chart_id: int) -> Dict[str, Any]:
+    chart = db.session.query(Slice).filter_by(id=chart_id).one()
+    try:
+        payload = {
+            "success": True,
+            "chart": chart.data,
+        }
+
+    except Exception as error:
+        payload = {"success": False, "errors": [str(error)]}
+    return payload
+
+
 query = ObjectType("Query")
 
 query.set_field("database", resolve_database)
 
-query.set_field("dashboardInfo", resolve_dashboardInfo)
+query.set_field("dashboard", resolve_dashboard)
+
+query.set_field("chart", resolve_chart)
 
 type_defs = load_schema_from_path("superset/views/schema.graphql")
 schema = make_executable_schema(type_defs, query, snake_case_fallback_resolvers)
 
 
 @app.route("/graphql", methods=["GET"])
-def graphql_playground():
+def graphql_playground() -> Any:
     return PLAYGROUND_HTML, 200
 
 
 @app.route("/graphql", methods=["POST"])
-def graphql_server():
+def graphql_server() -> Any:
     data = request.get_json()
 
     success, result = graphql_sync(schema, data, context_value=request, debug=app.debug)
