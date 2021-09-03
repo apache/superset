2d362777cb93f9484aec3eef4f1eb19e16c87680
lyndsiWilliams <kcatgirl@gmail.com>
2021-09-03 10:44:56 -0500

    RefreshIntervalModal_spec mostly converted to RTL

--- superset-frontend/spec/javascripts/dashboard/components/RefreshIntervalModal_spec.jsx
+++ superset-frontend/spec/javascripts/dashboard/components/RefreshIntervalModal_spec.jsx
@@ -18,47 +18,30 @@
  */
 import React from 'react';
 import { mount } from 'enzyme';
+import { render, screen } from 'spec/helpers/testing-library';
+import userEvent from '@testing-library/user-event';
+import fetchMock from 'fetch-mock';
 
 import ModalTrigger from 'src/components/ModalTrigger';
 import RefreshIntervalModal from 'src/dashboard/components/RefreshIntervalModal';
+import HeaderActionsDropdown from 'src/dashboard/components/Header/HeaderActionsDropdown';
 import Alert from 'src/components/Alert';
 import { supersetTheme, ThemeProvider } from '@superset-ui/core';
 
-const getMountWrapper = props =>
-  mount(<RefreshIntervalModal {...props} />, {
-    wrappingComponent: ThemeProvider,
-    wrappingComponentProps: {
-      theme: supersetTheme,
-    },
-  });
-
-describe('RefreshIntervalModal', () => {
+describe('RefreshIntervalModal - Enzyme', () => {
+  const getMountWrapper = props =>
+    mount(<RefreshIntervalModal {...props} />, {
+      wrappingComponent: ThemeProvider,
+      wrappingComponentProps: {
+        theme: supersetTheme,
+      },
+    });
   const mockedProps = {
     triggerNode: <i className="fa fa-edit" />,
     refreshFrequency: 10,
     onChange: jest.fn(),
     editMode: true,
   };
-  it('is valid', () => {
-    expect(
-      React.isValidElement(<RefreshIntervalModal {...mockedProps} />),
-    ).toBe(true);
-  });
-  it('renders the trigger node', () => {
-    const wrapper = getMountWrapper(mockedProps);
-    expect(wrapper.find('.fa-edit')).toExist();
-  });
-  it('should render a interval seconds', () => {
-    const wrapper = getMountWrapper(mockedProps);
-    expect(wrapper.prop('refreshFrequency')).toEqual(10);
-  });
-  it('should change refreshFrequency with edit mode', () => {
-    const wrapper = getMountWrapper(mockedProps);
-    wrapper.instance().handleFrequencyChange(30);
-    wrapper.instance().onSave();
-    expect(mockedProps.onChange).toHaveBeenCalled();
-    expect(mockedProps.onChange).toHaveBeenCalledWith(30, mockedProps.editMode);
-  });
   it('should show warning message', () => {
     const props = {
       ...mockedProps,
@@ -76,5 +59,177 @@ describe('RefreshIntervalModal', () => {
     wrapper.instance().handleFrequencyChange(3601);
     wrapper.update();
     expect(wrapper.find(ModalTrigger).find(Alert)).not.toExist();
+    wrapper.unmount();
+  });
+});
+
+const createProps = () => ({
+  addSuccessToast: jest.fn(),
+  addDangerToast: jest.fn(),
+  customCss: '#save-dash-split-button{margin-left: 100px;}',
+  dashboardId: 1,
+  dashboardInfo: {
+    id: 1,
+    dash_edit_perm: true,
+    dash_save_perm: true,
+    userId: '1',
+    metadata: {},
+    common: {
+      conf: {},
+    },
+  },
+  dashboardTitle: 'Title',
+  editMode: false,
+  expandedSlices: {},
+  forceRefreshAllCharts: jest.fn(),
+  hasUnsavedChanges: false,
+  isLoading: false,
+  layout: {},
+  dataMask: {},
+  onChange: jest.fn(),
+  onSave: jest.fn(),
+  refreshFrequency: 0,
+  setRefreshFrequency: jest.fn(),
+  shouldPersistRefreshFrequency: false,
+  showPropertiesModal: jest.fn(),
+  startPeriodicRender: jest.fn(),
+  updateCss: jest.fn(),
+  userCanEdit: false,
+  userCanSave: false,
+  userCanShare: false,
+  lastModifiedTime: 0,
+});
+
+const editModeOnProps = {
+  ...createProps(),
+  editMode: true,
+};
+
+const setup = () => (
+  <div className="dashboard-header">
+    <HeaderActionsDropdown {...editModeOnProps} />
+  </div>
+);
+
+fetchMock.get('glob:*/csstemplateasyncmodelview/api/read', {});
+
+const openRefreshIntervalModal = async () => {
+  const headerActionsButton = screen.getByRole('img', { name: 'more-horiz' });
+  userEvent.click(headerActionsButton);
+
+  const autoRefreshOption = screen.getByText('Set auto-refresh interval');
+  userEvent.click(autoRefreshOption);
+};
+
+const displayOptions = async () => {
+  // Click default refresh interval option to display other options
+  userEvent.click(screen.getByText(/don't refresh/i));
+};
+
+const defaultRefreshIntervalModalProps = {
+  triggerNode: <i className="fa fa-edit" />,
+  refreshFrequency: 0,
+  onChange: jest.fn(),
+  editMode: true,
+};
+
+describe('RefreshIntervalModal - RTL', () => {
+  it('is valid', () => {
+    expect(
+      React.isValidElement(
+        <RefreshIntervalModal {...defaultRefreshIntervalModalProps} />,
+      ),
+    ).toBe(true);
+  });
+
+  it('renders refresh interval modal', async () => {
+    render(setup(editModeOnProps));
+    await openRefreshIntervalModal();
+
+    // Assert that modal exists by checking for the modal title
+    expect(screen.getByText('Refresh interval')).toBeVisible();
+  });
+
+  it('renders refresh interval options', async () => {
+    render(setup(editModeOnProps));
+    await openRefreshIntervalModal();
+    await displayOptions();
+
+    // Assert that both "Don't refresh" instances exist
+    // - There will be two at this point, the default option and the dropdown option
+    const dontRefreshInstances = screen.getAllByText(/don't refresh/i);
+    expect(dontRefreshInstances).toHaveLength(2);
+    dontRefreshInstances.forEach(option => {
+      expect(option).toBeInTheDocument();
+    });
+
+    // Assert that all the other options exist
+    const options = [
+      screen.getByText(/10 seconds/i),
+      screen.getByText(/30 seconds/i),
+      screen.getByText(/1 minute/i),
+      screen.getByText(/5 minutes/i),
+      screen.getByText(/30 minutes/i),
+      screen.getByText(/1 hour/i),
+      screen.getByText(/6 hours/i),
+      screen.getByText(/12 hours/i),
+      screen.getByText(/24 hours/i),
+    ];
+    options.forEach(option => {
+      expect(option).toBeInTheDocument();
+    });
+  });
+
+  it('should change selected value', async () => {
+    render(setup(editModeOnProps));
+    await openRefreshIntervalModal();
+
+    // Initial selected value should be "Don't refresh"
+    const selectedValue = screen.getByText(/don't refresh/i);
+    expect(selectedValue.title).toMatch(/don't refresh/i);
+
+    // Display options and select "10 seconds"
+    await displayOptions();
+    userEvent.click(screen.getByText(/10 seconds/i));
+
+    // Selected value should now be "10 seconds"
+    expect(selectedValue.title).toMatch(/10 seconds/i);
+    expect(selectedValue.title).not.toMatch(/don't refresh/i);
+  });
+
+  it('should save a newly-selected value', async () => {
+    render(setup(editModeOnProps));
+    await openRefreshIntervalModal();
+    await displayOptions();
+
+    screen.logTestingPlaygroundURL();
+    // Select a new interval and click save
+    userEvent.click(screen.getByText(/10 seconds/i));
+    userEvent.click(screen.getByRole('button', { name: /save/i }));
+
+    expect(editModeOnProps.setRefreshFrequency).toHaveBeenCalled();
+    expect(editModeOnProps.setRefreshFrequency).toHaveBeenCalledWith(
+      10,
+      editModeOnProps.editMode,
+    );
+  });
+
+  it('should show warning message', async () => {
+    // TODO (lyndsiWilliams): This test is incomplete
+    const warningProps = {
+      ...editModeOnProps,
+      refreshLimit: 3600,
+      refreshWarning: 'Show warning',
+    };
+
+    render(setup(warningProps));
+    await openRefreshIntervalModal();
+    await displayOptions();
+
+    userEvent.click(screen.getByText(/30 seconds/i));
+    userEvent.click(screen.getByRole('button', { name: /save/i }));
+
+    // screen.debug(screen.getByRole('alert'));
+    expect.anything();
   });
 });
