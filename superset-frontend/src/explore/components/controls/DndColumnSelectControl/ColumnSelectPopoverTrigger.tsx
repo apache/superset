59c4cae99d2a06ad85a55ac88bec87de1c279506
Ville Brofeldt <ville.v.brofeldt@gmail.com>
2021-09-06 13:10:45 +0300

    fix imports

--- superset-frontend/src/explore/components/controls/DndColumnSelectControl/ColumnSelectPopoverTrigger.tsx
+++ superset-frontend/src/explore/components/controls/DndColumnSelectControl/ColumnSelectPopoverTrigger.tsx
@@ -17,8 +17,8 @@
  * under the License.
  */
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import { AdhocColumn, isAdhocColumn, isColumnMeta, t } from '@superset-ui/core';
-import { ColumnMeta } from '@superset-ui/chart-controls';
+import { AdhocColumn, t } from '@superset-ui/core';
+import { ColumnMeta, isAdhocColumn, isColumnMeta } from '@superset-ui/chart-controls';
 import Popover from 'src/components/Popover';
 import { ExplorePopoverContent } from 'src/explore/components/ExploreContentPopover';
 import ColumnSelectPopover from './ColumnSelectPopover';
