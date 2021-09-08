ac85a02c6dd9b3e771b9f76500c16ef83d0eb617
Jack Fragassi <jfragassi98@gmail.com>
2021-09-08 16:13:34 -0700

    Fix mock

--- tests/integration_tests/reports/scheduler_tests.py
+++ tests/integration_tests/reports/scheduler_tests.py
@@ -117,14 +117,14 @@ def test_scheduler_celery_no_timeout_utc(execute_mock):
         app.config["ALERT_REPORTS_WORKING_TIME_OUT_KILL"] = True
 
 
-@patch("superset.extensions.feature_flag_manager.is_feature_enabled")
+@patch("superset.tasks.scheduler.is_feature_enabled")
 @patch("superset.tasks.scheduler.execute.apply_async")
 def test_scheduler_feature_flag_off(execute_mock, is_feature_enabled):
     """
     Reports scheduler: Test scheduler with feature flag off
     """
-    is_feature_enabled("ALERT_REPORTS").return_value = False
     with app.app_context():
+        is_feature_enabled.return_value = False
         report_schedule = insert_report_schedule(
             type=ReportScheduleType.ALERT,
             name="report",
