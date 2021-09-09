18badd4aa95cba382996fbd5ebecad1d7f2d8441
Keith Wait <keith.w.wait@gmail.com>
2021-09-08 22:08:47 -0500

    adapt and merge POC to latest codebase

--- superset-frontend/src/explore/components/ExploreViewContainer.jsx
+++ superset-frontend/src/explore/components/ExploreViewContainer.jsx
@@ -618,7 +618,7 @@ function mapStateToProps(state) {
     forcedHeight: explore.forced_height,
     chart,
     timeout: explore.common.conf.SUPERSET_WEBSERVER_TIMEOUT,
-    ownState: dataMask[form_data.slice_id ?? 0]?.ownState, // 0 - unsaved chart
+    ownState: dataMask[form_data?.slice_id || 0]?.ownState, // 0 - unsaved chart
     impressionId,
     user: explore.user,
     reports,
