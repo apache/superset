ef08e74ec04efb6ddddf77857c0444c3c084c2b7
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2021-09-10 18:32:41 +0100

    Update superset/utils/feature_flag_manager.py
    Co-authored-by: Ville Brofeldt <33317356+villebro@users.noreply.github.com>

--- superset/utils/feature_flag_manager.py
+++ superset/utils/feature_flag_manager.py
@@ -36,7 +36,7 @@ class FeatureFlagManager:
     def get_feature_flags(self) -> Dict[str, Any]:
         if self._get_feature_flags_func:
             return self._get_feature_flags_func(deepcopy(self._feature_flags))
-        if self._is_feature_enabled_func:
+        if callable(self._is_feature_enabled_func):
             return dict(
                 map(
                     lambda kv: (kv[0], self._is_feature_enabled_func(kv[0], kv[1])),
