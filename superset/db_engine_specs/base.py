b2eb7b4c5aff918a9bd21c6b10f8699e8ca47f53
Ville Brofeldt <ville.v.brofeldt@gmail.com>
2021-09-10 11:03:45 +0300

    feat: add support for generic series limit

--- superset/db_engine_specs/base.py
+++ superset/db_engine_specs/base.py
@@ -272,6 +272,8 @@ class BaseEngineSpec:  # pylint: disable=too-many-public-methods
             GenericDataType.BOOLEAN,
         ),
     )
+
+    # Does database support join-free timeslot grouping
     time_groupby_inline = False
     limit_method = LimitMethod.FORCE_LIMIT
     time_secondary_columns = False
