39234f40727cc594733710138b7603151da5d0a5
John Bodley <4567245+john-bodley@users.noreply.github.com>
2021-09-13 13:22:19 -0700

    chore(pylint): Reenable too-many-lines check (#16287)
    Co-authored-by: John Bodley <john.bodley@airbnb.com>

--- superset/viz.py
+++ superset/viz.py
@@ -14,7 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-# pylint: disable=C,R,W,useless-suppression
+# pylint: disable=C,R,W,too-many-lines,useless-suppression
 """This module contains the 'Viz' objects
 
 These objects represent the backend of all the visualizations that
