1669947bcd429e926c6e8de5ceaafcfe5496b28b
John Bodley <4567245+john-bodley@users.noreply.github.com>
2021-09-13 13:22:19 -0700

    chore(pylint): Reenable too-many-lines check (#16287)
    Co-authored-by: John Bodley <john.bodley@airbnb.com>

--- superset/views/core.py
+++ superset/views/core.py
@@ -14,7 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-# pylint: disable=comparison-with-callable, line-too-long
+# pylint: disable=comparison-with-callable,line-too-long,too-many-lines
 from __future__ import annotations
 
 import logging
