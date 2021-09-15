e6c7a4020f728cd9eb3af9a1776f7bf3ca6400cf
John Bodley <4567245+john-bodley@users.noreply.github.com>
2021-09-15 09:30:23 -0700

    chore(pylint): Remove top-level disable (#16589)
    * chore(pylint): Remove top-level disable
    
    * Update examples.py
    
    * Update command.py
    
    Co-authored-by: John Bodley <john.bodley@airbnb.com>

--- tests/unit_tests/db_engine_specs/test_gsheets.py
+++ tests/unit_tests/db_engine_specs/test_gsheets.py
@@ -14,7 +14,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-# pylint: disable=unused-argument, invalid-name
 from flask.ctx import AppContext
 from pytest_mock import MockFixture
 
