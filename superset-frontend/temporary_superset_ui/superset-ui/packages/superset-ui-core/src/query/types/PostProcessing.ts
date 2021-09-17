13734f9cfc8cc13820f57082a6e76514a25e07d6
Yongjie Zhao <yongjie.zhao@gmail.com>
2021-09-17 08:48:07 +0100

    feat: add resample operator to advanced analytic (#1349)
    * chore: add resample operator to advanced analytic
    
    * wip
    
    * minor improvement
    
    * wip
    
    * fix UT
    
    * minor fix
    
    * udates
    
    * fix type

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-core/src/query/types/PostProcessing.ts
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-core/src/query/types/PostProcessing.ts
@@ -156,6 +156,16 @@ export interface PostProcessingSort {
   };
 }
 
+export interface PostProcessingResample {
+  operation: 'resample';
+  options: {
+    method: string;
+    rule: string;
+    fill_value?: number | null;
+    time_column: string;
+  };
+}
+
 /**
  * Parameters for chart data postprocessing.
  * See superset/utils/pandas_processing.py.
@@ -170,4 +180,5 @@ export type PostProcessingRule =
   | PostProcessingRolling
   | PostProcessingCum
   | PostProcessingCompare
-  | PostProcessingSort;
+  | PostProcessingSort
+  | PostProcessingResample;
