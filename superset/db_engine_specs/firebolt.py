5d9d581fc39cc85121c2a5c69178ca8c647ca3f3
apurva-sigmoid <89530372+apurva-sigmoid@users.noreply.github.com>
2021-09-23 18:48:59 +0530

    Adding time grain changes to firebolt.py

--- superset/db_engine_specs/firebolt.py
+++ superset/db_engine_specs/firebolt.py
@@ -6,3 +6,15 @@ class FireboltEngineSpec(BaseEngineSpec):
     engine = "firebolt"
     engine_name = "Firebolt"
     default_driver = "firebolt"
+    
+    _time_grain_expressions = {
+        None: "{col}",
+        "PT1S": "date_trunc('second', CAST({col} AS TIMESTAMP))",
+        "PT1M": "date_trunc('minute', CAST({col} AS TIMESTAMP))",
+        "PT1H": "date_trunc('hour', CAST({col} AS TIMESTAMP))",
+        "P1D": "date_trunc('day', CAST({col} AS TIMESTAMP))",
+        "P1W": "date_trunc('week', CAST({col} AS TIMESTAMP))",
+        "P1M": "date_trunc('month', CAST({col} AS TIMESTAMP))",
+        "P0.25Y": "date_trunc('quarter', CAST({col} AS TIMESTAMP))",
+        "P1Y": "date_trunc('year', CAST({col} AS TIMESTAMP))",
+    }
