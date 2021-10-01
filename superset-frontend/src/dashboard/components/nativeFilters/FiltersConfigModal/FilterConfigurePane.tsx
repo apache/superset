a7602fd809ca01f61d910d994f4998bd62a73fa7
Ajay Mancheery <ajay.m@outlook.com>
2021-10-01 11:15:58 -0400

    Fix tests, fix CI

--- superset-frontend/src/dashboard/components/nativeFilters/FiltersConfigModal/FilterConfigurePane.tsx
+++ superset-frontend/src/dashboard/components/nativeFilters/FiltersConfigModal/FilterConfigurePane.tsx
@@ -1,5 +1,22 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 import { styled } from '@superset-ui/core';
-import { curry } from 'lodash';
 import React from 'react';
 import FilterTitlePane from './FilterTitlePane';
 import { FilterRemoval } from './types';
@@ -51,7 +68,7 @@ const FiltureConfigurePane: React.FC<Props> = ({
           onChange={onChange}
           onEdit={onEdit}
           onRearrage={onRearrange}
-          onRemove={(id: string) => curry(onEdit)(id)('remove')}
+          onRemove={(id: string) => onEdit(id, 'remove')}
           removedFilters={removedFilters}
           restoreFilter={restoreFilter}
         />
