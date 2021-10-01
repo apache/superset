97ef8f68fb387625143686022ef452ee2f15f7b1
Yongjie Zhao <yongjie.zhao@gmail.com>
2021-10-01 12:53:21 +0800

    fix: replace absolute difference with difference in legacy charts

--- superset-frontend/src/explore/controlPanels/sections.tsx
+++ superset-frontend/src/explore/controlPanels/sections.tsx
@@ -233,13 +233,13 @@ export const NVD3TimeSeries: ControlPanelSectionConfig[] = [
             default: 'values',
             choices: [
               ['values', 'Actual values'],
-              ['absolute', 'Absolute difference'],
+              ['absolute', 'Difference'],
               ['percentage', 'Percentage change'],
               ['ratio', 'Ratio'],
             ],
             description: t(
               'How to display time shifts: as individual lines; as the ' +
-                'absolute difference between the main time series and each time shift; ' +
+                'difference between the main time series and each time shift; ' +
                 'as the percentage change; or as the ratio between series and time shifts.',
             ),
           },
