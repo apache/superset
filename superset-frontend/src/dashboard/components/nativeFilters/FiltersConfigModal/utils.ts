fac94d665332ab22fb83c9ff3bcb411f7fd03bb4
Ajay M <ajay.m@outlook.com>
2021-10-15 19:56:33 -0400

    feat(dashboard): Let users re-arrange native filters (#16154)
    * feat. flag: ON
    
    * refactor filter tabs
    
    * WIP
    
    * Drag and Rearrange, styling
    
    * refactoring dnd code
    
    * Add apache license header
    
    * Fix bug reported during PR review
    
    * Minor fix on remove
    
    * turn off filters
    
    * Fix status
    
    * Fix a test
    
    * Address PR comments
    
    * iSort fixes
    
    * Add type key to the new filters
    
    * Fix wrong attribute
    
    * indent
    
    * PR comments
    
    * PR comments
    
    * Fix failing tests
    
    * Styling
    
    * Fix remove filter
    
    * Fix the drag issue
    
    * Save works
    
    * fix
    
    * Write tests
    
    * Style changes
    
    * New Icon
    
    * Grab & Grabbing Cursor
    
    * Commented out code
    
    * Fix tests, fix CI
    
    * Fix failing tests
    
    * Fix test
    
    * Style fixes
    
    * portal nodes dependency
    
    * More style fixes
    
    * PR comments
    
    * add unique ids to collapse panels
    
    * Filter removal bug fixed
    
    * PR comments
    
    * Fix test warnings
    
    * delete filter tabs
    
    * Fix the breaking test
    
    * Fix warnings
    
    * Fix the weird bug on cancel
    
    * refactor
    
    * Fix broken scope

--- superset-frontend/src/dashboard/components/nativeFilters/FiltersConfigModal/utils.ts
+++ superset-frontend/src/dashboard/components/nativeFilters/FiltersConfigModal/utils.ts
@@ -19,8 +19,14 @@
 import { FormInstance } from 'antd/lib/form';
 import shortid from 'shortid';
 import { getInitialDataMask } from 'src/dataMask/reducer';
+
 import { t } from '@superset-ui/core';
-import { FilterRemoval, NativeFiltersForm } from './types';
+import {
+  FilterRemoval,
+  NativeFiltersForm,
+  FilterHierarchy,
+  FilterHierarchyNode,
+} from './types';
 import { Filter, FilterConfiguration, Target } from '../types';
 
 export const REMOVAL_DELAY_SECS = 5;
@@ -158,7 +164,49 @@ export const createHandleSave = (
 
   await saveForm(newFilterConfig);
 };
+export function buildFilterGroup(nodes: FilterHierarchyNode[]) {
+  const buildGroup = (
+    elementId: string,
+    nodeList: FilterHierarchyNode[],
+    found: string[],
+  ): string[] | null => {
+    const element = nodeList.find(el => el.id === elementId);
+    const didFind = found.includes(elementId);
+    let parent: string[] = [];
+    let children: string[] = [];
 
+    if (!element || didFind) {
+      return null;
+    }
+    found.push(elementId);
+    const { parentId } = element;
+
+    if (parentId) {
+      const parentArray = buildGroup(parentId, nodeList, found);
+      parent = parentArray ? parentArray.flat() : [];
+    }
+    const childrenArray = nodeList
+      .filter(el => el.parentId === elementId)
+      .map(el => buildGroup(el.id, nodeList, found));
+
+    if (childrenArray) {
+      children = childrenArray
+        ? (childrenArray.flat().filter(id => id) as string[])
+        : [];
+    }
+    return [...parent, elementId, ...children];
+  };
+  const rendered: string[] = [];
+  const group: string[][] = [];
+  for (let index = 0; index < nodes.length; index += 1) {
+    const element = nodes[index];
+    const subGroup = buildGroup(element.id, nodes, rendered);
+    if (subGroup) {
+      group.push(subGroup);
+    }
+  }
+  return group;
+}
 export const createHandleTabEdit = (
   setRemovedFilters: (
     value:
@@ -168,9 +216,25 @@ export const createHandleTabEdit = (
       | Record<string, FilterRemoval>,
   ) => void,
   setSaveAlertVisible: Function,
+  setOrderedFilters: (
+    val: string[][] | ((prevState: string[][]) => string[][]),
+  ) => void,
+  setFilterHierarchy: (
+    state: FilterHierarchy | ((prevState: FilterHierarchy) => FilterHierarchy),
+  ) => void,
   addFilter: Function,
+  filterHierarchy: FilterHierarchy,
 ) => (filterId: string, action: 'add' | 'remove') => {
   const completeFilterRemoval = (filterId: string) => {
+    const buildNewFilterHierarchy = (hierarchy: FilterHierarchy) =>
+      hierarchy
+        .filter(nativeFilter => nativeFilter.id !== filterId)
+        .map(nativeFilter => {
+          const didRemoveParent = nativeFilter.parentId === filterId;
+          return didRemoveParent
+            ? { ...nativeFilter, parentId: null }
+            : nativeFilter;
+        });
     // the filter state will actually stick around in the form,
     // and the filterConfig/newFilterIds, but we use removedFilters
     // to mark it as removed.
@@ -178,14 +242,39 @@ export const createHandleTabEdit = (
       ...removedFilters,
       [filterId]: { isPending: false },
     }));
+    // Remove the filter from the side tab and de-associate children
+    // in case we removed a parent.
+    setFilterHierarchy(prevFilterHierarchy =>
+      buildNewFilterHierarchy(prevFilterHierarchy),
+    );
+    setOrderedFilters((orderedFilters: string[][]) => {
+      const newOrder = [];
+      for (let index = 0; index < orderedFilters.length; index += 1) {
+        const doesGroupContainDeletedFilter =
+          orderedFilters[index].findIndex(id => id === filterId) >= 0;
+        // Rebuild just the group that contains deleted filter ID.
+        if (doesGroupContainDeletedFilter) {
+          const newGroups = buildFilterGroup(
+            buildNewFilterHierarchy(
+              filterHierarchy.filter(filter =>
+                orderedFilters[index].includes(filter.id),
+              ),
+            ),
+          );
+          newGroups.forEach(group => newOrder.push(group));
+        } else {
+          newOrder.push(orderedFilters[index]);
+        }
+      }
+      return newOrder;
+    });
   };
 
   if (action === 'remove') {
     // first set up the timer to completely remove it
-    const timerId = window.setTimeout(
-      () => completeFilterRemoval(filterId),
-      REMOVAL_DELAY_SECS * 1000,
-    );
+    const timerId = window.setTimeout(() => {
+      completeFilterRemoval(filterId);
+    }, REMOVAL_DELAY_SECS * 1000);
     // mark the filter state as "removal in progress"
     setRemovedFilters(removedFilters => ({
       ...removedFilters,
