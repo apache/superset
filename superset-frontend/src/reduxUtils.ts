d563e77b5e5b7bdc280520aee01b34a70fb66dcc
simcha90 <56388545+simcha90@users.noreply.github.com>
2021-10-17 09:56:54 +0300

    feat(Filter-sets): connect to api (#17055)
    * fix:fix get permission function
    
    * feat: filtersets new
    
    * feat: connect filter sets to api
    
    * lint: fix lint
    
    * doc: add comment

--- superset-frontend/src/reduxUtils.ts
+++ superset-frontend/src/reduxUtils.ts
@@ -19,7 +19,7 @@
 import shortid from 'shortid';
 import { compose } from 'redux';
 import persistState, { StorageAdapter } from 'redux-localstorage';
-import { isEqual, omitBy, isUndefined } from 'lodash';
+import { isEqual, omitBy, isUndefined, isNull } from 'lodash';
 
 export function addToObject(
   state: Record<string, any>,
@@ -177,13 +177,20 @@ export function areArraysShallowEqual(arr1: unknown[], arr2: unknown[]) {
 export function areObjectsEqual(
   obj1: any,
   obj2: any,
-  opts = { ignoreUndefined: false },
+  opts: {
+    ignoreUndefined?: boolean;
+    ignoreNull?: boolean;
+  } = { ignoreUndefined: false, ignoreNull: false },
 ) {
   let comp1 = obj1;
   let comp2 = obj2;
   if (opts.ignoreUndefined) {
-    comp1 = omitBy(obj1, isUndefined);
-    comp2 = omitBy(obj2, isUndefined);
+    comp1 = omitBy(comp1, isUndefined);
+    comp2 = omitBy(comp2, isUndefined);
+  }
+  if (opts.ignoreNull) {
+    comp1 = omitBy(comp1, isNull);
+    comp2 = omitBy(comp2, isNull);
   }
   return isEqual(comp1, comp2);
 }
