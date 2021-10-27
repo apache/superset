358373c0e96100db666f0785091fb012ccdca1e7
Keith Wait <keith@askstylo.com>
2021-10-26 20:34:08 -0500

    add support for stylo-pie-table visualization

--- superset-frontend/src/visualizations/presets/MainPreset.js
+++ superset-frontend/src/visualizations/presets/MainPreset.js
@@ -82,6 +82,7 @@ import {
 import { PivotTableChartPlugin as PivotTableChartPluginV2 } from '@superset-ui/plugin-chart-pivot-table';
 import FilterBoxChartPlugin from '../FilterBox/FilterBoxChartPlugin';
 import TimeTableChartPlugin from '../TimeTable/TimeTableChartPlugin';
+import { StyloPieTableChartPlugin } from '@superset-ui/plugin-chart-stylo-pie-table';
 
 export default class MainPreset extends Preset {
   constructor() {
@@ -166,6 +167,7 @@ export default class MainPreset extends Preset {
         new TimeColumnFilterPlugin().configure({ key: 'filter_timecolumn' }),
         new TimeGrainFilterPlugin().configure({ key: 'filter_timegrain' }),
         new EchartsTreeChartPlugin().configure({ key: 'tree_chart' }),
+	      new StyloPieTableChartPlugin().configure({ key: 'stylo-pie-table' }),
         ...experimentalplugins,
       ],
     });
