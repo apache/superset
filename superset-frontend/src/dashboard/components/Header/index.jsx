6151b5532d0df6e2d03c2b3fadc3b93ba2463ba2
AAfghahi <48933336+AAfghahi@users.noreply.github.com>
2021-10-28 16:39:14 -0400

    refactor: Arash/new state report (#16987)
    * code dry (#16358)
    
    * pexdax refactor (#16333)
    
    * refactor progress (#16339)
    
    * fix: Header Actions test refactor (#16336)
    
    * fixed tests
    
    * Update index.tsx
    
    Co-authored-by: Elizabeth Thompson <eschutho@gmail.com>
    
    * Fetch bug fixed (#16376)
    
    * continued refactoring (#16377)
    
    * refactor(reports): Arash/refactor reports (#16855)
    
    * pexdax refactor (#16333)
    
    * refactor progress (#16339)
    
    * fix: Header Actions test refactor (#16336)
    
    * fixed tests
    
    * Update index.tsx
    
    Co-authored-by: Elizabeth Thompson <eschutho@gmail.com>
    
    * code dry (#16358)
    
    * Fetch bug fixed (#16376)
    
    * continued refactoring (#16377)
    
    * refactor: Reports - ReportModal (#16622)
    
    * refactoring progress
    
    * removed consoles
    
    * Working, but with 2 fetches
    
    * report pickup
    
    Co-authored-by: Lyndsi Kay Williams <55605634+lyndsiWilliams@users.noreply.github.com>
    Co-authored-by: Elizabeth Thompson <eschutho@gmail.com>
    
    * refactor(reports):  Arash/again refactor reports (#16872)
    
    * pexdax refactor (#16333)
    
    * refactor progress (#16339)
    
    * fix: Header Actions test refactor (#16336)
    
    * fixed tests
    
    * Update index.tsx
    
    Co-authored-by: Elizabeth Thompson <eschutho@gmail.com>
    
    * code dry (#16358)
    
    * Fetch bug fixed (#16376)
    
    * continued refactoring (#16377)
    
    * refactor: Reports - ReportModal (#16622)
    
    * refactoring progress
    
    * removed consoles
    
    * Working, but with 2 fetches
    
    * it is still not working
    
    Co-authored-by: Lyndsi Kay Williams <55605634+lyndsiWilliams@users.noreply.github.com>
    Co-authored-by: Elizabeth Thompson <eschutho@gmail.com>
    
    * next changes
    
    Co-authored-by: Lyndsi Kay Williams <55605634+lyndsiWilliams@users.noreply.github.com>
    Co-authored-by: Elizabeth Thompson <eschutho@gmail.com>

--- superset-frontend/src/dashboard/components/Header/index.jsx
+++ superset-frontend/src/dashboard/components/Header/index.jsx
@@ -183,13 +183,6 @@ class Header extends React.PureComponent {
 >>>>>>> code dry (#16358)
   }
 
-  componentDidUpdate(prevProps) {
-    if (this.props.refreshFrequency !== prevProps.refreshFrequency) {
-      const { refreshFrequency } = this.props;
-      this.startPeriodicRender(refreshFrequency * 1000);
-    }
-  }
-
   UNSAFE_componentWillReceiveProps(nextProps) {
     if (
       UNDO_LIMIT - nextProps.undoLength <= 0 &&
@@ -593,6 +586,7 @@ class Header extends React.PureComponent {
                 </span>
               )}
               <HeaderReportActionsDropdown
+                key={dashboardInfo.id}
                 toggleActive={this.props.toggleActive}
                 deleteActiveReport={this.props.deleteActiveReport}
                 dashboardId={dashboardInfo.id}
