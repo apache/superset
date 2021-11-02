28b494c59724eb9b48e387c8a48c7a1b5cd741ca
Daman Arora <61474540+Damans227@users.noreply.github.com>
2021-11-02 14:51:16 -0400

    chore: migrate HiddenControl component from jsx to tsx (#17315)
    * migrate HiddenControl component from jsx to tsx
    
    * Update superset-frontend/src/explore/components/controls/HiddenControl.tsx
    
    Co-authored-by: Erik Ritter <erik.ritter@airbnb.com>
    
    * updating the type of value in the interface
    
    * Update superset-frontend/src/explore/components/controls/HiddenControl.tsx
    
    Co-authored-by: Erik Ritter <erik.ritter@airbnb.com>
    
    Co-authored-by: Erik Ritter <erik.ritter@airbnb.com>

--- superset-frontend/src/explore/components/controls/HiddenControl.tsx
+++ superset-frontend/src/explore/components/controls/HiddenControl.tsx
@@ -17,29 +17,14 @@
  * under the License.
  */
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Input } from 'src/common/components';
 
-const propTypes = {
-  onChange: PropTypes.func,
-  value: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number,
-    PropTypes.object,
-    PropTypes.bool,
-    PropTypes.array,
-    PropTypes.func,
-  ]),
-};
+interface HiddenControlsProps {
+  onChange: () => void;
+  value: string | number | readonly string[] | undefined;
+}
 
-const defaultProps = {
-  onChange: () => {},
-};
-
-export default function HiddenControl(props) {
+export default function HiddenControl(props: HiddenControlsProps) {
   // This wouldn't be necessary but might as well
   return <Input type="hidden" value={props.value} />;
 }
-
-HiddenControl.propTypes = propTypes;
-HiddenControl.defaultProps = defaultProps;
