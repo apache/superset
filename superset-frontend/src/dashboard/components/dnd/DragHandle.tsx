d8dbf0b772cf88a172da19f351e112c8f492f20d
Damans227 <61474540+Damans227@users.noreply.github.com>
2021-11-03 13:18:08 -0400

    Convert class based component to functional component

--- superset-frontend/src/dashboard/components/dnd/DragHandle.tsx
+++ superset-frontend/src/dashboard/components/dnd/DragHandle.tsx
@@ -25,30 +25,25 @@ interface DragHandleProps {
   dotCount: number;
 }
 
-export default class DragHandle extends React.PureComponent<DragHandleProps> {
-  static defaultProps = {
-    position: 'left',
-    innerRef: null,
-    dotCount: 8,
-  };
-
-  render() {
-    const { innerRef, position, dotCount } = this.props;
-    return (
-      <div
-        ref={innerRef}
-        className={cx(
-          'drag-handle',
-          position === 'left' && 'drag-handle--left',
-          position === 'top' && 'drag-handle--top',
-        )}
-      >
-        {Array(dotCount)
-          .fill(null)
-          .map((_, i) => (
-            <div key={`handle-dot-${i}`} className="drag-handle-dot" />
-          ))}
-      </div>
-    );
-  }
+export default function DragHandle({
+  position = 'left',
+  innerRef = null,
+  dotCount = 8,
+}: DragHandleProps) {
+  return (
+    <div
+      ref={innerRef}
+      className={cx(
+        'drag-handle',
+        position === 'left' && 'drag-handle--left',
+        position === 'top' && 'drag-handle--top',
+      )}
+    >
+      {Array(dotCount)
+        .fill(null)
+        .map((_, i) => (
+          <div key={`handle-dot-${i}`} className="drag-handle-dot" />
+        ))}
+    </div>
+  );
 }
