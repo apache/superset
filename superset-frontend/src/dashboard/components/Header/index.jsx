0943197a36ddac4aff0d89895ecb94674796f421
geido <diegopucci.me@gmail.com>
2021-11-05 14:28:40 +0000

    Show certification in detail page

--- superset-frontend/src/dashboard/components/Header/index.jsx
+++ superset-frontend/src/dashboard/components/Header/index.jsx
@@ -22,6 +22,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { styled, t } from '@superset-ui/core';
 import ButtonGroup from 'src/components/ButtonGroup';
+import CertifiedIcon from 'src/components/CertifiedIcon';
 
 import {
   LOG_ACTIONS_PERIODIC_RENDER_DASHBOARD,
@@ -492,6 +493,14 @@ class Header extends React.PureComponent {
         data-test-id={`${dashboardInfo.id}`}
       >
         <div className="dashboard-component-header header-large">
+          {dashboardInfo.certified_by && (
+            <>
+              <CertifiedIcon
+                certifiedBy={dashboardInfo.certified_by}
+                details={dashboardInfo.certification_details}
+              />{' '}
+            </>
+          )}
           <EditableTitle
             title={dashboardTitle}
             canEdit={userCanEdit && editMode}
@@ -612,7 +621,7 @@ class Header extends React.PureComponent {
                 dashboardInfoChanged({
                   slug: updates.slug,
                   metadata: JSON.parse(updates.jsonMetadata),
-                  certifiedBy: updates.certifiedBy,
+                  certified_by: updates.certifiedBy,
                   certification_details: updates.certificationDetails,
                 });
                 setColorSchemeAndUnsavedChanges(updates.colorScheme);
