3db9369cafb278090128b1607b32b6b3ed8a2a34
Yongjie Zhao <yongjie.zhao@gmail.com>
2021-11-09 10:39:04 +0000

    chore: bump @typescript-eslint/eslint-plugin 5.3.0 (#1457)
    * chore: bump eslint 5.3.0
    
    * fix lint

--- superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-demo/storybook/stories/plugins/plugin-chart-echarts/Tree/Stories.tsx
+++ superset-frontend/temporary_superset_ui/superset-ui/packages/superset-ui-demo/storybook/stories/plugins/plugin-chart-echarts/Tree/Stories.tsx
@@ -34,30 +34,28 @@ export default {
   decorators: [withKnobs, withResizableChartDemo],
 };
 
-export const Tree = ({ width, height }) => {
-  return (
-    <SuperChart
-      chartType="echarts-tree"
-      width={width}
-      height={height}
-      queriesData={[{ data }]}
-      formData={{
-        colorScheme: 'bnbColors',
-        datasource: '3__table',
-        granularity_sqla: 'ds',
-        metric: 'count',
-        id: select('Id Column', ['id_column', 'name_column', 'parent_column'], 'id_column'),
-        rootNodeId: text('Root Node', '1'),
-        parent: select('Parent Column', ['parent_column', 'id_column'], 'parent_column'),
-        name: select('Name Column', [null, 'name_column'], 'name_column'),
+export const Tree = ({ width, height }) => (
+  <SuperChart
+    chartType="echarts-tree"
+    width={width}
+    height={height}
+    queriesData={[{ data }]}
+    formData={{
+      colorScheme: 'bnbColors',
+      datasource: '3__table',
+      granularity_sqla: 'ds',
+      metric: 'count',
+      id: select('Id Column', ['id_column', 'name_column', 'parent_column'], 'id_column'),
+      rootNodeId: text('Root Node', '1'),
+      parent: select('Parent Column', ['parent_column', 'id_column'], 'parent_column'),
+      name: select('Name Column', [null, 'name_column'], 'name_column'),
 
-        position: select('Label Position', ['top', 'right', 'left', 'bottom'], 'top'),
-        layout: select('Tree Layout', ['orthogonal', 'radial'], 'orthogonal'),
-        orient: select('Orientation', ['LR', 'RL', 'TB', 'BT'], 'LR'),
-        emphasis: select('Emphasis', ['ancestor', 'descendant'], 'descendant'),
-        symbol: select('Symbol', ['emptyCircle', 'circle', 'rect', 'triangle'], 'circle'),
-        symbol_size: number('[Symbol Size', 7, { range: true, min: 5, max: 30, step: 2 }),
-      }}
-    />
-  );
-};
+      position: select('Label Position', ['top', 'right', 'left', 'bottom'], 'top'),
+      layout: select('Tree Layout', ['orthogonal', 'radial'], 'orthogonal'),
+      orient: select('Orientation', ['LR', 'RL', 'TB', 'BT'], 'LR'),
+      emphasis: select('Emphasis', ['ancestor', 'descendant'], 'descendant'),
+      symbol: select('Symbol', ['emptyCircle', 'circle', 'rect', 'triangle'], 'circle'),
+      symbol_size: number('[Symbol Size', 7, { range: true, min: 5, max: 30, step: 2 }),
+    }}
+  />
+);
