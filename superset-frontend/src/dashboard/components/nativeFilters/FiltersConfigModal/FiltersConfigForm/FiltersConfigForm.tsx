4803ab2b79f6bc97865dc83277b4d30278a03cd8
Matthew Houston <matthew.houston@trillianthealth.com>
2021-11-09 10:17:58 -0600

    Ignore invalid eslint errors regarding conditionally called hooks.

--- superset-frontend/src/dashboard/components/nativeFilters/FiltersConfigModal/FiltersConfigForm/FiltersConfigForm.tsx
+++ superset-frontend/src/dashboard/components/nativeFilters/FiltersConfigModal/FiltersConfigForm/FiltersConfigForm.tsx
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+/* eslint-disable react-hooks/rules-of-hooks */
 import {
   ColumnMeta,
   InfoTooltipWithTrigger,
