159b9e91ead611f6c04f95c40e16904e3ff41f7a
hughhhh <hughmil3s@gmail.com>
2021-11-11 14:57:06 -0500

    add introduction content

--- docs-v2/docusaurus.config.js
+++ docs-v2/docusaurus.config.js
@@ -53,7 +53,7 @@ const config = {
             type: 'doc',
             docId: 'intro',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Documentation',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
