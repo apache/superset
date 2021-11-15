c42ff7972f8879f83de807a411b3e5f84bab3d75
Elizabeth Thompson <eschutho@gmail.com>
2021-11-15 09:57:05 -0800

    use full resultType with csv download on chart in dashboard (#17431)
    (cherry picked from commit 71e3fa1bf350045da9a1c1e89443549c204a3516)

--- superset-frontend/src/dashboard/components/gridComponents/Chart.jsx
+++ superset-frontend/src/dashboard/components/gridComponents/Chart.jsx
@@ -242,7 +242,7 @@ export default class Chart extends React.Component {
       formData: isFullCSV
         ? { ...this.props.formData, row_limit: this.props.maxRows }
         : this.props.formData,
-      resultType: 'results',
+      resultType: 'full',
       resultFormat: 'csv',
     });
   }
