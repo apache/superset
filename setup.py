4f9f824d53139776c4e8476a333fa07318c00ea0
Ville Brofeldt <33317356+villebro@users.noreply.github.com>
2021-11-21 10:13:28 +0200

    chore(deps): bump cachelib from 0.1.1 to 0.4.1 (#17482)

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     install_requires=[
         "backoff>=1.8.0",
         "bleach>=3.0.2, <4.0.0",
-        "cachelib>=0.1.1,<0.2",
+        "cachelib>=0.4.1,<0.5",
         "celery>=4.3.0, <5.0.0, !=4.4.1",
         "click<8",
         "colorama",
