9fec0d26a9679dbef7500375fcaa19e8b8916172
Yongjie Zhao <yongjie.zhao@gmail.com>
2021-11-22 14:41:51 +0800

    refactor superset-generator
    fix refactor genertor lint error

--- superset-frontend/packages/generator-superset/test/package.test.js
+++ superset-frontend/packages/generator-superset/test/package.test.js
@@ -20,62 +20,41 @@
 const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
+const fs = require('fs-extra');
+const packageModule = require('../generators/package');
 
-describe('generator-superset:package', () => {
-  let dir;
-
-  beforeAll(() => {
-    dir = process.cwd();
-  });
-
-  /*
-   * Change working directory back to original working directory
-   * after the test has completed.
-   * yeoman tests switch to tmp directory and write files there.
-   * Usually this is fine for solo package.
-   * However, for a monorepo like this one,
-   * it made jest confuses with current directory
-   * (being in tmp directory instead of superset-ui root)
-   * and interferes with other tests in sibling packages
-   * that are run after the yeoman tests.
-   */
-  afterAll(() => {
-    process.chdir(dir);
-  });
-
-  describe('typescript', () => {
-    beforeAll(() =>
-      helpers
-        .run(path.join(__dirname, '../generators/package'))
-        .withPrompts({ name: 'my-package', language: 'typescript' })
-        .withOptions({ skipInstall: true }),
-    );
-
-    it('creates files', () => {
+test('generator-superset:package:creates typescript files', () =>
+  helpers
+    .run(packageModule)
+    .inTmpDir(function (dir) {
+      // `dir` is the path to the new temporary directory
+      fs.copySync(path.join(__dirname, '../generators/package'), dir);
+    })
+    .withPrompts({ name: 'my-package', language: 'typescript' })
+    .withOptions({ skipInstall: true })
+    .then(function () {
       assert.file([
         'package.json',
         'README.md',
         'src/index.ts',
         'test/index.test.ts',
       ]);
-    });
-  });
+    }));
 
-  describe('javascript', () => {
-    beforeAll(() =>
-      helpers
-        .run(path.join(__dirname, '../generators/package'))
-        .withPrompts({ name: 'my-package', language: 'javascript' })
-        .withOptions({ skipInstall: true }),
-    );
-
-    it('creates files', () => {
+test('generator-superset:package:creates javascript files', () =>
+  helpers
+    .run(packageModule)
+    .inTmpDir(function (dir) {
+      // `dir` is the path to the new temporary directory
+      fs.copySync(path.join(__dirname, '../generators/package'), dir);
+    })
+    .withPrompts({ name: 'my-package', language: 'javascript' })
+    .withOptions({ skipInstall: true })
+    .then(function () {
       assert.file([
         'package.json',
         'README.md',
         'src/index.js',
         'test/index.test.js',
       ]);
-    });
-  });
-});
+    }));
