773e476e6c385cb48c5a21b7cfe7b1a3c2961783
Yongjie Zhao <yongjie.zhao@gmail.com>
2021-11-22 14:41:51 +0800

    refactor superset-generator
    fix refactor genertor lint error

--- superset-frontend/packages/generator-superset/test/app.test.js
+++ superset-frontend/packages/generator-superset/test/app.test.js
@@ -17,43 +17,22 @@
  * under the License.
  */
 
-const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
+const appModule = require('../generators/app');
 
-describe('generator-superset:app', () => {
-  let dir;
-
-  beforeAll(() => {
-    dir = process.cwd();
-
-    return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({
+test('generator-superset:app:creates files', () =>
+  helpers
+    .run(appModule)
+    .withPrompts({
       subgenerator: 'package',
       name: 'my-package',
-    });
-  });
-
-  /*
-   * Change working directory back to original working directory
-   * after the test has completed.
-   * yeoman tests switch to tmp directory and write files there.
-   * Usually this is fine for solo package.
-   * However, for a monorepo like this one,
-   * it made jest confuses with current directory
-   * (being in tmp directory instead of superset-ui root)
-   * and interferes with other tests in sibling packages
-   * that are run after the yeoman tests.
-   */
-  afterAll(() => {
-    process.chdir(dir);
-  });
-
-  it('creates files', () => {
-    assert.file([
-      'package.json',
-      'README.md',
-      'src/index.ts',
-      'test/index.test.ts',
-    ]);
-  });
-});
+    })
+    .then(function () {
+      assert.file([
+        'package.json',
+        'README.md',
+        'src/index.ts',
+        'test/index.test.ts',
+      ]);
+    }));
