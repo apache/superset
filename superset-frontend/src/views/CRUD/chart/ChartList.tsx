2e29f36e7885b693a0af2cf8b0b8ca86d2970f64
Hugh A. Miles II <hughmil3s@gmail.com>
2021-11-25 20:10:41 -0800

    fix: RBAC for export for dashboard viewers (#17527)
    * set out export
    
    * update test
    
    * use default dataset
    
    * update test
    
    * these test work
    
    * fix test
    
    * update
    
    * fix
    
    * fix test
    
    * make the test better

--- superset-frontend/src/views/CRUD/chart/ChartList.tsx
+++ superset-frontend/src/views/CRUD/chart/ChartList.tsx
@@ -183,7 +183,7 @@ function ChartList(props: ChartListProps) {
   const canEdit = hasPerm('can_write');
   const canDelete = hasPerm('can_write');
   const canExport =
-    hasPerm('can_read') && isFeatureEnabled(FeatureFlag.VERSIONED_EXPORT);
+    hasPerm('can_export') && isFeatureEnabled(FeatureFlag.VERSIONED_EXPORT);
   const initialSort = [{ id: 'changed_on_delta_humanized', desc: true }];
 
   const handleBulkChartExport = (chartsToExport: Chart[]) => {
