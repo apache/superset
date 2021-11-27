7a06b2847f52bc21f6e104ca1530cf58bea0fa1c
Holger Bruch <hb@mfdz.de>
2021-11-27 13:50:33 +0100

    chore(translations): Add missing i18n (#17525)
    * Add *.ts to babel.cfg
    
    * Add missing i18n calls
    
    * Update pot file
    
    * Fix lint issues and review comment
    
    * Incorparate review feedback
    
    * Fix missing or non-extractable i18n calls
    
    * Update pot file
    
    * Fix syntax error in CommonParameters.tsx
    
    * Fix introduced issues

--- superset-frontend/src/views/CRUD/welcome/SavedQueries.tsx
+++ superset-frontend/src/views/CRUD/welcome/SavedQueries.tsx
@@ -284,7 +284,7 @@ const SavedQueries = ({
             name: (
               <>
                 <i className="fa fa-plus" />
-                SQL Query
+                {t('SQL Query')}
               </>
             ),
             buttonStyle: 'tertiary',
@@ -293,7 +293,7 @@ const SavedQueries = ({
             },
           },
           {
-            name: 'View All »',
+            name: t('View All »'),
             buttonStyle: 'link',
             onClick: () => {
               window.location.href = '/savedqueryview/list';
