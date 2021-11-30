c8e727dc5ed31d304423bc05002979c29579889d
Truong Trung Hieu <stephen@pop-os.localdomain>
2021-11-30 17:01:56 +0700

    dev: add trino oauth2 impersonate

--- superset/db_engine_specs/trino.py
+++ superset/db_engine_specs/trino.py
@@ -15,7 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 from datetime import datetime
-from typing import Any, Dict, List, Optional, TYPE_CHECKING
+from typing import Any, Dict, List, Optional
 from urllib import parse
 
 import simplejson as json
@@ -24,14 +24,12 @@ from sqlalchemy.engine.url import make_url, URL
 from superset.db_engine_specs.base import BaseEngineSpec
 from superset.utils import core as utils
 
-if TYPE_CHECKING:
-    from superset.models.core import Database
-
 
 class TrinoEngineSpec(BaseEngineSpec):
     engine = "trino"
     engine_name = "Trino"
 
+    # pylint: disable=line-too-long
     _time_grain_expressions = {
         None: "{col}",
         "PT1S": "date_trunc('second', CAST({col} AS TIMESTAMP))",
@@ -40,7 +38,7 @@ class TrinoEngineSpec(BaseEngineSpec):
         "P1D": "date_trunc('day', CAST({col} AS TIMESTAMP))",
         "P1W": "date_trunc('week', CAST({col} AS TIMESTAMP))",
         "P1M": "date_trunc('month', CAST({col} AS TIMESTAMP))",
-        "P3M": "date_trunc('quarter', CAST({col} AS TIMESTAMP))",
+        "P0.25Y": "date_trunc('quarter', CAST({col} AS TIMESTAMP))",
         "P1Y": "date_trunc('year', CAST({col} AS TIMESTAMP))",
         # "1969-12-28T00:00:00Z/P1W",  # Week starting Sunday
         # "1969-12-29T00:00:00Z/P1W",  # Week starting Monday
@@ -49,9 +47,7 @@ class TrinoEngineSpec(BaseEngineSpec):
     }
 
     @classmethod
-    def convert_dttm(
-        cls, target_type: str, dttm: datetime, db_extra: Optional[Dict[str, Any]] = None
-    ) -> Optional[str]:
+    def convert_dttm(cls, target_type: str, dttm: datetime) -> Optional[str]:
         tt = target_type.upper()
         if tt == utils.TemporalType.DATE:
             value = dttm.date().isoformat()
@@ -84,6 +80,7 @@ class TrinoEngineSpec(BaseEngineSpec):
         that can set the correct properties for impersonating users
         :param connect_args: config to be updated
         :param uri: URI string
+        :param impersonate_user: Flag indicating if impersonation is enabled
         :param username: Effective username
         :return: None
         """
@@ -113,12 +110,16 @@ class TrinoEngineSpec(BaseEngineSpec):
         return True
 
     @classmethod
-    def estimate_statement_cost(cls, statement: str, cursor: Any) -> Dict[str, Any]:
+    def estimate_statement_cost(  # pylint: disable=too-many-locals
+        cls, statement: str, cursor: Any
+    ) -> Dict[str, Any]:
         """
         Run a SQL query that estimates the cost of a given statement.
 
         :param statement: A single SQL statement
+        :param database: Database instance
         :param cursor: Cursor instance
+        :param username: Effective username
         :return: JSON response from Trino
         """
         sql = f"EXPLAIN (TYPE IO, FORMAT JSON) {statement}"
@@ -183,22 +184,3 @@ class TrinoEngineSpec(BaseEngineSpec):
             cost.append(statement_cost)
 
         return cost
-
-    @staticmethod
-    def get_extra_params(database: "Database") -> Dict[str, Any]:
-        """
-        Some databases require adding elements to connection parameters,
-        like passing certificates to `extra`. This can be done here.
-
-        :param database: database instance from which to extract extras
-        :raises CertificateException: If certificate is not valid/unparseable
-        """
-        extra: Dict[str, Any] = BaseEngineSpec.get_extra_params(database)
-        engine_params: Dict[str, Any] = extra.setdefault("engine_params", {})
-        connect_args: Dict[str, Any] = engine_params.setdefault("connect_args", {})
-
-        if database.server_cert:
-            connect_args["http_scheme"] = "https"
-            connect_args["verify"] = utils.create_ssl_cert_file(database.server_cert)
-
-        return extra
