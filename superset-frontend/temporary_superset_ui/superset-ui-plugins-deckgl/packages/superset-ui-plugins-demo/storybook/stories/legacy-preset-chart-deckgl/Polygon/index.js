fab90a27d1383e70616de6ad4b0fec9b5fc2040e
Yongjie Zhao <yongjie.zhao@gmail.com>
2021-11-30 15:09:17 +0800

    move deckgl

--- superset-frontend/temporary_superset_ui/superset-ui-plugins-deckgl/packages/superset-ui-plugins-demo/storybook/stories/legacy-preset-chart-deckgl/Polygon/index.js
+++
@@ -1,8 +0,0 @@
-import { PolygonChartPlugin } from '../../../../../superset-ui-legacy-preset-chart-deckgl';
-import Stories from './Stories';
-
-new PolygonChartPlugin().configure({ key: 'deck_polygon' }).register();
-
-export default {
-  examples: [...Stories],
-};
