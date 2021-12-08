08d4b9a270a4abdf2c5ab1729a0ca41338b66675
Ville Brofeldt <33317356+villebro@users.noreply.github.com>
2021-12-08 11:30:23 +0200

    feat: customize recent activity access (#17589)
    * feat: customize recent activity access
    
    * address comments
    
    * fix and add tests
    
    * add alert assertion and UPDATING.md entry
    
    * replace .get_id() with .id
    
    * fix updating comment
    
    * update config name

--- superset/config.py
+++ superset/config.py
@@ -1293,6 +1293,9 @@ MENU_HIDE_USER_INFO = False
 SQLALCHEMY_DOCS_URL = "https://docs.sqlalchemy.org/en/13/core/engines.html"
 SQLALCHEMY_DISPLAY_TEXT = "SQLAlchemy docs"
 
+# Set to False to only allow viewing own recent activity
+ENABLE_BROAD_ACTIVITY_ACCESS = True
+
 # -------------------------------------------------------------------
 # *                WARNING:  STOP EDITING  HERE                    *
 # -------------------------------------------------------------------
