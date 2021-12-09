42821f6c5e09288e39f255390bd3e67158ff7c2f
Yongjie Zhao <yongjie.zhao@gmail.com>
2021-12-09 08:45:18 +0800

    refactor(monorepo): relocate deckgl to Superset (#17596)

---
+++ superset-frontend/plugins/legacy-preset-chart-deckgl/src/layers/Grid/controlPanel.ts
@@ -0,0 +1,76 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import { ControlPanelConfig, sections } from '@superset-ui/chart-controls';
+import { t, validateNonEmpty } from '@superset-ui/core';
+import {
+  filterNulls,
+  autozoom,
+  jsColumns,
+  jsDataMutator,
+  jsTooltip,
+  jsOnclickHref,
+  extruded,
+  gridSize,
+  viewport,
+  spatial,
+  mapboxStyle,
+} from '../../utilities/Shared_DeckGL';
+
+const config: ControlPanelConfig = {
+  controlPanelSections: [
+    sections.legacyRegularTime,
+    {
+      label: t('Query'),
+      expanded: true,
+      controlSetRows: [
+        [spatial],
+        ['size'],
+        ['row_limit'],
+        [filterNulls],
+        ['adhoc_filters'],
+      ],
+    },
+    {
+      label: t('Map'),
+      controlSetRows: [
+        [mapboxStyle, viewport],
+        ['color_picker', autozoom],
+        [gridSize, extruded],
+      ],
+    },
+    {
+      label: t('Advanced'),
+      controlSetRows: [
+        [jsColumns],
+        [jsDataMutator],
+        [jsTooltip],
+        [jsOnclickHref],
+      ],
+    },
+  ],
+  controlOverrides: {
+    size: {
+      label: t('Height'),
+      description: t('Metric used to control height'),
+      validators: [validateNonEmpty],
+    },
+  },
+};
+
+export default config;
--- superset-frontend/temporary_superset_ui/superset-ui-plugins-deckgl/packages/superset-ui-plugins-demo/storybook/stories/legacy-preset-chart-deckgl/Scatter/index.js
+++
@@ -1,8 +0,0 @@
-import { ScatterChartPlugin } from '../../../../../superset-ui-legacy-preset-chart-deckgl';
-import Stories from './Stories';
-
-new ScatterChartPlugin().configure({ key: 'deck_scatter' }).register();
-
-export default {
-  examples: [...Stories],
-};
