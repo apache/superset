17668517117247abefb5f6ededb7c8c41295ad6e
Yongjie Zhao <yongjie.zhao@gmail.com>
2021-12-14 16:19:55 +0800

    refactor(monorepo): change coverage of core to 100% (#17698)

--- superset-frontend/jest.config.js
+++ superset-frontend/jest.config.js
@@ -56,9 +56,10 @@ module.exports = {
   coverageReporters: ['lcov', 'json-summary', 'html'],
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
-    // ts-jest can't load plugin 'babel-plugin-typescript-to-proptypes'
-    'reactify\\.tsx$': 'babel-jest',
-    '^.+\\.tsx?$': 'ts-jest',
+    // ts-jest doesn't work with `--coverage`. @superset-ui/core should
+    // 100% coverage, so we use babel-jest in packages and plugins.
+    '(plugins|packages)\\/.+\\.tsx?$': 'babel-jest',
+    '(((?!(plugins|packages)).)*)\\/.+\\.tsx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   snapshotSerializers: ['@emotion/jest/enzyme-serializer'],
