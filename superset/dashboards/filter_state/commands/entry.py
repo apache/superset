d6e3178e34c8be7aa2292d07ea3d9dda248e2b71
Ville Brofeldt <33317356+villebro@users.noreply.github.com>
2021-12-20 10:22:24 +0200

    fix: import typeddict from extensions (#17823)

--- superset/dashboards/filter_state/commands/entry.py
+++ superset/dashboards/filter_state/commands/entry.py
@@ -14,7 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-from typing import TypedDict
+from typing_extensions import TypedDict
 
 
 class Entry(TypedDict):
