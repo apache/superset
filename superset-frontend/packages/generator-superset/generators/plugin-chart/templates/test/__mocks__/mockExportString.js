431ffbbad63009aedf3ee04712afbe99cfcbfc32
Ville Brofeldt <33317356+villebro@users.noreply.github.com>
2021-12-21 11:44:21 +0200

    chore(generator-superset): migrate to monorepo (#17829)
    * chore(generator-superset): migrate to monorepo
    
    * add todo and remove webpack reference from template
    
    * fix linting errors
    
    * remove redundant test file

--- superset-frontend/packages/generator-superset/generators/plugin-chart/templates/test/__mocks__/mockExportString.js
+++ superset-frontend/packages/generator-superset/generators/plugin-chart/templates/test/__mocks__/mockExportString.js
@@ -16,19 +16,4 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import buildQuery from '../../src/plugin/buildQuery';
-
-describe('<%= packageLabel %> buildQuery', () => {
-  const formData = {
-    datasource: '5__table',
-    granularity_sqla: 'ds',
-    series: 'foo',
-    viz_type: 'my_chart',
-  };
-
-  it('should build groupby with series in form data', () => {
-    const queryContext = buildQuery(formData);
-    const [query] = queryContext.queries;
-    expect(query.columns).toEqual(['foo']);
-  });
-});
+module.exports = 'test-file-stub';
