e3fc9f83089e6d1aac852e0cfede341c7e73865f
Ajay Mancheery <ajay.m@outlook.com>
2021-12-22 12:48:54 -0800

    Fixing type issues

--- superset/reports/notifications/base.py
+++ superset/reports/notifications/base.py
@@ -27,7 +27,7 @@ from superset.models.reports import ReportRecipients, ReportRecipientType
 class NotificationContent:
     name: str
     csv: Optional[bytes] = None  # bytes for csv file
-    screenshots: List[bytes] = []  # bytes for the screenshot
+    screenshots: Optional[List[bytes]] = None  # bytes for the screenshot
     text: Optional[str] = None
     description: Optional[str] = ""
     url: Optional[str] = None  # url to chart/dashboard for this screenshot
