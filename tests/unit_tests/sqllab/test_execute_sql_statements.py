486ac278036e96a339908a7091e97b05ade69711
Mayur <mayurnewase111@gmail.com>
2021-12-29 13:16:09 +0530

    Import app from common

--- tests/unit_tests/sqllab/test_execute_sql_statements.py
+++ tests/unit_tests/sqllab/test_execute_sql_statements.py
@@ -1,8 +1,25 @@
-from contextlib import closing
-from unittest import mock
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+# isort:skip_file
 
+from unittest import mock
 import pytest
 
+from tests.common.test_app import app
 from superset import db
 from superset.db_engine_specs import BaseEngineSpec
 from superset.db_engine_specs.hive import HiveEngineSpec
@@ -16,7 +33,7 @@ from superset.sql_lab import (
     execute_sql_statements,
 )
 from superset.sql_parse import CtasMethod
-from superset.utils.core import get_example_database, QuerySource
+from superset.utils.core import get_example_database
 from tests.common.base_tests import SupersetTestCase
 
 
