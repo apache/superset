79b6dc251e76b3e733f7b82d1ef171e9fb093c00
Erik Ritter <erik.ritter@airbnb.com>
2022-01-06 21:44:27 -0800

    fix: package.json local file dependencies (#17957)

--- superset-frontend/webpack.config.js
+++ superset-frontend/webpack.config.js
@@ -430,7 +430,7 @@ const config = {
 // find all the symlinked plugins and use their source code for imports
 Object.entries(packageConfig.dependencies).forEach(([pkg, relativeDir]) => {
   const srcPath = path.join(APP_DIR, `./node_modules/${pkg}/src`);
-  const dir = relativeDir.replace('file://', '');
+  const dir = relativeDir.replace('file:', '');
 
   if (/^superset-plugin-/.test(pkg) && fs.existsSync(srcPath)) {
     console.log(
