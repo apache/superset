009402a470acbd49eb080180394b79e6c02e1e1a
cccs-Dustin <96579982+cccs-Dustin@users.noreply.github.com>
2022-01-14 13:48:23 +0000

    added '*args: Any' back into v0.py as it did not need to be removed. Removing it might cause headaches for someone trying to work on this particular piece of code in the future

--- superset/datasets/commands/importers/v0.py
+++ superset/datasets/commands/importers/v0.py
@@ -284,7 +284,9 @@ class ImportDatasetsCommand(BaseCommand):
     """
 
     # pylint: disable=unused-argument
-    def __init__(self, contents: Dict[str, str], **kwargs: Any):
+    def __init__(
+        self, contents: Dict[str, str], *args: Any, **kwargs: Any,
+    ):
         self.contents = contents
         self._configs: Dict[str, Any] = {}
 
