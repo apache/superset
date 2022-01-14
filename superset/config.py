f8f54e6d18be2f4307fa0d689476012d85ad4406
EvilDrW <keith.w.wait@gmail.com>
2022-01-13 22:15:52 -0600

    remove duplicated key

--- superset/config.py
+++ superset/config.py
@@ -419,7 +419,6 @@ DEFAULT_FEATURE_FLAGS: Dict[str, bool] = {
     # This could cause the server to run out of memory or compute.
     "ALLOW_FULL_CSV_EXPORT": False,
     "UX_BETA": False,
-    "CLEAR_FILTER_ON_TAB_NAV": False,
 }
 
 # Feature flags may also be set via 'SUPERSET_FEATURE_' prefixed environment vars.
