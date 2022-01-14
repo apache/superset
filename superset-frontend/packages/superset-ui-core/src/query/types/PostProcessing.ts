a4fd2a4c65dca219cd57af631e602e730ce3f9eb
Yongjie Zhao <yongjie.zhao@gmail.com>
2022-01-14 20:00:03 +0800

    typo

--- superset-frontend/packages/superset-ui-core/src/query/types/PostProcessing.ts
+++ superset-frontend/packages/superset-ui-core/src/query/types/PostProcessing.ts
@@ -167,7 +167,7 @@ export interface PostProcessingResample {
     rule: string;
     fill_value?: number | null;
     time_column: string;
-    // If AdhocColumn don't have a label, it will be undefined.
+    // If AdhocColumn doesn't have a label, it will be undefined.
     // todo: we have to give an explicit label for AdhocColumn.
     groupby_columns?: Array<string | undefined>;
   };
