128b90cfa43f226c782af61b9929d50a3e1025a7
David Aaron Suddjian <aasuddjian@gmail.com>
2022-01-18 15:31:09 -0800

    more tests

--- tests/integration_tests/security/guest_token_security_tests.py
+++ tests/integration_tests/security/guest_token_security_tests.py
@@ -23,6 +23,7 @@ from flask import g
 from superset import db, security_manager
 from superset.exceptions import SupersetSecurityException
 from superset.models.dashboard import Dashboard
+from superset.security.guest_token import GuestTokenResourceType
 from tests.integration_tests.base_tests import SupersetTestCase
 from tests.integration_tests.fixtures.birth_names_dashboard import (
     load_birth_names_dashboard_with_slices,
@@ -34,27 +35,100 @@ from tests.integration_tests.fixtures.birth_names_dashboard import (
     "superset.extensions.feature_flag_manager._feature_flags", EMBEDDED_SUPERSET=True,
 )
 class TestGuestUserSecurity(SupersetTestCase):
-    def test_user_is_not_guest(self):
+    def test_is_guest_user__regular_user(self):
         is_guest = security_manager.is_guest_user(security_manager.find_user("admin"))
         self.assertFalse(is_guest)
 
-    def test_anon_is_not_guest(self):
+    def test_is_guest_user__anonymous(self):
         is_guest = security_manager.is_guest_user(security_manager.get_anonymous_user())
         self.assertFalse(is_guest)
 
-    def test_guest_is_guest(self):
+    def test_is_guest_user__guest_user(self):
         authorized_guest = security_manager.get_guest_user_from_token(
             {"user": {}, "resources": []}
         )
         is_guest = security_manager.is_guest_user(authorized_guest)
         self.assertTrue(is_guest)
 
+    def test_get_guest_user__regular_user(self):
+        g.user = security_manager.find_user("admin")
+        guest_user = security_manager.get_current_guest_user_if_guest()
+        self.assertIsNone(guest_user)
+
+    def test_get_guest_user__anonymous_user(self):
+        g.user = security_manager.get_anonymous_user()
+        guest_user = security_manager.get_current_guest_user_if_guest()
+        self.assertIsNone(guest_user)
+
+    def test_get_guest_user__guest_user(self):
+        g.user = security_manager.get_guest_user_from_token(
+            {"user": {}, "resources": []}
+        )
+        guest_user = security_manager.get_current_guest_user_if_guest()
+        self.assertEqual(guest_user, g.user)
+
+    def test_has_guest_access__regular_user(self):
+        g.user = security_manager.find_user("admin")
+        has_guest_access = security_manager.has_guest_access(
+            GuestTokenResourceType.DASHBOARD, 42
+        )
+        self.assertFalse(has_guest_access)
+
+    def test_has_guest_access__anonymous_user(self):
+        g.user = security_manager.get_anonymous_user()
+        has_guest_access = security_manager.has_guest_access(
+            GuestTokenResourceType.DASHBOARD, 42
+        )
+        self.assertFalse(has_guest_access)
+
+    def test_has_guest_access__authorized_guest_user(self):
+        g.user = security_manager.get_guest_user_from_token(
+            {"user": {}, "resources": [{"type": "dashboard", "id": 42}]}
+        )
+        has_guest_access = security_manager.has_guest_access(
+            GuestTokenResourceType.DASHBOARD, 42
+        )
+        self.assertTrue(has_guest_access)
+
+    def test_has_guest_access__authorized_guest_user__non_zero_resource_index(self):
+        g.user = security_manager.get_guest_user_from_token(
+            {
+                "user": {},
+                "resources": [
+                    {"type": "dashboard", "id": 24},
+                    {"type": "dashboard", "id": 42},
+                ],
+            }
+        )
+        has_guest_access = security_manager.has_guest_access(
+            GuestTokenResourceType.DASHBOARD, 42
+        )
+        self.assertTrue(has_guest_access)
+
+    def test_has_guest_access__unauthorized_guest_user__different_resource_id(self):
+        g.user = security_manager.get_guest_user_from_token(
+            {"user": {}, "resources": [{"type": "dashboard", "id": 24}]}
+        )
+        has_guest_access = security_manager.has_guest_access(
+            GuestTokenResourceType.DASHBOARD, 42
+        )
+        self.assertFalse(has_guest_access)
+
+    def test_has_guest_access__unauthorized_guest_user__different_resource_type(self):
+        g.user = security_manager.get_guest_user_from_token(
+            {"user": {}, "resources": [{"type": "dirt", "id": 42}]}
+        )
+        has_guest_access = security_manager.has_guest_access(
+            GuestTokenResourceType.DASHBOARD, 42
+        )
+        self.assertFalse(has_guest_access)
+
 
 @mock.patch.dict(
     "superset.extensions.feature_flag_manager._feature_flags", EMBEDDED_SUPERSET=True,
 )
 @pytest.mark.usefixtures("load_birth_names_dashboard_with_slices")
-class TestGuestTokenDashboardAccess(SupersetTestCase):
+class TestGuestUserDashboardAccess(SupersetTestCase):
     def setUp(self) -> None:
         self.dash = db.session.query(Dashboard).filter_by(slug="births").first()
         self.authorized_guest = security_manager.get_guest_user_from_token(
