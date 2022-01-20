691486d33a229d8f271d7a1a25c9ff3589c8bae6
Ville Brofeldt <ville.v.brofeldt@gmail.com>
2022-01-20 11:12:04 +0200

    fix rat excludes and headers

--- docs-v2/src/components/HomepageFeatures.tsx
+++ docs-v2/src/components/HomepageFeatures.tsx
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 import React from 'react';
 import clsx from 'clsx';
 import styles from './HomepageFeatures.module.css';
