113fe4e6ccfbb48e7c266069ade22c2a18aeacaf
EugeneTorap <evgenykrutpro@gmail.com>
2022-01-25 07:24:15 +0300

    Refactoring

--- superset-frontend/src/SqlLab/components/ExploreResultsButton/index.tsx
+++ superset-frontend/src/SqlLab/components/ExploreResultsButton/index.tsx
@@ -17,20 +17,11 @@
  * under the License.
  */
 import React, { FC } from 'react';
-import { useSelector } from 'react-redux';
-import moment from 'moment';
-import Alert from 'src/components/Alert';
 import { t } from '@superset-ui/core';
 import { InfoTooltipWithTrigger } from '@superset-ui/chart-controls';
-import shortid from 'shortid';
 import Button, { OnClickHandler } from 'src/components/Button';
-import { Query } from 'src/SqlLab/types';
-import rootReducer from 'src/SqlLab/reducers';
-
-type RootState = ReturnType<typeof rootReducer>;
 
 interface ExploreResultsButtonProps {
-  query?: Query;
   database: {
     allows_subquery?: boolean;
   };
@@ -38,100 +29,9 @@ interface ExploreResultsButtonProps {
 }
 
 const ExploreResultsButton: FC<ExploreResultsButtonProps> = ({
-  query = {},
   database,
   onClick,
 }) => {
-  const conf = useSelector((state: RootState) => state.common.conf);
-  const timeout = conf ? conf.SUPERSET_WEBSERVER_TIMEOUT : null;
-
-  const getColumns = () => query?.results?.selected_columns ?? [];
-
-  const getQueryDuration = () => {
-    return moment
-      .duration(query.endDttm - query.startDttm)
-      .asSeconds();
-  };
-
-  const getInvalidColumns = () => {
-    const re1 = /__\d+$/; // duplicate column name pattern
-    const re2 = /^__timestamp/i; // reserved temporal column alias
-
-    return query.results.selected_columns
-      .map(col => col.name)
-      .filter(col => re1.test(col) || re2.test(col));
-  };
-
-  const datasourceName = () => {
-    const uniqueId = shortid.generate();
-    let datasourceName = uniqueId;
-    if (query) {
-      datasourceName = query.user ? `${query.user}-` : '';
-      datasourceName += `${query.tab}-${uniqueId}`;
-    }
-    return datasourceName;
-  };
-
-  const buildVizOptions = () => {
-    const { schema, sql, dbId, templateParams } = query;
-    return {
-      dbId,
-      schema,
-      sql,
-      templateParams,
-      datasourceName: datasourceName(),
-      columns: getColumns(),
-    };
-  };
-
-  const renderTimeoutWarning = () => {
-    return (
-      <Alert
-        type="warning"
-        message={
-          <>
-            {t(
-              'This query took %s seconds to run, ',
-              Math.round(getQueryDuration()),
-            ) +
-              t('and the explore view times out at %s seconds ', timeout) +
-              t(
-                'following this flow will most likely lead to your query timing out. ',
-              ) +
-              t(
-                'We recommend your summarize your data further before following that flow. ',
-              ) +
-              t('If activated you can use the ')}
-            <strong>CREATE TABLE AS </strong>
-            {t(
-              'feature to store a summarized data set that you can then explore.',
-            )}
-          </>
-        }
-      />
-    );
-  };
-
-  const renderInvalidColumnMessage = () => {
-    const invalidColumns = getInvalidColumns();
-    if (invalidColumns.length === 0) {
-      return null;
-    }
-    return (
-      <div>
-        {t('Column name(s) ')}
-        <code>
-          <strong>{invalidColumns.join(', ')} </strong>
-        </code>
-        {t(`cannot be used as a column name. The column name/alias "__timestamp"
-          is reserved for the main temporal expression, and column aliases ending with
-          double underscores followed by a numeric value (e.g. "my_col__1") are reserved
-          for deduplicating duplicate column names. Please use aliases to rename the
-          invalid column names.`)}
-      </div>
-    );
-  };
-
   const allowsSubquery = database && database.allows_subquery;
   return (
     <>
