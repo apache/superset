bb89055e43937f6db8868ac6f53e9a9e0e251298
cccs-RyanS <EMAIL>
2022-02-01 14:38:55 -0500

    [CLDN-1221] Adding select all button back in

--- superset-frontend/src/explore/components/controls/SelectControl.jsx
+++ superset-frontend/src/explore/components/controls/SelectControl.jsx
@@ -34,6 +34,7 @@ const propTypes = {
   multi: PropTypes.bool,
   isMulti: PropTypes.bool,
   name: PropTypes.string.isRequired,
+  allowAll: PropTypes.bool,
   onChange: PropTypes.func,
   onFocus: PropTypes.func,
   value: PropTypes.oneOfType([
@@ -77,11 +78,14 @@ const defaultProps = {
   isLoading: false,
   label: null,
   multi: false,
+  allowAll: false,
   onChange: () => {},
   onFocus: () => {},
   showHeader: true,
   valueKey: 'value',
 };
+
+const SELECT_ALL_STRING = 'Select all';
 
 export default class SelectControl extends React.PureComponent {
   constructor(props) {
@@ -119,12 +123,14 @@ export default class SelectControl extends React.PureComponent {
     if (typeof val === 'object' && val?.[valueKey] !== undefined) {
       onChangeVal = val[valueKey];
     }
+
     this.props.onChange(onChangeVal, []);
   }
 
   getOptions(props) {
-    const { choices, optionRenderer, valueKey } = props;
+    const { choices, optionRenderer, valueKey, allowAll } = props;
     let options = [];
+
     if (props.options) {
       options = props.options.map(o => ({
         ...o,
@@ -152,12 +158,39 @@ export default class SelectControl extends React.PureComponent {
         return { value: c, label: c };
       });
     }
+
+    if (allowAll === true) {
+      if (!this.optionsIncludesSelectAll(options)) {
+        options.unshift(this.createMetaSelectAllOption());
+      }
+    } else {
+      // options = options.filter(o => !this.isMetaSelectAllOption(o));
+    }
+
     return options;
   }
 
   handleFilterOptions(text, option) {
     const { filterOption } = this.props;
     return filterOption({ data: option }, text);
+  }
+
+  isMetaSelectAllOption(o) {
+    return o.meta && o.meta === true && o.label === 'Select all';
+  }
+
+  optionsIncludesSelectAll(o) {
+    return o.findIndex(o => this.isMetaSelectAllOption(o)) >= 0;
+  }
+
+  createMetaSelectAllOption() {
+    const option = {
+      label: SELECT_ALL_STRING,
+      meta: true,
+      value: SELECT_ALL_STRING,
+    };
+    option[this.props.valueKey] = SELECT_ALL_STRING;
+    return option;
   }
 
   render() {
