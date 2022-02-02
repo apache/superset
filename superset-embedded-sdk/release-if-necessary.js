ff69ca733b1642a08364aa5b64bfd7350034cdd0
David Aaron Suddjian <aasuddjian@gmail.com>
2022-02-01 20:03:43 -0800

    release script

---
+++ superset-embedded-sdk/release-if-necessary.js
@@ -0,0 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+const { execSync } = require('child_process');
+const { name, version } = require('./package.json');
+
+console.log(`checking if ${name}@${version} needs releasing`);
+
+// need to build and publish the package iif the current version does not exist on npm
+const needsPublishing = String(execSync(`npm view ${name}@${version} version`)) === '';
+
+if (needsPublishing) {
+  console.log('release required, building');
+  execSync('npm run build');
+  execSync('npm publish');
+  console.log(`published ${version}`)
+} else {
+  console.log('no release required, exiting');
+}
