51a1f7d2b7930eb59c20ddb1f0288acb0fd14a78
Phillip Kelley-Dotson <pkelleydotson@yahoo.com>
2022-02-03 09:50:21 -0800

    remove console logs

--- superset-frontend/src/SqlLab/actions/sqlLab.js
+++ superset-frontend/src/SqlLab/actions/sqlLab.js
@@ -775,9 +775,7 @@ export function queryEditorSetDb(queryEditor, dbId) {
 }
 
 export function queryEditorSetSchema(queryEditor, schema) {
-  console.log('queryEditor', typeof queryEditor, '---schema -->', schema)
   return function (dispatch) {
-    console.log('featureflag *******', isFeatureEnabled(FeatureFlag.SQLLAB_BACKEND_PERSISTENCE))
     const sync = isFeatureEnabled(FeatureFlag.SQLLAB_BACKEND_PERSISTENCE) && typeof queryEditor === 'object'
       ? SupersetClient.put({
           endpoint: encodeURI(`/tabstateview/${queryEditor.id}`),
