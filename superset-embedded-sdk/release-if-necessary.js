8b2c8efddb0947a0421f102c5884ee46f7ebab40
David Aaron Suddjian <aasuddjian@gmail.com>
2022-02-03 09:30:06 -0800

    fix some scripts

--- superset-embedded-sdk/release-if-necessary.js
+++ superset-embedded-sdk/release-if-necessary.js
@@ -18,18 +18,30 @@
  */
 
 const { execSync } = require('child_process');
+const axios = require('axios');
 const { name, version } = require('./package.json');
 
-console.log(`checking if ${name}@${version} needs releasing`);
+(async () => {
+  console.log(`checking if ${name}@${version} needs releasing`);
 
-// need to build and publish the package iff the current version does not exist on npm
-const needsPublishing = String(execSync(`npm view ${name}@${version} version`)) === '';
+  const packageUrl = `https://registry.npmjs.org/${name}/${version}`;
+  // npm commands output a bunch of garbage in the edge cases,
+  // and require sending semi-validated strings to the command line,
+  // so let's just use good old http.
+  const { status } = await axios.get(packageUrl, {
+    validateStatus: (status) => true // we literally just want the status so any status is valid
+  });
 
-if (needsPublishing) {
-  console.log('release required, building');
-  execSync('npm run build');
-  execSync('npm publish');
-  console.log(`published ${version}`)
-} else {
-  console.log('no release required, exiting');
-}
+  if (status === 200) {
+    console.log('version already exists on npm, exiting');
+  } else if (status === 404) {
+    console.log('release required, building');
+    execSync('npm run build');
+    execSync('npm publish --access public');
+    console.log(`published ${version} to npm`);
+  } else {
+    console.error(`ERROR: Received unexpected http status code ${status} from GET ${packageUrl}
+The embedded sdk release script might need to be fixed, or maybe you just need to try again later.`);
+    process.exitCode = 1;
+  }
+})();
