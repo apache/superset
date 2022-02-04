3b9773d4f8b2ec0980173212e1e67aa6f2588334
GITHUB_USERNAME <EMAIL>
2022-02-04 13:09:01 +0000

    [CLDN-1207] Unit tests for the business type API are now working, however, they need to be modified to make use of @mock as we don't want to have to run the server to be able to run the unit tests

--- tests/integration_tests/business_type/api_tests.py
+++ tests/integration_tests/business_type/api_tests.py
@@ -17,56 +17,56 @@
 # isort:skip_file
 """Unit tests for Superset"""
 
-import json
+import requests
 
 from tests.integration_tests.base_tests import SupersetTestCase
-from superset.business_type.business_type_response import BusinessTypeResponse
-from superset.utils.core import FilterStringOperators
+
 
 class TestBusinessTypeApi(SupersetTestCase):
     """This class includes the unit tests for the business type REST API"""
     # To run the unit tests below, use the following commands in the root Superset folder:
-    # docker-compose up
-    # scripts/tests/run.sh --module tests/integration_tests/business_type/api_tests.py
-    # docker-compose down -v      (only run this last command when you are done with the unit tests)
+    # tox -e py38 -- tests/integration_tests/business_type/api_tests.py
 
     def test_get_business_type(self):
         """
         business_type API: Test 'get', expect to get a BusinessTypeResponse
         """
 
-        self.login(username="admin")
-        uri = f"/api/v1/business_type/convert?q=(type:cidr,values:!('1.1.1.1'))"
-        http_response = self.get_assert_metric(uri, "get")
-        response = json.loads(http_response.data.decode("utf-8"))
+        uri = "http://localhost:9000/api/v1/business_type/convert?q=(type:cidr,values:!('1.1.1.1'))"
+        http_response = requests.get(url=uri)
 
-        expected_response: BusinessTypeResponse = {
-            "values": [16843009],
-            "error_message": "",
-            "display_value": "16843009",
-            "valid_filter_operators": [
-                FilterStringOperators.EQUALS,
-                FilterStringOperators.GREATER_THAN_OR_EQUAL,
-                FilterStringOperators.GREATER_THAN,
-                FilterStringOperators.IN,
-                FilterStringOperators.LESS_THAN,
-                FilterStringOperators.LESS_THAN_OR_EQUAL,
-            ],
+        response_data = http_response.json()
+
+        expected_response = {
+            "result": {
+                "display_value": "16843009",
+                "error_message": "",
+                "valid_filter_operators": [
+                    "EQUALS",
+                    "GREATER_THAN_OR_EQUAL",
+                    "GREATER_THAN",
+                    "IN",
+                    "LESS_THAN",
+                    "LESS_THAN_OR_EQUAL"
+                ],
+                "values": [
+                    16843009
+                ]
+            }
         }
 
         self.assertEqual(http_response.status_code, 200)
-        self.assertEqual(response, expected_response)
+        self.assertEqual(response_data, expected_response)
 
     def test_get_types_business_type(self):
         """
         business_type API: Test 'get_types', expect to get a list of the availiable business types
         """
 
-        self.login(username="admin")
-        uri = f"/api/v1/business_type/types"
-        http_response = self.get_assert_metric(uri, "get_types")
-        data = json.loads(http_response.data.decode("utf-8"))
+        uri = 'http://localhost:9000/api/v1/business_type/types'
+        http_response = requests.get(url=uri)
+
+        response_data = http_response.json()
 
         self.assertEqual(http_response.status_code, 200)
-        self.assertEqual(data, ['cidr', 'port'])
-        
+        self.assertEqual(response_data, {'result': ['cidr', 'port']})
