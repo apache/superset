2e1dba8bf19e0fc9280ff463434820fd7e56bc07
mqu <meitongdepaul@gmail.com>
2022-02-05 23:30:50 -0800

    convert DashboardGrid.test to RTL

--- superset-frontend/src/dashboard/components/DashboardGrid.jsx
+++ superset-frontend/src/dashboard/components/DashboardGrid.jsx
@@ -18,209 +18,288 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { isFeatureEnabled, t, FeatureFlag } from '@superset-ui/core';
 
-import { componentShape } from '../util/propShapes';
-import DashboardComponent from '../containers/DashboardComponent';
-import DragDroppable from './dnd/DragDroppable';
+import { PluginContext } from 'src/components/DynamicPlugins';
+import Loading from 'src/components/Loading';
+import getChartIdsFromLayout from '../util/getChartIdsFromLayout';
+import getLayoutComponentFromChartId from '../util/getLayoutComponentFromChartId';
+import DashboardBuilder from './DashboardBuilder/DashboardBuilder';
+import {
+  chartPropShape,
+  slicePropShape,
+  dashboardInfoPropShape,
+  dashboardStatePropShape,
+} from '../util/propShapes';
+import {
+  LOG_ACTIONS_HIDE_BROWSER_TAB,
+  LOG_ACTIONS_MOUNT_DASHBOARD,
+  Logger,
+} from '../../logger/LogUtils';
+import OmniContainer from '../../components/OmniContainer';
+import { areObjectsEqual } from '../../reduxUtils';
 
-import { GRID_GUTTER_SIZE, GRID_COLUMN_COUNT } from '../util/constants';
+import '../stylesheets/index.less';
+import getLocationHash from '../util/getLocationHash';
+import isDashboardEmpty from '../util/isDashboardEmpty';
+import { getAffectedOwnDataCharts } from '../util/charts/getOwnDataCharts';
 
 const propTypes = {
-  depth: PropTypes.number.isRequired,
-  editMode: PropTypes.bool.isRequired,
-  gridComponent: componentShape.isRequired,
-  handleComponentDrop: PropTypes.func.isRequired,
-  isComponentVisible: PropTypes.bool.isRequired,
-  resizeComponent: PropTypes.func.isRequired,
-  setDirectPathToChild: PropTypes.func.isRequired,
-  width: PropTypes.number.isRequired,
+  actions: PropTypes.shape({
+    addSliceToDashboard: PropTypes.func.isRequired,
+    removeSliceFromDashboard: PropTypes.func.isRequired,
+    triggerQuery: PropTypes.func.isRequired,
+    logEvent: PropTypes.func.isRequired,
+    clearDataMaskState: PropTypes.func.isRequired,
+  }).isRequired,
+  dashboardInfo: dashboardInfoPropShape.isRequired,
+  dashboardState: dashboardStatePropShape.isRequired,
+  charts: PropTypes.objectOf(chartPropShape).isRequired,
+  slices: PropTypes.objectOf(slicePropShape).isRequired,
+  activeFilters: PropTypes.object.isRequired,
+  chartConfiguration: PropTypes.object.isRequired,
+  datasources: PropTypes.object.isRequired,
+  ownDataCharts: PropTypes.object.isRequired,
+  layout: PropTypes.object.isRequired,
+  impressionId: PropTypes.string.isRequired,
+  initMessages: PropTypes.array,
+  timeout: PropTypes.number,
+  userId: PropTypes.string,
 };
 
-const defaultProps = {};
+const defaultProps = {
+  initMessages: [],
+  timeout: 60,
+  userId: '',
+};
 
-const renderDraggableContentBottom = dropProps =>
-  dropProps.dropIndicatorProps && (
-    <div className="drop-indicator drop-indicator--bottom" />
-  );
+class Dashboard extends React.PureComponent {
+  static contextType = PluginContext;
 
-const renderDraggableContentTop = dropProps =>
-  dropProps.dropIndicatorProps && (
-    <div className="drop-indicator drop-indicator--top" />
-  );
+  static onBeforeUnload(hasChanged) {
+    if (hasChanged) {
+      window.addEventListener('beforeunload', Dashboard.unload);
+    } else {
+      window.removeEventListener('beforeunload', Dashboard.unload);
+    }
+  }
 
-class DashboardGrid extends React.PureComponent {
+  static unload() {
+    const message = t('You have unsaved changes.');
+    window.event.returnValue = message; // Gecko + IE
+    return message; // Gecko + Webkit, Safari, Chrome etc.
+  }
+
   constructor(props) {
     super(props);
-    this.state = {
-      isResizing: false,
-      rowGuideTop: null,
-    };
-
-    this.handleResizeStart = this.handleResizeStart.bind(this);
-    this.handleResize = this.handleResize.bind(this);
-    this.handleResizeStop = this.handleResizeStop.bind(this);
-    this.handleTopDropTargetDrop = this.handleTopDropTargetDrop.bind(this);
-    this.getRowGuidePosition = this.getRowGuidePosition.bind(this);
-    this.setGridRef = this.setGridRef.bind(this);
-    this.handleChangeTab = this.handleChangeTab.bind(this);
+    this.appliedFilters = props.activeFilters ?? {};
+    this.appliedOwnDataCharts = props.ownDataCharts ?? {};
+    this.onVisibilityChange = this.onVisibilityChange.bind(this);
   }
 
-  getRowGuidePosition(resizeRef) {
-    if (resizeRef && this.grid) {
-      return (
-        resizeRef.getBoundingClientRect().bottom -
-        this.grid.getBoundingClientRect().top -
-        2
+  componentDidMount() {
+    const appContainer = document.getElementById('app');
+    const bootstrapData = appContainer?.getAttribute('data-bootstrap') || '{}';
+    const { dashboardState, layout } = this.props;
+    const eventData = {
+      is_soft_navigation: Logger.timeOriginOffset > 0,
+      is_edit_mode: dashboardState.editMode,
+      mount_duration: Logger.getTimestamp(),
+      is_empty: isDashboardEmpty(layout),
+      is_published: dashboardState.isPublished,
+      bootstrap_data_length: bootstrapData.length,
+    };
+    const directLinkComponentId = getLocationHash();
+    if (directLinkComponentId) {
+      eventData.target_id = directLinkComponentId;
+    }
+    this.props.actions.logEvent(LOG_ACTIONS_MOUNT_DASHBOARD, eventData);
+
+    // Handle browser tab visibility change
+    if (document.visibilityState === 'hidden') {
+      this.visibilityEventData = {
+        start_offset: Logger.getTimestamp(),
+        ts: new Date().getTime(),
+      };
+    }
+    window.addEventListener('visibilitychange', this.onVisibilityChange);
+    this.applyCharts();
+  }
+
+  componentDidUpdate() {
+    this.applyCharts();
+  }
+
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    const currentChartIds = getChartIdsFromLayout(this.props.layout);
+    const nextChartIds = getChartIdsFromLayout(nextProps.layout);
+
+    if (this.props.dashboardInfo.id !== nextProps.dashboardInfo.id) {
+      // single-page-app navigation check
+      return;
+    }
+
+    if (currentChartIds.length < nextChartIds.length) {
+      const newChartIds = nextChartIds.filter(
+        key => currentChartIds.indexOf(key) === -1,
+      );
+      newChartIds.forEach(newChartId =>
+        this.props.actions.addSliceToDashboard(
+          newChartId,
+          getLayoutComponentFromChartId(nextProps.layout, newChartId),
+        ),
+      );
+    } else if (currentChartIds.length > nextChartIds.length) {
+      // remove chart
+      const removedChartIds = currentChartIds.filter(
+        key => nextChartIds.indexOf(key) === -1,
+      );
+      removedChartIds.forEach(removedChartId =>
+        this.props.actions.removeSliceFromDashboard(removedChartId),
       );
     }
-    return null;
   }
 
-  setGridRef(ref) {
-    this.grid = ref;
-  }
+  applyCharts() {
+    const { hasUnsavedChanges, editMode } = this.props.dashboardState;
 
-  handleResizeStart({ ref, direction }) {
-    let rowGuideTop = null;
-    if (direction === 'bottom' || direction === 'bottomRight') {
-      rowGuideTop = this.getRowGuidePosition(ref);
+    const { appliedFilters, appliedOwnDataCharts } = this;
+    const { activeFilters, ownDataCharts, chartConfiguration } = this.props;
+    if (
+      isFeatureEnabled(FeatureFlag.DASHBOARD_CROSS_FILTERS) &&
+      !chartConfiguration
+    ) {
+      // For a first loading we need to wait for cross filters charts data loaded to get all active filters
+      // for correct comparing  of filters to avoid unnecessary requests
+      return;
     }
 
-    this.setState(() => ({
-      isResizing: true,
-      rowGuideTop,
-    }));
-  }
+    if (
+      !editMode &&
+      (!areObjectsEqual(appliedOwnDataCharts, ownDataCharts, {
+        ignoreUndefined: true,
+      }) ||
+        !areObjectsEqual(appliedFilters, activeFilters, {
+          ignoreUndefined: true,
+        }))
+    ) {
+      this.applyFilters();
+    }
 
-  handleResize({ ref, direction }) {
-    if (direction === 'bottom' || direction === 'bottomRight') {
-      this.setState(() => ({ rowGuideTop: this.getRowGuidePosition(ref) }));
+    if (hasUnsavedChanges) {
+      Dashboard.onBeforeUnload(true);
+    } else {
+      Dashboard.onBeforeUnload(false);
     }
   }
 
-  handleResizeStop({ id, widthMultiple: width, heightMultiple: height }) {
-    this.props.resizeComponent({ id, width, height });
-
-    this.setState(() => ({
-      isResizing: false,
-      rowGuideTop: null,
-    }));
+  componentWillUnmount() {
+    window.removeEventListener('visibilitychange', this.onVisibilityChange);
+    this.props.actions.clearDataMaskState();
   }
 
-  handleTopDropTargetDrop(dropResult) {
-    if (dropResult) {
-      this.props.handleComponentDrop({
-        ...dropResult,
-        destination: {
-          ...dropResult.destination,
-          // force appending as the first child if top drop target
-          index: 0,
-        },
+  onVisibilityChange() {
+    if (document.visibilityState === 'hidden') {
+      // from visible to hidden
+      this.visibilityEventData = {
+        start_offset: Logger.getTimestamp(),
+        ts: new Date().getTime(),
+      };
+    } else if (document.visibilityState === 'visible') {
+      // from hidden to visible
+      const logStart = this.visibilityEventData.start_offset;
+      this.props.actions.logEvent(LOG_ACTIONS_HIDE_BROWSER_TAB, {
+        ...this.visibilityEventData,
+        duration: Logger.getTimestamp() - logStart,
       });
     }
   }
 
-  handleChangeTab({ pathToTabIndex }) {
-    this.props.setDirectPathToChild(pathToTabIndex);
+  // return charts in array
+  getAllCharts() {
+    return Object.values(this.props.charts);
+  }
+
+  applyFilters() {
+    const { appliedFilters } = this;
+    const { activeFilters, ownDataCharts } = this.props;
+
+    // refresh charts if a filter was removed, added, or changed
+    const currFilterKeys = Object.keys(activeFilters);
+    const appliedFilterKeys = Object.keys(appliedFilters);
+
+    const allKeys = new Set(currFilterKeys.concat(appliedFilterKeys));
+    const affectedChartIds = getAffectedOwnDataCharts(
+      ownDataCharts,
+      this.appliedOwnDataCharts,
+    );
+    [...allKeys].forEach(filterKey => {
+      if (
+        !currFilterKeys.includes(filterKey) &&
+        appliedFilterKeys.includes(filterKey)
+      ) {
+        // filterKey is removed?
+        affectedChartIds.push(...appliedFilters[filterKey].scope);
+      } else if (!appliedFilterKeys.includes(filterKey)) {
+        // filterKey is newly added?
+        affectedChartIds.push(...activeFilters[filterKey].scope);
+      } else {
+        // if filterKey changes value,
+        // update charts in its scope
+        if (
+          !areObjectsEqual(
+            appliedFilters[filterKey].values,
+            activeFilters[filterKey].values,
+            {
+              ignoreUndefined: true,
+            },
+          )
+        ) {
+          affectedChartIds.push(...activeFilters[filterKey].scope);
+        }
+
+        // if filterKey changes scope,
+        // update all charts in its scope
+        if (
+          !areObjectsEqual(
+            appliedFilters[filterKey].scope,
+            activeFilters[filterKey].scope,
+          )
+        ) {
+          const chartsInScope = (activeFilters[filterKey].scope || []).concat(
+            appliedFilters[filterKey].scope || [],
+          );
+          affectedChartIds.push(...chartsInScope);
+        }
+      }
+    });
+
+    // remove dup in affectedChartIds
+    this.refreshCharts([...new Set(affectedChartIds)]);
+    this.appliedFilters = activeFilters;
+    this.appliedOwnDataCharts = ownDataCharts;
+  }
+
+  refreshCharts(ids) {
+    ids.forEach(id => {
+      this.props.actions.triggerQuery(true, id);
+    });
   }
 
   render() {
-    const {
-      gridComponent,
-      handleComponentDrop,
-      depth,
-      editMode,
-      width,
-      isComponentVisible,
-    } = this.props;
-    const columnPlusGutterWidth =
-      (width + GRID_GUTTER_SIZE) / GRID_COLUMN_COUNT;
-
-    const columnWidth = columnPlusGutterWidth - GRID_GUTTER_SIZE;
-    const { isResizing, rowGuideTop } = this.state;
-
-    return width < 100 ? null : (
-      <div className="dashboard-grid" ref={this.setGridRef}>
-        <div className="grid-content" data-test="grid-content">
-          {/* make the area above components droppable */}
-          {editMode && (
-            <DragDroppable
-              component={gridComponent}
-              depth={depth}
-              parentComponent={null}
-              index={0}
-              orientation="column"
-              onDrop={this.handleTopDropTargetDrop}
-              className="empty-droptarget"
-              editMode
-            >
-              {renderDraggableContentBottom}
-            </DragDroppable>
-          )}
-
-          {gridComponent.children.map((id, index) => (
-            <DashboardComponent
-              key={id}
-              id={id}
-              parentId={gridComponent.id}
-              depth={depth + 1}
-              index={index}
-              availableColumnCount={GRID_COLUMN_COUNT}
-              columnWidth={columnWidth}
-              isComponentVisible={isComponentVisible}
-              onResizeStart={this.handleResizeStart}
-              onResize={this.handleResize}
-              onResizeStop={this.handleResizeStop}
-              onChangeTab={this.handleChangeTab}
-            />
-          ))}
-
-          {/* make the area below components droppable */}
-          {editMode && gridComponent.children.length > 0 && (
-            <DragDroppable
-              component={gridComponent}
-              depth={depth}
-              parentComponent={null}
-              index={gridComponent.children.length}
-              orientation="column"
-              onDrop={handleComponentDrop}
-              className="empty-droptarget"
-              editMode
-            >
-              {renderDraggableContentTop}
-            </DragDroppable>
-          )}
-
-          {isResizing &&
-            Array(GRID_COLUMN_COUNT)
-              .fill(null)
-              .map((_, i) => (
-                <div
-                  key={`grid-column-${i}`}
-                  className="grid-column-guide"
-                  style={{
-                    left: i * GRID_GUTTER_SIZE + i * columnWidth,
-                    width: columnWidth,
-                  }}
-                />
-              ))}
-
-          {isResizing && rowGuideTop && (
-            <div
-              className="grid-row-guide"
-              style={{
-                top: rowGuideTop,
-                width,
-              }}
-            />
-          )}
-        </div>
-      </div>
+    if (this.context.loading) {
+      return <Loading />;
+    }
+    return (
+      <>
+        <OmniContainer />
+        <DashboardBuilder />
+      </>
     );
   }
 }
 
-DashboardGrid.propTypes = propTypes;
-DashboardGrid.defaultProps = defaultProps;
+Dashboard.propTypes = propTypes;
+Dashboard.defaultProps = defaultProps;
 
-export default DashboardGrid;
+export default Dashboard;
--- superset-frontend/src/dashboard/components/DashboardGrid.test.jsx
+++ superset-frontend/src/dashboard/components/DashboardGrid.test.jsx
@@ -17,84 +17,210 @@
  * under the License.
  */
 import React from 'react';
-import { shallow } from 'enzyme';
-import sinon from 'sinon';
+import PropTypes from 'prop-types';
 
-import DashboardComponent from 'src/dashboard/containers/DashboardComponent';
-import DashboardGrid from 'src/dashboard/components/DashboardGrid';
-import DragDroppable from 'src/dashboard/components/dnd/DragDroppable';
-import newComponentFactory from 'src/dashboard/util/newComponentFactory';
+import { componentShape } from '../util/propShapes';
+import DashboardComponent from '../containers/DashboardComponent';
+import DragDroppable from './dnd/DragDroppable';
 
-import { DASHBOARD_GRID_TYPE } from 'src/dashboard/util/componentTypes';
-import { GRID_COLUMN_COUNT } from 'src/dashboard/util/constants';
+import { GRID_GUTTER_SIZE, GRID_COLUMN_COUNT } from '../util/constants';
 
-describe('DashboardGrid', () => {
-  const props = {
-    depth: 1,
-    editMode: false,
-    gridComponent: {
-      ...newComponentFactory(DASHBOARD_GRID_TYPE),
-      children: ['a'],
-    },
-    handleComponentDrop() {},
-    resizeComponent() {},
-    width: 500,
-    isComponentVisible: true,
-    setDirectPathToChild() {},
-  };
+const propTypes = {
+  depth: PropTypes.number.isRequired,
+  editMode: PropTypes.bool.isRequired,
+  gridComponent: componentShape.isRequired,
+  handleComponentDrop: PropTypes.func.isRequired,
+  isComponentVisible: PropTypes.bool.isRequired,
+  resizeComponent: PropTypes.func.isRequired,
+  setDirectPathToChild: PropTypes.func.isRequired,
+  width: PropTypes.number.isRequired,
+};
 
-  function setup(overrideProps) {
-    const wrapper = shallow(<DashboardGrid {...props} {...overrideProps} />);
-    return wrapper;
+const defaultProps = {};
+
+const renderDraggableContentBottom = dropProps =>
+  dropProps.dropIndicatorProps && (
+    <div className="drop-indicator drop-indicator--bottom" />
+  );
+
+const renderDraggableContentTop = dropProps =>
+  dropProps.dropIndicatorProps && (
+    <div className="drop-indicator drop-indicator--top" />
+  );
+
+class DashboardGrid extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isResizing: false,
+      rowGuideTop: null,
+    };
+
+    this.handleResizeStart = this.handleResizeStart.bind(this);
+    this.handleResize = this.handleResize.bind(this);
+    this.handleResizeStop = this.handleResizeStop.bind(this);
+    this.handleTopDropTargetDrop = this.handleTopDropTargetDrop.bind(this);
+    this.getRowGuidePosition = this.getRowGuidePosition.bind(this);
+    this.setGridRef = this.setGridRef.bind(this);
+    this.handleChangeTab = this.handleChangeTab.bind(this);
   }
 
-  it('should render a div with class "dashboard-grid"', () => {
-    const wrapper = setup();
-    expect(wrapper.find('.dashboard-grid')).toExist();
-  });
+  getRowGuidePosition(resizeRef) {
+    if (resizeRef && this.grid) {
+      return (
+        resizeRef.getBoundingClientRect().bottom -
+        this.grid.getBoundingClientRect().top -
+        2
+      );
+    }
+    return null;
+  }
 
-  it('should render one DashboardComponent for each gridComponent child', () => {
-    const wrapper = setup({
-      gridComponent: { ...props.gridComponent, children: ['a', 'b'] },
-    });
-    expect(wrapper.find(DashboardComponent)).toHaveLength(2);
-  });
+  setGridRef(ref) {
+    this.grid = ref;
+  }
 
-  it('should render two empty DragDroppables in editMode to increase the drop target zone', () => {
-    const viewMode = setup({ editMode: false });
-    const editMode = setup({ editMode: true });
-    expect(viewMode.find(DragDroppable)).toHaveLength(0);
-    expect(editMode.find(DragDroppable)).toHaveLength(2);
-  });
+  handleResizeStart({ ref, direction }) {
+    let rowGuideTop = null;
+    if (direction === 'bottom' || direction === 'bottomRight') {
+      rowGuideTop = this.getRowGuidePosition(ref);
+    }
 
-  it('should render grid column guides when resizing', () => {
-    const wrapper = setup({ editMode: true });
-    expect(wrapper.find('.grid-column-guide')).not.toExist();
+    this.setState(() => ({
+      isResizing: true,
+      rowGuideTop,
+    }));
+  }
 
-    wrapper.setState({ isResizing: true });
+  handleResize({ ref, direction }) {
+    if (direction === 'bottom' || direction === 'bottomRight') {
+      this.setState(() => ({ rowGuideTop: this.getRowGuidePosition(ref) }));
+    }
+  }
 
-    expect(wrapper.find('.grid-column-guide')).toHaveLength(GRID_COLUMN_COUNT);
-  });
+  handleResizeStop({ id, widthMultiple: width, heightMultiple: height }) {
+    this.props.resizeComponent({ id, width, height });
 
-  it('should render a grid row guide when resizing', () => {
-    const wrapper = setup();
-    expect(wrapper.find('.grid-row-guide')).not.toExist();
-    wrapper.setState({ isResizing: true, rowGuideTop: 10 });
-    expect(wrapper.find('.grid-row-guide')).toExist();
-  });
+    this.setState(() => ({
+      isResizing: false,
+      rowGuideTop: null,
+    }));
+  }
 
-  it('should call resizeComponent when a child DashboardComponent calls resizeStop', () => {
-    const resizeComponent = sinon.spy();
-    const args = { id: 'id', widthMultiple: 1, heightMultiple: 3 };
-    const wrapper = setup({ resizeComponent });
-    const dashboardComponent = wrapper.find(DashboardComponent).first();
-    dashboardComponent.prop('onResizeStop')(args);
+  handleTopDropTargetDrop(dropResult) {
+    if (dropResult) {
+      this.props.handleComponentDrop({
+        ...dropResult,
+        destination: {
+          ...dropResult.destination,
+          // force appending as the first child if top drop target
+          index: 0,
+        },
+      });
+    }
+  }
 
-    expect(resizeComponent.callCount).toBe(1);
-    expect(resizeComponent.getCall(0).args[0]).toEqual({
-      id: 'id',
-      width: 1,
-      height: 3,
-    });
-  });
-});
+  handleChangeTab({ pathToTabIndex }) {
+    this.props.setDirectPathToChild(pathToTabIndex);
+  }
+
+  render() {
+    const {
+      gridComponent,
+      handleComponentDrop,
+      depth,
+      editMode,
+      width,
+      isComponentVisible,
+    } = this.props;
+    const columnPlusGutterWidth =
+      (width + GRID_GUTTER_SIZE) / GRID_COLUMN_COUNT;
+
+    const columnWidth = columnPlusGutterWidth - GRID_GUTTER_SIZE;
+    const { isResizing, rowGuideTop } = this.state;
+
+    return width < 100 ? null : (
+      <div className="dashboard-grid" ref={this.setGridRef}>
+        <div className="grid-content" data-test="grid-content">
+          {/* make the area above components droppable */}
+          {editMode && (
+            <DragDroppable
+              component={gridComponent}
+              depth={depth}
+              parentComponent={null}
+              index={0}
+              orientation="column"
+              onDrop={this.handleTopDropTargetDrop}
+              className="empty-droptarget"
+              editMode
+            >
+              {renderDraggableContentBottom}
+            </DragDroppable>
+          )}
+
+          {gridComponent.children.map((id, index) => (
+            <DashboardComponent
+              key={id}
+              id={id}
+              parentId={gridComponent.id}
+              depth={depth + 1}
+              index={index}
+              availableColumnCount={GRID_COLUMN_COUNT}
+              columnWidth={columnWidth}
+              isComponentVisible={isComponentVisible}
+              onResizeStart={this.handleResizeStart}
+              onResize={this.handleResize}
+              onResizeStop={this.handleResizeStop}
+              onChangeTab={this.handleChangeTab}
+            />
+          ))}
+
+          {/* make the area below components droppable */}
+          {editMode && gridComponent.children.length > 0 && (
+            <DragDroppable
+              component={gridComponent}
+              depth={depth}
+              parentComponent={null}
+              index={gridComponent.children.length}
+              orientation="column"
+              onDrop={handleComponentDrop}
+              className="empty-droptarget"
+              editMode
+            >
+              {renderDraggableContentTop}
+            </DragDroppable>
+          )}
+
+          {isResizing &&
+            Array(GRID_COLUMN_COUNT)
+              .fill(null)
+              .map((_, i) => (
+                <div
+                  key={`grid-column-${i}`}
+                  className="grid-column-guide"
+                  style={{
+                    left: i * GRID_GUTTER_SIZE + i * columnWidth,
+                    width: columnWidth,
+                  }}
+                />
+              ))}
+
+          {isResizing && rowGuideTop && (
+            <div
+              className="grid-row-guide"
+              style={{
+                top: rowGuideTop,
+                width,
+              }}
+            />
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+DashboardGrid.propTypes = propTypes;
+DashboardGrid.defaultProps = defaultProps;
+
+export default DashboardGrid;
