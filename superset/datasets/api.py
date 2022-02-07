3e5957ab53600da689028afd5afa2fc34a4158a2
Ville Brofeldt <33317356+villebro@users.noreply.github.com>
2022-02-07 09:38:55 +0200

    docs: add password example to dataset import API (#18580)

--- superset/datasets/api.py
+++ superset/datasets/api.py
@@ -697,7 +697,12 @@ class DatasetRestApi(BaseSupersetModelRestApi):
                       type: string
                       format: binary
                     passwords:
-                      description: JSON map of passwords for each file
+                      description: >-
+                        JSON map of passwords for each featured database in the
+                        ZIP file. If the ZIP includes a database config in the path
+                        `databases/MyDatabase.yaml`, the password should be provided
+                        in the following format:
+                        `{"databases/MyDatabase.yaml": "my_password"}`.
                       type: string
                     overwrite:
                       description: overwrite existing datasets?
