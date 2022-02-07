1cd1d32fbf9804980fcceb27c287d388af61a797
Phillip Kelley-Dotson <pkelleydotson@yahoo.com>
2022-02-07 11:21:39 -0800

    Update SqlAlchemyForm.tsx

--- superset-frontend/src/views/CRUD/data/database/DatabaseModal/SqlAlchemyForm.tsx
+++ superset-frontend/src/views/CRUD/data/database/DatabaseModal/SqlAlchemyForm.tsx
@@ -19,7 +19,6 @@
 import React, { EventHandler, ChangeEvent, MouseEvent } from 'react';
 import { t, SupersetTheme } from '@superset-ui/core';
 import SupersetText from 'src/utils/textUtils';
-// import SupersetText, { SupersetTextType, st } from 'src/utils/textUtils';
 import Button from 'src/components/Button';
 import { StyledInputContainer, wideButton } from './styles';
 
