4745dada14400b8782a568b35a826d953af0c3bc
cccs-Dustin <96579982+cccs-Dustin@users.noreply.github.com>
2022-02-07 19:07:51 +0000

    [CLDN-1229] Fixed assistiveText message

--- superset-frontend/src/explore/components/controls/SelectControl.jsx
+++ superset-frontend/src/explore/components/controls/SelectControl.jsx
@@ -282,6 +282,16 @@ export default class SelectControl extends React.PureComponent {
       tokenSeparators,
     };
 
+    const assistiveTextContents = inputArray => {
+      if (inputArray !== null) {
+        if (inputArray.length === 0) {
+          return '';
+        }
+        return `${inputArray.length} option(s)`;
+      }
+      return '';
+    };
+
     const deprecatedProps = {
       autoFocus,
       'aria-label': label,
@@ -304,7 +314,7 @@ export default class SelectControl extends React.PureComponent {
       value: getValue(),
       options: this.state.options,
       placeholder,
-      assistiveText: '',
+      assistiveText: assistiveTextContents(value),
       promptTextCreator,
       selectRef: this.getSelectRef,
       valueKey,
