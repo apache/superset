9900d066fb6fa14549d39c393cf7370925eb04ae
Daniel Moraes <daniel.moraes@venturus.org.br>
2022-02-11 13:33:57 -0300

    feat: It is possible to pass attributes to embedded iframe

--- superset-embedded-sdk/src/index.ts
+++ superset-embedded-sdk/src/index.ts
@@ -36,6 +36,8 @@ export type EmbedDashboardParams = {
   supersetDomain: string
   /** The html element within which to mount the iframe */
   mountPoint: HTMLElement
+  /** Attributes to be added to the iframe that will embed Superset */
+  iframeAttributes: Record<string, string> 
   /** A function to fetch a guest token from the Host App's backend server */
   fetchGuestToken: GuestTokenFetchFn
   /** Are we in debug mode? */
@@ -58,9 +60,11 @@ export async function embedDashboard({
   id,
   supersetDomain,
   mountPoint,
+  iframeAttributes,
   fetchGuestToken,
   debug = false
 }: EmbedDashboardParams): Promise<EmbeddedDashboard> {
+
   function log(...info: unknown[]) {
     if (debug) {
       console.debug(`[superset-embedded-sdk][dashboard ${id}]`, ...info);
@@ -73,6 +77,7 @@ export async function embedDashboard({
     return new Promise(resolve => {
       const iframe = document.createElement('iframe');
 
+      Object.entries(iframeAttributes).forEach(([key,value]) => iframe.setAttribute(key, value));
       // setup the iframe's sandbox configuration
       iframe.sandbox.add("allow-same-origin"); // needed for postMessage to work
       iframe.sandbox.add("allow-scripts"); // obviously the iframe needs scripts
