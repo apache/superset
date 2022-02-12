225015fd5a94c1a92cc9b110e3e3db5cd532d47b
David Aaron Suddjian <1858430+suddjian@users.noreply.github.com>
2022-02-11 20:29:23 -0800

    feat: Introduce a library for embedded iframe <-> host communication (#18652)
    * improved logging
    
    * switchboard, and window size
    
    * lint
    
    * tsconfig
    
    * fix path
    
    * fix release script
    
    * tests, debug mode, and error handling
    
    * moar debug mode
    
    * package lock
    
    * formatting
    
    * comment
    
    * wording
    
    * appease the coverage gods
    
    * Async assertions must be awaited or returned

--- superset-frontend/packages/superset-ui-switchboard/src/index.ts
+++ superset-frontend/packages/superset-ui-switchboard/src/index.ts
@@ -17,33 +17,4 @@
  * under the License.
  */
 
-const path = require('path');
-
-module.exports = {
-  entry: './src/index.ts',
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'bundle'),
-
-    // this exposes the library's exports under a global variable
-    library: {
-      name: "supersetEmbeddedSdk",
-      type: "umd"
-    }
-  },
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.[tj]s$/,
-        // babel-loader is faster than ts-loader because it ignores types.
-        // We do type checking in a separate process, so that's fine.
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-};
+export * from './switchboard';
