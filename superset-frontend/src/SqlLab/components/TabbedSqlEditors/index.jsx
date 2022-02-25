8073173d62f5003b135590a05a736896a9e33867
cccs-Dustin <96579982+cccs-Dustin@users.noreply.github.com>
2022-02-25 12:08:15 -0500

    Updated the code so that it adds to the untitled_query_numbers variable only if the character after the string 'Untitled Query ' is a number. This prevents any issues when trying to get the maximum value in the list.

--- superset-frontend/src/SqlLab/components/TabbedSqlEditors/index.jsx
+++ superset-frontend/src/SqlLab/components/TabbedSqlEditors/index.jsx
@@ -260,24 +260,23 @@ class TabbedSqlEditors extends React.PureComponent {
     let newTitle;
 
     if (this.props.queryEditors.length > 0) {
-      const untitled_query_names = this.props.queryEditors
-        .filter(x => x.title.includes('Untitled Query'))
-        .map(x => x.title);
-      if (untitled_query_names.length > 0) {
-        //When there are editors open, and at least one is called "Untitled Query #"
-        const largest_Number = Math.max.apply(
-          null,
-          untitled_query_names.map(x =>
-            x.replace('Untitled Query ', ''),
-          ),
-        );
+      const untitled_query_numbers = this.props.queryEditors
+        .filter(x => x.title.includes('Untitled Query '))
+        .map(x => x.title).map(x =>
+          x.replace('Untitled Query ', ''),
+        ).filter(x => !isNaN(x));
+      if (untitled_query_numbers.length > 0) {
+        //When there are query tabs open, and at least one is called "Untitled Query #"
+        //Where # is a valid number
+        const largest_Number = Math.max.apply(null, untitled_query_numbers);
         newTitle = t('Untitled Query %s', largest_Number + 1);
       } else {
-        //When there are editors open but none of them are called "Untitled Query #"
+        //When there are query tabs open but none of them are called "Untitled Query #"
+        //Where # is a valid number
         newTitle = 'Untitled Query 1';
       }
     } else {
-      //When there are no editors currently open
+      //When there are no query tabs currently open
       newTitle = 'Untitled Query 1';
     }
 
