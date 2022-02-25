fd314b73b6ef2f805e7dc625c5846e22ed838ac1
John Bodley <4567245+john-bodley@users.noreply.github.com>
2022-02-26 07:47:51 +1300

    Update manager.py

--- superset/security/manager.py
+++ superset/security/manager.py
@@ -93,7 +93,7 @@ class SupersetRoleListWidget(ListWidget):  # pylint: disable=too-few-public-meth
     """
 
     template = "superset/fab_overrides/list_role.html"
-    
+
     def __init__(self, **kwargs: Any) -> None:
         kwargs["appbuilder"] = current_app.appbuilder
         super().__init__(**kwargs)
