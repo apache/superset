fb1343bee81f94dbc7b93480e606468eec4f51e7
cccs-Dustin <96579982+cccs-Dustin@users.noreply.github.com>
2022-02-25 08:47:57 -0500

    Fixed a pylint error where a line was too long

--- superset/views/database/mixins.py
+++ superset/views/database/mixins.py
@@ -146,8 +146,9 @@ class DatabaseMixin:
             "This should be used with Presto DBs so that the syntax is correct<br/>"
             "5. The ``allows_virtual_table_explore`` field is a boolean specifying "
             "whether or not the Explore button in SQL Lab results is shown<br/>"
-            "6. The ``allows_preview_data`` field is a boolean specifying whether or not data "
-            "preview queries will be run when fetching table metadata in SQL Lab.",
+            "6. The ``allows_preview_data`` field is a boolean specifying whether or"
+            "not data preview queries will be run when fetching table metadata in"
+            "SQL Lab.",
             True,
         ),
         "encrypted_extra": utils.markdown(
