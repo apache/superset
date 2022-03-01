d2796c3f17cad941304fd6735efc1c0191c76551
Phillip Kelley-Dotson <pkelleydotson@yahoo.com>
2022-02-28 18:17:55 -0800

    fix lint

--- superset-frontend/src/views/CRUD/data/database/DatabaseList.test.jsx
+++ superset-frontend/src/views/CRUD/data/database/DatabaseList.test.jsx
@@ -18,7 +18,7 @@
  */
 import React from 'react';
 import thunk from 'redux-thunk';
-import * as redux from 'react-redux'
+import * as redux from 'react-redux';
 import configureStore from 'redux-mock-store';
 import fetchMock from 'fetch-mock';
 import { Provider } from 'react-redux';
@@ -40,19 +40,17 @@ import { act } from 'react-dom/test-utils';
 
 // store needed for withToasts(DatabaseList)
 const mockStore = configureStore([thunk]);
-const store = mockStore({ common: { config: {
-  CSV_EXTENSIONS: ['hello']
-} }});
+const store = mockStore({});
 
-const mockAppState = { 
-  common: { 
+const mockAppState = {
+  common: {
     config: {
       CSV_EXTENSIONS: ['csv'],
       EXCEL_EXTENSIONS: ['xls', 'xlsx'],
       COLUMNAR_EXTENSIONS: ['parquet', 'zip'],
       ALLOWED_EXTENSIONS: ['parquet', 'zip', 'xls', 'xlsx', 'csv'],
-    } 
-  }
+    },
+  },
 };
 
 const databasesInfoEndpoint = 'glob:*/api/v1/database/_info*';
@@ -112,7 +110,6 @@ fetchMock.get(databaseRelatedEndpoint, {
 const useSelectorMock = jest.spyOn(redux, 'useSelector');
 
 describe('DatabaseList', () => {
-
   useSelectorMock.mockReturnValue({
     CSV_EXTENSIONS: ['csv'],
     EXCEL_EXTENSIONS: ['xls', 'xlsx'],
@@ -129,7 +126,7 @@ describe('DatabaseList', () => {
   beforeAll(async () => {
     await waitForComponentToPaint(wrapper);
   });
-  
+
   it('renders', () => {
     expect(wrapper.find(DatabaseList)).toExist();
   });
@@ -221,7 +218,7 @@ describe('RTL', () => {
           <DatabaseList user={mockUser} />
         </QueryParamProvider>,
         { useRedux: true },
-        mockAppState 
+        mockAppState,
       );
     });
 
