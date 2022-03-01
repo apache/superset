178736ad27ac044817796d1b541458788e5c7bbc
smileydev <47900232+prosdev0107@users.noreply.github.com>
2022-03-01 10:05:11 -0500

    fix(tooltip-not-dissapeared): make the tooltip disappear when mouse away (#18892)
    * fix(tooltip-not-dissapeared): make the tooltip disappear when mouse away
    
    * fix(tooltip-not-dissapeared): change css props instead of sytles props
    
    (cherry picked from commit ca93d63bbb72630f90afcd7fc514014cad9b3e52)

--- superset-frontend/src/components/Button/index.tsx
+++ superset-frontend/src/components/Button/index.tsx
@@ -206,6 +206,7 @@ export default function Button(props: ButtonProps) {
             buttonStyle === 'link' ? 'transparent' : backgroundColorDisabled,
           borderColor:
             buttonStyle === 'link' ? 'transparent' : borderColorDisabled,
+          pointerEvents: 'none',
         },
         marginLeft: 0,
         '& + .superset-button': {
@@ -230,7 +231,11 @@ export default function Button(props: ButtonProps) {
       >
         {/* this ternary wraps the button in a span so that the tooltip shows up
         when the button is disabled.  */}
-        {disabled ? <span>{button}</span> : button}
+        {disabled ? (
+          <span css={{ cursor: 'not-allowed' }}>{button}</span>
+        ) : (
+          button
+        )}
       </Tooltip>
     );
   }
