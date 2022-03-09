253f80ab6d90d6c3c8e9337be2545779ead2c943
Elizabeth Thompson <eschutho@gmail.com>
2022-03-08 17:31:19 -0800

    chore: log multiple errors (#14064)
    * log all errors from db create
    
    * return unique set of errors
    
    * sort set for exceptions list
    
    * run black
    
    (cherry picked from commit c143b3712800d2d74c395525e39e7b87beb12c53)

--- superset/commands/exceptions.py
+++ superset/commands/exceptions.py
@@ -66,6 +66,9 @@ class CommandInvalidError(CommandException):
     def add_list(self, exceptions: List[ValidationError]) -> None:
         self._invalid_exceptions.extend(exceptions)
 
+    def get_list_classnames(self) -> List[str]:
+        return list(sorted({ex.__class__.__name__ for ex in self._invalid_exceptions}))
+
     def normalized_messages(self) -> Dict[Any, Any]:
         errors: Dict[Any, Any] = {}
         for exception in self._invalid_exceptions:
