ac791d75292f870d974572e6d2a4ece7ad4c7e37
smileydev <47900232+prosdev0107@users.noreply.github.com>
2022-03-10 06:59:07 -0500

    fix(plugin-chart-echarts): make to allow the custome of x & y axis title margin i… (#18947)
    * fix(chartviz): make to allow the custome of x & y axis title margin in  chart
    
    * fix(chartviz): add eslint radix error in chart.js
    
    * fix(chartviz): change the transformProps in chart plugin & creat helper.
    
    * fix(chartviz): lint fix & chart.js back
    
    * fix(plugin-chart-echarts): make to allow the custom margin of X & y axis in BoxPlot & Mixedtimeseries charts
    
    * fix(plugin-chart-echarts): make to change changeNumber to changeInteger
    
    * fix(plugin-chart-echarts): make to add license & change file name

--- superset-frontend/plugins/plugin-chart-echarts/src/MixedTimeseries/transformProps.ts
+++ superset-frontend/plugins/plugin-chart-echarts/src/MixedTimeseries/transformProps.ts
@@ -50,6 +50,7 @@ import {
   formatForecastTooltipSeries,
   rebaseForecastDatum,
 } from '../utils/forecast';
+import { convertInteger } from '../utils/convertInteger';
 import { defaultGrid, defaultTooltip, defaultYAxis } from '../defaults';
 import {
   getPadding,
@@ -251,8 +252,8 @@ export default function transformProps(
     null,
     addXAxisTitleOffset,
     yAxisTitlePosition,
-    yAxisTitleMargin,
-    xAxisTitleMargin,
+    convertInteger(yAxisTitleMargin),
+    convertInteger(xAxisTitleMargin),
   );
   const labelMap = rawSeriesA.reduce((acc, datum) => {
     const label = datum.name as string;
@@ -282,7 +283,7 @@ export default function transformProps(
     xAxis: {
       type: 'time',
       name: xAxisTitle,
-      nameGap: xAxisTitleMargin,
+      nameGap: convertInteger(xAxisTitleMargin),
       nameLocation: 'middle',
       axisLabel: {
         formatter: xAxisFormatter,
@@ -300,7 +301,7 @@ export default function transformProps(
         axisLabel: { formatter },
         scale: truncateYAxis,
         name: yAxisTitle,
-        nameGap: yAxisTitleMargin,
+        nameGap: convertInteger(yAxisTitleMargin),
         nameLocation: yAxisTitlePosition === 'Left' ? 'middle' : 'end',
         alignTicks,
       },
