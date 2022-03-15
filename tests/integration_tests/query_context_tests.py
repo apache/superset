9603127d7ac9931a8abd8dfab342e96f5e38f83b
Yongjie Zhao <yongjie.zhao@gmail.com>
2022-03-15 13:04:54 +0800

    clean up

--- tests/integration_tests/query_context_tests.py
+++ tests/integration_tests/query_context_tests.py
@@ -14,8 +14,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-import datetime
-import logging
 import re
 import time
 from typing import Any, Dict
