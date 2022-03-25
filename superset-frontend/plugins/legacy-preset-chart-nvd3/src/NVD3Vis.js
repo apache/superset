8c68bee7407309ad5e790d21fbbdfc06f9036df7
SpiderMan <codemaster08240328@gmail.com>
2022-03-25 12:34:11 -0400

    fix: Reset sorting bar issue in Barchart

--- superset-frontend/plugins/legacy-preset-chart-nvd3/src/NVD3Vis.js
+++ superset-frontend/plugins/legacy-preset-chart-nvd3/src/NVD3Vis.js
@@ -259,7 +259,7 @@ const formatter = getNumberFormatter();
 
 function nvd3Vis(element, props) {
   const {
-    data,
+    data: propsData,
     width: maxWidth,
     height: maxHeight,
     annotationData,
@@ -315,6 +315,8 @@ function nvd3Vis(element, props) {
     yIsLogScale,
     sliceId,
   } = props;
+
+  let data = JSON.parse(JSON.stringify(propsData));
 
   const isExplore = document.querySelector('#explorer-container') !== null;
   const container = element;
