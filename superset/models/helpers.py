52e500cba900710ea95b7d17630d6c1c291c21aa
Jesse Yang <jesse.yang@airbnb.com>
2022-03-28 16:36:58 -0700

    refactor: rewrite new dataset models with INSERT FROM

--- superset/models/helpers.py
+++ superset/models/helpers.py
@@ -477,7 +477,7 @@ class ExtraJSONMixin:
     @property
     def extra(self) -> Dict[str, Any]:
         try:
-            return json.loads(self.extra_json)
+            return json.loads(self.extra_json) if self.extra_json else {}
         except (TypeError, JSONDecodeError) as exc:
             logger.error(
                 "Unable to load an extra json: %r. Leaving empty.", exc, exc_info=True
@@ -522,7 +522,10 @@ class CertificationMixin:
 
 
 def clone_model(
-    target: Model, ignore: Optional[List[str]] = None, **kwargs: Any
+    target: Model,
+    ignore: Optional[List[str]] = None,
+    additional: Optional[List[str]] = None,
+    **kwargs: Any,
 ) -> Model:
     """
     Clone a SQLAlchemy model.
@@ -532,7 +535,7 @@ def clone_model(
     table = target.__table__
     data = {
         attr: getattr(target, attr)
-        for attr in table.columns.keys()
+        for attr in list(table.columns.keys()) + (additional or [])
         if attr not in table.primary_key.columns.keys() and attr not in ignore
     }
     data.update(kwargs)
