c50e920555b21a89b5a5f088dcdcda388f66eef4
David Aaron Suddjian <aasuddjian@gmail.com>
2022-03-29 11:01:24 -0700

    use the curator check

--- superset-frontend/src/dashboard/components/Header/index.jsx
+++ superset-frontend/src/dashboard/components/Header/index.jsx
@@ -672,7 +672,7 @@ class Header extends React.PureComponent {
             />
           )}
 
-          {isFeatureEnabled(FeatureFlag.EMBEDDED_SUPERSET) && (
+          {userCanCurate && (
             <DashboardEmbedModal
               show={this.state.showingEmbedModal}
               onHide={this.hideEmbedModal}
