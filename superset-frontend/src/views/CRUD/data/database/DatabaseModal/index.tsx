35b0a78475bed9dc201e2319637a95d239b0ab0d
SpiderMan <codemaster08240328@gmail.com>
2022-03-30 17:24:20 -0400

    feat: Remove legacy sql alchemy db connection link from G Sheet connection

--- superset-frontend/src/views/CRUD/data/database/DatabaseModal/index.tsx
+++ superset-frontend/src/views/CRUD/data/database/DatabaseModal/index.tsx
@@ -1318,32 +1318,36 @@ const DatabaseModal: FunctionComponent<DatabaseModalProps> = ({
                   validationErrors={validationErrors}
                 />
                 <div css={(theme: SupersetTheme) => infoTooltip(theme)}>
-                  <Button
-                    data-test="sqla-connect-btn"
-                    buttonStyle="link"
-                    onClick={() =>
-                      setDB({
-                        type: ActionType.configMethodChange,
-                        payload: {
-                          engine: db.engine,
-                          configuration_method:
-                            CONFIGURATION_METHOD.SQLALCHEMY_URI,
-                          database_name: db.database_name,
-                        },
-                      })
-                    }
-                    css={buttonLinkStyles}
-                  >
-                    {t(
-                      'Connect this database with a SQLAlchemy URI string instead',
-                    )}
-                  </Button>
-                  <InfoTooltip
-                    tooltip={t(
-                      'Click this link to switch to an alternate form that allows you to input the SQLAlchemy URL for this database manually.',
-                    )}
-                    viewBox="0 -6 24 24"
-                  />
+                  {dbModel.name !== 'Google Sheets' && (
+                    <>
+                      <Button
+                        data-test="sqla-connect-btn"
+                        buttonStyle="link"
+                        onClick={() =>
+                          setDB({
+                            type: ActionType.configMethodChange,
+                            payload: {
+                              engine: db.engine,
+                              configuration_method:
+                                CONFIGURATION_METHOD.SQLALCHEMY_URI,
+                              database_name: db.database_name,
+                            },
+                          })
+                        }
+                        css={buttonLinkStyles}
+                      >
+                        {t(
+                          'Connect this database with a SQLAlchemy URI string instead',
+                        )}
+                      </Button>
+                      <InfoTooltip
+                        tooltip={t(
+                          'Click this link to switch to an alternate form that allows you to input the SQLAlchemy URL for this database manually.',
+                        )}
+                        viewBox="0 -6 24 24"
+                      />
+                    </>
+                  )}
                 </div>
                 {/* Step 2 */}
                 {showDBError && errorAlert()}
