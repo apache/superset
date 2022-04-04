677c546151b5e5965900722990688a7bfa6c79ec
lyndsiWilliams <kcatgirl@gmail.com>
2022-04-04 11:23:18 -0500

    Removed animation from import db button

--- superset-frontend/src/views/CRUD/data/database/DatabaseModal/index.tsx
+++ superset-frontend/src/views/CRUD/data/database/DatabaseModal/index.tsx
@@ -1454,6 +1454,7 @@ const DatabaseModal: FunctionComponent<DatabaseModalProps> = ({
                   <Button
                     data-test="import-database-btn"
                     buttonStyle="link"
+                    type="link"
                     css={importDbButtonLinkStyles}
                   >
                     {t('Import database from file')}
