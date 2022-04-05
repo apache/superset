fe0ba91cdd8fc6671625dcb036aca7fe64fa6907
Craig <craig@craigrueda.com>
2022-04-05 09:38:11 -0700

    Fixing import

--- tests/integration_tests/utils_tests.py
+++ tests/integration_tests/utils_tests.py
@@ -24,6 +24,8 @@ import os
 import re
 from typing import Any, Tuple, List, Optional
 from unittest.mock import Mock, patch
+
+from superset.databases.commands.exceptions import DatabaseInvalidError
 from tests.integration_tests.fixtures.birth_names_dashboard import (
     load_birth_names_dashboard_with_slices,
     load_birth_names_data,
