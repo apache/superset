d088f14956d790f33780f2146ac0ef5c43eaa5f3
Phillip Kelley-Dotson <pkelleydotson@yahoo.com>
2022-04-11 15:50:46 -0700

    run lint

--- superset-frontend/src/components/DatabaseSelector/index.tsx
+++ superset-frontend/src/components/DatabaseSelector/index.tsx
@@ -16,7 +16,14 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import React, { ReactNode, useState, useMemo, useEffect, Dispatch, SetStateAction } from 'react';
+import React, {
+  ReactNode,
+  useState,
+  useMemo,
+  useEffect,
+  Dispatch,
+  SetStateAction,
+} from 'react';
 import { styled, SupersetClient, t } from '@superset-ui/core';
 import rison from 'rison';
 import { Select } from 'src/components';
