b02efef6a671129dae2092f1a174dac78c8e0ead
Josh Soref <jsoref@users.noreply.github.com>
2022-04-11 21:36:58 -0400

    spelling: extension
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- superset-frontend/src/views/CRUD/data/database/DatabaseList.tsx
+++ superset-frontend/src/views/CRUD/data/database/DatabaseList.tsx
@@ -36,7 +36,7 @@ import Icons from 'src/components/Icons';
 import { isUserAdmin } from 'src/dashboard/util/permissionUtils';
 import ListView, { FilterOperator, Filters } from 'src/components/ListView';
 import handleResourceExport from 'src/utils/export';
-import { ExtentionConfigs } from 'src/views/components/types';
+import { ExtensionConfigs } from 'src/views/components/types';
 import { UserWithPermissionsAndRoles } from 'src/types/bootstrapTypes';
 import type { MenuObjectProps } from 'src/types/bootstrapTypes';
 import DatabaseModal from './DatabaseModal';
@@ -119,7 +119,7 @@ function DatabaseList({ addDangerToast, addSuccessToast }: DatabaseListProps) {
     COLUMNAR_EXTENSIONS,
     EXCEL_EXTENSIONS,
     ALLOWED_EXTENSIONS,
-  } = useSelector<any, ExtentionConfigs>(state => state.common.conf);
+  } = useSelector<any, ExtensionConfigs>(state => state.common.conf);
 
   useEffect(() => {
     if (query?.databaseAdded) {
