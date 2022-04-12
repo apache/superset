cdb49606c7df787aba29b48355a3536225084c60
Josh Soref <jsoref@users.noreply.github.com>
2022-04-11 21:37:02 -0400

    spelling: nativefilter
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- superset-frontend/cypress-base/cypress/integration/dashboard/key_value.test.ts
+++ superset-frontend/cypress-base/cypress/integration/dashboard/key_value.test.ts
@@ -27,7 +27,7 @@ interface QueryString {
   native_filters_key: string;
 }
 
-xdescribe('nativefiler url param key', () => {
+xdescribe('nativefilter url param key', () => {
   // const urlParams = { param1: '123', param2: 'abc' };
   before(() => {
     cy.login();
