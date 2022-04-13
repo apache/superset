8e326af3e69983de59d906fdb3c07cd9d1eac3ba
Beto Dealmeida <roberto@dealmeida.net>
2022-04-13 09:47:20 -0700

    chore: upgrade Pillow (#19674)

--- setup.py
+++ setup.py
@@ -163,7 +163,7 @@ setup(
             "snowflake-sqlalchemy==1.2.4"
         ],  # PINNED! 1.2.5 introduced breaking changes requiring sqlalchemy>=1.4.0
         "teradata": ["teradatasql>=16.20.0.23"],
-        "thumbnails": ["Pillow>=8.3.2, <10.0.0"],
+        "thumbnails": ["Pillow>=9.0.1, <10.0.0"],
         "vertica": ["sqlalchemy-vertica-python>=0.5.9, < 0.6"],
         "netezza": ["nzalchemy>=11.0.2"],
     },
