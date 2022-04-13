26a0f0575931850f81a593785d29232f40fd7d71
Diego Medina <diegomedina24@gmail.com>
2022-04-13 10:42:12 -0400

    fix(sql lab): table selector should display all the selected tables (#19257)
    * fix: table Selector should clear once selected
    
    * Multi select
    
    * Add tests
    
    * refactor
    
    * PR comments

--- superset-frontend/src/components/Datasource/DatasourceEditor.jsx
+++ superset-frontend/src/components/Datasource/DatasourceEditor.jsx
@@ -1008,7 +1008,7 @@ class DatasourceEditor extends React.PureComponent {
                         handleError={this.props.addDangerToast}
                         schema={datasource.schema}
                         sqlLabMode={false}
-                        tableName={datasource.table_name}
+                        tableValue={datasource.table_name}
                         onSchemaChange={
                           this.state.isEditMode
                             ? schema =>
@@ -1024,7 +1024,7 @@ class DatasourceEditor extends React.PureComponent {
                                 )
                             : undefined
                         }
-                        onTableChange={
+                        onTableSelectChange={
                           this.state.isEditMode
                             ? table =>
                                 this.onDatasourcePropChange('table_name', table)
