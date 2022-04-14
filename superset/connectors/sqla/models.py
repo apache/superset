63650bca7171b77b45612a948414d59d0bdd0f0c
Ville Brofeldt <33317356+villebro@users.noreply.github.com>
2022-04-14 17:52:37 +0300

    fix: create virtual table with exotic type (#19714)
    * fix: create virtual table with exotic type
    
    * fix test

--- superset/connectors/sqla/models.py
+++ superset/connectors/sqla/models.py
@@ -2375,11 +2375,10 @@ class SqlaTable(Model, BaseDatasource):  # pylint: disable=too-many-public-metho
             referenced_tables = parsed.tables
             tables = load_or_create_tables(
                 session,
-                dataset.database_id,
+                database,
                 dataset.schema,
                 referenced_tables,
                 conditional_quote,
-                engine,
             )
 
         # create the new dataset
