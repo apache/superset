6a95ab21071e6b8f958720eaaa5d7fd9e515c8dc
Phillip Kelley-Dotson <pkelleydotson@yahoo.com>
2022-04-18 16:38:56 -0700

    fix assertions

--- tests/unit_tests/db_engine_specs/test_gsheets.py
+++ tests/unit_tests/db_engine_specs/test_gsheets.py
@@ -101,7 +101,11 @@ def test_validate_parameters_catalog(
             },
         ),
         SupersetError(
-            message="URL could not be identified",
+            message=(
+                "The URL could not be identified. Please check for typos "
+                "and make sure that ‘Type of Google Sheets allowed’ "
+                "selection matches the input."
+            ),
             error_type=SupersetErrorType.TABLE_DOES_NOT_EXIST_ERROR,
             level=ErrorLevel.WARNING,
             extra={
