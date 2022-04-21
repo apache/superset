6239e2f085dc06aa8e9c434083c9f16207fa0370
Ville Brofeldt <ville.v.brofeldt@gmail.com>
2022-04-21 16:57:59 +0300

    fix(key_value): use longblob on mysql

--- superset/migrations/versions/6766938c6065_add_key_value_store.py
+++ superset/migrations/versions/6766938c6065_add_key_value_store.py
@@ -30,6 +30,7 @@ from uuid import uuid4
 
 import sqlalchemy as sa
 from alembic import op
+from sqlalchemy.dialects.mysql import LONGBLOB
 from sqlalchemy_utils import UUIDType
 
 
@@ -38,7 +39,9 @@ def upgrade():
         "key_value",
         sa.Column("id", sa.Integer(), nullable=False),
         sa.Column("resource", sa.String(32), nullable=False),
-        sa.Column("value", sa.LargeBinary(), nullable=False),
+        sa.Column(
+            "value", sa.LargeBinary().with_variant(LONGBLOB, "mysql"), nullable=False
+        ),
         sa.Column("uuid", UUIDType(binary=True), default=uuid4),
         sa.Column("created_on", sa.DateTime(), nullable=True),
         sa.Column("created_by_fk", sa.Integer(), nullable=True),
