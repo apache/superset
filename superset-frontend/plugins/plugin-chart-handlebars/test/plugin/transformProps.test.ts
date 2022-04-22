13c4ffeb7084d92ae7095f27fa517cf00d3fd021
Jeremy <jeremy@savantly.net>
2022-04-22 02:12:07 -0500

    (plugin-handlebars-chart) use numbers for size

--- superset-frontend/plugins/plugin-chart-handlebars/test/plugin/transformProps.test.ts
+++ superset-frontend/plugins/plugin-chart-handlebars/test/plugin/transformProps.test.ts
@@ -9,8 +9,8 @@ describe('Handlebars tranformProps', () => {
     granularitySqla: 'ds',
     metric: 'sum__num',
     groupby: ['name'],
-    width: '500',
-    height: '500',
+    width: 500,
+    height: 500,
     viz_type: 'handlebars',
   };
   const chartProps = new ChartProps<QueryFormData>({
