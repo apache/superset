bc3ee852666a59a53c50d16b2d291fc911bfa74d
Ville Brofeldt <ville.v.brofeldt@gmail.com>
2022-04-25 17:13:15 +0300

    fix test and add license headers

--- superset-frontend/plugins/plugin-chart-handlebars/test/plugin/transformProps.test.ts
+++ superset-frontend/plugins/plugin-chart-handlebars/test/plugin/transformProps.test.ts
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 import { ChartProps, QueryFormData } from '@superset-ui/core';
 import { HandlebarsQueryFormData } from '../../src/types';
 import transformProps from '../../src/plugin/transformProps';
@@ -25,13 +43,14 @@ describe('Handlebars tranformProps', () => {
   });
 
   it('should tranform chart props for viz', () => {
-    expect(transformProps(chartProps)).toEqual({
-      formData,
-      width: 800,
-      height: 600,
-      data: [
-        { name: 'Hulk', sum__num: 1, __timestamp: new Date(599616000000) },
-      ],
-    });
+    expect(transformProps(chartProps)).toEqual(
+      expect.objectContaining({
+        width: 800,
+        height: 600,
+        data: [
+          { name: 'Hulk', sum__num: 1, __timestamp: new Date(599616000000) },
+        ],
+      }),
+    );
   });
 });
