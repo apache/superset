8f7daa306c92a20bfda0f7a9ea871767acdd502d
Yongjie Zhao <yongjie.zhao@gmail.com>
2022-04-26 21:50:03 +0800

    feat: add AA into mixed time series chart

--- superset-frontend/plugins/plugin-chart-echarts/src/MixedTimeseries/buildQuery.ts
+++ superset-frontend/plugins/plugin-chart-echarts/src/MixedTimeseries/buildQuery.ts
@@ -21,76 +21,88 @@ import {
   QueryFormData,
   QueryObject,
   normalizeOrderBy,
+  PostProcessingPivot,
 } from '@superset-ui/core';
 import {
   pivotOperator,
   renameOperator,
   flattenOperator,
+  isTimeComparison,
+  timeComparePivotOperator,
+  rollingWindowOperator,
+  timeCompareOperator,
+  resampleOperator,
 } from '@superset-ui/chart-controls';
+import {
+  removeFormDataSuffix,
+  removeUnusedFormData,
+} from '../utils/removeFormDataSuffix';
 
 export default function buildQuery(formData: QueryFormData) {
-  const {
-    adhoc_filters,
-    adhoc_filters_b,
-    groupby,
-    groupby_b,
-    limit,
-    limit_b,
-    timeseries_limit_metric,
-    timeseries_limit_metric_b,
-    metrics,
-    metrics_b,
-    order_desc,
-    order_desc_b,
-    ...baseFormData
-  } = formData;
-  baseFormData.is_timeseries = true;
-  const formData1 = {
-    ...baseFormData,
-    adhoc_filters,
-    columns: groupby,
-    limit,
-    timeseries_limit_metric,
-    metrics,
-    order_desc,
+  const baseFormData = {
+    ...formData,
+    is_timeseries: true,
+    columns: formData.groupby,
+    columns_b: formData.groupby_b,
   };
-  const formData2 = {
-    ...baseFormData,
-    adhoc_filters: adhoc_filters_b,
-    columns: groupby_b,
-    limit: limit_b,
-    timeseries_limit_metric: timeseries_limit_metric_b,
-    metrics: metrics_b,
-    order_desc: order_desc_b,
-  };
+  const formData1 = removeUnusedFormData(baseFormData, '_b');
+  const formData2 = removeFormDataSuffix(baseFormData, '_b');
 
   const queryContextA = buildQueryContext(formData1, baseQueryObject => {
-    const queryObjectA = {
+    const queryObject = {
       ...baseQueryObject,
       is_timeseries: true,
+    };
+
+    const pivotOperatorInRuntime: PostProcessingPivot = isTimeComparison(
+      formData1,
+      queryObject,
+    )
+      ? timeComparePivotOperator(formData1, queryObject)
+      : pivotOperator(formData1, queryObject);
+
+    const queryObjectA = {
+      ...queryObject,
+      time_offsets: isTimeComparison(formData1, queryObject)
+        ? formData1.time_compare
+        : [],
       post_processing: [
-        pivotOperator(formData1, { ...baseQueryObject, is_timeseries: true }),
-        renameOperator(formData1, {
-          ...baseQueryObject,
-          ...{ is_timeseries: true },
-        }),
-        flattenOperator(formData1, baseQueryObject),
+        pivotOperatorInRuntime,
+        rollingWindowOperator(formData1, queryObject),
+        timeCompareOperator(formData1, queryObject),
+        resampleOperator(formData1, queryObject),
+        renameOperator(formData1, queryObject),
+        flattenOperator(formData1, queryObject),
       ],
     } as QueryObject;
     return [normalizeOrderBy(queryObjectA)];
   });
 
   const queryContextB = buildQueryContext(formData2, baseQueryObject => {
-    const queryObjectB = {
+    const queryObject = {
       ...baseQueryObject,
       is_timeseries: true,
+    };
+
+    const pivotOperatorInRuntime: PostProcessingPivot = isTimeComparison(
+      formData2,
+      queryObject,
+    )
+      ? timeComparePivotOperator(formData2, queryObject)
+      : pivotOperator(formData2, queryObject);
+
+    const queryObjectB = {
+      ...queryObject,
+      time_offsets: isTimeComparison(formData2, queryObject)
+        ? formData2.time_compare
+        : [],
       post_processing: [
-        pivotOperator(formData2, { ...baseQueryObject, is_timeseries: true }),
-        renameOperator(formData2, {
-          ...baseQueryObject,
-          ...{ is_timeseries: true },
-        }),
-        flattenOperator(formData2, baseQueryObject),
+        pivotOperatorInRuntime,
+        rollingWindowOperator(formData2, queryObject),
+        timeCompareOperator(formData2, queryObject),
+        resampleOperator(formData2, queryObject),
+        renameOperator(formData2, queryObject),
+        flattenOperator(formData2, queryObject),
       ],
     } as QueryObject;
     return [normalizeOrderBy(queryObjectB)];
