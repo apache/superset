9327723fad999367c2b6cddad437793034001f0a
Luis Casillas <casillasljaime@gmail.com>
2022-04-28 16:59:27 -0500

    Update superset-frontend/src/dashboard/components/Header/HeaderActionsDropdown/index.jsx
    Co-authored-by: Evan Rusackas <evan@preset.io>

--- superset-frontend/src/dashboard/components/Header/HeaderActionsDropdown/index.jsx
+++ superset-frontend/src/dashboard/components/Header/HeaderActionsDropdown/index.jsx
@@ -158,7 +158,7 @@ class HeaderActionsDropdown extends React.PureComponent {
     switch (key) {
       case MENU_KEYS.REFRESH_DASHBOARD:
         this.props.forceRefreshAllCharts();
-        this.props.addSuccessToast(t('Started refreshing Charts'));
+        this.props.addSuccessToast(t('Refreshing Charts'));
         break;
       case MENU_KEYS.EDIT_PROPERTIES:
         this.props.showPropertiesModal();
