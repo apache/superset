a24be83ec25f9ed9acf18987c5a76a845fcd62c6
Stephen Liu <750188453@qq.com>
2022-05-04 21:33:57 +0800

    Update superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/Regular/Bar/controlPanel.tsx
    Co-authored-by: Ville Brofeldt <33317356+villebro@users.noreply.github.com>

--- superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/Regular/Bar/controlPanel.tsx
+++ superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/Regular/Bar/controlPanel.tsx
@@ -95,7 +95,7 @@ const config: ControlPanelConfig = {
       controlSetRows: [
         [
           {
-            name: 'bar_orient',
+            name: 'orientation',
             config: {
               type: 'RadioButtonControl',
               renderTrigger: true,
