6231f6b6e45425e7a09fecc31b540537a7ce86ae
Diego Medina <diegomedina24@gmail.com>
2022-05-13 10:14:30 -0300

    fix: Support the Clipboard API in modern browsers

--- superset-frontend/src/utils/copy.ts
+++ superset-frontend/src/utils/copy.ts
@@ -17,40 +17,73 @@
  * under the License.
  */
 
-const copyTextToClipboard = async (text: string) =>
-  new Promise<void>((resolve, reject) => {
-    const selection: Selection | null = document.getSelection();
-    if (selection) {
-      selection.removeAllRanges();
-      const range = document.createRange();
-      const span = document.createElement('span');
-      span.textContent = text;
-      span.style.position = 'fixed';
-      span.style.top = '0';
-      span.style.clip = 'rect(0, 0, 0, 0)';
-      span.style.whiteSpace = 'pre';
+// Use the new Clipboard API if the browser supports it
+const copyTextWithClipboardApi = async (getText: () => Promise<string>) => {
+  try {
+    // Safari (WebKit) does not support delayed generation of clipboard.
+    // This means that writing to the clipboard, from the moment the user
+    // interacts with the app, must be instantaneous.
+    // However, neither writeText nor write accepts a Promise, so
+    // we need to create a ClipboardItem that accepts said Promise to
+    // delay the text generation, as needed.
+    // Source: https://bugs.webkit.org/show_bug.cgi?id=222262
+    const clipboardItem = new ClipboardItem({
+      'text/plain': getText(),
+    });
+    await navigator.clipboard.write([clipboardItem]);
+  } catch {
+    // For Blink, the above method won't work, but we can use the
+    // default (intended) API, since the delayed generation of the
+    // clipboard is now supported.
+    // Source: https://bugs.chromium.org/p/chromium/issues/detail?id=1014310
+    const text = await getText();
+    await navigator.clipboard.writeText(text);
+  }
+};
 
-      document.body.appendChild(span);
-      range.selectNode(span);
-      selection.addRange(range);
+const copyTextToClipboard = async (getText: () => Promise<string>) => {
+  try {
+    await copyTextWithClipboardApi(getText);
+  } catch {
+    // If the Clipboard API is not supported, fallback to the older method.
+    const text = await getText();
+    const copyPromise = new Promise<void>((resolve, reject) => {
+      const selection: Selection | null = document.getSelection();
+      if (selection) {
+        selection.removeAllRanges();
+        const range = document.createRange();
+        const span = document.createElement('span');
+        span.textContent = text;
+        span.style.position = 'fixed';
+        span.style.top = '0';
+        span.style.clip = 'rect(0, 0, 0, 0)';
+        span.style.whiteSpace = 'pre';
 
-      try {
-        if (!document.execCommand('copy')) {
+        document.body.appendChild(span);
+        range.selectNode(span);
+        selection.addRange(range);
+
+        try {
+          if (!document.execCommand('copy')) {
+            reject();
+          }
+        } catch (err) {
           reject();
         }
-      } catch (err) {
-        reject();
+
+        document.body.removeChild(span);
+        if (selection.removeRange) {
+          selection.removeRange(range);
+        } else {
+          selection.removeAllRanges();
+        }
       }
 
-      document.body.removeChild(span);
-      if (selection.removeRange) {
-        selection.removeRange(range);
-      } else {
-        selection.removeAllRanges();
-      }
-    }
+      resolve();
+    });
 
-    resolve();
-  });
+    await copyPromise;
+  }
+};
 
 export default copyTextToClipboard;
