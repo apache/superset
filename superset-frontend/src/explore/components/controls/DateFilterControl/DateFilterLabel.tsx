320ce633546d93fd8190aa46d0cd47b09ed38232
aurelie.ehanno <aurelie.ehanno@kosmos.fr>
2022-05-16 10:25:47 -0400

    wrong position of translation

--- superset-frontend/src/explore/components/controls/DateFilterControl/DateFilterLabel.tsx
+++ superset-frontend/src/explore/components/controls/DateFilterControl/DateFilterLabel.tsx
@@ -314,11 +314,11 @@ export default function DateFilterLabel(props: DateFilterControlProps) {
       <Divider />
       <div>
         <div className="section-title">{t('Actual time range')}</div>
-        {validTimeRange && <div>{evalResponse}</div>}
+        {validTimeRange && <div>{t(evalResponse)}</div>}
         {!validTimeRange && (
           <IconWrapper className="warning">
             <Icons.ErrorSolidSmall iconColor={theme.colors.error.base} />
-            <span className="text error">{t(evalResponse)}</span>
+            <span className="text error">{evalResponse}</span>
           </IconWrapper>
         )}
       </div>
