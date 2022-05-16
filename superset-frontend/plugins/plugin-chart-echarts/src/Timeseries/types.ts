bf993d54f54c810ffb76295e1015f257951f3584
Stephen Liu <750188453@qq.com>
2022-05-16 21:48:36 +0800

    feat(plugin-chart-echarts): support horizontal bar chart (#19918)
    * feat(plugin-chart-echarts): support horizontal bar chart
    
    * Update superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/Regular/Bar/controlPanel.tsx
    
    Co-authored-by: Evan Rusackas <evan@preset.io>
    
    * Update superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/transformProps.ts
    
    Co-authored-by: Ville Brofeldt <33317356+villebro@users.noreply.github.com>
    
    * Update superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/transformProps.ts
    
    Co-authored-by: Ville Brofeldt <33317356+villebro@users.noreply.github.com>
    
    * Update superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/Regular/Bar/controlPanel.tsx
    
    Co-authored-by: Ville Brofeldt <33317356+villebro@users.noreply.github.com>
    
    * improve controlpanel
    
    * default value
    
    * fix ut
    
    Co-authored-by: Evan Rusackas <evan@preset.io>
    Co-authored-by: Ville Brofeldt <33317356+villebro@users.noreply.github.com>

--- superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/types.ts
+++ superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/types.ts
@@ -38,6 +38,11 @@ export enum EchartsTimeseriesContributionType {
   Column = 'column',
 }
 
+export enum OrientationType {
+  vertical = 'vertical',
+  horizontal = 'horizontal',
+}
+
 export enum EchartsTimeseriesSeriesType {
   Line = 'line',
   Scatter = 'scatter',
@@ -82,6 +87,7 @@ export type EchartsTimeseriesFormData = QueryFormData & {
   showValue: boolean;
   onlyTotal: boolean;
   percentageThreshold: number;
+  orientation?: OrientationType;
 } & EchartsLegendFormData &
   EchartsTitleFormData;
 
@@ -119,6 +125,7 @@ export const DEFAULT_FORM_DATA: EchartsTimeseriesFormData = {
   showValue: false,
   onlyTotal: false,
   percentageThreshold: 0,
+  orientation: OrientationType.vertical,
   ...DEFAULT_TITLE_FORM_DATA,
 };
 
