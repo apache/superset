148f400af33fee11676c0726ef170a7effb1c2ad
Hugh A. Miles II <hughmil3s@gmail.com>
2022-05-18 18:14:46 -0400

    Update dao.py

--- superset/datasource/dao.py
+++ superset/datasource/dao.py
@@ -1,3 +1,5 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
 # regarding copyright ownership.  The ASF licenses this file
 # to you under the Apache License, Version 2.0 (the
