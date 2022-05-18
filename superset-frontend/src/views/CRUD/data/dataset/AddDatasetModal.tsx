0cb0727ead5aa0be8d3c663e6c4193aaff408ee0
Krishna Gopal <gopalk@usc.edu>
2022-05-17 19:58:53 -0500

    fix: add primary button loading state to modals (#20018)

--- superset-frontend/src/views/CRUD/data/dataset/AddDatasetModal.tsx
+++ superset-frontend/src/views/CRUD/data/dataset/AddDatasetModal.tsx
@@ -57,7 +57,10 @@ const DatasetModal: FunctionComponent<DatasetModalProps> = ({
   const [currentSchema, setSchema] = useState<string | undefined>('');
   const [currentTableName, setTableName] = useState('');
   const [disableSave, setDisableSave] = useState(true);
-  const { createResource } = useSingleViewResource<Partial<DatasetAddObject>>(
+  const {
+    createResource,
+    state: { loading },
+  } = useSingleViewResource<Partial<DatasetAddObject>>(
     'dataset',
     t('dataset'),
     addDangerToast,
@@ -115,6 +118,7 @@ const DatasetModal: FunctionComponent<DatasetModalProps> = ({
   return (
     <Modal
       disablePrimaryButton={disableSave}
+      primaryButtonLoading={loading}
       onHandledPrimaryAction={onSave}
       onHide={hide}
       primaryButtonName={t('Add')}
