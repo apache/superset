1b3a170b68d5c146dbd9a6b42210e6259bc09e40
Ville Brofeldt <33317356+villebro@users.noreply.github.com>
2022-05-19 13:51:52 +0300

    feat(plugin-chart-echarts): add support for generic axis to mixed chart (#20097)
    * feat(plugin-chart-echarts): add support for generic axis to mixed chart
    
    * fix tests + add new tests
    
    * address review comments
    
    * simplify control panel
    
    * fix types and tests

--- superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/buildQuery.ts
+++ superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/buildQuery.ts
@@ -20,9 +20,9 @@ import {
   buildQueryContext,
   DTTM_ALIAS,
   ensureIsArray,
-  QueryFormData,
   normalizeOrderBy,
   PostProcessingPivot,
+  QueryFormData,
 } from '@superset-ui/core';
 import {
   rollingWindowOperator,
@@ -94,13 +94,13 @@ export default function buildQuery(formData: QueryFormData) {
           resampleOperator(formData, baseQueryObject),
           renameOperator(formData, {
             ...baseQueryObject,
-            ...{ is_timeseries },
+            is_timeseries,
           }),
           contributionOperator(formData, baseQueryObject),
           flattenOperator(formData, baseQueryObject),
           // todo: move prophet before flatten
           prophetOperator(formData, baseQueryObject),
-        ].filter(op => op),
+        ].filter(Boolean),
       },
     ];
   });
