5890a52fc5adf2beeadc2dc629ff2040e6a45f56
Samira-El <samira.elaabidi@transferwise.com>
2022-05-25 16:35:27 +0300

    get_impersonation_key method in engine_spec class  to construct an impersonation key

--- superset/db_engine_specs/base.py
+++ superset/db_engine_specs/base.py
@@ -1537,6 +1537,18 @@ class BaseEngineSpec:  # pylint: disable=too-many-public-methods
     def parse_sql(cls, sql: str) -> List[str]:
         return [str(s).strip(" ;") for s in sqlparse.parse(sql)]
 
+    @classmethod
+    def get_impersonation_key(
+        cls,
+        username: Optional[str]
+    ) -> Any:
+        """
+        Construct an impersonation key, by default it's the given username.
+
+        :param username: logged in user's username
+        """
+        return username
+
 
 # schema for adding a database by providing parameters instead of the
 # full SQLAlchemy URI
