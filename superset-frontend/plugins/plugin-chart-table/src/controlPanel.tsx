d4387abadb0b1a20ddbc1753581f4e426365fa36
stevetracvc <70416691+stevetracvc@users.noreply.github.com>
2022-05-24 20:10:57 -0600

    feat: add drag and drop column rearrangement for table viz (#19381)

--- superset-frontend/plugins/plugin-chart-table/src/controlPanel.tsx
+++ superset-frontend/plugins/plugin-chart-table/src/controlPanel.tsx
@@ -457,6 +457,20 @@ const config: ControlPanelConfig = {
         ],
         [
           {
+            name: 'allow_rearrange_columns',
+            config: {
+              type: 'CheckboxControl',
+              label: t('Allow columns to be rearranged'),
+              renderTrigger: true,
+              default: false,
+              description: t(
+                "Allow end user to drag-and-drop column headers to rearrange them. Note their changes won't persist for the next time they open the chart.",
+              ),
+            },
+          },
+        ],
+        [
+          {
             name: 'column_config',
             config: {
               type: 'ColumnConfigControl',
