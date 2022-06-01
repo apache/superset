5dfc01612bec59074781a155bf1335c506be2aac
Lily Kuang <lily@preset.io>
2022-06-01 16:10:42 -0700

    feat(embedded): enforce allow domains

--- superset-frontend/src/types/bootstrapTypes.ts
+++ superset-frontend/src/types/bootstrapTypes.ts
@@ -64,6 +64,19 @@ export type DashboardResponse = {
   result: DashboardData[];
 };
 
+export type EmbeddedDashboard = {
+  allowed_domains: string[];
+  changed_by: {
+    first_name: string;
+    id: number;
+    last_name: string;
+    username: string;
+  };
+  changed_on: string;
+  dashboard_id: string;
+  uuid: string;
+};
+
 export interface CommonBootstrapData {
   flash_messages: string[][];
   conf: JsonObject;
