2c5a6050e1dd99524863a667f1259668747ba124
cccs-Dustin <96579982+cccs-Dustin@users.noreply.github.com>
2022-06-03 11:20:39 -0400

    Changed the Hogwarts Table viz so that when the emit filters checkbox is checked, an emit filters button is present in the context menu

--- superset-frontend/src/cccs-viz/plugins/plugin-chart-cccs-grid/src/CccsGrid.tsx
+++ superset-frontend/src/cccs-viz/plugins/plugin-chart-cccs-grid/src/CccsGrid.tsx
@@ -16,8 +16,17 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import React, { ChangeEvent, useCallback, useEffect, useRef, useState } from 'react';
-//import { styled } from '@superset-ui/core';
+import React, { ChangeEvent, useCallback, useEffect, useState } from 'react';
+// import { styled } from '@superset-ui/core';
+import { AgGridReact } from '@ag-grid-community/react';
+import {
+  AllModules,
+  LicenseManager,
+  MenuItemDef,
+  GetContextMenuItemsParams,
+} from '@ag-grid-enterprise/all-modules';
+import { NULL_STRING } from 'src/utils/common';
+import { ensureIsArray } from '@superset-ui/core';
 import { CccsGridTransformedProps } from './types';
 
 import CountryValueRenderer from './CountryValueRenderer';
@@ -27,21 +36,13 @@ import DomainValueRenderer from './DomainValueRenderer';
 import JsonValueRenderer from './JsonValueRenderer';
 import CustomTooltip from './CustomTooltip';
 
-//// jcc
+/// / jcc
 
-//'use strict';
+// 'use strict';
 
-import { AgGridReact } from '@ag-grid-community/react';
 import '@ag-grid-community/all-modules/dist/styles/ag-grid.css';
 import '@ag-grid-community/core/dist/styles/ag-grid.css';
 import '@ag-grid-community/core/dist/styles/ag-theme-balham.css';
-
-import { AllModules } from '@ag-grid-enterprise/all-modules';
-import { NULL_STRING } from 'src/utils/common';
-
-import { LicenseManager } from '@ag-grid-enterprise/all-modules';
-
-import { ensureIsArray } from '@superset-ui/core';
 
 const DEFAULT_COLUMN_DEF = {
   editable: false,
@@ -62,22 +63,17 @@ export default function CccsGrid({
   selectedValues,
   tooltipShowDelay,
   rowSelection,
-  emitFilter = false,
+  emitFilter,
   include_search,
-  page_length = 0,
   filters: initialFilters = {},
 }: CccsGridTransformedProps) {
   LicenseManager.setLicenseKey(agGridLicenseKey);
 
-  const [,setFilters] = useState(initialFilters);
+  const [filters, setFilters] = useState(initialFilters);
 
   const [prevRow, setPrevRow] = useState(-1);
   const [prevColumn, setPrevColumn] = useState('');
   const [searchValue, setSearchValue] = useState('');
-
-  const gridRef = useRef<AgGridReact>(null);
-  const keyRefresh = useRef<number>(0);
-  const pageSize = useRef<number>(page_length);
 
   const handleChange = useCallback(
     filters => {
@@ -102,7 +98,7 @@ export default function CccsGrid({
                   return {
                     col,
                     op: 'IN',
-                    val: val,
+                    val,
                   };
                 }),
         },
@@ -114,25 +110,30 @@ export default function CccsGrid({
     [emitFilter, setDataMask],
   ); // only take relevant page size options
 
-  // getContextMenuItems = (params) => {
-  //   var result = [
-  //     {
-  //       name: 'GWWK of IP: 23.56.24.67',
-  //       action: function () {
-  //         window.open('http://10.162.232.22:8000/gwwk.html', '_self');
-  //       },
-  //       cssClasses: ['redFont', 'bold'],
-  //     },
-  //     {
-  //       name: 'Launch GWWK for domain: subsurface.com',
-  //       action: function () {
-  //         window.open('http://10.162.232.22:8000/gwwk.html', '_blank');
-  //       },
-  //       cssClasses: ['redFont', 'bold'],
-  //     },
-  //   ];
-  //   return result;
-  // };
+  const getContextMenuItems = useCallback(
+    (params: GetContextMenuItemsParams): (string | MenuItemDef)[] => {
+      let result: (string | MenuItemDef)[] = [];
+      if (!emitFilter) {
+        result = ['copy', 'copyWithHeaders', 'paste', 'separator', 'export'];
+      } else {
+        result = [
+          'copy',
+          'copyWithHeaders',
+          'paste',
+          'separator',
+          {
+            name: 'Emit Filter(s)',
+            action: () => handleChange(filters),
+            icon: '<img src="./images/emit_filters.png" />',
+          },
+          'separator',
+          'export',
+        ];
+      }
+      return result;
+    },
+    [emitFilter, filters, handleChange],
+  );
 
   const frameworkComponents = {
     countryValueRenderer: CountryValueRenderer,
@@ -155,19 +156,19 @@ export default function CccsGrid({
   };
 
   function isSingleCellSelection(cellRanges: any): boolean {
-    if (cellRanges.length != 1) {
+    if (cellRanges.length !== 1) {
       return false;
     }
     const range = cellRanges[0];
     return (
-      range.startRow.rowIndex == range.endRow.rowIndex &&
-      range.columns.length == 1
+      range.startRow.rowIndex === range.endRow.rowIndex &&
+      range.columns.length === 1
     );
   }
 
   function isSameSingleSelection(range: any): boolean {
     const singleRow = Math.min(range.startRow.rowIndex, range.endRow.rowIndex);
-    return prevRow == singleRow && prevColumn == range.columns[0].colId;
+    return prevRow === singleRow && prevColumn === range.columns[0].colId;
   }
 
   function cacheSingleSelection(range: any) {
@@ -182,7 +183,7 @@ export default function CccsGrid({
   }
 
   const onRangeSelectionChanged = (params: any) => {
-    if (params.finished == false) {
+    if (params.finished === false) {
       return;
     }
 
@@ -226,7 +227,6 @@ export default function CccsGrid({
     });
 
     setFilters(updatedFilters);
-    handleChange(updatedFilters);
   };
 
   function getEmitTarget(col: string) {
@@ -235,18 +235,12 @@ export default function CccsGrid({
 
   function autoSizeFirst100Columns(params: any) {
     // Autosizes only the first 100 Columns in Ag-Grid
-    const allColumnIds = params.columnApi.getAllColumns().map((col: any) => {
-      return col.getColId();
-    });
+    const allColumnIds = params.columnApi
+      .getAllColumns()
+      .map((col: any) => col.getColId());
     params.columnApi.autoSizeColumns(allColumnIds.slice(0, 100), false);
   }
 
-  useEffect(() => {
-    pageSize.current = page_length <= 0 ? 0 : page_length;
-    gridRef.current?.props.api?.paginationSetPageSize(pageSize.current);
-    keyRefresh.current += 1
-  }, [page_length]);
-  
   function setSearch(e: ChangeEvent<HTMLInputElement>) {
     const target = e.target as HTMLInputElement;
     e.preventDefault();
@@ -255,9 +249,9 @@ export default function CccsGrid({
 
   useEffect(() => {
     if (!include_search) {
-      setSearchValue("");
+      setSearchValue('');
     }
-  }, [include_search])
+  }, [include_search]);
 
   const gridOptions = {
     suppressColumnVirtualisation: true,
@@ -269,43 +263,39 @@ export default function CccsGrid({
 
   return (
     <div style={{ width, height }} className="ag-theme-balham">
-      { include_search ? (
-      <div className="form-inline" style={{ paddingBottom: "0.5em" }}> 
-        <div className="row">
-          <div className="col-sm-6"></div>
-          <div className="col-sm-6">
-            <span className="float-right">
-              Search{' '}
-              <input
-                className="form-control input-sm"
-                placeholder={`${rowData.length} records...`}
-                value={searchValue}
-                onChange={setSearch}
-              />
-            </span>
+      {include_search ? (
+        <div className="form-inline" style={{ paddingBottom: '0.5em' }}>
+          <div className="row">
+            <div className="col-sm-6" />
+            <div className="col-sm-6">
+              <span className="float-right">
+                Search{' '}
+                <input
+                  className="form-control input-sm"
+                  placeholder={`${rowData.length} records...`}
+                  value={searchValue}
+                  onChange={setSearch}
+                />
+              </span>
+            </div>
           </div>
         </div>
-      </div>
       ) : null}
       <AgGridReact
-        key={keyRefresh.current}
-        ref={gridRef}
         modules={AllModules}
         columnDefs={columnDefs}
         defaultColDef={DEFAULT_COLUMN_DEF}
         frameworkComponents={frameworkComponents}
-        enableRangeSelection={true}
-        allowContextMenuWithControlKey={true}
+        enableRangeSelection
+        allowContextMenuWithControlKey
         gridOptions={gridOptions}
         onGridColumnsChanged={autoSizeFirst100Columns}
-        //getContextMenuItems={getContextMenuItems}
+        getContextMenuItems={getContextMenuItems}
         onGridReady={onGridReady}
         onRangeSelectionChanged={onRangeSelectionChanged}
         onSelectionChanged={onSelectionChanged}
         rowData={rowData}
-        paginationPageSize={pageSize.current}
-        pagination={pageSize.current !== 0}
-        cacheQuickFilter={true}
+        cacheQuickFilter
         quickFilterText={searchValue}
         rowGroupPanelShow="always"
       />
