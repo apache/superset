b0fa2f012f6d2eef09d138ced9119bea2606341c
Cody Leff <cody@preset.io>
2022-06-08 16:32:19 -0600

    Reorder Dashboard Edit tabs and rename 'Components' tab to 'Layout Elements'.

--- superset-frontend/src/dashboard/components/BuilderComponentPane.tsx
+++ superset-frontend/src/dashboard/components/BuilderComponentPane.tsx
@@ -101,7 +101,18 @@ const BuilderComponentPane: React.FC<BCPProps> = ({
                     className="tabs-components"
                     data-test="dashboard-builder-component-pane-tabs-navigation"
                   >
-                    <Tabs.TabPane key={1} tab={t('Components')}>
+                    <Tabs.TabPane
+                      key={1}
+                      tab={t('Charts')}
+                      className="tab-charts"
+                    >
+                      <SliceAdder
+                        height={
+                          height + (isSticky ? SUPERSET_HEADER_HEIGHT : 0)
+                        }
+                      />
+                    </Tabs.TabPane>
+                    <Tabs.TabPane key={2} tab={t('Layout Elements')}>
                       <NewTabs />
                       <NewRow />
                       <NewColumn />
@@ -116,17 +127,6 @@ const BuilderComponentPane: React.FC<BCPProps> = ({
                             componentKey={componentKey}
                           />
                         ))}
-                    </Tabs.TabPane>
-                    <Tabs.TabPane
-                      key={2}
-                      tab={t('Charts')}
-                      className="tab-charts"
-                    >
-                      <SliceAdder
-                        height={
-                          height + (isSticky ? SUPERSET_HEADER_HEIGHT : 0)
-                        }
-                      />
                     </Tabs.TabPane>
                   </BuilderComponentPaneTabs>
                 </div>
