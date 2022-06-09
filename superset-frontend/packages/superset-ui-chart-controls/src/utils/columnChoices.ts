0a288b58f91d8666f782421bf79fd068e7e18d9e
Yongjie Zhao <yongjie.zhao@gmail.com>
2022-06-09 16:52:43 +0800

    fix: superset-ui/core codes coverage

--- superset-frontend/packages/superset-ui-chart-controls/src/utils/columnChoices.ts
+++ superset-frontend/packages/superset-ui-chart-controls/src/utils/columnChoices.ts
@@ -16,8 +16,8 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import { QueryResponse } from '@superset-ui/core';
-import { Dataset } from '../types';
+import { ensureIsArray, QueryResponse } from '@superset-ui/core';
+import { Dataset, isColumnMeta, isDataset, isQueryResponse } from '../types';
 
 /**
  * Convert Datasource columns to column choices
@@ -25,23 +25,24 @@ import { Dataset } from '../types';
 export default function columnChoices(
   datasource?: Dataset | QueryResponse | null,
 ): [string, string][] {
-  if (datasource?.columns[0]?.hasOwnProperty('column_name')) {
-    return (
-      (datasource as Dataset)?.columns
-        .map((col): [string, string] => [
-          col.column_name,
-          col.verbose_name || col.column_name,
-        ])
-        .sort((opt1, opt2) =>
-          opt1[1].toLowerCase() > opt2[1].toLowerCase() ? 1 : -1,
-        ) || []
-    );
+  if (isDataset(datasource) && isColumnMeta(datasource.columns[0])) {
+    return datasource.columns
+      .map((col): [string, string] => [
+        col.column_name,
+        col.verbose_name || col.column_name,
+      ])
+      .sort((opt1, opt2) =>
+        opt1[1].toLowerCase() > opt2[1].toLowerCase() ? 1 : -1,
+      );
   }
-  return (
-    (datasource as QueryResponse)?.columns
+
+  if (isQueryResponse(datasource)) {
+    return ensureIsArray(datasource.columns)
       .map((col): [string, string] => [col.name, col.name])
       .sort((opt1, opt2) =>
         opt1[1].toLowerCase() > opt2[1].toLowerCase() ? 1 : -1,
-      ) || []
-  );
+      );
+  }
+
+  return [];
 }
