63eebfd5dc855a9975b75b1bfbbea91db7599e42
Taylor <prosdev0107@outlook.com>
2022-06-20 15:41:26 -0400

    fix(db): make to show the db error msg when db installed and error is exist

--- superset-frontend/src/components/ImportModal/index.tsx
+++ superset-frontend/src/components/ImportModal/index.tsx
@@ -300,7 +300,12 @@ const ImportModelsModal: FunctionComponent<ImportModelsModalProps> = ({
           <Button loading={importingModel}>Select file</Button>
         </Upload>
       </StyledInputContainer>
-      {errorMessage && <ErrorAlert errorMessage={errorMessage} />}
+      {errorMessage && (
+        <ErrorAlert
+          errorMessage={errorMessage}
+          dbInstall={passwordFields.length > 0}
+        />
+      )}
       {renderPasswordFields()}
       {renderOverwriteConfirmation()}
     </Modal>
