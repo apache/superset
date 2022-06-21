4e8ff45d9904466ad422712a9872f2f967749487
Reese <10563996+reesercollins@users.noreply.github.com>
2022-06-21 11:02:05 -0400

    Fix merge error

--- superset/views/utils.py
+++ superset/views/utils.py
@@ -32,9 +32,8 @@ from sqlalchemy.orm.exc import NoResultFound
 import superset.models.core as models
 from superset import app, dataframe, db, result_set, viz
 from superset.common.db_query_status import QueryStatus
-
-from superset.datasource.dao import DatasourceDAO
 from superset.connectors.sqla.models import SqlaTable
+from superset.datasource.dao import DatasourceDAO
 from superset.errors import ErrorLevel, SupersetError, SupersetErrorType
 from superset.exceptions import (
     CacheLoadError,
