9da7c1f7b83abea20e3620d5461a47dc4a2ab019
Phillip Kelley-Dotson <pkelleydotson@yahoo.com>
2022-07-05 08:56:12 -0700

    feat: hack db connection modal  ctabtns (#20017)
    * add flow for dbconnmodal to datasetmodal
    
    * fix bug
    
    * second part
    
    * fix dataset to sql lab
    
    * fix lint
    
    * more lint
    
    * add key value
    
    * fix type
    
    * add new styling
    
    * remove undefined from types

--- superset-frontend/src/views/CRUD/data/database/DatabaseModal/styles.ts
+++ superset-frontend/src/views/CRUD/data/database/DatabaseModal/styles.ts
@@ -40,8 +40,6 @@ export const marginBottom = (theme: SupersetTheme) => css`
 `;
 
 export const StyledFormHeader = styled.header`
-  border-bottom: ${({ theme }) => `${theme.gridUnit * 0.25}px solid
-    ${theme.colors.grayscale.light2};`}
   padding: ${({ theme }) => theme.gridUnit * 2}px
     ${({ theme }) => theme.gridUnit * 4}px;
   line-height: ${({ theme }) => theme.gridUnit * 6}px;
