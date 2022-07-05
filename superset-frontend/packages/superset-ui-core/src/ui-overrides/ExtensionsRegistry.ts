899e705d980bf7a8a3d982c0890fc3e936a808d0
David Aaron Suddjian <aasuddjian@gmail.com>
2022-07-05 14:46:55 -0700

    uiOverrideRegistry -> extensionsRegistry

--- superset-frontend/packages/superset-ui-core/src/ui-overrides/ExtensionsRegistry.ts
+++ superset-frontend/packages/superset-ui-core/src/ui-overrides/ExtensionsRegistry.ts
@@ -21,28 +21,37 @@ import React from 'react';
 import { TypedRegistry } from '../models';
 import { makeSingleton } from '../utils';
 
-/** A function (or component) which returns text (or marked-up text) */
-type UiGeneratorText<P = void> = (props: P) => string | React.ReactElement;
+/**
+ * A function (or component) which returns text (or marked-up text)
+ * This != React.ComponentType. Use that when you want a react component.
+ */
+type ReturningDisplayable<P = void> = (props: P) => string | React.ReactElement;
 
 /**
  * This type defines all the UI override options which replace elements of Superset's default UI.
- * Idea with the keys here is generally to namespace following the form of 'domain.functonality.item'
+ * Namespace the keys here to follow the form of 'some_domain.functonality.item'.
+ * Take care to name your keys well, as the name describes what this extension point's role is in Superset.
  *
  * When defining a new option here, take care to keep any parameters to functions (or components) minimal.
  * Any removal or alteration to a parameter will be considered a breaking change.
  */
-export type UiOverrides = Partial<{
-  'embedded.documentation.description': UiGeneratorText;
+export type Extensions = Partial<{
+  'embedded.documentation.description': ReturningDisplayable;
   'embedded.documentation.url': string;
   'navbar.right': React.ComponentType;
   'welcome.banner': React.ComponentType;
 }>;
 
 /**
- * A registry containing UI customizations to replace elements of Superset's default UI.
+ * A registry containing extensions which can alter Superset's UI at specific points defined by Superset.
+ * See SIP-87: 
  */
-class UiOverrideRegistry extends TypedRegistry<UiOverrides> {
+class ExtensionsRegistry extends TypedRegistry<Extensions> {
   name = 'UiOverrideRegistry';
 }
 
-export const getUiOverrideRegistry = makeSingleton(UiOverrideRegistry, {});
+export const getExtensionsRegistry = makeSingleton(ExtensionsRegistry, {});
+
+// Exporting this under the old name for backwards compatibility.
+// After the SIP-87 vote passes it should be safe to remove this.
+export const getUiOverrideRegistry = getExtensionsRegistry;
