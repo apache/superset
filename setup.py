c0ebc3acb0bdb627d5cfd2a88e70337cc0e472f7
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2022-07-06 12:55:24 +0100

    chore: bump FAB to 4.1.3 (#20621)

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
         "cryptography>=3.3.2",
         "deprecation>=2.1.0, <2.2.0",
         "flask>=2.0.0, <3.0.0",
-        "flask-appbuilder>=4.1.2, <5.0.0",
+        "flask-appbuilder>=4.1.3, <5.0.0",
         "flask-caching>=1.10.0",
         "flask-compress",
         "flask-talisman",
