34e1336bca39b36f4cdbea143c5e0432b3072456
Yongjie Zhao <yongjie.zhao@gmail.com>
2022-07-07 17:21:14 +0800

    fix: controls validator glitch (#20623)

--- superset-frontend/src/explore/controlUtils/standardizedFormData.ts
+++ superset-frontend/src/explore/controlUtils/standardizedFormData.ts
@@ -187,6 +187,7 @@ export class StandardizedFormData {
      * 4. attach `standardizedFormData` to the initial form_data
      * 5. call formDataOverrides to transform initial form_data if the plugin was defined
      * 6. use final form_data to generate controlsState
+     * 7. to refresh validator message
      * */
     const latestFormData = this.getLatestFormData(targetVizType);
     const publicFormData = {};
@@ -204,6 +205,10 @@ export class StandardizedFormData {
       ...getFormDataFromControls(targetControlsState),
       standardizedFormData: this.serialize(),
     };
+    let rv = {
+      formData: targetFormData,
+      controlsState: targetControlsState,
+    };
 
     const controlPanel = getChartControlPanelRegistry().get(targetVizType);
     if (controlPanel?.formDataOverrides) {
@@ -216,16 +221,17 @@ export class StandardizedFormData {
         },
       };
       getStandardizedControls().clear();
-
-      return {
+      rv = {
         formData: transformed,
         controlsState: getControlsState(exploreState, transformed),
       };
     }
 
-    return {
-      formData: targetFormData,
-      controlsState: targetControlsState,
-    };
+    // refresh validator message
+    rv.controlsState = getControlsState(
+      { ...exploreState, controls: rv.controlsState },
+      rv.formData,
+    );
+    return rv;
   }
 }
