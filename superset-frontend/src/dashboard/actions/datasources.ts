5e80849499b010f85ee1d35f81daefa3105fc2cf
Michael S. Molina <70410625+michael-s-molina@users.noreply.github.com>
2022-07-20 17:03:19 -0300

    fix: Reverts #20749 and #20645 (#20796)
    * Revert "fix: Error when saving datasource from Explore (#20749)"
    
    This reverts commit 92f3621c754a4f3ddb99285aad273296be26567e.
    
    * Revert "refactor: Unify shared datasources reducers and actions (#20645)"
    
    This reverts commit 2a705406e1883834bb6696c1585ef65f787ce7b3.

--- superset-frontend/src/dashboard/actions/datasources.ts
+++ superset-frontend/src/dashboard/actions/datasources.ts
@@ -18,9 +18,39 @@
  */
 import { Dispatch } from 'redux';
 import { SupersetClient } from '@superset-ui/core';
-import { Dataset } from '@superset-ui/chart-controls';
-import { RootState } from 'src/dashboard/types';
-import { setDatasource } from 'src/datasource/actions';
+import { Datasource, RootState } from 'src/dashboard/types';
+
+// update datasources index for Dashboard
+export enum DatasourcesAction {
+  SET_DATASOURCES = 'SET_DATASOURCES',
+  SET_DATASOURCE = 'SET_DATASOURCE',
+}
+
+export type DatasourcesActionPayload =
+  | {
+      type: DatasourcesAction.SET_DATASOURCES;
+      datasources: Datasource[] | null;
+    }
+  | {
+      type: DatasourcesAction.SET_DATASOURCE;
+      key: Datasource['uid'];
+      datasource: Datasource;
+    };
+
+export function setDatasources(datasources: Datasource[] | null) {
+  return {
+    type: DatasourcesAction.SET_DATASOURCES,
+    datasources,
+  };
+}
+
+export function setDatasource(datasource: Datasource, key: string) {
+  return {
+    type: DatasourcesAction.SET_DATASOURCE,
+    key,
+    datasource,
+  };
+}
 
 export function fetchDatasourceMetadata(key: string) {
   return (dispatch: Dispatch, getState: () => RootState) => {
@@ -28,11 +58,11 @@ export function fetchDatasourceMetadata(key: string) {
     const datasource = datasources[key];
 
     if (datasource) {
-      return dispatch(setDatasource(datasource));
+      return dispatch(setDatasource(datasource, key));
     }
 
     return SupersetClient.get({
       endpoint: `/superset/fetch_datasource_metadata?datasourceKey=${key}`,
-    }).then(({ json }) => dispatch(setDatasource(json as Dataset)));
+    }).then(({ json }) => dispatch(setDatasource(json as Datasource, key)));
   };
 }
--- superset-frontend/src/explore/reducers/datasourcesReducer.ts
+++ superset-frontend/src/explore/reducers/datasourcesReducer.ts
@@ -1,6 +1,3 @@
-/* eslint-disable import/prefer-default-export */
-import { DatasourceType } from '@superset-ui/core';
-import { Datasource } from 'src/dashboard/types';
 /**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -19,25 +16,27 @@ import { Datasource } from 'src/dashboard/types';
  * specific language governing permissions and limitations
  * under the License.
  */
-export const PLACEHOLDER_DATASOURCE: Datasource = {
-  id: 0,
-  type: DatasourceType.Table,
-  uid: '_placeholder_',
-  datasource_name: '',
-  table_name: '',
-  columns: [],
-  column_types: [],
-  metrics: [],
-  column_format: {},
-  verbose_map: {},
-  main_dttm_col: '',
-  description: '',
-};
+import { Dataset } from '@superset-ui/chart-controls';
+import { getDatasourceUid } from 'src/utils/getDatasourceUid';
+import {
+  AnyDatasourcesAction,
+  SET_DATASOURCE,
+} from '../actions/datasourcesActions';
+import { HYDRATE_EXPLORE, HydrateExplore } from '../actions/hydrateExplore';
 
-export const MAIN_HEADER_HEIGHT = 53;
-export const CLOSED_FILTER_BAR_WIDTH = 32;
-export const OPEN_FILTER_BAR_WIDTH = 260;
-export const OPEN_FILTER_BAR_MAX_WIDTH = 550;
-export const FILTER_BAR_HEADER_HEIGHT = 80;
-export const FILTER_BAR_TABS_HEIGHT = 46;
-export const BUILDER_SIDEPANEL_WIDTH = 374;
+export default function datasourcesReducer(
+  // TODO: change type to include other datasource types
+  datasources: { [key: string]: Dataset },
+  action: AnyDatasourcesAction | HydrateExplore,
+) {
+  if (action.type === SET_DATASOURCE) {
+    return {
+      ...datasources,
+      [getDatasourceUid(action.datasource)]: action.datasource,
+    };
+  }
+  if (action.type === HYDRATE_EXPLORE) {
+    return { ...(action as HydrateExplore).data.datasources };
+  }
+  return datasources || {};
+}
