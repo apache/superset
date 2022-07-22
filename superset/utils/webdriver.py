4715a84e5ea654077bfb078bea4a5fe347cb2db6
Hugh A. Miles II <hughmil3s@gmail.com>
2022-07-22 13:56:33 -0400

    remove eleement reference (#20830)

--- superset/utils/webdriver.py
+++ superset/utils/webdriver.py
@@ -138,7 +138,6 @@ class WebDriverProxy:
             img = element.screenshot_as_png
         except TimeoutException:
             logger.warning("Selenium timed out requesting url %s", url, exc_info=True)
-            img = element.screenshot_as_png
         except StaleElementReferenceException:
             logger.error(
                 "Selenium got a stale element while requesting url %s",
