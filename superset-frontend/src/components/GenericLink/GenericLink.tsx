8581f26a3c6bac37aac9ffe4b632ed61f1b912e5
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2022-07-28 15:58:54 +0200

    Fix type

--- superset-frontend/src/components/GenericLink/GenericLink.tsx
+++ superset-frontend/src/components/GenericLink/GenericLink.tsx
@@ -20,15 +20,15 @@
 import React from 'react';
 import { Link, LinkProps } from 'react-router-dom';
 
-export const GenericLink = ({
+export const GenericLink = <S,>({
   to,
   component,
   replace,
   innerRef,
   children,
   ...rest
-}: // css prop type check was failing, override with any
-LinkProps & { css?: any }) => {
+}: React.PropsWithoutRef<LinkProps<S>> &
+  React.RefAttributes<HTMLAnchorElement>) => {
   if (typeof to === 'string' && /^https?:\/\//.test(to)) {
     return (
       <a data-test="external-link" href={to} {...rest}>
