923cc78276e9281d00b7aae7e90ae6f144baf29a
lyndsiWilliams <kcatgirl@gmail.com>
2022-08-02 16:36:56 -0500

    Fix tab state

--- superset/views/sql_lab.py
+++ superset/views/sql_lab.py
@@ -139,7 +139,8 @@ class TabStateView(BaseSupersetView):
         query_editor = json.loads(request.form["queryEditor"])
         tab_state = TabState(
             user_id=get_user_id(),
-            label=query_editor.get("title", "Untitled Query"),
+            label=query_editor.get("name")
+            or query_editor.get("title", "Untitled Query"),
             active=True,
             database_id=query_editor["dbId"],
             schema=query_editor.get("schema"),
