5ef569a5f0b6223bfbcdf888b9b7fd855cefb6e3
Hugh A. Miles II <hughmil3s@gmail.com>
2022-08-05 00:21:46 +0000

    oops

--- superset/common/query_context_processor.py
+++ superset/common/query_context_processor.py
@@ -43,6 +43,7 @@ from superset.exceptions import (
 )
 from superset.extensions import cache_manager, security_manager
 from superset.models.helpers import QueryResult
+from superset.models.sql_lab import Query
 from superset.utils import csv
 from superset.utils.cache import generate_cache_key, set_and_log_cache
 from superset.utils.core import (
@@ -307,7 +308,11 @@ class QueryContextProcessor:
             }
             join_keys = [col for col in df.columns if col not in metrics_mapping.keys()]
 
-            result = self._qc_datasource.query(query_object_clone_dct)
+            if isinstance(self._qc_datasource, Query):
+                result = self._qc_datasource.exc_query(query_object_clone_dct)
+            else:
+                result = self._qc_datasource.query(query_object_clone_dct)
+
             queries.append(result.query)
             cache_keys.append(None)
 
