525551fee7bc97a9d07ff3de3208eac3cd49ca09
EugeneTorap <evgenykrutpro@gmail.com>
2022-08-07 10:31:30 +0300

    Refactoring

--- setup.py
+++ setup.py
@@ -182,5 +182,6 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
