e301e92af04f05adf1af2770da630d4511956abd
Michael S. Molina <michael.s.molina@gmail.com>
2022-08-12 19:01:33 -0300

    feat: Adds the MetadataBar component

---
+++ superset-frontend/src/components/MetadataBar/ContentType.ts
@@ -0,0 +1,80 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+export type Dashboards = {
+  type: 'dashboards';
+  title: string;
+  description?: string;
+  onClick?: (type: string) => void;
+};
+
+export type Description = {
+  type: 'description';
+  value: string;
+  onClick?: (type: string) => void;
+};
+
+export type LastModified = {
+  type: 'lastModified';
+  value: Date;
+  modifiedBy: string;
+  onClick?: (type: string) => void;
+};
+
+export type Owner = {
+  type: 'owner';
+  createdBy: string;
+  owners: string[];
+  createdOn: Date;
+  onClick?: (type: string) => void;
+};
+
+export type Rows = {
+  type: 'rows';
+  title: string;
+  onClick?: (type: string) => void;
+};
+
+export type Sql = {
+  type: 'sql';
+  title: string;
+  onClick?: (type: string) => void;
+};
+
+export type Table = {
+  type: 'table';
+  title: string;
+  onClick?: (type: string) => void;
+};
+
+export type Tags = {
+  type: 'tags';
+  values: string[];
+  onClick?: (type: string) => void;
+};
+
+export type ContentType =
+  | Dashboards
+  | Description
+  | LastModified
+  | Owner
+  | Rows
+  | Sql
+  | Table
+  | Tags;
