884e2f1ca7f5f852766a480d89840e0bc455de07
Stephen Liu <750188453@qq.com>
2022-08-16 21:54:17 +0800

    fix(plugin-chart-echarts): gauge chart enhancements and fixes (#21007)
    * fix(plugin-chart-echarts): gauge chart enhancements and fixes
    
    * fix lint
    
    (cherry picked from commit b303d1e156185d134927246004a4804931cd6bca)

--- superset-frontend/plugins/plugin-chart-echarts/src/Gauge/controlPanel.tsx
+++ superset-frontend/plugins/plugin-chart-echarts/src/Gauge/controlPanel.tsx
@@ -17,7 +17,7 @@
  * under the License.
  */
 import React from 'react';
-import { t, validateNonEmpty, validateInteger } from '@superset-ui/core';
+import { t } from '@superset-ui/core';
 import {
   sharedControls,
   ControlPanelConfig,
@@ -81,8 +81,7 @@ const config: ControlPanelConfig = {
             config: {
               type: 'TextControl',
               isInt: true,
-              default: String(DEFAULT_FORM_DATA.minVal),
-              validators: [validateNonEmpty, validateInteger],
+              default: DEFAULT_FORM_DATA.minVal,
               renderTrigger: true,
               label: t('Min'),
               description: t('Minimum value on the gauge axis'),
@@ -94,7 +93,6 @@ const config: ControlPanelConfig = {
               type: 'TextControl',
               isInt: true,
               default: DEFAULT_FORM_DATA.maxVal,
-              validators: [validateNonEmpty, validateInteger],
               renderTrigger: true,
               label: t('Max'),
               description: t('Maximum value on the gauge axis'),
