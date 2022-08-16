12d5a8520509b6abcb1830ac24ff2f29880a8d48
justin-park <justin.park@airbnb.com>
2022-08-16 16:16:51 -0700

    fix(sqllab): Enable cypress specs for sqllab query

--- superset-frontend/cypress-base/cypress/integration/sqllab/query.test.ts
+++ superset-frontend/cypress-base/cypress/integration/sqllab/query.test.ts
@@ -17,144 +17,343 @@
  * under the License.
  */
 import * as shortid from 'shortid';
-import { selectResultsTab, assertSQLLabResultsAreEqual } from './sqllab.helper';
-
-function parseClockStr(node: JQuery) {
-  return Number.parseFloat(node.text().replace(/:/g, ''));
-}
+import { selectResultsTab } from './sqllab.helper';
 
 describe('SqlLab query panel', () => {
+  const mockTabId = '88889999';
+  const mockQueryId = 'mock_qid_28dvde';
+
   beforeEach(() => {
+    cy.intercept('/tableschemaview/', {
+      id: 1,
+    }).as('mockTableSchemaView');
+    cy.intercept('/tabstate/', {
+      id: 1,
+    }).as('mockTabStateView');
+    cy.intercept('GET', '/api/v1/database/?q=**', req => {
+      req.continue(res => {
+        res.send({ fixture: 'sqllab_databases.json' });
+      });
+    }).as('mockDatabaseList');
+    cy.intercept('GET', '/api/v1/database/*/function_names/', {
+      function_names: [],
+    }).as('mockFunctionNameList');
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/v1/database/*/schemas/?q=*',
+      },
+      {
+        result: ['mock'],
+      },
+    ).as('mockSchemaList');
+    cy.intercept(
+      {
+        method: 'POST',
+        url: '/superset/sql_json/',
+      },
+      req => {
+        req.reply({
+          delay: 800,
+          fixture: 'sqllab_sql_json.json',
+        });
+      },
+    ).as('mockSQLResponse');
+    cy.intercept('GET', '/superset/tables/1/**', {
+      fixture: 'sqllab_tables.json',
+    }).as('mockTableList');
+    cy.intercept('GET', '/api/v1/database/1/table/mock_table_1/*/', req => {
+      req.reply({
+        fixture: 'sqllab_table_1.json',
+        delay: 500,
+      });
+    }).as('mockTable1Schema');
+    cy.intercept('GET', '/api/v1/database/1/table_extra/mock_table_1/*/', {
+      body: {},
+    }).as('mockTable1SchemaExtra');
+    cy.intercept('GET', '/superset/queries/*', {
+      fixture: 'sqllab_queries_progress1.json',
+    });
+    cy.intercept('POST', 'tabstateview', {
+      id: mockTabId,
+    }).as('mockTabCreate');
+    cy.intercept('PUT', `tabstateview/*`, req => {
+      req.reply({
+        id: req.url.match(/\d+$/)?.[0],
+      });
+    }).as('mockTabUpdate');
+    cy.intercept('DELETE', `tabstateview/*`, req => {
+      req.reply({
+        id: req.url.match(/\d+$/)?.[0],
+      });
+    }).as('mockTabDelete');
+
+    cy.stub(shortid, 'generate').returns(mockQueryId);
+
     cy.login();
     cy.visit('/superset/sqllab');
+    cy.get(
+      '#a11y-query-editor-tabs .ant-tabs-nav-list .ant-tabs-nav-add',
+    ).click();
+    // wait for 1000 milliseconds for rendering
+    cy.wait(1000);
   });
 
-  it.skip('supports entering and running a query', () => {
-    // row limit has to be < ~10 for us to be able to determine how many rows
-    // are fetched below (because React _Virtualized_ does not render all rows)
-    let clockTime = 0;
+  it('supports entering and running a query', () => {
+    // multiple queries
+    ['SELECT 1', 'SELECT * FROM MOCK_DATA_TABLE'].forEach(expectedQuery => {
+      // multiple limit options
+      [1, 3].forEach(limitSelectorIndex => {
+        cy.get(
+          '#js-sql-toolbar .leftItems .ant-dropdown-trigger:last-child',
+        ).click();
+        cy.get(
+          `.ant-dropdown:last-child .ant-dropdown-menu-item:nth-child(${limitSelectorIndex}) [role="button"]`,
+        )
+          .click()
+          .then(el => {
+            const limitSize = Number(el.text().replace(/ /g, ''));
+            expect(limitSize).to.greaterThan(0);
 
-    const sampleResponse = {
-      status: 'success',
-      data: [{ '?column?': 1 }],
-      columns: [{ name: '?column?', type: 'INT', is_dttm: false }],
-      selected_columns: [{ name: '?column?', type: 'INT', is_dttm: false }],
-      expanded_columns: [],
-    };
+            cy.wait('@mockTabUpdate')
+              .its('request.body')
+              .should(postForm => {
+                expect(postForm.replace(/(?:\r\n|\r|\n)/g, '')).to.include(
+                  `"query_limit"${limitSize}`,
+                );
+              });
 
-    cy.intercept({
-      method: 'POST',
-      url: '/superset/sql_json/',
-      delay: 1000,
-      response: () => sampleResponse,
-    }).as('mockSQLResponse');
+            cy.get('#ace-editor textarea')
+              .focus()
+              .type(`{selectall}{backspace}${expectedQuery}`);
 
-    cy.get('.TableSelector .Select:eq(0)').click();
-    cy.get('.TableSelector .Select:eq(0) input[type=text]')
+            cy.wait('@mockTabUpdate')
+              .its('request.body')
+              .should(postForm => {
+                expect(postForm.replace(/(?:\r\n|\r|\n)/g, '')).to.include(
+                  `"sql""${expectedQuery}"`,
+                );
+              });
+            cy.get('#js-sql-toolbar button:eq(0)').eq(0).click();
+
+            cy.wait('@mockTabUpdate')
+              .its('request.body')
+              .should(postForm => {
+                expect(postForm.replace(/(?:\r\n|\r|\n)/g, '')).to.include(
+                  '"latest_query_id',
+                );
+              });
+
+            cy.wait('@mockSQLResponse')
+              .its('request.body')
+              .should(({ sql, sql_editor_id, queryLimit }) => {
+                expect(sql).to.equal(expectedQuery);
+                expect(sql_editor_id).to.equal(mockTabId);
+                expect(queryLimit).to.equal(limitSize);
+              });
+
+            selectResultsTab().then(resultsB => {
+              expect(
+                (resultsB[0].childNodes[1].childNodes[0] as HTMLDivElement)
+                  .innerText,
+              ).to.equal('1\ndata row 1\n2\ndata row 2');
+            });
+
+            // wait for 1000 milliseconds for rendering
+            cy.wait(1000);
+          });
+      });
+    });
+  });
+
+  it('supports autocomplete', () => {
+    cy.get('#ace-editor textarea')
       .focus()
-      .type('{enter}');
+      .type(`{selectall}{backspace}mock_table`);
 
-    cy.get('#brace-editor textarea')
+    // autocomplete for table names
+    cy.get('.ace_autocomplete')
+      .should('be.visible')
+      .should(el => {
+        expect(el.text()).to.include('mock_table_1');
+        expect(el.text()).to.include('mock_table_2');
+        expect(el.text()).to.include('mock_table_3');
+      });
+
+    // before a table schema loaded
+    cy.get('#ace-editor textarea')
+      .focus()
+      .type(`{selectall}{backspace}mock_tcolumn_nam`);
+
+    cy.get('.ace_autocomplete').should('not.be.visible');
+
+    cy.get('#ace-editor textarea').blur();
+
+    // select a table
+    cy.get('.SqlEditorLeftBar .divider + .section [type=search]')
+      .focus()
+      .type('1')
+      .type('{enter}')
+      .blur();
+
+    cy.wait('@mockTableSchemaView')
+      .its('request.body')
+      .should(postForm => {
+        expect(postForm).to.include('"dbId":1');
+        expect(postForm).to.include(`"queryEditorId":"${mockTabId}"`);
+      });
+
+    // inspect table record preview
+    cy.wait('@mockSQLResponse')
+      .its('request.body')
+      .should(({ sql_editor_id, database_id }) => {
+        expect(sql_editor_id).to.equal(null);
+        expect(database_id).to.equal(1);
+      });
+
+    selectResultsTab().then(resultsB => {
+      expect(
+        (resultsB[0].childNodes[1].childNodes[0] as HTMLDivElement).innerText,
+      ).to.equal('1\ndata row 1\n2\ndata row 2');
+    });
+
+    cy.get('#ace-editor textarea')
       .focus()
       .clear()
-      .type(`{selectall}{backspace}SELECT 1`);
+      .type(`{selectall}{backspace}mock_tcolumn_nam`);
 
-    cy.get('#js-sql-toolbar button:eq(0)').eq(0).click();
+    // autocomplete for table schema
+    cy.get('.ace_autocomplete')
+      .should('be.visible')
+      .should(el => {
+        expect(el.text()).to.include('mock_tcolumn_name');
+      });
 
-    // wait for 300 milliseconds
-    cy.wait(300);
+    cy.get('#ace-editor textarea').blur();
 
-    // started timer
-    cy.get('.sql-toolbar .label-success').then(node => {
-      clockTime = parseClockStr(node);
-      // should be longer than 0.2s
-      expect(clockTime).greaterThan(0.2);
-    });
+    // turn off autocomplete
+    cy.get(
+      '#js-sql-toolbar .rightItems .ant-dropdown-trigger:last-child',
+    ).click();
+    cy.get('.ant-dropdown button[name="autocomplete-switch"]').click();
 
-    cy.wait('@mockSQLResponse');
+    cy.get('#ace-editor textarea')
+      .focus()
+      .clear()
+      .type(`{selectall}{backspace}mock_table`);
 
-    // timer is increasing
-    cy.get('.sql-toolbar .label-success').then(node => {
-      const newClockTime = parseClockStr(node);
-      expect(newClockTime).greaterThan(0.9);
-      clockTime = newClockTime;
-    });
+    // autocomplete should be skipped
+    cy.get('.ace_autocomplete').should('not.be.visible');
+    cy.get('#ace-editor textarea').blur();
 
-    // rerun the query
-    cy.get('#js-sql-toolbar button:eq(0)').eq(0).click();
+    cy.get('#ace-editor textarea')
+      .focus()
+      .clear()
+      .type(`{selectall}{backspace}mock_tcolumn_nam`);
 
-    // should restart the timer
-    cy.get('.sql-toolbar .label-success').contains('00:00:00');
-    cy.wait('@mockSQLResponse');
-    cy.get('.sql-toolbar .label-success').then(node => {
-      expect(parseClockStr(node)).greaterThan(0.9);
-    });
+    cy.get('.ace_autocomplete').should('not.be.visible');
+
+    cy.get('#ace-editor textarea').blur();
   });
 
-  it.skip('successfully saves a query', () => {
-    cy.intercept('superset/tables/**').as('getTables');
-    cy.intercept('savedqueryviewapi/**').as('getSavedQuery');
-
+  it('successfully saves a query', () => {
     const query =
       'SELECT ds, gender, name, num FROM main.birth_names ORDER BY name LIMIT 3';
     const savedQueryTitle = `CYPRESS TEST QUERY ${shortid.generate()}`;
+    const savedQueryDescription = 'CYPRESS TEST DESCRIPTION';
 
-    // we will assert that the results of the query we save, and the saved query are the same
-    let initialResultsTable: HTMLElement | null = null;
-    let savedQueryResultsTable = null;
+    const mockResponse = {
+      item: {
+        db_id: 1,
+        description: savedQueryDescription,
+        extra_json: '{}',
+        id: 7766,
+        label: savedQueryTitle,
+        schema: 'main',
+        sql: query,
+      },
+      message: 'Added Row',
+      severity: 'success',
+    };
+    cy.intercept('POST', '/savedqueryviewapi/api/create', mockResponse).as(
+      'mockSaveQuery',
+    );
+    cy.intercept(
+      'PUT',
+      `/savedqueryviewapi/api/update/${mockResponse.item.id}`,
+      mockResponse,
+    ).as('mockUpdateQuery');
 
-    cy.get('#brace-editor textarea')
-      .clear({ force: true })
-      .type(`{selectall}{backspace}${query}`, { force: true })
-      .focus() // focus => blur is required for updating the query that is to be saved
-      .blur();
+    cy.get('#ace-editor textarea')
+      .focus()
+      .clear()
+      .type(`{selectall}{backspace}${query}`, { delay: 0 });
 
-    // ctrl + r also runs query
-    cy.get('#brace-editor textarea').type('{ctrl}r', { force: true });
+    cy.get('.SaveQuery [type=button]').click();
 
-    cy.wait('@sqlLabQuery');
+    cy.get('.save-query-modal [type=text]')
+      .focus()
+      .type(`{selectall}{backspace}${savedQueryTitle}`);
+    cy.get('.save-query-modal textarea')
+      .focus()
+      .type(`{selectall}{backspace}${savedQueryDescription}`);
 
-    // Save results to check against below
-    selectResultsTab().then(resultsA => {
-      initialResultsTable = resultsA[0];
-    });
+    cy.get('.save-query-modal .cta:last-child').click();
 
-    cy.get('#js-sql-toolbar button')
-      .eq(1) // save query
-      .click();
-
-    // Enter name + save into modal
-    cy.get('.modal-sm input')
-      .clear({ force: true })
-      .type(`{selectall}{backspace}${savedQueryTitle}`, {
-        force: true,
+    // save original query data
+    cy.wait('@mockSaveQuery')
+      .its('request.body')
+      .should(postForm => {
+        expect(postForm.replace(/(?:\r\n|\r|\n)/g, '')).to.include(
+          `name="label"${savedQueryTitle}`,
+        );
+        expect(postForm.replace(/(?:\r\n|\r|\n)/g, '')).to.include(
+          `name="sql"${query}`,
+        );
+        expect(postForm.replace(/(?:\r\n|\r|\n)/g, '')).to.include(
+          `name="description"${savedQueryDescription}`,
+        );
       });
 
-    cy.get('.modal-sm .modal-body button')
-      .eq(0) // save
-      .click();
+    // update limit size
+    cy.get(
+      '#js-sql-toolbar .leftItems .ant-dropdown-trigger:last-child',
+    ).click();
+    cy.get(
+      `.ant-dropdown:last-child .ant-dropdown-menu-item:nth-child(2) [role="button"]`,
+    )
+      .click()
+      .then(el => {
+        const limitSize = Number(el.text().replace(/ /g, ''));
 
-    // visit saved queries
-    cy.visit('/sqllab/my_queries/');
+        cy.get('.SaveQuery [type=button]').click();
 
-    // first row contains most recent link, follow back to SqlLab
-    cy.get('table tr:first-child a[href*="savedQueryId"').click();
+        // change title and description
+        cy.get('.save-query-modal [type=text]')
+          .focus()
+          .clear()
+          .type(`{selectall}{backspace}${savedQueryTitle}1`);
+        cy.get('.save-query-modal textarea')
+          .focus()
+          .clear()
+          .type(`{selectall}{backspace}${savedQueryDescription}1`);
 
-    // will timeout without explicitly waiting here
-    cy.wait(['@getSavedQuery', '@getTables']);
+        cy.get('.save-query-modal .cta:last-child').click();
 
-    // run the saved query
-    cy.get('#js-sql-toolbar button')
-      .eq(0) // run query
-      .click();
-
-    cy.wait('@sqlLabQuery');
-
-    // assert the results of the saved query match the initial results
-    selectResultsTab().then(resultsB => {
-      savedQueryResultsTable = resultsB[0];
-
-      assertSQLLabResultsAreEqual(initialResultsTable, savedQueryResultsTable);
-    });
+        // update existing query data
+        cy.wait('@mockUpdateQuery')
+          .its('request.body')
+          .should(postForm => {
+            expect(postForm.replace(/(?:\r\n|\r|\n)/g, '')).to.include(
+              `name="label"${savedQueryTitle}1`,
+            );
+            expect(postForm.replace(/(?:\r\n|\r|\n)/g, '')).to.include(
+              `name="sql"${query}`,
+            );
+            expect(postForm.replace(/(?:\r\n|\r|\n)/g, '')).to.include(
+              `name="queryLimit"${limitSize}`,
+            );
+          });
+      });
   });
 });
