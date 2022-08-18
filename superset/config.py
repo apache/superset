08b504d3beaa5e4b533659575266a7907431181d
AAfghahi <48933336+AAfghahi@users.noreply.github.com>
2022-08-18 10:32:25 -0400

    feat: add header_data into emails (#20903)
    * test sparkpost
    
    * added logging info
    
    * header function implementation
    
    * added test
    
    * daniel revisions
    
    * daniel revision
    
    * elizabeth review

--- superset/config.py
+++ superset/config.py
@@ -30,6 +30,7 @@ import re
 import sys
 from collections import OrderedDict
 from datetime import timedelta
+from email.mime.multipart import MIMEMultipart
 from typing import (
     Any,
     Callable,
@@ -1088,6 +1089,15 @@ def SQL_QUERY_MUTATOR(  # pylint: disable=invalid-name,unused-argument
     sql: str, **kwargs: Any
 ) -> str:
     return sql
+
+
+# This allows for a user to add header data to any outgoing emails. For example,
+# if you need to include metadata in the header or you want to change the specifications
+# of the email title, header, or sender.
+def EMAIL_HEADER_MUTATOR(  # pylint: disable=invalid-name,unused-argument
+    msg: MIMEMultipart, **kwargs: Any
+) -> MIMEMultipart:
+    return msg
 
 
 # This auth provider is used by background (offline) tasks that need to access
