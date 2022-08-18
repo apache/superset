166e1904875afc0f271b3c09d6126c574dd74d9d
John Bodley <john.bodley@gmail.com>
2022-08-17 17:11:28 -0700

    [fab] Add extra to list of columns

--- superset/connectors/sqla/views.py
+++ superset/connectors/sqla/views.py
@@ -209,7 +209,7 @@ class SqlMetricInlineView(  # pylint: disable=too-many-ancestors
     add_title = _("Add Metric")
     edit_title = _("Edit Metric")
 
-    list_columns = ["metric_name", "verbose_name", "metric_type"]
+    list_columns = ["metric_name", "verbose_name", "metric_type", "extra"]
     edit_columns = [
         "metric_name",
         "description",
