1389b8bc04076bc27e3094f2332cf1212a5f0dec
lyndsiWilliams <kcatgirl@gmail.com>
2022-08-30 19:00:06 -0500

    add testing for Header title

--- superset-frontend/src/views/CRUD/data/dataset/AddDataset/Header/Header.test.tsx
+++ superset-frontend/src/views/CRUD/data/dataset/AddDataset/Header/Header.test.tsx
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { render, screen, waitFor } from 'spec/helpers/testing-library';
 import Header from 'src/views/CRUD/data/dataset/AddDataset/Header';
@@ -23,13 +24,13 @@ import Header from 'src/views/CRUD/data/dataset/AddDataset/Header';
 describe('Header', () => {
   const mockSetDataset = jest.fn();
 
-  const waitForRender = () =>
+  const waitForRender = (datasetName: string) =>
     waitFor(() =>
-      render(<Header setDataset={mockSetDataset} datasetName="" />),
+      render(<Header setDataset={mockSetDataset} datasetName={datasetName} />),
     );
 
   it('renders a blank state Header', async () => {
-    await waitForRender();
+    await waitForRender('');
 
     const datasetNameTextbox = screen.getByRole('textbox', {
       name: /dataset name/i,
@@ -46,5 +47,33 @@ describe('Header', () => {
     expect(saveButton).toBeDisabled();
     expect(menuButton).toBeVisible();
     expect(menuButton).toBeDisabled();
+  });
+
+  it('updates display value of dataset name textbox when Header title is changed', async () => {
+    await waitForRender('');
+
+    const datasetNameTextbox = screen.getByRole('textbox', {
+      name: /dataset name/i,
+    });
+
+    // Textbox should start with an empty display value and placeholder text
+    expect(datasetNameTextbox).toHaveDisplayValue('');
+    expect(
+      screen.getByPlaceholderText(/add the name of the dataset/i),
+    ).toBeVisible();
+
+    // Textbox should update its display value when user inputs a new value
+    userEvent.type(datasetNameTextbox, 'Test name');
+    expect(datasetNameTextbox).toHaveDisplayValue('Test name');
+  });
+
+  it('passes an existing dataset title into the dataset name textbox', async () => {
+    await waitForRender('Existing Dataset Name');
+
+    const datasetNameTextbox = screen.getByRole('textbox', {
+      name: /dataset name/i,
+    });
+
+    expect(datasetNameTextbox).toHaveDisplayValue('Existing Dataset Name');
   });
 });
