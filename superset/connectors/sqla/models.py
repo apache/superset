01411fddefafea3358c6134ff656f659ae4ab1d3
Yongjie Zhao <yongjie.zhao@gmail.com>
2022-09-01 07:23:09 +0800

    fix pylint

--- superset/connectors/sqla/models.py
+++ superset/connectors/sqla/models.py
@@ -127,7 +127,6 @@ from superset.tables.models import Table as NewTable
 from superset.utils import core as utils
 from superset.utils.core import (
     GenericDataType,
-    get_axis_column,
     get_column_name,
     get_username,
     is_adhoc_column,
