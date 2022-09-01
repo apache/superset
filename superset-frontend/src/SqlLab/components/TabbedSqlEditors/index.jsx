5312cf0a2dcce6702abb928217bac943261621e8
justin-park <justin.park@airbnb.com>
2022-09-01 11:43:13 -0700

    feat(sqllab): Make LeftBar with resizable

--- superset-frontend/src/SqlLab/components/TabbedSqlEditors/index.jsx
+++ superset-frontend/src/SqlLab/components/TabbedSqlEditors/index.jsx
@@ -302,7 +302,6 @@ class TabbedSqlEditors extends React.PureComponent {
           editorQueries={this.state.queriesArray}
           dataPreviewQueries={this.state.dataPreviewQueries}
           actions={this.props.actions}
-          hideLeftBar={qe.hideLeftBar}
           defaultQueryLimit={this.props.defaultQueryLimit}
           maxRow={this.props.maxRow}
           displayLimit={this.props.displayLimit}
