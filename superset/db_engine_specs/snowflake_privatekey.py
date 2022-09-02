d3305076044d6d02564850e89e948663f717d12f
xiayanzheng <xiayanzheng@outlook.com>
2022-09-02 14:17:47 +0800

    temp update snowflake keypair auth

--- superset/db_engine_specs/snowflake_privatekey.py
+++ superset/db_engine_specs/snowflake_privatekey.py
@@ -15,6 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 import os
+from sqlalchemy.engine.url import make_url
 from snowflake.sqlalchemy import URL
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
@@ -24,46 +25,37 @@ from sqlalchemy import create_engine
 
 
 def get_param(sqlalchemy_url):
-    parameters_map = {}
-    # ['snowflake://{user}:{password}@{account}.{region}.{warehouse}.{database}/{schema}', 'privatekey={privatekey}&privatekeypw={privatekeypw}']
-    sqlalchemy_url_split_by_question_mark = sqlalchemy_url.split('?')
-    # {user}:{password}@{account}.{region}.{warehouse}.{database}/{schema}
-    sqlalchemy_url_part_a = sqlalchemy_url_split_by_question_mark[0].replace('snowflake://', '')
-    # ['{user}', '{password}@{account}.{region}.{warehouse}.{database}/{schema}']
-    sqlalchemy_url_part_a_split_by_colon = sqlalchemy_url_part_a.split(':')
-    parameters_map['user'] = sqlalchemy_url_part_a_split_by_colon[0]
-    # ['{account}.{region}.{warehouse}.{database}', '{schema}']
-    sqlalchemy_url_account_basic_and_schema = sqlalchemy_url_part_a_split_by_colon[1].split('@')[1].split('/')
-    parameters_map['database'] = sqlalchemy_url_account_basic_and_schema[1]
-    # ['{account}', '{region}', '{warehouse}', '{database}']
-    sqlalchemy_url_account_basic = sqlalchemy_url_account_basic_and_schema[0].split('.')
-    parameters_map['account'] = sqlalchemy_url_account_basic[0]
-    parameters_map['region'] = sqlalchemy_url_account_basic[1]
-    sqlalchemy_url_part_b = sqlalchemy_url_split_by_question_mark[1].split('&')
-    for param in sqlalchemy_url_part_b:
-        kv = param.split('=')
-        if kv[0] == 'privatekey':
-            parameters_map[kv[0]] = kv[1].replace('%2F', '/')
-        else:
-            parameters_map[kv[0]] = kv[1]
-    # {'user': '{user}', 'database': '{schema}', 'account': '{account}', 'region': '{region}', 'privatekey': '{privatekey}', 'privatekeypw': '{privatekeypw}'}
-    return parameters_map
+    url = make_url(sqlalchemy_url)
+    param = dict(url.query.items())
+    return url, param
 
 def create_snowflake_url_with_privatekey(sqlalchemy_url):
-    param = get_param(sqlalchemy_url)
-    return URL(
-        user=param['user'],
-        account=param['account'],
-        region=param['region'],
-        warehouse=param['warehouse'],
-        database=param['database'],
-        schema=param['schema'],
-        role=param['role'],
+    url, query = get_param(sqlalchemy_url)
+    url_obj = None
+    if query.get("region") is None:
+        url_obj = URL(
+            user=url.username,
+            account=url.host,
+            database=url.database,
+            warehouse=query.get("warehouse"),
+            schema=query.get("schema"),
+            role=query.get("role"),
         )
+    else:
+        url_obj = URL(
+            user=url.username,
+            account=url.host,
+            region=query.get("region"),
+            database=url.database,
+            warehouse=query.get("warehouse"),
+            schema=query.get("schema"),
+            role=query.get("role"),
+        )
+    return url_obj
     
 
 def create_snowflake_engine_with_privatekey(sqlalchemy_url):
-    param = get_param(sqlalchemy_url)
+    url, param = get_param(sqlalchemy_url)
     with open(param['privatekey'], "rb") as key:
         p_key = serialization.load_pem_private_key(
             key.read(),
