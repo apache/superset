99389961ab2bab3f78a2a4809a2742a1bafefcfc
Herbert Gainor <herbert.gainor@preset.io>
2022-09-02 18:31:21 -0600

    Sort dashboard list alphabetically when saving a chart

--- superset-frontend/src/explore/actions/saveModalActions.js
+++ superset-frontend/src/explore/actions/saveModalActions.js
@@ -40,6 +40,17 @@ export function fetchDashboards(userId) {
           value: id,
           label: (json.result[index] || {}).dashboard_title,
         }));
+        choices.sort((a, b) => {
+          const labelA = a.label.toUpperCase();
+          const labelB = b.label.toUpperCase();
+          if (labelA < labelB) {
+            return -1;
+          }
+          if (labelA > labelB) {
+            return 1;
+          }
+          return 0;
+        });
 
         return dispatch(fetchDashboardsSucceeded(choices));
       })
