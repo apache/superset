8c2719b11cebe451c5ba68193cf8dd51e4dce8e3
Phillip Kelley-Dotson <pkelleydotson@yahoo.com>
2022-09-07 13:42:47 -0700

    feat:  add tableselector to dataset creation page (#21075)
    Co-authored-by: Lyndsi Kay Williams <55605634+lyndsiWilliams@users.noreply.github.com>

--- superset-frontend/src/views/CRUD/data/dataset/AddDataset/types.tsx
+++ superset-frontend/src/views/CRUD/data/dataset/AddDataset/types.tsx
@@ -24,11 +24,9 @@ export enum DatasetActionType {
 }
 
 export interface DatasetObject {
-  database: {
-    id: string;
-    database_name: string;
-  };
-  owners: number[];
+  id: number;
+  database_name?: string;
+  owners?: number[];
   schema?: string | null;
   dataset_name: string;
   table_name?: string | null;
@@ -39,15 +37,16 @@ interface DatasetReducerPayloadType {
   value?: string;
 }
 
+export type Schema = {
+  schema?: string | null | undefined;
+};
+
 export type DSReducerActionType =
   | {
-      type:
-        | DatasetActionType.selectDatabase
-        | DatasetActionType.selectSchema
-        | DatasetActionType.selectTable;
+      type: DatasetActionType.selectDatabase | DatasetActionType.selectTable;
       payload: Partial<DatasetObject>;
     }
   | {
-      type: DatasetActionType.changeDataset;
+      type: DatasetActionType.changeDataset | DatasetActionType.selectSchema;
       payload: DatasetReducerPayloadType;
     };
