7401f15addda7e42ce9bf8adb966834ec0687278
EugeneTorap <evgenykrutpro@gmail.com>
2022-09-10 11:39:23 +0300

    chore: Bump sqlalchemy-utils to 0.38.3 (#21413)

--- setup.py
+++ setup.py
@@ -116,7 +116,7 @@ setup(
         "simplejson>=3.15.0",
         "slackclient==2.5.0",  # PINNED! slack changes file upload api in the future versions
         "sqlalchemy>=1.4, <2",
-        "sqlalchemy-utils>=0.37.8, <0.38",
+        "sqlalchemy-utils>=0.38.3, <0.39",
         "sqlparse==0.3.0",  # PINNED! see https://github.com/andialbrecht/sqlparse/issues/562
         "tabulate==0.8.9",
         # needed to support Literal (3.8) and TypeGuard (3.10)
