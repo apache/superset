apache/superset

commit aca0a24fdd098b82683c5470d9bdc4f93e95fada

chore(sqllab): Cleanup /tables/... endpoint (#21284)

superset-frontend/src/components/TableSelector/index.tsx
@@ -112,9 +112,9 @@ export interface TableOption {
 }
 
 export const TableOption = ({ table }: { table: Table }) => {
-  const { label, type, extra } = table;
+  const { value, type, extra } = table;
   return (
-    <TableLabel title={label}>
+    <TableLabel title={value}>
       {type === 'view' ? (
         <Icons.Eye iconSize="m" />
       ) : (
@@ -133,7 +133,7 @@ export const TableOption = ({ table }: { table: Table }) => {
           size="l"
         />
       )}
-      {label}
+      {value}
     </TableLabel>
   );
 };
@@ -286,9 +286,7 @@ const TableSelector: FunctionComponent<TableSelectorProps> = ({
   );
 
   function renderTableSelect() {
-    const disabled =
-      (currentSchema && !formMode && readOnly) ||
-      (!currentSchema && !database?.allow_multi_schema_metadata_fetch);
+    const disabled = (currentSchema && !formMode && readOnly) || !currentSchema;
 
     const header = sqlLabMode ? (
       <FormLabel>{t('See table schema')}</FormLabel>

superset-frontend/src/components/DatabaseSelector/index.tsx
@@ -74,14 +74,12 @@ type DatabaseValue = {
   id: number;
   database_name: string;
   backend: string;
-  allow_multi_schema_metadata_fetch: boolean;
 };
 
 export type DatabaseObject = {
   id: number;
   database_name: string;
   backend: string;
-  allow_multi_schema_metadata_fetch: boolean;
 };
 
 type SchemaValue = { label: string; value: string };
@@ -199,8 +197,6 @@ export default function DatabaseSelector({
             id: row.id,
             database_name: row.database_name,
             backend: row.backend,
-            allow_multi_schema_metadata_fetch:
-              row.allow_multi_schema_metadata_fetch,
           }));
 
           return {

superset-frontend/spec/fixtures/mockDatasource.js
@@ -171,7 +171,6 @@ export default {
     name: 'birth_names',
     owners: [{ first_name: 'joe', last_name: 'man', id: 1 }],
     database: {
-      allow_multi_schema_metadata_fetch: null,
       name: 'main',
       backend: 'sqlite',
     },

superset-frontend/src/SqlLab/components/SqlEditor/SqlEditor.test.jsx
@@ -64,7 +64,6 @@ const store = mockStore({
         allow_cvas: false,
         allow_dml: false,
         allow_file_upload: false,
-        allow_multi_schema_metadata_fetch: false,
         allow_run_async: false,
         backend: 'postgresql',
         database_name: 'examples',

superset-frontend/src/components/DatabaseSelector/DatabaseSelector.test.tsx
@@ -31,7 +31,6 @@ const createProps = (): DatabaseSelectorProps => ({
     id: 1,
     database_name: 'test',
     backend: 'test-postgresql',
-    allow_multi_schema_metadata_fetch: false,
   },
   formMode: false,
   isDatabaseSelectEnabled: true,
@@ -69,8 +68,6 @@ beforeEach(() => {
             allow_ctas: 'Allow Ctas',
             allow_cvas: 'Allow Cvas',
             allow_dml: 'Allow Dml',
-            allow_multi_schema_metadata_fetch:
-              'Allow Multi Schema Metadata Fetch',
             allow_run_async: 'Allow Run Async',
             allows_cost_estimate: 'Allows Cost Estimate',
             allows_subquery: 'Allows Subquery',
@@ -92,7 +89,6 @@ beforeEach(() => {
             'allow_ctas',
             'allow_cvas',
             'allow_dml',
-            'allow_multi_schema_metadata_fetch',
             'allow_run_async',
             'allows_cost_estimate',
             'allows_subquery',
@@ -126,7 +122,6 @@ beforeEach(() => {
               allow_ctas: false,
               allow_cvas: false,
               allow_dml: false,
-              allow_multi_schema_metadata_fetch: false,
               allow_run_async: false,
               allows_cost_estimate: null,
               allows_subquery: true,
@@ -147,7 +142,6 @@ beforeEach(() => {
               allow_ctas: false,
               allow_cvas: false,
               allow_dml: false,
-              allow_multi_schema_metadata_fetch: false,
               allow_run_async: false,
               allows_cost_estimate: null,
               allows_subquery: true,
@@ -272,7 +266,6 @@ test('Sends the correct db when changing the database', async () => {
         id: 2,
         database_name: 'test-mysql',
         backend: 'mysql',
-        allow_multi_schema_metadata_fetch: false,
       }),
     ),
   );

superset-frontend/src/components/TableSelector/TableSelector.test.tsx
@@ -31,7 +31,6 @@ const createProps = (props = {}) => ({
     id: 1,
     database_name: 'main',
     backend: 'sqlite',
-    allow_multi_schema_metadata_fetch: false,
   },
   schema: 'test_schema',
   handleError: jest.fn(),

superset-frontend/src/views/CRUD/data/database/DatabaseModal/ExtraOptions.tsx
@@ -151,24 +151,6 @@ const ExtraOptions = ({
             <StyledInputContainer css={no_margin_bottom}>
               <div className="input-container">
                 <IndeterminateCheckbox
-                  id="allow_multi_schema_metadata_fetch"
-                  indeterminate={false}
-                  checked={!!db?.allow_multi_schema_metadata_fetch}
-                  onChange={onInputChange}
-                  labelText={t('Allow Multi Schema Metadata Fetch')}
-                />
-                <InfoTooltip
-                  tooltip={t(
-                    'Allow SQL Lab to fetch a list of all tables and all views across all database ' +
-                      'schemas. For large data warehouse with thousands of tables, this can be ' +
-                      'expensive and put strain on the system.',
-                  )}
-                />
-              </div>
-            </StyledInputContainer>
-            <StyledInputContainer css={no_margin_bottom}>
-              <div className="input-container">
-                <IndeterminateCheckbox
                   id="cost_estimate_enabled"
                   indeterminate={false}
                   checked={!!db?.extra_json?.cost_estimate_enabled}

superset-frontend/src/views/CRUD/data/database/DatabaseModal/index.test.jsx
@@ -573,12 +573,6 @@ describe('DatabaseModal', () => {
         name: /allow dml/i,
       });
       const allowDMLText = screen.getByText(/allow dml/i);
-      const allowMultiSchemaMDFetchCheckbox = screen.getByRole('checkbox', {
-        name: /allow multi schema metadata fetch/i,
-      });
-      const allowMultiSchemaMDFetchText = screen.getByText(
-        /allow multi schema metadata fetch/i,
-      );
       const enableQueryCostEstimationCheckbox = screen.getByRole('checkbox', {
         name: /enable query cost estimation/i,
       });
@@ -619,7 +613,6 @@ describe('DatabaseModal', () => {
         checkboxOffSVGs[4],
         checkboxOffSVGs[5],
         checkboxOffSVGs[6],
-        checkboxOffSVGs[7],
         tooltipIcons[0],
         tooltipIcons[1],
         tooltipIcons[2],
@@ -627,7 +620,6 @@ describe('DatabaseModal', () => {
         tooltipIcons[4],
         tooltipIcons[5],
         tooltipIcons[6],
-        tooltipIcons[7],
         exposeInSQLLabText,
         allowCTASText,
         allowCVASText,
@@ -635,7 +627,6 @@ describe('DatabaseModal', () => {
         CTASCVASInput,
         CTASCVASHelperText,
         allowDMLText,
-        allowMultiSchemaMDFetchText,
         enableQueryCostEstimationText,
         allowDbExplorationText,
         disableSQLLabDataPreviewQueriesText,
@@ -646,7 +637,6 @@ describe('DatabaseModal', () => {
         allowCTASCheckbox,
         allowCVASCheckbox,
         allowDMLCheckbox,
-        allowMultiSchemaMDFetchCheckbox,
         enableQueryCostEstimationCheckbox,
         allowDbExplorationCheckbox,
         disableSQLLabDataPreviewQueriesCheckbox,
@@ -658,8 +648,8 @@ describe('DatabaseModal', () => {
       invisibleComponents.forEach(component => {
         expect(component).not.toBeVisible();
       });
-      expect(checkboxOffSVGs).toHaveLength(8);
-      expect(tooltipIcons).toHaveLength(8);
+      expect(checkboxOffSVGs).toHaveLength(7);
+      expect(tooltipIcons).toHaveLength(7);
     });
 
     test('renders the "Advanced" - PERFORMANCE tab correctly', async () => {

commit 32a48506cf9a684284d6366f14cebf7683baa78e

fix: Address regression introduced in #21284

superset-frontend/src/components/TableSelector/index.tsx
@@ -196,7 +196,7 @@ const TableSelector: FunctionComponent<TableSelectorProps> = ({
         ? data.options.map(table => ({
             value: table.value,
             label: <TableOption table={table} />,
-            text: table.label,
+            text: table.value,
           }))
         : [],
     [data],

superset-frontend/src/hooks/apiResources/tables.ts
@@ -24,7 +24,6 @@ export type FetchTablesQueryParams = {
   dbId?: string | number;
   schema?: string;
   forceRefresh?: boolean;
-  keyword?: string;
 };
 export interface Table {
   label: string;
@@ -52,14 +51,12 @@ export function fetchTables({
   dbId,
   schema,
   forceRefresh,
-  keyword,
 }: FetchTablesQueryParams) {
   const encodedSchema = schema ? encodeURIComponent(schema) : '';
-  const encodedKeyword = keyword ? encodeURIComponent(keyword) : 'undefined';
   // TODO: Would be nice to add pagination in a follow-up. Needs endpoint changes.
   const endpoint = `/superset/tables/${
     dbId ?? 'undefined'
-  }/${encodedSchema}/${encodedKeyword}/${forceRefresh}/`;
+  }/${encodedSchema}/${forceRefresh}/`;
   return SupersetClient.get({ endpoint }) as Promise<QueryData>;
 }
 
@@ -67,11 +64,11 @@ type Params = FetchTablesQueryParams &
   Pick<UseQueryOptions, 'onSuccess' | 'onError'>;
 
 export function useTables(options: Params) {
-  const { dbId, schema, keyword, onSuccess, onError } = options || {};
+  const { dbId, schema, onSuccess, onError } = options || {};
   const forceRefreshRef = useRef(false);
-  const params = { dbId, schema, keyword };
+  const params = { dbId, schema };
   const result = useQuery<QueryData, Error, Data>(
-    ['tables', { dbId, schema, keyword }],
+    ['tables', { dbId, schema }],
     () => fetchTables({ ...params, forceRefresh: forceRefreshRef.current }),
     {
       select: ({ json }) => ({

superset-frontend/src/views/CRUD/data/dataset/AddDataset/LeftPanel/index.tsx
@@ -172,7 +172,7 @@ export default function LeftPanel({
   useEffect(() => {
     if (loadTables) {
       const endpoint = encodeURI(
-        `/superset/tables/${dbId}/${encodedSchema}/undefined/${refresh}/`,
+        `/superset/tables/${dbId}/${encodedSchema}/${refresh}/`,
       );
       getTablesList(endpoint);
     }
@@ -191,7 +191,7 @@ export default function LeftPanel({
         const encodeTableName =
           value === '' ? undefined : encodeURIComponent(value);
         const endpoint = encodeURI(
-          `/superset/tables/${dbId}/${encodedSchema}/${encodeTableName}/`,
+          `/superset/tables/${dbId}/${encodedSchema}/`,
         );
         getTablesList(endpoint);
       }, FAST_DEBOUNCE),

superset-frontend/src/hooks/apiResources/tables.test.ts
@@ -103,7 +103,7 @@ describe('useTables hook', () => {
     });
     expect(SupersetClient.get).toHaveBeenCalledTimes(1);
     expect(SupersetClient.get).toHaveBeenCalledWith({
-      endpoint: `/superset/tables/${expectDbId}/${expectedSchema}/undefined/${forceRefresh}/`,
+      endpoint: `/superset/tables/${expectDbId}/${expectedSchema}/${forceRefresh}/`,
     });
     expect(result.current.data).toEqual(expectedData);
     await act(async () => {
@@ -111,36 +111,9 @@ describe('useTables hook', () => {
     });
     expect(SupersetClient.get).toHaveBeenCalledTimes(2);
     expect(SupersetClient.get).toHaveBeenCalledWith({
-      endpoint: `/superset/tables/${expectDbId}/${expectedSchema}/undefined/true/`,
+      endpoint: `/superset/tables/${expectDbId}/${expectedSchema}/true/`,
     });
     expect(result.current.data).toEqual(expectedData);
-  });
-
-  it('returns api response for search keyword', async () => {
-    const expectDbId = 'db1';
-    const expectedSchema = 'schemaA';
-    const expectedKeyword = 'my work';
-    const forceRefresh = false;
-    renderHook(
-      () =>
-        useTables({
-          dbId: expectDbId,
-          schema: expectedSchema,
-          keyword: expectedKeyword,
-        }),
-      {
-        wrapper: QueryProvider,
-      },
-    );
-    await act(async () => {
-      jest.runAllTimers();
-    });
-    expect(SupersetClient.get).toHaveBeenCalledTimes(1);
-    expect(SupersetClient.get).toHaveBeenCalledWith({
-      endpoint: `/superset/tables/${expectDbId}/${expectedSchema}/${encodeURIComponent(
-        expectedKeyword,
-      )}/${forceRefresh}/`,
-    });
   });
 
   it('returns hasMore when total is larger than result size', async () => {