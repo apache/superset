953a7a818e9b02463686f5255541897afa047c87
Rahul Raj <rahul.raj@careem.com>
2022-09-19 09:22:34 +0500

    fix: type errors

--- superset/views/explore_response.py
+++ superset/views/explore_response.py
@@ -14,7 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-# pylint: skip-file
+# type: ignore
 from typing import Any, cast, Dict, List, Tuple
 from uuid import uuid4
 
