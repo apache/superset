8514544ccdad7cdb725a4fd0ed28552a631f5075
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2022-09-22 16:12:33 +0200

    Create tier 1 and tier 2 slices

--- superset/examples/echarts_dashboard.py
+++ superset/examples/echarts_dashboard.py
@@ -50,31 +50,414 @@ def create_slices(tbl: SqlaTable) -> List[Slice]:
         "limit": "25",
         "time_range": "100 years ago : now",
         "granularity_sqla": "ds",
-        "groupby": ["gender"],
         "row_limit": "50000",
         "viz_type": "echarts_timeseries_bar",
+        "adhoc_filters": [
+            {
+                "clause": "WHERE",
+                "expressionType": "SIMPLE",
+                "filterOptionName": "filter_i7pmq9ob0vg_lvnj4s14yt",
+                "comparator": "10000",
+                "operator": ">",
+                "subject": "num_boys",
+            }
+        ],
     }
 
     slices = [
+        # ---------------------
+        # TIER 1
+        # ---------------------
+        Slice(
+            **slice_props,
+            slice_name="Big Number",
+            viz_type="big_number_total",
+            params=get_slice_json(
+                defaults,
+                viz_type="big_number_total",
+                metric="sum__num",
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Big Number with Trendline",
+            viz_type="big_number",
+            params=get_slice_json(
+                defaults,
+                viz_type="big_number",
+                metric="sum__num",
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Table",
+            viz_type="table",
+            params=get_slice_json(
+                defaults,
+                viz_type="table",
+                metrics=["sum__num"],
+                groupby=["gender"],
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Table",
+            viz_type="pivot_table_v2",
+            params=get_slice_json(
+                defaults,
+                viz_type="pivot_table_v2",
+                metrics=["sum__num"],
+                groupbyColumns=["gender"],
+                groupbyRows=["state"],
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Time-Series Line Chart",
+            viz_type="echarts_timeseries_line",
+            params=get_slice_json(
+                defaults,
+                viz_type="echarts_timeseries_line",
+                metrics=["sum__num"],
+                groupby=["gender"],
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Time-Series Area Chart",
+            viz_type="echarts_area",
+            params=get_slice_json(
+                defaults,
+                viz_type="echarts_area",
+                metrics=["sum__num"],
+                groupby=["gender"],
+            ),
+        ),
         Slice(
             **slice_props,
             slice_name="Time-Series Bar Chart V2",
             viz_type="echarts_timeseries_bar",
             params=get_slice_json(
                 defaults,
-                adhoc_filters=[
-                    {
-                        "clause": "WHERE",
-                        "expressionType": "SIMPLE",
-                        "filterOptionName": "filter_i7pmq9ob0vg_lvnj4s14yt",
-                        "comparator": "10000",
-                        "operator": ">",
-                        "subject": "num_boys",
-                    }
-                ],
+                viz_type="echarts_timeseries_bar",
+                metrics=["sum__num"],
+                groupby=["gender"],
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Time-Series Scatter Chart",
+            viz_type="echarts_timeseries_scatter",
+            params=get_slice_json(
+                defaults,
+                viz_type="echarts_timeseries_scatter",
+                metrics=["sum__num"],
+                groupby=["gender"],
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Pie Chart",
+            viz_type="pie",
+            params=get_slice_json(
+                defaults,
+                viz_type="pie",
+                metric="sum__num",
+                groupby=["gender"],
+                adhoc_filters=[],
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Bar Chart",
+            viz_type="dist_bar",
+            params=get_slice_json(
+                defaults,
                 viz_type="dist_bar",
                 metrics=["sum__num"],
                 groupby=["gender"],
+            ),
+        ),
+        # TODO: use a different dataset for world map
+        Slice(
+            **slice_props,
+            slice_name="World Map",
+            viz_type="world_map",
+            params=get_slice_json(
+                defaults,
+                viz_type="world_map",
+                metric="sum__num",
+                entity="gender",
+            ),
+        ),
+        # ---------------------
+        # TIER 2
+        # ---------------------
+        Slice(
+            **slice_props,
+            slice_name="Box Plot Chart",
+            viz_type="box_plot",
+            params=get_slice_json(
+                defaults,
+                viz_type="box_plot",
+                metrics=["sum__num"],
+                groupby=["gender"],
+                columns=["name"],
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Bubble Chart",
+            viz_type="bubble",
+            params=get_slice_json(
+                defaults,
+                viz_type="bubble",
+                size="count",
+                series="state",
+                entity="gender",
+                x={
+                    "expressionType": "SIMPLE",
+                    "column": {
+                        "column_name": "num_boys",
+                    },
+                    "aggregate": "SUM",
+                    "label": "SUM(num_boys)",
+                    "optionName": "metric_353e7rjj84m_cirsi2o2s1",
+                },
+                y={
+                    "expressionType": "SIMPLE",
+                    "column": {
+                        "column_name": "num_girls",
+                    },
+                    "aggregate": "SUM",
+                    "label": "SUM(num_girls)",
+                    "optionName": "metric_n8rvsr2ysmr_cb3eybtoe5f",
+                },
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Calendar heatmap",
+            viz_type="cal_heatmap",
+            params=get_slice_json(
+                defaults,
+                viz_type="cal_heatmap",
+                metrics=["sum__num"],
+                time_range="2008-01-01 : 2008-02-01",
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Chord Chart",
+            viz_type="chord",
+            params=get_slice_json(
+                defaults,
+                viz_type="chord",
+                metric="sum__num",
+                groupby="gender",
+                column="state",
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Time-Series Percent Change Chart",
+            viz_type="compare",
+            params=get_slice_json(
+                defaults,
+                viz_type="compare",
+                metrics=["sum__num"],
+                groupby=["gender"],
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Time-Series Generic Chart",
+            viz_type="echarts_timeseries",
+            params=get_slice_json(
+                defaults,
+                viz_type="echarts_timeseries",
+                metrics=["sum__num"],
+                groupby=["gender"],
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Time-Series Smooth Line Chart",
+            viz_type="echarts_timeseries_smooth",
+            params=get_slice_json(
+                defaults,
+                viz_type="echarts_timeseries_smooth",
+                metrics=["sum__num"],
+                groupby=["gender"],
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Time-Series Step Line Chart",
+            viz_type="echarts_timeseries_step",
+            params=get_slice_json(
+                defaults,
+                viz_type="echarts_timeseries_step",
+                metrics=["sum__num"],
+                groupby=["gender"],
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Funnel Chart",
+            viz_type="funnel",
+            params=get_slice_json(
+                defaults,
+                viz_type="funnel",
+                metric="sum__num",
+                groupby=["gender"],
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Gauge Chart",
+            viz_type="gauge_chart",
+            params=get_slice_json(
+                defaults,
+                viz_type="gauge_chart",
+                metric="sum__num",
+                groupby=["gender"],
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Heatmap Chart",
+            viz_type="heatmap",
+            params=get_slice_json(
+                defaults,
+                viz_type="funnel",
+                metric="sum__num",
+                all_columns_x="gender",
+                all_columns_y="state",
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Line Chart",
+            viz_type="line",
+            params=get_slice_json(
+                defaults,
+                viz_type="line",
+                metrics=["sum__num"],
+                groupby=["gender"],
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Mixed Chart",
+            viz_type="mixed_timeseries",
+            params=get_slice_json(
+                defaults,
+                viz_type="mixed_timeseries",
+                metrics=["sum__num"],
+                groupby=["gender"],
+                metrics_b=["count"],
+                groupby_b=["state"],
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Partition Chart",
+            viz_type="partition",
+            params=get_slice_json(
+                defaults,
+                viz_type="partition",
+                metrics=["sum__num"],
+                groupby=["gender"],
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Radar Chart",
+            viz_type="radar",
+            params=get_slice_json(
+                defaults,
+                viz_type="radar",
+                metrics=[
+                    "sum__num",
+                    "count",
+                    {
+                        "expressionType": "SIMPLE",
+                        "column": {
+                            "column_name": "num_boys",
+                        },
+                        "aggregate": "SUM",
+                        "label": "SUM(num_boys)",
+                        "optionName": "metric_353e7rjj84m_cirsi2o2s1",
+                    },
+                ],
+                groupby=["gender"],
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Nightingale Chart",
+            viz_type="rose",
+            params=get_slice_json(
+                defaults,
+                viz_type="rose",
+                metrics=["sum__num"],
+                groupby=["gender"],
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Sankey Chart",
+            viz_type="sankey",
+            params=get_slice_json(
+                defaults,
+                viz_type="sankey",
+                metric="sum__num",
+                groupby=["gender", "state"],
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Sunburst Chart",
+            viz_type="sunburst",
+            params=get_slice_json(
+                defaults,
+                viz_type="sunburst",
+                metric="sum__num",
+                groupby=["gender", "state"],
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Treemap Chart",
+            viz_type="treemap",
+            params=get_slice_json(
+                defaults,
+                viz_type="treemap",
+                metrics=["sum__num"],
+                groupby=["gender"],
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Treemap V2 Chart",
+            viz_type="treemap_v2",
+            params=get_slice_json(
+                defaults,
+                viz_type="treemap_v2",
+                metric="sum__num",
+                groupby=["gender"],
+            ),
+        ),
+        Slice(
+            **slice_props,
+            slice_name="Word Cloud Chart",
+            viz_type="word_cloud",
+            params=get_slice_json(
+                defaults,
+                viz_type="word_cloud",
+                metric="sum__num",
+                series="state",
             ),
         ),
     ]
