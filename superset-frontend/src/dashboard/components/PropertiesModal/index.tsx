930148dec6770493fe18d44ff357112eb4cec0bb
Mayur <mayurnewase111@gmail.com>
2022-09-26 19:53:38 +0530

    fix(dashboard): show correct roles for dashboard access dropdown (#21549)

--- superset-frontend/src/dashboard/components/PropertiesModal/index.tsx
+++ superset-frontend/src/dashboard/components/PropertiesModal/index.tsx
@@ -129,12 +129,14 @@ const PropertiesModal = ({
       return SupersetClient.get({
         endpoint: `/api/v1/dashboard/related/${accessType}?q=${query}`,
       }).then(response => ({
-        data: response.json.result.map(
-          (item: { value: number; text: string }) => ({
+        data: response.json.result
+          .filter((item: { extra: { active: boolean } }) =>
+            item.extra.active !== undefined ? item.extra.active : true,
+          )
+          .map((item: { value: number; text: string }) => ({
             value: item.value,
             label: item.text,
-          }),
-        ),
+          })),
         totalCount: response.json.count,
       }));
     },
