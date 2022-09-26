b36bd3f083d0b2c125f472c23caa39b035ee5f27
Antonio Rivero Martinez <38889534+Antonio-RiveroMartnez@users.noreply.github.com>
2022-09-26 14:08:11 -0300

    fix(databases): GSheets and Clickhouse DBs are not allowed to upload files (#21065)

--- superset/databases/api.py
+++ superset/databases/api.py
@@ -129,6 +129,7 @@ class DatabaseRestApi(BaseSupersetModelRestApi):
         "server_cert",
         "sqlalchemy_uri",
         "is_managed_externally",
+        "engine_information",
     ]
     list_columns = [
         "allow_file_upload",
@@ -151,6 +152,7 @@ class DatabaseRestApi(BaseSupersetModelRestApi):
         "force_ctas_schema",
         "id",
         "disable_data_preview",
+        "engine_information",
     ]
     add_columns = [
         "database_name",
@@ -1062,6 +1064,13 @@ class DatabaseRestApi(BaseSupersetModelRestApi):
                         parameters:
                           description: JSON schema defining the needed parameters
                           type: object
+                        engine_information:
+                          description: Dict with public properties form the DB Engine
+                          type: object
+                          properties:
+                            supports_file_upload:
+                              description: Whether the engine supports file uploads
+                              type: boolean
             400:
               $ref: '#/components/responses/400'
             500:
@@ -1078,6 +1087,7 @@ class DatabaseRestApi(BaseSupersetModelRestApi):
                 "engine": engine_spec.engine,
                 "available_drivers": sorted(drivers),
                 "preferred": engine_spec.engine_name in preferred_databases,
+                "engine_information": engine_spec.get_public_information(),
             }
 
             if engine_spec.default_driver:
