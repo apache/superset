7bd19da0818a0b6c6693bf15d39dd4dc33191366
Herbert Gainor <herbert.gainor@preset.io>
2022-09-26 18:54:48 -0600

    Use localeCompare with Natural Sorting

--- superset-frontend/src/explore/actions/saveModalActions.js
+++ superset-frontend/src/explore/actions/saveModalActions.js
@@ -46,13 +46,10 @@ export function fetchDashboards(userId) {
         choices.sort((a, b) => {
           const labelA = a.label.toUpperCase();
           const labelB = b.label.toUpperCase();
-          if (labelA < labelB) {
-            return -1;
-          }
-          if (labelA > labelB) {
-            return 1;
-          }
-          return 0;
+          return labelA.localeCompare(labelB, {
+            sensitivity: 'base',
+            numeric: true,
+          });
         });
 
         return dispatch(fetchDashboardsSucceeded(choices));
