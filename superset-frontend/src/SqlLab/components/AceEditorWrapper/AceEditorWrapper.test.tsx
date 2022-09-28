e62c7a5a0243c42fcd25c7b60d5d01020460a7e0
EugeneTorap <evgenykrutpro@gmail.com>
2022-09-28 03:04:34 +0300

    chore: refactor AceEditorWrapper to functional component (#21532)

--- superset-frontend/src/SqlLab/components/AceEditorWrapper/AceEditorWrapper.test.tsx
+++ superset-frontend/src/SqlLab/components/AceEditorWrapper/AceEditorWrapper.test.tsx
@@ -23,11 +23,6 @@ import { render, waitFor } from 'spec/helpers/testing-library';
 import { QueryEditor } from 'src/SqlLab/types';
 import { Store } from 'redux';
 import { initialState, defaultQueryEditor } from 'src/SqlLab/fixtures';
-import {
-  queryEditorSetSelectedText,
-  queryEditorSetFunctionNames,
-  addTable,
-} from 'src/SqlLab/actions/sqlLab';
 import AceEditorWrapper from 'src/SqlLab/components/AceEditorWrapper';
 import { AsyncAceEditorProps } from 'src/components/AsyncAceEditor';
 
@@ -54,11 +49,6 @@ const setup = (queryEditor: QueryEditor, store?: Store) =>
   render(
     <AceEditorWrapper
       queryEditor={queryEditor}
-      actions={{
-        queryEditorSetSelectedText,
-        queryEditorSetFunctionNames,
-        addTable,
-      }}
       height="100px"
       hotkeys={[]}
       database={{}}
@@ -79,27 +69,6 @@ describe('AceEditorWrapper', () => {
 
     expect(getByTestId('react-ace')).toHaveTextContent(
       JSON.stringify({ value: defaultQueryEditor.sql }).slice(1, -1),
-    );
-  });
-
-  it('renders sql from unsaved change', () => {
-    const expectedSql = 'SELECT updated_column\nFROM updated_table\nWHERE';
-    const { getByTestId } = setup(
-      defaultQueryEditor,
-      mockStore({
-        ...initialState,
-        sqlLab: {
-          ...initialState.sqlLab,
-          unsavedQueryEditor: {
-            id: defaultQueryEditor.id,
-            sql: expectedSql,
-          },
-        },
-      }),
-    );
-
-    expect(getByTestId('react-ace')).toHaveTextContent(
-      JSON.stringify({ value: expectedSql }).slice(1, -1),
     );
   });
 
