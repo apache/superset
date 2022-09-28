cf400d0ab70de010001d77d8e151c05b1889f316
cccs-Dustin <96579982+cccs-Dustin@users.noreply.github.com>
2022-09-28 10:28:03 -0400

    Removed unused config settings

--- superset-frontend/src/filters/components/Adhoc/controlPanel.ts
+++ superset-frontend/src/filters/components/Adhoc/controlPanel.ts
@@ -24,14 +24,7 @@ import {
 } from '@superset-ui/chart-controls';
 import { DEFAULT_FORM_DATA } from './types';
 
-const {
-  enableEmptyFilter,
-  inverseSelection,
-  multiSelect,
-  defaultToFirstItem,
-  searchAllOptions,
-  sortAscending,
-} = DEFAULT_FORM_DATA;
+const { enableEmptyFilter } = DEFAULT_FORM_DATA;
 
 const config: ControlPanelConfig = {
   controlPanelSections: [
@@ -59,31 +52,6 @@ const config: ControlPanelConfig = {
       controlSetRows: [
         [
           {
-            name: 'sortAscending',
-            config: {
-              type: 'CheckboxControl',
-              renderTrigger: true,
-              label: t('Sort ascending'),
-              default: sortAscending,
-              description: t('Check for sorting ascending'),
-            },
-          },
-        ],
-        [
-          {
-            name: 'multiSelect',
-            config: {
-              type: 'CheckboxControl',
-              label: t('Can select multiple values'),
-              default: multiSelect,
-              resetConfig: true,
-              affectsDataMask: true,
-              renderTrigger: true,
-            },
-          },
-        ],
-        [
-          {
             name: 'enableEmptyFilter',
             config: {
               type: 'CheckboxControl',
@@ -92,53 +60,6 @@ const config: ControlPanelConfig = {
               renderTrigger: true,
               description: t(
                 'User must select a value before applying the filter',
-              ),
-            },
-          },
-        ],
-        [
-          {
-            name: 'defaultToFirstItem',
-            config: {
-              type: 'CheckboxControl',
-              label: t('Select first filter value by default'),
-              default: defaultToFirstItem,
-              resetConfig: true,
-              affectsDataMask: true,
-              renderTrigger: true,
-              requiredFirst: true,
-              description: t(
-                'When using this option, default value can’t be set',
-              ),
-            },
-          },
-        ],
-        [
-          {
-            name: 'inverseSelection',
-            config: {
-              type: 'CheckboxControl',
-              renderTrigger: true,
-              affectsDataMask: true,
-              label: t('Inverse selection'),
-              default: inverseSelection,
-              description: t('Exclude selected values'),
-            },
-          },
-        ],
-        [
-          {
-            name: 'searchAllOptions',
-            config: {
-              type: 'CheckboxControl',
-              renderTrigger: true,
-              affectsDataMask: true,
-              label: t('Dynamically search all filter values'),
-              default: searchAllOptions,
-              description: t(
-                'By default, each filter loads at most 1000 choices at the initial page load. ' +
-                  'Check this box if you have more than 1000 filter values and want to enable dynamically ' +
-                  'searching that loads filter values as users type (may add stress to your database).',
               ),
             },
           },
