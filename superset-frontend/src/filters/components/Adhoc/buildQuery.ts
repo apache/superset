c796777c09055c2199c5b84216f785248c71a350
cccs-RyanK <102618419+cccs-RyanK@users.noreply.github.com>
2022-09-29 10:25:45 -0400

    removed column for filter config form

--- superset-frontend/src/filters/components/Adhoc/buildQuery.ts
+++ superset-frontend/src/filters/components/Adhoc/buildQuery.ts
@@ -18,62 +18,26 @@
  */
 import {
   buildQueryContext,
-  GenericDataType,
-  getColumnLabel,
-  isPhysicalColumn,
   QueryObject,
   QueryObjectFilterClause,
   BuildQuery,
 } from '@superset-ui/core';
-import { DEFAULT_FORM_DATA, PluginFilterSelectQueryFormData } from './types';
+import { PluginFilterSelectQueryFormData } from './types';
 
 const buildQuery: BuildQuery<PluginFilterSelectQueryFormData> = (
   formData: PluginFilterSelectQueryFormData,
-  options,
-) => {
-  const { search, coltypeMap } = options?.ownState || {};
-  const { sortAscending, sortMetric } = { ...DEFAULT_FORM_DATA, ...formData };
-  return buildQueryContext(formData, baseQueryObject => {
-    const { columns = [], filters = [] } = baseQueryObject;
+) =>
+  buildQueryContext(formData, baseQueryObject => {
+    const { filters = [] } = baseQueryObject;
     const extraFilters: QueryObjectFilterClause[] = [];
-    if (search) {
-      columns.filter(isPhysicalColumn).forEach(column => {
-        const label = getColumnLabel(column);
-        if (coltypeMap[label] === GenericDataType.STRING) {
-          extraFilters.push({
-            col: column,
-            op: 'ILIKE',
-            val: `%${search}%`,
-          });
-        } else if (
-          coltypeMap[label] === GenericDataType.NUMERIC &&
-          !Number.isNaN(Number(search))
-        ) {
-          // for numeric columns we apply a >= where clause
-          extraFilters.push({
-            col: column,
-            op: '>=',
-            val: Number(search),
-          });
-        }
-      });
-    }
-
-    const sortColumns = sortMetric ? [sortMetric] : columns;
     const query: QueryObject[] = [
       {
         ...baseQueryObject,
-        groupby: columns,
-        metrics: sortMetric ? [sortMetric] : [],
+        result_type: 'columns',
         filters: filters.concat(extraFilters),
-        orderby:
-          sortMetric || sortAscending !== undefined
-            ? sortColumns.map(column => [column, !!sortAscending])
-            : [],
       },
     ];
     return query;
   });
-};
 
 export default buildQuery;
