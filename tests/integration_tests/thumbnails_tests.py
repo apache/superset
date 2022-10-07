d3f6d1cd0564a5a619bfa72c03e81e849b957092
Rui Zhao <zhaorui@dropbox.com>
2022-10-06 17:24:42 -0700

    fix function references after pylint update

--- tests/integration_tests/thumbnails_tests.py
+++ tests/integration_tests/thumbnails_tests.py
@@ -66,7 +66,7 @@ class TestWebDriverScreenshotErrorDetector(SupersetTestCase):
 
     @patch("superset.utils.webdriver.WebDriverWait")
     @patch("superset.utils.webdriver.firefox")
-    @patch("superset.utils.webdriver.WebDriverProxy.find_unexpected_errors")
+    @patch("superset.utils.webdriver.find_unexpected_errors")
     def test_not_call_find_unexpected_errors_if_feature_disabled(
         self, mock_find_unexpected_errors, mock_firefox, mock_webdriver_wait
     ):
@@ -81,7 +81,7 @@ class TestWebDriverScreenshotErrorDetector(SupersetTestCase):
 
     @patch("superset.utils.webdriver.WebDriverWait")
     @patch("superset.utils.webdriver.firefox")
-    @patch("superset.utils.webdriver.WebDriverProxy.find_unexpected_errors")
+    @patch("superset.utils.webdriver.find_unexpected_errors")
     def test_call_find_unexpected_errors_if_feature_enabled(
         self, mock_find_unexpected_errors, mock_firefox, mock_webdriver_wait
     ):
