ed095c10655120706496fd2589b99426bb7416ef
lyndsiWilliams <kcatgirl@gmail.com>
2022-10-17 08:12:31 -0500

    Add table selected visuals

--- superset-frontend/src/views/CRUD/data/dataset/AddDataset/LeftPanel/index.tsx
+++ superset-frontend/src/views/CRUD/data/dataset/AddDataset/LeftPanel/index.tsx
@@ -127,6 +127,13 @@ const LeftPanelStyle = styled.div`
 `}
 `;
 
+export const Loader = (inline: string) => (
+  <div className="loading-container">
+    <Loading position="inline" />
+    <p>{inline}</p>
+  </div>
+);
+
 export default function LeftPanel({
   setDataset,
   schema,
@@ -221,13 +228,6 @@ export default function LeftPanel({
 
   const filteredOptions = tableOptions.filter(option =>
     option?.value?.toLowerCase().includes(searchVal.toLowerCase()),
-  );
-
-  const Loader = (inline: string) => (
-    <div className="loading-container">
-      <Loading position="inline" />
-      <p>{inline}</p>
-    </div>
   );
 
   return (
