009fc23a16e8dcfeb099dc61ac3beb3e2e909137
Michael S. Molina <michael.s.molina@gmail.com>
2022-10-18 09:59:39 -0300

    Improves property description

--- superset-frontend/src/components/MessageToasts/types.ts
+++ superset-frontend/src/components/MessageToasts/types.ts
@@ -31,8 +31,6 @@ export interface ToastMeta {
   /** Whether to skip displaying this message if there are another toast
    * with the same message. */
   noDuplicate?: boolean;
-  /** For security reasons, HTML rendering is disabled. Use this property to enable
-   * HTML rendering and make sure any required sanitization is applied.
-   */
+  /** For security reasons, HTML rendering is disabled by default. Use this property to enable it. */
   allowHtml?: boolean;
 }
