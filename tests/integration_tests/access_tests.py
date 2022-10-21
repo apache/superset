cbb400c2a6c61fb9cd7900061f4c90f7a5f7cb8a
hughhhh <hughmil3s@gmail.com>
2022-10-21 10:57:00 -0700

    refactor

--- tests/integration_tests/access_tests.py
+++ tests/integration_tests/access_tests.py
@@ -189,33 +189,33 @@ class TestRequestAccess(SupersetTestCase):
         with database.get_sqla_engine_with_context() as engine:
             schema = inspect(engine).default_schema_name
 
-            override_me = security_manager.find_role("override_me")
-            override_me.permissions.append(
-                security_manager.find_permission_view_menu(
-                    view_menu_name=self.get_table(name="energy_usage").perm,
-                    permission_name="datasource_access",
-                )
+        override_me = security_manager.find_role("override_me")
+        override_me.permissions.append(
+            security_manager.find_permission_view_menu(
+                view_menu_name=self.get_table(name="energy_usage").perm,
+                permission_name="datasource_access",
             )
-            db.session.flush()
+        )
+        db.session.flush()
 
-            perm_data = ROLE_TABLES_PERM_DATA.copy()
-            perm_data["database"][0]["schema"][0]["name"] = schema
+        perm_data = ROLE_TABLES_PERM_DATA.copy()
+        perm_data["database"][0]["schema"][0]["name"] = schema
 
-            response = self.client.post(
-                "/superset/override_role_permissions/",
-                data=json.dumps(perm_data),
-                content_type="application/json",
-            )
-            self.assertEqual(201, response.status_code)
-            updated_override_me = security_manager.find_role("override_me")
-            self.assertEqual(1, len(updated_override_me.permissions))
-            birth_names = self.get_table(name="birth_names")
-            self.assertEqual(
-                birth_names.perm, updated_override_me.permissions[0].view_menu.name
-            )
-            self.assertEqual(
-                "datasource_access", updated_override_me.permissions[0].permission.name
-            )
+        response = self.client.post(
+            "/superset/override_role_permissions/",
+            data=json.dumps(perm_data),
+            content_type="application/json",
+        )
+        self.assertEqual(201, response.status_code)
+        updated_override_me = security_manager.find_role("override_me")
+        self.assertEqual(1, len(updated_override_me.permissions))
+        birth_names = self.get_table(name="birth_names")
+        self.assertEqual(
+            birth_names.perm, updated_override_me.permissions[0].view_menu.name
+        )
+        self.assertEqual(
+            "datasource_access", updated_override_me.permissions[0].permission.name
+        )
 
     def test_clean_requests_after_role_extend(self):
         session = db.session
