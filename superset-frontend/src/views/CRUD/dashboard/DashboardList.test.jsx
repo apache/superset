976e34431d6c056b3ca88e619e3be34b4726b6fb
Rui Zhao <zhaorui@dropbox.com>
2022-10-24 16:57:17 -0700

    Simplify mocks in non-RefreshIntervalModal tests

--- superset-frontend/src/views/CRUD/dashboard/DashboardList.test.jsx
+++ superset-frontend/src/views/CRUD/dashboard/DashboardList.test.jsx
@@ -103,14 +103,6 @@ jest.mock('src/configs', () => ({
     DASHBOARD_AUTO_REFRESH_INTERVALS: [
       [0, "Don't refresh"],
       [10, '10 seconds'],
-      [30, '30 seconds'],
-      [60, '1 minute'],
-      [300, '5 minutes'],
-      [1800, '30 minutes'],
-      [3600, '1 hour'],
-      [21600, '6 hours'],
-      [43200, '12 hours'],
-      [86400, '24 hours'],
     ],
   })),
 }));
