773a6c86f64445a6d67c68cf7e30c245a45b2cd4
hughhhh <hughmil3s@gmail.com>
2022-10-25 11:44:48 -0400

    fix linting

--- superset/databases/models.py
+++ superset/databases/models.py
@@ -14,7 +14,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-from typing import List
 
 import sqlalchemy as sa
 from flask_appbuilder import Model
