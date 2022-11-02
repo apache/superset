518248b83c1d386f47c70eb9f6d11cf5277ce4f8
cccs-RyanK <102618419+cccs-RyanK@users.noreply.github.com>
2022-11-02 15:10:36 -0400

    disallow columns that are not a part of the chart to be selected as default emit filters

--- superset-frontend/src/cccs-viz/plugins/plugin-chart-cccs-grid/src/plugin/controlPanel.tsx
+++ superset-frontend/src/cccs-viz/plugins/plugin-chart-cccs-grid/src/plugin/controlPanel.tsx
@@ -340,6 +340,7 @@ const config: ControlPanelConfig = {
                     : [];
                 return newState;
               },
+              rerender: ['defaultEmitFilterColumn'],
               visibility: isRawMode,
               canCopy: true,
             } as typeof sharedControls.groupby,
@@ -431,10 +432,18 @@ const config: ControlPanelConfig = {
                     state: ControlPanelState,
                     controlState: ControlState,
                   ) => {
+                    const { controls } = state;
                     const originalMapStateToProps =
                       sharedControls?.columns?.mapStateToProps;
                     const newState =
                       originalMapStateToProps?.(state, controlState) ?? {};
+                    newState.choices = controls?.columns?.value;
+                    const invalidOption = ensureIsArray(
+                      controlState.value,
+                    ).find(c => !newState.choices.includes(c));
+                    newState.externalValidationErrors = invalidOption
+                      ? [`'${invalidOption}'${t(' is not a valid option')}`]
+                      : [];
                     return newState;
                   },
                   visibility: ({ controls }) =>
