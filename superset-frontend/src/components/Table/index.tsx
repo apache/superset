8dab6ffc998e4b4a50efe9e53c9b24368ab20c28
Eric Briscoe <eric.j.briscoe@gmail.com>
2022-11-07 10:51:08 -0800

    Update superset-frontend/src/components/Table/index.tsx
    Co-authored-by: Lyndsi Kay Williams <55605634+lyndsiWilliams@users.noreply.github.com>

--- superset-frontend/src/components/Table/index.tsx
+++ superset-frontend/src/components/Table/index.tsx
@@ -264,7 +264,7 @@ export function Table(props: TableProps) {
     if (resizable === true) {
       // eslint-disable-next-line no-console
       console.warn(
-        'EXPERIMENTAL FEATURE ENABLED: The "resizeable" prop of Table is experimental and NOT recommended for use in production deployments.',
+        'EXPERIMENTAL FEATURE ENABLED: The "resizable" prop of Table is experimental and NOT recommended for use in production deployments.',
       );
     }
   }, [reorderable, resizable]);
