af08fb568a141bbaeda1f561544652629dc399ee
Eric Briscoe <eric.j.briscoe@gmail.com>
2022-11-07 16:37:16 -0800

    Update superset-frontend/src/components/Table/cell-renderers/NumericCell/index.tsx
    Co-authored-by: Michael S. Molina <70410625+michael-s-molina@users.noreply.github.com>

--- superset-frontend/src/components/Table/cell-renderers/NumericCell/index.tsx
+++ superset-frontend/src/components/Table/cell-renderers/NumericCell/index.tsx
@@ -16,8 +16,8 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 import React from 'react';
+import { logging } from '@superset-ui/core';
 
 export interface NumericCellProps {
   /**
