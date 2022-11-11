9aa1167691ce84fd6a183ef629adafc82c8af559
Yongjie Zhao <yongjiezhao@apache.org>
2022-11-11 18:04:56 +0800

    refactor: update documents and rename variables (#22074)

--- superset-frontend/packages/superset-ui-chart-controls/src/operators/pivotOperator.ts
+++ superset-frontend/packages/superset-ui-chart-controls/src/operators/pivotOperator.ts
@@ -30,13 +30,13 @@ export const pivotOperator: PostProcessingFactory<PostProcessingPivot> = (
   queryObject,
 ) => {
   const metricLabels = ensureIsArray(queryObject.metrics).map(getMetricLabel);
-  const xAxis = getXAxisLabel(formData);
+  const xAxisLabel = getXAxisLabel(formData);
 
-  if (xAxis && metricLabels.length) {
+  if (xAxisLabel && metricLabels.length) {
     return {
       operation: 'pivot',
       options: {
-        index: [xAxis],
+        index: [xAxisLabel],
         columns: ensureIsArray(queryObject.columns).map(getColumnLabel),
         // Create 'dummy' mean aggregates to assign cell values in pivot table
         // use the 'mean' aggregates to avoid drop NaN. PR: https://github.com/apache-superset/superset-ui/pull/1231
