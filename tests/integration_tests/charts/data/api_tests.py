92450c6068e68262f68fcf81397b998ab9a042f0
AAfghahi <arash.afghahi@gmail.com>
2022-11-17 14:05:53 -0500

    added fixtures

--- tests/integration_tests/charts/data/api_tests.py
+++ tests/integration_tests/charts/data/api_tests.py
@@ -38,8 +38,12 @@ from tests.integration_tests.fixtures.birth_names_dashboard import (
     load_birth_names_data,
 )
 from tests.integration_tests.test_app import app
-
+from tests.integration_tests.fixtures.energy_dashboard import (
+    load_energy_table_with_slice,
+    load_energy_table_data,
+)
 import pytest
+from superset.models.slice import Slice
 
 from superset.charts.data.commands.get_data_command import ChartDataCommand
 from superset.connectors.sqla.models import TableColumn, SqlaTable
@@ -1027,6 +1031,7 @@ def test_data_cache_default_timeout(
 
 
 def test_chart_cache_timeout(
+    test_client,
     login_as_admin,
     physical_query_context,
     load_energy_table_with_slice: List[Slice],
