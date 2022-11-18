a17333ac0601aa73a40dde35b58438d4d4230a25
hughhhh <hughmil3s@gmail.com>
2022-11-18 14:45:56 -0500

    update test

--- tests/unit_tests/databases/ssh_tunnel/commands/create_test.py
+++ tests/unit_tests/databases/ssh_tunnel/commands/create_test.py
@@ -26,7 +26,6 @@ def test_create_ssh_tunnel_command() -> None:
     db = Database(database_name="my_database", sqlalchemy_uri="sqlite://")
 
     properties = {
-        "database_id": db.id,
         "server_address": "123.132.123.1",
         "server_port": "3005",
         "username": "foo",
