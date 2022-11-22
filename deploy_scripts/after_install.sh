0632d4bb62391170ff8d27ee56641d357e7b0c47
TusharVirk <Tushar.Virk@locationiq.com.au>
2022-11-23 09:37:10 +1100

    fixed deploy scripts, understood more about the lifecylce

---
+++ deploy_scripts/after_install.sh
@@ -0,0 +1,4 @@
+# Remove default config and replace with existing config'
+echo "Overriding settings"
+sudo rm /home/ec2-user/liq_superset/docker/pythonpath_dev/superset_config.py
+sudo cp /home/ec2-user/superset_config.py /home/ec2-user/liq_superset/docker/pythonpath_dev/
--- deploy_scripts/before_install.sh
+++ deploy_scripts/before_install.sh
@@ -8,7 +8,3 @@ else
   echo "Creating ${DIR} directory"
   mkdir ${DIR}
 fi
-
-# Remove default config and replace with existing config
-sudo rm /home/ec2-user/liq_superset/docker/pythonpath_dev/superset_config.py
-sudo cp /home/ec2-user/superset_config.py /home/ec2-user/liq_superset/docker/pythonpath_dev/
