df90802c105be7c5281fcaa748a38ec2a73a67b2
TusharVirk <Tushar.Virk@locationiq.com.au>
2022-11-23 10:01:14 +1100

    added sleep 5s just in case

--- deploy_scripts/after_install.sh
+++ deploy_scripts/after_install.sh
@@ -2,3 +2,4 @@
 echo "Overriding settings"
 sudo rm /home/ec2-user/liq_superset/docker/pythonpath_dev/superset_config.py
 sudo cp /home/ec2-user/superset_config.py /home/ec2-user/liq_superset/docker/pythonpath_dev/
+sleep 5
