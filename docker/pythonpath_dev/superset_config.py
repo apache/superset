b8a10395dc26754f510a5a20650d822d9a76efdb
大饼 <shenqihui0920@gmail.com>
2022-11-22 16:01:18 +0800

    change

--- docker/pythonpath_dev/superset_config.py
+++ docker/pythonpath_dev/superset_config.py
@@ -74,7 +74,7 @@ REDIS_PORT = get_env_variable("REDIS_PORT")
 REDIS_CELERY_DB = get_env_variable("REDIS_CELERY_DB", "0")
 REDIS_RESULTS_DB = get_env_variable("REDIS_RESULTS_DB", "1")
 
-RESULTS_BACKEND = FileSystemCache("/app/superset_home/sqllab")
+RESULTS_BACKEND = FileSystemCache("/www/sqllab")
 
 SECRET_KEY = "1CHPNk/gHXVuVz9iKlTcxI6c1fNIq0avLPibcM9P6sfLUyokwYtbhhvn"
 
