f00bd69dc9d6d5d99af79d8fb6b809f4067e77e2
Antonio Rivero <antonioriverocode@gmail.com>
2022-11-22 15:54:21 -0300

    SSH Tunnel:
    - Initial DELETE and CREATE ssh tunnel APIs
    - Add happy path tests for each API
    
    NOTE:
    - Updating a DB might also result in creating a Tunnel, that flow will get tested in the UPDATE API changes.

--- superset/databases/commands/create.py
+++ superset/databases/commands/create.py
@@ -31,6 +31,9 @@ from superset.databases.commands.exceptions import (
 )
 from superset.databases.commands.test_connection import TestConnectionDatabaseCommand
 from superset.databases.dao import DatabaseDAO
+from superset.databases.ssh_tunnel.commands.exceptions import SSHTunnelInvalidError
+from superset.databases.ssh_tunnel.dao import SSHTunnelDAO
+from superset.databases.ssh_tunnel.models import SSHTunnel
 from superset.exceptions import SupersetErrorsException
 from superset.extensions import db, event_logger, security_manager
 
@@ -77,6 +80,16 @@ class CreateDatabaseCommand(BaseCommand):
                 security_manager.add_permission_view_menu(
                     "schema_access", security_manager.get_schema_perm(database, schema)
                 )
+
+            if self._properties.get("ssh_tunnel"):
+                SSHTunnelDAO.create(
+                    {
+                        **self._properties.get("ssh_tunnel"),
+                        "database_id": database.id,
+                    },
+                    commit=False,
+                )
+
             db.session.commit()
         except DAOCreateFailedError as ex:
             db.session.rollback()
