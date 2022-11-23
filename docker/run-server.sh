05c96def02c6354503fb28d32601f235ddc45a03
TusharVirk <Tushar.Virk@locationiq.com.au>
2022-11-23 11:15:22 +1100

    attempting prod server gunicorn

--- docker/run-server.sh
+++ docker/run-server.sh
@@ -23,10 +23,12 @@ gunicorn \
     --bind "${SUPERSET_BIND_ADDRESS:-0.0.0.0}:${SUPERSET_PORT:-8088}" \
     --access-logfile "${ACCESS_LOG_FILE:-$HYPHEN_SYMBOL}" \
     --error-logfile "${ERROR_LOG_FILE:-$HYPHEN_SYMBOL}" \
-    --workers ${SERVER_WORKER_AMOUNT:-1} \
+    --workers ${SERVER_WORKER_AMOUNT:-10} \
+    -k gevent \
+    --worker-connections 1000 \
     --worker-class ${SERVER_WORKER_CLASS:-gthread} \
     --threads ${SERVER_THREADS_AMOUNT:-20} \
-    --timeout ${GUNICORN_TIMEOUT:-60} \
+    --timeout ${GUNICORN_TIMEOUT:-120} \
     --keep-alive ${GUNICORN_KEEPALIVE:-2} \
     --max-requests ${WORKER_MAX_REQUESTS:-0} \
     --max-requests-jitter ${WORKER_MAX_REQUESTS_JITTER:-0} \
