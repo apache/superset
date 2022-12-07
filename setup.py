b7cc11028139d5099f5845149fab5ef28030c5a2
EugeneTorap <evgenykrutpro@gmail.com>
2022-12-07 23:49:22 +0300

    Downgrade flask

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ setup(
         "cron-descriptor",
         "cryptography>=3.3.2",
         "deprecation>=2.1.0, <2.2.0",
-        "flask>=2.2.2, <2.3",
+        "flask>=2.1.3, <2.2",
         "flask-appbuilder>=4.1.6, <5.0.0",
         "flask-caching>=1.10.0",
         "flask-compress",
