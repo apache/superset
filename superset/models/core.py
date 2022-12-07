eee9391ceb7483428e554ee827990e470f9940a4
hughhhh <hughmil3s@gmail.com>
2022-12-07 17:16:28 -0500

    setup get raw connection context manager

--- superset/models/core.py
+++ superset/models/core.py
@@ -423,19 +423,20 @@ class Database(
             return create_engine(sqlalchemy_url, **params)
         except Exception as ex:
             raise self.db_engine_spec.get_dbapi_mapped_exception(ex)
-    
+
     @contextmanager
     def get_raw_connection(
         self,
         schema: Optional[str] = None,
         nullpool: bool = True,
         source: Optional[utils.QuerySource] = None,
-        **kwargs,
-        ) -> Connection:
-        with self.get_sqla_engine_with_context(**kwargs) as engine:
+    ) -> Connection:
+        with self.get_sqla_engine_with_context(
+            schema=schema, nullpool=nullpool, source=source
+        ) as engine:
             with closing(engine.raw_connection()) as conn:
                 yield conn
-    
+
     @property
     def quote_identifier(self) -> Callable[[str], str]:
         """Add quotes to potential identifiter expressions if needed"""
