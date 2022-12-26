7a8072c7e17e91723f03bae37aa2e9624665ad40
sivanitski <serhiiivan2@gmail.com>
2022-12-27 00:29:27 +0100

    file upload ui

--- superset-frontend/src/views/App.tsx
+++ superset-frontend/src/views/App.tsx
@@ -29,6 +29,7 @@ import { GlobalStyles } from 'src/GlobalStyles';
 import ErrorBoundary from 'src/components/ErrorBoundary';
 import Loading from 'src/components/Loading';
 import Menu from 'src/views/components/Menu';
+import Upload from 'src/views/components/Upload';
 import { bootstrapData } from 'src/preamble';
 import ToastContainer from 'src/components/MessageToasts/ToastContainer';
 import setupApp from 'src/setup/setupApp';
@@ -79,6 +80,7 @@ const App = () => (
       <RootContextProviders>
         <GlobalStyles />
         <Menu data={menu} isFrontendRoute={isFrontendRoute} />
+        <Upload />
         <Switch>
           {routes.map(({ path, Component, props = {}, Fallback = Loading }) => (
             <Route path={path} key={path}>
