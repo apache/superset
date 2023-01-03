b93d6e3d0b9c8ee3414708f6a0e7d4599b619ad1
Ville Brofeldt <ville.brofeldt@apple.com>
2023-01-03 07:07:40 +0000

    add ?. just in case

--- superset-frontend/packages/superset-ui-chart-controls/src/shared-controls/sharedControls.tsx
+++ superset-frontend/packages/superset-ui-chart-controls/src/shared-controls/sharedControls.tsx
@@ -193,8 +193,8 @@ const time_grain_sqla: SharedControlConfig<'SelectControl'> = {
     // If a chart is a new one that isn't saved, metadata is null. In this
     // case we want to default P1D. If the chart has been saved, we want
     // to use whichever value was chosen, either nothing or valid a time grain.
-    return state.metadata || 'time_grain_sqla' in (state?.form_data ?? {})
-      ? state.form_data?.time_grain_sqla
+    return state?.metadata || 'time_grain_sqla' in (state?.form_data ?? {})
+      ? state?.form_data?.time_grain_sqla
       : 'P1D';
   },
   description: t(
