8b4cd90ac930f127f65515c2d81b05b0ce8b1467
lyndsiWilliams <kcatgirl@gmail.com>
2023-01-05 19:09:39 -0600

    Remove filter bar from reports

--- superset-frontend/src/dashboard/components/DashboardBuilder/DashboardBuilder.tsx
+++ superset-frontend/src/dashboard/components/DashboardBuilder/DashboardBuilder.tsx
@@ -452,17 +452,19 @@ const DashboardBuilder: FC<DashboardBuilderProps> = () => {
                 >
                   <StickyPanel ref={containerRef} width={filterBarWidth}>
                     <ErrorBoundary>
-                      <FilterBar
-                        focusedFilterId={focusedFilterId}
-                        orientation={FilterBarOrientation.VERTICAL}
-                        verticalConfig={{
-                          filtersOpen: dashboardFiltersOpen,
-                          toggleFiltersBar: toggleDashboardFiltersOpen,
-                          width: filterBarWidth,
-                          height: filterBarHeight,
-                          offset: filterBarOffset,
-                        }}
-                      />
+                      {!isReport && (
+                        <FilterBar
+                          focusedFilterId={focusedFilterId}
+                          orientation={FilterBarOrientation.VERTICAL}
+                          verticalConfig={{
+                            filtersOpen: dashboardFiltersOpen,
+                            toggleFiltersBar: toggleDashboardFiltersOpen,
+                            width: filterBarWidth,
+                            height: filterBarHeight,
+                            offset: filterBarOffset,
+                          }}
+                        />
+                      )}
                     </ErrorBoundary>
                   </StickyPanel>
                 </FiltersPanel>
