5e79413b2bf2d914ede586eb963bba2ffb5d3025
RobertSnow <snowrobert636@gmail.com>
2023-01-06 12:04:31 -0500

    fix/columns-auto-complete Lint

--- superset-frontend/src/views/CRUD/data/datamanage/pages/ContentPage.tsx
+++ superset-frontend/src/views/CRUD/data/datamanage/pages/ContentPage.tsx
@@ -144,7 +144,7 @@ const ContentPage = () => {
 
   const handleSearch = (value: string) => {
     const optionTemp: Object[] =[];
-    tableData.columns.forEach((itm: any) => {
+    tableData.columns.forEach(function (itm: any) {
       optionTemp.push({ value: itm.column_name });
     });
 
