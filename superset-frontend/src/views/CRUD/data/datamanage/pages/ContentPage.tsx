a9c2dff5bda72205b7a318895c6284bf73261e3d
whitewolfdev410 <whitewolf96410@gmail.com>
2023-01-09 14:55:58 +0100

    share_table_20130109

--- superset-frontend/src/views/CRUD/data/datamanage/pages/ContentPage.tsx
+++ superset-frontend/src/views/CRUD/data/datamanage/pages/ContentPage.tsx
@@ -19,7 +19,6 @@ import {
   Switch,
   Dropdown,
   notification,
-  AutoComplete,
 } from 'antd';
 import {
   AppstoreOutlined,
@@ -109,6 +108,10 @@ const ContentPage = () => {
     setSearchtext(ev.target.value);
   };
 
+  const handleUserSearch = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchUser(ev.target.value);
+  };
+
   const handleSort = () => {
     setSort((sort + 1) % 3);
   };
@@ -117,8 +120,41 @@ const ContentPage = () => {
     setOwner(ev.key);
   };
 
+  const cancelSelectPeople = () => {
+    setIsSelectPeople(false);
+  };
+
   const handleDatasourceChange = (ev: CheckboxChangeEvent) => {
     setDatasourceOne(ev.target.checked);
+  };
+
+  const handleSelectPeople = () => {
+    SupersetClient.get({
+      endpoint: `/api/v1/users/all`,
+    }).then(async ({ json = {} }) => {
+      setUserList(
+        json.result.filter((user: any) => {
+          const index = sharePeople.findIndex(
+            (people: any) => people.id === user.id,
+          );
+          if (index === -1) return true;
+          sharePeople[index].email = user.email;
+          return false;
+        }),
+      );
+    });
+    setIsSelectPeople(true);
+  };
+  const onChangeCheck = (e: CheckboxChangeEvent) => {
+    if (e.target.checked) {
+      const index = userList.findIndex(
+        (user: any) => user.id.toString() === e.target.value,
+      );
+      setSharePeople(sharePeople.concat(userList[index]));
+      let temp_userList = [...userList];
+      temp_userList.splice(index, 1);
+      setUserList(temp_userList);
+    }
   };
 
   useEffect(() => {
@@ -166,42 +202,6 @@ const ContentPage = () => {
     setFilteredTableData(tempData);
   }, [searchtext, data, sort, owner, datasourceOne]);
 
-  const handleSearch = (value: string) => {
-    /* prettier-ignore */
-    const optionTemp: Array<any> =[];
-    /* prettier-ignore */
-    tableData.columns.forEach(function (itm: any) {
-      /* prettier-ignore */
-      optionTemp.push({ value: itm.column_name });
-    });
-
-    setOptions(!value ? [] : optionTemp);
-  };
-
-  const handleKeyPress = (ev: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    console.log('handleKeyPress', ev);
-  };
-
-  const onSelect = (value: string) => {
-    console.log('onSelect', value);
-    setColumnExpression(value);
-  };
-
-  const handleSearchtext = (ev: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchtext(ev.target.value);
-  };
-
-  const handleSort = () => {
-    setSort((sort + 1) % 3);
-  };
-
-  const handleOwner = (ev: any) => {
-    setOwner(ev.key);
-  };
-
-  const handleDatasourceChange = (ev: CheckboxChangeEvent) => {
-    setDatasourceOne(ev.target.checked);
-  };
   const theme: SupersetTheme = useTheme();
 
   const showLargeDrawer = (e: any) => {
@@ -828,19 +828,13 @@ const ContentPage = () => {
             />
           </Col>
         </Row>
-        <AutoComplete
-          style={{ width: '100%' }}
-          options={options}
-          onSelect={onSelect}
-          onSearch={handleSearch}
-        >
-          <TextArea
-            placeholder="input here"
-            className="custom"
-            style={{ height: 50 }}
-            onKeyPress={handleKeyPress}
-          />
-        </AutoComplete>
+        <TextArea
+          rows={4}
+          value={columnExpression}
+          onChange={(e: any) => {
+            setColumnExpression(e.target.value);
+          }}
+        />
         <Row justify="center" gutter={16} style={{ marginTop: '24px' }}>
           <Col span="12">
             <Button
