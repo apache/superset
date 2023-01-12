77233943b106a0cebdbc5c197b2e640ff38592f0
“SamraHanifCareem” <samra.hanif@careem.com>
2023-01-12 13:33:20 +0500

    fix: parser added to all updated query

--- tests/integration_tests/celery_tests.py
+++ tests/integration_tests/celery_tests.py
@@ -284,7 +284,7 @@ def test_run_async_query_cta_config(test_client, ctas_method):
     updated_query = query.executed_sql[query.executed_sql.index("*/") + 3 :]
     assert (
         f"CREATE {ctas_method} {CTAS_SCHEMA_NAME}.{tmp_table_name} AS \n{QUERY}"
-        == updated_query
+        == updated_query.strip(" \t\n\r;").format(updated_query, strip_comments=True)
     )
 
     delete_tmp_view_or_table(f"{CTAS_SCHEMA_NAME}.{tmp_table_name}", ctas_method)
@@ -312,7 +312,9 @@ def test_run_async_cta_query(test_client, ctas_method):
     assert QueryStatus.SUCCESS == query.status
     assert get_select_star(table_name, query.limit) in query.select_sql
     updated_query = query.executed_sql[query.executed_sql.index("*/") + 3 :]
-    assert f"CREATE {ctas_method} {table_name} AS \n{QUERY}" == updated_query
+    assert f"CREATE {ctas_method} {table_name} AS \n{QUERY}" == updated_query.strip(
+        " \t\n\r;"
+    ).format(updated_query, strip_comments=True)
 
     assert QUERY == query.sql
     assert query.rows == (1 if backend() == "presto" else 0)
@@ -348,7 +350,9 @@ def test_run_async_cta_query_with_lower_limit(test_client, ctas_method):
         else get_select_star(tmp_table, query.limit)
     )
     updated_query = query.executed_sql[query.executed_sql.index("*/") + 3 :]
-    assert f"CREATE {ctas_method} {tmp_table} AS \n{QUERY}" == updated_query
+    assert f"CREATE {ctas_method} {tmp_table} AS \n{QUERY}" == updated_query.strip(
+        " \t\n\r;"
+    ).format(updated_query, strip_comments=True)
 
     assert QUERY == query.sql
 
