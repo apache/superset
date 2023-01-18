2c3630c5be56fb4030b0bc661544e3a25b4174ee
Antonio Rivero Martinez <38889534+Antonio-RiveroMartnez@users.noreply.github.com>
2023-01-18 00:11:41 -0300

    feat(ssh_tunnel): Rename allow_ssh_tunneling and change the default value to False (#22723)

--- superset-frontend/src/views/CRUD/data/database/types.ts
+++ superset-frontend/src/views/CRUD/data/database/types.ts
@@ -98,7 +98,7 @@ export type DatabaseObject = {
   // DB Engine Spec information
   engine_information?: {
     supports_file_upload?: boolean;
-    allow_ssh_tunneling?: boolean;
+    disable_ssh_tunneling?: boolean;
   };
 
   // SSH Tunnel information
