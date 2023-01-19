118566064a9c5c325b402fdc8fdaee55072f3398
Jack Fragassi <jfragassi98@gmail.com>
2023-01-19 15:01:30 -0800

    Try debugging again

--- tests/integration_tests/log_api_tests.py
+++ tests/integration_tests/log_api_tests.py
@@ -282,5 +282,6 @@ class TestLogApi(SupersetTestCase):
         db.session.commit()
         self.assertEqual(rv.status_code, 200)
         response = json.loads(rv.data.decode("utf-8"))
-        self.assertEqual(response["result"], {})  # debugging
+        print(response["result"])
+        self.assertEqual(response["result"], {"a": 1})  # debugging
         self.assertEqual(len(response["result"]), 2)
