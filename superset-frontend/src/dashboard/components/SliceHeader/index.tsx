67dbe80c08dd4647f5664f4ae5a0680b32388755
JUST.in DO IT <justin.park@airbnb.com>
2023-01-20 07:59:50 -0800

    chore(dashboard): Log events for download as image (#22793)

--- superset-frontend/src/dashboard/components/SliceHeader/index.tsx
+++ superset-frontend/src/dashboard/components/SliceHeader/index.tsx
@@ -133,6 +133,7 @@ const SliceHeader: FC<SliceHeaderProps> = ({
   updateSliceName = () => ({}),
   toggleExpandSlice = () => ({}),
   logExploreChart = () => ({}),
+  logEvent,
   exportCSV = () => ({}),
   editMode = false,
   annotationQuery = {},
@@ -271,6 +272,7 @@ const SliceHeader: FC<SliceHeaderProps> = ({
                 toggleExpandSlice={toggleExpandSlice}
                 forceRefresh={forceRefresh}
                 logExploreChart={logExploreChart}
+                logEvent={logEvent}
                 exportCSV={exportCSV}
                 exportFullCSV={exportFullCSV}
                 supersetCanExplore={supersetCanExplore}
