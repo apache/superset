5151a8319dd777aaaee802fce56080572b9af8bc
Daniel Draper <daniel.draper@understand.ai>
2023-01-23 09:56:20 +0100

    fix black

--- setup.py
+++ setup.py
@@ -168,7 +168,9 @@ setup(
         "prophet": ["prophet>=1.0.1, <1.1", "pystan<3.0"],
         "redshift": ["sqlalchemy-redshift>=0.8.1, < 0.9"],
         "rockset": ["rockset>=0.8.10, <0.9"],
-        "shillelagh": ["shillelagh[datasetteapi,gsheetsapi,socrata,weatherapi]>=1.1.1, <2"],
+        "shillelagh": [
+            "shillelagh[datasetteapi,gsheetsapi,socrata,weatherapi]>=1.1.1, <2"
+        ],
         "snowflake": ["snowflake-sqlalchemy>=1.2.4, <2"],
         "spark": ["pyhive[hive]>=0.6.5", "tableschema", "thrift>=0.14.1, <1.0.0"],
         "teradata": ["teradatasql>=16.20.0.23"],
