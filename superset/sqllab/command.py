5a65aedbaf2eb5a6496e4ae0fa99384b1b70a990
geido <diegopucci.me@gmail.com>
2023-01-25 18:23:51 +0100

    Shallow copy query validation

--- superset/sqllab/command.py
+++ superset/sqllab/command.py
@@ -16,6 +16,7 @@
 # under the License.
 # pylint: disable=too-few-public-methods, too-many-arguments
 from __future__ import annotations
+import copy
 
 import logging
 from typing import Any, Dict, Optional, TYPE_CHECKING
@@ -145,8 +146,9 @@ class ExecuteSqlCommand(BaseCommand):
 
             self._execution_context.set_query(query)
             rendered_query = self._sql_query_render.render(self._execution_context)
-            query.sql = rendered_query
-            self._validate_access(query)
+            validate_rendered_query = copy.copy(query)
+            validate_rendered_query.sql = rendered_query
+            self._validate_access(validate_rendered_query)
             self._set_query_limit_if_required(rendered_query)
             self._query_dao.update(
                 query, {"limit": self._execution_context.query.limit}
