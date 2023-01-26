ee0f2f0d66cfa71e05b0db96fa8f9264b84f30d7
lyndsiWilliams <kcatgirl@gmail.com>
2023-01-26 16:47:17 -0600

    Better solution for top nav create dataset disabled bug

--- superset-frontend/src/views/components/RightMenu.tsx
+++ superset-frontend/src/views/components/RightMenu.tsx
@@ -41,6 +41,7 @@ import { isUserAdmin } from 'src/dashboard/util/permissionUtils';
 import {
   MenuObjectProps,
   UserWithPermissionsAndRoles,
+  MenuObjectChildProps,
 } from 'src/types/bootstrapTypes';
 import { RootState } from 'src/dashboard/types';
 import LanguagePicker from './LanguagePicker';
@@ -190,18 +191,21 @@ const RightMenu = ({
           name: 'Upload a CSV',
           url: '/csvtodatabaseview/form',
           perm: canUploadCSV && showUploads,
+          disable: isAdmin && !allowUploads,
         },
         {
           label: t('Upload columnar file to database'),
           name: 'Upload a Columnar file',
           url: '/columnartodatabaseview/form',
           perm: canUploadColumnar && showUploads,
+          disable: isAdmin && !allowUploads,
         },
         {
           label: t('Upload Excel file to database'),
           name: 'Upload Excel',
           url: '/exceltodatabaseview/form',
           perm: canUploadExcel && showUploads,
+          disable: isAdmin && !allowUploads,
         },
       ],
     },
@@ -293,16 +297,12 @@ const RightMenu = ({
     setShowDatabaseModal(false);
   };
 
-  const isDisabled = isAdmin && !allowUploads;
-
   const tooltipText = t(
     "Enable 'Allow file uploads to database' in any database's settings",
   );
 
-  const buildMenuItem = (item: Record<string, any>) => {
-    const disabledText =
-      isDisabled && item.url && !item.url.includes('dataset');
-    return disabledText ? (
+  const buildMenuItem = (item: MenuObjectChildProps) =>
+    item.disable ? (
       <Menu.Item key={item.name} css={styledDisabled}>
         <Tooltip placement="top" title={tooltipText}>
           {item.label}
@@ -313,7 +313,6 @@ const RightMenu = ({
         {item.url ? <a href={item.url}> {item.label} </a> : item.label}
       </Menu.Item>
     );
-  };
 
   const onMenuOpen = (openKeys: string[]) => {
     // We should query the API only if opening Data submenus
