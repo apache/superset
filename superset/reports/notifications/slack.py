53af1b3649088bf89208abca373c4177df42635f
“SamraHanifCareem” <samra.hanif@careem.com>
2023-01-31 18:24:57 +0500

    feat: description title set to bold

--- superset/reports/notifications/slack.py
+++ superset/reports/notifications/slack.py
@@ -60,17 +60,19 @@ class SlackNotification(BaseNotification):  # pylint: disable=too-few-public-met
             else ""
         )
         template = __(
-            """>*<%(link)s | %(name)s>* \n>Description:* _%(description)s_ \n>*Message Content:* %(msg_content)s \n>%(table)s
+            """>*<%(link)s | %(name)s>* \n>*Description:* _%(description)s_ \n>*Message Content:* %(msg_content)s \n>%(table)s
 """,
             name=self._content.name,
             description=self._content.description or "",
-            msg_content=self._content.msg_content or "",
+            msg_content=" ".join(self._content.msg_content.split())
+            if self._content.msg_content
+            else "",
             table=table,
             link=link,
         )
         if all([url is not None, url != ""]):
             template = __(
-                """>*<%(link)s | %(name)s>* \n>Description:* _%(description)s_ \n><%(url)s|Explore in Careem Insights> \n>%(table)s
+                """>*<%(link)s | %(name)s>* \n>*Description:* _%(description)s_ \n><%(url)s|Explore in Careem Insights> \n>%(table)s
 """,
                 name=self._content.name,
                 description=self._content.description or "",
