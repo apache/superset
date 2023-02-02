580481287a149a016e7487a66b15a07f9b43b810
Tushar Virk <tusharvirk@Tushars-MacBook-Pro.local>
2023-02-02 14:17:03 +1100

    added LIQ logo and adjusted CSS in navbar for it to look better

--- superset/config.py
+++ superset/config.py
@@ -276,7 +276,8 @@ SCHEDULED_QUERIES: Dict[str, Any] = {}
 APP_NAME = "Superset"
 
 # Specify the App icon
-APP_ICON = "/static/assets/images/superset-logo-horiz.png"
+# APP_ICON = "/static/assets/images/superset-logo-horiz.png"
+APP_ICON = "/static/liq_logo.png"
 
 # Specify where clicking the logo would take the user
 # e.g. setting it to '/' would take the user to '/superset/welcome/'
