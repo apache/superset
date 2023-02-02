cb9ab15f420d9f82730cc26deaeedbfa68ed078a
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2023-02-02 14:06:30 +0100

    fix(explore): Time comparison in Mixed Chart in GENERIC_CHART_AXES not working (#22945)

--- superset-frontend/plugins/plugin-chart-echarts/src/MixedTimeseries/buildQuery.ts
+++ superset-frontend/plugins/plugin-chart-echarts/src/MixedTimeseries/buildQuery.ts
@@ -67,11 +67,8 @@ export default function buildQuery(formData: QueryFormData) {
         fd,
         queryObject,
       )
-        ? timeComparePivotOperator(fd, queryObject)
-        : pivotOperator(fd, {
-            ...queryObject,
-            columns: fd.groupby,
-          });
+        ? timeComparePivotOperator(fd, { ...queryObject, columns: fd.groupby })
+        : pivotOperator(fd, { ...queryObject, columns: fd.groupby });
 
       const tmpQueryObject = {
         ...queryObject,
