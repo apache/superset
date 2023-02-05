41b2f20f7d065d7f3bea2b7a1c965c135d4dcb14
“SamraHanifCareem” <samra.hanif@careem.com>
2023-02-05 17:24:09 +0500

    fix: space added after eplore url

--- superset/reports/notifications/email.py
+++ superset/reports/notifications/email.py
@@ -147,7 +147,7 @@ class EmailNotification(BaseNotification):  # pylint: disable=too-few-public-met
         msgContent = ""
         url_renderer = ""
         if all([url is not None, url != ""]):
-            url_renderer = f"""<b><a href="{url}">{call_to_action}</a></b><p></p>"""
+            url_renderer = f"""<b><a href="{url}">{call_to_action}</a></b>"""
         if self._content.msg_content:
             msgContent = f"""<p><strong> Message Content: </strong>{" ".join(self._content.msg_content.split())}</p>"""
         body = textwrap.dedent(
@@ -168,6 +168,7 @@ class EmailNotification(BaseNotification):  # pylint: disable=too-few-public-met
               </head>
               <body>
                 <div><strong>Description: </strong>{description}</div>
+                <br/>
                 {url_renderer}
                 {msgContent}
                 <a href="{self._content.link}">Open Alert/Report in Careem Insights</a>
