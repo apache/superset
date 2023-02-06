5a47500691e1decee46c647be0c5e666ced2f061
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2023-02-06 08:17:08 -0800

    fix: add new config to allow for specific import data urls (#22942)

--- superset/datasets/commands/exceptions.py
+++ superset/datasets/commands/exceptions.py
@@ -191,3 +191,7 @@ class DatasetAccessDeniedError(ForbiddenError):
 
 class DatasetDuplicateFailedError(CreateFailedError):
     message = _("Dataset could not be duplicated.")
+
+
+class DatasetForbiddenDataURI(ForbiddenError):
+    message = _("Data URI is not allowed.")
