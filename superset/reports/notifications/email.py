383118b7c5436d54c643205f62474418720ec595
“SamraHanifCareem” <samra.hanif@careem.com>
2023-02-06 12:33:18 +0500

    fix: space added using br tag and removed the block elements

--- superset/reports/notifications/email.py
+++ superset/reports/notifications/email.py
@@ -147,7 +147,7 @@ class EmailNotification(BaseNotification):  # pylint: disable=too-few-public-met
         msgContent = ""
         url_renderer = ""
         if all([url is not None, url != ""]):
-            url_renderer = f"""<p><b><a href="{url}">{call_to_action}</a></b></p>"""
+            url_renderer = f"""<b><a href="{url}">{call_to_action}</a></b>"""
         if self._content.msg_content:
             msgContent = f"""<p><strong> Message Content: </strong>{" ".join(self._content.msg_content.split())}</p>"""
         body = textwrap.dedent(
@@ -170,8 +170,8 @@ class EmailNotification(BaseNotification):  # pylint: disable=too-few-public-met
                 <div><strong>Description: </strong>{description}</div>
                 {url_renderer}
                 {msgContent}
-                <br/>
-                <p><a href="{self._content.link}">Open Alert/Report in Careem Insights</a></p>
+                <br>
+                <a href="{self._content.link}">Open Alert/Report in Careem Insights</a>
                 {html_table}
                 {img_tag}
               </body>
