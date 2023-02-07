623f02bd8432d82d8eb0b53e28718d684e0aa716
Michael S. Molina <michael.s.molina@gmail.com>
2023-02-07 11:04:30 -0500

    fix: Time Column on Generic X-axis

--- superset/common/query_context_factory.py
+++ superset/common/query_context_factory.py
@@ -22,9 +22,11 @@ from superset import app, db
 from superset.charts.dao import ChartDAO
 from superset.common.chart_data import ChartDataResultFormat, ChartDataResultType
 from superset.common.query_context import QueryContext
+from superset.common.query_object import QueryObject
 from superset.common.query_object_factory import QueryObjectFactory
 from superset.datasource.dao import DatasourceDAO
 from superset.models.slice import Slice
+from superset.superset_typing import AdhocColumn
 from superset.utils.core import DatasourceDict, DatasourceType
 
 if TYPE_CHECKING:
@@ -65,8 +67,12 @@ class QueryContextFactory:  # pylint: disable=too-few-public-methods
         result_type = result_type or ChartDataResultType.FULL
         result_format = result_format or ChartDataResultFormat.JSON
         queries_ = [
-            self._query_object_factory.create(
-                result_type, datasource=datasource, **query_obj
+            self._process_query_object(
+                datasource_model_instance,
+                form_data,
+                self._query_object_factory.create(
+                    result_type, datasource=datasource, **query_obj
+                ),
             )
             for query_obj in queries
         ]
@@ -90,7 +96,6 @@ class QueryContextFactory:  # pylint: disable=too-few-public-methods
 
     # pylint: disable=no-self-use
     def _convert_to_model(self, datasource: DatasourceDict) -> BaseDatasource:
-
         return DatasourceDAO.get_datasource(
             session=db.session,
             datasource_type=DatasourceType(datasource["type"]),
@@ -99,3 +104,54 @@ class QueryContextFactory:  # pylint: disable=too-few-public-methods
 
     def _get_slice(self, slice_id: Any) -> Optional[Slice]:
         return ChartDAO.find_by_id(slice_id)
+
+    def _process_query_object(
+        self,
+        datasource: BaseDatasource,
+        form_data: Dict[str, Any],
+        query_object: QueryObject,
+    ) -> QueryObject:
+        self._apply_granularity(query_object, form_data, datasource)
+        self._apply_filters(query_object)
+        return query_object
+
+    def _apply_granularity(
+        self,
+        query_object: QueryObject,
+        form_data: Dict[str, Any],
+        datasource: BaseDatasource,
+    ):
+        temporal_columns = {
+            column.column_name for column in datasource.columns if column.is_temporal
+        }
+        granularity = query_object.granularity
+        x_axis = form_data.get("x_axis")
+        if granularity and x_axis in temporal_columns:
+            x_axis_column = next(
+                (
+                    column
+                    for column in query_object.columns
+                    if column["sqlExpression"] == x_axis
+                ),
+                None,
+            )
+            if x_axis_column:
+                x_axis_column["sqlExpression"] = granularity
+                x_axis_column["label"] = granularity
+                query_object.granularity = None
+                for post_processing in query_object.post_processing:
+                    if post_processing.get("operation") == "pivot":
+                        post_processing["options"]["index"] = [granularity]
+
+            x_axis_filter = x_axis and next(
+                (filter for filter in query_object.filter if filter["col"] == x_axis),
+                None,
+            )
+            if x_axis_filter:
+                x_axis_filter["col"] = granularity
+
+    def _apply_filters(self, query_object: QueryObject):
+        if query_object.time_range:
+            for filter in query_object.filter:
+                if filter["op"] == "TEMPORAL_RANGE":
+                    filter["val"] = query_object.time_range
