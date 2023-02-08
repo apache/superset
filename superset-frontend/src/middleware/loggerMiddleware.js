239fb2a466bf5a5ba81dda965e48381926511ede
justin-park <justin.park@airbnb.com>
2023-02-08 15:36:28 -0800

    feat(sqllab): Add event logger

--- superset-frontend/src/middleware/loggerMiddleware.js
+++ superset-frontend/src/middleware/loggerMiddleware.js
@@ -72,7 +72,7 @@ const loggerMiddleware = store => next => action => {
     return next(action);
   }
 
-  const { dashboardInfo, explore, impressionId, dashboardLayout } =
+  const { dashboardInfo, explore, impressionId, dashboardLayout, sqlLab } =
     store.getState();
   let logMetadata = {
     impression_id: impressionId,
@@ -89,6 +89,13 @@ const loggerMiddleware = store => next => action => {
       source: 'explore',
       source_id: explore.slice ? explore.slice.slice_id : 0,
       ...logMetadata,
+    };
+  } else if (sqlLab) {
+    logMetadata = {
+      source: 'sqlLab',
+      source_id: sqlLab.queryEditors.find(
+        ({ id }) => id === sqlLab.tabHistory.slice(-1)[0],
+      )?.dbId,
     };
   }
 
