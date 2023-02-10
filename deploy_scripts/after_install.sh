0d853c1dc2924680b7be0ac1b6778add15bc58e8
TusharVirk <Tushar.Virk@locationiq.com.au>
2023-02-10 11:55:18 +1100

    updated codedeploy after install script

--- deploy_scripts/after_install.sh
+++ deploy_scripts/after_install.sh
@@ -1,7 +1,9 @@
 # Remove default config and replace with existing config'
 echo "Overriding settings"
-sudo rm /home/ec2-user/liq_superset/docker/pythonpath_dev/superset_config.py
+sudo rm -f /home/ec2-user/liq_superset/docker/pythonpath_dev/superset_config.py
+sudo rm -f /home/ec2-user/liq_superset/docker/.env
+sudo rm -f /home/ec2-user/liq_superset/superset-frontend/liq_viz_plugins/liq_secrets.js
 sudo cp /home/ec2-user/superset_config.py /home/ec2-user/liq_superset/docker/pythonpath_dev/
-sudo /bin/cp -rf /home/ec2-user/.env /home/ec2-user/liq_superset/docker/
-sudo /bin/cp -rf /home/ec2-user/liq_secrets.js /home/ec2-user/liq_superset/superset-frontend/liq_viz_plugins/
+sudo cp /home/ec2-user/.env /home/ec2-user/liq_superset/docker/
+sudo cp /home/ec2-user/liq_secrets.js /home/ec2-user/liq_superset/superset-frontend/liq_viz_plugins/
 sleep 5
