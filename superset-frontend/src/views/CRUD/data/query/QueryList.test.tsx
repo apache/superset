bfeb5e2d78ec54b4183d647b2aec50d11069b4fc
Michael S. Molina <70410625+michael-s-molina@users.noreply.github.com>
2023-02-13 12:31:09 -0500

    chore: Removes icon warnings from tests (#23054)

--- superset-frontend/src/views/CRUD/data/query/QueryList.test.tsx
+++ superset-frontend/src/views/CRUD/data/query/QueryList.test.tsx
@@ -35,14 +35,6 @@ import SyntaxHighlighter from 'react-syntax-highlighter/dist/cjs/light';
 import SubMenu from 'src/views/components/SubMenu';
 import { QueryState } from '@superset-ui/core';
 
-jest.mock('src/components/Icons/Icon', () => ({
-  __esModule: true,
-  default: ({ fileName, role }: { fileName: string; role: string }) => (
-    <span role={role ?? 'img'} aria-label={fileName.replace('_', '-')} />
-  ),
-  StyledIcon: () => <span />,
-}));
-
 // store needed for withToasts
 const mockStore = configureStore([thunk]);
 const store = mockStore({});
