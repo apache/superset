baaf38ec8b622ee79d7787e3560c9e74a3f3fc6a
aurelie.ehanno <aurelie.ehanno@kosmos.fr>
2023-02-23 10:20:42 -0500

    ajout trad

--- superset-frontend/src/explore/components/controls/DateFilterControl/components/DateLabel.tsx
+++ superset-frontend/src/explore/components/controls/DateFilterControl/components/DateLabel.tsx
@@ -18,7 +18,7 @@
  */
 
 import React, { forwardRef, ReactNode, RefObject } from 'react';
-import { css, t, styled, useTheme } from '@superset-ui/core';
+import { css, styled, useTheme } from '@superset-ui/core';
 import Icons from 'src/components/Icons';
 
 export type DateLabelProps = {
@@ -88,7 +88,7 @@ export const DateLabel = forwardRef(
     return (
       <LabelContainer {...props} tabIndex={0}>
         <span className="date-label-content" ref={ref}>
-          {t(String(props.label))}
+          {props.label}
         </span>
         <Icons.CalendarOutlined
           iconSize="s"
