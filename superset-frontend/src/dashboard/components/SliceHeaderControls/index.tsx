98bb1e9a1d04d9ba18b152f32fe26ad6e8cdea6b
geido <diegopucci.me@gmail.com>
2023-02-27 10:07:29 +0100

    Remove scoping modal

--- superset-frontend/src/dashboard/components/SliceHeaderControls/index.tsx
+++ superset-frontend/src/dashboard/components/SliceHeaderControls/index.tsx
@@ -44,7 +44,6 @@ import { NoAnimationDropdown } from 'src/components/Dropdown';
 import ShareMenuItems from 'src/dashboard/components/menu/ShareMenuItems';
 import downloadAsImage from 'src/utils/downloadAsImage';
 import { FeatureFlag, isFeatureEnabled } from 'src/featureFlags';
-import CrossFilterScopingModal from 'src/dashboard/components/CrossFilterScopingModal/CrossFilterScopingModal';
 import { getSliceHeaderTooltip } from 'src/dashboard/util/getSliceHeaderTooltip';
 import { Tooltip } from 'src/components/Tooltip';
 import Icons from 'src/components/Icons';
@@ -57,7 +56,6 @@ import { DrillDetailMenuItems } from 'src/components/Chart/DrillDetail';
 import { LOG_ACTIONS_CHART_DOWNLOAD_AS_IMAGE } from 'src/logger/LogUtils';
 
 const MENU_KEYS = {
-  CROSS_FILTER_SCOPING: 'cross_filter_scoping',
   DOWNLOAD_AS_IMAGE: 'download_as_image',
   EXPLORE_CHART: 'explore_chart',
   EXPORT_CSV: 'export_csv',
@@ -157,7 +155,6 @@ type SliceHeaderControlsPropsWithRouter = SliceHeaderControlsProps &
   RouteComponentProps;
 interface State {
   showControls: boolean;
-  showCrossFilterScopingModal: boolean;
 }
 
 const dropdownIconsStyles = css`
@@ -256,7 +253,6 @@ class SliceHeaderControls extends React.PureComponent<
 
     this.state = {
       showControls: false,
-      showCrossFilterScopingModal: false,
     };
   }
 
@@ -286,9 +282,6 @@ class SliceHeaderControls extends React.PureComponent<
       case MENU_KEYS.FORCE_REFRESH:
         this.refreshChart();
         this.props.addSuccessToast(t('Data refreshed'));
-        break;
-      case MENU_KEYS.CROSS_FILTER_SCOPING:
-        this.setState({ showCrossFilterScopingModal: true });
         break;
       case MENU_KEYS.TOGGLE_CHART_DESCRIPTION:
         // eslint-disable-next-line no-unused-expressions
@@ -477,17 +470,6 @@ class SliceHeaderControls extends React.PureComponent<
           <Menu.Divider />
         )}
 
-        {isFeatureEnabled(FeatureFlag.DASHBOARD_CROSS_FILTERS) &&
-          isCrossFilter &&
-          crossFiltersEnabled && (
-            <>
-              <Menu.Item key={MENU_KEYS.CROSS_FILTER_SCOPING}>
-                {t('Cross-filter scoping')}
-              </Menu.Item>
-              <Menu.Divider />
-            </>
-          )}
-
         {supersetCanShare && (
           <Menu.SubMenu title={t('Share')}>
             <ShareMenuItems
@@ -538,11 +520,6 @@ class SliceHeaderControls extends React.PureComponent<
 
     return (
       <>
-        <CrossFilterScopingModal
-          chartId={slice.slice_id}
-          isOpen={this.state.showCrossFilterScopingModal}
-          onClose={() => this.setState({ showCrossFilterScopingModal: false })}
-        />
         {isFullSize && (
           <Icons.FullscreenExitOutlined
             style={{ fontSize: 22 }}
