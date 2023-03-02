c3a300f2d8a827447c0e7f0d9ef9b63bd95c0544
M. A. Rafsan Mazumder <rafsan.mazumder@bytedance.com>
2023-03-02 08:34:48 +0800

    Fix testcase for ByteHouse connector

--- superset/db_engine_specs/bytehouse.py
+++ superset/db_engine_specs/bytehouse.py
@@ -14,6 +14,11 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+from datetime import datetime
+from typing import Any, Dict, Optional
+
+from sqlalchemy import types
+
 from superset.db_engine_specs.base import BaseEngineSpec
 
 
@@ -22,3 +27,37 @@ class ByteHouseEngineSpec(BaseEngineSpec):  # pylint: disable=abstract-method
 
     engine = "bytehouse"
     engine_name = "ByteHouse"
+
+    time_secondary_columns = True
+    time_groupby_inline = True
+
+    _time_grain_expressions = {
+        None: "{col}",
+        "PT1M": "toStartOfMinute(toDateTime({col}))",
+        "PT5M": "toDateTime(intDiv(toUInt32(toDateTime({col})), 300)*300)",
+        "PT10M": "toDateTime(intDiv(toUInt32(toDateTime({col})), 600)*600)",
+        "PT15M": "toDateTime(intDiv(toUInt32(toDateTime({col})), 900)*900)",
+        "PT30M": "toDateTime(intDiv(toUInt32(toDateTime({col})), 1800)*1800)",
+        "PT1H": "toStartOfHour(toDateTime({col}))",
+        "P1D": "toStartOfDay(toDateTime({col}))",
+        "P1W": "toMonday(toDateTime({col}))",
+        "P1M": "toStartOfMonth(toDateTime({col}))",
+        "P3M": "toStartOfQuarter(toDateTime({col}))",
+        "P1Y": "toStartOfYear(toDateTime({col}))",
+    }
+
+    _show_functions_column = "name"
+
+    supports_file_upload = False
+
+    @classmethod
+    def convert_dttm(
+        cls, target_type: str, dttm: datetime, db_extra: Optional[Dict[str, Any]] = None
+    ) -> Optional[str]:
+        sqla_type = cls.get_sqla_column_type(target_type)
+
+        if isinstance(sqla_type, types.Date):
+            return f"toDate('{dttm.date().isoformat()}')"
+        if isinstance(sqla_type, types.DateTime):
+            return f"""toDateTime('{dttm.isoformat(sep=" ", timespec="seconds")}')"""
+        return None
