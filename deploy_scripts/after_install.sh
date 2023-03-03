6e76f151f0ee96723f1d0647ff660e6ea9f67242
TusharVirk <Tushar.Virk@locationiq.com.au>
2023-03-03 18:04:57 +1100

    version 1.0.0 for production

--- deploy_scripts/after_install.sh
+++ deploy_scripts/after_install.sh
@@ -3,7 +3,9 @@ echo "Overriding settings"
 sudo rm -f /home/ec2-user/liq_superset/docker/pythonpath_dev/superset_config.py
 sudo rm -f /home/ec2-user/liq_superset/docker/.env
 sudo rm -f /home/ec2-user/liq_superset/superset-frontend/liq_viz_plugins/liq_secrets.js
+sudo rm -rf /home/ec2-user/liq_superset/superset-frontend/liq_viz_plugins/liq_data
 sudo cp /home/ec2-user/superset_config.py /home/ec2-user/liq_superset/docker/pythonpath_dev/
 sudo cp /home/ec2-user/.env /home/ec2-user/liq_superset/docker/
 sudo cp /home/ec2-user/liq_secrets.js /home/ec2-user/liq_superset/superset-frontend/liq_viz_plugins/
+sudo cp -R /home/ec2-user/liq_data /home/ec2-user/liq_superset/superset-frontend/liq_viz_plugins/
 sleep 5
