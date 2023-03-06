d16512b7758e36a1263fc63bd7d9d1f93060dc93
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2023-03-06 13:42:52 +0100

    fix(dashboard): Allow selecting text in cells in Table and PivotTable without triggering cross filters (#23283)

--- superset-frontend/packages/superset-ui-core/src/utils/getSelectedText.ts
+++ superset-frontend/packages/superset-ui-core/src/utils/getSelectedText.ts
@@ -16,18 +16,4 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-export { default as convertKeysToCamelCase } from './convertKeysToCamelCase';
-export { default as ensureIsArray } from './ensureIsArray';
-export { default as ensureIsInt } from './ensureIsInt';
-export { default as isDefined } from './isDefined';
-export { default as isRequired } from './isRequired';
-export { default as isEqualArray } from './isEqualArray';
-export { default as makeSingleton } from './makeSingleton';
-export { default as promiseTimeout } from './promiseTimeout';
-export { default as logging } from './logging';
-export { default as removeDuplicates } from './removeDuplicates';
-export { lruCache } from './lruCache';
-export { getSelectedText } from './getSelectedText';
-export * from './featureFlags';
-export * from './random';
-export * from './typedMemo';
+export const getSelectedText = () => window.getSelection()?.toString();
