98d236ca228d86024fab21d84beeb6dbb432cc35
zef <zef@preset.io>
2023-03-13 12:29:06 -0700

    fixed lint

--- superset/dashboards/filters.py
+++ superset/dashboards/filters.py
@@ -14,8 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-import uuid
-from typing import Any, Optional, Union
+from typing import Any, Optional
 
 from flask import g
 from flask_appbuilder.security.sqla.models import Role
