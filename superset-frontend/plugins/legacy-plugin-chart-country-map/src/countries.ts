3158c0c58444a631927c107b54f980d0fe619065
Rukshan Ranatunge <arkruka@gmail.com>
2023-03-13 12:17:57 +0530

    feat: Added  SriLanka country map

--- superset-frontend/plugins/legacy-plugin-chart-country-map/src/countries.ts
+++ superset-frontend/plugins/legacy-plugin-chart-country-map/src/countries.ts
@@ -87,6 +87,7 @@ import singapore from './countries/singapore.geojson';
 import slovenia from './countries/slovenia.geojson';
 import sweden from './countries/sweden.geojson';
 import spain from './countries/spain.geojson';
+import srilanka from './countries/srilanka.geojson';
 import switzerland from './countries/switzerland.geojson';
 import syria from './countries/syria.geojson';
 import tanzania from './countries/tanzania.geojson';
@@ -174,6 +175,7 @@ export const countries = {
   slovenia,
   spain,
   sweden,
+  srilanka,
   switzerland,
   syria,
   tanzania,
