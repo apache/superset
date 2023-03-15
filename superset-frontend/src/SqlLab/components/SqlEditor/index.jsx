e319da189bbbe4a7368ccecd5ddff317560a321a
Beto Dealmeida <roberto@dealmeida.net>
2023-03-15 15:01:44 -0700

    fix: schedule query option showing up

--- superset-frontend/src/SqlLab/components/SqlEditor/index.jsx
+++ superset-frontend/src/SqlLab/components/SqlEditor/index.jsx
@@ -545,7 +545,7 @@ const SqlEditor = ({
             />
           </Menu.Item>
         )}
-        {scheduledQueriesConf && (
+        {Object.keys(scheduledQueriesConf).length > 0 && (
           <Menu.Item>
             <ScheduleQueryButton
               defaultLabel={qe.name}
