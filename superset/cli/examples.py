3a8d104048ec700382ecce152ed844aebe80d9a9
Beto Dealmeida <roberto@dealmeida.net>
2023-03-14 20:08:30 -0700

    Fix load examples

--- superset/cli/examples.py
+++ superset/cli/examples.py
@@ -20,7 +20,6 @@ import click
 from flask.cli import with_appcontext
 
 import superset.utils.database as database_utils
-from superset import security_manager
 
 logger = logging.getLogger(__name__)
 
@@ -114,8 +113,4 @@ def load_examples(
     force: bool = False,
 ) -> None:
     """Loads a set of Slices and Dashboards and a supporting dataset"""
-    # pylint: disable=import-outside-toplevel
-    from superset.utils.core import override_user
-
-    with override_user(security_manager.find_user(username="admin")):
-        load_examples_run(load_test_data, load_big_data, only_metadata, force)
+    load_examples_run(load_test_data, load_big_data, only_metadata, force)
