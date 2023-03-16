81edc07cb389087f3d7e1bb933144281a6a354da
hughhhh <hughmil3s@gmail.com>
2023-03-16 14:03:25 -0600

    yess

--- scripts/benchmark_migration.py
+++ scripts/benchmark_migration.py
@@ -120,7 +120,7 @@ def find_models(module: ModuleType) -> List[Type[Model]]:
     # sort topologically so we can create entities in order and
     # maintain relationships (eg, create a database before creating
     # a slice)
-    sorter = TopologicalSorter()  # type: ignore
+    sorter = TopologicalSorter()
     for model in models:
         inspector = inspect(model)
         dependent_tables: List[str] = []
