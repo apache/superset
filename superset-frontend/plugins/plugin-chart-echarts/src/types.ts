9bc5fab239d028fc954001eaaf2148ea671f97b8
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2023-03-20 12:56:15 +0100

    feat(echarts): Implement stream graph for Echarts Timeseries (#23410)

--- superset-frontend/plugins/plugin-chart-echarts/src/types.ts
+++ superset-frontend/plugins/plugin-chart-echarts/src/types.ts
@@ -29,7 +29,7 @@ import {
 } from '@superset-ui/core';
 import { EChartsCoreOption, ECharts } from 'echarts';
 import { TooltipMarker } from 'echarts/types/src/util/format';
-import { AreaChartExtraControlsValue } from './constants';
+import { StackControlsValue } from './constants';
 
 export type EchartsStylesProps = {
   height: number;
@@ -159,7 +159,7 @@ export interface TitleFormData {
   yAxisTitlePosition: string;
 }
 
-export type StackType = boolean | null | Partial<AreaChartExtraControlsValue>;
+export type StackType = boolean | null | Partial<StackControlsValue>;
 
 export interface TreePathInfo {
   name: string;
