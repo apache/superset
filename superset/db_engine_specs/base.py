0761ee02bb3c5d3db98fd9e93d057e046cfa8966
Rémy Dubois <rdubois.contact@gmail.com>
2023-03-21 11:54:00 +0100

    fix(#23440): fix the inexisting time grain issue

--- superset/db_engine_specs/base.py
+++ superset/db_engine_specs/base.py
@@ -696,7 +696,8 @@ class BaseEngineSpec:  # pylint: disable=too-many-public-methods
         time_grain_expressions.update(grain_addon_expressions.get(cls.engine, {}))
         denylist: List[str] = current_app.config["TIME_GRAIN_DENYLIST"]
         for key in denylist:
-            time_grain_expressions.pop(key)
+            if key in time_grain_expressions.keys():
+                time_grain_expressions.pop(key)
 
         return dict(
             sorted(
