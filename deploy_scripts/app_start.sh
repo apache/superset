e8f07e93df96bc04b0e4d3bb5ff28f667150d4de
TusharVirk <Tushar.Virk@locationiq.com.au>
2023-03-23 17:32:23 +1100

    changed deploy scripts

--- deploy_scripts/app_start.sh
+++ deploy_scripts/app_start.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
-sudo chmod -R 777 /home/ec2-user/liq_superset
 
 cd /home/ec2-user/liq_superset
 
-docker-compose stop
-docker-compose down
 docker-compose pull superset
 
 # Start containers in detached mode
 echo "Starting containers..."
-docker-compose up -d
-sleep 60
-docker-compose down
-sleep 5
 docker-compose up -d
 echo "Done"
