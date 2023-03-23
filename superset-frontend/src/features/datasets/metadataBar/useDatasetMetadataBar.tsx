d65be2be2f73013f54d16abb7ee5dafdd7a72ffe
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2023-03-23 14:54:24 +0100

    Add ut

--- superset-frontend/src/features/datasets/metadataBar/useDatasetMetadataBar.tsx
+++ superset-frontend/src/features/datasets/metadataBar/useDatasetMetadataBar.tsx
@@ -55,7 +55,7 @@ export const useDatasetMetadataBar = (datasetId: number | string) => {
         ? `${changed_by.first_name} ${changed_by.last_name}`
         : notAvailable;
       const formattedOwners =
-        owners.length > 0
+        owners?.length > 0
           ? owners.map(owner => `${owner.first_name} ${owner.last_name}`)
           : [notAvailable];
       items.push({
