79b6eb69f8a8c5f16dff0f3aea2a14125270fa42
TusharVirk <Tushar.Virk@locationiq.com.au>
2023-03-23 16:34:11 +1100

    fixed typo which should hopefully fix the deploy problem in #19

--- deploy_scripts/app_stop.sh
+++ deploy_scripts/app_stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Stop running containers and remove volumes except superset_db
 DIR="/home/ec2-user/liq_superset"
-if [-d "$DIR" ]; then
+if [ -d "$DIR" ]; then
     echo "Stopping app..."
     cd /home/ec2-user/liq_superset
     docker-compose stop
