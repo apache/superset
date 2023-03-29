d22b52addf5cddff5fd8aed15d3e6f11620ef0b0
TusharVirk <Tushar.Virk@locationiq.com.au>
2023-03-29 17:17:27 +1100

    updated deploy script

--- deploy_scripts/app_start.sh
+++ deploy_scripts/app_start.sh
@@ -8,17 +8,9 @@ sudo docker-compose pull superset
 echo "Starting containers..."
 sudo docker-compose up -d
 
-webpack_finish=$(docker-compose logs | grep "\[webpack.Progress\] 100%" | wc -l)
-node_exit=$(docker-compose ps | grep "superset_node" | grep "Exit" | wc -l)
+sleep 300
 
-while [ $webpack_finish -le 1 ] && [ $node_exit -le 1 ]
-do
-  webpack_finish=$(docker-compose logs | grep "\[webpack.Progress\] 100%" | wc -l)
-  node_exit=$(docker-compose ps | grep "superset_node" | grep "Exit" | wc -l)
-  sleep 5
-done
-
-sudo docker-compose down
+sudo docker-compose down --timeout 60
 sudo docker-compose up -d
 
 echo "Done"
