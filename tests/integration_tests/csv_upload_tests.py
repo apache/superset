50bf1c8c25b1bab2fd1ea76ced4d06bcfbf7f636
Hugh A. Miles II <hughmil3s@gmail.com>
2023-03-30 18:23:22 -0400

    chore: allow logs to show error levels for ssh tunnel (#23536)

--- tests/integration_tests/csv_upload_tests.py
+++ tests/integration_tests/csv_upload_tests.py
@@ -513,4 +513,4 @@ def test_import_parquet(mock_event_logger):
 
     with test_db.get_sqla_engine_with_context() as engine:
         data = engine.execute(f"SELECT * from {PARQUET_UPLOAD_TABLE}").fetchall()
-        assert data == [("john", 1), ("paul", 2), ("max", 3), ("bob", 4)]
+        assert data == [("max", 3), ("bob", 4), ("john", 1), ("paul", 2)]
