f97c686754ebc53f61bdc334b2ace68e06849e00
Mattc1221 <mchiang2112@gmail.com>
2023-04-01 20:55:44 -0500

    added deck gl heatmap

--- superset/viz.py
+++ superset/viz.py
@@ -2993,6 +2993,31 @@ class DeckHex(BaseDeckGLViz):
         return super().get_data(df)
 
 
+class DeckHeatmap(BaseDeckGLViz):
+
+    """deck.gl's HeatmapLayer"""
+
+    viz_type = "deck_heatmap"
+    verbose_name = _("Deck.gl - Heatmap")
+    spatial_control_keys = ["spatial"]
+
+    def get_properties(self, data: Dict[str, Any]) -> Dict[str, Any]:
+        # pdb.set_trace()
+        weight = 1
+        if (self.metric_label):
+            weight = data.get(self.metric_label)
+        return {
+            "position": data.get("spatial"),
+            "weight": (data.get(self.metric_label) if self.metric_label else None) or 1,
+        }
+
+    def get_data(self, df: pd.DataFrame) -> VizData:
+        # pdb.set_trace()
+        self.metric_label = (  # pylint: disable=attribute-defined-outside-init
+            utils.get_metric_name(self.metric) if self.metric else None
+        )
+        return super().get_data(df)
+
 class DeckGeoJson(BaseDeckGLViz):
 
     """deck.gl's GeoJSONLayer"""
