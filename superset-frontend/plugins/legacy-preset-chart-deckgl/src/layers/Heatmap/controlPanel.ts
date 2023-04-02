a15a305becae86fe936f8976b3c80ba35d093fed
Mattc1221 <mchiang2112@gmail.com>
2023-04-02 15:15:30 -0500

    fix: fix linting errors

--- superset-frontend/plugins/legacy-preset-chart-deckgl/src/layers/Heatmap/controlPanel.ts
+++ superset-frontend/plugins/legacy-preset-chart-deckgl/src/layers/Heatmap/controlPanel.ts
@@ -17,132 +17,132 @@
  * under the License.
  */
 import {
-    ControlPanelConfig,
-    sections,
-    formatSelectOptions,
-  } from '@superset-ui/chart-controls';
-  import {
-    t,
-    validateNonEmpty,
-    legacyValidateNumber,
-    legacyValidateInteger,
-  } from '@superset-ui/core';
-  import {
-    autozoom,
-    filterNulls,
-    jsColumns,
-    jsDataMutator,
-    jsOnclickHref,
-    jsTooltip,
-    mapboxStyle,
-    spatial,
-    viewport,
-  } from '../../utilities/Shared_DeckGL';
-  
-  const INTENSITY_OPTIONS = Array.from(
-    { length: 10 },
-    (_, index) => (index + 1) / 10,
-  );
-  const RADIUS_PIXEL_OPTIONS = Array.from(
-    { length: 14 },
-    (_, index) => index * 5 + 5,
-  );
-  
-  const config: ControlPanelConfig = {
-    controlPanelSections: [
-      sections.legacyRegularTime,
-      {
-        label: t('Query'),
-        expanded: true,
-        controlSetRows: [
-          [spatial],
-          ['size'],
-          ['row_limit'],
-          [filterNulls],
-          ['adhoc_filters'],
-          [
-            {
-              // TODO: add translations
-              name: 'intensity',
-              config: {
-                type: 'SelectControl',
-                label: 'Intesity',
-                description:
-                  'Intensity is the value multiplied by the weight to obtain the final weight',
-                freeForm: true,
-                clearable: false,
-                validators: [legacyValidateNumber],
-                default: 1,
-                choices: formatSelectOptions(INTENSITY_OPTIONS),
-              },
+  ControlPanelConfig,
+  sections,
+  formatSelectOptions,
+} from '@superset-ui/chart-controls';
+import {
+  t,
+  validateNonEmpty,
+  legacyValidateNumber,
+  legacyValidateInteger,
+} from '@superset-ui/core';
+import {
+  autozoom,
+  filterNulls,
+  jsColumns,
+  jsDataMutator,
+  jsOnclickHref,
+  jsTooltip,
+  mapboxStyle,
+  spatial,
+  viewport,
+} from '../../utilities/Shared_DeckGL';
+
+const INTENSITY_OPTIONS = Array.from(
+  { length: 10 },
+  (_, index) => (index + 1) / 10,
+);
+const RADIUS_PIXEL_OPTIONS = Array.from(
+  { length: 14 },
+  (_, index) => index * 5 + 5,
+);
+
+const config: ControlPanelConfig = {
+  controlPanelSections: [
+    sections.legacyRegularTime,
+    {
+      label: t('Query'),
+      expanded: true,
+      controlSetRows: [
+        [spatial],
+        ['size'],
+        ['row_limit'],
+        [filterNulls],
+        ['adhoc_filters'],
+        [
+          {
+            // TODO: add translations
+            name: 'intensity',
+            config: {
+              type: 'SelectControl',
+              label: 'Intesity',
+              description:
+                'Intensity is the value multiplied by the weight to obtain the final weight',
+              freeForm: true,
+              clearable: false,
+              validators: [legacyValidateNumber],
+              default: 1,
+              choices: formatSelectOptions(INTENSITY_OPTIONS),
             },
-          ],
-          [
-            {
-              // TODO: add translations
-              name: 'radius_pixels',
-              config: {
-                type: 'SelectControl',
-                label: 'Intensity Radius',
-                description:
-                  'Intensity Radius is the radius at which the weight is distributed',
-                freeForm: true,
-                clearable: false,
-                validators: [legacyValidateInteger],
-                default: 30,
-                choices: formatSelectOptions(RADIUS_PIXEL_OPTIONS),
-              },
+          },
+        ],
+        [
+          {
+            // TODO: add translations
+            name: 'radius_pixels',
+            config: {
+              type: 'SelectControl',
+              label: 'Intensity Radius',
+              description:
+                'Intensity Radius is the radius at which the weight is distributed',
+              freeForm: true,
+              clearable: false,
+              validators: [legacyValidateInteger],
+              default: 30,
+              choices: formatSelectOptions(RADIUS_PIXEL_OPTIONS),
             },
-          ],
+          },
         ],
-      },
-      {
-        label: t('Map'),
-        controlSetRows: [
-          [mapboxStyle, viewport],
-          ['linear_color_scheme'],
-          [autozoom],
-          [
-            {
-              name: 'aggregation',
-              config: {
-                type: 'SelectControl',
-                label: t('Aggregation'),
-                description: t(
-                  'The function to use when aggregating points into groups',
-                ),
-                default: 'sum',
-                clearable: false,
-                renderTrigger: true,
-                choices: [
-                  ['sum', t('sum')],
-                  ['mean', t('mean')],
-                ],
-              },
-            },
-          ],
-        ],
-      },
-      {
-        label: t('Advanced'),
-        controlSetRows: [
-          [jsColumns],
-          [jsDataMutator],
-          [jsTooltip],
-          [jsOnclickHref],
-        ],
-      },
-    ],
-    controlOverrides: {
-      size: {
-        label: t('Weight'),
-        description: t("Metric used as a weight for the grid's coloring"),
-        validators: [validateNonEmpty],
-      },
+      ],
     },
-    formDataOverrides: formData => ({
-      ...formData,
-    }),
-  };
-  
-  export default config;
+    {
+      label: t('Map'),
+      controlSetRows: [
+        [mapboxStyle, viewport],
+        ['linear_color_scheme'],
+        [autozoom],
+        [
+          {
+            name: 'aggregation',
+            config: {
+              type: 'SelectControl',
+              label: t('Aggregation'),
+              description: t(
+                'The function to use when aggregating points into groups',
+              ),
+              default: 'sum',
+              clearable: false,
+              renderTrigger: true,
+              choices: [
+                ['sum', t('sum')],
+                ['mean', t('mean')],
+              ],
+            },
+          },
+        ],
+      ],
+    },
+    {
+      label: t('Advanced'),
+      controlSetRows: [
+        [jsColumns],
+        [jsDataMutator],
+        [jsTooltip],
+        [jsOnclickHref],
+      ],
+    },
+  ],
+  controlOverrides: {
+    size: {
+      label: t('Weight'),
+      description: t("Metric used as a weight for the grid's coloring"),
+      validators: [validateNonEmpty],
+    },
+  },
+  formDataOverrides: formData => ({
+    ...formData,
+  }),
+};
+
+export default config;
