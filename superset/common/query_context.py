07e2d5208247d765135ddf98c67cb1a37a06b750
squalou <squalou.jenkins@gmail.com>
2023-04-04 14:47:54 +0200

    #23375 (fix after fourth review) set columns numeric datatypes on excel export - rmeove default value

--- superset/common/query_context.py
+++ superset/common/query_context.py
@@ -88,7 +88,7 @@ class QueryContext:
     def get_data(
         self,
         df: pd.DataFrame,
-        coltypes:List[str] = []
+        coltypes:List[str]
     ) -> Union[str, List[Dict[str, Any]]]:
         return self._processor.get_data(df, coltypes)
 
