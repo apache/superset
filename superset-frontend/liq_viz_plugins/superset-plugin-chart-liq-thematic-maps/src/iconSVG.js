185577272b1c834874fc0b02b9d34ffe1d457545
TusharVirk <Tushar.Virk@locationiq.com.au>
2023-04-05 17:20:59 +1000

    changed colors to match style spec

--- superset-frontend/liq_viz_plugins/superset-plugin-chart-liq-thematic-maps/src/iconSVG.js
+++ superset-frontend/liq_viz_plugins/superset-plugin-chart-liq-thematic-maps/src/iconSVG.js
@@ -158,125 +158,131 @@ class LiqMarker {
 const diamondPoints = (size) => `${size/2},0 0,${size/2} ${size/2},${size} ${size},${size/2}`;
 const reverseTrianglePoints = (size) => `0,0 ${size/2},${size} ${size},0`;
 
+const REGIONAL_SIZE = 35;
+const SUB_REGIONAL_SIZE = 30;
+const NEIGHBOURHOOD_SIZE = 25;
+const OTHER_SC_SIZE = 20;
+const OTHER_SIZE = 15;
+
 // Shopping Centres
-export const regionalSC = (new LiqMarker(35, 4, 'red', 'none').createCircle().img);
-export const subRegionalSC = (new LiqMarker(30, 3, 'blue', 'none').createCircle().img);
-export const neighbourhoodSC = (new LiqMarker(25, 3, 'orange', 'none').createCircle().img);
-export const cityCentreSC = (new LiqMarker(25, 3, '#e8e80c', 'none').createCircle().img);
-export const themedSC = (new LiqMarker(25, 3, '#FF69B4', 'none').createCircle().img);
-export const lfrSC = (new LiqMarker(25, 3, 'blue', 'none').createCircle().img);
-export const outletSC = (new LiqMarker(25, 3, 'brown', 'none').createCircle().img);
-export const marketSC = (new LiqMarker(25, 3, 'green', 'none').createCircle().img);
+export const regionalSC = (new LiqMarker(REGIONAL_SIZE, 4, '#d02033', 'none').createCircle().img);
+export const subRegionalSC = (new LiqMarker(SUB_REGIONAL_SIZE, 3, '#2b2bc9', 'none').createCircle().img);
+export const neighbourhoodSC = (new LiqMarker(NEIGHBOURHOOD_SIZE, 3, '#ffbd01', 'none').createCircle().img);
+export const cityCentreSC = (new LiqMarker(OTHER_SC_SIZE, 3, '#e8e80c', 'none').createCircle().img);
+export const themedSC = (new LiqMarker(OTHER_SC_SIZE, 3, '#FF69B4', 'none').createCircle().img);
+export const lfrSC = (new LiqMarker(OTHER_SC_SIZE, 3, 'purple', 'none').createCircle().img);
+export const outletSC = (new LiqMarker(OTHER_SC_SIZE, 3, 'brown', 'none').createCircle().img);
+export const marketSC = (new LiqMarker(OTHER_SC_SIZE, 3, 'green', 'none').createCircle().img);
 
 // Department stores
-export const davidJones = (new LiqMarker(25, 2, 'black', '#ADD8E6').createStar().img);
-export const myer = (new LiqMarker(25, 2, 'black', 'red').createStar().img);
-export const harrisScarfe = (new LiqMarker(25, 2, 'black', 'blue').createSquare().img);
-export const unknownDS = (new LiqMarker(25, 2, 'black', 'black').createStar().img)
+export const davidJones = (new LiqMarker(OTHER_SIZE, 2, 'black', '#6bc1ff').createStar().img);
+export const myer = (new LiqMarker(OTHER_SIZE, 2, 'black', '#bb1431').createStar().img);
+export const harrisScarfe = (new LiqMarker(OTHER_SIZE, 2, 'black', '#1111af').createSquare().img);
+export const unknownDS = (new LiqMarker(OTHER_SIZE, 2, 'black', '#252525').createStar().img)
 
 // Discount department stores
-export const kmart = (new LiqMarker(25, 2, 'black', 'red').createTriangle().img);
-export const kmartHub = (new LiqMarker(25, 2, 'black', 'red').createPolygon(
-  reverseTrianglePoints(25)
+export const kmart = (new LiqMarker(OTHER_SIZE, 2, 'black', '#bb1431').createTriangle().img);
+export const kmartHub = (new LiqMarker(OTHER_SIZE, 2, 'black', '#bb1431').createPolygon(
+  reverseTrianglePoints(OTHER_SIZE)
 ).img)
-export const target = (new LiqMarker(25, 2, 'black', 'yellow').createTriangle().img);
-export const targetCountry = (new LiqMarker(25, 2, 'black', 'yellow').createPolygon(
-  reverseTrianglePoints(25)
+export const target = (new LiqMarker(OTHER_SIZE, 2, 'black', '#ffc527').createTriangle().img);
+export const targetCountry = (new LiqMarker(OTHER_SIZE, 2, 'black', '#ffc527').createPolygon(
+  reverseTrianglePoints(OTHER_SIZE)
 ).img);
-export const bigW = (new LiqMarker(25, 2, 'black', 'blue').createTriangle().img);
-export const unknownDDS = (new LiqMarker(25, 2, 'black', 'black').createTriangle().img);
+export const bigW = (new LiqMarker(OTHER_SIZE, 2, 'black', '#1111af').createTriangle().img);
+export const unknownDDS = (new LiqMarker(OTHER_SIZE, 2, 'black', '#252525').createTriangle().img);
 
 // LFR
-export const amart = (new LiqMarker(25, 2, 'black', 'gray').createPolygon(
-  diamondPoints(25)
+export const amart = (new LiqMarker(OTHER_SIZE, 2, 'black', 'gray').createPolygon(
+  diamondPoints(OTHER_SIZE)
 ).img);
-export const anaconda = (new LiqMarker(25, 2, 'black', 'orange').createTriangle().img);
-export const bunnings = (new LiqMarker(25, 2, 'black', '#008080').createSquare().img);
-export const domayne = (new LiqMarker(25, 2, 'black', 'green').createPolygon(
-  diamondPoints(25)
+export const anaconda = (new LiqMarker(OTHER_SIZE, 2, 'black', 'orange').createTriangle().img);
+export const bunnings = (new LiqMarker(OTHER_SIZE, 2, 'black', '#008080').createSquare().img);
+export const domayne = (new LiqMarker(OTHER_SIZE, 2, 'black', 'green').createPolygon(
+  diamondPoints(OTHER_SIZE)
 ).img);
-export const fantasticFurniture = (new LiqMarker(25, 2, 'black', 'red').createPolygon(
-  diamondPoints(25)
+export const fantasticFurniture = (new LiqMarker(OTHER_SIZE, 2, 'black', 'red').createPolygon(
+  diamondPoints(OTHER_SIZE)
 ).img);
-export const fortyWinks = (new LiqMarker(25, 2, 'black', 'blue').createPolygon(
-  reverseTrianglePoints(25)
+export const fortyWinks = (new LiqMarker(OTHER_SIZE, 2, 'black', 'blue').createPolygon(
+  reverseTrianglePoints(OTHER_SIZE)
 ).img);
-export const harveyNorman = (new LiqMarker(25, 2, 'black', '#ADD8E6').createStar().img);
-export const ikea = (new LiqMarker(25, 2, 'black', 'yellow').createPolygon(
-  diamondPoints(25)
+export const harveyNorman = (new LiqMarker(OTHER_SIZE, 2, 'black', '#ADD8E6').createStar().img);
+export const ikea = (new LiqMarker(OTHER_SIZE, 2, 'black', 'yellow').createPolygon(
+  diamondPoints(OTHER_SIZE)
 ).img);
-export const lincraft = (new LiqMarker(25, 2, 'black', 'purple').createCircle().img);
-export const snooze = (new LiqMarker(25, 2, 'black', 'brown').createPolygon(
-  reverseTrianglePoints(25)
+export const lincraft = (new LiqMarker(OTHER_SIZE, 2, 'black', 'purple').createCircle().img);
+export const snooze = (new LiqMarker(OTHER_SIZE, 2, 'black', 'brown').createPolygon(
+  reverseTrianglePoints(OTHER_SIZE)
 ).img);
-export const spotlight = (new LiqMarker(25, 2, 'black', 'red').createCircle().img);
-export const theGoodGuys = (new LiqMarker(25, 2, 'black', 'purple').createStar().img);
+export const spotlight = (new LiqMarker(OTHER_SIZE, 2, 'black', 'red').createCircle().img);
+export const theGoodGuys = (new LiqMarker(OTHER_SIZE, 2, 'black', 'purple').createStar().img);
 
 // Mini majors
-export const appleStore = (new LiqMarker(25, 2, 'black', '#90EE90').createStar().img);
-export const bestAndLess = (new LiqMarker(25, 2, 'black', 'blue').createCircle().img);
-export const chemistWarehouse = (new LiqMarker(25, 2, 'black', 'brown').createPolygon(
-  diamondPoints(25)
+export const appleStore = (new LiqMarker(OTHER_SIZE, 2, 'black', '#85cc14').createStar().img);
+export const bestAndLess = (new LiqMarker(OTHER_SIZE, 2, 'black', '#b74fc9').createCircle().img);
+export const chemistWarehouse = (new LiqMarker(OTHER_SIZE, 2, 'black', '#bb1431').createPolygon(
+  diamondPoints(OTHER_SIZE)
 ).img);
-export const cottonOn = (new LiqMarker(25, 2, 'black', 'brown').createCircle().img);
-export const countryRoad = (new LiqMarker(25, 2, 'black', 'pink').createCircle().img);
-export const daiso = (new LiqMarker(25, 2, 'black', 'blue').createSquare().img);
-export const danMurphys = (new LiqMarker(25, 2, 'black', 'green').createPentagon().img);
-export const firstChoiceLiquor = (new LiqMarker(25, 2, 'black', 'brown').createPentagon().img);
-export const glueStore = (new LiqMarker(25, 2, 'black', 'orange').createCircle().img);
-export const hAndM = (new LiqMarker(25, 2, 'black', 'yellow').createCircle().img);
-export const harrisFarmMarkets = (new LiqMarker(25, 2, 'black', 'blue').createPentagon().img);
-export const hsHome = (new LiqMarker(25, 2, 'black', 'purple').createPolygon(
-  reverseTrianglePoints(25)
+export const cottonOn = (new LiqMarker(OTHER_SIZE, 2, 'black', '#bb1431').createCircle().img);
+export const countryRoad = (new LiqMarker(OTHER_SIZE, 2, 'black', '#6bc1ff').createCircle().img);
+export const daiso = (new LiqMarker(OTHER_SIZE, 2, 'black', '#b74fc9').createSquare().img);
+export const danMurphys = (new LiqMarker(OTHER_SIZE, 2, 'black', '#015c00').createPentagon().img);
+export const firstChoiceLiquor = (new LiqMarker(OTHER_SIZE, 2, 'black', '#bb1431').createPentagon().img);
+export const glueStore = (new LiqMarker(OTHER_SIZE, 2, 'black', '#ff6e00').createCircle().img);
+export const hAndM = (new LiqMarker(OTHER_SIZE, 2, 'black', '#ffc527').createCircle().img);
+export const harrisFarmMarkets = (new LiqMarker(OTHER_SIZE, 2, 'black', '#6bc1ff').createPentagon().img);
+export const hsHome = (new LiqMarker(OTHER_SIZE, 2, 'black', '#1111af').createPolygon(
+  reverseTrianglePoints(OTHER_SIZE)
 ).img);
-export const jbhifi = (new LiqMarker(25, 2, 'black', 'orange').createStar().img);
-export const kathmandu = (new LiqMarker(25, 2, 'black', 'yellow').createTriangle().img);
-export const meccaCosmetica = (new LiqMarker(25, 2, 'black', 'orange').createPolygon(
-  reverseTrianglePoints(25)
+export const jbhifi = (new LiqMarker(OTHER_SIZE, 2, 'black', '#ff6e00').createStar().img);
+export const kathmandu = (new LiqMarker(OTHER_SIZE, 2, 'black', '#ff6e00').createTriangle().img);
+export const meccaCosmetica = (new LiqMarker(OTHER_SIZE, 2, 'black', '#7fc314').createPolygon(
+  reverseTrianglePoints(OTHER_SIZE)
 ).img);
-export const pricelinePharmacy = (new LiqMarker(25, 2, 'black', 'pink').createPolygon(
-  diamondPoints(25)
+export const pricelinePharmacy = (new LiqMarker(OTHER_SIZE, 2, 'black', '#ffc527').createPolygon(
+  diamondPoints(OTHER_SIZE)
 ).img);
-export const rebelSports = (new LiqMarker(25, 2, 'black', '#008080').createTriangle().img);
-export const rivers = (new LiqMarker(25, 2, 'black', '#90EE90').createCircle().img);
-export const sephora = (new LiqMarker(25, 2, 'black', 'green').createPolygon(
-  reverseTrianglePoints(25)
+export const rebelSports = (new LiqMarker(OTHER_SIZE, 2, 'black', '#6bc1ff').createTriangle().img);
+export const rivers = (new LiqMarker(OTHER_SIZE, 2, 'black', '#1198a4').createCircle().img);
+export const sephora = (new LiqMarker(OTHER_SIZE, 2, 'black', '#bb1431').createPolygon(
+  reverseTrianglePoints(OTHER_SIZE)
 ).img);
-export const terryWhiteChemmart = (new LiqMarker(25, 2, 'black', 'blue').createPolygon(
-  diamondPoints(25)
+export const terryWhiteChemmart = (new LiqMarker(OTHER_SIZE, 2, 'black', '#7fc314').createPolygon(
+  diamondPoints(OTHER_SIZE)
 ).img);
-export const theRejectShop = (new LiqMarker(25, 2, 'black', 'brown').createSquare().img);
-export const tkMaxx = (new LiqMarker(25, 2, 'black', '#008080').createCircle().img);
-export const uniqlo = (new LiqMarker(25, 2, 'black', 'purple').createCircle().img);
-export const zara = (new LiqMarker(25, 2, 'black', 'green').createCircle().img);
+export const theRejectShop = (new LiqMarker(OTHER_SIZE, 2, 'black', '#ffc527').createSquare().img);
+export const tkMaxx = (new LiqMarker(OTHER_SIZE, 2, 'black', '#1111af').createCircle().img);
+export const uniqlo = (new LiqMarker(OTHER_SIZE, 2, 'black', '#ff77c2').createCircle().img);
+export const zara = (new LiqMarker(OTHER_SIZE, 2, 'black', '#85cc14').createCircle().img);
 
-// Woolworths
-export const woolworths = (new LiqMarker(25, 2, 'black', '#90EE90').createCircle().img);
-export const coles = (new LiqMarker(25, 2, 'black', 'red').createCircle().img);
-export const aldi = (new LiqMarker(25, 2, 'black', '#ADD8E6').createCircle().img);
-export const iga = (new LiqMarker(25, 2, 'black', 'yellow').createCircle().img);
-export const foodWorks = (new LiqMarker(25, 2, 'black', 'orange').createCircle().img);
-export const costco = (new LiqMarker(25, 2, 'black', 'purple').createCircle().img);
-export const drakes = (new LiqMarker(25, 2, 'black', 'white').createCircle().img);
-export const spar = (new LiqMarker(25, 2, 'black', 'brown').createCircle().img);
-export const igaExpress = (new LiqMarker(25, 2, 'black', 'blue').createCircle().img);
-export const otherSmkt = (new LiqMarker(25, 2, 'black', 'blue').createCircle().img);
-export const unknownSmkt = (new LiqMarker(25, 2, 'black', 'black').createCircle().img);
+// Supermarkets
+export const woolworths = (new LiqMarker(OTHER_SIZE, 2, 'black', '#01b208').createCircle().img);
+export const coles = (new LiqMarker(OTHER_SIZE, 2, 'black', '#bb1431').createCircle().img);
+export const aldi = (new LiqMarker(OTHER_SIZE, 2, 'black', '#6bc1ff').createCircle().img);
+export const iga = (new LiqMarker(OTHER_SIZE, 2, 'black', '#ffc527').createCircle().img);
+export const foodWorks = (new LiqMarker(OTHER_SIZE, 2, 'black', '#ff6e00').createCircle().img);
+export const costco = (new LiqMarker(OTHER_SIZE, 2, 'black', '#b74fc9').createCircle().img);
+export const drakes = (new LiqMarker(OTHER_SIZE, 2, 'black', '#a9a9a9').createCircle().img);
+export const spar = (new LiqMarker(OTHER_SIZE, 2, 'black', '#700184').createCircle().img);
+export const igaExpress = (new LiqMarker(OTHER_SIZE, 2, 'black', '#ffc527').createCircle().img);
+export const otherSmkt = (new LiqMarker(OTHER_SIZE, 2, 'black', '#1111af').createCircle().img);
+export const unknownSmkt = (new LiqMarker(OTHER_SIZE, 2, 'black', '#252525').createCircle().img);
 
 // Liquor
-export const liquorLand = (new LiqMarker(25, 2, 'black', 'brown').createPolygon(
-  diamondPoints(25)
+export const liquorLand = (new LiqMarker(OTHER_SIZE, 2, 'black', '#e31a1c').createPolygon(
+  diamondPoints(OTHER_SIZE)
 ).img);
-export const bws = (new LiqMarker(25, 2, 'black', 'green').createPolygon(
-  diamondPoints(25)
+export const bws = (new LiqMarker(OTHER_SIZE, 2, 'black', '#ff6e00').createPolygon(
+  diamondPoints(OTHER_SIZE)
 ).img);
-export const igaLiquor = (new LiqMarker(25, 2, 'black', 'yellow').createPolygon(
-  diamondPoints(25)
+export const igaLiquor = (new LiqMarker(OTHER_SIZE, 2, 'black', '#f9d729').createPolygon(
+  diamondPoints(OTHER_SIZE)
 ).img);
-export const aldiLiquor = (new LiqMarker(25, 2, 'black', '#008080').createPolygon(
-  diamondPoints(25)
+export const aldiLiquor = (new LiqMarker(OTHER_SIZE, 2, 'black', '#6bc1ff').createPolygon(
+  diamondPoints(OTHER_SIZE)
 ).img);
-export const vintageCellars = (new LiqMarker(25, 2, 'black', 'purple').createPolygon(
-  diamondPoints(25)
+export const vintageCellars = (new LiqMarker(OTHER_SIZE, 2, 'black', '#d346d1').createPolygon(
+  diamondPoints(OTHER_SIZE)
 ).img);
-export const otherLiquor = (new LiqMarker(25, 2, 'black', 'blue').createCircle().img);
+export const otherLiquor = (new LiqMarker(OTHER_SIZE, 2, 'black', '#0000ff').createCircle().img);
