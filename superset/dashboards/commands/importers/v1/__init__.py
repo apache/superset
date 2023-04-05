a2bb048ba3d73fe925f579bdd506a63eb7a69920
Jack Fragassi <jfragassi98@gmail.com>
2023-04-04 18:14:46 -0700

    Fix lint

--- superset/dashboards/commands/importers/v1/__init__.py
+++ superset/dashboards/commands/importers/v1/__init__.py
@@ -114,7 +114,9 @@ class ImportDashboardsCommand(ImportModelsCommand):
                 # update datasource id, type, and name
                 dataset_dict = dataset_info[config["dataset_uuid"]]
                 config.update(dataset_dict)
-                dataset_uid = f"{dataset_dict['datasource_id']}__{dataset_dict['datasource_type']}"
+                dataset_uid = (
+                    f"{dataset_dict['datasource_id']}__{dataset_dict['datasource_type']}"
+                )
                 config["params"].update({"datasource": dataset_uid})
                 if "query_context" in config:
                     del config["query_context"]
