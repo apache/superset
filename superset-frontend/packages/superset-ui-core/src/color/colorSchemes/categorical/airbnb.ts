7f197e33f5ddd793d58faab84f0f67d231a61fbd
John Bodley <4567245+john-bodley@users.noreply.github.com>
2023-04-07 11:36:16 +1200

    chore(colors): Updating Airbnb branch colors

--- superset-frontend/packages/superset-ui-core/src/color/colorSchemes/categorical/airbnb.ts
+++ superset-frontend/packages/superset-ui-core/src/color/colorSchemes/categorical/airbnb.ts
@@ -24,27 +24,20 @@ const schemes = [
     id: 'bnbColors',
     label: 'Airbnb Colors',
     colors: [
-      '#ff5a5f', // rausch
-      '#7b0051', // hackb
-      '#007A87', // kazan
-      '#00d1c1', // babu
-      '#8ce071', // lima
-      '#ffb400', // beach
-      '#b4a76c', // barol
-      '#ff8083',
-      '#cc0086',
-      '#00a1b3',
-      '#00ffeb',
-      '#bbedab',
-      '#ffd266',
-      '#cbc29a',
-      '#ff3339',
-      '#ff1ab1',
-      '#005c66',
-      '#00b3a5',
-      '#55d12e',
-      '#b37e00',
-      '#988b4e',
+      '#ff385c', // rausch
+      '#92174d', // plus
+      '#460479', // luxe
+      '#222222', // hof
+      '#dddddd', // deco
+      '#c13515', // arches
+      '#d0650b', // ondo
+      '#ffaf0f', // beach
+      '#7dv734', // lima
+      '#22bc4e', // spruce
+      '#00ba92', // norrr
+      '#007e82', // babu
+      '#2875f0', // mykonou
+      '#9b3197', // omiya
     ],
   },
 ].map(s => new CategoricalScheme(s));
