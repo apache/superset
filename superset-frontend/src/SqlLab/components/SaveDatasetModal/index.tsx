dfe7191b8c7470c31bf5a64df6808eeb38fb6107
hughhhh <hughmil3s@gmail.com>
2023-04-07 13:34:12 -0400

    fe build fix

--- superset-frontend/src/SqlLab/components/SaveDatasetModal/index.tsx
+++ superset-frontend/src/SqlLab/components/SaveDatasetModal/index.tsx
@@ -301,9 +301,7 @@ export const SaveDatasetModal = ({
           ...formDataWithDefaults,
           datasource: `${data.table_id}__table`,
           ...(defaultVizType === 'table' && {
-            all_columns: selectedColumns.map(
-              column => column.column_name || column.name,
-            ),
+            all_columns: selectedColumns.map(column => column.column_name),
           }),
         }),
       )
