6aee98bfb8e949dbf76ca2d93a6173d0b9d83ba1
hughhhh <hughmil3s@gmail.com>
2023-04-13 10:58:17 -0400

    added tests

--- superset/migrations/versions/2023-03-27_12-30_7e67aecbf3f1_chart_ds_constraint.py
+++ superset/migrations/versions/2023-03-27_12-30_7e67aecbf3f1_chart_ds_constraint.py
@@ -26,6 +26,8 @@ Create Date: 2023-03-27 12:30:01.164594
 revision = "7e67aecbf3f1"
 down_revision = "07f9a902af1b"
 
+import json
+
 import sqlalchemy as sa
 from alembic import op
 from sqlalchemy.ext.declarative import declarative_base
@@ -39,9 +41,21 @@ class Slice(Base):  # type: ignore
     __tablename__ = "slices"
 
     id = sa.Column(sa.Integer, primary_key=True)
-    slice_name = sa.Column(sa.String(250))
+    params = sa.Column(sa.String(250))
     datasource_type = sa.Column(sa.String(200))
-    query_context = sa.Column(sa.Text)
+
+
+def upgrade_slc(slc: Slice) -> None:
+    # clean up all charts with datasource_type not != table
+    slc.datasource_type = "table"
+    try:
+        params_dict = json.loads(slc.params)
+        ds_id, ds_type = params_dict["datasource"].split("__")
+        params_dict["datasource"] = f"{ds_id}__table"
+        slc.params = json.dumps(params_dict)
+    except Exception:
+        # skip any malformatted params
+        pass
 
 
 def upgrade():
@@ -50,8 +64,7 @@ def upgrade():
 
     with op.batch_alter_table("slices") as batch_op:
         for slc in session.query(Slice).filter(Slice.datasource_type == "query").all():
-            # clean up all charts with datasource_type not != table
-            slc.datasource_type = "table"
+            upgrade_slc(slc)
             session.add(slc)
 
         batch_op.create_check_constraint(
