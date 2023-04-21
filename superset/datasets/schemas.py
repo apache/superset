b31efbae7c58fdf665f4910e895b4f9c1d7ebe1b
Sebastian Liebscher <112352529+sebastianliebscher@users.noreply.github.com>
2023-04-21 08:11:46 +0200

    chore: remove RemovedInMarshmallow4 warnings (#23704)

--- superset/datasets/schemas.py
+++ superset/datasets/schemas.py
@@ -49,14 +49,14 @@ class DatasetColumnsPutSchema(Schema):
     column_name = fields.String(required=True, validate=Length(1, 255))
     type = fields.String(allow_none=True)
     advanced_data_type = fields.String(allow_none=True, validate=Length(1, 255))
-    verbose_name = fields.String(allow_none=True, Length=(1, 1024))
+    verbose_name = fields.String(allow_none=True, metadata={Length: (1, 1024)})
     description = fields.String(allow_none=True)
     expression = fields.String(allow_none=True)
     extra = fields.String(allow_none=True)
     filterable = fields.Boolean()
     groupby = fields.Boolean()
     is_active = fields.Boolean(allow_none=True)
-    is_dttm = fields.Boolean(default=False)
+    is_dttm = fields.Boolean(dump_default=False)
     python_date_format = fields.String(
         allow_none=True, validate=[Length(1, 255), validate_python_date_format]
     )
@@ -71,7 +71,7 @@ class DatasetMetricsPutSchema(Schema):
     metric_name = fields.String(required=True, validate=Length(1, 255))
     metric_type = fields.String(allow_none=True, validate=Length(1, 32))
     d3format = fields.String(allow_none=True, validate=Length(1, 128))
-    verbose_name = fields.String(allow_none=True, Length=(1, 1024))
+    verbose_name = fields.String(allow_none=True, metadata={Length: (1, 1024)})
     warning_text = fields.String(allow_none=True)
     uuid = fields.UUID(allow_none=True)
 
@@ -82,7 +82,7 @@ class DatasetPostSchema(Schema):
     table_name = fields.String(required=True, allow_none=False, validate=Length(1, 250))
     sql = fields.String(allow_none=True)
     owners = fields.List(fields.Integer())
-    is_managed_externally = fields.Boolean(allow_none=True, default=False)
+    is_managed_externally = fields.Boolean(allow_none=True, dump_default=False)
     external_url = fields.String(allow_none=True)
 
 
@@ -104,7 +104,7 @@ class DatasetPutSchema(Schema):
     columns = fields.List(fields.Nested(DatasetColumnsPutSchema))
     metrics = fields.List(fields.Nested(DatasetMetricsPutSchema))
     extra = fields.String(allow_none=True)
-    is_managed_externally = fields.Boolean(allow_none=True, default=False)
+    is_managed_externally = fields.Boolean(allow_none=True, dump_default=False)
     external_url = fields.String(allow_none=True)
 
 
@@ -127,16 +127,18 @@ class DatasetRelatedDashboard(Schema):
 
 
 class DatasetRelatedCharts(Schema):
-    count = fields.Integer(description="Chart count")
+    count = fields.Integer(metadata={"description": "Chart count"})
     result = fields.List(
-        fields.Nested(DatasetRelatedChart), description="A list of dashboards"
+        fields.Nested(DatasetRelatedChart),
+        metadata={"description": "A list of dashboards"},
     )
 
 
 class DatasetRelatedDashboards(Schema):
-    count = fields.Integer(description="Dashboard count")
+    count = fields.Integer(metadata={"description": "Dashboard count"})
     result = fields.List(
-        fields.Nested(DatasetRelatedDashboard), description="A list of dashboards"
+        fields.Nested(DatasetRelatedDashboard),
+        metadata={"description": "A list of dashboards"},
     )
 
 
@@ -160,8 +162,8 @@ class ImportV1ColumnSchema(Schema):
     column_name = fields.String(required=True)
     extra = fields.Dict(allow_none=True)
     verbose_name = fields.String(allow_none=True)
-    is_dttm = fields.Boolean(default=False, allow_none=True)
-    is_active = fields.Boolean(default=True, allow_none=True)
+    is_dttm = fields.Boolean(dump_default=False, allow_none=True)
+    is_active = fields.Boolean(dump_default=True, allow_none=True)
     type = fields.String(allow_none=True)
     advanced_data_type = fields.String(allow_none=True)
     groupby = fields.Boolean()
@@ -224,19 +226,21 @@ class ImportV1DatasetSchema(Schema):
     version = fields.String(required=True)
     database_uuid = fields.UUID(required=True)
     data = fields.URL()
-    is_managed_externally = fields.Boolean(allow_none=True, default=False)
+    is_managed_externally = fields.Boolean(allow_none=True, dump_default=False)
     external_url = fields.String(allow_none=True)
 
 
 class GetOrCreateDatasetSchema(Schema):
-    table_name = fields.String(required=True, description="Name of table")
+    table_name = fields.String(required=True, metadata={"description": "Name of table"})
     database_id = fields.Integer(
-        required=True, description="ID of database table belongs to"
+        required=True, metadata={"description": "ID of database table belongs to"}
     )
     schema = fields.String(
-        description="The schema the table belongs to", allow_none=True
+        metadata={"description": "The schema the table belongs to"}, allow_none=True
     )
-    template_params = fields.String(description="Template params for the table")
+    template_params = fields.String(
+        metadata={"description": "Template params for the table"}
+    )
 
 
 class DatasetSchema(SQLAlchemyAutoSchema):
