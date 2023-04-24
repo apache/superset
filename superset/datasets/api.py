c7c05a7e71956f742b1721d86073dd64c7ab5501
hughhhh <hughmil3s@gmail.com>
2023-04-24 12:56:20 -0400

    ok

--- superset/datasets/api.py
+++ superset/datasets/api.py
@@ -22,16 +22,16 @@ from typing import Any
 from zipfile import is_zipfile, ZipFile
 
 import yaml
-from flask import g, request, Response, send_file
-from flask_appbuilder.api import expose, permission_name, protect, rison, safe
+from flask import request, Response, send_file
+from flask_appbuilder.api import expose, protect, rison, safe
 from flask_appbuilder.models.sqla.interface import SQLAInterface
-from flask_babel import gettext as __, lazy_gettext as _, ngettext
+from flask_babel import ngettext
 from marshmallow import ValidationError
 
-from superset import db, event_logger, is_feature_enabled
+from superset import event_logger, is_feature_enabled
 from superset.commands.importers.exceptions import NoValidFilesFoundError
 from superset.commands.importers.v1.utils import get_contents_from_bundle
-from superset.connectors.sqla.models import SqlaTable, SqlMetric, TableColumn
+from superset.connectors.sqla.models import SqlaTable
 from superset.constants import MODEL_API_RW_METHOD_PERMISSION_MAP, RouteMethod
 from superset.databases.filters import DatabaseFilter
 from superset.datasets.commands.bulk_delete import BulkDeleteDatasetCommand
@@ -63,18 +63,8 @@ from superset.datasets.schemas import (
     get_export_ids_schema,
     GetOrCreateDatasetSchema,
 )
-from superset.errors import ErrorLevel, SupersetError, SupersetErrorType
-from superset.exceptions import SupersetErrorException, SupersetGenericErrorException
-from superset.models.core import Database
-from superset.sql_parse import ParsedQuery
 from superset.utils.core import parse_boolean_string
-from superset.views.base import (
-    DatasourceFilter,
-    generate_download_headers,
-    json_errors_response,
-    json_success,
-    validate_sqlatable,
-)
+from superset.views.base import DatasourceFilter, generate_download_headers
 from superset.views.base_api import (
     BaseSupersetModelRestApi,
     RelatedFieldFilter,
@@ -83,7 +73,6 @@ from superset.views.base_api import (
     statsd_metrics,
 )
 from superset.views.filters import BaseFilterRelatedUsers, FilterRelatedOwners
-from superset.views.utils import sanitize_datasource_data
 
 logger = logging.getLogger(__name__)
 
