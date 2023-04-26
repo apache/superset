99e5fdab68d3331f3bbfc6b2de57c8a95d23bc23
jwilliams-ocient <60358443+jwilliams-ocient@users.noreply.github.com>
2023-04-26 12:59:51 -0500

    chore: remove unused import

--- superset/db_engine_specs/ocient.py
+++ superset/db_engine_specs/ocient.py
@@ -15,7 +15,6 @@
 # specific language governing permissions and limitations
 # under the License.
 
-import json
 import re
 import threading
 from typing import Any, Callable, Dict, List, NamedTuple, Optional, Pattern, Set, Tuple
