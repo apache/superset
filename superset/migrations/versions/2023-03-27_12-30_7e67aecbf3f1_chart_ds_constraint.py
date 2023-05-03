acdb06eca85e24613411ba82f9e04dc22f537a10
Hugh A. Miles II <hughmil3s@gmail.com>
2023-05-03 17:55:21 -0400

    Update 2023-03-27_12-30_7e67aecbf3f1_chart_ds_constraint.py

--- superset/migrations/versions/2023-03-27_12-30_7e67aecbf3f1_chart_ds_constraint.py
+++ superset/migrations/versions/2023-03-27_12-30_7e67aecbf3f1_chart_ds_constraint.py
@@ -59,11 +59,7 @@ def upgrade_slc(slc: Slice) -> None:
         # the assumption here is that the query was saved as a dataset
         # but the type wasn't written properly to the slice
         # by updating the type here we expect it will either work
-        # or it will 404 when the dataset is looked up.        
-        # the assumption here is that the query was saved as a dataset
-        # but the type wasn't written properly to the slice
-        # by updating the type here we expect it will either work
-        # or it will 404 when the dataset is looked up.        
+        # or it will 404 when the dataset is looked up.              
         params_dict["datasource"] = f"{ds_id}__table"
         slc.params = json.dumps(params_dict)
         logger.warning("updated slice datasource from %s__%s to %s__table", ds_id, ds_type, ds_id)
