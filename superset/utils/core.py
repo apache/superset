be6576b55a4a263bc888a1d07362a8afbb8f104b
sebastianliebscher <liebscher.sebastian@protonmail.ch>
2023-05-10 12:34:04 +0200

    pylint

--- superset/utils/core.py
+++ superset/utils/core.py
@@ -1799,10 +1799,9 @@ def strtobool(val: str) -> bool:
     val = val.lower()
     if val in {"y", "yes", "t", "true", "on", "1"}:
         return True
-    elif val in {"n", "no", "f", "false", "off", "0"}:
+    if val in {"n", "no", "f", "false", "off", "0"}:
         return False
-    else:
-        raise ValueError("invalid truth value %r" % (val,))
+    raise ValueError("invalid truth value %r" % (val,))
 
 
 def is_test() -> bool:
