da85a2108a92ddd85aac0b4252f862e2317e7cf8
Multazim Deshmukh <multazim.deshmukh@morningstar.com>
2023-05-17 07:47:15 +0530

    Hive CI Test with PY 3.9 and Patched PyHive Commit

--- setup.py
+++ setup.py
@@ -160,7 +160,7 @@ setup(
         "firebolt": ["firebolt-sqlalchemy>=0.0.1"],
         "gsheets": ["shillelagh[gsheetsapi]>=1.0.14, <2"],
         "hana": ["hdbcli==2.4.162", "sqlalchemy_hana==0.4.0"],
-        "hive": ["pyhive[hive]>=0.6.5", "tableschema", "thrift>=0.14.1, <1.0.0"],
+        "hive": ["PyHive@git+https://github.com/dropbox/PyHive.git@a4c30f21f7e0cfe830b3a04ade7edf5cd103c678", "pyhive[hive]>=0.6.5", "tableschema", "thrift>=0.14.1, <1.0.0"],
         "impala": ["impyla>0.16.2, <0.17"],
         "kusto": ["sqlalchemy-kusto>=2.0.0, <3"],
         "kylin": ["kylinpy>=2.8.1, <2.9"],
