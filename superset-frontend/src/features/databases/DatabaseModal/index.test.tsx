c1ccff2fb18ef4c346ceb8a65eac0ec755400d05
Hugh A. Miles II <hughmil3s@gmail.com>
2023-05-26 19:24:37 +0000

    update test

--- superset-frontend/src/features/databases/DatabaseModal/index.test.tsx
+++ superset-frontend/src/features/databases/DatabaseModal/index.test.tsx
@@ -124,6 +124,10 @@ fetchMock.mock(AVAILABLE_DB_ENDPOINT, {
             description: 'Additional parameters',
             type: 'object',
           },
+          ssh: {
+            description: 'Create SSH Tunnel',
+            type: 'boolean',
+          },
           username: {
             description: 'Username',
             nullable: true,
