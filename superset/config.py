5df0b7ad57fc8e3dd8e8cdd764905f2b64015174
Elizabeth Thompson <eschutho@gmail.com>
2023-06-06 15:58:54 -0700

    lint

--- superset/config.py
+++ superset/config.py
@@ -1205,6 +1205,7 @@ def SQL_QUERY_MUTATOR(  # pylint: disable=invalid-name,unused-argument
 # functionality for both the SQL_Lab and Charts.
 MUTATE_AFTER_SPLIT = False
 
+
 # This allows for a user to add header data to any outgoing emails. For example,
 # if you need to include metadata in the header or you want to change the specifications
 # of the email title, header, or sender.
@@ -1584,7 +1585,7 @@ elif importlib.util.find_spec("superset_config") and not is_test():
     try:
         # pylint: disable=import-error,wildcard-import,unused-wildcard-import
         import superset_config
-        from superset_config import *  # type: ignore
+        from superset_config import *  # noqa
 
         print(f"Loaded your LOCAL configuration at [{superset_config.__file__}]")
     except Exception:
