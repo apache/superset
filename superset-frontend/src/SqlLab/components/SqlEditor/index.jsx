bac886e6ce2fb187762fb096a237fd759e99064f
justin-park <justin.park@airbnb.com>
2023-06-07 15:58:51 -0700

    add shortcut for run current sql

--- superset-frontend/src/SqlLab/components/SqlEditor/index.jsx
+++ superset-frontend/src/SqlLab/components/SqlEditor/index.jsx
@@ -328,6 +328,49 @@ const SqlEditor = ({
         },
       },
       {
+        name: 'runQuery3',
+        key: 'ctrl+shift+enter',
+        descr: t('Run current query'),
+        func: editor => {
+          if (!editor.getValue().trim()) {
+            return;
+          }
+          const session = editor.getSession();
+          const cursorPosition = editor.getCursorPosition();
+          const totalLine = session.getLength();
+          let end = editor.find(';', {
+            backwards: false,
+            skipCurrent: true,
+            start: cursorPosition,
+          })?.end;
+          if (!end || end.row < cursorPosition.row) {
+            end = {
+              row: totalLine + 1,
+              column: 0,
+            };
+          }
+          let currentLine = editor.find(';', {
+            backwards: true,
+            skipCurrent: true,
+            start: cursorPosition,
+          })?.end?.row;
+          if (!currentLine || currentLine > cursorPosition.row) {
+            currentLine = 0;
+          }
+          let content = session.getLine(0).trim();
+          while (!content && currentLine < totalLine) {
+            currentLine += 1;
+            content = session.getLine(currentLine).trim();
+          }
+          const start = { row: currentLine, column: 0 };
+          editor.selection.setRange({
+            start: start ?? { row: 0, column: 0 },
+            end,
+          });
+          startQuery();
+        },
+      },
+      {
         name: 'newTab',
         key: userOS === 'Windows' ? 'ctrl+q' : 'ctrl+t',
         descr: t('New tab'),
