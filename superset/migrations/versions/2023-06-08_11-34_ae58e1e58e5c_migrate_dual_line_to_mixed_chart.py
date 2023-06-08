3fd5ac263bf2c39d7e39dabb88b1a02572fdea54
Michael S. Molina <michael.s.molina@gmail.com>
2023-06-08 11:35:16 -0300

    Rebases

--- superset/migrations/versions/2023-06-08_11-34_ae58e1e58e5c_migrate_dual_line_to_mixed_chart.py
+++ superset/migrations/versions/2023-06-08_11-34_ae58e1e58e5c_migrate_dual_line_to_mixed_chart.py
@@ -17,14 +17,14 @@
 """migrate_dual_line_to_mixed_chart
 
 Revision ID: ae58e1e58e5c
-Revises: 9c2a5681ddfd
-Create Date: 2023-05-02 13:11:36.241939
+Revises: 4c5da39be729
+Create Date: 2023-06-08 11:34:36.241939
 
 """
 
 # revision identifiers, used by Alembic.
 revision = "ae58e1e58e5c"
-down_revision = "9c2a5681ddfd"
+down_revision = "4c5da39be729"
 
 from superset.migrations.shared.migrate_viz.processors import MigrateDualLine
 
