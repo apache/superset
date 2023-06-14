b2f78dc32bea481891cc54e287d5e8716d3a1c21
Maxime Beauchemin <maximebeauchemin@gmail.com>
2023-06-14 13:49:01 -0700

    cover +DEBUG

--- superset/config.py
+++ superset/config.py
@@ -246,6 +246,7 @@ WTF_CSRF_EXEMPT_LIST = [
 ]
 
 # Whether to run the web server in debug mode or not
+DEBUG = os.environ.get("FLASK_DEBUG")
 FLASK_USE_RELOAD = True
 
 # Enable profiling of Python calls. Turn this on and append ``?_instrument=1``
