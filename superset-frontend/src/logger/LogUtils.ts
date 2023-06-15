fa82ee1947987becc91d0286ca78b0f35ab2ea6d
Vitali Logvin <82170648+vitoldi@users.noreply.github.com>
2023-06-15 14:43:34 +0300

    feat: add dashboard page full xlsx export (#24287)
    Co-authored-by: Vitali Logvin <vitali.logvin@noogadev.com>

--- superset-frontend/src/logger/LogUtils.ts
+++ superset-frontend/src/logger/LogUtils.ts
@@ -35,7 +35,7 @@ export const LOG_ACTIONS_EXPLORE_DASHBOARD_CHART = 'explore_dashboard_chart';
 export const LOG_ACTIONS_EXPORT_CSV_DASHBOARD_CHART =
   'export_csv_dashboard_chart';
 export const LOG_ACTIONS_EXPORT_XLSX_DASHBOARD_CHART =
-  'export_csv_dashboard_chart';
+  'export_xlsx_dashboard_chart';
 export const LOG_ACTIONS_CHANGE_DASHBOARD_FILTER = 'change_dashboard_filter';
 export const LOG_ACTIONS_DATASET_CREATION_EMPTY_CANCELLATION =
   'dataset_creation_empty_cancellation';
