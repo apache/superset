e85c222b8c2bbff6cd3d6a6ce4b972e734f3cd30
Multazim Deshmukh <multazimd@gmail.com>
2023-06-18 15:32:44 +0530

    use sasl with pyhive

--- setup.py
+++ setup.py
@@ -159,7 +159,7 @@ setup(
         "firebolt": ["firebolt-sqlalchemy>=0.0.1"],
         "gsheets": ["shillelagh[gsheetsapi]>=1.0.14, <2"],
         "hana": ["hdbcli==2.4.162", "sqlalchemy_hana==0.4.0"],
-        "hive": ["pyhive[hive]@git+https://github.com/dropbox/PyHive.git@e38eb23fb41ade5991d9fe4507c551962a693951", "tableschema", "thrift>=0.14.1, <1.0.0"],
+        "hive": ["pyhive[hive_pure_sasl]@git+https://github.com/dropbox/PyHive.git@e38eb23fb41ade5991d9fe4507c551962a693951", "tableschema", "thrift>=0.14.1, <1.0.0"],
         "impala": ["impyla>0.16.2, <0.17"],
         "kusto": ["sqlalchemy-kusto>=2.0.0, <3"],
         "kylin": ["kylinpy>=2.8.1, <2.9"],
@@ -183,7 +183,7 @@ setup(
             "shillelagh[datasetteapi,gsheetsapi,socrata,weatherapi]>=1.1.1, <2"
         ],
         "snowflake": ["snowflake-sqlalchemy>=1.2.4, <2"],
-        "spark": ["pyhive[hive]@git+https://github.com/dropbox/PyHive.git@e38eb23fb41ade5991d9fe4507c551962a693951", "tableschema", "thrift>=0.14.1, <1.0.0"],
+        "spark": ["pyhive[hive_pure_sasl]@git+https://github.com/dropbox/PyHive.git@e38eb23fb41ade5991d9fe4507c551962a693951", "tableschema", "thrift>=0.14.1, <1.0.0"],
         "teradata": ["teradatasql>=16.20.0.23"],
         "thumbnails": ["Pillow>=9.5.0, <10.0.0"],
         "vertica": ["sqlalchemy-vertica-python>=0.5.9, < 0.6"],
