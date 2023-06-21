ea89949118a95b75eb47613280d31624c3cee618
Ville Brofeldt <33317356+villebro@users.noreply.github.com>
2023-06-21 22:35:46 +0300

    fix(oracle): denormalize column names where applicable (#24471)

--- superset/connectors/sqla/utils.py
+++ superset/connectors/sqla/utils.py
@@ -67,6 +67,7 @@ def get_physical_table_metadata(
     for col in cols:
         try:
             if isinstance(col["type"], TypeEngine):
+                name = db_engine_spec.denormalize_name(db_dialect, col["column_name"])
                 db_type = db_engine_spec.column_datatype_to_string(
                     col["type"], db_dialect
                 )
@@ -75,6 +76,8 @@ def get_physical_table_metadata(
                 )
                 col.update(
                     {
+                        "name": name,
+                        "column_name": name,
                         "type": db_type,
                         "type_generic": type_spec.generic_type if type_spec else None,
                         "is_dttm": type_spec.is_dttm if type_spec else None,
