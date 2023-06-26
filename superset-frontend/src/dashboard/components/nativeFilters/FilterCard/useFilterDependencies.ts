6d764d940d5ce9a3caa371ef7e089b50359dfaa5
Justin Park <justinpark@apache.org>
2023-06-26 15:18:05 -0700

    fix(native filter): undefined filter dependencies

--- superset-frontend/src/dashboard/components/nativeFilters/FilterCard/useFilterDependencies.ts
+++ superset-frontend/src/dashboard/components/nativeFilters/FilterCard/useFilterDependencies.ts
@@ -27,7 +27,9 @@ export const useFilterDependencies = (filter: Filter) => {
       return [];
     }
     return filterDependencyIds.reduce((acc: Filter[], filterDependencyId) => {
-      acc.push(state.nativeFilters.filters[filterDependencyId] as Filter);
+      if (state.nativeFilters.filters[filterDependencyId]) {
+        acc.push(state.nativeFilters.filters[filterDependencyId] as Filter);
+      }
       return acc;
     }, []);
   });
