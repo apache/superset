a90f740a815828e48672872cfb96bfd7f477de2d
Michael S. Molina <70410625+michael-s-molina@users.noreply.github.com>
2023-06-28 11:40:10 -0300

    fix: Release scripts Python version (#24544)

--- RELEASING/from_tarball_entrypoint.sh
+++ RELEASING/from_tarball_entrypoint.sh
@@ -19,7 +19,10 @@ set -ex
 
 echo "[WARNING] this entrypoint creates an admin/admin user"
 echo "[WARNING] it should only be used for lightweight testing/validation"
-if $SUPERSET_TESTENV then echo "SUPERSET IS RUNNING IN TEST MODE"
+
+if [ -z "${SUPERSET_TESTENV}" ]; then
+  echo "SUPERSET IS RUNNING IN TEST MODE"
+fi
 
 # Create an admin user (you will be prompted to set username, first and last name before setting a password)
 superset fab create-admin \
