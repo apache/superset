c34b0845927949ffc5fe475e0f5ab4cde1f69831
Michael S. Molina <70410625+michael-s-molina@users.noreply.github.com>
2023-06-28 13:35:44 -0300

    fix: Revert enable strong session protection by default (#24256) (#24545)

--- superset/config.py
+++ superset/config.py
@@ -1380,8 +1380,6 @@ TALISMAN_CONFIG = {
 SESSION_COOKIE_HTTPONLY = True  # Prevent cookie from being read by frontend JS?
 SESSION_COOKIE_SECURE = False  # Prevent cookie from being transmitted over non-tls?
 SESSION_COOKIE_SAMESITE: Literal["None", "Lax", "Strict"] | None = "Lax"
-# Accepts None, "basic" and "strong", more details on: https://flask-login.readthedocs.io/en/latest/#session-protection
-SESSION_PROTECTION = "strong"
 
 # Cache static resources.
 SEND_FILE_MAX_AGE_DEFAULT = int(timedelta(days=365).total_seconds())
