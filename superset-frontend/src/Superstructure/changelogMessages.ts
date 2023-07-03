39ad883249e5dcf85c7a03d820bef0ec572613b3
liamtorrelli <atleast24.7@list.ru>
2023-07-03 12:12:45 +0800

    Added translations

--- superset-frontend/src/Superstructure/changelogMessages.ts
+++ superset-frontend/src/Superstructure/changelogMessages.ts
@@ -104,16 +104,24 @@ const NEW_FEATURES_MAY_2023_RU = {
 
 const NEW_FEATURES_JUNE_2023_RU = {
   title: 'Обновления [Июнь 2023]',
-  date: '29.06.2023',
+  date: '30.06.2023',
   subTitle: RELEASE_IS_STABLE,
   extra: IF_QUESTIONS_RU,
   listTitle: 'Новая функциональность / исправлены проблемы',
   releases: [
     {
-      date: `${TESTING_EMOJI} 29.06.2023`,
-      status: RELEASE_IN_TESTING,
+      date: `${DONE_EMOJI} 30.06.2023`,
+      status: RELEASE_IS_STABLE,
       messages: [
-        'Доделали переводы на русский язык в плагине во Вкладке Аналитика в DODOIS',
+        `${EXTRA_EMOJI} Добавили 2 вида форматирования: "С пробелом" и "С пробелом округл."`,
+        'Обновили недостающие переводы в разных частях системы',
+      ],
+    },
+    {
+      date: `${DONE_EMOJI} 29.06.2023`,
+      status: RELEASE_IS_STABLE,
+      messages: [
+        '80% Доделали переводы на русский язык в плагине во Вкладке Аналитика в DODOIS',
         'Исправили переводы на русский язык в Standalone на меню в графике',
       ],
     },
@@ -176,7 +184,8 @@ const NEW_FEATURES_JUNE_2023_RU = {
   messagesExtra: [
     `${DONE_EMOJI} Добавить все необходимые валюты в форматирование`,
     `${DONE_EMOJI} Улучшить и локализовать форматирование чисел`,
-    `${DONE_EMOJI} Форматирование чисел через пробел`,
+    `${DONE_EMOJI} Форматирование чисел через пробел без округления`,
+    `${DONE_EMOJI} Форматирование чисел через пробел с округлением ${EXTRA_EMOJI}`,
     `${DONE_EMOJI} Использовать локализацию из DODOIS во вкладке Аналитика ${EXTRA_EMOJI}`,
     `${NOT_DONE_EMOJI} Доработать Pivot Table v2: возможность сортировки столбцов`,
     `${DONE_EMOJI} Упростить выбор дат в фильтрах`,
--- superset-frontend/src/filters/components/TimeGrain/TimeGrainFilterPlugin.tsx
+++ superset-frontend/src/filters/components/TimeGrain/TimeGrainFilterPlugin.tsx
@@ -1,21 +1,4 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+// DODO was here
 import {
   ensureIsArray,
   ExtraFormData,
@@ -106,7 +89,8 @@ export default function PluginFilterTimegrain(
     (row: { name: string; duration: string }) => {
       const { name, duration } = row;
       return {
-        label: name,
+        // DODO changed
+        label: t(name),
         value: duration,
       };
     },
