04157d4010f9b44927f7683abffa85817fc4c384
SamraHanifCareem <samra.hanif@careem.com>
2023-07-06 18:28:02 +0500

    fix: reverted code cov

--- .github/workflows/codecov.sh
+++ .github/workflows/codecov.sh
@@ -1826,77 +1826,77 @@ else
   # now add token to query
   query=$(echo "package=$package-$VERSION&token=$token&$query" | tr -d ' ')
 
-  # if [ "$ft_s3" = "1" ];
-  # then
-  #   say "${e}->${x}  Pinging Codecov"
-  #   say "$url/upload/v4?$queryNoToken"
-  #   # shellcheck disable=SC2086,2090
-  #   res=$(curl $curl_s -X POST $cacert \
-  #         --retry 5 --retry-delay 2 --connect-timeout 2 \
-  #         -H 'X-Reduced-Redundancy: false' \
-  #         -H 'X-Content-Type: application/x-gzip' \
-  #         -H 'Content-Length: 0' \
-  #         --write-out "\n%{response_code}\n" \
-  #         $curlargs \
-  #         "$url/upload/v4?$query" || true)
-  #   # a good reply is "https://codecov.io" + "\n" + "https://storage.googleapis.com/codecov/..."
-  #   s3target=$(echo "$res" | sed -n 2p)
-  #   status=$(tail -n1 <<< "$res")
+  if [ "$ft_s3" = "1" ];
+  then
+    say "${e}->${x}  Pinging Codecov"
+    say "$url/upload/v4?$queryNoToken"
+    # shellcheck disable=SC2086,2090
+    res=$(curl $curl_s -X POST $cacert \
+          --retry 5 --retry-delay 2 --connect-timeout 2 \
+          -H 'X-Reduced-Redundancy: false' \
+          -H 'X-Content-Type: application/x-gzip' \
+          -H 'Content-Length: 0' \
+          --write-out "\n%{response_code}\n" \
+          $curlargs \
+          "$url/upload/v4?$query" || true)
+    # a good reply is "https://codecov.io" + "\n" + "https://storage.googleapis.com/codecov/..."
+    s3target=$(echo "$res" | sed -n 2p)
+    status=$(tail -n1 <<< "$res")
 
-  #   if [ "$status" = "200" ] && [ "$s3target" != "" ];
-  #   then
-  #     say "${e}->${x}  Uploading to"
-  #     say "${s3target}"
+    if [ "$status" = "200" ] && [ "$s3target" != "" ];
+    then
+      say "${e}->${x}  Uploading to"
+      say "${s3target}"
 
-  #     # shellcheck disable=SC2086
-  #     s3=$(curl -fiX PUT \
-  #         --data-binary @"$upload_file.gz" \
-  #         -H 'Content-Type: application/x-gzip' \
-  #         -H 'Content-Encoding: gzip' \
-  #         $curlawsargs \
-  #         "$s3target" || true)
+      # shellcheck disable=SC2086
+      s3=$(curl -fiX PUT \
+          --data-binary @"$upload_file.gz" \
+          -H 'Content-Type: application/x-gzip' \
+          -H 'Content-Encoding: gzip' \
+          $curlawsargs \
+          "$s3target" || true)
 
-  #     if [ "$s3" != "" ];
-  #     then
-  #       say "    ${g}->${x} Reports have been successfully queued for processing at ${b}$(echo "$res" | sed -n 1p)${x}"
-  #       exit 0
-  #     else
-  #       say "    ${r}X>${x} Failed to upload"
-  #     fi
-  #   elif [ "$status" = "400" ];
-  #   then
-  #       # 400 Error
-  #       say "${r}${res}${x}"
-  #       exit ${exit_with}
-  #   else
-  #       say "${r}${res}${x}"
-  #   fi
-  # fi
+      if [ "$s3" != "" ];
+      then
+        say "    ${g}->${x} Reports have been successfully queued for processing at ${b}$(echo "$res" | sed -n 1p)${x}"
+        exit 0
+      else
+        say "    ${r}X>${x} Failed to upload"
+      fi
+    elif [ "$status" = "400" ];
+    then
+        # 400 Error
+        say "${r}${res}${x}"
+        exit ${exit_with}
+    else
+        say "${r}${res}${x}"
+    fi
+  fi
 
-  # say "${e}==>${x} Uploading to Codecov"
+  say "${e}==>${x} Uploading to Codecov"
 
-  # # shellcheck disable=SC2086,2090
-  # res=$(curl -X POST $cacert \
-  #       --data-binary @"$upload_file.gz" \
-  #       --retry 5 --retry-delay 2 --connect-timeout 2 \
-  #       -H 'Content-Type: text/plain' \
-  #       -H 'Content-Encoding: gzip' \
-  #       -H 'X-Content-Encoding: gzip' \
-  #       -H 'Accept: text/plain' \
-  #       $curlargs \
-  #       "$url/upload/v2?$query&attempt=$i" || echo 'HTTP 500')
-  # # HTTP 200
-  # # http://....
-  # status=$(echo "$res" | head -1 | cut -d' ' -f2)
-  # if [ "$status" = "" ] || [ "$status" = "200" ];
-  # then
-  #   say "    Reports have been successfully queued for processing at ${b}$(echo "$res" | head -2 | tail -1)${x}"
-  #   exit 0
-  # else
-  #   say "    ${g}${res}${x}"
-  #   exit ${exit_with}
-  # fi
+  # shellcheck disable=SC2086,2090
+  res=$(curl -X POST $cacert \
+        --data-binary @"$upload_file.gz" \
+        --retry 5 --retry-delay 2 --connect-timeout 2 \
+        -H 'Content-Type: text/plain' \
+        -H 'Content-Encoding: gzip' \
+        -H 'X-Content-Encoding: gzip' \
+        -H 'Accept: text/plain' \
+        $curlargs \
+        "$url/upload/v2?$query&attempt=$i" || echo 'HTTP 500')
+  # HTTP 200
+  # http://....
+  status=$(echo "$res" | head -1 | cut -d' ' -f2)
+  if [ "$status" = "" ] || [ "$status" = "200" ];
+  then
+    say "    Reports have been successfully queued for processing at ${b}$(echo "$res" | head -2 | tail -1)${x}"
+    exit 0
+  else
+    say "    ${g}${res}${x}"
+    exit ${exit_with}
+  fi
 
-  # say "    ${r}X> Failed to upload coverage reports${x}"
+  say "    ${r}X> Failed to upload coverage reports${x}"
 fi
 exit ${exit_with}
