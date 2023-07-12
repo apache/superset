2cbdbc006143dcbe6f07711eca083e2aa81d8f02
Arkadii Yakovets <ark@cho.red>
2023-07-12 08:19:00 -0700

    Change upper bound from '<0.29' to '<1.0'

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ setup(
         "geopy",
         "gunicorn>=20.1.0; sys_platform != 'win32'",
         "hashids>=1.3.1, <2",
-        "holidays>=0.28, <0.29",
+        "holidays>=0.28, <1.0",
         "humanize",
         "importlib_metadata",
         "isodate",
