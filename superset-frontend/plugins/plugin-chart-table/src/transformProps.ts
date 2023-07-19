47c2599fd4d981c55231099fd387332f3502abbb
boyan <wangpeng.cosmo@haier.com>
2023-07-19 14:10:39 +0800

    <initial commit>

--- superset-frontend/plugins/plugin-chart-table/src/transformProps.ts
+++ superset-frontend/plugins/plugin-chart-table/src/transformProps.ts
@@ -71,9 +71,7 @@ const processDataRecords = memoizeOne(function processDataRecords(
       timeColumns.forEach(({ key, formatter }) => {
         // Convert datetime with a custom date class so we can use `String(...)`
         // formatted value for global search, and `date.getTime()` for sorting.
-        datum[key] = new DateWithFormatter(
-          parseInt(String(x[key]?.toString()), 10),
-          {
+        datum[key] = new DateWithFormatter(x[key], {
             formatter: formatter as TimeFormatter,
           },
         );
