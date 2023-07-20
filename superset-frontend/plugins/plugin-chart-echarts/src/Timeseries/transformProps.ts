ea783f8a2aeb4096b101d973c0a34145e2ad9d83
Michael S. Molina <michael.s.molina@gmail.com>
2023-07-20 16:27:51 -0300

    Apply review suggestion

--- superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/transformProps.ts
+++ superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/transformProps.ts
@@ -526,11 +526,9 @@ export default function transformProps(
                 defaultFormatter,
           });
           const { focusedSeries } = ownState;
-          if (key === focusedSeries) {
-            rows.push(`<span style="font-weight: 700">${content}</span>`);
-          } else {
-            rows.push(`<span style="opacity: 0.7">${content}</span>`);
-          }
+          const contentStyle =
+            key === focusedSeries ? 'font-weight: 700' : 'opacity: 0.7';
+          rows.push(`<span style="${contentStyle}">${content}</span>`);
         });
         if (stack) {
           rows.reverse();
