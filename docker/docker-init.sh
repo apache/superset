45d856c4caf5f7a5ce70cb016c16f1f33a094891
Rene Heijdens <rene.heijdens@x-ip.nl>
2023-07-20 12:09:19 +0200

    Revert init docker container and changed docker-init.sh

--- docker/docker-init.sh
+++ docker/docker-init.sh
@@ -49,30 +49,30 @@ fi
 echo_step "1" "Starting" "Applying DB migrations"
 superset db upgrade
 echo_step "1" "Complete" "Applying DB migrations"
-
-# Create an admin user
-echo_step "2" "Starting" "Setting up admin user ( admin / $ADMIN_PASSWORD )"
-superset fab create-admin \
-              --username admin \
-              --firstname Superset \
-              --lastname Admin \
-              --email admin@superset.com \
-              --password $ADMIN_PASSWORD
-echo_step "2" "Complete" "Setting up admin user"
-# Create default roles and permissions
-echo_step "3" "Starting" "Setting up roles and perms"
-superset init
-echo_step "3" "Complete" "Setting up roles and perms"
-
-if [ "$SUPERSET_LOAD_EXAMPLES" = "yes" ]; then
-    # Load some data to play with
-    echo_step "4" "Starting" "Loading examples"
-    # If Cypress run which consumes superset_test_config – load required data for tests
-    if [ "$CYPRESS_CONFIG" == "true" ]; then
-        superset load_test_users
-        superset load_examples --load-test-data
-    else
-        superset load_examples --force
-    fi
-    echo_step "4" "Complete" "Loading examples"
-fi
+#
+## Create an admin user
+#echo_step "2" "Starting" "Setting up admin user ( admin / $ADMIN_PASSWORD )"
+#superset fab create-admin \
+#              --username admin \
+#              --firstname Superset \
+#              --lastname Admin \
+#              --email admin@superset.com \
+#              --password $ADMIN_PASSWORD
+#echo_step "2" "Complete" "Setting up admin user"
+## Create default roles and permissions
+#echo_step "3" "Starting" "Setting up roles and perms"
+#superset init
+#echo_step "3" "Complete" "Setting up roles and perms"
+#
+#if [ "$SUPERSET_LOAD_EXAMPLES" = "yes" ]; then
+#    # Load some data to play with
+#    echo_step "4" "Starting" "Loading examples"
+#    # If Cypress run which consumes superset_test_config – load required data for tests
+#    if [ "$CYPRESS_CONFIG" == "true" ]; then
+#        superset load_test_users
+#        superset load_examples --load-test-data
+#    else
+#        superset load_examples --force
+#    fi
+#    echo_step "4" "Complete" "Loading examples"
+#fi
