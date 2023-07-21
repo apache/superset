c8f5712b16a546788e01f2f16baaac23ae392ee4
Ethan Lewis <ethanlewis787@gmail.com>
2023-07-21 10:17:02 -0700

    fix: update import order based on pylint

--- superset/db_engine_specs/cockroachdb.py
+++ superset/db_engine_specs/cockroachdb.py
@@ -14,12 +14,12 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-from superset.db_engine_specs.postgres import PostgresEngineSpec
-
 from datetime import datetime
 from typing import Any, Optional
 
 from sqlalchemy import types
+
+from superset.db_engine_specs.postgres import PostgresEngineSpec
 
 class CockroachDbEngineSpec(PostgresEngineSpec):
     engine = "cockroachdb"
