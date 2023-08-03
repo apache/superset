af8fefc63cc1a41f0cbed6d462c8e204c3cf2894
Jack Fragassi <jfragassi98@gmail.com>
2023-08-03 13:03:12 -0700

    Fix tests

--- tests/integration_tests/migrations/0769ef90fddd_fix_schema_perm_for_datasets__tests.py
+++ tests/integration_tests/migrations/0769ef90fddd_fix_schema_perm_for_datasets__tests.py
@@ -21,6 +21,7 @@ import pytest
 from superset import db
 from superset.connectors.sqla.models import SqlaTable
 from superset.models.slice import Slice
+from superset.utils.core import backend, get_example_default_schema
 from tests.integration_tests.fixtures.birth_names_dashboard import (
     load_birth_names_dashboard_with_slices,
     load_birth_names_data,
@@ -37,6 +38,9 @@ fix_charts_schema_perm = migration_module.fix_charts_schema_perm
 
 @pytest.mark.usefixtures("load_birth_names_dashboard_with_slices")
 def test_fix_schema_perm():
+    if backend() == "sqlite":
+        return
+
     dataset = db.session.query(SqlaTable).filter_by(table_name="birth_names").one()
     chart = db.session.query(Slice).filter_by(slice_name="Girls").one()
     dataset.schema_perm = "wrong"
@@ -45,8 +49,8 @@ def test_fix_schema_perm():
 
     fix_datasets_schema_perm(db.session)
     db.session.commit()
-    assert dataset.schema_perm == "[examples].[public]"
+    assert dataset.schema_perm == f"[examples].[{get_example_default_schema()}]"
 
     fix_charts_schema_perm(db.session)
     db.session.commit()
-    assert chart.schema_perm == "[examples].[public]"
+    assert chart.schema_perm == f"[examples].[{get_example_default_schema()}]"
