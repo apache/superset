d1e28bfadd37e0ddced1137d8c425396fdc351fc
Jack Fragassi <jfragassi98@gmail.com>
2023-08-03 09:57:18 -0700

    Migration for fixing schema_perm

---
+++ superset/migrations/versions/2023-08-02_15-23_0769ef90fddd_fix_schema_perm_for_datasets.py
@@ -0,0 +1,76 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""Fix schema perm for datasets
+
+Revision ID: 0769ef90fddd
+Revises: ee179a490af9
+Create Date: 2023-08-02 15:23:58.242396
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "0769ef90fddd"
+down_revision = "ee179a490af9"
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.ext.declarative import declarative_base
+
+from superset import db
+
+Base = declarative_base()
+
+
+class SqlaTable(Base):
+    __tablename__ = "tables"
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    schema = sa.Column(sa.String(255))
+    schema_perm = sa.Column(sa.String(1000))
+    database_id = sa.Column(sa.Integer, sa.ForeignKey("dbs.id"))
+
+
+class Database(Base):
+    __tablename__ = "dbs"
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    database_name = sa.Column(sa.String(250))
+
+
+def upgrade():
+    bind = op.get_bind()
+    session = db.Session(bind=bind)
+
+    for result in (
+        session.query(SqlaTable, Database.database_name)
+        .join(Database)
+        .filter(SqlaTable.schema.isnot(None))
+        .filter(
+            SqlaTable.schema_perm
+            != sa.func.concat("[", Database.database_name, "].[", SqlaTable.schema, "]")
+        )
+    ):
+        result.SqlaTable.schema_perm = (
+            f"[{result.database_name}].[{result.SqlaTable.schema}]"
+        )
+
+    session.commit()
+    session.close()
+
+
+def downgrade():
+    pass
