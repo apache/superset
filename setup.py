a010791c95ba96433d14fdc5f2f07f42ad7e7883
artemaslamov <aslamov@promsoft.ru>
2023-08-07 18:00:06 +0800

    other: add package

--- setup.py
+++ setup.py
@@ -116,6 +116,7 @@ setup(
         # needed to support Literal (3.8) and TypeGuard (3.10)
         "typing-extensions>=3.10, <4",
         "wtforms-json",
+        "xlsxwriter==3.1.2"
     ],
     extras_require={
         "athena": ["pyathena>=1.10.8, <1.11"],
