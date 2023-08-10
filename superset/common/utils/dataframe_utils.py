1e35d4ead06fb91e0839ed5807999ee5e970a19e
artemaslamov <aslamov@promsoft.ru>
2023-08-11 00:28:51 +0800

    other: deleted timezone

--- superset/common/utils/dataframe_utils.py
+++ superset/common/utils/dataframe_utils.py
@@ -37,8 +37,8 @@ def delete_tz_from_df(d: dict) -> pd.DataFrame:
         for k, type_col in enumerate(coltypes):
             if type_col == GenericDataType.TEMPORAL:
                 name_col = colnames[k]
-                df[name_col] = pd.to_datetime(df[name_col])
-                df[name_col].dt.tz_localize(None)
+                df[name_col] = pd.to_datetime(df[name_col], utc=True)
+                df[name_col] = df[name_col].dt.tz_localize(None)
 
         return df
     return pd.DataFrame(data)
