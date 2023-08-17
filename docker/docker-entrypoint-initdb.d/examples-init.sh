269c99293f42089958dc98b5d6e5899509fc3111
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2023-08-17 11:50:24 +0100

    chore: isolate examples database by default (#25003)

---
+++ docker/docker-entrypoint-initdb.d/examples-init.sh
@@ -0,0 +1,15 @@
+# ------------------------------------------------------------------------
+# Creates the examples database and repective user. This database location
+# and access credentials are defined on the environment variables
+# ------------------------------------------------------------------------
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" <<-EOSQL
+  CREATE USER ${EXAMPLES_USER} WITH PASSWORD '${EXAMPLES_PASSWORD}';
+  CREATE DATABASE ${EXAMPLES_DB};
+  GRANT ALL PRIVILEGES ON DATABASE ${EXAMPLES_DB} TO ${EXAMPLES_USER};
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" -d "${EXAMPLES_DB}" <<-EOSQL
+   GRANT ALL ON SCHEMA public TO ${EXAMPLES_USER};
+EOSQL
