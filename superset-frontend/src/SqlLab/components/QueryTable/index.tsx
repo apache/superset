c8873c0e6f5c3d4a9825a11c2d9ebd0c7ec10e7a
justinpark <justin.hojun.park@gmail.com>
2023-08-31 10:08:18 -0700

    add localized date value

--- superset-frontend/src/SqlLab/components/QueryTable/index.tsx
+++ superset-frontend/src/SqlLab/components/QueryTable/index.tsx
@@ -213,7 +213,7 @@ const QueryTable = ({
             {q.db}
           </Button>
         );
-        q.started = moment(q.startDttm).format('HH:mm:ss');
+        q.started = moment(q.startDttm).format('ll HH:mm:ss');
         q.querylink = (
           <Button
             buttonSize="small"
