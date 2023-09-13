0061c66c11ae328cbf58ddabbdc48229091cc555
justinpark <justin.hojun.park@gmail.com>
2023-09-12 19:30:08 -0700

    method name

--- superset/views/sqllab.py
+++ superset/views/sqllab.py
@@ -42,5 +42,5 @@ class SqllabView(BaseSupersetView):
     @has_access
     @permission_name("read")
     @event_logger.log_this
-    def root(self) -> FlaskResponse:
+    def history(self) -> FlaskResponse:
         return self.render_app_template()
