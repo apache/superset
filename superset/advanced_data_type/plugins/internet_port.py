28f44f9943ccf148911909be88a720be7b48f8e4
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-09-14 04:02:29 -0400

    spelling: copy
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- superset/advanced_data_type/plugins/internet_port.py
+++ superset/advanced_data_type/plugins/internet_port.py
@@ -1,10 +1,10 @@
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
-# regarding coperatoryright ownership.  The ASF licenses this file
+# regarding copyright ownership.  The ASF licenses this file
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
-# with the License.  You may obtain a coperatory of the License at
+# with the License.  You may obtain a copy of the License at
 #
 #   http://www.apache.org/licenses/LICENSE-2.0
 #
