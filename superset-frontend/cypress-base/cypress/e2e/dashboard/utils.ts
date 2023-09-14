b22e57738c81ae4d2cad63e6e32c290d8de02c44
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-09-14 08:20:35 -0400

    spelling: collapses
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- superset-frontend/cypress-base/cypress/e2e/dashboard/utils.ts
+++ superset-frontend/cypress-base/cypress/e2e/dashboard/utils.ts
@@ -204,7 +204,7 @@ export function expandFilterOnLeftPanel() {
 }
 
 /** ************************************************************************
- * Collapes Native Filter from the left panel on dashboard
+ * Collapses Native Filter from the left panel on dashboard
  * @returns {None}
  * @summary helper for collapse native filter
  ************************************************************************* */
