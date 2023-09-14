a707cf87ca3c61145a6b0d790f455a96a13252cf
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-09-14 04:25:53 -0400

    spelling: from-a
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- tests/integration_tests/databases/api_tests.py
+++ tests/integration_tests/databases/api_tests.py
@@ -3633,7 +3633,7 @@ class TestDatabaseApi(SupersetTestCase):
         Database API: validate SQL with errors
         """
         request_payload = {
-            "sql": "SELECT col1 froma table1",
+            "sql": "SELECT col1 fromA table1",
             "schema": None,
             "template_params": None,
         }
