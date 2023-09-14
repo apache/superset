7e3442ba7dd01cd61375c07cc476aceef3b6e222
justinpark <justin.hojun.park@gmail.com>
2023-09-14 13:49:50 -0700

    separate test spec

--- tests/integration_tests/sql_lab/api_tests.py
+++ tests/integration_tests/sql_lab/api_tests.py
@@ -52,6 +52,22 @@ class TestSqlLabApi(SupersetTestCase):
     )
     def test_get_from_empty_bootsrap_data(self):
         self.login(username="gamma_sqllab_no_data")
+        resp = self.client.get("/api/v1/sqllab/")
+        assert resp.status_code == 200
+        data = json.loads(resp.data.decode("utf-8"))
+        result = data.get("result")
+        assert result["active_tab"] == None
+        assert result["queries"] == {}
+        assert result["tab_state_ids"] == []
+        self.assertEqual(len(result["databases"]), 0)
+
+    @mock.patch.dict(
+        "superset.extensions.feature_flag_manager._feature_flags",
+        {"SQLLAB_BACKEND_PERSISTENCE": False},
+        clear=True,
+    )
+    def test_get_from_bootstrap_data_for_non_persisted_tab_state(self):
+        self.login("admin")
         # create a tab
         data = {
             "queryEditor": json.dumps(
