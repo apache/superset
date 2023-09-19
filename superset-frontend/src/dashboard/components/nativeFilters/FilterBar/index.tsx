1e708ce9015595d64c46e37086b422d59e81caa4
Stepan <66589759+Always-prog@users.noreply.github.com>
2023-09-19 10:08:52 +0300

    update dataMaskSelectedRef sync logic
    Co-authored-by: JUST.in DO IT <justin.park@airbnb.com>

--- superset-frontend/src/dashboard/components/nativeFilters/FilterBar/index.tsx
+++ superset-frontend/src/dashboard/components/nativeFilters/FilterBar/index.tsx
@@ -151,6 +151,7 @@ const FilterBar: React.FC<FiltersBarProps> = ({
   const [filtersInScope] = useSelectFiltersInScope(nativeFilterValues);
 
   const dataMaskSelectedRef = useRef(dataMaskSelected);
+  dataMaskSelectedRef.current = dataMaskSelected;
   const handleFilterSelectionChange = useCallback(
     (
       filter: Pick<Filter, 'id'> & Partial<Filter>,
