0c6db230afb42bea786cf939b0eb7a16c2f40893
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2023-09-27 08:55:29 +0100

    fix: swagger UI CSP error (#25368)
    (cherry picked from commit 1716b9f8f68c7abe4c1a082e11ccdb26dbe6a3db)

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
         "cryptography>=39.0.1, <40",
         "deprecation>=2.1.0, <2.2.0",
         "flask>=2.2.5, <3.0.0",
-        "flask-appbuilder>=4.3.6, <5.0.0",
+        "flask-appbuilder>=4.3.7, <5.0.0",
         "flask-caching>=1.11.1, <2.0",
         "flask-compress>=1.13, <2.0",
         "flask-talisman>=1.0.0, <2.0",
