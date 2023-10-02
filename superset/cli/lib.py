72fc19033679d213287b9051e05911738dba475a
Craig <craig@craigrueda.com>
2023-10-02 09:37:12 -0700

    Linting stuff I didn't change again

--- superset/cli/lib.py
+++ superset/cli/lib.py
@@ -16,7 +16,6 @@
 # specific language governing permissions and limitations
 # under the License.
 import logging
-from typing import Callable, Any
 
 from superset import config
 
@@ -25,9 +24,10 @@ logger = logging.getLogger(__name__)
 
 feature_flags = config.DEFAULT_FEATURE_FLAGS.copy()
 feature_flags.update(config.FEATURE_FLAGS)
-feature_flags_func: Callable[[Any], Any] = config.GET_FEATURE_FLAGS_FUNC
+feature_flags_func = config.GET_FEATURE_FLAGS_FUNC
 if feature_flags_func:
     try:
+        # pylint: disable=not-callable
         feature_flags = feature_flags_func(feature_flags)
     except Exception:  # pylint: disable=broad-except
         # bypass any feature flags that depend on context
