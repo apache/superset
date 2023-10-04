db9590c2f441ad4aaa773477a584a08526f04b82
Hugh A. Miles II <hughmil3s@gmail.com>
2023-10-04 23:51:05 +0000

    update the modal

--- superset-frontend/src/features/tags/BulkTagModal.tsx
+++ superset-frontend/src/features/tags/BulkTagModal.tsx
@@ -67,7 +67,18 @@ const BulkTagModal: React.FC<BulkTagModalProps> = ({
       },
     })
       .then(({ json = {} }) => {
-        addSuccessToast(t('Tagged %s %ss', selected.length, resourceName));
+        const skipped = json.objects_tagged;
+        const tagged = json.objects_skipped;
+        if (skipped.length > 0) {
+          addSuccessToast(
+            t(
+              '%s items could not be tagged because you don’t have edit permissions to all selected objects.',
+              skipped.length,
+              resourceName,
+            ),
+          );
+        }
+        addSuccessToast(t('Tagged %s %ss', tagged.length, resourceName));
       })
       .catch(err => {
         addDangerToast(t('Failed to tag items'));
