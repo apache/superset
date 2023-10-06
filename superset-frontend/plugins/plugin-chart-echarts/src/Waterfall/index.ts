fdbe874d3ccf08d6a407144f796fcdc3752e4e5c
Michael S. Molina <michael.s.molina@gmail.com>
2023-10-06 11:27:24 -0300

    feat: Improves the Waterfall chart

--- superset-frontend/plugins/plugin-chart-echarts/src/Waterfall/index.ts
+++ superset-frontend/plugins/plugin-chart-echarts/src/Waterfall/index.ts
@@ -47,7 +47,11 @@ export default class EchartsWaterfallChartPlugin extends ChartPlugin<
         behaviors: [Behavior.INTERACTIVE_CHART, Behavior.DRILL_TO_DETAIL],
         credits: ['https://echarts.apache.org'],
         category: t('Evolution'),
-        description: '',
+        description: t(
+          `A waterfall chart is a form of data visualization that helps in understanding
+          the cumulative effect of sequentially introduced positive or negative values.
+          These intermediate values can either be time based or category based.`,
+        ),
         exampleGallery: [],
         name: t('Waterfall Chart'),
         thumbnail,
