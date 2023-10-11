732c5b1f0808a1eef95c0faaa0e8fc0366cae84b
Corbin Bullard <corbindbullard@gmail.com>
2023-10-11 13:31:21 -0400

    fix(Charts): Set max row limit + removed the option to use an empty row limit value (#25579)
    (cherry picked from commit f556ef53f3177746ec2526b4b963da4ef00c2d58)

--- superset-frontend/packages/superset-ui-core/test/validator/validateMaxValue.test.ts
+++ superset-frontend/packages/superset-ui-core/test/validator/validateMaxValue.test.ts
@@ -17,9 +17,22 @@
  * under the License.
  */
 
-export { default as legacyValidateInteger } from './legacyValidateInteger';
-export { default as legacyValidateNumber } from './legacyValidateNumber';
-export { default as validateInteger } from './validateInteger';
-export { default as validateNumber } from './validateNumber';
-export { default as validateNonEmpty } from './validateNonEmpty';
-export { default as validateMaxValue } from './validateMaxValue';
+import { validateMaxValue } from '@superset-ui/core';
+import './setup';
+
+describe('validateInteger()', () => {
+  it('returns the warning message if invalid', () => {
+    expect(validateMaxValue(10.1, 10)).toBeTruthy();
+    expect(validateMaxValue(1, 0)).toBeTruthy();
+    expect(validateMaxValue('2', 1)).toBeTruthy();
+  });
+  it('returns false if the input is valid', () => {
+    expect(validateMaxValue(0, 1)).toBeFalsy();
+    expect(validateMaxValue(10, 10)).toBeFalsy();
+    expect(validateMaxValue(undefined, 1)).toBeFalsy();
+    expect(validateMaxValue(NaN, NaN)).toBeFalsy();
+    expect(validateMaxValue(null, 1)).toBeFalsy();
+    expect(validateMaxValue('1', 1)).toBeFalsy();
+    expect(validateMaxValue('a', 1)).toBeFalsy();
+  });
+});
