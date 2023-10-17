73e71b472fc5a94616d5f53e3713ef8d0af25d5d
Mikel Vuka <mikel.vuka@zalando.de>
2023-10-17 23:17:31 +0200

    feat: remove unused imports from test

--- tests/integration_tests/db_engine_specs/elasticsearch_tests.py
+++ tests/integration_tests/db_engine_specs/elasticsearch_tests.py
@@ -14,22 +14,9 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-import unittest.mock as mock
-
-import pytest
-from pandas import DataFrame
 from sqlalchemy import column
-
-from superset.connectors.sqla.models import TableColumn
-from superset.db_engine_specs.base import BaseEngineSpec
 from superset.db_engine_specs.elasticsearch import ElasticSearchEngineSpec
-from superset.errors import ErrorLevel, SupersetError, SupersetErrorType
-from superset.sql_parse import Table
 from tests.integration_tests.db_engine_specs.base_tests import TestDbEngineSpec
-from tests.integration_tests.fixtures.birth_names_dashboard import (
-    load_birth_names_dashboard_with_slices,
-    load_birth_names_data,
-)
 
 
 class TestElasticsearchDbEngineSpec(TestDbEngineSpec):
