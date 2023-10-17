bd6b1c6aff5cd0da336211688f585aa4b73cb63d
Anmol Majithia <majithiaanmol12@gmail.com>
2023-10-17 14:45:07 -0400

    [IPLT-4231][fix][26] linter issue

--- superset-frontend/src/dashboard/components/gridComponents/IkiEitlColumn.jsx
+++ superset-frontend/src/dashboard/components/gridComponents/IkiEitlColumn.jsx
@@ -37,7 +37,6 @@ import { MarkdownEditor } from 'src/components/AsyncAceEditor';
 
 import DeleteComponentButton from 'src/dashboard/components/DeleteComponentButton';
 import DragDroppable from 'src/dashboard/components/dnd/DragDroppable';
-import HoverMenu from 'src/dashboard/components/menu/HoverMenu';
 import ResizableContainer from 'src/dashboard/components/resizable/ResizableContainer';
 import MarkdownModeDropdown from 'src/dashboard/components/menu/MarkdownModeDropdown';
 import WithPopoverMenu from 'src/dashboard/components/menu/WithPopoverMenu';
