315e75811f5dbbc33ac1220bd95277cfc89465ed
Igor Khrol <igor.khrol@automattic.com>
2023-10-19 21:03:44 +0300

    fix: remove unnecessary redirect (#25679)
    (cherry picked from commit da42bf2dbb82a40d5ffcc9bfdc46584cb36af616)

--- superset-frontend/src/SqlLab/components/SaveDatasetModal/index.tsx
+++ superset-frontend/src/SqlLab/components/SaveDatasetModal/index.tsx
@@ -257,7 +257,7 @@ export const SaveDatasetModal = ({
       });
 
       return SupersetClient.get({
-        endpoint: `/api/v1/dataset?q=${queryParams}`,
+        endpoint: `/api/v1/dataset/?q=${queryParams}`,
       }).then(response => ({
         data: response.json.result.map(
           (r: { table_name: string; id: number; owners: [DatasetOwner] }) => ({
