8da27eda4059202438cbfaea511f71dea828afdf
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2023-10-20 11:33:40 +0100

    fix: bump to FAB 4.3.9 remove CSP exception (#25712)
    (cherry picked from commit 8fb0c8da56f572c086126cc5ca16676ce74e7a3c)

--- superset/config.py
+++ superset/config.py
@@ -1421,7 +1421,6 @@ TALISMAN_CONFIG = {
         "style-src": [
             "'self'",
             "'unsafe-inline'",
-            "https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui.css",
         ],
         "script-src": ["'self'", "'strict-dynamic'"],
     },
@@ -1443,7 +1442,6 @@ TALISMAN_DEV_CONFIG = {
         "style-src": [
             "'self'",
             "'unsafe-inline'",
-            "https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui.css",
         ],
         "script-src": ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
     },
