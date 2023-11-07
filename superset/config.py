2ab8963f95d61d204ea4ebb583bfc3b5e762319c
Ravikumar911 <ravikumar.rajagopalan@cleartax.in>
2023-11-07 18:29:54 +0530

    change-config

--- superset/config.py
+++ superset/config.py
@@ -1461,7 +1461,6 @@ TALISMAN_DEV_CONFIG = {
         ],
         "script-src": ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
     },
-    "content_security_policy_nonce_in": ["script-src"],
     "force_https": False,
 }
 
