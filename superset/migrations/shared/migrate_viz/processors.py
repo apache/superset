5f5951543d5e7c44aec9c557c20710e1dc28606f
Michael S. Molina <michael.s.molina@gmail.com>
2023-11-10 14:29:18 -0300

    feat: Adds Area chart migration logic

--- superset/migrations/shared/migrate_viz/processors.py
+++ superset/migrations/shared/migrate_viz/processors.py
@@ -36,40 +36,6 @@ class MigrateTreeMap(MigrateViz):
             self.data["metric"] = self.data["metrics"][0]
 
 
-class MigrateAreaChart(MigrateViz):
-    """
-    Migrate area charts.
-
-    This migration is incomplete, see https://github.com/apache/superset/pull/24703#discussion_r1265222611
-    for more details. If you fix this migration, please update the ``migrate_chart``
-    function in ``superset/charts/commands/importers/v1/utils.py`` so that it gets
-    applied in chart imports.
-    """
-
-    source_viz_type = "area"
-    target_viz_type = "echarts_area"
-    remove_keys = {"contribution", "stacked_style", "x_axis_label"}
-
-    def _pre_action(self) -> None:
-        if self.data.get("contribution"):
-            self.data["contributionMode"] = "row"
-
-        if stacked := self.data.get("stacked_style"):
-            stacked_map = {
-                "expand": "Expand",
-                "stack": "Stack",
-            }
-            self.data["show_extra_controls"] = True
-            self.data["stack"] = stacked_map.get(stacked)
-
-        if x_axis := self.data.get("granularity_sqla"):
-            self.data["x_axis"] = x_axis
-
-        if x_axis_label := self.data.get("x_axis_label"):
-            self.data["x_axis_title"] = x_axis_label
-            self.data["x_axis_title_margin"] = 30
-
-
 class MigratePivotTable(MigrateViz):
     source_viz_type = "pivot_table"
     target_viz_type = "pivot_table_v2"
@@ -137,10 +103,20 @@ class MigrateSunburst(MigrateViz):
 
 class TimeseriesChart(MigrateViz):
     has_x_axis_control = True
+    rename_keys = {
+        "bottom_margin": "x_axis_title_margin",
+        "left_margin": "y_axis_title_margin",
+        "x_axis_label": "x_axis_title",
+        "x_axis_format": "x_axis_time_format",
+        "x_ticks_layout": "xAxisLabelRotation",
+        "y_axis_label": "y_axis_title",
+        "y_axis_showminmax": "truncateYAxis",
+        "y_log_scale": "logAxis",
+    }
 
     def _pre_action(self) -> None:
         self.data["contributionMode"] = "row" if self.data.get("contribution") else None
-        self.data["zoomable"] = self.data.get("show_brush") != "no"
+        self.data["zoomable"] = self.data.get("show_brush") == "yes"
         self.data["markerEnabled"] = self.data.get("show_markers") or False
         self.data["y_axis_showminmax"] = True
 
@@ -167,15 +143,6 @@ class TimeseriesChart(MigrateViz):
 class MigrateLineChart(TimeseriesChart):
     source_viz_type = "line"
     target_viz_type = "echarts_timeseries_line"
-    rename_keys = {
-        "x_axis_label": "x_axis_title",
-        "bottom_margin": "x_axis_title_margin",
-        "x_axis_format": "x_axis_time_format",
-        "y_axis_label": "y_axis_title",
-        "left_margin": "y_axis_title_margin",
-        "y_axis_showminmax": "truncateYAxis",
-        "y_log_scale": "logAxis",
-    }
 
     def _pre_action(self) -> None:
         super()._pre_action()
@@ -189,3 +156,26 @@ class MigrateLineChart(TimeseriesChart):
         elif line_interpolation == "step-after":
             self.target_viz_type = "echarts_timeseries_step"
             self.data["seriesType"] = "end"
+
+
+class MigrateAreaChart(TimeseriesChart):
+    source_viz_type = "area"
+    target_viz_type = "echarts_area"
+    remove_keys = {"contribution", "stacked_style", "x_axis_label"}
+    stacked_map = {
+        "expand": "Expand",
+        "stack": "Stack",
+        "stream": "Stream",
+    }
+
+    def _pre_action(self) -> None:
+        super()._pre_action()
+
+        self.data["stack"] = (
+            self.stacked_map.get(self.data.get("stacked_style")) or "Stack"
+        )
+
+        if x_ticks_layout := self.data.get("x_ticks_layout"):
+            self.data["x_ticks_layout"] = 45 if x_ticks_layout == "45°" else 0
+
+        self.data["opacity"] = 0.7
