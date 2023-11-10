42140160455adab17e780d547ce54969292e7a76
maciej-or <maciej.orlowski@scalac.io>
2023-11-10 15:39:38 +0100

    jump to dashboard postMessage

--- superset-frontend/plugins/plugin-chart-with-jump-to-dashboard/src/utils/jumpToDashboard.ts
+++ superset-frontend/plugins/plugin-chart-with-jump-to-dashboard/src/utils/jumpToDashboard.ts
@@ -5,10 +5,17 @@ export const createJumpToDashboardHandler = (chartProps: any) => {
     if (jumpTo) {
       console.log('jump to dashboard', jumpTo, window.location);
       if (window.location.href.indexOf('embedded') > -1) {
-        window.parent?.postMessage({
-          type: 'JUMP_TO_DASHBOARD',
-          dashboard_uuid: jumpTo,
-        });
+        window.parent?.postMessage(
+          'JUMP_TO_DASHBOARD string test',
+          '*',
+        );
+        window.parent?.postMessage(
+          {
+            type: 'JUMP_TO_DASHBOARD',
+            dashboard_uuid: jumpTo,
+          },
+          '*',
+        );
       }
     }
   };
