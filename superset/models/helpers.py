e5e352baab663e996f2ccc907686d20450ab64e8
Hugh A. Miles II <hughmil3s@gmail.com>
2023-11-13 13:19:39 -0500

    Update helpers.py

--- superset/models/helpers.py
+++ superset/models/helpers.py
@@ -1343,7 +1343,7 @@ class ExploreMixin:  # pylint: disable=too-many-public-methods
     def values_for_column(self, column_name: str, limit: int = 10000) -> list[Any]:
         # always denormalize column name before querying for values
         db_dialect = self.database.get_dialect()
-        denomalized_col_name = self.database.db_engine_spec.denormalize_name(
+        denormalized_col_name = self.database.db_engine_spec.denormalize_name(
             db_dialect, column_name
         )
         cols = {col.column_name: col for col in self.columns}
