1d24494a6a21f09a80dc36fe7d25bbf7f2d8ca1f
gnought <1684105+gnought@users.noreply.github.com>
2023-11-16 15:00:15 +0800

    rename vars

--- superset/cli/thumbnails.py
+++ superset/cli/thumbnails.py
@@ -56,13 +56,13 @@ logger = logging.getLogger(__name__)
     default=False,
     help="Force refresh, even if previously cached",
 )
-@click.option("--model_id", "-i", multiple=True)
+@click.option("--model_id", "-i", "model_ids", multiple=True)
 def compute_thumbnails(
     asynchronous: bool,
     dashboards_only: bool,
     charts_only: bool,
     force: bool,
-    model_id: list[int],
+    model_ids: list[int],
 ) -> None:
     """Compute thumbnails"""
     # pylint: disable=import-outside-toplevel
@@ -76,12 +76,12 @@ def compute_thumbnails(
     def compute_generic_thumbnail(
         friendly_type: str,
         model_cls: Union[type[Dashboard], type[Slice]],
-        model_id: list[int],
+        model_ids: list[int],
         compute_func: CallableTask,
     ) -> None:
         query = db.session.query(model_cls)
-        if model_id:
-            query = query.filter(model_cls.id.in_(model_id))
+        if model_ids:
+            query = query.filter(model_cls.id.in_(model_ids))
         dashboards = query.all()
         count = len(dashboards)
         for i, model in enumerate(dashboards):
@@ -97,7 +97,7 @@ def compute_thumbnails(
 
     if not charts_only:
         compute_generic_thumbnail(
-            "dashboard", Dashboard, model_id, cache_dashboard_thumbnail
+            "dashboard", Dashboard, model_ids, cache_dashboard_thumbnail
         )
     if not dashboards_only:
-        compute_generic_thumbnail("chart", Slice, model_id, cache_chart_thumbnail)
+        compute_generic_thumbnail("chart", Slice, model_ids, cache_chart_thumbnail)
