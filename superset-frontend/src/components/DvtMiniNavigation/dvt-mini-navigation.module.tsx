0dccb9f81001ec1ea9c27621a40fc468ecf82be5
okancanokan <okancan.okan@gmail.com>
2023-11-17 18:51:07 +0300

    feat: new mini navigation component added

---
+++ superset-frontend/src/components/DvtMiniNavigation/dvt-mini-navigation.module.tsx
@@ -0,0 +1,82 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import { styled } from '@superset-ui/core';
+import { Link } from 'react-router-dom';
+
+// const rotate = keyframes`
+//   from {
+//     transform: rotate(0deg);
+//   }
+
+//   to {
+//     transform: rotate(360deg);
+//   }
+// `;
+
+interface FadeInIconProps {
+  $fadeIn: boolean;
+}
+
+const DvtMiniNavigationAnimatedIcon = styled.div<FadeInIconProps>`
+  transition: all 300ms;
+  transform: ${({ $fadeIn }) => ($fadeIn ? 'rotate(90deg)' : 'rotate(0)')};
+`;
+
+const StyledDvtMiniNavigation = styled.div`
+  height: 100px;
+  width: 100px;
+`;
+
+const DvtMiniNavigationHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 122px;
+  height: 24px;
+  cursor: 'pointer';
+`;
+
+const DvtMiniNavigationHeaderTitle = styled.div`
+  color: ${({ theme }) => theme.colors.dvt.text.label};
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  letter-spacing: 0.2px;
+`;
+
+const DvtMiniNavigationData = styled.div``;
+
+const DvtMiniNavigationDataItem = styled(Link)`
+  padding-left: 10px;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  color: ${({ theme }) => theme.colors.dvt.text.label};
+  width: 57px;
+  height: 22px;
+`;
+
+export {
+  StyledDvtMiniNavigation,
+  DvtMiniNavigationHeader,
+  DvtMiniNavigationData,
+  DvtMiniNavigationDataItem,
+  DvtMiniNavigationHeaderTitle,
+  DvtMiniNavigationAnimatedIcon,
+};
