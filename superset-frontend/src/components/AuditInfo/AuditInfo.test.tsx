2c7ed41ac5e3980bed169e7594a4a6e3e3ad32dc
Ville Brofeldt <ville.brofeldt@apple.com>
2023-11-20 12:18:34 -0800

    lint + improve test

--- superset-frontend/src/components/AuditInfo/AuditInfo.test.tsx
+++ superset-frontend/src/components/AuditInfo/AuditInfo.test.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 
 import { supersetTheme, ThemeProvider } from '@superset-ui/core';
 import { AuditInfo, AuditInfoProps, AuditInfoType } from '.';
-import userEvent from '@testing-library/user-event';
 
-const TEST_DATE = '2023-01-01';
+const TEST_DATE = '2023-11-20';
 const USER = {
   id: 1,
   first_name: 'Foo',
@@ -24,7 +24,9 @@ test('should render a created tooltip when user is provided', async () => {
 
   const dateElement = screen.getByTestId('audit-info-date');
   expect(dateElement).toBeInTheDocument();
+  expect(screen.getByText(TEST_DATE)).toBeInTheDocument();
   userEvent.hover(dateElement);
+  expect(screen.queryByText('Created by: Foo Bar')).not.toBeInTheDocument();
   const tooltip = await screen.findByRole('tooltip');
   expect(tooltip).toBeInTheDocument();
   expect(screen.getByText('Created by: Foo Bar')).toBeInTheDocument();
@@ -37,6 +39,8 @@ test('should render a modified tooltip when user is provided', async () => {
 
   const dateElement = screen.getByTestId('audit-info-date');
   expect(dateElement).toBeInTheDocument();
+  expect(screen.getByText(TEST_DATE)).toBeInTheDocument();
+  expect(screen.queryByText('Modified by: Foo Bar')).not.toBeInTheDocument();
   userEvent.hover(dateElement);
   const tooltip = await screen.findByRole('tooltip');
   expect(tooltip).toBeInTheDocument();
@@ -48,6 +52,7 @@ test('should render only the date if username is not provided', async () => {
 
   const dateElement = screen.getByTestId('audit-info-date');
   expect(dateElement).toBeInTheDocument();
+  expect(screen.getByText(TEST_DATE)).toBeInTheDocument();
   userEvent.hover(dateElement);
   await waitFor(
     () => {
