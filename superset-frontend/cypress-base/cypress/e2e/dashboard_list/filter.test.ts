f2ed0e7dfb1eb12c2fcf9bc261ca38faae84041e
Ville Brofeldt <ville.brofeldt@apple.com>
2023-11-20 19:09:36 -0800

    fix cypress tests

--- superset-frontend/cypress-base/cypress/e2e/dashboard_list/filter.test.ts
+++ superset-frontend/cypress-base/cypress/e2e/dashboard_list/filter.test.ts
@@ -35,11 +35,6 @@ describe('Dashboards filters', () => {
     setFilter('Owner', 'admin user');
   });
 
-  it('should allow filtering by "Created by" correctly', () => {
-    setFilter('Created by', 'alpha user');
-    setFilter('Created by', 'admin user');
-  });
-
   it('should allow filtering by "Status" correctly', () => {
     setFilter('Status', 'Published');
     setFilter('Status', 'Draft');
