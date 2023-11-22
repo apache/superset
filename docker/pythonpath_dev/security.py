5d857da2b6a7a9325931f2e1a05ce94df4d0bdd9
Leticia-Astra <leticia@astrasi.com>
2023-11-22 13:11:12 -0600

    Superset users are created automatically

--- docker/pythonpath_dev/security.py
+++ docker/pythonpath_dev/security.py
@@ -1,3 +1,88 @@
+from flask_appbuilder.security.views import AuthDBView
+from flask_appbuilder import expose
+from flask_login import login_user
+from flask import request, redirect, flash
+from superset.security.manager import SupersetSecurityManager
+import requests
+import json
+import os
+
+class CustomAuthDBView(AuthDBView):
+    def create_user(self, user_data):
+        # create an user with the data session
+        # and assign to him/her a default role
+        user = self.appbuilder.sm.add_user(
+            username = user_data["username"],
+            first_name = user_data["first_name"],
+            last_name = user_data["last_name"],
+            email = user_data["email"],
+            role = self.appbuilder.sm.find_role("Gamma"),
+            password = "test"
+        )
+
+        return user
+    
+    def get_users(self):
+        data = {}
+
+        with open(os.path.join(os.path.dirname(__file__),"users.json"), "r") as users_json:
+            data = json.load(users_json)
+
+        return data
+    
+    def get_user_info(self, token):
+        users = self.get_users()
+        search = list(filter(lambda person: person["token"] == token, users))
+
+        if search:
+            user_info = search[0]["user_info"]
+            print(user_info)
+            return user_info
+    
+    def login_and_redirect(self, user, request):
+        login_user(user, remember=True)
+
+        if request.args.get('redirect') is not None:
+            redirect_url = request.args.get('redirect')
+
+        standalone = str(request.args.get('standalone'))
+
+        if standalone is None:
+            standalone = 'false'
+
+        return redirect(redirect_url+'?standalone=' + standalone)
+
+    @expose('/login/', methods=['GET', 'POST'])
+    def login(self):
+        token = request.values.get('token')
+
+        if token is None:
+            return "No token found"
+
+        user_data = self.get_user_info(token)
+        
+        if user_data is None:
+            return "Invalid Session"
+
+        user = self.appbuilder.sm.find_user(
+            username=user_data["username"]
+        )
+
+        try:
+            return self.login_and_redirect(user, request)
+        
+        except Exception:
+            flash('Creating new user...')
+            user = self.create_user(user_data)
+            return self.login_and_redirect(user, request)
+
+class CustomSecurityManager(SupersetSecurityManager):
+    authdbview = CustomAuthDBView
+    def __init__(self, appbuilder):
+        super(CustomSecurityManager, self).__init__(appbuilder)
+
+
+'''
 from flask import redirect, g, flash, request
 from flask_appbuilder.security.views import UserDBModelView,AuthDBView
 from superset.security import SupersetSecurityManager
@@ -27,3 +112,4 @@ class CustomSecurityManager(SupersetSecurityManager):
     authdbview = CustomAuthDBView
     def __init__(self, appbuilder):
         super(CustomSecurityManager, self).__init__(appbuilder)
+'''
