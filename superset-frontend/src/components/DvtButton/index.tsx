53e4bc64b8e0ef9f5f20c841f3dc6963d489d804
Ulaş Baran Tomak <ulasbaran.tomak@std.yeditepe.edu.tr>
2023-11-22 12:06:48 +0300

    fix: fixed feat/dvt-button

--- superset-frontend/src/components/DvtButton/index.tsx
+++ superset-frontend/src/components/DvtButton/index.tsx
@@ -17,19 +17,17 @@ const DvtButton: React.FC<DvtButtonProps> = ({
   colour = 'primary',
   typeColour = 'basic',
   maxWidth = false,
-}) => {
-  return (
-    <StyledDvtButton
-      $label={label}
-      $maxWidth={maxWidth}
-      $colour={colour}
-      $typeColour={typeColour}
-      onClick={onClick}
-    >
-      {icon && <img src={icon} alt="Button Icon" />}
-      {label}
-    </StyledDvtButton>
-  );
-};
+}) => (
+  <StyledDvtButton
+    $label={label}
+    $maxWidth={maxWidth}
+    $colour={colour}
+    $typeColour={typeColour}
+    onClick={onClick}
+  >
+    {icon && <img src={icon} alt="Button Icon" />}
+    {label}
+  </StyledDvtButton>
+);
 
 export default DvtButton;
