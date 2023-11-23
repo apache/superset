59631b182ebd3e80a4521dc69c9d153394201bd6
Muhammed-baban <93913650+Muhammed-baban@users.noreply.github.com>
2023-11-23 16:06:19 +0300

    update

--- superset-frontend/src/components/DvtCalendar/dvt-calendar.stories.tsx
+++ superset-frontend/src/components/DvtCalendar/dvt-calendar.stories.tsx
@@ -24,9 +24,7 @@ export default {
   title: 'Dvt-Components/DvtCalendar',
   component: DvtCalendar,
 };
-const handleSelect = (date: Moment | null) => {
-  return date;
-};
+const handleSelect = (date: Moment | null) => date;
 export const InteractiveDatePicker = () => {
   const [isOpen, setIsOpen] = useState<boolean>(true);
   const [selectedDate, setSelectedDate] = useState<Moment | null>(
