cd1f4007efb1a7bb2b587640b3fea6b9a1179147
Corbin <corbindbullard@gmail.com>
2023-11-27 15:18:46 -0500

    default notification method

--- superset-frontend/src/features/alerts/AlertReportModal.tsx
+++ superset-frontend/src/features/alerts/AlertReportModal.tsx
@@ -378,7 +378,7 @@ interface NotificationMethodAddProps {
 }
 
 export const TRANSLATIONS = {
-  ADD_NOTIFICATION_METHOD_TEXT: t('Add notification method'),
+  ADD_NOTIFICATION_METHOD_TEXT: t('Add another notification method'),
   ADD_DELIVERY_METHOD_TEXT: t('Add delivery method'),
   SAVE_TEXT: t('Save'),
   ADD_TEXT: t('Add'),
@@ -510,10 +510,8 @@ const AlertReportModal: FunctionComponent<AlertReportModalProps> = ({
   const [notificationSettings, setNotificationSettings] = useState<
     NotificationSetting[]
   >([]);
-
   const onNotificationAdd = () => {
     const settings: NotificationSetting[] = notificationSettings.slice();
-
     settings.push({
       recipients: '',
       options: allowedNotificationMethods,
@@ -575,6 +573,7 @@ const AlertReportModal: FunctionComponent<AlertReportModalProps> = ({
 
   const removeNotificationSetting = (index: number) => {
     const settings = notificationSettings.slice();
+    console.log('settings remove', settings);
 
     settings.splice(index, 1);
     setNotificationSettings(settings);
@@ -592,6 +591,7 @@ const AlertReportModal: FunctionComponent<AlertReportModalProps> = ({
 
   // Functions
   const hide = () => {
+    console.log('HIDE');
     clearError();
     setIsHidden(true);
     onHide();
@@ -1053,7 +1053,13 @@ const AlertReportModal: FunctionComponent<AlertReportModalProps> = ({
             ]
           : [],
       });
-      setNotificationSettings([]);
+      setNotificationSettings([
+        {
+          recipients: '',
+          options: allowedNotificationMethods,
+          method: 'Email',
+        },
+      ]);
       setNotificationAddState('active');
     }
   }, [alert]);
@@ -1602,10 +1608,6 @@ const AlertReportModal: FunctionComponent<AlertReportModalProps> = ({
             style={{ borderBottom: 'none' }}
           >
             <div>
-              <StyledSectionTitle>
-                <h4>{TRANSLATIONS.NOTIFICATION_METHOD_TEXT}</h4>
-                <span className="required">*</span>
-              </StyledSectionTitle>
               {notificationSettings.map((notificationSetting, i) => (
                 <StyledNotificationMethodWrapper>
                   <NotificationMethod
