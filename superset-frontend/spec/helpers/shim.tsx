9e7a8daaecf48663fa98efe76f20c1cc5db23b2a
Evan Rusackas <evan@rusackas.com>
2023-11-28 13:16:52 -0700

    restoring shim ¯\_(ツ)_/¯

--- superset-frontend/spec/helpers/shim.tsx
+++ superset-frontend/spec/helpers/shim.tsx
@@ -48,17 +48,15 @@ if (defaultView != null) {
 }
 
 const g = global as any;
-if (typeof window === 'undefined') {
-  g.window = {};
-  g.window.location = { href: 'about:blank' };
-  g.window.performance = { now: () => new Date().getTime() };
-  g.window.Worker = Worker;
-  g.window.IntersectionObserver = IntersectionObserver;
-  g.window.ResizeObserver = ResizeObserver;
-  g.window.featureFlags = {};
-  g.URL.createObjectURL = () => '';
-  g.caches = new CacheStorage();
-}
+g.window = g.window || {};
+g.window.location = { href: 'about:blank' };
+g.window.performance = { now: () => new Date().getTime() };
+g.window.Worker = Worker;
+g.window.IntersectionObserver = IntersectionObserver;
+g.window.ResizeObserver = ResizeObserver;
+g.window.featureFlags = {};
+g.URL.createObjectURL = () => '';
+g.caches = new CacheStorage();
 
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
