6f712f141b0bf2fdd0b1a9d764461649d7996b5e
omerfbolat <anekdo1@gmail.com>
2023-11-29 12:47:34 +0300

    feat: hooks new use onclick outsite add fixed

---
+++ superset-frontend/src/hooks/useOnClickOutsite.ts
@@ -0,0 +1,28 @@
+import { useEffect, RefObject } from 'react';
+
+const useOnClickOutside = (
+  ref: RefObject<HTMLElement>,
+  handler: (event: MouseEvent | TouchEvent) => void,
+) => {
+  useEffect(() => {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      if (!ref.current || ref.current.contains(event.target as Node)) {
+        return;
+      }
+      handler(event);
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+    document.addEventListener('contextmenu', event => {
+      event.preventDefault();
+    });
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler]);
+};
+
+export default useOnClickOutside;
