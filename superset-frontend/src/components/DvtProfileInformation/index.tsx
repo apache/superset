4d93e3dc2121398bdaede7fb5eeea75313295a12
Muhammed-baban <93913650+Muhammed-baban@users.noreply.github.com>
2023-11-30 14:48:44 +0300

    update

--- superset-frontend/src/components/DvtProfileInformation/index.tsx
+++ superset-frontend/src/components/DvtProfileInformation/index.tsx
@@ -77,7 +77,7 @@ const DvtProfileInformation: React.FC<DvtProfileInformationProps> = ({
           <StyledProfileImage>
             <img
               src={user.image}
-              alt={`Profile image of ${user.header}`}
+              alt="Profile image"
               height={155}
               width={155}
               style={{ borderRadius: '155px' }}
