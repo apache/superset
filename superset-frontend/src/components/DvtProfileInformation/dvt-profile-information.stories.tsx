39ee01fffca9d4463bf9ed50ba54fd67c46aba7a
Muhammed-baban <93913650+Muhammed-baban@users.noreply.github.com>
2023-11-30 12:55:54 +0300

    feat: DvtProfileInformatio Completed

---
+++ superset-frontend/src/components/DvtProfileInformation/dvt-profile-information.stories.tsx
@@ -0,0 +1,53 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import React from 'react';
+import { SupersetTheme } from '@superset-ui/core';
+import DvtProfileInformation, { DvtProfileInformationProps } from '.';
+
+export default {
+  title: 'Dvt-Components/DvtProfileInformation',
+  component: DvtProfileInformation,
+};
+
+export const Default = (args: DvtProfileInformationProps) => {
+  return (
+    <div
+      css={(theme: SupersetTheme) => ({
+        backgroundColor: theme.colors.dvt.grayscale.light2,
+        padding: '100px',
+      })}
+    >
+      <DvtProfileInformation {...args} />
+    </div>
+  );
+};
+
+Default.args = {
+  userData: [
+    {
+      image:
+        'https://s3-alpha-sig.figma.com/img/b367/a97e/b3452351a5be6194229715c7cb6da622?Expires=1702252800&Signature=KEpNfEpqoD1WcoinBiGaLTMNU0ZYjlHnJmCgOakjRhMMWwn8iHVMw~CNAEDGCcTfl8lmn9dFKq5DFeqEPisgIGSGo02ykcyI5HZ4D35kqJK7HayItECqzKDtHyu~Fp~U0E06kwiG00xzWlvgsvN5n4Kosq~5i3IZLIQe7D7vRWU0MLKlVgTCaDeq6kvZSRFAUXPpYJ37TiM6s83rjJpFRO1A1yHO7MIn7CvZrh4WwdKbhBoqDu5WPjJ~jx45GMMQYCi6XjVdA3LZMzTIPeLIr206h7nQzQM0giDm5q6B8GvvNpxeITuQkvZARsyLVVvU4RY3n1-CemXBVPbFotFT8g__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+      header: 'AppName Admin',
+      location: 'Istanbul',
+      joinedDate: '2023-11-30 00:34:23',
+      title: 'Frontend developer',
+      test: 'Lorem Ipsum Lorem',
+    },
+  ],
+};
---
+++ superset-frontend/src/components/DvtProfileInformation/index.tsx
@@ -0,0 +1,97 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import React from 'react';
+import {
+  StyledProfileIndormation,
+  StyledProfileImage,
+  StyledHeading,
+  StyledInformation,
+  StyledInformationDiv,
+  StyledLabel,
+} from './dvt-profile-information.module';
+
+export interface DvtProfileInformationProps {
+  userData: {
+    image: string;
+    header: string;
+    location: string;
+    joinedDate: string;
+    title: string;
+    test: string;
+  }[];
+}
+
+const DvtProfileInformation: React.FC<DvtProfileInformationProps> = ({
+  userData,
+}) => {
+  const getTimeAgo = (joinedDate: Date): string => {
+    const now = new Date();
+    const diffTime = Math.abs(now.getTime() - joinedDate.getTime());
+    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+
+    if (diffDays < 1) {
+      const diffHours = Math.floor(diffTime / (1000 * 60 * 60));
+      return `Joined ${diffHours} ${diffHours === 1 ? 'hour' : 'hours'} ago`;
+    } else if (diffDays < 7) {
+      return `Joined ${diffDays} ${diffDays === 1 ? 'day' : 'days'} ago`;
+    } else if (diffDays < 30) {
+      const diffWeeks = Math.floor(diffDays / 7);
+      return `Joined ${diffWeeks} ${diffWeeks === 1 ? 'week' : 'weeks'} ago`;
+    } else if (diffDays < 365) {
+      const diffMonths = Math.floor(diffDays / 30);
+      return `Joined ${diffMonths} ${
+        diffMonths === 1 ? 'month' : 'months'
+      } ago`;
+    } else {
+      const diffYears = Math.floor(diffDays / 365);
+      const remainingMonths = Math.floor((diffDays % 365) / 30);
+      return `Joined ${diffYears} ${diffYears === 1 ? 'year' : 'years'} ${
+        remainingMonths > 0 ? `and ${remainingMonths} months` : ''
+      } ago`;
+    }
+  };
+
+  return (
+    <StyledProfileIndormation>
+      {userData.map(user => (
+        <StyledInformationDiv>
+          <StyledProfileImage>
+            <img
+              src={user.image}
+              height={155}
+              width={155}
+              style={{ borderRadius: '155px' }}
+            />
+          </StyledProfileImage>
+          <StyledHeading>{user.header}</StyledHeading>
+          <StyledInformation>
+            <StyledInformationDiv>{user.location}</StyledInformationDiv>
+            <StyledInformationDiv>
+              {getTimeAgo(new Date(user.joinedDate))}
+            </StyledInformationDiv>
+          </StyledInformation>
+          <StyledLabel>Title: {user.title}</StyledLabel>
+          <StyledLabel>Test: {user.test}</StyledLabel>
+        </StyledInformationDiv>
+      ))}
+    </StyledProfileIndormation>
+  );
+};
+
+export default DvtProfileInformation;
