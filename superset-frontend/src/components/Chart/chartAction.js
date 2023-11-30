342bc26482f587d4187dbf78b2521362e3d11e6f
Beto Dealmeida <roberto@dealmeida.net>
2023-11-30 10:23:51 -0500

    Improve comment

--- superset-frontend/src/components/Chart/chartAction.js
+++ superset-frontend/src/components/Chart/chartAction.js
@@ -269,10 +269,9 @@ export function runAnnotationQuery({
       return Promise.resolve();
     }
 
-    // remap from controls to formData naming; in the original formData the
-    // `granularity` attribute represents the time grain, but in the request
-    // payload it corresponds to the name of the column where the time grain
-    // should be applied...
+    // In the original formData the `granularity` attribute represents the time grain (eg
+    // `P1D`), but in the request payload it corresponds to the name of the column where
+    // the time grain should be applied (eg, `Date`), so we need to move things around.
     fd.time_grain_sqla = fd.time_grain_sqla || fd.granularity;
     fd.granularity = fd.granularity_sqla;
 
