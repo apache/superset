20ead3dc45dd42435f559b3077b335eba4e6f732
Corbin <corbindbullard@gmail.com>
2023-12-06 15:11:42 -0500

    fix NumberInputs for linter

--- superset-frontend/src/features/alerts/components/NumberInput.tsx
+++ superset-frontend/src/features/alerts/components/NumberInput.tsx
@@ -2,17 +2,15 @@ import React, { useState, ChangeEvent } from 'react';
 
 interface NumberInputProps {
   timeUnit: string;
-  type: string;
   min: number;
   name: string;
-  value: string;
+  value: string | number;
   placeholder: string;
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
 export default function NumberInput({
   timeUnit,
-  type,
   min,
   name,
   value,
@@ -26,7 +24,7 @@ export default function NumberInput({
       type="text"
       min={min}
       name={name}
-      value={value ? `${value}${!isFocused ? ' ' + timeUnit : ''}` : ''}
+      value={value ? `${value}${!isFocused ? ` ${timeUnit}` : ''}` : ''}
       placeholder={placeholder}
       onFocus={() => setIsFocused(true)}
       onBlur={() => setIsFocused(false)}
