d19b03065a96a8957b864b0d687dbfede717a4a7
omerfbolat <anekdo1@gmail.com>
2023-12-11 18:27:52 +0300

    fix: dvt-app dvt-root context provider change fixed

--- superset-frontend/src/views/dvt-app.tsx
+++ superset-frontend/src/views/dvt-app.tsx
@@ -40,7 +40,7 @@ import setupExtensions from 'src/setup/setupExtensions';
 import { styled } from '@superset-ui/core';
 import { logEvent } from 'src/logger/actions';
 import { store } from 'src/views/dvt-store';
-import { RootContextProviders } from './RootContextProviders';
+import { DvtRootContextProviders } from './DvtRootContextProviders';
 import { ScrollToTop } from './ScrollToTop';
 
 const StyledApp = styled.div`
@@ -96,7 +96,7 @@ const DvtApp = () => (
   <Router>
     <ScrollToTop />
     <LocationPathnameLogger />
-    <RootContextProviders>
+    <DvtRootContextProviders>
       <StyledApp>
         <GlobalStyles />
         <DvtSidebar data={[]} isFrontendRoute={isFrontendRoute} />
@@ -118,7 +118,7 @@ const DvtApp = () => (
         </Main>
         <ToastContainer />
       </StyledApp>
-    </RootContextProviders>
+    </DvtRootContextProviders>
   </Router>
 );
 
