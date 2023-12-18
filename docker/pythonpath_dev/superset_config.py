6222ae19e3e205243fcd1d3ec5f00bce4d57e397
mayank-pathania <108799966+mayank-pathania@users.noreply.github.com>
2023-12-18 19:51:58 +0530

    Update superset_config.py

--- docker/pythonpath_dev/superset_config.py
+++ docker/pythonpath_dev/superset_config.py
@@ -99,6 +99,7 @@ WEBDRIVER_BASEURL = "http://superset:8088/"
 WEBDRIVER_BASEURL_USER_FRIENDLY = WEBDRIVER_BASEURL
 
 SQLLAB_CTAS_NO_LIMIT = True
+ENABLE_TEMPLATE_PROCESSING = True
 
 #
 # Optionally import superset_config_docker.py (which will have been included on
