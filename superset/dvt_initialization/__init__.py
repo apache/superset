41e584bcdab29e195b87f0ae997a492c0dafc95b
Eray <contact@erayerturk.com>
2023-12-19 12:18:49 +0300

    feat/dvt-login: Fix pylint

--- superset/dvt_initialization/__init__.py
+++ superset/dvt_initialization/__init__.py
@@ -4,6 +4,7 @@ from superset.initialization import SupersetAppInitializer
 
 class DVTAppInitializer(SupersetAppInitializer):
     def init_views(self) -> None:
+        # pylint: disable=import-outside-toplevel
         super().init_views()
         from superset.dvt_auth.login import DVTAuthDBView
 
