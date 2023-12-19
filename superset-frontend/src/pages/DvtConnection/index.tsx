b45e57353172a8a66859fe2b468cbb85b73d3f5e
Muhammed-baban <93913650+Muhammed-baban@users.noreply.github.com>
2023-12-19 12:51:42 +0300

    update

--- superset-frontend/src/pages/DvtConnection/index.tsx
+++ superset-frontend/src/pages/DvtConnection/index.tsx
@@ -30,59 +30,40 @@ function ReportList() {
   const [apiData, setApiData] = useState([]);
 
   const [page, setPage] = useState<number>(1);
-  const reportsSelector = useAppSelector(state => state.dvtSidebar.reports);
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('/api/v1/report/');
+        const response = await fetch('/api/v1/dataset/');
         const data = await response.json();
+        const editedData = data.result.map((item: any) => ({
+          database: item.database.database_name,
+          admin: `${item.owners[0]?.first_name} ${item.owners[0]?.last_name}`,
+          date: new Date(item.changed_on_utc).toLocaleString('tr-TR', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+          }),
+        }));
 
-        const editedData = data.result
-          .filter((item: any) => item.type === 'Report')
-          .map((item: any) => ({
-            name: item.name,
-            type: item.type,
-            crontab_humanized: item.crontab_humanized,
-            date: new Date(item.created_on).toLocaleString('tr-TR', {
-              day: '2-digit',
-              month: '2-digit',
-              year: 'numeric',
-              hour: '2-digit',
-              minute: '2-digit',
-              second: '2-digit',
-            }),
-            created_by: `${item.created_by.first_name} ${item.created_by.last_name}`,
-            changed_by: `${item.changed_by.first_name} ${item.changed_by.last_name}`,
-            last_state: item.last_state,
-          }));
-
-        const filteredData = editedData.filter(
-          (item: any) =>
-            (reportsSelector.createdBy
-              ? item.created_by === reportsSelector.createdBy
-              : true) &&
-            (reportsSelector.chartType
-              ? item.last_state === reportsSelector.chartType
-              : true),
-        );
-        setApiData(filteredData);
+        setApiData(editedData);
+        console.log(data.result);
       } catch (error) {
         console.error('Hata:', error);
       }
     };
 
     fetchData();
-  }, [reportsSelector]);
+  }, []);
   const modifiedData = {
     header: [
-      { id: 1, title: 'Name', field: 'name', heartIcon: true },
-      { id: 2, title: 'Visualization Type', field: 'type' },
-      { id: 3, title: 'Dataset', field: 'crontab_humanized' },
-      { id: 4, title: 'Modified date', field: 'date' },
-      { id: 5, title: 'Modified by', field: 'changed_by' },
-      { id: 6, title: 'Created by', field: 'created_by' },
+      { id: 1, title: 'Database', field: 'database', heartIcon: true },
+      { id: 2, title: 'Admin', field: 'admin' },
+      { id: 3, title: 'Last Modified', field: 'date' },
       {
-        id: 9,
+        id: 4,
         title: 'Action',
         clicks: [
           {
