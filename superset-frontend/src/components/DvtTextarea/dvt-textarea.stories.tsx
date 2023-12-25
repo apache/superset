34e3f23939385652ae538c703df986082436b890
Muhammed BABAN <93913650+Muhammed-baban@users.noreply.github.com>
2023-12-25 13:17:45 +0300

    completed

---
+++ superset-frontend/src/components/DvtTextarea/dvt-textarea.stories.tsx
@@ -0,0 +1,40 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import React, { useState } from 'react';
+import { SupersetTheme } from '@superset-ui/core';
+import DvtTextarea, { DvtTextareaProps } from '.';
+
+export default {
+  title: 'Dvt-Components/DvtTextarea',
+  component: DvtTextarea,
+};
+
+export const Default: React.FC<DvtTextareaProps> = () => {
+  const [text, setText] = useState<string>('');
+  return (
+    <div
+      css={(theme: SupersetTheme) => ({
+        backgroundColor: theme.colors.dvt.grayscale.light2,
+        padding: '40px',
+      })}
+    >
+      <DvtTextarea value={text} onChange={setText} />
+    </div>
+  );
+};
