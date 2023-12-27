d5819a2d9684973f3dffb3fb3c45a0aa5a77d89f
Muhammed BABAN <93913650+Muhammed-baban@users.noreply.github.com>
2023-12-27 15:09:26 +0300

    update

---
+++ superset-frontend/src/explore/components/controls/VizTypeControl/DvtVizTypeGallery.tsx
@@ -0,0 +1,408 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import React, {
+    useMemo,
+  useState,
+} from 'react';
+import Fuse from 'fuse.js';
+import {
+  t,
+  styled,
+  css,
+  ChartMetadata,
+  SupersetTheme,
+  useTheme,
+} from '@superset-ui/core';
+import { usePluginContext } from 'src/components/DynamicPlugins';
+import { nativeFilterGate } from 'src/dashboard/components/nativeFilters/utils';
+
+interface VizTypeGalleryProps {
+  onChange: (vizType: string | null) => void;
+  onDoubleClick: () => void;
+  selectedViz: string | null;
+  className?: string;
+  denyList: string[];
+}
+
+type VizEntry = {
+  key: string;
+  value: ChartMetadata;
+};
+
+enum SECTIONS {
+  ALL_CHARTS = 'ALL_CHARTS',
+  CATEGORY = 'CATEGORY',
+  TAGS = 'TAGS',
+  RECOMMENDED_TAGS = 'RECOMMENDED_TAGS',
+}
+
+const DEFAULT_ORDER = [
+  'line',
+  'big_number',
+  'big_number_total',
+  'table',
+  'pivot_table_v2',
+  'echarts_timeseries_line',
+  'echarts_area',
+  'echarts_timeseries_bar',
+  'echarts_timeseries_scatter',
+  'pie',
+  'mixed_timeseries',
+  'filter_box',
+  'dist_bar',
+  'area',
+  'bar',
+  'deck_polygon',
+  'time_table',
+  'histogram',
+  'deck_scatter',
+  'deck_hex',
+  'time_pivot',
+  'deck_arc',
+  'heatmap',
+  'deck_grid',
+  'deck_screengrid',
+  'treemap_v2',
+  'box_plot',
+  'sunburst',
+  'sankey',
+  'word_cloud',
+  'mapbox',
+  'kepler',
+  'cal_heatmap',
+  'rose',
+  'bubble',
+  'bubble_v2',
+  'deck_geojson',
+  'horizon',
+  'deck_multi',
+  'compare',
+  'partition',
+  'event_flow',
+  'deck_path',
+  'graph_chart',
+  'world_map',
+  'paired_ttest',
+  'para',
+  'country_map',
+];
+
+const typesWithDefaultOrder = new Set(DEFAULT_ORDER);
+
+const THUMBNAIL_GRID_UNITS = 100;
+
+export const MAX_ADVISABLE_VIZ_GALLERY_WIDTH = 1090;
+
+const OTHER_CATEGORY = t('Other');
+
+const ALL_CHARTS = t('All charts');
+
+export const VIZ_TYPE_CONTROL_TEST_ID = 'viz-type-control';
+
+const VizPickerLayout = styled.div`
+  display: grid;
+  height: 100%;
+`;
+
+const RightPane = styled.div``;
+
+const IconsPane = styled.div`
+  overflow: auto;
+  display: grid;
+  grid-template-columns: repeat(
+    auto-fill,
+    ${({ theme }) => theme.gridUnit * THUMBNAIL_GRID_UNITS}px
+  );
+  grid-auto-rows: max-content;
+  justify-content: space-evenly;
+  grid-gap: ${({ theme }) => theme.gridUnit * 2}px;
+  justify-items: center;
+  // for some reason this padding doesn't seem to apply at the bottom of the container. Why is a mystery.
+  padding: ${({ theme }) => theme.gridUnit * 2}px;
+`;
+
+const DetailsPane = (theme: SupersetTheme) => css`
+  grid-area: details;
+  border-top: 1px solid ${theme.colors.grayscale.light2};
+`;
+
+const DetailsPopulated = (theme: SupersetTheme) => css`
+  padding: ${theme.gridUnit * 4}px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: auto auto 1fr;
+  grid-template-areas:
+    'viz-name examples-header'
+    'viz-tags examples'
+    'description examples';
+`;
+
+// overflow hidden on the details pane and overflow auto on the description
+// (plus grid layout) enables the description to scroll while the header stays in place.
+
+const thumbnailContainerCss = (theme: SupersetTheme) => css`
+  cursor: pointer;
+  width: ${theme.gridUnit * THUMBNAIL_GRID_UNITS}px;
+  position: relative;
+
+  img {
+    max-width: 547px;
+    border: 1px solid ${theme.colors.grayscale.light2};
+    border-radius: ${theme.gridUnit}px;
+    transition: border-color ${theme.transitionTiming};
+  }
+
+  &.selected img {
+    border: 2px solid ${theme.colors.primary.light2};
+  }
+
+  &:hover:not(.selected) img {
+    border: 1px solid ${theme.colors.grayscale.light1};
+  }
+
+  .viztype-label {
+    margin-top: ${theme.gridUnit * 2}px;
+    text-align: center;
+  }
+`;
+
+const HighlightLabel = styled.div`
+  ${({ theme }) => `
+    border: 1px solid ${theme.colors.primary.dark1};
+    box-sizing: border-box;
+    border-radius: ${theme.gridUnit}px;
+    background: ${theme.colors.grayscale.light5};
+    line-height: ${theme.gridUnit * 2.5}px;
+    color: ${theme.colors.primary.dark1};
+    font-size: ${theme.typography.sizes.s}px;
+    font-weight: ${theme.typography.weights.bold};
+    text-align: center;
+    padding: ${theme.gridUnit * 0.5}px ${theme.gridUnit}px;
+    text-transform: uppercase;
+    cursor: pointer;
+
+    div {
+      transform: scale(0.83,0.83);
+    }
+  `}
+`;
+
+const ThumbnailLabelWrapper = styled.div`
+  position: absolute;
+  right: ${({ theme }) => theme.gridUnit}px;
+  top: ${({ theme }) => theme.gridUnit * 19}px;
+`;
+
+function vizSortFactor(entry: VizEntry) {
+  if (typesWithDefaultOrder.has(entry.key)) {
+    return DEFAULT_ORDER.indexOf(entry.key);
+  }
+  return DEFAULT_ORDER.length;
+}
+
+interface ThumbnailProps {
+  entry: VizEntry;
+  selectedViz: string | null;
+  setSelectedViz: (viz: string) => void;
+  onDoubleClick: () => void;
+}
+
+const Thumbnail: React.FC<ThumbnailProps> = ({
+  entry,
+  selectedViz,
+  setSelectedViz,
+  onDoubleClick,
+}) => {
+  const theme = useTheme();
+  const { key, value: type } = entry;
+  const isSelected = selectedViz === entry.key;
+
+  return (
+    <div
+      role="button"
+      // using css instead of a styled component to preserve
+      // the data-test attribute
+      css={thumbnailContainerCss(theme)}
+      tabIndex={0}
+      className={isSelected ? 'selected' : ''}
+      onClick={() => setSelectedViz(key)}
+      onDoubleClick={onDoubleClick}
+      data-test="viztype-selector-container"
+    >
+      <div
+        className="viztype-label"
+        data-test={`${VIZ_TYPE_CONTROL_TEST_ID}__viztype-label`}
+      >
+        {type.name}
+      </div>
+      <img
+        alt={type.name}
+        width="100%"
+        className={`viztype-selector ${isSelected ? 'selected' : ''}`}
+        src={type.thumbnail}
+      />
+      {type.label && (
+        <ThumbnailLabelWrapper>
+          <HighlightLabel>
+            <div>{t(type.label)}</div>
+          </HighlightLabel>
+        </ThumbnailLabelWrapper>
+      )}
+    </div>
+  );
+};
+
+interface ThumbnailGalleryProps {
+  vizEntries: VizEntry[];
+  selectedViz: string | null;
+  setSelectedViz: (viz: string) => void;
+  onDoubleClick: () => void;
+}
+
+/** A list of viz thumbnails, used within the viz picker modal */
+const ThumbnailGallery: React.FC<ThumbnailGalleryProps> = ({
+  vizEntries,
+  ...props
+}) => (
+  <IconsPane data-test={`${VIZ_TYPE_CONTROL_TEST_ID}__viz-row`}>
+    {vizEntries.map(entry => (
+      <Thumbnail key={entry.key} {...props} entry={entry} />
+    ))}
+  </IconsPane>
+);
+
+export default function VizTypeGallery(props: VizTypeGalleryProps) {
+  const { selectedViz, onChange, onDoubleClick, className } = props;
+  const { mountedPluginMetadata } = usePluginContext();
+
+  const selectedVizMetadata: ChartMetadata | null = selectedViz
+    ? mountedPluginMetadata[selectedViz]
+    : null;
+
+  const chartMetadata: VizEntry[] = useMemo(() => {
+    const result = Object.entries(mountedPluginMetadata)
+      .map(([key, value]) => ({ key, value }))
+      .filter(({ key }) => !props.denyList.includes(key))
+      .filter(
+        ({ value }) =>
+          nativeFilterGate(value.behaviors || []) && !value.deprecated,
+      );
+    result.sort((a, b) => vizSortFactor(a) - vizSortFactor(b));
+    return result;
+  }, [mountedPluginMetadata]);
+
+  const chartsByCategory = useMemo(() => {
+    const result: Record<string, VizEntry[]> = {};
+    chartMetadata.forEach(entry => {
+      const category = entry.value.category || OTHER_CATEGORY;
+      if (!result[category]) {
+        result[category] = [];
+      }
+      result[category].push(entry);
+    });
+    return result;
+  }, [chartMetadata]);
+
+  const chartsByTags = useMemo(() => {
+    const result: Record<string, VizEntry[]> = {};
+    chartMetadata.forEach(entry => {
+      const tags = entry.value.tags || [];
+      tags.forEach(tag => {
+        if (!result[tag]) {
+          result[tag] = [];
+        }
+        result[tag].push(entry);
+      });
+    });
+    return result;
+  }, [chartMetadata]);
+
+  const sortedMetadata = useMemo(
+    () => chartMetadata.sort((a, b) => a.key.localeCompare(b.key)),
+    [chartMetadata],
+  );
+
+  const [activeSelector, setActiveSelector] = useState<string>(
+    () => selectedVizMetadata?.category || ALL_CHARTS,
+  );
+
+  const [activeSection, setActiveSection] = useState<string>(
+    () => SECTIONS.ALL_CHARTS,
+  );
+
+  // get a fuse instance for fuzzy search
+  const fuse = useMemo(
+    () =>
+      new Fuse(chartMetadata, {
+        ignoreLocation: true,
+        threshold: 0.3,
+        keys: [
+          {
+            name: 'value.name',
+            weight: 4,
+          },
+          {
+            name: 'value.tags',
+            weight: 2,
+          },
+          'value.description',
+        ],
+      }),
+    [chartMetadata],
+  );
+
+  const getVizEntriesToDisplay = () => {
+    if (
+      activeSelector === ALL_CHARTS &&
+      activeSection === SECTIONS.ALL_CHARTS
+    ) {
+      return sortedMetadata;
+    }
+    if (
+      activeSection === SECTIONS.CATEGORY &&
+      chartsByCategory[activeSelector]
+    ) {
+      return chartsByCategory[activeSelector];
+    }
+    if (
+      (activeSection === SECTIONS.TAGS ||
+        activeSection === SECTIONS.RECOMMENDED_TAGS) &&
+      chartsByTags[activeSelector]
+    ) {
+      return chartsByTags[activeSelector];
+    }
+    return [];
+  };
+
+  return (
+    <VizPickerLayout
+      className={className}
+    >
+      <RightPane>
+        <ThumbnailGallery
+          vizEntries={getVizEntriesToDisplay()}
+          selectedViz={selectedViz}
+          setSelectedViz={onChange}
+          onDoubleClick={onDoubleClick}
+        />
+      </RightPane>
+
+    </VizPickerLayout>
+  );
+}
--- superset-frontend/src/pages/DvtChartAdd/index.tsx
+++ superset-frontend/src/pages/DvtChartAdd/index.tsx
@@ -16,71 +16,171 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-import React from 'react';
-import withToasts from 'src/components/MessageToasts/withToasts';
+import React, { ReactNode } from 'react';
+import rison from 'rison';
+import querystring from 'query-string';
 import {
-  StyledChart,
-  StyledChartAdd,
-  StyledImgRender,
-} from './dvt-chart-add.module';
+  isFeatureEnabled,
+  FeatureFlag,
+  isDefined,
+  JsonResponse,
+  SupersetClient,
+  t,
+} from '@superset-ui/core';
+import { getUrlParam } from 'src/utils/urlUtils';
+import { URL_PARAMS } from 'src/constants';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
+import withToasts from 'src/components/MessageToasts/withToasts';
+import { findPermission } from 'src/utils/findPermission';
+import { UserWithPermissionsAndRoles } from 'src/types/bootstrapTypes';
+import getBootstrapData from 'src/utils/getBootstrapData';
+import {
+  Dataset,
+  DatasetSelectLabel,
+} from 'src/features/datasets/DatasetSelectLabel';
+import DvtVizTypeGallery from 'src/explore/components/controls/VizTypeControl/DvtVizTypeGallery';
 
-const photoFileNames = [
-  { label: 'Lorem Ipsum Dolor Sit', fileName: 'area.png' },
-  { label: 'Lorem Ipsum Dolor Sit', fileName: 'bar.png' },
-  { label: 'Lorem Ipsum Dolor Sit', fileName: 'big_number_total.png' },
-  { label: 'Lorem Ipsum Dolor Sit', fileName: 'big_number.png' },
-  { label: 'Lorem Ipsum Dolor Sit', fileName: 'box_plot.png' },
-  { label: 'Lorem Ipsum Dolor Sit', fileName: 'bubble.png' },
-  { label: 'Lorem Ipsum Dolor Sit', fileName: 'bullet.png' },
-  { label: 'Lorem Ipsum Dolor Sit', fileName: 'cal_heatmap.png' },
-  { label: 'Lorem Ipsum Dolor Sit', fileName: 'chord.png' },
-  { label: 'Lorem Ipsum Dolor Sit', fileName: 'compare.png' },
-  { label: 'Lorem Ipsum Dolor Sit', fileName: 'country_map.png' },
-  { label: 'Lorem Ipsum Dolor Sit', fileName: 'deck_arc.png' },
-  { label: 'Lorem Ipsum Dolor Sit', fileName: 'deck_grid.png' },
-];
+export interface ChartCreationProps extends RouteComponentProps {
+  user: UserWithPermissionsAndRoles;
+  addSuccessToast: (arg: string) => void;
+}
 
-class ChartAdd extends React.Component {
-  renderPhotos = () => {
-    return photoFileNames.map((item, index) => {
-      const imgSrc = require(`../../../src/assets/images/viz_thumbnails/${item.fileName}`);
-      const imgAlt = `Photo ${index + 1}`;
-      return (
-        <StyledImgRender>
-          {item.label}
-          <img key={index} src={imgSrc} alt={imgAlt} />
-        </StyledImgRender>
-      );
+export type ChartCreationState = {
+  datasource?: { label: string; value: string };
+  datasetName?: string | string[] | null;
+  vizType: string | null;
+  canCreateDataset: boolean;
+};
+
+const bootstrapData = getBootstrapData();
+const denyList: string[] = bootstrapData.common.conf.VIZ_TYPE_DENYLIST || [];
+
+if (
+  isFeatureEnabled(FeatureFlag.DASHBOARD_NATIVE_FILTERS) &&
+  !denyList.includes('filter_box')
+) {
+  denyList.push('filter_box');
+}
+
+
+export class ChartCreation extends React.PureComponent<
+  ChartCreationProps,
+  ChartCreationState
+> {
+  constructor(props: ChartCreationProps) {
+    super(props);
+    this.state = {
+      vizType: null,
+      canCreateDataset: findPermission(
+        'can_write',
+        'Dataset',
+        props.user.roles,
+      ),
+    };
+
+    this.changeDatasource = this.changeDatasource.bind(this);
+    this.changeVizType = this.changeVizType.bind(this);
+    this.gotoSlice = this.gotoSlice.bind(this);
+    this.loadDatasources = this.loadDatasources.bind(this);
+    this.onVizTypeDoubleClick = this.onVizTypeDoubleClick.bind(this);
+  }
+
+  componentDidMount() {
+    const params = querystring.parse(window.location.search)?.dataset as string;
+    if (params) {
+      this.loadDatasources(params, 0, 1).then(r => {
+        const datasource = r.data[0];
+        // override here to force styling of option label
+        // which expects a reactnode instead of string
+        // @ts-expect-error
+        datasource.label = datasource.customLabel;
+        this.setState({ datasource });
+      });
+      this.props.addSuccessToast(t('The dataset has been saved'));
+    }
+  }
+
+  exploreUrl() {
+    const dashboardId = getUrlParam(URL_PARAMS.dashboardId);
+    let url = `/explore/?viz_type=${this.state.vizType}&datasource=${this.state.datasource?.value}`;
+    if (isDefined(dashboardId)) {
+      url += `&dashboard_id=${dashboardId}`;
+    }
+    return url;
+  }
+
+  gotoSlice() {
+    this.props.history.push(this.exploreUrl());
+  }
+
+  changeDatasource(datasource: { label: string; value: string }) {
+    this.setState({ datasource });
+  }
+
+  changeVizType(vizType: string | null) {
+    this.setState({ vizType });
+  }
+
+  isBtnDisabled() {
+    return !(this.state.datasource?.value && this.state.vizType);
+  }
+
+  onVizTypeDoubleClick() {
+    if (!this.isBtnDisabled()) {
+      this.gotoSlice();
+    }
+  }
+
+  loadDatasources(search: string, page: number, pageSize: number) {
+    const query = rison.encode({
+      columns: [
+        'id',
+        'table_name',
+        'datasource_type',
+        'database.database_name',
+        'schema',
+      ],
+      filters: [{ col: 'table_name', opr: 'ct', value: search }],
+      page,
+      page_size: pageSize,
+      order_column: 'table_name',
+      order_direction: 'asc',
     });
-  };
+    return SupersetClient.get({
+      endpoint: `/api/v1/dataset/?q=${query}`,
+    }).then((response: JsonResponse) => {
+      const list: {
+        customLabel: ReactNode;
+        id: number;
+        label: string;
+        value: string;
+      }[] = response.json.result.map((item: Dataset) => ({
+        id: item.id,
+        value: `${item.id}__${item.datasource_type}`,
+        customLabel: DatasetSelectLabel(item),
+        label: item.table_name,
+      }));
+      return {
+        data: list,
+        totalCount: response.json.count,
+      };
+    });
+  }
 
   render() {
+
     return (
-      <StyledChartAdd>
-        <StyledChart>{this.renderPhotos()}</StyledChart>
-      </StyledChartAdd>
+
+                <DvtVizTypeGallery
+                  denyList={denyList}
+                  className="viz-gallery"
+                  onChange={this.changeVizType}
+                  onDoubleClick={this.onVizTypeDoubleClick}
+                  selectedViz={this.state.vizType}
+                />
+
     );
   }
 }
 
-export default withToasts(ChartAdd);
+export default withRouter(withToasts(ChartCreation));
