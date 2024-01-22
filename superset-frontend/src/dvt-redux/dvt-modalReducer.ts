76d6247ee9297bfeb8519029f9c3c4a1d98fc87c
Muhammed BABAN <93913650+Muhammed-baban@users.noreply.github.com>
2024-01-22 14:07:39 +0300

    error prettier completed

--- superset-frontend/src/dvt-redux/dvt-modalReducer.ts
+++ superset-frontend/src/dvt-redux/dvt-modalReducer.ts
@@ -19,35 +19,35 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface ModalState {
-    component: string;
-    lastComponent?: string;
-    title?: string;
-    meta?: any;
+  component: string;
+  lastComponent?: string;
+  title?: string;
+  meta?: any;
 }
 
 const initialState: ModalState = {
-    component: "",
-    lastComponent: "",
-    title: "",
-    meta: {}
+  component: '',
+  lastComponent: '',
+  title: '',
+  meta: {},
 };
 
 const dvtModalSlice = createSlice({
-    name: 'dvt-modal',
-    initialState,
-    reducers: {
-        openModal: (state, action: PayloadAction<ModalState>) => {
-            state.component = action.payload.component;
-            state.title = action.payload.title;
-            state.meta = action.payload.meta ? action.payload.meta : {};
-        },
-        closeModal: (state) => {
-            state.component = "";
-            state.lastComponent = state.component;
-            state.title = "";
-            state.meta = {};
-        },
+  name: 'dvt-modal',
+  initialState,
+  reducers: {
+    openModal: (state, action: PayloadAction<ModalState>) => {
+      state.component = action.payload.component;
+      state.title = action.payload.title;
+      state.meta = action.payload.meta ? action.payload.meta : {};
     },
+    closeModal: state => {
+      state.component = '';
+      state.lastComponent = state.component;
+      state.title = '';
+      state.meta = {};
+    },
+  },
 });
 
 export const { openModal, closeModal } = dvtModalSlice.actions;
