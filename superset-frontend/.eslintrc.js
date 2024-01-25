5efd51d059ee39d351f26b41713ad9af9dd66502
liamtorrelli <atleast24.7@list.ru>
2024-01-25 15:26:13 +0800

    Added ignore patterns

--- superset-frontend/.eslintrc.js
+++ superset-frontend/.eslintrc.js
@@ -1,21 +1,4 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+// DODO was here
 const packageConfig = require('./package');
 
 const importCoreModules = [];
@@ -26,13 +9,13 @@ Object.entries(packageConfig.dependencies).forEach(([pkg]) => {
 });
 
 // ignore files in production mode
-let ignorePatterns = [];
+// DODO changed
+let ignorePatterns = ['packages/generator-superset/**/*'];
 if (process.env.NODE_ENV === 'production') {
   ignorePatterns = [
     '*.test.{js,ts,jsx,tsx}',
     'plugins/**/test/**/*',
     'packages/**/test/**/*',
-    'packages/generator-superset/**/*',
   ];
 }
 
