b53e3c6428f8bd2e5ec12b881b65c0580133f7b8
Maxime Beauchemin <maximebeauchemin@gmail.com>
2024-01-25 11:00:39 -0800

    dd

--- scripts/docker_build_push.sh
+++ scripts/docker_build_push.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-    needs: config
-    if: needs.config.outputs.has-secrets == 'true'
 #
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
