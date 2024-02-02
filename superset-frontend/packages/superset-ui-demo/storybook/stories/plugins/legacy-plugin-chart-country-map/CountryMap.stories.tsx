326b52ad5baeb2bc487add1a9207da1ed637329e
Evan Rusackas <evan@rusackas.com>
2024-02-02 09:37:54 -0700

    partially fixed country map... still getting a weird 404

--- superset-frontend/packages/superset-ui-demo/storybook/stories/plugins/legacy-plugin-chart-country-map/CountryMap.stories.tsx
+++ superset-frontend/packages/superset-ui-demo/storybook/stories/plugins/legacy-plugin-chart-country-map/CountryMap.stories.tsx
@@ -1,21 +1,3 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 import React, { useEffect, useState } from 'react';
 import {
   JsonObject,
@@ -27,31 +9,32 @@ import {
 import CountryMapChartPlugin, {
   countries,
 } from '@superset-ui/legacy-plugin-chart-country-map';
-import { withKnobs, select } from '@storybook/addon-knobs';
 import { withResizableChartDemo } from '../../../shared/components/ResizableChartDemo';
 
 new CountryMapChartPlugin().configure({ key: 'country-map' }).register();
 
 export default {
-  title: 'Legacy Chart Plugins/legacy-plugin-chart-country-map',
-  decorators: [withKnobs, withResizableChartDemo],
+  title: 'Legacy Chart Plugins/CountryMap',
+  component: SuperChart,
+  decorators: [withResizableChartDemo],
+  args: {
+    country: 'france',
+    colorSchema: SequentialD3.find(x => x.id === 'schemeOranges')?.id,
+  },
+  argTypes: {
+    country: {
+      control: 'select',
+      options: Object.keys(countries),
+    },
+    colorSchema: {
+      control: 'select',
+      options: SequentialD3.map((x) => x.id),
+    },
+  },
 };
 
-function generateData(geojson: JsonObject) {
-  return geojson.features.map(feat => ({
-    metric: Math.round(seedRandom() * 10000) / 100,
-    country_id: feat.properties.ISO,
-  }));
-}
-
-export const basic = function BasicCountryMapStory({ width, height }) {
+export const BasicCountryMapStory = ({ width, height, country, colorSchema }) => {
   const theme = useTheme();
-  const country = select('Country', Object.keys(countries!), 'france');
-  const colorSchema = select<any>(
-    'Color schema',
-    SequentialD3,
-    SequentialD3.find(x => x.id === 'schemeOranges'),
-  );
   const [data, setData] = useState<JsonObject>();
 
   useEffect(() => {
@@ -62,9 +45,7 @@ export const basic = function BasicCountryMapStory({ width, height }) {
       .then(geojson => {
         setData(generateData(geojson));
       });
-    return () => {
-      controller.abort();
-    };
+    return () => controller.abort();
   }, [country]);
 
   if (!data) {
@@ -88,10 +69,17 @@ export const basic = function BasicCountryMapStory({ width, height }) {
       height={height}
       queriesData={[{ data }]}
       formData={{
-        linearColorScheme: colorSchema.id,
+        linearColorScheme: colorSchema,
         numberFormat: '.3s',
         selectCountry: country,
       }}
     />
   );
 };
+
+function generateData(geojson: JsonObject) {
+  return geojson.features.map(feat => ({
+    metric: Math.round(seedRandom() * 10000) / 100,
+    country_id: feat.properties.ISO,
+  }));
+}
