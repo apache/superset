729ba6396fea113ca5de176309eb9cd9fa6ca2ec
Antonio Rivero <antonioriverocode@gmail.com>
2024-02-05 20:34:47 +0100

    BigNumber with Time Comparison:
    - fix lint

--- superset-frontend/plugins/plugin-chart-period-over-period-kpi/src/plugin/index.ts
+++ superset-frontend/plugins/plugin-chart-period-over-period-kpi/src/plugin/index.ts
@@ -35,7 +35,8 @@ export default class PopKPIPlugin extends ChartPlugin {
    */
   constructor() {
     const metadata = new ChartMetadata({
-      description: 'Showcases a metric along with a comparison of value, change, and percent change for a selected time period.',
+      description:
+        'Showcases a metric along with a comparison of value, change, and percent change for a selected time period.',
       name: t('Big Number with Time Period Comparison'),
       thumbnail,
     });
