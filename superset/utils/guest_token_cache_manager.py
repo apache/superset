f4eceb49ef45fd9c635f6f078bb9a3f74934ac1b
febinct <82861700+febinct@users.noreply.github.com>
2024-02-07 00:51:41 +0530

    edge case handling

--- superset/utils/guest_token_cache_manager.py
+++ superset/utils/guest_token_cache_manager.py
@@ -1,5 +1,6 @@
 import os
 import uuid
+from typing import Optional
 
 import redis
 
@@ -10,6 +11,7 @@ class GuestTokenCacheManager:
         _redis_host = os.environ.get("REDIS_HOST", "localhost")
         _redis_port = os.environ.get("REDIS_PORT", 6379)
         _redis_db = os.environ.get("REDIS_DB", 6)
+        self._token_prefix = "superset::guest_token::"
 
         self._redis: redis.Redis = redis.Redis(
             host=_redis_host, port=_redis_port, db=_redis_db
@@ -17,15 +19,19 @@ class GuestTokenCacheManager:
 
     def _generate_token(self) -> str:
         random_id = str(uuid.uuid4()).replace("-", "")
-        while self._redis.get(random_id):
+        while self._redis.get(f"{self._token_prefix}{random_id}"):
             random_id = str(uuid.uuid4()).replace("-", "")
 
         return random_id
 
     def save_guest_token(self, raw_token: str, exp_sec: int) -> str:
         random_id = self._generate_token()
-        self._redis.set(name=random_id, value=raw_token, ex=exp_sec)
+        self._redis.setex(name=f"{self._token_prefix}{random_id}", value=raw_token, time=exp_sec)
         return random_id
 
-    def retrieve_guest_token(self, token_v2: str) -> str:
-        return self._redis.get(token_v2).decode('utf-8')
+    def retrieve_guest_token(self, token_v2: str) -> Optional[str]:
+        token_with_prefix = f"{self._token_prefix}{token_v2}"
+        token = self._redis.get(token_with_prefix)
+        if token:
+            return token.decode('utf-8')
+        return None
