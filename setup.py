534e8f394e96667a04a9f31e3fa2865b0786b2ce
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2024-02-07 15:58:19 +0000

    fix: bump FAB to 4.3.11 (#27039)

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
         "cryptography>=41.0.2, <41.1.0",
         "deprecation>=2.1.0, <2.2.0",
         "flask>=2.2.5, <3.0.0",
-        "flask-appbuilder>=4.3.10, <5.0.0",
+        "flask-appbuilder>=4.3.11, <5.0.0",
         "flask-caching>=2.1.0, <3",
         "flask-compress>=1.13, <2.0",
         "flask-talisman>=1.0.0, <2.0",
