b9972c1b713a1b9b84f6c53cfcc1dc0d234b6a3b
Evan Rusackas <evan@rusackas.com>
2024-02-13 21:42:33 -0700

    adding missing licenses

--- superset-frontend/src/components/MetadataBar/MetadataBarOverview.stories.tsx
+++ superset-frontend/src/components/MetadataBar/MetadataBarOverview.stories.tsx
@@ -1,4 +1,24 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import { Source } from '@storybook/addon-docs';
+import { Story } from '@storybook/addon-docs';
 import Markdown from 'markdown-to-jsx';
 import React from 'react';
 
@@ -24,9 +44,11 @@ Some of the common applications in Superset are:
   at while accessing the feature in the dashboard
 
 ## Basic example
-
-<Story id="design-system-components-metadatabar-examples--basic" />
-
+`}
+    </Markdown>
+    <Story id="design-system-components-metadatabar-examples--basic" />
+    <Markdown>
+      {`
 ## Variations
 
 The metadata bar is by default a static component (besides the links in text).
