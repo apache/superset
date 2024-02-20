48e2a69d58b294935506f5b0ad14c492073c3743
Daniel Gaspar <danielvazgaspar@gmail.com>
2024-02-20 15:01:30 +0000

    fix: CSRF exempt unit_tests

--- tests/unit_tests/security/api_test.py
+++ tests/unit_tests/security/api_test.py
@@ -28,5 +28,4 @@ def test_csrf_not_exempt(app_context: None) -> None:
     """
     Test that REST API is not exempt from CSRF.
     """
-    raise Exception(csrf._exempt_blueprints)
-    assert csrf._exempt_blueprints == {"MenuApi", "SecurityApi", "OpenApi"}
+    assert {blueprint.name for blueprint in csrf._exempt_blueprints} == {"MenuApi", "SecurityApi", "OpenApi"}
