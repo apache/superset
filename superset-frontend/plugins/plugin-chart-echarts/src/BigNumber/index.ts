dc95c35eb0837661ac985b8b5dd7d9ba023879ee
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2024-02-22 15:13:55 +0100

    export fix

--- superset-frontend/plugins/plugin-chart-echarts/src/BigNumber/index.ts
+++ superset-frontend/plugins/plugin-chart-echarts/src/BigNumber/index.ts
@@ -19,4 +19,4 @@
 
 export { default as BigNumberChartPlugin } from './BigNumberWithTrendline';
 export { default as BigNumberTotalChartPlugin } from './BigNumberTotal';
-export { PopKPIPlugin } from './BigNumberPeriodOverPeriod';
+export { default as PopKPIPlugin } from './BigNumberPeriodOverPeriod';
