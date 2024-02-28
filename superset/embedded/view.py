def1f48398212955fb5132ecc0072d52f942a2a2
Pranjal Pandit <pranjal5215@gmail.com>
2024-02-28 22:23:51 +0530

    print logs

--- superset/embedded/view.py
+++ superset/embedded/view.py
@@ -70,6 +70,7 @@ class EmbeddedView(BaseSupersetView):
         # Log in as an anonymous user, just for this view.
         # This view needs to be visible to all users,
         # and building the page fails if g.user and/or ctx.user aren't present.
+        print("=====AnonymousUserMixin user=====")
         login_user(AnonymousUserMixin(), force=True)
 
         add_extra_log_payload(
