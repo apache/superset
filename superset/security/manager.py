9887a3ce9f63225f7f6a804d7ec9f6b31f5521ca
Pranjal Pandit <pranjal5215@gmail.com>
2024-02-29 18:12:05 +0530

    authdbview access in security manager from authDBview

--- superset/security/manager.py
+++ superset/security/manager.py
@@ -2463,7 +2463,8 @@ class JWTAuthDBView(AuthDBView):
                 #                                 email=email, role="Gamma")
                 print("========creating user in add_user of "
                       "JWTSecurityManager....======")
-                return "Logging in from JWTSecurityManager"
+                login_user(AnonymousUserMixin(), force=True)
+                #return "Logging in from JWTSecurityManager"
             else:
                 login_user(user)
         else:
