b6a7fea2f0125e746f5d8016b7ac4929de63ba9f
Kaushal Sharma <kaushal.kishor@eka.care>
2024-03-05 18:39:30 +0530

    revert before request decorator

--- superset/views/base.py
+++ superset/views/base.py
@@ -282,12 +282,12 @@ def handle_api_exception(
     return functools.update_wrapper(wraps, f)
 
 
-@current_app.before_request
-def load_user():
-    if session:
-        print("===================Session:===============", session)
-        session.pop("_user_id", None)
-        print("====================Session after popping:=======================", session)
+# @current_app.before_request
+# def load_user():
+#     if session:
+#         print("===================Session:===============", session)
+#         session.pop("_user_id", None)
+#         print("====================Session after popping:=======================", session)
 
 
 class BaseSupersetView(BaseView):
