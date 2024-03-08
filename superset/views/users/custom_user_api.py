618cbbae661f6c8d741e3a540189bababa5d3141
Kaushal Sharma <kaushal.kishor@eka.care>
2024-03-08 15:51:39 +0530

    add custom user api

---
+++ superset/views/users/custom_user_api.py
@@ -0,0 +1,36 @@
+from flask import request, jsonify
+from flask_appbuilder.api import expose, protect, safe
+
+from superset.views.base import BaseSupersetView
+
+
+class CustomUserAPI(BaseSupersetView):
+    route_base = '/api/v1/'
+
+    @expose("/add_user/", methods=("POST",))
+    @protect()
+    @safe
+    def add_user(self):
+        try:
+            body = request.json
+            username = body.get("username")
+            first_name = body.get("first_name")
+            last_name = body.get("last_name")
+            email = body.get("email")
+            role_id = body.get("role_id")  # Use role_id instead of role_name
+
+            # Ensure role exists and is valid
+            role = self.appbuilder.sm.find_role("Doctor")
+            if not role:
+                return jsonify({"success": False, "message": "Role not found"}), 400
+
+            # Create user with the specified role
+            user = self.appbuilder.sm.add_user(username, first_name, last_name, email,
+                                               role)
+            if user:
+                return jsonify(
+                    {"success": True, "message": "User added successfully"}), 200
+            else:
+                return jsonify({"success": False, "message": "Failed to add user"}), 400
+        except Exception as error:
+            return jsonify({"success": False, "message": str(error)}), 400
