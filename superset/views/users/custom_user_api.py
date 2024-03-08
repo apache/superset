6c4175736e5d051a6d5d213f4b9fdb244173db7f
Kaushal Sharma <kaushal.kishor@eka.care>
2024-03-08 17:16:28 +0530

    add is_active flag

--- superset/views/users/custom_user_api.py
+++ superset/views/users/custom_user_api.py
@@ -18,7 +18,7 @@ class CustomUserAPI(BaseSupersetView):
             first_name = body.get("first_name")
             last_name = body.get("last_name")
             email = body.get("email")
-            role_id = body.get("role_id")  # Use role_id instead of role_name
+            is_active = body.get("active")
 
             # Ensure role exists and is valid
             role = self.appbuilder.sm.find_role("Doctor")
@@ -27,7 +27,7 @@ class CustomUserAPI(BaseSupersetView):
 
             # Create user with the specified role
             user = self.appbuilder.sm.add_user(username, first_name, last_name, email,
-                                               role)
+                                               role, is_active)
             if user:
                 return jsonify(
                     {"success": True, "message": "User added successfully"}), 200
