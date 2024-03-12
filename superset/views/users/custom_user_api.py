958b45d28f42618f7b623a114bfbd1df58dd817b
Kaushal Sharma <kaushal.kishor@eka.care>
2024-03-12 18:33:57 +0530

    add update user role api

--- superset/views/users/custom_user_api.py
+++ superset/views/users/custom_user_api.py
@@ -36,6 +36,33 @@ class CustomUserAPI(BaseSupersetView):
         except Exception as error:
             return jsonify({"success": False, "message": str(error)}), 400
 
+    @expose("/update_user_role/", methods=("POST",))
+    @safe
+    def update_user_role(self):
+        try:
+            body = request.json
+            username = body.get("username")
+            new_role_name = body.get("role_name")
+
+            # Find the user
+            user = self.appbuilder.sm.find_user(username=username)
+            if not user:
+                return jsonify({"success": False, "message": "User not found"}), 400
+
+            # Find the new role
+            new_role = self.appbuilder.sm.find_role(new_role_name)
+            if not new_role:
+                return jsonify({"success": False, "message": "Role not found"}), 400
+
+            # Update the user's roles
+            user.roles = [new_role]
+            self.appbuilder.sm.update_user(user)
+
+            return jsonify({"success": True,
+                            "message": f"{user.username}'s role updated to {new_role_name}"}), 200
+        except Exception as error:
+            return jsonify({"success": False, "message": str(error)}), 400
+
     def response_403(self):
         """
         Custom response for 403 Forbidden errors.
