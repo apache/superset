15f16b98718f1eef9e294630e847caa4def01dfa
Maxime Beauchemin <maximebeauchemin@gmail.com>
2024-03-11 23:41:59 -0700

    missing license headers

--- requirements/pip-compile-superset.sh
+++ requirements/pip-compile-superset.sh
@@ -1,8 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Forward all script arguments to pip-compile
-# you can pass things like `--no-upgrade` or target a specific package
-# with `-P "flask"`
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# A simple bash script to "compile"/pin our python dependencies using pip-compile
+
+# Let's forward all script arguments to pip-compile
+# you can pass things like `--no-upgrade` or target a specific package with `-P "flask"`
 pip_compile_flags="$@"
 
 # Compile the base requirements
