f1184bcad013a0e23004665838c1674c2d54bc9b
Maxime Beauchemin <maximebeauchemin@gmail.com>
2024-03-11 22:35:05 -0700

    more progress

--- requirements/compile.sh
+++ requirements/compile.sh
@@ -1,6 +1,5 @@
-pip-compile -o requirements/base.txt requirements/base.in
+pip-compile -o requirements/base.txt
 pip-compile -o requirements/development.txt  -v \
-    --extra bigquery,druid,hive,gevent,mysql,postgres,presto,prophet,trino,gsheets,playwright,thumbnails \
-    requirements/base.in
+    --extra bigquery,development,druid,hive,gevent,mysql,postgres,presto,prophet,trino,gsheets,playwright,thumbnails \
 echo "-e ." >> requirements/base.txt
 echo "-e ." >> requirements/development.txt
