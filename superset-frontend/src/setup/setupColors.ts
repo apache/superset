42bf87aeddd25634ecf484cbff044f40243c6ede
liamtorrelli <atleast24.7@list.ru>
2024-03-13 21:25:33 +0800

    Added an example color scheme

--- superset-frontend/src/setup/setupColors.ts
+++ superset-frontend/src/setup/setupColors.ts
@@ -1,21 +1,4 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+// DODO was here
 import {
   CategoricalScheme,
   ColorScheme,
@@ -31,6 +14,7 @@ import {
   CategoricalLyft,
   CategoricalPreset,
   CategoricalSuperset,
+  CategoricalDodoCustom,
   SequentialCommon,
   SequentialD3,
   ColorSchemeRegistry,
@@ -71,6 +55,7 @@ export default function setupColors(
       ...CategoricalLyft,
       ...CategoricalPreset,
       ...CategoricalSuperset,
+      ...CategoricalDodoCustom,
       ...extraCategoricalColorSchemes,
     ],
     'supersetColors',
