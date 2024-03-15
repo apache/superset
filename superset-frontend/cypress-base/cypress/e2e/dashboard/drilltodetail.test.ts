92a476076ab349892ca1f3a629b0e80fad812235
Sam Firke <SFirke@a2gov.org>
2024-03-15 13:42:44 +0000

    update test for new sort order

--- superset-frontend/cypress-base/cypress/e2e/dashboard/drilltodetail.test.ts
+++ superset-frontend/cypress-base/cypress/e2e/dashboard/drilltodetail.test.ts
@@ -179,7 +179,7 @@ describe.skip('Drill to detail modal', () => {
         cy.on('uncaught:exception', () => false);
         cy.wait('@samples');
         cy.get('.virtual-table-cell').should($rows => {
-          expect($rows).to.contain('Kelly');
+          expect($rows).to.contain('Charles');
         });
 
         // verify scroll top on pagination
