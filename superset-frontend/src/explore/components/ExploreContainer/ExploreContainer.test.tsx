9c26fa3dae162fc80fc757e9650729409b3ffb83
justinpark <justin.hojun.park@gmail.com>
2024-03-19 09:46:29 -0700

    fix unavailable sorting issue

--- superset-frontend/src/explore/components/ExploreContainer/ExploreContainer.test.tsx
+++ superset-frontend/src/explore/components/ExploreContainer/ExploreContainer.test.tsx
@@ -21,6 +21,7 @@ import { fireEvent, render } from 'spec/helpers/testing-library';
 import { OptionControlLabel } from 'src/explore/components/controls/OptionControls';
 
 import ExploreContainer, { DraggingContext } from '.';
+import OptionWrapper from '../controls/DndColumnSelectControl/OptionWrapper';
 
 const MockChildren = () => {
   const dragging = React.useContext(DraggingContext);
@@ -59,10 +60,12 @@ test('should update the style on dragging state', () => {
         index={1}
         label={<span>Label 1</span>}
       />
-      <OptionControlLabel
+      <OptionWrapper
         {...defaultProps}
         index={2}
-        label={<span>Label 2</span>}
+        label="Label 2"
+        clickClose={() => {}}
+        onShiftOptions={() => {}}
       />
       <MockChildren />
     </ExploreContainer>,
@@ -75,5 +78,8 @@ test('should update the style on dragging state', () => {
   fireEvent.dragStart(getByText('Label 1'));
   expect(container.getElementsByClassName('dragging')).toHaveLength(1);
   fireEvent.dragEnd(getByText('Label 1'));
+  expect(container.getElementsByClassName('dragging')).toHaveLength(0);
+  // don't show dragging state for the sorting item
+  fireEvent.dragStart(getByText('Label 2'));
   expect(container.getElementsByClassName('dragging')).toHaveLength(0);
 });
