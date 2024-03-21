89ffd273ed1c49c13ce9b981d2260b786924b70c
Maxime Beauchemin <maximebeauchemin@gmail.com>
2024-03-21 11:52:35 -0700

    more tests

--- .github/supersetbot/src/cli.test.js
+++ .github/supersetbot/src/cli.test.js
@@ -1,19 +1,12 @@
 import { spawnSync } from 'child_process';
 
 describe('CLI Test', () => {
-  it('should contain "--target dev" in the output when calling "supersetbot docker --preset dev --dry-run"', () => {
-    // Run the CLI command
-    const result = spawnSync('./src/supersetbot', ['docker', '--preset', 'dev', '--dry-run']);
-
-    if (result.error) {
-      // Handle the error, for example, by failing the test
-      throw new Error(result.error.message);
-    }
-
-    // Convert the stdout buffer to a string
+  test.each([
+    ['./src/supersetbot', ['docker', '--preset', 'dev', '--dry-run'], '--target dev'],
+    ['./src/supersetbot', ['docker', '--dry-run'], '--target lean'],
+  ])('returns %s for release %s', (command, arg, contains) => {
+    const result = spawnSync(command, arg);
     const output = result.stdout.toString();
-
-    // Check if the output contains "--target dev"
-    expect(output).toContain('--target dev');
+    expect(result.stdout.toString()).toContain(contains);
   });
 });
