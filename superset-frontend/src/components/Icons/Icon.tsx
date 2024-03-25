05e2b7a83a3697c5bf2eeb05a47fd2ea65123eef
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2024-03-25 13:26:49 -0700

    refactor(accessibility): remove unnecessary role="button" Icon changes

--- superset-frontend/src/components/Icons/Icon.tsx
+++ superset-frontend/src/components/Icons/Icon.tsx
@@ -47,7 +47,7 @@ export interface IconProps extends IconType {
 }
 
 export const Icon = (props: IconProps) => {
-  const { fileName, onClick, ...iconProps } = props;
+  const { fileName, ...iconProps } = props;
   const [, setLoaded] = useState(false);
   const ImportedSVG = useRef<React.FC<React.SVGProps<SVGSVGElement>>>();
   const name = fileName.replace('_', '-');
@@ -70,7 +70,6 @@ export const Icon = (props: IconProps) => {
 
   return (
     <StyledIcon
-      role={onClick ? 'button' : undefined}
       component={ImportedSVG.current || TransparentIcon}
       aria-label={name}
       {...iconProps}
