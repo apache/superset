f9ac0622eabe338ad0dd0353d5ea8d812f8633cc
Maxime Beauchemin <maximebeauchemin@gmail.com>
2024-03-25 14:47:43 -0700

    remove unused

--- .github/workflows/bashlib.sh
+++ .github/workflows/bashlib.sh
@@ -31,22 +31,6 @@ say() {
   fi
 }
 
-# default command to run when the `run` input is empty
-default-setup-command() {
-  apt-get-install
-  pip-upgrade
-}
-
-apt-get-install() {
-  say "::group::apt-get install dependencies"
-  sudo apt-get update && sudo apt-get install --yes \
-    libsasl2-dev \
-    libldap2-dev \
-    libffi-dev \
-    libssl-dev
-  say "::endgroup::"
-}
-
 pip-upgrade() {
   say "::group::Upgrade pip"
   pip install --upgrade pip
