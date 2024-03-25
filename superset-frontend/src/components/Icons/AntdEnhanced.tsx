7c9739643541599fd0d16f716f1be8f16356655c
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2024-03-25 13:26:49 -0700

    refactor(accessibility): remove unnecessary role="button" Icon changes

--- superset-frontend/src/components/Icons/AntdEnhanced.tsx
+++ superset-frontend/src/components/Icons/AntdEnhanced.tsx
@@ -26,11 +26,7 @@ const AntdEnhancedIcons = Object.keys(AntdIcons)
   .filter(k => !k.includes('TwoTone'))
   .map(k => ({
     [k]: (props: IconType) => (
-      <StyledIcon
-        component={AntdIcons[k]}
-        role={props.onClick ? 'button' : undefined}
-        {...props}
-      />
+      <StyledIcon component={AntdIcons[k]} {...props} />
     ),
   }))
   .reduce((l, r) => ({ ...l, ...r }));
