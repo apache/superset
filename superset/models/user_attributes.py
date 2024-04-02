34b68174b657c0278b403697c246355e24d62525
Maxime Beauchemin <maximebeauchemin@gmail.com>
2024-04-01 18:09:11 -0700

    feat: Slack Avatar integration
    Our current Avatars are a little dry, using a limited variety of colors
    and showing the user's initials. The app could use a little more
    personality.
    
    Now given we already have a slack integration and administrators can
    configure their `SLACK_API_TOKEN`, I was thinking we can use this
    integration to get Slack's avatars integrated in Superset. Given the
    popularity of Slack and users familiarity with the avatars there,
    I thought it'd be neat to bring a bit more color in Superset.
    
    The approach here is centered around a new endpoint
    `/api/v1/user/{id}/avatar.png`, which fetches from a new column in
    UserAttribute (our one-to-one extension to FAB's User model) for
    avatar_url, and redirects to the destination. When loading for the
    first time, if information is empty and Slack is configured, we ask
    Slack for the URL, which is secret, but serves the image from outside
    the authentication part of their API.

--- superset/models/user_attributes.py
+++ superset/models/user_attributes.py
@@ -14,8 +14,9 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+
 from flask_appbuilder import Model
-from sqlalchemy import Column, ForeignKey, Integer
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
 from superset import security_manager
@@ -39,6 +40,6 @@ class UserAttribute(Model, AuditMixinNullable):
     user = relationship(
         security_manager.user_model, backref="extra_attributes", foreign_keys=[user_id]
     )
-
     welcome_dashboard_id = Column(Integer, ForeignKey("dashboards.id"))
     welcome_dashboard = relationship("Dashboard")
+    avatar_url = Column(String(100))
