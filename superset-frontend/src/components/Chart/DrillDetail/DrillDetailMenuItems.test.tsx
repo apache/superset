34b1db219cbe155a9cf57f7c9abb36492c482106
Elizabeth Thompson <eschutho@gmail.com>
2024-04-08 09:40:57 -0700

    feat(accessibility): add tabbing to chart menu in dashboard (#26138)
    Co-authored-by: geido <diegopucci.me@gmail.com>
    Co-authored-by: Diego Pucci <geido@Diegos-MBP.wind3.hub>

--- superset-frontend/src/components/Chart/DrillDetail/DrillDetailMenuItems.test.tsx
+++ superset-frontend/src/components/Chart/DrillDetail/DrillDetailMenuItems.test.tsx
@@ -69,6 +69,28 @@ const filterB: BinaryQueryObjectFilterClause = {
   formattedVal: 'Two days ago',
 };
 
+const MockRenderChart = ({
+  chartId,
+  formData,
+  isContextMenu,
+  filters,
+}: Partial<DrillDetailMenuItemsProps>) => {
+  const [showMenu, setShowMenu] = React.useState(false);
+
+  return (
+    <Menu>
+      <DrillDetailMenuItems
+        chartId={chartId ?? defaultChartId}
+        formData={formData ?? defaultFormData}
+        filters={filters}
+        isContextMenu={isContextMenu}
+        showModal={showMenu}
+        setShowModal={setShowMenu}
+      />
+    </Menu>
+  );
+};
+
 const renderMenu = ({
   chartId,
   formData,
@@ -77,14 +99,12 @@ const renderMenu = ({
 }: Partial<DrillDetailMenuItemsProps>) => {
   const store = getMockStoreWithNativeFilters();
   return render(
-    <Menu>
-      <DrillDetailMenuItems
-        chartId={chartId ?? defaultChartId}
-        formData={formData ?? defaultFormData}
-        filters={filters}
-        isContextMenu={isContextMenu}
-      />
-    </Menu>,
+    <MockRenderChart
+      chartId={chartId}
+      formData={formData}
+      isContextMenu={isContextMenu}
+      filters={filters}
+    />,
     { useRouter: true, useRedux: true, store },
   );
 };
