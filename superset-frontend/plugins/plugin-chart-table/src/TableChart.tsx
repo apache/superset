d6e0ff5879a11c7a51f7e0b79932088bad4ba6b8
Geido <60598000+geido@users.noreply.github.com>
2024-04-10 12:10:13 +0200

    fix(Dashboard): Add aria-label to filters and search forms (#27968)
    (cherry picked from commit 4202fba0f1da1d4f785c479c5972ee4dc4846e3d)

--- superset-frontend/plugins/plugin-chart-table/src/TableChart.tsx
+++ superset-frontend/plugins/plugin-chart-table/src/TableChart.tsx
@@ -168,6 +168,7 @@ function SearchInput({ count, value, onChange }: SearchInputProps) {
         className="form-control input-sm"
         placeholder={tn('search.num_records', count)}
         value={value}
+        aria-label={t('Search %s records', count)}
         onChange={onChange}
       />
     </span>
