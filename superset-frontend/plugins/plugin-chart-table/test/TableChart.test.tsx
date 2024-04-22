608f47e26cfd412e85f0cbc42c92b044200471d0
Ross Mabbett <92495987+rtexelm@users.noreply.github.com>
2024-04-22 16:27:03 -0400

    chore(TableTimeComparison): Add test for currency removal on percent columns (#28175)

--- superset-frontend/plugins/plugin-chart-table/test/TableChart.test.tsx
+++ superset-frontend/plugins/plugin-chart-table/test/TableChart.test.tsx
@@ -66,6 +66,18 @@ describe('plugin-chart-table', () => {
       expect(String(parsedDate)).toBe('2020-01-01 12:34:56');
       expect(parsedDate.getTime()).toBe(1577882096000);
     });
+
+    it('does not include currencyFormat in percent columns', () => {
+      const transformedProps = transformProps(testData.advancedWithCurrency);
+      const transformedColumns = transformedProps.columns;
+      const percentColumns = transformedColumns.filter(
+        col => col.isPercentMetric,
+      );
+      expect(percentColumns.length).toBeGreaterThan(0);
+      percentColumns.forEach(col => {
+        expect(col?.config?.currencyFormat).toBeUndefined();
+      });
+    });
   });
 
   describe('TableChart', () => {
