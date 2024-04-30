a38dc90abe97c34c24990d9ad11df55b69464f97
Ross Mabbett <92495987+rtexelm@users.noreply.github.com>
2024-04-30 09:07:55 -0400

    fix(Webpack dev-sever warnings): Add ignoreWarning to webpack config for @data-ui error (#28232)

--- superset-frontend/webpack.config.js
+++ superset-frontend/webpack.config.js
@@ -214,6 +214,19 @@ const config = {
   },
   output,
   stats: 'minimal',
+  /*
+   Silence warning for missing export in @data-ui's internal structure. This
+   issue arises from an internal implementation detail of @data-ui. As it's
+   non-critical, we suppress it to prevent unnecessary clutter in the build
+   output. For more context, refer to:
+   https://github.com/williaster/data-ui/issues/208#issuecomment-946966712
+   */
+  ignoreWarnings: [
+    {
+      message:
+        /export 'withTooltipPropTypes' \(imported as 'vxTooltipPropTypes'\) was not found/,
+    },
+  ],
   performance: {
     assetFilter(assetFilename) {
       // don't throw size limit warning on geojson and font files
