d727f582475ad5ce5a70bfaf5f1a395247c3ebe0
artem12345-png <78581804+artem12345-png@users.noreply.github.com>
2024-05-06 09:49:08 +0700

    Fix drill to detail (#77)
    * воспользовался фиксом от апачи
    
    * сделал логирование debug
    
    * сделал изменение ззапроса, если в нем есть "as "COUNT(*)""
    
    * убрал debug логи
    
    ---------
    
    Co-authored-by: artemaslamov <aslamov@promsoft.ru>

--- superset/views/datasource/utils.py
+++ superset/views/datasource/utils.py
@@ -15,6 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 from typing import Any, Optional
+import logging
 
 from superset import app, db
 from superset.common.chart_data import ChartDataResultType
@@ -25,6 +26,9 @@ from superset.daos.datasource import DatasourceDAO
 from superset.datasets.commands.exceptions import DatasetSamplesFailedError
 from superset.utils.core import QueryStatus
 from superset.views.datasource.schemas import SamplesPayloadSchema
+
+
+logger = logging.getLogger(__name__)
 
 
 def get_limit_clause(page: Optional[int], per_page: Optional[int]) -> dict[str, int]:
@@ -43,7 +47,7 @@ def get_limit_clause(page: Optional[int], per_page: Optional[int]) -> dict[str, 
     return {"row_offset": offset, "row_limit": limit}
 
 
-def get_samples(  # pylint: disable=too-many-arguments,too-many-locals
+def get_samples(  # pylint: disable=too-many-arguments
     datasource_type: str,
     datasource_id: int,
     force: bool = False,
@@ -104,21 +108,17 @@ def get_samples(  # pylint: disable=too-many-arguments,too-many-locals
         result_type=ChartDataResultType.FULL,
         force=force,
     )
-    samples_results = samples_instance.get_payload()
-    count_star_results = count_star_instance.get_payload()
 
     try:
-        sample_data = samples_results["queries"][0]
-        count_star_data = count_star_results["queries"][0]
-        failed_status = (
-            sample_data.get("status") == QueryStatus.FAILED
-            or count_star_data.get("status") == QueryStatus.FAILED
-        )
-        error_msg = sample_data.get("error") or count_star_data.get("error")
-        if failed_status and error_msg:
-            cache_key = sample_data.get("cache_key")
-            QueryCacheManager.delete(cache_key, region=CacheRegion.DATA)
-            raise DatasetSamplesFailedError(error_msg)
+        count_star_data = count_star_instance.get_payload()["queries"][0]
+
+        if count_star_data.get("status") == QueryStatus.FAILED:
+            raise DatasetSamplesFailedError(count_star_data.get("error"))
+
+        sample_data = samples_instance.get_payload()["queries"][0]
+        if sample_data.get("status") == QueryStatus.FAILED:
+            QueryCacheManager.delete(count_star_data.get("cache_key"), CacheRegion.DATA)
+            raise DatasetSamplesFailedError(sample_data.get("error"))
 
         sample_data["page"] = page
         sample_data["per_page"] = per_page
