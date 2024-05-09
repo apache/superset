8bf84e5bf6842fe49ea08ce3cc14042f9efba619
Daniel Vaz Gaspar <danielvazgaspar@gmail.com>
2024-05-09 14:44:24 +0100

    fix: type annotation breaking on py3.9 (#28396)
    (cherry picked from commit b224b8331ab7ce804be358675c20c340522663c3)

--- superset/utils/pandas_postprocessing/contribution.py
+++ superset/utils/pandas_postprocessing/contribution.py
@@ -14,6 +14,8 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+from __future__ import annotations
+
 from decimal import Decimal
 from typing import Any
 
