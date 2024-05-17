913c6cea8efd8fb07dfa5f76a3bc1c0386f4386f
Jordan Zucker <jordan.zucker@gmail.com>
2024-05-16 23:10:33 -0700

    Super curious about this

--- superset/models/core.py
+++ superset/models/core.py
@@ -179,6 +179,7 @@ class Database(Model, AuditMixinNullable, ImportExportMixin):  # pylint: disable
         "external_url",
         "encrypted_extra",
         "impersonate_user",
+        "allow_multi_catalog",
     ]
     export_children = ["tables"]
 
