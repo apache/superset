34350005be5f9265f58861b8e1a7971b9a679227
Petro Lozhkin <peter.lozhkin@gmail.com>
2024-05-20 18:15:12 +0300

    added events listeners to fix chunk error #28259

--- superset-frontend/src/setup/setupApp.ts
+++ superset-frontend/src/setup/setupApp.ts
@@ -87,6 +87,27 @@ export default function setupApp() {
         window.location.reload();
       });
     });
+    // Chuck errors fix: https://github.com/apache/superset/issues/28259
+    window.addEventListener('error', e => {
+      // prompt user to confirm refresh
+      if (/Loading chunk [\d]+ failed/.test(e.message)) {
+        alert(
+          'A new version released. Need to reload the page to apply changes.',
+        );
+        window.location.reload();
+      }
+    });
+    const origin = console.error;
+    console.error = error => {
+      if (/Loading chunk [\d]+ failed/.test(error.message)) {
+        alert(
+          'A new version released. Need to reload the page to apply changes.',
+        );
+        window.location.reload();
+      } else {
+        origin(error);
+      }
+    };
   });
 
   // A set of hacks to allow apps to run within a FAB template
