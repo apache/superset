9ad1ead4587cac4a28994c2a99308fc33bb38c43
Vitor Avila <vitor.avila@preset.io>
2024-06-07 19:29:02 -0300

    fix: Improving handling for tag relationship when deleting assets

--- superset/models/sql_lab.py
+++ superset/models/sql_lab.py
@@ -415,10 +415,11 @@ class SavedQuery(
     tags = relationship(
         "Tag",
         secondary="tagged_object",
-        overlaps="tags",
-        primaryjoin="and_(SavedQuery.id == TaggedObject.object_id)",
-        secondaryjoin="and_(TaggedObject.tag_id == Tag.id, "
+        overlaps="objects,tag,tags",
+        primaryjoin="and_(SavedQuery.id == TaggedObject.object_id, "
         "TaggedObject.object_type == 'query')",
+        secondaryjoin="and_(TaggedObject.tag_id == Tag.id)",
+        passive_deletes=True,
     )
 
     export_parent = "database"
