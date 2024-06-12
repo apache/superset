3b07a2e169d5719db3fea71a68f2c201bd047199
lilykuang <jialikuang@gmail.com>
2024-06-11 18:25:53 -0700

    TimeComparison: update test coverage for customTimeRangeDecode

--- superset-frontend/packages/superset-ui-core/test/time-comparison/customTimeRangeDecode.test.ts
+++ superset-frontend/packages/superset-ui-core/test/time-comparison/customTimeRangeDecode.test.ts
@@ -127,4 +127,79 @@ describe('customTimeRangeDecode', () => {
       matchedFlag: true,
     });
   });
+
+  it('6) specific : relative (now)', () => {
+    expect(
+      customTimeRangeDecode('now : DATEADD(DATETIME("now"), 7, day)'),
+    ).toEqual({
+      customRange: {
+        sinceDatetime: 'now',
+        sinceMode: 'now',
+        sinceGrain: 'day',
+        sinceGrainValue: -7,
+        untilDatetime: 'now',
+        untilMode: 'relative',
+        untilGrain: 'day',
+        untilGrainValue: 7,
+        anchorMode: 'now',
+        anchorValue: 'now',
+      },
+      matchedFlag: true,
+    });
+  });
+
+  it('7) default', () => {
+    const SEVEN_DAYS_AGO = new Date();
+    const MIDNIGHT = new Date();
+    SEVEN_DAYS_AGO.setUTCHours(0, 0, 0, 0);
+    MIDNIGHT.setUTCHours(0, 0, 0, 0);
+    expect(
+      customTimeRangeDecode('now : DATEADD(DATETIME("TODAY"), -7, day)'),
+    ).toEqual({
+      customRange: {
+        sinceDatetime: SEVEN_DAYS_AGO.setUTCDate(
+          SEVEN_DAYS_AGO.getUTCDate() - 7,
+        ).toString(),
+        sinceMode: 'relative',
+        sinceGrain: 'day',
+        sinceGrainValue: -7,
+        untilDatetime: MIDNIGHT.toString(),
+        untilMode: 'specific',
+        untilGrain: 'day',
+        untilGrainValue: 7,
+        anchorMode: 'now',
+        anchorValue: 'now',
+      },
+      matchedFlag: false,
+    });
+  });
+
+  it('8) relative : relative return default', () => {
+    const SEVEN_DAYS_AGO = new Date();
+    SEVEN_DAYS_AGO.setUTCHours(0, 0, 0, 0);
+
+    const MIDNIGHT = new Date();
+    MIDNIGHT.setUTCHours(0, 0, 0, 0);
+    expect(
+      customTimeRangeDecode(
+        'DATEADD(DATETIME("2021-01-26T00:00:00"), -55, day) : DATEADD(DATETIME("2021-01-27T00:00:00"), 7, day)',
+      ),
+    ).toEqual({
+      customRange: {
+        sinceDatetime: SEVEN_DAYS_AGO.setUTCDate(
+          SEVEN_DAYS_AGO.getUTCDate() - 7,
+        ).toString(),
+        sinceMode: 'relative',
+        sinceGrain: 'day',
+        sinceGrainValue: -7,
+        untilDatetime: MIDNIGHT.toString(),
+        untilMode: 'specific',
+        untilGrain: 'day',
+        untilGrainValue: 7,
+        anchorMode: 'now',
+        anchorValue: 'now',
+      },
+      matchedFlag: false,
+    });
+  });
 });
