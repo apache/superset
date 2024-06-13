88c2b785cbc9233786f95ad750f234d59fb904ca
Michael S. Molina <michael.s.molina@gmail.com>
2024-06-13 16:04:12 -0300

    Fixes frontend tests

--- superset-frontend/src/components/Chart/DrillBy/DrillByMenuItems.test.tsx
+++ superset-frontend/src/components/Chart/DrillBy/DrillByMenuItems.test.tsx
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+import { ReactChild } from 'react';
 import userEvent from '@testing-library/user-event';
 import {
   Behavior,
@@ -31,10 +32,23 @@ import { DrillByMenuItems, DrillByMenuItemsProps } from './DrillByMenuItems';
 
 /* eslint jest/expect-expect: ["warn", { "assertFunctionNames": ["expect*"] }] */
 
-const DATASET_ENDPOINT = 'glob:*/api/v1/dataset/7';
+const DATASET_ENDPOINT = 'glob:*/api/v1/dataset/7*';
 const CHART_DATA_ENDPOINT = 'glob:*/api/v1/chart/data*';
 const FORM_DATA_KEY_ENDPOINT = 'glob:*/api/v1/explore/form_data';
 const { form_data: defaultFormData } = chartQueries[sliceId];
+
+jest.mock(
+  'react-virtualized-auto-sizer',
+  () =>
+    ({ children }: { children: (params: { height: number }) => ReactChild }) =>
+      children({ height: 500 }),
+);
+
+jest.mock('lodash/debounce', () => (fn: Function & { debounce: Function }) => {
+  // eslint-disable-next-line no-param-reassign
+  fn.debounce = jest.fn();
+  return fn;
+});
 
 const defaultColumns = [
   { column_name: 'col1', groupby: true },
