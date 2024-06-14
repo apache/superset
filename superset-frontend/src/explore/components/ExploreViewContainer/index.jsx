ac9316af76ac6bafd600e961d985e3a636f70925
rtexelm <texelist@gmail.com>
2024-06-13 23:38:18 -0400

    Fix small errors in component, add tests

--- superset-frontend/src/explore/components/ExploreViewContainer/index.jsx
+++ superset-frontend/src/explore/components/ExploreViewContainer/index.jsx
@@ -711,11 +711,10 @@ function ExploreViewContainer(props) {
 
 ExploreViewContainer.propTypes = propTypes;
 
-const retainQueryModeRequirements = hiddenFormData => {
+const retainQueryModeRequirements = hiddenFormData =>
   Object.keys(hiddenFormData ?? {}).filter(
     key => !QUERY_MODE_REQUISITES.has(key),
   );
-};
 
 function mapStateToProps(state) {
   const {
@@ -732,7 +731,7 @@ function mapStateToProps(state) {
   const hasQueryMode = !!controls.query_mode?.value;
   const fieldsToOmit = hasQueryMode
     ? retainQueryModeRequirements(hiddenFormData)
-    : hiddenFormData;
+    : Object.keys(hiddenFormData ?? {});
   const form_data = omit(getFormDataFromControls(controls), fieldsToOmit);
   const slice_id = form_data.slice_id ?? slice?.slice_id ?? 0; // 0 - unsaved chart
   form_data.extra_form_data = mergeExtraFormData(
