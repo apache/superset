e274925b2a975165b4c22e87860075c90d7e4832
Đỗ Trọng Hải <41283691+hainenber@users.noreply.github.com>
2024-06-29 02:45:38 +0700

    chore(frontend): remove obsolete ESLint rules in tests (#29405)
    Signed-off-by: hainenber <dotronghai96@gmail.com>

--- superset-frontend/src/explore/components/controls/TextAreaControl.test.jsx
+++ superset-frontend/src/explore/components/controls/TextAreaControl.test.jsx
@@ -16,7 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-/* eslint-disable no-unused-expressions */
 import sinon from 'sinon';
 import { styledMount as mount } from 'spec/helpers/theming';
 import { TextAreaEditor } from 'src/components/AsyncAceEditor';
