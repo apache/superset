3975bf585b4875e1efccc7b99c9fdbc00ebc1549
Vitor Avila <vitor.avila@preset.io>
2024-07-11 11:18:33 -0300

    Add missing import for newer type hint syntax

--- superset/tags/filters.py
+++ superset/tags/filters.py
@@ -14,6 +14,8 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+from __future__ import annotations
+
 from typing import Any
 
 from flask_babel import lazy_gettext as _
