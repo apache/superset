84b2188750c12e48b421350a3f8400a6703596a7
Michael S. Molina <michael.s.molina@gmail.com>
2024-07-15 17:01:36 -0300

    refactor: Removes dead code of Word Cloud plugin

--- superset-frontend/plugins/plugin-chart-word-cloud/test/buildQuery.test.ts
+++ superset-frontend/plugins/plugin-chart-word-cloud/test/buildQuery.test.ts
@@ -16,3 +16,21 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+import { WordCloudFormData } from '../src';
+import buildQuery from '../src/plugin/buildQuery';
+
+describe('WordCloud buildQuery', () => {
+  const formData: WordCloudFormData = {
+    datasource: '5__table',
+    granularity_sqla: 'ds',
+    series: 'foo',
+    viz_type: 'word_cloud',
+  };
+
+  it('should build columns from series in form data', () => {
+    const queryContext = buildQuery(formData);
+    const [query] = queryContext.queries;
+    expect(query.columns).toEqual(['foo']);
+  });
+});
