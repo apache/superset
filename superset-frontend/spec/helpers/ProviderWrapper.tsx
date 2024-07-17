e3fce0a48a8604ec4a64621822bba6cc407420f1
Evan Rusackas <evan@rusackas.com>
2024-07-17 15:17:10 -0600

    Adding AntdThemeWrapper to ProviderWrapper for testing purposes

--- superset-frontend/spec/helpers/ProviderWrapper.tsx
+++ superset-frontend/spec/helpers/ProviderWrapper.tsx
@@ -20,20 +20,23 @@
 import { ThemeProvider } from '@superset-ui/core';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { QueryParamProvider } from 'use-query-params';
+import { AntdThemeProvider } from 'src/components/AntdThemeProvider';
 
 export function ProviderWrapper(props: any) {
   const { children, theme } = props;
 
   return (
     <ThemeProvider theme={theme}>
-      <Router>
-        <QueryParamProvider
-          ReactRouterRoute={Route}
-          stringifyOptions={{ encode: false }}
-        >
-          {children}
-        </QueryParamProvider>
-      </Router>
+      <AntdThemeProvider>
+        <Router>
+          <QueryParamProvider
+            ReactRouterRoute={Route}
+            stringifyOptions={{ encode: false }}
+          >
+            {children}
+          </QueryParamProvider>
+        </Router>
+      </AntdThemeProvider>
     </ThemeProvider>
   );
 }
