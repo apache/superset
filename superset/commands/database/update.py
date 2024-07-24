8fb2e508d37c5d7f166d166736f956a182487103
Diego Pucci <diegopucci.me@gmail.com>
2024-07-24 13:57:58 +0300

    fix(Database): Refresh catalogs on db update

--- superset/commands/database/update.py
+++ superset/commands/database/update.py
@@ -166,37 +166,39 @@ class UpdateDatabaseCommand(BaseCommand):
         )
 
         for catalog in catalogs:
-            schemas = self._get_schema_names(database, catalog, ssh_tunnel)
-
-            if catalog:
-                perm = security_manager.get_catalog_perm(
-                    original_database_name,
-                    catalog,
-                )
-                existing_pvm = security_manager.find_permission_view_menu(
-                    "catalog_access",
-                    perm,
-                )
-                if not existing_pvm:
-                    # new catalog
-                    security_manager.add_permission_view_menu(
-                        "catalog_access",
-                        security_manager.get_catalog_perm(
-                            database.database_name,
-                            catalog,
-                        ),
+            try:
+                schemas = self._get_schema_names(database, catalog, ssh_tunnel)
+                if catalog:
+                    perm = security_manager.get_catalog_perm(
+                        original_database_name,
+                        catalog,
                     )
-                    for schema in schemas:
+                    existing_pvm = security_manager.find_permission_view_menu(
+                        "catalog_access",
+                        perm,
+                    )
+                    if not existing_pvm:
+                        # new catalog
                         security_manager.add_permission_view_menu(
-                            "schema_access",
-                            security_manager.get_schema_perm(
+                            "catalog_access",
+                            security_manager.get_catalog_perm(
                                 database.database_name,
                                 catalog,
-                                schema,
                             ),
                         )
-                    continue
-
+                        for schema in schemas:
+                            security_manager.add_permission_view_menu(
+                                "schema_access",
+                                security_manager.get_schema_perm(
+                                    database.database_name,
+                                    catalog,
+                                    schema,
+                                ),
+                            )
+                        continue
+            except DatabaseConnectionFailedError:
+                continue
+            
             # add possible new schemas in catalog
             self._refresh_schemas(
                 database,
