9f5eb899e87a1640887212b1942ed816a87cbec4
Joe Li <joe@preset.io>
2024-08-12 15:21:33 -0700

    fix: add imports back to celery file (#29921)

--- superset/tasks/celery_app.py
+++ superset/tasks/celery_app.py
@@ -32,6 +32,9 @@ from superset.extensions import celery_app, db
 flask_app = create_app()
 
 # Need to import late, as the celery_app will have been setup by "create_app()"
+# ruff: noqa: E402, F401
+# pylint: disable=wrong-import-position, unused-import
+from . import cache, scheduler
 
 # Export the celery app globally for Celery (as run on the cmd line) to find
 app = celery_app
