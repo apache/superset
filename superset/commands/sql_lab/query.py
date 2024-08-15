2755a0a6163ee871bb047ff8966448d6ae110f67
Michael S. Molina <michael.s.molina@gmail.com>
2024-08-15 16:29:30 -0400

    Add batch processing

--- superset/commands/sql_lab/query.py
+++ superset/commands/sql_lab/query.py
@@ -22,7 +22,6 @@ import sqlalchemy as sa
 from superset import db
 from superset.commands.base import BaseCommand
 from superset.models.sql_lab import Query
-from superset.utils.decorators import transaction
 
 logger = logging.getLogger(__name__)
 
@@ -46,22 +45,52 @@ class QueryPruneCommand(BaseCommand):
         """
         self.retention_period_days = retention_period_days
 
-    @transaction()
     def run(self) -> None:
         """
         Executes the prune command.
         """
-        result = db.session.execute(
-            sa.delete(Query).where(
-                Query.changed_on
-                < datetime.now() - timedelta(days=self.retention_period_days)
+        retention_date = datetime.now() - timedelta(days=self.retention_period_days)
+
+        # Query the total number of rows to be deleted
+        total_rows = db.session.query(Query).filter(
+            Query.changed_on < retention_date
+        ).count()
+
+        logger.info("Total rows to be deleted: %s", total_rows)
+
+        batch_size = 100_000
+
+        total_deleted = 0
+
+        for _ in range(0, total_rows, batch_size):
+            # Select a batch of records to delete
+            subquery = (
+                sa.select(Query.id)
+                .where(Query.changed_on < retention_date)
+                .limit(batch_size)
+                .subquery()
             )
-        )
-        logger.info(
-            "Deleted %s rows from the query table older than %s days",
-            result.rowcount,
-            self.retention_period_days,
-        )
+
+            # Delete the selected batch using equality
+            result = db.session.execute(
+                sa.delete(Query).where(Query.id == subquery.c.id).execution_options(synchronize_session="fetch")
+            )
+
+            # Update the total number of deleted records
+            total_deleted += result.rowcount
+
+            # Explicitly commit the transaction given that if an error occurs, we want to ensure that the
+            # records that have been deleted so far are committed
+            db.session.commit()
+
+            # Log the number of deleted records
+            logger.info(
+                "Deleted %s rows from the query table older than %s days",
+                total_deleted,
+                self.retention_period_days,
+            )
+
+        logger.info("Pruning complete")
 
     def validate(self) -> None:
         pass
