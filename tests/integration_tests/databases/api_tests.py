cee48440af1ddc3af2303c339cacbb4428c49c9b
Evan Rusackas <evan@preset.io>
2024-08-20 14:29:57 -0600

    Update tests/integration_tests/databases/api_tests.py

--- tests/integration_tests/databases/api_tests.py
+++ tests/integration_tests/databases/api_tests.py
@@ -3840,7 +3840,7 @@ class TestDatabaseApi(SupersetTestCase):
         Database API: validate SQL with errors
         """
         request_payload = {
-            "sql": "SELECT col1 FROM table1",
+            "sql": "SELECT col1 from_ table1",
             "schema": None,
             "template_params": None,
         }
