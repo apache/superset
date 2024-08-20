1fc3d7a514b3a96ea29df190661d112975bc265f
Maxime Beauchemin <maximebeauchemin@gmail.com>
2024-08-19 17:02:29 -0700

    dummy lint with --no-verify

--- superset/__init__.py
+++ superset/__init__.py
@@ -14,8 +14,8 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-from flask import current_app, Flask
 from werkzeug.local import LocalProxy
+from flask import current_app, Flask
 
 from superset.app import create_app  # noqa: F401
 from superset.extensions import (
