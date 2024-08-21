c1c2b9b806ebf820ee16adf4d3502bc2aeaef692
BrianEliasAda <brian@Brian-BHP>
2024-08-21 12:28:54 +0300

    bookmark

--- superset-frontend/src/pages/Assistant/ContextBuilder/DatasourceSchema.tsx
+++ superset-frontend/src/pages/Assistant/ContextBuilder/DatasourceSchema.tsx
@@ -1,4 +1,4 @@
-import { Collapse, Spin } from "antd";
+import { Collapse, Spin, Input } from "antd";
 import { DatasourceTableProps } from "./DatasourceTable";
 import { DatasourceTable } from "./DatasourceTable";
 import React, { Component } from "react";
@@ -15,6 +15,7 @@ export interface DatasourceSchemaProps {
     tables?: DatasourceTableProps[];
     onChange?: (data: DatasourceTableProps) => void;
     loading?: boolean;
+    descriptionFocused?: boolean;
 }
 
 /**
@@ -26,6 +27,7 @@ export class DatasourceSchema extends Component<DatasourceSchemaProps> {
         selected: this.props.selected || false,
         tables: this.props.tables || [],
         loading: true,
+        descriptionFocused: false,
         ...this.props
     };
 
@@ -61,10 +63,16 @@ export class DatasourceSchema extends Component<DatasourceSchemaProps> {
         this.state.onChange?.call(this, data);
     };
 
-    
+    handleDescriptionFocus = (isFocused:boolean) => {
+        this.setState({
+            descriptionFocused: isFocused
+        });
+    };
+
+
 
     render() {
-        const { loading, schemaName, tables } = this.state;
+        const { loading, schemaName, tables, description, descriptionFocused } = this.state;
         return (
             <>
                 <div style={{
@@ -79,13 +87,36 @@ export class DatasourceSchema extends Component<DatasourceSchemaProps> {
                         }}
                             header={
                                 <div style={{
-                                    display: 'flex',
-                                    flexDirection: 'row',
-                                }}>
-                                    <span>{schemaName}</span>
-                                    <span style={{ width: '10px' }}></span>
-                                    {loading && <Spin size="small" />}
+
+                                }} >
+                                    <div style={{
+                                        display: 'flex',
+                                        flexDirection: 'row',
+                                    }}>
+                                        <span>{schemaName}</span>
+                                        <span style={{ width: '10px' }}></span>
+                                        {loading && <Spin size="small" />}
+                                    </div>
+                                    <div>
+                                        <Input 
+                                        prefix={
+                                            <img height={'24px'} width={'24px'} src="/static/assets/images/assistant_logo_b_n_w.svg" />
+                                        }
+                                        placeholder={description || 'What data does this database schema contain?'} 
+                                        onFocus={()=>{this.handleDescriptionFocus(true)}} 
+                                        onBlur={()=>{this.handleDescriptionFocus(false)}}
+                                        style={{
+                                            
+                                            width: '100%',
+                                            padding: '10px',
+                                            margin: '10px 0px',
+                                            borderRadius: '5px',
+                                            border: descriptionFocused ? '1px solid #1890ff' : '0px solid #d9d9d9',
+                                        }}
+                                        />
+                                    </div>
                                 </div>
+
                             } key={schemaName}>
                             <div style={{
                                 display: 'flex',
@@ -93,7 +124,7 @@ export class DatasourceSchema extends Component<DatasourceSchemaProps> {
                                 flexWrap: 'wrap',
                             }}>
                                 {tables?.map((table) => (
-                                    <DatasourceTable key={'tables'+table.tableName} {...table} onChange={this.handleOnChange} />
+                                    <DatasourceTable key={'tables' + table.tableName} {...table} onChange={this.handleOnChange} />
                                 ))}
                             </div>
                         </Collapse.Panel>
