d497dcad41b3f888a15404a838c74a2363a2f378
Michael S. Molina <70410625+michael-s-molina@users.noreply.github.com>
2024-08-23 11:39:45 -0300

    fix: Security manager incorrect calls (#29884)

--- superset/databases/api.py
+++ superset/databases/api.py
@@ -2274,6 +2274,6 @@ class DatabaseRestApi(BaseSupersetModelRestApi):
         # otherwise the database should have been filtered out
         # in CsvToDatabaseForm
         schemas_allowed_processed = security_manager.get_schemas_accessible_by_user(
-            database, schemas_allowed, True
+            database, database.get_default_catalog(), schemas_allowed, True
         )
         return self.response(200, schemas=schemas_allowed_processed)
