8332eff9f435dfdaa13eb3e70129cba27b7def2a
Ville Brofeldt <ville.brofeldt@apple.com>
2024-08-26 13:49:13 -0700

    fix(user-dao): return user_model instances

--- tests/unit_tests/dao/user_test.py
+++ tests/unit_tests/dao/user_test.py
@@ -21,7 +21,9 @@ from flask_appbuilder.security.sqla.models import User
 from sqlalchemy.orm.exc import NoResultFound
 
 from superset.daos.user import UserDAO
+from superset.extensions import security_manager
 from superset.models.user_attributes import UserAttribute
+from tests.unit_tests.fixtures.common import admin_user, after_each  # noqa: F401
 
 
 @pytest.fixture
@@ -90,3 +92,17 @@ def test_set_avatar_url_without_existing_attributes(mock_db_session):
     assert len(user.extra_attributes) == 1
     assert user.extra_attributes[0].avatar_url == new_url
     mock_db_session.add.assert_called()  # New attribute should be added
+
+
+def test_get_by_id_custom_user_class(
+    monkeypatch: pytest.MonkeyPatch,
+    admin_user: User,  # noqa: F811
+    after_each: None,  # noqa: F811
+):
+    class CustomUserModel(User):
+        __tablename__ = "ab_user"
+
+    monkeypatch.setattr(security_manager, "user_model", CustomUserModel)
+
+    user = UserDAO.get_by_id(admin_user.id)
+    assert isinstance(user, CustomUserModel)
