e6f9cdb437710a38452a44700f1bad0b2ab4f65a
Ksenia Litvinova <ksenia.litvinova@keeprise.ru>
2024-08-27 09:21:19 +0300

    fix import

--- superset/initialization/__init__.py
+++ superset/initialization/__init__.py
@@ -26,7 +26,6 @@ import wtforms_json
 from deprecation import deprecated
 from flask import Flask, redirect
 from flask_appbuilder import expose, IndexView
-from flask_babel import gettext as __
 from flask_compress import Compress
 from flask_session import Session
 from werkzeug.middleware.proxy_fix import ProxyFix
