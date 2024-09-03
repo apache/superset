22698e1aa8630bee64ce8e51f15d0c24ca134b82
Michael S. Molina <michael.s.molina@gmail.com>
2024-09-03 16:57:40 -0300

    fix: DeckGL legend layout

--- superset-frontend/plugins/legacy-preset-chart-deckgl/src/components/Legend.tsx
+++ superset-frontend/plugins/legacy-preset-chart-deckgl/src/components/Legend.tsx
@@ -40,11 +40,13 @@ const StyledLegend = styled.div`
       margin: 0;
 
       & li a {
+        display: flex;
         color: ${theme.colors.grayscale.base};
         text-decoration: none;
+        padding: ${theme.gridUnit}px 0;
 
         & span {
-          margin-right: ${theme.gridUnit * 3}px;
+          margin-right: ${theme.gridUnit}px;
         }
       }
     }
