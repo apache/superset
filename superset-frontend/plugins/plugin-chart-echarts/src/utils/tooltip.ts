d85fdf4bf9afc0c6ebed48da4c2ddba01308693c
JUST.in DO IT <justin.park@airbnb.com>
2024-09-11 16:30:34 -0700

    fix: filters panel broken due to tabs scroll (#30180)
    (cherry picked from commit be0a0ced251a3b168b17a230759c5138603800a7)

--- superset-frontend/plugins/plugin-chart-echarts/src/utils/tooltip.ts
+++ superset-frontend/plugins/plugin-chart-echarts/src/utils/tooltip.ts
@@ -25,6 +25,8 @@ export function getDefaultTooltip(refs: Refs) {
   return {
     appendToBody: true,
     borderColor: 'transparent',
+    // CSS hack applied on this class to resolve https://github.com/apache/superset/issues/30058
+    className: 'echarts-tooltip',
     position: (
       canvasMousePos: [number, number],
       params: CallbackDataParams,
