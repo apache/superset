2bded1e0023bfe60e85de61012ed22ba6eb1ab5b
Michael S. Molina <michael.s.molina@gmail.com>
2024-09-19 16:48:10 -0300

    Reverts db.session.is_active

--- superset/utils/decorators.py
+++ superset/utils/decorators.py
@@ -260,9 +260,7 @@ def transaction(  # pylint: disable=redefined-outer-name
                 db.session.commit()  # pylint: disable=consider-using-transaction
                 return result
             except Exception as ex:
-                # An error can happen before the session becomes active
-                if db.session.is_active:
-                    db.session.rollback()  # pylint: disable=consider-using-transaction
+                db.session.rollback()  # pylint: disable=consider-using-transaction
 
                 if on_error:
                     return on_error(ex)
