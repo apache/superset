ea4a65ed59f812e3116a803478d5f7a270f1d7e8
Mateusz Kopeć <3338226+mkopec87@users.noreply.github.com>
2024-10-09 14:17:11 +0200

    - Add new memory units adaptive formatter to format bytes units into kB, MB, GB, etc.

--- superset-frontend/packages/superset-ui-core/src/number-format/index.ts
+++ superset-frontend/packages/superset-ui-core/src/number-format/index.ts
@@ -31,5 +31,6 @@ export {
 export { default as NumberFormatterRegistry } from './NumberFormatterRegistry';
 export { default as createD3NumberFormatter } from './factories/createD3NumberFormatter';
 export { default as createDurationFormatter } from './factories/createDurationFormatter';
+export { default as createMemoryFormatter } from './factories/createMemoryFormatter';
 export { default as createSiAtMostNDigitFormatter } from './factories/createSiAtMostNDigitFormatter';
 export { default as createSmartNumberFormatter } from './factories/createSmartNumberFormatter';
