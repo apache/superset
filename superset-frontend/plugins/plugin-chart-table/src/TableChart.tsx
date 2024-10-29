4ecb97ae8689b3617c5d1b50cc8271c3a24a18f0
David Rivera <drivera@webgains.com>
2024-10-29 15:31:10 +0100

    SRV-820: Adding titles to comparison tables

--- superset-frontend/plugins/plugin-chart-table/src/TableChart.tsx
+++ superset-frontend/plugins/plugin-chart-table/src/TableChart.tsx
@@ -267,9 +267,9 @@ export default function TableChart<D extends DataRecord = DataRecord>(
   } = props;
   const comparisonColumns = [
     { key: 'all', label: t('Display all') },
-    { key: '#', label: '#' },
-    { key: '△', label: '△' },
-    { key: '%', label: '%' },
+    { key: '#', label: t('Previous value') },
+    { key: '△', label: t('Difference') },
+    { key: '%', label: t('% Difference') },
   ];
   const timestampFormatter = useCallback(
     value => getTimeFormatterForGranularity(timeGrain)(value),
@@ -404,7 +404,7 @@ export default function TableChart<D extends DataRecord = DataRecord>(
     };
   };
 
-  const comparisonLabels = [t('Main'), '#', '△', '%'];
+  const comparisonLabels = [t('Main'), t('Previous value'), t('Difference'), t('% Difference')];
   const filteredColumnsMeta = useMemo(() => {
     if (!isUsingTimeComparison) {
       return columnsMeta;
