ada44171c282f8e8ca5d336a5cab2698189f7a5d
Diego Pucci <diegopucci.me@gmail.com>
2024-10-31 15:17:58 +0200

    chore(Config): Remove default THUMBNAIL_CACHE_CONFIG

--- tests/integration_tests/superset_test_config.py
+++ tests/integration_tests/superset_test_config.py
@@ -122,12 +122,6 @@ FILTER_STATE_CACHE_CONFIG = {
     "CACHE_DEFAULT_TIMEOUT": int(timedelta(minutes=10).total_seconds()),
 }
 
-THUMBNAIL_CACHE_CONFIG = {
-    "CACHE_TYPE": "SimpleCache",
-    "CACHE_THRESHOLD": math.inf,
-    "CACHE_DEFAULT_TIMEOUT": int(timedelta(minutes=10).total_seconds()),
-}
-
 EXPLORE_FORM_DATA_CACHE_CONFIG = {
     "CACHE_TYPE": "SimpleCache",
     "CACHE_THRESHOLD": math.inf,
