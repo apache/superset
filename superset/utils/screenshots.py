1975b9fbc1f6c946e9afc60a20df5c4c6aee3c3a
Lautaro Chiarle <lautaro@datakimia.com>
2024-11-01 11:57:20 -0300

    fix default docker image tag value and add logs for thumbnails cache key

--- superset/utils/screenshots.py
+++ superset/utils/screenshots.py
@@ -17,6 +17,7 @@
 from __future__ import annotations
 
 import logging
+import json
 from io import BytesIO
 from typing import TYPE_CHECKING
 
@@ -75,6 +76,7 @@ class BaseScreenshot:
         window_size: bool | WindowSize | None = None,
         thumb_size: bool | WindowSize | None = None,
     ) -> str:
+        logger.info("******************* About to generate cache key with received window size %s and thumb size %s",window_size, thumb_size)        
         window_size = window_size or self.window_size
         thumb_size = thumb_size or self.thumb_size
         args = {
@@ -84,6 +86,7 @@ class BaseScreenshot:
             "window_size": window_size,
             "thumb_size": thumb_size,
         }
+        logger.info("******************* Generating cache key with args %s",json.dumps(args, indent=2))  
         return md5_sha_from_dict(args)
 
     def get_screenshot(
