816c650485ad9b7341cfd4bbd6a2a078b04d74d6
Maxime ALAY-EDDINE <maxime@galeax.com>
2024-11-24 18:39:42 +0100

    Add country map for France with all overseas territories

--- superset-frontend/plugins/legacy-plugin-chart-country-map/src/countries.ts
+++ superset-frontend/plugins/legacy-plugin-chart-country-map/src/countries.ts
@@ -80,8 +80,10 @@ import ethiopia from './countries/ethiopia.geojson';
 import fiji from './countries/fiji.geojson';
 import finland from './countries/finland.geojson';
 import france from './countries/france.geojson';
+import france_overseas from './countries/france_overseas.geojson';
 import france_regions from './countries/france_regions.geojson';
 import french_polynesia from './countries/french_polynesia.geojson';
+import french_southern_and_antarctic_lands from './countries/french_southern_and_antarctic_lands.geojson';
 import gabon from './countries/gabon.geojson';
 import gambia from './countries/gambia.geojson';
 import germany from './countries/germany.geojson';
@@ -280,8 +282,10 @@ export const countries = {
   fiji,
   finland,
   france,
+  france_overseas,
   france_regions,
   french_polynesia,
+  french_southern_and_antarctic_lands,
   gabon,
   gambia,
   germany,
@@ -426,6 +430,9 @@ export const countryOptions = Object.keys(countries).map(x => {
   }
   if (x === 'france_regions') {
     return [x, 'France (regions)'];
+  }
+  if (x === 'france_overseas') {
+    return [x, 'France (with overseas)'];
   }
   if (x === 'turkey_regions') {
     return [x, 'Turkey (regions)'];
