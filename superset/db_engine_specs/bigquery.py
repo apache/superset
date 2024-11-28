CoverGenius/private-superset

BW-2722: add param to indicate metadata fetch query

superset/db_engine_specs/bigquery.py
@@ -456,7 +456,7 @@ class BigQueryEngineSpec(BaseEngineSpec):  # pylint: disable=too-many-public-met
         In BigQuery, a catalog is called a "project".
         """
         engine: Engine
-        with database.get_sqla_engine_with_context() as engine:
+        with database.get_sqla_engine_with_context(fetch_metadata=True) as engine:
             client = cls._get_client(engine)
             projects = client.list_projects()
 

apache/superset

superset/db_engine_specs/bigquery.py
@@ -492,7 +492,7 @@ class BigQueryEngineSpec(BaseEngineSpec):  # pylint: disable=too-many-public-met
         In BigQuery, a catalog is called a "project".
         """
         engine: Engine
-        with database.get_sqla_engine() as engine:
+        with database.get_sqla_engine(fetch_metadata=True) as engine:
             client = cls._get_client(engine)
             projects = client.list_projects()
 