f27cd4e2eef3f073813bbd8d1ade6b21b514c92b
Evan Rusackas <evan@rusackas.com>
2024-12-03 23:23:58 -0700

    no longer using theme so I won't use useTheme

--- superset-frontend/src/dashboard/components/nativeFilters/FiltersConfigModal/FilterTitlePane.tsx
+++ superset-frontend/src/dashboard/components/nativeFilters/FiltersConfigModal/FilterTitlePane.tsx
@@ -18,7 +18,7 @@
  */
 import { useRef, FC } from 'react';
 
-import { NativeFilterType, styled, t, useTheme } from '@superset-ui/core';
+import { NativeFilterType, styled, t } from '@superset-ui/core';
 import { Button } from 'src/components';
 import Icons from 'src/components/Icons';
 import ButtonGroup from 'src/components/ButtonGroup';
