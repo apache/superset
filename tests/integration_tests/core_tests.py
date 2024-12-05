25eb7142b6ebce214d0270e925c9b63e830c4a56
Joe Li <joe@preset.io>
2024-12-05 09:14:52 -0800

    fix: linting to pass pre-commit

--- tests/integration_tests/core_tests.py
+++ tests/integration_tests/core_tests.py
@@ -56,18 +56,18 @@ from superset.utils.database import get_example_database
 from superset.views.database.views import DatabaseView
 from tests.integration_tests.conftest import with_feature_flags
 from tests.integration_tests.constants import ADMIN_USERNAME, GAMMA_USERNAME
-from tests.integration_tests.fixtures.birth_names_dashboard import \
-    load_birth_names_dashboard_with_slices  # noqa: F401
-from tests.integration_tests.fixtures.birth_names_dashboard import \
-    load_birth_names_data  # noqa: F401
-from tests.integration_tests.fixtures.energy_dashboard import \
-    load_energy_table_data  # noqa: F401
-from tests.integration_tests.fixtures.energy_dashboard import \
-    load_energy_table_with_slice  # noqa: F401
-from tests.integration_tests.fixtures.world_bank_dashboard import \
-    load_world_bank_dashboard_with_slices  # noqa: F401
-from tests.integration_tests.fixtures.world_bank_dashboard import \
-    load_world_bank_data  # noqa: F401
+from tests.integration_tests.fixtures.birth_names_dashboard import (
+    load_birth_names_dashboard_with_slices,  # noqa: F401
+    load_birth_names_data,  # noqa: F401
+)
+from tests.integration_tests.fixtures.energy_dashboard import (
+    load_energy_table_data,  # noqa: F401
+    load_energy_table_with_slice,  # noqa: F401
+)
+from tests.integration_tests.fixtures.world_bank_dashboard import (
+    load_world_bank_dashboard_with_slices,  # noqa: F401
+    load_world_bank_data,  # noqa: F401
+)
 from tests.integration_tests.test_app import app
 
 from .base_tests import SupersetTestCase
