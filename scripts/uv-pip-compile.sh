9d2a75bc32f45f3c553ba9e8430571e3b3c501af
Maxime Beauchemin <maximebeauchemin@gmail.com>
2024-12-05 13:54:34 -0800

    docs

--- scripts/uv-pip-compile.sh
+++ scripts/uv-pip-compile.sh
@@ -1,2 +1,8 @@
-uv pip compile pyproject.toml -c requirements/base.in -o requirements/base.txt
-uv pip compile pyproject.toml requirements/development.in -c requirements/base.txt -o requirements/development.txt
+#!/usr/bin/env bash
+
+set -e
+
+ADDITIONAL_ARGS="$@"
+
+uv pip compile pyproject.toml -c requirements/base.in -o requirements/base.txt $ADDITIONAL_ARGS
+uv pip compile pyproject.toml requirements/development.in -c requirements/base.txt -o requirements/development.txt $ADDITIONAL_ARGS
