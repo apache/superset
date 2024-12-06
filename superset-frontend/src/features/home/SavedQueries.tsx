cbc85e82f2976c70b3269b7194fc05bd00527c2c
JUST.in DO IT <justin.park@airbnb.com>
2024-12-06 10:09:02 -0800

    fix(sqllab): Remove update_saved_query_exec_info to reduce lag (#31294)

--- superset-frontend/src/features/home/SavedQueries.tsx
+++ superset-frontend/src/features/home/SavedQueries.tsx
@@ -278,7 +278,7 @@ const SavedQueries = ({
                 url={`/sqllab?savedQueryId=${q.id}`}
                 title={q.label}
                 imgFallbackURL="/static/assets/images/empty-query.svg"
-                description={t('Ran %s', q.changed_on_delta_humanized)}
+                description={t('Modified %s', q.changed_on_delta_humanized)}
                 cover={
                   q?.sql?.length && showThumbnails && featureFlag ? (
                     <QueryContainer>
