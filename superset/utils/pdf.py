3b7fde9722279825f42cfcf3996b46153fc5140d
Erkka Tahvanainen <erkka.tahvanainen@confidently.fi>
2024-12-09 16:17:19 +0200

    Revert "pdf export with img2pdf"
    This reverts commit d7d7d93ee92a6f408ada54f63a196819710aa7b4.

--- superset/utils/pdf.py
+++ superset/utils/pdf.py
@@ -16,7 +16,7 @@
 # under the License.
 
 import logging
-import img2pdf
+from io import BytesIO
 
 from superset.commands.report.exceptions import ReportSchedulePdfFailedError
 
@@ -28,11 +28,21 @@ except ModuleNotFoundError:
 
 
 def build_pdf_from_screenshots(snapshots: list[bytes]) -> bytes:
+    images = []
+
+    for snap in snapshots:
+        img = Image.open(BytesIO(snap))
+        if img.mode == "RGBA":
+            img = img.convert("RGB")
+        images.append(img)
     logger.info("building pdf")
     try:
-        pdf_bytes = img2pdf.convert(snapshots)
+        new_pdf = BytesIO()
+        images[0].save(new_pdf, "PDF", save_all=True, append_images=images[1:])
+        new_pdf.seek(0)
     except Exception as ex:
         raise ReportSchedulePdfFailedError(
             f"Failed converting screenshots to pdf {str(ex)}"
         ) from ex
-    return pdf_bytes
+
+    return new_pdf.read()
