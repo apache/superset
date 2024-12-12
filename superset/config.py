efc418363165d665669b75caa73eb37dc0b9a3ab
Jeremy Poulter <jpoulter@webgains.com>
2024-12-12 11:40:56 +0000

    pre-commit fixes

--- superset/config.py
+++ superset/config.py
@@ -399,7 +399,7 @@ LANGUAGES = {
     "it": {"flag": "it", "name": "Italian"},
     "fr": {"flag": "fr", "name": "French"},
     "de": {"flag": "de", "name": "German"},
-    "nl": {"flag": "nl", "name": "Dutch"}
+    "nl": {"flag": "nl", "name": "Dutch"},
 }
 
 
