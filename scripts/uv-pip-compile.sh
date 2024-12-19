8f788c01cfa1b04b87675fe419597aa7da767831
Maxime Beauchemin <maximebeauchemin@gmail.com>
2024-12-18 16:21:08 -0800

    simplifying

--- scripts/uv-pip-compile.sh
+++ scripts/uv-pip-compile.sh
@@ -22,9 +22,9 @@ set -e
 ADDITIONAL_ARGS="$@"
 
 # Generate the requirements/base.txt file
-uv pip compile pyproject.toml -c requirements/base.in -o requirements/base.txt $ADDITIONAL_ARGS
+uv pip compile pyproject.toml requirements/base.in -o requirements/base.txt $ADDITIONAL_ARGS
 
 # Generate the requirements/development.txt file, making sure requirements/base.txt is a constraint to keep the versions in sync
-uv pip compile pyproject.toml requirements/development.in -c requirements/base.txt -o requirements/development.txt $ADDITIONAL_ARGS
+uv pip compile requirements/development.in -c requirements/base.txt -o requirements/development.txt $ADDITIONAL_ARGS
 
 uv pip compile requirements/translations.in -o requirements/translations.txt $ADDITIONAL_ARGS
