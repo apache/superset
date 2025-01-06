c7936deefefba30a89c83e7f0eb346f1744fd4ed
Maxime Beauchemin <maximebeauchemin@gmail.com>
2025-01-06 13:08:34 -0800

    rollback trigger hack

--- superset/config.py
+++ superset/config.py
@@ -12,8 +12,8 @@
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 # KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations under
-# the License.
+# specific language governing permissions and limitations
+# under the License.
 """The main config file for Superset
 
 All configuration in this file can be overridden by providing a superset_config
