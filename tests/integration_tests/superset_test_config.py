3dc67fd5d83d7ccfaabeaf9f55de25dc09c1d4a1
justinpark <justin.hojun.park@gmail.com>
2025-01-13 15:44:33 -0800

    Remove KV_STORE and docs

--- tests/integration_tests/superset_test_config.py
+++ tests/integration_tests/superset_test_config.py
@@ -68,8 +68,6 @@ SQLLAB_CTAS_NO_LIMIT = True  # SQL_MAX_ROW will not take effect for the CTA quer
 FEATURE_FLAGS = {
     **FEATURE_FLAGS,  # noqa: F405
     "foo": "bar",
-    "KV_STORE": True,
-    "SHARE_QUERIES_VIA_KV_STORE": True,
     "ENABLE_TEMPLATE_PROCESSING": True,
     "ALERT_REPORTS": True,
     "AVOID_COLORS_COLLISION": True,
