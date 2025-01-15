4acd67dfea85557cd1fc28c4cc4800ae6006cffe
Michael Gerber <michael.gerber@nxt.engineering>
2025-01-15 10:55:34 +0100

    respect extra_form_data as well

--- superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/transformProps.ts
+++ superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/transformProps.ts
@@ -40,6 +40,7 @@ import {
   t,
   TimeseriesChartDataResponseResult,
   NumberFormats,
+  convertKeysToCamelCase,
 } from '@superset-ui/core';
 import {
   extractExtraMetrics,
@@ -194,7 +195,11 @@ export default function transformProps(
     yAxisTitlePosition,
     zoomable,
     stackDimension,
-  }: EchartsTimeseriesFormData = { ...DEFAULT_FORM_DATA, ...formData };
+  }: EchartsTimeseriesFormData = {
+    ...DEFAULT_FORM_DATA,
+    ...formData,
+    ...convertKeysToCamelCase(formData.extra_form_data),
+  };
   const refs: Refs = {};
   const groupBy = ensureIsArray(groupby);
   const labelMap: { [key: string]: string[] } = Object.entries(
