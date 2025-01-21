83b2ec39014b520ef5f58629492b7af2fdd36fb1
Michael S. Molina <michael.s.molina@gmail.com>
2025-01-21 10:49:31 -0300

    refactor: Removes legacy dashboard endpoints

--- superset/initialization/__init__.py
+++ superset/initialization/__init__.py
@@ -172,7 +172,6 @@ class SupersetAppInitializer:  # pylint: disable=too-many-public-methods
         from superset.views.dashboard.views import (
             Dashboard,
             DashboardModelView,
-            DashboardModelViewAsync,
         )
         from superset.views.database.views import DatabaseView
         from superset.views.datasource.views import DatasetEditor, Datasource
@@ -299,7 +298,6 @@ class SupersetAppInitializer:  # pylint: disable=too-many-public-methods
         appbuilder.add_view_no_menu(Api)
         appbuilder.add_view_no_menu(CssTemplateAsyncModelView)
         appbuilder.add_view_no_menu(Dashboard)
-        appbuilder.add_view_no_menu(DashboardModelViewAsync)
         appbuilder.add_view_no_menu(Datasource)
         appbuilder.add_view_no_menu(DatasetEditor)
         appbuilder.add_view_no_menu(EmbeddedView)
