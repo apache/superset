f7bdc5e3f18c1ec4a7b3d1eca2a3fa73d6fb0d62
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2025-01-24 19:09:19 +0300

    chore(DropdownSelectableIcon): remove custom styles

--- superset-frontend/src/components/DropdownSelectableIcon/index.tsx
+++ superset-frontend/src/components/DropdownSelectableIcon/index.tsx
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import { addAlpha, styled, useTheme } from '@superset-ui/core';
+import { useTheme } from '@superset-ui/core';
 import { RefObject, useMemo, ReactNode, useState } from 'react';
 import Icons from 'src/components/Icons';
 import { Menu, MenuProps } from 'src/components/Menu';
@@ -39,47 +39,6 @@ export interface DropDownSelectableProps extends Pick<MenuProps, 'onSelect'> {
   selectedKeys?: string[];
 }
 
-const StyledMenu = styled(Menu)`
-  ${({ theme }) => `
-    box-shadow:
-        0 3px 6px -4px ${addAlpha(theme.colors.grayscale.dark2, 0.12)},
-        0 6px 16px 0
-      ${addAlpha(theme.colors.grayscale.dark2, 0.08)},
-        0 9px 28px 8px
-      ${addAlpha(theme.colors.grayscale.dark2, 0.05)};
-    .info {
-      font-size: ${theme.typography.sizes.s}px;
-      color: ${theme.colors.grayscale.base};
-      padding: ${theme.gridUnit}px ${theme.gridUnit * 3}px ${
-        theme.gridUnit
-      }px ${theme.gridUnit * 3}px;
-    }
-    .antd5-dropdown-menu-item-selected {
-      color: ${theme.colors.grayscale.dark1};
-      background-color: ${theme.colors.primary.light5};
-    }
-  `}
-`;
-
-const StyleMenuItem = styled(Menu.Item)<{ divider?: boolean }>`
-  display: flex;
-  justify-content: space-between;
-  > span {
-    width: 100%;
-  }
-  border-bottom: ${({ divider, theme }) =>
-    divider ? `1px solid ${theme.colors.grayscale.light3};` : 'none;'};
-`;
-
-const StyleSubmenuItem = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  > div {
-    flex-grow: 1;
-  }
-`;
-
 const DropdownSelectableIcon = (props: DropDownSelectableProps) => {
   const theme = useTheme();
   const [visible, setVisible] = useState(false);
@@ -95,8 +54,8 @@ const DropdownSelectableIcon = (props: DropDownSelectableProps) => {
   };
   const menuItem = useMemo(
     () => (label: string | ReactNode, key: string, divider?: boolean) => (
-      <StyleMenuItem key={key} divider={divider}>
-        <StyleSubmenuItem>
+      <>
+        <Menu.Item key={key}>
           {label}
           {selectedKeys?.includes(key) && (
             <Icons.Check
@@ -105,8 +64,9 @@ const DropdownSelectableIcon = (props: DropDownSelectableProps) => {
               iconSize="xl"
             />
           )}
-        </StyleSubmenuItem>
-      </StyleMenuItem>
+        </Menu.Item>
+        {divider && <Menu.Divider />}
+      </>
     ),
     [selectedKeys, theme.colors.primary.base],
   );
@@ -119,7 +79,7 @@ const DropdownSelectableIcon = (props: DropDownSelectableProps) => {
             {info}
           </div>
         )}
-        <StyledMenu
+        <Menu
           selectedKeys={selectedKeys}
           onSelect={handleMenuSelect}
           selectable
@@ -137,7 +97,7 @@ const DropdownSelectableIcon = (props: DropDownSelectableProps) => {
               menuItem(m.label, m.key, m.divider)
             ),
           )}
-        </StyledMenu>
+        </Menu>
       </>
     ),
     [selectedKeys, onSelect, info, menuItems, menuItem, handleMenuSelect],
