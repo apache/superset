01d88deb1ea7e285b0ed3ea5c17cff4c33ee1789
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2025-01-27 13:49:54 +0300

    fix(DownloadAsPdf): check for menuitem instead of button

--- superset-frontend/src/dashboard/components/menu/DownloadMenuItems/DownloadAsPdf.test.tsx
+++ superset-frontend/src/dashboard/components/menu/DownloadMenuItems/DownloadAsPdf.test.tsx
@@ -58,7 +58,7 @@ test('Should call download pdf on click', async () => {
     expect(mockAddDangerToast).toHaveBeenCalledTimes(0);
   });
 
-  userEvent.click(screen.getByRole('button', { name: 'Export as PDF' }));
+  userEvent.click(screen.getByRole('menuitem', { name: 'Export as PDF' }));
 
   await waitFor(() => {
     expect(downloadAsPdf).toHaveBeenCalledTimes(1);
@@ -66,8 +66,8 @@ test('Should call download pdf on click', async () => {
   });
 });
 
-test('Component is rendered with role="button"', async () => {
+test('Component is rendered with role="menuitem"', async () => {
   renderComponent();
-  const button = screen.getByRole('button', { name: 'Export as PDF' });
+  const button = screen.getByRole('menuitem', { name: 'Export as PDF' });
   expect(button).toBeInTheDocument();
 });
