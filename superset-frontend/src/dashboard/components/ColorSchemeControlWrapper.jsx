efde7898ce3b846405049d4d95cd2e920838d991
hainenber <dotronghai96@gmail.com>
2025-01-28 20:27:57 +0700

    fix(ci): resolve all `tsc` failures
    Signed-off-by: hainenber <dotronghai96@gmail.com>

--- superset-frontend/src/dashboard/components/ColorSchemeControlWrapper.jsx
+++ superset-frontend/src/dashboard/components/ColorSchemeControlWrapper.jsx
@@ -17,60 +17,35 @@
  * under the License.
  */
 /* eslint-env browser */
-import PropTypes from 'prop-types';
-import { PureComponent } from 'react';
 import { getCategoricalSchemeRegistry, t } from '@superset-ui/core';
-
 import ColorSchemeControl from 'src/explore/components/controls/ColorSchemeControl';
 
-const propTypes = {
-  onChange: PropTypes.func,
-  labelMargin: PropTypes.number,
-  colorScheme: PropTypes.string,
-  hasCustomLabelsColor: PropTypes.bool,
+const ColorSchemeControlWrapper = ({
+  colorScheme,
+  labelMargin = 0,
+  hasCustomLabelsColor = false,
+  onChange = () => {},
+}) => {
+  // Registry initialization
+  const categoricalSchemeRegistry = getCategoricalSchemeRegistry();
+  const choices = categoricalSchemeRegistry.keys().map(s => [s, s]);
+  const schemes = categoricalSchemeRegistry.getMap();
+
+  return (
+    <ColorSchemeControl
+      description={t(
+        "Any color palette selected here will override the colors applied to this dashboard's individual charts",
+      )}
+      labelMargin={labelMargin}
+      name="color_scheme"
+      onChange={onChange}
+      value={colorScheme}
+      choices={choices}
+      clearable
+      schemes={schemes}
+      hasCustomLabelsColor={hasCustomLabelsColor}
+    />
+  );
 };
-
-const defaultProps = {
-  hasCustomLabelsColor: false,
-  colorScheme: undefined,
-  onChange: () => {},
-};
-
-class ColorSchemeControlWrapper extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = { hovered: false };
-    this.categoricalSchemeRegistry = getCategoricalSchemeRegistry();
-    this.choices = this.categoricalSchemeRegistry.keys().map(s => [s, s]);
-    this.schemes = this.categoricalSchemeRegistry.getMap();
-  }
-
-  setHover(hovered) {
-    this.setState({ hovered });
-  }
-
-  render() {
-    const { colorScheme, labelMargin = 0, hasCustomLabelsColor } = this.props;
-    return (
-      <ColorSchemeControl
-        description={t(
-          "Any color palette selected here will override the colors applied to this dashboard's individual charts",
-        )}
-        labelMargin={labelMargin}
-        name="color_scheme"
-        onChange={this.props.onChange}
-        value={colorScheme}
-        choices={this.choices}
-        clearable
-        schemes={this.schemes}
-        hovered={this.state.hovered}
-        hasCustomLabelsColor={hasCustomLabelsColor}
-      />
-    );
-  }
-}
-
-ColorSchemeControlWrapper.propTypes = propTypes;
-ColorSchemeControlWrapper.defaultProps = defaultProps;
 
 export default ColorSchemeControlWrapper;
