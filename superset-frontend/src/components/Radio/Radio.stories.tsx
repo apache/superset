a90ed3585e8049db20ace1e0a4021d384d3ec39b
Enzo Martellucci <enzomartellucci@gmail.com>
2025-01-28 22:08:31 +0100

    refactor(Radio stories): Upgrade radio's stories

--- superset-frontend/src/components/Radio/Radio.stories.tsx
+++ superset-frontend/src/components/Radio/Radio.stories.tsx
@@ -16,40 +16,140 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import { useArgs } from '@storybook/preview-api';
-import { Radio } from './index';
+import { Space } from 'src/components/Space';
+import {
+  BarChartOutlined,
+  DotChartOutlined,
+  LineChartOutlined,
+  PieChartOutlined,
+} from '@ant-design/icons';
+import { Radio, RadioProps, RadioGroupWrapperProps } from './index';
 
 export default {
   title: 'Radio',
   component: Radio,
-  parameters: {
-    controls: { hideNoControlsWarning: true },
-  },
-  argTypes: {
-    theme: {
-      table: {
-        disable: true,
-      },
-    },
-    checked: { control: 'boolean' },
-    disabled: { control: 'boolean' },
-  },
+  tags: ['autodocs'],
 };
 
-export const SupersetRadio = () => {
-  const [{ checked, ...rest }, updateArgs] = useArgs();
-  return (
-    <Radio
-      checked={checked}
-      onChange={() => updateArgs({ checked: !checked })}
-      {...rest}
-    >
-      Example
-    </Radio>
-  );
+const RadioArgsType = {
+  onChange: { action: 'changed' },
+  checked: { control: 'boolean' },
+  disabled: { control: 'boolean' },
+  defaultChecked: { control: 'boolean' },
 };
 
-SupersetRadio.args = {
+const radioGroupWrapperArgsType = {
+  onChange: { action: 'changed' },
+  disabled: { control: 'boolean' },
+  direction: {
+    control: 'select',
+    options: ['horizontal', 'vertical'],
+  },
+  size: {
+    control: 'select',
+    options: ['small', 'middle', 'large'],
+  },
+  spaceSize: {
+    control: 'select',
+    options: ['small', 'middle', 'large', 'custom'],
+  },
+  customSpaceSize: {
+    control: 'number',
+    if: { arg: 'spaceSize', eq: 'custom' },
+    defaultValue: 8,
+  },
+  useSpace: { control: 'boolean' },
+  options: { control: 'object' },
+};
+
+// Radio story
+export const RadioStory = {
+  args: {
+    children: 'Radio',
+    value: 'radio1',
+    disabled: false,
+    checked: false,
+  },
+  argTypes: RadioArgsType,
+};
+
+// Radio Button story
+export const RadioButtonStory = (args: RadioProps) => (
+  <Radio.Button {...args}>Radio Button</Radio.Button>
+);
+RadioButtonStory.args = {
+  value: 'button1',
+  disabled: false,
   checked: false,
+};
+RadioButtonStory.argTypes = RadioArgsType;
+
+// Radio Group with Custom Radio story
+export const RadioGroupWithCustomRadioStory = (
+  args: RadioGroupWrapperProps,
+) => (
+  <Radio.GroupWrapper {...args}>
+    <Radio value="option1">Custom Option 1</Radio>
+    <Radio value="option2">Custom Option 2</Radio>
+    <Radio value="option3">Custom Option 3</Radio>
+  </Radio.GroupWrapper>
+);
+RadioGroupWithCustomRadioStory.args = {
+  useSpace: true,
+  direction: 'vertical',
+  spaceSize: 'middle',
+  size: 'middle',
+  options: undefined,
   disabled: false,
 };
+RadioGroupWithCustomRadioStory.argTypes = radioGroupWrapperArgsType;
+
+// Radio Group with Options story
+export const RadioGroupWithOptionsStory = (args: RadioGroupWrapperProps) => (
+  <Radio.GroupWrapper {...args} />
+);
+RadioGroupWithOptionsStory.args = {
+  useSpace: false,
+  direction: 'horizontal',
+  size: 'middle',
+  options: [
+    {
+      value: 1,
+      label: (
+        <Space align="center" direction="vertical">
+          <LineChartOutlined style={{ fontSize: 18 }} />
+          LineChart
+        </Space>
+      ),
+    },
+    {
+      value: 2,
+      label: (
+        <Space align="center" direction="vertical">
+          <DotChartOutlined style={{ fontSize: 18 }} />
+          DotChart
+        </Space>
+      ),
+    },
+    {
+      value: 3,
+      label: (
+        <Space align="center" direction="vertical">
+          <BarChartOutlined style={{ fontSize: 18 }} />
+          BarChart
+        </Space>
+      ),
+    },
+    {
+      value: 4,
+      label: (
+        <Space align="center" direction="vertical">
+          <PieChartOutlined style={{ fontSize: 18 }} />
+          PieChart
+        </Space>
+      ),
+    },
+  ],
+  disabled: false,
+};
+RadioGroupWithOptionsStory.argTypes = radioGroupWrapperArgsType;
