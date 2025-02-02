348ceed3f828f808dd61dceaf05b230608efe634
Alex Duan <417921451@qq.com>
2025-02-02 14:02:30 +0800

    fix: security problem with password

--- superset/db_engine_specs/TDengine.py
+++ superset/db_engine_specs/TDengine.py
@@ -10,7 +10,7 @@ class TDengineEngineSpec(BaseEngineSpec):
     engine_name = "TDengine"
     max_column_name_length = 64
     default_driver = "taosws"
-    sqlalchemy_uri_placeholder = ("taosws://user:password@host:port/dbname[?key=value&key=value...]")
+    sqlalchemy_uri_placeholder = ("taosws://user:******@host:port/dbname[?key=value&key=value...]")
     
     # time grain
     _time_grain_expressions = {
