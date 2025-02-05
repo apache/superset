6a7ffbf02f3411540bf86fcd8fd6deb466cb186a
Alex Duan <417921451@qq.com>
2025-02-05 19:38:17 +0800

    fix: TDengine.py and test_TDengine.py checkok by pre-commit on local

--- superset/db_engine_specs/TDengine.py
+++ superset/db_engine_specs/TDengine.py
@@ -16,18 +16,22 @@
 # under the License.
 from __future__ import annotations
 
-from superset.db_engine_specs.base import  BaseEngineSpec
-from urllib import parse
 from typing import Any
-from sqlalchemy import types
+from urllib import parse
+
 from sqlalchemy.engine.url import make_url, URL  # noqa: F401
+
+from superset.db_engine_specs.base import BaseEngineSpec
+
 
 class TDengineEngineSpec(BaseEngineSpec):
     engine = "taosws"
     engine_name = "TDengine"
     max_column_name_length = 64
     default_driver = "taosws"
-    sqlalchemy_uri_placeholder = ("taosws://user:******@host:port/dbname[?key=value&key=value...]")
+    sqlalchemy_uri_placeholder = (
+        "taosws://user:******@host:port/dbname[?key=value&key=value...]"
+    )
 
     # time grain
     _time_grain_expressions = {
@@ -35,8 +39,8 @@ class TDengineEngineSpec(BaseEngineSpec):
         "PT1S": "TIMETRUNCATE({col}, 1s, 0)",
         "PT1M": "TIMETRUNCATE({col}, 1m, 0)",
         "PT1H": "TIMETRUNCATE({col}, 1h, 0)",
-        "P1D":  "TIMETRUNCATE({col}, 1d, 0)",
-        "P1W":  "TIMETRUNCATE({col}, 1w, 0)",
+        "P1D": "TIMETRUNCATE({col}, 1d, 0)",
+        "P1W": "TIMETRUNCATE({col}, 1w, 0)",
     }
 
     @classmethod
