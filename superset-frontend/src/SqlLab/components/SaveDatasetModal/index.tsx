cd27dd7dd35fadc2a4047a437c659f20530c6d2e
Enzo Martellucci <enzomartellucci@gmail.com>
2025-02-11 18:21:39 +0100

    lint

--- superset-frontend/src/SqlLab/components/SaveDatasetModal/index.tsx
+++ superset-frontend/src/SqlLab/components/SaveDatasetModal/index.tsx
@@ -96,36 +96,36 @@ interface SaveDatasetModalProps {
 }
 
 const Styles = styled.div`
+  ${({ theme }) => `
   .sdm-body {
-    margin: 0 ${({ theme }) => theme.gridUnit * 2}px;
+    margin: 0 ${theme.gridUnit * 2}px;
   }
   .sdm-input {
-    margin-left: ${({ theme }) => theme.gridUnit * 10}px;
+    margin-left: ${theme.gridUnit * 10}px;
     width: 401px;
   }
   .sdm-autocomplete {
     width: 401px;
     align-self: center;
-    margin-left: ${({ theme }) => theme.gridUnit}px;
+    margin-left: ${theme.gridUnit}px;
   }
   .sdm-radio {
     height: 30px;
     margin: 10px 0px;
-    line-height: ${({ theme }) => theme.gridUnit * 7.5}px;
+    line-height: ${theme.gridUnit * 7.5}px;
   }
   .sdm-radio span {
     display: inline-flex;
     padding-right: 0px;
   }
   .sdm-overwrite-msg {
-    margin: ${({ theme }) => theme.gridUnit * 2}px;
+    margin: ${theme.gridUnit * 2}px;
   }
   .sdm-overwrite-container {
     flex: 1 1 auto;
     display: flex;
-  }
+  `}
 `;
-
 const updateDataset = async (
   dbId: number,
   datasetId: number,
