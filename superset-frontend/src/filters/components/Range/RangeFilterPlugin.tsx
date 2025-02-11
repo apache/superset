5b598e2a0554e37e873239331834b7cd9a4808df
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2025-02-11 15:37:56 +0300

    refactor(RangeFilterPlugin): make the component a bit more clear

--- superset-frontend/src/filters/components/Range/RangeFilterPlugin.tsx
+++ superset-frontend/src/filters/components/Range/RangeFilterPlugin.tsx
@@ -108,15 +108,11 @@ export default function RangeFilterPlugin(props: PluginFilterRangeProps) {
   const enableSingleExactValue = enableSingleValue === SingleValueType.Exact;
 
   const [col = ''] = ensureIsArray(groupby).map(getColumnLabel);
-  const [value, setValue] = useState<[number | null, number | null]>([
-    defaultValue?.[minIndex] ?? null,
-    defaultValue?.[maxIndex] ?? null,
-  ]);
-  const [previousMode, setPreviousMode] = useState<SingleValueType | null>(
-    null,
+  const [inputValue, setInputValue] = useState<[number | null, number | null]>(
+    filterState.value != null
+      ? filterState.value
+      : (defaultValue ?? [null, null]),
   );
-
-  const minMax = value ?? [min, max];
 
   const getBounds = useCallback(
     (
@@ -133,27 +129,8 @@ export default function RangeFilterPlugin(props: PluginFilterRangeProps) {
         upper: upperRaw !== null && upperRaw < max ? upperRaw : null,
       };
     },
-    [max, min, enableSingleExactValue],
+    [max, min],
   );
-
-  const handleAfterChange = useCallback(
-    (value: [number | null, number | null]): void => {
-      const { lower, upper } = getBounds(value);
-      setDataMask({
-        extraFormData: getRangeExtraFormData(col, lower, upper),
-        filterState: {
-          value: lower !== null || upper !== null ? value : null,
-          label: getLabel(lower, upper, enableSingleExactValue),
-        },
-      });
-    },
-    [col, getBounds, setDataMask],
-  );
-
-  useEffect(() => {
-    console.log('Datamask has been called');
-    console.log(filterState);
-  }, [setDataMask]);
 
   const metadataText = useMemo(() => {
     if (enableSingleMinValue) {
@@ -168,46 +145,62 @@ export default function RangeFilterPlugin(props: PluginFilterRangeProps) {
     return '';
   }, [enableSingleValue]);
 
-  const handleChange = (newValue: number, index: 0 | 1) => {
-    const updatedValue: [number | null, number | null] = [...value];
-
-    if (enableSingleExactValue) {
-      setValue([newValue, newValue]);
-      handleAfterChange([newValue, newValue]);
-      return;
-    }
-    if (enableSingleMinValue) {
-      updatedValue[minIndex] = newValue;
-      setValue(updatedValue);
-      handleAfterChange(updatedValue);
-      return;
-    }
-
-    if (enableSingleMaxValue) {
-      updatedValue[maxIndex] = newValue;
-      setValue(updatedValue);
-      handleAfterChange(updatedValue);
-      return;
-    }
-
-    updatedValue[index] = newValue;
-    console.log(updatedValue);
-    setValue(updatedValue);
-    handleAfterChange(updatedValue);
+  const handleChange = (newValue: number | null, index: 0 | 1) => {
+    setInputValue(prev => {
+      if (index === minIndex) {
+        return [newValue, prev[maxIndex]];
+      }
+      return [prev[minIndex], newValue];
+    });
   };
 
-  // useEffect(() => {
-  //   // when switch filter type and queriesData still not updated we need ignore this case (in FilterBar)
-  //   if (row?.min === undefined && row?.max === undefined) {
-  //     return;
-  //   }
-  //   console.log(filterState)
-  //   const filterStateValue = filterState.value ?? [null, null];
-  //   setValue(filterStateValue);
-  //   handleAfterChange(filterStateValue);
-  // }, [
-  //   filterState?.value, row?.min, row?.max
-  // ]);
+  const handleBlur = (index: 0 | 1) => {
+    if (inputValue[index] === null) {
+      return;
+    }
+
+    let realValue = inputValue;
+
+    if (enableSingleExactValue) {
+      realValue = [inputValue[minIndex], realValue[minIndex]];
+    }
+    if (enableSingleMinValue) {
+      realValue = [inputValue[minIndex], null];
+    }
+    if (enableSingleMaxValue) {
+      realValue = [null, inputValue[maxIndex]];
+    }
+
+    const { lower, upper } = getBounds(realValue);
+    setInputValue(() => {
+      setDataMask({
+        extraFormData: getRangeExtraFormData(col, lower, upper),
+        filterState: {
+          value: [lower, upper],
+          label: getLabel(lower, upper, enableSingleExactValue),
+        },
+      });
+      return [lower, upper];
+    });
+  };
+
+  useEffect(() => {
+    if (row?.min === undefined && row?.max === undefined) {
+      return;
+    }
+    if (filterState.value) {
+      setInputValue(filterState.value);
+      return;
+    }
+    setInputValue([null, null]);
+    setDataMask({
+      extraFormData: getRangeExtraFormData(col, null, null),
+      filterState: {
+        value: null,
+        label: '',
+      },
+    });
+  }, [row?.min, row?.max, filterState?.value, getBounds]);
 
   const formItemExtra = useMemo(() => {
     if (filterState.validateMessage) {
@@ -219,81 +212,6 @@ export default function RangeFilterPlugin(props: PluginFilterRangeProps) {
     }
     return undefined;
   }, [filterState.validateMessage, filterState.validateStatus]);
-
-  const updateValue = (newValue: [number, number], mode: SingleValueType) => {
-    setValue(newValue);
-    setPreviousMode(mode);
-    handleAfterChange(newValue);
-  };
-
-  const getNewValue = (
-    currentMode: SingleValueType,
-    previousMode: SingleValueType | undefined | null,
-  ): [number, number] => {
-    switch (currentMode) {
-      case SingleValueType.Maximum:
-        return (
-          (previousMode === SingleValueType.Exact && [min, minMax[maxIndex]]) ||
-          (previousMode === SingleValueType.Minimum && [
-            min,
-            minMax[minIndex],
-          ]) || [min, minMax[maxIndex]]
-        );
-
-      case SingleValueType.Minimum:
-        return (
-          (previousMode === SingleValueType.Exact && [minMax[minIndex], max]) ||
-          (previousMode === SingleValueType.Maximum && [
-            minMax[maxIndex],
-            max,
-          ]) || [minMax[minIndex], max]
-        );
-
-      case SingleValueType.Exact:
-        return (
-          (previousMode === SingleValueType.Maximum && [
-            minMax[maxIndex],
-            minMax[maxIndex],
-          ]) ||
-          (previousMode === SingleValueType.Minimum && [
-            minMax[minIndex],
-            minMax[minIndex],
-          ]) || [minMax[minIndex], minMax[minIndex]]
-        );
-
-      default:
-        throw new Error('Invalid SingleValueType provided');
-    }
-  };
-
-  useEffect(() => {
-    if (enableSingleMaxValue) {
-      const newValue = getNewValue(SingleValueType.Maximum, previousMode);
-      updateValue(newValue, SingleValueType.Maximum);
-    }
-  }, [enableSingleMaxValue]);
-
-  useEffect(() => {
-    if (enableSingleMinValue) {
-      const newValue = getNewValue(SingleValueType.Minimum, previousMode);
-      updateValue(newValue, SingleValueType.Minimum);
-    }
-  }, [enableSingleMinValue]);
-
-  useEffect(() => {
-    if (enableSingleExactValue) {
-      const newValue = getNewValue(SingleValueType.Exact, previousMode);
-      updateValue(newValue, SingleValueType.Exact);
-    }
-  }, [enableSingleExactValue]);
-
-  const handleBlur = (index: 0 | 1) => {
-    if (index === minIndex) {
-      handleChange(filterState?.value[minIndex], minIndex);
-    } else if (index === maxIndex) {
-      handleChange(filterState?.value[maxIndex], maxIndex);
-    }
-  };
 
   return (
     <FilterPluginStyle height={height} width={width}>
@@ -317,42 +235,36 @@ export default function RangeFilterPlugin(props: PluginFilterRangeProps) {
             onMouseDown={() => setFilterActive(true)}
             onMouseUp={() => setFilterActive(false)}
           >
-            {enableSingleValue !== undefined ? (
-              <>
-                <InputNumber
-                  min={min}
-                  max={max}
-                  onChange={val => handleChange(Number(val), minIndex)}
-                  placeholder={t('Number')}
-                  data-test="native-filter-single-value"
-                />
-                {filterBarOrientation === FilterBarOrientation.Vertical && (
-                  <Metadata value={metadataText} />
-                )}
-              </>
-            ) : (
-              <>
-                <InputNumber
-                  value={filterState?.value?.[minIndex] ?? null}
-                  min={min}
-                  max={max}
-                  onChange={val => handleChange(val, minIndex)}
-                  // onBlur={() => handleBlur(minIndex)}
-                  placeholder={t(`${min}`)}
-                  data-test="native-filter-from-input"
-                />
-                <StyledDivider>-</StyledDivider>
-                <InputNumber
-                  value={filterState?.value?.[maxIndex] ?? null}
-                  min={min}
-                  max={max}
-                  onChange={val => handleChange(val, maxIndex)}
-                  // onBlur={() => handleBlur(maxIndex)}
-                  placeholder={t(`${max}`)}
-                  data-test="native-filter-to-input"
-                  // changeOnBlur={false}
-                />
-              </>
+            {(enableSingleValue === 0 ||
+              enableSingleValue === 1 ||
+              enableSingleValue === undefined) && (
+              <InputNumber
+                value={inputValue[0]}
+                min={min}
+                max={inputValue[maxIndex] ?? max}
+                onChange={val => handleChange(val, minIndex)}
+                onBlur={() => handleBlur(minIndex)}
+                placeholder={`${min}`}
+                data-test="range-filter-min-input"
+              />
+            )}
+            {enableSingleValue === undefined && (
+              <StyledDivider>-</StyledDivider>
+            )}
+            {(enableSingleValue === 2 || enableSingleValue === undefined) && (
+              <InputNumber
+                value={inputValue[1]}
+                min={inputValue[minIndex] ?? min}
+                max={max}
+                onChange={val => handleChange(val, maxIndex)}
+                onBlur={() => handleBlur(maxIndex)}
+                placeholder={`${max}`}
+                data-test="range-filter-max-input"
+              />
+            )}
+            {(enableSingleValue !== undefined ||
+              filterBarOrientation === FilterBarOrientation.Vertical) && (
+              <Metadata value={metadataText} />
             )}
           </Wrapper>
         </StyledFormItem>
