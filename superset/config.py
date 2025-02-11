e070becebad0ffce95ff301d493a7e1e9e5d7f13
Daniel Gaspar <danielvazgaspar@gmail.com>
2025-02-11 09:25:47 +0000

    test: ephemeral env up

--- superset/config.py
+++ superset/config.py
@@ -308,7 +308,7 @@ AUTH_RATE_LIMIT = "5 per second"
 # GLOBALS FOR APP Builder
 # ------------------------------
 # Uncomment to setup Your App name
-APP_NAME = "Superset"
+APP_NAME = "Superset TESTENV UP"
 
 # Specify the App icon
 APP_ICON = "/static/assets/images/superset-logo-horiz.png"
