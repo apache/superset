781d5b29e7a8702cff841f15a4a43b000943cf67
Pranjal Pandit <pranjal5215@gmail.com>
2025-02-18 12:31:49 +0530

    Revert "Update config.py"
    This reverts commit f3ecb483e2ec9d45dd9e48c489ea729df5133110.

--- superset/config.py
+++ superset/config.py
@@ -259,7 +259,7 @@ SQLGLOT_DIALECTS_EXTENSIONS: DialectExtensions | Callable[[], DialectExtensions]
 QUERY_SEARCH_LIMIT = 1000
 
 # Flask-WTF flag for CSRF
-WTF_CSRF_ENABLED = False
+WTF_CSRF_ENABLED = True
 
 # Add endpoints that need to be exempt from CSRF protection
 WTF_CSRF_EXEMPT_LIST = [
