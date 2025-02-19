e246499e4f91e1f0d6a9a56d81be74c2df9e17ef
Evan Rusackas <evan@rusackas.com>
2025-02-19 13:53:03 -0700

    feat(flag flip): Setting Horizontal Filters to True by default.

--- superset/config.py
+++ superset/config.py
@@ -526,7 +526,7 @@ DEFAULT_FEATURE_FLAGS: dict[str, bool] = {
     "DRILL_TO_DETAIL": True,  # deprecated
     "DRILL_BY": True,
     "DATAPANEL_CLOSED_BY_DEFAULT": False,
-    "HORIZONTAL_FILTER_BAR": False,
+    "HORIZONTAL_FILTER_BAR": True,
     # The feature is off by default, and currently only supported in Presto and Postgres,  # noqa: E501
     # and Bigquery.
     # It also needs to be enabled on a per-database basis, by adding the key/value pair
