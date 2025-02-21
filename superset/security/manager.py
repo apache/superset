3d7117e38d2ba81f7104f1420c26b4baf7a17e24
Le Xich Long <codenamelxl@users.noreply.github.com>
2025-02-21 15:33:45 +0800

    Grant TableSchemaView to all default roles

--- superset/security/manager.py
+++ superset/security/manager.py
@@ -302,7 +302,6 @@ class SupersetSecurityManager(  # pylint: disable=too-many-public-methods
         "Manage",
         "Queries",
         "ReportSchedule",
-        "TableSchemaView",
     }
 
     ALPHA_ONLY_PMVS = {
