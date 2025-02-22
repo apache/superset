976fe76f1ec367325800479436467db3bf9b3e75
Enzo Martellucci <enzomartellucci@gmail.com>
2025-02-22 19:26:33 +0100

    Merge branch master into enxdev/refactor-antd5-icons

--- superset-frontend/src/features/home/ChartTable.tsx
+++ superset-frontend/src/features/home/ChartTable.tsx
@@ -18,7 +18,6 @@
  */
 import { useEffect, useMemo, useState } from 'react';
 import { t, useTheme } from '@superset-ui/core';
-import { filter } from 'lodash';
 import {
   useChartEditModal,
   useFavoriteStatus,
@@ -78,7 +77,7 @@ function ChartTable({
     TableTab.Other,
   );
 
-  const filteredOtherTabData = filter(otherTabData, obj => 'viz_type' in obj);
+  const filteredOtherTabData = otherTabData?.filter(obj => 'viz_type' in obj);
 
   const {
     state: { loading, resourceCollection: charts, bulkSelectEnabled },
