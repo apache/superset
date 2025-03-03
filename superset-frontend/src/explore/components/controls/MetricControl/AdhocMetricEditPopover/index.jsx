4e42aeaf11488af8e5f3a674c01c12acbbaa8055
Kirill Kuzmin <123979827+etokiryau@users.noreply.github.com>
2025-03-03 17:04:41 +0400

    feat(44993666): single features (#203)

--- superset-frontend/src/explore/components/controls/MetricControl/AdhocMetricEditPopover/index.jsx
+++ superset-frontend/src/explore/components/controls/MetricControl/AdhocMetricEditPopover/index.jsx
@@ -1,21 +1,4 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+// DODO was here
 /* eslint-disable camelcase */
 import { PureComponent } from 'react';
 import PropTypes from 'prop-types';
@@ -337,6 +320,7 @@ export default class AdhocMetricEditPopover extends PureComponent {
       onChange: this.onSavedMetricChange,
       allowClear: true,
       autoFocus: true,
+      optionFilterProps: ['value', 'verbose_name_EN', 'verbose_name_RU'], // DODO added 44993666
     };
 
     const stateIsValid = adhocMetric.isValid() || savedMetric?.metric_name;
@@ -381,6 +365,8 @@ export default class AdhocMetricEditPopover extends PureComponent {
                     savedMetric => ({
                       value: savedMetric.metric_name,
                       label: savedMetric.metric_name,
+                      verbose_name_EN: savedMetric?.verbose_name_EN ?? '', // DODO added 44993666
+                      verbose_name_RU: savedMetric?.verbose_name_RU ?? '', // DODO added 44993666
                       customLabel: this.renderMetricOption(savedMetric),
                       key: savedMetric.id,
                     }),
