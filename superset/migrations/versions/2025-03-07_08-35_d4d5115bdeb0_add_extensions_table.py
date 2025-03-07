c90d6c2b19987ab27edd8bc7b448bcebfcd744e9
Ville Brofeldt <ville.brofeldt@apple.com>
2025-03-07 09:02:33 -0800

    add extensions migration

---
+++ superset/migrations/versions/2025-03-07_08-35_d4d5115bdeb0_add_extensions_table.py
@@ -0,0 +1,60 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""add extensions table
+
+Revision ID: d4d5115bdeb0
+Revises: 74ad1125881c
+Create Date: 2025-03-07 08:35:14.112691
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "d4d5115bdeb0"
+down_revision = "74ad1125881c"
+
+from uuid import uuid4  # noqa: E402
+
+import sqlalchemy as sa  # noqa: E402
+from alembic import op  # noqa: E402
+from sqlalchemy_utils import UUIDType  # noqa: E402
+
+
+def upgrade():
+    op.create_table(
+        "extensions",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("name", sa.String(255), nullable=False),
+        sa.Column("metadata", sa.Text(), nullable=False),
+        sa.Column("bundle", sa.Text(), nullable=False),
+        sa.Column("enabled", sa.Boolean(), nullable=False),
+        sa.Column("uuid", UUIDType(binary=True), default=uuid4),
+        sa.Column("created_on", sa.DateTime(), nullable=True),
+        sa.Column("created_by_fk", sa.Integer(), nullable=True),
+        sa.Column("changed_on", sa.DateTime(), nullable=True),
+        sa.Column("changed_by_fk", sa.Integer(), nullable=True),
+        sa.ForeignKeyConstraint(["created_by_fk"], ["ab_user.id"]),
+        sa.ForeignKeyConstraint(["changed_by_fk"], ["ab_user.id"]),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_index(op.f("ix_extensions_uuid"), "extensions", ["uuid"], unique=True)
+    op.create_index(op.f("ix_extensions_name"), "extensions", ["name"], unique=True)
+
+
+def downgrade():
+    op.drop_index(op.f("ix_extensions_uuid"), table_name="extensions")
+    op.drop_index(op.f("ix_extensions_name"), table_name="extensions")
+    op.drop_table("extensions")
