15a89565b51137e3cb56560472e2b4b7e370965e
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2025-03-07 20:57:35 +0100

    fix tests

--- superset-frontend/packages/superset-ui-chart-controls/test/utils/defineSavedMetrics.test.tsx
+++ superset-frontend/packages/superset-ui-chart-controls/test/utils/defineSavedMetrics.test.tsx
@@ -49,6 +49,7 @@ describe('defineSavedMetrics', () => {
       {
         metric_name: 'COUNT(*) non-default-dataset-metric',
         expression: 'COUNT(*) non-default-dataset-metric',
+        uuid: '1',
       },
     ]);
     // @ts-ignore
