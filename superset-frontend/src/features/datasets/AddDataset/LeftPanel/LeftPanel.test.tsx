3e0c6af09081d46dc34d69c6979b7ea9086ef9e2
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2025-03-08 20:45:59 +0300

    fix(LeftPanel): tests

--- superset-frontend/src/features/datasets/AddDataset/LeftPanel/LeftPanel.test.tsx
+++ superset-frontend/src/features/datasets/AddDataset/LeftPanel/LeftPanel.test.tsx
@@ -241,7 +241,7 @@ test('searches for a table name', async () => {
 
   // Click 'public' schema to access tables
   userEvent.click(schemaSelect);
-  userEvent.click(screen.getAllByText('public')[1]);
+  userEvent.click(screen.getByText('public'));
   await waitFor(() => expect(fetchMock.calls(tablesEndpoint).length).toBe(1));
   userEvent.click(tableSelect);
 
@@ -310,7 +310,7 @@ test('renders a warning icon when a table name has a preexisting dataset', async
 
   // Click 'public' schema to access tables
   userEvent.click(schemaSelect);
-  userEvent.click(screen.getAllByText('public')[1]);
+  userEvent.click(screen.getByText('public'));
   userEvent.click(tableSelect);
 
   await waitFor(() => {
