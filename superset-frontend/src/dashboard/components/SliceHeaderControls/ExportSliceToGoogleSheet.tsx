61b5e97ff6a6cd19117ddb1ae1c4feee3ae8030e
Ben Tran <benjamintran0684@gmail.com>
2025-03-12 17:56:58 -0400

    update key

--- superset-frontend/src/dashboard/components/SliceHeaderControls/ExportSliceToGoogleSheet.tsx
+++ superset-frontend/src/dashboard/components/SliceHeaderControls/ExportSliceToGoogleSheet.tsx
@@ -38,7 +38,7 @@ export default function ExportSliceToGoogleSheet({
   };
 
   return (
-    <Menu.Item key="google-sheets" {...rest}>
+    <Menu.Item key="google-sheets-export" {...rest}>
       <div onClick={handleGoogleSheetsExport} role="button" tabIndex={0}>
         {t('Export to Google Sheets')}
       </div>
