1244ad05bb27c7d7b6b67cebcd07fa77af0da6f7
“Mujib <mujib.ishola@gmail.com>
2025-03-17 00:25:47 +0100

    fix: updated code

--- docker-old/pythonpath_dev/superset_config.py
+++ docker-old/pythonpath_dev/superset_config.py
@@ -97,8 +97,7 @@ class CeleryConfig:
 
 
 CELERY_CONFIG = CeleryConfig
-
-FEATURE_FLAGS = {"ALERT_REPORTS": True}
+FEATURE_FLAGS = {"HORIZONTAL_FILTER_BAR": True, "ALERT_REPORTS": True,"ALLOW_FULL_CSV_EXPORT": True, "ENABLE_EXPORT": True,"CHART_PLUGINS_EXPERIMENTAL": True}
 ALERT_REPORTS_NOTIFICATION_DRY_RUN = True
 WEBDRIVER_BASEURL = "http://superset:8088/"  # When using docker compose baseurl should be http://superset_app:8088/  # noqa: E501
 # The base URL for the email report hyperlinks.
