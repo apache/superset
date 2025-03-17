686d8acc30e7c7e938aa24304c4dcac66e20613f
Enzo Martellucci <enzomartellucci@gmail.com>
2025-03-18 00:06:33 +0100

    fix rtl tests

--- superset-frontend/src/components/ErrorMessage/BasicErrorAlert.tsx
+++ superset-frontend/src/components/ErrorMessage/BasicErrorAlert.tsx
@@ -58,9 +58,7 @@ export default function BasicErrorAlert({
 
   return (
     <StyledContainer level={level} role="alert">
-      {level === 'error' && (
-        <Icons.ExclamationCircleFilled iconColor={iconColor} />
-      )}
+      <Icons.ExclamationCircleFilled iconColor={iconColor} />
       <StyledContent>
         <StyledTitle>{title}</StyledTitle>
         <p>{body}</p>
