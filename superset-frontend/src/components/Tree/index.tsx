e0abc1e7f4d19d017165ea25f2704eb2211c6290
Geido <60598000+geido@users.noreply.github.com>
2025-03-20 20:17:51 +0200

    Update superset-frontend/src/components/Tree/index.tsx
    Co-authored-by: Mehmet Salih Yavuz <salih.yavuz@proton.me>

--- superset-frontend/src/components/Tree/index.tsx
+++ superset-frontend/src/components/Tree/index.tsx
@@ -17,9 +17,6 @@
  * under the License.
  */
 import { Tree } from 'antd-v5';
-import type { TreeProps, DataNode as TreeDataNode } from 'antd-v5/es/tree';
-import DirectoryTree from 'antd-v5/es/tree/DirectoryTree';
 
-export type { TreeProps };
-export { TreeDataNode, DirectoryTree };
+export type { TreeProps, DataNode as TreeDataNode } from 'antd-v5/es/tree';
 export default Tree;
