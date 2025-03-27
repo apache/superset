06d737ec9f5b782246f0bc54f8035b9063f5d8ef
Maxime Beauchemin <maximebeauchemin@gmail.com>
2025-03-26 18:02:49 -0700

    add theming support to Storybook

---
+++ superset-frontend/.storybook/themes.js
@@ -0,0 +1,17 @@
+// .storybook/themes.js
+export const themes = {
+  superset: {
+    algorithm: 'light',
+  },
+  dark: {
+    token: {},
+    algorithm: 'dark',
+  },
+  funky: {
+    token: {
+      colorPrimary: '#ff00ff',
+      borderRadius: 10,
+    },
+    algorithm: 'light',
+  },
+};
