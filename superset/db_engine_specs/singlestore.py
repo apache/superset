df54cc3ca458661e5486f52bda4233c75e679f6d
Adalbert Makarovych <amakarovych0ua@singlestore.com>
2025-03-28 11:44:29 +0200

    Fixed unit tests, added license

--- superset/db_engine_specs/singlestore.py
+++ superset/db_engine_specs/singlestore.py
@@ -1,3 +1,20 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 import logging
 import re
 from datetime import datetime
@@ -482,17 +499,10 @@ class SingleStoreSpec(BaseEngineSpec):
         catalog: Optional[str] = None,
         schema: Optional[str] = None,
     ) -> tuple[URL, dict[str, Any]]:
-        uri, new_connect_args = super().adjust_engine_params(
-            uri,
-            connect_args,
-            catalog,
-            schema,
-        )
-
         if schema:
             uri = uri.set(database=parse.quote(schema, safe=""))
 
-        return uri, new_connect_args
+        return uri, connect_args
 
     @classmethod
     def get_schema_from_engine_params(
--- tests/unit_tests/db_engine_specs/test_singlestore.py
+++ tests/unit_tests/db_engine_specs/test_singlestore.py
@@ -1,3 +1,20 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 from datetime import datetime
 from typing import Any, Optional
 from unittest.mock import MagicMock, Mock
@@ -155,7 +172,7 @@ def test_get_function_names_no_db() -> None:
     mock_database = MagicMock()
     mock_database.get_inspector.return_value = mock_inspector_ctx
 
-    assert len(SingleStoreSpec.get_function_names(mock_database)) == 304
+    assert len(SingleStoreSpec.get_function_names(mock_database)) == 292
 
 
 def test_get_function_names_with_db() -> None:
@@ -184,7 +201,7 @@ def test_get_function_names_with_db() -> None:
     mock_database.get_df.return_value = df
 
     functions = SingleStoreSpec.get_function_names(mock_database)
-    assert len(functions) == 305
+    assert len(functions) == 293
     assert functions[-1] == "is_prime"
 
     mock_database.get_df.assert_called_once_with("SHOW FUNCTIONS IN `db``1`")
