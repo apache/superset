4d8433a6715b665410bbc2f99ae3f43d5bc699d1
Enzo Martellucci <enzomartellucci@gmail.com>
2025-03-28 11:44:46 +0100

    feat(Radio): add tests for Radio component

---
+++ superset-frontend/src/components/Radio/Radio.test.tsx
@@ -0,0 +1,53 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import { render, screen, fireEvent } from '@testing-library/react';
+import { ThemeProvider, supersetTheme } from '@superset-ui/core';
+import '@testing-library/jest-dom';
+import Button from 'src/components/Button';
+
+describe('Button Component', () => {
+  test('renders button with text', () => {
+    render(
+      <ThemeProvider theme={supersetTheme}>
+        <Button>Click Me</Button>
+      </ThemeProvider>,
+    );
+    expect(screen.getByText('Click Me')).toBeInTheDocument();
+  });
+
+  test('calls onClick handler when clicked', () => {
+    const handleClick = jest.fn();
+    render(
+      <ThemeProvider theme={supersetTheme}>
+        <Button onClick={handleClick}>Click Me</Button>
+      </ThemeProvider>,
+    );
+    fireEvent.click(screen.getByText('Click Me'));
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+
+  test('renders a disabled button', () => {
+    render(
+      <ThemeProvider theme={supersetTheme}>
+        <Button disabled>Disabled</Button>
+      </ThemeProvider>,
+    );
+    expect(screen.getByRole('button', { name: 'Disabled' })).toBeDisabled();
+  });
+});
