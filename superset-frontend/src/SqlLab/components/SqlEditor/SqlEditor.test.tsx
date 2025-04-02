b4cec0cf0cbc3c4579d87b7721e66ef3a5e3ce22
Maxime Beauchemin <maximebeauchemin@gmail.com>
2025-04-02 13:24:31 -0700

    fix

--- superset-frontend/src/SqlLab/components/SqlEditor/SqlEditor.test.tsx
+++ superset-frontend/src/SqlLab/components/SqlEditor/SqlEditor.test.tsx
@@ -43,7 +43,6 @@ import ResultSet from 'src/SqlLab/components/ResultSet';
 import { api } from 'src/hooks/apiResources/queryApi';
 import setupExtensions from 'src/setup/setupExtensions';
 import type { Action, Middleware, Store } from 'redux';
-import { TextArea } from 'src/components/Input';
 import SqlEditor, { Props } from '.';
 
 jest.mock('src/components/AsyncAceEditor', () => ({
@@ -57,7 +56,7 @@ jest.mock('src/components/AsyncAceEditor', () => ({
     onBlur: FocusEventHandler<HTMLTextAreaElement>;
     value: string;
   }) => (
-    <TextArea
+    <textarea
       data-test="react-ace"
       onChange={evt => onChange(evt.target.value)}
       onBlur={onBlur}
