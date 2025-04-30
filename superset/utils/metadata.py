4cf8fce0ec3b04518abb96bc7f2a2191b9f186f2
Always-Prog <always.prog@gmail.com>
2025-04-30 17:27:58 +0300

    added SuspendSession for chart data queries

---
+++ superset/utils/metadata.py
@@ -0,0 +1,9 @@
+from superset import db
+
+class SuspendSession:
+    def __enter__(self):
+        self.session_objects = db.session.identity_map.values()
+        db.session.remove()
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        for obj in self.session_objects:
+            db.session.add(obj)
