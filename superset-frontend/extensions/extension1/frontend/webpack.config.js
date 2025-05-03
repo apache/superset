a4c3d2c086e5e9d8171551e99925983d233066c1
Ville Brofeldt <ville.brofeldt@apple.com>
2025-05-03 08:08:37 -0700

    remove dev assets

--- superset-frontend/extensions/extension1/frontend/webpack.config.js
+++ superset-frontend/extensions/extension1/frontend/webpack.config.js
@@ -22,65 +22,69 @@ const { ModuleFederationPlugin } = require('webpack').container;
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const packageConfig = require('./package');
 
-module.exports = {
-  entry: './src/index.tsx',
-  mode: 'development',
-  devServer: {
-    port: 3000,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+
+  return {
+    entry: isProd ? {} : './src/index.tsx',
+    mode: isProd ? 'production' : 'development',
+    devServer: {
+      port: 3000,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
     },
-  },
-  output: {
-    filename: '[name].[contenthash].js',
-    chunkFilename: '[name].[contenthash].js',
-    clean: true,
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: `/api/v1/extensions/${packageConfig.name}/`,
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  },
-  externalsType: 'window',
-  externals: {
-    '@apache-superset/types': 'superset',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
+    output: {
+      filename: isProd ? undefined : '[name].[contenthash].js',
+      chunkFilename: '[name].[contenthash].js',
+      clean: true,
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: `/api/v1/extensions/${packageConfig.name}/`,
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    externalsType: 'window',
+    externals: {
+      '@apache-superset/types': 'superset',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: packageConfig.name,
+        filename: 'remoteEntry.[contenthash].js',
+        exposes: {
+          './index': './src/index.tsx',
+        },
+        shared: {
+          react: {
+            singleton: true,
+            requiredVersion: packageConfig.peerDependencies.react,
+            import: false,
+          },
+          'react-dom': {
+            singleton: true,
+            requiredVersion: packageConfig.peerDependencies['react-dom'],
+            import: false,
+          },
+          'antd-v5': {
+            singleton: true,
+            requiredVersion: 'npm:antd@^5.18.0',
+            import: false,
+          },
+        },
+      }),
+      new CopyWebpackPlugin({
+        patterns: [{ from: './src/publicAPI.d.ts', to: './publicAPI.d.ts' }],
+      }),
     ],
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: packageConfig.name,
-      filename: 'remoteEntry.[contenthash].js',
-      exposes: {
-        './index': './src/index.tsx',
-      },
-      shared: {
-        react: {
-          singleton: true,
-          requiredVersion: packageConfig.peerDependencies.react,
-          import: false,
-        },
-        'react-dom': {
-          singleton: true,
-          requiredVersion: packageConfig.peerDependencies['react-dom'],
-          import: false,
-        },
-        'antd-v5': {
-          singleton: true,
-          requiredVersion: 'npm:antd@^5.18.0',
-          import: false,
-        },
-      },
-    }),
-    new CopyWebpackPlugin({
-      patterns: [{ from: './src/publicAPI.d.ts', to: './publicAPI.d.ts' }],
-    }),
-  ],
+  };
 };
--- superset-frontend/extensions/extension2/frontend/webpack.config.js
+++ superset-frontend/extensions/extension2/frontend/webpack.config.js
@@ -21,62 +21,66 @@ const path = require('path');
 const { ModuleFederationPlugin } = require('webpack').container;
 const packageConfig = require('./package');
 
-module.exports = {
-  entry: './src/index.tsx',
-  mode: 'development',
-  devServer: {
-    port: 3000,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+
+  return {
+    entry: isProd ? {} : './src/index.tsx',
+    mode: isProd ? 'production' : 'development',
+    devServer: {
+      port: 3000,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
     },
-  },
-  output: {
-    clean: true,
-    filename: '[name].[contenthash].js',
-    chunkFilename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: `/api/v1/extensions/${packageConfig.name}/`,
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  },
-  externalsType: 'window',
-  externals: {
-    '@apache-superset/types': 'superset',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
+    output: {
+      clean: true,
+      filename: isProd ? undefined : '[name].[contenthash].js',
+      chunkFilename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: `/api/v1/extensions/${packageConfig.name}/`,
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    externalsType: 'window',
+    externals: {
+      '@apache-superset/types': 'superset',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: 'extension2',
+        filename: 'remoteEntry.[contenthash].js',
+        exposes: {
+          './index': './src/index.tsx',
+        },
+        shared: {
+          react: {
+            singleton: true,
+            requiredVersion: packageConfig.peerDependencies.react,
+            import: false,
+          },
+          'react-dom': {
+            singleton: true,
+            requiredVersion: packageConfig.peerDependencies['react-dom'],
+            import: false,
+          },
+          'antd-v5': {
+            singleton: true,
+            requiredVersion: 'npm:antd@^5.18.0',
+            import: false,
+          },
+        },
+      }),
     ],
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: 'extension2',
-      filename: 'remoteEntry.[contenthash].js',
-      exposes: {
-        './index': './src/index.tsx',
-      },
-      shared: {
-        react: {
-          singleton: true,
-          requiredVersion: packageConfig.peerDependencies.react,
-          import: false,
-        },
-        'react-dom': {
-          singleton: true,
-          requiredVersion: packageConfig.peerDependencies['react-dom'],
-          import: false,
-        },
-        'antd-v5': {
-          singleton: true,
-          requiredVersion: 'npm:antd@^5.18.0',
-          import: false,
-        },
-      },
-    }),
-  ],
+  };
 };
