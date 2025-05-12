da67ff4c1a8fba80cf56ec3eb6f8f66123914d76
Always-Prog <always.prog@gmail.com>
2025-05-12 18:07:41 +0300

    closing session instead of removing

--- superset/utils/metadata.py
+++ superset/utils/metadata.py
@@ -3,7 +3,7 @@ from superset import db
 class SuspendSession:
     def __enter__(self):
         self.session_objects = db.session.identity_map.values()
-        db.session.remove()
+        db.session.close()
     def __exit__(self, exc_type, exc_val, exc_tb):
         for obj in self.session_objects:
             db.session.add(obj)
