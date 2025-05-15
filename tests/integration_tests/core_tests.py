99f1bf1628dd28ce1ca12cc81c8aaac554c19d42
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2025-05-15 13:42:06 +0300

    fix: change toast assert text

--- tests/integration_tests/core_tests.py
+++ tests/integration_tests/core_tests.py
@@ -111,13 +111,14 @@ class TestCore(SupersetTestCase):
 
     def test_login(self):
         resp = self.get_resp("/login/", data=dict(username="admin", password="general"))  # noqa: S106, C408
-        assert "User confirmation needed" not in resp
+        assert "Invalid login" not in resp
 
         resp = self.get_resp(
             "/login/",
             data=dict(username="admin", password="wrongPassword"),  # noqa: S106, C408
+            follow_redirects=True,
         )
-        assert "User confirmation needed" in resp
+        assert "Invalid login" in resp
 
     def test_dashboard_endpoint(self):
         self.login(ADMIN_USERNAME)
