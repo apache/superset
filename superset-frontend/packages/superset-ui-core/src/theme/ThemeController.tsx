a9b01cac97b52d850ef4426973cf4918b046519d
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2025-05-20 10:38:58 +0300

    fix: post merge

--- superset-frontend/packages/superset-ui-core/src/theme/ThemeController.tsx
+++ superset-frontend/packages/superset-ui-core/src/theme/ThemeController.tsx
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import { theme as antdThemeImport } from 'antd-v5';
+import { theme as antdThemeImport } from 'antd';
 import { Theme } from './Theme';
 import { AnyThemeConfig } from './types';
 
