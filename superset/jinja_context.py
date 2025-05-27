4f6c3f3587bb6625f8da8e3e9ba377f22d14019c
Michael S. Molina <michael.s.molina@gmail.com>
2025-05-27 16:03:38 -0300

    fix: Missing processor context when rendering Jinja

--- superset/jinja_context.py
+++ superset/jinja_context.py
@@ -641,6 +641,12 @@ class BaseTemplateProcessor:
         self._context.update(kwargs)
         self._context.update(context_addons())
 
+    def get_context(self) -> dict[str, Any]:
+        """
+        Returns the current template context.
+        """
+        return self._context
+
     def process_template(self, sql: str, **kwargs: Any) -> str:
         """Processes a sql template
 
