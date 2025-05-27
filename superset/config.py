084f1a6d5e246055d824aceb30bc7e7f421b058e
Sam Firke <sfirke@users.noreply.github.com>
2025-05-27 13:22:23 -0400

    chore(alerts & reports): increase Playwright timeout from 30 -> 60 seconds (#33567)

--- superset/config.py
+++ superset/config.py
@@ -765,7 +765,7 @@ SCREENSHOT_WAIT_FOR_ERROR_MODAL_INVISIBLE = 5
 SCREENSHOT_PLAYWRIGHT_WAIT_EVENT = "load"
 # Default timeout for Playwright browser context for all operations
 SCREENSHOT_PLAYWRIGHT_DEFAULT_TIMEOUT = int(
-    timedelta(seconds=30).total_seconds() * 1000
+    timedelta(seconds=60).total_seconds() * 1000
 )
 
 # ---------------------------------------------------
