b4d422a7823625cb861075730a0e55b1fd3a96d4
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2025-06-02 15:32:10 +0300

    fix(RightMenu): import order

--- superset-frontend/src/features/home/RightMenu.tsx
+++ superset-frontend/src/features/home/RightMenu.tsx
@@ -16,8 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-// TODO: Remove fa-icon
-/* eslint-disable icons/no-fa-icons-usage */
 import { Fragment, useState, useEffect, FC, PureComponent } from 'react';
 
 import rison from 'rison';
@@ -55,13 +53,13 @@ import DatabaseModal from 'src/features/databases/DatabaseModal';
 import UploadDataModal from 'src/features/databases/UploadDataModel';
 import { uploadUserPerms } from 'src/views/CRUD/utils';
 import TelemetryPixel from '@superset-ui/core/components/TelemetryPixel';
+import { useThemeContext } from 'src/theme/ThemeProvider';
 import LanguagePicker from './LanguagePicker';
 import {
   ExtensionConfigs,
   GlobalMenuDataOptions,
   RightMenuProps,
 } from './types';
-import { useThemeContext } from 'src/theme/ThemeProvider';
 
 const extensionsRegistry = getExtensionsRegistry();
 
