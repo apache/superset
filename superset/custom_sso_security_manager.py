b4b782feb1bf7188bbd8485edf375e13b0a861f9
ian.kangethe <ianngech@gmail.com>
2025-06-03 15:24:54 +0300

    redirect-uri

--- superset/custom_sso_security_manager.py
+++ superset/custom_sso_security_manager.py
@@ -7,7 +7,7 @@ class CustomSsoSecurityManager(SupersetSecurityManager):
         if provider == 'signin-oidc':
             # Get user info from the userinfo endpoint
             me = self.appbuilder.sm.oauth_remotes[provider].get(
-                'https://cybqa.pesapal.com/pesapalsso/v2/connect/userinfo')
+                'https://myaccount.pesapal.com/pesapalsso/v2/connect/userinfo')
 
             data = me.json()
             print("user_data: {0}".format(data))
