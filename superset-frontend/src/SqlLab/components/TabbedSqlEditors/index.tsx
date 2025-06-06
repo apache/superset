0c4916bb313602bc37bd0d9816a210c634d8ee8c
Maxime Beauchemin <maximebeauchemin@gmail.com>
2025-06-06 16:37:03 -0700

    chore: delete remaining Enzyme tests (#33715)

--- superset-frontend/src/SqlLab/components/TabbedSqlEditors/index.tsx
+++ superset-frontend/src/SqlLab/components/TabbedSqlEditors/index.tsx
@@ -230,7 +230,6 @@ class TabbedSqlEditors extends PureComponent<TabbedSqlEditorsProps> {
       <EditableTabs.TabPane
         key={qe.id}
         tab={<SqlEditorTabHeader queryEditor={qe} />}
-        // for tests - key prop isn't handled by enzyme well bcs it's a react keyword
         data-key={qe.id}
       >
         <SqlEditor
