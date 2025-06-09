781ef3c91d32b255aea53ac511a905b0935ee59b
Vitor Avila <vitor.avila@preset.io>
2025-06-09 16:39:39 -0300

    Making the downgrade a no op

--- superset/migrations/versions/2025-06-06_00-39_363a9b1e8992_convert_metric_currencies_from_str_to_json.py
+++ superset/migrations/versions/2025-06-06_00-39_363a9b1e8992_convert_metric_currencies_from_str_to_json.py
@@ -74,18 +74,11 @@ def upgrade():
 
 def downgrade():
     """
-    The downgrade just dumps the metric as a str. Might not retrieve the old value,
-    but the syntax was a bug and shouldn't be preserved either way.
+    No op downgrade.
+
+    The downgrade could just do `metric.currency = json.dumps(metric.currency)`. However
+    this is happening after `f1edd4a4d4f2` which already converted the currency column
+    to JSON at the DB level, so we shouldn't have currencies in str anymore. It was the
+    case because the client was still stringifying it.
     """
-    bind = op.get_bind()
-    session = db.Session(bind=bind)
-    currency_configs = session.query(SqlMetric).filter(SqlMetric.currency.isnot(None))
-    for metric in paginated_update(
-        currency_configs,
-        lambda current, total: logger.info((f"Downgrading {current}/{total} metrics")),
-    ):
-        try:
-            metric.currency = json.dumps(metric.currency)
-        except Exception as e:
-            logger.error(f"Error dumping metric {metric.metric_name} to string: {e}")
-            metric.currency = {}
+    pass
