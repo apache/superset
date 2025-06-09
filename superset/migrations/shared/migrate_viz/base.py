c43074725f545e88c3405c088327b200a389569f
Luiz Otavio <45200344+luizotavio32@users.noreply.github.com>
2025-06-09 08:32:12 -0300

    fix: Migrate charts with empty query_context (#33710)

--- superset/migrations/shared/migrate_viz/base.py
+++ superset/migrations/shared/migrate_viz/base.py
@@ -4,7 +4,7 @@
 # regarding copyright ownership.  The ASF licenses this file
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
+# with the License. You may obtain a copy of the License at
 #
 #   http://www.apache.org/licenses/LICENSE-2.0
 #
@@ -157,9 +157,10 @@ class MigrateViz:
             # because a source viz can be mapped to different target viz types
             slc.viz_type = clz.target_viz_type
 
-            backup = {FORM_DATA_BAK_FIELD_NAME: form_data_bak}
+            backup: Any | dict[str, Any] = {FORM_DATA_BAK_FIELD_NAME: form_data_bak}
 
             query_context = try_load_json(slc.query_context)
+            queries_bak = None
 
             if query_context:
                 if "form_data" in query_context:
@@ -169,10 +170,11 @@ class MigrateViz:
 
                 queries = clz._build_query()["queries"]
                 query_context["queries"] = queries
+            else:
+                query_context = clz._build_query()
 
-                slc.query_context = json.dumps(query_context)
-                backup[QUERIES_BAK_FIELD_NAME] = queries_bak
-
+            slc.query_context = json.dumps(query_context)
+            backup[QUERIES_BAK_FIELD_NAME] = queries_bak
             slc.params = json.dumps({**clz.data, **backup})
 
         except Exception as e:
@@ -189,11 +191,14 @@ class MigrateViz:
                 slc.viz_type = form_data_bak.get("viz_type")
                 query_context = try_load_json(slc.query_context)
                 queries_bak = form_data.get(QUERIES_BAK_FIELD_NAME, {})
-                query_context["queries"] = queries_bak
-                if "form_data" in query_context:
-                    query_context["form_data"] = form_data_bak
+                if queries_bak:
+                    query_context["queries"] = queries_bak
+                    if "form_data" in query_context:
+                        query_context["form_data"] = form_data_bak
+                        slc.query_context = json.dumps(query_context)
+                else:
+                    slc.query_context = None
 
-                slc.query_context = json.dumps(query_context)
         except Exception as e:
             logger.warning(f"Failed to downgrade slice {slc.id}: {e}")
 
