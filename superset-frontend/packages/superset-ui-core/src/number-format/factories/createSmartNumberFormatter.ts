5f580e72901ab0512e5f8aeb82bf0c089531aee1
Chintukumar Bhanderi <chintukumarbhanderi@Chintukumars-MacBook-Air.local>
2025-06-11 15:22:05 +0530

    formate change to india

--- superset-frontend/packages/superset-ui-core/src/number-format/factories/createSmartNumberFormatter.ts
+++ superset-frontend/packages/superset-ui-core/src/number-format/factories/createSmartNumberFormatter.ts
@@ -25,6 +25,26 @@ const siFormatter = d3Format(`.3~s`);
 const float2PointFormatter = d3Format(`.2~f`);
 const float4PointFormatter = d3Format(`.4~f`);
 
+function formatIndianNumber(value: number): string {
+  if (value === 0) return '0';
+
+  const absValue = Math.abs(value);
+  const sign = value < 0 ? '-' : '';
+
+  const format = (num: number) =>
+    Number.isInteger(num)
+      ? num.toString()
+      : parseFloat(num.toFixed(2)).toString();
+
+  if (absValue < 0.000001) return `${sign}${absValue.toExponential(2)}`;
+
+  if (absValue < 0.001) return `${sign}${format(absValue * 1_000_000)}µ`;
+  if (absValue < 1_000) return `${sign}${format(absValue)}`;
+  if (absValue < 1_00_000) return `${sign}${format(absValue / 1_000)}K`;
+  if (absValue < 1_00_00_000) return `${sign}${format(absValue / 1_00_000)}L`;
+  return `${sign}${format(absValue / 1_00_00_000)}Cr`;
+}
+
 function formatValue(value: number) {
   if (value === 0) {
     return '0';
@@ -57,10 +77,17 @@ export default function createSmartNumberFormatter(
 ) {
   const { description, signed = false, id, label } = config;
   const getSign = signed ? (value: number) => (value > 0 ? '+' : '') : () => '';
+  const urlParams = new URLSearchParams(window.location.search);
+  const existingParams = Object.fromEntries(urlParams.entries());
+  const isIndiaCountryTenant = existingParams?.tenant_country === 'IN';
+  console.log('existingParams->', existingParams, isIndiaCountryTenant);
 
   return new NumberFormatter({
     description,
-    formatFunc: value => `${getSign(value)}${formatValue(value)}`,
+    formatFunc: value =>
+      `${getSign(value)}${
+        isIndiaCountryTenant ? formatIndianNumber(value) : formatValue(value)
+      }`,
     id:
       id || signed
         ? NumberFormats.SMART_NUMBER_SIGNED
