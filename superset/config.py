37bc5a498f739e093f62051c85505877b1968d3a
Alex W <ctr-awilliams@ext.tenstorrent.com>
2025-06-24 16:22:23 -0400

    Bump the max row limit in SQL Lab to 100000

--- superset/config.py
+++ superset/config.py
@@ -154,7 +154,7 @@ BUILD_NUMBER = None
 DEFAULT_VIZ_TYPE = "table"
 
 # default row limit when requesting chart data
-ROW_LIMIT = 50000
+ROW_LIMIT = 100000
 # default row limit when requesting samples from datasource in explore view
 SAMPLES_ROW_LIMIT = 1000
 # default row limit for native filters
