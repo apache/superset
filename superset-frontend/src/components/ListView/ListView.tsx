f3dea33552bde363e043169be69249ee71cc0056
Vedant Prajapati <40185967+vedantprajapati@users.noreply.github.com>
2025-07-09 12:16:51 +0000

    import from core

--- superset-frontend/src/components/ListView/ListView.tsx
+++ superset-frontend/src/components/ListView/ListView.tsx
@@ -30,8 +30,8 @@ import {
   EmptyState,
   type EmptyStateProps,
 } from '@superset-ui/core/components';
-import { Dropdown } from 'src/components/Dropdown';
-import { Menu } from 'src/components/Menu';
+import { Dropdown } from '@superset-ui/core/components';
+import { Menu } from '@superset-ui/core/components';
 import CardCollection from './CardCollection';
 import FilterControls from './Filters';
 import { CardSortSelect } from './CardSortSelect';
