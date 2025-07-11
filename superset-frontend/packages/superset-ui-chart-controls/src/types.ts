d780b27a8b281746672791fb3d3f2b653920687e
justinpark <justin.hojun.park@gmail.com>
2025-07-11 10:28:52 -0700

    fix type

--- superset-frontend/packages/superset-ui-chart-controls/src/types.ts
+++ superset-frontend/packages/superset-ui-chart-controls/src/types.ts
@@ -90,7 +90,7 @@ export interface Dataset {
   database?: Record<string, unknown>;
   normalize_columns?: boolean;
   always_filter_main_dttm?: boolean;
-  extra?: object;
+  extra?: object | string;
 }
 
 export interface ControlPanelState {
