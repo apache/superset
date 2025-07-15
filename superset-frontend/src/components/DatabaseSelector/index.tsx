c9f6a2622c2250706951836d6aac52a2e2871a58
Beto Dealmeida <roberto@dealmeida.net>
2025-07-15 18:27:04 -0400

    Different fix

--- superset-frontend/src/components/DatabaseSelector/index.tsx
+++ superset-frontend/src/components/DatabaseSelector/index.tsx
@@ -196,7 +196,6 @@ export function DatabaseSelector({
               />
             ),
             value: row.id,
-            id: row.id,
             database_name: row.database_name,
             backend: row.backend,
             allow_multi_catalog: row.allow_multi_catalog,
