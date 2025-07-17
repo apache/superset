3efcac64536bcf377d5a040ff1a47a2f7af9ee69
Lisahusband <2071619763@qq.com>
2025-07-18 03:13:10 +0800

    fixed issue34136

--- superset/utils/core.py
+++ superset/utils/core.py
@@ -48,7 +48,16 @@ from enum import Enum, IntEnum
 from io import BytesIO
 from timeit import default_timer
 from types import TracebackType
-from typing import Any, Callable, cast, NamedTuple, TYPE_CHECKING, TypedDict, TypeVar
+from typing import (
+    Any,
+    Callable,
+    cast,
+    NamedTuple,
+    Optional,
+    TYPE_CHECKING,
+    TypedDict,
+    TypeVar,
+)
 from urllib.parse import unquote_plus
 from zipfile import ZipFile
 
@@ -1485,8 +1494,10 @@ def get_column_name_from_metric(metric: Metric) -> str | None:
     :param metric: Ad-hoc metric
     :return: column name if simple metric, otherwise None
     """
+    logger.info("metricol", metric)
     if is_adhoc_metric(metric):
         metric = cast(AdhocMetric, metric)
+        logger.info("expressionType", metric["expressionType"])
         if metric["expressionType"] == AdhocMetricExpressionType.SIMPLE:
             return cast(dict[str, Any], metric["column"])["column_name"]
     return None
@@ -1501,6 +1512,71 @@ def get_column_names_from_metrics(metrics: list[Metric]) -> list[str]:
     :return: column name if simple metric, otherwise None
     """
     return [col for col in map(get_column_name_from_metric, metrics) if col]
+
+
+def map_sql_type_to_inferred_type(sql_type: Optional[str]) -> str:
+    """把数据库类型映射成 pandas dtype 里 infer_dtype 可识别的类型字符串"""
+    if sql_type is None:
+        return "string"  # 默认类型
+    if "INT" in sql_type:
+        return "integer"
+    if "CHAR" in sql_type or "TEXT" in sql_type or "VARCHAR" in sql_type:
+        return "string"
+    if (
+        "DECIMAL" in sql_type
+        or "NUMERIC" in sql_type
+        or "FLOAT" in sql_type
+        or "DOUBLE" in sql_type
+    ):
+        return "floating"
+    if "BOOL" in sql_type:
+        return "boolean"
+    if "DATE" in sql_type or "TIME" in sql_type:
+        return "datetime64"
+    # 默认返回字符串类型
+    return "string"
+
+
+# 函数：根据列名获取度量类型
+def get_metric_type_from_column(column: Any, datasource: BaseDatasource | Query) -> str:
+    # 度量类型映射
+    metric_type_map = {
+        "SUM": "floating",
+        "AVG": "floating",
+        "COUNT": "floating",
+        "COUNT_DISTINCT": "floating",
+        "MIN": "floating",
+        "MAX": "floating",
+        "FIRST": "",
+        "LAST": "",
+    }
+
+    from superset.connectors.sqla.models import SqlMetric
+
+    # 1. 查找该列名是否是度量列
+    metric: SqlMetric = next(
+        (metric for metric in datasource.metrics if metric.metric_name == column),
+        SqlMetric(metric_name=""),
+    )
+
+    if metric is None:
+        # 不是度量列
+        return ""
+
+    # 2. 提取度量的 SQL 表达式
+    expression: str = metric.expression
+
+    # 3. 使用正则表达式从 expression 中提取聚合操作（如 SUM, COUNT 等）
+    match = re.match(
+        r"(SUM|AVG|COUNT|COUNT_DISTINCT|MIN|MAX|FIRST|LAST)\((.*)\)", expression
+    )
+
+    if match:
+        operation = match.group(1)  # 聚合操作，如 SUM, COUNT
+        # 获取对应的 GenericDataType
+        return metric_type_map.get(operation, "")
+
+    return ""  # 如果没有匹配到有效的操作，返回空字符串
 
 
 def extract_dataframe_dtypes(
@@ -1533,7 +1609,19 @@ def extract_dataframe_dtypes(
     for column in df.columns:
         column_object = columns_by_name.get(column)
         series = df[column]
-        inferred_type = infer_dtype(series)
+        inferred_type: str = ""
+        # 判断列数据是否全为null
+        if series.isna().all():
+            # 通过列名去 columns_types 查找数据库类型，再映射为 pandas 推断类型字符串
+            sql_type: Optional[str] = ""
+            if datasource and hasattr(datasource, "columns_types"):
+                if column in datasource.columns_types:
+                    sql_type = datasource.columns_types.get(column)
+                    inferred_type = map_sql_type_to_inferred_type(sql_type)
+                else:
+                    inferred_type = get_metric_type_from_column(column, datasource)
+            else:
+                inferred_type = infer_dtype(series)
         if isinstance(column_object, dict):
             generic_type = (
                 GenericDataType.TEMPORAL
