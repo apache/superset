58df239246f3f747d472f64204d85f28102e5682
Enzo Martellucci <enzomartellucci@gmail.com>
2025-07-22 14:51:22 +0200

    fix e2e test

--- superset-frontend/cypress-base/cypress/e2e/dashboard/horizontalFilterBar.test.ts
+++ superset-frontend/cypress-base/cypress/e2e/dashboard/horizontalFilterBar.test.ts
@@ -155,7 +155,7 @@ describe('Horizontal FilterBar', () => {
     ]);
     setFilterBarOrientation('horizontal');
 
-    cy.get('.filter-item-wrapper').should('have.length', 4);
+    cy.get('.filter-item-wrapper').should('have.length', 3);
     openMoreFilters();
     cy.getBySel('form-item-value').should('have.length', 12);
     cy.getBySel('filter-control-name').contains('test_3').should('be.visible');
