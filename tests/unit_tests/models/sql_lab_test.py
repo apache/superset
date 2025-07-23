fc9970786c59d633b0d88526a77545f18aa4e8cb
Michael S. Molina <michael.s.molina@gmail.com>
2025-07-23 16:31:09 -0300

    Add tests

--- tests/unit_tests/models/sql_lab_test.py
+++ tests/unit_tests/models/sql_lab_test.py
@@ -22,7 +22,7 @@ from jinja2.exceptions import TemplateError
 from pytest_mock import MockerFixture
 
 from superset.errors import ErrorLevel, SupersetError, SupersetErrorType
-from superset.exceptions import SupersetSecurityException
+from superset.exceptions import SupersetParseError, SupersetSecurityException
 from superset.models.sql_lab import Query, SavedQuery
 
 
@@ -43,6 +43,13 @@ from superset.models.sql_lab import Query, SavedQuery
                 level=ErrorLevel.ERROR,
             )
         ),
+        SupersetParseError(
+            SupersetError(
+                error_type=SupersetErrorType.GENERIC_DB_ENGINE_ERROR,
+                message="Invalid SQL syntax",
+                level=ErrorLevel.ERROR,
+            )
+        ),
         TemplateError,
     ],
 )
@@ -57,3 +64,45 @@ def test_sql_tables_mixin_sql_tables_exception(
     )
 
     assert klass(sql="SELECT 1", database=MagicMock()).sql_tables == []
+
+
+@pytest.mark.parametrize(
+    "klass",
+    [
+        Query,
+        SavedQuery,
+    ],
+)
+@pytest.mark.parametrize(
+    "invalid_sql",
+    [
+        "SELECT * FROM table WHERE invalid syntax",
+        "INVALID SQL STATEMENT",
+        "SELECT * FROM; DROP TABLE users;",
+        "",
+        None,
+    ],
+)
+def test_sql_tables_mixin_invalid_sql_returns_empty_list(
+    klass: type[Model],
+    invalid_sql: str,
+    mocker: MockerFixture,
+) -> None:
+    """Test that SqlTablesMixin returns empty list when SQL parsing fails."""
+    mocker.patch(
+        "superset.models.sql_lab.extract_tables_from_jinja_sql",
+        side_effect=SupersetParseError(
+            SupersetError(
+                error_type=SupersetErrorType.GENERIC_DB_ENGINE_ERROR,
+                message=f"Failed to parse SQL: {invalid_sql}",
+                level=ErrorLevel.ERROR,
+            )
+        ),
+    )
+
+    instance = (
+        klass(sql=invalid_sql, database=MagicMock())
+        if invalid_sql is not None
+        else klass(database=MagicMock())
+    )
+    assert instance.sql_tables == []
