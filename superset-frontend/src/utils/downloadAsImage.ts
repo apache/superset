gofynd/superset-zenith

chore(linting): setting no-console to error, and migrating instances to use `logging`

superset-frontend/src/utils/downloadAsImage.ts
@@ -20,7 +20,7 @@ import { SyntheticEvent } from 'react';
 import domToImage from 'dom-to-image-more';
 import { kebabCase } from 'lodash';
 // eslint-disable-next-line no-restricted-imports
-import { SupersetTheme, t } from '@superset-ui/core';
+import { SupersetTheme, t, logging } from '@superset-ui/core';
 import { addWarningToast } from 'src/components/MessageToasts/actions';
 
 /**
@@ -82,7 +82,7 @@ export default function downloadAsImage(
         link.click();
       })
       .catch((e: Error) => {
-        console.error('Creating image failed', e);
+        logging.error('Creating image failed', e);
       });
   };
 }

apache/superset

superset-frontend/src/utils/downloadAsImage.ts
@@ -20,7 +20,7 @@ import { SyntheticEvent } from 'react';
 import domToImage from 'dom-to-image-more';
 import { kebabCase } from 'lodash';
 // eslint-disable-next-line no-restricted-imports
-import { SupersetTheme, t } from '@superset-ui/core';
+import { SupersetTheme, t, logging } from '@superset-ui/core';
 import { addWarningToast } from 'src/components/MessageToasts/actions';
 
 const IMAGE_DOWNLOAD_QUALITY = 0.95;
@@ -282,7 +282,7 @@ export default function downloadAsImageOptimized(
       link.href = dataUrl;
       link.click();
     } catch (error) {
-      console.error('Creating image failed', error);
+      logging.error('Creating image failed', error);
       addWarningToast(
         t('Image download failed, please refresh and try again.'),
       );