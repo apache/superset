ee2a5915b255fed9e710846fccf579a82d821d80
JUST.in DO IT <justin.park@airbnb.com>
2025-08-04 12:17:06 -0700

    fix(native filters): throws an error when a chart containing a bigint value (#34539)
    (cherry picked from commit 2f8939d229db3b2829e1dd7096875f4fc5196bc1)

--- superset-frontend/src/dashboard/components/nativeFilters/FiltersConfigModal/FiltersConfigForm/FiltersConfigForm.tsx
+++ superset-frontend/src/dashboard/components/nativeFilters/FiltersConfigModal/FiltersConfigForm/FiltersConfigForm.tsx
@@ -754,7 +754,7 @@ const FiltersConfigForm = (
     });
     return excluded;
   }, [
-    JSON.stringify(charts),
+    JSON.stringify(Object.values(charts).map(chart => chart.id)),
     formFilter?.dataset?.value,
     JSON.stringify(loadedDatasets),
   ]);
