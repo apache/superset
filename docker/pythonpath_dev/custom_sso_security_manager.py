29fe7fc6cb58113b314358ec97e8a44e9f8b6fcc
David Ramos <david.ramos@datakimia.com>
2025-08-07 13:07:28 +0200

    DP-627: add can_dashboard_permalink on Superset permission

--- docker/pythonpath_dev/custom_sso_security_manager.py
+++ docker/pythonpath_dev/custom_sso_security_manager.py
@@ -95,6 +95,7 @@ guest_role_pvms = [
     ("can_view_chart_as_table", "Dashboard"),          
     ("can_explore_json", "Superset"),
     ("can_share_dashboard", "Superset"),
+    ("can_dashboard_permalink", "Superset"),
 ]
 
 client_admin_pvms = [
