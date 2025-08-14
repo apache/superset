81e60afc0af7e0d35687d3b1c81f1beeb115b410
Michael S. Molina <michael.s.molina@gmail.com>
2025-08-14 16:58:24 -0300

    fix: Timeseries annotation layers

--- superset-frontend/src/explore/components/controls/AnnotationLayerControl/AnnotationLayer.jsx
+++ superset-frontend/src/explore/components/controls/AnnotationLayerControl/AnnotationLayer.jsx
@@ -31,6 +31,7 @@ import {
   styled,
   getColumnLabel,
   withTheme,
+  VizType,
 } from '@superset-ui/core';
 import SelectControl from 'src/explore/components/controls/SelectControl';
 import TextControl from 'src/explore/components/controls/TextControl';
@@ -244,7 +245,7 @@ class AnnotationLayer extends PureComponent {
         chartMetadata.canBeAnnotationType(annotationType),
       )
       .map(({ key, value: chartMetadata }) => ({
-        value: key,
+        value: key === VizType.Line ? 'line' : key,
         label: chartMetadata.name,
       }));
     // Prepend native source if applicable
