b79618f9723a6d5f6b8f17c003562427972d9183
Michael S. Molina <70410625+michael-s-molina@users.noreply.github.com>
2025-08-15 12:59:30 -0300

    fix: Timeseries annotation layers (#34709)

--- superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/transformProps.ts
+++ superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/transformProps.ts
@@ -493,7 +493,7 @@ export default function transformProps(
         ForecastSeriesEnum.Observation,
     )
     .map(entry => entry.name || '')
-    .concat(extractAnnotationLabels(annotationLayers, annotationData));
+    .concat(extractAnnotationLabels(annotationLayers));
 
   let xAxis: any = {
     type: xAxisType,
