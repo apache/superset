7f3db1e93a6e93098a49bba6971a450338a11b81
justinpark <justin.hojun.park@gmail.com>
2025-08-28 11:21:46 -0700

    replace describe/it by test

--- superset-frontend/packages/superset-ui-core/src/chart/components/SuperChartCore.test.tsx
+++ superset-frontend/packages/superset-ui-core/src/chart/components/SuperChartCore.test.tsx
@@ -27,66 +27,64 @@ import {
 
 import SuperChartCore from './SuperChartCore';
 
-describe('processChartProps', () => {
-  const props = {
-    chartType: 'line',
-  };
-  const FakeChart = () => <span>test</span>;
+const props = {
+  chartType: 'line',
+};
+const FakeChart = () => <span>test</span>;
 
-  beforeEach(() => {
-    const metadata = new ChartMetadata({
-      name: 'test-chart',
-      thumbnail: '',
-    });
-    const buildQuery = () => ({
-      datasource: { id: 1, type: DatasourceType.Table },
-      queries: [{ granularity: 'day' }],
-      force: false,
-      result_format: 'json',
-      result_type: 'full',
-    });
-    const controlPanel = { abc: 1 };
-    const plugin = new ChartPlugin({
-      metadata,
-      Chart: FakeChart,
-      buildQuery,
-      controlPanel,
-    });
-    plugin.configure({ key: props.chartType }).register();
+beforeEach(() => {
+  const metadata = new ChartMetadata({
+    name: 'test-chart',
+    thumbnail: '',
   });
-
-  it('should return the result from cache unless transformProps has changed', async () => {
-    const pre = jest.fn(x => x);
-    const transform = jest.fn(x => x);
-    const post = jest.fn(x => x);
-    expect(getChartComponentRegistry().get(props.chartType)).toBe(FakeChart);
-
-    expect(pre).toHaveBeenCalledTimes(0);
-    const { rerender } = render(
-      <SuperChartCore
-        {...props}
-        preTransformProps={pre}
-        overrideTransformProps={transform}
-        postTransformProps={post}
-      />,
-    );
-
-    await waitFor(() => expect(pre).toHaveBeenCalledTimes(1));
-    expect(transform).toHaveBeenCalledTimes(1);
-    expect(post).toHaveBeenCalledTimes(1);
-
-    const updatedPost = jest.fn(x => x);
-
-    rerender(
-      <SuperChartCore
-        {...props}
-        preTransformProps={pre}
-        overrideTransformProps={transform}
-        postTransformProps={updatedPost}
-      />,
-    );
-    await waitFor(() => expect(updatedPost).toHaveBeenCalledTimes(1));
-    expect(transform).toHaveBeenCalledTimes(1);
-    expect(pre).toHaveBeenCalledTimes(1);
+  const buildQuery = () => ({
+    datasource: { id: 1, type: DatasourceType.Table },
+    queries: [{ granularity: 'day' }],
+    force: false,
+    result_format: 'json',
+    result_type: 'full',
   });
+  const controlPanel = { abc: 1 };
+  const plugin = new ChartPlugin({
+    metadata,
+    Chart: FakeChart,
+    buildQuery,
+    controlPanel,
+  });
+  plugin.configure({ key: props.chartType }).register();
+});
+
+test('should return the result from cache unless transformProps has changed', async () => {
+  const pre = jest.fn(x => x);
+  const transform = jest.fn(x => x);
+  const post = jest.fn(x => x);
+  expect(getChartComponentRegistry().get(props.chartType)).toBe(FakeChart);
+
+  expect(pre).toHaveBeenCalledTimes(0);
+  const { rerender } = render(
+    <SuperChartCore
+      {...props}
+      preTransformProps={pre}
+      overrideTransformProps={transform}
+      postTransformProps={post}
+    />,
+  );
+
+  await waitFor(() => expect(pre).toHaveBeenCalledTimes(1));
+  expect(transform).toHaveBeenCalledTimes(1);
+  expect(post).toHaveBeenCalledTimes(1);
+
+  const updatedPost = jest.fn(x => x);
+
+  rerender(
+    <SuperChartCore
+      {...props}
+      preTransformProps={pre}
+      overrideTransformProps={transform}
+      postTransformProps={updatedPost}
+    />,
+  );
+  await waitFor(() => expect(updatedPost).toHaveBeenCalledTimes(1));
+  expect(transform).toHaveBeenCalledTimes(1);
+  expect(pre).toHaveBeenCalledTimes(1);
 });
