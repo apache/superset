1c465872dad333112a48e14b7579b54d19ef545b
Damian Pendrak <dpendrak@gmail.com>
2025-08-28 15:52:56 +0200

    Improve CustomDocLink component

--- superset-frontend/src/components/ErrorMessage/CustomDocLink.tsx
+++ superset-frontend/src/components/ErrorMessage/CustomDocLink.tsx
@@ -17,19 +17,17 @@
  * under the License.
  */
 
-import { useTheme } from '@superset-ui/core';
-import { Icons } from '@superset-ui/core/components';
+import { Flex, Icons } from '@superset-ui/core/components';
 
 export type CustomDocLinkProps = {
   url: string;
   label: string;
 };
 
-export const CustomDocLink = ({ url, label }: CustomDocLinkProps) => {
-  const theme = useTheme();
-  return (
-    <a href={url} target="_blank" rel="noopener noreferrer">
-      {label} <Icons.Full iconSize="m" iconColor={theme.colorPrimary} />
-    </a>
-  );
-};
+export const CustomDocLink = ({ url, label }: CustomDocLinkProps) => (
+  <a href={url} target="_blank" rel="noopener noreferrer">
+    <Flex align="center" gap={4}>
+      {label} <Icons.Full iconSize="m" />
+    </Flex>
+  </a>
+);
