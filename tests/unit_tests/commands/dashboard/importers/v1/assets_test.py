670a16bde79da671446be5c6ff44feb428748a9d
Adolfo Carvalho <adolfotcar@gmail.com>
2025-08-29 08:07:47 +0100

    Committed with pre-commit now...

--- tests/unit_tests/commands/dashboard/importers/v1/assets_test.py
+++ tests/unit_tests/commands/dashboard/importers/v1/assets_test.py
@@ -19,36 +19,36 @@ import copy
 
 from pytest_mock import MockerFixture
 from sqlalchemy.orm.session import Session
-from sqlalchemy.sql import select
 
 from tests.unit_tests.fixtures.assets_configs import (
     charts_config_1,
-    charts_config_2,
     dashboards_config_1,
-    dashboards_config_2,
     databases_config,
     datasets_config,
 )
 
-def test_import_dashboard_overwrite_charts_and_datasets(mocker: MockerFixture, session: Session) -> None:
+
+def test_import_dashboard_overwrite_charts_and_datasets(
+    mocker: MockerFixture, session: Session
+) -> None:
     """
     Test that existing dashboards are overwritten.
     """
+    import time
+
     from superset import db, security_manager
     from superset.commands.dashboard.importers.v1 import ImportDashboardsCommand
+    from superset.connectors.sqla.models import SqlaTable
     from superset.models.dashboard import Dashboard
     from superset.models.slice import Slice
-    from superset.connectors.sqla.models import SqlaTable
-    import time
-    from datetime import datetime
 
     mocker.patch.object(security_manager, "can_access", return_value=True)
 
     engine = db.session.get_bind()
-    Dashboard.metadata.create_all(engine)  
+    Dashboard.metadata.create_all(engine)
 
-    #for some reason it won't allow me to reuse the same config in the second import
-    #thus declaring two configs with the same content
+    # for some reason it won't allow me to reuse the same config in the second import
+    # thus declaring two configs with the same content
     base_configs = {
         **copy.deepcopy(databases_config),
         **copy.deepcopy(datasets_config),
@@ -62,7 +62,7 @@ def test_import_dashboard_overwrite_charts_and_datasets(mocker: MockerFixture, s
         **copy.deepcopy(dashboards_config_1),
     }
 
-    #gettings uuids
+    # gettings uuids
     dasboard_configs = list(dashboards_config_1.values())
     dashboard_uuid = dasboard_configs[0]["uuid"]
     chart_configs = list(charts_config_1.values())
@@ -70,34 +70,47 @@ def test_import_dashboard_overwrite_charts_and_datasets(mocker: MockerFixture, s
     dataset_configs = list(datasets_config.values())
     dataset_uuid = dataset_configs[0]["uuid"]
 
-    #importing first time and retrieving initial records
+    # importing first time and retrieving initial records
     ImportDashboardsCommand._import(base_configs, overwrite=True)
-    imported_dashboard = db.session.query(Dashboard).filter_by(uuid=dashboard_uuid).one()
+    imported_dashboard = (
+        db.session.query(Dashboard).filter_by(uuid=dashboard_uuid).one()
+    )
     imported_chart = db.session.query(Slice).filter_by(uuid=chart_uuid).one()
     imported_dataset = db.session.query(SqlaTable).filter_by(uuid=dataset_uuid).one()
 
-    #extracting changed_on field to compare later, ignoring milliseconds
-    initial_dashboard_changed_on = imported_dashboard.changed_on.strftime("%Y-%m-%d %H:%M:%S")
+    # extracting changed_on field to compare later, ignoring milliseconds
+    initial_dashboard_changed_on = imported_dashboard.changed_on.strftime(
+        "%Y-%m-%d %H:%M:%S"
+    )
     initial_chart_changed_on = imported_chart.changed_on.strftime("%Y-%m-%d %H:%M:%S")
-    initial_dataset_changed_on = imported_dataset.changed_on.strftime("%Y-%m-%d %H:%M:%S")
-   
-    #ensuring the changed_on field will be different
+    initial_dataset_changed_on = imported_dataset.changed_on.strftime(
+        "%Y-%m-%d %H:%M:%S"
+    )
+
+    # ensuring the changed_on field will be different
     time.sleep(1)
 
-    #importing second time and retrieving updated records
+    # importing second time and retrieving updated records
     ImportDashboardsCommand._import(new_configs, overwrite=True)
-    imported_dashboard = db.session.query(Dashboard).filter_by(uuid=dashboard_uuid).one()
+    imported_dashboard = (
+        db.session.query(Dashboard).filter_by(uuid=dashboard_uuid).one()
+    )
     imported_chart = db.session.query(Slice).filter_by(uuid=chart_uuid).one()
     imported_dataset = db.session.query(SqlaTable).filter_by(uuid=dataset_uuid).one()
 
-    #extracting changed_on field to compare with the previous retrieved, ignoring milliseconds
-    final_dashboard_changed_on = imported_dashboard.changed_on.strftime("%Y-%m-%d %H:%M:%S")
+    # extracting changed_on field to compare with the previous retrieved
+    # ignoring milliseconds
+    final_dashboard_changed_on = imported_dashboard.changed_on.strftime(
+        "%Y-%m-%d %H:%M:%S"
+    )
     final_chart_changed_on = imported_chart.changed_on.strftime("%Y-%m-%d %H:%M:%S")
     final_dataset_changed_on = imported_dataset.changed_on.strftime("%Y-%m-%d %H:%M:%S")
 
-    #asserting the changed_on field was updated on all three records
+    # asserting the changed_on field was updated on all three records
     assert initial_dashboard_changed_on != final_dashboard_changed_on
     assert initial_chart_changed_on != final_chart_changed_on
     assert initial_dataset_changed_on != final_dataset_changed_on
-    #asserting the changed_on field was updated to the same value on all three records
-    assert final_dashboard_changed_on == final_chart_changed_on == final_dataset_changed_on
+    # asserting the changed_on field was updated to the same value on all three records
+    assert (
+        final_dashboard_changed_on == final_chart_changed_on == final_dataset_changed_on
+    )
