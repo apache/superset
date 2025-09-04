b65d73859fccb96d288e55b4f9d4368c3152e165
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2025-09-04 20:43:59 +0300

    fix(RoleListEditModal): display user's other properties in table (#35017)

--- superset-frontend/src/features/roles/RoleListEditModal.tsx
+++ superset-frontend/src/features/roles/RoleListEditModal.tsx
@@ -132,6 +132,9 @@ function RoleListEditModal({
       mapResult: (user: UserObject) => ({
         id: user.id,
         username: user.username,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email,
       }),
     });
   }, [user_ids]);
