afeee08b49d4946f55c1327b023964c5700ff11c
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2025-09-20 17:05:43 +0300

    chore: remove test logs

--- superset-frontend/src/pages/ThemeList/index.tsx
+++ superset-frontend/src/pages/ThemeList/index.tsx
@@ -112,9 +112,6 @@ function ThemesList({
   const [importingTheme, showImportModal] = useState<boolean>(false);
   const [appliedThemeId, setAppliedThemeId] = useState<number | null>(null);
 
-  addDangerToast('TEST DANGER TOAST');
-  addSuccessToast('TEST SUCCESS TOAST');
-
   const canCreate = hasPerm('can_write');
   const canEdit = hasPerm('can_write');
   const canDelete = hasPerm('can_write');
