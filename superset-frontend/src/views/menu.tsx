e1678fd5c756a916e8d183f5c1c9e1e4ab9d1308
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2025-09-21 17:47:42 +0300

    fix: lint

--- superset-frontend/src/views/menu.tsx
+++ superset-frontend/src/views/menu.tsx
@@ -29,8 +29,8 @@ import createCache from '@emotion/cache';
 import { ThemeProvider, theme } from '@superset-ui/core';
 import Menu from 'src/features/home/Menu';
 import getBootstrapData from 'src/utils/getBootstrapData';
-import { setupStore } from './store';
 import { NotificationProvider } from 'src/components/MessageToasts/NotificationProvider';
+import { setupStore } from './store';
 
 // Disable connecting to redux debugger so that the React app injected
 // Below the menu like SqlLab or Explore can connect its redux store to the debugger
