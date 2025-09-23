5fbda3af40416d3ee8c66c72b413b6d683bc906e
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2025-09-23 22:31:10 +0300

    chore(effect): add eslint plugin to reduce rerenders (#35223)

--- superset-frontend/packages/superset-ui-core/src/components/Select/Select.tsx
+++ superset-frontend/packages/superset-ui-core/src/components/Select/Select.tsx
@@ -127,13 +127,9 @@ const Select = forwardRef(
     const shouldShowSearch = allowNewOptions ? true : showSearch;
     const [selectValue, setSelectValue] = useState(value);
     const [inputValue, setInputValue] = useState('');
-    const [isLoading, setIsLoading] = useState(loading);
     const [isDropdownVisible, setIsDropdownVisible] = useState(false);
     const [isSearching, setIsSearching] = useState(false);
     const [visibleOptions, setVisibleOptions] = useState<SelectOptionsType>([]);
-    const [maxTagCount, setMaxTagCount] = useState(
-      propsMaxTagCount ?? MAX_TAG_COUNT,
-    );
     const [onChangeCount, setOnChangeCount] = useState(0);
     const previousChangeCount = usePrevious(onChangeCount, 0);
     const fireOnChange = useCallback(
@@ -141,11 +137,11 @@ const Select = forwardRef(
       [onChangeCount],
     );
 
-    useEffect(() => {
-      if (oneLine) {
-        setMaxTagCount(isDropdownVisible ? 0 : 1);
-      }
-    }, [isDropdownVisible, oneLine]);
+    const maxTagCount = oneLine
+      ? isDropdownVisible
+        ? 0
+        : 1
+      : (propsMaxTagCount ?? MAX_TAG_COUNT);
 
     const mappedMode = isSingleMode ? undefined : 'multiple';
 
@@ -510,6 +506,8 @@ const Select = forwardRef(
       ],
     );
 
+    const isLoading = loading ?? false;
+
     const popupRender = (
       originNode: ReactElement & { ref?: RefObject<HTMLElement> },
     ) =>
@@ -535,12 +533,6 @@ const Select = forwardRef(
       setSelectOptions(initialOptions);
       setVisibleOptions(initialOptions);
     }, [initialOptions]);
-
-    useEffect(() => {
-      if (loading !== undefined && loading !== isLoading) {
-        setIsLoading(loading);
-      }
-    }, [isLoading, loading]);
 
     useEffect(() => {
       setSelectValue(value);
