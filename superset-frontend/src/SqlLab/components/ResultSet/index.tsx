6704c0aaec792e3cd934e9721231d0b9121a414b
Martyn Gigg <martyn.gigg@gmail.com>
2025-10-27 21:29:30 +0000

    fix(sqllab): Fix CSV export button href in SQL Lab when application root is defined (#35118)

--- superset-frontend/src/SqlLab/components/ResultSet/index.tsx
+++ superset-frontend/src/SqlLab/components/ResultSet/index.tsx
@@ -86,6 +86,7 @@ import {
 } from 'src/logger/LogUtils';
 import { Icons } from '@superset-ui/core/components/Icons';
 import { findPermission } from 'src/utils/findPermission';
+import { ensureAppRoot } from 'src/utils/pathUtils';
 import { useConfirmModal } from 'src/hooks/useConfirmModal';
 import ExploreCtasResultsButton from '../ExploreCtasResultsButton';
 import ExploreResultsButton from '../ExploreResultsButton';
@@ -303,7 +304,7 @@ const ResultSet = ({
   };
 
   const getExportCsvUrl = (clientId: string) =>
-    `/api/v1/sqllab/export/${clientId}/`;
+    ensureAppRoot(`/api/v1/sqllab/export/${clientId}/`);
 
   const renderControls = () => {
     if (search || visualize || csv) {
