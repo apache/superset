67fa83cd5a430c460c332901c79f1b1a51e88f43
ExuApplePie <nic.louie@mail.utoronto.ca>
2025-11-23 19:52:32 -0500

    Change screenshot location to be in the docs folder

--- superset-frontend/playwright/tests/docs/docs-screenshots.spec.ts
+++ superset-frontend/playwright/tests/docs/docs-screenshots.spec.ts
@@ -1,6 +1,8 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
+const docsPath: string = '../docs/static/img/screenshots/'
+
 test('chart type screenshot', async ({ page }) => {
     await page.goto('/chart/add');
     await page.waitForLoadState('domcontentloaded');
@@ -8,7 +10,7 @@ test('chart type screenshot', async ({ page }) => {
     await page.getByRole('tab', { name: 'All charts' }).click();
     await page.evaluate(() => document.body.style.zoom = '0.8');
     await page.waitForTimeout(1000); // wait for charts to load
-    await (page.locator('.viz-gallery')).screenshot({ path: 'playwright/tests/docs/Screenshots/chart_type_gallery.png' });
+    await (page.locator('.viz-gallery')).screenshot({ path: docsPath + 'gallery.jpg', type: 'jpeg' });
 });
 
 test('dashboard content screenshot', async ({ page }) => {
@@ -17,7 +19,7 @@ test('dashboard content screenshot', async ({ page }) => {
     await page.getByRole('link', { name: 'Slack Dashboard' }).click();
     await page.evaluate(() => document.body.style.zoom = '0.8');
     await page.waitForTimeout(5000); // wait for chart to load
-    await (page.locator('[data-test="dashboard-content-wrapper"]')).screenshot({ path: 'playwright/tests/docs/Screenshots/dashboard-content.png' });
+    await (page.locator('[data-test="dashboard-content-wrapper"]')).screenshot({ path: docsPath + 'slack_dash.jpg', type: 'jpeg' });
 });
 
 test('chart editor screenshot', async ({ page }) => {
@@ -27,7 +29,7 @@ test('chart editor screenshot', async ({ page }) => {
     await page.locator('[data-test="filters-search"]').press('Enter');
     await page.locator('[data-test="Life Expectancy VS Rural %-list-chart-title"]').click();
     await page.waitForTimeout(10000); // wait for charts to load
-    await page.screenshot({ path: 'playwright/tests/docs/Screenshots/chart-editor.png', fullPage: true });
+    await page.screenshot({ path: docsPath + 'explore.jpg', type: 'jpeg', fullPage: true });
 });
 
 test('sqllab screenshot', async ({ page }) => {
@@ -39,11 +41,13 @@ test('sqllab screenshot', async ({ page }) => {
     // await page.locator('div').filter({ hasText: /^main$/ }).nth(2).click();
     await page.getByRole('combobox', { name: 'Select schema or type to' }).fill('main');
     await page.getByRole('combobox', { name: 'Select schema or type to' }).press('Enter');
+    await page.waitForTimeout(1000);
     await page.getByRole('combobox', { name: 'Select table or type to' }).fill('covid');
     await page.getByRole('combobox', { name: 'Select table or type to' }).press('Enter');
     await page.locator('.ace_content').click();
     await page.getByRole('textbox', { name: 'Cursor at row' }).fill('SELECT "developer_researcher",\n"stage_of_development",\n"product_category",\n"country_name",\nFROM main.covid_vaccines');
     await page.locator('[data-test="run-query-action"]').click();
+    await page.locator('.ace_content').click();
     await page.waitForTimeout(3000); // wait for charts to load
-    await page.screenshot({ path: 'playwright/tests/docs/Screenshots/sqllab.png', fullPage: true });
+    await page.screenshot({ path: docsPath + 'sql_lab.jpg', type: 'jpeg', fullPage: true });
 });
