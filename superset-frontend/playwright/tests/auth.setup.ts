bb17199c7acdc0b1a698ca8c999700703860a2c5
ExuApplePie <nic.louie@mail.utoronto.ca>
2025-11-25 16:58:47 -0500

    change auth.setup.ts to use html id for username and password so it works in dev environment

--- superset-frontend/playwright/tests/auth.setup.ts
+++ superset-frontend/playwright/tests/auth.setup.ts
@@ -6,9 +6,9 @@ const authFile = path.join(__dirname, '../.auth/admin.json');
 setup('authenticate', async ({ page }) => {
   await page.goto('/login/');
   await page.waitForLoadState('domcontentloaded');
-  await page.locator('[data-test="username-input"]').fill('admin');
-  await page.locator('[data-test="password-input"]').fill('admin');
-  await page.locator('[data-test="login-button"]').click();
+  await page.locator('#username').fill('admin');
+  await page.locator('#password').fill('admin');
+  await page.locator('[type="submit"]').click();
 
   // wait until redirected to any /superset/ URL
   await page.waitForURL("http://localhost:8088/superset/welcome/", { timeout: 60000});
