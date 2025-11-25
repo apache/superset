089e11df2d5406d6d0831e6581e2f4147381a09b
amaannawab923 <amaannawab923@gmail.com>
2025-11-25 23:25:35 +0530

    ag grid time filter correction

--- superset-frontend/plugins/plugin-chart-ag-grid-table/src/utils/dateFilterComparator.ts
+++ superset-frontend/plugins/plugin-chart-ag-grid-table/src/utils/dateFilterComparator.ts
@@ -17,25 +17,49 @@
  * under the License.
  */
 
+/**
+ * Timezone-safe date comparator for AG Grid date filters.
+ *
+ * This comparator normalizes both dates to UTC midnight before comparison,
+ * fixing the off-by-one day bug that occurs when:
+ * - User's timezone differs from UTC
+ * - Data is stored in UTC but filtered in local time
+ * - Midnight boundary crossing due to timezone offset
+ *
+ * Bug references:
+ * - AG Grid Issue #8611: UTC Date Editor Problem
+ * - AG Grid Issue #3921: DateFilter timezone regression
+ *
+ */
 const dateFilterComparator = (filterDate: Date, cellValue: Date) => {
+  if (cellValue == null) {
+    return -1;
+  }
+
   const cellDate = new Date(cellValue);
-  cellDate.setHours(0, 0, 0, 0);
-  if (Number.isNaN(cellDate?.getTime())) return -1;
+  if (Number.isNaN(cellDate?.getTime())) {
+    return -1;
+  }
 
-  const cellDay = cellDate.getDate();
-  const cellMonth = cellDate.getMonth();
-  const cellYear = cellDate.getFullYear();
 
-  const filterDay = filterDate.getDate();
-  const filterMonth = filterDate.getMonth();
-  const filterYear = filterDate.getFullYear();
+  const filterUTC = Date.UTC(
+    filterDate.getFullYear(),
+    filterDate.getMonth(),
+    filterDate.getDate(),
+  );
 
-  if (cellYear < filterYear) return -1;
-  if (cellYear > filterYear) return 1;
-  if (cellMonth < filterMonth) return -1;
-  if (cellMonth > filterMonth) return 1;
-  if (cellDay < filterDay) return -1;
-  if (cellDay > filterDay) return 1;
+  const cellUTC = Date.UTC(
+    cellDate.getUTCFullYear(),
+    cellDate.getUTCMonth(),
+    cellDate.getUTCDate(),
+  );
+
+  if (cellUTC < filterUTC) {
+    return -1;
+  }
+  if (cellUTC > filterUTC) {
+    return 1;
+  }
 
   return 0;
 };
