c82e0bb4d65b7130c45f8b97191f05d91b98d4cc
luizotavio32 <luizotavioindiana@hotmail.com>
2026-01-07 10:00:53 -0300

    pre-commit fixes

--- superset-frontend/packages/superset-ui-core/src/query/types/QueryResponse.ts
+++ superset-frontend/packages/superset-ui-core/src/query/types/QueryResponse.ts
@@ -16,7 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 import { GenericDataType } from '@apache-superset/core/api/core';
 import { TimeseriesDataRecord } from '../../chart';
 import { AnnotationData } from './AnnotationLayer';
