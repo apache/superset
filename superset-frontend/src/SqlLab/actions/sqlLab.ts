d0ab78235be259d0696314157c05e4e0144ec52e
justinpark <justin.hojun.park@gmail.com>
2026-01-06 17:58:31 -0800

    feat(sqllab): Improved query status indicator bar

--- superset-frontend/src/SqlLab/actions/sqlLab.ts
+++ superset-frontend/src/SqlLab/actions/sqlLab.ts
@@ -92,6 +92,7 @@ export interface SqlExecuteQueryResult {
   limitingFactor?: string;
   tempTable?: string | null;
   progress?: number;
+  progressText?: string | null;
   state?: string;
   [key: string]: unknown;
 }
@@ -392,7 +393,7 @@ export function startQuery(query: Query, runPreviewOnly?: boolean) {
     id: query.id ? query.id : nanoid(11),
     progress: 0,
     startDttm: now(),
-    state: query.runAsync ? 'pending' : 'running',
+    state: 'pending',
     cached: false,
   });
   return { type: START_QUERY, query, runPreviewOnly } as const;
