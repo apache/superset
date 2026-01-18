89232fe786f3a8c26c94d92f346ef25b0a6b233c
Reynold Morel <reynoldmorel@gmail.com>
2026-01-18 02:26:02 -0400

    fix(chart): Show metric label in tooltips

--- superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/transformProps.ts
+++ superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/transformProps.ts
@@ -81,6 +81,7 @@ import {
   getAnnotationData,
 } from '../utils/annotation';
 import {
+  addLabelMapToVerboseMap,
   extractForecastSeriesContext,
   extractForecastSeriesContexts,
   extractForecastValuesFromTooltipParams,
@@ -134,7 +135,7 @@ export default function transformProps(
   let focusedSeries: string | null = null;
 
   const {
-    verboseMap = {},
+    verboseMap: originalVerboseMap = {},
     columnFormats = {},
     currencyFormats = {},
     currencyCodeColumn,
@@ -145,6 +146,8 @@ export default function transformProps(
     label_map = {},
     detected_currency: backendDetectedCurrency,
   } = queryData as TimeseriesChartDataResponseResult;
+
+  const verboseMap = addLabelMapToVerboseMap(label_map, originalVerboseMap);
 
   const dataTypes = getColtypesMapping(queryData);
   const annotationData = getAnnotationData(chartProps);
