509f88d294db02220de0994d5a71b5b52b9e1ed9
Nicolas <n.berry@urios.com>
2026-01-23 22:53:09 +0100

    Table chart: Put colon inside "search by" translated string

--- superset-frontend/plugins/plugin-chart-table/src/DataTable/DataTable.tsx
+++ superset-frontend/plugins/plugin-chart-table/src/DataTable/DataTable.tsx
@@ -563,7 +563,7 @@ export default typedMemo(function DataTable<D extends object>({
                 <>
                   {serverPagination && (
                     <Space size="small" className="search-select-container">
-                      <span className="search-by-label">{t('Search by')}:</span>
+                      <span className="search-by-label">{t('Search by:')}</span>
                       <SearchSelectDropdown
                         searchOptions={searchOptions}
                         value={serverPaginationData?.searchColumn || ''}
