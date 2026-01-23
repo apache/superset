c5be5dbe8eb40a8007d960581faed579fb892f0e
ionik0 <snikhilkr001@gmail.com>
2026-01-23 22:30:55 +0530

    fix(typing): add type ignore for Windows-incompatible signal module and remove unused type comments

--- superset/utils/core.py
+++ superset/utils/core.py
@@ -704,8 +704,8 @@ class SigalrmTimeout:
     def __enter__(self) -> None:
         try:
             if threading.current_thread() == threading.main_thread():
-                signal.signal(signal.SIGALRM, self.handle_timeout)
-                signal.alarm(self.seconds)
+                signal.signal(signal.SIGALRM, self.handle_timeout)  # type: ignore[attr-defined]
+                signal.alarm(self.seconds)  # type: ignore[attr-defined]
         except ValueError as ex:
             logger.warning("timeout can't be used in the current context")
             logger.exception(ex)
@@ -714,7 +714,7 @@ class SigalrmTimeout:
         self, type: Any, value: Any, traceback: TracebackType
     ) -> None:
         try:
-            signal.alarm(0)
+            signal.alarm(0)  # type: ignore[attr-defined]
         except ValueError as ex:
             logger.warning("timeout can't be used in the current context")
             logger.exception(ex)
