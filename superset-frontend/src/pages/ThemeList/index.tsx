03edc2725790793e6a90440a47fdb5338389fc72
Jean Massucatto <massucattoj@gmail.com>
2026-01-23 20:05:43 -0300

    fix(themes): correct action icons size and restore missing tooltips

--- superset-frontend/src/pages/ThemeList/index.tsx
+++ superset-frontend/src/pages/ThemeList/index.tsx
@@ -386,14 +386,14 @@ function ThemesList({
                 </Tooltip>
               )}
               {original.is_system_default && (
-                <Tooltip title={t('This is the system default theme')}>
+                <Tooltip title={t('This is the default light theme')}>
                   <IconTag color="warning" icon={<Icons.SunOutlined />}>
                     {t('Default')}
                   </IconTag>
                 </Tooltip>
               )}
               {original.is_system_dark && (
-                <Tooltip title={t('This is the system dark theme')}>
+                <Tooltip title={t('This is the default dark theme')}>
                   <IconTag color="default" icon={<Icons.MoonOutlined />}>
                     {t('Dark')}
                   </IconTag>
@@ -443,8 +443,7 @@ function ThemesList({
               ? {
                   label: 'apply-action',
                   tooltip: t(
-                    'Set local theme. Will be applied to your session until unset.',
-                  ),
+                    'Set local theme for testing'),
                   placement: 'bottom',
                   icon: 'ThunderboltOutlined',
                   onClick: handleApply,
@@ -454,8 +453,8 @@ function ThemesList({
               ? {
                   label: 'edit-action',
                   tooltip: original.is_system
-                    ? t('View theme')
-                    : t('Edit theme'),
+                    ? t('View')
+                    : t('Edit'),
                   placement: 'bottom',
                   icon: original.is_system ? 'EyeOutlined' : 'EditOutlined',
                   onClick: handleEdit,
@@ -464,7 +463,7 @@ function ThemesList({
             canExport
               ? {
                   label: 'export-action',
-                  tooltip: t('Export theme'),
+                  tooltip: t('Export'),
                   placement: 'bottom',
                   icon: 'UploadOutlined',
                   onClick: handleExport,
@@ -473,7 +472,7 @@ function ThemesList({
             canSetSystemThemes && !original.is_system_default
               ? {
                   label: 'set-default-action',
-                  tooltip: t('Set as system default theme'),
+                  tooltip: t('Set as default light theme'),
                   placement: 'bottom',
                   icon: 'SunOutlined',
                   onClick: () => handleSetSystemDefault(original),
@@ -482,7 +481,7 @@ function ThemesList({
             canSetSystemThemes && original.is_system_default
               ? {
                   label: 'unset-default-action',
-                  tooltip: t('Remove as system default theme'),
+                  tooltip: t('Clear default light theme'),
                   placement: 'bottom',
                   icon: 'StopOutlined',
                   onClick: () => handleUnsetSystemDefault(),
@@ -491,7 +490,7 @@ function ThemesList({
             canSetSystemThemes && !original.is_system_dark
               ? {
                   label: 'set-dark-action',
-                  tooltip: t('Set as system dark theme'),
+                  tooltip: t('Set as default dark theme'),
                   placement: 'bottom',
                   icon: 'MoonOutlined',
                   onClick: () => handleSetSystemDark(original),
@@ -500,7 +499,7 @@ function ThemesList({
             canSetSystemThemes && original.is_system_dark
               ? {
                   label: 'unset-dark-action',
-                  tooltip: t('Remove as system dark theme'),
+                  tooltip: t('Clear default dark theme'),
                   placement: 'bottom',
                   icon: 'StopOutlined',
                   onClick: () => handleUnsetSystemDark(),
