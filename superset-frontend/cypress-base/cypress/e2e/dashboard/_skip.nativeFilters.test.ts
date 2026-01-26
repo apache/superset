68cd764e1f388ccf5be87a40d20dc1d3f32e3217
LevisNgigi <ngigilevis@gmail.com>
2026-01-26 09:44:51 +0300

    fix tests

--- superset-frontend/cypress-base/cypress/e2e/dashboard/_skip.nativeFilters.test.ts
+++ superset-frontend/cypress-base/cypress/e2e/dashboard/_skip.nativeFilters.test.ts
@@ -47,7 +47,7 @@ import {
 } from './shared_dashboard_functions';
 
 function selectFilter(index: number) {
-  cy.get("[data-test='filter-title-container'] [draggable='true']")
+  cy.get("[data-test='filter-title-container'] [role='tab']")
     .eq(index)
     .click();
 }
