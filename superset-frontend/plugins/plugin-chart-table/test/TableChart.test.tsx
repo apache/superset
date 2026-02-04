f7dfc417b2a33271b80df368304b920620adeddf
SBIN2010 <SBIN2010@mail.ru>
2026-02-04 20:42:00 +0300

    fix: old test

--- superset-frontend/plugins/plugin-chart-table/test/TableChart.test.tsx
+++ superset-frontend/plugins/plugin-chart-table/test/TableChart.test.tsx
@@ -17,6 +17,7 @@
  * under the License.
  */
 import '@testing-library/jest-dom';
+import { ObjectFormattingEnum } from '@superset-ui/chart-controls';
 import {
   render,
   screen,
@@ -1235,6 +1236,112 @@ describe('plugin-chart-table', () => {
           'rgba(172, 225, 196, 1)',
         );
         expect(getComputedStyle(screen.getByText('N/A')).background).toBe('');
+      });
+
+      test('render color with column color formatter to entire row', () => {
+        render(
+          ProviderWrapper({
+            children: (
+              <TableChart
+                {...transformProps({
+                  ...testData.advanced,
+                  rawFormData: {
+                    ...testData.advanced.rawFormData,
+                    conditional_formatting: [
+                      {
+                        colorScheme: '#ACE1C4',
+                        column: 'sum__num',
+                        operator: '>',
+                        targetValue: 2467,
+                        columnFormating: ObjectFormattingEnum.ENTIRE_ROW,
+                      },
+                    ],
+                  },
+                })}
+              />
+            ),
+          }),
+        );
+
+        expect(getComputedStyle(screen.getByText('Michael')).background).toBe(
+          'rgba(172, 225, 196, 1)',
+        );
+        expect(getComputedStyle(screen.getByTitle('2467063')).background).toBe(
+          'rgba(172, 225, 196, 1)',
+        );
+        expect(getComputedStyle(screen.getByTitle('0.123456')).background).toBe(
+          'rgba(172, 225, 196, 1)',
+        );
+      });
+
+      test('display text color using column color formatter', () => {
+        render(
+          ProviderWrapper({
+            children: (
+              <TableChart
+                {...transformProps({
+                  ...testData.advanced,
+                  rawFormData: {
+                    ...testData.advanced.rawFormData,
+                    conditional_formatting: [
+                      {
+                        colorScheme: '#ACE1C4',
+                        column: 'sum__num',
+                        operator: '>',
+                        targetValue: 2467,
+                        objectFormating: ObjectFormattingEnum.TEXT_COLOR,
+                      },
+                    ],
+                  },
+                })}
+              />
+            ),
+          }),
+        );
+
+        expect(getComputedStyle(screen.getByTitle('2467063')).color).toBe(
+          'rgba(172, 225, 196, 1)',
+        );
+        expect(getComputedStyle(screen.getByTitle('2467')).color).toBe(
+          'rgba(0, 0, 0, 0.88)',
+        );
+      });
+
+      test('display text color using column color formatter for entire row', () => {
+        render(
+          ProviderWrapper({
+            children: (
+              <TableChart
+                {...transformProps({
+                  ...testData.advanced,
+                  rawFormData: {
+                    ...testData.advanced.rawFormData,
+                    conditional_formatting: [
+                      {
+                        colorScheme: '#ACE1C4',
+                        column: 'sum__num',
+                        operator: '>',
+                        targetValue: 2467,
+                        columnFormating: ObjectFormattingEnum.ENTIRE_ROW,
+                        objectFormating: ObjectFormattingEnum.TEXT_COLOR,
+                      },
+                    ],
+                  },
+                })}
+              />
+            ),
+          }),
+        );
+
+        expect(getComputedStyle(screen.getByText('Michael')).color).toBe(
+          'rgba(172, 225, 196, 1)',
+        );
+        expect(getComputedStyle(screen.getByTitle('2467063')).color).toBe(
+          'rgba(172, 225, 196, 1)',
+        );
+        expect(getComputedStyle(screen.getByTitle('0.123456')).color).toBe(
+          'rgba(172, 225, 196, 1)',
+        );
       });
 
       test('render color with useGradient false returns solid color', () => {
