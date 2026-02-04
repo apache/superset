17089641db47670366f0afb265ba02a3bfd6ea7b
Michael S. Molina <michael.s.molina@gmail.com>
2026-02-04 12:18:06 -0300

    Use compatible API

--- superset-frontend/packages/superset-ui-core/src/chart/components/FallbackComponent.tsx
+++ superset-frontend/packages/superset-ui-core/src/chart/components/FallbackComponent.tsx
@@ -20,11 +20,13 @@
 import { t } from '@apache-superset/core';
 import { SupersetTheme } from '@apache-superset/core/ui';
 import { FallbackPropsWithDimension } from './SuperChart';
-import { getErrorMessage } from 'react-error-boundary';
 
 export type Props = Partial<FallbackPropsWithDimension>;
 
 export default function FallbackComponent({ error, height, width }: Props) {
+  const errorMessage =
+    error instanceof Error ? error.message : String(error ?? 'Unknown Error');
+
   return (
     <div
       css={(theme: SupersetTheme) => ({
@@ -39,7 +41,7 @@ export default function FallbackComponent({ error, height, width }: Props) {
         <div>
           <b>{t('Oops! An error occurred!')}</b>
         </div>
-        <code>{error ? getErrorMessage(error) : 'Unknown Error'}</code>
+        <code>{errorMessage}</code>
       </div>
     </div>
   );
