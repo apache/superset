f8e4bc2f22c562d7c0527be48f0c957794f8d343
Vanessa Giannoni <vanessa.o.giannoni@gmail.com>
2026-02-11 11:02:37 -0300

    fix(table): make time grain visibility case-insensitive in control panel

--- superset-frontend/plugins/plugin-chart-table/src/controlPanel.tsx
+++ superset-frontend/plugins/plugin-chart-table/src/controlPanel.tsx
@@ -273,7 +273,7 @@ const config: ControlPanelConfig = {
               visibility: ({ controls }) => {
                 const dttmLookup = Object.fromEntries(
                   ensureIsArray(controls?.groupby?.options).map(option => [
-                    option.column_name,
+                    (option.column_name || '').toLowerCase(),
                     option.is_dttm,
                   ]),
                 );
@@ -284,7 +284,7 @@ const config: ControlPanelConfig = {
                       return true;
                     }
                     if (isPhysicalColumn(selection)) {
-                      return !!dttmLookup[selection];
+                      return !!dttmLookup[(selection || '').toLowerCase()];
                     }
                     return false;
                   })
