89458957e76eaca93692e8ec4cfa895e8ce5e3a4
hainenber <dotronghai96@gmail.com>
2026-03-22 23:28:17 +0700

    chore: skip Jest transform for ESM-only `json-stringify-pretty-compact` and type-cast for MemoizedFunction type in test
    Signed-off-by: hainenber <dotronghai96@gmail.com>

--- superset-frontend/plugins/plugin-chart-echarts/src/BigNumber/BigNumberTotal/transformProps.test.ts
+++ superset-frontend/plugins/plugin-chart-echarts/src/BigNumber/BigNumberTotal/transformProps.test.ts
@@ -234,7 +234,9 @@ describe('BigNumberTotal transformProps', () => {
   test('should propagate colorThresholdFormatters from getColorFormatters', () => {
     // Override the getColorFormatters mock to return specific value
     const mockFormatters = [{ formatter: 'red' }];
-    (getColorFormatters as jest.Mock).mockReturnValueOnce(mockFormatters);
+    (getColorFormatters as unknown as jest.Mock).mockReturnValueOnce(
+      mockFormatters,
+    );
 
     const chartProps = {
       width: 800,
