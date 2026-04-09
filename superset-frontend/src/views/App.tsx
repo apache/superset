806c8b2adaceeb11b4849c887214f1d40f7842ed
Fedo Hagge-Kubat <office@aitema.org>
2026-04-09 21:17:46 +0200

    fix(a11y): render SkipLink in App shell and add main-content target ID

--- superset-frontend/src/views/App.tsx
+++ superset-frontend/src/views/App.tsx
@@ -28,6 +28,7 @@ import { css } from '@apache-superset/core/theme';
 import { Layout, Loading } from '@superset-ui/core/components';
 import { setupAGGridModules } from '@superset-ui/core/components/ThemedAgGridReact';
 import { ErrorBoundary } from 'src/components';
+import { SkipLink } from 'src/components/Accessibility';
 import Menu from 'src/features/home/Menu';
 import getBootstrapData, { applicationRoot } from 'src/utils/getBootstrapData';
 import ToastContainer from 'src/components/MessageToasts/ToastContainer';
@@ -75,6 +76,7 @@ const App = () => (
     <ScrollToTop />
     <LocationPathnameLogger />
     <RootContextProviders>
+      <SkipLink />
       <Menu
         data={bootstrapData.common.menu_data}
         isFrontendRoute={isFrontendRoute}
@@ -86,6 +88,7 @@ const App = () => (
               <Suspense fallback={<Fallback />}>
                 <Layout>
                   <Layout.Content
+                    id="main-content"
                     css={css`
                       display: flex;
                       flex-direction: column;
