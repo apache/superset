8fbac68a006528769702084ebfe9909c3085dbbf
Fedo Hagge-Kubat <office@aitema.org>
2026-04-09 20:50:03 +0200

    fix(a11y): WCAG 1.3.5 — add autocomplete attributes to form inputs for input purpose identification

--- superset-frontend/src/components/ImportModal/index.tsx
+++ superset-frontend/src/components/ImportModal/index.tsx
@@ -259,7 +259,7 @@ export const ImportModal: FunctionComponent<ImportModelsModalProps> = ({
                 </div>
                 <Input
                   name={`password-${fileName}`}
-                  autoComplete={`password-${fileName}`}
+                  autoComplete="off"
                   type="password"
                   value={passwords[fileName]}
                   onChange={event =>
@@ -279,7 +279,7 @@ export const ImportModal: FunctionComponent<ImportModelsModalProps> = ({
                 </div>
                 <Input
                   name={`ssh_tunnel_password-${fileName}`}
-                  autoComplete={`ssh_tunnel_password-${fileName}`}
+                  autoComplete="off"
                   type="password"
                   value={sshTunnelPasswords[fileName]}
                   onChange={event =>
@@ -300,7 +300,7 @@ export const ImportModal: FunctionComponent<ImportModelsModalProps> = ({
                 </div>
                 <Input.TextArea
                   name={`ssh_tunnel_private_key-${fileName}`}
-                  autoComplete={`ssh_tunnel_private_key-${fileName}`}
+                  autoComplete="off"
                   value={sshTunnelPrivateKeys[fileName]}
                   onChange={event =>
                     setSSHTunnelPrivateKeys({
@@ -322,7 +322,7 @@ export const ImportModal: FunctionComponent<ImportModelsModalProps> = ({
                 </div>
                 <Input
                   name={`ssh_tunnel_private_key_password-${fileName}`}
-                  autoComplete={`ssh_tunnel_private_key_password-${fileName}`}
+                  autoComplete="off"
                   type="password"
                   value={sshTunnelPrivateKeyPasswords[fileName]}
                   onChange={event =>
