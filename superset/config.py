61cdde1ff95e73c83b86569d2d7543855ac50fb0
David Kopelent <david.kopelent@saltpay.co>
2026-04-13 14:08:54 +0200

    chore: address bito comments on translations

--- superset/config.py
+++ superset/config.py
@@ -433,7 +433,7 @@ LANGUAGES = {
     "pt_BR": {"flag": "br", "name": "Brazilian Portuguese"},
     "ru": {"flag": "ru", "name": "Russian"},
     "ko": {"flag": "kr", "name": "Korean"},
-    "cs": {"flag": "cs", "name": "Czech"},
+    "cs": {"flag": "cz", "name": "Czech"},
     "sk": {"flag": "sk", "name": "Slovak"},
     "sl": {"flag": "si", "name": "Slovenian"},
     "nl": {"flag": "nl", "name": "Dutch"},
