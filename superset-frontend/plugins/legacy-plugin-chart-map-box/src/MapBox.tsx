62d9ee732baa8fee182573e350b3ca695d966533
Evan Rusackas <evan@rusackas.com>
2026-04-17 12:18:32 -0700

    revert: restore map-box class components (plugin deleted on master)
    The legacy-plugin-chart-map-box has been removed on master. Keeping
    the original class component form for these files avoids test failures
    that would be rebased away. Other files in this PR still convert as
    intended.

--- superset-frontend/plugins/legacy-plugin-chart-map-box/src/MapBox.tsx
+++ superset-frontend/plugins/legacy-plugin-chart-map-box/src/MapBox.tsx
@@ -16,7 +16,9 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import { useState, useCallback, useEffect, useMemo, useRef, memo } from 'react';
+/* eslint-disable react/jsx-sort-default-props, react/sort-prop-types */
+/* eslint-disable react/forbid-prop-types, react/require-default-props */
+import { Component } from 'react';
 import MapGL from 'react-map-gl';
 import { WebMercatorViewport } from '@math.gl/web-mercator';
 import ScatterPlotGlowOverlay from './ScatterPlotGlowOverlay';
@@ -64,184 +66,178 @@ interface MapBoxProps {
   viewportZoom?: number;
 }
 
-function computeFitBounds(
-  width: number,
-  height: number,
-  bounds?: [[number, number], [number, number]],
-): Viewport {
-  if (bounds && bounds[0] && bounds[1]) {
-    const mercator = new WebMercatorViewport({ width, height }).fitBounds(
-      bounds,
-    );
+interface MapBoxState {
+  viewport: Viewport;
+}
+
+const defaultProps: Partial<MapBoxProps> = {
+  width: 400,
+  height: 400,
+  globalOpacity: 1,
+  onViewportChange: NOOP,
+  pointRadius: DEFAULT_POINT_RADIUS,
+  pointRadiusUnit: 'Pixels',
+};
+
+class MapBox extends Component<MapBoxProps, MapBoxState> {
+  static defaultProps = defaultProps;
+
+  constructor(props: MapBoxProps) {
+    super(props);
+
+    const fitBounds = this.computeFitBoundsViewport();
+
+    this.state = {
+      viewport: this.mergeViewportWithProps(fitBounds),
+    };
+    this.handleViewportChange = this.handleViewportChange.bind(this);
+  }
+
+  handleViewportChange(viewport: Viewport) {
+    this.setState({ viewport });
+    const { onViewportChange } = this.props;
+    onViewportChange!(viewport);
+  }
+
+  mergeViewportWithProps(
+    fitBounds: Viewport,
+    viewport: Viewport = fitBounds,
+    props: MapBoxProps = this.props,
+    useFitBoundsForUnset = true,
+  ): Viewport {
+    const { viewportLongitude, viewportLatitude, viewportZoom } = props;
+
     return {
-      latitude: mercator.latitude,
-      longitude: mercator.longitude,
-      zoom: mercator.zoom,
+      ...viewport,
+      longitude:
+        viewportLongitude ??
+        (useFitBoundsForUnset ? fitBounds.longitude : viewport.longitude),
+      latitude:
+        viewportLatitude ??
+        (useFitBoundsForUnset ? fitBounds.latitude : viewport.latitude),
+      zoom:
+        viewportZoom ?? (useFitBoundsForUnset ? fitBounds.zoom : viewport.zoom),
     };
   }
-  return { latitude: 0, longitude: 0, zoom: 1 };
-}
 
-function mergeViewportWithProps(
-  fitBounds: Viewport,
-  viewport: Viewport,
-  viewportLongitude: number | undefined,
-  viewportLatitude: number | undefined,
-  viewportZoom: number | undefined,
-  useFitBoundsForUnset: boolean,
-): Viewport {
-  return {
-    ...viewport,
-    longitude:
-      viewportLongitude ??
-      (useFitBoundsForUnset ? fitBounds.longitude : viewport.longitude),
-    latitude:
-      viewportLatitude ??
-      (useFitBoundsForUnset ? fitBounds.latitude : viewport.latitude),
-    zoom:
-      viewportZoom ?? (useFitBoundsForUnset ? fitBounds.zoom : viewport.zoom),
-  };
-}
-
-function MapBox({
-  width = 400,
-  height = 400,
-  aggregatorName,
-  clusterer,
-  globalOpacity = 1,
-  hasCustomMetric,
-  mapStyle,
-  mapboxApiKey,
-  onViewportChange = NOOP,
-  pointRadius = DEFAULT_POINT_RADIUS,
-  pointRadiusUnit = 'Pixels',
-  renderWhileDragging,
-  rgb,
-  bounds,
-  viewportLongitude,
-  viewportLatitude,
-  viewportZoom,
-}: MapBoxProps) {
-  const initialViewport = useMemo((): Viewport => {
-    const fitBounds = computeFitBounds(width, height, bounds);
-    return mergeViewportWithProps(
-      fitBounds,
-      fitBounds,
-      viewportLongitude,
-      viewportLatitude,
-      viewportZoom,
-      true,
-    );
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []); // Compute once on mount; subsequent updates handled via useEffect below
-
-  const [viewport, setViewport] = useState<Viewport>(initialViewport);
-
-  const prevRef = useRef({
-    width,
-    height,
-    bounds,
-    viewportLongitude,
-    viewportLatitude,
-    viewportZoom,
-  });
-
-  useEffect(() => {
-    const prev = prevRef.current;
-    const fitBoundsInputsChanged =
-      prev.width !== width ||
-      prev.height !== height ||
-      prev.bounds !== bounds;
-    const viewportPropsChanged =
-      prev.viewportLongitude !== viewportLongitude ||
-      prev.viewportLatitude !== viewportLatitude ||
-      prev.viewportZoom !== viewportZoom;
-
-    if (fitBoundsInputsChanged || viewportPropsChanged) {
-      const fitBounds = computeFitBounds(width, height, bounds);
-      setViewport(prevViewport =>
-        mergeViewportWithProps(
-          fitBounds,
-          prevViewport,
-          viewportLongitude,
-          viewportLatitude,
-          viewportZoom,
-          fitBoundsInputsChanged || viewportPropsChanged,
-        ),
+  computeFitBoundsViewport(): Viewport {
+    const { width = 400, height = 400, bounds } = this.props;
+    if (bounds && bounds[0] && bounds[1]) {
+      const mercator = new WebMercatorViewport({ width, height }).fitBounds(
+        bounds,
       );
+      return {
+        latitude: mercator.latitude,
+        longitude: mercator.longitude,
+        zoom: mercator.zoom,
+      };
+    }
+    return { latitude: 0, longitude: 0, zoom: 1 };
+  }
+
+  componentDidUpdate(prevProps: MapBoxProps) {
+    const { viewport } = this.state;
+    const fitBoundsInputsChanged =
+      prevProps.width !== this.props.width ||
+      prevProps.height !== this.props.height ||
+      prevProps.bounds !== this.props.bounds;
+    const viewportPropsChanged =
+      prevProps.viewportLongitude !== this.props.viewportLongitude ||
+      prevProps.viewportLatitude !== this.props.viewportLatitude ||
+      prevProps.viewportZoom !== this.props.viewportZoom;
+
+    if (!fitBoundsInputsChanged && !viewportPropsChanged) {
+      return;
     }
 
-    prevRef.current = {
+    const fitBounds = this.computeFitBoundsViewport();
+    const nextViewport = this.mergeViewportWithProps(
+      fitBounds,
+      viewport,
+      this.props,
+      fitBoundsInputsChanged || viewportPropsChanged,
+    );
+
+    const viewportChanged =
+      nextViewport.longitude !== viewport.longitude ||
+      nextViewport.latitude !== viewport.latitude ||
+      nextViewport.zoom !== viewport.zoom;
+
+    if (viewportChanged) {
+      this.setState({ viewport: nextViewport });
+    }
+  }
+
+  render() {
+    const {
       width,
       height,
+      aggregatorName,
+      clusterer,
+      globalOpacity,
+      mapStyle,
+      mapboxApiKey,
+      pointRadius,
+      pointRadiusUnit,
+      renderWhileDragging,
+      rgb,
+      hasCustomMetric,
       bounds,
-      viewportLongitude,
-      viewportLatitude,
-      viewportZoom,
-    };
-  }, [width, height, bounds, viewportLongitude, viewportLatitude, viewportZoom]);
+    } = this.props;
+    const { viewport } = this.state;
+    const isDragging =
+      viewport.isDragging === undefined ? false : viewport.isDragging;
 
-  const handleViewportChange = useCallback(
-    (newViewport: Viewport) => {
-      setViewport(newViewport);
-      onViewportChange(newViewport);
-    },
-    [onViewportChange],
-  );
+    // Compute the clusters based on the original bounds and current zoom level. Note when zoom/pan
+    // to an area outside of the original bounds, no additional queries are made to the backend to
+    // retrieve additional data.
+    // add this variable to widen the visible area
+    const offsetHorizontal = ((width ?? 400) * 0.5) / 100;
+    const offsetVertical = ((height ?? 400) * 0.5) / 100;
 
-  const isDragging =
-    viewport.isDragging === undefined ? false : viewport.isDragging;
+    // Guard against empty datasets where bounds may be undefined
+    const bbox =
+      bounds && bounds[0] && bounds[1]
+        ? [
+            bounds[0][0] - offsetHorizontal,
+            bounds[0][1] - offsetVertical,
+            bounds[1][0] + offsetHorizontal,
+            bounds[1][1] + offsetVertical,
+          ]
+        : [-180, -90, 180, 90]; // Default to world bounds
 
-  // Compute the clusters based on the original bounds and current zoom level. Note when zoom/pan
-  // to an area outside of the original bounds, no additional queries are made to the backend to
-  // retrieve additional data.
-  // add this variable to widen the visible area
-  const offsetHorizontal = (width * 0.5) / 100;
-  const offsetVertical = (height * 0.5) / 100;
+    const clusters = clusterer.getClusters(bbox, Math.round(viewport.zoom));
 
-  // Guard against empty datasets where bounds may be undefined
-  const bbox =
-    bounds && bounds[0] && bounds[1]
-      ? [
-          bounds[0][0] - offsetHorizontal,
-          bounds[0][1] - offsetVertical,
-          bounds[1][0] + offsetHorizontal,
-          bounds[1][1] + offsetVertical,
-        ]
-      : [-180, -90, 180, 90]; // Default to world bounds
-
-  const clusters = clusterer.getClusters(bbox, Math.round(viewport.zoom));
-
-  const lngLatAccessor = useCallback((location: GeoJSONLocation) => {
-    const { coordinates } = location.geometry;
-    return [coordinates[0], coordinates[1]] as [number, number];
-  }, []);
-
-  return (
-    <MapGL
-      {...viewport}
-      mapStyle={mapStyle}
-      width={width}
-      height={height}
-      mapboxApiAccessToken={mapboxApiKey}
-      onViewportChange={handleViewportChange}
-      preserveDrawingBuffer
-    >
-      <ScatterPlotGlowOverlay
+    return (
+      <MapGL
         {...viewport}
-        isDragging={isDragging}
-        locations={clusters}
-        dotRadius={pointRadius}
-        pointRadiusUnit={pointRadiusUnit}
-        rgb={rgb}
-        globalOpacity={globalOpacity}
-        compositeOperation="screen"
-        renderWhileDragging={renderWhileDragging}
-        aggregation={hasCustomMetric ? aggregatorName : undefined}
-        lngLatAccessor={lngLatAccessor}
-      />
-    </MapGL>
-  );
+        mapStyle={mapStyle}
+        width={width}
+        height={height}
+        mapboxApiAccessToken={mapboxApiKey}
+        onViewportChange={this.handleViewportChange}
+        preserveDrawingBuffer
+      >
+        <ScatterPlotGlowOverlay
+          {...viewport}
+          isDragging={isDragging}
+          locations={clusters}
+          dotRadius={pointRadius}
+          pointRadiusUnit={pointRadiusUnit}
+          rgb={rgb}
+          globalOpacity={globalOpacity}
+          compositeOperation="screen"
+          renderWhileDragging={renderWhileDragging}
+          aggregation={hasCustomMetric ? aggregatorName : undefined}
+          lngLatAccessor={(location: GeoJSONLocation) => {
+            const { coordinates } = location.geometry;
+
+            return [coordinates[0], coordinates[1]];
+          }}
+        />
+      </MapGL>
+    );
+  }
 }
 
-export default memo(MapBox);
+export default MapBox;
