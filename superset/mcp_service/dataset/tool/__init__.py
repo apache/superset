052ed7caccc49f16a85ae1f2405ccfad7a7308b0
Amin Ghadersohi <amin.ghadersohi@gmail.com>
2026-04-30 18:03:41 -0400

    feat(mcp): add query_dataset tool to query datasets using semantic layer (#39727)
    (cherry picked from commit f29d82b3b1df3e7ce2bed6d8a48fbaf9da31222d)

--- superset/mcp_service/dataset/tool/__init__.py
+++ superset/mcp_service/dataset/tool/__init__.py
@@ -17,8 +17,10 @@
 
 from .get_dataset_info import get_dataset_info
 from .list_datasets import list_datasets
+from .query_dataset import query_dataset
 
 __all__ = [
     "list_datasets",
     "get_dataset_info",
+    "query_dataset",
 ]
