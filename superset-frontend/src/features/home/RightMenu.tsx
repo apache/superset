a43ae96aed8566a95ad1f8e596509465e85c675d
DanielKL <fwydegg@yandex.ru>
2026-06-04 12:26:59 +0400

    Remove Powered by Apache Superset from About menu

--- superset-frontend/src/features/home/RightMenu.tsx
+++ superset-frontend/src/features/home/RightMenu.tsx
@@ -546,8 +546,6 @@ const RightMenu = ({
                   `}
                 >
                   {[
-                    navbarRight.show_watermark &&
-                      t('Powered by Apache Superset'),
                     navbarRight.version_string &&
                       `${t('Version')}: ${navbarRight.version_string}`,
                     navbarRight.version_sha &&
