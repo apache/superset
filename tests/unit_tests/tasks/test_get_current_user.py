29a835e81bd7226a0e9a8bfb71135a586dfb1ec2
André Meyer <andre.dedemeyer@gmail.com>
2026-06-08 22:43:50 -0300

    fix: add Apache license header to test_get_current_user.py

--- tests/unit_tests/tasks/test_get_current_user.py
+++ tests/unit_tests/tasks/test_get_current_user.py
@@ -1,4 +1,20 @@
-﻿"""Testes unitários para get_current_user (superset/tasks/utils.py).
+﻿# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""Testes unitários para get_current_user (superset/tasks/utils.py).
 
 Técnicas aplicadas:
   - Caixa-preta : particionamento de equivalência (CV1, CI1-CI3)
@@ -134,3 +150,4 @@ class TestGetCurrentUser:
         with app.app_context():
             with patch.object(_mod, "g", mock_g):
                 assert get_current_user() is None
+
