73e32b6340ede277d68371d13656ef40f661c949
Ibrahim Ercan <ibrahim.ercan@vlmedia.com.tr>
2026-06-16 13:10:56 +0300

    unset logfile arg if log destination is not defined

--- docker/docker-bootstrap.sh
+++ docker/docker-bootstrap.sh
@@ -67,18 +67,24 @@ else
   echo "Skipping local overrides"
 fi
 
-HYPHEN_SYMBOL='-'
+LOGFILE_ARG=""
 
 case "${1}" in
   worker)
     echo "Starting Celery worker..."
+    if [ -n "$WORKER_LOG_FILE" ]; then
+      LOGFILE_ARG="--logfile=$WORKER_LOG_FILE"
+    fi
     # setting up only 2 workers by default to contain memory usage in dev environments
-    celery --app=superset.tasks.celery_app:app worker -O fair -l INFO --concurrency=${CELERYD_CONCURRENCY:-2} --logfile="${WORKER_LOG_FILE:-$HYPHEN_SYMBOL}"
+    celery --app=superset.tasks.celery_app:app worker -O fair -l INFO --concurrency=${CELERYD_CONCURRENCY:-2} ${LOGFILE_ARG}
     ;;
   beat)
     echo "Starting Celery beat..."
+    if [ -n "$BEAT_LOG_FILE" ]; then
+      LOGFILE_ARG="--logfile=$BEAT_LOG_FILE"
+    fi
     rm -f /tmp/celerybeat.pid
-    celery --app=superset.tasks.celery_app:app beat --pidfile /tmp/celerybeat.pid -l INFO -s "${SUPERSET_HOME}"/celerybeat-schedule --logfile="${BEAT_LOG_FILE:-$HYPHEN_SYMBOL}"
+    celery --app=superset.tasks.celery_app:app beat --pidfile /tmp/celerybeat.pid -l INFO -s "${SUPERSET_HOME}"/celerybeat-schedule ${LOGFILE_ARG}
     ;;
   app)
     echo "Starting web app (using development server)..."
