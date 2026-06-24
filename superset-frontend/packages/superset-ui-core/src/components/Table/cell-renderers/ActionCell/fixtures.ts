6cb7d3f876c8a5d4223e7313c59e47a89c095ae2
tomerkl65 <kleinfeld.tomer@gmail.com>
2026-06-24 19:58:55 +0300

    fix(storybook): use addon-actions import path

--- superset-frontend/packages/superset-ui-core/src/components/Table/cell-renderers/ActionCell/fixtures.ts
+++ superset-frontend/packages/superset-ui-core/src/components/Table/cell-renderers/ActionCell/fixtures.ts
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import { action } from 'storybook/actions';
+import { action } from '@storybook/addon-actions';
 import { ActionMenuItem } from './index';
 
 export const exampleMenuOptions: ActionMenuItem[] = [
