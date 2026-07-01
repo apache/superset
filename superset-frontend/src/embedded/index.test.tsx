5a73c2ee2baffbc4f85de941f90a9f542f0b9d5b
Durgaprasad M L <durgaprasadmadikeri@gmail.com>
2026-07-01 23:40:21 +0530

    fix(embedded): resolve ag-grid ci issues

--- superset-frontend/src/embedded/index.test.tsx
+++ superset-frontend/src/embedded/index.test.tsx
@@ -1,15 +1,52 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 import { setupAGGridModules } from '@superset-ui/core/components/ThemedAgGridReact';
+
+jest.mock('src/public-path', () => ({}));
+
+jest.mock('query-string', () => ({}));
 
 jest.mock('@superset-ui/core/components/ThemedAgGridReact', () => ({
   setupAGGridModules: jest.fn(),
 }));
+
 jest.mock('src/preamble', () => jest.fn().mockResolvedValue(true));
+
 jest.mock('src/setup/setupPlugins', () => jest.fn(), { virtual: true });
+
 jest.mock('src/setup/setupCodeOverrides', () => jest.fn(), { virtual: true });
+
 jest.mock('src/utils/getBootstrapData', () => ({
   __esModule: true,
-  default: () => ({ embedded: { dashboard_id: '123' } }),
+  default: () => ({
+    embedded: { dashboard_id: '123' },
+    common: {
+      application_root: '/',
+      static_assets_prefix: '/',
+      conf: {
+        SQLLAB_DEFAULT_DBID: 1,
+        DEFAULT_SQLLAB_LIMIT: 1000,
+      },
+    },
+  }),
   applicationRoot: () => '/',
+  staticAssetsPrefix: () => '/',
 }));
 
 describe('embedded/index.tsx', () => {
@@ -17,7 +54,7 @@ describe('embedded/index.tsx', () => {
     document.body.innerHTML = '<div id="app"></div>';
   });
 
-  it('initializes AG Grid modules on bootstrap', async () => {
+  test('initializes AG Grid modules on bootstrap', async () => {
     require('./index');
 
     // index.tsx uses initPreamble().then(...) to initialize plugins and AG grid
