114f7cb92ed49f6be0412b1733754bf91a7ff3d1
SBIN2010 <SBIN2010@mail.ru>
2026-07-08 23:10:43 +0300

    fix: pre-commit

--- superset-frontend/plugins/plugin-chart-echarts/src/Pie/transformProps.ts
+++ superset-frontend/plugins/plugin-chart-echarts/src/Pie/transformProps.ts
@@ -84,7 +84,7 @@ const HALF_DONUT_START_ANGLE = 180;
 export const isHalfDonut = (
   sweptAngle: number,
   startAngle: number,
-): boolean => 
+): boolean =>
   (
     sweptAngle <= HALF_DONUT_SWEEP_LIMIT &&
     startAngle === HALF_DONUT_START_ANGLE
