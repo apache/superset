9e3a1db8eba9a0d7848ccc23a49f9ff7f23fa0b4
Mike Bridge <michael.bridge@ext.preset.io>
2026-07-13 10:53:42 -0600

    chore(version-history): refresh generated feature flag docs

--- superset-frontend/src/explore/components/ExploreViewContainer/index.tsx
+++ superset-frontend/src/explore/components/ExploreViewContainer/index.tsx
@@ -1098,12 +1098,14 @@ function ExploreViewContainer(props: ExploreViewContainerProps) {
             aria-disabled={isChartVersionPreviewActive}
             css={css`
               height: 100%;
-              ${isChartVersionPreviewActive
-                ? `
+              ${
+                isChartVersionPreviewActive
+                  ? `
                   pointer-events: none;
                   opacity: 0.5;
                 `
-                : ''}
+                  : ''
+              }
             `}
             {...(isChartVersionPreviewActive ? { inert: '' } : {})}
           >
@@ -1166,12 +1168,14 @@ function ExploreViewContainer(props: ExploreViewContainerProps) {
             aria-disabled={isChartVersionPreviewActive}
             css={css`
               height: 100%;
-              ${isChartVersionPreviewActive
-                ? `
+              ${
+                isChartVersionPreviewActive
+                  ? `
                   pointer-events: none;
                   opacity: 0.5;
                 `
-                : ''}
+                  : ''
+              }
             `}
             // inert blocks keyboard focus too; React 18 needs the spread form
             {...(isChartVersionPreviewActive ? { inert: '' } : {})}
