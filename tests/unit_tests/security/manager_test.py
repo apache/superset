e54e3ca8832a54fa6dc37177f976b28c17cc03cf
jayvenn21 <jvennamreddy@gmail.com>
2026-07-14 11:13:35 -0400

    annotation fixes

--- tests/unit_tests/security/manager_test.py
+++ tests/unit_tests/security/manager_test.py
@@ -20,7 +20,7 @@
 import json  # noqa: TID251
 import logging
 from types import SimpleNamespace
-from typing import Any, Optional
+from typing import Any, Callable, Optional
 from unittest.mock import MagicMock
 
 import pytest
@@ -3334,7 +3334,7 @@ def test_skip_legacy_fab_password_view_registration_keeps_forced_change_target(
     )
     current_app.config["ENABLE_FORCE_PASSWORD_CHANGE"] = enable_force_password_change
 
-    original_add_view_no_menu = fake_appbuilder.add_view_no_menu
+    original_add_view_no_menu: Callable[..., Any] = fake_appbuilder.add_view_no_menu
     try:
         original_add_view_no_menu = sm._skip_legacy_fab_password_view_registration()
 
