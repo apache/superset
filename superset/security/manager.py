5d49d86d215077f1f5e1a24fb67f9423935421ca
jayvenn21 <jvennamreddy@gmail.com>
2026-07-14 11:59:41 -0400

    fix(auth): preserve forced password change reset route

--- superset/security/manager.py
+++ superset/security/manager.py
@@ -5068,7 +5068,7 @@ class SupersetSecurityManager(  # pylint: disable=too-many-public-methods
                 "/roles",
                 "/users",
                 "/groups",
-                "registrations",
+                "/registrations",
             ]:
                 self.appbuilder.baseviews.remove(view)
 
