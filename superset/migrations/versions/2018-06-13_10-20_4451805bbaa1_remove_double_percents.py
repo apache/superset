b0bb20fec58e6b08c0653fff03deb0fbe72fae71
Hans Yu <hans.yu@outlook.de>
2026-07-21 21:40:08 +0200

    fix: frame session transaction instead of explicit commit

--- superset/migrations/versions/2018-06-13_10-20_4451805bbaa1_remove_double_percents.py
+++ superset/migrations/versions/2018-06-13_10-20_4451805bbaa1_remove_double_percents.py
@@ -68,37 +68,33 @@ class Database(Base):
 
 
 def replace(source, target):
-    bind = op.get_bind()
-    session = db.Session(bind=bind)
+    with db.Session(bind=op.get_bind()) as session:
+        with session.begin():
+            query = (
+                session.query(Slice, Database)
+                .join(Table, Slice.datasource_id == Table.id)
+                .join(Database, Table.database_id == Database.id)
+                .filter(Slice.datasource_type == "table")
+                .all()
+            )
 
-    query = (
-        session.query(Slice, Database)
-        .join(Table, Slice.datasource_id == Table.id)
-        .join(Database, Table.database_id == Database.id)
-        .filter(Slice.datasource_type == "table")
-        .all()
-    )
+            for slc, database in query:
+                try:
+                    engine = create_engine(database.sqlalchemy_uri, future=True)
 
-    for slc, database in query:
-        try:
-            engine = create_engine(database.sqlalchemy_uri, future=True)
+                    if engine.dialect.identifier_preparer._double_percents:
+                        params = json.loads(slc.params)
 
-            if engine.dialect.identifier_preparer._double_percents:
-                params = json.loads(slc.params)
+                        if "adhoc_filters" in params:
+                            for filt in params["adhoc_filters"]:
+                                if "sqlExpression" in filt:
+                                    filt["sqlExpression"] = filt[
+                                        "sqlExpression"
+                                    ].replace(source, target)
 
-                if "adhoc_filters" in params:
-                    for filt in params["adhoc_filters"]:
-                        if "sqlExpression" in filt:
-                            filt["sqlExpression"] = filt["sqlExpression"].replace(
-                                source, target
-                            )
-
-                    slc.params = json.dumps(params, sort_keys=True)
-        except Exception:  # noqa: S110
-            pass
-
-    session.commit()
-    session.close()
+                            slc.params = json.dumps(params, sort_keys=True)
+                except Exception:  # noqa: S110
+                    pass
 
 
 def upgrade():
