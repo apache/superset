72278c96fcb421b0d1d05ce968e1e9b472f6665f
hainenber <dotronghai96@gmail.com>
2026-08-02 17:22:24 +0700

    chore: format newly added codes

--- superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/transformProps.ts
+++ superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/transformProps.ts
@@ -625,7 +625,8 @@ export default function transformProps(
     }
 
     let symbolSizeFn:
-      ((value: (number | string | null)[]) => number) | undefined;
+      | ((value: (number | string | null)[]) => number)
+      | undefined;
     if (sizeExtent) {
       const extent = sizeExtent;
       const sizeLookup = sizeLookups?.get(getSeriesDimsKey(entryName));
