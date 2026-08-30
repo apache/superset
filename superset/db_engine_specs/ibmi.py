552c5d89cf318da5d866fa6ff91410ed404ae3f6
FrancescoCastaldi <info@francescocastaldi.it>
2026-08-30 22:19:53 +0200

    feat(db_engine_specs): add metadata block to IBMiEngineSpec

--- superset/db_engine_specs/ibmi.py
+++ superset/db_engine_specs/ibmi.py
@@ -14,19 +14,41 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+from superset.db_engine_specs.base import DatabaseCategory
 from .db2 import Db2EngineSpec
 
 
 class IBMiEngineSpec(Db2EngineSpec):
-    """IBM Db2 for i (AS/400) engine spec.
-
-    Note: Documentation is in Db2EngineSpec's compatible_databases section.
-    This spec exists for runtime support of the ibmi driver.
-    """
+    """IBM Db2 for i (AS/400) engine spec."""
 
     engine = "ibmi"
     engine_name = "IBM Db2 for i"
     max_column_name_length = 128
+
+    metadata = {
+        "description": (
+            "IBM Db2 for i (formerly AS/400) is an integrated relational database "
+            "engine on IBM Power systems running IBM i."
+        ),
+        "logo": "ibm-db2.svg",
+        "homepage_url": "https://www.ibm.com/products/db2-for-i",
+        "categories": [
+            DatabaseCategory.TRADITIONAL_RDBMS,
+            DatabaseCategory.PROPRIETARY,
+        ],
+        "pypi_packages": ["sqlalchemy-ibmi"],
+        "connection_string": "ibmi://{username}:{password}@{host}:{port}/{database}",
+        "default_port": 50000,
+        "parameters": {
+            "username": "IBM i username",
+            "password": "IBM i password",
+            "host": "IBM i system host",
+            "port": "Default 50000",
+            "database": "Library/schema name",
+        },
+        "docs_url": "https://github.com/IBM/sqlalchemy-ibmi",
+        "sqlalchemy_docs_url": "https://github.com/IBM/sqlalchemy-ibmi",
+    }
 
     @classmethod
     def epoch_to_dttm(cls) -> str:
