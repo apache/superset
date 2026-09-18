b4b4156c74d5a6d017ce2496ee467011ff2393b2
Vihar N <vihar.n@digitalblanket.ai>
2026-09-18 14:52:29 +0530

    test(plugin-chart-echarts): fix no-proto lint rule in series test

--- superset-frontend/plugins/plugin-chart-echarts/test/utils/series.test.ts
+++ superset-frontend/plugins/plugin-chart-echarts/test/utils/series.test.ts
@@ -898,7 +898,9 @@ describe('extractShowValueIndexes', () => {
 
     expect(Object.prototype.hasOwnProperty.call(result, '__proto__')).toBe(true);
     expect(Object.prototype.hasOwnProperty.call(result, 'constructor')).toBe(true);
-    expect(result['__proto__']).toEqual([0, 0]);
+    expect(
+      Object.getOwnPropertyDescriptor(result, '__proto__')?.value,
+    ).toEqual([0, 0]);
     expect(result['constructor']).toEqual([1, 1]);
   });
 });
