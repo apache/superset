246d5c30c9ec91f0d44a031c1f672dff4cb56f50
Jay Masiwal <masiwaljay.02@gmail.com>
2026-09-22 07:17:26 +0000

    fix: remove duplicate import and restore license header

--- superset-frontend/src/dashboard/actions/dashboardState.ts
+++ superset-frontend/src/dashboard/actions/dashboardState.ts
@@ -1,4 +1,3 @@
-import { getClientErrorObject } from "src/utils/getClientErrorObject";
 /**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
